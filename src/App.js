796f5c6707eb907d4d37e15af08e33889601c38f
전소희 <indian_potato@naver.com>
2023-11-02 21:34:04 +0900

    feat: 구입금액 입력받기
     MissionUtils의 readLineAsync 메서드를 통해 유저의 콘솔에 '구입 금액을 입력해 주세요.' 라는 문구를 출력하며 구입 금액을 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const enterCost = await MissionUtils.Console.readLineAsync(
+      "구입 금액을 입력해 주세요. \n"
+    );
+  }
 }
 
 export default App;
