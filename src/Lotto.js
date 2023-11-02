e5ff2debaba74281686b6adeedeaf455d0d5ef24
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-11-02 16:51:21 +0900

    feat: check number function
    duplicate number check Function

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,15 +10,15 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    let numberSet = new Set(numbers);
+    const numberSet = new Set(numbers);
     if (numberSet.size !== numbers.length) {
       throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
-    for (let number of numbers) {
+    numbers.forEach((number) => {
       if (number < 1 || number > 45) {
         throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 값이어야 합니다.");
       }
-    }
+    });
   }
 
   get numbers() {
