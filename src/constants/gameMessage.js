69bf1289a31adeee75191c23692978f02d98cbc4
Hyurim <kyo2301@naver.com>
2023-11-02 17:41:43 +0900

    feat(constants/gameMessage.js): 게임 메시지 상수 파일 생성

---
+++ src/constants/gameMessage.js
@@ -0,0 +1,18 @@
+const GAME_MESSAGE = Object.freeze({
+    buyMoney: '구입금액을 입력해주세요.\n',
+    bought: '8개를 구매했습니다.\n',
+    winningNumber: '당첨 번호를 입력해 주세요.\n',
+    bonusNumber: '보너스 번호를 입력해 주세요.\n',
+    result: '당첨 통계\n',
+    hyphon: '---\n',
+    threeMatching: '3개 일치 (5,000원) - ',
+    fourMatching: '4개 일치 (50,000원) - ',
+    fiveMatching: '5개 일치 (1,500,000원) - ',
+    bonusFiveMatching: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+    sixMatching: '6개 일치 (2,000,000,000원) - ',
+    piece: '개',
+    moneyReturn: '총 수익률은 ',
+    textEnd: '입니다.',
+});
+
+export default GAME_MESSAGE;
