b332cb3c501e8bc8a1501ac7f1584103c6737403
JiSeong Kim <12180543@inha.edu>
2023-11-02 17:12:46 +0900

    docs: add README.md in docs folder

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,16 +1,16 @@
-class Lotto {
-  #numbers;
+class Lotto	{
+	#numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
+	constructor(numbers) {
+		this.#validate(numbers);
+		this.#numbers = numbers;
+	}
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
+	#validate(numbers) {
+		if (numbers.length !== 6) {
+			throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+		}
+	}
 
   // TODO: 추가 기능 구현
 }
