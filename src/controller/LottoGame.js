82af8e1a638f3be59de959865820807fcd75c342
yui880 <kimyuna521@gmail.com>
2023-11-02 18:22:05 +0900

    feat(LottoGame): 로또 개수 구하는 기능 구현

--- src/controller/LottoGame.js
+++ src/controller/LottoGame.js
@@ -1,19 +1,27 @@
 import InputView from '../view/InputView.js';
 import AmountValidator from '../validator/AmountValidator.js';
+import { CONSTANT } from '../constants/Constant.js';
 
 class LottoGame {
   async startGame() {
     const amount = await this.#getAmountInput();
+    const lottoCount = this.#getLottoCount(amount);
   }
 
   async #getAmountInput() {
     const amount = await InputView.inputAmount();
     this.#validateAmountInput(amount);
+
+    return amount;
   }
 
   #validateAmountInput(amount) {
     AmountValidator.checkIsNegative(amount);
     AmountValidator.checkIsNotInUnit(amount);
+  }
+
+  #getLottoCount(amount) {
+    return amount / CONSTANT.amountUnit;
   }
 }
 
