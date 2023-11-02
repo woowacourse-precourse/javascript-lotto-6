7252a0ee19b12f9512c06ddc0ed05d175fd8cf3b
thumbthing <ilscc0902@gmail.com>
2023-11-03 03:19:39 +0900

    feat: Lotto class에 중복 검사 기능 적용

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import checkHasDuplicate from './util/validate/checkHasDuplicate';
+
 class Lotto {
   #numbers;
 
@@ -8,8 +10,9 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    checkHasDuplicate(numbers);
   }
 
   // TODO: 추가 기능 구현
