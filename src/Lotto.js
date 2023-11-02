6d5966184d00021c4b4a709590164ec951746fdf
Steadystudy <sgmin206@naver.com>
2023-11-02 22:33:55 +0900

    Feat: Lotto 클래스에서 당첨 번호와 비교해서 값을 리턴하기

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { PRIZE } from './constant.js';
+
 class Lotto {
   #numbers;
 
@@ -14,6 +16,27 @@ class Lotto {
 
   getNumbers() {
     return this.#numbers.sort((a, b) => a - b); // 오름차순 정렬
+  }
+
+  getIntersection(winningLotteryNumbers) {
+    return winningLotteryNumbers.filter((number) => this.#numbers.includes(number)).length;
+  }
+
+  compareNumbersOf(winningLotteryNumbers, bonusNumber) {
+    const intersection = this.getIntersection(winningLotteryNumbers);
+
+    switch (intersection) {
+      case 3:
+        return PRIZE.FIFTH;
+      case 4:
+        return PRIZE.FOURTH;
+      case 5:
+        return this.#numbers.includes(bonusNumber) ? PRIZE.SECOND : PRIZE.THIRD;
+      case 6:
+        return PRIZE.FIRST;
+      default:
+        return null;
+    }
   }
 }
 
