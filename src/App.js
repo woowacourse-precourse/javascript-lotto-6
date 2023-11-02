8fb636313f4ec6582c0bb481480bb0c20e71ce94
Elisha0510 <sjeong35@knu.ac.kr>
2023-11-03 01:22:49 +0900

    feat: 6개의 랜덤값을 담은 배열을 만드는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
+  constructor() {
+    this.lottoRandomNumber = [];
+    this.lottoTicket;
+    this.countNumber = 0;
+  }
+
   getLottoTicket(lottoPurchaseAmount) {
     return Number(lottoPurchaseAmount / 1000);
   }
@@ -11,12 +17,20 @@ class App {
     if (isNaN(lottoPrice)) throw new Error('[ERROR] 숫자만 입력하세요.');
   }
 
+  makeRandomNumber() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
   async play() {
     const lottoPrice =
       await Console.readLineAsync('구입 금액을 입력해 주세요.\n');
     this.checkLottoPrice(lottoPrice);
-    const lottoTicket = this.getLottoTicket(lottoPrice);
+    this.lottoTicket = this.getLottoTicket(lottoPrice);
     Console.print(`${lottoTicket}개를 구매했습니다.`);
+
+    while (this.countNumber < this.lottoTicket) {
+      randomNumberArray = this.makeRandomNumber();
+    }
   }
 }
 
