1c0082a4b1baa0bebbbfa05574cf95e8fdd31f7f
dyddks <dyddks0115@naver.com>
2023-11-03 01:57:29 +0900

    feat: 사용자 입력과 출력 처리

---
+++ src/View/View.js
@@ -0,0 +1,48 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class View {
+  #price;
+  #winningNumber;
+  #bonusNumber;
+  
+  async inputPrice() {
+    this.#price = await Console.readLineAsync('구매금액을 입력해 주세요.\n');
+    return this.#price;
+  }
+  
+  inputWinningNumber() {
+    this.#winningNumber = Number((Console.readLineAsync('당첨 번호를 입력해 주세요.')).split(','));
+    return this.#winningNumber;
+  }
+
+  inputBonus() {
+    this.#bonusNumber = Number(Console.readLineAsync('보너스 번호를 입력해 주세요.'));
+    return this.#bonusNumber;
+  }
+
+  printQuantity(quantity) {
+    Console.print(`\n${quantity}개를 구매했습니다.`);
+  }
+  
+  printLottoNumber(lottoNumber) {
+    for (let i=0; i<lottoNumber.length; i+=1) {
+      Console.print(lottoNumber[i]);
+    }
+    Console.print('');
+  }
+
+  printResult(result) {
+    Console.print('당첨 통계');
+    Console.print(`3개 일치 (5,000원) - ${result[0]}개`);
+    Console.print(`4개 일치 (50,000원) - ${result[1]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${result[2]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[3]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${result[4]}개`);
+  }
+  
+  printROI(roi) {
+    Console.print(`총 수익률은 ${roi}%입니다.`);
+  }
+}
+
+export default View;
