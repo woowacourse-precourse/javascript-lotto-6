90ca995f62bc704d016588cfbc627efdfae70b7a
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-02 21:35:28 +0900

    feat(빈 줄): 빈 줄 상수화

--- src/functions/InputManager.js
+++ src/functions/InputManager.js
@@ -1,28 +1,28 @@
 import { Console } from "@woowacourse/mission-utils";
-import { INPUT_MESSAGE } from "../constants/messages.js";
+import { EMPTY_LINE, INPUT_MESSAGE } from "../constants/messages.js";
 
 // 입력받은 값에 대한 검증 및 가공 처리
 class InputManager {
   async moneyForLotto() {
     Console.print(INPUT_MESSAGE.moneyForLotto);
-    const moneyForLotto = await Console.readLineAsync("");
-    Console.print("");
+    const moneyForLotto = await Console.readLineAsync(EMPTY_LINE);
+    Console.print(EMPTY_LINE);
 
     return moneyForLotto;
   }
 
   async winningBallNumbers() {
     Console.print(INPUT_MESSAGE.winningBalls);
-    const winningBalls = await Console.readLineAsync("");
-    Console.print("");
+    const winningBalls = await Console.readLineAsync(EMPTY_LINE);
+    Console.print(EMPTY_LINE);
 
     return winningBalls;
   }
 
   async bonusBallNumbers() {
     Console.print(INPUT_MESSAGE.bonusBall);
-    const bonusBall = await Console.readLineAsync("");
-    Console.print("");
+    const bonusBall = await Console.readLineAsync(EMPTY_LINE);
+    Console.print(EMPTY_LINE);
 
     return bonusBall;
   }
