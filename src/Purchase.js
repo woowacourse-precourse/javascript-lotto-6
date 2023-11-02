f7dc32b7ddb1ad6296d3944a3cbc98e6c4f294aa
skdltn210 <rhdmstn202@gmail.com>
2023-11-02 17:53:22 +0900

    feat: 로또 구입 금액과 발행할 수량을 처리하는 Purchase 객체 생성

---
+++ src/Purchase.js
@@ -0,0 +1,21 @@
+import { INPUT, ERROR } from "Constants/Constants.js";
+import { Console } from "@woowacourse/mission-utils";
+
+class Purchase {
+  #purchaseAmount;
+  #lottoCount;
+  constructor(purchaseAmount) {
+    this.#validate(purchaseAmount);
+    this.#purchaseAmount = purchaseAmount;
+    this.#lottoCount = purchaseAmount / 1000;
+  }
+  #validate(purchaseAmount) {
+    if (
+      typeof purchaseAmount !== "number" ||
+      purchaseAmount <= 0 ||
+      purchaseAmount % 1000 !== 0
+    ) {
+      throw new Error(ERROR.purchase_amount_error);
+    }
+  }
+}
