b668a1f2dc35397ef16493ef5326305642964f1d
yui880 <kimyuna521@gmail.com>
2023-11-02 18:47:03 +0900

    feat(Lotto): 로또번호 오름차순으로 정렬하는 기능 구현

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -3,16 +3,18 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = this.#sortNumbers(numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  #sortNumbers(numbers) {
+    return numbers.sort((first, second) => first - second);
+  }
 }
 
 export default Lotto;
