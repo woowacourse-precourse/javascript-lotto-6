fa1fd81de53b0b4e228145df4ef61b19b4e38d48
PARK-CHANUK <doosan1224@naver.com>
2023-11-02 22:40:00 +0900

    feat: 예외처리한 메시지를 콘솔에 출력하고 구입금액을 다시 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import InputValidator from './utils/InputValidator.js';
+import InputView from './view/InputView.js';
+import OutputView from './view/OutputView.js';
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = this.#getPurchaseAmount();
+  }
+
+  async #getPurchaseAmount() {
+    try {
+      const answer = await InputView.getLottoPurchaseAmount();
+      if (InputValidator.validatePurchaseAmount(answer)) {
+        return answer;
+      }
+    } catch (error) {
+      OutputView.printError(error.message);
+      this.#getPurchaseAmount();
+    }
+  }
 }
 
 export default App;
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
   async getLottoPurchaseAmount() {
-    const answer = await Console.readLineAsync('구입금액을 입력해 주세요.');
+    const answer = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     return answer;
   },
 };
