aa8adf06c651ef14c6ff6e7b35309ad16aaf868d
JiSeong Kim <12180543@inha.edu>
2023-11-02 18:05:48 +0900

    feat(util): add thirdPlace method in CheckLotto
    CheckLotto 클래스 안에 3등 당첨을 체크하는 thirdPlace 메소드 추가

--- src/util/CheckLotto.js
+++ src/util/CheckLotto.js
@@ -22,6 +22,17 @@ class CheckLotto {
 
 		return correctCount === lottoNumber.fourthPlaceCount;
 	}
+
+	static thirdPlace(lottoNumberList, winningNumberList) {
+		const correctCount = lottoNumberList.reduce((count, lottoNumber) => {
+			if (winningNumberList.includes(lottoNumber)) {
+				return count + 1;
+			}
+			return count;
+		}, 0);
+
+		return correctCount === lottoNumber.thirdPlaceCount;
+	}
 }
 
 export default CheckLotto;
