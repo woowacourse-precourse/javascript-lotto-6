b90ac94faeee0b7b85b50ec1e65db336a68a1df3
JiSeong Kim <12180543@inha.edu>
2023-11-02 17:36:34 +0900

    feat(constants): add errorMessage
    - NOT_NUMBER: 숫자가 입력될 곳에 문자가 입력
    - INVALID_PURCHASE_AMOUNT: 1000원으로 나눠지지 않는 금액이 입력
    - NOT_ENOUGH_PURCHASE_AMOUNT: 1000원보다 적은 금액이 입력
    - DUPLICATED_NUMBER: 당첨 번호 입력에 중복 숫자 존재
    - OUT_OF_RANGE: 1~45의 범위를 벗어난 숫자 존재
    - ALREADY_PICKED_NUMBER: 당첨번호로 선택된 수가 보너스 숫자로 선택

---
+++ src/constants/errorMessage.js
@@ -0,0 +1,10 @@
+const errorMessage = Object.freeze({
+	NOT_NUMBER: '문자가 아닌 숫자를 입력해주세요.',
+	INVALID_PURCHASE_AMOUNT: '1000원 단위의 금액을 입력해주세요.',
+	NOT_ENOUGH_PURCHASE_AMOUNT: '금액이 부족합니다.',
+	DUPLICATED_NUMBER: '중복되지 않은 숫자를 입력해주세요.',
+	OUT_OF_RANGE: '1부터 45사이의 숫자만 입력해주세요.',
+	ALREADY_PICKED_NUMBER: '당첨 번호로 선택하지 않은 수 중에 입력해주세요.'
+});
+
+export default errorMessage;
