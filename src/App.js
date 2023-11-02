e1a4efc00b73e8e1ec09e9e92f64010ef9cb6835
jbilee <julishahn@gmail.com>
2023-11-02 17:19:44 +0900

    feat: 금액 입력 기능 구현 - 사용자로부터 로또 구입 금액을 입력 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await this.getPurchaseAmount();
+  }
+  
+  async getPurchaseAmount() {
+    const input = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    return input;
+  }
 }
 
 export default App;
