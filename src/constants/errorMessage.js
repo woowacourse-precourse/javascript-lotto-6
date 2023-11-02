802cca4492a75446cc21a56a8f9a05e9bc7cba60
JiSeong Kim <12180543@inha.edu>
2023-11-03 01:51:31 +0900

    fix(constants): modify Message in errorMessage
    각각의 오류메시지 앞에 '[ERROR]'를 추가하여 형식을 맞춤

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,10 +1,10 @@
 const errorMessage = Object.freeze({
-	NOT_NUMBER: '문자가 아닌 숫자를 입력해주세요.',
-	INVALID_PURCHASE_AMOUNT: '1000원 단위의 금액을 입력해주세요.',
-	NOT_ENOUGH_PURCHASE_AMOUNT: '금액이 부족합니다.',
-	DUPLICATED_NUMBER: '중복되지 않은 숫자를 입력해주세요.',
-	OUT_OF_RANGE: '1부터 45사이의 숫자만 입력해주세요.',
-	ALREADY_PICKED_NUMBER: '당첨 번호로 선택하지 않은 수 중에 입력해주세요.'
+	NOT_NUMBER: '[ERROR] 문자가 아닌 숫자를 입력해주세요.',
+	INVALID_PURCHASE_AMOUNT: '[ERROR] 1000원 단위의 금액을 입력해주세요.',
+	NOT_ENOUGH_PURCHASE_AMOUNT: '[ERROR] 금액이 부족합니다.',
+	DUPLICATED_NUMBER: '[ERROR] 중복되지 않은 숫자를 입력해주세요.',
+	OUT_OF_RANGE: '[ERROR] 1부터 45사이의 숫자만 입력해주세요.',
+	ALREADY_PICKED_NUMBER: '[ERROR] 당첨 번호로 선택하지 않은 수 중에 입력해주세요.'
 });
 
 export default errorMessage;
