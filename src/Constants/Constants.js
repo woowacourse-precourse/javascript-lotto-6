02f7a8691144fce80b7a586483d3ae124c18d4e8
skdltn210 <rhdmstn202@gmail.com>
2023-11-02 17:30:34 +0900

    feat: 상수를 저장하는 Constants 객체 생성

---
+++ src/Constants/Constants.js
@@ -0,0 +1,29 @@
+export const INPUT = {
+  purchase_amount: "구입금액을 입력해 주세요.\n",
+  guess_numbers: "당첨 번호를 입력해 주세요.",
+  bonus_numbers: "보너스 번호를 입력해 주세요.",
+};
+export const OPTION = {
+  max_name_length: 5,
+  min_random_num: 1,
+  max_random_num: 45,
+  lotto_num_count: 6,
+  bonus_num_count: 1,
+};
+export const OUTPUT = {
+  linebreak: "\n",
+  how_many_purchased: "개를 구매했습니다.",
+  correct_3: "3개 일치 (5,000원) - ",
+  correct_4: "4개 일치 (50,000원) - ",
+  correct_5: "5개 일치 (1,500,000원) - ",
+  correct_5_bouns: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  correct_6: "6개 일치 (2,000,000,000원) - ",
+  count: "개",
+  statics: "당첨 통계\n---",
+  rate_of_return: "총 수익률은 ",
+  percent_is: "%입니다.",
+};
+export const ERROR = {
+  lotto_count_error: "[ERROR] 로또 번호는 6개여야 합니다.",
+  lotto_num_error: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+};
