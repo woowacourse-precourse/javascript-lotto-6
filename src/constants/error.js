70411e96faceb2abff4c3a84ad2182d7ef136b4c
Goongam <dlwjddn341@naver.com>
2023-11-03 03:49:21 +0900

    Feat: 에러 메시지 상수 정의

---
+++ src/constants/error.js
@@ -0,0 +1,5 @@
+export const ERROE_MESSAGE = {
+    MONEY_ISNAN: '[ERROR] 1000단위의 숫자를 입력해주세요.',
+    MONEY_UNIT_1000: '[ERROR] 1000단위로 입력해 주세요',
+    MONEY_UPPER_1000: '[ERROR] 1000이상 숫자를 입력해주세요',
+}
