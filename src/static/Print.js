fb257032952c5ca5694ec25ba05961d2888da1de
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-02 21:53:46 +0900

    feat: 수익률 계산에 사용되는 배열 상수화

--- src/static/Print.js
+++ src/static/Print.js
@@ -1,4 +1,4 @@
-const Print = Object.freeze({
+const PRINT = Object.freeze({
   purchase: "구입금액을 입력해 주세요.",
   purchaseCount: `\n${count}개를 구매했습니다.`,
   winningNumber: "\n당첨 번호를 입력해 주세요.",
@@ -7,4 +7,4 @@ const Print = Object.freeze({
   resultRate: `총 수익률은 ${rate}%입니다.`,
 });
 
-export default Print;
+export default PRINT;
