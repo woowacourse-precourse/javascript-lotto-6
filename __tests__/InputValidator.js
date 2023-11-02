d93e5f7bb236e1bff8e8803aea52654b90308cdd
PARK-CHANUK <doosan1224@naver.com>
2023-11-02 22:22:24 +0900

    test(InputValidatorTest): 테스트 코드 작성 case1: 구입금액이 1000원으로 나누어떨어지면 로또 구입가능 case2: 구입금액이 숫자가 아니면 예외처리 case3: 구입금액이 1000원으로 나누어떨어지지 않으면 예외처리

---
+++ __tests__/InputValidator.js
@@ -0,0 +1,32 @@
+import InputValidator from '../src/utils/InputValidator';
+
+describe('로또 구입금액 유효성 검사 테스트', () => {
+  test.each(['1000', '2000', '3000'])(
+    '1000원으로 나누어 떨어지면 로또를 구입할 수 있습니다.',
+    (input) => {
+      expect(() => {
+        InputValidator.validatePurchaseAmount(input);
+      }).not.toThrow();
+    },
+  );
+
+  test.each(['', ' ', 'a', '1000f'])(
+    '숫자를 입력하지 않으면 예외처리',
+    (input) => {
+      expect(() => {
+        InputValidator.validatePurchaseAmount(input);
+      }).toThrow('[ERROR] 숫자만 입력해주세요.');
+    },
+  );
+
+  test.each(['0', '1001', '2500', '3500', '8900'])(
+    '1000원으로 나누어 떨어지지 않으면 예외처리',
+    (input) => {
+      expect(() => {
+        InputValidator.validatePurchaseAmount(input);
+      }).toThrow(
+        '[ERROR] 입력한 금액이 1000원으로 나누어떨어지지 않습니다. 다시 입력해주세요.',
+      );
+    },
+  );
+});
