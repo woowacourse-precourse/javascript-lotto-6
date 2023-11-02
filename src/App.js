6054502dfeb9b59d5ce0e67d70d5dff7a80d42fa
ktyoung <ktyoung0507@gmail.com>
2023-11-02 17:18:42 +0900

    feat(results): 당첨 내역 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -16,20 +16,52 @@ class App {
       lottos.push(lotto);
     }
 
-    Console.print(`${lottoCount}개를 구매했습니다.`);
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
     lottos.forEach((lotto) => {
       Console.print(`[${lotto.getNumbers().join(", ")}]`);
     });
 
     const winningNumbersInput = await Console.readLineAsync(
-      "당첨 번호를 입력해 주세요. \n"
+      "\n당첨 번호를 입력해 주세요.\n"
     );
     const winningNumbers = winningNumbersInput
       .split(",")
       .map((number) => parseInt(number.trim()));
-    const bonusNumbers = await Console.readLineAsync(
-      "보너스 번호를 입력해 주세요. \n"
+    const bonusNumberInput = await Console.readLineAsync(
+      "\n보너스 번호를 입력해 주세요.\n"
     );
+    const bonusNumber = parseInt(bonusNumberInput.trim(), 10);
+
+    const results = lottos.map((lotto) => {
+      const numbers = lotto.getNumbers();
+      const matchCount = numbers.filter((number) =>
+        winningNumbers.includes(number)
+      ).length;
+      const bonusMatch = numbers.includes(bonusNumber);
+
+      if (matchCount === 6) return "6";
+      if (matchCount === 5 && bonusMatch) return "5+1";
+      if (matchCount === 5) return "5";
+      if (matchCount === 4) return "4";
+      if (matchCount === 3) return "3";
+      return "꽝";
+    });
+
+    const resultCounts = results.reduce((countByResult, result) => {
+      countByResult[result] = (countByResult[result] || 0) + 1;
+      return countByResult;
+    }, {});
+
+    const prizeMessages = [
+      `3개 일치 (5,000원) - ${resultCounts["3"] || 0}개`,
+      `4개 일치 (50,000원) - ${resultCounts["4"] || 0}개`,
+      `5개 일치 (1,500,000원) - ${resultCounts["5"] || 0}개`,
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${resultCounts["5+1"] || 0}개`,
+      `6개 일치 (2,000,000,000원) - ${resultCounts["6"] || 0}개`,
+    ];
+
+    Console.print("\n당첨 통계\n---");
+    prizeMessages.forEach((message) => Console.print(message));
   }
 }
 
