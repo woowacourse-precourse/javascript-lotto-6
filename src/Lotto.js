45b23374555d67b8424a11216f64a538d2d01928
Hyebin Lee <qnd1215@naver.com>
2023-11-02 21:30:52 +0900

    write README

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
@@ -8,7 +10,7 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
