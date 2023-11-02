7fe63c4c6ff85fbae4c676cac1d9102650a90da4
Hyuk <poiu03290@gmail.com>
2023-11-03 01:48:41 +0900

    refactor: update UserInput class

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import UserInput from './view/userInput.js';
-import Amount from './controller/Amount.js';
 import Purchase from './controller/Purchase.js';
-import Lotto from './controller/Lotto.js';
-import Bonus from './controller/Bonus.js';
 
 class App {
   constructor() {
@@ -11,21 +8,13 @@ class App {
 
   async play() {
     const amount = await this.UserInput.RequestAmount();
-    new Amount(amount);
+
     const purchase = new Purchase(amount);
     const arrays = purchase.public();
-    const numbers = await this.UserInput.RequestWinningNumbers();
-    new Lotto(numbers);
 
-    const bonusNumber = await this.UserInput.RequestBonus();
-    const bonus = new Bonus(bonusNumber, numbers);
-    
-    // this.lotto = new Lotto();
-    // this.lotto.calculateResult();
+    const numbers = await this.UserInput.RequestNumbers();
 
-    // const numbers = await this.UserInput.RequestWinningNumbers();
-    // this.lotto = new Lotto(numbers);
-    // this.lotto.RequestInput();
+    const bonus = await this.UserInput.RequestBonus(numbers);
   }
 }
 
--- src/view/UserInput.js
+++ src/view/UserInput.js
@@ -1,31 +1,36 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-import Validate from '../controller/Validate.js';
+import Amount from '../controller/Amount.js';
+import Lotto from '../controller/Lotto.js';
+import Bonus from '../controller/Bonus.js';
+
 import { MESSAGE } from '../data/message.js';
 
 class UserInput {
     #amount;
 
     constructor() {
-        this.validate = new Validate();
         this.#amount = 0;
     }
     
     async RequestAmount() {
         this.#amount = await MissionUtils.Console.readLineAsync(`${MESSAGE.PURCHASE_AMOUNT}\n`);
+        new Amount(this.#amount);
 
         return this.#amount;
     }
 
-    async RequestWinningNumbers() {
+    async RequestNumbers() {
         const input = await MissionUtils.Console.readLineAsync(`\n${MESSAGE.WINNING_NUMBERS}\n`);
-        const winningNumbers = input.split(',');
+        const numbers = input.split(',');
+        new Lotto(numbers);
 
-        return winningNumbers;
+        return numbers;
     }
 
-    async RequestBonus() {
+    async RequestBonus(numbers) {
         const bonus = await MissionUtils.Console.readLineAsync(`\n${MESSAGE.BONUS_NUMBER}\n`);
+        new Bonus(bonus, numbers);
 
         return bonus;
     }
