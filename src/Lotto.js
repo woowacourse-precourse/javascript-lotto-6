d1948f1c04a21b80deb8681c806c2f2063fa2944
조예진 <joyejin@joyejins-MacBook-Air.local>
2023-11-02 23:34:18 +0900

    feat: 로또 번호 입력시 중복 예외, 1~45 범위 벗어낫을 때 예외 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,7 +8,19 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    // 중복 예외
+    if (new Set(numbers).sizs !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    }
+
+    // 1~45 범위 벗어낫을 때
+    for (const number of numbers) {
+      if (number > 45 || number < 1) {
+        throw new Error('[ERROR] 로또 번호는 1~45 사이로 입력해주세요.');
+      }
     }
   }
 
