ef141a3c27dfb2d1d6e4a5503f50dcbc7b0821da
김미르 <kimmireu@gimmileuui-iMac.local>
2023-11-02 16:17:41 +0900

    feat : 발행한 로또 번호 출력

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Lotto from "./Lotto";
 class App {
   async play() {
     const totalPrice = await getTotalPrice();
+
     const totalCount = parseInt(totalPrice / 1000);
     Console.print(`${totalCount}개를 구매했습니다.`);
+
+    const lottos = getLottos(totalCount);
+    printLottos(lottos);
   }
 }
 
@@ -20,6 +24,24 @@ const validateTotalPrice = (price) => {
   }
   if (Number.isNaN(price)) {
     throw new Error("[ERROR] 구입 금액은 숫자여야 합니다.");
+  }
+};
+
+const getLottos = (counts) => {
+  const lottos = [];
+  for (let count = 0; count < counts; count++) {
+    const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    numbers.sort((a, b) => a - b);
+    const lotto = new Lotto(numbers);
+    lottos.push(lotto);
+  }
+  return lottos;
+};
+
+const printLottos = (lottos) => {
+  for (let lotto of lottos) {
+    const numbers = lotto.getNumbers();
+    Console.print(numbers);
   }
 };
 
