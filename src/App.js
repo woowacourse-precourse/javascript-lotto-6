2de1498840b284157cabfee565aa4d983c616716
최진욱 <jinwook.fe@gmail.com>
2023-11-03 01:41:08 +0900

    [feat] 보너스 번호 입력 로직 추가

--- src/App.js
+++ src/App.js
@@ -59,6 +59,33 @@ class GetWinningNumber {
   }
 }
 
+class GetBonusNumber {
+  #bonusNumber;
+
+  constructor(winningNumber, bonusNumber) {
+    this.#bonusNumberValidateForm(bonusNumber);
+    this.#bonusNumberValidateDuplicate(winningNumber, bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  #bonusNumberValidateForm(bonusNumber) {
+    const regexNumber = /^(?:[1-9]|[1-3]\d|4[0-5])$/;
+    if (!regexNumber.test(bonusNumber)) {
+      throw new Error("[ERROR] 1~45 사이의 숫자 형식의 입력만 가능합니다.");
+    }
+  }
+
+  #bonusNumberValidateDuplicate(winningNumber, bonusNumber) {
+    if (winningNumber.includes(bonusNumber)) {
+      throw new Error("[ERROR] 보너스 번호는 당첨 번호와 같을 수 없습니다.");
+    }
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+}
+
 class App {
   async getPurchaseAmount() {
     try {
@@ -111,6 +138,22 @@ class App {
     }
   }
 
+  async getBonusNumber(winningNumber) {
+    try {
+      const bonusNumber = await MissionUtils.Console.readLineAsync(
+        "보너스 번호를 입력해 주세요.\n",
+      );
+      const checkBonusNumber = new GetBonusNumber(winningNumber, bonusNumber);
+      const getBonusNumber = checkBonusNumber.getBonusNumber();
+
+      return getBonusNumber;
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+
+      return await this.getBonusNumber(winningNumber);
+    }
+  }
+
   async play() {
     const purchaseAmount = await this.getPurchaseAmount();
     const purchaseNumber = purchaseAmount / UNIT_OF_PURCHASE;
@@ -119,6 +162,7 @@ class App {
     this.printPurchaseLottos(purchaseNumber, getLottoNumbers);
 
     const winningNumber = await this.getWinningNumbers();
+    const bonusNumber = await this.getBonusNumber(winningNumber);
   }
 }
 
