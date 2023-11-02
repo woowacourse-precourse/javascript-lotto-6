78e3319b123aa047aa749e2a9b129741c87e2d88
parkjeehoon <gkstj8300@naver.com>
2023-11-02 22:17:33 +0900

    feat: 입출력 메세지를 위한 상수 추가

---
+++ src/comm/Message.js
@@ -0,0 +1,15 @@
+const MESSAGE = {
+    MSG_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
+    MSG_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.',
+    MSG_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+};
+
+const ERROR_MESSAGE = {
+    INPUT_PURCHASE_AMOUNT_ERROR: '[ERROR] 입력한 구입 금액이 천 단위가 아닙니다.',
+    INPUT_WINNING_NUMBERS_ERROR: '[ERROR] 입력한 당첨 번호가 1 ~ 45의 숫자가 아닙니다.',
+    INPUT_MORE_WINNING_NUMBERS_ERROR: '[ERROR] 입력한 당첨 번호가 6개 이상입니다.',
+    INPUT_BLANK_ERROR: '[ERROR] 입력한 값이 공백입니다.',
+    INPUT_NUMBER_ERROR: '[ERROR] 입력한 값이 숫자가 아닙니다.'
+};
+
+export { MESSAGE, ERROR_MESSAGE }
