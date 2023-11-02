73683dbfc48962040fefd703d68bdaa65b1e770f
JiSeong Kim <12180543@inha.edu>
2023-11-03 00:30:27 +0900

    feat(view): implement OutputView Class
    - printNewLine: 줄바꿈을 위함
    - printPurchase: lottoNumberList를 받아 구매 이력을 출력
    - printWinningResult: lottoResult와 rateOfReturn을 받아 당첨 결과를 출력

---
+++ src/view/OutputView.js
@@ -0,0 +1,27 @@
+import { Console } from '@woowacourse/mission-utils';
+import viewMessage from '../constants/viewMessage.js';
+
+class OutputView {
+	static printNewLine() {
+		Console.print();
+	}
+
+	static printPurchaseHistory(lottoNumberList) {
+		Console.print(`${lottoNumberList.length}${viewMessage.OUTPUT_PURCHASE_HISTORY}`);
+		lottoNumberList.forEach(lottoNumber => {
+			Console.print(lottoNumber);
+		});
+	}
+
+	static printWinningResult(lottoResult, rateOfReturn) {
+		Console.print(viewMessage.OUTPUT_WINNING_RESULT);
+		Console.print(`${viewMessage.OUTPUT_FIFTH_PLACE}${lottoResult.fifthPlaceCount}${viewMessage.OUTPUT_COUNT}`);
+		Console.print(`${viewMessage.OUTPUT_FOURTH_PLACE}${lottoResult.fourthPlaceCount}${viewMessage.OUTPUT_COUNT}`);
+		Console.print(`${viewMessage.OUTPUT_THIRD_PLACE}${lottoResult.thirdPlaceCount}${viewMessage.OUTPUT_COUNT}`);
+		Console.print(`${viewMessage.OUTPUT_SECOND_PLACE}${lottoResult.secondPlaceCount}${viewMessage.OUTPUT_COUNT}`);
+		Console.print(`${viewMessage.OUTPUT_FIRST_PLACE}${lottoResult.firstPlaceCount}${viewMessage.OUTPUT_COUNT}`);
+		Console.print(`${viewMessage.OUTPUT_RATE_RETURN_PRE}${rateOfReturn}${viewMessage.OUTPUT_RATE_RETURN_POST}`);
+	}
+}
+
+export default OutputView;
