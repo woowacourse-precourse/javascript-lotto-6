7cc42794d5b55dbfc20522408b3715806312156e
김미르 <kimmireu@gimmileuui-iMac.local>
2023-11-02 16:42:40 +0900

    feat : 당첨 통계 출력 구현

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ class App {
 
     const targetNumbers = getTargetNumbers();
     const bonusNumber = getBonusNumber();
+    const result = getResult(lottos, targetNumbers, bonusNumber);
+    const income = getIncome(result);
+    printResult(totalPrice, income, result);
   }
 }
 
@@ -77,8 +80,66 @@ const getBonusNumber = async () => {
 
 const validateBonusNumbers = (number) => {
   if (Number.isNaN(number)) {
-    throw new Error("[ERROR] 당첨 번호는 6자리입니다.");
+    throw new Error("[ERROR] 보너스 번호는 숫자입니다.");
   }
+};
+
+const getResult = (lottos, targetNumbers, bonusNumber) => {
+  const result = {
+    5: 0,
+    4: 0,
+    3: 0,
+    2: 0,
+    1: 0,
+  };
+  for (let lotto of lottos) {
+    const [correctTarget, correctBonus] = calculateResult(
+      lotto,
+      targetNumbers,
+      bonusNumber
+    );
+    judgeResult(correctTarget, correctBonus, result);
+  }
+  return result;
+};
+
+const calculateResult = (lotto, targetNumbers, bonusNumber) => {
+  let corretTarget = 0;
+  let corretBonus = 0;
+  for (let targetNumber of targetNumbers) {
+    if (lotto.includes(targetNumber)) corretTarget += 1;
+  }
+  if (lotto.includes(bonusNumber)) corretBonus += 1;
+
+  return [corretTarget, corretBonus];
+};
+
+const judgeResult = (correctTarget, correctBonus, result) => {
+  if (correctTarget == 6) result[1] += 1;
+  if (correctTarget == 5 && correctBonus) result[2] += 1;
+  if (correctTarget == 5 && !correctBonus) result[3] += 1;
+  if (correctTarget == 4) result[4] += 1;
+  if (correctTarget == 3) result[5] += 1;
+};
+
+const getIncome = (result) => {
+  return (
+    result[5] * 5000 +
+    result[4] * 50000 +
+    result[3] +
+    1500000 +
+    result[2] * 30000000 +
+    result[1] * 2000000000
+  );
+};
+
+const printResult = (totalPrice, income, result) => {
+  Console.print(`3개 일치 (5,000원) - ${result[5]}개`);
+  Console.print(`4개 일치 (50,000원) - ${result[4]}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${result[3]}개`);
+  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[2]}개`);
+  Console.print(`6개 일치 (2,000,000,000원) - ${result[1]}개`);
+  Console.print(`총 수익률은 ${(income / totalPrice).toFixed(1)}%입니다.`);
 };
 
 export default App;
