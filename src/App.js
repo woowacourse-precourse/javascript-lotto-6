1b9875c9daac9a57112ac4e3174bcce3855433c0
ktyoung <ktyoung0507@gmail.com>
2023-11-02 15:59:09 +0900

    feat(user-input): 사용자 입력 기능 추가
    - 로또 구입 금액을 입력 받는 기능 추가
    - README.md 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const PurchaseAmount = await Console.readLineAsync(
+      "구입금액을 입력해 주세요. \n"
+    );
+  }
 }
 
 export default App;
