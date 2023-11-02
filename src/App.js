745c9478b2567b689d4f8b638fa5300000874ef5
Elisha0510 <sjeong35@knu.ac.kr>
2023-11-02 19:44:34 +0900

    feat: 로또 구입 금액에 따라 로또 개수를 구하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
+  getLottoTicket(lottoPurchaseAmount) {
+    return Number(lottoPurchaseAmount / 1000);
+  }
+
   async play() {
     const lottoPurchaseAmount =
-      await Console.print('구입 금액을 입력해 주세요.\n');
+      await Console.readLineAsync('구입 금액을 입력해 주세요.\n');
+    const lottoTicket = this.getLottoTicket(lottoPurchaseAmount);
   }
 }
 
