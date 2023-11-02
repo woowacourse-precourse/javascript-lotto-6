b76d99ac701f3ae127bf3a8d76d45a3a91f52563
minki3 <86992006+minki3@users.noreply.github.com>
2023-11-02 23:25:59 +0900

    test:숫자 입력 유효성 테스트 코드 작성

--- src/numberVerification.js
+++ src/numberVerification.js
@@ -2,7 +2,7 @@ const numberVerification = (number) => {
   const array = number.split(",");
   const arrayNumber = array.map((item) => Number(item));
   for (let i = 0; i < arrayNumber.length; i++) {
-    if (array[i] >= 45 || arrayNumber.length !== 6 || isNaN(array[i])) {
+    if (1 >= array[i] <= 45 || arrayNumber.length !== 6 || isNaN(array[i])) {
       throw new Error("[ERROR] : 숫자가 잘못된 형식입니다.");
     }
   }
