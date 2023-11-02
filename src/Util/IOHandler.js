5f31627f8dd962fba2610dff2eabb17602b26a76
6puritans9 <6puritans9@gmail.com>
2023-11-02 22:55:46 +0900

    chore(Util): Exported IOHandler

--- src/Util/IOHandler.js
+++ src/Util/IOHandler.js
@@ -1,6 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 
-class IO {
+export default class IO {
   static async get(query) {
     const input = await Console.readLineAsync(query);
     return input;
