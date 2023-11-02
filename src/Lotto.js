f19365765553cdabd740e67f2348d63133cc9987
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-11-02 16:16:01 +0900

    feat: check non-overlapping numbers
    implementation of a function to check that lotto numbers are non-overlapping numbers in the range of 1 to 45

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,13 +3,26 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    let numberSet = new Set(numbers);
+    if (numberSet.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+    for (let number of numbers) {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 값이어야 합니다.");
+      }
+    }
+  }
+
+  get numbers() {
+    return this.#numbers;
   }
 
   // TODO: 추가 기능 구현
