4209a0442b095a93b2a382ebf3f4f9c29578caf3
Goongam <dlwjddn341@naver.com>
2023-11-03 04:37:18 +0900

    Feat: 보너스 번호 입력 및 에외 처리

--- src/App.js
+++ src/App.js
@@ -39,15 +39,16 @@ class App {
     const RANDOMS = await Random.pickUniqueNumbersInRange(1, 45, 6);
     const lotto = new Lotto(RANDOMS);
     this.lottos.push(lotto);
+    return lotto;
   }
   
-  async makeNumberArray(numberString){
+  makeNumberArray(numberString){
     const numbers = numberString.split(',');
     if(numbers.length !== 6) throw new Error("6개여야함");
-    return numbers.map(this.validNumber);
+    return numbers.map(this.validateNumber);
   }
 
-  validNumber(number){
+  validateNumber(number){
     const N = +number;
     if(isNaN(N)) throw new Error("숫자가 아님");
     if(N < 1 || N > 45) throw new Error("범위 밖임");
@@ -56,18 +57,41 @@ class App {
     return N;
   }
 
+  validateBonusNumber(number){
+    const N = +number;
+    if(isNaN(N)) throw new Error("숫자가 아님");
+    if(N < 1 || N > 45) throw new Error("범위 밖임");
+    if(Math.floor(N) !== N) throw new Error("정수가 아님");
+    return N;
+  }
+
+  printResult(){
+    this.lottos.forEach(lotto => {
+      
+    });
+  }
+
   async play() {
+    //돈 입력 및 로또 개수 구하기
     const INPUT_MONEY = await input(PRINT_MESSAGE.INPUT_MONEY);
     const LOTTO_COUNT = this.getLottoCount(+INPUT_MONEY);
+
     await this.printBuyCount(LOTTO_COUNT);
-    
     print('');
 
-    this.createLottos(LOTTO_COUNT);
+    //로또 생성
+    await this.createLottos(LOTTO_COUNT);
 
-    const INPUT_NUMBERS = await input(PRINT_MESSAGE.INPUT_NUMBERS);
+    //각 로또 번호 생성
+    const INPUT_NUMBERS = await input(PRINT_MESSAGE.NUMBERS);
     const NUMBERS = this.makeNumberArray(INPUT_NUMBERS);
 
+    //보너스 번호 생성
+    const INPUT_BONUS_NUMBERS = await input(PRINT_MESSAGE.BONUS_NUMBER);
+    const BONUS_NUMBERS = this.validateBonusNumber(INPUT_BONUS_NUMBERS);
+
+    
+    
   }
 }
 
