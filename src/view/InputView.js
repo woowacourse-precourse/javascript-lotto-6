a2da7c8baa55a88198b96ad88077581aaee8d5fd
laon2019 <laon5987@nate.com>
2023-11-02 21:19:51 +0900

    refactor(InputView.js): DEFAULT_ERROR 메시지 적용

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,13 +1,13 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { GAME_MESSAGES } from "../utils/Messages";
+import { GAME_MESSAGES, ERROR_MESSAGES } from "../utils/Messages";
 
 class InputView {
     static async getLottoNumbers(){
         try {
-            const lottoNumbers = await Console.readLineAsync(GAME_MESSAGES.ENTER_PURCHASE_AMOUNT);
+            const lottoNumbers = await MissionUtils.Console.readLineAsync(GAME_MESSAGES.ENTER_PURCHASE_AMOUNT);
             return lottoNumbers;
         } catch (error) {
-            throw new Error("[ERROR]");
+            throw new Error(ERROR_MESSAGES.DEFAULT_ERROR);
         }
     }
 }
