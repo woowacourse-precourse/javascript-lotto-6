a76c3e8bad58a71a043ec64829003e9dd11ca530
Hong sung soo <sow9707@daum.net>
2023-11-03 00:57:01 +0900

    feat: 로또 당첨 번호 유효성 검사 함수 구현 및 입금액 유효성 검사 수정

--- src/validation.js
+++ src/validation.js
@@ -1,11 +1,22 @@
 const LOTTOPRICE = 1000;
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 45;
 
 
 function validationUserMoney(userMoney) {
-    if(+userMoney) {
+    if(!+userMoney) {
         throw new Error("[ERROR] 숫자를 입력해주세요.");
     }
     if((userMoney/LOTTOPRICE) - Math.floor(userMoney/LOTTOPRICE)) {
         throw new Error("[ERROR] 1000원 단위로 입금해주세요.");
+    }
+}
+
+function qualificationWinnerNumber(winnerNumber) {
+    if(winnerNumber < MIN_NUMBER || winnerNumber > MAX_NUMBER) {
+        throw new Error("[ERROR] 1~45 내의 숫자를 입력해주세요.");
+    }
+    if(!+winnerNumber) {
+        throw new Error("[ERROR] 숫자를 입력해주세요.");
     }
 }
