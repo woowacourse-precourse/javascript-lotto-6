67c4b2a697f9fa75e4f9786796f1047421e7bc40
ss0526100 <ss0526100@gmail.com>
2023-11-02 20:43:23 +0900

    fix(Is): add condition check neg number

--- src/modules/Is.js
+++ src/modules/Is.js
@@ -2,7 +2,8 @@ class Is {
   static positiveIntegerString(string) {
     if (string.trim() === '') return false;
     if (Number.isNaN(Number(string))) return false;
-    if (Number(inputString) !== parseInt(inputString)) return false;
+    if (Number(string) !== parseInt(string)) return false;
+    if (Number(string) <= 0) return false;
     return true;
   }
 }
