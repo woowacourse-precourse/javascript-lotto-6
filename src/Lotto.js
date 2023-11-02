bc33353a10cf3a52a3f28927e9b9e5e452e7b697
Elisha0510 <sjeong35@knu.ac.kr>
2023-11-03 03:06:32 +0900

    feat: 로또 번호를 입력하지 않는 예외 처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,9 @@ class Lotto {
   }
 
   #validate(numbers) {
+    if (number === '') throw new Error('[ERROR] 로또 번호를 입력하세요.');
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
