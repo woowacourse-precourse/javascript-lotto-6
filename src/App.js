71d4f3c7f66128e7806057d49bca0d71ed9c4e74
Goongam <dlwjddn341@naver.com>
2023-11-03 03:49:49 +0900

    Feat: 돈 입력 메소드 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Lotto from './Lotto.js';
+import {input} from './util/input.js';
+import {print} from './util/output.js';
+import {PRINT_MESSAGE} from './constants/message.js';
+import { ERROE_MESSAGE } from './constants/error.js';
+import { Random } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  
+  lottos = [];
+
+
+ 
+
+  async play() {
+    const INPUT_MONEY = await input(PRINT_MESSAGE.INPUT_MONEY);
+
+    
+  }
 }
 
 export default App;
