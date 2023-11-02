14cc61da69710c09740d81049c6e83ed3632c74e
ktyoung <ktyoung0507@gmail.com>
2023-11-02 16:17:08 +0900

    feat(user-output): 로또 발행 기능 구현
    - Lotto 클래스 내 getNumbers() 메서드 추가
    - 발행한 로또 수량 및 번호 출력 기능 구현
    - README.md 업데이트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import { Random, Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
   async play() {
-    const PurchaseAmount = await Console.readLineAsync(
+    const purchaseAmount = await Console.readLineAsync(
       "구입금액을 입력해 주세요. \n"
     );
+
+    const lottoCount = Math.floor(purchaseAmount / 1000);
+    const lottos = [];
+
+    for (let i = 0; i < lottoCount; i++) {
+      const numbers = PickLottoNumbers();
+      const lotto = new Lotto(numbers);
+      lottos.push(lotto);
+    }
+
+    Console.print(`${lottoCount}개를 구매했습니다.`);
+    lottos.forEach((lotto) => {
+      Console.print(`[${lotto.getNumbers().join(", ")}]`);
+    });
   }
 }
+
+const PickLottoNumbers = () => {
+  return Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+};
 
 export default App;
