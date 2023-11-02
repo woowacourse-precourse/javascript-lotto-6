434f5f10158c842632f4176c69262f3fce6cf49e
leeyulgok <dbfrhr20@gmail.com>
2023-11-03 04:08:19 +0900

    feat: 당첨 번호 추첨 테스트 및 기능 구현
    중복되는 메서드 통일 및 switch 문을 통해 허브 역할 구성

--- src/App.js
+++ src/App.js
@@ -1,46 +1,76 @@
 import Lotto from "./Lotto.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+const LOTTO_LENGTH = 6;
+
 class App {
   async play() {
-    let userMoney;
-    while (true) {
-      try {
-        userMoney = await this.getUserMoney("구입금액을 입력해 주세요.\n");
-        break;
-      } catch (error) {
-        MissionUtils.Console.print(`${error.message}`);
-      }
-    }
+    const userMoney = await this.playUserRequest("구입금액을 입력해 주세요.\n");
 
     const lottos = this.createLottos(userMoney);
     MissionUtils.Console.print(`\n${lottos.length}개를 구매했습니다.`);
     lottos.forEach((lotto) => MissionUtils.Console.print(lotto.getNumbers()));
+    MissionUtils.Console.print("");
+
+    const winningNumbers = await this.playUserRequest("당첨 번호를 입력해 주세요.\n");
+    const bonusNumbers = await this.playUserRequest(
+      "보너스 번호를 입력해 주세요.\n",
+      winningNumbers
+    );
+  }
+
+  async playUserRequest(prompt, winningNumbers) {
+    switch (prompt) {
+      case "구입금액을 입력해 주세요.\n":
+        return await this.getUserMoney(prompt);
+      case "당첨 번호를 입력해 주세요.\n":
+        return await this.getWinningNumber(prompt);
+      case "보너스 번호를 입력해 주세요.\n":
+        return await this.getBonusNumber(prompt, winningNumbers);
+      default:
+        throw new Error("[ERROR]");
+    }
   }
 
   async getUserInput(msg) {
     return await MissionUtils.Console.readLineAsync(msg);
   }
 
-  async getUserMoney(promptMsg) {
+  async getValidatedInput(promptMsg, validateFn, winningNumbers) {
     let firstAttempt = true;
 
     while (true) {
       try {
-        const userInput = await this.getUserInput(
-          firstAttempt ? promptMsg : ""
-        );
+        const userInput = await this.getUserInput(firstAttempt ? promptMsg : "");
         firstAttempt = false;
 
-        const userMoney = this.checkValidate(userInput);
-        return userMoney;
+        return validateFn(userInput, winningNumbers);
       } catch (error) {
         MissionUtils.Console.print(`${error.message}`);
       }
     }
   }
 
-  checkValidate(userInput) {
+  async getUserMoney(promptMsg) {
+    return await this.getValidatedInput(promptMsg, this.checkMoneyValidate);
+  }
+
+  async getWinningNumber(promptMsg) {
+    return await this.getValidatedInput(
+      promptMsg,
+      this.checkLottoNumberValidate
+    );
+  }
+
+  async getBonusNumber(promptMsg, winningNumbers) {
+    return await this.getValidatedInput(
+      promptMsg,
+      this.checkBonusNumberValidate,
+      winningNumbers
+    );
+  }
+
+  checkMoneyValidate(userInput) {
     let userMoney = parseInt(userInput, 10);
 
     if (isNaN(userMoney) || userMoney % 1000 !== 0) {
@@ -48,6 +78,41 @@ class App {
     }
 
     return userMoney;
+  }
+
+  checkLottoNumberValidate(userInput) {
+    const errorMessage = "[ERROR] 로또 번호는 1부터 45 사이의 중복되지 않는 6자리 숫자여야 합니다.";
+    let numbers = userInput.split(",").map((number) => {
+      let num = parseInt(number, 10);
+      if (isNaN(num) || num <= 0 || num > 45) {
+        throw new Error(errorMessage);
+      }
+
+      return num;
+    });
+
+    if (new Set(numbers).size !== LOTTO_LENGTH) {
+      throw new Error(errorMessage);
+    }
+
+    return numbers;
+  }
+
+  checkBonusNumberValidate(userInput, winningNumbers) {
+    const bonusNumber = parseInt(userInput, 10);
+
+    if (
+      isNaN(bonusNumber) ||
+      bonusNumber <= 0 ||
+      bonusNumber > 45 ||
+      winningNumbers.includes(bonusNumber)
+    ) {
+      throw new Error(
+        "[ERROR] 보너스 번호는 1부터 45 사이의 당첨 번호와 중복되지 않은 숫자여야 합니다."
+      );
+    }
+
+    return bonusNumber;
   }
 
   createLottos(money) {
