9b28584138b9477e66f78c12688cb9e831e5bda3
Elisha0510 <sjeong35@knu.ac.kr>
2023-11-03 03:34:22 +0900

    feat: 입력받은 로또 번호가 중복이면 예외 처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,14 +11,19 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
-    const arrayLength = this.checkRange(numbers);
-    if (arrayLength !== 0)
+    if (this.checkRange(numbers))
       throw new Error('[ERROR] 1~45 사이의 숫자를 입력하세요.');
+    const numbersSet = getNumbersSet(numbers);
+    if ([...numbersSet].length !== 6)
+      throw new Error('[ERROR] 중복되지 않는 숫자를 입력하세요');
   }
 
   checkRange(numbers) {
-    const numberArray = numbers.split(', ');
-    return numberArray.filter(value => 1 <= value && value <= 45).length;
+    return numbers.filter(value => 1 <= value && value <= 45).length;
+  }
+
+  getNumbersSet(numbers) {
+    return new Set(numbers.split('').map(Number));
   }
   // TODO: 추가 기능 구현
 }
