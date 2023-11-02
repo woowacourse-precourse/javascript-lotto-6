c07841894b2abda7fab502135b3b17115e6d9822
Goongam <dlwjddn341@naver.com>
2023-11-03 03:49:11 +0900

    Feat: 출력 메시지 상수 정의

---
+++ src/constants/message.js
@@ -0,0 +1,7 @@
+export const PRINT_MESSAGE = {
+  INPUT_MONEY: "구입금액을 입력해 주세요.",
+  BUY_COUNT: "개를 구매했습니다.",
+  NUMBERS: "당첨 번호를 입력해 주세요.",
+  BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+  PRINT_STATS: "당첨 통계",
+};
