da58034dd45aa6fece7c4a7af77d53551a8b9b36
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-02 21:37:50 +0900

    feat(숫자): 로또, 당첨금 관련 상수 추가

---
+++ src/constants/numbers.js
@@ -0,0 +1,14 @@
+export const LOTTO_NUMBERS = Object.freeze({
+  minimum: 1,
+  maximum: 45,
+  purchased: 6,
+  ranks: 5,
+});
+
+export const WINNING_PRIZE_NUMBERS = Object.freeze({
+  rank5: 5000,
+  rank4: 50000,
+  rank3: 1500000,
+  rank2: 30000000,
+  rank1: 2000000000,
+});
