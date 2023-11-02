9b5f27f8ecf10a269a8552e459361a18d39a63da
김미르 <kimmireu@gimmileuui-iMac.local>
2023-11-02 16:12:56 +0900

    feat : 구입 금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto";
 class App {
-  async play() {}
+  async play() {
+    const totalPrice = await getTotalPrice();
+  }
 }
+
+const getTotalPrice = async () => {
+  const totalPrice = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+  validateTotalPrice(totalPrice);
+  return totalPrice;
+};
+
+const validateTotalPrice = (price) => {
+  if (price % 1000) {
+    throw new Error("[ERROR] 구입 금액이 1,000원 단위가 아닙니다.");
+  }
+  if (Number.isNaN(price)) {
+    throw new Error("[ERROR] 구입 금액은 숫자여야 합니다.");
+  }
+};
 
 export default App;
