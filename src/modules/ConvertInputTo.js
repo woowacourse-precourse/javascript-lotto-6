2fd1efb0b8d9f7974632ac4f5472606d1ee479cd
ss0526100 <ss0526100@gmail.com>
2023-11-02 19:37:49 +0900

    feat: input purchase price

---
+++ src/modules/ConvertInputTo.js
@@ -0,0 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const PURCHASE_PRICE_INPUT_MESSAGE = '구입금액을 입력해 주세요.';
+class ConvertInputTo {
+  static async purchasePrice() {
+    const inputString = await Console.readLineAsync(
+      PURCHASE_PRICE_INPUT_MESSAGE
+    );
+
+    return Number(inputString);
+  }
+}
