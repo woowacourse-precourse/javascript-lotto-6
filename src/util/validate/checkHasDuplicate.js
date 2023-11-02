857f019e65cd452c58575b380ee917257022dd59
thumbthing <ilscc0902@gmail.com>
2023-11-03 03:18:21 +0900

    feat: 입력값의 중복검사 기능 구현

---
+++ src/util/validate/checkHasDuplicate.js
@@ -0,0 +1,11 @@
+import ERROR_CODE from '../error/errorCode';
+
+export default function checkHasDuplicate(numbers) {
+  const LottoSet = new Set(numbers);
+
+  const isNotValid = LottoSet.size !== numbers.length;
+
+  if (isNotValid) {
+    throw new Error(`[ERROR] ${ERROR_CODE.isDuplicated}`);
+  }
+}
