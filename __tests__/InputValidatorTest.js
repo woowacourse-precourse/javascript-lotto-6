b3d4a309c578ab4122ce98c8a8f889f004b02a69
장호빈 <devhobin@gmail.com>
2023-11-03 01:48:41 +0900

    test<InutValidatorTest>: 당첨 로또 번호 validation 테스트 작성

--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -15,4 +15,20 @@ describe("InputValidator 테스트", () => {
       }).toThrow("[ERROR]");
     });
   });
+
+  describe("당첨 로또 번호", () => {
+    test.each([
+      ["빈값일 때", ""],
+      ["구분자만 있을 때", ","],
+      ["숫자가 아닌 값이 있을 때", "1,2,a,b,5,6"],
+      ["로또 번호가 아닌 숫자가 있을 때", "0,1,2,3,4,5"],
+      ["로또 번호가 6개가 아닐 때", "1,2,3,4,5"],
+    ])("%s 에러를 발생시킨다", (_, input) => {
+      const validator = new InputValidator();
+
+      expect(() => {
+        validator.validateWinningNumbers(input);
+      }).toThrow("[ERROR]");
+    });
+  });
 });
