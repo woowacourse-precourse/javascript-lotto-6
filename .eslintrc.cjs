061b88208e8add698e7a6cf471595b1881bf8a87
jiyun1006 <jiyun4251@gmail.com>
2023-11-02 17:16:59 +0900

    📝 docs : 프로젝트 시작 및 구현할 기능 목록 작성
    구현할 기능목록, 기능목록 외 생각할 부분, 유의사항 을 README에 정리하고,
    기본 eslint, prettier세팅 완료.

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // 들여쓰기 깊이 제한
+    'max-depth': ['error', 2],
+  },
+}
