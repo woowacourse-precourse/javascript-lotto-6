b2693777f85b53ca6faec88bd0b71f06040fb75f
today-is-first <diligent0644@gmail.com>
2023-11-02 16:31:25 +0900

    feat: 랜덤한 로또 번호 생성

--- src/util/Utils.js
+++ src/util/Utils.js
@@ -1,11 +1,22 @@
 import { Random, Console } from '@woowacourse/mission-utils';
-import INPUT_MESSAGES from '../constant/constants.js';
+import { INPUT_MESSAGES, GAME_RULE_NUMBER } from '../constant/constants.js';
 
-async function getPurchasingMoney() {
-  const purchasingMoney = await Console.readLineAsync(
-    INPUT_MESSAGES.purcahsingMoney,
-  );
-  return purchasingMoney;
+async function getUserInput(message) {
+  const userInput = await Console.readLineAsync(message);
+  return userInput;
 }
 
-getPurchasingMoney();
+const lottoNumber = () =>
+  Random.pickUniqueNumbersInRange(
+    GAME_RULE_NUMBER.min,
+    GAME_RULE_NUMBER.max,
+    GAME_RULE_NUMBER.length,
+  );
+
+const getUserLottoNumbers = (amount) => {
+  const userLottoNumbers = [];
+  for (let i = 0; i < amount; i += 1) {
+    userLottoNumbers.push(lottoNumber().sort((a, b) => a - b));
+  }
+  return userLottoNumbers;
+};
