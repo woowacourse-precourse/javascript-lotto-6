fe8bdb8e49a875ba8a192722a17df885d811d014
coldok <2coldok@gmail.com>
2023-11-03 01:49:14 +0900

    feat : 로또 당첨 통계 계산기 구현

---
+++ src/domain/LottoStatistics.js
@@ -0,0 +1,47 @@
+
+class LottoStatistics {
+  #rankingStatusBoard = {
+    1 : 0,
+    2 : 0,
+    3 : 0,
+    4 : 0,
+    5 : 0,
+  }
+
+  #rankingArray = [];
+
+  #purchaseAmount;
+
+  constructor(rankingArray, purchaseAmount) {
+    this.#rankingArray = rankingArray;
+    this.#rankingUpdate();
+    this.#purchaseAmount = purchaseAmount;
+
+  }
+
+  #rankingUpdate() {
+    this.#rankingArray.forEach((ranking) => {
+      this.#rankingStatusBoard[ranking] += 1;
+    })
+  }
+
+  getResult() {
+    return [
+      `3개 일치 (5,000원) - ${this.#rankingStatusBoard[5]}개`,
+      `4개 일치 (50,000원) - ${this.#rankingStatusBoard[4]}개`,
+      `5개 일치 (1,500,000원) - ${this.#rankingStatusBoard[3]}개`,
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.#rankingStatusBoard[2]}개`,
+      `6개 일치 (2,000,000,000원) - ${this.#rankingStatusBoard[1]}개`
+    ];
+  }
+
+  #rateOfReturn() {
+    
+    
+  }
+
+  
+}
+
+const a = new LottoStatistics([1,1,1,1,1,1,1,4,4,4,4,3,3,3,3,3,5]);
+console.log(a.getResult())
