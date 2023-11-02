8108b146c28e82d16bd66a5250944e039fd1d1c0
noodle <noodle>
2023-11-03 03:24:01 +0900

    feat: 러프하게 기능구현을 목적으로 전반적인 로직 작성 WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { ERROR_MESSAGE, INPUT_MESSAGE, OUTPUT_MESSAGE } from './constants/index.js';
+
+import { Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+
 class App {
-  async play() {}
+  #lotto;
+
+  constructor() {
+    this.#lotto;
+  }
+
+  async play() {
+    // 1. (입력) 로또 구입 금액 입력
+    const purchaseAmount = await Console.readLineAsync(INPUT_MESSAGE.PURCHASE_AMOUNT);
+
+    // 1.1. 1000원 단위로 로또 갯수 구하기
+    const getTotalCount = (purchaseAmount) => {
+      const unit = 1000;
+      if (purchaseAmount % unit) throw new Error(ERROR_MESSAGE.INVALID_UNIT);
+      return purchaseAmount / unit;
+    };
+    const totalCount = getTotalCount(purchaseAmount);
+
+    // 2. (출력) 로또 구매 갯수 출력
+    await Console.print(OUTPUT_MESSAGE.PURCHASE_CONFIRM(totalCount));
+
+    // 3. (출력) 구매 갯수만큼 로또 번호 출력
+    const lottoList = Lotto.getLottoList(totalCount);
+    lottoList.forEach((lotto) => Console.print(lotto));
+
+    // 4. (입력) 당첨 번호 6개 번호를 입력 받기
+    const winningNumber = await Console.readLineAsync(INPUT_MESSAGE.WINNING_NUMBERS);
+    this.#lotto = new Lotto(winningNumber.split(',').map(Number));
+
+    // 5. (입력) 보너스 번호 입력 받기
+    const bonusNumber = await Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER);
+
+    // 6. (출력) 당첨 내역 출력
+    this.#lotto.compareWinningAndLotto(winningNumber, bonusNumber, lottoList);
+  }
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
+import { OUTPUT_MESSAGE } from './constants/index.js';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +11,71 @@ class Lotto {
   }
 
   #validate(numbers) {
+    console.log(numbers); // [1,2,3,4,5,6]
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
+  // TODO: 함수 분리, SOLID 원칙 적용하기
+
+  // 로또 번호 생성
+  static getOneLotto() {
+    let curCount = 0;
+    let limit = 6;
+    let lottoNumbers = [];
+    while (curCount < limit) {
+      const a = Random.pickNumberInRange(1, 45);
+      if (!lottoNumbers.includes(a)) {
+        lottoNumbers.push(a);
+        curCount += 1;
+      }
+    }
+    return lottoNumbers.sort((a, b) => a - b);
+    // pickUniqueNumbersInRange를 사용하면 더 짧음.
+    // const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+  }
+
+  // 넘겨 받은 갯수만큼 로또 리스트 생성
+  static getLottoList(num) {
+    let curCount = 0;
+    let lottoNumbersList = [];
+    while (curCount < num) {
+      const numbers = this.getOneLotto();
+      lottoNumbersList.push(numbers);
+      curCount += 1;
+    }
+    return lottoNumbersList;
+  }
+
+  // 당첨 번호와 비교
+  compareWinningAndLotto(winningNumbers, bonusNumber, lottoList) {
+    Console.print(OUTPUT_MESSAGE.RESULT_HEADER);
+
+    const matchingCount = lottoList.map((lotto) => {
+      return lotto.filter((num) => winningNumbers.includes(num)).length;
+    });
+
+    this.lotteryStatistics(matchingCount);
+  }
+
+  // 당첨 통계
+  lotteryStatistics(matchingCount) {
+    const result = {};
+    matchingCount.forEach((count) => {
+      if (count < 3) return;
+      result[count] = result[count] ? result[count] + 1 : 1;
+    });
+
+    console.log(result);
+    for (let index = 3; index <= 6; index++) {
+      Console.print(OUTPUT_MESSAGE.RESULT_ROW(index, result[Number(index)]));
     }
   }
 
-  // TODO: 추가 기능 구현
+  // 수익률 계산
+  calculateRateOfReturn(구매금액, result) {
+    //
+  }
 }
 
 export default Lotto;
