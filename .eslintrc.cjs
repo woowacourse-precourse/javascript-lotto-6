eed756f23f48a1124b0b699057ed6a1144a45bd2
jiyun1006 <jiyun4251@gmail.com>
2023-11-02 18:04:14 +0900

    🔨 chore : eslint와 (CommonJS, ES6)간의 충돌 사항 수정.
    import 경로 문제와 'no-console'오류 해결하기 위한
    .eslintrc.cjs 코드 추가.(rules 추가)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,8 @@ module.exports = {
   },
   rules: {
     // 들여쓰기 깊이 제한
-    'max-depth': ['error', 2],
+    "max-depth": ["error", 2],
+    "import/extensions": ["error", "ignorePackages"],
+    "no-console": process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 }
