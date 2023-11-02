fc45a2bdff60173e711a63e81c32acef7e0aaa97
laon2019 <laon5987@nate.com>
2023-11-02 21:16:19 +0900

    feat(InputView.js): 로또 번호 입력 메소드 생성

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,7 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { GAME_MESSAGES } from "../utils/Messages";
 
 class InputView {
-
+    static async getLottoNumbers(){
+        try {
+            const lottoNumbers = await Console.readLineAsync(GAME_MESSAGES.ENTER_PURCHASE_AMOUNT);
+            return lottoNumbers;
+        } catch (error) {
+            throw new Error("[ERROR]");
+        }
+    }
 }
 
 export default InputView;
