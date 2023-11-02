e61f076440dafee1d0e880ef8d501feeb1fe7a4c
yui880 <kimyuna521@gmail.com>
2023-11-02 17:57:39 +0900

    feat(Constant): 오류 메세지 출력에 사용될 ERROR 상수 선언

--- src/constants/Constant.js
+++ src/constants/Constant.js
@@ -8,3 +8,13 @@ export const MESSAGE = Object.freeze({
   enterWinning: '당첨 번호를 입력해 주세요.\n',
   enterBonus: '보너스 번호를 입력해 주세요\n',
 });
+
+export const ERROR = Object.freeze({
+  errorPrefix: '[ERROR]',
+  isNotInAmountUnit: `입력된 금액이 ${CONSTANT.amountUnit} 단위가 아닙니다.`,
+  isNegative: '입력된 금액이 음수입니다. 금액은 양수여야 합니다.',
+  isEmpty: '공백이 입력되었습니다. 6자리의 숫자를 입력해야합니다.',
+  isNotANumber: '숫자가 아닌 값이 입력되었습니다. 번호는 숫자만 가능합니다.',
+  isNotSix: '6개가 입력되지 않았습니다. 6개의 숫자를 입력해주세요.',
+  isNotInRange: '입력된 숫자 중 1-45 범위가 아닌 숫자가 있습니다. 범위 안의 숫자를 입력해주세요',
+});
