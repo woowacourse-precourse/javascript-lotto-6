7c6eda22a6dbeaa4c2bb19c9f6701a3b9f26db95
최진욱 <jinwook.fe@gmail.com>
2023-11-03 00:53:48 +0900

    [feat] 정렬된 로또 용지 출력 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+const UNIT_OF_PURCHASE = 1000;
+
 class GetPurchaseAmount {
   #purchase;
 
@@ -12,7 +14,7 @@ class GetPurchaseAmount {
     const regex = /^\d+$/;
     if (!regex.test(purchase)) {
       throw new Error("[ERROR] 구매 금액은 숫자 형식만 가능합니다.");
-    } else if (purchase % 1000 !== 0) {
+    } else if (purchase % UNIT_OF_PURCHASE !== 0) {
       throw new Error("[ERROR] 구매 금액은 1000으로 나누어 떨어져야 합니다.");
     }
 
@@ -41,8 +43,30 @@ class App {
     }
   }
 
+  getRandomLottoNumbers(purchaseNumber) {
+    const lottoNumbers = [];
+    for (let i = 0; i < purchaseNumber; i++) {
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      const sortedNumbers = numbers.sort((a, b) => a - b);
+      lottoNumbers.push(sortedNumbers);
+    }
+
+    return lottoNumbers;
+  }
+
+  printPurchaseLottos(purchaseNumber, lottoNumbers) {
+    MissionUtils.Console.print(`\n${purchaseNumber}개를 구매했습니다.`);
+    lottoNumbers.forEach((lottoNumber) =>
+      MissionUtils.Console.print(lottoNumber),
+    );
+  }
+
   async play() {
     const purchaseAmount = await this.getPurchaseAmount();
+    const purchaseNumber = purchaseAmount / UNIT_OF_PURCHASE;
+    const getLottoNumbers = this.getRandomLottoNumbers(purchaseNumber);
+
+    this.printPurchaseLottos(purchaseNumber, getLottoNumbers);
   }
 }
 
