795b15157049e66416c8b4f14b1d7c355c0e9550
JiSeong Kim <12180543@inha.edu>
2023-11-03 02:19:49 +0900

    feat(constants): add NOT_SIX_COUNT in errorMessage
    받아온 숫자가 총 6개가 아닐 때의 오류메시지 추가

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,6 +1,7 @@
 const errorMessage = Object.freeze({
 	NOT_NUMBER: '[ERROR] 문자가 아닌 숫자를 입력해주세요.',
 	INVALID_PURCHASE_AMOUNT: '[ERROR] 1000원 단위의 금액을 입력해주세요.',
+	NOT_SIX_COUNT: '[ERROR] 총 6자리의 당첨 번호를 입력해주세요',
 	NOT_ENOUGH_PURCHASE_AMOUNT: '[ERROR] 금액이 부족합니다.',
 	DUPLICATED_NUMBER: '[ERROR] 중복되지 않은 숫자를 입력해주세요.',
 	OUT_OF_RANGE: '[ERROR] 1부터 45사이의 숫자만 입력해주세요.',
