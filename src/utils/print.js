7daf5df34d2f29998210d4a975f217fc3dfc3e7e
jomoo02 <jomootgrn88@naver.com>
2023-11-02 22:30:10 +0900

    feat: 출력 기능 추가
    구입금액 입력 문구 출력,
    n개 로또 구입 문구 출력,
    로또 숫자 출력,
    당첨 번호 입력 문구 출력,
    보너스 번호 입력 문구 출력,
    당첨 통계 출력,
    총 수익률 출력

---
+++ src/utils/print.js
@@ -0,0 +1,65 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const print = (printString = '') => {
+  MissionUtils.Console.print(printString);
+};
+
+const printInputPurchaseAmountPhrase = () => {
+  const INPUT_PURCHASE_AMOUNT_PHRASE = '구입금액을 입력해 주세요.';
+  print(INPUT_PURCHASE_AMOUNT_PHRASE);
+};
+
+const printNPurchasePhrase = (n) => {
+  const PURCHASE_PHRASE = '개를 구매헀습니다.';
+  const nPurchasePhrase = `${n}${PURCHASE_PHRASE}`;
+  print(nPurchasePhrase);
+};
+
+const printLottoNumbers = (lottos) => {
+  lottos.forEach((lotto) => {
+    print(lotto);
+  });
+};
+
+const printInputWinnigNumbersPhrase = () => {
+  const INPUT_WINNING_NUMBER_PHRASE = '당첨 번호를 입력해 주세요.';
+  print(INPUT_WINNING_NUMBER_PHRASE);
+};
+
+const printInputBonusNumberPhrase = () => {
+  const INPUT_BONUS_NUMBER_PHRASE = '보너스 번호를 입력해 주세요.';
+  print(INPUT_BONUS_NUMBER_PHRASE);
+};
+
+const formatCurrency = (money) => {
+  return `${money.toLocaleString('en-US')}원`;
+};
+
+const getNMatchPhrase = ({ matchString, money, count }) => {
+  return `${matchString} (${formatCurrency(money)}) - ${count}개`;
+};
+
+const printWinningStatistics = (matchResults) => {
+  const WINNING_STATISTICS_PHRASE = '당첨 통계';
+  const CONTOUR = '---';
+
+  print(WINNING_STATISTICS_PHRASE);
+  print(CONTOUR);
+  matchResults.forEach((result) => {
+    print(getNMatchPhrase(result));
+  });
+};
+
+const printTotalReturn = (totalReturn) => {
+  return `총 수익률은 ${totalReturn}%입니다.`;
+};
+
+export {
+  printInputPurchaseAmountPhrase,
+  printNPurchasePhrase,
+  printLottoNumbers,
+  printInputWinnigNumbersPhrase,
+  printInputBonusNumberPhrase,
+  printWinningStatistics,
+  printTotalReturn,
+};
