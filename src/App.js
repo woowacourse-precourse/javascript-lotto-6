c1f02115c0ebbc1f79d5f8b902bb9a246d9be66f
전소희 <indian_potato@naver.com>
2023-11-02 21:43:33 +0900

    feat: 유저 구입 금액 검증
    Number.isInterger()를 사용해 purchaseCost / 1000 === false일 경우 예외를 발생시키며 유저의 콘솔창에 ERROR문구를 발생시킨다

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const enterCost = await MissionUtils.Console.readLineAsync(
+    const purchaseCost = await MissionUtils.Console.readLineAsync(
       "구입 금액을 입력해 주세요. \n"
     );
+
+    console.log(Number.isInteger(purchaseCost / 1000));
+
+    if (Number.isInterger(purchaseCost / 1000) === false) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력 가능합니다.");
+    }
   }
 }
 
