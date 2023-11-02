de184cec6659e50a299246f4f9b66691780a072d
ss0526100 <ss0526100@gmail.com>
2023-11-02 22:51:19 +0900

    test: make lottoArray test code

---
+++ __tests__/moduleTest/GetTest.js
@@ -0,0 +1,14 @@
+import Get from '../../src/modules/Get.js';
+
+describe('Get 테스트', () => {
+  test.each([[1], [2], [6], [10]])('lottoArray', input => {
+    //when
+    const lottoArray = Get.lottoArray(input);
+
+    //then
+    expect(lottoArray).toHaveLength(input);
+    lottoArray.map(lotto => {
+      expect(lotto.getNumbers()).toHaveLength(6);
+    });
+  });
+});
