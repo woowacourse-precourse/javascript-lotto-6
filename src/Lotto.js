016a688f2cd424089adf323173a58b26cfe5c93c
Elisha0510 <sjeong35@knu.ac.kr>
2023-11-03 03:12:49 +0900

    feat: 1~45 사이의 숫자를 입력하지 않았을 때의 예외 처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,10 +7,16 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (number === '') throw new Error('[ERROR] 로또 번호를 입력하세요.');
+    if (numbers === '') throw new Error('[ERROR] 로또 번호를 입력하세요.');
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    const numberArray = numbers.split(', ');
+    const arrayLength = numberArray.filter(
+      value => 1 <= value && value <= 45,
+    ).length;
+    if (arrayLength !== 0)
+      throw new Error('[ERROR] 1~45 사이의 숫자를 입력하세요.');
   }
 
   // TODO: 추가 기능 구현
