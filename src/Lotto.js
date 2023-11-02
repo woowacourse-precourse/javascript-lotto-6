f9c7fd132cd8d14ce705965f65467923199683a2
jijiseong <jiseong5036@gmail.com>
2023-11-02 17:33:22 +0900

    style: eslint airbnb 규칙 적용

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 class Lotto {
   #numbers;
 
@@ -7,8 +8,12 @@ class Lotto {
   }
 
   #validate(numbers) {
+    const setNumbers = new Set(numbers);
+    if (setNumbers.length !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호는 중복이 있으면 안 됩니다.');
+    }
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
