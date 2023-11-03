ce6f9e40cbce004b8b5070df0c5d33deed0fd628
j8won <joowoni99@gmail.com>
2023-11-04 00:04:03 +0900

    feat(Validator): 배열 내에 중복된 값 여부를 반환하는 checkArrayForDuplicate 메서드 생성

--- src/Validator.js
+++ src/Validator.js
@@ -2,6 +2,10 @@ class Validator {
   static checkArrayLength(array, number) {
     return array.length === number;
   }
+
+  static checkArrayForDuplicate(array) {
+    return new Set(array).size !== array.length;
+  }
 }
 
 export default Validator;
