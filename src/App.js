8838a8c9b71c2ffe74901df9de5c23b25dfdb711
장호빈 <devhobin@gmail.com>
2023-11-03 17:25:25 +0900

    feat<App>: 수익률 계산 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const WINNING_RESULT_TO_PRICE = {
+  three: 5000,
+  four: 50000,
+  five: 1500000,
+  fiveBonus: 30000000,
+  six: 2000000000,
+};
 class App {
   async play() {}
+  #calculateProfitRate(totalResult) {
+    const winningAmount = Object.keys(WINNING_RESULT_TO_PRICE).reduce(
+      (acc, key) => acc + WINNING_RESULT_TO_PRICE[key] * totalResult[key],
+      0,
+    );
+    return ((winningAmount / this.#amount - 1) * 100).toFixed(1);
+  }
 }
 
 export default App;
