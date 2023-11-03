aa2e8301bbd71a9e50e6dade214bb2c2fb12598b
Jeongwook Park <hatchling3713@gmail.com>
2023-11-03 15:39:38 +0900

    fix(Lotto): add missing validation logic

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,15 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    const deduplicated = new Set(numbers);
+
+    if (numbers.length !== deduplicated.size) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
