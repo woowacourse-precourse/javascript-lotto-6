c7f078a20dacc9a9e17d5a56245b2829af0c35ce
jomoo02 <jomootgrn88@naver.com>
2023-11-03 20:43:56 +0900

    feat: Lotto 추가
    class Lotto 추가
    - 로또 번호들을 조건에 맞는지 확인하고 저장
    - 로또 번호를 반환
    - 조건에 맞지 않으면 이유가 포함된 에러 메시지를 던짐

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,68 @@
+import {
+  SIX_NUMBERS_ERROR_MESSAGE,
+  DUPLICATE_ERROR_MESSAGE,
+  INTEGER_ERROR_MESSAGE,
+  INRANGE_ERROR_MESSAGE,
+  ASCENDING_ORDER_ERROR_MESSAGE,
+} from './constants/errorMessage';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    Lotto.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  static #validate(numbers) {
+    Lotto.#isSixNumbers(numbers);
+    Lotto.#isDuplicate(numbers);
+    Lotto.#isIneger(numbers);
+    Lotto.#isInRange(numbers);
+    Lotto.#isAscendingOrder(numbers);
+  }
+
+  static #isSixNumbers(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      Lotto.#throwError(SIX_NUMBERS_ERROR_MESSAGE);
     }
   }
 
-  // TODO: 추가 기능 구현
+  static #isDuplicate(numbers) {
+    if (new Set([...numbers]).size !== numbers.length) {
+      Lotto.#throwError(DUPLICATE_ERROR_MESSAGE);
+    }
+  }
+
+  static #isIneger(numbers) {
+    if (!numbers.every((number) => Number.isInteger(number))) {
+      Lotto.#throwError(INTEGER_ERROR_MESSAGE);
+    }
+  }
+
+  static #isInRange(numbers) {
+    if (!numbers.every((number) => number >= 1 && number <= 45)) {
+      Lotto.#throwError(INRANGE_ERROR_MESSAGE);
+    }
+  }
+
+  static #isAscendingOrder(numbers) {
+    const sortedNumbers = [...numbers].sort((a, b) => a - b);
+    const isEqual = (a, b) => a === b;
+    if (
+      !numbers.every((number, index) => isEqual(number, sortedNumbers[index]))
+    ) {
+      Lotto.#throwError(ASCENDING_ORDER_ERROR_MESSAGE);
+    }
+  }
+
+  static #throwError(errorMessage) {
+    throw new Error(`[ERROR] ${errorMessage}`);
+  }
 }
 
 export default Lotto;
---
+++ src/constants/errorMessage.js
@@ -0,0 +1,13 @@
+const SIX_NUMBERS_ERROR_MESSAGE = '로또 번호는 6개여야 합니다.';
+const DUPLICATE_ERROR_MESSAGE = '로또 번호는 중복되지 않아야 합니다';
+const INTEGER_ERROR_MESSAGE = '로또 번호는 정수여야 합니다.';
+const INRANGE_ERROR_MESSAGE = '로또 번호는 1이상 45이하여야 합니다.';
+const ASCENDING_ORDER_ERROR_MESSAGE = '로또 번호는 오름차순이여야 합니다.';
+
+export {
+  SIX_NUMBERS_ERROR_MESSAGE,
+  DUPLICATE_ERROR_MESSAGE,
+  INTEGER_ERROR_MESSAGE,
+  INRANGE_ERROR_MESSAGE,
+  ASCENDING_ORDER_ERROR_MESSAGE,
+};
