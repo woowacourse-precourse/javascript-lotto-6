143e9f62d42d19c921901ecc960b6de1ee2652da
djLee77 <dkdnl232@gmail.com>
2023-11-03 16:05:04 +0900

    chore(setup): eslint와 prettier 설정 추가
    eslint 설정
    -eslint 패키지 설치
    
    prettier 설정
    -prettier 패키지 설치
    
    eslint, prettier 규칙 충돌 방지
    -eslint-config-prettier 패키지 설치
    -eslint-plugin-prettier 설치
    - .eslintrc.cjs 파일 수정
    
    eslint jest 설정
    .eslintrc.cjs 파일의 env섹션에 jest 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:jest/recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+        jest: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jest", "prettier"],
+  rules: { "prettier/prettier": "error" },
+};
