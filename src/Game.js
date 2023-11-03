a5ac106b7e108d085293275c16f14db07b5d5825
UBiiiii <willowman55@gmail.com>
2023-11-03 22:19:08 +0900

    [feat] 로또 생성 기능 구현

--- src/Game.js
+++ src/Game.js
@@ -2,13 +2,14 @@ import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto";
 
 class Game {
-  #lottos;
+  lottos;
   #choice;
   #bonus;
 
   async getMoney() {
     const input = await Console.readLineAsync("구입 금액을 입력해주세요.\n");
     this.#validateMoney(input); 
+    this.#getLottos(input/1_000);
   }
 
   #validateMoney(money) {
@@ -16,6 +17,15 @@ class Game {
       throw new Error("[ERROR] 구입 금액은 1,000원 단위의 수로만 입력할 수 있습니다.");
     }
   }
+
+  #getLottos(number) {
+    const lottos = [];
+    for(let i=0;i<number;i++) {
+      lottos.push(new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6).sort((a,b)=>a-b)));
+    }
+    this.lottos = lottos;
+  }
+
 };
 
 export default Game;
