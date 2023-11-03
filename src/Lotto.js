73a0afa050f46d67a2a9b923839073290bff8888
rustandbone <dlehdgh14@naver.com>
2023-11-04 00:46:32 +0900

    기능 구현: 로또 구입 금액 입력 및 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,6 +14,20 @@ class Lotto {
     }
   }
 
+  async buyLotto() {
+    const lottoPurchaseMoney =
+      await Console.readLineAsync('구입금액을 입력해 주세요.');
+
+    if (Number.isNaN(+lottoPurchaseMoney))
+      throw new Error('[ERROR] 구입 금액이 잘못되었습니다.');
+
+    if (lottoPurchaseMoney % 1000 !== 0)
+      throw new Error('[ERROR] 구입 금액이 잘못되었습니다.');
+
+    const lottoPieces = lottoPurchaseMoney / 1000;
+    return lottoPieces;
+  }
+
   createLottoNumber() {
     const lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
     return lottoNumber.sort((a, b) => a - b);
