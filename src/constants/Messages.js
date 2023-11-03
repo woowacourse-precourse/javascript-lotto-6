094d2b01c7bb230019d9d6d4342a60758b0b5d5d
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-11-03 21:57:40 +0900

    feat : 메세지 내용 추가

---
+++ src/constants/Messages.js
@@ -0,0 +1,11 @@
+const INPUT_MESSAGES = {
+  INPUT_CASH_MESSAGE: "구입금액을 입력해 주세요.\n",
+  INPUT_WINNING_NUMBER: "당첨 번호를 입력해 주세요.\n",
+  INPUT_BOUNUS_NUMBER: "보너스 번호를 입력해 주세요.\n",
+};
+
+const OUTPUT_MESSAGES = {
+  OUTPUT_PURCHASE_QUANTITY: "개를 구매했습니다.",
+};
+
+export { INPUT_MESSAGES, OUTPUT_MESSAGES };
