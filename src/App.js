9b3266b514779885c1ca8353a07b78d9c7f66763
Narachan97 <qkrtkf3112@naver.com>
2023-11-03 21:46:13 +0900

    feat(): 6개의 랜덤 숫자 , 중복체크
    src/App.js 6개의 랜덤 숫자 추가
    src/Lotto.js 랜덤숫자 중복 체크
    
    App.js 새로운 기능
    -1~45의 숫자중 6개를 랜덤숫자를 추출해서 Lotto 클래스의 매개변수가 된다.
    
    Lotto.js 새로운 기능
    -랜덤 숫자의 중복 체크 를 확인한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Lotto from "./Lotto.js";
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const random = lottoRandomNumber();
+    const lotto= new Lotto(random);
+  }
+}
+
+function lottoRandomNumber() {
+   const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+   return Array.from(randomNumbers);
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -6,13 +6,18 @@ class Lotto {
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+ #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+     const uniqueNumbers = new Set(numbers);
+     if (uniqueNumbers.size !== 6) {
+       throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+     }
   }
+  
 
-  // TODO: 추가 기능 구현
+
 }
 
 export default Lotto;
