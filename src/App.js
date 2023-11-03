96daaec124940219acdfe9477fc4fbd646b11dae
ooherin <555ohr@naver.com>
2023-11-03 11:15:41 +0900

    Feat : 로또 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const lottoPurchasePrice =
+      await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
+    const LOTTO_PRICE = 1000;
+    //예외 처리
+    const isValidPrice = (price) => {
+      if (price % LOTTO_PRICE !== 0)
+        return new Error("[ERROR] 유효한 숫자가 아닙니다.");
+    };
+
+    isValidPrice(lottoPurchasePrice);
+    let lottoTickets = lottoPurchasePrice / LOTTO_PRICE;
+
+    const lotto = new Lotto();
+  }
 }
 
 export default App;
