00d9b9fc8a98e35bd032e003f6b3bfb4b13094f1
DongWook Kang <rkdehddnr15123@gmail.com>
2023-11-03 13:07:40 +0900

    feat: 로또 생성 기능 구현

--- src/model/Model.js
+++ src/model/Model.js
@@ -1,8 +1,19 @@
+import { Random } from "@woowacourse/mission-utils";
+import Lotto from "../Lotto.js";
 import { validatePriceInput } from "../utils/vaildateInput.js";
 
 class Model {
   makeLotto(priceInput) {
     const price = validatePriceInput(priceInput);
+    const lotties = [];
+
+    while (lotties.length < price) {
+      const lottoRandomNum = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      const lotto = new Lotto(lottoRandomNum);
+      lotties.push(lotto);
+    }
+
+    return lotties;
   }
 }
 
