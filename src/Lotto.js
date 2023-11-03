000392f87c46c9131b872107e24d3f50a4158a29
zxxng <jeayeong123@naver.com>
2023-11-04 01:16:20 +0900

    feat: 로또 번호의 개수가 6개 이상이거나 중복된 경우 예외 발생 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,7 @@
-class Lotto {
-  #numbers;
+import { Random } from '@woowacourse/mission-utils';
 
+class Lotto {
+  #numbers = [];
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
@@ -8,11 +9,19 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    numbers.forEach((number) => {
+      if (this.duplicateCount(numbers, number) >= 2) {
+        throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+      }
+    });
   }
 
-  // TODO: 추가 기능 구현
+  duplicateCount(numbers, number) {
+    return numbers.filter((currnt) => currnt === number).length;
+  }
 }
 
 export default Lotto;
