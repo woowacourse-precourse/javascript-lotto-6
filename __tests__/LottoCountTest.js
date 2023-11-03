e62c20229b3df34e0f130bca4cf2889e4c530a3a
whai2 <whai2125@gmail.com>
2023-11-03 16:54:25 +0900

    test: 입력 금액이 숫자가 아닐 경우, 예외 처리하는 단위 테스트 선언

--- __tests__/LottoCountTest.js
+++ __tests__/LottoCountTest.js
@@ -1,4 +1,4 @@
-import { lottoCounter } from "../src/App.js";
+import { lottoCounter, inputMoneyValidater } from "../src/App.js";
 
 describe("구입 금액에 따른 로또 개수 테스트", () => {
   test("구입 금액에 따라 올바른 개수의 로또를 발행한다.", () => {
@@ -9,4 +9,16 @@ describe("구입 금액에 따른 로또 개수 테스트", () => {
     expect(result).toBe(OUTPUT);
   });
   // 아래에 추가 테스트 작성 가능
+
+  test.each([
+    [["만 원"]],
+    [["10000.0"]],
+    [["-500"]],
+    [["0b101"]],
+    [["1E3"]],
+  ])("입력이 숫자가 아닐 경우, 에러가 발생한다.", async (inputs) => {
+  
+    // then
+    expect(inputMoneyValidater(inputs)).rejects.toThrow("[ERROR]");
+  });
 });
