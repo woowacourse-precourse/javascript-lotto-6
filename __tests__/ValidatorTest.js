e9f551a1a47b475a6359f5e09b3cb748571491fb
j8won <joowoni99@gmail.com>
2023-11-04 00:04:48 +0900

    test(ValidatorTest): checkArrayForDuplicate 메서드가 적절한 boolean을 반환하는지 테스트 코드 작성

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,13 +1,23 @@
-import Validator from '../src/Validator.js';
+import Validator from "../src/Validator.js";
 
-describe('Validator 클래스 테스트', () => {
+describe("Validator 클래스 테스트", () => {
   test.each([
-    ['false 반환', [1, 2, 3], 2, false],
-    ['true 반환', [1, 2, 3], 3, true],
+    ["false 반환", [1, 2, 3], 2, false],
+    ["true 반환", [1, 2, 3], 3, true],
   ])(
-    '배열의 크기가 주어진 숫자와 같으면 true, 다르면 false 반환 - %s',
+    "배열의 크기가 주어진 숫자와 같으면 true, 다르면 false 반환 - %s",
     (testName, array, number, expectedOutput) => {
       expect(Validator.checkArrayLength(array, number)).toBe(expectedOutput);
-    },
+    }
+  );
+
+  test.each([
+    ["false 반환", [1, 2, 3, 4], false],
+    ["true 반환", [1, 2, 3, 1], true],
+  ])(
+    "배열 내에 중복된 값이 있다면 true, 없다면 false 반환 - %s",
+    (testName, array, expectedOutput) => {
+      expect(Validator.checkArrayForDuplicate(array)).toBe(expectedOutput);
+    }
   );
 });
