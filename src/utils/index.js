bbdea415869b18a4e08f33cb91cf72e627f3657f
noodle <noodle>
2023-11-04 03:35:38 +0900

    feat: utils 동결객체 추가

---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+export const deepFreeze = (object) => {
+  for (let key in object) {
+    if (typeof object[key] === 'object' && object[key] !== null) {
+      deepFreeze(object[key]);
+    }
+  }
+  return Object.freeze(object);
+};
