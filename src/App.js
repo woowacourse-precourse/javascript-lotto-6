916bfada3c9cac17bc478ef95d124862e5a1a6ae
whai2 <whai2125@gmail.com>
2023-11-03 14:40:33 +0900

    feat: 로또 구입 금액을 입력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    await inputMoney();
+  }
 }
 
 export default App;
+
+PURCASE_COMMENT = "구입금액을 입력해 주세요."
+
+async function inputMoney() {
+  MissionUtils.Console.print(PURCASE_COMMENT);
+  const totalMoney = await MissionUtils.Console.readLineAsync('');
+  return totalMoney;
+}
