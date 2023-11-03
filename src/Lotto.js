468f67b6c5e2fc3249eaa44c53d54d0f49cd8176
chiman <rlaclghks123@naver.com>
2023-11-03 17:37:27 +0900

    feat: 유효성 검사 : 로또 번호가 중복되는 경우 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,17 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    this.#validateDuplicated(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  #validateDuplicated(numbers) {
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호가 중복되면 안됩니다.');
+    }
+  }
 }
 
 export default Lotto;
