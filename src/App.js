1bed3f6c27c671f3022431916d5bbf7ed0f95f38
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-03 16:42:33 +0900

    Feat: 구매금액 입력 기능 및 에러 처리, 구매 개수 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { Console, Random } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  constructor() {
+    this.purchasePrice = 0;
+    this.purchaseCount = 0;
+    this.randomLottoNumber = [];
+  }
+  async play() {
+    this.purchasePrice = await this.inputMyLottoNumber();
+    this.isValidInputPrice = this.isValidInputPrice();
+    this.printPriceError(this.isValidInputPrice);
+    this.purchaseCount = this.calculatePurchaseCount();
+    this.printPurchaseCount();
+  }
+  async inputMyLottoNumber() {
+    const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    return +input;
+  }
+
+  isValidInputPrice() {
+    return this.purchasePrice % 1000 === 0;
+  }
+  printPriceError(isValid) {
+    if (!isValid) {
+      throw new Error("[ERROR] 1,000원 단위의 금액을 입력해 주세요.");
+    }
+  }
+
+  calculatePurchaseCount() {
+    return this.purchasePrice / 1000;
+  }
+
+  printPurchaseCount() {
+    Console.print(`${this.purchaseCount}개를 구매했습니다.`);
+  }
 }
 
 export default App;
