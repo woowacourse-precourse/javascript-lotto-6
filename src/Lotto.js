059beddc13ccbfd002233b92e781d52466736b13
leedbswo107 <ljyu1@naver.com>
2023-11-03 22:54:32 +0900

    feat: 숫자 예외 조건 코드 추가 업데이트

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE } from "./common/OutputMessage";
+
 class Lotto {
   #numbers;
 
@@ -7,11 +9,11 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) throw new Error(ERROR_MESSAGE.NUMBER_COUNT);
+    if (numbers.length !== new Set(numbers).length) throw new Error(ERROR_MESSAGE.NUMBER_DUPLICATE);
+    if ((numbers<46 && numbers > 0) !== true) throw new Error(ERROR_MESSAGE.NUMBER_RANGE);
+    if (typeof numbers !== number) throw new Error(ERROR_MESSAGE.NUMBER_TYPE);
   }
-
   // TODO: 추가 기능 구현
 }
 
