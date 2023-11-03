b876d2cda8eba001b8a2e0d6519dcfbf45e905df
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 23:41:43 +0900

    Test: LottoYeildCaculator에서 로또 1등하는 경우 테스트 코드 추가

--- __tests__/Service/LottoYieldCalculatorTest.js
+++ __tests__/Service/LottoYieldCalculatorTest.js
@@ -1,11 +1,23 @@
 import DrawnLotto from "../../src/Models/DrawnLotto.js";
 import Lotto from "../../src/Models/Lotto.js";
+import LOTTO_SETTINGS from "../../src/config/gameSetting.js";
 import LottoYieldCalculator from "../../src/service/LottoYieldCalculator.js";
 
 describe("LottoYeildCaculator 클래스 테스트", () => {
+  const { PRIZES } = LOTTO_SETTINGS;
   test("5개 맞으면 보너스 번호도 체크해야한다.", () => {
     const lotto = new Lotto(["1", "2", "3", "4", "5", "8"]);
     const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "5", "6"], "8");
-    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(6);
+    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
+      PRIZES.SECOND
+    );
+  });
+
+  test("5개 맞으면 보너스 번호도 체크해야한다.", () => {
+    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
+    const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "5", "6"], "8");
+    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
+      PRIZES.FIRST
+    );
   });
 });
