84f5181a59437b50b39a9a700481cbb4ea83a8bb
fecapark <fecapark@gmail.com>
2023-11-03 17:37:48 +0900

    feat: 구입 금액 입력에 대한 에러 클래스 구현

---
+++ src/utils/Error.js
@@ -0,0 +1,34 @@
+import { DEFAULT_ERROR_MESSAGE } from "../core/const";
+
+export class PurchaseValueError extends Error {
+  static TYPE_NOT_NUMBER = 0;
+  static TYPE_HAS_MONEY_CHANGE = 1;
+  static TYPE_NOT_PURCHASED = 2;
+
+  /**
+   * @param {number} errorType
+   */
+  constructor(errorType = -1) {
+    super(PurchaseValueError.#getMessage(errorType));
+  }
+
+  /**
+   *
+   * @param {number} type
+   */
+  static #getMessage(type) {
+    if (type === InvalidPlayerNameError.TYPE_NOT_NUMBER) {
+      return `${DEFAULT_ERROR_MESSAGE} 올바르지 않은 숫자 형식입니다.`;
+    }
+
+    if (type === InvalidPlayerNameError.TYPE_HAS_MONEY_CHANGE) {
+      return `${DEFAULT_ERROR_MESSAGE} 입력한 금액이 1000원 단위가 아닙니다.`;
+    }
+
+    if (type === InvalidPlayerNameError.TYPE_NOT_PURCHASED) {
+      return `${DEFAULT_ERROR_MESSAGE} 최소 1개 이상을 구입하셔야 합니다.`;
+    }
+
+    return DEFAULT_ERROR_MESSAGE;
+  }
+}
