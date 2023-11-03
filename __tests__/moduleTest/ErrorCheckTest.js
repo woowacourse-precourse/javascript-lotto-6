516f623d6b5155a54339ba7a6dc7a4c694ff6914
ss0526100 <ss0526100@gmail.com>
2023-11-03 14:19:15 +0900

    test: make number in range test

--- __tests__/moduleTest/ErrorCheckTest.js
+++ __tests__/moduleTest/ErrorCheckTest.js
@@ -34,15 +34,19 @@ describe('ErrorCheck 테스트', () => {
   });
 
   test.each([
-    [['100000', 6], false],
-    [['1,2,3,4,5,6'.split(','), 6], false],
-    [[[2, 4, 6, 8, 10], 5], false],
-    [['123', 2], true],
-    [['1,2,3,4'.split(','), 6], true],
-    [[[2, 4, 5, 6, 1], 3], true],
-  ])('arrayLikeLength()', (input, isThrowing) => {
-    const targetFunction = () => ErrorCheck.arrayLikeLength(...input);
+    [[1, 0, 4], false],
+    [[1, 1, 1], false],
+    [[9, 1, 9], false],
+    [[1, 1, 45], false],
+    [[45, 1, 45], false],
+    [[0, 1, 45], true],
+    [[46, 1, 45], true],
+    [[2, 1, 1], true],
+  ])('numberInRange()', (input, isThrowing) => {
+    const targetFunction = () => ErrorCheck.numberInRange(...input);
     if (isThrowing) expect(targetFunction).toThrow(ERROR_FORMAT);
     if (!isThrowing) expect(targetFunction).not.toThrow();
   });
+
+  test.each([]);
 });
