13df906065f58f9c347a631a76cf5b4a81644656
jaeml00 <jaeml0630@gmail.com>
2023-11-03 14:17:46 +0900

    feat: add numbers error handling
    로또 번호 6개가 아닐 때 예외
    로또 번호 중복 번호가 있을 때 예외
    1~45사이의 정수가 아닐 때 예외

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,37 @@
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers = []) {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+  #validate(numbers = []) {
+    this.#validateNumbersLength(numbers);
+    this.#validateUniqueNumbers(numbers);
+    this.#validateNumberRange(numbers);
+  }
+  #validateNumbersLength(numbers = []) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateUniqueNumbers(numbers = []) {
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복되지 않은 6개여야 합니다.');
+    }
+  }
+
+  #validateNumberRange(numbers = []) {
+    if (!numbers.every((number) => this.#isValidNumber(number))) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 정수여야 합니다.');
+    }
+  }
+
+  #isValidNumber(number = []) {
+    return Number.isInteger(number) && number >= 1 && number <= 45;
+  }
 }
 
 export default Lotto;
