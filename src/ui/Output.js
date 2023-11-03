b820622f163927db580ea8ffd5d06b5fb7523a92
Joseph Park <rkfnahs12e@gmail.com>
2023-11-03 23:39:11 +0900

    feat(src/ui) : Output.js 파일에 발행된 로또를 출력해주는 printLottoNumber함수 구현

--- src/ui/Output.js
+++ src/ui/Output.js
@@ -1,1 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
+
+// 객체를 받아 값 출력
+const Outputs = {
+  printLottoNumber(lottos) {
+    Object.values(lottos).forEach((items) => {
+      Console.print(`[${items.sort((a, b) => a - b).join(', ')}]`);
+    });
+  },
+};
