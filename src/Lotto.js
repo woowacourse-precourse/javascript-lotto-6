46194d362b45bcb36b1ad94b65694a5f5fee7326
장호빈 <devhobin@gmail.com>
2023-11-03 14:23:06 +0900

    feat<Lotto>: 로또 validation 로직, 로또 번호 배열 반환 메서드 추가
    - 로또는 getInformation 메서드 실행시 정렬된 로또번호 배열 사본을 반환한다

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import AppError from "./AppError";
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,16 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new AppError("로또 번호는 6개여야 합니다.");
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new AppError("중복된 로또 번호가 존재합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  getInformation() {
+    return [...this.#numbers].sort((a, b) => a - b);
+  }
 }
 
 export default Lotto;
