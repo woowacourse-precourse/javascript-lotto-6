6d4019c66d1949793feffe8fb5001e6f429cae0d
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-04 00:02:25 +0900

    refactor(Message): 인자를 받아야 하는 상수는 prefix로 변경

--- src/static/Message.js
+++ src/static/Message.js
@@ -1,7 +1,10 @@
 const MESSAGE = Object.freeze({
   purchase: "구입금액을 입력해 주세요.\n",
-  winningNumber: "\n당첨 번호를 입력해 주세요.",
-  bonusNumber: "\n보너스 번호를 입력해 주세요.",
+  purchaseAmount: "개를 구매했습니다.\n",
+  winningNumber: "\n당첨 번호를 입력해 주세요.\n",
+  bonusNumber: "\n보너스 번호를 입력해 주세요.\n",
+  revenuePrefix: "총 수익률은",
+  revenueSuffix: "%입니다.\n",
   resultStatic: "\n당첨통계\n---",
 });
 
