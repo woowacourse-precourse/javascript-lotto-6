106298ca01061808ec9f7cbe0f15714dc08377fb
yong seock <myongdol132@gmail.com>
2023-11-03 17:04:05 +0900

    Feat: 당첨번호 입력 구현
    - 로또 구매금액에 대한 사용자 입력을 추가하였습니다.
    - 입력된 금액에 따라 로또를 발급합니다.
    - 6개의 로또 당첨번호에 대한 사용자 입력이 추가되었습니다.
    - 금액 또는 번호 입력시 오류에 대한 메시지를 출력합니다.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     const amount = await this.inputAmount();
+    const winningNumbers = await this.inputWinningNumbers();
     this.issueLottos(amount);
   };
   
@@ -28,10 +29,21 @@ class App {
     for (let i = 0; i < lottoCount; i++) {
       const numbers = Lotto.generateNumbers();
       this.lottos.push(new Lotto(numbers));
+      Console.print(`[${numbers}]`);
     }
     
     Console.print(`${lottoCount}개를 구매했습니다.`)
-  }
+  };
+
+  async inputWinningNumbers() {
+    const input = await Console.readLineAsync("6개의 로또 당첨 번호를 입력해주세요. (쉼표로 구분):");
+    const numbers = input.split(',').map(num => parseInt(num.trim(), 10));
+
+    if (numbers.length !== 6 || numbers.some(num => num < 1 || num > 45)) {
+      Console.print("[ERROR] 올바른 로또 번호를 입력해주세요!");
+      return this.inputWinningNumbers();
+    }
+  };
   
 };
 
