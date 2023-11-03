df3c621d48d2ba4e1a49df4a64584c4cf226a4b7
Moon Sojeong <sjeong35@naver.com>
2023-11-03 22:13:13 +0900

    fix: 다른 예외를 발생시키는 오류 수정

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ class App {
 
   checkLottoPrice(lottoPrice) {
     // test code
+    if (isNaN(lottoPrice)) throw new Error('[ERROR] 숫자만 입력하세요.');
     if (lottoPrice < 1000)
       throw new Error('[ERROR] 로또 최소 구입 금액은 1000원입니다.');
     if (Number(lottoPrice) % 1000 !== 0)
       throw new Error('[ERROR] 금액은 1,000원 단위로 입력해주세요');
-    if (isNaN(lottoPrice)) throw new Error('[ERROR] 숫자만 입력하세요.');
   }
 
   makeRandomNumber() {
