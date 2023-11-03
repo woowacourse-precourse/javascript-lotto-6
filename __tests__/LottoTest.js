d3c14e0cff4b804146efe6e2d7e972b3d2105a99
장호빈 <devhobin@gmail.com>
2023-11-03 14:23:42 +0900

    test<LottoTest>: 로또 getInformation 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,15 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 정보를 가져왔을 때 정렬된 로또 번호의 배열을 반환한다.", () => {
+    const lotto = new Lotto([6, 2, 3, 5, 4, 1]);
+
+    expect(lotto.getInformation()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
