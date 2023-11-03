607f5656d284c6f3c763573aed2ad32368b11a75
Hong sung soo <sow9707@daum.net>
2023-11-03 17:45:59 +0900

    feat: 보너스 번호 유효성 검사 함수 구현

--- src/validation.js
+++ src/validation.js
@@ -20,3 +20,19 @@ function qualificationWinnerNumber(winnerNumber) {
         throw new Error("[ERROR] 숫자를 입력해주세요.");
     }
 }
+
+function validationBonusNumber(bonusNumber, winnerNumbers) {
+    winnerNumbers.forEach((winnerNumber) => qualificationBonusNumber(bonusNumber, winnerNumber));
+    if(bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
+        throw new Error("[ERROR] 1~45 내의 숫자를 입력해주세요.");
+    }
+    if(!+bonusNumber) {
+        throw new Error("[ERROR] 숫자를 입력해주세요.");
+    }
+}
+
+function qualificationBonusNumber(bonusNumber, winnerNumber) {
+    if(bonusNumber == winnerNumber) {
+        throw new Error("[ERROR] 당첨 번호와 다른 번호를 입력하세요.");
+    }
+}
