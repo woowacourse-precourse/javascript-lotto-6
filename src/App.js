b14cfad79b4d3086fb9a503dabeb3c19a5497449
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-03 11:51:01 +0900

    feat(메인 로직): 출력기능 메인 로직에서 분리

--- src/App.js
+++ src/App.js
@@ -1,68 +1,59 @@
-import { Console } from "@woowacourse/mission-utils";
 import InputManager from "./functions/InputManager.js";
-import { devideMoneyForLotto, profitRate } from "./utils/caculate.js";
-import { generateUniqueRandomNumbers } from "./utils/random.js";
-import { sortNumbers } from "./utils/sort.js";
-import { EMPTY_LINE, STATISTICS_MESSAGE } from "./constants/messages.js";
-import { devideIntoCommas } from "./utils/conversion.js";
-import { checkBonusBall, countMatchingBalls } from "./utils/condition.js";
-import { LOTTO_NUMBERS, WINNING_PRIZE_NUMBERS } from "./constants/numbers.js";
+import {
+  devideMoneyForLotto,
+  profitRate,
+  sumOfWinning,
+} from "./utils/caculate.js";
+import {
+  devideIntoCommas,
+  stringToNumber,
+  stringsToNumbers,
+} from "./utils/conversion.js";
+import {
+  checkBonusBall,
+  countMatchingBalls,
+  recordRanks,
+} from "./utils/condition.js";
+import OutputManager from "./functions/OutputManager.js";
 
 class App {
   constructor() {
     this.inputManager = new InputManager();
+    this.outputManager = new OutputManager();
   }
 
   async play() {
     const moneyForLotto = await this.inputManager.moneyForLotto();
-    const numbersOfLotto = devideMoneyForLotto(Number(moneyForLotto));
+    const moneyForLottoToNumber = stringToNumber(moneyForLotto);
 
-    let purchasedLotto = [];
-    for (let i = 0; i < Number(numbersOfLotto); i++) {
-      let newLottoNumbers = sortNumbers(generateUniqueRandomNumbers());
-      Console.print(newLottoNumbers);
-      purchasedLotto.push(newLottoNumbers);
-    }
-    Console.print(EMPTY_LINE);
+    const countsOfLotto = devideMoneyForLotto(moneyForLottoToNumber);
+    const countsOfLottoToNumber = stringToNumber(countsOfLotto);
+
+    const purchasedLotto = this.outputManager.showLottoNumbers(
+      countsOfLottoToNumber
+    );
+    this.outputManager.showEmptyLine();
 
     const winningBalls = await this.inputManager.winningBallNumbers();
-    const winningBallsArr = devideIntoCommas(winningBalls).map(Number);
-    const bonusBall = await this.inputManager.bonusBallNumbers();
+    const winningBallsArr = devideIntoCommas(winningBalls);
+    const winningBallsToNumberArr = stringsToNumbers(winningBallsArr);
 
-    Console.print(countMatchingBalls(purchasedLotto, winningBallsArr));
-    Console.print(checkBonusBall(purchasedLotto, Number(bonusBall)));
+    const bonusBall = await this.inputManager.bonusBallNumbers();
+    const bonusBallToNumber = stringToNumber(bonusBall);
+
+    const countMatchArr = countMatchingBalls(
+      purchasedLotto,
+      winningBallsToNumberArr
+    );
+    const checkBonusArr = checkBonusBall(purchasedLotto, bonusBallToNumber);
 
     // +1을 해서 1등~N등까지 그대로 적용할 수 있도록 하기(시작값 = 0)
-    let rankCounts = Array.from({ length: LOTTO_NUMBERS.ranks + 1 }, () => 0);
-    const winningPrizeSum =
-      rankCounts[1] * WINNING_PRIZE_NUMBERS.rank1 +
-      rankCounts[2] * WINNING_PRIZE_NUMBERS.rank2 +
-      rankCounts[3] * WINNING_PRIZE_NUMBERS.rank3 +
-      rankCounts[4] * WINNING_PRIZE_NUMBERS.rank4 +
-      rankCounts[5] * WINNING_PRIZE_NUMBERS.rank5;
+    const rankCounts = recordRanks(countMatchArr, checkBonusArr);
+    const winningPrizeSum = sumOfWinning(rankCounts);
 
     const earnRate = profitRate(moneyForLotto, winningPrizeSum);
 
-    Console.print(STATISTICS_MESSAGE.winningStatistics);
-    Console.print(STATISTICS_MESSAGE.devideLine);
-    Console.print(
-      `${STATISTICS_MESSAGE.rank5}${rankCounts[5]}${STATISTICS_MESSAGE.unit}`
-    );
-    Console.print(
-      `${STATISTICS_MESSAGE.rank4}${rankCounts[4]}${STATISTICS_MESSAGE.unit}`
-    );
-    Console.print(
-      `${STATISTICS_MESSAGE.rank3}${rankCounts[3]}${STATISTICS_MESSAGE.unit}`
-    );
-    Console.print(
-      `${STATISTICS_MESSAGE.rank2}${rankCounts[2]}${STATISTICS_MESSAGE.unit}`
-    );
-    Console.print(
-      `${STATISTICS_MESSAGE.rank1}${rankCounts[1]}${STATISTICS_MESSAGE.unit}`
-    );
-    Console.print(
-      `${STATISTICS_MESSAGE.finalReturn}${earnRate}${STATISTICS_MESSAGE.finalReturnAfter}`
-    );
+    this.outputManager.showStatistics(rankCounts, earnRate);
   }
 }
 
