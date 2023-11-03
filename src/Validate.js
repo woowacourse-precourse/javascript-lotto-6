222051b5fbbb9aeb634962cd5bfdf651052a0126
zelkovaria <hyein396@gmail.com>
2023-11-03 17:45:34 +0900

    feat: 구입 금액 단위 예외처리 추가

--- src/Validate.js
+++ src/Validate.js
@@ -1,2 +1,11 @@
 import ERROR_MESSAGE from "src/Errors.js";
-class Validator {}
+class Validator {
+  static #MONEY_UNIT = 1000;
+
+  static validateMoneyUnit(inputMoney) {
+    if (inputMoney % Validator.#MONEY_UNIT !== 0) {
+      throw new Error(ERROR_MESSAGE.moneyUnit);
+    }
+  }
+}
+export default Validator;
