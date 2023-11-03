71cfb5804f7dd0484aebae1b167801bfde11182e
ss0526100 <ss0526100@gmail.com>
2023-11-03 14:43:37 +0900

    test: check duplicated number in numbers

--- __tests__/moduleTest/ErrorCheckTest.js
+++ __tests__/moduleTest/ErrorCheckTest.js
@@ -48,5 +48,14 @@ describe('ErrorCheck 테스트', () => {
     if (!isThrowing) expect(targetFunction).not.toThrow();
   });
 
-  test.each([]);
+  test.each([
+    [[1, 2, 3, 4, 5, 6], false],
+    [[1, 8, 9, 7, 6, 5], false],
+    [[1, 8, 9, 7, 6, 1], true],
+    [[1, 1, 1, 1, 1, 1], true],
+  ])('differentElementArray()', (input, isThrowing) => {
+    const targetFunction = () => ErrorCheck.differentElementArray(input);
+    if (isThrowing) expect(targetFunction).toThrow(ERROR_FORMAT);
+    if (!isThrowing) expect(targetFunction).not.toThrow();
+  });
 });
