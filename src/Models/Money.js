485a6a39264abf4e5c25d5ce22323421201ccc8b
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 18:07:01 +0900

    Money 클래스에 입력받은 금액이 1000단위가 아니면 에러 처리 추가

--- src/Models/Money.js
+++ src/Models/Money.js
@@ -1,10 +1,13 @@
 import LottoError from "../Error/LottoError";
+import LOTTO_SETTINGS from "../config/gameSetting";
 
 export default class Money {
   #money;
+
   constructor(money) {
     const formattedMoney = this.#removeCommaAndDot(money);
     this.#validateIsNumber(formattedMoney);
+    this.#validateIsDevided(formattedMoney);
     this.#money = Number(formattedMoney);
   }
 
@@ -14,6 +17,14 @@ export default class Money {
 
   #validateIsNumber(money) {
     if (isNaN(money)) throw new LottoError("숫자를 입력해주세요.");
+  }
+
+  #validateIsDevided(money) {
+    if (Number(money) % 1000 !== 0) {
+      throw new LottoError(
+        `금액은 ${LOTTO_SETTINGS.TICKET_PRICE}단위여야합니다.`
+      );
+    }
   }
 
   getMoney() {
