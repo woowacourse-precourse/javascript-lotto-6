5cda6e71ad643a1865782790acdba84c233d66c7
thumbthing <ilscc0902@gmail.com>
2023-11-04 03:31:09 +0900

    feat: ASC로 정렬된 배열 반환 기능, createPurchaseData에 적용

---
+++ src/util/parse/acendingSortList.js
@@ -0,0 +1,4 @@
+export default function acendingSortList(list) {
+  const sortedArray = list.sort((a, b) => a - b);
+  return sortedArray;
+}
--- src/util/purchase/createPurchaseData.js
+++ src/util/purchase/createPurchaseData.js
@@ -1,10 +1,13 @@
 import ERROR_CODE from '../error/errorCode';
 import { pickSixNumber } from '../libararyFeatures/MissionUtilHandler';
+import acendingSortList from '../parse/acendingSortList';
 
 export default async function createPurchaseData(lottoCount, lottos) {
   try {
     const singleLotto = pickSixNumber();
-    const lottoList = lottos.push(singleLotto);
+    const sortedLotto = acendingSortList(singleLotto);
+    const lottoList = [...lottos];
+    lottoList.push(sortedLotto);
     const isNotPurchased = lottoList.length !== lottoCount;
 
     if (isNotPurchased) {
