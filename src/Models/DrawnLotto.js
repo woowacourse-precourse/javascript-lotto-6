f320e07ddadb9ed61e56995337cc0ef5a2cbc5b0
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 19:46:37 +0900

    Feat: 당첨번호와 보너스 번호가 필드인 DrawnLotto 추가
    6개번호 + 보너스 번호로 이루어져있는 당첨로또(DrawnLotto)를 Lotto를 상속받아 구현

--- src/Models/DrawnLotto.js
+++ src/Models/DrawnLotto.js
@@ -1,22 +1,17 @@
 import Lotto from "./Lotto.js";
 import LottoError from "../Error/LottoError.js";
+import LOTTO_SETTINGS from "../config/gameSetting.js";
 
 class DrawnLotto extends Lotto {
   #bonusNumber;
 
   constructor(numbers, bonusNumber) {
     super(numbers);
-    this.#validatePlusNumber(bonusNumber);
-    this.#bonusNumber = bonusNumber;
+    this.#bonusNumber = Number(bonusNumber);
   }
 
   getPlusNumber() {
     return this.#bonusNumber;
-  }
-
-  #validatePlusNumber(bonusNumber) {
-    if (this.getNumbers().includes(bonusNumber))
-      throw new LottoError("중복된 숫자가 있습니다.");
   }
 }
 
