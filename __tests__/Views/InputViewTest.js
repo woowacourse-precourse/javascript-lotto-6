6d93d2ce75bd648ac378cb5478386bec20980f50
0jenn0 <jenn0.6n@gmail.com>
2023-11-04 03:32:04 +0900

    Test: InputView 로또 보너스 번호 받기 추가

--- __tests__/Views/InputViewTest.js
+++ __tests__/Views/InputViewTest.js
@@ -18,4 +18,12 @@ describe("InputView 클래스 테스트", () => {
 
     expect(numbers).toEqual(["1", "2", "3", "4", "5", "6"]);
   });
+
+  test("로또 보너스 번호 입력 받기", async () => {
+    mockQuestions(["1 "]);
+
+    const numbers = await InputView.promptForBonusNumber();
+
+    expect(numbers).toBe("1");
+  });
 });
