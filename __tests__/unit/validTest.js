74cf9f49298edc3273bfeb5a683dc9d425e55780
thumbthing <ilscc0902@gmail.com>
2023-11-03 14:20:05 +0900

    test: isNaN의 Lotto 적용, 단일 기능 테스트 케이스 추가

--- __tests__/unit/validTest.js
+++ __tests__/unit/validTest.js
@@ -2,6 +2,7 @@ import Lotto from '../../src/Lotto';
 import ERROR_CODE from '../../src/util/error/errorCode';
 import checkHasDuplicate from '../../src/util/validate/checkHasDuplicate';
 import checkHasNoRemainder from '../../src/util/validate/checkHasNoRemainder';
+import { checkIsNaN } from '../../src/util/validate/checkIsNaN';
 
 describe('[Lotto] 유효성 검사 테스트', () => {
   test('로또 번호에 중복된 숫자가 존재할 경우 에러를 발생시킨다', () => {
@@ -9,11 +10,17 @@ describe('[Lotto] 유효성 검사 테스트', () => {
       new Lotto([1, 1, 1, 1, 1, 1]);
     }).toThrow(`${ERROR_CODE.isDuplicated}`);
   });
+
+  test('로또 번호에 유효하지 않은 값이 존재할 경우 에러를 발생시킨다.', () => {
+    expect(() => {
+      new Lotto([1, '2', 'q', 'zx', '-', '*']);
+    }).toThrow(`${ERROR_CODE.valueIsNaN}`);
+  });
 });
 
 describe('[function] 유효성 검사 테스트 ', () => {
   test('구입 금액이 1000으로 나누어 떨어지지 않는 경우 에러를 발생시킨다', () => {
-    // given
+    // give
     const input = 9999999;
 
     // when
@@ -22,5 +29,20 @@ describe('[function] 유효성 검사 테스트 ', () => {
     expect(() => {
       checkHasNoRemainder(input);
     }).toThrow(`${ERROR_CODE.hasRemainder}`);
+  });
+
+  test('입력값이 숫자 이외의 값이 결우 에러를 발생시킨다', () => {
+    // given
+    const firstTestInput = 'a';
+    const secondTestInput = ';';
+
+    // thedn
+    expect(() => {
+      checkIsNaN(firstTestInput);
+    }).toThrow(`${ERROR_CODE.valueIsNaN}`);
+
+    expect(() => {
+      checkIsNaN(secondTestInput);
+    }).toThrow(`${ERROR_CODE.valueIsNaN}`);
   });
 });
