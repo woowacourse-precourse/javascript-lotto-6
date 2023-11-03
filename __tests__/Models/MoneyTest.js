b9762f5a938bd8358bd92ee049b4043f67329e61
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 17:59:16 +0900

    Money 클래스에서 숫자가 아닌값 입력받으면 에러처리하기 테스트 코드 추가

--- __tests__/Models/MoneyTest.js
+++ __tests__/Models/MoneyTest.js
@@ -1,8 +1,14 @@
+import LottoError from "../../src/Error/LottoError.js";
 import Money from "../../src/Models/Money.js";
 
 describe("Money 클래스 테스트", () => {
   test("1,000과 1.0000 포맷의 금액도 입력 받을 수 있어야 한다.", () => {
     expect(new Money("1,000").getMoney()).toBe(1000);
     expect(new Money("1.000").getMoney()).toBe(1000);
+  });
+
+  test("숫자가 아닌 값을 입력 받으면 에러 처리해야한다.", () => {
+    expect(() => new Money("1.천")).toThrow(LottoError.PREFIX);
+    expect(() => new Money("천원")).toThrow(LottoError.PREFIX);
   });
 });
