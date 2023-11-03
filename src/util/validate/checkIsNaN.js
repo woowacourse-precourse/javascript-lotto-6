eb9437a140b189e3732fcfe10f903637929a7ff4
thumbthing <ilscc0902@gmail.com>
2023-11-04 02:06:27 +0900

    refactor: error를 통일된 방식으로 예외처리하도록 변경

--- src/util/validate/checkIsNaN.js
+++ src/util/validate/checkIsNaN.js
@@ -3,6 +3,7 @@ import ERROR_CODE from '../error/errorCode.js';
 export function checkIsNaN(verifyValue) {
   const parsedValue = Number(verifyValue);
   const IsInputNaN = Number.isNaN(parsedValue);
+
   if (IsInputNaN) {
     throw new Error(`${ERROR_CODE.valueIsNaN}`);
   }
