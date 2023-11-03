48a31aa2085c9f81d3e43b7bdda229b2f78f3c1d
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-04 07:36:04 +0900

    :sparkles: feat: 에러 메세지 상수화 로또 번호에 대해 유효성 검증 예외시 출력되는 메세지들을 상수화시킴

---
+++ src/constants/messages.js
@@ -0,0 +1,8 @@
+const ERROR_MESSAGE = Object.freeze({
+  invalidLength: '[ERROR] 로또 번호는 6개여야 합니다.',
+  invalidNumberic: '[ERROR] 숫자가 잘못된 형식입니다.',
+  invalidRange: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  invalidUnique: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
+});
+
+export { ERROR_MESSAGE };
