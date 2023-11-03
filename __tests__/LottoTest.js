03778bb4c2a5031b551f728bb2f20483ae65a512
Moon Sojeong <sjeong35@naver.com>
2023-11-03 23:25:28 +0900

    test: Lotto 클래스 #validate 함수에 대한 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,7 @@
 import Lotto from "../src/Lotto.js";
 
 describe("로또 클래스 테스트", () => {
+
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -14,5 +15,18 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호 2개가 1~45 사이 숫자가 아닐 때 length는 2일까 ?", () => {
+    const testNumbers = [3,56,4,7,9,89];
+    expect(() => {
+      new Lotto(testNumbers);
+    }).toThrow('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+  });
+
+  test("로또 번호 2개가 1~45 사이 숫자가 아닐 때 length는 2일까 ?", () => {
+    const testNumbers = [];
+    expect(() => {
+      new Lotto(testNumbers);
+    }).toThrow('[ERROR] 로또 번호를 입력하세요.');
+  });
+  
 });
