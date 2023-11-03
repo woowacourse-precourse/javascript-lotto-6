a14604390e53b682ded42d986643d6589c60c6f4
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 21:05:32 +0900

    Fix(test) : DrawnLotto 테스트 코드들 number인풋들 string인풋으로 수정

--- __tests__/Models/DrawnLottoTest.js
+++ __tests__/Models/DrawnLottoTest.js
@@ -4,13 +4,13 @@ import LOTTO_SETTINGS from "../../src/config/gameSetting";
 
 describe("DrawnLotto 클래스 테스트", () => {
   test("당첨번호와 보너스 번호가 중복되면 에러 처리해야한다.", () => {
-    expect(() => new DrawnLotto([1, 2, 3, 4, 5, 6], 6)).toThrow(
+    expect(() => new DrawnLotto(["1", "2", "3", "4", "5", "6"], "6")).toThrow(
       LottoError.PREFIX
     );
   });
 
   test(`보너스 번호는 ${LOTTO_SETTINGS.NUMBER_RANGE.MIN} 이상  ${LOTTO_SETTINGS.NUMBER_RANGE.MAX} 이하여야한다.`, () => {
-    expect(() => new DrawnLotto([1, 2, 3, 4, 5, 6], 90)).toThrow(
+    expect(() => new DrawnLotto(["1", "2", "3", "4", "5", "6"], "90")).toThrow(
       LottoError.PREFIX
     );
   });
