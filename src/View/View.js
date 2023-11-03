957664d818f0af30905bee8200ed9dd21356b128
dyddks <dyddks0115@naver.com>
2023-11-03 21:43:36 +0900

    fix: 보너스번호 입력 수정

--- src/View/View.js
+++ src/View/View.js
@@ -14,12 +14,11 @@ class View {
     const winningNumber = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
     this.#winningNumber = winningNumber.split(',');
     this.#winningNumber = this.#winningNumber.map(num => Number(num));
-    Console.print(this.#winningNumber);
     return this.#winningNumber;
   }
 
   async inputBonus() {
-    this.#bonusNumber = await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    this.#bonusNumber = Number(await Console.readLineAsync('보너스 번호를 입력해 주세요.\n'));
     return this.#bonusNumber;
   }
 
@@ -31,11 +30,13 @@ class View {
     for (let i=0; i<lottoNumber.length; i+=1) {
       Console.print(lottoNumber[i]);
     }
+
     Console.print('');
   }
 
   printResult(result) {
-    Console.print('당첨 통계');
+    Console.print('\n당첨 통계');
+    Console.print('---');
     Console.print(`3개 일치 (5,000원) - ${result[0]}개`);
     Console.print(`4개 일치 (50,000원) - ${result[1]}개`);
     Console.print(`5개 일치 (1,500,000원) - ${result[2]}개`);
