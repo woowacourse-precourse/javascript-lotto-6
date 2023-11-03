720fd8cb052a3ee47edd1d74899c3b5947259ca7
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-04 08:40:14 +0900

    :white_check_mark: test: 로또번호와 당첨번호 비교기능 테스트 코드추가 로또번호와 당첨번호가 얼마나 일치하는지 카운팅한 수를 반환한다. 당첨번호와 비교해서 카운팅한 수와, 보너스번호의 포함여부를 반환하는지 테스트한다

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -16,12 +16,32 @@ describe('로또 클래스 테스트', () => {
   test('로또 번호에 보너스 번호가 포함되어 있으면 true를 반환한다.', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const hasBonus = lotto.hasContainBonusNumber(6);
+
     expect(hasBonus).toBe(true);
   });
 
   test('로또 번호에 보너스 번호가 포함되어 있지 않으면 false를 반환한다.', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     const hasBonus = lotto.hasContainBonusNumber(7);
+
     expect(hasBonus).toBe(false);
+  });
+
+  test('로또 번호와 당첨 번호를 비교하여 일치하는 숫자 개수를 반환한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningNumbers = [1, 2, 3, 7, 8, 9];
+    const matchingCount = lotto.countMatchWinningNumbers(winningNumbers);
+
+    expect(matchingCount).toBe(3);
+  });
+
+  test('로또 번호와 당첨 번호, 보너스 번호를 비교하여 결과를 반환한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningNumbers = [1, 2, 3, 7, 8, 9];
+    const bonusNumber = 6;
+    const result = lotto.getLottoComparisonResults(winningNumbers, bonusNumber);
+
+    expect(result.matchingCount).toBe(3);
+    expect(result.hasBonusNumber).toBe(true);
   });
 });
