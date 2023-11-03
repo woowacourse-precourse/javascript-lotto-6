59da7650d02aad542c01aab430f7e7976e63ebe5
whai2 <whai2125@gmail.com>
2023-11-03 16:47:26 +0900

    fix(lottoCounter test): 변수 미선언 에러 수정

--- __tests__/LottoCountTest.js
+++ __tests__/LottoCountTest.js
@@ -2,8 +2,8 @@ import { lottoCounter } from "../src/App.js";
 
 describe("구입 금액에 따른 로또 개수 테스트", () => {
   test("구입 금액에 따라 올바른 개수의 로또를 발행한다.", () => {
-    INPUT = 10000;
-    OUTPUT = 10;
+    const INPUT = 10000;
+    const OUTPUT = 10;
 
     const result = lottoCounter(INPUT);
     expect(result).toBe(OUTPUT);
