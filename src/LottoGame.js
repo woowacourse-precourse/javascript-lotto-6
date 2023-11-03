603caa32c10a67ddfbe5c619fbd286a63c209912
PARK-CHANUK <doosan1224@naver.com>
2023-11-03 17:31:41 +0900

    feat: 일치갯수를 통해 수익 계산 기능구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -5,25 +5,43 @@ export default class LottoGame {
 
   #bonus;
 
+  #result;
+
+  #income;
+
   constructor(autoLottos, winningLotto, bonus) {
     this.#autoLottos = autoLottos;
     this.#winningLotto = winningLotto;
     this.#bonus = bonus;
+    this.#result = Array.from({ length: this.#autoLottos.length }).fill(0);
+    this.#income = 0;
   }
 
   getWinningResult() {
-    const result = Array.from({ length: this.#autoLottos.length }).fill(0);
     this.#autoLottos.reduce((acc, autoLotto, index) => {
       acc.forEach((winningNum) => {
-        if (autoLotto.includes(winningNum)) result[index] += 1;
+        if (autoLotto.includes(winningNum)) this.#result[index] += 1;
       });
-      if (result[index] === 5) {
-        result[index] = autoLotto.includes(this.#bonus)
-          ? [result[index], true]
-          : [result[index], false];
+      if (this.#result[index] === 5) {
+        this.#result[index] = autoLotto.includes(this.#bonus)
+          ? [this.#result[index], true]
+          : [this.#result[index], false];
       }
       return acc;
     }, this.#winningLotto);
-    return result;
+
+    return this.#result;
+  }
+
+  getIncome() {
+    this.#result.forEach((matchCount) => {
+      if (matchCount === 3) this.#income += 5000;
+      if (matchCount === 4) this.#income += 50000;
+      if (Array.isArray(matchCount)) {
+        this.#income += matchCount[1] ? 30000000 : 1500000;
+      }
+      if (matchCount === 6) this.#income += 2000000000;
+    });
+    return this.#income;
   }
 }
