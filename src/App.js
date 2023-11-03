6d5d195e8d2ab0550d3f70c0ae148f3c6f1cd6a1
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-11-04 01:47:48 +0900

    feature : 사용자 로또 구입 금액 입력 기능 분리

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ class App {
 }
 
 class LottoPurchase {
+  async #lottoAmount() {
+    return parseInt(
+      await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.")
+    );
+  }
+
   async #askPurchaseLottoAmount() {
     let amount;
 
     while (true) {
       try {
-        amount = parseInt(
-          await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.")
-        );
+        amount = await this.#lottoAmount;
 
         if (!amount || amount < 1000 || amount % 1000 !== 0) {
           throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
