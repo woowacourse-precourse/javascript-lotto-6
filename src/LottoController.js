22bd607fa90ae4c6e9142d499a60e9f75bd97ec0
j8won <joowoni99@gmail.com>
2023-11-04 00:44:00 +0900

    feat(LottoController): 사용자가 입력한 구입 금액이 숫자 외의 문자로 이루어졌다면 예외 발생시키는 validateTotalPrice 메서드 생성

---
+++ src/LottoController.js
@@ -0,0 +1,18 @@
+import Validator from "./Validator.js";
+import LOTTO_CONTROLLER from "./constant/LOTTO_CONTROLLER.js";
+
+class LottoController {
+  #ticketAmount;
+
+  constructor(totalPrice) {
+    this.#validateTotalPrice(totalPrice);
+  }
+
+  #validateTotalPrice(string) {
+    if (!Validator.isStringOnlyDigits(string)) {
+      throw new Error(LOTTO_CONTROLLER.ERROR.PRICE_NOT_NUMBER);
+    }
+  }
+}
+
+export default LottoController;
---
+++ src/constant/LOTTO_CONTROLLER.js
@@ -0,0 +1,6 @@
+const ERROR = {
+  PRICE_NOT_NUMBER: "[ERROR] 구입 금액은 숫자만 입력해 주세요.",
+};
+
+Object.freeze(ERROR);
+export default { ERROR };
