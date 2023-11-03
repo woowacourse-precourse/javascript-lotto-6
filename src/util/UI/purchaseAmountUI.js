423cdd68dd6bd64e63a5d533f982fcc8d9287142
thumbthing <ilscc0902@gmail.com>
2023-11-04 05:14:29 +0900

    feat: 콘솔창에 표현될 UI 기능 구현

---
+++ src/util/UI/purchaseAmountUI.js
@@ -0,0 +1,12 @@
+import ERROR_CODE from '../error/errorCode.js';
+import { consoleInput } from '../libararyFeatures/MissionUtilHandler.js';
+
+export default async function purchaseAmountUI() {
+  try {
+    const GUIDANCE_TEXT = '구입금액을 입력해 주세요 \n';
+    const input = await consoleInput(GUIDANCE_TEXT);
+    return input;
+  } catch (error) {
+    throw new Error(`${ERROR_CODE.purchaseAmount}`);
+  }
+}
---
+++ src/util/UI/purchaseResultUI.js
@@ -0,0 +1,11 @@
+import { consolePrint } from '../libararyFeatures/MissionUtilHandler.js';
+
+export default async function purchaseResaultUI(count, purchaseData) {
+  const purchaseCount = count;
+  const guidanceText = `${purchaseCount}개를 구매했습니다.`;
+
+  consolePrint(guidanceText);
+  purchaseData.forEach((element) => {
+    consolePrint(element);
+  });
+}
