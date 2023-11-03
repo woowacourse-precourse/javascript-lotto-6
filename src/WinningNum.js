10af19e1723fb1c9e45c017726d9d2c750d51af0
zelkovaria <hyein396@gmail.com>
2023-11-04 02:23:32 +0900

    rename: askWinningNum 함수명 변경

--- src/WinningNum.js
+++ src/WinningNum.js
@@ -5,7 +5,7 @@ class WinningNum {
   static #WINNINGNUM_INPUT = "당첨 번호를 입력해 주세요.\n";
   static #BONUSNUM_INPUT = "보너스 번호를 입력해 주세요.\n";
 
-  async askWinngNum() {
+  async askWinningNum() {
     const userInputWinningNum = await MissionUtils.Console.readLineAsync("");
     const winningNum = userInputWinningNum.split(",");
     Validator.validateInputMoney(winningNum);
