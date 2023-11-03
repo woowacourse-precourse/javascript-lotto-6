03c85b737bb429112a0e352db9b7990c282108fe
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-03 17:15:08 +0900

    [refactor] : 유효성 검사를 위한 문자열 정수 선언

--- src/constants/validate.js
+++ src/constants/validate.js
@@ -1,1 +1,7 @@
-const ERROR_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다."
+const ERROR_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
+const ERROR_NUMBER_DUPLICATION = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
+const ERROR_NUMBER_RANGE =
+  "[ERROR] 로또 번호는 1부터 45 사이의 숫자만 해당합니다.";
+const ERROR_NUMBER_TYPE = "[ERROR] 로또 번호는 정수형 숫자 외에는 입력하지 않아야 합니다."
+
+export { ERROR_NUMBER_COUNT, ERROR_NUMBER_DUPLICATION, ERROR_NUMBER_RANGE, ERROR_NUMBER_TYPE };
