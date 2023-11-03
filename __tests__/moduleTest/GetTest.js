1a725ed50e524f2d4500ef7568c2617b02fd77fc
ss0526100 <ss0526100@gmail.com>
2023-11-03 11:18:08 +0900

    test: modify Get test

--- __tests__/moduleTest/GetTest.js
+++ __tests__/moduleTest/GetTest.js
@@ -3,7 +3,7 @@ import Get from '../../src/modules/Get.js';
 describe('Get 테스트', () => {
   test.each([[1], [2], [6], [10]])('lottoArray', input => {
     //when
-    const lottoArray = Get.lottoArray(input);
+    const lottoArray = Get.randomLottoArray(input);
 
     //then
     expect(lottoArray).toHaveLength(input);
