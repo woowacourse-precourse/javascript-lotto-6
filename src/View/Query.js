d1e849b8d22713d2d88503f69169b759d7ded472
jaeml00 <jaeml0630@gmail.com>
2023-11-03 15:45:08 +0900

    fix: add return purchaseAmount
    리턴 값 추가

--- src/View/Query.js
+++ src/View/Query.js
@@ -1,9 +1,11 @@
+import { Console, Random } from "@woowacourse/mission-utils";
 class Query {
   static async getPurchaseAmount() {
     try {
       const purchaseAmount = await Console.readLineAsync(
         '구입금액을 입력해 주세요.\n'
       );
+      return purchaseAmount;
     } catch (error) {
       throw new Error('ERROR')
     }
