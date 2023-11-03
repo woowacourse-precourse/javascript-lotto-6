ba8006114fec09e146891f0d15c77fc3823719f2
0jenn0 <jenn0.6n@gmail.com>
2023-11-04 03:29:53 +0900

    Test: InputView의 로또 당첨번호 입력받기 테스트 코드 추가

---
+++ __tests__/Views/InputViewTest.js
@@ -0,0 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "../../src/Views/InputView";
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe("InputView 클래스 테스트", () => {
+  test("로또 당첨 번호 입력 받기", async () => {
+    mockQuestions(["1, 2, 3, 4, 5, 6, "]);
+
+    const numbers = await InputView.promptForDrawnLottoNumbers();
+
+    expect(numbers).toEqual(["1", "2", "3", "4", "5", "6"]);
+  });
+});
