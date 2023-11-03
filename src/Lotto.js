82abfb56151f455a51b852ab37acd84df68569a7
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-03 17:14:45 +0900

    [feat] : Add 사용자 입력에 대한 예외 처리 관련 유효성 검사

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,10 @@
+import {
+  ERROR_NUMBER_COUNT,
+  ERROR_NUMBER_DUPLICATION,
+  ERROR_NUMBER_RANGE,
+  ERROR_NUMBER_TYPE,
+} from "./constants/validate";
+
 class Lotto {
   #numbers;
 
@@ -8,6 +15,9 @@ class Lotto {
 
   #validate(numbers) {
     this.#validateNumberCount(numbers);
+    this.#validateDuplication(numbers);
+    this.#validateNumberRange(numbers);
+    this.#validateNumberType(numbers);
   }
 
   // TODO: 추가 기능 구현
@@ -17,8 +27,31 @@ class Lotto {
 
   #validateNumberCount(numbers) {
     if (numbers.length !== 6) {
-      this.#occurError("[ERROR] 로또 번호는 6개여야 합니다.");
+      this.#occurError(ERROR_NUMBER_COUNT);
     }
+  }
+
+  #validateDuplication(numbers) {
+    const checkedNumbers = new Set(numbers);
+    if (checkedNumbers.size !== 6) {
+      this.#occurError(ERROR_NUMBER_DUPLICATION);
+    }
+  }
+
+  #validateNumberRange(numbers) {
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        this.#occurError(ERROR_NUMBER_RANGE);
+      }
+    });
+  }
+
+  #validateNumberType(numbers) {
+    numbers.forEach((number) => {
+      if (isNaN(number)) {
+        this.#occurError(ERROR_NUMBER_TYPE);
+      }
+    });
   }
 }
 
