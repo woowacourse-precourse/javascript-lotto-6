2d98557c98c63b924cc1a29dff80a76bdcd001c1
mieum12 <qpdlqltb1215@gmail.com>
2023-11-03 19:29:45 +0900

    feat: 사용자 입력값을 받는 Input View 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  async play() {}
+import {LottoController} from "./controller/LottoController.js";
+import {InputView} from "./view/InputView.js";
+import {OutputView} from "./view/OutputView.js";
+import {InputValidator} from "./view/InputValidator.js";
+import {InputConverter} from "./view/InputConverter.js";
+
+export class App {
+    async play() {
+
+        const lottoController = new LottoController(new InputView(new InputValidator(), new InputConverter()), new OutputView());
+        await lottoController.start()
+    }
 }
 
-export default App;
+
---
+++ src/view/InputView.js
@@ -0,0 +1,65 @@
+import {Console} from "@woowacourse/mission-utils";
+import {InputValidator} from "./InputValidator.js";
+import {InputConverter} from "./InputConverter.js";
+
+export class InputView {
+    /**
+     * @type {InputValidator}
+     */
+    #inputValidator;
+    /**
+     *
+     * @type {InputConverter}
+     */
+    #inputConverter;
+
+    /**
+     *
+     * @param {InputValidator} inputValidator
+     * @param {InputConverter} inputConverter
+     */
+
+    constructor(inputValidator, inputConverter) {
+        this.#inputValidator = inputValidator;
+        this.#inputConverter = inputConverter;
+    }
+
+    /**
+     *
+     * @return {Promise<number>}
+     * @description - [1. 구입 금액 입력]
+     *
+     * 검증 한 후 `number`로 변환해서 리턴
+     */
+    async inputMoney() {
+        const input = await Console.readLineAsync("구입금액을 입력해 주세요.")
+        this.#inputValidator.validateMoneyInput(input)
+        return this.#inputConverter.convertToMoney(input)
+    }
+
+    /**
+     *
+     * @return {Promise<WinningNumbers>}
+     * @description - [2. 당첨 번호 입력]
+     *
+     * 검증 한 후 리스트로 변환해서 리턴
+     */
+    async inputWinningNumbers() {
+        const input = await Console.readLineAsync("당첨 번호를 입력해 주세요.")
+        this.#inputValidator.validateWinningNumberInput(input)
+        return this.#inputConverter.convertToWinningNumbers(input)
+    }
+
+    /**
+     *
+     * @return {Promise<number>}
+     * @description - [3.보너스 번호 입력]
+     *
+     * 검증 한 후 `number`로 변환해서 리턴
+     */
+    async inputBonusNumber() {
+        const input = await Console.readLineAsync("보너스 번호를 입력해 주세요.")
+        this.#inputValidator.validateBonusNumberInput(input)
+        return this.#inputConverter.convertToBonusNumber(input)
+    }
+}
