5535db6847668ba839e1ccc1ae771478cf6aff9c
kimdw524 <kimdw524@naver.com>
2023-11-04 01:48:16 +0900

    test: 로또 자동 발급 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,36 @@
-import Lotto from "../src/Lotto.js";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from '../src/models/Lotto.js';
+import { OPTION } from '../src/constants/Lotto.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
+};
+
+describe('로또 클래스 테스트', () => {
+  test(`로또 번호의 개수가 ${OPTION.BALL_COUNT}개가 넘어가면 예외가 발생한다.`, () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // 아래에 추가 테스트 작성 가능
+
+  test('로또 자동 발급 시 오름차순으로 발급한다.', () => {
+    const randoms = new Array(OPTION.BALL_COUNT)
+      .fill(0)
+      .map((item, index) => index + 1);
+    mockRandoms([randoms.reverse()]);
+    expect(Lotto.pickRandomNumbers()).toEqual(randoms.reverse());
+  });
 });
