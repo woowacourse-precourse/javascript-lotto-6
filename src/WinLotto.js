983effc0c6319edbb3ff665dc9019258ad343fe4
zelkovaria <hyein396@gmail.com>
2023-11-04 02:22:51 +0900

    fix: 문자열 변수 파일 위치 변경

--- src/WinLotto.js
+++ src/WinLotto.js
@@ -6,8 +6,5 @@ class WinLotto {
     this.winningNumbers = winningNumbers;
     this.bonusNumber = bonusNumber;
   }
-
-  static #WINNINGNUM_INPUT = "당첨 번호를 입력해 주세요.\n";
-  static #BONUSNUM_INPUT = "보너스 번호를 입력해 주세요.\n";
 }
 export default WinLotto;
--- src/WinningNum.js
+++ src/WinningNum.js
@@ -2,6 +2,9 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import Validator from "./Validate.js";
 
 class WinningNum {
+  static #WINNINGNUM_INPUT = "당첨 번호를 입력해 주세요.\n";
+  static #BONUSNUM_INPUT = "보너스 번호를 입력해 주세요.\n";
+
   async askWinngNum() {
     const userInputWinningNum = await MissionUtils.Console.readLineAsync("");
     const winningNum = userInputWinningNum.split(",");
