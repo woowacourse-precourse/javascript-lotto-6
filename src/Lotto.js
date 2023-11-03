fcefc90aa3a56f853fc6956b0218e0e1a77a00e4
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-04 07:40:58 +0900

    :sparkles: feat: 로또 클래스와 로또번호 유효성검사 모델을 연결 해당 모델에 검증된 값만 로또번호로 반환할 수 있도록 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,30 @@
+import LottoValidator from './domain/LottoValidator.js';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.map(Number);
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    LottoValidator.validLottoNumber(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  getLotto() {
+    return this.#numbers;
+  }
+
+  // TODO: 로또한장에 보너스번호가 포함되어있는지 확인한다.
+  // TODO: 당첨번호와 보너스번호를 받아서 포함된 숫자를 카운팅한다.
 }
 
 export default Lotto;
+
+// 테스트용 코드
+/*
+const lottos = '1, 2, 35, 4, 5, 6'.split(',').map((number) => number.trim());
+const lotto = new Lotto(lottos);
+lotto.getLotto();
+*/
