684f5cbc149d1f30a1530e907f3373e735b4587f
조예진 <joyejin@joyejins-MacBook-Air.local>
2023-11-03 22:41:00 +0900

    fix: Lotto.js의 #validate 내부에 조건문 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
+
 class App {
-  async play() {}
+  async printTicketNumber() {
+    // 구입금액 입력 (8)
+    const purchaseAmountStr = await Console.readLineAsync(
+      '구입금액을 입력해 주세요.',
+    );
+    const purchaseAmount = parseInt(purchaseAmountStr);
+    Console.print(`${purchaseAmount / 1000}개를 구매했습니다.`);
+
+    const lotto = new Lotto(null, purchaseAmount);
+  }
+
+  async play() {
+    this.printTicketNumber();
+  }
 }
 
 export default App;
