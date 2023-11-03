cc7c1a46d545b8dd46436c849707776fb151f906
gunyongbok <qhrrjsdyd123@gmail.com>
2023-11-03 16:16:45 +0900

    feat: 구매한 로또 1장을 생성해주는 기능 구현

---
+++ src/controller/makeLotto.js
@@ -0,0 +1,14 @@
+import { Random } from "@woowacourse/mission-utils";
+
+const makeLotto = () => {
+  const randomLottoNumbers = [];
+  while (randomLottoNumbers.length < 6) {
+    const number = Random.pickNumberInRange(1, 45);
+    if (!randomLottoNumbers.includes(number)) {
+      randomLottoNumbers.push(number);
+    }
+  }
+  return randomLottoNumbers.sort((a, b) => a - b);
+};
+
+export default makeLotto();
