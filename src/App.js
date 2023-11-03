27d9bc08a84babccd7e7f48ec662d4d50feb022e
장호빈 <devhobin@gmail.com>
2023-11-03 18:56:51 +0900

    feat<App>: 입력받은 구입금액만큼 로또를 구입 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import UI from "./UI.js";
+import LottoMachine from "./LottoMachine.js";
 import AppError, { ERROR_TYPE } from "./AppError.js";
 
 const WINNING_RESULT_TO_PRICE = {
@@ -8,21 +9,28 @@ const WINNING_RESULT_TO_PRICE = {
   fiveBonus: 30000000,
   six: 2000000000,
 };
+
 class App {
   #status;
 
+  #amount;
+
+  #lotteries;
+
   #ui;
 
-  #amount;
+  #lottoMachine;
 
   constructor() {
     this.#status = "purchase";
-    this.#ui = new UI();
     this.#amount = 0;
+    this.#lotteries = [];
+    this.#ui = new UI();
+    this.#lottoMachine = new LottoMachine();
   }
 
   async play() {
-    this.#transition("purchase");
+    await this.#transitionEffect();
   }
 
   async #transition(status) {
@@ -37,6 +45,20 @@ class App {
         case "purchase": {
           const amount = await this.#ui.askAmountForPurchase();
           this.#amount = amount;
+
+          const lotteries = this.#lottoMachine.sell(this.#amount);
+          this.#lotteries = lotteries;
+
+          this.#ui.linebreak();
+
+          this.#ui.printLottoPurchaseInformation(
+            this.#lotteries.map((lotto) => lotto.getInformation()),
+          );
+
+          await this.#transition("winningNumberSetting");
+          break;
+        }
+        case "winningNumberSetting": {
           break;
         }
         default: {
