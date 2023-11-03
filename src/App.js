1dcfec8b2b09bd0335061b4dd67610dc47ae9d57
songudeum <songudeum@gmail.com>
2023-11-04 04:28:48 +0900

    feat: 로또 구입 금액 예외처리 기능 구현 중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+    async play() {
+        Console.print("구입금액을 입력해 주세요.");
+    }
+    async getPurchaseAmount() {
+        const purchaseAmount = await Console.readLineAsync().then((value) =>
+            Number(value)
+        );
+
+        this.isValidPurchaseAmount(purchaseAmount);
+
+        return purchaseAmount;
+    }
+    isValidPurchaseAmount(input) {
+        if (isNaN(input)) {
+            throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+        if (input % 1000 !== 0) {
+            throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+    }
 }
 
 export default App;
