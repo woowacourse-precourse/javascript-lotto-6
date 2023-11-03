f840bf71837e01d8282b6d3f371cdd66443d9063
haesa <xdhaesa@gmail.com>
2023-11-03 19:19:04 +0900

    [Feat]: 1개의 로또를 발행할 때 로또 번호는 1~45 사이의 중복되지 않는 숫자 6개여야 한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,19 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    const INVALID_RANGE = numbers.some(number => number < 1 || number > 45);
+    if (INVALID_RANGE) {
+      throw new Error('[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.');
+    }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복이 없어야 합니다.');
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
