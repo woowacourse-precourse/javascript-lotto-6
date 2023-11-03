9aaea541f1d3f0158e58e9fed3c6461740bf3a5e
Moon Sojeong <sjeong35@naver.com>
2023-11-03 22:56:04 +0900

    fix: 입력받은 로또 번호가 없을 경우의 예외 처리 오류 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,7 +7,7 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers === '') throw new Error('[ERROR] 로또 번호를 입력하세요.');
+    if (numbers.length === 0) throw new Error('[ERROR] 로또 번호를 입력하세요.');
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
