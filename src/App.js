f3b5288810539b2320a0beb00e9c8a4728f92fd0
unknown <dkvz@naver.com>
2023-11-03 11:37:16 +0900

    입력 처리 부분 class 나누기 시도중

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import { ERROR } from "./const/error";
 
 class App {
   constructor() {
-    this.price = 0;
     this.winning = [];
     this.bonus = 0;
+    this.count = 0;
   }
   async play() {
+    
 
   }
 
   async getLottoPrice() {
     const priceInput = await MissionUtils.Console.readLineAsync(MESSAGE.BUY);
-    this.price = this.checkPrice(priceInput)
+    this.count = this.checkPrice(priceInput);
+
   }
   checkPrice(priceInput) {
     const checkPrice = parseInt(priceInput);
@@ -27,13 +29,14 @@ class App {
 
 // 이후 getNumers에서 입력 받기 전에 몇개 구매한지 출력해야 함
 
+}
 
+class inputNumbers{
   async getNumbers(){
     const winningNumbers = await MissionUtils.Console.readLineAsync(MESSAGE.WINNING_NUMBER);
     this.winning = this.checkWinningNumbers(winningNumbers);
   }
 
-  // 여기와 보너스 넘버쪽 if문이 4개나 되는데 switch 쓸 것인지 고찰
   checkWinningNumbers(winningNumbers){
     const checkWinningNumber = winningNumbers.spilt(",").map(Number);
 
@@ -45,7 +48,6 @@ class App {
   async getBonusNumbers(){
     const bonusNumbers = await MissionUtils.Console.readLineAsync(MESSAGE.BONUS_NUMBER);
     this.bonus = this.checkBonusNumbers(bonusNumbers);
-    
   }
   checkBonusNumbers(bonusNumbers){
     const checkBonusNumber = bonusNumbers.spilt(",").map(Number);
@@ -60,5 +62,17 @@ class App {
     if(numbers > 45) {throw new Error(ERROR.FORTY_FIVE)};
   }
 }
+
+// class 나눌거면 상속으로 count 받아와야 하고 위쪽 APP에서 아래 클래스 다 진행시킬 수 있게.
+// ㄴ 돌아가는 꼴을 봐서는 Lotto 쪽에 합치고 여기에서는 Lotto.js 파일을 받아와서 진행하는 방향일듯
+class progressLotto{
+  getMyLottos(){
+    for(let i = 0; i<count; i++){
+
+    }
+  }
+
+}
+
 
 export default App;
