cfc5e0856cc898cfce18ca6a199daf71f6a9d226
ss0526100 <ss0526100@gmail.com>
2023-11-03 20:02:13 +0900

    test: add check lottoNumberString

--- __tests__/moduleTest/ErrorCheckTest.js
+++ __tests__/moduleTest/ErrorCheckTest.js
@@ -58,4 +58,21 @@ describe('ErrorCheck 테스트', () => {
     if (isThrowing) expect(targetFunction).toThrow(ERROR_FORMAT);
     if (!isThrowing) expect(targetFunction).not.toThrow();
   });
+
+  test.each([
+    ['5', false],
+    ['15', false],
+    ['', true],
+    ['true', true],
+    [' ', true],
+    ['+', true],
+    ['1.1', true],
+    ['-10', true],
+    ['46', true],
+    ['0', true],
+  ])('lottoNumberString()', (input, isThrowing) => {
+    const targetFunction = () => ErrorCheck.lottoNumberString(input);
+    if (isThrowing) expect(targetFunction).toThrow(ERROR_FORMAT);
+    if (!isThrowing) expect(targetFunction).not.toThrow();
+  });
 });
