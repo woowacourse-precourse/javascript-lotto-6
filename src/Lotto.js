d2121f590cdc64d304d4a1a41429cab96a9aa513
jbilee <julishahn@gmail.com>
2023-11-03 12:05:53 +0900

    feat: 로또 번호 입력에 대한 예외 처리 기능 추가 - 문자가 포함된 경우 예외 처리 - 정수가 아닐 경우 예외 처리 - 숫자가 중복될 경우 예외 처리 - 1~45 사이가 아닐 경우 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,14 +7,44 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6)
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    if (numbers.includes(NaN))
+      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+    if (this.#containsDecimals(numbers))
+      throw new Error('[ERROR] 로또 번호는 정수여야 합니다.');
+    if (this.#containsDuplicates(numbers))
+      throw new Error('[ERROR] 중복되는 숫자가 있습니다.');
+    if (this.#isOutOfRange(numbers))
+      throw new Error('[ERROR] 로또 번호는 1에서 45 사이여야 합니다.');
   }
 
   // TODO: 추가 기능 구현
   getLottoNumbers() {
     return this.#numbers;
+  }
+
+  #containsDuplicates(numbers) {
+    const set = new Set(numbers);
+    if (set.size !== numbers.length) return true;
+    return false;
+  }
+
+  #containsDecimals(numbers) {
+    let containsDecimals = false;
+    numbers.forEach(x => {
+      const string = x.toString();
+      if (string.includes('.')) containsDecimals = true;
+    });
+    return containsDecimals;
+  }
+
+  #isOutOfRange(numbers) {
+    let isOutOfRange = false;
+    numbers.forEach(x => {
+      if (x < 1 || x > 45) isOutOfRange = true;
+    });
+    return isOutOfRange;
   }
 }
 
