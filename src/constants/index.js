52692f41771dac5c743497ff695aad0b8a88fa45
noodle <noodle>
2023-11-04 03:35:12 +0900

    feat: 상수 obj -> 순서대로 출력하기 위해 Map으로 변경 이에 따른 메시지 로직 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,30 +1,36 @@
-export const MATCH_COUNT_PRIZES = {
-  3: 5000,
-  4: 50000,
-  5: 1500000,
-  '5_and_bonus': 30000000,
-  6: 2000000000,
-};
+import { deepFreeze } from '../utils/index.js';
 
-export const INPUT_MESSAGE = {
-  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
-  WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
-  BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
-};
+export const MATCH_COUNT_PRIZES = deepFreeze(
+  new Map([
+    ['3', { prize: 5000, text: '3개 일치' }],
+    ['4', { prize: 50000, text: '4개 일치' }],
+    ['5', { prize: 1500000, text: '5개 일치' }],
+    ['5_bonus', { prize: 30000000, text: '5개 일치, 보너스 볼 일치' }],
+    ['6', { prize: 2000000000, text: '6개 일치' }],
+  ]),
+);
 
 export const OUTPUT_MESSAGE = {
-  PURCHASE_CONFIRM: (number) => `${number}개를 구매했습니다.`,
-  RESULT_HEADER: '당첨 통계\n---',
-  RESULT_ROW: (matchCount, winCount = 0) => {
-    const prize = MATCH_COUNT_PRIZES[matchCount];
-    return `${matchCount}개 일치 (${prize.toLocaleString()}원) - ${winCount}개`;
+  PURCHASE_CONFIRM: (number) => `${number}개를 구매했습니다.\n`,
+  RESULT_HEADER: '\n당첨 통계\n---',
+  RESULT_ROW: (key, winCount = 0) => {
+    const prizeInfo = MATCH_COUNT_PRIZES.get(key);
+    const prize = prizeInfo.prize.toLocaleString();
+    const text = prizeInfo.text;
+    return `${text} (${prize}원) - ${winCount}개`;
   },
-  TOTAL_RETURN_RATE: (rate) => `총 수익률은 ${rate}%입니다.`,
+  TOTAL_RETURN_RATE: (rate) => `총 수익률은 ${parseFloat(rate.toFixed(2))}%입니다.`,
 };
+
+export const INPUT_MESSAGE = Object.freeze({
+  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  WINNING_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
+});
 
 export const NEW_LINE = '\n';
 
-export const ERROR_MESSAGE = {
+export const ERROR_MESSAGE = Object.freeze({
   HEADER_PREFIX: '[ERROR]',
   INVALID_UNIT: '1,000원 단위로 입력해주세요.',
   INVALID_NUMBER_RANGE: '로또 번호는 1부터 45 사이의 숫자여야 합니다.',
@@ -32,9 +38,10 @@ export const ERROR_MESSAGE = {
   INVALID_NUMBER_COUNT: '6개의 번호를 입력해주세요.',
   INVALID_BONUS_NUMBER_COUNT: '1개의 보너스 번호를 입력해주세요.',
   DUPLICATE_NUMBERS: '중복된 숫자가 있습니다. 다시 입력해주세요.',
-};
+});
 
 export default {
+  MATCH_COUNT_PRIZES,
   INPUT_MESSAGE,
   OUTPUT_MESSAGE,
   NEW_LINE,
