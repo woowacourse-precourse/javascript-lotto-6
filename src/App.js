ce48a1c612116ae10c1f37cb116b65036cb30cac
yong seock <myongdol132@gmail.com>
2023-11-03 16:52:44 +0900

    Feat: 로또번호 생성 및 유효성 검사
    - Lotto클래스 내 번호 생성기능 구현 하였습니다.
    - 번호의 유효성 검사를 추가 하였습니다.
     + 중복, 개수 확인
    - App클래스에서 로또 구매 및 유효성 검사 로직을 수정 및 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,33 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     const amount = await this.inputAmount();
+    this.issueLottos(amount);
   };
+  
+  constructor() {
+    this.lottos = [];
+  }
 
   async inputAmount() {
     const amount = await Console.readLineAsync("로또를 구매할 금액을 입력 해주세요. (1,000원 단위): ");
       
     if (amount % 1000 !== 0) {
-      Console.print("금액은 1,000원 단위로만 입력 가능합니다!");
+      Console.print("[ERROR] 금액은 1,000원 단위로만 입력 가능합니다!");
       return this.inputAmount();
     }
       return parseInt(amount, 10);
     };
+  
+  issueLottos(amount) {
+    const lottoCount = amount / 1000;
 
+    for (let i = 0; i < lottoCount; i++) {
+      const numbers = Lotto.generateNumbers();
+      this.lottos.push(new Lotto(numbers));
+    }
+    
+    Console.print(`${lottoCount}개를 구매했습니다.`)
+  }
   
 };
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -18,8 +18,18 @@ class Lotto {
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    };
+
+    if (this.#hasDuplicate(numbers)) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다."); 
+    };
   }
+
+  #hasDuplicate(numbers) {
+    return new Set(numbers).size !== numbers.length;
+  };
+  
+  
 }
 
 
