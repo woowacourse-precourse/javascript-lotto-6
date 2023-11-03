5ded2217b55a950f993b645c2d2878147c16b235
UBiiiii <willowman55@gmail.com>
2023-11-03 16:07:15 +0900

    [feat] 로또 번호 출력 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,11 +12,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if(numbers.filter((num, idx)=>numbers.indexOf(num)===idx).length !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복된 수가 존재해선 안됩니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
-  #printLotto() {
+  printLotto() {
     Console.print(`[${this.#numbers.join(", ")}]`);
+    return `[${this.#numbers.join(", ")}]`;
   }
 }
 
