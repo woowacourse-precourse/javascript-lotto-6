74393a3df5f3456982a41d0bda0b821966955354
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-03 14:58:44 +0900

    feat(message): add error messages

---
+++ src/constants/errors.js
@@ -0,0 +1,6 @@
+import { ERROR_CONVENTION } from "./conventions.js";
+
+export const ERROR_MESSAGE = Object.freeze({
+  isNumberDuplicated: `${ERROR_CONVENTION} 중복된 숫자가 있습니다.`,
+  isEmpty: `${ERROR_CONVENTION} 입력받은 값이 존재하지 않습니다.`,
+});
