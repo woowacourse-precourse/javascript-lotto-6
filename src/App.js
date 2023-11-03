f7444a5300dd63f5c1e91b1dc552783dbcd89c2c
unknown <dkvz@naver.com>
2023-11-03 10:39:11 +0900

    당첨 번호 받고 에러 체크

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { MESSAGE } from "./const/message";
+import { ERROR } from "./const/error";
 
 
 class App {
@@ -13,15 +14,36 @@ class App {
   }
 
   async getLottoPrice() {
-    const priceInput = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    const priceInput = await MissionUtils.Console.readLineAsync(MESSAGE.BUY);
     this.price = this.checkPrice(priceInput)
   }
   checkPrice(priceInput) {
     const checkPrice = parseInt(priceInput);
     const remainder = checkPrice % 1000;
-    if(remainder !== 0 ){throw new Error(MESSAGE.THOUSAND)};
+
+    if(remainder !== 0 ){throw new Error(ERROR.THOUSAND)};
     return checkPrice;
   }
+  async getNumbers(){
+    const winningNumbers = await MissionUtils.Console.readLineAsync(MESSAGE.WINNING_NUMBER);
+    this.winning = this.checkNumbers(winningNumbers);
+  }
+
+  // 여기 if문이 4개나 되는데 switch 쓸 것인지 고찰
+  checkNumbers(winningNumbers){
+    const checkNumber = winningNumbers.spilt(",").map(Number);
+
+    if(!winningNumbers.includes(',')) {throw new Error(ERROR.NO_COMMA)};
+    if(!Number.isInteger(checkNumber)) {throw new Error(ERROR.INTEGER)};
+    if(checkNumber < 1) {throw new Error(ERROR.ONE)};
+    if(checkNumber > 45) {throw new Error(ERROR.FORTY_FIVE)};
+    return checkNumber;
+  }
+  async getBonusNumbers(){
+    const bonusNumbers = await MissionUtils.Console.readLineAsync(MESSAGE.BONUS_NUMBER);
+    
+  }
+
 }
 
 export default App;
--- src/const/message.js
+++ src/const/message.js
@@ -1,4 +1,8 @@
 export const MESSAGE = {
-    THOUSAND : '구입 금액은 1,000원 단위로 입력해야 합니다'
+    BUY : '구입금액을 입력해 주세요.\n',
+    WINNING_NUMBER : '당첨 번호를 입력해 주세요.\n',
+    BONUS_NUMBER : '보너스 번호를 입력해 주세요.\n'
+
+    
 
 }
