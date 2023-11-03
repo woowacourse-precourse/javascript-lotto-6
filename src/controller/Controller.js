fc1f20e5eca12c9abe0cc825dfca15a0c653b30b
laon2019 <laon5987@nate.com>
2023-11-03 21:09:07 +0900

    feat(Controller.js): 로또 구매 모델과 뷰 연결

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,4 +1,21 @@
+import InputView from "../view/InputView";
+import OutputView from "../view/OutputView";
+import UserLottoModel from "../domain/UserLottoModel";
+
+
 class LottoController {
+  #userLottoModel;
+
+  constructor() {
+    this.#userLottoModel = new UserLottoModel();
+  }
+  async playGame() {
+    const quantity = await InputView.getLottoNumbers();
+    OutputView.printQuantity(quantity);
+
+    this.#userLottoModel.generateLottoTicket(quantity);
+    OutputView.printLottoTickets(this.#userLottoModel.getLottoTickets());
+  }
 }
 
 export default LottoController;
