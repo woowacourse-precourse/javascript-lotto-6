1a7759c8c9dd45571262c346af63f26189b747a2
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-03 17:08:34 +0900

    [refactor] : 예외 발생 함수로 만들기

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,19 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    this.#validateNumberCount(numbers);
   }
 
   // TODO: 추가 기능 구현
+  #occurError(errorMessage) {
+    throw new Error(errorMessage);
+  }
+
+  #validateNumberCount(numbers) {
+    if (numbers.length !== 6) {
+      this.#occurError("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
 }
 
 export default Lotto;
