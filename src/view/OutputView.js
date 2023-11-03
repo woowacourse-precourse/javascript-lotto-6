2a6d45d7be786c706edece331a0b6c1c78b89e08
PARK-CHANUK <doosan1224@naver.com>
2023-11-03 22:14:43 +0900

    feat: 당첨내역과 수익률을 포함한 당첨 통계 출력기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -12,7 +12,23 @@ const OutputView = {
     });
   },
 
-  printLotteryResultsSummary(winningResult, rateOfReturn) {},
+  printLotteryResultsSummary(winningResult, rateOfReturn) {
+    const {
+      threeMatching,
+      fourMatching,
+      fiveMatchingNotBonus,
+      fiveMatchingAndBonus,
+      allMatching,
+    } = winningResult;
+    Console.print(`3개 일치 (5,000원) - ${threeMatching}개`);
+    Console.print(`4개 일치 (50,000원) - ${fourMatching}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${fiveMatchingNotBonus}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${fiveMatchingAndBonus}개`,
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${allMatching}개`);
+    Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
+  },
 };
 
 export default OutputView;
