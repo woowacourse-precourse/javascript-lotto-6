24fecede67e1016dfb04c984b76563f32831a6f0
ss0526100 <ss0526100@gmail.com>
2023-11-03 12:24:16 +0900

    feat: check winningList length

--- src/modules/ConvertInputTo.js
+++ src/modules/ConvertInputTo.js
@@ -2,9 +2,22 @@ import { Console } from '@woowacourse/mission-utils';
 import ErrorCheck from './ErrorCheck.js';
 
 const LOTTO_PRICE = 1000;
+const LOTTO_UPPER_NUMBER = 45;
 
 const PURCHASE_PRICE_INPUT_MESSAGE = '구입금액을 입력해 주세요.';
+const WINNING_NUMBER_INPUT_MESSAGE = '당첨 번호를 입력해 주세요.';
+
+const MISS_STATE = 0;
+const BONUS_STATE = 0.5;
+const HIT_STATE = 1;
+
 class ConvertInputTo {
+  static async lottoArray() {
+    return await ConvertInputTo.purchasePrice().then(purchasePrice =>
+      Get.randomLottoArray(purchasePrice / LOTTO_PRICE)
+    );
+  }
+
   static async purchasePrice() {
     const inputString = await Console.readLineAsync(
       PURCHASE_PRICE_INPUT_MESSAGE
@@ -14,9 +27,10 @@ class ConvertInputTo {
     return Number(inputString);
   }
 
-  static async lottoArray() {
-    return await ConvertInputTo.purchasePrice().then(purchasePrice =>
-      Get.randomLottoArray(purchasePrice / LOTTO_PRICE)
+  static async numberBoard() {
+    const board = new Array(LOTTO_UPPER_NUMBER + 1).fill(MISS_STATE);
+    const inputString = await Console.readLineAsync(
+      WINNING_NUMBER_INPUT_MESSAGE
     );
   }
 }
--- src/modules/ErrorCheck.js
+++ src/modules/ErrorCheck.js
@@ -1,19 +1,29 @@
 import Is from './Is.js';
 const NOT_AN_INTEGER_ERROR_MESSAGE = '[ERROR] 정수가 아닙니다.';
 const NOT_MULTIPLES_ERROR_MESSAGE = '[ERROR] 로또를 깔끔하게 살 수 없습니다.';
+const WRONG_LENGTH_ERROR_MESSAGE = '[ERROR] 잘못된 길이입니다';
 
 class ErrorCheck {
   static purchasePrice(inputString, lottoPrice) {
     ErrorCheck.positiveIntegerString(inputString);
     ErrorCheck.multiplesInPositive(Number(inputString), lottoPrice);
   }
+
   static positiveIntegerString(inputString) {
     if (!Is.positiveIntegerString(inputString))
       throw new Error(NOT_AN_INTEGER_ERROR_MESSAGE);
   }
+
   static multiplesInPositive(inputNumber, targetNumber) {
     if (!Is.multiplesInPositive(inputNumber, targetNumber))
       throw new Error(NOT_MULTIPLES_ERROR_MESSAGE);
+  }
+
+  static lottoListString(inputString) {}
+
+  static arrayLikeLength(arrayLike, length) {
+    if (arrayLike.length !== length)
+      throw new Error(WRONG_LENGTH_ERROR_MESSAGE);
   }
 }
 
