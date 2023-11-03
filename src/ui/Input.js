a08384b514a795a81bae45e20b04c3e0d2f8a5b7
Joseph Park <rkfnahs12e@gmail.com>
2023-11-03 22:52:05 +0900

    style : 들여쓰기가 Tab으로 되던 것을 공백으로 수정

--- src/ui/Input.js
+++ src/ui/Input.js
@@ -1,47 +1,47 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const Inputs = {
-	async getAmount(message) {
-		const userInput = await Console.readLineAsync(message);
-		if (userInput % 1000 !== 0) {
-			throw new Error('[ERROR] 구매 단위가 1000원으로 떨어져야 합니다.');
-		}
-		// 차후 검증 코드 추가.
-		console.log(userInput);
-		return userInput;
-	},
+  async getAmount(message) {
+    const userInput = await Console.readLineAsync(message);
+    if (userInput % 1000 !== 0) {
+      throw new Error('[ERROR] 구매 단위가 1000원으로 떨어져야 합니다.');
+    }
+    // 차후 검증 코드 추가.
+    console.log(userInput);
+    return userInput;
+  },
 
-	async getWinningNumber(message) {
-		const userInput = await Console.readLineAsync(message);
-		const userInputList = userInput
-			.split(',')
-			.map((element) => parseInt(element, 10));
-		console.log(userInputList);
-		// 차후 검증 코드 추가.
+  async getWinningNumber(message) {
+    const userInput = await Console.readLineAsync(message);
+    const userInputList = userInput
+      .split(',')
+      .map((element) => parseInt(element, 10));
+    console.log(userInputList);
+    // 차후 검증 코드 추가.
 
-		// 중복 검사
-		const isDuplicated = new Set(userInputList).size !== userInputList.length;
+    // 중복 검사
+    const isDuplicated = new Set(userInputList).size !== userInputList.length;
 
-		if (isDuplicated) {
-			throw new Error('[ERROR] 중복 값이 존재합니다.');
-		}
+    if (isDuplicated) {
+      throw new Error('[ERROR] 중복 값이 존재합니다.');
+    }
 
-		// 6자리 검사
-		if (userInputList.length !== 6) {
-			throw new Error('[ERROR] 당첨 번호가 6자리가 아닙니다.');
-		}
-		return userInputList;
-	},
+    // 6자리 검사
+    if (userInputList.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호가 6자리가 아닙니다.');
+    }
+    return userInputList;
+  },
 
-	async getBonusNumber(message, winningNumber) {
-		const userInput = parseInt(await Console.readLineAsync(message), 10);
-		// 당첨번호와 중복된 번호 있는지 검사.
-		winningNumber.map((item, idx) => {
-			if (item === userInput) {
-				throw new Error('[ERROR] 당첨 번호 중 중복된 번호가 있습니다.');
-			}
-		});
-		console.log(userInput);
-		return userInput;
-	},
+  async getBonusNumber(message, winningNumber) {
+    const userInput = parseInt(await Console.readLineAsync(message), 10);
+    // 당첨번호와 중복된 번호 있는지 검사.
+    winningNumber.map((item, idx) => {
+      if (item === userInput) {
+        throw new Error('[ERROR] 당첨 번호 중 중복된 번호가 있습니다.');
+      }
+    });
+    console.log(userInput);
+    return userInput;
+  },
 };
