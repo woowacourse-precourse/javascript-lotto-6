9a789ac5404afd55cca8fb6075f3c1947981779c
hellosehyun <sehyun030526@gmail.com>
2023-11-03 17:38:32 +0900

    docs: README.md 기능명세서 수정
    사용자
    
    -   [ ] 로또 구입 금액을 입력 받음
    -   [ ] 보너스 번호를 입력 받음
    -   [ ] 로또 번호 입력 받음
            모델
    -   변수
    
        -   [ ] 사용자 로또 구입 금액
        -   [ ] 발행한 로또 객체 모음
        -   [ ] 당첨 로또 번호
        -   [ ] 보너스 번호
    
    -   상수
        -   [ ] 기본 출력 메시지들
    
    로또
    
    -   [ ] 당첨 번호가 담김
    -   [ ] 당첨 번호의 예외처리
    -   [ ] 랜덤 6개의 숫자름 담을 수도 있음
    
    컨트롤러
    
    -   [ ] 로또 구입 금액에 따른 갯수를 반환
    -   [ ] 당첨 통계 계산
    -   [ ] 수익률 계산
    -   [ ] 발행 로또 번호 와 당첨 번호를 비교해 결과 반환
    
    뷰
    
    -   [ ] 방행 로또 번호를 텍스트로 출력
    -   [ ] 당첨 결과 통계를 텍스트로 출력
    -   [ ] 수익률을 출력

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,16 +1,21 @@
 class Lotto {
     #numbers;
 
+    // numbers = [1,2,3,4,5,6]
     constructor(numbers) {
         // # = private
         this.#validate(numbers);
         this.#numbers = numbers;
     }
 
+    // 예외처리
     #validate(numbers) {
         if (numbers.length !== 6) {
             throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        numbers.forEach((number) => {
+            element;
+        });
     }
 
     // TODO: 추가 기능 구현
