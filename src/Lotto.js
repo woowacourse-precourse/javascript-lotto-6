ab01a037a2fd0e4d78ee174b36301d506c11cc92
dyddks <dyddks0115@naver.com>
2023-11-03 17:16:24 +0900

    feat: 로또당첨번호와 보너스번호 관리

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,43 @@ class Lotto {
   }
 
   #validate(numbers) {
+    if (!Number.parseInt(numbers)) {
+      throw new Error("[ERROR] 잘못된 입력입니다.");
+    }
+
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (numbers.some(number => number > 45)) {
+      throw new Error("[ERROR] 로또 번호는 45이하의 숫자여야 합니다.");
+    }
   }
 
+  #validateBonus(bonus) {
+    if (!Number.parseInt(bonus)) {
+      throw new Error("[ERROR] 잘못된 입력입니다.");
+    }
+
+    if (bonus > 45 || bonus < 1) {
+      throw new Error("[ERROR] 로또 번호는 45이하의 숫자여야 합니다.");
+    }
+  }
+
+  setBonus(bonus) {
+    this.#validateBonus(bonus)
+    this.#numbers.push(bonus);
+  }
+
+  getWinningNumbers() {
+    return this.#numbers.slice(0,6);
+  }
+
+  getBonus() {
+    return this.#numbers[6];
+  }
   // TODO: 추가 기능 구현
+
 }
 
 export default Lotto;
