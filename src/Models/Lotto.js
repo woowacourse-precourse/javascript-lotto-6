08243a54ea790720011f335f1ac9f0435c06622d
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 16:05:16 +0900

    Refactor: Lotto의 숫자 개수 검증 함ㅅ무에서 커스텀 에러과 상수 사용
    커스텀 에러를 사용하고 6개 하드코딩된 부분을 상수를 이용하도록 리팩토링함

--- src/Models/Lotto.js
+++ src/Models/Lotto.js
@@ -5,18 +5,17 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    this.#validateSize(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+  #validateSize(numbers) {
     if (numbers.length !== 6) {
       throw new LottoError(
         `로또 번호는 ${LOTTO_SETTINGS.NUMBER_PER_TICKET}개여야 합니다.`
       );
     }
   }
-
   // TODO: 추가 기능 구현
 }
 
