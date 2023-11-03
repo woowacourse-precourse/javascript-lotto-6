a49888439b8923788ab4de6bb42a4dff5d756e51
chestnut1044 <wbk1620@gmail.com>
2023-11-03 16:00:22 +0900

    feat : 로또 구입 금액 입력 및 예외 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import View from './View.js';
+
 class App {
-  async play() {}
+  #coin;
+  #view = new View();
+
+  async play() {
+    this.#coin = await this.#view.inputCash();
+  }
 }
 
 export default App;
