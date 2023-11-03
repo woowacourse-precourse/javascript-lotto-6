11b90fdf590faf1fe468d4c96695a6270a11a875
zelkovaria <hyein396@gmail.com>
2023-11-04 02:14:33 +0900

    fix: 당첨 번호 및 보너스 번호 입력 로직 분리

--- src/WinningNum.js
+++ src/WinningNum.js
@@ -1,15 +1,21 @@
-import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 import Validator from "./Validate.js";
 
 class WinningNum {
   async askWinngNum() {
     const userInputWinningNum = await MissionUtils.Console.readLineAsync("");
     const winningNum = userInputWinningNum.split(",");
+    Validator.validateInputMoney(winningNum);
 
+    return winningNum;
+  }
+
+  async askBonusNum() {
     const userInputBonusNum = await MissionUtils.Console.readLineAsync("");
     const bonusNum = userInputBonusNum.split(",");
+    Validator.validateInputMoney(bonusNum);
 
-    console.log(winningNum);
+    return bonusNum;
   }
 }
 export default WinningNum;
