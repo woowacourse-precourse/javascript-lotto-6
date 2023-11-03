a72586eb647c32698bcd3ab9f2c1f0139dcdcb2a
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 17:57:10 +0900

    Feat: Money 클래스에 전달 받은 값이 숫자 아니면 에러 처리하기 추가
    ,와 .을 제거한 후에도 isNaN이면 에러처리하게함

--- src/Models/Money.js
+++ src/Models/Money.js
@@ -1,12 +1,19 @@
+import LottoError from "../Error/LottoError";
+
 export default class Money {
   #money;
   constructor(money) {
     const formattedMoney = this.#removeCommaAndDot(money);
+    this.#validateIsNumber(formattedMoney);
     this.#money = Number(formattedMoney);
   }
 
   #removeCommaAndDot(money) {
     return money.replace(/[,.]/g, "");
+  }
+
+  #validateIsNumber(money) {
+    if (isNaN(money)) throw new LottoError("숫자를 입력해주세요.");
   }
 
   getMoney() {
