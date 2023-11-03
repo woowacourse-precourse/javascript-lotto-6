3ae2f2455d973e6fb2e63e01ccb2cdd15e7459c3
yheseul_kang <kangyheseul@naver.com>
2023-11-03 20:03:12 +0900

    Feat:   입력 받는 기능 구현 // 로또 구입 금액, 당첨 번호, 보너스 번호 입력 받는 기능 구현

---
+++ src/User.js
@@ -0,0 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "./message.js";
+import { purchaseAmountValidation } from "./validator/purchaseAmountValidation.js";
+import { winningNumberLength } from "./validator/winningNumberValidation.js";
+import { bonusNumberValidation } from "./validator/bonusNumberValidation.js";
+
+class User {
+  static async inputLottoPurchaseAmout() {
+    const inputValue = await Console.readLineAsync(MESSAGE.LOTTO_PURCHASE_AMOUNT);
+    purchaseAmountValidation(inputValue);
+    return inputValue;
+  }
+
+  static async inputWinningNumber() {
+    const inputValue = await Console.readLineAsync(MESSAGE.WINNING_NUMBER);
+    winningNumberLength(inputValue);
+    return inputValue.split(',');
+  }
+
+  static async inputBonusNumber() {
+    const inputValue = await Console.readLineAsync(MESSAGE.BONUS_NUMBER);
+    bonusNumberValidation(inputValue);
+    return inputValue;
+  }
+}
+
+export default User;
