1c04943a84a1befc2c3e4d76a43ad4a85f7489e7
0jenn0 <jenn0.6n@gmail.com>
2023-11-04 03:42:55 +0900

    Feat: InputView에서 쓰인 메시지들 상수화

---
+++ src/Views/messages.js
@@ -0,0 +1,7 @@
+const MESSAGES = Object.freeze({
+  PROMPT_PURCHASE_VALUE: "구입금액을 입력해 주세요",
+  PROMPT_DRAWN_LOTTO_NUMBERS: "당첨 번호를 입력해주세요.",
+  PROMPT_BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+});
+
+export default MESSAGES;
