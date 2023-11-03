9a0cc8f81c00f4d9b9e76ebeb97a5d690892d10d
j8won <joowoni99@gmail.com>
2023-11-04 00:44:30 +0900

    test(LottoControllerTest): 사용자가 입력한 구입 금액이 숫자 외의 문자로 이루어졌다면 예외 발생시키는지 확인하는 테스트 코드 작성

---
+++ __tests__/LottoControllerTest.js
@@ -0,0 +1,11 @@
+import LottoController from "../src/LottoController.js";
+import LOTTO_CONTROLLER from "../src/constant/LOTTO_CONTROLLER.js";
+
+describe("로또 컨트롤러 클래스 테스트", () => {
+  test("사용자가 입력한 구입 금액이 숫자 외의 문자로 이루어졌다면 예외가 발생한다", () => {
+    const totalPrice = "1000o0";
+    expect(() => {
+      new LottoController(totalPrice);
+    }).toThrow(LOTTO_CONTROLLER.ERROR.PRICE_NOT_NUMBER);
+  });
+});
