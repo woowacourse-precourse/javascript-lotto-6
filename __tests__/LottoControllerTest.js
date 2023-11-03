812de192df8ef9d1f3cbae6fa3c684198fef682d
j8won <joowoni99@gmail.com>
2023-11-04 00:58:20 +0900

    test(LottoControllerTest): 사용자가 입력한 구입 금액이 1000 단위가 아니라면 예외 발생하는지 확인하는 테스트 코드 작

--- __tests__/LottoControllerTest.js
+++ __tests__/LottoControllerTest.js
@@ -8,4 +8,11 @@ describe("로또 컨트롤러 클래스 테스트", () => {
       new LottoController(totalPrice);
     }).toThrow(LOTTO_CONTROLLER.ERROR.PRICE_NOT_NUMBER);
   });
+
+  test("사용자가 입력한 구입 금액이 티켓 구입 가격인 1000 단위가 아니라면 예외가 발생한다", () => {
+    const totalPrice = "10010";
+    expect(() => {
+      new LottoController(totalPrice);
+    }).toThrow(LOTTO_CONTROLLER.ERROR.PRICE_NOT_DIVISIBLE);
+  });
 });
