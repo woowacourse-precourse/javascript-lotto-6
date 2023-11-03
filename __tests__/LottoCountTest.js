50d556f1b77dca136b202aef41e7eec9ad318c24
whai2 <whai2125@gmail.com>
2023-11-03 17:09:28 +0900

    test: 로또 구입 금액이 로또 가격으로 나눠 떨어지지 않을 경우, 예외 처리 단위 테스트

--- __tests__/LottoCountTest.js
+++ __tests__/LottoCountTest.js
@@ -1,4 +1,4 @@
-import { lottoCounter, inputMoneyValidater } from "../src/App.js";
+import { lottoCounter, inputMoneyValidater, inputMoneyDivideValidater } from "../src/App.js";
 
 describe("구입 금액에 따른 로또 개수 테스트", () => {
   test("구입 금액에 따라 올바른 개수의 로또를 발행한다.", () => {
@@ -20,5 +20,11 @@ describe("구입 금액에 따른 로또 개수 테스트", () => {
   
     // then
     expect(inputMoneyValidater(inputs)).rejects.toThrow("[ERROR]");
+  });
+
+  test("구입 금액이 로또 금액으로 나눠 떨어지지 않을 경우, 에러가 발생한다.", () => {
+    const INPUT = 5500;
+
+    expect(inputMoneyDivideValidater(INPUT)).rejects.toThrow("[ERROR]");
   });
 });
