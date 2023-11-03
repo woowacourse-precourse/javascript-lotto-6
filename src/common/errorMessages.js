d20df0f9bd7f5835e9fcbde4ca4db490d33d38de
gunyongbok <qhrrjsdyd123@gmail.com>
2023-11-03 15:43:49 +0900

    feat: 올바르지 않은 구입 금액 입력시 에러 처리

---
+++ src/common/errorMessages.js
@@ -0,0 +1,5 @@
+const ErrorMessages = {
+  PRICE_INPUT_ERRORMESSAGE: "[ERROR] 입력한 구입 금액은 올바르지 않습니다.",
+};
+
+export default ErrorMessages;
--- src/ui/PurchasePriceInput.js
+++ src/ui/PurchasePriceInput.js
@@ -1,9 +1,13 @@
 import { Console } from "@woowacourse/mission-utils";
 import Messages from "../common/messages.js";
+import validatePriceInput from "../util/validatePriceInput.js";
+import ErrorMessages from "../common/errorMessages.js";
 
 class PurchasePriceInput {
   async print() {
     let price = await Console.readLineAsync(Messages.PURCHASE_INPUT_MESSAGE);
+    if (!validatePriceInput(price))
+      throw new Error(ErrorMessages.PRICE_INPUT_ERRORMESSAGE);
   }
 }
 
