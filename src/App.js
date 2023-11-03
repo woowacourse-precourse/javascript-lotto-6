f89f604e289397367762877d7a588ace7ad0fa18
yong seock <myongdol132@gmail.com>
2023-11-03 16:25:51 +0900

    Feat(App.js) 금액 입력
    - 사용자로부터 로또의 구매할 금액을 입력 받습니다.
    - 구입 금액은 천원 단위로만 입력가능합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 class App {
-  async play() {}
-}
+  async play() {
+    const amount = this.inputAmount();
+  };
+
+  inputAmount() {
+    const amount = prompt("로또를 구매할 금액을 입력 해주세요. (1,000원 단위): ");
+      
+    if (amount % 1000 !== 0) {
+      alert("금액은 1,000원 단위로만 입력 가능합니다!");
+      return this.inputAmount();
+    }
+      return parseInt(amount, 10);
+    };
+};
+
 
 export default App;
