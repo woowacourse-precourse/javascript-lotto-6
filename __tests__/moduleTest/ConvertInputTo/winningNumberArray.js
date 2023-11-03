cad364cf492a7bae0dbab2ceea47e4809b746b16
ss0526100 <ss0526100@gmail.com>
2023-11-03 18:18:37 +0900

    test: add winningNumberArray error case

--- __tests__/moduleTest/ConvertInputTo/winningNumberArray.js
+++ __tests__/moduleTest/ConvertInputTo/winningNumberArray.js
@@ -13,6 +13,12 @@ const mockQuestions = inputs => {
   });
 };
 
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe('winningNumbersArray()', () => {
   test.each([
     [['1,2,3,4,5,6'], [1, 2, 3, 4, 5, 6]],
@@ -29,20 +35,46 @@ describe('winningNumbersArray()', () => {
   });
 
   test.each([
-    [['1,2,3,4,5']],
-    [['1,2,3,4,5']],
-    [['1,2,3,4,5,6,7']],
-    [['0,1,2,3,4,5,6']],
-    [['46,45,44,43,42,41']],
-    [['1,1,1,1,1,1']],
-    [[',,,,,']],
-  ])('예외처리', async input => {
+    [
+      ['1,2,3,4,5', '1,2,3,4,5,6'],
+      [1, 2, 3, 4, 5, 6],
+    ],
+    [
+      ['1,2,3,4,5,', '1,2,3,4,5,6'],
+      [1, 2, 3, 4, 5, 6],
+    ],
+    [
+      ['1,2,3,4,5,6,7', '1,2,3,', '1,2,3,4,5,6'],
+      [1, 2, 3, 4, 5, 6],
+    ],
+    [
+      ['0,1,2,3,4,5,6', '2,4,6,8,10,12'],
+      [2, 4, 6, 8, 10, 12],
+    ],
+    [
+      ['46,45,44,43,42,41', '45,44,43,42,41,40'],
+      [45, 44, 43, 42, 41, 40],
+    ],
+    [
+      ['1,1,1,1,1,1', '1,2,3,4,5,6'],
+      [1, 2, 3, 4, 5, 6],
+    ],
+    [
+      [',,,,,', '1,2,3,4,5,6'],
+      [1, 2, 3, 4, 5, 6],
+    ],
+  ])('예외처리', async (input, expectedValue) => {
     //given
     mockQuestions(input);
 
+    //when
+    const winningNumbers = await ConvertInputTo.winningNumbersArray();
+    const logSpy = getLogSpy();
+
     //then
-    await expect(ConvertInputTo.winningNumbersArray()).rejects.toThrow(
-      ERROR_FORMAT
-    );
+    for (let i = 0; i < logSpy.mock.calls.length - 1; i++) {
+      expect(String(logSpy.mock.calls[i][0])).toMatch(ERROR_FORMAT);
+    }
+    expect(winningNumbers).toEqual(expectedValue);
   });
 });
