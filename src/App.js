08e62c4a18e39c49ce0bf24cfed2e61f77e44abd
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-11-04 02:17:30 +0900

    feature : 사용자 로또 금액 입력 기능과 유효성 검사 기능 오류 픽스

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ class App {
 
 class LottoPurchase {
   async #lottoAmount() {
-    return parseInt(
-      await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.")
+    return await parseInt(
+      MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.")
     );
   }
 
   #validateAmount(lottoAmount) {
     if (
+      isNaN(lottoAmount) ||
       !lottoAmount ||
       lottoAmount < 1000 ||
-      lottoAmount % 1000 !== 0 ||
-      isNaN(lottoAmount)
+      lottoAmount % 1000 !== 0
     ) {
       throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
     }
@@ -27,8 +27,8 @@ class LottoPurchase {
 
     while (true) {
       try {
-        amount = await this.#lottoAmount;
-
+        amount = await this.#lottoAmount();
+        this.#validateAmount(amount);
         break;
       } catch (error) {
         MissionUtils.Console.print(error.message);
