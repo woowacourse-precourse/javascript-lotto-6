edda463643ee3976e9216a67f113fa04f6e2ef54
ss0526100 <ss0526100@gmail.com>
2023-11-03 11:57:59 +0900

    fix: modify print purchased lotto

--- src/modules/Print.js
+++ src/modules/Print.js
@@ -1,10 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const PURCHASED_LOTTO_FORMAT = '개를 구매했습니다.';
+const LOTTO_FRONT_COVER = '[';
+const LOTTO_BEHIND_COVER = ']';
 class Print {
   static purchasedLotto(lottoArray) {
     Console.print(`${lottoArray.length}${PURCHASED_LOTTO_FORMAT}`);
-    lottoArray.foreach(lotto => Console.print(lotto.getNumbers));
+    lottoArray.forEach(Print.lottoNumbers);
+  }
+
+  static lottoNumbers(lotto) {
+    Console.print(
+      `${LOTTO_FRONT_COVER}${lotto
+        .getNumbers()
+        .join(', ')}${LOTTO_BEHIND_COVER}`
+    );
   }
 }
 
