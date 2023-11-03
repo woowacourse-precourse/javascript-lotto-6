f5e4c5691f3261453e819fe93687fdf18be00c13
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-03 11:56:00 +0900

    feat(계산): 우승상금 합산 기능 추가

--- src/utils/caculate.js
+++ src/utils/caculate.js
@@ -1,3 +1,5 @@
+import { WINNING_PRIZE_NUMBERS } from "../constants/numbers.js";
+
 export function devideMoneyForLotto(moneyForLotto) {
   return moneyForLotto / 1000;
 }
@@ -5,4 +7,14 @@ export function devideMoneyForLotto(moneyForLotto) {
 export function profitRate(moneyForLotto, winningPrizeSum) {
   const rate = (winningPrizeSum / moneyForLotto) * 100;
   return Math.round(rate * 10) / 10;
+}
+
+export function sumOfWinning(rankCounts) {
+  const sum =
+    rankCounts[1] * WINNING_PRIZE_NUMBERS.rank1 +
+    rankCounts[2] * WINNING_PRIZE_NUMBERS.rank2 +
+    rankCounts[3] * WINNING_PRIZE_NUMBERS.rank3 +
+    rankCounts[4] * WINNING_PRIZE_NUMBERS.rank4 +
+    rankCounts[5] * WINNING_PRIZE_NUMBERS.rank5;
+  return sum;
 }
