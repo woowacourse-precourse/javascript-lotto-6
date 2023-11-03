de5ccdd5ca2a75f91d6c8a42c209e137d728930c
rbgksqkr <rbgks1937@naver.com>
2023-11-04 00:30:35 +0900

    refactor: 입력 메세지 상수화

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { Random, Console } from '@woowacourse/mission-utils';
+import MESSAGE from './constant/message.js';
 import Lotto from './Lotto.js';
 import inputView from './View/inputView.js';
 
 class App {
   async play() {
-    const purchasePrice = await inputView.readPurchasePriceAsync('구입금액을 입력해 주세요.\n');
+    const purchasePrice = await inputView.readPurchasePriceAsync(MESSAGE.INPUT_PRICE);
     const lottoCount = purchasePrice / 1000;
-    Console.print(`${lottoCount}개를 구매했습니다.`);
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
     const myLottoNumbers = this.purchaseLottos(lottoCount);
-    const winningNumbers = await inputView.readWinningNumbersAsync(
-      '\n당첨 번호를 입력해 주세요.\n'
-    );
+    const winningNumbers = await inputView.readWinningNumbersAsync(MESSAGE.INPUT_WINNING_NUMBER);
     const lotto = new Lotto(winningNumbers);
-    const bonusNumber = await inputView.readBonusNumberAsync('\n보너스 번호를 입력해 주세요.\n');
+    const bonusNumber = await inputView.readBonusNumberAsync(MESSAGE.INPUT_BONUS_NUMBER);
     lotto.getLottoResult(myLottoNumbers, bonusNumber, purchasePrice);
   }
 
---
+++ src/constant/message.js
@@ -0,0 +1,7 @@
+const MESSAGE = {
+  INPUT_PRICE: '구입금액을 입력해 주세요.\n',
+  INPUT_WINNING_NUMBER: '\n당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
+};
+
+export default MESSAGE;
