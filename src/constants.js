287f5ebfc65a612b1ebb4843ef56a96188bbd891
김영은 <jessicak0828@gmail.com>
2023-11-03 21:35:05 +0900

    feat: 매직 넘버/리터럴을 constants.js 로 분리

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+const ONE_LOTTO_PRICE = 1000;
+const LOTTO_NUMBER_LENGTH = 6;
+const POSSIBLE_MIN_NUMBER = 1;
+const POSSIBLE_MAX_NUMBER = 45;
+const LOTTO_TO_MONEY = {
+	THREE: 5000,
+	FOUR: 50000,
+	FIVE: 1500000,
+	FIVE_AND_BONUS: 30000000,
+	SIX: 2000000000,
+};
+
+const ASK_HOW_MUCH_MONEY = '구입금액을 입력해 주세요.';
+const TELL_HOW_MANY_LOTTOS_BOUGHT = '개를 구매했습니다.';
+const ASK_LOTTO_NUMBER = '당첨 번호를 입력해 주세요.';
+const ASK_BONUS_NUMBER = '보너스 번호를 입력해 주세요.';
+const RESULT = '당첨 통계';
+const DIVISION_LINE = '---';
+const UNIT_GAE = '개';
+const THREE_EQUAL = '3개 일치 (5,000원) - ';
+const FOUR_EQUAL = '4개 일치 (50,000원) - ';
+const FIVE_EQUAL = '5개 일치 (1,500,000원) - ';
+const FIVE_AND_BONUS_EQUAL = '5개 일치, 보너스 볼 일치 (30,000,000원) - ';
+const SIX_EQUAL = '6개 일치 (2,000,000,000원) - ';
