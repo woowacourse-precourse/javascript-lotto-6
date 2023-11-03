d8852951295d0938ff70188d8d8c1ecd8ef05840
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-03 21:00:31 +0900

    [feat] : Add 입력 관련 클래스 실행 로직

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import {
+  MESSAGE_BONUS_NUMBER,
+  MESSAGE_PURCHASING_AMOUNT,
+  MESSAGE_PURCHASING_NUMBER,
+  MESSAGE_WINNING_NUMBER,
+} from "./constants/message";
+import Purchase from "./Purchase";
+import Issuance from "./Issuance";
+import Lotto from "./Lotto";
+import Bonus from "./Bonus";
+
 class App {
-  async play() {}
+  async play() {
+    const purchasingAmount = await MissionUtils.Console.readLineAsync(
+      MESSAGE_PURCHASING_AMOUNT
+    );
+    const checkedAmount = new Purchase(purchasingAmount);
+    MissionUtils.Console.print(
+      checkedAmount.getPurchaseCount + MESSAGE_PURCHASING_NUMBER
+    );
+
+    const issuance = new Issuance(checkedAmount.getPurchaseCount);
+    issuance.printIssuanceNumbers();
+
+    const winningNumber = MissionUtils.Console.readLineAsync(
+      MESSAGE_WINNING_NUMBER
+    );
+    const checkedWinningNumber = new Lotto(winningNumber);
+
+    const bonus = await MissionUtils.Console.readLineAsync(
+      MESSAGE_BONUS_NUMBER
+    );
+    const bonusNumber = new Bonus(bonus, checkedWinningNumber.get());
+  }
 }
 
 export default App;
