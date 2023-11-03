91acba11f9b4f26c220db7a801e27182bfc24e8a
brgndy <brgndy96@gmail.com>
2023-11-04 08:57:02 +0900

    feat(input): 로또 구입 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { LOTTO_MESSAGES } from './constants/lottoMessage.js';
+import { asyncFnHandlerWithError } from './utils/asyncFnHandlerWithError.js';
+import { InputView } from './views/InputView.js';
+import { OutputView } from './views/OutputView.js';
+import Money from './domains/Money.js';
+
 class App {
-  async play() {}
+  /**
+   * @type { number }
+   */
+
+  #lottoAmount;
+
+  async play() {
+    await asyncFnHandlerWithError(this.inputMoney, this);
+  }
+
+  async inputMoney() {
+    const money = await InputView.readLine(LOTTO_MESSAGES.input_money);
+    this.#lottoAmount = Money.getLottoAmount(money);
+    OutputView.divideLine();
+  }
 }
 
 export default App;
---
+++ src/constants/lottoMessage.js
@@ -0,0 +1,3 @@
+export const LOTTO_MESSAGES = Object.freeze({
+  input_money: '구입금액을 입력해 주세요.\n',
+});
