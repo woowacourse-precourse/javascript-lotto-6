2695df16478866750a58bd484aac3ba687d2e503
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-03 12:11:57 +0900

    refactor(조건): 함수 모듈화

--- src/utils/condition.js
+++ src/utils/condition.js
@@ -16,26 +16,33 @@ export function checkBonusBall(purchasedLotto, bonusBall) {
 
 export function recordRanks(countMatchArr, checkBonusArr) {
   let rankCounts = Array.from({ length: LOTTO_NUMBERS.ranks + 1 }, () => 0);
-  for (let i = 0; i < countMatchArr.length; i += 1) {
-    switch (countMatchArr[i]) {
-      case 3:
-        rankCounts[5] += 1; // 3개 일치는 5등
-        break;
-      case 4:
-        rankCounts[4] += 1; // 4개 일치는 4등
-        break;
-      case 5:
-        if (checkBonusArr[i]) {
-          rankCounts[2] += 1; // 5개 일치하고 보너스 볼이 일치하면 2등
-        } else {
-          rankCounts[3] += 1; // 5개 일치만 되어도 3등
-        }
-        break;
-      case 6:
-        rankCounts[1] += 1; // 6개 일치는 1등
-        break;
-      // 다른 경우에는 아무것도 하지 않습니다.
-    }
+  for (let index = 0; index < countMatchArr.length; index += 1) {
+    caseOfWinning(index, countMatchArr, checkBonusArr, rankCounts);
   }
   return rankCounts;
+}
+
+export function caseOfWinning(index, countMatchArr, checkBonusArr, rankCounts) {
+  switch (countMatchArr[index]) {
+    case 3:
+      rankCounts[5] += 1; // 3개 일치는 5등
+      break;
+    case 4:
+      rankCounts[4] += 1; // 4개 일치는 4등
+      break;
+    case 5:
+      checkBonusBallIsMatching(index, checkBonusArr, rankCounts);
+      break;
+    case 6:
+      rankCounts[1] += 1; // 6개 일치는 1등
+      break;
+  }
+}
+
+export function checkBonusBallIsMatching(index, checkBonusArr, rankCounts) {
+  if (checkBonusArr[index]) {
+    rankCounts[2] += 1; // 5개 일치하고 보너스 볼이 일치하면 2등
+  } else {
+    rankCounts[3] += 1; // 5개 일치만 되어도 3등
+  }
 }
