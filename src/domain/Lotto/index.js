f4a271a6545f27f83d1afbb797ab2787902eff70
jang_yoonsu <shagrat8546@gmail.com>
2023-11-03 16:43:06 +0900

    feat : Lotto 클래스 구현 - #validate() - getNumbers() - printNumbers()

---
+++ src/domain/Lotto/index.js
@@ -0,0 +1,47 @@
+import { LOTTO_RULE, MESSAGES } from "../../constants";
+import { Validation } from "../../utils";
+import { CustomError } from "../../exception";
+import { OutputView } from "../../view";
+
+const length = LOTTO_RULE.LENGTH;
+const min = LOTTO_RULE.RANGE.MIN;
+const max = LOTTO_RULE.RANGE.MAX;
+
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  #validate(numbers) {
+    if (!Validation.isLength(numbers, length)) {
+      throw new CustomError(MESSAGES.ERROR.LOTTO.NOT_LENGTH(length));
+    }
+
+    numbers.every((number) => {
+      if (!Validation.isNumber(number)) {
+        throw new CustomError(MESSAGES.ERROR.COMMON.NOT_NUMBER);
+      }
+
+      if (!Validation.isOnRange(number, min, max)) {
+        throw new CustomError(MESSAGES.ERROR.LOTTO.NOT_ON_RANGE(min, max));
+      }
+
+      if (!Validation.isInteger(number)) {
+        throw new CustomError(MESSAGES.ERROR.COMMON.NOT_INTEGER);
+      }
+    });
+  }
+
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  printNumbers() {
+    OutputView.print(this.#numbers.join(", "));
+  }
+}
+
+export default Lotto;
