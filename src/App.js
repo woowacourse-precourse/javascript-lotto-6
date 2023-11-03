150c69427c9e328ec9b77996a96315713923cb7a
whai2 <whai2125@gmail.com>
2023-11-03 14:48:50 +0900

    feat: 구입 금액에 해당하는 로또의 수를 파악하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ class App {
 export default App;
 
 PURCASE_COMMENT = "구입금액을 입력해 주세요."
+LOTTO_PRICE = 1000;
 
 async function inputMoney() {
   MissionUtils.Console.print(PURCASE_COMMENT);
   const totalMoney = await MissionUtils.Console.readLineAsync('');
   return totalMoney;
+}
+
+export function lottoCounter(money) {
+  const lottocounts = money/LOTTO_PRICE;
+  return lottocounts
 }
