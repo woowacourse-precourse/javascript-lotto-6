02c957362ee02b40c0dac7f0d7dcf1e1592fda01
skiende74 <skiende74@gmail.com>
2023-11-03 18:36:59 +0900

    feat(Lotto): 중복 테스트에 통과하게끔 중복검사기능 추가
    - validate 메소드 내부에, validateLength, validateRedundancy(중복) 넣음.

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,8 +9,19 @@ class Lotto {
   }
 
   #validate(numbers) {
+    this.#validateLength(numbers);
+    this.#validateRedundancy(numbers);
+  }
+
+  #validateLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
+
+  #validateRedundancy(numbers) {
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
     }
   }
 
