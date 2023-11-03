8275d15c966e27ed2b2bf9ac9592f6657b352aff
hyj5076 <hyj5076@hanmail.net>
2023-11-03 22:25:50 +0900

    chore(Messages): 상수 메세지 작성

---
+++ src/const/Messages.js
@@ -0,0 +1,25 @@
+export const ASK = {
+  INPUT_MONEY: "구입금액을 입력해 주세요.",
+  INPUT_NUMBERS: "당첨 번호를 입력해 주세요.",
+  INPUT_BONUS: "보너스 번호를 입력해 주세요.",
+};
+
+export const PRINT = {
+  HOW_MANY_BUY: "개를 구매했습니다.",
+  WINNING_STATISTICS: "당첨 통계",
+  DASH: "-",
+  DASH_LINE: "---",
+  WON: "(원)",
+  COUNT: "개",
+  RIGHT_COUNT: "개 일치",
+  RIGHT_BONUS: "보너스 볼 일치",
+  RETURN_RATE: "총 수익률은 %입니다.",
+};
+
+export const ERROR = {
+  ONLY_NUMBER: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  UNIQUE_NUMBER: "[ERROR] 로또 번호는 서로 다른 숫자여야 합니다.",
+  HOW_MANY_SIX: "[ERROR] 6개의 숫자를 입력하세요.",
+  HOW_MANY_ONE: "[ERROR] 1개의 보너스 숫자를 입력하세요.",
+  THOUSAND_UNIT: "[ERROR] 구입금액은 천 원 단위로 입력하세요.",
+};
