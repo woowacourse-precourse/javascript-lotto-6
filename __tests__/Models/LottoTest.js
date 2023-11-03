245b28fa3322db5ecbbf53fb9271e2e6e36e169e
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 21:53:29 +0900

    Fix(test): LottoTest 테스트 케이스 input들 string으로 수정

--- __tests__/Models/LottoTest.js
+++ __tests__/Models/LottoTest.js
@@ -5,24 +5,26 @@ import LOTTO_SETTINGS from "../../src/config/gameSetting.js";
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      new Lotto(["1", "2", "3", "4", "5", "6", "7"]);
     }).toThrow(LottoError.PREFIX);
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
+      new Lotto(["1", "2", "3", "4", "5", "5"]);
     }).toThrow(LottoError.PREFIX);
   });
 
   test("로또 번호는 오름차순으로 정렬되어야한다.", () => {
-    expect(new Lotto([6, 5, 4, 3, 2, 1]).getNumbers()).toEqual([
+    expect(new Lotto(["6", "5", "4", "3", "2", "1"]).getNumbers()).toEqual([
       1, 2, 3, 4, 5, 6,
     ]);
   });
 
   test(`로또 번호는 ${LOTTO_SETTINGS.NUMBER_RANGE.MIN} 이상  ${LOTTO_SETTINGS.NUMBER_RANGE.MAX} 이하여야한다.`, () => {
-    expect(() => new Lotto([1, 2, 3, 4, 5, 90])).toThrow(LottoError.PREFIX);
+    expect(() => new Lotto(["1", "2", "3", "4", "5", "90"])).toThrow(
+      LottoError.PREFIX
+    );
   });
 });
