9d184e2770fdc7fc30951fd867dd0aff86527cac
hyj5076 <hyj5076@hanmail.net>
2023-11-04 03:04:18 +0900

    test: 사용자 로또 번호 출력 로직 단위 테스트

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,3 +1,18 @@
+// UserLottoNumber.js 관련 단위 테스트
+function parseInput(input) {
+  return input.split(",").map((number) => parseInt(number.trim(), 10));
+}
+
+describe("사용자 로또 번호", () => {
+  test("공백 무시하고 10진수의 배열로 받음", () => {
+    const input = "5, 12,23 , 32,45, 52";
+    const output = [5, 12, 23, 32, 45, 52];
+    const result = parseInput(input);
+    expect(result).toEqual(output);
+  });
+});
+
+// RunLotteryMachine.js 관련 단위 테스트
 const MissionUtils = {
   Random: {
     pickUniqueNumbersInRange: jest.fn(),
