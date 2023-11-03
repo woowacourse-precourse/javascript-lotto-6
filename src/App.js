67c72e4016572890d1fa10fe0c01c94d9d41f7ae
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-11-03 23:24:51 +0900

    feature : 사용자의 로또 구입 금액 입력을 받고, 그에 대한 유효성 검사 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+}
+
+class LottoPurchase {
+  async #askPurchaseLottoAmount() {
+    let amount;
+
+    while (true) {
+      try {
+        amount = parseInt(
+          await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.")
+        );
+
+        if (!amount || amount < 1000 || amount % 1000 !== 0) {
+          throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+        }
+
+        break;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
+
+    return amount;
+  }
 }
 
 export default App;
