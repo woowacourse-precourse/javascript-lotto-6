61b3ce4ea75e5ed2995f2bf85f1a4f3a75cb4fc5
Hong sung soo <sow9707@daum.net>
2023-11-03 17:58:09 +0900

    refactor: 당첨 번호 범위,문자열,공백 에 따른 예외처리하는 함수 분리 후 유효성 함수 리팩토링

--- src/validation.js
+++ src/validation.js
@@ -12,27 +12,26 @@ function validationUserMoney(userMoney) {
     }
 }
 
-function qualificationWinnerNumber(winnerNumber) {
-    if(winnerNumber < MIN_NUMBER || winnerNumber > MAX_NUMBER) {
+function qualificationLottoNumber(number) {
+    if(number < MIN_NUMBER || number > MAX_NUMBER) {
         throw new Error("[ERROR] 1~45 내의 숫자를 입력해주세요.");
     }
-    if(!+winnerNumber) {
+    if(!+number) {
         throw new Error("[ERROR] 숫자를 입력해주세요.");
     }
 }
 
-function validationBonusNumber(bonusNumber, winnerNumbers) {
-    winnerNumbers.forEach((winnerNumber) => qualificationBonusNumber(bonusNumber, winnerNumber));
-    if(bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
-        throw new Error("[ERROR] 1~45 내의 숫자를 입력해주세요.");
-    }
-    if(!+bonusNumber) {
-        throw new Error("[ERROR] 숫자를 입력해주세요.");
-    }
+function validationWinnerNumber(winnerNumbers) {
+    winnerNumbers.forEach((winnerNumber) => qualificationLottoNumber(winnerNumber));
 }
 
 function qualificationBonusNumber(bonusNumber, winnerNumber) {
     if(bonusNumber == winnerNumber) {
         throw new Error("[ERROR] 당첨 번호와 다른 번호를 입력하세요.");
     }
+}
+
+function validationBonusNumber(bonusNumber, winnerNumbers) {
+    winnerNumbers.forEach((winnerNumber) => qualificationBonusNumber(bonusNumber, winnerNumber));
+    qualificationLottoNumber(bonusNumber);
 }
