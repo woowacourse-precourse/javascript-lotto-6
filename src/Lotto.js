df9457d8283ce21c676edf6e6724c3e52b0203ea
skiende74 <skiende74@gmail.com>
2023-11-03 19:15:12 +0900

    style(Lotto): Airbnb 코드 Style 적용.
    - validate 메소드들은 정적메소드로 변경.
    -- this.변수에 접근하지 않기 때문.

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,22 +4,22 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    Lotto.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    this.#validateLength(numbers);
-    this.#validateRedundancy(numbers);
+  static #validate(numbers) {
+    Lotto.#validateLength(numbers);
+    Lotto.#validateRedundancy(numbers);
   }
 
-  #validateLength(numbers) {
+  static #validateLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  #validateRedundancy(numbers) {
+  static #validateRedundancy(numbers) {
     if (numbers.length !== new Set(numbers).size) {
       throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
     }
