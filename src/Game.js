4172bc7966b3d3d59abdac04ed7d8a1fa848d49d
UBiiiii <willowman55@gmail.com>
2023-11-03 22:29:57 +0900

    [feat] 보너스 번호 입력 기능 구현

--- src/Game.js
+++ src/Game.js
@@ -37,6 +37,18 @@ class Game {
     this.choice = new Lotto(input.split(","));
   }
 
+  async getBonusNumber() {
+    const input = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+    this.#validateNumber(input);
+    this.bonus = input;
+  }
+
+  #validateNumber(number) {
+    if(isNaN(number) || number < 1 || number > 45) {
+      throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.")
+    }
+  }
+
 };
 
 export default Game;
