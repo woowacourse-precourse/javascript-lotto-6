2178b4496c87033de27aa0cae16da52fbd98b59b
jaeml00 <jaeml0630@gmail.com>
2023-11-03 17:23:11 +0900

    test: remove spaces and convert to array
    공백 제거 후 숫자 배열로 전환 테스트

--- __tests__/LottoGameTest.js
+++ __tests__/LottoGameTest.js
@@ -13,5 +13,9 @@ describe("로또게임 클래스 테스트", () => {
   test("구입금액이 1000원 단위가 아닌 경우 예외 발생", () => {
     expect(() => LottoGame.validatePurchaseAmount('10001')).toThrow("[ERROR]");
   });
+
+  test("로또 번호 사이에 공백이 있는 경우 공백 제거", () => {
+    expect(LottoGame.winningNumberToWinningNumberArray('1,2,3,4, 5,6')).toEqual([1,2,3,4,5,6]);
+  });
   // 아래에 추가 테스트 작성 가능
 });
