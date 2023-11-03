8d0bb2db2485dd6fdc7d328bed47628f8fa1a93f
rustandbone <dlehdgh14@naver.com>
2023-11-04 01:24:05 +0900

    기능 구현: 보너스 번호 입력, private field 생성

--- src/GuessLotto.js
+++ src/GuessLotto.js
@@ -1,16 +1,27 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class GuessLotto {
+  #guessNumbers;
+
+  #guessBonus;
+
   constructor() {
-    this.guessNumbers = [];
+    this.#guessNumbers = [];
+    this.#guessBonus = null;
   }
 
   async inputLottoNumber() {
     const personalLottoNumber =
       await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
 
-    this.guessNumbers = personalLottoNumber.split(',');
-    return this.guessNumbers;
+    this.#guessNumbers = personalLottoNumber.split(',');
+    return this.#guessNumbers;
+  }
+
+  async inputBonusNumber() {
+    this.#guessBonus =
+      await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    return this.#guessBonus;
   }
 }
 
