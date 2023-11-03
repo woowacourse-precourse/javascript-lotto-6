a2243f5ea3aca7c71bfb7885852a90b3665829f7
0jenn0 <jenn0.6n@gmail.com>
2023-11-03 23:52:26 +0900

    Test: LottoYeildCaculator 클래스 테스트에 1등~5등,당첨금 못받는 케이스 테스트 코츠 추가

--- __tests__/Service/LottoYieldCalculatorTest.js
+++ __tests__/Service/LottoYieldCalculatorTest.js
@@ -13,11 +13,41 @@ describe("LottoYeildCaculator 클래스 테스트", () => {
     );
   });
 
-  test("5개 맞으면 보너스 번호도 체크해야한다.", () => {
+  test("1등하는 케이스.", () => {
     const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
     const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "5", "6"], "8");
     expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
       PRIZES.FIRST
     );
+  });
+
+  test("3등하는 케이스", () => {
+    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
+    const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "5", "7"], "8");
+    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
+      PRIZES.THIRD
+    );
+  });
+
+  test("4등하는 케이스", () => {
+    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
+    const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "7", "8"], "9");
+    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
+      PRIZES.FOURTH
+    );
+  });
+
+  test("5등하는 케이스", () => {
+    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
+    const drawnLotto = new DrawnLotto(["1", "2", "3", "7", "8", "9"], "10");
+    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
+      PRIZES.FIFTH
+    );
+  });
+
+  test("당첨금을 못받는 경우", () => {
+    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
+    const drawnLotto = new DrawnLotto(["1", "2", "7", "8", "9", "10"], "11");
+    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(0);
   });
 });
