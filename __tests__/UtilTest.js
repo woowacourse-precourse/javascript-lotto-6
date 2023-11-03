c6678fa7c5ef3f5128f707f0d84bab5007eae79d
ReturnReason <reasonz@daum.net>
2023-11-04 03:44:45 +0900

    test(UtilTest): calcProfit 유틸 함수 테스트

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -1,3 +1,4 @@
+import calcProfit from '../src/utils/calcProfit';
 import formatLottoNumbers from '../src/utils/formatLottoNumbers';
 
 describe('💙 Util 함수를 테스트합니다. ฅ^._.^ฅ', () => {
@@ -6,5 +7,19 @@ describe('💙 Util 함수를 테스트합니다. ฅ^._.^ฅ', () => {
     const expectedResult = '[8, 21, 23, 41, 42, 43]';
 
     expect(formatLottoNumbers(lottoNumbers)).toBe(expectedResult);
+  });
+
+  test('[calcProfit] 구입 금액과 당첨 금액을 인자로 받아 포매팅된 수익률을 반환해요.', () => {
+    const mockData = {
+      expectedResults: ['62.5%', '100,000.0%'],
+      purchaseAmounts: [8_000, 30_000],
+      winningAmounts: [5_000, 30_000_000],
+    };
+
+    mockData.expectedResults.forEach((expectedResult, i) => {
+      expect(
+        calcProfit(mockData.purchaseAmounts[i], mockData.winningAmounts[i]),
+      ).toBe(expectedResult);
+    });
   });
 });
