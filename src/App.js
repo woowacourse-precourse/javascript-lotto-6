c542152753b6318dd7665bce71eaa4ce64baa6f4
dxwwan <kkw1700@gmail.com>
2023-11-03 23:31:26 +0900

    feat: 로또를 구매하는 기능의 클래스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 class App {
-  async play() {}
+class purchaseLotto {
+  #amountOfLotto;
+  constructor() {
+    this.credit = 0;
+    this.#amountOfLotto = 0;
+  }
+  async getCredit() {
+    const input = Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    this.credit = input;
+    return this.credit;
+  }
+
+  getAmountOfLotto() {
+    this.#amountOfLotto = (this.credit / 1000).toFixed(2);
+    console.log(this.#amountOfLotto);
+    return this.#amountOfLotto;
+  }
+
+  printAmountOfLotto() {
+    Console.print(`총 ${this.#amountOfLotto}개를 구매했습니다.`);
+  }
 }
 
 export default App;
