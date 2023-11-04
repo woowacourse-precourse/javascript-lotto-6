2d0539e5cab5e27006062a8aa532de2974684a45
Seo San <dannysir@naver.com>
2023-11-04 19:26:08 +0900

    feat : getNumbers() 생성 Lotto 클래스에 입력한 로또를 받기 위해 get함수 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import MY_LOTTO from "./MyLotto.js";
 import OUT_MY_LOTTO_ARR from "./OutMyLotto.js";
 import INPUT_ANSWER from "./InputAnswer.js";
 import INPUT_BONUS from "./InputBonus.js";
+import Lotto from "./Lotto.js";
 
 class App {
   async play() {
     const MONEY = (await INPUT_MONEY())/1000;
     const LOTTO_ARR = MY_LOTTO(MONEY);
     OUT_MY_LOTTO_ARR(MONEY,LOTTO_ARR);
-    const ANSWER_ARR = await INPUT_ANSWER();
+    const ANSWER_ARR = new Lotto(await INPUT_ANSWER());
     const BONUS_NUM = await INPUT_BONUS();
   }
 }
--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const SET_NUM = new Set([...numbers]);
+    if (SET_NUM.size !== numbers.length) {
+      throw new Error("[ERROR] 중복된 값을 입력하지 마세요");
+    }
   }
 
   // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
