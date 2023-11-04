0f24df4209f7ac67477aa5ed2a25c85a89cc6561
Hyebin Lee <qnd1215@naver.com>
2023-11-04 19:05:10 +0900

    로또 당첨금 계산 함수

---
+++ __tests__/DangchemTest.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-new */
+// 작동안됨.. 뭐가 문젤까.......
+import App from '../src/App.js';
+
+describe('로또 클래스 테스트', () => {
+  test('로또 당첨 결과를 확인한다.', () => {
+    const app = new App();
+    const num = 6;
+    const arrayofLotto = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+      [2, 3, 4, 5, 6, 7],
+      [3, 4, 5, 6, 7, 8],
+    ];
+    const inputNumbers = {
+      winning: [1, 2, 3, 4, 5, 6],
+      bonus: 7,
+    };
+    expect(app.play(num, arrayofLotto, inputNumbers)).toEqual([2, 2, 0, 1, 0]);
+  });
+});
--- src/App.js
+++ src/App.js
@@ -3,99 +3,10 @@ import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    this.#makeLottoNum();
-    // await this.allOfInputfunc();
-    await this.#allOfMoneyfunc();
-  }
-
-  // 당첨번호 & 보너스번호 입력 함수 총집합
-  static async allOfInputfunc() {
-    const winningNum = await this.stringToNum();
-    this.validateWinningNumLength(winningNum);
-    const bonusNum = await this.getBonusNum();
-    this.duplCheckOfWinngNum(winningNum);
-    this.duplCheckOfBonus(winningNum, bonusNum);
-    this.validateNumRange(winningNum);
-    this.validateNumRange([bonusNum]); // 배열 형태로 전달
-  }
-
-  #makeLottoNum() {
-    const NEED_NUM = 6;
-    const myLottoNum = [];
-    while (myLottoNum.length < NEED_NUM) {
-      const pick = MissionUtils.Random.pickNumberInRange(1, 45);
-      if (!myLottoNum.includes(pick)) {
-        myLottoNum.push(pick); // 일단 이 배열은 정렬 안되어 있음. 하지만 숫자상태
-      }
-    }
-    MissionUtils.Console.print(myLottoNum.sort((a, b) => a - b)); // 정렬되었지만 문자열상태 다른함수로 뺄거임.
-
-    return myLottoNum;
-  }
-
-  static async getWinningNum() {
-    const getNum =
-      await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
-    const winningNum = String(getNum).split(','); // 문자열 형태
-
-    return winningNum; // 문자열 상태
-  }
-
-  // 입력 값이 숫자인지 유효성도 같이 확인하면서 문자열을 숫자로 변환.
-  static async stringToNum() {
-    const getWinningNum = await this.getWinningNum();
-    const numTypeOfWin = getWinningNum.map(Number);
-    if (numTypeOfWin.includes(NaN)) {
-      throw new Error(`[ERROR] 입력 값은 숫자여야 합니다.`);
-    }
-    Console.print(numTypeOfWin); // 지워야 함
-    return numTypeOfWin;
-  }
-
-  // 보너스 넘버
-  static async getBonusNum() {
-    const bonusNum =
-      await MissionUtils.Console.readLineAsync(
-        '보너스 번호를 입력해 주세요.\n',
-      );
-    const NumTypeBonus = Number(bonusNum);
-    if (Number.isNaN(NumTypeBonus)) {
-      throw new Error(`보너스 입력값은 숫자여야 합니다.`);
-    }
-    return NumTypeBonus;
-  }
-
-  static duplCheckOfBonus(winningNum, bonusNum) {
-    if (winningNum.includes(Number(bonusNum))) {
-      throw new Error(`[ERRPR] 보너스 숫자는 중복될 수 없습니다.`);
-    }
-  }
-
-  // 정답 숫자 길이 유효성 확인
-  static validateWinningNumLength(winningNum) {
-    const WINNINGNUM_LENGTH = 6;
-    if (winningNum.length !== WINNINGNUM_LENGTH) {
-      throw new Error(`[ERROR] 숫자 6개를 입력해야 합니다.`);
-    }
-  }
-
-  // 정답 숫자 입력 범위 확인, 정답, 보너스 둘 다 사용 가능
-  static validateNumRange(inputNum) {
-    const MIN_NUM = 1;
-    const MAX_NUM = 45;
-    inputNum.forEach((number) => {
-      if (number < MIN_NUM || MAX_NUM < number) {
-        throw new Error(`[ERROR] 숫자 범위는 1~45 사이입니다.`);
-      }
-    });
-  }
-
-  // 정답 숫자 중복 확인.
-  static duplCheckOfWinngNum(winningNum) {
-    const setWinningNum = new Set(winningNum);
-    if (winningNum.length !== setWinningNum.size) {
-      throw new Error(`[ERROR] 당첨 번호는 중복될 수 없습니다.`);
-    }
+    const numOfBuy = await this.#allOfMoneyfunc();
+    const arrayofLotto = this.#makeArrayOfLottoNum(numOfBuy);
+    const numbers = await this.#allOfInputfunc();
+    this.#allofPrizeMoneyCalculfunc(numOfBuy, arrayofLotto, numbers);
   }
 
   // ------로또구입 함수------
@@ -103,13 +14,14 @@ class App {
     const inputMoney = await this.getMoney();
     this.#validateMoneyIsNum(inputMoney);
     this.#validateMoneyUnit(inputMoney);
-    this.#claculateNumOfBuy(inputMoney);
+    const numOfBuy = this.#claculateNumOfBuy(inputMoney);
+
+    return numOfBuy; // 숫자형
   }
 
   // 로또 구입 금액 입력받기
   async getMoney() {
-    const inputMoney =
-      await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    const inputMoney = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
 
     return Number(inputMoney);
   }
@@ -136,7 +48,139 @@ class App {
     return numOfBuy;
   }
 
+  // -------------------------------랜덤 로또생성함수-----
+
+  // 랜덤 로또 번호 생성 함수 (반복되어야 함)
+  #makeLottoNum() {
+    const NEED_NUM = 6;
+    const myLottoNum = [];
+    while (myLottoNum.length < NEED_NUM) {
+      const pick = MissionUtils.Random.pickNumberInRange(1, 45);
+      if (!myLottoNum.includes(pick)) {
+        myLottoNum.push(pick); // 일단 이 배열은 정렬 안되어 있음. 하지만 숫자상태
+      }
+    }
+    const sortLottoNum = myLottoNum.sort((a, b) => a - b);
+    // MissionUtils.Console.print(myLottoNum.sort((a, b) => a - b)); // 정렬되었지만 숫자형태 다른함수로 뺄거임.
+    Console.print(sortLottoNum);
+
+    return sortLottoNum;
+  }
+
+  #makeArrayOfLottoNum(numOfBuy) {
+    const arrayofLotto = [];
+    while (arrayofLotto.length < numOfBuy) {
+      arrayofLotto.push(this.#makeLottoNum());
+    }
+
+    return arrayofLotto;
+  }
+
+  // ------------------당첨번호 & 보너스번호 입력 함수 총집합--------------------
+  async #allOfInputfunc() {
+    const winningNum = await this.#stringToNum();
+    this.#validateWinningNumLength(winningNum);
+    const bonusNum = await this.#getBonusNum();
+    this.#duplCheckOfWinngNum(winningNum);
+    this.#duplCheckOfBonus(winningNum, bonusNum);
+    this.#validateNumRange(winningNum);
+    this.#validateNumRange([bonusNum]); // 배열 형태로 전달
+    const numbers = {
+      winning: winningNum,
+      bonus: bonusNum,
+    };
+    return numbers;
+  }
+
+  async #getWinningNum() {
+    const getNum = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    const winningNum = String(getNum).split(','); // 문자열 형태
+
+    return winningNum; // 문자열 상태
+  }
+
+  // 입력 값이 숫자인지 유효성도 같이 확인하면서 문자열을 숫자로 변환.
+  async #stringToNum() {
+    const getWinningNum = await this.#getWinningNum();
+    const numTypeOfWin = getWinningNum.map(Number);
+    if (numTypeOfWin.includes(NaN)) {
+      throw new Error(`[ERROR] 입력 값은 숫자여야 합니다.`);
+    }
+    Console.print(numTypeOfWin); // 지워야 함
+    return numTypeOfWin;
+  }
+
+  // 보너스 넘버
+  async #getBonusNum() {
+    const bonusNum = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    const NumTypeBonus = Number(bonusNum);
+    if (Number.isNaN(NumTypeBonus)) {
+      throw new Error(`보너스 입력값은 숫자여야 합니다.`);
+    }
+    return NumTypeBonus;
+  }
+
+  #duplCheckOfBonus(winningNum, bonusNum) {
+    if (winningNum.includes(Number(bonusNum))) {
+      throw new Error(`[ERRPR] 보너스 숫자는 중복될 수 없습니다.`);
+    }
+  }
+
+  // 정답 숫자 길이 유효성 확인
+  #validateWinningNumLength(winningNum) {
+    const WINNINGNUM_LENGTH = 6;
+    if (winningNum.length !== WINNINGNUM_LENGTH) {
+      throw new Error(`[ERROR] 숫자 6개를 입력해야 합니다.`);
+    }
+  }
+
+  // 정답 숫자 입력 범위 확인, 정답, 보너스 둘 다 사용 가능
+  #validateNumRange(inputNum) {
+    const MIN_NUM = 1;
+    const MAX_NUM = 45;
+    inputNum.forEach((number) => {
+      if (number < MIN_NUM || MAX_NUM < number) {
+        throw new Error(`[ERROR] 숫자 범위는 1~45 사이입니다.`);
+      }
+    });
+  }
+
+  // 정답 숫자 중복 확인.
+  #duplCheckOfWinngNum(winningNum) {
+    const setWinningNum = new Set(winningNum);
+    if (winningNum.length !== setWinningNum.size) {
+      throw new Error(`[ERROR] 당첨 번호는 중복될 수 없습니다.`);
+    }
+  }
+
   // --------------------------------당첨금 계산 함수-------
+  #allofPrizeMoneyCalculfunc(numOfBuy, arrayofLotto, inputNumbers) {
+    const countOfWinning = this.#calculateFistPrize(arrayofLotto, inputNumbers);
+
+    Console.print(`당첨결과 : ${countOfWinning}`);
+  }
+
+  #calculateFistPrize(arrayofLotto, inputNumbers) {
+    const countOfWinning = [0, 0, 0, 0, 0];
+    Console.print(`당첨숫자 : ${countOfWinning}`);
+    arrayofLotto.forEach((myLotto) => {
+      const matchArray = myLotto.filter((value) => inputNumbers.winning.includes(value));
+      const ranks = matchArray.length;
+      if (ranks === 6) countOfWinning[0] += 1; // 1st
+      if (ranks === 5 && inputNumbers.winning.includes(inputNumbers.bonus)) {
+        // 2nd
+        countOfWinning[1] += 1;
+      }
+      if (ranks === 5 && !inputNumbers.winning.includes(inputNumbers.bonus)) {
+        // 3rd
+        countOfWinning[2] += 1;
+      }
+      if (ranks === 4) countOfWinning[3] += 1; // 4th
+      if (ranks === 3) countOfWinning[4] += 1; // 5th
+    });
+    Console.print(`당첨결과 : ${countOfWinning}`);
+    return countOfWinning;
+  }
 }
 
 export default App;
