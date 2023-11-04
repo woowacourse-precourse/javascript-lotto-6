69d0e91896f34bd8ad82832d7aa554738ad70c8a
rbgksqkr <rbgks1937@naver.com>
2023-11-04 23:36:57 +0900

    test: 로또 구매 및 출력 단위 테스트 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,6 @@
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from '../src/Lotto.js';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -122,6 +123,66 @@ describe('로또 테스트', () => {
       '5개 일치, 보너스 볼 일치 (30,000,000원) - 1개',
       '6개 일치 (2,000,000,000원) - 0개',
       '총 수익률은 1000000.0%입니다.',
+    ];
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('로또를 원하는 개수만큼 구매하는 지 테스트', async () => {
+    // given
+    const logSpy = getLogSpy();
+
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+    ]);
+
+    const PURCHASE_COUNT = 3;
+
+    // when
+    const app = new App();
+    app.purchaseLottos(PURCHASE_COUNT);
+
+    // then
+    const logs = ['[8, 21, 23, 41, 42, 43]', '[3, 5, 11, 16, 32, 38]', '[7, 11, 16, 35, 36, 44]'];
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test('당첨번호, 로또번호, 구매금액이 주어졌을 때 정상적으로 출력되는 지 테스트', async () => {
+    // given
+    const logSpy = getLogSpy();
+
+    const MY_LOTTO_NUMBERS = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
+    const BONUS_NUMBER = 33;
+    const PURCHASE_PRICE = 8000;
+
+    // when
+    const lotto = new Lotto([8, 21, 23, 24, 25, 26]);
+    lotto.getLottoResult(MY_LOTTO_NUMBERS, BONUS_NUMBER, PURCHASE_PRICE);
+
+    // then
+    const logs = [
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 62.5%입니다.',
     ];
 
     logs.forEach((log) => {
