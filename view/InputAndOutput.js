d54fcdc34891a9234ff4ec7cb8cf4762420f83d2
leedbswo107 <ljyu1@naver.com>
2023-11-05 00:06:03 +0900

    feat: 사용자 입력 완료

--- view/InputAndOutput.js
+++ view/InputAndOutput.js
@@ -2,16 +2,21 @@ import { Console } from "@woowacourse/mission-utils";
 import { MESSAGE } from "../src/common/OutputMessage.js";
 import Validate from "../src/common/validate.js";
 import Lotto from "../src/Lotto.js";
+import Lottogenerator from "../src/Lottogenerator.js";
 
 class InputAndOutput{
     
     constructor(){
         this.validate = new Validate();
+        this.lottogenerator = new Lottogenerator();
     }
 
     async inputPurchaseAmount(){
         const PURCHASE_AMOUNT = await Console.readLineAsync(`${MESSAGE.PURCHASE_NUMBER_INPUT}\n`);   
         const VALIDATE_PURCHASE_AMOUNT = this.validate.validatePrice(PURCHASE_AMOUNT);
+        const LOTTO_AMOUNT = VALIDATE_PURCHASE_AMOUNT/1000;
+        this.purchasedLottoOutput(LOTTO_AMOUNT);
+
         return VALIDATE_PURCHASE_AMOUNT;
     }
     async inputWinNumber(){
@@ -26,6 +31,15 @@ class InputAndOutput{
         const BONUS_NUMBER = await Console.readLineAsync(`${MESSAGE.BONUS_NUMBER_INPUT}\n`);
         const VALIDATE_BONUS_NUMBER = this.validate.validateBonusNumber(BONUS_NUMBER);
         return VALIDATE_BONUS_NUMBER;     
+    }
+    purchasedLottoOutput(amount){
+        const PURCHASE_LOTTO_NUMBER = this.lottogenerator.autoLottoGenerator(amount);;
+        Console.print(`${amount}${MESSAGE.NUMBER_OF_PURCHASE}`);
+
+        PURCHASE_LOTTO_NUMBER.forEach(e => {
+           Console.print(e); 
+        });
+        Console.print('');
     }
 }
 export default InputAndOutput;
