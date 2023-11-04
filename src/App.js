7f57baa82caac79b870d2ddf1d3de1945c8840d0
yesiamok3 <jaehyunok@cau.ac.kr>
2023-11-05 01:24:39 +0900

    feat: 구입 금액만큼 로또 발행
    입력받은 금액을 1000으로 나누어 발행할 티켓 수 계산
    1~45 사이 서로 다른 숫자 6개로 티켓 발행

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import { ERROR } from './Constant.js'
 
 class App {
   async play() {
     let lottoMoney = await inputMoney();
     checkMoney(lottoMoney);
+    let tickets = issueTickets(lottoMoney);
   }
 }
 
@@ -17,6 +18,19 @@ async function inputMoney() {
 function checkMoney(lottoMoney) {
   if (Number.isNaN(lottoMoney)) throw new Error(ERROR.MONEY_IS_NAN);
   if (lottoMoney % 1000 != 0) throw new Error(ERROR.MONEY_HAS_REMAINDER);
+}
+
+function issueTickets(lottoMoney) {
+  ticketsToIssue = lottoMoney / 1000;
+  tickets = [];
+
+  for (let i = 0; i < ticketsToIssue; i++) {
+    ticketNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    ticket = Lotto(ticketNumbers);
+    tickets.append(ticket);
+  }
+
+  return tickets
 }
 
 export default App;
