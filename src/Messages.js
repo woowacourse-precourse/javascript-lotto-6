3e35340444a43cfb54da9fd147d377c7c594a7f2
chysis <chysiss@naver.com>
2023-11-04 23:11:15 +0900

    feat(Messages): Messages 객체 추가 구현

--- src/Messages.js
+++ src/Messages.js
@@ -1,8 +1,9 @@
 const Messages = {
-  PURCHASE_AMOUNT_INPUT: '구입금액을 입력해 주세요.',
+  PURCHASE_AMOUNT_INPUT: '구입금액을 입력해 주세요.\n',
   PURCHASE_AMOUNT_ISNAN: '[ERROR] 숫자를 입력해 주세요.',
   PURCHASE_AMOUNT_NOT_DIVIDED:
     '[ERROR] 구입금액은 1000원 단위로 입력해 주세요.',
+  PURCHASED_TICKET_PRINT: '개를 구매했습니다.',
 };
 
 export default Messages;
