eac1a3842f824ecc6dc4620ec4b1252c2392aa00
Rki0 <pky00823@gmail.com>
2023-11-04 16:39:35 +0900

    test: Lotto 클래스 테스트 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,20 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호에 숫자가 아닌 값이 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, NaN, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 1 이상 45 이하의 값이 아니라면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("getter를 통해 검증된 로또 번호를 얻는다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getUserLotto()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
