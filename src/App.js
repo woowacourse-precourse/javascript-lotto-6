9960d64a61641fa609ec99eebbe62f2d472a623b
jadewisemann <jadewisemann@gmail.com>
2023-11-05 08:23:21 +0900

    FEAT: Add Function, in 'Print.js', 'printArray(array)'

--- src/App.js
+++ src/App.js
@@ -6,10 +6,24 @@ class App {
     // const userInputMoney = await new UserInput().getPurchasingPrice();
     const maxLottoNumber = 0 // 최대 구매 가능량 계산 로직
     print.maxLottoNumber(maxLottoNumber)
-    
-    
+    // 로또 숫자 랜덤 생성
+    // 전부다 프린트
+    const array = [
+      [1, 2, 3, 4],
+      [6, 5, 3, 2],
+      [1, 1, 2, 3],
+      
+    ]
+    print.lottoNumber(array)
+    // 순회하면서 Lotto 생성
+    // 당첨 번호 입력 받기
+    // 보너스 번호 입력 받기
+    // 당첨 통계
+
+
     // const userInputLotto = await new UserInput().getLottoNumber();
     
+
   }
 }
 
--- src/Print.js
+++ src/Print.js
@@ -2,7 +2,14 @@ import { Console } from "@woowacourse/mission-utils";
 const print = {
   maxLottoNumber(maxNumber) {
     printMessage(`${maxNumber}개를 구매했습니다.`)
-  }  
+  },
+  lottoNumber(array) {
+    array.map(numbers => printArray(numbers))
+  } 
+}
+
+function printArray(array) {
+  printMessage('[' + array.join(', ') + ']');
 }
 
 function printMessage(message) {
