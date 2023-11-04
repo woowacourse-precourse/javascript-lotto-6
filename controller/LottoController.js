5c8125c2d0439bf3cb62b108ce47ea5c5d372acc
이용성 <ukal5006@kakao.com>
2023-11-04 17:52:29 +0900

    feat(LottoController): 로또 수량 설정 함수 추가

---
+++ controller/LottoController.js
@@ -0,0 +1,30 @@
+import PurchaseAmount from '../models/PurchaseAmount.js';
+
+class LottoController {
+  #lottoCount;
+
+  constructor(inputView, outputView) {
+    this.inputView = inputView;
+    this.outputView = outputView;
+  }
+
+  async start() {
+    await this.setLottoCount();
+  }
+
+  async setLottoCount() {
+    while (true) {
+      let purchaseAmountInput;
+      try {
+        purchaseAmountInput = await this.inputView.getPurchaseAmount();
+        const purchaseAmount = new PurchaseAmount(purchaseAmountInput);
+        this.#lottoCount = purchaseAmount.getLottoCount();
+        break;
+      } catch (error) {
+        this.outputView.printError(error.message);
+      }
+    }
+  }
+}
+
+export default LottoController;
