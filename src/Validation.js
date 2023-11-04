c602191fec35b423c97e351f21145dfc165d4d86
Jeongeun Lee <leeju1009@gmail.com>
2023-11-04 22:39:32 +0900

    refactor: 당첨 번호 유효성 검사 함수를 분리

--- src/Validation.js
+++ src/Validation.js
@@ -1,10 +1,17 @@
 class Validation {
-  static isValidWinningNumbers(numbers) {
+  isValidWinningNumbers(numbers) {
+    this.isNonDuplicatedNumbers(numbers);
+    this.isNumbersInRange(numbers);
+  }
+
+  static isNonDuplicatedNumbers(numbers) {
     const nonDuplicatedNumbers = new Set(numbers);
     if (nonDuplicatedNumbers.size !== numbers.length) {
       throw new Error('[ERROR] 중복된 숫자는 입력할 수 없습니다.');
     }
+  }
 
+  static isNumbersInRange(numbers) {
     numbers.forEach((number) => {
       if (number < 1 || number > 45) {
         throw new Error('[ERROR] 1이상 45이하의 숫자만 입력할 수 있습니다.');
