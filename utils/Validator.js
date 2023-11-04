6bba7073f0bf79ad8015f8dc0400f4c396faf89d
Jules <dali1999@naver.com>
2023-11-05 01:13:47 +0900

    feat(validateWin): '당첨 번호'입력 예외 처리 기능 추가

--- utils/Validator.js
+++ utils/Validator.js
@@ -9,5 +9,37 @@ class Validator {
       throw new Error(ErrorMessage.INVALID_PRICE);
     }
   }
+
+  static validateWinNum(win) {
+    if (win.length !== 6) {
+      throw new Error(ErrorMessage.INVALID_NUM_COUNT);
+    }
+
+    const set = new Set(win);
+    if (win.length !== set.size) {
+      throw new Error(ErrorMessage.DUPLICATE_NUMBER);
+    }
+
+    win.map((num) => {
+      if (isNaN(num)) {
+        throw new Error(ErrorMessage.INVALID_INPUT);
+      }
+      if (num < 1 || num > 45) {
+        throw new Error(ErrorMessage.INVALID_RANGE);
+      }
+    });
+  }
+
+  static validateBonusNum(bonus, win) {
+    if (isNaN(bonus)) {
+      throw new Error(ErrorMessage.INVALID_INPUT);
+    }
+    if (bonus < 1 || bonus > 45) {
+      throw new Error(ErrorMessage.INVALID_RANGE);
+    }
+    if (win.includes(bonus)) {
+      throw new Error(ErrorMessage.DUPLICATE_BONUS_WIN);
+    }
+  }
 }
 export default Validator;
