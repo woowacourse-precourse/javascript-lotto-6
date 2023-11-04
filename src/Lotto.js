deb9a87c7530bd51df22f73632054d642111b977
whai2 <whai2125@gmail.com>
2023-11-04 16:01:15 +0900

    feat(Lotto): 로또 번호 1~45 사이가 아닐 경우, 예외 처리 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,11 +1,10 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
     this.#validateDuplicate(numbers);
+    this.#validateRange(numbers);
     this.#numbers = numbers;
   }
 
@@ -20,6 +19,17 @@ class Lotto {
       if (numbers[i] === numbers[i+1]) {
         throw new Error("[ERROR] 로또 번호가 중복되었습니다.")
       }
+    }
+  }
+  #validateRange(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+      this.#validateRangeEachNumber(numbers[i]);
+    }
+  }
+
+  #validateRangeEachNumber(number) {
+    if (number > 45 || number < 1) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
     }
   }
 }
