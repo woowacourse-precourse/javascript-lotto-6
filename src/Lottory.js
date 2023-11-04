b8b93aae0c20bcaff5a9620a47450c3d610d94dd
Seo San <dannysir@naver.com>
2023-11-04 19:58:51 +0900

    feat : 정답 배열 생성 기능 구현 크기가 5인 배열에 순서대로 1등에 당첨된 로또의 갯수부터 순서대로 입력

--- src/Lottory.js
+++ src/Lottory.js
@@ -1,28 +1,41 @@
 class Lottory {
     #MY_LOTTO;
     #ANSWER;
+    #BONUS;
     #RESULT = [0, 0, 0, 0, 0];
 
-    constructor(Lotto, answer) {
-        this.#MY_LOTTO = (Lotto);
-        this.#ANSWER = (answer);
-        //this.checkLotto(Lotto);
+    constructor(Lotto, answer, Bonus) {
+        this.#MY_LOTTO = Lotto;
+        this.#ANSWER = answer;
+        this.#BONUS = Bonus;
+        this.checkLotto(Lotto);
     }
 
     checkLotto(Lotto) {
         Lotto.forEach(v => {
-
+            if (this.check(v) !== 0) this.#RESULT[this.check(v) - 1]++
         });
     }
 
     check(ARR) {
         let cnt = 0;
+        let isBonus = false;
         ARR.forEach(v => {
             if (this.#ANSWER.includes(v)) {
                 cnt++;
             }
+            if (v === this.#BONUS) isBonus = true;
         });
-        return cnt;
+        if (cnt === 3) return 5;
+        if (cnt === 4) return 4;
+        if (cnt ===5 && !isBonus) return 3;
+        if (cnt === 5 && isBonus) return 2;
+        if (cnt ===6) return 1;
+        return 0;
+    }
+
+    getAnswer() {
+        return this.#RESULT;
     }
 }
 
