ece4ba3d0be5478c2eaa5a255ee411ad586ffb3f
김민승 <albert050509@naver.com>
2023-11-05 03:27:34 +0900

    feat(lottogame) : feat getLottoPrice Function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "./constants/inputMessage.js";
+import Lottos from "./Lottos.js";
+
 class App {
-  async play() {}
+  lottos;
+
+  async play() {
+    await this.gameStart();
+  }
+
+  async gameStart() {
+    await this.getLottoPrice();
+  }
+
+  async getLottoPrice() {
+    const lottoPriceInput = await Console.readLineAsync(INPUT_MESSAGE.price);
+
+    try {
+      this.lottos = new Lottos(lottoPriceInput);
+    } catch (error) {
+      Console.print(error.message);
+      await this.getLottoPrice();
+    }
+  }
 }
 
 export default App;
