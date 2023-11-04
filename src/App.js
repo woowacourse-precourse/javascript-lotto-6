78dceda3d4e0fffc0d5a8c2f1792a95ff79122d6
Seo San <dannysir@naver.com>
2023-11-04 20:29:28 +0900

    feat : 당첨 통계 출력 당첨 통계 화면 출력

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import OUT_MY_LOTTO_ARR from "./OutMyLotto.js";
 import INPUT_ANSWER from "./InputAnswer.js";
 import INPUT_BONUS from "./InputBonus.js";
 import Lotto from "./Lotto.js";
+import Lottory from "./Lottory.js";
+import {MissionUtils} from "@woowacourse/mission-utils";
+import OUT_RESULT from "./OutResult.js";
 
 class App {
   async play() {
     const MONEY = (await INPUT_MONEY())/1000;
     const LOTTO_ARR = MY_LOTTO(MONEY);
     OUT_MY_LOTTO_ARR(MONEY,LOTTO_ARR);
-    const ANSWER_ARR = new Lotto(await INPUT_ANSWER());
+    const ANSWER = new Lotto(await INPUT_ANSWER());
     const BONUS_NUM = await INPUT_BONUS();
+    const MY_ANSWER = new Lottory(LOTTO_ARR, ANSWER.getNumbers(), BONUS_NUM);
+    OUT_RESULT(MY_ANSWER.getAnswer());
   }
 }
 
