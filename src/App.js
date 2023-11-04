eb460faa7fb4c01be8de7dbb1bf41f30c56894fc
gon <hss3522@gmail.com>
2023-11-04 11:09:09 +0900

    feat: Add validation for winning numbers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import LottoGenerator from "./LottoGenerator.js";
+import Lotto from "./Lotto.js";
 
 const lottoTickets = new LottoGenerator();
 
@@ -8,9 +9,18 @@ class App {
     Console.print(
       "당첨 번호 6개를 입력해 주세요. 번호는 쉼표(,)를 기준으로 구분합니다"
     );
-    const winningNumbers = await Console.readLineAsync("");
+    const rawWinningNumbers = await Console.readLineAsync("");
+    const winningNumbers = rawWinningNumbers
+      .split(",")
+      .map((number) => parseInt(number.trim(), 10));
 
-    return winningNumbers.split(",");
+    this.validateWinningNumbers(winningNumbers);
+
+    return winningNumbers;
+  }
+
+  validateWinningNumbers(winningNumbers) {
+    new Lotto(winningNumbers); // 생성 과정에서 유효성 검사가 수행됨
   }
 
   async getBonusNumbersFromUserInput() {
@@ -22,10 +32,13 @@ class App {
   }
 
   async play() {
-    await lottoTickets.getLottoTickets();
-
-    await this.getWinningNumbersFromUserInput();
-    await this.getBonusNumbersFromUserInput();
+    try {
+      await lottoTickets.getLottoTickets();
+      await this.getWinningNumbersFromUserInput();
+      await this.getBonusNumbersFromUserInput();
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 }
 
--- src/utils/numberArrayUtils.js
+++ src/utils/numberArrayUtils.js
@@ -1,4 +1,13 @@
 /**
+ * 제공된 배열이 숫자만 포함하는지 확인합니다.
+ *
+ * @param {number[]} arr - 확인할 숫자 배열입니다.
+ * @returns {boolean} - 배열이 숫자만 포함하면 true를 반환하고, 그렇지 않으면 false를 반환합니다.
+ */
+export const isArrayAllIntegers = (arr) =>
+  arr.every((number) => Number.isInteger(number));
+
+/**
  * 제공된 배열이 유일한 숫자만 포함하는지 확인합니다.
  *
  * @param {number[]} arr - 확인할 숫자 배열입니다.
