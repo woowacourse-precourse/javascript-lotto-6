bc189049e3b42a50d055e09153a063333e4b34c1
chysis <chysiss@naver.com>
2023-11-04 22:32:00 +0900

    feat(LottoGame): 로또 구입 금액 입력 받기 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,12 +1,27 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import Messages from './Messages.js';
 
 class LottoGame {
   #purchaseAmount;
 
+  getPurchaseAmount = async () => {
+    const input_amount = await MissionUtils.Console.readLineAsync(
+      Messages.PURCHASE_AMOUNT_INPUT
+    );
+    if (isNaN(input_amount)) {
+      throw new Error(Messages.PURCHASE_AMOUNT_ISNAN);
+    }
+    if (input_amount % 1000 !== 0) {
+      throw new Error(Messages.PURCHASE_AMOUNT_NOT_DIVIDED);
+    }
+    this.#purchaseAmount = parseInt(input_amount) / 1000;
+  };
+
   playGame = () => {
     try {
+      this.getPurchaseAmount();
     } catch (error) {
-      MissionUtils.Console.print(error);
+      throw new Error(error);
     }
   };
 }
