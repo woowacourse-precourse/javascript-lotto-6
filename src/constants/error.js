4a2b5ccfe4338b25290fe197a57632ecbd2281fc
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-11-04 21:59:09 +0900

    feat: Player class validation 구현

--- src/constants/error.js
+++ src/constants/error.js
@@ -6,5 +6,6 @@ export const LOTTO_ERROR_MSG = {
   NOT_DIGIT_ERR: "숫자가 아닌 문자가 있습니다.",
   RANGE_ERR: `로또 번호는 ${LOTTO_CONSTANT.RANGE.MIN}부터 
   ${LOTTO_CONSTANT.RANGE.MAX} 사이의 숫자여야 합니다.`,
+  DIVIDE_ERR: "숫자가 잘못된 형식입니다.",
 };
 Object.freeze(LOTTO_ERROR_MSG);
---
+++ src/model/Player.js
@@ -0,0 +1,43 @@
+import LottoGameError from "../Error";
+import { LOTTO_ERROR_MSG } from "../constants/error";
+import { MONEY_CONSTANT } from "../constants/game";
+
+class Player {
+  #seedMoney;
+  #reward;
+  #lottos;
+
+  constructor(seedMoney) {
+    this.#validate(seedMoney);
+    this.#seedMoney = seedMoney;
+    this.#reward = 0;
+    this.#lottos = [];
+  }
+
+  #validate(seedMoney) {
+    this.#validateDigit(seedMoney);
+    if (seedMoney % MONEY_CONSTANT.LOTTO_PRICE !== 0) {
+      throw new LottoGameError(LOTTO_ERROR_MSG.DIVIDE_ERR);
+    }
+  }
+
+  #validateDigit(number) {
+    const DIGIT_CHECK = /^[0-9]+$/;
+
+    const isDigit = DIGIT_CHECK.test(number);
+    if (!isDigit) {
+      throw new LottoGameError(LOTTO_ERROR_MSG.NOT_DIGIT_ERR);
+    }
+  }
+
+  #getBuyableLottos() {}
+  #buyLottos() {}
+
+  get playerReward() {
+    return this.#reward;
+  }
+
+  set playerReward(reward) {
+    this.#reward = reward;
+  }
+}
