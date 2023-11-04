dc571476043b884162a2d6742b2ec11c2d4cb9c9
6puritans9 <6puritans9@gmail.com>
2023-11-04 18:15:33 +0900

    feat(Lotto): You can see lotto numbers

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import Check from "./Util/Check.js";
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,15 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size < numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 포함될 수 없습니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
+  show() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
