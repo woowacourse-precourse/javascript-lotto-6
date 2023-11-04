3699661f506e1bfff479631472f4aa5167be8f27
노태균 <taegyunn123@gmail.com>
2023-11-04 21:23:26 +0900

    refactor: Error class에 주석 추가

--- src/error/IncorrectFormatError.js
+++ src/error/IncorrectFormatError.js
@@ -1,6 +1,7 @@
 import { ErrorMessage } from "../utils/message.js";
 import BasicError from "./BasicError.js";
 
+// 숫자가 잘못된 형식일 경우
 class IncorrectFormatError extends BasicError {
   constructor() {
     super(ErrorMessage.incorrectFormat());
