df6763bb15b9ccacf044c33c531814edd667b2d0
Jwirang <haha71119@gamil.com>
2023-11-04 15:13:45 +0900

    feat: 발행한 로또 수량만큼 1~45까지 중복 없는 숫자 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import { Console, MissionUtils } from '@woowacourse/mission-utils';
 class App {
   async play() {
-    await this.getAmount();
+    const amount = await this.getAmount();
+    this.getLottoNumbers(amount);
   }
 
   async getAmount() {
-     const amount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
-     this.validateAmount(amount);
+    const amount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    this.validateAmount(amount);
+
+    return amount;
   }
 
   validateAmount(amount) {
     if (isNaN(amount) || amount < 1000 || amount % 1000 !== 0) {
       throw new Error('[ERROR] 입력 값이 잘못 되었습니다.');
     }
-    return amount;
+  }
+
+  getLottoNumbers(count) {
+    const playCount = count / 1000;
+
+    for(let i = 0; i < playCount; i++) {
+      this.printRandomLottoNumbers();
+    }
+  }
+
+  printRandomLottoNumbers() {
+    const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    Console.print(randomNumbers);
   }
 }
 
