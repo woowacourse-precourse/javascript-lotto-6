2a034db0ceeb6db0edbc066640aacddc0703ac0d
Rki0 <pky00823@gmail.com>
2023-11-04 16:49:52 +0900

    fix: 보너스 번호 검증 타입 에러 수정
    보너스 번호의 검증이 진행될 때, 보너스 번호는 string 타입인데 number로 가정하고 진행했다.
    따라서, number 타입으로 변환 후 검증을 진행하도록 한다.

--- src/Bonus.js
+++ src/Bonus.js
@@ -3,16 +3,17 @@ import Validate from "./Validate";
 class Bonus {
   constructor(bonus, userLotto) {
     this.#validate(bonus, userLotto);
-    this.bonus = bonus;
+    this.bonus = parseInt(bonus, 10);
   }
 
   #validate(bonus, userLotto) {
+    const parsedBonus = parseInt(bonus, 10);
     const validate = new Validate();
-    validate.isValidBonusNumber(bonus, userLotto);
+    validate.isValidBonusNumber(parsedBonus, userLotto);
   }
 
   getBonus() {
-    return parseInt(this.bonus, 10);
+    return this.bonus;
   }
 }
 
