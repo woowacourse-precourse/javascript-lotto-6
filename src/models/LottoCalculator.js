bf0c35362ef620bc1a16b24e8a69a3c42e504162
kimdw524 <kimdw524@naver.com>
2023-11-04 18:35:07 +0900

    feat: 당첨 번호와 보너스 번호 설정하기

--- src/models/LottoCalculator.js
+++ src/models/LottoCalculator.js
@@ -1,5 +1,26 @@
+import Validation from '../validations/Lotto';
+
 class LottoCalculator {
+  /** @type {Lotto[]} */
   #lottoes;
+
+  /** @type {number[]} */
+  #numbers;
+
+  /** @type {number} */
+  #bonus;
+
+  /**
+   * 당첨 번호와 보너스 번호 정하기
+   * @param {number[]} numbers
+   * @param {number} bonus
+   */
+  setWinningNumber(numbers, bonus) {
+    Validation.validateLottoNumbersWithBonus(numbers, bonus);
+
+    this.#numbers = numbers;
+    this.#bonus = bonus;
+  }
 
   /**
    * 계산할 로또 클래스 입력
