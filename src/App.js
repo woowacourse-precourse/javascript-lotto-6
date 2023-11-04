674ecd3c11b16a3842ae3a2d6dad5239efc67e3e
yjh1008 <exesea12@gmail.com>
2023-11-04 11:51:47 +0900

    feat (APP.js) : 기능 사항 추가.
    1, 게임 시작 문구 생성.
    2. 로또 구매 금액과 갯수를 받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import { BUY_LOTTO, LOTTO_COUNT } from "./GameComment.";
 class App {
-  async play() {}
+
+  validPrice(price) {
+     if(isNaN(price)) throw new Error("[ERROR]");
+    if(price%1000) throw new Error("[ERROR]");
+
+    return price/1000;
+  }
+
+  async play() {
+    const price = await Console.readLineAsync(BUY_LOTTO);
+    console.log(price);
+    const count = this.validPrice(price);
+    
+    Console.print(LOTTO_COUNT(count));
+  }
 }
 
 export default App;
--- src/GameComment..js
+++ src/GameComment..js
@@ -2,3 +2,7 @@ const BUY_LOTTO = "구맥 금액을 입력해주세요.";
 
 const LOTTO_COUNT = (count) => `${count}개를 구매했습니다.`;
 
+export {
+  BUY_LOTTO, 
+  LOTTO_COUNT,
+}
