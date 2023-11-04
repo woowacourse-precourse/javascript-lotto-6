f376e8ac05138ce5013b6273266471294612ed87
lbj23han <ljhan0215@gmail.com>
2023-11-04 20:12:28 +0900

    feat: 번호 비교, 수익률 계산 기능

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Lotto from "./Lotto.js";
+import LottoCalculator from "./LottoResults.js";
 import UserInput from "./UserInput.js";
 
 class App {
   async play() {
     const lottoAmount = await UserInput.inputAmount();
-    const bonusNumber = await UserInput.inputBonus();
-    const selectedNumbers = await UserInput.inputNumber();
 
     const lottoNumber = Lotto.generateNumbers();
     const numberOfSets = lottoAmount / 1000;
     const lottoNumbers = Lotto.generateNumbers(numberOfSets);
 
-    lottoNumbers.forEach((numbers) => console.log(`[${numbers}]`));
+    lottoNumbers.forEach((numbers) => {
+      console.log(`[${numbers}]`);
+    });
+
+    const winningNumbers = await UserInput.inputNumber();
+    const bonusNumber = await UserInput.inputBonus();
   }
 }
 
--- src/Constant.js
+++ src/Constant.js
@@ -3,3 +3,12 @@ export const MESSAGE = {
   LOTTO_NUMBER: `당첨 번호를 입력해 주세요.`,
   BONUS_NUMBER: `보너스 번호를 입력해 주세요.`,
 };
+
+export const STATISTICS = {
+  3: 0,
+  4: 0,
+  5: 0,
+  "5+bonus": 0,
+  6: 0,
+  totalPrize: 0,
+};
