346f85d51fe07a2c85e76e0a1fd762f3ca972dfc
yebin <yebind3308@gmail.com>
2023-11-04 18:17:22 +0900

    feat: 구입금액 입력과 에러검사 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() { }
+  
+  async purchasePrice() {
+    const priceInput = await Console.readLineAsync('구입금액을 입력해 주세요. \n');
+    if (this.priceValidity(priceInput)) {
+      throw new Error("[ERROR] 이름이 잘못된 형식입니다.");
+    };
+    return priceInput;
+  };
+  
+  priceValidity(priceInput) {
+    return priceInput % 1000 !== 0;
+  }
 }
 
 export default App;
