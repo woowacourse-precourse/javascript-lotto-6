d6ff57ceb3a23b2bec006f7a7e1595059b752e9d
thumbthing <ilscc0902@gmail.com>
2023-11-05 02:16:41 +0900

    feat: 당첨번호, 보너스 번호 UI 담당 기능 구현

---
+++ src/util/UI/gameStart/receiveBonusNumberUI.js
@@ -0,0 +1,14 @@
+import { consoleInput, consolePrint } from '../../libararyFeatures/MissionUtilHandler';
+
+export default async function receiveBonusNumberUI() {
+  try {
+    const GUIDANCE_TEXT_BONUS_NUMBER = '보너스 번호를 입력해 주세요. \n';
+    const bonusNumber = await consoleInput(GUIDANCE_TEXT_BONUS_NUMBER);
+    consolePrint('\n');
+
+    return bonusNumber;
+  } catch (error) {
+    const errorMessage = error.message;
+    throw new Error(errorMessage);
+  }
+}
---
+++ src/util/UI/gameStart/receiveWinningLottoUI.js
@@ -0,0 +1,14 @@
+import { consoleInput, consolePrint } from '../../libararyFeatures/MissionUtilHandler';
+
+export default async function receiveWinningLottoUI() {
+  try {
+    const GUIDANCE_TEXT_WINNING_LOTTO = '당첨 번호를 입력해 주세요. \n';
+    const winningLotto = await consoleInput(GUIDANCE_TEXT_WINNING_LOTTO);
+    consolePrint('\n');
+
+    return winningLotto;
+  } catch (error) {
+    const errorMessage = error.message;
+    throw new Error(errorMessage);
+  }
+}
