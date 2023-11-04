ace010aa5a2f2beb519aae21f232058ba15ae3ce
cobocho <rlaalsrb1111@naver.com>
2023-11-04 10:19:13 +0900

    feat: `LottoMachine` 기능 구현

---
+++ src/domain/LottoMachine.js
@@ -0,0 +1,53 @@
+import { Random } from '@woowacourse/mission-utils';
+import Lotto from '../Lotto.js';
+import LottoNumber from './LottoNumber.js';
+import { sortAscending } from '../utils/sort.js';
+import ERROR_MESSAGE_GENERATOR from '../constants/error.js';
+
+class LottoMachine {
+  /**
+   * 로또 하나의 가격입니다.
+   * @type {1_000}
+   */
+  static LOTTO_PRICE = 1_000;
+
+  static ERROR_MESSAGES = Object.freeze({
+    notNumber: ERROR_MESSAGE_GENERATOR.notNumber('구매 금액'),
+    indivisible: `${LottoMachine.LOTTO_PRICE.toLocaleString()}으로 나누어 떨어지는 금액을 입력해주세요!`,
+  });
+
+  /**
+   * @returns {LottoMachine}
+   */
+  static of() {
+    return new LottoMachine();
+  }
+
+  /**
+   * 입력받은 money에 따라 Lotto 배열을 반환합니다.
+   * @param {number} money
+   * @returns {Lotto[]}
+   */
+  buy(money) {
+    const sheets = money / LottoMachine.LOTTO_PRICE;
+    const lottos = Array.from({ length: sheets }, () => this.#publish());
+
+    return lottos;
+  }
+
+  /**
+   * 로또를 하나 발행합니다.
+   * @returns {Lotto}
+   */
+  #publish() {
+    const randomNumber = Random.pickUniqueNumbersInRange(
+      LottoNumber.MIN_NUMBER,
+      LottoNumber.MAX_NUMBER,
+      Lotto.NUMBER_QUANTITY,
+    );
+
+    return Lotto.of(sortAscending(randomNumber));
+  }
+}
+
+export default LottoMachine;
--- src/domain/index.js
+++ src/domain/index.js
@@ -1,2 +1,3 @@
 export { default as LottoNumber } from './LottoNumber.js';
 export { default as Lotto } from '../Lotto.js';
+export { default as LottoMachine } from './LottoMachine.js';
