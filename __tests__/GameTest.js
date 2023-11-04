460c341514af5b9cd0c16af4ae3ea85b12400628
sohyun <kshyun0215@gmail.com>
2023-11-04 15:56:44 +0900

    test: 로또 가격 미만일 때 테스트 추가

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -1,19 +1,28 @@
-import Game from '../src/Game.js';
 import { Random } from '@woowacourse/mission-utils';
+
+import Game from '../src/Game.js';
+import { ERROR_MESSAGE } from '../src/constants/messages.js';
 
 describe('Game 클래스 테스트', () => {
   test('로또 구입 금액이 1,000으로 나누어 떨어지지 않으면 예외가 발생한다.', () => {
     const game = new Game();
     expect(() => {
       game.validate(1001);
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR_MESSAGE.invalidUnit);
   });
 
   test('구입 금액에 숫자가 아닌 값이 입력되면 예외가 발생한다.', () => {
     const game = new Game();
     expect(() => {
       game.validate('a');
-    }).toThrow('[ERROR]');
+    }).toThrow(ERROR_MESSAGE.invalidType);
+  });
+
+  test('구입 금액이 1,000원 미만이면 예외가 발생한다.', () => {
+    const game = new Game();
+    expect(() => {
+      game.validate(0);
+    }).toThrow(ERROR_MESSAGE.invalidAmount);
   });
 
   test('구입 금액에 해당하는 만큼 로또를 발행한다.', () => {
