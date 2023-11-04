bd9dfbe8bcacdf9b15dd8d5c9265de03e87dc338
sohyun <kshyun0215@gmail.com>
2023-11-04 13:54:48 +0900

    test: Game 클래스 테스트 수정

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -2,19 +2,22 @@ import Game from '../src/Game.js';
 
 describe('Game 클래스 테스트', () => {
   test('로또 구입 금액이 1,000으로 나누어 떨어지지 않으면 예외가 발생한다.', () => {
+    const game = new Game();
     expect(() => {
-      new Game(1004);
+      game.validate(1001);
     }).toThrow('[ERROR]');
   });
 
-  test('숫자가 아닌 값이 입력되면 예외가 발생한다.', () => {
+  test('구입 금액에 숫자가 아닌 값이 입력되면 예외가 발생한다.', () => {
+    const game = new Game();
     expect(() => {
-      new Game('a');
+      game.validate('a');
     }).toThrow('[ERROR]');
   });
 
   test('구입 금액에 해당하는 만큼 로또를 발행한다.', () => {
-    const game = new Game(2000);
+    const game = new Game();
+    game.purchaseLotto(2000);
     const lottos = game.getLottos();
 
     expect(lottos.length).toBe(2);
