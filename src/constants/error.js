8758a45b3adbeb77c4c2180dd70dcf7b6981c41b
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-11-05 01:21:28 +0900

    refactor: InputView 및 Error field 수정 및 메서드 추가

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,17 +1,13 @@
 const message = Object.freeze({
   emptyString: '사용자의 입력이 없습니다.',
+  notPositiveInteger: '양의 정수만 입력할 수 있습니다.',
 });
 
 const name = Object.freeze({
   inputView: 'inputViewError',
 });
 
-const errorCase = Object.freeze({
-  emptyString: '',
-});
-
 const ERROR = Object.freeze({
-  errorCase,
   message,
   name,
 });
