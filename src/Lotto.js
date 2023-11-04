ef1a3b253ee9e463b253430626a1e203441c7f16
김민승 <albert050509@naver.com>
2023-11-05 03:37:00 +0900

    feat(lottogame) : feat Lotto Class(object) file

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE } from "./constants/errorMessage.js";
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,29 @@ class Lotto {
   }
 
   #validate(numbers) {
+    const numberSet = [...new Set(numbers)];
+
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR_MESSAGE.lottoIsExceedCount);
     }
+
+    if (numberSet.length !== numbers.length) {
+      throw new Error(ERROR_MESSAGE.lottoIsDuplicated);
+    }
+
+    numbers.forEach((number) => {
+      if (number > 45 || number < 1) {
+        throw new Error(ERROR_MESSAGE.lottoIsExceedNumber);
+      }
+    });
   }
 
   // TODO: 추가 기능 구현
+
+  getLotto() {
+    this.#numbers = this.#numbers.map((number) => parseInt(number));
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
