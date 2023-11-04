57ac19c0891814a6284a4a7b249a89b8b4055bbb
rustandbone <dlehdgh14@naver.com>
2023-11-04 13:52:08 +0900

    기능 구현: 클래스 메서드 이동

--- src/GuessLotto.js
+++ src/GuessLotto.js
@@ -1,13 +1,19 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class GuessLotto {
   #guessNumbers;
 
   #guessBonus;
 
+  #lottoPieces;
+
+  #lottoNumbers;
+
   constructor() {
     this.#guessNumbers = [];
     this.#guessBonus = null;
+    this.#lottoPieces = 0;
+    this.#lottoNumbers = [];
   }
 
   async inputLottoNumber() {
@@ -22,6 +28,27 @@ class GuessLotto {
     this.#guessBonus =
       await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
     return this.#guessBonus;
+  }
+
+  async buyLotto() {
+    const lottoPurchaseMoney =
+      await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+
+    if (Number.isNaN(+lottoPurchaseMoney))
+      throw new Error('[ERROR] 구입 금액이 잘못되었습니다.');
+
+    if (lottoPurchaseMoney % 1000 !== 0)
+      throw new Error('[ERROR] 구입 금액이 잘못되었습니다.');
+
+    this.#lottoPieces = lottoPurchaseMoney / 1000;
+    return this.#lottoPieces;
+  }
+
+  generateLottoNumber() {
+    let lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
+    lottoNumber = lottoNumber.sort((a, b) => a - b);
+    this.#lottoNumbers.push(lottoNumber);
+    return lottoNumber;
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -13,25 +13,6 @@ class Lotto {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
-
-  async buyLotto() {
-    const lottoPurchaseMoney =
-      await Console.readLineAsync('구입금액을 입력해 주세요.');
-
-    if (Number.isNaN(+lottoPurchaseMoney))
-      throw new Error('[ERROR] 구입 금액이 잘못되었습니다.');
-
-    if (lottoPurchaseMoney % 1000 !== 0)
-      throw new Error('[ERROR] 구입 금액이 잘못되었습니다.');
-
-    const lottoPieces = lottoPurchaseMoney / 1000;
-    return lottoPieces;
-  }
-
-  createLottoNumber() {
-    const lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
-    return lottoNumber.sort((a, b) => a - b);
-  }
 }
 
 export default Lotto;
