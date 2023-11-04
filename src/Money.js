9f695b04d9881c6a531483b5cf472490cfbebafd
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-11-04 15:46:11 +0800

    feat: 로또 구입 금액에 대한 예외 처리

---
+++ src/Money.js
@@ -0,0 +1,42 @@
+import { ERROR } from "./constants/messages";
+import { Console } from "@woowacourse/mission-utils";
+
+class Money {
+    #money;
+
+    constructor(money) {
+        this.#money = Number(money);
+        this.#validate();
+    }
+
+    #validate() {
+        this.checkNumber();
+        this.checkUnit();
+    }
+
+    checkNumber() {
+        try {
+            if (isNaN(this.#money)) throw new Error(ERROR.MONEY_NUMBER);
+        }
+        catch (error) {
+            Console.print(error.message);
+        } 
+        finally {
+            //숫자를 재입력할 Console.readLine()함수 
+        }
+    }
+
+    checkUnit() {
+        try {
+            if (this.#money % 1000 !== 0) throw new Error(ERROR.MONEY_UNIT);
+        }
+        catch (error) {
+            Console.print(error.message);
+        } 
+        finally {
+            //숫자를 재입력할 Console.readLine()함수 
+        }
+    }
+}
+
+export default Money;
