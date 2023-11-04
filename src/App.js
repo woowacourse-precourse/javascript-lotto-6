71d21cfdd1009fb089d6789c971f4309a20e802c
jadewisemann <jadewisemann@gmail.com>
2023-11-05 05:58:46 +0900

    FEAT: Add 'Get Purchasing Price'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import { Console } from "@woowacourse/mission-utils"
-import message from "./Message.js";
+import { inputMessage, errorMessage } from "./Message.js";
 import Lotto from './Lotto.js';   
 
 
 class App {
   async play() {
-    const test = await getUserInput(message.inputPurchasingPrice)
+    const userInput = new UserInput()
+    userInput.getPurchasingPrice()
   }
-
-
 }
 
-async function getUserInput(message) {
-  return  await Console.readLineAsync(message);
+class UserInput {
+  async getPurchasingPrice() {
+    await this.getUserInput(inputMessage.purchasingPrice)
+    this.validatePurchasingPrice()
+  }
+
+  async getUserInput(message) {
+    this.input = await Console.readLineAsync(message);
+  }
+
+  validatePurchasingPrice() {
+    this.checkIsMultipleOfThousand()
+  }
+
+  checkIsMultipleOfThousand() {
+    if (this.input % 1000 !== 0) {
+      throw new Error (errorMessage.purchasingPrice);
+    }
+  }
+
 }
 
 
--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,10 @@
-const message = {
-  inputPurchasingPrice: '구입금액을 입력해 주세요.',  
+const inputMessage = {
+  purchasingPrice: '구입금액을 입력해 주세요.',  
 };
 
-export default message;
+const errorMessage = {
+  purchasingPrice: '[ERROR] 구입 금액이 1,000으로 나누어 떨어지지 않습니다.',
+
+}
+
+export { inputMessage, errorMessage };
