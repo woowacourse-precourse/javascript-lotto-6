0abd2a1d6e041a020148aa0366c82e4547cf26af
ohgus <anakin1108@naver.com>
2023-11-04 16:45:21 +0900

    feat: 출력 메시지 상수로 만들기

---
+++ src/constants/messages.js
@@ -0,0 +1,12 @@
+export const MESSAGES = {
+  HOW_MUCH: "구입금액을 입력해 주세요.",
+  YOU_BOUGHT: "개를 구매했습니다.",
+  INPUT_WINNING_NUMBERS: "당첨 번호를 입력해 주세요.",
+  INPUT_BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+  WINNING_STASTISTICS: `당첨 통계${"\n"}---`,
+  THREE_COINCIDE: "3개 일치 (5,000원) - ",
+  FOUR_COINCIDE: "4개 일치 (50,000원) - ",
+  FIVE_COINCIDE: "5개 일치 (1,5000,000원) - ",
+  FIVE_AND_BONUS_COINCIDE: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  ALL_COINCIDE: "6개 일치 (2,000,000,000원) - ",
+};
