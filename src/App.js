ec9d47047ddcb1e214332fd707c5851f2a509970
soyoung125 <sypark041754@gmail.com>
2023-11-04 23:32:17 +0900

    feat(App): 로또 구입 금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "./utils/CONSTANT.js";
+
 class App {
-  async play() {}
+  #purchaseAmount;
+
+  async inputPurchaseAmount() {
+    const input = await MissionUtils.Console.readLineAsync(
+      INPUT_MESSAGE.purchaseAmount,
+    );
+    this.purchaseAmount = input;
+  }
+
+  async play() {
+    this.inputPurchaseAmount();
+  }
 }
 
 export default App;
