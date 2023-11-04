0937b4f684cccaa859445365d80cd7f450ae2af8
Jeongeun Lee <leeju1009@gmail.com>
2023-11-05 01:12:11 +0900

    feat: 보너스 번호의 유효성을 검사하는 함수 생성

--- src/Validation.js
+++ src/Validation.js
@@ -8,6 +8,12 @@ class Validation {
     this.isNonDuplicatedNumber(numbers);
   }
 
+  isValidBonusNumber(bonusNumber, winningNumbers) {
+    this.isNumber(bonusNumber);
+    this.isNumberInRange(bonusNumber);
+    this.isNonDuplicatedBonusNumber(bonusNumber, winningNumbers);
+  }
+
   isNumber(number) {
     if (Number.isNaN(number)) {
       throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
@@ -18,6 +24,14 @@ class Validation {
     const nonDuplicatedNumbers = new Set(numbers);
     if (nonDuplicatedNumbers.size !== numbers.length) {
       throw new Error('[ERROR] 중복된 숫자는 입력할 수 없습니다.');
+    }
+  }
+
+  isNonDuplicatedBonusNumber(bonusNumber, winningNumbers) {
+    if (winningNumbers.includes(bonusNumber)) {
+      throw new Error(
+        '[ERROR] 당첨 번호와 중복되는 숫자는 입력할 수 없습니다.',
+      );
     }
   }
 
