931082ccdae59b553a56f49d17272d72d33e3367
ss0526100 <ss0526100@gmail.com>
2023-11-04 21:57:53 +0900

    feat: calculate return ration

--- src/modules/Get.js
+++ src/modules/Get.js
@@ -7,9 +7,15 @@ const {
   MISS_STATE,
   BONUS_STATE,
   HIT_STATE,
+  LOTTO_PRICE,
   NUMBER_OF_LOTTO_NUMBERS,
   LOTTO_NUMBER_LOWER,
   LOTTO_NUMBER_UPPER,
+  FIRST_PLACE_WINNINGS,
+  SECOND_PLACE_WINNINGS,
+  THIRD_PLACE_WINNINGS,
+  FOURTH_PLACE_WINNINGS,
+  FIFTH_PLACE_WINNINGS,
 } = CONSTANTS;
 
 class Get {
@@ -38,6 +44,18 @@ class Get {
     return lotto
       .getNumbers()
       .reduce((previous, current) => previous + lottoBoard[current], 0);
+  }
+
+  static lottoReturnRatio(resultArray, numberOfLotto) {
+    return (
+      (((resultArray[6] + resultArray[7]) * FIFTH_PLACE_WINNINGS +
+        (resultArray[8] + resultArray[9]) * FOURTH_PLACE_WINNINGS +
+        resultArray[10] * THIRD_PLACE_WINNINGS +
+        resultArray[11] * SECOND_PLACE_WINNINGS +
+        resultArray[12] * FIRST_PLACE_WINNINGS) /
+        (numberOfLotto * LOTTO_PRICE)) *
+      100
+    );
   }
 }
 
