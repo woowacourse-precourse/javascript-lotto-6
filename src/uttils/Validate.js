5348b3176a8f2ce2e1ada399d072cd43125d3860
badahertz52 <badahertz52@gmail.com>
2023-11-04 18:19:13 +0900

    refactor<Validate> : validateNumberRange 수정
    - validateNumberRange 수정

--- src/uttils/Validate.js
+++ src/uttils/Validate.js
@@ -1,12 +1,12 @@
-import { ERROR_MESSAGE, LOTTO_FORM } from '../constant';
+import { ERROR_MESSAGE, NUMBER_RANGE } from '../constant';
 import { getErrorMessage, throwError } from './MessageFactory';
 /**
  * 번호가 1~45까지의 범위 내의 숫자인지 확인
  * @param {*} number
  */
 const validateNumberRange = (number) => {
-  const { range } = LOTTO_FORM;
-  if (number < range.min || number > range.max) throwError(ERROR_MESSAGE.range);
+  const { min, max } = NUMBER_RANGE;
+  if (number < min || number > max) throwError(ERROR_MESSAGE.range);
 };
 
 //유지
