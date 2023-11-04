336648b7ec5f0d6e3fac317127b889fd6a4a4148
hongst222 <tmdxordusdo@gmail.com>
2023-11-04 18:50:52 +0900

    feat: 로또 룰 관련 상수 추가
    dir: constants
    file: lottoRules.js

---
+++ constants/lottoRules.js
@@ -0,0 +1,25 @@
+const RULE = [
+    {
+        count: 3,
+        money: "5,000"
+    },
+    {
+        count: 4,
+        money:"50,000"
+    },
+    {
+        count: 5,
+        money: "1,500,000"
+    },
+    {
+        count: 5,
+        money:"30,000,000"
+    },
+    {
+        count:6,
+        money: "2,000,000,000",
+    }
+
+]
+
+export { RULE };
