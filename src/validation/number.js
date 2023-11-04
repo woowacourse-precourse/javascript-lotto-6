c3319ca8d92e98d77a82b3b0a206be39be3c13f1
Goongam <dlwjddn341@naver.com>
2023-11-04 22:25:11 +0900

    Fix: 에러 메시지 상수화

--- src/validation/number.js
+++ src/validation/number.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE } from "../constants/error";
+
 export const getValidatedNumbers = (numbers) => {
   validateNumbers(numbers);
   return numbers.map((number) => {
@@ -15,13 +17,13 @@ export const getValidatedNumber = (number) => {
 };
 
 const validateNumber = (N) => {
-  if (isNaN(N)) throw new Error("숫자가 아님");
-  if (N < 1 || N > 45) throw new Error("범위 밖임");
-  if (Math.floor(N) !== N) throw new Error("정수가 아님");
+  if (isNaN(N)) throw new Error(ERROR_MESSAGE.MONEY_ISNAN);
+  if (N < 1 || N > 45) throw new Error(ERROR_MESSAGE.LOTTO_RANGE);
+  if (Math.floor(N) !== N) throw new Error(ERROR_MESSAGE.DECIMAL);
 };
 
 const validateNumbers = (numbers) => {
-  if (numbers.length !== 6) throw new Error("6개여야함");
+  if (numbers.length !== 6) throw new Error(ERROR_MESSAGE.NUMBER6);
   if (new Set(numbers).size !== numbers.length)
-    throw new Error("중복된 숫자가 있음");
+    throw new Error(ERROR_MESSAGE.OVERLAP);
 };
