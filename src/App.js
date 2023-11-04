4a99122b6c095336eb9194de9bd206771fae3306
jadewisemann <jadewisemann@gmail.com>
2023-11-05 06:21:09 +0900

    FEAT: Add 'Get Lotto Number'

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Lotto from './Lotto.js';
 
 class App {
   async play() {
-    const userInput = new UserInput()
-    userInput.getPurchasingPrice()
+    const userInputNumber = await new UserInput().getPurchasingPrice();
+    
+    const userInputLotto = await new UserInput().getLottoNumber();
+    console.log('userInputLotto', userInputLotto)
+    
   }
 }
 
@@ -14,6 +17,13 @@ class UserInput {
   async getPurchasingPrice() {
     await this.getUserInput(inputMessage.purchasingPrice)
     this.validatePurchasingPrice()
+    return this
+  }
+
+  async getLottoNumber() {
+    await this.getUserInput(inputMessage.lottoNumber)
+    this.parseLottoNumber()
+    return this
   }
 
   async getUserInput(message) {
@@ -29,6 +39,20 @@ class UserInput {
       throw new Error (errorMessage.purchasingPrice);
     }
   }
+
+  parseLottoNumber() {
+    const parsedArray = this.input.split(',');
+    parsedArray.map(number => this.checkIsNumberInRange(number))
+    this.parsedArray = parsedArray;
+  }
+
+  checkIsNumberInRange(number) {
+    if (!(0 < number <= 45)) {
+      throw new Error (errorMessage.numberOutOfRange)
+    } 
+  }
+  
+  
 
 }
 
--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,11 @@
 const inputMessage = {
   purchasingPrice: '구입금액을 입력해 주세요.',  
+  lottoNumber: '당첨 번호를 입력해 주세요.',
 };
 
 const errorMessage = {
   purchasingPrice: '[ERROR] 구입 금액이 1,000으로 나누어 떨어지지 않습니다.',
+  numberOutOfRange: '[ERROR] 로또 숫자가 잘못된 형식입니다. 1~45 사이의 자연수 입니다.'
 
 }
 
