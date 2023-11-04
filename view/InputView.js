050f0b2cd7852957d6c3f41f54fe6bb97ae989ba
Jules <dali1999@naver.com>
2023-11-05 01:17:02 +0900

    feat(InputView): 예외 발생 시 재입력 받는 기능 추가

--- view/InputView.js
+++ view/InputView.js
@@ -1,33 +1,62 @@
 import { Console } from '@woowacourse/mission-utils';
 import PromptMessage from '../constants/PromptMessage.js';
+import Validator from '../utils/Validator.js';
 
 class InputView {
   constructor() {
     this.price = 0;
-    this.winNum = 0;
+    this.count = 0;
+    this.winNum = [];
     this.bonusNum = 0;
   }
 
-  // 구입 금액 입력
   async getPrice() {
-    const price = await Console.readLineAsync(PromptMessage.ENTER_PRICE);
-    this.price = price;
-    Console.print(this.price);
+    let validInput = false;
+    while (!validInput) {
+      this.price = await Console.readLineAsync(PromptMessage.ENTER_PRICE);
+      try {
+        Validator.validatePrice(this.price);
+        validInput = true;
+      } catch (error) {
+        Console.print('[ERROR] 올바른 금액을 입력하십시오\n');
+      }
+    }
   }
 
-  // 당첨 번호 입력
   async getWinNum() {
-    const winNum = await Console.readLineAsync(PromptMessage.ENTER_WIN_NUM);
-    this.winNum = winNum.split(',').map((e) => parseInt(e.trim(), 10));
-    Console.print(this.winNum);
+    let validInput = false;
+    while (!validInput) {
+      const winNum = await Console.readLineAsync(PromptMessage.ENTER_WIN_NUM);
+      this.winNum = winNum.split(',').map((e) => parseInt(e.trim(), 10));
+      try {
+        Validator.validateWinNum(this.winNum);
+        validInput = true;
+      } catch (error) {
+        Console.print('[ERROR] 올바른 당첨 번호를 입력하십시오\n');
+      }
+    }
   }
 
-  // 보너스 번호 입력
   async getBonusNum() {
     const bonusNum = await Console.readLineAsync(PromptMessage.ENTER_BONUS_NUM);
     this.bonusNum = parseInt(bonusNum, 10);
-    Console.print(this.bonusNum);
   }
+
+  //   async getBonusNum() {
+  //     let validInput = false;
+  //     while (!validInput) {
+  //       const bonusNum = await Console.readLineAsync(
+  //         PromptMessage.ENTER_BONUS_NUM,
+  //       );
+  //       this.bonusNum = parseInt(bonusNum, 10);
+  //       try {
+  //         Validator.validateBonusNum(this.bonusNum);
+  //         validInput = true;
+  //       } catch (error) {
+  //         Console.print('[ERROR] 올바른 당첨 번호를 입력하십시오\n');
+  //       }
+  //     }
+  //   }
 }
 
 export default InputView;
