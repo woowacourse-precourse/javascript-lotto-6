d6ff88cb9c449c6ec0c1d2aa9808c6aec03cc47f
고세종 <fulfilled7418@gmail.com>
2023-11-05 01:46:00 +0900

    test: 추가 예외사항

--- __tests__/module_test/ValidationTest.js
+++ __tests__/module_test/ValidationTest.js
@@ -1,29 +1,30 @@
-import Validation from '../../Controller/Validation';
+import Validator from '../../Controller/modules/Validator.js';
 
 describe('사용자 입력 유효성 검사', () => {
   const insertedMoneyTestInput = ['1500', '1000k', 'abcd', '', ' '];
   const winNumberInput = [
-    '1,2,3,4,5,a',
-    ' ,1,3,5,6,7',
-    '1,2,3,4,5',
-    '1,2,3,4,5,0',
+    [[1, 2, 3, 4, 5, 'a']],
+    [[' ', 1, 3, 5, 6, 7]],
+    [[1, 2, 3, 4, 5]],
+    [[1, 2, 3, 4, 5, 0]],
+    [[1, 1, 2, 3, 4, 5]],
   ];
-  const bonusNumberInput = ['a', '', ' ', '72', '0'];
+  const bonusNumberInput = ['a', '', ' ', 72, 0];
 
   test.each(insertedMoneyTestInput)(
     '입력된 구입 금액 유효성 테스트.',
     (input) => {
-      expect(() => Validation.checkInsertedMoneyIsValid(input)).toThrow(
+      expect(() => Validator.checkInsertedMoneyIsValid(input)).toThrow(
         '[ERROR]'
       );
     }
   );
 
   test.each(winNumberInput)('입력된 당첨 번호 유효성 테스트.', (input) => {
-    expect(() => Validation.checkWinNumberIsValid(input)).toThrow('[ERROR]');
+    expect(() => Validator.checkWinNumberIsValid(input)).toThrow('[ERROR]');
   });
 
   test.each(bonusNumberInput)('입력된 보너스 번호 유효성 테스트.', (input) => {
-    expect(() => Validation.checkBonusNumberIsValid(input)).toThrow('[ERROR]');
+    expect(() => Validator.checkBonusNumberIsValid(input)).toThrow('[ERROR]');
   });
 });
