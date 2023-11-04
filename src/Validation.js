a0524031e2f5741222abe32237190ecc66aa665d
Jeongeun Lee <leeju1009@gmail.com>
2023-11-04 22:48:17 +0900

    refactor: 당첨 번호 유효성 검사 함수들의 변수명 변경

--- src/Validation.js
+++ src/Validation.js
@@ -1,11 +1,11 @@
 class Validation {
   isValidWinningNumbers(numbers) {
-    this.isNumbers(numbers);
-    this.isNonDuplicatedNumbers(numbers);
-    this.isNumbersInRange(numbers);
+    this.isNumber(numbers);
+    this.isNonDuplicatedNumber(numbers);
+    this.isNumberInRange(numbers);
   }
 
-  static isNumbers(numbers) {
+  static isNumber(numbers) {
     numbers.forEach((number) => {
       if (Number.isNaN(number)) {
         throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
@@ -13,14 +13,14 @@ class Validation {
     });
   }
 
-  static isNonDuplicatedNumbers(numbers) {
+  static isNonDuplicatedNumber(numbers) {
     const nonDuplicatedNumbers = new Set(numbers);
     if (nonDuplicatedNumbers.size !== numbers.length) {
       throw new Error('[ERROR] 중복된 숫자는 입력할 수 없습니다.');
     }
   }
 
-  static isNumbersInRange(numbers) {
+  static isNumberInRange(numbers) {
     numbers.forEach((number) => {
       if (number < 1 || number > 45) {
         throw new Error('[ERROR] 1이상 45이하의 숫자만 입력할 수 있습니다.');
