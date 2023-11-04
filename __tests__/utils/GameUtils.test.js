2897172c1aa48d0f866d20961e6b57bf67f2603e
김진호 <rlawlsgh1227@gmail.com>
2023-11-04 19:32:03 +0900

    test: 1~45 랜덤한 수를 6개 받는 기능 검증

--- __tests__/utils/GameUtils.test.js
+++ __tests__/utils/GameUtils.test.js
@@ -1,3 +1,4 @@
+import LOTTO_NUMBER from "../../src/constants/LottoNumber.js";
 import GameUtils from "../../src/utils/GameUtils.js";
 
 describe('게임 유틸 테스트', () => {
@@ -11,5 +12,16 @@ describe('게임 유틸 테스트', () => {
 
     // then
     expect(lottoTickets).toEqual(answer);
+  });
+
+  test('1부터 45까지의 랜덤한 수 6개를 생성받습니다.', () => {
+    // given
+    const randomNumberCount = LOTTO_NUMBER.COUNT;
+
+    // when
+    const randomNumber = GameUtils.createRandomUniqueSixNumberFromOneToForthFive();
+
+    // then
+    expect(randomNumber.length).toEqual(randomNumberCount);
   });
 });
