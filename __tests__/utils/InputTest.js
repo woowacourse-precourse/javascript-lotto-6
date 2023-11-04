28047c32a7436d8c8a73f8e5663285fe777c17b3
이호연 <ho991217@kakao.com>
2023-11-05 03:14:35 +0900

    test: readIntegerAsync 에 대한 테스트 케이스 추가

--- __tests__/utils/InputTest.js
+++ __tests__/utils/InputTest.js
@@ -34,4 +34,29 @@ describe('Input', () => {
       await expect(result).rejects.toThrow('[ERROR]');
     });
   });
+
+  describe('readIntegerAsync', () => {
+    test('사용자에게 메시지를 출력한다.', () => {
+      // given
+      const expectedMessage = '메시지';
+      Console.readLineAsync.mockResolvedValue('1');
+
+      // when
+      Input.readIntegerAsync(expectedMessage);
+
+      // then
+      expect(Console.readLineAsync).toHaveBeenCalledWith(expectedMessage);
+    });
+
+    test('입력받은 값이 정수가 아니라면 에러를 던진다.', async () => {
+      // given
+      Console.readLineAsync.mockResolvedValue('1.1');
+
+      // when
+      const result = Input.readIntegerAsync();
+
+      // then
+      await expect(result).rejects.toThrow('[ERROR]');
+    });
+  });
 });
