7547f748e14584173587fba5bd1547ffc849ac40
최원서 <cwonseo@choewonseoui-MacBookAir.local>
2023-11-04 15:50:00 +0900

    feat: 상수 파일 추가
    고정적으로 사용할 에러메세지, 입력 메세지를 하나의 파일에 정리하였다.

---
+++ constant/Messages.js
@@ -0,0 +1,14 @@
+const ERROR_MESSAGE = {
+  BUY_MONEY_1000:
+    '[ERROR] 구입 금액으로 1000원으로 나누어 떨어지는 수를 입력해주세요',
+  RANDOM_NUM_6: '[ERROR] 당첨 번호를 6개 입력해 주세요',
+  RANDOM_NUM_DUPLICATE: '[ERROR] 서로 다른 당첨 번호를 입력해 주세요.',
+  BONUMUS_NUM_RANGE: '[ERROR] 1 ~ 45 사이의 숫자를 입력해 주세요.',
+};
+
+const INPUT_MESSAGE = {
+  INPUT_NUY_MONEY: '구입금액을 입력해 주세요.',
+  INPUT_WINNINGNUM: '당첨 번호를 입력해 주세요.',
+  INPUT_BONUSNUM: '보너스 번호를 입력해 주세요.',
+  RESULT: '당첨 통계\n---',
+};
