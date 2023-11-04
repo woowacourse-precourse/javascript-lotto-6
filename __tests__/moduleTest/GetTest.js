01362abc42f97308ad106a24b7cbbc3b1b781c76
ss0526100 <ss0526100@gmail.com>
2023-11-04 14:19:42 +0900

    test: check lotto result

--- __tests__/moduleTest/GetTest.js
+++ __tests__/moduleTest/GetTest.js
@@ -1,4 +1,13 @@
 import Get from '../../src/modules/Get.js';
+import Lotto from '../../src/Lotto.js';
+
+const mockBoard = (winningNumbers, bonusNumber) => {
+  const board = new Array(LOTTO_UPPER_NUMBER + 1).fill(MISS_STATE);
+  winningNumbers.forEach(number => (board[number] = HIT_STATE));
+  board[bonusNumber] = BONUS_STATE;
+
+  return Object.freeze(board);
+};
 
 describe('Get 테스트', () => {
   test.each([[1], [2], [6], [10]])('lottoArray', input => {
@@ -10,5 +19,34 @@ describe('Get 테스트', () => {
     lottoArray.map(lotto => {
       expect(lotto.getNumbers()).toHaveLength(6);
     });
+  });
+
+  test.each([
+    [
+      [
+        new Lotto([1, 2, 3, 4, 5, 6]),
+        new Lotto([2, 3, 4, 5, 6, 7]),
+        new Lotto([10, 11, 12, 13, 14, 15]),
+        new Lotto([3, 4, 5, 6, 7, 8]),
+        new Lotto([40, 41, 42, 43, 44, 45]),
+      ],
+      mockBoard([1, 2, 3, 4, 5, 6], 7),
+      [2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1],
+    ],
+    [
+      [
+        new Lotto([1, 2, 3, 4, 7, 8]),
+        new Lotto([10, 11, 12, 13, 14, 15]),
+        new Lotto([1, 10, 20, 30, 40, 45]),
+      ],
+      mockBoard([10, 15, 20, 25, 30, 35], 40),
+      Object.freeze([1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]),
+    ],
+  ])('lottoResult()', (lottoArray, lottoBoard, expectedValue) => {
+    //when
+    const lottoResult = Get.lottoResult(lottoArray, lottoBoard);
+
+    //then
+    expect(lottoResult).toEqual(expectedValue);
   });
 });
