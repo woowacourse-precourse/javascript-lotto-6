7e956e00d29c5142a1bd111eb276372aee669132
ohgus <anakin1108@naver.com>
2023-11-05 01:09:47 +0900

    feat: 보너스 번호 입력에 대한 예외처리 기능 추가

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -28,4 +28,17 @@ export class InputValidator {
       }
     });
   }
+
+  bonusNumberValidator(number, winningNumbers) {
+    const bonusNumber = Number(number);
+
+    if (
+      bonusNumber < 1 ||
+      bonusNumber > 45 ||
+      Number.isNaN(bonusNumber) ||
+      winningNumbers.includes(bonusNumber)
+    ) {
+      throw new Error(ERROR_MESSAGES.UNCORRECT_INPUT);
+    }
+  }
 }
