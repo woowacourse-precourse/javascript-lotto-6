d7c74eb9f513941162924c6e5574a49d93125fa2
coolIlive <galilee155@naver.com>
2023-11-04 13:14:45 +0900

    feat: 로또 수량 계산 기능 추가

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -4,25 +4,33 @@ import Validator from "../model/Validator.js";
 import { Console } from "@woowacourse/mission-utils";
 
 export default class LottoController {
+  #lottoAmount;
+
   constructor() {}
 
   async start() {
-    await this.initializeMoney();
+    await this.initializeLottoAmount();
   }
 
-  async initializeMoney() {
+  async initializeLottoAmount() {
     const moneyInput = await InputView.moneyInput();
-    this.validate(moneyInput, Validator.moneyCheck);
-    // Console.print(moneyInput);
+    this.#validate(moneyInput, Validator.moneyCheck);
+    this.#countLotto(moneyInput);
+    // Console.print(this.#lottoAmount);
   }
 
   // 유효성 검사로 가는 함수
-  validate(inputValue, checkingFunction) {
+  #validate(inputValue, checkingFunction) {
     try {
       checkingFunction(inputValue);
     } catch (error) {
       OutputView.printError(error);
-      this.initializeMoney();
+      this.initializeLottoAmount();
     }
+  }
+
+  // 로또 개수 count
+  #countLotto(moneyInput) {
+    this.#lottoAmount = parseInt(Number(moneyInput) / 1000);
   }
 }
