6793bf9b0546cdb36db7036d1bbcc7cd316dcd9b
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-05 01:26:46 +0900

    refactor(모듈화): 주요 로직 단위 모듈화 LottoGameManager.js 파일을 추가하고, LottoGame 관련 주요 로직을 블록 단위로 모듈화

--- src/App.js
+++ src/App.js
@@ -1,60 +1,32 @@
 import OutputManager from "./functions/OutputManager.js";
-import {
-  devideMoneyForLotto,
-  profitRate,
-  sumOfWinning,
-} from "./utils/caculate.js";
-import {
-  devideIntoCommas,
-  stringToNumber,
-  stringsToNumbers,
-} from "./utils/conversion.js";
-import {
-  checkBonusBall,
-  countMatchingBalls,
-  recordRanks,
-} from "./utils/condition.js";
-import ValidateManager from "./functions/validateManager.js";
+import ValidateManager from "./functions/ValidateManager.js";
+import LottoGameManager from "./functions/LottoGameManager.js";
 class App {
   constructor() {
     this.outputManager = new OutputManager();
     this.validateManager = new ValidateManager();
+    this.lottoGameManager = new LottoGameManager();
   }
 
   async play() {
     try {
-      // purchaseLotto
-      const moneyForLotto = await this.validateManager.getMoneyForLotto();
-      const moneyForLottoToNumber = stringToNumber(moneyForLotto);
-      const countsOfLotto = devideMoneyForLotto(moneyForLottoToNumber);
-      const countsOfLottoToNumber = stringToNumber(countsOfLotto);
-      this.outputManager.showPurchaseAmount(countsOfLotto);
+      const [moneyForLotto, purchasedLotto] =
+        await this.lottoGameManager.purchaseLotto();
+      const winningBalls = await this.lottoGameManager.setWinningNumbers();
+      const bonusBall = await this.lottoGameManager.setBonusNumber();
 
-      const purchasedLotto = this.outputManager.showLottoNumbers(
-        countsOfLottoToNumber
+      const [rankCounts, winningPrizeSum] =
+        this.lottoGameManager.checkIsWinning(
+          purchasedLotto,
+          winningBalls,
+          bonusBall
+        );
+
+      this.lottoGameManager.showResult(
+        moneyForLotto,
+        winningPrizeSum,
+        rankCounts
       );
-      this.outputManager.showEmptyLine();
-      // purchaseLotto
-      // setWinningNumber
-      const winningBalls = await this.validateManager.getWinningBalls();
-      const winningBallsArr = devideIntoCommas(winningBalls);
-      const winningBallsToNumberArr = stringsToNumbers(winningBallsArr);
-
-      const bonusBall = await this.validateManager.getBonusBall();
-      const bonusBallToNumber = stringToNumber(bonusBall);
-
-      const countMatchArr = countMatchingBalls(
-        purchasedLotto,
-        winningBallsToNumberArr
-      );
-      const checkBonusArr = checkBonusBall(purchasedLotto, bonusBallToNumber);
-
-      const rankCounts = recordRanks(countMatchArr, checkBonusArr);
-      const winningPrizeSum = sumOfWinning(rankCounts);
-
-      const earnRate = profitRate(moneyForLotto, winningPrizeSum);
-
-      this.outputManager.showStatistics(rankCounts, earnRate);
     } catch (error) {
       this.outputManager.showError(error.message);
     }
