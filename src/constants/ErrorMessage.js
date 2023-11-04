27058e6c604c8847202e9ad4e31c4d0409d904fb
1eecan <busyx2modernsociety@gmail.com>
2023-11-05 03:15:55 +0900

    feat: 출력메세지 상수 설정
    * input 메세지
    * output 메세지
    * error 메세지 초안

---
+++ src/constants/ErrorMessage.js
@@ -0,0 +1,5 @@
+const ERROR_MESSAGE = Object.freeze({
+  ERROR_MESSAGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+});
+
+export default ERROR_MESSAGE;
---
+++ src/constants/OutputMessage.js
@@ -0,0 +1,20 @@
+const OUTPUT_MESSAGE = Object.freeze({
+  PRINT_PURCHASED_NUMBER: '개를 구매했습니다.',
+
+  PRINT_WINNING_LOG: '당첨 통계',
+
+  PRINT_HORIZONTAL_LINE: '---',
+  PRINT_ENTER: '',
+
+  PRINT_FIFTH_PLACE: '3개 일치 (5,000원) - ',
+  PRINT_FORTH_PLACE: '4개 일치 (50,000원) - ',
+  PRINT_THIRD_PLACE: '5개 일치 (1,500,000원) - ',
+  PRINT_SECOND_PLACE: '5개 일치 (30,000,000원) - ',
+  PRINT_FIRST_PLACE: '6개 일치 (2,000,000,000원) - ',
+
+  PRINT_SUFFIX: '개',
+  PRINT_TOTAL_RETURN_PREFIX: '총 수익률은 ',
+  PRINT_TOTAL_RETURN_SUFFIX: '%입니다.',
+});
+
+export default OUTPUT_MESSAGE;
