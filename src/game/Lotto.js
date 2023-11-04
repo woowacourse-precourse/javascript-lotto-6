f10aa584fe5fb1b5dcc6a0e7ce7a746cf225f129
Joseph Park <rkfnahs12e@gmail.com>
2023-11-04 16:17:47 +0900

    feat(src/game) : Lotto.js 파일에 로또 번호를 발행해주는 makeLottos 메서드를 구현

--- src/game/Lotto.js
+++ src/game/Lotto.js
@@ -1,17 +1,25 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
+  constructor(numbers, amount) {
+    // this.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
-  }
+  // #validate(numbers) {
+  //   if (numbers.length !== 6) {
+  //     throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+  //   }
+  // }
 
+  #makeLottos(amount) {
+    const quantity = amount / 1000;
+    const emptyArrays = Array.from({ length: quantity }, () => {
+      return Random.pickUniqueNumbersInRange(1, 45, 6);
+    });
+  }
   // TODO: 추가 기능 구현
 }
 
