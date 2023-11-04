b64143f4bbe45150a0a5016ca6bfa4c546f710bd
thumbthing <ilscc0902@gmail.com>
2023-11-05 02:15:39 +0900

    refactor: 불필요한 중복검사 기능 삭제

--- src/util/purchase/createPurchaseData.js
+++ src/util/purchase/createPurchaseData.js
@@ -1,20 +1,12 @@
 import ERROR_CODE from '../error/errorCode.js';
 import { pickSixNumber } from '../libararyFeatures/MissionUtilHandler.js';
 import acendingSortList from '../parse/acendingSortList.js';
-import { checkLottoDuplicate } from '../validate/checkHasDuplicate.js';
 
 export default async function createPurchaseData(lottoCount, lottos) {
   try {
     const singleLotto = pickSixNumber();
     const sortedLotto = acendingSortList(singleLotto);
     const lottoList = [...lottos];
-    const isFirstTry = lottoList.length === 0;
-
-    const isLottoDuplicated = isFirstTry ? false : checkLottoDuplicate(sortedLotto, lottoList);
-
-    if (isLottoDuplicated) {
-      return createPurchaseData(lottoCount, lottoList);
-    }
 
     lottoList.push(sortedLotto);
     const isNotCompleted = lottoList.length !== lottoCount;
