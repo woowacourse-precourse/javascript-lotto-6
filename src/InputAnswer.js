7f9a71b8bc1caaf73e8be3ce2501aaf004f62209
Seo San <dannysir@naver.com>
2023-11-04 20:12:37 +0900

    fix : 정답 입력 버그 수정 정답 문자열을 배열로 저장하여 return

--- src/InputAnswer.js
+++ src/InputAnswer.js
@@ -1,6 +1,7 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
 
 const INPUT_ANSWER = async () => {
-    return await MissionUtils.Console.readLineAsync("\n당첨 번호를 입력해 주세요.");
+    const ANSWER = await MissionUtils.Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
+    return ANSWER.split(",").map(v => parseInt(v));
 };
 export default INPUT_ANSWER;
