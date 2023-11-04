ecd036146e4e36fd825e8352409f57784803214f
thumbthing <ilscc0902@gmail.com>
2023-11-05 03:04:44 +0900

    rename: 기능에 따른 파일명, 함수명 구체화

--- src/util/validate/checkIsNotInRange.js
+++ src/util/validate/checkIsNotInRange.js
@@ -1,9 +1,13 @@
 import { PURCHASE_ERROR_CODE } from '../error/errorCode.js';
 
-export default function checkIsTooSmall(input) {
+export function checkPurchaseIsNotInRange(input) {
   const inputValue = Number(input);
   const isTooSmall = inputValue < 1000;
   if (isTooSmall) {
     throw new Error(`${PURCHASE_ERROR_CODE.valueIsTooSmall}`);
   }
+}
+
+export function checkLottoIsNotInRange(input) {
+  return input;
 }
