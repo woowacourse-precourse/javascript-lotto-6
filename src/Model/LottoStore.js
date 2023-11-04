24ec5db5a75f5f0f7fae5feea5930c506337fc21
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-05 01:49:16 +0900

    feat: 중복되지 않는 로또 숫자 뽑는 기능 구현

--- src/Model/LottoStore.js
+++ src/Model/LottoStore.js
@@ -1,8 +1,24 @@
+import Lotto from "../Lotto.js";
+import { Random } from "@woowacourse/mission-utils";
+
 class LottoStore {
-  #purchaseQuantity;
+  #lottos = [];
 
   constructor(purchaseQuantity) {
-    this.#purchaseQuantity = purchaseQuantity;
+    const randomLottoNumbers = this.#createRandomLottoNumbers(purchaseQuantity);
+  }
+
+  #createRandomLottoNumbers(purchaseQuantity) {
+    const randomLottoNumbers = Array.from({ length: purchaseQuantity }, () =>
+      this.#pickRandomNumbers()
+    );
+
+    return randomLottoNumbers;
+  }
+
+  #pickRandomNumbers() {
+    const RandomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return RandomNumbers;
   }
 }
 
