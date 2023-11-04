58450c7b4dd2aff47e14d583f82d3c5469205fe9
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-04 18:50:54 +0900

    test: isInteger 학습 테스트 추가

--- __tests__/NumberTest.js
+++ __tests__/NumberTest.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 describe('숫자 테스트', () => {
   test.each([
     { input: 2000000000, expected: '2,000,000,000' },
@@ -6,5 +7,12 @@ describe('숫자 테스트', () => {
   ])('toLocaleString 메서드로 천단위마다 쉼표(,) 삽입', ({ input, expected }) => {
     const KOREA_LANGUAGE_FORMAT = 'ko-KR';
     expect(input.toLocaleString(KOREA_LANGUAGE_FORMAT)).toBe(expected);
+  });
+
+  test('isInteger 메서드로 정수인지 실수인지 확인', () => {
+    const input = 3.2;
+    const result = false;
+
+    expect(Number.isInteger(input)).toEqual(result);
   });
 });
