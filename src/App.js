def6d29a3f162fdb460634e84eea72a8588d142e
MacBook Air <macbookair@MacBookui-MacBookAir.local>
2023-11-04 15:38:56 +0900

    Feat: 랜덤한 수 출력 기능 구현 구매 개수만큼 반복하며 랜덤한 수 6개를 뽑아 오름차순으로 정렬 후 출력

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 //import { MissionUtils } from "@woowacourse/mission-utils";
 import * as MissionUtils from "@woowacourse/mission-utils";
+function getRandom(num,arr){
+  for(let i =0;i<num;i++){
+    let pick = MissionUtils.Random.pickUniqueNumbersInRange(1,45,6);
+    pick.sort((a,b)=>(a-b));
+    arr[i]=pick;
+    MissionUtils.Console.print(pick);
+  }
+}
+
 class App {
   async play() {
     let price = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
     
-    MissionUtils.Console.print("\n" + price/1000 + "개를 구매했습니다.");
+    if(price%1000 !== 0){
+      throw new Error("[ERROR] 구입 금액이 잘못되었습니다.");
+    }
+
+    let times = price/1000;
+    MissionUtils.Console.print("\n" + times + "개를 구매했습니다.");
+    var numArr = new Array(times);
+
+    getRandom(numArr.length,numArr);
   }
 }
 
