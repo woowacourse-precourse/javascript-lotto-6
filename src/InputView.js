eb84f583e7ffa5685fafd96b063913d2c9bf320c
SSY1203 <tlstjsdud566@naver.com>
2023-11-04 22:16:39 +0900

    [feat] 로또 구매 금액 입력

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,10 @@
-const InputView = {};
+import { Console, PLZ_PURCHASE_AMOUNT } from './Constant';
+
+const InputView = {
+  async readPurchaseAmount() {
+    const answer = await Console.readLineAsync(PLZ_PURCHASE_AMOUNT);
+    return answer;
+  },
+};
 
 export default InputView;
--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,3 +1,11 @@
-class LottoGame {}
+import InputView from './InputView';
+
+class LottoGame {
+  #purchaseAmount;
+
+  async gameStart() {
+    this.#purchaseAmount = await InputView.readPurchaseAmount();
+  }
+}
 
 export default LottoGame;
