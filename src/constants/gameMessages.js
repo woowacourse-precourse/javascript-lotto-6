f0fc194bd3c8444c5ff267a2b2aa1db17d9f87ad
jinsil <jinsil378900@gmail.com>
2023-11-05 00:56:00 +0900

    Feat: 메세지 상수 작성

---
+++ src/constants/gameMessages.js
@@ -0,0 +1,16 @@
+const gameMessage = Object.freeze({
+  INPUT: {
+    BUY: '구입금액을 입력해 주세요.\n',
+    WINNING: '\n당첨 번호를 입력해 주세요.',
+    BONUS: '\n보너스 번호를 입력해 주세요.',
+  },
+
+  OUTPUT: {
+    BUY: (quantity) => `${quantity}개를 구매했습니다.`,
+    RESULT_TEXT: '당첨 통계\n---',
+    RESULT: (match, reward, quantity) =>
+      `${match} (${reward}원) - ${quantity}개`,
+    INCOME: (rate) => `총 수익률은 ${rate}%입니다.`,
+  },
+});
+export default gameMessage;
