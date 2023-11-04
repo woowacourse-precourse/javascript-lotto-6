86017442bb3814d0a56a316fcdc46ee30dcfa3ce
Ryong-E <think8867@gmail.com>
2023-11-04 16:22:22 +0900

    feature: 구입한 로또 출력 매서드 생성

--- src/views/View.js
+++ src/views/View.js
@@ -1,24 +1,40 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import PurChaseValidator from '../validators/PurchaseValidator.js';
 import LottoValidator from '../validators/LottoValidator.js';
+import { OUTPUT_MESSAGE } from '../constant.js';
 
 class View {
+  static print(message) {
+    MissionUtils.Console.print(message);
+  }
+
   static async askPurchase(message) {
     const answer = await MissionUtils.Console.readLineAsync(message);
     PurChaseValidator.checkPurchase(answer);
+
     return answer;
   }
 
   static async askWinningNumber(message) {
     const answer = await MissionUtils.Console.readLineAsync(message);
     LottoValidator.isLotto(answer);
+
     return answer;
   }
 
   static async askBonusNumber(winningNumbers, message) {
     const answer = await MissionUtils.Console.readLineAsync(message);
     LottoValidator.bonusValidate(winningNumbers, answer);
+
     return answer;
+  }
+
+  static printLotto(lottos) {
+    this.print(`${lottos.length}${OUTPUT_MESSAGE.purchase}`);
+
+    lottos.forEach((lotto) => {
+      this.print(lotto);
+    });
   }
 }
 export default View;
