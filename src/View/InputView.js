9474a9d54a13d11432ba89081a12def1f1730e7f
dooohun <ehgns0305@naver.com>
2023-11-04 16:45:34 +0900

    Feat : 구입 금액의 유효성 검사 기능 추가

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -5,6 +5,18 @@ class InputView {
     const purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     return purchaseAmount;
   }
+
+  static validatePurchaseAmount(purchaseAmount) {
+    if (isNaN(purchaseAmount)) {
+      throw new Error('[ERROR] 구입금액은 숫자로 입력해야 합니다.');
+    }
+    if (Number(purchaseAmount) <= 0) {
+      throw new Error('[ERROR] 구입금액은 양수여야 합니다.');
+    }
+    if (Number(purchaseAmount) % 1000 !== 0) {
+      throw new Error('[ERROR] 구입금액은 1000원으로 나누어 떨어져야 합니다.');
+    }
+  }
 }
 
 export default InputView;
