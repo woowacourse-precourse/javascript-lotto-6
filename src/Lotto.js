ebd7aa1dc06fd6d654b84d74de515a067ddca4a9
chorong <chorong4641@naver.com>
2023-11-04 23:36:45 +0900

    feat(lotto): 로또 유효성 검사 로직 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,17 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers.every(number => number < 1 || number > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+    if(new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 중복되지 않은 숫자여야 합니다.")
+    }
   }
 
-  // TODO: 추가 기능 구현
+  get numbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
