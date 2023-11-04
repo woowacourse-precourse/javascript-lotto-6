7bb24540f241bd1f62fe4bb7d54e6c00485b7bb7
이용성 <ukal5006@kakao.com>
2023-11-05 01:36:40 +0900

    feat(LottoTicket): 저장, 길이반환, 티켓반환 기능 추가

---
+++ src/models/LottoTicket.js
@@ -0,0 +1,20 @@
+class LottoTicket {
+  #lottoTicket;
+
+  constructor() {
+    this.#lottoTicket = [];
+  }
+
+  addLottoToLottoTicket(lotto) {
+    this.#lottoTicket.push(lotto);
+  }
+  getLottoTicketLength() {
+    return this.#lottoTicket.length;
+  }
+
+  getLottoTicket() {
+    return this.#lottoTicket;
+  }
+}
+
+export default LottoTicket;
