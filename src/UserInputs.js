1468c44858cf8abbf835cb7f8604938554f9c504
Jeongeun Lee <leeju1009@gmail.com>
2023-11-04 18:01:07 +0900

    refactor: 사용자 입력값을 받는 함수가 질문을 인자로 받도록 리팩토링

--- src/UserInputs.js
+++ src/UserInputs.js
@@ -1,25 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class UserInputs {
-  QUESTIONS = {
-    winningNumbers: '당첨 번호를 입력해 주세요.',
-    bonusNumber: '보너스 번호를 입력해 주세요.',
-  };
+  static async ask(question) {
+    const userAnswer = await Console.readLineAsync(question);
 
-  static async askWinningNumbers() {
-    const userNumbers = await Console.readLineAsync(
-      this.QUESTIONS.winningNumbers,
-    );
-
-    return userNumbers;
-  }
-
-  static async askBonusNumber() {
-    const userBonusNumber = await Console.readLineAsync(
-      this.QUESTIONS.bonusNumber,
-    );
-
-    return userBonusNumber;
+    return userAnswer;
   }
 }
 
