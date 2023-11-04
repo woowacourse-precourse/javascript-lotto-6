25b380ddcf9e8ee51c36c0c127c1398887249116
noodle <noodle>
2023-11-05 05:24:51 +0900

    refactor: utils/messageFormat.js 파일 분리 및 수정

---
+++ src/utils/messageFormat.js
@@ -0,0 +1,16 @@
+import ERROR from '../constants/error.js';
+import LOTTO from '../constants/lotto.js';
+import { formatNumberWithCommasAndDecimals } from './index.js';
+
+export const MessageFormat = {
+  error: (message) => `${ERROR.message.prefix} ${message}`,
+  purchaseConfirm: (number) => `${number}개를 구매했습니다.\n`,
+  resultRow: (key, winCount = 0) => {
+    const prizeInfo = LOTTO.lottoPrizesMap.get(key);
+    const prize = prizeInfo.prize.toLocaleString();
+    const description = prizeInfo.description;
+
+    return `${description} (${prize}원) - ${winCount}개`;
+  },
+  totalReturnRate: (rate) => `총 수익률은 ${formatNumberWithCommasAndDecimals(rate)}%입니다.`,
+};
