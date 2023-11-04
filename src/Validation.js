feb34a2ddd847702094e2cae0c19ddf67b1060d1
Jeongeun Lee <leeju1009@gmail.com>
2023-11-05 00:37:01 +0900

    refactor: 당첨 번호 유효성 검사 함수들을 배열단위 검사에서 숫자(1개) 단위 검사로 리팩토링

--- src/Validation.js
+++ src/Validation.js
@@ -1,16 +1,17 @@
 class Validation {
   isValidWinningNumbers(numbers) {
-    this.isNumber(numbers);
+    numbers.forEach((number) => {
+      this.isNumber(number);
+      this.isNumberInRange(number);
+    });
+
     this.isNonDuplicatedNumber(numbers);
-    this.isNumberInRange(numbers);
   }
 
-  isNumber(numbers) {
-    numbers.forEach((number) => {
-      if (Number.isNaN(number)) {
-        throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
-      }
-    });
+  isNumber(number) {
+    if (Number.isNaN(number)) {
+      throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
+    }
   }
 
   isNonDuplicatedNumber(numbers) {
@@ -20,12 +21,10 @@ class Validation {
     }
   }
 
-  isNumberInRange(numbers) {
-    numbers.forEach((number) => {
-      if (number < 1 || number > 45) {
-        throw new Error('[ERROR] 1이상 45이하의 숫자만 입력할 수 있습니다.');
-      }
-    });
+  isNumberInRange(number) {
+    if (number < 1 || number > 45) {
+      throw new Error('[ERROR] 1이상 45이하의 숫자만 입력할 수 있습니다.');
+    }
   }
 }
 
