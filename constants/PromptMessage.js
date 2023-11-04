7785de2b1c29e9aac0984c95c949b312b17bb2ee
Jules <dali1999@naver.com>
2023-11-05 01:41:29 +0900

    style(PromptMessage): 줄바꿈 문자 추가

--- constants/PromptMessage.js
+++ constants/PromptMessage.js
@@ -2,22 +2,18 @@ class PromptMessage {
   static ENTER_PRICE = '구입금액을 입력해 주세요.\n';
 
   static PRINT_PURCHASED_NUM(num) {
-    return `${num}개를 구매했습니다.`;
+    return `\n${num}개를 구매했습니다.`;
   }
 
-  static ENTER_WIN_NUM = '당첨 번호를 입력해 주세요.\n';
+  static ENTER_WIN_NUM = '\n당첨 번호를 입력해 주세요.\n';
 
-  static ENTER_BONUS_NUM = '보너스 번호를 입력해 주세요.\n';
+  static ENTER_BONUS_NUM = '\n보너스 번호를 입력해 주세요.\n';
 
-  static PRINT_STATISTICS = '당첨 통계\n---';
+  static PRINT_STATISTICS = '\n당첨 통계\n---';
 
-  static PRINT_RETURN_RATE = '총 수익률은 62.5%입니다.';
-
-  //   static a = '구입금액을 입력해 주세요.';
-
-  //   static a = '구입금액을 입력해 주세요.';
-
-  //   static a = '구입금액을 입력해 주세요.';
+  static PRINT_RETURN_RATE(rate) {
+    return `총 수익률은 ${rate}%입니다.`;
+  }
 }
 
 export default PromptMessage;
