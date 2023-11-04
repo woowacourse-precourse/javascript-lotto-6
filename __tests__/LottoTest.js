38623848542e3924512c0151f219f93125b63ec8
6puritans9 <6puritans9@gmail.com>
2023-11-04 18:10:45 +0900

    test(LottoTest): Added amount test

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 import Lotto from "../src/Lotto.js";
+import Printer from "../src/Game/Printer.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +16,15 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test.each([10000, 35000, 4000])(
+    "구입 금액을 가격으로 나눈 몫만큼 구매 개수가 출력된다.",
+    () => {
+      const printer = new Printer();
+
+      const results = [10, 35, 4];
+      const result = results.shift();
+
+      expect(printer.amount).toBe(result);
+    }
+  );
 });
