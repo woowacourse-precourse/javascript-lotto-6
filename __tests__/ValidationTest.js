5e444994bc28d2317a4c9393c4917e5c2a12b3b1
chiman <rlaclghks123@naver.com>
2023-11-05 01:36:48 +0900

    test: 구매금액 테스트 수정
    좁은범위의 각각 테스트 했었는데, inputLottoPrice로 테스트 수정

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -4,13 +4,13 @@ describe('로또 금액 테스트', () => {
   describe('로또 금액이 1,000원 단위 테스트', () => {
     test('로또 금액이 1,000원 단위일 경우', () => {
       expect(() => {
-        Validation.currencyAmount('3000');
+        Validation.inputLottoPrice('3000');
       }).not.toThrow();
     });
 
     test('로또 금액이 1,000원 단위가 아닌 경우', () => {
       expect(() => {
-        Validation.currencyAmount('1350');
+        Validation.inputLottoPrice('1350');
       }).toThrow();
     });
   });
@@ -18,44 +18,44 @@ describe('로또 금액 테스트', () => {
   describe('로또 금액이 숫자 형태인지 테스트', () => {
     test('로또 금액이 문자열 + 숫자 형태인 경우 Ex)+3000일 경우', () => {
       expect(() => {
-        Validation.numberType('+3000');
+        Validation.inputLottoPrice('+3000');
       }).toThrow();
     });
 
     test('로또 금액이 문자열 + 숫자 형태인 경우 Ex) -3000일 경우', () => {
       expect(() => {
-        Validation.numberType('-3000');
+        Validation.inputLottoPrice('-3000');
       }).toThrow();
     });
 
     test('로또 금액에 공백이 포함된 경우', () => {
       expect(() => {
-        Validation.numberType('3000  ');
+        Validation.inputLottoPrice('3000  ');
       }).toThrow();
     });
 
     test('로또 금액이 빈값인 경우', () => {
       expect(() => {
-        Validation.numberType('');
+        Validation.inputLottoPrice('');
       }).toThrow();
     });
 
     test('로또 금액이 여러개의 빈값인 경우', () => {
       expect(() => {
-        Validation.numberType('          ');
+        Validation.inputLottoPrice('          ');
       }).toThrow();
     });
   });
 
   test('로또 금액이 너무 큰 경우 테스트 -> 200001000인 경우', () => {
     expect(() => {
-      Validation.limitPrice('2000001000');
+      Validation.inputLottoPrice('2000001000');
     }).toThrow();
   });
 
   test('로또 금액이 1000원 이하인 경우 테스트 -> 0인 경우', () => {
     expect(() => {
-      Validation.minimumPrice('0');
+      Validation.inputLottoPrice('0');
     }).toThrow();
   });
 });
