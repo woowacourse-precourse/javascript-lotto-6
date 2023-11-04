63c36f616d3ee26fb481011979af090026d69623
lee juyoung <juyung0903@naver.com>
2023-11-04 23:19:31 +0900

    feat: 구입금액 검증 및 구입할수 있는 수만큼 로또발행 (LottoGame클래스에 구현)

---
+++ src/LottoGame.js
@@ -0,0 +1,34 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+class LottoGame {
+    #lottos;
+    #purchaseNumber;
+    constructor (purchaseAmount) {
+        this.#lottos = [];
+        this.#purchaseNumber = purchaseAmount/1000;
+        this.#issueLotto();
+    }
+
+    #validate(purchaseAmount) {
+        if (purchaseAmount % 1000 !== 0) {
+          throw new Error("[ERROR]");
+        }
+      }
+
+    #issueLotto() {
+        for(let i=0; i<this.#purchaseNumber; i++) {
+            const LOTTO = new Lotto(this.generateRandomNumbers());
+            this.#lottos.push(LOTTO);
+        }
+    }
+
+    generateRandomNumbers() {
+        const RANDOM_NUMBERS = [];
+        for (let i=0; i<6; i++) {
+            RANDOM_NUMBERS.push(MissionUtils.Random.pickNumberInRange(1,45));      
+        }
+        return RANDOM_NUMBERS;
+    }
+ 
+}
+export default LottoGame;
