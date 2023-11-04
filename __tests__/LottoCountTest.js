015fe2a0c0e3008baf76b10f7380fdbf7f9c78b0
whai2 <whai2125@gmail.com>
2023-11-04 16:28:37 +0900

    refactor(Counter): 카운터 클래스를 통해, 구매 개수 객체화

--- __tests__/LottoCountTest.js
+++ __tests__/LottoCountTest.js
@@ -1,4 +1,4 @@
-import { lottoCounter, inputMoneyValidater, inputMoneyDivideValidater } from "../src/App.js";
+import { lottoCounter, Counter } from "../src/App.js";
 
 describe("구입 금액에 따른 로또 개수 테스트", () => {
   test("구입 금액에 따라 올바른 개수의 로또를 발행한다.", () => {
@@ -8,7 +8,6 @@ describe("구입 금액에 따른 로또 개수 테스트", () => {
     const result = lottoCounter(INPUT);
     expect(result).toBe(OUTPUT);
   });
-  // 아래에 추가 테스트 작성 가능
 
   test.each([
     [["만 원"]],
@@ -16,15 +15,14 @@ describe("구입 금액에 따른 로또 개수 테스트", () => {
     [["-500"]],
     [["0b101"]],
     [["1E3"]],
-  ])("입력이 숫자가 아닐 경우, 에러가 발생한다.", async (inputs) => {
-  
-    // then
-    expect(inputMoneyValidater(inputs)).rejects.toThrow("[ERROR]");
+  ])("입력이 숫자가 아닐 경우, 에러가 발생한다.", (inputs) => {
+
+    expect(() => {new Counter(inputs)}).toThrow("[ERROR]");
   });
 
   test("구입 금액이 로또 금액으로 나눠 떨어지지 않을 경우, 에러가 발생한다.", () => {
     const INPUT = 5500;
 
-    expect(inputMoneyDivideValidater(INPUT)).rejects.toThrow("[ERROR]");
+    expect(() => {new Counter(INPUT)}).toThrow("[ERROR]");
   });
 });
--- src/App.js
+++ src/App.js
@@ -88,3 +88,27 @@ export async function bonusNumberDuplicateValidater(winning, bonus) {
     }
   }
 }
+
+export class Counter {
+  #money;
+
+  constructor(money) {
+    this.#inputMoneyValidater(money);
+    this.#inputMoneyDivideValidater(money);
+    this.#money = money;
+  }
+
+  #inputMoneyValidater(money) {
+    if (/^[+]?[1-9]\d*$/.test(money)) {
+      return Number(money);
+    } 
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+  }
+
+  #inputMoneyDivideValidater(money) {
+    let price = LOTTO_PRICE;
+    if (money%price !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1000원 단위로 입력 가능합니다.");
+    }
+  }
+}
