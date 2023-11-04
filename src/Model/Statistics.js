9a373e5f8f3a0c415472da93fa906c8191f2622c
dyddks <dyddks0115@naver.com>
2023-11-04 16:46:15 +0900

    feat: 당첨등수와 수익률계산

---
+++ src/Model/Statistics.js
@@ -0,0 +1,73 @@
+import { Console } from '@woowacourse/mission-utils'
+
+class Statistics {
+  #winningNumber
+  #lottoNumber
+  #bonus
+  #checkArray
+  #result
+
+  constructor(winningNumber, lottoNumber, bonus) {
+    this.#winningNumber = winningNumber;
+    this.#lottoNumber = lottoNumber;
+    this.#bonus = bonus;
+    this.#checkArray = [];
+    this.#result = [0, 0, 0, 0, 0];
+
+    // 각 로또에 일치하는 숫자 개수 집계
+    this.#lottoNumber.forEach(innerArray => {
+      const sameNumber = innerArray.filter(element => this.#winningNumber.includes(element));
+      this.#checkArray.push(sameNumber.length);
+    });
+  }
+
+  // 등수 집계
+  #winningRank() {
+    for (let i=0; i<this.#checkArray.length; i+=1) {
+      if (this.#checkArray[i] === 3) {
+        this.#result[0] += 1;
+      }
+
+      if (this.#checkArray[i] === 4) {
+        this.#result[1] += 1;
+      }
+
+      if (this.#checkArray[i] === 5) {
+        this.#checkBonus(i);
+      }
+
+      if (this.#checkArray[i] === 6) {
+        this.#result[4] += 1;
+      }
+    }
+  }
+
+  #checkBonus(i) {
+    if ((this.#lottoNumber[i]).includes(this.#bonus)) {
+      this.#result[3] += 1;
+    } else {
+      this.#result[2] += 1;
+    }
+  }
+
+  getResult() {
+    this.#winningRank();
+    return this.#result;
+  }
+
+  getROI() {
+    const reword = [5000, 50000, 1500000, 30000000, 2000000000];
+    const investment = this.#lottoNumber.length *1000;
+    let profit = 0;
+
+    for (let i=0; i<this.#result.length; i+=1) {
+      profit += this.#result[i] * reword[i];
+    }
+
+    const roi = (profit/investment)*100;
+
+    return roi.toFixed(1);
+  }
+}
+
+export default Statistics;
