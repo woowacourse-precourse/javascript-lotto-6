84a21c060777abd4fe12cbc2f675dce0b7b67f5e
pakxe <pigkill40@naver.com>
2023-11-04 20:22:20 +0900

    feat: 당첨 번호의 개수가 유효하지 않으면 띄울 오류 메세지 추가 작성

--- src/constant/Error.js
+++ src/constant/Error.js
@@ -6,6 +6,7 @@ const ERROR = Object.freeze({
   isNan: '숫자를 입력해주세요.',
   money: `금액은 ${LOTTO.price}원 단위로 입력해주세요.`,
   range: `번호는 ${LOTTO.minLottoNum} ~ ${LOTTO.maxLottoNum} 사이의 값으로 입력해주세요.`,
+  count: `당첨 번호는 ${LOTTO.count}개로 입력해주세요.`,
 });
 
 export default ERROR;
