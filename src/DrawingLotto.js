d0685ba31e368d2ef681e7b56593697d30580967
Jeongeun Lee <leeju1009@gmail.com>
2023-11-04 19:51:42 +0900

    feat: 보너스 번호를 추첨하는 기능 추가
    사용자에게 보너스 번호를 입력 받아, 보너스 번호를 추첨하는 기능 추가

--- src/DrawingLotto.js
+++ src/DrawingLotto.js
@@ -4,6 +4,7 @@ import Utils from './Utils.js';
 class DrawingLotto {
   QUESTIONS = {
     winningNumbers: '당첨 번호를 입력해 주세요.',
+    bonusNumber: '보너스 번호를 입력해 주세요.',
   };
 
   async #drawWinningNumbers() {
@@ -12,9 +13,19 @@ class DrawingLotto {
     return winningNumbers;
   }
 
+  async #drawBonusNumber() {
+    const userNumber = await UserInputs.ask(this.QUESTIONS.bonusNumber);
+    return userNumber;
+  }
+
   async getWinningNumbers() {
     const winningNumbers = await this.#drawWinningNumbers();
     return winningNumbers;
+  }
+
+  async getBonusNumber() {
+    const bonusNumber = await this.#drawBonusNumber();
+    return bonusNumber;
   }
 }
 
