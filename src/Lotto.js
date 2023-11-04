afbb5fa867dacfb3760e4672ba5ce76d6c3142f9
pakxe <pigkill40@naver.com>
2023-11-04 17:08:06 +0900

    feat: 로또에 사용되는 매직넘버 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,13 +2,13 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    Lotto.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+  static #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
