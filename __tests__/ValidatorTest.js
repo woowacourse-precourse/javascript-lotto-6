7ca16bae9021544e0263d8d4b4ce835e0700de78
BangDori <qudwns8865@naver.com>
2023-11-04 22:42:34 +0900

    test: 구매 금액으로 로또를 구매할 수 있는지 테스트 추가

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -20,4 +20,24 @@ describe('Money Validator 클래스 테스트', () => {
       });
     });
   });
+
+  describe('로또 구매 여부 테스트', () => {
+    test('숫자가 1000 이상이라면 에러가 발생하지 않는다.', () => {
+      const money = 1000;
+
+      expect(() =>
+        MoneyValidator.validateLottoPurchaseAmount(money),
+      ).not.toThrow();
+    });
+
+    test('숫자가 1000보다 작다면 예외가 발생한다.', () => {
+      const moneyList = [-1, 0, 999];
+
+      moneyList.forEach(money =>
+        expect(() => MoneyValidator.validateLottoPurchaseAmount(money)).toThrow(
+          '[ERROR]',
+        ),
+      );
+    });
+  });
 });
