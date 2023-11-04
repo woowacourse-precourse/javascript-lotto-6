a3ec8f2a2936de173cc245dcda6c93b026ead4df
heonq <gkagjsrb@naver.com>
2023-11-04 22:25:22 +0900

    feat: 기능 구현에 필요한 상수를 담은 CONSTANTS 객체 생성

---
+++ utils/Constants.js
@@ -0,0 +1,11 @@
+const CONSTANTS = Object.freeze({
+  minimumNumber: 1,
+  maximumNumber: 45,
+  eachLottoPrice: 1000,
+  bonusNumberCount: 1,
+  mainNumberCount: 6,
+  numberDivision: ',',
+  bonusNumberConditionCount: 5,
+});
+
+export default CONSTANTS;
