7ce46280ce67099c203410c8faaf5ebecf9027b2
brgndy <brgndy96@gmail.com>
2023-11-04 09:02:11 +0900

    test: Money 클래스 관련 단위 테스트 코드 작성

---
+++ __tests__/Money/MoneyTest.js
@@ -0,0 +1,29 @@
+import Money from '../../src/domains/Money.js';
+import AppError from '../../src/errors/AppError.js';
+import { ERROR_MESSAGES } from '../../src/constants/errorMessages.js';
+
+describe('구입 금액 클래스 테스트', () => {
+  test('구입 금액이 1000원 보다 작을때 예외가 발생한다.', () => {
+    expect(() => {
+      new Money('800');
+    }).toThrow(new AppError(ERROR_MESSAGES.lack_money));
+  });
+
+  test('구입 금액에 공백이 포함 된 경우 예외가 발생한다.', () => {
+    expect(() => {
+      new Money('1000 23');
+    }).toThrow(new AppError(ERROR_MESSAGES.not_a_number));
+  });
+
+  test('입력 받은 금액이 숫자가 아닌 경우 예외가 발생한다.', () => {
+    expect(() => {
+      new Money('  ');
+    }).toThrow(new AppError(ERROR_MESSAGES.lack_money));
+  });
+
+  test('천원 단위로 나누어 떨어지지 않는 경우 예외가 발생한다.', () => {
+    expect(() => {
+      new Money('1234');
+    }).toThrow(new AppError(ERROR_MESSAGES.not_divded));
+  });
+});
