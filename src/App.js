d7f44f115c44ddac0b23ff5531657fbd665cea8c
ReturnReason <reasonz@daum.net>
2023-11-05 01:01:46 +0900

    feat(LottoGameController): 구입 금액을 입력받는 로직 추가

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import OutputView from './view/OutputView.js';
 
 class App {
   async play() {
-    const randomNumberGeneration = new RandomNumberStrategy();
     const inputView = new InputView();
     const outputView = new OutputView();
+    const randomNumberGeneration = new RandomNumberStrategy();
     const lottoGameController = new LottoGameController({
       randomNumberGeneration,
       inputView,
       outputView,
     });
-    try {
-      await lottoGameController.start();
-    } catch (errorMessage) {
-      throw new Error(errorMessage);
-    }
+    await lottoGameController.start();
   }
 }
 
--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -1,21 +1,39 @@
-import { MESSAGE } from '../constants/messages.js';
+import Money from '../model/Money.js';
+import InputValidator from '../validator/InputValidator.js';
 import convertType from '../utils/convertType.js';
+import { MESSAGE } from '../constants/messages.js';
 
 class LottoGameController {
+  #moneyInstance;
+
   constructor({ randomNumberGeneration, inputView, outputView }) {
+    this.randomNumberGeneration = randomNumberGeneration;
     this.inputView = inputView;
     this.outputView = outputView;
-    this.randomNumberGeneration = randomNumberGeneration;
   }
 
   async start() {
-    await this.getPurchaseAmount();
+    while (true) {
+      try {
+        this.#moneyInstance = await this.createMoneyInstance();
+        break;
+      } catch (error) {
+        this.outputView.print(error);
+      }
+    }
   }
 
   async getPurchaseAmount() {
     const money = convertType(
       await this.inputView.getUserInputAsync(MESSAGE.INPUT),
     );
+    InputValidator.validateMoney(money);
+    return money;
+  }
+
+  async createMoneyInstance() {
+    const money = await this.getPurchaseAmount();
+    return new Money(money);
   }
 }
 
