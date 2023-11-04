d58d918f5f6a02264c7013a690be01a90eabcce3
simple0710 <thesimpleman@naver.com>
2023-11-04 18:20:30 +0900

    feat: 로또 당첨 번호 선택 검증 기능 추가
    - findStringInNumbers : 주어진 배열에 문자가 있는 경우 true 반환 후 오류 메시지 출력
    - findOverNumbers : 주어진 배열에 숫자의 범위를 벗어난 경우 true 반환 후 오류 메시지 출력
    - findDuplicationInNumbers : 중복된 숫자가 있는 경우 true 반환 후 오류 메시지 출력

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,10 +9,28 @@ class Lotto {
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    } else if (this.findStringInNumbers(numbers)) {
+      throw new Error("[ERROR] 로또 번호에 문자가 있습니다.")
+    } else if (this.findOverNumbers(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
+    } else if (this.findDuplicationInNumbers(numbers)) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
     }
   }
 
   // TODO: 추가 기능 구현
+  findStringInNumbers(numbers) {
+    return numbers.some(item => isNaN(item));
+  }
+  
+  findOverNumbers(numbers) {
+    return numbers.some(item => item < 1 || item > 45);
+  }
+
+  findDuplicationInNumbers(numbers) {
+    const duplication_numbers = new Set(numbers);
+    return duplication_numbers.size !== numbers.length;
+  }
 }
 
 export default Lotto;
