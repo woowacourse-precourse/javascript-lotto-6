79fb1efb08be40d37d4677924e9a58082bbc704a
김미르 <kimmireu@gimmileuui-iMac.local>
2023-11-04 22:37:52 +0900

    refactor : OutputView 코드 간결화

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -3,20 +3,21 @@ import MESSAGES from "../constants/messages.js";
 
 class OutputView {
   printLottos(totalCount, lottos) {
-    Console.print(`${totalCount}개를 ${MESSAGES.buy}`);
-    for (let lotto of lottos) {
-      Console.print(`[${lotto.getNumbers().join(", ")}]`);
-    }
+    this.print(`${totalCount}개를 ${MESSAGES.buy}`);
+    lottos.forEach((lotto) => {
+      this.print(`[${lotto.getNumbers().join(", ")}]`);
+    });
   }
 
   printResult(price, result, income) {
+    this.print(`${MESSAGES.prize.fifth} - ${result[5]}개`);
+    this.print(`${MESSAGES.prize.fourth} - ${result[4]}개`);
+    this.print(`${MESSAGES.prize.third} - ${result[3]}개`);
+    this.print(`${MESSAGES.prize.second} - ${result[2]}개`);
+    this.print(`${MESSAGES.prize.first} - ${result[1]}개`);
+
     const rateOnReturn = ((income / price) * 100).toLocaleString();
-    Console.print(`${MESSAGES.prize.fifth} - ${result[5]}개`);
-    Console.print(`${MESSAGES.prize.fourth} - ${result[4]}개`);
-    Console.print(`${MESSAGES.prize.third} - ${result[3]}개`);
-    Console.print(`${MESSAGES.prize.second} - ${result[2]}개`);
-    Console.print(`${MESSAGES.prize.first} - ${result[1]}개`);
-    Console.print(`총 수익률은 ${rateOnReturn}%입니다.`);
+    this.print(`총 수익률은 ${rateOnReturn}%입니다.`);
   }
 
   print(message) {
