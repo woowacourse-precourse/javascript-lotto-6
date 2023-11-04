61bdfeba95a36774f80fab6f2f48762346fc6bcd
yesiamok3 <jaehyunok@cau.ac.kr>
2023-11-05 00:17:44 +0900

    feat: 구입 금액 입력
    로또 구입 금액 입력 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    let lottoMoney = await inputMoney();
+  }
+}
+
+async function inputMoney() {
+  let money = await MissionUtils.Console.readLineAsync('구입금액을 입력해주세요.\n');
+  money = parseInt(money);
+  return money
 }
 
 export default App;
