845bd396ebb01b353dc3fbe10aac20ef6988abcc
김영은 <jessicak0828@gmail.com>
2023-11-04 21:38:16 +0900

    feat: 클래스 Lotto 생성
    Lotto는 입력한 숫자열, 입력한 보너스 숫자, 구매한 로또 개수, 구매한 로또들 세트 를 가지고 있다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,10 @@
 class Lotto {
-  #numbers;
-
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
-  // TODO: 추가 기능 구현
+	constructor() {
+		this.numbers;
+		this.bonus;
+		this.amount;
+		this.randomArraySet;
+	}
 }
 
 export default Lotto;
