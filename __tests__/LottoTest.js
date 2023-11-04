28b4203c3bc9701c68f4d31b4950d7f221678873
alexgoni <goni000211@gmail.com>
2023-11-05 00:28:23 +0900

    test(LottoTest): add unit test for ascending order

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,11 @@
 import Lotto from "../src/Lotto.js";
+import { Console } from "@woowacourse/mission-utils";
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -17,5 +24,17 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 100]);
     }).toThrow("[ERROR]");
+  });
+
+  test("출력되는 로또 번호가 오름차순으로 정렬되어 있는지 테스트", () => {
+    // given
+    const logSpy = getLogSpy();
+
+    // when
+    const lotto = new Lotto([5, 4, 3, 6, 2, 1]);
+    lotto.printNumbers();
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith([1, 2, 3, 4, 5, 6]);
   });
 });
