fbc5095f03ae977acd82c94c06403473d3aa7d32
BangDori <qudwns8865@naver.com>
2023-11-05 00:36:39 +0900

    refactor: 에러 메시지 분리

---
+++ src/constants/message/error.js
@@ -0,0 +1,15 @@
+import { LOTTO } from '../setting.js';
+
+export const ERROR_PREFIX = '[ERROR] ';
+
+export const MONEY_ERROR = Object.freeze({
+  number: '구입금액은 숫자만 입력 가능합니다.',
+  purchaseAmount: `구입금액은 ${LOTTO.price}원 이상만 가능합니다.`,
+  amountExactness: `구입금액은 ${LOTTO.price}원 단위로만 가능합니다.`,
+});
+
+export const LOTTO_ERROR = Object.freeze({
+  count: `로또 번호는 ${LOTTO.count}개여야 합니다.`,
+  duplication: '로또 번호는 중복될 수 없습니다.',
+  number: `로또 번호는 ${LOTTO.minNumber}이상 ${LOTTO.maxNumber}이하의 숫자로 구성되어야 합니다.`,
+});
