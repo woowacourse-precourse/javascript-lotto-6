e56f65a409e8cc5d07831c5eacbfdb8e98890a3a
BangDori <qudwns8865@naver.com>
2023-11-04 20:35:36 +0900

    feat: 로또 번호 중복 검사 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,24 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.#duplicate(numbers);
     this.#numbers = numbers;
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  #duplicate(numbers) {
+    const uniqueNumbers = new Set(numbers);
+
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    }
+  }
 }
 
 export default Lotto;
