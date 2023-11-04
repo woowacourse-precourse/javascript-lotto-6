deb2a91266f64b196dd3cdb77823e17fda61c05c
seoyeon-jung <jungluchia1204@gmail.com>
2023-11-04 23:47:10 +0900

    feat: message 추가
    - 매번 사용할 message를 Constant.js에 추가했습니다
    - 추후 error message 추가 예정입니다

---
+++ src/Constant.js
@@ -0,0 +1,10 @@
+const GAME_MESSAGE = {
+  BUY_AMOUNT: "구입금액을 입력해주세요.\n",
+  GUESS_NUM: "당첨 번호를 입력해주세요.\n",
+  BONUS_NUM: "보너스 번호를 입력해주세요\n",
+  STATIC: "당첨 통계\n---",
+};
+
+const ERROR_MESSAGE = {};
+
+module.exports = { GAME_MESSAGE, ERROR_MESSAGE };
