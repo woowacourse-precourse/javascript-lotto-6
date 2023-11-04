400286ddd4903ff5092910b0e2367088078323e0
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-04 19:52:56 +0900

    refactor(위치수정): 라인 위치 변경

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,8 +4,8 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
     this.#numbers = numbers;
+    this.#validate(numbers);
     this.#isNumbersUnique(numbers); // 중복 번호 검증
   }
 
