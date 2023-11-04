9ddd3a12b3fd6f5df85baf6bcd44340f7f7d6f8e
hyeong1 <mi75625@gmail.com>
2023-11-04 20:09:19 +0900

    feat: 구입 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+import PRINT from "../src/constant/print.js";
+import ERROR from "../src/constant/error.js";
+
 class App {
-  async play() {}
+  async askMoney() {
+    const money = await MissionUtils.Console.readLineAsync(PRINT.ASK_BUY);
+    return parseInt(money);
+  }
+
+  moneyTypeCheck(money) {
+    if (isNaN(money)) {
+      throw new Error(ERROR.NOT_NUMBER);
+    }
+    if (money % 1000 !== 0) {
+      throw new Error(ERROR.NOT_1000N);
+    }
+  }
+
+  async play() {
+    const inputMoney = await this.askMoney();
+    this.moneyTypeCheck(inputMoney);
+  }
 }
 
 export default App;
