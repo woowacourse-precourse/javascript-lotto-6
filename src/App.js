e5ac787774b276017ea4ed161a917cc90a9d73b4
jadewisemann <jadewisemann@gmail.com>
2023-11-05 05:04:22 +0900

    FEAT: Make Function, 'getUserInput()'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils"
+import message from "./Message.js";
+import Lotto from './Lotto.js';   
+
+
 class App {
-  async play() {}
+  async play() {
+    const test = await getUserInput(message.inputPurchasingPrice)
+  }
+
+
 }
+
+async function getUserInput(message) {
+  return  await Console.readLineAsync(message);
+}
+
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,5 @@
+const message = {
+  inputPurchasingPrice: '구입금액을 입력해 주세요.',  
+};
+
+export default message;
