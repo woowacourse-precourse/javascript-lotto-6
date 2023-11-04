9387c1823868a33c395260a53b4d8768b3e06770
unknown <pih9134@naver.com>
2023-11-05 02:56:12 +0900

    feat: 구입금액 입력값이 0일 경우 예외 처리

--- src/Validation.js
+++ src/Validation.js
@@ -4,7 +4,8 @@ class Validation {
 
     if (!cleanedPurchaseAmount) throw new Error('[ERROR] 구입 금액을 입력해주세요.');
     if (!Validation.isSafeInteger(cleanedPurchaseAmount)) throw new Error('[ERROR] 숫자를 입력해주세요.');
-    if (cleanedPurchaseAmount % 1000 !== 0) throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
+    if (cleanedPurchaseAmount % 1000 !== 0 || cleanedPurchaseAmount % 1 !== 0)
+      throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
 
     return cleanedPurchaseAmount;
   }
