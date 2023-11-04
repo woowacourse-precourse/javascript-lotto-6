8993c6732ec3d675749617601ff77e50322ae672
Jules <dali1999@naver.com>
2023-11-05 01:36:40 +0900

    feat(Label): '일치하는 번호 갯수' 메시지 와 금액 상수 처리

---
+++ constants/Label.js
@@ -0,0 +1,13 @@
+class Label {
+  static WIN_COUNT_LABELS = [
+    '3개 일치',
+    '4개 일치',
+    '5개 일치',
+    '5개 일치, 보너스 볼 일치',
+    '6개 일치',
+  ];
+
+  static PRICE_AMOUNT = [5000, 50000, 1500000, 30000000, 2000000000];
+}
+
+export default Label;
