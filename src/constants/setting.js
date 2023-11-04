fe8cfc7dcdae7f38f1e76520cc5b30da2f8b4b6b
BangDori <qudwns8865@naver.com>
2023-11-05 00:12:13 +0900

    refactor: 로또 관련 매직 넘버 객체로 리팩토링

--- src/constants/setting.js
+++ src/constants/setting.js
@@ -1,3 +1,9 @@
 export const DELIMITER = ',';
 
-export const LOTTO_PRICE = 1000;
+export const LOTTO = Object.freeze({
+  minNumber: 1,
+  maxNumber: 45,
+  count: 6,
+  price: 1000,
+  noRemainder: 0,
+});
