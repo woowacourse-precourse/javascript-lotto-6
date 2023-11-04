472869cf05fe5389530f2219a531d8e6f1fa12bd
이호연 <ho991217@kakao.com>
2023-11-05 02:59:38 +0900

    test: 테스트 케이스 수정
    단순 호출만을 검사하던 로직에서 '구입금액을 입력해 주세요.'라는 문구를 출력하는지 검사하도록 수정

--- __tests__/utils/InputTest.js
+++ __tests__/utils/InputTest.js
@@ -1,12 +1,26 @@
 import Input from '../../src/utils/Input';
+import { Console } from '@woowacourse/mission-utils';
 
 describe('Input', () => {
+  beforeEach(() => {
+    Console.readLineAsync = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('getCost', () => {
     test('구입금액을 입력해 주세요. 라는 문구를 출력한다.', async () => {
-      Input.getCost = jest.fn();
+      // given
+      const expectedMessage = '구입금액을 입력해 주세요.';
+      Console.readLineAsync.mockResolvedValue('1000');
 
+      // when
       await Input.getCost();
-      expect(Input.getCost).toBeCalled();
+
+      // then
+      expect(Console.readLineAsync).toHaveBeenCalledWith(expectedMessage);
     });
   });
 });
