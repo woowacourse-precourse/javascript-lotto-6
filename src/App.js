c125eaa4a3ba741ace19b07614a3ec595be4dc83
yebin <yebind3308@gmail.com>
2023-11-04 23:00:31 +0900

    feat: 당첨 번호를 입력 구현

--- src/App.js
+++ src/App.js
@@ -2,31 +2,61 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const purchaseAmount = await this.purchasePrice();
+    const lottoPrice = await this.totalPrice();
+    const lottoTickets = this.totalLottoTickets(lottoPrice); 
+    const winningNumbers = await this.winningNumber(); 
   }
 
-  async purchasePrice() {
+  async totalPrice() {
     const priceInput = await Console.readLineAsync('구입금액을 입력해 주세요. \n');
-    if (this.priceValidity(priceInput)) {
-      throw new Error("[ERROR] 이름이 잘못된 형식입니다.");
+    if (this.inputPriceValidity(priceInput)) {
+      throw new Error("[ERROR] 구입 금액이 잘못된 형식입니다.");
     }
-
-    const numberOfLotto = ~~(priceInput / 1000);
-    Console.print(`\n${numberOfLotto}개를 구매했습니다.`);
-
-    for (let i = 0; i < numberOfLotto; i++) {
-      const lottoNumbers = this.pickLottoNumbers();
-      const randomLottoNumbers = lottoNumbers.sort((a, b) => a - b);
-      Console.print(randomLottoNumbers);
-    }
+    return priceInput;
   }
 
-  priceValidity(priceInput) {
-    return priceInput % 1000 !== 0;
+  inputPriceValidity(priceInput) {
+    const unitError = priceInput % 1000;
+    return unitError !== 0;
   }
 
-  pickLottoNumbers() {
+  totalLottoTickets(lottoPrice) {
+    const unitLottoNumbers = ~~(lottoPrice / 1000);
+    this.lottoTotalMessage(unitLottoNumbers);
+    const lottoTickets = [];
+    for (let i = 0; i < unitLottoNumbers; i++) {
+      const randomLottoNumbers = this.randomNumbers();
+      const lottoNumbers = randomLottoNumbers.sort((a, b) => a - b);
+      lottoTickets.push(lottoNumbers);
+      Console.print(lottoNumbers); 
+    }
+    return lottoTickets;
+  }
+
+  lottoTotalMessage(unitLottoNumbers) {
+    Console.print(`\n${unitLottoNumbers}개를 구매했습니다.`);
+  }
+
+  randomNumbers() {
     return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  async winningNumber() {
+    const winningNumberInput = await Console.readLineAsync('당첨 번호를 입력해 주세요. \n');
+    const lottoNumbers = winningNumberInput.split(',').map(Number);
+    if (!this.inputNumbersValidity(winningNumberInput)) {
+      throw new Error("[ERROR] 당첨 번호가 잘못된 형식입니다.");
+    }
+    return lottoNumbers;
+  }
+
+  inputNumbersValidity(lottoNumbers) {
+    const Numbers = lottoNumbers.split(',').map(Number);
+    const uniqueNumbers = Array.from(new Set(Numbers));
+    const lengthError = Numbers.length !== 6;
+    const rangeError = Numbers.some(number => number < 1 || number > 45);
+    const overlapError = uniqueNumbers.length !== 6
+    return (!lengthError && !rangeError && !overlapError) 
   }
 }
 
