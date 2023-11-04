9c63088071ebb106b45d91718e08416d560dab02
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-04 18:58:33 +0900

    feat: 로또 번호에 대한 예외 처리 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,10 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.#duplicateCheck(numbers);
+    this.#numberRangeValidate(numbers);
+    this.#numericValidate(numbers);
+    this.#integerValidate(numbers);
     this.#numbers = numbers;
   }
 
@@ -13,6 +17,37 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  #duplicateCheck(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (numbers.length !== uniqueNumbers.size) {
+      throw new Error('[ERROR] 로또 번호가 중복되지 않아야 합니다.');
+    }
+  }
+
+  #numericValidate(numbers) {
+    const DIGITS_ONLY_PATTERN = /^\d+$/;
+    numbers.forEach((number) => {
+      if (!DIGITS_ONLY_PATTERN.test(number)) {
+        throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+      }
+    });
+  }
+
+  #integerValidate(numbers) {
+    numbers.forEach((number) => {
+      if (!Number.isInteger(number)) {
+        throw new Error('[ERROR] 로또 번호는 정수여야 합니다.');
+      }
+    });
+  }
+
+  #numberRangeValidate(numbers) {
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error('[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.');
+      }
+    });
+  }
 }
 
 export default Lotto;
