2e885e55f976f287a3c3fd5ed27e58a8eb4649fc
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-04 17:00:00 +0900

    feat(Money): 구매 금액과 관련된 기능을 담당하는 클래스 구현

---
+++ src/domain/Money.js
@@ -0,0 +1,27 @@
+import NUMBER from "../static/Number.js";
+import WINNING_REWARD from "../static/WinningReward.js";
+
+class Money {
+  #count;
+  #revenue;
+
+  constructor(input) {
+    this.#count = Number(input) / NUMBER.price;
+    this.#revenue = 0;
+  }
+
+  getAmount() {
+    return this.#count;
+  }
+
+  getRevenueRate(winningStatic) {
+    const purchasePrice = this.#count * NUMBER.price;
+    winningStatic.forEach((count, index) => {
+      this.#revenue += WINNING_REWARD[index] * count;
+    });
+
+    return ((this.#revenue / purchasePrice) * 100).toFixed(2);
+  }
+}
+
+export default Money;
