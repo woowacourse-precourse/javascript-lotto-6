cad41e8954c6658834143d270dc5a83001384e20
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-11-04 16:13:06 +0900

    feat(App, InsertMoney) : 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+// App.js
+import InsertMoney from "./InsertMoney.js";
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await InsertMoney.inputMoney();
+    // 이후 로또 번호 생성 및 게임 진행 로직 구현
+  }
 }
 
-export default App;
+const app = new App();
+app.play();
---
+++ src/InsertMoney.js
@@ -0,0 +1,29 @@
+// InsertMoney.js
+import readline from "readline";
+
+class InsertMoney {
+  static async inputMoney() {
+    return new Promise((resolve, reject) => {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.question("구입금액을 입력해 주세요: ", (money) => {
+        // 입력 받은 구입금액을 정수로 변환
+        const purchaseAmount = parseInt(money, 10);
+
+        if (isNaN(purchaseAmount) || purchaseAmount % 1000 !== 0) {
+          console.error("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.");
+          rl.close();
+          reject();
+        } else {
+          rl.close();
+          resolve(purchaseAmount);
+        }
+      });
+    });
+  }
+}
+
+export default InsertMoney;
