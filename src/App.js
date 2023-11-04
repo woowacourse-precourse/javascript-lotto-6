38455e460c905b42cce5649056b6ce0a4a06f93d
Jwirang <haha71119@gamil.com>
2023-11-04 14:58:32 +0900

    feat: 구입 금액 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    await this.getAmount();
+  }
+
+  async getAmount() {
+     const amount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+     this.validateAmount(amount);
+  }
+
+  validateAmount(amount) {
+    if (isNaN(amount) || amount < 1000 || amount % 1000 !== 0) {
+      throw new Error('[ERROR] 입력 값이 잘못 되었습니다.');
+    }
+    return amount;
+  }
 }
 
 export default App;
