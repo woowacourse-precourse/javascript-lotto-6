155af1e312988fd268079e41d68d6542371df122
양진성 <anfakt0606@naver.com>
2023-11-05 01:38:08 +0900

    [ feat ] 로또 구입금액 입력기능 추가 App.js 로또 구입금액 입력기능 추가 constants.js 생성 후 구입금앱 입력기능관련 상수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { Console } from "@woowacourse/mission-utils";
+import { 
+  USER_INPUT,
+  ERROR_MESSAGE,
+  PRICE_UNIT,
+  PRICE_ZERO
+} from "./constants.js";
+
 class App {
-  async play() {}
+  lottoPrice;
+
+  constructor() {
+    this.lottoPrice = 0;
+  }
+
+  async play() {
+    while(!this.lottoPrice){
+      await this.getLottoPrice();
+    }
+  }
+
+  async getLottoPrice() {
+    const input = await Console.readLineAsync(USER_INPUT.LOTTO_PRICE);
+    const lottoPrice = Number(input);
+  
+    try{
+      this.checkLottoPriceValidity(lottoPrice);
+      this.lottoPrice = lottoPrice;
+    }catch (error){
+      Console.print(error);
+    }
+  }
+
+  checkLottoPriceValidity(lottoPrice) {
+    if(isNaN(lottoPrice)) throw ERROR_MESSAGE.PRICE_NOT_STRING;
+    if(lottoPrice === PRICE_ZERO) throw ERROR_MESSAGE.PRICE_NOT_ZERO;
+    if(lottoPrice % PRICE_UNIT != PRICE_ZERO) throw ERROR_MESSAGE.PRICE_NOT_REST;
+  }
+
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const USER_INPUT = Object.freeze({
+  LOTTO_PRICE: '구입금액을 입력해 주세요. \n',
+  TARGET_NUMBER: '당첨 번호를 입력해 주세요. \n',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요. \n'
+})
+
+export const ERROR_MESSAGE = Object.freeze({
+  PRICE_NOT_INPUT: '[ERROR] 금액이 입력되지 않았습니다.',
+  PRICE_NOT_STRING: '[ERROR] 금액은 숫자가 입력되어야 합니다',
+  PRICE_NOT_ZERO: '[ERROR] 금액은 0원 일 수 없습니다.',
+  PRICE_NOT_REST: '[ERROR] 금액은 1,000원 단위여야 합니다.',
+})
+
+export const PRICE_UNIT = 1000;
+export const PRICE_ZERO = 0;
