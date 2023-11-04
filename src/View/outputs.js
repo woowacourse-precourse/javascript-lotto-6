4d07d8dc54591f60cb2c4c0c69cab4f818c7a9dc
8leamnk <silkviewrk@gmail.com>
2023-11-04 11:17:31 +0900

    feat(outputs, message): 로또 발행 개수를 출력하는 기능을 추가함
    추가 내용:
    - printNumberOfLotto: 로또 발행 개수를 출력하는 기능을 추가
    - message: 출력을 위한 메시지를 상수로 추가

--- src/View/outputs.js
+++ src/View/outputs.js
@@ -1,6 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
+import MESSAGE from '../constants/message.js';
 
 const outputs = {
+  printNumberOfLotto(number) {
+    Console.print(`${number}${MESSAGE.output.numberOfLotto}\n`);
+  },
   printRandoms(random) {
     const output = random.join('');
     Console.print(output);
--- src/constants/message.js
+++ src/constants/message.js
@@ -2,6 +2,9 @@ const MESSAGE = {
   input: {
     purchasePrice: '구입금액을 입력해 주세요.\n',
   },
+  output: {
+    numberOfLotto: '개를 구매했습니다.\n',
+  },
   error: {
     priceUnit: '[ERROR] 1,000원 단위의 숫자를 입력해주세요.',
     numericPrice: '[ERROR] 숫자만 입력해주세요(1,000원 단위).',
