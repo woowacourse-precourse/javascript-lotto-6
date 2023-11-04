6e84c18791b55616f0dc9adaa68b903c1cdfe250
이용성 <ukal5006@kakao.com>
2023-11-04 16:29:00 +0900

    refactor(PurchaseAmount): 검증 추가 및 수정
    0으로 시작하는 입력에 대한 검증과 에러 문구를 수정했습니다.

--- models/PurchaseAmount.js
+++ models/PurchaseAmount.js
@@ -3,20 +3,24 @@ class PurchaseAmount {
 
   constructor(amount) {
     this.#validate(amount);
-    this.#amount = amount;
+    this.#amount = Number(amount);
   }
 
   #validate(amount) {
-    if (!Number.isInteger(amount)) {
+    if (!Number.isInteger(Number(amount))) {
       throw new Error('[ERROR] 금액에 숫자 이외의 다른 문자는 올수 없습니다.');
     }
 
-    if (amount < 1000) {
-      throw new Error('[ERROR] 금액은 1000보다 커야 합니다.');
+    if (amount.length !== Number(amount).toString().length) {
+      throw new Error('[ERROR] 0으로 시작하는 금액은 입력할 수 없습니다.');
     }
 
-    if (amount % 1000 !== 0) {
-      throw new Error('[ERROR] 금액은 1000단위 여야 합니다.');
+    if (Number(amount) < 1000) {
+      throw new Error('[ERROR] 금액은 1000이상 이어야 합니다.');
+    }
+
+    if (Number(amount) % 1000 !== 0) {
+      throw new Error('[ERROR] 금액은 1000단위 이어야 합니다.');
     }
   }
 
