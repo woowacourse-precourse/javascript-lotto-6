22850d38d11cc4d6fbb35e8d2fe90c950978cdde
노태균 <taegyunn123@gmail.com>
2023-11-04 21:12:11 +0900

    test: LottoTest 숫자 형식 체크 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -4,23 +4,32 @@ import Lotto from "../src/model/Lotto.js";
 import { ErrorMessage } from "../src/utils/message.js";
 
 const getLottoNumbers = (start, end) => {
-  if (end - start < LOTTO.MAX_COUNT) {
+  if (end - start < LOTTO.NUMBER_COUNT) {
     throw new BasicError("로또 번호 만들기 불가능");
   }
   return [start, start + 1, start + 2, start + 3, start + 4, end];
 };
 
+const createLotto = (numbers) => () => new Lotto(numbers);
+
 describe("로또 클래스 테스트", () => {
-  test(`로또 번호의 개수가 ${LOTTO.MAX_COUNT}와 다르면 IncorrectLottoCountError`, () => {
+  test(`로또 번호의 개수가 ${LOTTO.NUMBER_COUNT}와 다르면 IncorrectLottoCountError`, () => {
     const numbers = [...getLottoNumbers(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER), 1];
 
-    expect(() => new Lotto(numbers)).toThrow(ErrorMessage.incorrectLottoCount());
+    expect(createLotto(numbers)).toThrow(ErrorMessage.incorrectLottoCount());
+  });
+
+  test(`로또 번호가 숫자 형식이 아니면 IncorrectFormatError`, () => {
+    const numbers = getLottoNumbers(LOTTO.MIN_NUMBER - 1, LOTTO.MAX_NUMBER);
+    numbers[0] = "0";
+
+    expect(createLotto(numbers)).toThrow(ErrorMessage.incorrectFormat());
   });
 
   test(`${LOTTO.MIN_NUMBER} ~ ${LOTTO.MAX_NUMBER} 사이의 숫자가 아니면 IncorrectLottoNumberError`, () => {
     const numbers = getLottoNumbers(LOTTO.MIN_NUMBER - 1, LOTTO.MAX_NUMBER);
 
-    expect(() => new Lotto(numbers)).toThrow(ErrorMessage.incorrectLottoNumber());
+    expect(createLotto(numbers)).toThrow(ErrorMessage.incorrectLottoNumber());
     expect(() => new Lotto([1, 2, 3, 4, 5, LOTTO.MAX_NUMBER + 1])).toThrow(
       ErrorMessage.incorrectLottoNumber()
     );
@@ -30,7 +39,7 @@ describe("로또 클래스 테스트", () => {
     const numbers = getLottoNumbers(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER);
     numbers[0] = numbers[1];
 
-    expect(() => new Lotto(numbers)).toThrow(ErrorMessage.duplicateNumbers());
+    expect(createLotto(numbers)).toThrow(ErrorMessage.duplicateNumbers());
   });
 
   test("getMatchCount : 정답 번호와 일치하는 개수 리턴", () => {
@@ -40,7 +49,7 @@ describe("로또 클래스 테스트", () => {
 
     const [matchLotto, matchBonus] = lotto.getMatchCount(numbers, bonusNumber);
 
-    expect(matchLotto).toBe(LOTTO.MAX_COUNT);
+    expect(matchLotto).toBe(LOTTO.NUMBER_COUNT);
     expect(matchBonus).toBeTruthy();
   });
 });
