757dd4b0f1daa7ac5a7d327b5ea28042e2282c81
Jeongeun Lee <leeju1009@gmail.com>
2023-11-05 00:16:28 +0900

    fix: 당첨 번호 유효성 검사 함수를 인식하지 못하는 문제 수정

--- src/Validation.js
+++ src/Validation.js
@@ -5,7 +5,7 @@ class Validation {
     this.isNumberInRange(numbers);
   }
 
-  static isNumber(numbers) {
+  isNumber(numbers) {
     numbers.forEach((number) => {
       if (Number.isNaN(number)) {
         throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
@@ -13,14 +13,14 @@ class Validation {
     });
   }
 
-  static isNonDuplicatedNumber(numbers) {
+  isNonDuplicatedNumber(numbers) {
     const nonDuplicatedNumbers = new Set(numbers);
     if (nonDuplicatedNumbers.size !== numbers.length) {
       throw new Error('[ERROR] 중복된 숫자는 입력할 수 없습니다.');
     }
   }
 
-  static isNumberInRange(numbers) {
+  isNumberInRange(numbers) {
     numbers.forEach((number) => {
       if (number < 1 || number > 45) {
         throw new Error('[ERROR] 1이상 45이하의 숫자만 입력할 수 있습니다.');
