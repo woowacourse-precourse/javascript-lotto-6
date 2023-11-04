0ede23ba95e5997532f0c1adb4a56adddc6873a9
MacBook Air <macbookair@MacBookui-MacBookAir.local>
2023-11-04 15:00:22 +0900

    Feat: 로또 구매 개수 출력 기능 구현 사용자로부터 구입금액 입력받고, 로또 구매 개수를 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+//import { MissionUtils } from "@woowacourse/mission-utils";
+import * as MissionUtils from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    let price = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    
+    MissionUtils.Console.print("\n" + price/1000 + "개를 구매했습니다.");
+  }
 }
 
 export default App;
