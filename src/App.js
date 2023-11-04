0bffd1d59c2e9b1b12988a398fffe6816d74d52b
Jules <dali1999@naver.com>
2023-11-05 01:40:09 +0900

    feat(play, getWinningDetails): 전체 로직을 실행하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import LottoController from '../controller/LottoController.js';
+import InputView from '../view/InputView.js';
+import OutputView from '../view/OutputView.js';
+import Validator from '../utils/Validator.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.controller = new LottoController();
+    this.input = new InputView();
+    this.output = new OutputView();
+  }
+
+  async play() {
+    await this.input.getPrice();
+    this.controller.printLotto(this.input.price);
+    await this.input.getWinNum();
+    await this.input.getBonusNum();
+    Validator.validateBonusNum(this.input.bonusNum, this.input.winNum);
+    this.getWinningDetails();
+  }
+
+  getWinningDetails() {
+    this.controller.checkWin(
+      this.input.price,
+      this.input.winNum,
+      this.input.bonusNum,
+    );
+    this.controller.getWinCountArr();
+    this.controller.printWinCount();
+    this.controller.calculatePrice();
+    this.controller.printReturnRate(this.input.price);
+  }
 }
+const app = new App();
+app.play();
 
 export default App;
