2782253e02039c19919b43a950dc85a04359c9fa
bmj0053 <bmj05378@gmail.com>
2023-11-04 21:04:52 +0900

    feat: 보유 금액 입력하는 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { MESSAGE, MESSAGE_INPUT } from "./constants/constant.js";
+
 class App {
-  async play() {}
+  async play() {
+    // 보유 금액 입력 - 유효한지 확인하는 코드 추가하기
+    const userMoney = await Console.readLineAsync(MESSAGE.START);
+    const count = (userMoney / 1000);
+    Console.print(MESSAGE_INPUT(count).COUNT);
+
+  }
 }
 
 export default App;
