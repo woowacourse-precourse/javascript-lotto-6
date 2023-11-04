dfa7c9056cd54164687bd53f4e858112461a77d8
seok-pyo <hongseokpyou@gmail.com>
2023-11-05 01:29:32 +0900

    feat(lotto): 중복 숫자 확인 기능 구현

--- src/Validation.js
+++ src/Validation.js
@@ -1,17 +1,20 @@
 const validate = {
-  money: (input) => {
+  money(input) {
     const reg = /[^0-9]/;
     if (reg.test(input)) throw new Error('[ERROR] 정확한 값이 아닙니다.');
     if (input % 1000 !== 0) throw new Error('[ERROR] 금액은 1000원 단위로 입력해주세요.');
     return input;
   },
-  winningNumbers: (input) => {
+
+  winningNumbers(input) {
     const inputArray = input.split(',');
     if (!inputArray) throw new Error('[ERROR] 정확한 값을 입력해주세요.');
     if (inputArray.length !== 6) throw new Error('[ERROR] 6개의 숫자를 입력해주세요');
+    if (inputArray.length !== new Set(inputArray).size) throw new Error('[ERROR] 서로 다른 숫자를 입력해주세요.');
     return inputArray.map((str) => Number(str)).sort((a, b) => a - b);
   },
-  bonusNumber: (input) => {
+
+  bonusNumber(input) {
     const reg = /[^0-9]/;
     if (reg.test(input)) throw new Error('[ERROR] 정확한 값이 아닙니다.');
     if (input > 45) throw new Error('[ERROR] 1에서 45사이에 숫자를 입력해주세요.');
