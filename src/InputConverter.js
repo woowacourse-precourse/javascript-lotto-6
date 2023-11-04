7cd2bf1c39b994b45c63090367311ffb34db93dc
ohgus <anakin1108@naver.com>
2023-11-04 19:35:48 +0900

    feat: 당첨 번호 배열,숫자타입으로 변환하는 기능 추가

--- src/InputConverter.js
+++ src/InputConverter.js
@@ -2,7 +2,11 @@ export class InputConverter {
   moneyConverter(money) {
     return Number(money);
   }
+
+  winningNumbersConverter(numbers) {
+    return numbers.split(",").map(Number);
+  }
 }
 
 const inputConverter = new InputConverter();
-console.log(inputConverter.moneyConverter("3000"));
+console.log(inputConverter.winningNumbersConverter("1,2,3,4,5,6"));
