f15147b09235ea97fe47e7da432ad282f8b9f5fe
ReturnReason <reasonz@daum.net>
2023-11-04 20:46:49 +0900

    feat(InputValidator): 입력 받은 금액이 유효한지 체크하는 validateMoney 메서드 추가

---
+++ src/validator/InputValidator.js
@@ -0,0 +1,28 @@
+import { GAME_RULE } from '../constants/gameRule.js';
+import { ERROR_MESSAGE } from '../constants/messages.js';
+
+class InputValidator {
+  /**
+   * @param {number} money
+   * @return {Error | undefined}
+   */
+  static validateMoney(money) {
+    if (Number.isNaN(money)) {
+      throw new Error(ERROR_MESSAGE.NOT_A_NUMBER);
+    }
+
+    if (money <= 0) {
+      throw new Error(ERROR_MESSAGE.NEGATIVE_VALUE);
+    }
+
+    if (money % GAME_RULE.MIN_AMOUNT_UNIT !== 0) {
+      throw new Error(ERROR_MESSAGE.NOT_BEING_DIVIDED);
+    }
+
+    if (money > GAME_RULE.MAX_AMOUNT_UNIT) {
+      throw new Error(ERROR_MESSAGE.OVER_THE_LIMIT);
+    }
+  }
+}
+
+export default InputValidator;
