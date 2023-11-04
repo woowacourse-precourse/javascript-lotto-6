d5b4c023c2a30143bd4c59ef22096a3cecc7bb83
gon <hss3522@gmail.com>
2023-11-04 10:22:57 +0900

    feat: Add feature to input winning numbers and bonus number

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { Console } from "@woowacourse/mission-utils";
 import LottoGenerator from "./LottoGenerator.js";
 
 const lottoTickets = new LottoGenerator();
 
 class App {
+  async getWinningNumbersFromUserInput() {
+    Console.print(
+      "당첨 번호 6개를 입력해 주세요. 번호는 쉼표(,)를 기준으로 구분합니다"
+    );
+    const winningNumbers = await Console.readLineAsync("");
+
+    return winningNumbers.split(",");
+  }
+
+  async getBonusNumbersFromUserInput() {
+    Console.print("보너스 번호를 입력해 주세요.");
+
+    const bonusNumber = await Console.readLineAsync("");
+
+    return parseInt(bonusNumber, 10);
+  }
+
   async play() {
-    lottoTickets.getLottoTickets();
+    await lottoTickets.getLottoTickets();
+
+    await this.getWinningNumbersFromUserInput();
+    await this.getBonusNumbersFromUserInput();
   }
 }
 
