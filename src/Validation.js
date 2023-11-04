f5b0cd1a3d9a1aeb1d1f1b87e70e87de1e5b0491
unknown <pih9134@naver.com>
2023-11-05 01:54:22 +0900

    refactor: Validation 클래스 리팩토링
    중복 코드 제거 및 함수 길이 단축

--- src/Validation.js
+++ src/Validation.js
@@ -1,34 +1,39 @@
 class Validation {
   static validatePurchaseAmount(purchaseAmount) {
-    const getPurchaseAmount = purchaseAmount.trim();
-    if (!getPurchaseAmount) {
-      throw new Error('[ERROR] 구입 금액을 입력해주세요.');
-    }
+    const cleanedPurchaseAmount = Validation.cleanInput(purchaseAmount);
 
-    if (!Number.isSafeInteger(Number(getPurchaseAmount))) {
-      throw new Error('[ERROR] 숫자를 입력해주세요.');
-    }
-    if (getPurchaseAmount % 1000 !== 0) {
-      throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
-    }
-    return getPurchaseAmount;
+    if (!cleanedPurchaseAmount) throw new Error('[ERROR] 구입 금액을 입력해주세요.');
+    if (!Validation.isSafeInteger(cleanedPurchaseAmount)) throw new Error('[ERROR] 숫자를 입력해주세요.');
+    if (cleanedPurchaseAmount % 1000 !== 0) throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
+
+    return cleanedPurchaseAmount;
   }
 
   static validateWinningNumbers(winningNumbers) {
-    const winningNumbersArray = winningNumbers.split(',').map((num) => Number(num.trim()));
-    if (!winningNumbersArray || winningNumbersArray.length !== 6) {
-      throw new Error('[ERROR] 6개의 당첨 번호를 입력해주세요.');
-    }
+    const cleanedNumbers = Validation.cleanInput(winningNumbers);
+    const numbers = cleanedNumbers.split(',').map((num) => Number(num.trim()));
 
-    const uniqueWinningNumbers = Array.from(new Set(winningNumbersArray));
+    if (numbers.length !== 6) throw new Error('[ERROR] 6개의 당첨 번호를 입력해주세요.');
     if (
-      uniqueWinningNumbers.length !== 6 ||
-      uniqueWinningNumbers.some((num) => !Number.isSafeInteger(num) || num < 1 || num > 45)
+      new Set(numbers).size !== 6 ||
+      numbers.some((num) => !Validation.isSafeInteger(num) || !Validation.isInRange(num))
     ) {
       throw new Error('[ERROR] 1부터 45 사이의 6개 중복되지 않는 자연수를 입력해주세요.');
     }
 
-    return uniqueWinningNumbers;
+    return numbers;
+  }
+
+  static cleanInput(input) {
+    return input.trim();
+  }
+
+  static isSafeInteger(value) {
+    return Number.isSafeInteger(Number(value));
+  }
+
+  static isInRange(value) {
+    return value >= 1 && value <= 45;
   }
 }
 
