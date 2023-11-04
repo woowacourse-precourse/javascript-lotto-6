877cf93f04cafc2d23a288ac2e1881fafc019b09
dyddks <dyddks0115@naver.com>
2023-11-04 23:05:02 +0900

    fix: 예외처리 수정

--- src/Model/Consumer.js
+++ src/Model/Consumer.js
@@ -12,12 +12,12 @@ class Consumer {
   }
 
   #validate(price) {
-    if (!Number.parseInt(price)) {
-      throw new Error('[ERROR]: 잘못된 입력입니다.');
+    if (isNaN(price)) {
+      throw new Error("[ERROR] 유효하지 않은 입력입니다.");
     }
 
     if (price % 1000 !== 0) {
-      throw new Error('[ERROR]: 1000원 단위로 입력해 주세요.');
+      throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
     }
   }
 
