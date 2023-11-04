e2736ffe68095eb71bc68cb9660792632ae6ade8
jeongminsang <101001956+jeongminsang@users.noreply.github.com>
2023-11-04 18:16:18 +0900

    feat: 입력 값에 따른 로또 생성 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const purchasePrice = await MissionUtils.Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+    const lottoCnt = this.repeatLottoNumber(purchasePrice);
+    MissionUtils.Console.print("\n" + lottoCnt + "개를 구매했습니다.");
+    const lottoArr = this.generateRandomNumber(lottoCnt);
+  }
+  generateRandomNumber(lottoCnt) {
+    const lottoArr = [];
+    for(let i = 0 ; i < lottoCnt ; i++) {
+      const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      MissionUtils.Console.print(randomNumbers);
+      lottoArr.push(randomNumbers);
+    }
+    return lottoArr;
+  }
+  repeatLottoNumber(purchasePrice) {
+    const lottoCnt = purchasePrice / 1000;
+    return lottoCnt;
+  }
 }
 
 export default App;
