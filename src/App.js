1e85518e91a9a15140cfbead7c1c687e46ea1613
jadewisemann <jadewisemann@gmail.com>
2023-11-05 08:44:22 +0900

    FEAT: Add Function, 'generateLottoNumberArray'

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import Lotto from './Lotto.js';   
 import UserInput from "./Input.js";
 import print from './Print.js';
+import model from './Model.js';
+
 class App {
   async play() {
-    // const userInputMoney = await new UserInput().getPurchasingPrice();
-    const maxLottoNumber = 0 // 최대 구매 가능량 계산 로직
-    print.maxLottoNumber(maxLottoNumber)
-    // 로또 숫자 랜덤 생성
-    // 전부다 프린트
-    const array = [
-      [1, 2, 3, 4],
-      [6, 5, 3, 2],
-      [1, 1, 2, 3],
-      
-    ]
-    print.lottoNumber(array)
+    const userInputMoney = await new UserInput().getPurchasingPrice(); // 사용자가 구입에 사용할 금액 입력 받기
+    const maxLottoNumber = model.calculateMaximumLotto(userInputMoney) // 최대 구매 가능량 계산 로직
+    print.maxLottoNumber(maxLottoNumber) // 최대 구매 가능량 출력
+    const lottoArray = model.generateLottoNumberArray(maxLottoNumber) // 로또 숫자 랜덤 생성
+    print.lottoNumber(lottoArray) // 전부다 프린트
     // 순회하면서 Lotto 생성
     // 당첨 번호 입력 받기
     // 보너스 번호 입력 받기
