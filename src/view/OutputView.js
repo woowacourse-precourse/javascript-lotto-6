2862254a0e673eccf6a2a8df5751187c6177902d
mieum12 <qpdlqltb1215@gmail.com>
2023-11-05 02:33:38 +0900

    fix: 로또 배열 출력 공백 제거

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,25 +2,44 @@ import {Console} from "@woowacourse/mission-utils";
 import {LottosDto} from "../domain/dto/LottosDto.js";
 
 export class OutputView {
-    /**
-     * @return {number}
-     * @param money
-     */
-    printAmount(money) {
-        const amount = money / 1000
-        Console.print(`\n${amount}개를 구매했습니다.`)
+
+
+    printFinalResult() {
     }
 
     /**
      *
-     * @param {LottosDto} LottosDto
+     * @param {LottosDto} lottosDto
+     * @return {void}
      */
-    printLottos(LottosDto) {
-        LottosDto.lottos.forEach(lottoDto => {
-            Console.print(lottoDto.number)
+    printLottosDto(lottosDto) {
+        this.#printLottsLenght(lottosDto.lottos.length) //배열의 길이로 몇개의 로또인지 구함
+        lottosDto.lottos.forEach(lottoDto => {
+            this.#printLottoDto(lottoDto)
         })
     }
 
-    printFinalResult() {
+    /**
+     *
+     * @param {LottoDto} lottoDto
+     * 앞뒤 공백 제거를 위한..
+     * @return {void}`
+     */
+    #printLottoDto(lottoDto) {
+        //[1,2,3,4,5,6] -> '1, 2, 3, 4, 5, 6'
+        const lottoNumberString = lottoDto.numbers.join(', ')
+        // -> '[1, 2, 3, 4, 5, 6]'
+        Console.print(`[${lottoNumberString}]`)
+    }
+
+
+    /**
+     *
+     * @param {number} length
+     * @return {void}
+     */
+
+    #printLottsLenght(length) {
+        Console.print(`\n${length}개를 구매했습니다.`)
     }
 }
