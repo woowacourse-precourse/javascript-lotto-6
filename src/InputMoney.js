f25b34a9024983d3e7439be3de41a1ac3384eddd
Seo San <dannysir@naver.com>
2023-11-04 18:07:32 +0900

    feat : 구입금액 입력 구입 금액 입력 화면, 천원 단위 예외 처리

--- src/InputMoney.js
+++ src/InputMoney.js
@@ -1,5 +1,12 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
 
 const INPUT_MONEY = async () => {
-    return await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
+    const MONEY = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
+    MONEY_VALIDATE(MONEY);
+    return MONEY;
 };
+
+const MONEY_VALIDATE = (MONEY) => {
+    if (MONEY % 1000 !== 0) throw new Error("[ERROR] 천원 단위만 입력해주세요");
+};
+export {INPUT_MONEY,MONEY_VALIDATE};
