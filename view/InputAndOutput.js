0e085ee3048cafb83bb8a8f3412d2e4fd6e269a3
leedbswo107 <ljyu1@naver.com>
2023-11-04 22:23:19 +0900

    fix: import 경로 및 오타 수정

--- view/InputAndOutput.js
+++ view/InputAndOutput.js
@@ -1,33 +1,31 @@
 import { Console } from "@woowacourse/mission-utils";
-import { MESSAGE } from "../src/common/OutputMessage";
+import { MESSAGE } from "../src/common/OutputMessage.js";
+import Validate from "../src/common/validate.js";
+import Lotto from "../src/Lotto.js";
 
 class InputAndOutput{
     
     constructor(){
-
+        this.validate = new Validate();
     }
 
     async inputPurchaseAmount(){
-        const PURCHASE_AMOUNT = await Console.print(`${MESSAGE.PURCHASE_NUMBER_INPUT}\n`);
-        
+        const PURCHASE_AMOUNT = await Console.readLineAsync(`${MESSAGE.PURCHASE_NUMBER_INPUT}\n`);   
+        const VALIDATE_PURCHASE_AMOUNT = this.validate.validatePrice(PURCHASE_AMOUNT);
+        return VALIDATE_PURCHASE_AMOUNT;
+    }
+    async inputWinNumber(){
+        const WIN_NUMBER = await Console.readLineAsync(`${MESSAGE.WIN_NUMBER_INPUT}\n`);
+        const SPLIT_WIN_NUMBER = WIN_NUMBER.split(',');
+
+        // TODO: 입력된 로또 번호의 유효성 검사를 진행
+        new Lotto(SPLIT_WIN_NUMBER);
+        return SPLIT_WIN_NUMBER;
+    }
+    async inputBonusNumber(){
+        const BONUS_NUMBER = await Console.readLineAsync(`${MESSAGE.BONUS_NUMBER_INPUT}\n`);
+        const VALIDATE_BONUS_NUMBER = this.validate.validateBonusNumber(BONUS_NUMBER);
+        return VALIDATE_BONUS_NUMBER;     
     }
 }
 export default InputAndOutput;
-
-// class Lotto {
-//     #numbers;
-  
-//     constructor(numbers) {
-//       this.#validate(numbers);
-//       this.#numbers = numbers;
-//     }
-  
-//     #validate(numbers) {
-//       if (numbers.length !== 6) throw new Error(ERROR_MESSAGE.NUMBER_COUNT);
-//       if (numbers.length !== new Set(numbers).length) throw new Error(ERROR_MESSAGE.NUMBER_DUPLICATE);
-//       if (typeof numbers !== number) throw new Error(ERROR_MESSAGE.NUMBER_TYPE);
-//     }
-//     // TODO: 추가 기능 구현
-//   }
-  
-//   export default Lotto;
