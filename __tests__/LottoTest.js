07d0039bf8723453ce6db51a9bce495f6f966b4e
Seo San <dannysir@naver.com>
2023-11-04 19:57:40 +0900

    test : 정답 배열 생성 테스트 코드 사이즈가 5인 배열에 1등부터 순서대로 당첨된 숫자가 들어있는 배열 생성하는 테스트 코드

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -19,7 +19,22 @@ describe("로또 클래스 테스트", () => {
   test("로또 정답과 입력을 비교하여 몇개 맞췄는지 출력", () => {
     const LOTTO = [1, 2, 3, 4, 5, 6];
     const ANSWER = [4, 5, 6, 7, 8, 9];
+    const RESULT = 5;
     const MY_ANSWER = new Lottory(LOTTO, ANSWER);
-    expect(MY_ANSWER.check(LOTTO)).toBe(3);
+    expect(MY_ANSWER.check(LOTTO)).toBe(RESULT);
+  });
+
+  test("결과를 배열 값으로 출력", () => {
+    const MY_LOTTOS = [
+      [1, 2, 3, 4, 5, 6],
+      [6, 7, 8, 9, 10, 11],
+      [2, 3, 4, 5, 6, 10]
+    ];
+    const ANSWER = [1, 2, 3, 4, 5, 6];
+    const BONUS = 10;
+    const ANSWER_ARR = [1, 1, 0, 0, 0];
+    const MY_ANSWER = new Lottory(MY_LOTTOS, ANSWER, BONUS);
+
+    expect(MY_ANSWER.getAnswer()).toStrictEqual(ANSWER_ARR);
   });
 });
