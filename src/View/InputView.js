870f46381e5d09ee87b4761d0e2c87fc09770f50
dooohun <ehgns0305@naver.com>
2023-11-04 20:12:36 +0900

    Feat : 로또 당첨 번호의 유효성 검사 추가

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -3,6 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 class InputView {
   static async requestPurchaseAmount() {
     const purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    this.validatePurchaseAmount(purchaseAmount);
     return purchaseAmount;
   }
 
@@ -20,7 +21,30 @@ class InputView {
 
   static async requestWinningNumbers() {
     const winningNumbers = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
-    return winningNumbers;
+    this.validateWinningNumbers(winningNumbers);
+    return winningNumbers.split(',').map(Number);
+  }
+
+  static validateWinningNumbers(winningNumbers) {
+    const numbers = winningNumbers.split(',');
+    const areAllNumbers = numbers.every(number => !isNaN(number));
+    if (!areAllNumbers) {
+      throw new Error('[ERROR] 당첨 번호는 숫자여야 합니다.');
+    }
+
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+    }
+
+    const isValidRangeofNumbers = numbers.map(Number).filter((number) => number >= 1 && number <= 45);
+    if (isValidRangeofNumbers.length !== numbers.length) {
+      throw new Error('[ERROR] 당첨 번호는 1부터 45 범위 내이어야 합니다.');
+    }
+    
+    const set = new Set(numbers);
+    if (numbers.length !== set.size) {
+      throw new Error('[ERROR] 중복된 숫자를 입력하면 안됩니다.');
+    }
   }
 
   static async requestBonusNumber() {
