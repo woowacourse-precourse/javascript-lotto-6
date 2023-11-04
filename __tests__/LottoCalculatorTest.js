fa9ef96e477072dc5ad773d0c2cee0271c0b5455
kimdw524 <kimdw524@naver.com>
2023-11-04 22:52:35 +0900

    test: 추첨 후 수익률 계산 테스트 코드 추가

--- __tests__/LottoCalculatorTest.js
+++ __tests__/LottoCalculatorTest.js
@@ -63,4 +63,26 @@ describe('LottoCalculator 클래스 테스트', () => {
 
     expect(lottoCalculator.getResult()).toEqual(result);
   });
+
+  test('추첨 후 수익률 계산', () => {
+    const numbers = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
+    const winningNumber = [1, 2, 3, 4, 5, 6];
+    const bonus = 7;
+    const lottoCalculator = new LottoCalculator();
+    lottoCalculator.setLottoes(numbers.map((number) => new Lotto(number)));
+    lottoCalculator.setWinningNumber(winningNumber, bonus);
+
+    expect(
+      LottoCalculator.getProfitWithResult(lottoCalculator.getResult()),
+    ).toEqual({ prize: 5000, profit: 62.5 });
+  });
 });
