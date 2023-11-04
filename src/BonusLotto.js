be4fbc27a5d26315a67d95f29c4d5f8846291343
simple0710 <thesimpleman@naver.com>
2023-11-04 18:53:31 +0900

    feat: 보너스 번호 검증 기능 추가
    - 입력을 받지 않은 경우
    - 문자가 있는 경우
    - 숫자의 범위를 벗어난 경우
    - 기존 로또에 포함된 번호를 뽑은 경우

---
+++ src/BonusLotto.js
@@ -0,0 +1,22 @@
+class BonusLotto {
+  #number
+
+  constructor(number, lotto) {
+    this.#validation(number, lotto);
+    this.#number = number;
+  }
+
+  #validation(number, lotto) {
+    if (number.length === 0) {
+      throw new Error("[ERROR] 입력을 받지 않았습니다.");
+    } else if (isNaN(number)) {
+      throw new Error("[ERROR] 숫자 안에 문자가 있습니다.");
+    } else if (number < 1 || number > 45) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+    } else if (lotto.includes(number)) {
+      throw new Error("[ERROR] 당첨 번호에 포함된 번호를 뽑을 수 없습니다.");
+    };
+  }
+}
+
+export default BonusLotto;
