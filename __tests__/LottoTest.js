cc4fbe1957aad99d8e24db1fb693717cb1ec9121
Rki0 <pky00823@gmail.com>
2023-11-04 17:02:59 +0900

    test: Lotto 클래스 예외 테스트 구체화
    예외를 발생시킬 경우 표시될 에러 문구를 확인하는 것으로 더 정확한 테스트를 진행한다.

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 import Lotto from "../src/Lotto.js";
+import { ERROR } from "../src/constants/message.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -18,13 +19,13 @@ describe("로또 클래스 테스트", () => {
   test("로또 번호에 숫자가 아닌 값이 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, NaN, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.INVALID_FORMAT);
   });
 
   test("로또 번호가 1 이상 45 이하의 값이 아니라면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 46]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.INVALID_RANGE);
   });
 
   test("getter를 통해 검증된 로또 번호를 얻는다.", () => {
