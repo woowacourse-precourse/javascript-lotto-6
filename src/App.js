8dc6a40fad83ac369fcc7e8fbb9b7c277b85f93d
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-11-04 21:30:10 +0900

    feat: 구입금액을 바탕으로 구입한 로또 갯수를 계산하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App {
     const price = await Console.readLineAsync('구입금액을 입력해 주세요.');
     const lottoGame = new LottoGame(price);
 
+    Console.print(`${lottoGame.count}개를 구매했습니다.`);
+   
   }
 
 }
--- src/LottoGame.js
+++ src/LottoGame.js
@@ -2,7 +2,13 @@
 class LottoGame {
   constructor(price) {
     this.price = price;
+    this.count = this.calculateLottoCount(price);
   }
+
+  calculateLottoCount(price) {
+    return price / 1000;
+  }
+  
 }
 
 export default LottoGame
