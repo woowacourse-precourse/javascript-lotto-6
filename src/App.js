09b26652ec3239cb089f77fde7857ccc4a0c1cf1
songhyunseop <gustjq99455352@gmail.com>
2023-11-04 20:42:23 +0900

    Feat: 입력된 구매금액에 따라 계산된 로또 횟수만큼 랜덤 번호 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const payAmount = await Console.readLineAsync('구입금액을 입력해주세요\n');
+
+    const purchaseQuantity = payAmount / 1000;
+
+    const lottoNumbers = Array.from({ length: purchaseQuantity }, () => {
+      const random = Random.pickUniqueNumbersInRange(1, 45, 6);
+      return random.sort((a, b) => a - b);
+    });
+  }
 }
 
 export default App;
