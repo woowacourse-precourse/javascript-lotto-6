182d94747b7984a3b4083ec178d841f62b3b5a53
JS Park <junseongpark.dev@gmail.com>
2023-11-04 20:24:14 +0900

    feat: 당첨 결과 출력 구현

--- src/App.js
+++ src/App.js
@@ -90,12 +90,59 @@ class App {
       if (Number(input) < 1 || Number(input) > 45) {
         throw new Error('[ERROR] 로또 번호는 1 ~ 45 사이여야 합니다.');
       }
-
       this.#bonus = Number(input);
+      this.printResult();
     } catch (error) {
       Console.print(error.message);
       this.#initBonusNumber();
     }
+  }
+
+  printResult() {
+    const result = {
+      6: 0,
+      '5 + 1': 0,
+      5: 0,
+      4: 0,
+      3: 0,
+    };
+
+    let totalReward = 0;
+
+    for (const lotto of this.#lottoEntries.values()) {
+      let match = 0;
+      const numbers = lotto.getNumbers();
+
+      for (const number of numbers) {
+        if (this.#winNumbers.includes(number)) {
+          match += 1;
+        }
+
+        if (match === 5 && numbers.includes(this.#bonus)) {
+          match = '5 + 1';
+        }
+      }
+      result[match] += 1;
+    }
+
+    totalReward +=
+      5000 * result[3] +
+      50000 * result[4] +
+      1500000 * result[5] +
+      30000000 * result['5 + 1'] +
+      2000000000 * result[6];
+
+    Console.print('\n당첨 통계\n---');
+    Console.print(`3개 일치 (5,000원) - ${result[3]}개`);
+    Console.print(`4개 일치 (50,000원) - ${result[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${result[5]}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result['5 + 1']}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${result[6]}개`);
+    Console.print(
+      `총 수익률은 ${((totalReward * 100) / this.#cash).toFixed(1)}%입니다.`
+    );
   }
 
   async play() {
