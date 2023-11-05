80d438e08dda140f886430144b62e51fa197f75b
Majesty-jun <hkwns01@gmail.com>
2023-11-06 00:41:48 +0900

    FEAT: 구매 금액에 따른 로또 갯수 구하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 import inputPrice from './input/inputPrice.js';
+import getAmountOfLottery from './lottery/getAmountOfLottery.js';
 
 class App {
   async play() {
     this.price = await inputPrice();
-    Console.print(this.price);
+    this.lotteryAmount = getAmountOfLottery(this.price);
+    Console.print(this.lotteryAmount);
   }
 }
 
