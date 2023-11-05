16ffe1d83f1f1a4b703e62d51053e2289d6fe5e5
rustandbone <dlehdgh14@naver.com>
2023-11-06 00:18:12 +0900

    문서: 기능 구현 및 예외 처리 목록 체크 기능 구현: App.js play() 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console } from '@woowacourse/mission-utils';
+import GuessLotto from './GuessLotto.js';
+import Lotto from './Lotto.js';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const guess = new GuessLotto();
+      await guess.buyLotto();
+
+      Console.print(`${guess.lottoPieces}개를 구매했습니다.`);
+
+      for (let i = 1; i <= guess.lottoPieces; i += 1) {
+        Console.print(guess.generateLottoNumber());
+      }
+      Console.print('');
+
+      const winningNumber = await guess.inputLottoNumber();
+
+      const lotto = new Lotto(winningNumber);
+
+      const winningBonus = await guess.inputBonusNumber();
+
+      lotto.checkWin(winningBonus, guess.lottoNumbers);
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
 }
 
 export default App;
