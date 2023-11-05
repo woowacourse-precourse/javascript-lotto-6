47b016b01317bbf056c89eacf01c1acba92ed035
pjc0179 <pjc0179@gmail.com>
2023-11-06 04:03:31 +0900

    refactor(constants):matches.js 변수 네이밍 변경

--- src/constants/matches.js
+++ src/constants/matches.js
@@ -1,24 +1,31 @@
-const MATCHES = Object.freeze({
-  threeMatches: {
+import CONFIG from './config.js';
+
+const MATCHES = {
+  threeMatch: {
     reward: 5000,
     message: '3개 일치 (5,000원)',
   },
-  fourMatches: {
+  fourMatch: {
     reward: 50000,
     message: '4개 일치 (50,000원)',
   },
-  fiveMatches: {
+  fiveMatch: {
     reward: 1500000,
     message: '5개 일치 (1,500,000원)',
   },
-  fiveAndBonusMatches: {
+  bonusMatch: {
     reward: 30000000,
-    message: '5개 일치, 보너스 볼 일치 (30,000,000원)',
+    message: `${CONFIG.bonusNumberMatch}개 일치, 보너스 볼 일치 (30,000,000원)`,
   },
-  sixMatches: {
+  sixMatch: {
     reward: 2000000000,
     message: '6개 일치 (2,000,000,000원)',
   },
-});
+};
+
+MATCHES[CONFIG.bonusMatchKey] = {
+  reward: 30000000,
+  message: `${CONFIG.bonusNumberMatch}개 일치, 보너스 볼 일치 (30,000,000원)`,
+};
 
 export default MATCHES;
