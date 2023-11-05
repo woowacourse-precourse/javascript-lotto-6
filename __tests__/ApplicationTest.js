79f84f7ef370d87f0fb8aaeade23092d2aa019b5
deokisys <sjb378@me.com>
2023-11-05 21:14:14 +0900

    [feat] 로또 구매 금액 입력
    1000단위로 입력, 잘못 입력하면 다시 입력하게
    랜덤 숫자를 지정, 출력까지 진행

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,6 +47,44 @@ describe("로또 테스트", () => {
     jest.restoreAllMocks();
   })
 
+  test("구매 테스트", async () => {
+    // given
+    const logSpy = getLogSpy();
+
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+    mockQuestions(["8000"]);
+
+    // when
+    const app = new App();
+    await app.buy();
+
+    // then
+    const logs = [
+      "8개를 구매했습니다.",
+      "[8, 21, 23, 41, 42, 43]",
+      "[3, 5, 11, 16, 32, 38]",
+      "[7, 11, 16, 35, 36, 44]",
+      "[1, 8, 11, 31, 41, 42]",
+      "[13, 14, 16, 38, 42, 45]",
+      "[7, 11, 30, 40, 42, 43]",
+      "[2, 13, 22, 32, 38, 45]",
+      "[1, 3, 5, 14, 22, 45]",
+    ];
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
   test("기능 테스트", async () => {
     // given
     const logSpy = getLogSpy();
--- src/App.js
+++ src/App.js
@@ -1,5 +1,86 @@
+import { MissionUtils,Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  buyLottos=[];
+
+  async play() {
+    await this.buy();
+
+    await this.bonusInput();
+    this.result();
+  }
+
+  async buy(){
+    let money;
+
+    while(true){
+      money = await Console.readLineAsync('구입금액을 입력해 주세요.');
+      if(this.isNum(money)){
+        break;
+      }
+    }
+
+
+    if(Number(money)%1000!==0) return new Error("[ERROR] 1000 단위에 맞춰주세요.");
+
+    this.makeNumbers(Number(money)/1000);
+    this.printBuyLottos();
+  }
+  async bonusInput(){
+    const bonusNumber = await Console.readLineAsync('보너스 번호를 입력해 주세요.');
+
+  }
+
+  /**
+   * 숫자인지 판별
+   * @param {*} number 
+   * @returns 
+   */
+  isNum(number){
+    const re = new RegExp("^[0-9]+$");
+    if(!re.test(number)){
+      Console.print("[ERROR] 숫자 형식을 맞춰주세요.");
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * number만큼 로또번호를 생성
+   * @param {*} number 
+   */
+  makeNumbers(number){
+    for(let i=0;i<number;i++){
+      this.buyLottos.push(this.makeNumber());
+    }
+  }
+
+  /**
+   * 6자리의 1~45까지 숫자를 중복없이 생성
+   */
+  makeNumber(){
+    //1~45까지 겹치지 않는 숫자 6개 반환
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45,6);;
+  }
+
+  /**
+   * 구매한 로또들 출력
+   */
+  printBuyLottos(){
+    Console.print(`${this.buyLottos.length}개를 구매했습니다.`);
+    this.buyLottos.forEach(lotto => {
+      Console.print(`[${lotto.join(', ')}]`);
+    });
+  }
+
+  result(){
+    Console.print("3개 일치 (5,000원) - 1개");
+    Console.print("4개 일치 (50,000원) - 0개");
+    Console.print("5개 일치 (1,500,000원) - 0개");
+    Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개");
+    Console.print("6개 일치 (2,000,000,000원) - 0개");
+    Console.print("총 수익률은 62.5%입니다.");
+  }
 }
 
 export default App;
