f232624cce51f7af0ef737549648782215699a5c
hamo-o <yheel369@gmail.com>
2023-11-06 00:53:02 +0900

    feat: 에러 메시지 출력 및 입력 다시 받기

--- src/Input.js
+++ src/Input.js
@@ -3,20 +3,37 @@ import Validate from './Validate.js';
 
 const Input = {
   async getLottoPrice() {
-    const price = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
-    Validate.checkPurchaseAmount(price);
-    return +price;
+    try {
+      const price = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+      Validate.checkPurchaseAmount(price);
+      return +price;
+    } catch (error) {
+      Console.print(error.message);
+      return this.getLottoPrice();
+    }
   },
   async getLottoNumber() {
-    const numbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
-    Validate.checkDuplicateNumber(numbers.split(','));
-    return numbers.split(',').map((number) => Number(number));
+    try {
+      const numbers = await Console.readLineAsync(
+        '당첨 번호를 입력해 주세요.\n'
+      );
+      Validate.checkDuplicateNumber(numbers.split(','));
+      return numbers.split(',').map((number) => Number(number));
+    } catch (error) {
+      Console.print(error.message);
+      return this.getLottoNumber();
+    }
   },
   async getLottoBonusNumber() {
-    const number = await Console.readLineAsync(
-      '보너스 번호를 입력해 주세요.\n'
-    );
-    return Number(number);
+    try {
+      const number = await Console.readLineAsync(
+        '보너스 번호를 입력해 주세요.\n'
+      );
+      return Number(number);
+    } catch (error) {
+      Console.print(error.message);
+      return this.getLottoBonusNumber();
+    }
   },
 };
 
