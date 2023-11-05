414558c61bcfc7ab7cf5a899dccbff703dd58609
Boeun <boeun0904@naver.com>
2023-11-06 01:58:43 +0900

    fix(UserInput): 에러 발생시키도록 수정

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,44 +1,36 @@
 import UserInput from '../src/UserInput.js';
-import { ERROR } from '../src/constants.js';
 
 describe('유저 입력 테스트', () => {
   describe('로또 구입 금액 입력 테스트', () => {
-    test('빈 값을 입력할 경우 에러를 반환한다.', () => {
-      expect(UserInput.getPurchaseAmountValidation('')).toBe(ERROR.BLANK_INPUT);
+    test('빈 값을 입력할 경우 예외를 발생시킨다.', () => {
+      expect(() => UserInput.checkPurchaseAmountValidation('')).toThrow(
+        '[ERROR]',
+      );
     });
 
     test.each(['abc', 'ㄱㄴㄷ'])(
-      '숫자가 아닌 값을 입력할 경우 에러를 반환한다.',
+      '숫자가 아닌 값을 입력할 경우 예외를 발생시킨다.',
       (input) => {
-        expect(UserInput.getPurchaseAmountValidation(input)).toBe(
-          ERROR.NOT_A_NUMBER,
+        expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow(
+          '[ERROR]',
         );
       },
     );
 
     test.each(['-1000', '0'])(
-      '0 이하의 숫자를 입력할 경우 에러를 반환한다.',
+      '0 이하의 숫자를 입력할 경우 예외를 발생시킨다.',
       (input) => {
-        expect(UserInput.getPurchaseAmountValidation(input)).toBe(
-          ERROR.NOT_A_NATURAL_NUMBER,
+        expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow(
+          '[ERROR]',
         );
       },
     );
 
     test.each(['1', '100', '1234'])(
-      '1000으로 나누어떨어지지 않는 숫자를 입력할 경우 에러를 반환한다.',
+      '1000으로 나누어떨어지지 않는 숫자를 입력할 경우 예외를 발생시킨다.',
       (input) => {
-        expect(UserInput.getPurchaseAmountValidation(input)).toBe(
-          ERROR.NOT_DIVIDED_BY_THOUSAND,
-        );
-      },
-    );
-
-    test.each(['1000', '10000'])(
-      '올바른 값을 입력할 경우 VALID를 반환한다.',
-      (input) => {
-        expect(UserInput.getPurchaseAmountValidation(input)).toBe(
-          UserInput.VALID,
+        expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow(
+          '[ERROR]',
         );
       },
     );
--- src/UserInput.js
+++ src/UserInput.js
@@ -1,29 +1,29 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE, ERROR } from './constants.js';
+import { InputError } from './utils/error.js';
 
 export default class UserInput {
-  static VALID = 'valid';
-
   static async getPurchaseAmount() {
     while (true) {
       const input = await Console.readLineAsync(MESSAGE.INPUT_PURCHASE_AMOUNT);
-      const result = this.getPurchaseAmountValidation(input);
-
-      if (result === this.VALID) return Number(input);
-      this.printError(result);
+      try {
+        this.checkPurchaseAmountValidation(input);
+        return Number(input);
+      } catch (exception) {
+        this.printError(exception.message);
+      }
     }
   }
 
   static printError(error) {
-    Console.print(ERROR.getError(error));
+    Console.print(error);
   }
 
-  static getPurchaseAmountValidation(input) {
-    if (input.length === 0) return ERROR.BLANK_INPUT;
+  static checkPurchaseAmountValidation(input) {
+    if (input.length === 0) throw new InputError(ERROR.BLANK_INPUT);
     const num = Number(input);
-    if (Number.isNaN(num)) return ERROR.NOT_A_NUMBER;
-    if (num <= 0) return ERROR.NOT_A_NATURAL_NUMBER;
-    if (num % 1000 !== 0) return ERROR.NOT_DIVIDED_BY_THOUSAND;
-    return this.VALID;
+    if (Number.isNaN(num)) throw new InputError(ERROR.NOT_A_NUMBER);
+    if (num <= 0) throw new InputError(ERROR.NOT_A_NATURAL_NUMBER);
+    if (num % 1000 !== 0) throw new InputError(ERROR.NOT_DIVIDED_BY_THOUSAND);
   }
 }
