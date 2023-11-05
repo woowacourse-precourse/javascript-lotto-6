473c8245a06fbcd4340bb528e9a767b9eb7a43f4
SSY1203 <tlstjsdud566@naver.com>
2023-11-05 23:36:17 +0900

    [feat] 로또 구입 금액에 대한 로직 복구

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,48 @@
-const InputView = {};
+import {
+  Console,
+  ERROR_PURCHASE_AMOUNT_PRICE,
+  ERROR_PURCHASE_AMOUNT_STRING,
+  ERROR_PURCHASE_AMOUNT_UNIT,
+  PLZ_PURCHASE_AMOUNT,
+  PLZ_WINNING_NUMBERS,
+  PRICE_UNIT,
+} from './Constant';
+
+const InputView = {
+  async readPurchaseAmount() {
+    while (true) {
+      try {
+        const answer = await Console.readLineAsync(PLZ_PURCHASE_AMOUNT);
+        this.priceNumberValidator(answer);
+        this.priceCheckValidator(answer);
+        this.priceUnitValidator(answer);
+        return answer;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  },
+
+  priceNumberValidator(answer) {
+    const purchaseAmount = Number(answer);
+    if (isNaN(purchaseAmount)) {
+      throw new Error(ERROR_PURCHASE_AMOUNT_STRING);
+    }
+  },
+
+  priceUnitValidator(answer) {
+    const purchaseAmount = Number(answer);
+    if (purchaseAmount % PRICE_UNIT !== 0) {
+      throw new Error(ERROR_PURCHASE_AMOUNT_UNIT);
+    }
+  },
+
+  priceCheckValidator(answer) {
+    const purchaseAmount = Number(answer);
+    if (purchaseAmount < PRICE_UNIT || purchaseAmount === 0) {
+      throw new Error(ERROR_PURCHASE_AMOUNT_PRICE);
+    }
+  },
+};
 
 export default InputView;
--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,7 +1,11 @@
+import InputView from './InputView';
+
 class LottoGame {
   #purchaseAmount;
 
-  async gameStart() {}
+  async gameStart() {
+    this.#purchaseAmount = InputView.readPurchaseAmount();
+  }
 }
 
 export default LottoGame;
