376ca12b9859b79d4464899fe5d4ae643d3f0803
김진호 <rlawlsgh1227@gmail.com>
2023-11-05 16:04:32 +0900

    test: 당첨번호 입력에서 숫자가 아닌 입력이 들어왔을 때 에러를 검증

--- __tests__/controller/LottoGame.test.js
+++ __tests__/controller/LottoGame.test.js
@@ -53,4 +53,19 @@ describe('로또 게임 컨트롤 클래스 테스트.', () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
     }
   });
+
+  test('당첨번호 입력에서 숫자가 아닌 입력이 들어오면 에러를 일으킵니다.', async () => {
+    // given
+    const invalidInput = '1,2,3,4,5,공습경보';
+    const validInput = '1,2,3,4,5,6';
+    const logSpy = getLogSpy();
+
+    mockQuestions([invalidInput, validInput]);
+
+    // when & then
+    const lottoGame = new LottoGame();
+    await lottoGame.inputWinningNumbers();
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
+  });
 });
