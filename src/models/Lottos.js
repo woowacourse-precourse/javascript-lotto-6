6122b5a579a75931efcaf97c22ba0ea5a021f185
Ryong-E <think8867@gmail.com>
2023-11-05 15:36:47 +0900

    feature: 당첨 결과와 수익률을 반환하는 기능 추가
    당첨번호에 구입한 로또 번호가 존재하면 +1
    보너스번호가 구입한 로또 번호에 존재하면 +0.5
    카운트한 숫자를 등수로 변환하여 반환합니다
    
    구입 금액과 총당첨금을 계산하여 수익률을 반환합니다

--- src/models/Lottos.js
+++ src/models/Lottos.js
@@ -1,5 +1,12 @@
 import Lotto from '../Lotto.js';
-import { LOTTO_SETTING } from '../constant.js';
+import {
+  FIVE_RANK,
+  FOUR_RANK,
+  LOTTO_SETTING,
+  ONE_RANK,
+  THREE_RANK,
+  TWO_RANK,
+} from '../constant.js';
 import Util from '../utils/Util.js';
 
 class Lottos {
@@ -7,9 +14,18 @@ class Lottos {
 
   #lottos;
 
+  #rankTemplate;
+
   constructor(purchase) {
     this.#lottos = [];
     this.#purchase = purchase;
+    this.#rankTemplate = {
+      [FIVE_RANK]: 0,
+      [FOUR_RANK]: 0,
+      [THREE_RANK]: 0,
+      [TWO_RANK]: 0,
+      [ONE_RANK]: 0,
+    };
   }
 
   setPurchase(purchase) {
@@ -34,6 +50,67 @@ class Lottos {
     }
 
     return this.#lottos.map((lotto) => lotto.getLottoNumbers());
+  }
+
+  getWinningResult(winningNumber, bonusNumber) {
+    const results = this.#checkLottos(winningNumber, bonusNumber);
+    const rate = this.#calulateRate(results);
+
+    return { results, rate };
+  }
+
+  #calulateRate(result) {
+    const sum = Lottos.#sumPrize(result);
+    return ((sum / this.#purchase) * 100).toFixed(1);
+  }
+
+  static #sumPrize(result) {
+    let sum = 0;
+    [...Object.entries(result)].forEach(([rank, count]) => {
+      sum += LOTTO_SETTING.prize[rank] * count;
+    });
+
+    return sum;
+  }
+
+  #checkLottos(winningNumber, bonusNumber) {
+    return this.#lottos.reduce((accu, currentLotto) => {
+      const result = { ...accu };
+      let count = 0;
+      count += Lottos.#sameNumberCount(winningNumber, currentLotto.getLottoNumbers());
+      count += Lottos.#haveBonusNumber(currentLotto.getLottoNumbers(), bonusNumber);
+      const key = Lottos.#transNumberToRank(count);
+
+      if (key) result[key] += 1;
+
+      return result;
+    }, this.#rankTemplate);
+  }
+
+  static #transNumberToRank(number) {
+    if (number === 3) return FIVE_RANK;
+    if (number === 4) return FOUR_RANK;
+    if (number === 5) return THREE_RANK;
+    if (number === 5.5) return TWO_RANK;
+    if (number === 6) return ONE_RANK;
+    return false;
+  }
+
+  static #sameNumberCount(referenceArray, findArray) {
+    let count = 0;
+    referenceArray.forEach((number) => {
+      if (findArray.includes(number)) count += 1;
+    });
+
+    return count;
+  }
+
+  static #haveBonusNumber(referenceArray, bonusNumber) {
+    if (referenceArray.includes(bonusNumber)) {
+      return 0.5;
+    }
+
+    return 0;
   }
 }
 
