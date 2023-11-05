78b63b027b71063b84005b229a308c44f129843b
Ryong-E <think8867@gmail.com>
2023-11-05 13:05:31 +0900

    feature: 구입 금액만큼 로또를 생성하는 기능 추가

--- src/models/Lottos.js
+++ src/models/Lottos.js
@@ -1,3 +1,7 @@
+import Lotto from '../Lotto.js';
+import { LOTTO_SETTING } from '../constant.js';
+import Util from '../utils/Util.js';
+
 class Lottos {
   #purchase;
 
@@ -10,6 +14,18 @@ class Lottos {
 
   setPurchase(purchase) {
     this.#purchase = purchase;
+  }
+
+  generateLotto() {
+    const amount = this.#purchase / LOTTO_SETTING.unit;
+    const newLottos = [];
+    Array.from({ length: amount }).forEach(() => newLottos.push(Lottos.#makeLotto()));
+    this.#lottos = newLottos;
+  }
+
+  static #makeLotto() {
+    const randomNumber = Util.getLottoNumberList();
+    return new Lotto(randomNumber);
   }
 }
 
