cc2cdc309772486a9da8b224934edfe5bd3e61a3
BangDori <qudwns8865@naver.com>
2023-11-05 15:17:12 +0900

    feat: 구매금액 만큼의 로또 발급 기능 추가

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,3 +1,9 @@
+import { Random } from '@woowacourse/mission-utils';
+
+import { LOTTO } from '../constants/setting.js';
+import Lotto from '../Lotto.js';
+import OutputView from '../views/Output.js';
+
 class LottoController {
   #money;
   #lottos;
@@ -17,6 +23,23 @@ class LottoController {
       match5Bonus: { count: 0, prize: 30000000 },
       match6: { count: 0, prize: 2000000000 },
     };
+  }
+
+  purchaseLotto() {
+    const lottoCount = this.#money / LOTTO.price;
+    OutputView.printPurchaseMessage(lottoCount);
+
+    for (let i = 0; i < lottoCount; i += 1) {
+      const lotto = this.issueLotto();
+      this.#lottos.push(lotto);
+    }
+  }
+
+  issueLotto() {
+    const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    const lotto = new Lotto(lottoNumbers);
+
+    return lotto;
   }
 }
 
--- src/views/Output.js
+++ src/views/Output.js
@@ -4,6 +4,10 @@ class OutputView {
   static printErrorMessage(error) {
     Console.print(`${error}\n`);
   }
+
+  static printPurchaseMessage(lottoCount) {
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+  }
 }
 
 export default OutputView;
