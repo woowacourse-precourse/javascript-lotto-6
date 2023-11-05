3666002fa17f2b0c276a60d1472456f989331781
badahertz52 <badahertz52@gmail.com>
2023-11-05 14:36:12 +0900

    feat,test :User 클래스 추가  및 관련 테스트 코드 추가
    - User :사용자의 지불 금액을 읽어오고, 발행된 로또들을 저장하고 출력함

---
+++ __tests__/UserTest.js
@@ -0,0 +1,53 @@
+import Lotto from '../src/Lotto';
+import User from '../src/User';
+import { MESSAGE } from '../src/constant';
+import { getLogSpy, mockQuestions } from '../testUtils';
+
+describe('User 클래스 테스트', () => {
+  test('로또 구매 금액 읽고  구매 금액을 숫자 타입으로 저장하기', () => {
+    const questions = ['1000', ' 1000 ', '1000원', '$1000', '천원'];
+    const expectedOutput = [1000, 1000, NaN, NaN, NaN];
+
+    questions.forEach(async (v, i) => {
+      mockQuestions([v]);
+
+      const user = new User();
+      const money = await user.pay();
+
+      expect(user.getReceipt().paymentAmount).toEqual(expectedOutput[i]);
+    });
+  });
+
+  test('구매한 로또들을 저장하고 출력한다.', () => {
+    const lottosNumbers = [
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+    ];
+    const lottos = lottosNumbers.map((v) => new Lotto(v));
+
+    const logs = [
+      `${lottos.length}${MESSAGE.numberOfTickets}`,
+      JSON.stringify(lottosNumbers[0]),
+      JSON.stringify(lottosNumbers[1]),
+    ];
+
+    const logSpy = getLogSpy();
+
+    const user = new User();
+    user.getLottos(lottos);
+
+    const userLottos = user.getReceipt().lottos;
+
+    //출력
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+
+    //발행된 로또 저장
+    userLottos.forEach((v, i) => {
+      const isEqual =
+        v.getLottoNumbers().join(',') === lottos[i].getLottoNumbers().join(',');
+      expect(isEqual).toBeTruthy();
+    });
+  });
+});
---
+++ src/User.js
@@ -0,0 +1,29 @@
+import Lotto from './Lotto';
+import { printPurchasedLottos, readPayment } from './utils';
+
+class User {
+  #paymentAmount = 0;
+  #lottos;
+
+  async pay() {
+    const money = await readPayment();
+    this.#paymentAmount = money;
+  }
+  /**
+   *
+   * @param {Lotto[]} lottos
+   */
+  getLottos(lottos) {
+    this.#lottos = lottos;
+    printPurchasedLottos(lottos);
+  }
+
+  getReceipt() {
+    return {
+      paymentAmount: this.#paymentAmount,
+      lottos: this.#lottos,
+    };
+  }
+}
+
+export default User;
