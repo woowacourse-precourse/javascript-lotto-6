14b4986e607f6bbeaf5a35b35444b956ea70d377
nijuy <youjin6325@naver.com>
2023-11-05 23:19:10 +0900

    test(payment): 로또 구입 금액 입력 테스트 수정
    - Validate 클래스를 이용한 코드로 수정

--- __tests__/PaymentTest.js
+++ __tests__/PaymentTest.js
@@ -1,21 +1,21 @@
-import App from '../src/App';
 import ERROR_MESSAGE from '../src/ErrorMessage';
+import Validate from '../src/Validate';
 
-describe('App - validatePayment() 테스트', () => {
+describe('Validate 클래스 테스트', () => {
   test.each(['abc', '!@#'])(
-    'Number로 변환할 수 없는 값을 입력하면 예외가 발생한다.',
+    'Payment: Number로 변환할 수 없는 값을 입력하면 예외가 발생한다.',
     (input) => {
       expect(() => {
-        App.validatePayment(input);
+        Validate.validatePayment(input);
       }).toThrow(ERROR_MESSAGE.nonNumeric);
     },
   );
 
   test.each(['-1', '0', '1.234'])(
-    '1,000원으로 나눠떨어지는 양수가 아니면 예외가 발생한다.',
+    'Payment: 1,000원으로 나눠떨어지는 양수가 아니면 예외가 발생한다.',
     (input) => {
       expect(() => {
-        App.validatePayment(input);
+        Validate.validatePayment(input);
       }).toThrow(ERROR_MESSAGE.invalidAmount);
     },
   );
