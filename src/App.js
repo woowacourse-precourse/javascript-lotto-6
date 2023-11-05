1eea9d99f70df782d32469b51608bf156eea2c3e
swJaNG12 <cbdjej38483@gmail.com>
2023-11-06 07:18:57 +0900

    feat(App) : use View and Lotto instances in play method
    app class create instances of the View and Lotto classes
    and use them in the play method.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { View } from './LottoView.js';
+import Lotto from './Lotto.js';
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.view = new View();
+  }
+  async play() {
+    const lottoAmount = await this.view.getLottoPerchaseAmount();
+    let model = new Lotto(lottoAmount);
+
+    const isValidateLottoAmount = model.validateLottoAmount();
+  }
 }
 
 export default App;
