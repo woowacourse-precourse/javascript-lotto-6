2135e36cf279267f2998617d0ca8c72baef1e445
chiman <rlaclghks123@naver.com>
2023-11-05 15:37:36 +0900

    refactor: errorHandler 수정

--- src/util/errorHandler.js
+++ src/util/errorHandler.js
@@ -1,6 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const errorHandler = async (input, validation) => {
+const errorHandler = async (callback, validator, arg) => {
+  while (true) {
+    const input = await callback();
+    let isValid = null;
+
+    if (arg) isValid = await checkValidate({ bonusNumber: input, winningNumbers: arg }, validator);
+    if (!arg) isValid = await checkValidate(input, validator);
+    if (!isValid) continue;
+
+    return input;
+  }
+};
+
+const checkValidate = async (input, validation) => {
   try {
     validation(input);
     return true;
