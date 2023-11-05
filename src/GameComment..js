3d6694a9047e746c97b43db44ec352be03568f96
yjh1008 <exesea12@gmail.com>
2023-11-05 21:49:39 +0900

    feat : 당첨 내역을 출력하는 문자열 생성.

--- src/GameComment..js
+++ src/GameComment..js
@@ -10,8 +10,17 @@ const PRINT_PURCHASE_LOTTO = (lottoContainer) => {
   return str;
 }
 
+const WINNING_OUTCOME = (arr) => {
+  return `3개 일치 (5,000원) - ${arr[0]}개\n
+  4개 일치 (50,000원) - ${arr[1]}개\n
+  5개 일치 (1,500,000원) - ${arr[2]}개\n
+  5개 일치, 보너스 볼 일치 (30,000,000원) - ${arr[3]}개\n
+  6개 일치 (2,000,000,000원) - ${arr[4]}개\n`
+}
+
 export {
   BUY_LOTTO, 
   LOTTO_COUNT,
-  PRINT_PURCHASE_LOTTO
+  PRINT_PURCHASE_LOTTO,
+  WINNING_OUTCOME
 }
