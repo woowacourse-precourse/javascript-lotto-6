237c220a94a8c0b5153d81b8f099808b5e3e7e68
thumbthing <ilscc0902@gmail.com>
2023-11-06 06:29:47 +0900

    feat: 수익률 반환 기능 구현

---
+++ src/util/yield/getRateOfReturn.js
@@ -0,0 +1,22 @@
+const LOTTO_DATA = new Map([
+  [0, 2000000000],
+  [1, 30000000],
+  [2, 1500000],
+  [3, 50000],
+  [4, 5000],
+]);
+
+export default function getRateOfReturn(resultMap, purchaseAmount) {
+  let profits = 0;
+  const keys = [...resultMap.keys()];
+  const values = [...resultMap.values()];
+
+  values.forEach((elements, index) => {
+    if (elements !== 0) profits += LOTTO_DATA.get(keys[index]);
+  });
+
+  const rateOfReturn = ((profits - purchaseAmount) / purchaseAmount) * 100;
+  const roundedRate = Math.round(rateOfReturn * 100) / 100;
+  const result = roundedRate.toFixed(1);
+  return result;
+}
