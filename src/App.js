03da052b4aa1d7adf9451c0d5bc004602927b27c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-05 13:41:13 +0900

    ✨feat($App): 로또 구입 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { GUIDE_MESSAGE } from './constants/constants'
+import LottoTickets from './LottoTickets';
+import Input from './view/Input';
+import Output from './view/Output';
+
+// @note - 금액 저장, 당첨 번호 저장, 보너스 번호 저장, 수익률 출력
+
 class App {
-  async play() {}
+  #money
+
+  constructor() {
+    this.#money = 0;
+    this.input = new Input();
+    this.output = new Output();
+  }
+
+  async play() {
+    await this.requestPurchaseAmounts();
+  }
+
+  async requestPurchaseAmounts() {
+    const INPUT = await this.input.getValue(GUIDE_MESSAGE.insertPurchaseAmount)
+
+    this.lottos = new LottoTickets(String(INPUT));
+    this.#money = this.lottos.returnMoney()
+    
+    this.output.printMoney(this.#money);
+  }
 }
 
 export default App;
