9aaa484d759b67d1d57fb974b2eafcc8afd013c8
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-05 23:22:07 +0900

    Refactor: 당첨 번호 입력 변수 재정의 및 LottoResultChecker 생성자 함수 정의

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 import LottoManagement from "./LottoManagement.js";
+import LottoResultChecker from "./LottoResultChecker.js";
+
 const lottoPrice = 1000;
 class App {
   constructor() {
     this.lottoManagement = new LottoManagement();
+    this.lottoResultChecker = new LottoResultChecker();
   }
   async play() {
     const purchasePrice = await this.inputPurchasePrice();
     const lottoCount = Number(purchasePrice / lottoPrice);
+
     const generatedLottoNumbers =
       this.lottoManagement.getLottoArray(lottoCount);
+
     Console.print(`${lottoCount}개를 구매했습니다.`);
+
+    generatedLottoNumbers.forEach((lotto) => {
+      lotto.print();
+    });
+
+    const winningNumbers = this.lottoResultChecker.convertToArr(
+      this.lottoResultChecker.inputWinningLottoNum()
+    );
   }
+
   async inputPurchasePrice() {
     const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
     return +input;
