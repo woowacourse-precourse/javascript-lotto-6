5c8ecfb1e0afbb17d2a5a48ee65619eda6f1c204
lbj23han <ljhan0215@gmail.com>
2023-11-05 15:23:02 +0900

    fix: Lotto.js 중복 숫자 생성 방지

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,13 +12,25 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== 6) {
+      throw new Error("[ERROR] 중복된 로또 번호가 있습니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
   static generateNumbers(count) {
     const lottoNumbers = [];
     for (let i = 0; i < count; i++) {
-      const uniqueNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      let uniqueNumbers;
+      do {
+        uniqueNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      } while (
+        lottoNumbers.some((numbers) =>
+          numbers.every((num) => uniqueNumbers.includes(num))
+        )
+      );
       const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
       lottoNumbers.push(sortedNumbers);
     }
