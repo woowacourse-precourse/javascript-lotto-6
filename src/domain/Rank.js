a6b60364cf63881defbceff27e560a5a48e6d33e
jaeochoii <jaeochoiii0716@inha.edu>
2023-11-05 16:30:39 +0900

    fix(Rank): lotto 배열을 인자로 전해 lotto 참조 오류 해결

--- src/domain/Rank.js
+++ src/domain/Rank.js
@@ -1,31 +1,32 @@
 class Rank {
+  #lottos;
   #rankStatic;
 
-  constructor() {
+  constructor(lottos) {
     this.#rankStatic = Array(5).fill(0);
+    this.#lottos = lottos;
   }
 
-  getRank(props) {
-    const sameNumberCount = this.getSameNumberCount(props.winningNumber);
+  getRank(lotto, winningNumber, bonusNumber) {
+    const sameNumberCount = this.getSameNumberCount(lotto, winningNumber);
     if (sameNumberCount === 3) return 5;
     if (sameNumberCount === 4) return 4;
-    if (sameNumberCount === 5 && props.lotto.includes(bonusNumber)) return 2;
+    if (sameNumberCount === 5 && lotto.includes(bonusNumber)) return 2;
     if (sameNumberCount === 5) return 3;
     if (sameNumberCount === 6) return 1;
   }
 
-  getRankStatistic(props) {
-    props.lottos.forEach((lotto) => {
-      const rank = lotto.getRank(props.winningNumber, bonusNumber);
+  getRankStatistic(winningNumber, bonusNumber) {
+    this.#lottos.forEach((lotto) => {
+      const rank = this.getRank(lotto, winningNumber, bonusNumber);
       if (rank) this.#rankStatic[rank - 1] += 1;
     });
     return this.#rankStatic;
   }
 
-  getSameNumberCount(props) {
-    return props.winningNumber
-      .map(Number)
-      .filter((number) => props.lotto.includes(number)).length;
+  getSameNumberCount(lotto, winningNumber) {
+    return winningNumber.map(Number).filter((number) => lotto.includes(number))
+      .length;
   }
 }
 
