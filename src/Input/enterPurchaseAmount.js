87b561ef9697a4a426674d73738cb6fa7a59a367
sehyun <e6d1fe@gmail.com>
2023-11-05 11:18:03 +0900

    fix: enterPurchaseAmount export 추가

--- src/Input/enterPurchaseAmount.js
+++ src/Input/enterPurchaseAmount.js
@@ -4,3 +4,5 @@ import { message } from '../Constants';
 async function enterPurchaseAmount() {
   const purchaseAmountInput = await Console.readLineAsync(message.ENTER_PURCHASE_AMOUNT);
 }
+
+export default enterPurchaseAmount;
