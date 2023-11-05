9ebb2d8452ae03cdb065c6fcfae6df3a60b37971
yu-retel <dbdudals100@naver.com>
2023-11-06 04:10:15 +0900

    feat(OutputView): 로또 출력 기능구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,6 +3,10 @@ import { Console } from '@woowacourse/mission-utils';
 const OutputView = {
   buyLotto(quantity) {
     Console.print(`\n${quantity}개를 구매했습니다.`);
+  },
+
+  issuedLotto(lotto) {
+    Console.print(`[${lotto.sort((a, b) => a - b).toString()}]`);
   }
 }
 
