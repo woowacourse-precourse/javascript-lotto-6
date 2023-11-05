e130017e257ec0f0274d893f85b1f6c67bb052bb
yu-retel <dbdudals100@naver.com>
2023-11-06 04:25:46 +0900

    feat(InputView): 당첨 번호 입력 기능구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,6 +5,11 @@ const InputView = {
     const amount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     return amount;
   },
+
+  async winningNumber() {
+    const winningNumber = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+    return winningNumber;
+  }
 }
 
 export default InputView;
