dfa82f69d2d349dc0539be4f3912fd7549d6ff59
Boeun <boeun0904@naver.com>
2023-11-06 02:18:26 +0900

    feat(constants): 로또 관련 수치 상수화

--- src/constants.js
+++ src/constants.js
@@ -18,9 +18,16 @@ export const ERROR = {
   NOT_DIVIDED_BY_THOUSAND: '숫자가 1000 단위여야 합니다.',
   NOT_SIX_NUMBERS: '로또 번호는 6개여야 합니다.',
   ALREADY_SELECTED: '당첨 번호에 존재하는 숫자입니다.',
-  OUT_OF_RANGE: (start, end) =>
-    `로또 번호는 ${start}부터 ${end} 사이의 숫자여야 합니다.`,
+  OUT_OF_RANGE: (start, end) => `로또 번호는 ${start}부터 ${end} 사이의 숫자여야 합니다.`,
+};
+
+export const LOTTERY = {
+  MIN_NUM: 1,
+  MAX_NUM: 45,
+  NUM_COUNT: 6,
+  PRICE: 1000,
 };
 
 Object.freeze(MESSAGE);
 Object.freeze(ERROR);
+Object.freeze(LOTTERY);
