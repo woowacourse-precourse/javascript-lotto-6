7ebdd4d080ac7b16c116f0710ef1a00a3e235616
ChanYong Park <krap5075@naver.com>
2023-11-06 03:39:34 +0900

    feat: 구매 개수만큼 로또 번호 생성 기능 추가

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,24 +1,38 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
 import Lotto from '../model/Lotto.js';
 
+const { Random } = MissionUtils;
 class LottoController {
   constructor() {
     this.inputView = new InputView();
     this.outputView = new OutputView();
-    this.lotto = new Lotto();
     this.usedMoney = 0;
     this.countOfLottos = 0;
+    this.lottoArray = [];
   }
 
   async playLotto() {
     this.usedMoney = await this.inputView.inputMoney(); // 정상 입력 시에 통과
     this.countOfLottos = this.howManyLottos();
-    console.log(this.countOfLottos);
+    this.lottoArray = this.makeNewLottos(); // 개수만큼 로또생성
   }
 
   howManyLottos() {
     return this.usedMoney / 1000;
+  }
+
+  // 로또 구매 개수만큼 객체 생성
+  makeNewLottos() {
+    for (let i = 0; i < this.countOfLottos; i += 1) {
+      const lotto = new Lotto(this.pickRandomNumbers());
+      this.lottoArray.push(lotto);
+    }
+  }
+
+  pickRandomNumbers() {
+    Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 
   calculateBenefit(earnedMoney) {
