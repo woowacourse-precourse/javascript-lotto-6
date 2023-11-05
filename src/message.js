60a63b152122f313db70db01ccda0ad7ad5b430f
Moon Sojeong <sjeong35@naver.com>
2023-11-05 17:42:07 +0900

    refactor: Lotto.js의 에러 메세지도 ERROR_MESSAGE에 추가

--- src/message.js
+++ src/message.js
@@ -2,8 +2,12 @@ const ERROR_MESSAGE = Object.freeze({
     NOT_NUMBER : '[ERROR] 숫자가 잘못된 형식입니다.',
     MIN_PRICE : '[ERROR] 로또 최소 구입 금액은 1000원입니다.',
     THOUSAND_UNIT : '[ERROR] 금액은 1,000원 단위로 입력해주세요.',
-    EMPTY : '[ERROR] 숫자를 입력하세요.',
+    NUMBER_EMPTY : '[ERROR] 숫자를 입력하세요.',
     RANGE_BONUS : '[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.',
+    LOTTO_EMPTY : '[ERROR] 로또 번호를 입력하세요.',
+    SIX_NUMBERS : '[ERROR] 로또 번호는 6개여야 합니다.',
+    LOTTO_RANGE : '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+    SAME_NUMBERS : '[ERROR] 중복되지 않는 숫자를 입력하세요',
 });
 
 const PRINT_MESSAGE = Object.freeze({
