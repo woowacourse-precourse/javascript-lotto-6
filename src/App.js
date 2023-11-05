f68822904e2bb92426087fab63900cade36dd6e8
jonghyunRyu <willowise@naver.com>
2023-11-05 11:54:38 +0900

    feat(App): 구입금액 입력과 예외 처리 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Random, Console } from "@woowacourse/mission-utils"
+
 class App {
-  async play() {}
+
+  async buyLotto() {
+    const money = await Console.readLineAsync("구입금액을 입력해 주세요. \n");
+
+    this.validateBuyLotto(money);
+    return money;
+  }
+  
+  validateBuyLotto(input) {
+    if (input % 1000 !== 0) {
+      throw new Error("[ERROR] 천 원 단위로 입력해 주세요.");
+    }
+  }
+
+  async play() {
+    
+  }
+  
 }
 
 export default App;
