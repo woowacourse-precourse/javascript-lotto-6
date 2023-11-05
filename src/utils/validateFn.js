6d6dcb23d2d022e9d3e22bba7f8b87771546e87f
donggyun <donggyunkim12@gmail.com>
2023-11-05 11:17:23 +0900

    feat(validateFn): 로또 번호 생성 및 입력 기능 구현

--- src/utils/validateFn.js
+++ src/utils/validateFn.js
@@ -15,3 +15,23 @@ export const validateDivisible = (number) => {
     throw new Error(ERROR_MESSAGE.INVALID_NON_DIVISIBLE);
   }
 };
+
+export const validateLength = (numberArr) => {
+  if (numberArr.length !== 6) {
+    throw new Error(ERROR_MESSAGE.INVALID_LOTTO_LENGTH);
+  }
+};
+
+export const validateRange = (numberArr) => {
+  numberArr.forEach((number) => {
+    if (number < 1 || number > 45) {
+      throw new Error(ERROR_MESSAGE.INVALID_LOTTO_RANGE);
+    }
+  });
+};
+
+export const validateUnique = (numberArr) => {
+  if (numberArr.length !== new Set(numberArr).size) {
+    throw new Error(ERROR_MESSAGE.INVALID_LOTTO_UNIQUE);
+  }
+};
