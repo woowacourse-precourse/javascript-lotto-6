9694422524ba154c9ef200c73361da869efd3ffd
1eecan <busyx2modernsociety@gmail.com>
2023-11-06 01:34:50 +0900

    refactor:  OutputMessage.js 리팩터링
    * 객체필드에서 변수를 받아 문자열을 출력하게 수정

--- src/constants/OutputMessage.js
+++ src/constants/OutputMessage.js
@@ -6,15 +6,14 @@ const OUTPUT_MESSAGE = Object.freeze({
   PRINT_HORIZONTAL_LINE: '---',
   PRINT_ENTER: '',
 
-  PRINT_FIFTH_PLACE: '3개 일치 (5,000원) - ',
-  PRINT_FORTH_PLACE: '4개 일치 (50,000원) - ',
-  PRINT_THIRD_PLACE: '5개 일치 (1,500,000원) - ',
-  PRINT_SECOND_PLACE: '5개 일치 (30,000,000원) - ',
-  PRINT_FIRST_PLACE: '6개 일치 (2,000,000,000원) - ',
+  PRINT_FIFTH_PLACE: (number) => `3개 일치 (5,000원) - ${number}개`,
+  PRINT_FORTH_PLACE: (number) => `4개 일치 (50,000원) - ${number}개`,
+  PRINT_THIRD_PLACE: (number) => `5개 일치 (1,500,000원) - ${number}개`,
+  PRINT_SECOND_PLACE: (number) =>
+    `5개 일치, 보너스 볼 일치 (30,000,000원) - ${number}개`,
+  PRINT_FIRST_PLACE: (number) => `6개 일치 (2,000,000,000원) - ${number}개`,
 
-  PRINT_SUFFIX: '개',
-  PRINT_TOTAL_RETURN_PREFIX: '총 수익률은 ',
-  PRINT_TOTAL_RETURN_SUFFIX: '%입니다.',
+  PRINT_TOTAL_RETURN_RATE: (number) => `총 수익률은 ${number}%입니다.`,
 });
 
 export default OUTPUT_MESSAGE;
