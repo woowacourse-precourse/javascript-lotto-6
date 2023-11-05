92d70c1a6e93464aae7822bad093a30e37014ab4
0jenn0 <jenn0.6n@gmail.com>
2023-11-06 07:17:22 +0900

    Refactor: 구매 로또들 등수 결과 구하는 부분 리팩토링함
    - result 배열을 하드코딩하지 않고 Object메서드를 이용하여 생성

--- src/service/LottoYieldCalculator.js
+++ src/service/LottoYieldCalculator.js
@@ -1,12 +1,47 @@
 import LOTTO_SETTINGS from "../config/gameSetting.js";
 
 export default class LottoYieldCalculator {
-  static checkMatchCount(lottonumbers, { drawnLottoNumbers, bonusNumber }) {
+  static checkMatchCount(lottonumbers, drawnLottoNumbers, bonusNumber) {
     const matchCount = lottonumbers.filter((number) =>
       drawnLottoNumbers.includes(number)
     ).length;
     const hasBonusNumber = lottonumbers.includes(bonusNumber);
 
     return { matchCount, hasBonusNumber };
+  }
+
+  static getResult(lottosNumbers, drawnLottoNumbers, bonusNumber) {
+    const result = Object.keys(LOTTO_SETTINGS.WINNINGS).reduce((acc, prize) => {
+      acc[prize] = 0;
+      return acc;
+    }, {});
+
+    lottosNumbers.forEach((lottoNumbers) => {
+      const { matchCount, hasBonusNumber } = this.checkMatchCount(
+        lottoNumbers,
+        drawnLottoNumbers,
+        bonusNumber
+      );
+
+      const prize = this.findPrizeKey(matchCount, hasBonusNumber);
+
+      if (prize) result[prize] += 1;
+    });
+
+    return result;
+  }
+
+  static findPrizeKey(matchCount, isBonusMatched) {
+    for (const [key, { matchNum, prize }] of Object.entries(
+      LOTTO_SETTINGS.WINNINGS
+    )) {
+      if (
+        matchCount === matchNum &&
+        (!isBonusMatched || key === "SECOND_PRIZE")
+      ) {
+        return key;
+      }
+    }
+    return null;
   }
 }
