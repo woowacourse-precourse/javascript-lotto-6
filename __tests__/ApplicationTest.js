af95370828f6adf958937ae77233549c74532a02
SSY1203 <tlstjsdud566@naver.com>
2023-11-05 21:47:54 +0900

    [feat] 구입 금액이 1,000원 단위가 아닐 경우, 예외 발생

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -94,4 +94,8 @@ describe('로또 테스트', () => {
   test('구매금액이 숫자가 아닐 경우, 예외 테스트', async () => {
     await runException('1000j');
   });
+
+  test('구매금액이 1,000원 단위가 아닐 경우, 예외 테스트', async () => {
+    await runException('10500');
+  });
 });
--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,10 @@
-import { Console, ERROR_PURCHASE_AMOUNT_STRING, PLZ_PURCHASE_AMOUNT } from './Constant';
+import {
+  Console,
+  ERROR_PURCHASE_AMOUNT_STRING,
+  ERROR_PURCHASE_AMOUNT_UNIT,
+  PAYMENT_UNIT,
+  PLZ_PURCHASE_AMOUNT,
+} from './Constant';
 
 const InputView = {
   async readPurchaseAmount() {
@@ -6,7 +12,7 @@ const InputView = {
       try {
         const answer = await Console.readLineAsync('로또 구입 금액을 입력하세요: ');
         this.paymentAmountNumberValidator(answer);
-
+        this.paymentUnitValidator(answer);
         return answer;
       } catch (error) {
         Console.print(error.message);
@@ -18,6 +24,13 @@ const InputView = {
     const purchaseAmount = Number(answer);
     if (isNaN(purchaseAmount) || purchaseAmount % 1000 !== 0) {
       throw new Error(ERROR_PURCHASE_AMOUNT_STRING);
+    }
+  },
+
+  paymentUnitValidator(answer) {
+    const purchaseAmount = Number(answer);
+    if (purchaseAmount % PAYMENT_UNIT !== 0) {
+      throw new Error(ERROR_PURCHASE_AMOUNT_UNIT);
     }
   },
 };
