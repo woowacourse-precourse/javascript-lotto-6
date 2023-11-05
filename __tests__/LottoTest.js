3484143bac08981b4f309dd65836f1b7ab8110cb
heonq <gkagjsrb@naver.com>
2023-11-05 17:39:15 +0900

    test(Lotto): 로또 생성에 대한 테스트 코드 작성 - 로또 번호에 1에서 45사이의 정수가 아닌 숫자가 있으면 예외가 발생하는 테스트 - 로또 번호에 모두 조건에 맞는 번호가 있으면 예외가 발생하지 않는 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,12 @@
 import Lotto from '../src/Lotto.js';
+import ERROR_MESSAGE from '../utils/ErrorMessages.js';
+
+const invalidLottoNumbers = [
+  [-1, 1, 2, 3, 4, 5],
+  [1, 2, 3, 4, 5, 46],
+  [1.1, 2, 3, 4, 5, 6],
+];
+const validLottoNumber = [1, 2, 3, 4, 5, 6];
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -15,4 +23,13 @@ describe('로또 클래스 테스트', () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+
+  test('로또 번호에 1에서 45사이의 정수가 아닌 숫자가 있으면 예외가 발생한다.', () => {
+    invalidLottoNumbers.forEach((invalidLottoNumber) =>
+      expect(() => new Lotto(invalidLottoNumber)).toThrow(ERROR_MESSAGE.invalidLottoNumber),
+    );
+  });
+  test('로또 번호에 모두 조건에 맞는 번호가 있으면 예외가 발생하지 않는다.', () => {
+    expect(() => new Lotto(validLottoNumber)).not.toThrow(ERROR_MESSAGE.invalidLottoNumber);
+  });
 });
