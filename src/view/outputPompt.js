30af82df0c2123683073846a91554d6e17b4d866
donggyun <donggyunkim12@gmail.com>
2023-11-05 18:42:36 +0900

    feat(outputPrompt): 통계 출력 전 문구 기능 구현

--- src/view/outputPompt.js
+++ src/view/outputPompt.js
@@ -2,9 +2,14 @@ import { Console } from '@woowacourse/mission-utils';
 import { OUTPUT_MESSAGE } from '../constant/message.js';
 
 export const printBuyLottery = (amount) => {
-  return Console.print(OUTPUT_MESSAGE.OUTPUT_BUY_LOTTERY(amount));
+  Console.print(OUTPUT_MESSAGE.OUTPUT_BUY_LOTTERY(amount));
 };
 
 export const printLottery = (lotto) => {
-  return Console.print(`[${lotto.toString().replaceAll(',', ', ')}]`);
+  Console.print(`[${lotto.toString().replaceAll(',', ', ')}]`);
+};
+
+export const printBeforeResult = () => {
+  Console.print(OUTPUT_MESSAGE.OUTPUT_RESULT);
+  Console.print(OUTPUT_MESSAGE.OUTPUT_DIVIDER);
 };
