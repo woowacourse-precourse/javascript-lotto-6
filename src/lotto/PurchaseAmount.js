fea531726ac3a0b32320c83f1b8c7c33047e15b0
jiyun1006 <jiyun4251@gmail.com>
2023-11-05 23:12:01 +0900

    ♻️ refactor : PurchaseAmount 관련 메서드 파일 경로 수정
    createObject 등의 메서드가
    PurchaseAmount, Lotto 클래스가 공동으로 사용하는 메서드로 변경

--- src/lotto/PurchaseAmount.js
+++ src/lotto/PurchaseAmount.js
@@ -1,5 +1,4 @@
-import { validatePurchaseAmount } from '../utils/index.js';
-import { magicNumber } from '../constants/index.js';
+import { magicNumber, errorConstants } from '../constants/index.js';
 
 export default class PurchaseAmount {
   #purchaseAmount;
@@ -21,7 +20,14 @@ export default class PurchaseAmount {
   }
 
   #validate(purchaseAmount) {
-    validatePurchaseAmount(purchaseAmount);
+    // 숫자인지 체크
+    if (isNaN(purchaseAmount)) throw new Error(errorConstants.NOT_A_NUMBER);
+    // 공백 체크
+    if (/\s/.test(String(purchaseAmount)))
+      throw new Error(errorConstants.NOT_EMPTY);
+    // 1000단위 체크
+    if (purchaseAmount % magicNumber.UNIT !== magicNumber.ZERO)
+      throw new Error(errorConstants.WRONG_UNIT);
   }
 
   createLottoCnt(purchaseAmount) {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
 export { inputMethod, printMethod } from './ui-method.js';
 export { createPurchaseAmount, createLottoNumber } from './create-object.js';
 export { inputPurchaseAmount, inputLottoNumber } from './input-number.js';
-export { default as validatePurchaseAmount } from './validate-purchase-amount.js';
