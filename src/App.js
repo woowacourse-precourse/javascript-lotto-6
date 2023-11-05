d71622969bc1a95fae305c5b33b1408eef8946ad
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-11-05 22:20:21 +0900

    당첨 통계를 출력 기능완료

--- src/App.js
+++ src/App.js
@@ -2,38 +2,49 @@ import { Console } from '@woowacourse/mission-utils';
 import { printMessage } from './utils/printMessage.js';
 import MESSAGES from './constants/messages.js';
 import NUMBERS from './constants/numbers.js';
-import {
-  isValidPerchaseAmount,
-  isValidLottoNumber,
-  isValidBouseNumber,
-} from './utils/validator.js';
+import isValidPerchaseAmount from './utils/validator.js';
 import Lotto from './Lotto.js';
 import makeLottoNumbers from './utils/makeLottoNumbers.js';
 import BonusNumber from './BonusNumber.js';
+import WinningResult from './WinningResult.js';
 
 class App {
-  constructor() {}
-
   async play() {
     const purchaseAmount = await Console.readLineAsync(MESSAGES.purchaseAmount);
-    isValidPerchaseAmount(purchaseAmount);
+
+    await isValidPerchaseAmount(purchaseAmount);
+
     const perchaseQuentity = parseInt(
       purchaseAmount / NUMBERS.perchaseUnit,
       10,
     );
+
     printMessage(`${perchaseQuentity}${MESSAGES.purchaseQuantity}`);
-    makeLottoNumbers(perchaseQuentity);
+
+    const winningNumbers = await makeLottoNumbers(perchaseQuentity);
+
     const userNumber = await Console.readLineAsync(
       MESSAGES.userLottoNumberInput,
     );
+
     const userLottoNumberInput = userNumber.split(',');
+
     const lotto = new Lotto(userLottoNumberInput);
+
     const userBonusNumberInput = await Console.readLineAsync(
       MESSAGES.askBonusNumber,
     );
+
     const bonusNumber = new BonusNumber(
       userBonusNumberInput,
       userLottoNumberInput,
+    );
+
+    const result = new WinningResult(
+      purchaseAmount,
+      userBonusNumberInput,
+      userLottoNumberInput,
+      winningNumbers,
     );
   }
 }
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,15 +3,23 @@ import NUMBERS from './numbers.js';
 const MESSAGES = {
   purchaseAmount: `최대 ${NUMBERS.maxPerchaseAmount}원까지 ${NUMBERS.perchaseUnit}원 단위로 구입금액을 입력해주세요.\n`,
   errorHeader: '[ERROR]: ',
-  nonThousandUnitMessage: ` ${NUMBERS.perchaseUnit}원 단위로 입력해주세요.`,
+  nonThousandUnitMessage: `${NUMBERS.perchaseUnit}원 단위로 입력해주세요.`,
   inputLimitMessage: `${NUMBERS.maxPerchaseAmount}원이 넘지 않게 입력해주세요.`,
   emptyPurchaseAmountInput: '금액을 입력해주세요.',
   purchaseQuantity: '개를 구매했습니다.',
-  userLottoNumberInput: `${NUMBERS.minLottoNumber}~${NUMBERS.maxLottoNumber}사이의 당첨 번호 ${NUMBERS.lottoNumberLength}개를 입력해 주세요.\n`,
+  userLottoNumberInput: `\n${NUMBERS.minLottoNumber}~${NUMBERS.maxLottoNumber}사이의 당첨 번호 ${NUMBERS.lottoNumberLength}개를 입력해 주세요.\n`,
   invalidLength: `로또 번호는 ${NUMBERS.lottoNumberLength}개여야 합니다.`,
   invalidRange: `${NUMBERS.minLottoNumber}~${NUMBERS.maxLottoNumber}사이의 번호를 입력해주세요.`,
   duplicatedNumber: '중복되지 않은 숫자를 입력해주세요',
-  askBonusNumber: `${NUMBERS.minLottoNumber}~${NUMBERS.maxLottoNumber} 사이의 보너스 번호를 입력해 주세요.\n`,
+  askBonusNumber: `\n${NUMBERS.minLottoNumber}~${NUMBERS.maxLottoNumber} 사이의 보너스 번호를 입력해 주세요.\n`,
+  winningResult: '\n당첨 통계',
+  divisionLine: '---',
+  matchThree: '3개 일치 (5,000원) - ',
+  matchFour: '4개 일치 (50,000원) - ',
+  matchFive: '5개 일치 (1,500,000원) - ',
+  matchFiveAndBonus: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  matchSix: '6개 일치 (2,000,000,000원) - ',
+  profitRate: '총 수익률은 ',
 };
 
 Object.freeze(MESSAGES);
