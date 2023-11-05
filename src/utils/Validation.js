1f8e1ecee5eacecc4a28f3cfa21192881a5bdfb1
kimjinwoo <rlawlsdn177@naver.com>
2023-11-05 17:59:41 +0900

    feat($checkBonusNumber): 사용자 입력 보너스 당첨 번호 유효성 검사
    - ($checkBonusNumbers): 당첨번호에 보너스 번호가 포함되는지 유효성 검사 후
    유효하지 않을 경우 에러를 던진다

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,3 +1,15 @@
 export const checkBonus = (lotto, bonusNumber) => (
     lotto.getNumbers().includes(bonusNumber)
 );
+
+export const checkBonusNumbers = (bonusNumber, winningNumbers) => {
+    if (isNaN(bonusNumber)) {
+        throw new Error('보너스 볼은 당첨 번호와 달라야 하며, 1부터 45 사이의 숫자여야 합니다.');
+    }
+    if (winningNumbers.includes(bonusNumber)) {
+        throw new Error('보너스 볼은 당첨 번호와 달라야 하며, 1부터 45 사이의 숫자여야 합니다.');
+    }
+    if (bonusNumber < 1 || bonusNumber > 45) {
+        throw new Error('보너스 볼은 당첨 번호와 달라야 하며, 1부터 45 사이의 숫자여야 합니다.');
+    }
+};
