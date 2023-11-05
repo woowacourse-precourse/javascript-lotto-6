65428bf53cf5f4c4773a9262d4d08f02bfa93e34
kimdaye77 <kimdaye77@naver.com>
2023-11-05 14:54:55 +0900

    feat: 번호 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -12,11 +12,20 @@ class App {
     //TODO: 예외처리
     
     const lottoCnt = Number(purchaseAmountInput)/1000;
+
+    Console.print(`${lottoCnt}개를 구매했습니다.\n`);
     for(let i = 0; i<lottoCnt; i++){
       const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
-      Console.print(numbers)
+      Console.print(numbers) //TODO: 로또에 메서드 넣기
       const lotto = new Lotto(numbers);
     }
+
+    const WinningNum = await Console.readLineAsync(
+      "당첨 번호를 입력해 주세요.\n"
+    );
+    const BonusNum = await Console.readLineAsync(
+      "보너스 번호를 입력해 주세요.\n"
+    );
   }
 }
 export default App;
