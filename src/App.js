23fcb489decd0f36f16dbea04cdf053c95942e8d
Majesty-jun <hkwns01@gmail.com>
2023-11-06 00:44:50 +0900

    FEAT: 로또 갯수 출력

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 import inputPrice from './input/inputPrice.js';
 import getAmountOfLottery from './lottery/getAmountOfLottery.js';
+import { BUY_LOTTO } from './constants.js';
 
 class App {
   async play() {
     this.price = await inputPrice();
     this.lotteryAmount = getAmountOfLottery(this.price);
-    Console.print(this.lotteryAmount);
+    Console.print(`\n${this.lotteryAmount}${BUY_LOTTO}`);
   }
 }
 
