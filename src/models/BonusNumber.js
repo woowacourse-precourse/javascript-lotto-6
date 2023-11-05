2b03c00fd5c9ff28b1f07907a3781a8d20af04cd
이용성 <ukal5006@kakao.com>
2023-11-05 19:56:13 +0900

    feat(BonusNumber): 보너스 번호 검증, 반환 기능 추가

---
+++ src/models/BonusNumber.js
@@ -0,0 +1,32 @@
+class BonusNumber {
+  #bonusNumber;
+
+  constructor(winningNumber, bonusNumber) {
+    this.#validate(winningNumber, bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  #validate(winningNumber, bonusNumber) {
+    if (!Number.isInteger(Number(bonusNumber))) {
+      throw new Error('[ERROR] 보너스 번호는 정수 이여야 합니다.');
+    }
+
+    if (bonusNumber.length !== Number(bonusNumber).toString().length) {
+      throw new Error('[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.');
+    }
+
+    if (Number(bonusNumber) > 45 || Number(bonusNumber) < 1) {
+      throw new Error('[ERROR] 보너스 번호는 1~45 사이의 수 이여야 합니다.');
+    }
+
+    if (winningNumber.includes(bonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+    }
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+}
+
+export default BonusNumber;
