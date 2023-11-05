a337db6a522d7f3616d12b93d599cd037d192512
rokaf340 <rokaf340@gmail.com>
2023-11-05 15:31:22 +0900

    Feat: Add 당첨 번호 랜덤으로 뽑기 - 발행한 로또 수량 및 번호를 오름차순으로 출력한다 - 당첨번호는 1부터 45까지 숫자중 랜덤으로 뽑는다

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,8 +1,36 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 import { NUMBER, TEXT } from './data.js';
 
+function lottoRandomNumer() {
+  const randomNumbers = [];
+  while (randomNumbers.length < NUMBER.LOTTO_LENGTH) {
+    const number = Random.pickNumberInRange(
+      NUMBER.MIN_LOTTO_NUMBER,
+      NUMBER.MAX_LOTTO_NUMBER,
+    );
+    if (randomNumbers.indexOf(number) === -1) {
+      randomNumbers.push(number);
+    }
+  }
+  randomNumbers.sort((a, b) => a - b);
+  return randomNumbers;
+}
+
+function outputNumber(inputAmount) {
+  const lottoCount = inputAmount / NUMBER.INITIAL_AMOUNT;
+  const lottoINITIAL = Array(lottoCount)
+    .fill()
+    .map((numbers) => {
+      numbers = lottoRandomNumer();
+      return numbers;
+    });
+  Console.print(`${lottoCount}${TEXT.OUTPUT_NUMBER}`);
+  lottoINITIAL.forEach((numbers) => Console.print(numbers));
+}
+
 export async function lottoAmountGet() {
   const inputAmount = await Console.readLineAsync(TEXT.INPUT_AMOUNT);
+  outputNumber(inputAmount);
 }
 
 class Lotto {
