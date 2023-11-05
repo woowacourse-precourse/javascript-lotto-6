7728c040b397179c0b49d9c0c20fde189029e56d
hong <ahi200@naver.com>
2023-11-06 00:48:21 +0900

    test: IsBonusNumberMatch 메소드의 테스트코드 구현

--- __tests__/LotteryPrizeTest.js
+++ __tests__/LotteryPrizeTest.js
@@ -1,6 +1,6 @@
 import LotteryPrize from '../src/LotteryPrize.js';
 describe('LotteryPrize.getMatchCount', () => {
-  test('LotteryPrize.getMatchCount', () => {
+  test('두 번호를 비교해 공통된 번호의 개수를 리턴해야 한다.', () => {
     expect(
       LotteryPrize.getMatchCount([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12])
     ).toBe(0);
@@ -22,5 +22,19 @@ describe('LotteryPrize.getMatchCount', () => {
     expect(
       LotteryPrize.getMatchCount([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])
     ).toBe(6);
+  });
+});
+
+describe('LotteryPrize.IsBonusNumberMatch', () => {
+  test('보너스 번호가 포함되면 true이다.', () => {
+    expect(LotteryPrize.IsBonusNumberMatch([1, 2, 3, 4, 5, 6], 1)).toBe(true);
+    expect(LotteryPrize.IsBonusNumberMatch([1, 2, 3, 4, 5, 6], 2)).toBe(true);
+    expect(LotteryPrize.IsBonusNumberMatch([1, 2, 3, 4, 5, 6], 3)).toBe(true);
+  });
+
+  test('보너스 번호가 포함되지 않으면 false이다.', () => {
+    expect(LotteryPrize.IsBonusNumberMatch([1, 2, 3, 4, 5, 6], 7)).toBe(false);
+    expect(LotteryPrize.IsBonusNumberMatch([1, 2, 3, 4, 5, 6], 8)).toBe(false);
+    expect(LotteryPrize.IsBonusNumberMatch([1, 2, 3, 4, 5, 6], 9)).toBe(false);
   });
 });
