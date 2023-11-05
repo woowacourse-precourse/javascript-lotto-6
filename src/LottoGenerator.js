3e083d0068ca5ea2ff2194ab8ed4fa7dceef0fc2
hectick <ch.yeondev@gmail.com>
2023-11-05 16:37:19 +0900

    feat: 한 개의 로또를 생성하는 기능 구현

---
+++ src/LottoGenerator.js
@@ -0,0 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class LottoGenerator {
+  #RANGE_START = 1;
+  #RANGE_END = 45;
+  #QUANTITY = 6;
+
+  generate() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(
+      this.#RANGE_START,
+      this.#RANGE_END,
+      this.#QUANTITY
+    );
+  }
+}
+
+export default LottoGenerator;
