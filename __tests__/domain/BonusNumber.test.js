2fb39185ccbf4f51c66a2d5222fcf4a170315bd4
김진호 <rlawlsgh1227@gmail.com>
2023-11-05 22:47:33 +0900

    test: 보너스 번호가 로또 번호 범위가 아닐 때 에러 검증

--- __tests__/domain/BonusNumber.test.js
+++ __tests__/domain/BonusNumber.test.js
@@ -3,7 +3,18 @@ import BonusNumber from "../../src/domain/BounsNumber";
 describe('보너스번호 입력 테스트', () => {
   test('숫자가 아닌 입력이 들어올 때 에러 메시지가 출력된다.', () => {
     // given
-    expect(() => new BonusNumber('공습경보', [1, 2, 3, 4, 5, 6])).toThrow('[ERROR]');
-  
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const invalidBonusNumber = '공습경보';
+
+    expect(() => new BonusNumber(invalidBonusNumber, winningNumbers)).toThrow('[ERROR]');
+  });
+
+  test('로또 범위에 맞지 않는 숫자가 들어올 때 에러 메시지가 출력된다.', () => {
+    // given
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const invalidBonusNumber = 46;
+
+    // when & then
+    expect(() => new BonusNumber(invalidBonusNumber, winningNumbers)).toThrow('[ERROR]');
   });
 });
