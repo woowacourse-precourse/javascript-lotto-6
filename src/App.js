b05b489105f00ead4d21e27b1c3092d6c5285442
lurgi <wahoo9040@naver.com>
2023-11-05 11:12:24 +0900

    feat(App): 중복되지 앟는 6개 숫자 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+
+  static getRandomSixNumbers() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
 }
 
 export default App;
