3af4323a3534457c61e1da7bdd34beb718484f2a
yu-retel <dbdudals100@naver.com>
2023-11-06 02:44:56 +0900

    feat(OutputView): 구입한 로또 개수 출력 기능구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  buyLotto(quantity) {
+    Console.print(`\n${quantity}개를 구매했습니다.`);
+  }
+}
+
+export default OutputView;
