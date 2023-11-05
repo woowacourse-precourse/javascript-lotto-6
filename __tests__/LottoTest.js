fa516d5ee0223368b7d4b21117f85e285bf75ecb
BK <3031011@naver.com>
2023-11-05 23:20:06 +0900

    test: 로또 번호 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,4 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from "../src/classes/Lotto";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -14,5 +14,15 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호에 문자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 'a', 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 범위를 넘어가는 수가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
 });
