b663b631cfb99f47f66f96be98dbeb3afe557c85
nyeonge <nnnyeonge@gmail.com>
2023-11-06 00:46:33 +0900

    Feat: Add UserInput Function & Exception Function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import UserInput from "./UserInput.js";
+// import UserInput from "./UserInput.js";
 import PlayGame from "./PlayGame.js";
+import Lotto from "./Lotto.js";
 
 class App {
   constructor() {
-    this.UserInput = new UserInput();
+    // this.UserInput = new UserInput();
     this.PlayGame = new PlayGame();
+    this.Lotto = new Lotto();
   }
   async play() {
-    const ticketAmount = await this.UserInput.purchaseAmount();
+    const ticketAmount = await this.Lotto.inputPurchaseAmount();
     MissionUtils.Console.print(`\n${ticketAmount}개를 구매했습니다.\n`);
     const RandomArray = await this.PlayGame.getRandomNumber(ticketAmount);
+    const inputWinningNumber = await this.Lotto.inputWinningNumber()
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,65 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    // this.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  #validateWinningNumber(numbers, length) {
+    if (isNaN(numbers.join(''))) {
+      throw new Error("[ERROR] 로또 번호는 숫자 형식으로 입력해 주세요.");
+    } else if (numbers.length !== length) {
+      throw new Error(`[ERROR] 로또 번호는 ${length}개여야 합니다.`);
+    } else if (!this.#compareElement(numbers)) {
+      throw new Error("[ERROR] 1부터 45 사이의 숫자만 가능합니다.");
+    } else if (new Set(numbers).size != numbers.length) {
+      throw new Error('[ERROR] 서로 다른 숫자를 입력해야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  #compareElement(numbers) {
+    console.log('compareelement', numbers)
+    numbers.forEach(el => {
+      if ((el < 1) || (el > 45)) {
+        return true;
+      }
+    })
+    return false;
+  }
+
+  #validatePurchase(purchaseAmount) {
+    if (isNaN(purchaseAmount)) {
+      throw new Error("[ERROR] 숫자만 입력 가능합니다.");
+    } else if (Number(purchaseAmount) % 1000 !== 0) {
+      throw new Error("[ERROR] 구매 단위는 1000원 입니다.");
+    }
+  }
+
+  async inputPurchaseAmount() {
+    const purchaseAmount = await MissionUtils.Console.readLineAsync('구입급액을 입력해 주세요. \n');
+    this.#validatePurchase(purchaseAmount);
+    const ticketAmount = Number(purchaseAmount) / 1000;
+    return ticketAmount;
+  }
+
+  async inputWinningNumber() {
+    const stringWinningNumber = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    let winningNumber = stringWinningNumber.split(',').map(Number);
+    if (winningNumber.at(-1) === 0) {
+      winningNumber.pop();
+    }
+    this.#validateWinningNumber(winningNumber, 6);
+    const stringBonusNumber = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    const bonusNumber = Number(stringBonusNumber);
+    console.log('bonusNumber', bonusNumber)
+    winningNumber.push(bonusNumber);
+    console.log('winningNumber', winningNumber)
+    this.#validateWinningNumber(winningNumber, 7);
+    return winningNumber;
+  }
 }
 
 export default Lotto;
