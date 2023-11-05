5102dae5dc5b365edfcad68a1906086ef808cece
JiSeong Kim <12180543@inha.edu>
2023-11-06 04:56:19 +0900

    fix(validator): fix input format conversion and unhandled exception detection
    - Number 형태의 입력에서 string 형태의 입력으로 바꾸어 이에 맞춰 코드를 변경
    - 기존의 예외 경우에 더해 자연수 검증, 올바르지 않은 입력 검증 기능을 추가

--- src/validator/BonusNumberValidator.js
+++ src/validator/BonusNumberValidator.js
@@ -3,28 +3,42 @@ import lottoNumber from '../constants/lottoNumber.js';
 
 class BonusNumberValidator {
 	validateNumber(bonusNumber) {
-		if (Number.isNaN(bonusNumber)) {
+		if (isNaN(bonusNumber)) {
 			throw new Error(errorMessage.NOT_NUMBER);
 		}
 	}
 
 	validateNumberRange(bonusNumber) {
-		if (bonusNumber < lottoNumber.firstNumber || bonusNumber > lottoNumber.lastNumber) {
+		if (Number(bonusNumber) < lottoNumber.firstNumber || Number(bonusNumber) > lottoNumber.lastNumber) {
 			throw new Error(errorMessage.OUT_OF_RANGE);
 		}
 	}
 
 	validateAlreadyPickedNumber(bonusNumber, winningNumberArray) {
-		if (winningNumberArray.includes(bonusNumber)) {
+		if (winningNumberArray.includes(Number(bonusNumber))) {
 			throw new Error(errorMessage.ALREADY_PICKED_NUMBER);
+		}
+	}
+
+	validateNaturalNumber(bonusNumber) {
+		if (!Number.isInteger(Number(bonusNumber))) {
+			throw new Error(errorMessage.NOT_NATURAL_NUMBER);
+		}
+	}
+
+	validateUnusualCase(bonusNumber) {
+		if (bonusNumber !== Number(bonusNumber).toString()) {
+			throw new Error(errorMessage.UNUSUAL_INPUT);
 		}
 	}
 
 	static validate(bonusNumber, winningNumberArray) {
 		const bonusNumberValidator = new BonusNumberValidator();
 		bonusNumberValidator.validateNumber(bonusNumber);
-		bonusNumberValidator.validateAlreadyPickedNumber(bonusNumber, winningNumberArray);
 		bonusNumberValidator.validateNumberRange(bonusNumber);
+		bonusNumberValidator.validateUnusualCase(bonusNumber);
+		bonusNumberValidator.validateNaturalNumber(bonusNumber);
+		bonusNumberValidator.validateAlreadyPickedNumber(bonusNumber, winningNumberArray);
 	}
 }
 
