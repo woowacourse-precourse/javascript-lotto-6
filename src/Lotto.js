71661c8dd790228df21582deeac122643eb5bd1b
ss0526100 <ss0526100@gmail.com>
2023-11-06 07:00:01 +0900

    chore: apply new formatter

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,21 +8,20 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6)
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    if(numbers.length!==new Set(numbers).size)
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    if (numbers.length !== new Set(numbers).size)
       throw new Error('[ERROR] 중복된 값이 존재합니다.');
-    numbers.forEach((number)=>{
-      if(!Number.isInteger(number))
+    numbers.forEach(number => {
+      if (!Number.isInteger(number))
         throw new Error('[ERROR] 정수가 아닌 값이 존재합니다.');
-      if(number<1||45<number)
+      if (number < 1 || 45 < number)
         throw new Error('[ERROR] 1~45범위 밖의 숫자가 존재합니다.');
-    })
+    });
   }
 
-  getNumbers(){
+  getNumbers() {
     return this.#numbers;
   }
-
 }
 
 export default Lotto;
