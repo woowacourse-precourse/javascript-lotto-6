f804b87fb11d215571bbce5fc8a3e1579e943f11
이용성 <ukal5006@kakao.com>
2023-11-05 15:58:00 +0900

    feat(WinningNumber): 당첨 번호 검증, 반환 기능 추가

---
+++ src/models/WinnigNumber.js
@@ -0,0 +1,35 @@
+class WinningNumber {
+  #winningNumber;
+
+  constructor(winningNumber) {
+    const numbers = this.#splitWinningNumber(winningNumber);
+    this.#validate(numbers);
+    this.#winningNumber = numbers;
+  }
+
+  #splitWinningNumber(winningNumber) {
+    return winningNumber.split(',');
+  }
+
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+    }
+
+    numbers.forEach((number) => {
+      if (!Number.isInteger(Number(number))) {
+        throw new Error('[ERROR] 당첨 번호는 정수 이여야 합니다.');
+      }
+
+      if (Number(number) > 45 || Number(number) < 1) {
+        throw new Error('[ERROR] 당첨 번호는 1~45 사이의 수 이여야 합니다.');
+      }
+    });
+  }
+
+  getWinningNumber() {
+    return this.#winningNumber;
+  }
+}
+
+export default WinningNumber;
