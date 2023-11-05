cb29f1c1b16b665838da85b1b88d4937ea5617ff
nijuy <youjin6325@naver.com>
2023-11-05 18:07:57 +0900

    chore: ERROR_MESSAGE 객체 수정
    - outOfRange 예외의 메시지를 수정함

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -2,7 +2,7 @@ const ERROR_MESSAGE = Object.freeze({
   nonNumeric: '[Error] 숫자만 입력해주세요!',
   invalidAmount: '[Error] 1,000원 단위로 입력해주세요!',
   invalidListSize: '[Error] 로또 번호는 6개여야 합니다!',
-  outOfRange: '[Error] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  outOfRange: '[Error] 로또 번호는 1부터 45 사이의 정수여야 합니다.',
   duplicateNumber: '[Error] 중복된 번호가 포함되어 있습니다!',
   duplicateWinningNumber:
     '[Error] 당첨 번호와 겹치지 않는 번호를 입력해주세요!',
