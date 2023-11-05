db000c954c54f5012ccd76ea23f56761cef93d4d
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-11-05 22:39:06 +0900

    test: 로또 구입 금액에 맞게 로또가 생성되는지 확인하는 테스트 코드 작성

--- __tests__/LottoShopTest.js
+++ __tests__/LottoShopTest.js
@@ -16,11 +16,23 @@ describe('로또 판매점 클래스 테스트', () => {
         lottoShop.sell(2001);
       }).toThrow(ERROR.notBeDividedByThousand);
     });
+
+    test('에러가 발생하지 않는 경우', () => {
+      expect(() => lottoShop.sell(1000)).not.toThrow();
+      expect(() => lottoShop.sell(2000)).not.toThrow();
+      expect(() => lottoShop.sell(10000)).not.toThrow();
+    });
   });
 
-  test('에러가 발생하지 않는 경우', () => {
-    expect(() => lottoShop.sell(1000)).not.toThrow();
-    expect(() => lottoShop.sell(2000)).not.toThrow();
-    expect(() => lottoShop.sell(10000)).not.toThrow();
+  describe('로또 구입 금액에 맞게 로또가 생성되는지 확인', () => {
+    test('1000을 입력하면 1개가 생성된다.', () => {
+      expect(lottoShop.sell(1000)).toHaveLength(1);
+    });
+    test('2000을 입력하면 2개가 생성된다.', () => {
+      expect(lottoShop.sell(2000)).toHaveLength(2);
+    });
+    test('10000을 입력하면 10개가 생성된다.', () => {
+      expect(lottoShop.sell(10000)).toHaveLength(10);
+    });
   });
 });
