4650eb28fa3fcc1b6ddb42626eb0aa9d706844df
leelsj2007 <leelsj2007@gmail.com>
2023-11-05 18:36:26 +0900

    test : (입력) 구입 금액이 0 이하일 경우 예외 처리 테스트 작성

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -7,29 +7,41 @@ const mockInput = (input) => {
 };
 
 describe("입력값 테스트", () => {
-    test("구입금액 입력 테스트", async () => {
+    test("구입 금액 입력 테스트", async () => {
         //given
         const input = 14000;
         mockInput(input);
 
         //when
-        const inputValue = new InputVeiw()
+        const inputValue = new InputVeiw();
         const output = await inputValue.readPurchaseAmount();
 
         //then
         expect(output).toEqual(14);
     });
 
-    test("구입금액이 1000으로 나누어 떨어지지 않을 경우 예외 처리", async () => {
+    test("구입 금액이 1000으로 나누어 떨어지지 않을 경우 예외 처리", async () => {
         //given
         const input = 14500;
         mockInput(input);
 
         //when
-        const inputValue = new InputVeiw()
+        const inputValue = new InputVeiw();
+
+        //then
+        await expect(inputValue.readPurchaseAmount()).rejects.toThrow("[ERROR]");
+    });
+
+    test.each([
+        0, -1000
+      ])("구입 금액이 0 이하 일 경우 예외 처리", async (inputs) => {
+        //given
+        mockInput(inputs);
+
+        //when
+        const inputValue = new InputVeiw();
 
         //then
         await expect(inputValue.readPurchaseAmount()).rejects.toThrow("[ERROR]");
     });
 });
-  
