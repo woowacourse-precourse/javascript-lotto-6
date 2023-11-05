0ad5ef5fd86fe6d658cf2d98d30143d6ab61be98
Hyunji Seok <alleyji126@gmail.com>
2023-11-05 21:59:21 +0900

    fix(App): modify play function to add user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { INPUT } from './constants/messages.js';
+import Money from './Money.js';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const price = await MissionUtils.Console.readLineAsync(INPUT.price);
+      const money = new Money(price);
+    } catch (error) {
+      console.error(`${error.message}`);
+      return this.play();
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
