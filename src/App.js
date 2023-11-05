9114b09ae5247636c25a703a4045aea217750e1f
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-05 22:27:22 +0900

    Fix: 로또 개수 변수명 변경 및 재배치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
+const lottoPrice = 1000;
 class App {
   constructor() {}
   async play() {
     const purchasePrice = await this.inputPurchasePrice();
+    const lottoCount = Number(purchasePrice / lottoPrice);
   }
   async inputPurchasePrice() {
     const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
