b90855418fdc050d3636d29ec23ff74307957973
dyddks <dyddks0115@naver.com>
2023-11-05 16:52:16 +0900

    style: 코드컨벤션 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,7 +8,7 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.every(item => isNaN(item))) {
-      throw new Error("[ERROR] 유효하지 않은 입력입니다.");
+      throw new Error('[ERROR] 유효하지 않은 입력입니다.');
     }
 
     if (numbers.length !== 6) {
@@ -16,25 +16,25 @@ class Lotto {
     }
 
     if (new Set(numbers).size !== numbers.length) {
-      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
     }
 
     if (numbers.some(number => number > 45 || number < 1)) {
-      throw new Error("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.');
     }
   }
 
   #validateBonus(bonus) {
     if (isNaN(bonus)) {
-      throw new Error("[ERROR] 유효하지 않은 입력입니다.");
+      throw new Error('[ERROR] 유효하지 않은 입력입니다.');
     }
 
     if (bonus > 45 || bonus < 1) {
-      throw new Error("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.');
     }
 
     if ((this.#numbers).includes(bonus)) {
-      throw new Error("[ERROR] 이미 입력된 숫자입니다.");
+      throw new Error('[ERROR] 이미 입력된 숫자입니다.');
     }
   }
 
