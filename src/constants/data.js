b1673b654be3a82af2c44b2a824180583905dd58
Hyunji Seok <alleyji126@gmail.com>
2023-11-05 20:30:57 +0900

    feat(messages, data): add constants files

---
+++ src/constants/data.js
@@ -0,0 +1,4 @@
+const ZERO = 0;
+const THOUSAND = 1000;
+
+export { ZERO, THOUSAND };
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,16 +1,18 @@
 const INPUT = {
-  buy_amount: "구입 금액을 입력해 주세요. \n",
-  winning_numbers: "당첨 번호를 입력해 주세요. \n",
-  bonus_number: "보너스 번호를 입력해 주세요. \n",
-}
+  price: '구입금액을 입력해 주세요. \n',
+  winning_numbers: '당첨 번호를 입력해 주세요. \n',
+  bonus_number: '보너스 번호를 입력해 주세요. \n',
+};
 
 const ERROR = {
-  price_unit: "[ERROR] 로또 구입 금액은 1,000원 단위로 입력이 가능합니다.",
-  not_a_number: "[ERROR] 로또 구입 금액은 숫자만 입력이 가능합니다.",
+  not_a_valid_number: '[ERROR] 로또 구입금액은 자연수만 입력이 가능합니다.',
+  not_a_thousand_unit: '[ERROR] 로또 구입금액은 1,000원 단위로 입력이 가능합니다.',
 };
 
 const OUTPUT = {
 
 };
 
-export { INPUT, ERROR, OUTPUT };
+const LINE_BREAK = '\n';
+
+export { INPUT, ERROR, OUTPUT, LINE_BREAK };
