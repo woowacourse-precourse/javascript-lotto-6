36721822360a88b3f1e261d91dfd55b8b2454433
thumbthing <ilscc0902@gmail.com>
2023-11-06 07:18:46 +0900

    feat: 구현-모듈화된 기능 App에 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import getPurchase from './service/getPurchase.js';
+import getLottoNumber from './service/getLottoNumber.js';
+import { consolePrint } from './util/libararyFeatures/MissionUtilHandler.js';
+import getBonusNumber from './service/getBonusNumber.js';
+import lottoMatcher from './util/matcher/lottoMatcher.js';
+import winningStatisticsUI from './util/UI/gameResult/winningStatisticsUI.js';
+import purchaseAmountUI from './util/UI/gameStart/purchaseAmountUI.js';
+import receiveWinningLottoUI from './util/UI/gameStart/receiveWinningLottoUI.js';
+import receiveBonusNumberUI from './util/UI/gameStart/receiveBonusNumberUI.js';
+import lottoCompareService from './service/lottoCompareService.js';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const purchase = await purchaseAmountUI();
+      const purchaseValues = await getPurchase(purchase);
+
+      const winningNumber = await receiveWinningLottoUI();
+      const lotto = await getLottoNumber(winningNumber);
+
+      const bonusInput = await receiveBonusNumberUI();
+      const bonusNumber = await getBonusNumber(bonusInput, lotto);
+
+      const matchResult = lottoMatcher(purchaseValues.purchasedLotto, winningNumber, bonusNumber);
+      const lottoResult = lottoCompareService(matchResult, purchaseValues.purchaseAmount);
+
+      winningStatisticsUI(lottoResult);
+    } catch (error) {
+      const errorMessage = error.message;
+      consolePrint(errorMessage);
+    }
+  }
 }
 
 export default App;
