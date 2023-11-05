2db06c80e892de75fed23b73b20d1f50dafaeffb
nijuy <youjin6325@naver.com>
2023-11-05 11:43:33 +0900

    fix: ERROR_MESSAGE 객체 수정
    - 당첨 번호 예외 조건 수정에 따라 nonNumericInList 삭제

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -3,7 +3,6 @@ const ERROR_MESSAGE = Object.freeze({
   invalidAmount: '[Error] 1,000원 단위로 입력해주세요!',
   invalidListSize: '[Error] 로또 번호는 6개여야 합니다!',
   outOfRange: '[Error] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
-  nonNumericInList: '[Error] 숫자가 아닌 문자열이 포함되어 있습니다!',
   duplicateNumber: '[Error] 중복된 번호가 포함되어 있습니다!',
   duplicateWinningNumber:
     '[Error] 당첨 번호와 겹치지 않는 번호를 입력해주세요!',
