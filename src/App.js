7e848b4286a7bea5639752a93771970097fe283e
chiman <rlaclghks123@naver.com>
2023-11-05 15:39:29 +0900

    refactor: 에러 핸들링 작업 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import InputView from './view/InputView.js';
 import OutputView from './view/OutputView.js';
 
-import Validation from './util/Validation.js';
 import LottoGame from './LottoGame.js';
 import errorHandler from './util/errorHandler.js';
+import lottoPriceValidator from './validator/lottoPriceValidator.js';
+import winnigNumberValidator from './validator/winningNumberValidator.js';
+import bonusNumberValidator from './validator/bonusNumberValidator.js';
 
 class App {
   #lottoGame;
@@ -20,21 +22,36 @@ class App {
   }
 
   async #generateLottoProcess() {
-    while (true) {
-      const lottoPrice = await InputView.getLottoPrice();
-      const isValid = await errorHandler(lottoPrice, Validation.inputLottoPrice);
+    const lottoPrice = await this.#checkLottoPrice();
+    const lottos = this.#lottoGame.purchace(lottoPrice);
+    OutputView.printTotalLottos(lottos);
+  }
 
-      const lottos = this.#lottoGame.purchace(lottoPrice);
-      OutputView.printTotalLottos(lottos);
-      if (isValid) break;
-    }
+  async #checkLottoPrice() {
+    return await errorHandler(InputView.getLottoPrice, lottoPriceValidator.checkLottoPrice);
   }
 
   async #lottoResultProcess() {
-    const winnigNumbers = await InputView.getWinnigNumbers();
-    const bonusNumber = await InputView.getBonusNumber();
-    const result = this.#lottoGame.resultOfWinningDetails(winnigNumbers, bonusNumber);
+    const winningNumbers = await this.#checkWinningNumbers();
+    const bonusNumber = await this.#checkBonusNumber(winningNumbers);
+
+    const result = this.#lottoGame.resultOfWinningDetails(winningNumbers, bonusNumber);
     OutputView.printResult(result);
+  }
+
+  async #checkWinningNumbers() {
+    return await errorHandler(
+      InputView.getWinnigNumbers,
+      winnigNumberValidator.checkWinningNumbers
+    );
+  }
+
+  async #checkBonusNumber(winningNumbers) {
+    return await errorHandler(
+      InputView.getBonusNumber,
+      bonusNumberValidator.checkBonusNumber,
+      winningNumbers
+    );
   }
 
   #gameOver() {
