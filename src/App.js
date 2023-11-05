ef2b5491f350abe30ff90f8f4f0cef2780e88f3c
songudeum <songudeum@gmail.com>
2023-11-05 15:50:43 +0900

    feat: 입력한 구입 금액 처리 및 예외 처리 후 재입력 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
     async play() {
         Console.print("구입금액을 입력해 주세요.");
+        const purchaseAmount = await this.getPurchaseAmount();
+        Console.print(purchaseAmount);
     }
+
     async getPurchaseAmount() {
-        const purchaseAmount = await Console.readLineAsync().then((value) =>
-            Number(value)
-        );
-
-        this.isValidPurchaseAmount(purchaseAmount);
-
+        const purchaseAmount = await Console.readLineAsync()
+            .then((value) => {
+                this.isValidPurchaseAmount(Number(value));
+                console.log(`value: ${Number(value)}`);
+                return Number(value);
+            })
+            .catch((err) => {
+                return this.getPurchaseAmount();
+            });
         return purchaseAmount;
     }
+
     isValidPurchaseAmount(input) {
         if (isNaN(input)) {
             throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
@@ -21,6 +28,7 @@ class App {
             throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
         }
     }
+    
 }
 
 export default App;
