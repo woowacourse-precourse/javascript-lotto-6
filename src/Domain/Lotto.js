67497e2741b19295b096397f345aad5992cc03f0
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-11-05 16:41:16 +0900

    chore: Lotto 파일 위치 변경(src => Domain) 및 에러 메시지 참조오류 수정

---
+++ src/Domain/Lotto.js
@@ -0,0 +1,67 @@
+import ERROR from '../constants/error.js';
+import CustomError from '../errors/error.js';
+import Validator from '../utils/Validator.js';
+
+class Lotto {
+  static #minNumber = 1;
+
+  static #maxNumber = 45;
+
+  static #length = 6;
+
+  #numbers;
+
+  /**
+   *
+   * @param {string[]} numbers
+   */
+  constructor(numbers) {
+    this.#validate(numbers);
+
+    this.#numbers = this.#formatLotto(numbers);
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
+
+  #formatLotto(numbers) {
+    return numbers.map(Number).sort((a, b) => a - b);
+  }
+
+  #validate(numbers) {
+    this.#validateLength(numbers);
+    this.#validateLottoNumbers(numbers);
+    this.#validateDuplicate(numbers);
+  }
+
+  #validateLength(numbers) {
+    if (numbers.length !== Lotto.#length) {
+      throw CustomError.lotto(ERROR.message.lotto.length);
+    }
+  }
+
+  #validateDuplicate(numbers) {
+    if (new Set(numbers).size !== Lotto.#length) {
+      throw CustomError.lotto(ERROR.message.lotto.notUnique);
+    }
+  }
+
+  isLottoNumber(number) {
+    return (
+      Validator.isPositiveInteger(number) &&
+      number >= Lotto.#minNumber &&
+      number <= Lotto.#maxNumber
+    );
+  }
+
+  #validateLottoNumbers(numbers) {
+    const validNumbers = numbers.every(this.isLottoNumber);
+
+    if (!validNumbers) {
+      throw CustomError.lotto(ERROR.message.lotto.notInRange);
+    }
+  }
+}
+
+export default Lotto;
