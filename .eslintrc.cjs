62039b3b75b7de767c50dfa3855bd9693e8cb156
parkgeonwoong <hk77kh@gmail.com>
2023-11-05 22:07:04 +0900

    chore : eslint jest 새로운 규칙 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,23 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['prettier', 'eslint:recommended'],
+  extends: ['prettier', 'eslint:recommended', 'plugin:jest/recommended'],
   overrides: [
     {
       env: {
         node: true,
+        jest: true,
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script',
+      },
+    },
+    {
+      files: ['**/__tests__/*.js'],
+      rules: {
+        'max-depth': 'off',
+        'max-lines-per-function': 'off',
       },
     },
   ],
