4299f78ba1f897f0946eb55b63ce96c1fd32e3dc
nijuy <youjin6325@naver.com>
2023-11-05 11:15:40 +0900

    refactor(App): 로또 발행을 담당하는 generateLottos() 생성
    - play() 길이를 줄이기 위해 수량만큼 로또를 발행하는 작업을 별도 함수로 분리함

--- src/App.js
+++ src/App.js
@@ -20,18 +20,22 @@ class App {
     }
   }
 
-  async play() {
-    const payment = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
-    App.validatePayment(payment);
-    const amount = payment / LOTTO_PRICE;
-    Console.print(`\n${amount}개를 구매했습니다.`);
-
+  generateLottos(amount) {
     for (let i = 0; i < amount; i += 1) {
       const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
       numbers.sort((a, b) => a - b);
       this.lottos.push(new Lotto(numbers));
       this.lottos[i].print();
     }
+  }
+
+  async play() {
+    const payment = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    App.validatePayment(payment);
+    const amount = payment / LOTTO_PRICE;
+    Console.print(`\n${amount}개를 구매했습니다.`);
+
+    this.generateLottos(amount);
   }
 }
 
