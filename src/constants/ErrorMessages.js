1605611c7e5e1683b5bd3e7c53cf9400174f9a2a
Trophy198 <spsp8426@naver.com>
2023-11-05 23:20:46 +0900

    feat : 에러 메시지를 담은 상수 파일 추가

---
+++ src/constants/ErrorMessages.js
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGES = {
+  EMPTY_AMOUNT: '[ERROR] 구입 금액을 입력해야 합니다.',
+  AMOUNT_NOT_IN_UNITS: '[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.',
+  EMPTY_NUMBER: '[ERROR] 모든 당첨 번호를 쉼표(,)로 구분하여 입력해야 합니다.',
+  NOT_SIX_NUMBERS: '[ERROR] 당첨 번호는 쉼표(,)로 구분된 6개의 숫자여야 합니다.',
+  NUMBER_OUT_OF_RANGE: '[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.',
+  DUPLICATE_NUMBER: '[ERROR] 당첨 번호에 중복된 숫자가 있습니다.',
+  BONUS_NUMBER_DUPLICATE: '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
+};
