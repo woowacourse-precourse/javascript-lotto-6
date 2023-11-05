39f0f2f21d63062a899fe984776c653657daf722
Jeongeun Lee <leeju1009@gmail.com>
2023-11-05 21:01:54 +0900

    refactor: Validation 클래스를 객체 리터럴로 리팩토링

--- src/Validation.js
+++ src/Validation.js
@@ -1,45 +1,30 @@
-class Validation {
-  isValidWinningNumbers(numbers) {
-    numbers.forEach((number) => {
-      this.isNumber(number);
-      this.isNumberInRange(number);
-    });
-
-    this.isNonDuplicatedNumber(numbers);
-  }
-
-  isValidBonusNumber(bonusNumber, winningNumbers) {
-    this.isNumber(bonusNumber);
-    this.isNumberInRange(bonusNumber);
-    this.isNonDuplicatedBonusNumber(bonusNumber, winningNumbers);
-  }
-
-  isNumber(number) {
+const Validation = {
+  isNumber: (number) => {
     if (Number.isNaN(number)) {
       throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
     }
-  }
+  },
 
-  isNonDuplicatedNumber(numbers) {
+  isNonDuplicatedNumber: (numbers) => {
     const nonDuplicatedNumbers = new Set(numbers);
     if (nonDuplicatedNumbers.size !== numbers.length) {
       throw new Error('[ERROR] 중복된 숫자는 입력할 수 없습니다.');
     }
-  }
+  },
 
-  isNonDuplicatedBonusNumber(bonusNumber, winningNumbers) {
+  isNonDuplicatedBonusNumber: (bonusNumber, winningNumbers) => {
     if (winningNumbers.includes(bonusNumber)) {
       throw new Error(
         '[ERROR] 당첨 번호와 중복되는 숫자는 입력할 수 없습니다.',
       );
     }
-  }
+  },
 
-  isNumberInRange(number) {
+  isNumberInRange: (number) => {
     if (number < 1 || number > 45) {
       throw new Error('[ERROR] 1이상 45이하의 숫자만 입력할 수 있습니다.');
     }
-  }
-}
+  },
+};
 
 export default Validation;
