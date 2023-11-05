5265f346be27a4b5007414b48baefd34ae004c46
leehyewon0531 <ifpffk@naver.com>
2023-11-05 20:44:32 +0900

    feat: 구입 금액 1000으로 나누어 떨어지는지 여부 판별 함수 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -3,8 +3,11 @@ import Validation from "./Validation.js";
 
 class LottoGame {
   async start() {
-    const amount = await IO.receiveUserInput('구입금액을 입력해 주세요.\n');
-    Validation.isNumber(amount);
+    const amountStr = await IO.receiveUserInput('구입금액을 입력해 주세요.\n');
+    Validation.isNumber(amountStr);
+
+    const amount = Number(amountStr);
+    Validation.isDivisible(amount);
   }
 }
 
--- src/Validation.js
+++ src/Validation.js
@@ -3,6 +3,10 @@ class Validation {
     const regEx = /^[1-9]\d*$/;
     if(!value.match(regEx)) throw new Error('[ERROR] 구입 금액은 0으로 시작하지 않는 숫자 형식입니다.');
   }
+
+  static isDivisible(num) {
+    if(num % 1000 !== 0) throw new Error('[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.')
+  }
 }
 
 export default Validation;
