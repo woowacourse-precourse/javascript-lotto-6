dacc6237d6aa037fb00990db4993a9a4092e6646
김다인 <dbslzhs1218@naver.com>
2023-11-05 17:22:04 +0900

    feat(lotto): 로또 구입 기능 구현
    - 로또 구입 금액 입력 기능 구현
    - 입력한 금액 유효성 검증 구현
    - 입력한 금액에 따라 로또 개수 구하는 기능 구현
    - 에러 메세지 상수화

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+import { prompt, result } from './constants/message.js';
+import Buyer from './Buyer.js';
+
 class App {
+  buyer = new Buyer();
+
   async play() {
-    await this.null;
+    const expense = await Console.readLineAsync(prompt.ASK_AMOUNT);
+    const lotteryTicketCount = this.buyer.buyLottoCount(expense);
+
+    Console.print(`\n${lotteryTicketCount}${result.PURCHASE}`);
   }
 }
 
--- src/Buyer.js
+++ src/Buyer.js
@@ -1,6 +1,30 @@
+import error from './constants/error.js';
+
 class Buyer {
-  buyLottoCount() {
-    return this.null;
+  expense;
+
+  buyLottoCount(expense) {
+    this.expense = Number(expense);
+
+    Buyer.validationExpense(this.expense);
+
+    const count = this.expense / 1000;
+
+    return count;
+  }
+
+  static validationExpense(expense) {
+    if (/[^0-9]/.test(expense)) {
+      throw new Error(error.EXPENSE_INTEGER);
+    }
+
+    if (expense < 1000) {
+      throw new Error(error.EXPENSE_MORE_THEN_ONE_THOUSAND_WON);
+    }
+
+    if (expense % 1000 !== 0) {
+      throw new Error(error.EXPENSE_UNIT_ONE_THOUSAND_WON);
+    }
   }
 }
 export default Buyer;
