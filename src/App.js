fd2b38bd234664d58076a8292451f6723014c56d
yheseul_kang <kangyheseul@naver.com>
2023-11-06 00:52:35 +0900

    Feat: 로또 번호 출력 기능 적용 // 발행한 로또 수량 만큼 번호 출력(로또 번호는 오름차순으로 정렬) 기능 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
 import User from "./User.js";
+import Computer from "./Computer.js";
 
 class App {
   async play() {
-    await User.inputLottoPurchaseAmout();
+    const purchaseAmount = await User.inputLottoPurchaseAmout();
     await User.inputWinningNumber();
     await User.inputBonusNumber();
+
+    const purchaseNumber = Computer.purchaseList(purchaseAmount);
+    Console.print(`${purchaseNumber}개를 구매했습니다.`);
+    Computer.randomNumberList(purchaseNumber);
   }
 }
-
-export default App;
+const app = new App();
+app.play();
+// export default App;`
