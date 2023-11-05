0cd931022e5c7d10b3c4afd2d28358a8f7ce65e2
minSsan <msp1125@hanyang.ac.kr>
2023-11-05 23:44:01 +0900

    feat: 당첨 로또 갯수 확인 메소드(get_winning_count) 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,7 +12,28 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  /**
+   * 주어진 값이 로또에 존재하는지 확인하는 메소드
+   * @param {int} number
+   * @returns {boolean} is_include
+   */
+  #is_include(number) {
+    return this.#numbers.includes(number);
+  }
+
+  /**
+   * 주어진 로또와 몇 개의 숫자가 일치하는지 리턴하는 메소드
+   * @param {int[]} numbers
+   * @returns {int} winning
+   */
+  get_winning_count(numbers) {
+    const WINNING = numbers.reduce(
+      (accumulator, current) => accumulator + this.#is_include(current),
+      0
+    );
+
+    return WINNING;
+  }
 }
 
 export default Lotto;
