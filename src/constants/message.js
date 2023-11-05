3a023acafbd167530f5f1b68b415686fdd4b3a97
pjc0179 <pjc0179@gmail.com>
2023-11-06 02:18:20 +0900

    feat(constants): message.js 프로퍼티추가
    - 로또의 구매 수를 표시하는 BUY_MESSAGE추가
    - 로또 당첨 통계를 출력하는 totalWinning추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -2,7 +2,8 @@ const MESSAGES = Object.freeze({
   USER_INPUT_LOTTO_TOTAL_BUY_PRICE: '구입금액을 입력해 주세요.\n',
   USER_INPUT_LOTTO_CORRECT_NUMBER: '당첨 번호를 입력해 주세요.\n',
   USER_INPUT_LOTTO_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
-  BUY_MESSAGE: '개를 구매했습니다.\n',
+  BUY_MESSAGE: '개를 구매했습니다.',
+  totalWinning: '당첨 통계 \n---\n',
 });
 
 export default MESSAGES;
