fd53efa7639c3792d6bdd58f00f9df293242b24c
hamo-o <yheel369@gmail.com>
2023-11-05 15:54:18 +0900

    feat: 구입 금액이 숫자인지 판단하는 유효성 검사 추가

--- src/Validate.js
+++ src/Validate.js
@@ -1,6 +1,9 @@
 const Validate = {
   checkPurchaseAmount(price) {
-    if (price < 1000) {
+    const pattern = /^[0-9]+$/;
+    if (!pattern.test(price)) {
+      throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
+    } else if (price < 1000) {
       throw new Error('[ERROR] 구입 금액은 최소 1000원 이상이어야 합니다.');
     } else if (price % 1000) {
       throw new Error('[ERROR] 구입 금액은 1000원 단위여야 합니다.');
