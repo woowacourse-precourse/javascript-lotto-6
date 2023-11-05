9fd3513dd189e333acb8846100ac59b6e59791ae
SSY1203 <tlstjsdud566@naver.com>
2023-11-05 21:40:46 +0900

    [feat] 구입 금액 문자열일 경우, 예외 발생

--- src/Constant.js
+++ src/Constant.js
@@ -2,7 +2,8 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 export const { Console, Random } = MissionUtils;
 
-export const PLZ_PURCHASE_AMOUNT = '구입금액을 입력해 주세요.';
+export const PAYMENT_UNIT = 1000;
+export const PLZ_PURCHASE_AMOUNT = '구매금액을 입력해 주세요.';
 export const PLZ_WINNING_NUMBER = '당첨 번호를 입력해 주세요.';
 export const PLZ_BONUS_NUMBER = '보너스 번호를 입력해 주세요.';
 export const PRINT_PURCHASE_COUNT = '개를 구매했습니다.';
@@ -14,9 +15,9 @@ export const FIVE_AND_BONUS_MATCHES = '5개 일치 (30,000,000원) - ';
 export const SIX_MATCHES = '6개 일치 (2,000,000,000원) - ';
 export const TOTAL_REVENUE = '총 수익률은 ';
 
-export const ERROR_PURCHASE_AMOUNT_UNIT = '[ERROR] 로또 구입 금액은 1,000원 단위입니다.';
+export const ERROR_PURCHASE_AMOUNT_UNIT = '[ERROR] 로또 구매 금액은 1,000원 단위입니다.';
 export const ERROR_PURCHASE_AMOUNT_PRICE = '[ERROR] 로또 1장의 가격은 1,000원부터 입니다.';
-export const ERROR_PURCHASE_AMOUNT_STRING = '[ERROR] 로또 구입 금액은 숫자여야 합니다.';
+export const ERROR_PURCHASE_AMOUNT_STRING = '[ERROR] 로또 구매 금액은 숫자여야 합니다.';
 
 export const ERROR_LOTTO_REPEAT = '[ERROR] 로또 번호는 중복될 수 없습니다.';
 export const ERROR_LOTTO_SIX_NUMBERS = '[ERROR] 로또 번호는 6자리여야 합니다.';
--- src/InputView.js
+++ src/InputView.js
@@ -1,9 +1,24 @@
-import { Console, PLZ_PURCHASE_AMOUNT } from './Constant';
+import { Console, ERROR_PURCHASE_AMOUNT_STRING, PLZ_PURCHASE_AMOUNT } from './Constant';
 
 const InputView = {
   async readPurchaseAmount() {
-    const answer = await Console.readLineAsync(PLZ_PURCHASE_AMOUNT);
-    return answer;
+    while (true) {
+      try {
+        const answer = await Console.readLineAsync('로또 구입 금액을 입력하세요: ');
+        this.paymentAmountNumberValidator(answer);
+
+        return answer;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  },
+
+  paymentAmountNumberValidator(answer) {
+    const purchaseAmount = Number(answer);
+    if (isNaN(purchaseAmount) || purchaseAmount % 1000 !== 0) {
+      throw new Error(ERROR_PURCHASE_AMOUNT_STRING);
+    }
   },
 };
 
