4f5d21286f8f739e577fa1692358023f8794504d
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-05 23:47:05 +0900

    Refactor: 보너스 번호 입력 함수 이동

--- src/LottoResultChecker.js
+++ src/LottoResultChecker.js
@@ -8,6 +8,9 @@ class LottoResultChecker {
   async inputWinningLottoNum() {
     return await Console.readLineAsync("당첨 번호를 입력해주세요.");
   }
+  async inputBonusNumber() {
+    return await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+  }
 }
 
 export default LottoResultChecker;
