9f4ac6b0aedb4c1a42d9b81d3574d11ccb2de5e7
donggyun <donggyunkim12@gmail.com>
2023-11-05 15:28:31 +0900

    add(message): 에러 메시지 추가

--- src/constant/message.js
+++ src/constant/message.js
@@ -3,13 +3,15 @@ export const ERROR_MESSAGE = Object.freeze({
   INVALID_NON_DIVISIBLE: '[ERROR] 금액은 1000원 단위여야 합니다.',
   INVALID_LOTTO_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
   INVALID_LOTTO_UNIQUE: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
+  INVALID_LOTTO_BONUS:
+    '[ERROR] 보너스 번호는 로또 번호와 중복되지 않아야 합니다.',
   INVALID_LOTTO_RANGE: '[ERROR] 로또 번호는 1~45 사이여야 합니다.',
 });
 
 export const INPUT_MESSAGE = Object.freeze({
   INPUT_MONEY: '구입금액을 입력해 주세요.\n',
-  INPUT_WINNER_NUMBER: '당첨 번호를 입력해 주세요.\n',
-  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+  INPUT_WINNER_NUMBER: '\n당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
 });
 
 export const OUTPUT_MESSAGE = Object.freeze({
