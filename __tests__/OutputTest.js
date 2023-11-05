e1bf2f37aec6ab7f322965443b4bb544f084b7df
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-11-06 03:04:36 +0900

    test: Output 로또 구입 내역 출력 test

---
+++ __tests__/OutputTest.js
@@ -0,0 +1,67 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import Input from "../src/Input";
+import Output from "../src/Output";
+import Player from "../src/model/Player";
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("Output class 출력 테스트", () => {
+  test("사용자가 로또를 8개 구입했을때 출력 테스트", async () => {
+    //given
+    const INPUT_STRING = ["8000"];
+    const logSpy = getLogSpy();
+
+    //when
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+    mockQuestions(INPUT_STRING);
+
+    //then
+    const logs = [
+      "8개를 구매했습니다.",
+      "[8, 21, 23, 41, 42, 43]",
+      "[3, 5, 11, 16, 32, 38]",
+      "[7, 11, 16, 35, 36, 44]",
+      "[1, 8, 11, 31, 41, 42]",
+      "[13, 14, 16, 38, 42, 45]",
+      "[7, 11, 30, 40, 42, 43]",
+      "[2, 13, 22, 32, 38, 45]",
+      "[1, 3, 5, 14, 22, 45]",
+    ];
+    const money = await Input.getMoney();
+    const player = new Player(money);
+    Output.printLotto(player);
+    logs.forEach((log) =>
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log))
+    );
+  });
+});
