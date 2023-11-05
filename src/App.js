70bf7a2c261fdfe490ab1b6b3e177457b3c2db73
noodle <noodle>
2023-11-06 02:36:05 +0900

    refactor: App.js 예외 처리를 play에서만 진행, 불필요하게 반복되는 함수 분리

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Lotto from './Domain/Lotto.js';
 import LottoNumberCreation from './Domain/LottoNumberCreation.js';
 import MESSAGE from './constants/message.js';
 import { MessageFormat } from './utils/messageFormat.js';
+import { calcaulateTicketCountFromAmount } from './utils/index.js';
 
 class App {
   #lotto;
+  #winningNumbers;
+  #bonusNumber;
 
   constructor() {
     this.validatedAmount = 0;
@@ -15,18 +18,29 @@ class App {
 
   async play() {
     try {
-      const amount = await this.inputUserPurchaseAmount();
-      const lottoTickets = this.purchaseLotto(amount);
+      const purchaseAmount = await this.inputWithRetry(this.inputUserPurchaseAmount.bind(this));
+      const lottoTickets = this.purchaseLotto(purchaseAmount);
 
-      // 6. (출력) 당첨 내역 출력
-      const userLotto = await this.inputUserLotto();
-      this.#lotto = new Lotto(userLotto.winningNumbers.split(',').map(Number));
-      this.#lotto.compareWinningAndLotto(userLotto.bonusNumber, lottoTickets);
+      this.#winningNumbers = await this.inputWithRetry(this.inputUserWinningNumbers.bind(this));
+      this.#bonusNumber = await this.inputWithRetry(this.inputUserBonusNumber.bind(this));
+
+      this.#lotto = new Lotto(this.#winningNumbers.split(LOTTO.string.comma).map(Number));
+      this.#lotto.compareWinningAndLotto(this.#bonusNumber, lottoTickets);
       this.#lotto.printTotalResult();
 
       this.resultReturnRate();
     } catch (error) {
-      Console.print(`"play" ${error.message}`);
+      Console.print(`${error.stack} ${error.message}`);
+    }
+  }
+
+  async inputWithRetry(inputMethod) {
+    while (true) {
+      try {
+        return await inputMethod();
+      } catch (error) {
+        Console.print(`${error.message}`);
+      }
     }
   }
 
@@ -34,20 +48,15 @@ class App {
    * 1. (입력) 로또 구입 금액 입력
    */
   async inputUserPurchaseAmount() {
-    while (true) {
-      try {
-        const amount = await Console.readLineAsync(MESSAGE.input.PurchaseAmount);
-        this.validatedAmount = ExceptionHandler.validateAmount(amount);
-        return this.calculateTicketCountFromAmount(this.validatedAmount);
-      } catch (error) {
-        Console.print(`${error.message}`);
-      }
-    }
+    const amount = await Console.readLineAsync(MESSAGE.input.PurchaseAmount);
+    this.validatedAmount = ExceptionHandler.validateAmount(amount);
+    return calcaulateTicketCountFromAmount(this.validatedAmount);
   }
 
-  calculateTicketCountFromAmount(validatedAmount) {
-    return validatedAmount / LOTTO.unit.unit;
-  }
+  // utils로 빼는 것이 좋겠..다?
+  // calculateTicketCountFromAmount(validatedAmount) {
+  //   return validatedAmount / LOTTO.unit.unit;
+  // }
 
   /**
    * 2. (출력) 로또 구매
@@ -58,17 +67,17 @@ class App {
     return this.purchaseLottoTickets(ticketCount);
   }
 
-  /**
-   * 2.1. 입력한 금액에 따른 구매 갯수
-   */
   printPurchaseConfirm(ticketCount) {
+    /**
+     * 2.1. 입력한 금액에 따른 구매 갯수
+     */
     Console.print(MessageFormat.purchaseConfirm(ticketCount));
   }
 
-  /**
-   * 2.2 (출력) 구매 갯수만큼 로또 번호 출력
-   */
   purchaseLottoTickets(ticketCount) {
+    /**
+     * 2.2 (출력) 구매 갯수만큼 로또 번호 출력
+     */
     const lottoTickets = LottoNumberCreation.generateLottoTickets(ticketCount);
     lottoTickets.forEach((ticket) => this.printLottoTickets(ticket));
 
@@ -83,29 +92,14 @@ class App {
    * 3. (입력)
    * -  당첨 번호 6개와 보너스 번호 입력 받기
    */
-
-  async inputUserLotto() {
-    while (true) {
-      try {
-        const userLotto = await this.getUserLottoInput();
-        return userLotto;
-      } catch (error) {
-        Console.print(`${error.message}`);
-      }
-    }
+  async inputUserWinningNumbers() {
+    const inputWinningNumbers = await Console.readLineAsync(MESSAGE.input.WinningNumbers);
+    return ExceptionHandler.validateWinningNumbers(inputWinningNumbers);
   }
 
-  async getUserLottoInput() {
-    const inputWinningNumbers = await Console.readLineAsync(MESSAGE.input.WinningNumbers);
-    const winningNumbers = ExceptionHandler.validateWinningNumbers(inputWinningNumbers);
-
+  async inputUserBonusNumber() {
     const inputBonusNumber = await Console.readLineAsync(MESSAGE.input.BonusNumbers);
-    const bonusNumber = ExceptionHandler.validateBonusNumber(inputBonusNumber, winningNumbers);
-
-    return {
-      winningNumbers,
-      bonusNumber,
-    };
+    return ExceptionHandler.validateBonusNumber(inputBonusNumber, this.#winningNumbers);
   }
 
   /**
