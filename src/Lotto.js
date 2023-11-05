698c9bbac86f97261aa3f903b722cfb21d04f183
Kysiam <how2logout@gmail.com>
2023-11-05 22:04:34 +0900

    test: lotto class validation 중복 숫자 test

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,10 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    if (new Set(numbers).size <= 5) {
+      throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    }
   }
 
   // TODO: 추가 기능 구현
