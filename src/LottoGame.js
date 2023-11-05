9b1120c06f672d6faab166e17ad9c2bf16a09f6f
SSY1203 <tlstjsdud566@naver.com>
2023-11-06 00:01:29 +0900

    [feat] 발행된 로또 수량 및 번호 출력

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,10 +1,43 @@
+import { PRICE_UNIT, Random } from './Constant';
 import InputView from './InputView';
+import Lotto from './Lotto';
+import OutputView from './OutputView';
 
 class LottoGame {
   #purchaseAmount;
+  #lottoList;
+
+  constructor() {
+    this.#lottoList = [];
+  }
 
   async gameStart() {
-    this.#purchaseAmount = InputView.readPurchaseAmount();
+    this.#lottoList = [];
+    this.#purchaseAmount = await InputView.readPurchaseAmount();
+    const lottoCount = this.#purchaseAmount / PRICE_UNIT;
+    for (let i = 0; i < lottoCount; i++) {
+      const randomNumbers = this.getRandomNumbers();
+      this.#lottoList.push(new Lotto(randomNumbers));
+    }
+    OutputView.printLottoCount(this.#lottoList);
+    this.#lottoList.map(lotto => {
+      OutputView.printLotto(lotto.numbers);
+    });
+  }
+
+  getRandomNumbers() {
+    const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    const sortedList = this.sortRandomNumbers(randomNumbers);
+    return sortedList;
+  }
+
+  sortRandomNumbers(numbers) {
+    const sortedList = numbers.sort((a, b) => a - b);
+    return sortedList;
+  }
+
+  get lottoList() {
+    return this.#lottoList;
   }
 }
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,13 @@
-const OutputView = {};
+import { Console, PRINT_PURCHASE_COUNT } from './Constant';
+
+const OutputView = {
+  printLottoCount(lottoList) {
+    Console.print(`${lottoList.length}${PRINT_PURCHASE_COUNT}`);
+  },
+
+  printLotto(lotto) {
+    Console.print(lotto);
+  },
+};
 
 export default OutputView;
