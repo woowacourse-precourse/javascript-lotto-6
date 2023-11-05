dbbf5ebfffd97124bd215624b07fa078e4e0a142
Lee sang Yeop <mma7710@naver.com>
2023-11-05 17:03:20 +0900

    chore(constants): 로또 시스템에 필요한 상수 선언 - 로또 가격 - 로또 번호의 숫자 범위 - 로또 1등~5등 기준 - 로또 1등~5등 상금

---
+++ src/constants/LottoSystem.js
@@ -0,0 +1,23 @@
+const LOTTO_SYSTEM = Object.freeze({
+	lotto_price: 1000,
+
+	minimum_number: 1,
+	maximum_number: 45,
+	lotto_numbers: 6,
+
+	first_winning_number: 6,
+	second_winning_number: 5,
+	second_winning_bonus: true,
+	third_winning_number: 5,
+	third_winning_bonus: true,
+	fourth_winning_number: 4,
+	fifth_winning_number: 3,
+
+	first_place_prize: 2000000000,
+	second_place_prize: 30000000,
+	third_place_prize: 1500000,
+	fourth_place_prize: 50000,
+	fifth_place_prize: 5000,
+});
+
+export default LOTTO_SYSTEM;
