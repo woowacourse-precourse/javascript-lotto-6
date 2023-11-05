1c1b609f74b4f8d35a1c26630f80c27b6425a64d
0jenn0 <jenn0.6n@gmail.com>
2023-11-06 05:45:25 +0900

    Refactor: 일치갯수 계산하는 메서드에서 보너스 번호 가지고있는지도 함께 객체로 리턴하도록 수정함

--- src/service/LottoYieldCalculator.js
+++ src/service/LottoYieldCalculator.js
@@ -1,54 +1,12 @@
 import LOTTO_SETTINGS from "../config/gameSetting.js";
 
 export default class LottoYieldCalculator {
-  static calculateYield(lottos, drawnLotto) {
-    const totalPrize = this.getTotalPrize(lottos, drawnLotto);
-    const lottosPrice = lottos.length * LOTTO_SETTINGS.TICKET_PRICE;
-    const yieldRate = (totalPrize / lottosPrice) * 100;
-
-    return Number(yieldRate.toFixed(2));
-  }
-
-  static getTotalPrize(lottos, drawnLotto) {
-    const totalPrize = lottos.reduce((accumulator, lotto) => {
-      return accumulator + this.calculate(lotto, drawnLotto);
-    }, 0);
-
-    return totalPrize;
-  }
-
-  static calculate(lotto, drawnLotto) {
-    const lottoNumbers = lotto.getNumbers();
-    const { numbers, bonusNumber } = drawnLotto.getFullNumbers();
-
-    const matchCount = this.#getMatchCount(lottoNumbers, numbers);
-    if (matchCount === 5 && lottoNumbers.includes(bonusNumber)) {
-      return LOTTO_SETTINGS.PRIZES.SECOND;
-    }
-    return this.#getPrize(matchCount);
-  }
-
-  static #getMatchCount(lottoNumbers, numbers) {
-    const matchCount = lottoNumbers.filter((lottoNumber) =>
-      numbers.includes(lottoNumber)
+  static checkMatchCount(lottonumbers, { drawnLottoNumbers, bonusNumber }) {
+    const matchCount = lottonumbers.filter((number) =>
+      drawnLottoNumbers.includes(number)
     ).length;
-    return matchCount;
-  }
+    const hasBonusNumber = lottonumbers.includes(bonusNumber);
 
-  static #getPrize(matchCount) {
-    const { NUMBERS_PER_TICKET, PRIZES } = LOTTO_SETTINGS;
-
-    switch (matchCount) {
-      case NUMBERS_PER_TICKET - 3:
-        return PRIZES.FIFTH;
-      case NUMBERS_PER_TICKET - 2:
-        return PRIZES.FOURTH;
-      case NUMBERS_PER_TICKET - 1:
-        return PRIZES.THIRD;
-      case NUMBERS_PER_TICKET:
-        return PRIZES.FIRST;
-      default:
-        return 0;
-    }
+    return { matchCount, hasBonusNumber };
   }
 }
