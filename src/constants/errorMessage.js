6f1c20692f7c74de51b3fa3acedb6fb80e41ac9c
JiSeong Kim <12180543@inha.edu>
2023-11-06 04:51:43 +0900

    feat(constants): add error messages
    - 자연수가 아닌 수가 들어왔을 경우 에러 메시지
    - 형변환으로 인한 예기치 못한 입력이 들어왔을 경우 에러 메시지

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -5,7 +5,9 @@ const errorMessage = Object.freeze({
 	NOT_ENOUGH_PURCHASE_AMOUNT: '[ERROR] 금액이 부족합니다.',
 	DUPLICATED_NUMBER: '[ERROR] 중복되지 않은 숫자를 입력해주세요.',
 	OUT_OF_RANGE: '[ERROR] 1부터 45사이의 숫자만 입력해주세요.',
-	ALREADY_PICKED_NUMBER: '[ERROR] 당첨 번호로 선택하지 않은 수 중에 입력해주세요.'
+	ALREADY_PICKED_NUMBER: '[ERROR] 당첨 번호로 선택하지 않은 수 중에 입력해주세요.',
+	NOT_NATURAL_NUMBER: '[ERROR] 자연수만 입력해주세요.',
+	UNUSUAL_INPUT: '[ERROR] 올바른 형태로 입력해주세요.',
 });
 
 export default errorMessage;
