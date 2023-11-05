a3b555e5cf5cdc05189f72d160fc3254fa48e22b
Lee sang Yeop <mma7710@naver.com>
2023-11-06 01:16:08 +0900

    feat(Lotto): 로또 번호를 가지는 로또 클래스 추가 로또 고유 번호를 가지는 클래스 - 로또 클래스의 값 유효성 검사를 앞에서 만든 validators 로 검사하도록 변경 - 로또 고유 번호를 출력하기 위해 getter 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,16 @@
+import validators from './utils/validators.js';
+
 class Lotto {
-  #numbers;
+	#numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
+	constructor(numbers) {
+		validators.checkLottoNumbers(numbers);
+		this.#numbers = numbers;
+	}
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
-
-  // TODO: 추가 기능 구현
+	get numbers() {
+		return this.#numbers;
+	}
 }
 
 export default Lotto;
