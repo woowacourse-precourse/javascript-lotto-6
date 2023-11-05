91eacba75fec918c7d8b3d70c90df80c9feba6fa
Majesty-jun <hkwns01@gmail.com>
2023-11-05 23:53:27 +0900

    INIT: ESLint, Prettier, husky 프로젝트 초기 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': 0,
+  },
+};
