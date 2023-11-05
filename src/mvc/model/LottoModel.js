7dd6a22f07f6a1464c929ffd5ea820e4a60da2f1
ss0526100 <ss0526100@gmail.com>
2023-11-05 21:27:00 +0900

    feat: set purchased lotto array

--- src/mvc/model/LottoModel.js
+++ src/mvc/model/LottoModel.js
@@ -1,3 +1,15 @@
-class LottoModel {}
+import Get from './utils/Get.js';
+
+class LottoModel {
+  constructor() {
+    this.purchasedLottoArray;
+    this.lottoBoard;
+    this.lottoResult;
+  }
+
+  setPurchasedLottoArray(numberOfLotto) {
+    this.purchasedLottoArray = Get.randomLottoArray(numberOfLotto);
+  }
+}
 
 export default LottoModel;
