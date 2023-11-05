36c901d70551ca65423c45695658f072c4ca9d38
badahertz52 <badahertz52@gmail.com>
2023-11-05 14:35:00 +0900

    feat: 입력과 관련된 유틸 함수들 생성
    - utils/Input.js 파일 생성
    - readInput, readWinningNumbers, readBonusNumber, readPayment 함수 생성

---
+++ src/utils/Input.js
@@ -0,0 +1,42 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { MESSAGE } from '../constant';
+import { hasNoReset, throwError } from '.';
+
+const readInput = async (query) => {
+  try {
+    const value = await MissionUtils.Console.readLineAsync(query);
+    return value.replaceAll(' ', '');
+  } catch (error) {
+    throwError(error);
+  }
+};
+
+const readWinningNumbers = async () => {
+  try {
+    const value = await readInput(MESSAGE.winningNumbersQuery);
+    hasNoReset(value);
+    return value.split(',').map((v) => Number(v));
+  } catch (error) {
+    throwError(error);
+  }
+};
+
+const readBonusNumber = async () => {
+  try {
+    const value = await readInput(MESSAGE.bonusBallNumberQuery);
+    return Number(value);
+  } catch (error) {
+    throwError(error);
+  }
+};
+
+const readPayment = async () => {
+  try {
+    const value = await readInput(MESSAGE.paymentQuery);
+    return Number(value);
+  } catch (error) {
+    throwError(error);
+  }
+};
+
+export { readInput, readBonusNumber, readPayment, readWinningNumbers };
