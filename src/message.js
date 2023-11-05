69e557eaa382d2dc4d01ac1540c27ca25445e1f4
Moon Sojeong <sjeong35@naver.com>
2023-11-05 17:18:11 +0900

    fix: 변수명 달라서 생긴 오류 해결

--- src/message.js
+++ src/message.js
@@ -7,15 +7,15 @@ const ERROR_MESSAGE = Object.freeze({
 });
 
 const PRINT_MESSAGE = Object.freeze({
-    PURCHASE : (amount) => `${this.lottoTicket}개를 구매했습니다.`,
+    PURCHASE : (amount) => `${amount}개를 구매했습니다.`,
     MATCH_STATS : '당첨 통계\n',
     LINE : '---\n',
-    THREE_MATCHES : (match) => `3개 일치 (5,000원) - ${lottoResult[0]}개\n`,
-    FOUR_MATCHES : (match) => `4개 일치 (50,000원) - ${lottoResult[1]}개\n`,
-    FIVE_MATCHES : (match) => `5개 일치 (1,500,000원) - ${lottoResult[2]}개\n`,
-    FIVE_BONUS : (match) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult[3]}개\n`,
-    SIX_MATCHES : (match) => `6개 일치 (2,000,000,000원) - ${lottoResult[4]}개\n`,
-    TOTAL_RATE : (rate) => `총 수익률은 ${lottoRate}%입니다.\n`,
+    THREE_MATCHES : (match) => `3개 일치 (5,000원) - ${match}개\n`,
+    FOUR_MATCHES : (match) => `4개 일치 (50,000원) - ${match}개\n`,
+    FIVE_MATCHES : (match) => `5개 일치 (1,500,000원) - ${match}개\n`,
+    FIVE_BONUS : (match) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${match}개\n`,
+    SIX_MATCHES : (match) => `6개 일치 (2,000,000,000원) - ${match}개\n`,
+    TOTAL_RATE : (rate) => `총 수익률은 ${rate}%입니다.\n`,
 });
 
 export {ERROR_MESSAGE, PRINT_MESSAGE};
