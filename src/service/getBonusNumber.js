94c6934f26417624ecd2ed48659599de3c718397
thumbthing <ilscc0902@gmail.com>
2023-11-06 01:41:10 +0900

    feat: 유효성 검사에 당첨번호와 비교 기능 추가, 매개변수 추가

--- src/service/getBonusNumber.js
+++ src/service/getBonusNumber.js
@@ -1,10 +1,10 @@
 import receiveBonusNumberUI from '../util/UI/gameStart/receiveBonusNumberUI.js';
 import bonusNumberValidService from './validate/bonusNumberValidService.js';
 
-export default async function getBonusNumber() {
+export default async function getBonusNumber(winningNumber) {
   try {
     const input = await receiveBonusNumberUI();
-    await bonusNumberValidService(input);
+    await bonusNumberValidService(input, winningNumber);
     const bonusNumber = Number(input);
     return bonusNumber;
   } catch (error) {
--- src/service/validate/bonusNumberValidService.js
+++ src/service/validate/bonusNumberValidService.js
@@ -1,14 +1,16 @@
 import { checkIsEmpty } from '../../util/validate/checkIsEmpty.js';
+import checkIsInWinningNumber from '../../util/validate/checkIsInWinningNumber.js';
 import { checkIsNaN } from '../../util/validate/checkIsNaN';
 import { checkNumberIsNotInRange } from '../../util/validate/checkIsNotInRange';
 import { checkIsInteger } from '../../util/validate/checkisInteger';
 
-export default async function bonusNumberValidService(verifyValue) {
+export default async function bonusNumberValidService(verifyValue, winningNumber) {
   try {
     checkIsEmpty(verifyValue);
     checkIsNaN(verifyValue);
     checkIsInteger(verifyValue);
     checkNumberIsNotInRange(verifyValue);
+    checkIsInWinningNumber(verifyValue, winningNumber);
   } catch (error) {
     throw new Error(error.message);
   }
