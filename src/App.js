381106f7a304260348d0d4e2faa0f9ba3c3bef0e
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-11-05 17:07:31 +0900

    fix : App.js에 있던 LottoPutchase 클래스를 Lotto.js로 옮김

--- src/App.js
+++ src/App.js
@@ -4,44 +4,4 @@ class App {
   async play() {}
 }
 
-class LottoPurchase {
-  async #getLottoAmount() {
-    const USER_INPUT =
-      await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
-    const USER_INPUT_PRICE = parseInt(USER_INPUT, 10);
-    return USER_INPUT_PRICE;
-  }
-
-  #validateAmount(lottoAmount) {
-    if (
-      isNaN(lottoAmount) ||
-      !lottoAmount ||
-      lottoAmount < 1000 ||
-      lottoAmount % 1000 !== 0
-    ) {
-      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
-    }
-  }
-
-  async #purchaseLottoAmount() {
-    let purchaseAmount;
-
-    while (true) {
-      try {
-        purchaseAmount = await this.#getLottoAmount();
-        this.#validateAmount(purchaseAmount);
-        break;
-      } catch (error) {
-        MissionUtils.Console.print(error.message);
-      }
-    }
-
-    return purchaseAmount;
-  }
-
-  async askPurchaseLottoAmount() {
-    return this.#purchaseLottoAmount();
-  }
-}
-
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -17,6 +17,46 @@ class Lotto {
   // TODO: 추가 기능 구현
 }
 
+class LottoPurchase {
+  async #getLottoAmount() {
+    const USER_INPUT =
+      await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
+    const USER_INPUT_PRICE = parseInt(USER_INPUT, 10);
+    return USER_INPUT_PRICE;
+  }
+
+  #validateAmount(lottoAmount) {
+    if (
+      isNaN(lottoAmount) ||
+      !lottoAmount ||
+      lottoAmount < 1000 ||
+      lottoAmount % 1000 !== 0
+    ) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+    }
+  }
+
+  async #purchaseLottoAmount() {
+    let purchaseAmount;
+
+    while (true) {
+      try {
+        purchaseAmount = await this.#getLottoAmount();
+        this.#validateAmount(purchaseAmount);
+        break;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
+
+    return purchaseAmount;
+  }
+
+  async askPurchaseLottoAmount() {
+    return this.#purchaseLottoAmount();
+  }
+}
+
 class LottoTicket {
   #lottoTickets = [];
 
