ece01c0e92278d33f06858fb04cb263de99b70d4
Trophy198 <spsp8426@naver.com>
2023-11-05 23:35:53 +0900

    refactor : 매직 넘버 상수 처리

--- src/model/LottoMachine.js
+++ src/model/LottoMachine.js
@@ -1,12 +1,17 @@
 import Lotto from './Lotto.js';
 import { generateUniqueNumbers } from '../utils/GenerateRandomNumberUtils.js';
+import { GAME_SETTINGS } from '../constants/gameSettings.js';
 
 export default class LottoMachine {
   generateTickets(purchaseAmount) {
-    const numberOfTickets = purchaseAmount / 1000;
+    const numberOfTickets = purchaseAmount / GAME_SETTINGS.TICKET_PRICE;
 
     const tickets = Array.from({ length: numberOfTickets }, () => {
-      const numbers = generateUniqueNumbers(1, 45, 6);
+      const numbers = generateUniqueNumbers(
+        GAME_SETTINGS.MIN_LOTTO_NUMBER,
+        GAME_SETTINGS.MAX_LOTTO_NUMBER,
+        GAME_SETTINGS.NUMBERS_PER_TICKET,
+      );
       return new Lotto(numbers);
     });
 
