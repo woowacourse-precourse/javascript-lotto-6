cd2ae880fe4e8ee5dc4f36f4102527f05479607b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-05 20:13:54 +0900

    ✨feat($constants): 메세지 변경
    * 에러 발생 시 게임 종료가 아닌 재입력으로 인해 '게임을 종료합니다' 문구 삭제

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -18,29 +18,29 @@ const GUIDE_MESSAGE = Object.freeze({
 })
 
 const COMMON_ERROR_MESSAGE = Object.freeze({
-  emptyString: '[ERROR] 입력된 값이 없습니다. 게임을 종료합니다.',
-  onlyNumber: '[ERROR] 잘못된 형식입니다. 숫자 외의 문자는 입력할 수 없습니다. 게임을 종료합니다.',
-  wrongRange: '[ERROR] 잘못된 범위입니다. 로또 번호는 1부터 45까지 입력할 수 있습니다. 게임을 종료합니다.',
-  wrongCount: '[ERROR] 잘못된 입력입니다. 로또 한 장의 숫자 개수는 6개 입니다. 게임을 종료합니다.',
-  detectedDuplicate: '[ERROR] 잘못된 입력입니다. 중복된 숫자는 입력이 불가합니다. 게임을 종료합니다.',
+  emptyString: '[ERROR] 입력된 값이 없습니다.',
+  onlyNumber: '[ERROR] 잘못된 형식입니다. 숫자 외의 문자는 입력할 수 없습니다.',
+  wrongRange: '[ERROR] 잘못된 범위입니다. 로또 번호는 1부터 45까지 입력할 수 있습니다.',
+  wrongCount: '[ERROR] 잘못된 입력입니다. 로또 한 장의 숫자 개수는 6개 입니다.',
+  detectedDuplicate: '[ERROR] 잘못된 입력입니다. 중복된 숫자는 입력이 불가합니다',
 })
 
 const PURCHASE_AMOUNT_ERROR_MESSAGE = Object.freeze({
-  underThousand: '[ERROR] 잘못된 입력입니다. 1000원 미만의 단위는 입력이 불가합니다. 게임을 종료합니다.',
-  maximumNumber: '[ERROR] 잘못된 범위입니다. 금액을 1,000,000원으로 제한하고 있습니다. 게임을 종료합니다.',
-  wrongUnit: '[ERROR] 잘못된 형식입니다. 1,000원 단위로 입력하시기 바랍니다. 게임을 종료합니다.',
+  underThousand: '[ERROR] 잘못된 입력입니다. 1000원 미만의 단위는 입력이 불가합니다.',
+  maximumNumber: '[ERROR] 잘못된 범위입니다. 금액을 1,000,000원으로 제한하고 있습니다.',
+  wrongUnit: '[ERROR] 잘못된 형식입니다. 1,000원 단위로 입력하시기 바랍니다.',
 })
 
 const WINNING_NUMBERS_ERROR_MESSAGE = Object.freeze({
-  detectedSpace: '[ERROR] 잘못된 형식입니다. 공백은 입력이 불가합니다. 게임을 종료합니다.',
-  detectedLastComma: '[ERROR] 잘못된 입력입니다. 입력된 값 없이 콤마(,)를 입력할 수 없습니다. 게임을 종료합니다.',
-  wrongWinningNumber: '[ERROR] 잘못된 형식입니다. 숫자와 쉼표 외에는 입력할 수 없습니다. 게임을 종료합니다.',
+  detectedSpace: '[ERROR] 잘못된 형식입니다. 공백은 입력이 불가합니다.',
+  detectedLastComma: '[ERROR] 잘못된 입력입니다. 입력된 값 없이 콤마(,)를 입력할 수 없습니다.',
+  wrongWinningNumber: '[ERROR] 잘못된 형식입니다. 숫자와 쉼표 외에는 입력할 수 없습니다.',
 })
 
 const BONUS_NUMBER_ERROR_MESSAGE = Object.freeze({
-  wrongBonusNumber: '[ERROR] 잘못된 개수입니다. 보너스 숫자는 한개만 입력할 수 있습니다. 게임을 종료합니다.',
-  underZero: '[ERROR] 잘못된 입력입니다. 0 이하의 값은 입력이 불가합니다. 게임을 종료합니다.',
-  duplicatedNumberWithWinningNumbers: '[ERROR] 잘못된 숫자입니다. 당첨 번호에 포함된 숫자는 입력이 불가합니다. 게임을 종료합니다.',
+  wrongBonusNumber: '[ERROR] 잘못된 개수입니다. 보너스 숫자는 한개만 입력할 수 있습니다.',
+  underZero: '[ERROR] 잘못된 입력입니다. 0 이하의 값은 입력이 불가합니다.',
+  duplicatedNumberWithWinningNumbers: '[ERROR] 잘못된 숫자입니다. 당첨 번호에 포함된 숫자는 입력이 불가합니다.',
 })
 
 const WINNING_AMOUNTS = Object.freeze({
