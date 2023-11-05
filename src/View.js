ed5375c055fda35ce2d1a0ff36d3a529f28da0a5
jw <always1997@kakao.com>
2023-11-05 15:19:31 +0900

    feat: 당첨결과 출력 기능

--- src/View.js
+++ src/View.js
@@ -19,6 +19,16 @@ class View {
     });
   }
 
+  async getLottoResult(당첨결과) {
+    Console.print("당첨 통계");
+    Console.print(`3개 일치 (5,000원) - ${fifth}개`);
+    Console.print(`4개 일치 (50,000원) - ${fourth}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${third}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${second}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${first}개`);
+    Console.print(`총 수익률은 ${수익률}%입니다.`);
+  }
+
   async readLine(query) {
     return await Console.readLineAsync(query);
   }
