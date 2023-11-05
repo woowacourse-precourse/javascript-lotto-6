a5b891267e6bb3493d44c589325be6dcea5f4476
leedbswo107 <ljyu1@naver.com>
2023-11-06 00:01:36 +0900

    feat: 메소드 명 변경 및 기능 추가

--- view/InputAndOutput.js
+++ view/InputAndOutput.js
@@ -2,44 +2,49 @@ import { Console } from "@woowacourse/mission-utils";
 import { MESSAGE } from "../src/common/OutputMessage.js";
 import Validate from "../src/common/validate.js";
 import Lotto from "../src/Lotto.js";
-import Lottogenerator from "../src/Lottogenerator.js";
+import LottoPlay from "../src/Lottoplay.js";
 
 class InputAndOutput{
     
     constructor(){
         this.validate = new Validate();
-        this.lottogenerator = new Lottogenerator();
+        this.lottoPlay = new LottoPlay();
     }
 
-    async inputPurchaseAmount(){
-        const PURCHASE_AMOUNT = await Console.readLineAsync(`${MESSAGE.PURCHASE_NUMBER_INPUT}\n`);   
-        const VALIDATE_PURCHASE_AMOUNT = this.validate.validatePrice(PURCHASE_AMOUNT);
-        const LOTTO_AMOUNT = VALIDATE_PURCHASE_AMOUNT/1000;
-        this.purchasedLottoOutput(LOTTO_AMOUNT);
-
-        return VALIDATE_PURCHASE_AMOUNT;
+    async inputPurchasePrice(){
+        const PURCHASE_PRICE = await Console.readLineAsync(`${MESSAGE.PURCHASE_NUMBER_INPUT}\n`);   
+        const VALIDATE_PURCHASE_PRICE = this.validate.validatePrice(PURCHASE_PRICE);
+        return VALIDATE_PURCHASE_PRICE;
     }
     async inputWinNumber(){
         const WIN_NUMBER = await Console.readLineAsync(`${MESSAGE.WIN_NUMBER_INPUT}\n`);
-        const SPLIT_WIN_NUMBER = WIN_NUMBER.split(',');
-
+        const SPLIT_WIN_NUMBER = WIN_NUMBER.split(',').map(Number);
+        Console.print('');
         // TODO: 입력된 로또 번호의 유효성 검사를 진행
         new Lotto(SPLIT_WIN_NUMBER);
         return SPLIT_WIN_NUMBER;
     }
-    async inputBonusNumber(){
-        const BONUS_NUMBER = await Console.readLineAsync(`${MESSAGE.BONUS_NUMBER_INPUT}\n`);
-        const VALIDATE_BONUS_NUMBER = this.validate.validateBonusNumber(BONUS_NUMBER);
-        return VALIDATE_BONUS_NUMBER;     
+    async inputBonusNumber(numbers){
+        const BONUS_NUMBER = parseInt(await Console.readLineAsync(`${MESSAGE.BONUS_NUMBER_INPUT}\n`));
+        this.validate.validateBonusNumber(BONUS_NUMBER,numbers);
+        Console.print('');
+        return BONUS_NUMBER;     
     }
-    purchasedLottoOutput(amount){
-        const PURCHASE_LOTTO_NUMBER = this.lottogenerator.autoLottoGenerator(amount);;
+    async purchasedLottoOutput(number, amount){
+        // const PURCHASE_LOTTO_NUMBER = this.lottoPlay.autoLottoGenerator(amount);
         Console.print(`${amount}${MESSAGE.NUMBER_OF_PURCHASE}`);
-
-        PURCHASE_LOTTO_NUMBER.forEach(e => {
+        number.forEach(e => {
            Console.print(e); 
         });
         Console.print('');
+    }
+    gameResultOutput(win, bonus){
+        Console.print(MESSAGE.WIN_STATISTICS);
+        Console.print(MESSAGE.DIVIDE_LINE);
+        for(const key in MESSAGE.RANK) {
+            Console.print(`${MESSAGE.RANK[key]}${MESSAGE.UNIT}`);
+        }
+        Console.print(`${MESSAGE.TOTAL_RETURN.FRONT}${MESSAGE.TOTAL_RETURN.BACK}`);
     }
 }
 export default InputAndOutput;
