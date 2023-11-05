7ee17cad8d26c49683d7ccc38f2efb61ee581f60
sunny <js_park95@naver.com>
2023-11-06 00:06:33 +0900

    feat: 로또 게임 기본 조건 설정
    로또 번호의 최저, 최고 숫자, 번호 개수, 에러 처리 문구, 출력 메시지 설정

---
+++ src/constants/error.js
@@ -0,0 +1,25 @@
+const common = Object.freeze({
+  empty: '값이 입력되지 않았습니다.',
+  notANumber: '입력한 값이 숫자가 아닙니다.',
+  notNatural: '입력 값이 자연수가 아닙니다.',
+})
+
+const amounts = Object.freeze({
+  rest: '잔돈이 생기지 않도록 금액을 입력해주세요.',
+})
+
+const winNumber = Object.freeze({
+  unsplit: '쉼표(,)로 숫자를 구분해주세요.',
+  invalidRange: '1에서 45 사이의 숫자를 입력해주세요.',
+  invalidCount: '6개의 숫자를 입력해주세요.',
+  invalidBounsCount: '1개의 숫자를 입력해주세요.',
+  duplicate: '숫자가 중복되지 않게 입력해주세요.',
+})
+
+const ERROR = Object.freeze({
+  common,
+  amounts,
+  winNumber,
+})
+
+export default ERROR
---
+++ src/constants/lotto.js
@@ -0,0 +1,12 @@
+const lotto = Object.freeze({
+  price: 1000,
+  min: 1,
+  max: 45,
+  number: 6,
+})
+
+const SET_LOTTO = Object.freeze({
+  lotto,
+})
+
+export default SET_LOTTO
