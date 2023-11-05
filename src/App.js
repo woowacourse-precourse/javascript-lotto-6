b6ce7cb99a18cb51af723df697d69618515c6046
soyunim <togksk123@naver.com>
2023-11-06 00:22:15 +0900

    feat: 구입 개수 계산, 구입 개수 만큼의 로또 랜덤 출력 기능-초기작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import { GAME_MESSAGE } from "./constants/gameConstant";
+import GetInputValue from "./GetInputValue";
 class App {
-  async play() {}
+  async play() {
+    try {
+      Console.print("게임 시작");
+      const purchaseAmount = await GetInputValue.getPurchaseAmount();
+      Console.print(`${purchaseAmount/1000}${GAME_MESSAGE.PURCHASE_AMOUNT}`);
+      for(let i=0; i<purchaseAmount/1000; i++){
+        const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+        Console.print(lottoNumbers);
+      }
+    } catch (error) {
+      throw new Error("[ERROR]");
+    }
+  }
 }
 
 export default App;
