e5b6ec4cd38a6b99188fae39e9a9027c0612555d
okxooxoo <okxooxoo@naver.com>
2023-11-06 06:20:14 +0900

    ✨ feat: 기능➊ 구입한 로또 장수 출력

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
   async play() {
     // 기능 ➊ 사용자에게 구입 금액을 입력받는다.
     const money = await InputView.readMoney();
-
+    // 기능 ➊ 구입한 로또 장수를 출력한다.
     OutputView.printPurchase(money / 1000);
   }
 }
