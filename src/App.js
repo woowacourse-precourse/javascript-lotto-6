9ce06b069c9fc7f3d69c4a1f80ff046912ee8a47
kimdaye77 <kimdaye77@naver.com>
2023-11-05 14:52:46 +0900

    feat: 구입 금액 만큼 로또 발행 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
-import Lotto from "./Lotto";
+import Lotto from "./Lotto.js";
 
 class App {
   async play() {
 
     const purchaseAmountInput = await Console.readLineAsync(
-      "구입금액을 입력해 주세요\n"
+      "구입금액을 입력해 주세요.\n"
     );
 
     //TODO: 예외처리
     
-  
+    const lottoCnt = Number(purchaseAmountInput)/1000;
+    for(let i = 0; i<lottoCnt; i++){
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      Console.print(numbers)
+      const lotto = new Lotto(numbers);
+    }
   }
 }
 export default App;
