8b10f448435c31d73a3bc19b8d5a3231083bf5bc
OhJeongHwan <wjdghks0316@naver.com>
2023-11-05 23:14:47 +0900

    feat: 구입 금액을 입력받는 함수 inputCost와 입력 값의 유효성을 판단한는 costValid 작성

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import Lotto from "./Lotto.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  #cost = 0;
+  #winningNum = [];
+  #extraNum = 0;
+
+  async inputCost() {
+    const input = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    this.costValid(input);
+  }
+  costValid(input) {
+    if (/\d/.test(input) && input % 1000 === 0 && input > 0) {
+      this.#cost = input;
+    } else {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+  }
+
+  constructor() {}
+
+  async play() {
+    await this.inputCost();
+  }
 }
 
 export default App;
