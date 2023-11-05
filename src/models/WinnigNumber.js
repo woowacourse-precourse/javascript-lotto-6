dc7d2a9f7de882ec9a29df9ba428639ebd6a0ccf
이용성 <ukal5006@kakao.com>
2023-11-05 17:13:49 +0900

    refactor(WinningNumber): 검증 추가 및 수정
    중복과 0으로 시작하는 입력에 대한 검증을 추가했습니다.

--- src/models/WinnigNumber.js
+++ src/models/WinnigNumber.js
@@ -16,9 +16,17 @@ class WinningNumber {
       throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
     }
 
+    if ([...new Set(numbers)].length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 중복될 수 없습니다.');
+    }
+
     numbers.forEach((number) => {
       if (!Number.isInteger(Number(number))) {
         throw new Error('[ERROR] 당첨 번호는 정수 이여야 합니다.');
+      }
+
+      if (number.length !== Number(number).toString().length) {
+        throw new Error('[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.');
       }
 
       if (Number(number) > 45 || Number(number) < 1) {
