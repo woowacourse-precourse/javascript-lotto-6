9481a1148ada0bb25748e0759c25c8385cbf4b3d
yhyem <yoonhyemin0120@gmail.com>
2023-11-06 03:22:45 +0900

    feat: 로또 갯수 만큼 로또 발행

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import Validate from "./Validate.js";
+import Lotto from "./Lotto.js";
 
 const validate = new Validate();
 class App {
+  LottoList = [];
+
   async start() {
     const price = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
     validate.priceValidate(price);
@@ -10,9 +13,20 @@ class App {
     return price / 1000;
   }
 
+  getLottoList(count) {
+    let selectedNumber = [];
+    for (let i = 0; i < count; i++) {
+      selectedNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
+      this.LottoList.push(selectedNumber);
+      Console.print(selectedNumber);
+    }
+  }
+
   async play() {
-    const count = await this.start();
-    Console.print(count + "개를 구매했습니다.");
+    const lottoCount = await this.start();
+    Console.print(lottoCount + "개를 구매했습니다.");
+
+    this.getLottoList(lottoCount);
   }
 }
 
