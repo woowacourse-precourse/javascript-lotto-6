1f9b8357b86bd61e87b7b8f2c9e6417b374c9e6a
hyeong1 <mi75625@gmail.com>
2023-11-05 17:41:48 +0900

    docs: 개수 오류문 추가

--- src/constant/error.js
+++ src/constant/error.js
@@ -4,6 +4,7 @@ const ERROR = {
   NOT_NUMBER: "[ERROR] 숫자를 입력하세요.",
   NOT_RANGE: "[ERROR] 1~45 사이의 숫자를 입력하세요.",
   DUPLICATION: "[ERROR] 중복된 숫자가 입력되었습니다.",
+  NOT_6: "[ERROR] 로또 번호는 6개여야 합니다.",
 };
 
 export default ERROR;
