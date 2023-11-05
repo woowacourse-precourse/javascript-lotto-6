973c11e5d97d61696ff66b89558daf5c9b8908bc
sehyun <e6d1fe@gmail.com>
2023-11-05 12:22:38 +0900

    feat: enterBonusNumber에 보너스 번호 검증 로직 추가

--- src/Input/enterBonusNumber.js
+++ src/Input/enterBonusNumber.js
@@ -1,8 +1,16 @@
 import { Console } from '@woowacourse/mission-utils';
 import { message } from '../Constants.js';
 
+import validateBonusNumber from '../Validation/validateBonusNumber.js';
+
 async function enterBonusNumber() {
   const bonusNumberInput = await Console.readLineAsync(message.ENTER_BONUS_NUMBER);
+
+  if (validateBonusNumber(bonusNumberInput)) {
+    return Number(bonusNumberInput);
+  }
+
+  throw new Error(message.ERROR);
 }
 
 export default enterBonusNumber;
