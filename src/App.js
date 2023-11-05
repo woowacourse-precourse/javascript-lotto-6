486d4df1aa8e9e897611af436c4c26753f0a54bb
osohyun0224 <osohyun0224@naver.com>
2023-11-06 01:36:33 +0900

    feat(App): 로또 수량 구하는 기능 구현
    - 사용자로부터 입력받은 구매 금액으로 로또의 수량을 나타내는 lottoCount 변수를 선언하였습니다.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import InputView from "./views/InputView.js";
 class App {
   async play() {
     const purchasePrice = await InputView.getPurchasePrice();
+    const lottoCount = parseInt(purchasePrice / 1000);
   }
 }
 export default App;
