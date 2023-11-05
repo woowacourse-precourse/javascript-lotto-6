3d31fb575e869d1dad150b91510ea35ba879d8ad
nijuy <youjin6325@naver.com>
2023-11-05 10:59:17 +0900

    feat(App): 로또 구입 수량 계산 후 출력
    - LOTTO_PRICE를 play()에서도 사용하기 위해 선언 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 import ERROR_MESSAGE from './ErrorMessage';
 
+const LOTTO_PRICE = 1000;
+
 class App {
   static validatePayment(input) {
-    const LOTTO_PRICE = 1000;
     const payment = Number(input);
 
     if (Number.isNaN(payment)) {
@@ -17,6 +18,8 @@ class App {
   async play() {
     const payment = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     App.validatePayment(payment);
+    const amount = payment / LOTTO_PRICE;
+    Console.print(`\n${amount}개를 구매했습니다.`);
   }
 }
 
