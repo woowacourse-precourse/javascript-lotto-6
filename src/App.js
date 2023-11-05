d66809f9b38b0f27a5ac03ae2ce515f9f230b246
seoyeon-jung <jungluchia1204@gmail.com>
2023-11-05 21:49:18 +0900

    feat: 구입 금액 입력 문구 출력
    - Console.js : Console 기능 파일

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { GAME_MESSAGE } from "./Constant.js";
+
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  async play() {}
+  async play() {
+    this.inputMoney();
+  }
+
+  inputMoney() {
+    Console.readLineAsync(GAME_MESSAGE.BUY_AMOUNT, (money) => {});
+  }
 }
 
 export default App;
---
+++ src/Console.js
@@ -0,0 +1,9 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+class Console {
+  static print(text) {
+    Console.print(text);
+  }
+}
+
+module.exports = Console;
