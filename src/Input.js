e21bb0a469b0e7b58da4ccbccd6bef33ebf24b08
ohgus <anakin1108@naver.com>
2023-11-05 17:06:17 +0900

    refactor: moneyValidator 기능 Input class 로 이동 및 validateMoney 로 이름 변경

--- src/Input.js
+++ src/Input.js
@@ -24,4 +24,17 @@ export class Input {
 
     return bonusNumber;
   }
+
+  // 예외 처리 기능
+  validateMoney(money) {
+    if (!Number(money)) {
+      throw new Error(ERROR_MESSAGES.NOT_A_NUMBER);
+    } else if (Number(money) < 1000) {
+      throw new Error(ERROR_MESSAGES.IT_IS_TO_SMALL);
+    } else if (money.includes(".") || money.includes(" ")) {
+      throw new Error(ERROR_MESSAGES.ONLY_NUMBER);
+    } else if (!Number(money) % 1000 === 0) {
+      throw new Error(ERROR_MESSAGES.ONLY_THOUSANDWON_UNIT);
+    }
+  }
 }
--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,18 +1,6 @@
 import { ERROR_MESSAGES } from "./constants/messages.js";
 
 export class InputValidator {
-  moneyValidator(money) {
-    if (!Number(money)) {
-      throw new Error(ERROR_MESSAGES.NOT_A_NUMBER);
-    } else if (Number(money) < 1000) {
-      throw new Error(ERROR_MESSAGES.IT_IS_TO_SMALL);
-    } else if (money.includes(".") || money.includes(" ")) {
-      throw new Error(ERROR_MESSAGES.ONLY_NUMBER);
-    } else if (!Number(money) % 1000 === 0) {
-      throw new Error(ERROR_MESSAGES.ONLY_THOUSANDWON_UNIT);
-    }
-  }
-
   winningNumbersValidator(numbers) {
     const winningNumbers = numbers.split(",").map(Number);
 
