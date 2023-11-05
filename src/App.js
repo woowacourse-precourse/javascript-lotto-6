bc0dcce5f6f348ea02e0405b168fc017735ae5d9
kimdaye77 <kimdaye77@naver.com>
2023-11-05 15:40:45 +0900

    feat: 당첨 통계 출력 구현

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App {
       second_prize: 0,
       third_prize: 0,
       fourth_prize: 0,
-      fifth_prize:0
+      fifth_prize:0,
+      totalPrice:0,
     };
   }
   async play() {
@@ -45,21 +46,41 @@ class App {
       switch (res.cnt){
         case 3:
           this.statistics.fifth_prize++;
+          this.totalPrice+=5000;
           break;
         case 4:
           this.statistics.fourth_prize++;
+          this.totalPrice+=50000;
           break;
         case 5:
-          if(res.hasBonus) this.statistics.second_prize++;
-          else this.statistics.third_prize++;
+          if(res.hasBonus) {
+            this.statistics.second_prize++;
+            this.totalPrice+=30000000;
+            break;
+          }
+          this.statistics.third_prize++;
+          this.totalPrice+=1500000;
           break;
         case 6:
           this.statistics.first_prize++;
+          this.totalPrice+=2000000000;
           break;
         default:
           break;
       }
     }
+
+   
+    Console.print(`
+    당첨 통계
+    ---
+    3개 일치 (5,000원) - ${this.statistics.fifth_prize}개
+    4개 일치 (50,000원) - ${this.statistics.fourth_prize}개
+    5개 일치 (1,500,000원) - ${this.statistics.third_prize}개
+    5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.statistics.second_prize}개
+    6개 일치 (2,000,000,000원) - ${this.statistics.first_prize}개
+    총 수익률은 ${totalPrice/Number(purchaseAmountInput)}%입니다.
+`);
   }
 }
 export default App;
