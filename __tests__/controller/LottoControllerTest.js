7f87106d147cfd6260f8660c029ee0df4f01c0cd
BangDori <qudwns8865@naver.com>
2023-11-05 16:26:54 +0900

    test: 로또 번호 일치 테스트 추가

--- __tests__/controller/LottoControllerTest.js
+++ __tests__/controller/LottoControllerTest.js
@@ -1,5 +1,6 @@
 import LottoController from '../../src/controller/LottoController';
 import { LOTTO } from '../../src/constants/setting';
+import Lotto from '../../src/Lotto';
 
 describe('LottoController 클래스 테스트', () => {
   test.each([
@@ -13,5 +14,61 @@ describe('LottoController 클래스 테스트', () => {
 
     lottoController.purchaseLotto();
     expect(lottoController.issueLotto).toHaveBeenCalledTimes(lottoCount);
+  });
+
+  describe('로또 번호 일치 테스트', () => {
+    let lottoController;
+    const winningNumbers = [7, 14, 21, 28, 35, 42];
+    const bonusNumber = 45;
+
+    beforeEach(() => {
+      lottoController = new LottoController();
+    });
+
+    test.each([
+      { lottoNumbers: [7, 8, 9, 10, 11, 12], expectedCount: 1 },
+      { lottoNumbers: [7, 14, 15, 16, 17, 18], expectedCount: 2 },
+      { lottoNumbers: [7, 14, 21, 22, 23, 24], expectedCount: 3 },
+      { lottoNumbers: [7, 14, 21, 28, 29, 30], expectedCount: 4 },
+      { lottoNumbers: [7, 14, 21, 28, 35, 36], expectedCount: 5 },
+      { lottoNumbers: [7, 14, 21, 28, 35, 42], expectedCount: 6 },
+    ])(
+      '로또 번호가 $expectedCount개 일치, 보너스 번호 불일치',
+      ({ lottoNumbers, expectedCount }) => {
+        const lotto = new Lotto(lottoNumbers);
+
+        const { count, bonus } = lottoController.matchLotto(
+          lotto,
+          winningNumbers,
+          bonusNumber,
+        );
+
+        expect(count).toBe(expectedCount);
+        expect(bonus).toBeFalsy();
+      },
+    );
+
+    test.each([
+      { lottoNumbers: [1, 2, 3, 4, 5, 45], expectedCount: 0 },
+      { lottoNumbers: [7, 8, 9, 10, 11, 45], expectedCount: 1 },
+      { lottoNumbers: [7, 14, 15, 16, 17, 45], expectedCount: 2 },
+      { lottoNumbers: [7, 14, 21, 22, 23, 45], expectedCount: 3 },
+      { lottoNumbers: [7, 14, 21, 28, 29, 45], expectedCount: 4 },
+      { lottoNumbers: [7, 14, 21, 28, 35, 45], expectedCount: 5 },
+    ])(
+      '로또 번호가 $expectedCount개 일치, 보너스 번호 일치',
+      ({ lottoNumbers, expectedCount }) => {
+        const lotto = new Lotto(lottoNumbers);
+
+        const { count, bonus } = lottoController.matchLotto(
+          lotto,
+          winningNumbers,
+          bonusNumber,
+        );
+
+        expect(count).toBe(expectedCount);
+        expect(bonus).toBeTruthy();
+      },
+    );
   });
 });
