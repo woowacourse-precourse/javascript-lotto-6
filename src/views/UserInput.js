c4faabc9ed1ff8ec4dcf0d08ed6bafacd78642f0
Lee sang Yeop <mma7710@naver.com>
2023-11-05 20:55:21 +0900

    feat(UserInput): 사용자에게 입력받는 View 추가 추가한 기능들 - 로또 구입금액 입력 메서드 - 로또 당첨 번호 입력 메서드 - 로또 보너스 번호 입력 메서드

---
+++ src/views/UserInput.js
@@ -0,0 +1,32 @@
+import { Console } from '@woowacourse/mission-utils';
+import SYSTEM_MESSAGES from '../constants/SystemMessages.js';
+import validators from '../utils/validators.js';
+
+class UserInput {
+	async lottoPurchase(question = SYSTEM_MESSAGES.input_purchase_amount) {
+		const userInput = await Console.readLineAsync(question);
+		const trimmedInput = userInput.trim();
+		validators.inputPurchasePrice(trimmedInput);
+
+		return trimmedInput;
+	}
+
+	async winningNumber(question = SYSTEM_MESSAGES.input_winning_numbers) {
+		const userInput = await Console.readLineAsync(question);
+		const splittedInput = userInput.split(',').map((input) => input.trim());
+
+		validators.inputWinningNumber(splittedInput);
+
+		return splittedInput;
+	}
+
+	async winningBonusNumber(question = SYSTEM_MESSAGES.input_bonus_number) {
+		const userInput = await Console.readLineAsync(question);
+		const trimmedInput = userInput.trim();
+		validators.inputWinningBonusNumber(trimmedInput);
+
+		return trimmedInput;
+	}
+}
+
+export default UserInput;
