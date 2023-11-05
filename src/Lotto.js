1564244ae539cf8d266ae07c394d03f53f513a92
OhJeongHwan <wjdghks0316@naver.com>
2023-11-05 20:51:01 +0900

    feat: lotto에서 로또 번호 중복을 제한하는 조건인  noSameNumber() 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,7 @@ class Lotto {
   constructor(numbers) {
     this.#validate(numbers);
     this.#inRange(numbers);
+    this.#noSameNumber(numbers);
     this.#numbers = numbers;
   }
 
@@ -18,6 +19,12 @@ class Lotto {
         throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
       }
     });
+  }
+  #noSameNumber(numbers) {
+    const noSameNum = new Set(numbers);
+    if (noSameNum.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
