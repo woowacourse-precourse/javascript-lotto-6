a070ac73b4e8a051244b139569807536664d5329
djLee77 <dkdnl232@gmail.com>
2023-11-05 16:04:59 +0900

    refactor(App): 입력 값 유효성 검사 기능 클래스 분리
    ValidateInput 클래스 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import NumberIssuance from "./NumberIssuance.js";
-
-const MIN_WINNING_NUMBER = 1;
-const MAX_WINNING_NUMBER = 45;
+import ValidateInput from "./ValidateInput.js";
 
 class App {
   async play() {
@@ -20,7 +18,7 @@ class App {
       try {
         const input = await Console.readLineAsync("구입금액을 입력해주세요.\n");
         amount = Number(input);
-        App.validateAmount(amount);
+        ValidateInput.validateAmount(amount);
         return amount;
       } catch (error) {
         Console.print(error.message);
@@ -35,7 +33,7 @@ class App {
         const input =
           await Console.readLineAsync("당첨 번호를 입력해주세요.\n");
         numbers = input.split(",").map((item) => Number(item));
-        App.validateWinningNumber(numbers);
+        ValidateInput.validateWinningNumber(numbers);
         return numbers;
       } catch (error) {
         Console.print(error.message);
@@ -48,37 +46,6 @@ class App {
     str += `${EA}개를 구매했습니다. \n`;
     str += `${lottoNumbers.map((item) => `[${item.join(", ")}]`).join("\n")}\n`;
     Console.print(str);
-  }
-
-  static validateAmount(amount) {
-    if (isNaN(amount) || amount <= 0) {
-      throw new Error("[ERROR] 구입금액은 자연수만 입력 가능합니다.\n");
-    }
-
-    if (amount % 1000 !== 0) {
-      throw new Error("[ERROR] 구입금액은 1,000원 단위만 가능합니다.\n");
-    }
-  }
-
-  static validateWinningNumber(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error(
-        "[ERROR] 당첨 번호는 쉼표(,)로 구분하여 6개 입력해주세요.\n",
-      );
-    }
-
-    if (
-      numbers.some(
-        (number) =>
-          isNaN(number) ||
-          number < MIN_WINNING_NUMBER ||
-          number > MAX_WINNING_NUMBER,
-      )
-    ) {
-      throw new Error(
-        "[ERROR] 당첨 번호는 1부터 45사이의 숫자로 입력해주세요.\n",
-      );
-    }
   }
 }
 
---
+++ src/ValidateInput.js
@@ -0,0 +1,37 @@
+const MIN_WINNING_NUMBER = 1;
+const MAX_WINNING_NUMBER = 45;
+
+class ValidateInput {
+  static validateAmount(amount) {
+    if (isNaN(amount) || amount <= 0) {
+      throw new Error("[ERROR] 구입금액은 자연수만 입력 가능합니다.\n");
+    }
+
+    if (amount % 1000 !== 0) {
+      throw new Error("[ERROR] 구입금액은 1,000원 단위만 가능합니다.\n");
+    }
+  }
+
+  static validateWinningNumber(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error(
+        "[ERROR] 당첨 번호는 쉼표(,)로 구분하여 6개 입력해주세요.\n",
+      );
+    }
+
+    if (
+      numbers.some(
+        (number) =>
+          isNaN(number) ||
+          number < MIN_WINNING_NUMBER ||
+          number > MAX_WINNING_NUMBER,
+      )
+    ) {
+      throw new Error(
+        "[ERROR] 당첨 번호는 1부터 45사이의 숫자로 입력해주세요.\n",
+      );
+    }
+  }
+}
+
+export default ValidateInput;
