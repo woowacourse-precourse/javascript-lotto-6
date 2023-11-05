2b762eb95e724202a03ee0506b721f99ad002099
nkEllaaa <dhrtns515@gmail.com>
2023-11-05 21:12:00 +0900

    feat: 유효성 검사 및 티켓 생성 관련 함수 호출 기능 구현

--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
+import { countLottoTickets } from './utils/countLottoTickets';
+import validateMoney from './validation/validateMoney';
+import validateLotto from './validation/validateLotto';
 
-const inputPurchaseAmount = async () => {
-  const purchaseAmount =
-    await Console.readLineAsync('구입금액을 입력해 주세요.');
+const inputUserMoney = async () => {
+  const userMoney = await Console.readLineAsync('구입 금액을 입력해 주세요.');
+
+  validateMoney(userMoney);
+  countLottoTickets(userMoney);
 };
 
 const inputLottoNumber = async () => {
   const lottoNumber = await Console.readLineAsync('당첨 번호를 입력해 주세요.');
+
+  validateLotto(lottoNumber);
 };
 
 const inputBonusNumber = async () => {
@@ -14,4 +21,4 @@ const inputBonusNumber = async () => {
     await Console.readLineAsync('보너스 번호를 입력해 주세요.');
 };
 
-module.exports = { inputPurchaseAmount, inputLottoNumber, inputBonusNumber };
+module.exports = { inputUserMoney, inputLottoNumber, inputBonusNumber };
