98fda414a9ea48f93adca3f3af5b14b2deaccd14
donggyun <donggyunkim12@gmail.com>
2023-11-05 10:39:55 +0900

    feat(message): ERROR_MESSAGE, INPUT_MESSAGE 상수 정의

---
+++ src/constant/message.js
@@ -0,0 +1,13 @@
+export const ERROR_MESSAGE = Object.freeze({
+  INVALID_INPUT_NUMBER: '[ERROR] 숫자만 입력해주세요.',
+  INVALID_NON_DIVISIBLE: '[ERROR] 금액은 1000원 단위여야 합니다.',
+  INVALID_LOTTO_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
+  INVALID_LOTTO_UNIQUE: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
+  INVALID_LOTTO_RANGE: '[ERROR] 로또 번호는 1~45 사이여야 합니다.',
+});
+
+export const INPUT_MESSAGE = Object.freeze({
+  INPUT_MONEY: '구입금액을 입력해 주세요.\n',
+  INPUT_WINNER_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+});
