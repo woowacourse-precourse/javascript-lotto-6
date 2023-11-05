df5f66716c73cb95e213c791dfbe459bddd7ce7c
Lim-shweety <smile70200@naver.com>
2023-11-05 22:46:29 +0900

    :sparkles: feat: 중복되는 로또 번호 validate 기능 추가 + lotto number 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Lotto from "./Lotto.js";
+import {MissionUtils} from "@woowacourse/mission-utils";
 class App {
   async play() {
+    const MONEY= await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.")
+    const INPUT_NUMBERS = await MissionUtils.Console.readLineAsync("당첨 번호를 입력해 주세요.")
+    const LOTTO_NUMBERS=new Lotto(INPUT_NUMBERS);
+
+
 
 
   }
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,20 +1,27 @@
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
+    constructor(numbers) {
+        this.#validate(numbers);
+        this.#validateDuplicate(numbers);
+        this.#numbers = numbers;
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-  }
 
+    #validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
 
+    #validateDuplicate(numbers) {
+        const SET_NUMBERS = new Set(numbers);
+        if (SET_NUMBERS.length !== numbers.length) {
+            throw new Error("[ERROR] 중복되는 로또 번호가 입력 되었습니다.");
+        }
 
-  // TODO: 추가 기능 구현
+    }
+
 }
 
 export default Lotto;
