7104083ee0834f12b5f1f0f2f257f191c3b1dbb0
jw <always1997@kakao.com>
2023-11-05 13:54:10 +0900

    feat: 기능목록 작성 및 로또머신 클래스 생성

---
+++ src/LottoMachine.js
@@ -0,0 +1,11 @@
+class LottoMachine {
+  #winningNumbers;
+  #bonusNumber;
+
+  constructor(winningNumbers, bonusNumber) {
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNumber = bonusNumber;
+  }
+}
+
+export default LottoMachine;
