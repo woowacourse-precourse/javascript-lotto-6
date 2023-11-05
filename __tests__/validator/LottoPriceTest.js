3e148285bc546096134383099320c55744857817
chiman <rlaclghks123@naver.com>
2023-11-05 13:02:36 +0900

    refactor: 로또 금액 테스트 파일 분리

---
+++ __tests__/validator/LottoPriceTest.js
@@ -0,0 +1,61 @@
+import lottoPriceValidator from '../../src/validator/lottoPriceValidator.js';
+
+describe('로또 금액 테스트', () => {
+  describe('로또 금액이 1,000원 단위 테스트', () => {
+    test('로또 금액이 1,000원 단위일 경우', () => {
+      expect(() => {
+        lottoPriceValidator.checkLottoPrice('3000');
+      }).not.toThrow();
+    });
+
+    test('로또 금액이 1,000원 단위가 아닌 경우', () => {
+      expect(() => {
+        lottoPriceValidator.checkLottoPrice('1350');
+      }).toThrow();
+    });
+  });
+
+  describe('로또 금액이 숫자 형태인지 테스트', () => {
+    test('로또 금액이 문자열 + 숫자 형태인 경우 Ex)+3000일 경우', () => {
+      expect(() => {
+        lottoPriceValidator.checkLottoPrice('+3000');
+      }).toThrow();
+    });
+
+    test('로또 금액이 문자열 + 숫자 형태인 경우 Ex) -3000일 경우', () => {
+      expect(() => {
+        lottoPriceValidator.checkLottoPrice('-3000');
+      }).toThrow();
+    });
+
+    test('로또 금액에 공백이 포함된 경우', () => {
+      expect(() => {
+        lottoPriceValidator.checkLottoPrice('3000  ');
+      }).toThrow();
+    });
+
+    test('로또 금액이 빈값인 경우', () => {
+      expect(() => {
+        lottoPriceValidator.checkLottoPrice('');
+      }).toThrow();
+    });
+
+    test('로또 금액이 여러개의 빈값인 경우', () => {
+      expect(() => {
+        lottoPriceValidator.checkLottoPrice('          ');
+      }).toThrow();
+    });
+  });
+
+  test('로또 금액이 너무 큰 경우 테스트 -> 200001000인 경우', () => {
+    expect(() => {
+      lottoPriceValidator.checkLottoPrice('2000001000');
+    }).toThrow();
+  });
+
+  test('로또 금액이 1000원 이하인 경우 테스트 -> 0인 경우', () => {
+    expect(() => {
+      lottoPriceValidator.checkLottoPrice('0');
+    }).toThrow();
+  });
+});
