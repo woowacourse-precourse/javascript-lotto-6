8b1b44d76d754d138f64b3528eef05a5a5d83ff9
Boeun <boeun0904@naver.com>
2023-11-05 23:26:31 +0900

    style: MESSAGE 프로퍼티 순서 변경

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 export const MESSAGE = {
   INPUT_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
-  PURCHASE_COUNT: (cnt) => `${cnt}개를 구매했습니다.`,
   INPUT_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.',
   INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
   WINNING_STATISTICS: '당첨 통계',
   DASHES: '---',
+  PURCHASE_COUNT: (cnt) => `${cnt}개를 구매했습니다.`,
   SAME_NUMBERS_COUNT: (numCnt, winnings, lottoCnt) =>
     `${numCnt}개 일치 (${winnings}원) - ${lottoCnt}개`,
   TOTAL_ROR: (total) => `총 수익률은 ${total}%입니다.`,
