cd8497abf4db21188986e43ffcb6088ab8435a70
rustandbone <dlehdgh14@naver.com>
2023-11-05 16:24:42 +0900

    기능 구현: 로또 번호 getter, setter 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,7 +1,13 @@
-import { Random, Console } from '@woowacourse/mission-utils';
-
 class Lotto {
   #numbers;
+
+  get numbers() {
+    return this.#numbers;
+  }
+
+  set numbers(numbers) {
+    this.#numbers = numbers;
+  }
 
   constructor(numbers) {
     this.#validate(numbers);
@@ -12,6 +18,7 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    this.#numbers = numbers;
   }
 }
 
