81a564005a6b1207c9825686db4aa676145f0541
PARK-CHANUK <doosan1224@naver.com>
2023-11-06 00:38:25 +0900

    refactor: 매칭 테이블을 테이블 템플릿으로 상수화

--- src/MatchingTable.js
+++ src/MatchingTable.js
@@ -1,56 +1,8 @@
-import {
-  COUNT,
-  DEFAULT_NUM,
-  MATCH_COUNTS,
-  PRIZE_MONEY,
-  RANKING,
-} from './constants/conditions';
+import { COUNT } from './constants/conditions.js';
+import TABLE_TEMPLATE from './constants/tableTemplate.js';
 
 export default class MatchingTable {
-  #table = new Map([
-    [
-      {
-        matchCount: MATCH_COUNTS.three,
-        ranking: RANKING.fifth,
-        prize: PRIZE_MONEY.fifth,
-      },
-      DEFAULT_NUM,
-    ],
-    [
-      {
-        matchCount: MATCH_COUNTS.four,
-        ranking: RANKING.fourth,
-        prize: PRIZE_MONEY.fourth,
-      },
-      DEFAULT_NUM,
-    ],
-    [
-      {
-        matchCount: MATCH_COUNTS.five,
-        isBonusMatch: false,
-        ranking: RANKING.third,
-        prize: PRIZE_MONEY.third,
-      },
-      DEFAULT_NUM,
-    ],
-    [
-      {
-        matchCount: MATCH_COUNTS.five,
-        isBonusMatch: true,
-        ranking: RANKING.second,
-        prize: PRIZE_MONEY.second,
-      },
-      DEFAULT_NUM,
-    ],
-    [
-      {
-        matchCount: MATCH_COUNTS.all,
-        ranking: RANKING.first,
-        prize: PRIZE_MONEY.first,
-      },
-      DEFAULT_NUM,
-    ],
-  ]);
+  #table = new Map(TABLE_TEMPLATE);
 
   updateTable(count, isBonusMatch) {
     this.#table.forEach((value, key) => {
