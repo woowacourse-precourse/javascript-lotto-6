bef4a406469b52d0b6abed607b3fe49d5a981cda
sehyun <e6d1fe@gmail.com>
2023-11-05 11:15:29 +0900

    feat: 구입금액 입력 받는 기능 구현

---
+++ src/Input/enterPurchaseAmount.js
@@ -0,0 +1,6 @@
+import { Console } from '@woowacourse/mission-utils';
+import { message } from '../Constants';
+
+async function enterPurchaseAmount() {
+  const purchaseAmountInput = await Console.readLineAsync(message.ENTER_PURCHASE_AMOUNT);
+}
