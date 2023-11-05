2aca7b4bd593c24a129ad9bcd76d44423322aa1f
chiman <rlaclghks123@naver.com>
2023-11-05 12:51:24 +0900

    feat: 쉼표를 기준으로 구분했을 때 각 값들이 숫자가 아닌 경우 예외처리

---
+++ src/validator/winningNumberValidator.js
@@ -0,0 +1,21 @@
+import commonValidator from './commonValidator.js';
+
+const winnigNumberValidator = {
+  numberOfLottoLitmit(splitedWinnigNumbers) {
+    if (splitedWinnigNumbers.length !== 6) {
+      throw new Error('당첨 번호는 쉼표(,)를 기준으로 6자리를 입력해주세요');
+    }
+  },
+
+  checkNumberType(splitedWinnigNumbers) {
+    splitedWinnigNumbers.forEach((number) => commonValidator.checkNumberType(number));
+  },
+
+  checkWinningNumbers(winnigNumbers) {
+    const splitedWinnigNumbers = winnigNumbers.split(',');
+    winnigNumberValidator.numberOfLottoLitmit(splitedWinnigNumbers);
+    winnigNumberValidator.checkNumberType(splitedWinnigNumbers);
+  },
+};
+
+export default winnigNumberValidator;
