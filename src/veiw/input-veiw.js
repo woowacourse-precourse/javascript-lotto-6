e3ff45c8e5cce4d8efc9806ce3159f60ac7e159c
leelsj2007 <leelsj2007@gmail.com>
2023-11-05 16:46:40 +0900

    feat : (입력) 구입금액이 1000으로 나누어 떨어지지 않을 경우 예외 처리 기능

--- src/veiw/input-veiw.js
+++ src/veiw/input-veiw.js
@@ -2,12 +2,15 @@ import { Console } from "@woowacourse/mission-utils";
 import { INPUT_MESSAGE } from "../constants/constants.js";
 
 class InputVeiw{
-    async readPurchaseAmount(){
+    async readPurchaseAmount() {
         const inputMoney = await Console.readLineAsync(INPUT_MESSAGE.inputPurchaseAmount);
+        if (inputMoney%1000 != 0) {
+            throw new Error("[ERROR] 구입금액은 1000으로 나누어 떨어지는 수 입니다.");
+        }
         return inputMoney/1000;
     }
 
-    async readWinningNumber(){
+    async readWinningNumber() {
         return null;
     }
 }
