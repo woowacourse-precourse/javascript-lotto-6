b1a3836873e2630b94b01173ae37f8a874593510
donggyun <donggyunkim12@gmail.com>
2023-11-05 23:22:47 +0900

    feat(outputPrompt): 당첨 통계 출력 기능 구현

--- src/view/outputPompt.js
+++ src/view/outputPompt.js
@@ -13,3 +13,31 @@ export const printBeforeResult = () => {
   Console.print(OUTPUT_MESSAGE.OUTPUT_RESULT);
   Console.print(OUTPUT_MESSAGE.OUTPUT_DIVIDER);
 };
+
+export const printWinningStatus = (rewardCount) => {
+  Console.print(
+    `${
+      OUTPUT_MESSAGE.OUTPUT_THREE_MATCH
+    } - ${OUTPUT_MESSAGE.OUTPUT_MATCH_AMOUNT(rewardCount['3'])}`,
+  );
+  Console.print(
+    `${OUTPUT_MESSAGE.OUTPUT_FOUR_MATCH} - ${OUTPUT_MESSAGE.OUTPUT_MATCH_AMOUNT(
+      rewardCount['4'],
+    )}`,
+  );
+  Console.print(
+    `${OUTPUT_MESSAGE.OUTPUT_FIVE_MATCH} - ${OUTPUT_MESSAGE.OUTPUT_MATCH_AMOUNT(
+      rewardCount['5'],
+    )}`,
+  );
+  Console.print(
+    `${
+      OUTPUT_MESSAGE.OUTPUT_FIVE_AND_BONUS_MATCH
+    } - ${OUTPUT_MESSAGE.OUTPUT_MATCH_AMOUNT(rewardCount['5b'])}`,
+  );
+  Console.print(
+    `${OUTPUT_MESSAGE.OUTPUT_SIX_MATCH} - ${OUTPUT_MESSAGE.OUTPUT_MATCH_AMOUNT(
+      rewardCount['6'],
+    )}`,
+  );
+};
