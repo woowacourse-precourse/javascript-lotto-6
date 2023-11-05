2e4fbbb61f5454df1994f02829f4933de710ae2b
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2023-11-05 20:02:10 +0900

    feat : 구입할 금액을 입력하는 기능
    구입할 금액을 입력받고 그에대한 예외처리 (숫자형식, 1000원단위) 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { MissionUtils, Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 class App {
-  async play() {}
+    async play() {
+        const purchaseAmount = await this.getValidPurchaseAmount();
+    }
+
+    async getValidPurchaseAmount() {
+        while (true) {
+            const purchaseAmount = await this.getInputNumber(
+                "구입 금액을 입력해 주세요."
+            );
+            if (this.isPurchaseAmountValid(purchaseAmount)) {
+                return purchaseAmount;
+            }
+        }
+    }
+    async getInputNumber(prompt) {
+        const input = await MissionUtils.Console.readLineAsync(prompt);
+        const number = parseInt(input);
+
+        if (!isNaN(number)) {
+            return number;
+        } else {
+            return this.handleInvalidInput();
+        }
+    }
+
+    async handleInvalidInput() {
+        MissionUtils.Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
+        return NaN;
+    }
+    isPurchaseAmountValid(purchaseAmount) {
+        if (purchaseAmount % 1000 === 0) {
+            return true;
+        } else {
+            Console.print("[ERROR] 1,000원 단위로 입력해 주세요.");
+            return false;
+        }
+    }
 }
 
 export default App;
