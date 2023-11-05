b5f7205852f5ec1bbc81e4b154bc5fe27e9168d4
손진영 <sm099@naver.com>
2023-11-05 13:56:35 +0900

    test(commonValidation): 테스트 케이스 내 inputValue 프로퍼티를 input으로 변경

--- __tests__/validations/commonValidation.test.js
+++ __tests__/validations/commonValidation.test.js
@@ -7,18 +7,18 @@ describe('validateCommon 테스트', () => {
   describe('예외 테스트', () => {
     test.each([
       {
-        inputValue: '',
+        input: '',
         expectedErrorMessage: COMMON_VALIDATION_TYPES.emptyValues.errorMessage,
       },
       {
-        inputValue: 'Some text with space',
+        input: 'Some text with space',
         expectedErrorMessage: COMMON_VALIDATION_TYPES.existSpaces.errorMessage,
       },
     ])(
-      '입력값이 "$inputValue"일 때 "$expectedErrorMessage" 메시지와 함께 에러가 발생해야 한다.',
-      ({ inputValue, expectedErrorMessage }) => {
+      '입력값이 "$input"일 때 "$expectedErrorMessage" 메시지와 함께 에러가 발생해야 한다.',
+      ({ input, expectedErrorMessage }) => {
         // then
-        expect(startValidation(inputValue)).toThrow(new AppError(expectedErrorMessage));
+        expect(startValidation(input)).toThrow(new AppError(expectedErrorMessage));
       },
     );
   });
@@ -26,14 +26,14 @@ describe('validateCommon 테스트', () => {
   describe('비 예외 테스트', () => {
     test.each([
       {
-        inputValue: 'abc',
+        input: 'abc',
       },
       {
-        inputValue: '123',
+        input: '123',
       },
-    ])('입력값이 "$inputValue"일 때 에러가 발생하지 않는다.', ({ inputValue }) => {
+    ])('입력값이 "$input"일 때 에러가 발생하지 않는다.', ({ input }) => {
       // then
-      expect(startValidation(inputValue)).not.toThrow();
+      expect(startValidation(input)).not.toThrow();
     });
   });
 });
