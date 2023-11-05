61d1dbd4657ae0f77b667ddd26839b9e650275f3
leehyewon0531 <ifpffk@naver.com>
2023-11-05 17:27:08 +0900

    feat: 사용자에게 구입금액 입력 받고 저장

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  async play() {
+    const amount = await this.receiveUserInput('구입금액을 입력해 주세요.\n');
+  }
 
   async receiveUserInput(guideMsg) {
     const userInput = await Console.readLineAsync(guideMsg);
