1856d05f6649b8a5adbecc53431837899bb83fd0
Baek <woonn97@gmail.com>
2023-11-05 23:02:12 +0900

    feat: 랜덤 숫자 생성 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class Lotto {
   #numbers;
 
@@ -11,8 +12,18 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
   // TODO: 추가 기능 구현
+  generateRandomNumber() {
+    //랜덤 숫자 생성
+    const randomNumbers = [];
+    while (randomNumbers.length < 6) {
+      const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      if (!randomNumbers.includes(randomNumber)) {
+        randomNumbers.push(randomNumber);
+      }
+    }
+    return randomNumbers;
+  }
 }
 
 export default Lotto;
