daf82723044cbf36a4064ad3114e8fa0d0705805
pakxe <pigkill40@naver.com>
2023-11-05 17:54:10 +0900

    rename: 에러 메세지 프로퍼티 명을 가독성 좋은 이름으로 변경

--- src/constant/Error.js
+++ src/constant/Error.js
@@ -4,7 +4,7 @@ const ERROR = Object.freeze({
   prefix: '[ERROR]',
   empty: '공백이 아닌 값을 입력해주세요.',
   isNan: '숫자를 입력해주세요.',
-  money: `금액은 ${LOTTO.price}원 단위로 입력해주세요.`,
+  price: `금액은 ${LOTTO.price}원 단위로 입력해주세요.`,
   range: `번호는 ${LOTTO.minLottoNum} ~ ${LOTTO.maxLottoNum} 사이의 값으로 입력해주세요.`,
   count: `당첨 번호는 ${LOTTO.count}개로 입력해주세요.`,
 });
