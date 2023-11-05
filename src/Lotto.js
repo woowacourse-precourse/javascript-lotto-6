261c9f7251c15c22623eca3b5cd4c8dadbd6b709
abi-hong <flffjakfwm20@gmail.com>
2023-11-05 21:24:40 +0900

    feat(Lotto): 로또 숫자 getter 함수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,6 @@
+// 제공된 Lotto 클래스를 활용해 구현해야 한다.
+// numbers의 # prefix를 변경할 수 없다.
+// Lotto에 필드를 추가할 수 없다.
 class Lotto {
   #numbers;
 
@@ -12,7 +15,12 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  // TODO: 추가 기능 
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  
 }
 
 export default Lotto;
