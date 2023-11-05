a4920d1ee6dbcc93409701e4a2f4feabadfdd3a4
sehyun <e6d1fe@gmail.com>
2023-11-05 12:49:47 +0900

    feat: enterWinningNumbers에 당첨 번호 검증 로직 추가

--- src/Input/enterWinningNumbers.js
+++ src/Input/enterWinningNumbers.js
@@ -1,9 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 import { message } from '../Constants.js';
 
+import validateWinningNumbers from '../Validation/validateWinningNumbers.js';
+
 async function enterWinningNumbers() {
   const winningNumbers = await Console.readLineAsync(message.ENTER_WINNING_NUMBERS);
   const winningNumbersArray = winningNumbers.split(',').map((item) => item.trim());
+
+  if (validateWinningNumbers(winningNumbersArray)) {
+    return winningNumbersArray;
+  }
+
+  throw new Error(message.ERROR);
 }
 
 export default enterWinningNumbers;
