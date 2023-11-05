8dc0e409b8538363a6954b004b76170089a9d80e
hectick <ch.yeondev@gmail.com>
2023-11-05 17:41:47 +0900

    feat: 당첨 번호와 보너스 번호를 입력받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,10 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 const InputView = {
+  readPurchasePrice() {
+    return MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+  },
 
-    readPurchasePrice() {
-      return MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
-    }
-  };
-  
-  export default InputView;
+  readWinningNumbers() {
+    return MissionUtils.Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+  },
+
+  readBonusNumber() {
+    return MissionUtils.Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+  },
+};
+
+export default InputView;
