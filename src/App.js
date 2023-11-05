75981aa9f5efdb121e551817922d0cd75498497c
donggyun <donggyunkim12@gmail.com>
2023-11-05 15:55:35 +0900

    feat(App): 로또 구매 프로세스, 당첨번호 생성 프로세스 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import LottoGame from './controller/LottoGame.js';
+
 class App {
-  async play() {}
+  async play() {
+    const lottoGame = new LottoGame();
+
+    await lottoGame.buyLotto();
+    await lottoGame.drawLotto();
+  }
 }
 
 export default App;
