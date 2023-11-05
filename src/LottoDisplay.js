3842dd3ef0ff8301f0e083e3d1ac50029a9e4ff8
gon <hss3522@gmail.com>
2023-11-05 16:10:54 +0900

    refactor: Improve display output logic

--- src/LottoDisplay.js
+++ src/LottoDisplay.js
@@ -1,5 +1,13 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const PRIZE_MESSAGES = {
+  "5등": "3개 일치 (5,000원)",
+  "4등": "4개 일치 (50,000원)",
+  "3등": "5개 일치 (1,500,000원)",
+  "2등": "5개 일치, 보너스 볼 일치 (30,000,000원)",
+  "1등": "6개 일치 (2,000,000,000원)",
+};
+
 class LottoDisplay {
   requestMoneyInput() {
     Console.print("구입금액을 입력해 주세요.");
@@ -36,13 +44,9 @@ class LottoDisplay {
   displayResults(winningCounts, rateOfReturn) {
     Console.print("당첨 통계");
     Console.print("---");
-    Console.print(`3개 일치 (5,000원) - ${winningCounts["5등"]}개`);
-    Console.print(`4개 일치 (50,000원) - ${winningCounts["4등"]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${winningCounts["3등"]}개`);
-    Console.print(
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningCounts["2등"]}개`
-    );
-    Console.print(`6개 일치 (2,000,000,000원) - ${winningCounts["1등"]}개`);
+    Object.keys(PRIZE_MESSAGES).forEach((rank) => {
+      Console.print(`${PRIZE_MESSAGES[rank]} - ${winningCounts[rank]}개`);
+    });
     Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
   }
 }
