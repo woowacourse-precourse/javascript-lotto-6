dd37e7783889aa53be0daadd22d6db695c3020e2
Ryong-E <think8867@gmail.com>
2023-11-05 15:43:41 +0900

    feature: LottoController 생성 구입금액을 받아 로또 생성 기능 추가
     예외 발생시 에러메세지를 출력하고 재입력을 받습니다

---
+++ src/controllers/LottoController.js
@@ -0,0 +1,32 @@
+import Lottos from '../models/Lottos.js';
+import WinningNumber from '../models/WinningNumber.js';
+import InputView from '../views/InputView.js';
+import OutputView from '../views/OutputView.js';
+
+class LottoController {
+  #lottos;
+
+  #winnginNumber;
+
+  constructor() {
+    this.#lottos = new Lottos();
+    this.#winnginNumber = new WinningNumber();
+  }
+
+  async play() {
+    await this.askPurchase();
+  }
+
+  async askPurchase() {
+    try {
+      const purchase = await InputView.askPurchase();
+      this.#lottos.setPurchase(purchase);
+      this.#lottos.generateLotto();
+      OutputView.printLotto(this.#lottos.getLottos());
+    } catch (error) {
+      OutputView.print(error.message);
+      await this.askPurchase();
+    }
+  }
+}
+export default LottoController;
