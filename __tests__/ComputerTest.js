65123f521194b7b2983a6ffe6e144ffd8c0257f2
chiman <rlaclghks123@naver.com>
2023-11-05 10:57:14 +0900

    test: generateLotto 반환값 수정에 의한 테스트 수정

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -5,16 +5,14 @@ describe('컴퓨터 클래스 테스트', () => {
     const generator = () => [1, 2, 3, 4, 5, 6];
 
     const computer = new Computer(generator);
-    const lottos = Array.from({ length: 3 }, () => [1, 2, 3, 4, 5, 6]);
-    expect(computer.generateLotto('3000')).toEqual({ lottos, quantity: 3 });
+    expect(computer.generateLotto('3000')).toHaveLength(3);
   });
 
   test('금액에 맞게 로또 개수를 계산하는 테스트 -> 12000 입력시', () => {
     const generator = () => [1, 2, 3, 4, 5, 6];
 
     const computer = new Computer(generator);
-    const lottos = Array.from({ length: 12 }, () => [1, 2, 3, 4, 5, 6]);
-    expect(computer.generateLotto('12000')).toEqual({ lottos, quantity: 12 });
+    expect(computer.generateLotto('12000')).toHaveLength(12);
   });
 
   test('로또 개수와 번호가 올바르게 나오는지 테스트 2000 입력시', () => {
@@ -22,12 +20,6 @@ describe('컴퓨터 클래스 테스트', () => {
 
     const computer = new Computer(generator);
 
-    expect(computer.generateLotto('2000')).toEqual({
-      lottos: [
-        [10, 11, 12, 15, 19, 20],
-        [10, 11, 12, 15, 19, 20],
-      ],
-      quantity: 2,
-    });
+    expect(computer.generateLotto('2000')).toHaveLength(2);
   });
 });
