43b9f9905ee781d3aedf5693e62370fb09c7ac03
swJaNG12 <cbdjej38483@gmail.com>
2023-11-06 07:12:18 +0900

    rafactor(Lotto) : lotto class refactored
    validate private method removed and add a new validateLottoAmount method
    validateLottoAmount method checks if the lotto purchase amount is
    a multiple of 1000 and returns a boolean value.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,17 @@
+const LOTTO_PRICE = 1000;
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  validateLottoAmount() {
+    return Number.isInteger(
+      parseInt(this.#numbers.split(',').join(''), 10) / LOTTO_PRICE
+    );
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
