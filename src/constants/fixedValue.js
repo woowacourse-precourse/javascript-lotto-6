940ecac5be612c4352ce7af8d89daddacce0114f
Seoyeon Kim <kseoy0607@gmail.com>
2023-11-05 21:26:26 +0900

    :sparkles: rename: 상수 값 추가로 인한 파일 이름 수정

---
+++ src/constants/fixedValue.js
@@ -0,0 +1,22 @@
+export const LIMITS = {
+  minRange: 1,
+  maxRange: 45,
+  count: 6,
+  priceUnit: 1000,
+  decimalPlaces: 1,
+};
+
+export const MATCHED_COUNT = {
+  fifthPlace: 3,
+  fourthPlace: 4,
+  secondAndThirdPlace: 5,
+  firstPlace: 6,
+};
+
+export const PRIZE_AMOUNT = {
+  fifthPlace: 5000,
+  fourthPlace: 50000,
+  thirdPlace: 1500000,
+  secondPlace: 30000000,
+  firstPlace: 2000000000,
+};
--- src/constants/limits.js
+++
@@ -1,8 +0,0 @@
-const LIMITS = {
-  minRange: 1,
-  maxRange: 45,
-  count: 6,
-  priceUnit: 1000,
-};
-
-export default LIMITS;
