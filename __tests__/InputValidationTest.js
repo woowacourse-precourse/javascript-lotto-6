46890d04f9985c1363eaf13ff7d755cd0c1067c1
jeeyng <petitesize22@gmail.com>
2023-11-05 15:32:37 +0900

    FIX: isThousands() 비교 연산자 수정

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -1,4 +1,11 @@
+import { Console } from '@woowacourse/mission-utils';
 import VALIDATION from '../src/Controller/Validation';
+import INPUT_VIEW from '../src/View/inputView';
+
+const mockfn = (input) => {
+  Console.readLineAsync = jest.fn();
+  Console.readLineAsync.mockImplementation(() => Promise.resolve(input));
+};
 
 describe('Input Validation Test', () => {
   test('빈 값 입력 테스트', async () => {
@@ -14,5 +21,12 @@ describe('Input Validation Test', () => {
   test('1,000 단위 입력 테스트', async () => {
     const input = '1';
     expect(VALIDATION.priceValidation(input)).rejects.toThrow('[ERROR]');
+  });
+
+  test('PRICE return 테스트', async () => {
+    const input = '1000';
+    mockfn(input);
+    const result = await INPUT_VIEW.inputPrice(input);
+    expect(result).toEqual(input);
   });
 });
