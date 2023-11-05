4461cb7041b28bce4e666ad4e3549aa3c2ee619c
leehyewon0531 <ifpffk@naver.com>
2023-11-05 17:20:45 +0900

    feat: 사용자에게 입력 받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 class App {
   async play() {}
+
+  async receiveUserInput(guideMsg) {
+    const userInput = await Console.readLineAsync(guideMsg);
+    return userInput;
+  }
 }
 
 export default App;
