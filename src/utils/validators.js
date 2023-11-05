ff6db006617edeb560cba8f6b6a3b96b91ef7147
Lee sang Yeop <mma7710@naver.com>
2023-11-05 20:21:18 +0900

    feat(validators): 사용자 입력 값에 대한 에러 핸들링 추가 - 사용자가 처음 구입 금액에 대한 입력 에러 핸들링 - 사용자가 입력하는 당첨 번호 값 에러 핸들링 - 사용자가 입력하는 보너스 번호 에러 핸들링
    그 이외 다른 클래스의 데이터를 검증하는 함수를 추후 추가할 예정

---
+++ src/utils/validators.js
@@ -0,0 +1,64 @@
+import ERROR_MESSAGES from '../constants/ErrorMessages';
+import LOTTO_SYSTEM from '../constants/LottoSystem';
+import PURCHASE_CONDITIONS from '../constants/PurchaseConditions';
+
+const validateCondition = {
+	isDigit(value) {
+		if (!/^-?\d+$/.test(value)) throw new Error(ERROR_MESSAGES.value_is_not_digit);
+	},
+
+	isPositive(value) {
+		if (Number(value) <= 0) throw new Error(ERROR_MESSAGES.value_is_not_positive);
+	},
+
+	isTotalPriceDivisibleByLottoPrice(value) {
+		if (Number(value) % LOTTO_SYSTEM.lotto_price !== 0)
+			throw new Error(ERROR_MESSAGES.value_is_indivisible);
+	},
+
+	isLottoNumberInRange(value) {
+		if (Number(value) < LOTTO_SYSTEM.minimum_number || value > LOTTO_SYSTEM.maximum_number)
+			throw new Error(ERROR_MESSAGES.value_is_not_in_range);
+	},
+
+	isPurchasePriceExceedMaximumPrice(value) {
+		if (Number(value) > PURCHASE_CONDITIONS.maximum_lotto_purchase_price)
+			throw new Error(ERROR_MESSAGES.value_is_over_maximum_price);
+	},
+
+	checkLottoNumberCount(value) {
+		if (value.length !== LOTTO_SYSTEM.lotto_numbers)
+			throw new Error(ERROR_MESSAGES.lotto_number_count_not_valid);
+	},
+
+	checkDuplicate(value) {
+		const uniqueValue = new Set(value);
+		if (uniqueValue.size !== value.length) throw new Error(ERROR_MESSAGES.duplicate_value);
+	},
+};
+
+const validators = {
+	inputPurchasePrice(price) {
+		validateCondition.isDigit(price);
+		validateCondition.isPositive(price);
+		validateCondition.isTotalPriceDivisibleByLottoPrice(price);
+		validateCondition.isPurchasePriceExceedMaximumPrice(price);
+	},
+
+	inputWinningNumber(numbers) {
+		validateCondition.checkDuplicate(numbers);
+		validateCondition.checkLottoNumberCount(numbers);
+
+		numbers.forEach((number) => {
+			validateCondition.isDigit(number);
+			validateCondition.isLottoNumberInRange(number);
+		});
+	},
+
+	inputWinningBonusNumber(number) {
+		validateCondition.isDigit(number);
+		validateCondition.isLottoNumberInRange(number);
+	},
+};
+
+export default validators;
