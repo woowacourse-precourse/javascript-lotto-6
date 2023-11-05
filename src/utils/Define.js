38359d0760402d671875c070cccf669c70d336e7
kimjinwoo <rlawlsdn177@naver.com>
2023-11-05 18:05:45 +0900

    refactor(Validation.js): 유효성 검사 구현부 상수 처리
    - (Define.js): 에러 메세지 ERROR 객체에 추가 및 매직 넘버 선언
    - ($checkBonusNumbers, $checkPurchaseAmount, $checkWinningNumbers): 에러 메세지 변경,
    매직 넘버 변경

--- src/utils/Define.js
+++ src/utils/Define.js
@@ -1,5 +1,16 @@
+export const ZERO = 0;
+
+export const LOTTO_PRICE = 1000;
+
+export const MIN_LOTTO_NUMBER = 1;
+export const MAX_LOTTO_NUMBER = 45;
+export const LOTTO_NUMBER_COUNT = 6;
+
 export const ERROR = {
     HEAD: '[ERROR]',
     DUPLICATE_LOTTO_NUMBER: '로또 번호는 중복된 값을 가질 수 없습니다.',
     NOT_INPUT_SIX_NUMBER: '로또 번호는 6개를 입력해야 합니다.',
+    INVALID_AMOUNT_MESSAGE: '구매 금액은 1,000원 단위여야 합니다.',
+    INVALID_WINNING_NUMBERS_MESSAGE: '당첨 번호는 1부터 45 사이의 유니크한 6개 숫자여야 합니다.',
+    INVALID_BONUS_NUMBER_MESSAGE: '보너스 볼은 당첨 번호와 달라야 하며, 1부터 45 사이의 숫자여야 합니다.',
 };
--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,33 +1,42 @@
+import {
+    ERROR,
+    LOTTO_NUMBER_COUNT,
+    LOTTO_PRICE,
+    MAX_LOTTO_NUMBER, 
+    MIN_LOTTO_NUMBER,
+    ZERO,
+} from './Define';
+
 export const checkBonus = (lotto, bonusNumber) => (
     lotto.getNumbers().includes(bonusNumber)
 );
 
 export const checkBonusNumbers = (bonusNumber, winningNumbers) => {
     if (isNaN(bonusNumber)) {
-        throw new Error('보너스 볼은 당첨 번호와 달라야 하며, 1부터 45 사이의 숫자여야 합니다.');
+        throw new Error(ERROR.INVALID_BONUS_NUMBER_MESSAGE);
     }
     if (winningNumbers.includes(bonusNumber)) {
-        throw new Error('보너스 볼은 당첨 번호와 달라야 하며, 1부터 45 사이의 숫자여야 합니다.');
+        throw new Error(ERROR.INVALID_BONUS_NUMBER_MESSAGE);
     }
-    if (bonusNumber < 1 || bonusNumber > 45) {
-        throw new Error('보너스 볼은 당첨 번호와 달라야 하며, 1부터 45 사이의 숫자여야 합니다.');
+    if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+        throw new Error(ERROR.INVALID_BONUS_NUMBER_MESSAGE);
     }
 };
 
 export const checkPurchaseAmount = (amount) => {
-    if (isNaN(amount) || amount % 1000 !== 0) {
-        throw new Error('구매 금액은 1,000원 단위여야 합니다.');
+    if (isNaN(amount) || amount % LOTTO_PRICE !== ZERO) {
+        throw new Error(ERROR.INVALID_AMOUNT_MESSAGE);
     }
 };
 
 export const checkWinningNumbers = (numbers) => {
     if (numbers.some(isNaN)) {
-        throw new Error('당첨 번호는 1부터 45 사이의 유니크한 6개 숫자여야 합니다.');
+        throw new Error(ERROR.INVALID_WINNING_NUMBERS_MESSAGE);
     }
-    if (numbers.length !== 6) {
-        throw new Error('당첨 번호는 1부터 45 사이의 유니크한 6개 숫자여야 합니다.');
+    if (numbers.length !== LOTTO_NUMBER_COUNT) {
+        throw new Error(ERROR.INVALID_WINNING_NUMBERS_MESSAGE);
     }
-    if (new Set(numbers).size !== 6) {
-        throw new Error('당첨 번호는 1부터 45 사이의 유니크한 6개 숫자여야 합니다.');
+    if (new Set(numbers).size !== LOTTO_NUMBER_COUNT) {
+        throw new Error(ERROR.INVALID_WINNING_NUMBERS_MESSAGE);
     }
 };
