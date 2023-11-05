3c0156d26f711e888289c5e95dd9e1a05086b5a1
kimdaye77 <kimdaye77@naver.com>
2023-11-05 14:49:07 +0900

    feat: 구입금액 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-class App {
-  async play() {}
-}
+import { Console } from "@woowacourse/mission-utils";
 
+import Lotto from "./Lotto";
+
+class App {
+  async play() {
+
+    const purchaseAmountInput = await Console.readLineAsync(
+      "구입금액을 입력해 주세요\n"
+    );
+
+    //TODO: 예외처리
+    
+  
+  }
+}
 export default App;
