48bc78c307ecaea62070b42871ef95bff331b94f
prgmr99 <johnyeom24@naver.com>
2023-11-06 03:19:17 +0900

    [feat] 중복되지 않은 로또 번호 생성

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,9 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
