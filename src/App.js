2e34032f9f52371ff7f95b00cafe6964d8dcad51
kimjinwoo <rlawlsdn177@naver.com>
2023-11-05 19:42:26 +0900

    feat($generateAndPrintLottos): 로또 객체 생성 및 로또 번호 출력 메서드 구현
    - ($generateLotto) 호출로 로또 객체 및 로또 번호 생성
    - lottos에 로또 객체 추가
    - ($printLottoNumbers): 로또 번호 출력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import {
     INIT_ZERO,
 } from './utils/Define';
+import {
+    generateLotto,
+} from './utils/Calculate';
+import {
+    printLottoNumbers,
+} from './io/Output';
+
 class App {
     lottos;
     results;
@@ -11,7 +18,16 @@ class App {
         this.results = { '3': INIT_ZERO, '4': INIT_ZERO, '5': INIT_ZERO, '5+': INIT_ZERO, '6': INIT_ZERO };
         this.purchaseAmount = INIT_ZERO;
     }
-  async play() {}
+    async play() {
+    }
+
+    generateAndPrintLottos(numberOfLottos) {
+        for (let i = INIT_ZERO; i < numberOfLottos; i += ONE) {
+            const lotto = generateLotto();
+            this.lottos.push(lotto);
+            printLottoNumbers(lotto);
+        }
+    }
 }
 
 export default App;
