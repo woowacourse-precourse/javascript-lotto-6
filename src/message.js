7acd56586a2258bcd819b8542f1197e0651f3ab6
Moon Sojeong <sjeong35@naver.com>
2023-11-05 16:33:04 +0900

    refactor: App.js 에러 메세지를 담는 객체 생성

---
+++ src/message.js
@@ -0,0 +1,7 @@
+const ERROR = object.freeze({
+    NOT_NUMBER : '[ERROR] 숫자가 잘못된 형식입니다.',
+    MIN_PRICE : '[ERROR] 로또 최소 구입 금액은 1000원입니다.',
+    THOUSAND_UNIT : '[ERROR] 금액은 1,000원 단위로 입력해주세요.',
+    EMPTY : '[ERROR] 숫자를 입력하세요.',
+    RANGE_BONUS : '[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.',
+});
