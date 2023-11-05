57f083234194c1d2822c2bb7c92784ef31fe6050
j8won <joowoni99@gmail.com>
2023-11-05 16:40:01 +0900

    test(LottoPrizeManagerTest): 보너스 번호가 로또 숫자 범위 내의 숫자가 아니라면 예외 처리하는지 확인하는 테스트 코드 작

--- __tests__/LottoPrizeManagerTest.js
+++ __tests__/LottoPrizeManagerTest.js
@@ -1,5 +1,7 @@
 import LottoPrizeManager from "../src/LottoPrizeManager.js";
 import PRIZE from "../src/constant/PRIZE.js";
+가;
+const correctWinningNumberArray = ["1", "2", "3", "4", "5", "6"];
 
 describe("LottoPrizeManager 클래스 테스트", () => {
   test("당첨 번호가 6개가 아니라면 예외 처리한다", () => {
@@ -28,4 +30,13 @@ describe("LottoPrizeManager 클래스 테스트", () => {
       new LottoPrizeManager(winningNumberArray);
     }).toThrow(PRIZE.ERROR.WINING_NUMBER_DUPLICATE);
   });
+
+  test.each([["100"], ["123s"], ["015"]])(
+    "보너스 번호가 로또 숫자 범위 내의 숫자가 아니라면 예외 처리한다 - %s",
+    (bonusNumber) => {
+      expect(() => {
+        new LottoPrizeManager(correctWinningNumberArray, bonusNumber);
+      }).toThrow(PRIZE.ERROR.BONUS_NUMBER_RANGE_NUMBER);
+    }
+  );
 });
