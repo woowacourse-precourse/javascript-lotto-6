ecbfe41d79352329d853b363e92a679d0db2d46b
LJH <049494@ewhain.net>
2023-11-05 19:58:26 +0900

    ✨ feat : 발행한 로또 모두 출력하기

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import Lotto from "./Lotto.js";
 class App {
   #purchase_amount;
   #purchase_quantity;
-  #lotto;
+  #lotto_list;
 
-  constructor(purchase_amount, purchase_quantity, lotto) {
+  constructor(purchase_amount, purchase_quantity, lotto_list) {
     this.#purchase_amount = purchase_amount;
     this.#purchase_quantity = purchase_quantity;
-    this.#lotto = lotto;
+    this.#lotto_list = lotto_list;
   }
 
   async play() {
@@ -21,9 +21,10 @@ class App {
     this.#purchase_amount = await getPurchaseAmount();
     this.#purchase_quantity = calcPurchaseQuantity(this.#purchase_amount);
     const LOTTO = new Lotto();
-    this.#lotto = LOTTO.returnLotto(this.#purchase_quantity);
+    this.#lotto_list = LOTTO.returnLotto(this.#purchase_quantity);
     printMessage("\n");
     printMessage(`${this.#purchase_quantity}개를 구매했습니다.`);
+    this.#lotto_list.map((lotto) => printMessage(lotto));
   }
 }
 
