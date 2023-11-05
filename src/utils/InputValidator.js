f89eb429655c561d368eedf4c83db78aa16ab2ea
Trophy198 <spsp8426@naver.com>
2023-11-05 23:22:14 +0900

    refactor : 구입 금액 입력시 빈칸일때 검증 추가 및 코드 가독성 개선

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -1,40 +1,48 @@
+import { ERROR_MESSAGES } from '../constants/ErrorMessages';
+
 export default class InputValidator {
   static validatePurchaseAmount(amount) {
+    if (amount === undefined || amount === null || amount.trim() === '') {
+      throw new Error(ERROR_MESSAGES.EMPTY_AMOUNT);
+    }
     if (amount % 1000 !== 0) {
-      throw new Error('구입 금액은 1000원 단위로 입력해야 합니다.');
+      throw new Error(ERROR_MESSAGES.AMOUNT_NOT_IN_UNITS);
     }
   }
 
-  static validateWinningNumbers(winningNumbersString) {
-    const winningNumbers = winningNumbersString.split(',').map((num) => {
-      const trimmedNum = num.trim();
-      if (trimmedNum === '') {
-        throw new Error('모든 당첨 번호를 쉼표(,)로 구분하여 입력해야 합니다.');
+  static parseAndValidateNumbers(numbersString) {
+    return numbersString.split(',').map((numString) => {
+      const number = parseInt(numString.trim(), 10);
+      if (Number.isNaN(number)) {
+        throw new Error(ERROR_MESSAGES.EMPTY_NUMBER);
       }
-      return parseInt(trimmedNum, 10);
+      if (number < 1 || number > 45) {
+        throw new Error(ERROR_MESSAGES.NUMBER_OUT_OF_RANGE);
+      }
+      return number;
     });
+  }
+
+  static validateWinningNumbers(winningNumbersString) {
+    const winningNumbers = InputValidator.parseAndValidateNumbers(winningNumbersString);
 
     if (winningNumbers.length !== 6) {
-      throw new Error('당첨 번호는 쉼표(,)로 구분된 6개의 숫자여야 합니다.');
+      throw new Error(ERROR_MESSAGES.NOT_SIX_NUMBERS);
     }
-
-    if (winningNumbers.some((num) => Number.isNaN(num) || num < 1 || num > 45)) {
-      throw new Error('당첨 번호는 1부터 45 사이의 숫자여야 합니다.');
-    }
-
     if (new Set(winningNumbers).size !== 6) {
-      throw new Error('당첨 번호에 중복된 숫자가 있습니다.');
+      throw new Error(ERROR_MESSAGES.DUPLICATE_NUMBER);
     }
   }
 
   static validateBonusNumber(bonusNumberString, winningNumbersString) {
     const bonusNumber = parseInt(bonusNumberString.trim(), 10);
-    const winningNumbers = winningNumbersString.split(',').map((num) => parseInt(num.trim(), 10));
-    if (winningNumbers.includes(bonusNumber)) {
-      throw new Error('보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+    if (Number.isNaN(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
+      throw new Error(ERROR_MESSAGES.NUMBER_OUT_OF_RANGE);
     }
-    if (bonusNumber < 1 || bonusNumber > 45) {
-      throw new Error('보너스 번호는 1부터 45 사이의 숫자여야 합니다.');
+
+    const winningNumbers = InputValidator.parseAndValidateNumbers(winningNumbersString);
+    if (winningNumbers.includes(bonusNumber)) {
+      throw new Error(ERROR_MESSAGES.BONUS_NUMBER_DUPLICATE);
     }
   }
 }
