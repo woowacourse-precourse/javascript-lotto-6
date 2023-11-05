7cc030fa093de12bb5cf6c5eee2f59054bad2e96
donggyun <donggyunkim12@gmail.com>
2023-11-05 15:37:23 +0900

    feat(inputPrompt): 당첨번호,보너스번호 입력 기능 구현

--- src/view/inputPrompt.js
+++ src/view/inputPrompt.js
@@ -4,3 +4,11 @@ import { INPUT_MESSAGE } from '../constant/message.js';
 export const inputMoney = () => {
   return Console.readLineAsync(INPUT_MESSAGE.INPUT_MONEY);
 };
+
+export const inputWinningNumber = () => {
+  return Console.readLineAsync(INPUT_MESSAGE.INPUT_WINNER_NUMBER);
+};
+
+export const inputBonusNumber = () => {
+  return Console.readLineAsync(INPUT_MESSAGE.INPUT_BONUS_NUMBER);
+};
