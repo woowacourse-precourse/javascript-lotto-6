ace8f73a322ad2fe8411fccd2bca8ec631e6123c
songhyunseop <gustjq99455352@gmail.com>
2023-11-05 11:57:24 +0900

    Feat: 최종 로또 추첨결과 수익금 합산 로직 구현

--- src/App.js
+++ src/App.js
@@ -28,27 +28,37 @@ class App {
     );
 
     const winningNumbers = {};
-    const winningStats = {
-      3: '3개 일치 (5,000원) ',
-      4: '4개 일치 (50,000원) ',
-      5: '5개 일치 (1,500,000원) ',
-      '2nd': '5개 일치, 보너스 볼 일치 (30,000,000원) ',
-      6: '6개 일치 (2,000,000,000원) ',
+    const stats = {
+      3: '3개 일치 (5,000원)',
+      4: '4개 일치 (50,000원)',
+      5: '5개 일치 (1,500,000원)',
+      '2nd': '5개 일치, 보너스 볼 일치 (30,000,000원)',
+      6: '6개 일치 (2,000,000,000원)',
     };
+
+    const initialLottoStats = Object.keys(stats).reduce((acc, key) => {
+      acc[stats[key]] = 0;
+
+      return acc;
+    }, {});
 
     lottoNumbers.forEach((lottoNum) => {
       const sameCount = lotto.checkLotteryResult(lottoNum, Number(BonusNumber));
 
-      if (sameCount > 2) {
-        winningNumbers[sameCount] = (winningNumbers[sameCount] || 0) + 1;
-      }
+      if (sameCount > 2) initialLottoStats[stats[sameCount]] += 1;
     });
 
-    Object.keys(winningStats).forEach((el) => {
-      if (!winningStats[el]) winningStats[el] = 0;
-      if (!winningNumbers[el]) winningNumbers[el] = 0;
+    Object.keys(initialLottoStats).forEach((el) => {
+      const regex1 = /\(([^)]+)\)/;
+      const splitAmount = el.match(regex1)[0];
 
-      Console.print(`${winningStats[el]}- ${winningNumbers[el]}개`);
+      Console.print(`${splitAmount}`);
+
+      const moneyAmount = splitAmount.replace(/[^0-9]/g, '');
+      const getMoneyAmount = moneyAmount * initialLottoStats[el];
+
+      Console.print(`${el} - ${initialLottoStats[el]}개`);
+      Console.print(`결과${getMoneyAmount}`);
     });
   }
 }
