fe285d3e7b9e1e0e075d4f93b1bbe080648628c8
thumbthing <ilscc0902@gmail.com>
2023-11-06 08:59:21 +0900

    refactor: 변수명 수정

--- src/util/matcher/lottoMatcher.js
+++ src/util/matcher/lottoMatcher.js
@@ -1,7 +1,6 @@
 import parseToMatchedCount from '../parse/parseToMatchedCount.js';
 
-export default function lottoMatcher(puchases, winningNumber, bonusNumber) {
-  const match = puchases.map((element) => parseToMatchedCount(element, winningNumber, bonusNumber));
-
+export default function lottoMatcher(purchase, winningNumber, bonusNumber) {
+  const match = purchase.map((element) => parseToMatchedCount(element, winningNumber, bonusNumber));
   return match;
 }
