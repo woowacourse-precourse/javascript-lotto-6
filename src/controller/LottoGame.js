0f2c6e02802510f6de4b96511c92d0f6948b0ae8
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-11-06 04:10:28 +0900

    feat: LottoGame 당첨된 로또 개수 확인 기능 구현

--- src/controller/LottoGame.js
+++ src/controller/LottoGame.js
@@ -27,4 +27,48 @@ class LottoGame {
     this.winningLotto.setBonusNumber(bonusNumber);
     Output.printNewLine();
   }
+
+  saveLottoRank() {
+    const ranks = Array.from({ length: 6 }, () => 0);
+    this.player.playerLottos.forEach((lotto) => {
+      const rank = this.compareLotto(lotto.lottoNumbers, this.winningLotto);
+      ranks[rank] += 1;
+    });
+    return ranks;
+  }
+
+  compareLotto(lottoNumbers, winningLotto) {
+    let count = 0;
+    let bonusCheck = false;
+
+    count = winningLotto.lottoNumbers.reduce((acc, currentNumber) => {
+      if (lottoNumbers.includes(currentNumber)) {
+        return acc + 1;
+      }
+      return acc;
+    }, count);
+    if (lottoNumbers.includes(winningLotto.getBonusNumber())) {
+      bonusCheck = true;
+    }
+
+    return this.getRank(count, bonusCheck);
+  }
+
+  getRank(count, bonusCheck) {
+    switch (count) {
+      case 3:
+        return 5;
+      case 4:
+        return 4;
+      case 5:
+        if (bonusCheck) {
+          return 2;
+        }
+        return 3;
+      case 6:
+        return 1;
+      default:
+        return 0;
+    }
+  }
 }
