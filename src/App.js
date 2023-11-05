23ab470bc45d365d6323339d4102e80f13b4ff31
MinGu Jeong <mingu830@gmail.com>
2023-11-05 16:49:42 +0900

    feat: 당첨번호 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import Lotto from './domain/Lotto.js';
+import { Console, Random } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    await autoLottoNumber(await amount());
+  }
 }
 
 export default App;
+
+const amount = async function inputAmount() {
+  const input = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  validateAmount(input);
+  return input;
+};
+
+const validateAmount = function validateAmount(amount) {
+  if (amount % 1000 !== 0) {
+    throw new Error('[ERROR] 1000원 단위로 입력해 주세요.');
+  }
+};
+
+const winLottoNumber = async function inputWinLottoNumber() {
+  let input = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+  input = input.split(',').map((number) => Number(number.trim()));
+};
+
+const bonusLottoNumber = async function inputBonusLottoNumber() {
+  let input = await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+  input = Number(input);
+};
+
+const autoLottoNumber = function generateAutoLottoNumber(amount) {
+  const lottoNumbers = [];
+  for (let i = 0; i < amount / 1000; i++) {
+    const lottoNumberSet = new Set();
+    while (lottoNumberSet.size < 6) {
+      lottoNumberSet.add(Random.pickNumberInRange(1, 45));
+    }
+    lottoNumbers.push([...lottoNumberSet]);
+  }
+  console.log(lottoNumbers);
+  return lottoNumbers;
+};
