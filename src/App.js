b68f10c018ba1aec7b611dda300dd1286c65c000
soi-ha <soy2302ten@gmail.com>
2023-11-05 22:31:30 +0900

    feat: 보너스 번호 입력 유효성 검사 BonusLotto.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { InputValue } from './InputValue.js';
 import Lotto from './Lotto.js';
 import { BuyLotto } from './BuyLotto.js';
+import { BonusLotto } from './BonusLotto.js';
 
 class App {
 	async play() {
 		try {
-			// const inputValue = new InputValue();
-			// const win = await inputValue.winningNumber();
-			new BuyLotto(-100);
-			// new Lotto(win);
+			const inputValue = new InputValue();
+			const win = await inputValue.winningNumber();
+			new Lotto(win);
+			const bonus = await inputValue.bonusNumber();
+			new BonusLotto(bonus, win);
+			// new BuyLotto(-100);
 		} catch (error) {
 			console.log(error.message);
 		}
