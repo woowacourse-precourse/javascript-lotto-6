f7fc2e0bbc59fc7ba634c2073c8cfdeea65daf61
ohgus <anakin1108@naver.com>
2023-11-05 21:00:03 +0900

    feat: 필드 및 constructor 선언

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
 class App {
+  #money;
+  #winningNumbers;
+  #bonusNumber;
+
+  constructor() {
+    this.lottoBoard = {
+      threeSame: 0,
+      fourSame: 0,
+      fiveSame: 0,
+      fiveAndBonusSame: 0,
+      sixSame: 0,
+      rateOfReturn: 0,
+    };
+  }
+
   async play() {}
 }
 
