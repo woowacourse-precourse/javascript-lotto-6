bcd1f2e1e9e1404d34396e801a1f98c2b21b1622
yjh1008 <exesea12@gmail.com>
2023-11-05 23:16:43 +0900

    feat : 게임 문구 추가.
    1. 당첨 번호 및 보너스 번호를 입력받는 문자열 추가.
    2. 당쳠 내역을 출력하는 문자열 추가.

--- src/GameComment..js
+++ src/GameComment..js
@@ -8,19 +8,25 @@ const PRINT_PURCHASE_LOTTO = (lottoContainer) => {
     str += `[${v.get().join(", ")}]\n`
   });
   return str;
-}
+};
+
+const INPUT_WINNING_NUMBER = "당첨 번호를 입력해주세요";
 
 const WINNING_OUTCOME = (arr) => {
   return `3개 일치 (5,000원) - ${arr[0]}개\n
-  4개 일치 (50,000원) - ${arr[1]}개\n
-  5개 일치 (1,500,000원) - ${arr[2]}개\n
-  5개 일치, 보너스 볼 일치 (30,000,000원) - ${arr[3]}개\n
-  6개 일치 (2,000,000,000원) - ${arr[4]}개\n`
+4개 일치 (50,000원) - ${arr[1]}개\n
+5개 일치 (1,500,000원) - ${arr[2]}개\n
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${arr[3]}개\n
+6개 일치 (2,000,000,000원) - ${arr[4]}개\n`
 }
+
+const GENERATE_BONUS_NUM = "보너스 번호를 입력해 주세요.";
 
 export {
   BUY_LOTTO, 
   LOTTO_COUNT,
   PRINT_PURCHASE_LOTTO,
-  WINNING_OUTCOME
+  INPUT_WINNING_NUMBER,
+  WINNING_OUTCOME,
+  GENERATE_BONUS_NUM
 }
