2f0149b2e2e9a20e872a8908d874230b73f77667
mywhzy <cmj2393a@naver.com>
2023-11-05 22:21:56 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  amount;
+
+  async play() {
+    await this.start();
+  }
+
+  async start() {
+    await this.enterAmount();
+    this.validateAmount();
+    MissionUtils.Console.print('');
+  }
+
+  async enterAmount() {
+    this.amount = await MissionUtils.Console.readLineAsync(
+      '구입금액을 입력해 주세요.\n'
+    );
+  }
+
+  validateAmount() {
+    if (/\D/.test(this.amount)) {
+      throw new Error('[ERROR] 로또 구입 금액은 숫자형태로만 입력해야 합니다.');
+    } else if (this.amount < 1000) {
+      throw new Error('[ERROR] 로또 구입은 1000원부터 가능합니다.');
+    } else if (this.amount % 1000 !== 0) {
+      throw new Error('[ERROR] 로또 구입은 1000원 단위로 가능합니다.');
+    }
+  }
 }
 
 export default App;
