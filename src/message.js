698a336f623f9ac5e450b9785859cd30d0c82cb8
Moon Sojeong <sjeong35@naver.com>
2023-11-05 17:00:12 +0900

    refactor: ERROR 객체 이름을 ERROR_MESSAGE로 수정

--- src/message.js
+++ src/message.js
@@ -1,4 +1,4 @@
-const ERROR = Object.freeze({
+const ERROR_MESSAGE = Object.freeze({
     NOT_NUMBER : '[ERROR] 숫자가 잘못된 형식입니다.',
     MIN_PRICE : '[ERROR] 로또 최소 구입 금액은 1000원입니다.',
     THOUSAND_UNIT : '[ERROR] 금액은 1,000원 단위로 입력해주세요.',
@@ -6,4 +6,8 @@ const ERROR = Object.freeze({
     RANGE_BONUS : '[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.',
 });
 
-export {ERROR};
+const PRINT_MESSAGE = Object.freeze({
+
+});
+
+export {ERROR_MESSAGE};
