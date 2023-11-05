12c885230877acfbb8f2ec1ba7627822403f9c41
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2023-11-06 00:28:19 +0900

    feat : 정기적으로 출력되는 문구 구현현

---
+++ src/Constants.js
@@ -0,0 +1,9 @@
+// 정기적으로 출력되는 문구를 변수로 저장
+const GAME_MESSAGE = {
+  purchaseAmount: "구입 금액을 입력하세요\n",
+  guessNumbers: "당첨 번호를 입력하세요\n",
+  bonusNumber: "보너스 번호를 입력하세요\n",
+  static: "당첨 통계\n---",
+};
+
+module.exports = { GAME_MESSAGE };
