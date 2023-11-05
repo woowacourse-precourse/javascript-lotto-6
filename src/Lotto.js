57e68e1d925e4d40f41c9f7a41f019230232e18d
SU_VIN <pyotnqls25@naver.com>
2023-11-05 14:20:39 +0900

    Feat: 당첨 번호 입력하는 기능
    - 예외처리 : ,로 나누어 6글자인지
    - 예외처리 : 입력값이 1~45 사이의 숫자인지
    - 예외처리 : 중복된 값이 없는지

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+import { Message } from './Message.js';
 class Lotto {
   #numbers;
 
@@ -5,10 +6,21 @@ class Lotto {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
+  get numbers() {
+    return this.#numbers;
+  }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(Message.error.NOT_SIX_LENGTH);
+    }
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error(Message.error.NOT_RANGE);
+      }
+    });
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error(Message.error.NOT_UNIQUE);
     }
   }
 
--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,18 @@
 import { LottoStoreUI } from './LottoStoreUI.js';
 import { LottoMachine } from './LottoMachine.js';
+import { LottoInput } from './LottoInput.js';
 
 class Main {
   constructor() {
     this.lottoStroreUI = new LottoStoreUI();
+    this.lottoInput = new LottoInput();
   }
 
   start = async () => {
     const numberOfLotto = await this.lottoStroreUI.printPurchasAmount();
     const lottoMachine = new LottoMachine(numberOfLotto);
     const lottoList = await lottoMachine.createLottoNumber();
+    const winningNumbers = await this.lottoInput.printWinningNumber();
   };
 }
 
