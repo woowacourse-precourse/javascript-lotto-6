ae023433cbb1bf4c9436382246ef955e929a914a
juseong <jjs7280@gmail.com>
2023-11-05 17:32:48 +0900

    refactor: App 클래스가 사용자의 상호작용을 담당하도록 getUserPrice 메서드 Price 클래스에서 이동

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Console } from '@woowacourse/mission-utils';
 import Price from './Price.js';
+import { CONSOLE_MESSAGE, ERROR_MESSAGE } from './constants.js';
 
 class App {
+  async getUserPrice() {
+    const priceInput = await Console.readLineAsync(
+      CONSOLE_MESSAGE.PROMPT_USER_PRICE
+    );
+    if (!Price.isValidPrice(priceInput)) {
+      throw new Error(
+        isNaN(priceInput)
+          ? ERROR_MESSAGE.FORMAT_ERROR
+          : ERROR_MESSAGE.PRICE_UNIT_ERROR
+      );
+    }
+    const price = Number(priceInput);
+    return price;
+  }
+
   async play() {
-    const userPriceInput = await Price.getUserPrice();
+    const userPriceInput = await this.getUserPrice();
     const lottoAmount = Price.calculateLottoAmount(userPriceInput);
+    console.log(lottoAmount);
   }
 }
 
--- src/Price.js
+++ src/Price.js
@@ -1,30 +1,10 @@
-import { Console } from '@woowacourse/mission-utils';
-import {
-  CONSOLE_MESSAGE,
-  ERROR_MESSAGE,
-  LOTTO_PRICE_UNIT,
-} from './constants.js';
+import { LOTTO_PRICE_UNIT } from './constants.js';
 
 class Price {
   constructor() {}
 
   static isValidPrice(price) {
     return !isNaN(price) && price % LOTTO_PRICE_UNIT === 0;
-  }
-
-  static async getUserPrice() {
-    const priceInput = await Console.readLineAsync(
-      CONSOLE_MESSAGE.PROMPT_USER_PRICE
-    );
-    if (!this.isValidPrice(priceInput)) {
-      throw new Error(
-        isNaN(priceInput)
-          ? ERROR_MESSAGE.FORMAT_ERROR
-          : ERROR_MESSAGE.PRICE_UNIT_ERROR
-      );
-    }
-    const price = Number(priceInput);
-    return price;
   }
 
   static calculateLottoAmount(price) {
