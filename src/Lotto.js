5b2df79dee8f23fc92417a7cbbf5222e4007e88d
lurgi <wahoo9040@naver.com>
2023-11-05 09:35:53 +0900

    feat(Lotto): 1등~5등 확인 로직
    docs(docs): 1등~5등 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { validLottoNumber } from "./Valid";
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,24 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    validLottoNumber(numbers);
   }
 
   // TODO: 추가 기능 구현
+  getLottoResult({ winNumbers, bonusNumber }) {
+    let numberOfCorrect = 0;
+    let isBonusCorrect = false;
+    this.#numbers.forEach((lottoNumber) => {
+      winNumbers.forEach((winNumber) => {
+        if (winNumber === lottoNumber) numberOfCorrect += 1;
+      });
+      if (lottoNumber === bonusNumber) isBonusCorrect = true;
+    });
+
+    if (numberOfCorrect === 6) return 1;
+    if (numberOfCorrect === 5 && isBonusCorrect) return 2;
+    return 8 - numberOfCorrect;
+  }
 }
 
 export default Lotto;
