4644836e8953e4d24863c9a217a65c873c5729bc
nijuy <youjin6325@naver.com>
2023-11-05 12:06:30 +0900

    test(Lotto): 범위 밖 숫자를 넣었을 때 테스트 추가
    - 1~45 사이가 아닌 정수, 1~45 사이 소수를 넣었을 때 예외를 잘 발생시키는지 확인하기 위함

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,28 @@
-import Lotto from "../src/Lotto.js";
+import ERROR_MESSAGE from '../src/ErrorMessage.js';
+import Lotto from '../src/Lotto.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGE.invalidListSize);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGE.duplicateNumber);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호에 범위 밖 숫자가 있으면 예외가 발생한다. (정수)', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 450]);
+    }).toThrow(ERROR_MESSAGE.outOfRange);
+  });
+
+  test('로또 번호에 범위 밖 숫자가 있으면 예외가 발생한다. (소수)', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 1.234]);
+    }).toThrow(ERROR_MESSAGE.outOfRange);
+  });
 });
