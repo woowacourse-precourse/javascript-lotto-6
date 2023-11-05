2405dc8fa97f58ea086da014529a84c95c37b15a
kimjinwoo <rlawlsdn177@naver.com>
2023-11-05 19:36:42 +0900

    feat(field, constructor): 필드 및 생성자 구현
    - (lottos): 전체 로또 객체 저장
    - (results): 당첨 금액 별 당첨 로또 갯수 저장
    - (purchaseAmount): 구매 로또 갯수 저장

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import {
+    INIT_ZERO,
+} from './utils/Define';
 class App {
+    lottos;
+    results;
+    purchaseAmount;
+
+    constructor() {
+        this.lottos = [];
+        this.results = { '3': INIT_ZERO, '4': INIT_ZERO, '5': INIT_ZERO, '5+': INIT_ZERO, '6': INIT_ZERO };
+        this.purchaseAmount = INIT_ZERO;
+    }
   async play() {}
 }
 
