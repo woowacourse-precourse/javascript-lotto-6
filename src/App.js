ea7e93ec492c73086a109b6c07ae6e0252e26c75
hamo-o <yheel369@gmail.com>
2023-11-05 14:26:05 +0900

    feat: 당첨 내역 도출 및 출력

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 import Input from './Input.js';
+import Lotto from './Lotto.js';
+import PRICE from './constants/price.js';
 
 class App {
   #count;
 
   #lottos;
 
+  #results;
+
   constructor() {
     this.#count = 0;
     this.#lottos = [];
+    this.#results = {
+      [PRICE.three]: 0,
+      [PRICE.four]: 0,
+      [PRICE.five]: 0,
+      [PRICE.fivePlus]: 0,
+      [PRICE.six]: 0,
+    };
   }
 
   countLotto(price) {
@@ -27,6 +38,25 @@ class App {
     return lotto;
   }
 
+  printResults() {
+    Console.print('당첨 통계\n---');
+    Console.print(
+      `3개 일치 (${PRICE.three}원) - ${this.#results[PRICE.three]}개`
+    );
+    Console.print(
+      `4개 일치 (${PRICE.four}원) - ${this.#results[PRICE.four]}개`
+    );
+    Console.print(
+      `5개 일치 (${PRICE.five}원) - ${this.#results[PRICE.five]}개`
+    );
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (${PRICE.fivePlus}원) - ${
+        this.#results[PRICE.fivePlus]
+      }개`
+    );
+    Console.print(`6개 일치 (${PRICE.six}원) - ${this.#results[PRICE.six]}개`);
+  }
+
   async play() {
     const price = await Input.getLottoPrice();
     Console.print('');
@@ -41,11 +71,21 @@ class App {
     this.#lottos.forEach((lotto) => Console.print(lotto));
     Console.print('');
 
-    const numbers = await Input.getLottoNumber();
+    const answerNumbers = await Input.getLottoNumber();
     Console.print('');
 
     const bonusNumber = await Input.getLottoBonusNumber();
     Console.print('');
+
+    this.#lottos.forEach((lotto) => {
+      const result = new Lotto(lotto);
+      const key = result.compareLotto(answerNumbers, bonusNumber);
+      if (key) {
+        this.#results[key] += 1;
+      }
+    });
+
+    this.printResults();
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import PRICE from './constants/price.js';
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,29 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  compareLotto(answer, bonus) {
+    const correct = this.#numbers.filter((number) => answer.includes(number));
+
+    const number = correct.length;
+    if (number === 6) {
+      return PRICE.six;
+    } else if (number === 5 && this.#numbers.includes(bonus)) {
+      return PRICE.fivePlus;
+    } else if (number === 5) {
+      return PRICE.five;
+    } else if (number === 4) {
+      return PRICE.four;
+    } else if (number === 3) {
+      return PRICE.three;
+    } else {
+      return 0;
+    }
+  }
 }
 
 export default Lotto;
