f32b00fd3b6286222385a5f70e1499476c93c9f7
chiman <rlaclghks123@naver.com>
2023-11-05 14:24:20 +0900

    feat: 유효성 검사 : 보너스 번호와 당첨 번호가 중복될 경우 예외처리

--- src/validator/bonusNumberValidator.js
+++ src/validator/bonusNumberValidator.js
@@ -1,9 +1,16 @@
 import commonValidator from './commonValidator.js';
 
 const bonusNumberValidator = {
-  checkBonusNumber({ bonusNumber, winnigNumbers }) {
+  checkDuplicate(bonusNumber, winningNumbers) {
+    const splitedNumbers = winningNumbers.split(',');
+    const mixNumbers = [...splitedNumbers, bonusNumber];
+    commonValidator.checkDuplicate(mixNumbers);
+  },
+
+  checkBonusNumber({ bonusNumber, winningNumbers }) {
     commonValidator.checkNumberType(bonusNumber);
     commonValidator.checkLottoNumberRange(bonusNumber);
+    bonusNumberValidator.checkDuplicate(bonusNumber, winningNumbers);
   },
 };
 
