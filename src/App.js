809caa76f7ffaf46a8879d49350e4e938df22f63
Seo San <dannysir@naver.com>
2023-11-05 23:00:03 +0900

    feat : 메인 함수 예외처리 추가 test 통과를 위해 main로직에 try...catch문을 이용하여 예외처리를 진행한 후 콘솔에 에러 문구 출력

--- src/App.js
+++ src/App.js
@@ -10,14 +10,19 @@ import OUT_RESULT from "./OutResult.js";
 
 class App {
   async play() {
-    const MONEY = (await INPUT_MONEY())/1000;
-    const LOTTO_ARR = MY_LOTTO(MONEY);
-    OUT_MY_LOTTO_ARR(MONEY,LOTTO_ARR);
-    const ANSWER = new Lotto(await INPUT_ANSWER());
-    const BONUS_NUM = await INPUT_BONUS();
-    const MY_ANSWER = new Lottory(LOTTO_ARR, ANSWER.getNumbers(), BONUS_NUM);
-    OUT_RESULT(MY_ANSWER.getAnswer());
-    MissionUtils.Console.print(MY_ANSWER.getEarning());
+    try{
+      const MONEY = (await INPUT_MONEY())/1000;
+      const LOTTO_ARR = MY_LOTTO(MONEY);
+      OUT_MY_LOTTO_ARR(MONEY,LOTTO_ARR);
+      const ANSWER = new Lotto(await INPUT_ANSWER());
+      const BONUS_NUM = await INPUT_BONUS();
+      const MY_ANSWER = new Lottory(LOTTO_ARR, ANSWER.getNumbers(), BONUS_NUM);
+      OUT_RESULT(MY_ANSWER.getAnswer(), MY_ANSWER.getEarning());
+    }catch (e) {
+      MissionUtils.Console.print("[ERROR]");
+    }
+
+
   }
 }
 
