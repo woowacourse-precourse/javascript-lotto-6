dfef891ad217c5324608510b75670566d27b2545
at8in <at8in@naver.com>
2023-11-06 07:01:08 +0900

    feat: 가격 검증 로직 추가 및 예외 처리

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,9 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+import LottoMaker from '../utils/LottoMaker.js';
 import InputView from '../views/InputView.js';
 
 class GameController {
+  #lottoMaker;
+
+  constructor() {
+    this.#lottoMaker = new LottoMaker();
+  }
+
   async startGame() {
-    const price = await InputView.inputPrices();
-    console.log(price);
+    const publishCount = await this.getLottoPublishCount();
+    console.log(publishCount);
+  }
+
+  async getLottoPublishCount() {
+    const price = await InputView.inputPrice();
+    const count = this.#lottoMaker.calcLottoPublishCount(price);
+    try {
+      this.#lottoMaker.validatePublishCount(count);
+      return count;
+    } catch (e) {
+      Console.print(e.toString());
+      return this.getLottoPublishCount();
+    }
   }
 }
 
