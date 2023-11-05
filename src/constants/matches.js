68547bf117b1da05ed0a72a153e49bb18c634a6f
pjc0179 <pjc0179@gmail.com>
2023-11-06 02:20:01 +0900

    feat(constants): 로또의 당첨금액과 메세지를 저장하는 객체 matches.js 생성

---
+++ src/constants/matches.js
@@ -0,0 +1,24 @@
+const MATCHES = Object.freeze({
+  threeMatches: {
+    reward: 5000,
+    message: '3개 일치 (5,000원)',
+  },
+  fourMatches: {
+    reward: 50000,
+    message: '4개 일치 (50,000원)',
+  },
+  fiveMatches: {
+    reward: 1500000,
+    message: '5개 일치 (1,500,000원)',
+  },
+  fiveAndBonusMatches: {
+    reward: 30000000,
+    message: '5개 일치, 보너스 볼 일치 (30,000,000원)',
+  },
+  sixMatches: {
+    reward: 2000000000,
+    message: '6개 일치 (2,000,000,000원)',
+  },
+});
+
+export default MATCHES;
