77e05321241271ad9d2b30a36482bee5fb3f4b2a
thumbthing <ilscc0902@gmail.com>
2023-11-06 06:58:49 +0900

    feat: 당첨번호 UI 담당 기능 분리

--- src/service/getLottoNumber.js
+++ src/service/getLottoNumber.js
@@ -1,13 +1,11 @@
 import Lotto from '../Lotto.js';
-import receiveWinningLottoUI from '../util/UI/gameStart/receiveWinningLottoUI.js';
 import ascendingSortList from '../util/parse/ascendingSortList.js';
 import parseToNumberArray from '../util/parse/parseToNumberArray.js';
 import splitInput from '../util/parse/splitInput.js';
 
-export default async function getLottoNumber() {
+export default async function getLottoNumber(winningNumber) {
   try {
-    const input = await receiveWinningLottoUI();
-    const isolatedInput = splitInput(input);
+    const isolatedInput = splitInput(winningNumber);
     const parsedLotto = parseToNumberArray(isolatedInput);
     const winningNumbers = ascendingSortList(parsedLotto);
 
