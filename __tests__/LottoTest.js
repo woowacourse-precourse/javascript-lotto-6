54226b9e5883498203101822223f45e598432a5e
Yoos99 <yoosh2520@naver.com>
2023-11-06 04:23:19 +0900

    test: 구매 금액 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 import Lotto from "../src/Lotto.js";
+import LottoValidator from "../src/Validator.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -22,5 +23,16 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 46]);
     }).toThrow("[ERROR]");
+  });
+});
+
+describe("구매 금액 테스트", () => {
+  test("구매 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.", () => {
+    const input = [2500];
+    input.forEach((input) => {
+      expect(() => LottoValidator.validatePurchaseAmount(input)).toThrow(
+        "[ERROR]"
+      );
+    });
   });
 });
