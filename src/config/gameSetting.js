ecaee3b93d4a4a74c9a67afaedf5b9a0569ffb2d
0jenn0 <jenn0.6n@gmail.com>
2023-11-06 05:38:59 +0900

    Feat: LOTTO_SETTINGS에mathchNum도 추가
    - 각 등수마다 일치갯수 NUMBERS_PER_TICKET로 나타낼려다가 가독성이 떨어지고 이렇게하는게 로또 당첨갯수 구하는 함수 구현하는데 더 편할것같아서 수정함

--- src/config/gameSetting.js
+++ src/config/gameSetting.js
@@ -18,12 +18,12 @@ const LOTTO_SETTINGS = deepFreeze({
   NUMBER_RANGE: { MIN: 1, MAX: 45 },
   NUMBERS_PER_TICKET: 6,
   TICKET_PRICE: 1000,
-  PRIZES: {
-    FIRST: 2000000000,
-    SECOND: 30000000,
-    THIRD: 1500000,
-    FOURTH: 50000,
-    FIFTH: 5000,
+  WINNINGS: {
+    FIRST_PRIZE: { prize: 2000000000, matchNum: 6 },
+    SECOND_PRIZE: { prize: 30000000, matchNum: 5 },
+    THIRD_PRIZE: { prize: 1500000, matchNum: 5 },
+    FOURTH_PRIZE: { prize: 50000, matchNum: 4 },
+    FIFTH_PRIZE: { prize: 5000, matchNum: 3 },
   },
 });
 
