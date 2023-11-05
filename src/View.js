0564dbfaf7904b687c41d8491eedf92b9c34e518
jw <always1997@kakao.com>
2023-11-05 15:18:28 +0900

    feat: 당첨번호 및 보너스 번호 입력 기능

--- src/View.js
+++ src/View.js
@@ -5,6 +5,14 @@ class View {
     return await this.readLine("구입금액을 입력해 주세요.\n");
   }
 
+  async getLottoWinningNumbers() {
+    return await this.readLine("당첨 번호를 입력해 주세요.\n");
+  }
+
+  async getLottoBonusNumber() {
+    return await this.readLine("보너스 번호를 입력해 주세요.\n");
+  }
+
   async readLine(query) {
     return await Console.readLineAsync(query);
   }
