280323866501380262c6ea72b73a33ed55f60fdc
lurgi <wahoo9040@naver.com>
2023-11-05 11:27:24 +0900

    feat(App): 금액 만큼 로또 발행

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto";
 
 class App {
   async play() {}
 
   static getRandomSixNumbers() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  static getLottoWithMoney(money) {
+    const NUMBER_OF_LOTTO = Math.floor(money / 1000);
+    return Array.from(
+      { length: NUMBER_OF_LOTTO },
+      () => new Lotto(this.getRandomSixNumbers())
+    );
   }
 }
 
