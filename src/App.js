f180158de6bd027bc12744d11e32a7d6f6cb420b
yhyem <yoonhyemin0120@gmail.com>
2023-11-05 16:51:51 +0900

    feat: 로또 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async start() {
+    const price = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    return Math.floor(price / 1000);
+  }
+
+  async play() {
+    const count = await this.start();
+    Console.print(count + "개를 구매했습니다.");
+  }
 }
 
 export default App;
