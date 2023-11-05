ff3680d306d142af4f218f3fa4bd41de8d30e024
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-11-05 23:41:50 +0800

    style: deveulope function

--- src/Modules/Calculations.js
+++ src/Modules/Calculations.js
@@ -1,20 +1,20 @@
 import { CALCULATE, REWARD_ARRAY } from '../constants/values';
 import { integrateWinLottery } from './Result';
 
-function totalWinningAmount() { //총 당첨된 금액 계산
-    let winningDetails = integrateWinLottery(winningNumbers, bonusNumber);
-    let total = 0;
-    winningDetails.forEach((amount, idx) => {
-      total += amount * REWARD_ARRAY[idx];
-    })
-    return total;
+function totalWinningAmount(winningNumbers, bonusNumber) { //총 당첨된 금액 계산
+  let winningDetails = integrateWinLottery(winningNumbers, bonusNumber);
+  let total = 0;
+  winningDetails.forEach((amount, idx) => {
+    total += amount * REWARD_ARRAY[idx];
+  })
+  return total;
 }
   
     
-function calculateReturns(purchaseAmount) { //수익률 계산
-    let total = totalWinningAmount(winningDetails, reward);
-    let returns = (total / purchaseAmount * CALCULATE.PERCENTAGE).toFixed(CALCULATE.DECIMAL);
-    return returns;
+function calculateReturns(purchaseAmount, winningNumbers, bonusNumber) { //수익률 계산
+  let total = totalWinningAmount(winningNumbers, bonusNumber);
+  let returns = (total / purchaseAmount * CALCULATE.PERCENTAGE).toFixed(CALCULATE.DECIMAL);
+  return returns;
 }
 
 export default calculateReturns;
--- src/Modules/Result.js
+++ src/Modules/Result.js
@@ -1,14 +1,14 @@
 import { WIN, WIN_INDEX } from '../constants/values';
 import { issuLotto } from './LotteryIssuance';
 
-function integrateWinLottery(winningNumbers, bonusNumber) { //당첨 결과 내역 종합
+function integrateWinLottery(winningNumbers, bonusNumber, quantity) { //당첨 결과 내역 종합
     let lotteries = issuLotto(quantity);
     let winningDetails = Array(WIN.ARRAY).fill(0); //1등 ~ 5등 
     lotteries.forEach((lotto) => {
         let result = compareNumbers(lotto, winningNumbers, bonusNumber);
         result < WIN.ARRAY ? winningDetails[result] += 1 : null;
     })
-    return winningDetails;
+    return winningDetails.reverse();
 }
 
 function compareNumbers(lotto, winningNumbers, bonusNumber) { //발행된 로또 번호 이차원 배열과 당첨 번호 배열 비교하여 등수 결정
