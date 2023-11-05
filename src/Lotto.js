c08c915a6b1e15d9ad11bc6f74fab7068140d0f7
stupidJoon <junsang.yu3@gmail.com>
2023-11-05 20:04:43 +0900

    fix: add Lotto number duplicate check

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,10 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    if (new Set(numbers) !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
+    }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
