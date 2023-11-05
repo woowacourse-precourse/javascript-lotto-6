cc7375d79adfa16c71fa1f1af8e538ab06bf6333
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-11-05 20:46:35 +0900

    feat: 로또 번호 리스트 오름차순 정렬 추가

--- src/utils/generateLottoNumbers.js
+++ src/utils/generateLottoNumbers.js
@@ -2,13 +2,24 @@ import { Random } from "@woowacourse/mission-utils";
 import { LOTTO_NUMBER } from "../constants/constants";
 import Lotto from "../Lotto";
 
-function generateLottoNumbers() {
+function generateRandomLottoNumbers() {
   const randomNumbers = Random.pickUniqueNumbersInRange(
     LOTTO_NUMBER.START_RANGE,
     LOTTO_NUMBER.END_RANGE,
     LOTTO_NUMBER.NUMBERS_TO_PICK
   );
-  return new Lotto(randomNumbers);
+  return randomNumbers;
+}
+
+function sortedLottoNumbers(randomNumbers) {
+  const sortedNumbers = randomNumbers.slice().sort((a, b) => a - b);
+  return new Lotto(sortedNumbers);
+}
+
+function generateLottoNumbers() {
+  const randomNumbers = generateRandomLottoNumbers();
+  const sortedNumbers = sortedLottoNumbers(randomNumbers);
+  return sortedNumbers;
 }
 
 export default generateLottoNumbers;
