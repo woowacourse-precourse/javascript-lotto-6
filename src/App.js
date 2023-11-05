9bdda88e2d7f403f6c67167d7898adb5650f4862
kinajin <kinajin22@gmail.com>
2023-11-05 20:19:59 +0900

    feat:당첨 계산 외 전반적인 기능

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { getPayment, purchaseLotto } from './Human.js';
+import Lotto from './Lotto.js';
 
 class App {
-  async play() {}
+  async play() {
+    let lottoQuantity = await getPayment();
+    let purchasedLottos = await purchaseLotto(lottoQuantity);
+    let lottoNumber = await MissionUtils.Console.readLineAsync(
+      '\n당첨번호를 입력해주세요.\n',
+    );
+    let bonusNumber = await MissionUtils.Console.readLineAsync(
+      '\n보너스 번호를 입력해 주세요.\n',
+    );
+    let theLotto = new Lotto(lottoNumber, bonusNumber);
+  }
 }
 
 export default App;
+
+let test = new App();
+test.play();
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,8 +3,10 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  // 질문사항
+  constructor(numbers, bonusNumber) {
     this.#validate(numbers);
+    this.#bonusNumberValidate(bonusNumber);
     this.#numbers = numbers;
   }
 
@@ -12,18 +14,32 @@ class Lotto {
     // make input numer array
     const numberStringsArray = numbers.split(',');
     const numbersArray = numberStringsArray.map((number) => Number(number));
+    const numbersSet = new Set(numbersArray);
 
+    if (numbersArray.length !== numbersSet.size) {
+      throw new Error('[ERROR] 로또 번호에서 중복된 숫자를 입력하지 마세요.');
+    }
     if (numbersArray.length !== 6) {
+      console.log('lengthproblem');
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
     numbersArray.forEach((number) => {
       if (number < 0 || number > 45 || Number.isInteger(number) === false) {
-        throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+        console.log(number);
+        console.log('problem');
+        throw new Error('[ERROR] 로또 번호는 0 이상 정수여야합니다.');
       }
     });
   }
-}
 
-let testclass = new Lotto('1,2,3,4.5,5,6');
+  #bonusNumberValidate(bonusNumber) {
+    if (bonusNumber.length !== 1) {
+      throw new Error('[ERROR] 보너스 번호는 한자리만 입력하세요.');
+    }
+    if (bonusNumber < 0 || Number.isInteger(Number(bonusNumber)) === false) {
+      throw new Error('[ERROR] 로또 번호는 0 이상 정수여야합니다.');
+    }
+  }
+}
 
 export default Lotto;
