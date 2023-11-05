6f1caafc470ac2404936baf305a26015665a2339
ohgus <anakin1108@naver.com>
2023-11-05 17:38:56 +0900

    feat: 사용자의 구매 금액 만큼 로또를 생성하는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,6 @@
+import { Random } from "@woowacourse/mission-utils";
+import { PICK_NUMBERS, ONE_LOTTO_PRICE } from "./constants/lottoNumbers.js";
+
 class Lotto {
   #numbers;
 
@@ -13,6 +16,24 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  makeLotto(money) {
+    const howManyLotto = money / ONE_LOTTO_PRICE;
+    const lottos = [];
+
+    for (let i = 0; i < howManyLotto; i++) {
+      lottos.push(
+        Random.pickUniqueNumbersInRange(
+          PICK_NUMBERS.START_RANGE,
+          PICK_NUMBERS.END_RANGE,
+          PICK_NUMBERS.HOW_MANY
+        )
+      );
+    }
+  }
 }
+
+// const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+// lotto.makeLotto(3000);
 
 export default Lotto;
---
+++ src/constants/lottoNumbers.js
@@ -0,0 +1,7 @@
+export const PICK_NUMBERS = {
+  START_RANGE: 1,
+  END_RANGE: 45,
+  HOW_MANY: 6,
+};
+
+export const ONE_LOTTO_PRICE = 1000;
