fa1b1c467f486d2c764a2c5291314f3e4328563d
hongst222 <tmdxordusdo@gmail.com>
2023-11-05 14:10:10 +0900

    feat: Model들을 묶어주는 파일 생성
    index.js

---
+++ src/Model/index.js
@@ -0,0 +1,13 @@
+import TicketManager from './TicketManager.js';
+import PayMoney from './PayMoney.js';
+import Lotto from './Lotto.js';
+import CalcMoney from './CalcMoney.js';
+import BonusNumber from './BonusNumber.js';
+
+export {
+    TicketManager,
+    PayMoney,
+    Lotto,
+    CalcMoney,
+    BonusNumber
+}
