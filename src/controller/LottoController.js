d78720e6193039594c9f7738288623a5bb45cf54
BangDori <qudwns8865@naver.com>
2023-11-05 21:07:11 +0900

    feat: 로또 당첨 통계 객체에 속성 추가

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -19,11 +19,11 @@ class LottoController {
 
   initLottoStatistics() {
     return {
-      match3: { count: 0, prize: 5000 },
-      match4: { count: 0, prize: 50000 },
-      match5: { count: 0, prize: 1500000 },
-      match5Bonus: { count: 0, prize: 30000000 },
-      match6: { count: 0, prize: 2000000000 },
+      match3: { match: 3, bonus: false, count: 0, prize: 5000 },
+      match4: { match: 4, bonus: false, count: 0, prize: 50000 },
+      match5: { match: 5, bonus: false, count: 0, prize: 1500000 },
+      match5Bonus: { match: 5, bonus: true, count: 0, prize: 30000000 },
+      match6: { match: 6, bonus: false, count: 0, prize: 2000000000 },
     };
   }
 
