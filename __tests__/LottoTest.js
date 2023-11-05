0d375cc559200d6a5a47ded2b26d7e9f38f14ab0
lurgi <wahoo9040@naver.com>
2023-11-05 09:35:27 +0900

    test(LottoTest): 1등~5등 확인 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,24 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  const TEST_CASES = [
+    [[8, 10, 11, 17, 32, 35], 14, 1],
+    [[8, 10, 11, 17, 32, 33], 35, 2],
+    [[8, 10, 11, 17, 32, 36], 33, 3],
+    [[9, 10, 11, 17, 32, 44], 35, 4],
+    [[9, 10, 11, 19, 32, 45], 35, 5],
+    [[1, 4, 6, 18, 33, 41], 11, 8],
+  ];
+
+  test.each(TEST_CASES)(
+    "등수 반환 테스트",
+    (winNumbers, bonusNumber, expectResult) => {
+      const LOTTO = new Lotto([8, 10, 11, 17, 32, 35]);
+      const WIN_RESULT = LOTTO.getLottoResult({
+        winNumbers,
+        bonusNumber,
+      });
+      expect(WIN_RESULT).toEqual(expectResult);
+    }
+  );
 });
