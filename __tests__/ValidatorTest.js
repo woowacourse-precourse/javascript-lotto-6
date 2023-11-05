e9139f61b70b39487b3f93a8add6305762bc23dc
heonq <gkagjsrb@naver.com>
2023-11-05 14:39:33 +0900

    test(Validator): 구입 금액에 대한 유효성 검사 테스트 작성 - 유효한 구입 금액에 대한 테스트 - [1000,2000,3000,4000,10000] 입력 - 유효하지 않은 구입 금액에 대한 테스트 수정: 입력할 구입 금액 배열의 이름을 invalidPurchaseAmounts로 수정

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -2,9 +2,15 @@ import Validator from '../utils/Validator.js';
 
 describe('유효성 검사 기능 테스트', () => {
   test('유효하지 않은 구입 금액에 대한 테스트', () => {
-    const purchaseAmounts = [900, 1100, -1000, 1000.5, '천원'];
-    purchaseAmounts.forEach((purchaseAmount) =>
-      expect(Validator.validatePurchaseAmount(purchaseAmount)).toEqual(false),
+    const invalidPurchaseAmounts = [900, 1100, -1000, 1000.5, '천원'];
+    invalidPurchaseAmounts.forEach((invalidPurchaseAmount) =>
+      expect(Validator.validatePurchaseAmount(invalidPurchaseAmount)).toEqual(false),
+    );
+  });
+  test('유효한 구입 금액에 대한 테스트', () => {
+    const validPurchaseAmounts = [1000, 2000, 3000, 4000, 10000];
+    validPurchaseAmounts.forEach((validPurchaseAmount) =>
+      expect(Validator.validatePurchaseAmount(validPurchaseAmount)).toEqual(true),
     );
   });
 });
