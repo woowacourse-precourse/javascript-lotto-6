bc753fb19798b63fff051664161195c8dd27d831
nijuy <youjin6325@naver.com>
2023-11-05 10:18:16 +0900

    feat(App): 로또 구입 금액 입력 받기, 예외 처리
    - 사용자로부터 로또 구입 금액을 입력 받는다. / 입력 금액이 조건에 어긋나면 예외를 발생시킨다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console } from '@woowacourse/mission-utils';
+import ERROR_MESSAGE from './ErrorMessage';
+
 class App {
-  async play() {}
+  static validatePayment(input) {
+    const LOTTO_PRICE = 1000;
+    const payment = Number(input);
+
+    if (Number.isNaN(payment)) {
+      throw new Error(ERROR_MESSAGE.nonNumeric);
+    }
+    if (payment <= 0 || !Number.isInteger(payment / LOTTO_PRICE)) {
+      throw new Error(ERROR_MESSAGE.invalidAmount);
+    }
+  }
+
+  async play() {
+    const payment = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    App.validatePayment(payment);
+  }
 }
 
 export default App;
