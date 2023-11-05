fb3b38887f4b8cda6cda654787c3685da05f60a5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-05 13:47:14 +0900

    ✨feat($App): 발행한 로또 수량 출력

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import { GUIDE_MESSAGE } from './constants/constants'
+import { GUIDE_MESSAGE, WINNING_AMOUNTS } from './constants/constants'
 import LottoTickets from './LottoTickets';
+import BonusNumber from './BonusNumber'
 import Input from './view/Input';
 import Output from './view/Output';
+import Lotto from './Lotto';
 
 // @note - 금액 저장, 당첨 번호 저장, 보너스 번호 저장, 수익률 출력
 
 class App {
   #money
+  #lottos
+  #bonusNumber
+  #winningNumbers
 
   constructor() {
     this.#money = 0;
+    this.#lottos = [];
+    this.#bonusNumber = 0;
+    this.#winningNumbers = [];
     this.input = new Input();
     this.output = new Output();
   }
 
   async play() {
     await this.requestPurchaseAmounts();
+    this.requestLottoTickets()
+    await this.requestWinningNumbers();
+    await this.requestBonusNumber();
+    this.requestResult();
   }
 
   async requestPurchaseAmounts() {
@@ -25,6 +37,10 @@ class App {
     this.#money = this.lottos.returnMoney()
     
     this.output.printMoney(this.#money);
+  }
+
+  requestLottoTickets() {
+    this.output.printTickets(this.#money / 1000)
   }
 }
 
