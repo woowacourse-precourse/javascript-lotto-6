c56c13fc0fe8971dd38c8c1bd8cec30fd761972e
Boeun <boeun0904@naver.com>
2023-11-06 02:40:54 +0900

    feat(User): 당첨 번호 입력

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -3,36 +3,60 @@ import UserInput from '../src/UserInput.js';
 describe('유저 입력 테스트', () => {
   describe('로또 구입 금액 입력 테스트', () => {
     test('빈 값을 입력할 경우 예외를 발생시킨다.', () => {
-      expect(() => UserInput.checkPurchaseAmountValidation('')).toThrow(
-        '[ERROR]',
-      );
+      expect(() => UserInput.checkPurchaseAmountValidation('')).toThrow('[ERROR]');
     });
 
-    test.each(['abc', 'ㄱㄴㄷ'])(
-      '숫자가 아닌 값을 입력할 경우 예외를 발생시킨다.',
-      (input) => {
-        expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow(
-          '[ERROR]',
-        );
-      },
-    );
+    test.each(['abc', 'ㄱㄴㄷ'])('숫자가 아닌 값을 입력할 경우 예외를 발생시킨다.', (input) => {
+      expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow('[ERROR]');
+    });
 
-    test.each(['-1000', '0'])(
-      '0 이하의 숫자를 입력할 경우 예외를 발생시킨다.',
-      (input) => {
-        expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow(
-          '[ERROR]',
-        );
-      },
-    );
+    test.each(['-1000', '0'])('0 이하의 숫자를 입력할 경우 예외를 발생시킨다.', (input) => {
+      expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow('[ERROR]');
+    });
 
     test.each(['1', '100', '1234'])(
       '1000으로 나누어떨어지지 않는 숫자를 입력할 경우 예외를 발생시킨다.',
       (input) => {
-        expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow(
-          '[ERROR]',
-        );
+        expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow('[ERROR]');
       },
     );
+  });
+
+  describe('당첨 번호 입력 테스트', () => {
+    test('빈 값을 입력할 경우 예외를 발생시킨다.', () => {
+      expect(() => UserInput.checkWinningNumbersValidation('')).toThrow('[ERROR]');
+    });
+
+    test.each(['abc,1,2,3,4,5', '1,2,3,4,5,ㄱㄴㄷ'])(
+      '숫자가 아닌 값을 입력할 경우 예외를 발생시킨다.',
+      (input) => {
+        expect(() => UserInput.checkWinningNumbersValidation(input)).toThrow('[ERROR]');
+      },
+    );
+
+    test.each(['-1000,1,2,3,4,5', '1,2,3,4,5,0'])(
+      '0 이하의 숫자를 입력할 경우 예외를 발생시킨다.',
+      (input) => {
+        expect(() => UserInput.checkWinningNumbersValidation(input)).toThrow('[ERROR]');
+      },
+    );
+
+    test.each(['1,2,3,4', '1,2,3,4,5,6,7'])(
+      '입력한 숫자가 6개가 아닐 경우 예외를 발생시킨다.',
+      (input) => {
+        expect(() => UserInput.checkWinningNumbersValidation(input)).toThrow('[ERROR]');
+      },
+    );
+
+    test.each(['1,2,3,4,4,4', '1,1,1,1,1,1'])(
+      '같은 숫자를 여러 번 입력할 경우 예외를 발생시킨다.',
+      (input) => {
+        expect(() => UserInput.checkWinningNumbersValidation(input)).toThrow('[ERROR]');
+      },
+    );
+
+    test('값을 올바르게 입력할 경우 예외를 발생하지 않는다.', () => {
+      expect(() => UserInput.checkWinningNumbersValidation('1,2,3,4,5,6')).not.toThrow('[ERROR]');
+    });
   });
 });
--- src/UserInput.js
+++ src/UserInput.js
@@ -1,5 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
-import { MESSAGE, ERROR } from './constants.js';
+import { MESSAGE, ERROR, LOTTERY } from './constants.js';
 import { InputError } from './utils/error.js';
 
 export default class UserInput {
@@ -15,15 +15,41 @@ export default class UserInput {
     }
   }
 
-  static printError(error) {
-    Console.print(error);
-  }
-
   static checkPurchaseAmountValidation(input) {
     if (input.length === 0) throw new InputError(ERROR.BLANK_INPUT);
     const num = Number(input);
     if (Number.isNaN(num)) throw new InputError(ERROR.NOT_A_NUMBER);
     if (num <= 0) throw new InputError(ERROR.NOT_A_NATURAL_NUMBER);
-    if (num % 1000 !== 0) throw new InputError(ERROR.NOT_DIVIDED_BY_THOUSAND);
+    if (num % LOTTERY.PRICE !== 0) throw new InputError(ERROR.NOT_DIVIDED_BY_THOUSAND);
+  }
+
+  static async getWinningNumbers() {
+    while (true) {
+      const input = await Console.readLineAsync(MESSAGE.INPUT_WINNING_NUMBERS);
+      try {
+        this.checkWinningNumbersValidation(input);
+        return input.split(',').map(Number);
+      } catch (exception) {
+        this.printError(exception.message);
+      }
+    }
+  }
+
+  static checkWinningNumbersValidation(input) {
+    if (input.length === 0) throw new InputError(ERROR.BLANK_INPUT);
+    const arr = input.split(',').map(Number);
+    if (arr.some((num) => Number.isNaN(num))) throw new InputError(ERROR.NOT_A_NUMBER);
+    if (
+      !arr.every((num) => {
+        return num >= LOTTERY.MIN_NUM && num <= LOTTERY.MAX_NUM;
+      })
+    )
+      throw new InputError(ERROR.OUT_OF_RANGE(LOTTERY.MIN_NUM, LOTTERY.MAX_NUM));
+    if (arr.length !== LOTTERY.NUM_COUNT) throw new InputError(ERROR.NOT_SIX_NUMBERS);
+    if (new Set(arr).size !== LOTTERY.NUM_COUNT) throw new InputError(ERROR.NOT_SIX_NUMBERS);
+  }
+
+  static printError(error) {
+    Console.print(error);
   }
 }
