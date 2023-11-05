34f57cd458972ce5d0d6707f76848c248f64d5bb
JiSeong Kim <12180543@inha.edu>
2023-11-06 03:23:06 +0900

    test(util): add test code for Formatting
    - insertCommasByThousandUnits test
    - convertArrayToString test

---
+++ __tests__/implementedFunctionTest/utilTest/FormattingTest.js
@@ -0,0 +1,21 @@
+import Formatting from '../../../src/util/Formatting.js';
+
+describe('Formatting 테스트', () => {
+	test('insertCommasByThousandUnits 테스트', () => {
+		const input = ['0.0', '100.0', '1000.0', '100000.0'];
+		const expectedResult = ['0.0', '100.0', '1,000.0', '100,000.0'];
+
+		const result = input.map((number) => {
+			return Formatting.insertCommasByThousandUnits(number);
+		});
+
+		expect(result).toEqual(expectedResult);
+	});
+
+	test('convertArrayToString 테스트', () => {
+		const input = [1, 2, 3, 4, 5, 6];
+		const expectedResult = '[1, 2, 3, 4, 5, 6]';
+
+		expect(Formatting.convertArrayToString(input)).toEqual(expectedResult);
+	});
+});
