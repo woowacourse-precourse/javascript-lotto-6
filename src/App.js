de106736e50be11d730cca1dda3993bb7462eb11
sihyun10 <hyhy200170@gmail.com>
2023-11-05 17:59:37 +0900

    feat: 로또 번호 무작위 생성, 오름차순 정렬 기능 구현 로또 번호를 1~45 숫자 범위의 중복되지않는 6개의 숫자를 뽑기 위해 Random.pickUniqueNumbersInRange를 사용해 뽑는 기능을 구현했고, 뽑은 로또 번호를 오름차순 정렬시키는 기능도 추가해두었습니다. 이로써 로또 번호가 무작위로 중복 없이 잘 생성되는지 확인해볼 수 있었습니다. 또한, 올바른 초기 로또 번호를 제공해야 예외를 발생시키지 않게 됩니다. 따라서 [1,2,3,4,5,6] 값으로 초기 로또 번호를 제공하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Lotto from './Lotto.js';
+
 class App {
-  async play() {}
+  async play() {
+    const initialLottoNumbers = [1, 2, 3, 4, 5, 6];
+
+    const lotto = new Lotto(initialLottoNumbers);
+    lotto.generateRandomNumbers();
+    console.log('무작위로 생성된 로또 번호:', lotto.getNumbers());
+  }
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,19 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  // 추가 기능 구현 : 로또 번호 무작위 생성
+  generateRandomNumbers() {
+    this.#numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    this.#numbers.sort((a, b) => a - b);
+  }
+
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
