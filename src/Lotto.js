bdfcdae705d1f7d154c85ccf3a779472bb142b9e
kinajin <kinajin22@gmail.com>
2023-11-05 18:43:24 +0900

    feat: 초기 6개 숫자 입력 제한

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,22 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
+    // make input numer array
+    const numberStringsArray = numbers.split(',');
+    const numbersArray = numberStringsArray.map((number) => Number(number));
 
+    if (numbersArray.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    numbersArray.forEach((number) => {
+      if (number < 0 || number > 45 || Number.isInteger(number) === false) {
+        throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      }
+    });
+  }
   // TODO: 추가 기능 구현
 }
+
+let testclass = new Lotto('1,2,3,4.5,5,6');
 
 export default Lotto;
