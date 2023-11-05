ab0db8f75793d2ec06b2d6632c0377f09dcda336
kimjinwoo <rlawlsdn177@naver.com>
2023-11-05 19:05:50 +0900

    feat($printAllMatchResults): 전체 로또 당첨 금액 별 결과 메세지 출력
    - ($printAllMatchResults): 결과 객체를 입력받아 각 금액 별 당첨 갯수를
    printMatchResult 메서드 호출하여 전체 로또 당첨 금액 별 결과 메세지 출력

--- src/io/Output.js
+++ src/io/Output.js
@@ -15,3 +15,11 @@ const printMatchResult = (matchCount, prize, count) => {
         : `${matchCount}${RESULT_MESSAGES.FIRST}${formattedPrize}${RESULT_MESSAGES.SECOND}${count}${RESULT_MESSAGES.COUNT}`;
     Console.print(message);
 };
+
+const printAllMatchResults = (results) => {
+    Object.keys(results).forEach(matchCount => {
+        const prize = PRIZE_MAP[matchCount] || ZERO;
+        const count = results[matchCount];
+        printMatchResult(matchCount, prize, count);
+    });
+};
