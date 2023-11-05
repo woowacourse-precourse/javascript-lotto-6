cb61623549ef57d17808b92810791d8273c5a427
yook-jongho <dbrwhdgh1203@naver.com>
2023-11-05 23:40:10 +0900

    로또번호 생성 기능 - 지정 범위 내 원하는 개수의 숫자 무작위 생성 및 중복 불가 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,18 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  createLotto() {
+    const lottoNumberList = [];
+    while (lottoNumberList.length < 6) {
+      pick = Random.pickUniqueNumbersInRange(1, 45);
+      if (!lottoNumberList.includes(pick)) lottoNumberList.push(pick);
+    }
+  }
 }
 
 export default Lotto;
