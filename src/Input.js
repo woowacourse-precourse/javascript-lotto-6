063f7ca4800555b2fd2d4678da13db1b13d8da3d
nijuy <youjin6325@naver.com>
2023-11-05 23:46:54 +0900

    fix(Input): 잘못된 값 입력 시 에러 출력 및 재입력 받기
    - 에러 던지고 끝내는 게 아니라 재입력 받아야함

--- src/Input.js
+++ src/Input.js
@@ -4,24 +4,46 @@ import Validate from './Validate';
 
 class Input {
   static async payment() {
-    const payment = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
-    Validate.validatePayment(payment);
-    return Number(payment);
+    while (true) {
+      try {
+        const payment = await Console.readLineAsync(
+          '구입금액을 입력해 주세요.\n',
+        );
+        Validate.validatePayment(payment);
+        return Number(payment);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 
   static async winning() {
-    const userSelected = await Console.readLineAsync(
-      '\n당첨 번호를 입력해 주세요.\n',
-    );
-    return new Lotto(userSelected.split(',').map((string) => Number(string)));
+    while (true) {
+      try {
+        const userSelected = await Console.readLineAsync(
+          '\n당첨 번호를 입력해 주세요.\n',
+        );
+        return new Lotto(
+          userSelected.split(',').map((string) => Number(string)),
+        );
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 
   static async bonusNumber(winning) {
-    const bonus = await Console.readLineAsync(
-      '\n보너스 번호를 입력해 주세요.\n',
-    );
-    Validate.validateBonusNumber(bonus, winning);
-    return Number(bonus);
+    while (true) {
+      try {
+        const bonus = await Console.readLineAsync(
+          '\n보너스 번호를 입력해 주세요.\n',
+        );
+        Validate.validateBonusNumber(bonus, winning);
+        return Number(bonus);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 }
 
