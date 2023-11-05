6e047b55bb61c7c5f48243ded37ca49deaa65c62
8leamnk <silkviewrk@gmail.com>
2023-11-05 21:56:22 +0900

    refactor(randomModel): 로또 발행 기능을 리팩토링함
    변경 내용:
    - 기존에 있던 Model/RandomModel을 models/randomModel 파일로 변경함
    - 클래스에서 직접 객체로 만드는 방식을 시도함

--- src/models/randomModel.js
+++ src/models/randomModel.js
@@ -1,28 +1,28 @@
 import { Random } from '@woowacourse/mission-utils';
 import VALUE from '../constants/value.js';
 
-class RandomModel {
-  static getOneOfRandom() {
+const randomModel = {
+  getOneOfRandom() {
     const { start, end, count } = VALUE.range;
     const oneOfRandom = Random.pickUniqueNumbersInRange(start, end, count);
 
     oneOfRandom.sort((a, b) => a - b);
 
     return [...oneOfRandom];
-  }
+  },
 
-  static getRandom(number) {
+  getRandom(number) {
     const random = [];
 
     for (let i = 1; i <= number; i += 1) {
-      const oneOfRandom = RandomModel.getOneOfRandom();
+      const oneOfRandom = this.getOneOfRandom();
       random.push(oneOfRandom);
     }
 
     return [...random];
-  }
+  },
 
-  static getStringOfRandom(random) {
+  getStringOfRandom(random) {
     const stringOfRandom = [];
 
     for (let i = 0; i < random.length; i += 1) {
@@ -30,7 +30,7 @@ class RandomModel {
     }
 
     return stringOfRandom;
-  }
-}
+  },
+};
 
-export default RandomModel;
+export default randomModel;
