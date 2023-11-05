851818b5f03b5e0dd14701caff414d55d782001f
LJH <049494@ewhain.net>
2023-11-05 18:59:05 +0900

    ✨ feat : 구입 금액 입력 안내 문구 출력하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { printMessage } from "./PrintMessages.js";
+import { PURCHASE_AMOUNT_INPUT_REQUEST } from "./constants.js";
+
 class App {
-  async play() {}
+  async play() {
+    printMessage(PURCHASE_AMOUNT_INPUT_REQUEST);
+  }
 }
+
+const app = new App();
+await app.play();
 
 export default App;
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const PURCHASE_AMOUNT_INPUT_REQUEST = "구입금액을 입력해 주세요.";
-const WINNING_NUMBERS_INPUT_REQUEST = "당첨 번호를 입력해 주세요.";
-const BONUS_NUMBER_INPUT_REQUEST = "보너스 번호를 입력해 주세요.";
-const WINNING_STATISTICS_PRINT_MESSAGE = "당첨 통계";
+export const PURCHASE_AMOUNT_INPUT_REQUEST = "구입금액을 입력해 주세요.";
+export const WINNING_NUMBERS_INPUT_REQUEST = "당첨 번호를 입력해 주세요.";
+export const BONUS_NUMBER_INPUT_REQUEST = "보너스 번호를 입력해 주세요.";
+export const WINNING_STATISTICS_PRINT_MESSAGE = "당첨 통계";
