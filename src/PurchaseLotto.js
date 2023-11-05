6fb675a2c761122ff7ec4a968bf49de281ef4d01
woo7i <or2447221@gmail.com>
2023-11-06 03:54:09 +0900

    fix(Purchase) : 값을 확인하기 위한 콘솔 값 지정

--- src/PurchaseLotto.js
+++ src/PurchaseLotto.js
@@ -1,27 +1,26 @@
 import { inputLottoNum, consoleError } from './utils.js';
-import Validation from './validation.js';
 import { LOTTO_NUMBERS, ERROR_MESSAGE, INPUT_MESSAGE } from './constants.js';
 
 class PurchaseLotto {
   constructor() {
     this.inputPrice = null;
+    this.lottoCount = 0;
   }
 
-  // 초기화 값에 비동기 값을 할당하기 위한 메서드
+  // 비동기로 처리해야 할 inputPrice 값을 다시 초기화 하기 위한 함수.
   async initialize() {
     this.inputPrice = await inputLottoNum();
+    this.lottoCount = this.inputPrice / LOTTO_NUMBERS.PAY_LOTTO_MONEY;
   }
 
   async alertPurchaseLotto() {
     if (this.inputPrice % LOTTO_NUMBERS.PAY_LOTTO_MONEY !== 0) {
       return consoleError(ERROR_MESSAGE.UNIT);
     }
-
-    return this.countingLotto();
+    this.countingLotto(this.lottoCount);
   }
 
-  async countingLotto() {
-    const lottoCount = this.inputPrice / LOTTO_NUMBERS.PAY_LOTTO_MONEY;
+  countingLotto(lottoCount) {
     return INPUT_MESSAGE.PURCHASED_MESSAGE(lottoCount);
   }
 }
@@ -29,9 +28,11 @@ class PurchaseLotto {
 const lotto = new PurchaseLotto();
 
 // 비동기 초기화 메서드 호출
-lotto.initialize().then(() => {
+lotto.initialize().then(async () => {
   // 초기화가 완료된 후에 추가 작업을 수행
-  lotto.alertPurchaseLotto().then((message) => {
-    console.log(message);
-  });
+  console.log(lotto.lottoCount); // 따로 값이 빠지는 지 확인 콘솔.
+  const message = await lotto.alertPurchaseLotto();
+  console.log(message); // 반환된 메세지 출력하기 위한 콘솔.
 });
+
+export default PurchaseLotto;
