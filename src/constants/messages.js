4101ba5d008274384faef13188e7fc225fe51c9d
Hyunji Seok <alleyji126@gmail.com>
2023-11-05 16:03:57 +0900

    feat(messages): add input and error messages

---
+++ src/constants/messages.js
@@ -0,0 +1,16 @@
+const INPUT = {
+  buy_amount: "구입 금액을 입력해 주세요. \n",
+  winning_numbers: "당첨 번호를 입력해 주세요. \n",
+  bonus_number: "보너스 번호를 입력해 주세요. \n",
+}
+
+const ERROR = {
+  price_unit: "[ERROR] 로또 구입 금액은 1,000원 단위로 입력이 가능합니다.",
+  not_a_number: "[ERROR] 로또 구입 금액은 숫자만 입력이 가능합니다.",
+};
+
+const OUTPUT = {
+
+};
+
+export { INPUT, ERROR, OUTPUT };
