6da4113e453032f2429b6de52ca0a8fdab037df2
손진영 <sm099@naver.com>
2023-11-05 11:39:41 +0900

    test(lottoGameConsole): 보상 정보 및 수익률 포맷팅에 대한 테스트 추가

--- __tests__/cli/LottoGameConsole.test.js
+++ __tests__/cli/LottoGameConsole.test.js
@@ -28,5 +28,38 @@ describe('lottoGameConsole 테스트', () => {
         expect(lottoGameConsole.output.messages.lottoNumbers(input)).toMatch(output);
       });
     });
+
+    describe('rewardInfo 메서드 테스트', () => {
+      test.each([
+        {
+          input: { '1st': 1, '2nd': 1, '3rd': 1, '4th': 1, '5th': 1 },
+          output:
+            '3개 일치 (5,000원) - 1개\n4개 일치 (50,000원) - 1개\n5개 일치 (1,500,000원) - 1개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n6개 일치 (2,000,000,000원) - 1개',
+        },
+        {
+          input: { '1st': 1, '3rd': 1 },
+          output:
+            '3개 일치 (5,000원) - 0개\n4개 일치 (50,000원) - 0개\n5개 일치 (1,500,000원) - 1개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n6개 일치 (2,000,000,000원) - 1개',
+        },
+        {
+          input: {},
+          output:
+            '3개 일치 (5,000원) - 0개\n4개 일치 (50,000원) - 0개\n5개 일치 (1,500,000원) - 0개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n6개 일치 (2,000,000,000원) - 0개',
+        },
+      ])('rewardInfo가 $input일 때 포맷팅 된 메시지는 $output이다.', ({ input, output }) => {
+        // given - when - then
+        expect(lottoGameConsole.output.messages.rewardInfo(input)).toMatch(output);
+      });
+    });
+
+    describe('rateOfReturn 메서드 테스트', () => {
+      test.each([{ input: '1000', output: '총 수익률은 1000%입니다.' }])(
+        '수익률이 $input일 때, 포맷팅 된 메시지는 $output이다.',
+        ({ input, output }) => {
+          // given - when - then
+          expect(lottoGameConsole.output.messages.rateOfReturn(input)).toMatch(output);
+        },
+      );
+    });
   });
 });
