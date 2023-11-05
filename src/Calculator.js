54097df771da96dd2b4f5ed255bb8725c92dedc7
badahertz52 <badahertz52@gmail.com>
2023-11-05 23:00:15 +0900

    feat: Calculator 클래스 생성
    -Calaculator: 로또 당첨 결과에 따른 수익률 계산

---
+++ src/Calculator.js
@@ -0,0 +1,30 @@
+import { WINNINGS_MONEY } from './constant';
+
+class Calculator {
+  calculateWinnings(winningResult) {
+    return winningResult.map((v) => WINNINGS_MONEY[v.rank] * v.number);
+  }
+
+  getTotalWinnings(winningResult) {
+    return this.calculateWinnings(winningResult).reduce((a, c) => a + c, 0);
+  }
+
+  round(number) {
+    return Math.round(number * 100) / 100;
+  }
+
+  calculateRateOfReturn(paymentAmount, winnings) {
+    const value = ((paymentAmount - winnings) / paymentAmount) * 100;
+
+    return this.round(value);
+  }
+
+  getRateOfReturn(winningResult, paymentAmount) {
+    const winnings = this.calculateWinnings(winningResult);
+
+    const rateOfReturn = this.calculateRateOfReturn(paymentAmount, winnings);
+    return rateOfReturn;
+  }
+}
+
+export default Calculator;
