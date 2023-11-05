12651bc8fca6e7010494309bb399db4da8f143fa
parkblo <badmuxi@gmail.com>
2023-11-05 23:29:00 +0900

    feat: 로또 번호 발행 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import { Console } from "@woowacourse/mission-utils";
-import { validatePurchasePrice } from "./Util";
+import { Random, Console } from "@woowacourse/mission-utils";
+import * as Util from "./Util";
+import { Lotto } from "./Lotto.js";
 
 class App {
-  async play() {
-    this.enterPurchasePrice();
+  numbersArray;
+  purchaseAmount;
+  basedAmount;
+
+  constructor() {
+    this.basedAmount = 1000;
   }
 
-  async enterPurchasePrice() {
+  async play() {
+    this.enterPurchaseAmount();
+    this.createRandomLottoNumbers();
+  }
+
+  async enterPurchaseAmount() {
     const userPrice = await Console.readLineAsync("구입금액을 입력해 주세요.");
-    validatePurchasePrice(userPrice);
+    Util.validatePurchasePrice(userPrice,this.basedAmount);
+    this.purchaseAmount = userPrice / this.basedAmount;
+  }
+
+  createRandomLottoNumbers() {
+    this.numbersArray = Util.create2DArray(this.purchaseAmount,6);
+    for (let i = 0 ; i < this.purchaseAmount ; i += 1) {
+      for (let j = 0 ; j < 6 ; j += 1) {
+        this.numbersArray[i][j] = Random.pickNumberInRange(1, 45);
+      }
+    }
   }
 }
 
--- src/Util.js
+++ src/Util.js
@@ -1,6 +1,10 @@
-export const validatePurchasePrice = (price) => {
-    const basedAmount = 1000;
+export function validatePurchasePrice(price,basedAmount) {
     if (price % basedAmount != 0) {
         throw new Error("[ERROR] 구매 금액은 1,000원 단위 입니다.")
     }
+};
+
+export function create2DArray(rows, columns) {
+    let arr = Array.from(Array(rows), () => new Array(columns));
+    return arr;
 };
