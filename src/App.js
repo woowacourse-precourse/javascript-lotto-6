7a5843dc349861ae0d59dce6dfc6a3726f67205c
Seung Jun <pache31@naver.com>
2023-11-05 18:51:06 +0900

    [feat] 로또 구매 및 당첨 번호 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import { LOTTO, INPUT, OUTPUT, ERROR } from './Constant';
+import ValidationCheck from './Validation';
+import BuyLotto from './BuyLotto';
+import Lotto from './Lotto';
+
 class App {
-  async play() {}
+  async play() {
+    const purchasedLotto = await this.getPurchaseLotto();
+    const winningNumber = await this.getWinningNumber();
+  }
+
+  async getPurchaseLotto() {
+    let purchasedLotto;
+    while (!purchasedLotto) {
+      try {
+        const price = await Console.readLineAsync(INPUT.PURCHASE_AMOUNT);
+        const buyLotto = new BuyLotto(price);
+        purchasedLotto = buyLotto.getLottos();
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return purchasedLotto;
+  }
+
+  async getWinningNumber() {
+    let winningNumber;
+    while (!winningNumber) {
+      try {
+        const winningNumbers = await Console.readLineAsync(
+          INPUT.WINNING_NUMBER,
+        );
+        const winningLotto = new Lotto(winningNumbers.split(OUTPUT.COMMA));
+        winningNumber = winningNumbers;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return winningNumber;
+  }
 }
 
 export default App;
