7ccd655b438092a7825d2ce327d02d4eda1fa2a8
xxziiko <xxziiko@gmail.com>
2023-11-06 00:27:35 +0900

    refactor: 금액과 수량 유효성 검사 로직 수정 및 입력한 금액의 예외처리

--- src/App.js
+++ src/App.js
@@ -5,46 +5,73 @@ class App {
   constructor() {
     this.error = {
       INVALID_PRICE: "[ERROR] 금액은 1,000원 단위로 입력해주세요.",
+      INCORRECT_TYPE: "[ERROR] 숫자만 입력해주세요.",
     };
   }
 
   displayErrorMessage(message) {
-    throw new Error(message);
+    MissionUtils.Console.print(`\n${message}`);
   }
 
   getMoney() {
     const money =
-      MissionUtils.Console.readLineAsync("구입 금액을 입력해주세요 \n");
+      MissionUtils.Console.readLineAsync("구입 금액을 입력해주세요. \n");
 
     return money;
   }
 
-  async getLottoQuantity() {
-    const money = await this.getMoney();
-    const quantity = +money / 1000;
+  async repeatValid() {
+    let price;
 
-    if (!Number.isInteger(+quantity))
+    while (true) {
+      price = await this.getMoney();
+      const validatedPrice = this.validatePrice(price);
+
+      if (validatedPrice) {
+        return validatedPrice;
+      }
+    }
+  }
+
+  validatePrice(price) {
+    if (price % 1000 !== 0) {
       this.displayErrorMessage(this.error.INVALID_PRICE);
+      return null;
+    }
+
+    if (isNaN(price)) {
+      this.displayErrorMessage(this.error.INCORRECT_TYPE);
+      return null;
+    }
+
+    return +price;
+  }
+
+  async getLottoQuantity(money) {
+    const quantity = money / 1000;
 
     return quantity;
   }
 
   setLotto(quantity) {
-    MissionUtils.Console.print(`\n ${quantity}개를 구매했습니다.`);
+    MissionUtils.Console.print(`\n${quantity}개를 구매했습니다.`);
 
     for (let i = 0; i < quantity; i += 1) {
-      const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
-        1,
-        45,
-        6
-      );
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
 
-      new Lotto(lottoNumbers).displayNumbers();
+      new Lotto(numbers).displayNumbers();
     }
   }
 
+  setBonusNumber() {
+    const winningNumber = this.getWinningNumber();
+
+    new Lotto(winningNumber).setWinnigNumber();
+  }
+
   async play() {
-    const quantity = await this.getLottoQuantity();
+    const money = await this.repeatValid();
+    const quantity = await this.getLottoQuantity(money);
     this.setLotto(quantity);
   }
 }
