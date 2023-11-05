0c7a18986cd9a78086996df8ace4283865d96819
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-05 12:13:52 +0900

    :sparkles: feat: 등수에 따른 당첨금 상수화, 로또 가격 상수화

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,4 +4,14 @@ const LOTTO_CONDITION = Object.freeze({
   length: 6,
 });
 
-export { LOTTO_CONDITION };
+const PRIZE_AMOUNT = Object.freeze({
+  firstPrize: 2_000_000_000,
+  secondPrize: 30_000_000,
+  thirdPrize: 1_500_000,
+  fourthPrize: 50_000,
+  fifthPrize: 5_000,
+});
+
+const PRICE_PER_TICKET = Object.freeze(1000);
+
+export { LOTTO_CONDITION, PRIZE_AMOUNT, PRICE_PER_TICKET };
