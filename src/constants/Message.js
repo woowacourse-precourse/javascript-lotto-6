9ea820dd23042193d93cb6fe9d6716e8646f3c5e
osohyun0224 <osohyun0224@naver.com>
2023-11-05 23:23:23 +0900

    feat(Message): 로또 게임 진행에 필요한 메세지 상수화하여 정의
    - 로또 게임 진행에 필요한 메세지들을 GUIDE_MESSAGE에 상수로 정의하였습니다.

---
+++ src/constants/Message.js
@@ -0,0 +1,9 @@
+export const GUIDE_MESSAGE =Object.freeze({
+  insertMoney: '구입 금액을 입력해 주세요.\n',
+  insertWinningNumbers: '당첨 번호를 입력해 주세요.\n',
+  insertBonusNumber: '보너스 번호를 입력해 주세요.\n',
+  totalTickets: '개를 구매했습니다.\n',
+  winningStatistics: '당첨 통계',
+  divider: '---',
+  totalReturn: '총 수익률은 *%입니다.'
+})
