071048bf30f7e4b8cb572115076da4fb6b635825
윤국현 <djaudwls7873@gmail.com>
2023-11-05 15:17:25 +0900

    test ~ing

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,30 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (this.#hasDuplicate(numbers)) {
+      throw new Error("[ERROR] 로또 번호에는 중복된 숫자가 없어야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  #hasDuplicate(numbers) {
+    return new Set(numbers).size !== numbers.length;
+  }
+
+  toString() {
+    return `[${this.#numbers.join(", ")}]`;
+  }
+
+  getWinningResult(winningNumbers, bonusNumber) {
+    const matchedNumbers = this.#numbers.filter((number) =>
+      winningNumbers.includes(number)
+    );
+    const isBonusMatched = this.#numbers.includes(bonusNumber);
+    return {
+      matchCount: matchedNumbers.length,
+      isBonusMatched,
+    };
+  }
 }
 
 export default Lotto;
