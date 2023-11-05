3c5442dcf22cb810c052b4cfb9575a4c3e7bb2b5
seokho10007 <sukho1007@naver.com>
2023-11-05 23:48:12 +0900

    ✨ feat: 당첨 번호와 보너스 번호를 관리하는 LottoManageModel class 추가
    - 보너스 번호에 대한 유효성 검사 실행
    - Lotto class를 통홰 당첨 번호를 저장
    - 특정 로또 번호를 입력 받아 당첨 여부를 판단하는 메서드 추가

---
+++ src/model/LottoManageModel.js
@@ -0,0 +1,60 @@
+import Lotto from '../Lotto.js';
+
+import { throwError } from '../utils/index.js';
+import { MESSAGE } from '../utils/constants.js';
+
+class LottoManageModel {
+  /**
+   * @description 당첨 번호 - 6개의 숫자로 이루어진 배열이다.
+   * @example [1, 2, 3, 4, 5, 6]
+   */
+  #winningNumbers;
+
+  /**
+   * @description 보너스 번호 - 당첨 번호와 중복되지 않아야 한다.
+   * @example 7
+   */
+  #bonusNumber;
+
+  getWinningNumbers() {
+    return this.#winningNumbers.getNumbers();
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+
+  setWinningNumbers(winningNumbers) {
+    this.#winningNumbers = new Lotto(winningNumbers);
+  }
+
+  setBonusNumber(bonusNumber) {
+    this.#isBonusNumberDuplicate(bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  returnMatchCountByLotto(lotto) {
+    const matchCount = this.#matchByWinningNumbers(lotto);
+    const bonusCount = this.#matchByBonusNumber(lotto);
+
+    return [matchCount, bonusCount];
+  }
+
+  #matchByWinningNumbers(lotto) {
+    return this.getWinningNumbers().filter((number) => lotto.includes(number))
+      .length;
+  }
+
+  #matchByBonusNumber(lotto) {
+    return Number(lotto.includes(this.#bonusNumber));
+  }
+
+  #isBonusNumberDuplicate(bonusNumber) {
+    throwError(
+      MESSAGE.bonus_number_duplicate,
+      this.getWinningNumbers().includes(bonusNumber),
+    );
+  }
+}
+
+export default LottoManageModel;
