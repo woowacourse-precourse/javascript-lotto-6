66ec7792b13adcfa2b57b77383fe1e478385a2c9
Yoos99 <yoosh2520@naver.com>
2023-11-06 04:23:01 +0900

    feat: 에러 메시지 관리 상수 추가

---
+++ src/ErrorMessages.js
@@ -0,0 +1,10 @@
+const ErrorMessages = {
+  NOT_DIVISIBLE_AMOUNT:
+    "[ERROR] 구매 금액은 1,000원으로 나누어 떨어져야 합니다.",
+  INVALID_NUMBER_INPUT: "[ERROR] 유효하지 않은 숫자 입력입니다.",
+  DUPLICATE_NUMBER: "[ERROR] 로또 번호와 보너스 번호에 중복이 발생했습니다.",
+  TOO_MANY_WINNING_NUMBERS: "[ERROR] 당첨 번호는 6개를 초과할 수 없습니다.",
+  NUMBER_OUT_OF_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+};
+
+export default ErrorMessages;
