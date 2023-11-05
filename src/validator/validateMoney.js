b9b473ba904ce3a9f28b5fc49e4b43b98a34553e
pakxe <pigkill40@naver.com>
2023-11-05 18:00:28 +0900

    feat: 유효성 검사기의 메서드를 조합해 돈의 유효성 검사를 하는 함수 작성

---
+++ src/validator/validateMoney.js
@@ -0,0 +1,15 @@
+import ERROR from '../constant/Error.js';
+import LOTTO from '../constant/Lotto.js';
+import MoneyError from '../error/MoneyError.js';
+import InputValidator from './InputValidator.js';
+
+const isInvalidMoneyPrice = (money) => {
+  return money % LOTTO.price !== 0;
+};
+
+const validateMoney = (money) => {
+  if (InputValidator.isNan(money)) throw new MoneyError(ERROR.isNan);
+  if (isInvalidMoneyPrice(money)) throw new MoneyError(ERROR.moneyPrice);
+};
+
+export default validateMoney;
