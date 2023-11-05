111cecd97130701757234845f03c7d3eb03db45b
donggyun <donggyunkim12@gmail.com>
2023-11-05 11:23:30 +0900

    feat(LottoGame): 돈의 액수에 따른 로또 추가 기능 구현

--- src/controller/LottoGame.js
+++ src/controller/LottoGame.js
@@ -1,11 +1,15 @@
-import { inputMoney } from '../view/inputPrompt.js';
+import Lotto from '../model/Lotto.js';
+import LottoList from '../model/LottoList.js';
+import { inputMoney } from '../view/InputPrompt.js';
 import { validateDivisible, validateNumber } from '../utils/validateFn.js';
 
 class LottoGame {
   #money = 0;
+  #myLottos = new LottoList();
 
   async buyLotto() {
     await this.#setMoney();
+    this.#setMyLottoList();
   }
 
   async #setMoney() {
@@ -13,6 +17,13 @@ class LottoGame {
     validateNumber(input);
     validateDivisible(input);
     this.#money = parseInt(input);
+  }
+
+  #setMyLottoList() {
+    const amount = this.#money / 1000;
+    for (let i = 0; i < amount; i++) {
+      this.#myLottos.add(Lotto.setLottery());
+    }
   }
 }
 
