f1a30373445acf1fb4e4b66ecd86b610ad604672
hamo-o <yheel369@gmail.com>
2023-11-05 13:56:15 +0900

    fix: 로또 당첨 번호 문자열을 숫자 배열로 변환해서 반환

--- src/Input.js
+++ src/Input.js
@@ -7,7 +7,7 @@ const Input = {
   },
   async getLottoNumber() {
     const numbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
-    return numbers;
+    return numbers.split(',').map((number) => Number(number));
   },
   async getLottoBonusNumber() {
     const number = await Console.readLineAsync(
