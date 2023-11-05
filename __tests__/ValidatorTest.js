1ec6c234203a2dd61dd2cf6a9e13a7d04df784a5
heonq <gkagjsrb@naver.com>
2023-11-05 14:45:22 +0900

    refactor(ValidatorTest): 유효성 검사 기능 테스트 리팩터링 - test.each를 통해 반복되는 테스트 코드 단축

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,16 +1,13 @@
 import Validator from '../utils/Validator.js';
 
+const invalidPurchaseAmounts = [900, 1100, -1000, 1000.5, '천원'];
+const validPurchaseAmounts = [1000, 2000, 3000, 4000, 10000];
+
 describe('유효성 검사 기능 테스트', () => {
-  test('유효하지 않은 구입 금액에 대한 테스트', () => {
-    const invalidPurchaseAmounts = [900, 1100, -1000, 1000.5, '천원'];
-    invalidPurchaseAmounts.forEach((invalidPurchaseAmount) =>
-      expect(Validator.validatePurchaseAmount(invalidPurchaseAmount)).toEqual(false),
-    );
-  });
-  test('유효한 구입 금액에 대한 테스트', () => {
-    const validPurchaseAmounts = [1000, 2000, 3000, 4000, 10000];
-    validPurchaseAmounts.forEach((validPurchaseAmount) =>
-      expect(Validator.validatePurchaseAmount(validPurchaseAmount)).toEqual(true),
-    );
+  test.each([
+    [invalidPurchaseAmounts, Validator.validatePurchaseAmount, false],
+    [validPurchaseAmounts, Validator.validatePurchaseAmount, true],
+  ])('구입 금액에 대한 유효성 검사 테스트', (purchaseAmounts, validation, result) => {
+    purchaseAmounts.forEach((purchaseAmount) => expect(validation(purchaseAmount)).toEqual(result));
   });
 });
