113ffcdfb0b79b2a7160e4eddc7978161f5abd4c
osohyun0224 <osohyun0224@naver.com>
2023-11-06 04:28:34 +0900

    feat(Message): 입력받은 당첨 번호에 대한 유효성 검사 상수 메시지 구현
    - 사용자에게 입력받은 당첨 번호에 대해 유효성 검사를 구현하기 위해 에러 메세지를 정의하였습니다.

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -13,3 +13,12 @@ export const PURCHASE_AMOUNT_ERROR_MESSAGE = Object.freeze({
   underThousand: '[ERROR] 잘못된 입력입니다. 1,000원 미만의 금액은 입력이 불가합니다. 게임을 종료합니다.',
   notNumber: '[ERROR] 잘못된 입력입니다. 구입 금액은 숫자여야 합니다. 게임을 종료합니다.',
 })
+
+export const WINNING_NUMBERS_ERROR_MESSAGE = Object.freeze({
+  notNaturalNumber: '[ERROR]: 잘못된 입력입니다. 0 이하의 값은 입력이 불가합니다. 게임을 종료합니다.',
+  wrongRange: '[ERROR] 잘못된 범위입니다. 로또 번호는 1부터 45까지 입력할 수 있습니다. 게임을 종료합니다.',
+  detectedLastComma: '[ERROR]: 잘못된 입력입니다. 입력된 값 없이 콤마(,)를 입력할 수 없습니다. 게임을 종료합니다.',
+  wrongWinningNumber: '[ERROR]: 잘못된 형식입니다. 숫자와 쉼표 외에는 입력할 수 없습니다. 게임을 종료합니다.',
+  wrongCount: '[ERROR] 잘못된 입력입니다. 로또 한 장의 숫자 개수는 6개 입니다. 게임을 종료합니다.',
+  emptyInput: '[ERROR] 입력된 값이 없습니다. 게임을 종료합니다.',
+})
