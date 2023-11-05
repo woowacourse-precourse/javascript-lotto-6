db0c7bdf7ee60b338963d9e37c3f188213e98126
whai2 <whai2125@gmail.com>
2023-11-05 14:03:35 +0900

    test: 당첨 내역을 출력, 단위 테스트

--- __tests__/ResultTest.js
+++ __tests__/ResultTest.js
@@ -1,4 +1,5 @@
-import { lottoReader } from "../src/App.js" 
+import { lottoReader, lottoResultPrinter } from "../src/App.js" 
+
 describe("당첨 통계 테스트", () => {
   test("당첨 번호와 로또 번호를 비교한다.", () => {
     const lotto = [1, 3, 5, 14, 22, 45];
@@ -7,6 +8,16 @@ describe("당첨 통계 테스트", () => {
     const output = [3,0];
 
     const result = lottoReader(lotto,winninglotto,BONUS_NUMBER);
+
+    expect(result).toEqual(output);
+  });
+
+  test("당첨 내역을 출력한다.", () => {
+    const winningInput = [3,0];
+    const countInput = 1;
+    const output = "3개 일치 (5,000원) - 1개";
+
+    const result = lottoResultPrinter(winningInput, countInput);
 
     expect(result).toEqual(output);
   });
