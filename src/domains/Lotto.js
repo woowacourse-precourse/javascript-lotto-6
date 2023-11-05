c478a775b3533f568288a1a04eed6da6e6fcf171
brgndy <brgndy96@gmail.com>
2023-11-05 09:46:38 +0900

    feat(lotto): 로또 관련한 클래스 및 객체 생성

--- src/domains/Lotto.js
+++ src/domains/Lotto.js
@@ -1,18 +1,64 @@
+import { newLottoGenerator } from '../utils/newLottoGenerator.js';
+import LottoError from '../errors/LottoError.js';
+import { ERROR_MESSAGES } from '../constants/errorMessages.js';
+
 class Lotto {
+  /**
+   * @member {number} LOTTO_LENGTH 로또가 가질 수 있는 숫자 갯수
+   * @member {number} MIN_NUM 로또가 가질 수 있는 숫자의 최소값
+   * @member {number} MAX_NUM 로또가 가질 수 있는 숫자의 최대값
+   */
+
+  static LOTTO_LENGTH = 6;
+
+  static MIN_NUM = 1;
+
+  static MAX_NUM = 45;
+
+  /**
+   * @type {[number, number, number, number, number, number]}
+   */
+
   #numbers;
 
+  /**
+   *
+   * @param {[number, number, number, number, number, number]} numbers
+   */
   constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
+    this.validate();
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+  static getRandomNums() {
+    const lottoNums = newLottoGenerator(Lotto.MIN_NUM, Lotto.MAX_NUM, Lotto.LOTTO_LENGTH);
+
+    return new Lotto(lottoNums);
+  }
+
+  /**
+   *
+   * @param {number} lottoAmount
+   * @returns { number[] }
+   */
+
+  static getPurchasedLottos(lottoAmount) {
+    return [...Array(lottoAmount)].map(() => Lotto.getRandomNums());
+  }
+
+  toStringFromNumbers() {
+    return `[${this.#numbers.join(', ')}]`;
+  }
+
+  validate() {
+    this.validateLength();
+  }
+
+  validateLength() {
+    if (this.#numbers.length !== Lotto.LOTTO_LENGTH) {
+      throw new LottoError(ERROR_MESSAGES.lotto_not_match_length);
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
