b028b391837e2bd7923198d7429e85ef6b350b1c
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-11-05 21:26:12 +0900

    refactor: LottoPurchaser 클래스 내부에서 static private으로 설정한 필드와 메서드 private으로만 설정

--- src/LottoPurchaser.js
+++ src/LottoPurchaser.js
@@ -1,20 +1,20 @@
 import { ERROR } from './Message.js';
 
 class LottoPurchaser {
-  static #ZERO = 0;
-  static #THOUSAND = 1000;
+  #ZERO = 0;
+  #THOUSAND = 1000;
 
   #purchaseAmount;
 
   constructor(purchaseAmount) {
-    LottoPurchaser.#validate(Number(purchaseAmount));
+    this.#validate(Number(purchaseAmount));
     this.#purchaseAmount = Number(purchaseAmount);
   }
 
-  static #validate(value) {
-    if (value === LottoPurchaser.#ZERO) throw new Error(ERROR.falsy);
+  #validate(value) {
+    if (value === this.#ZERO) throw new Error(ERROR.falsy);
     if (Number.isNaN(value)) throw new Error(ERROR.falsy);
-    if (value % LottoPurchaser.#THOUSAND !== LottoPurchaser.#ZERO) {
+    if (value % this.#THOUSAND !== this.#ZERO) {
       throw new Error(ERROR.notBeDividedByThousand);
     }
   }
