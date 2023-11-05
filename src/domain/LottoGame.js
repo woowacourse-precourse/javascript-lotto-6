bdcc3f3c72962071c307559d07eea9be6d192625
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-05 10:41:22 +0900

    :sparkles: feat: 로또 비교결과에 따라 해당하는 등수를 카운팅하는 기능추가

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -1,6 +1,5 @@
 import Lotto from '../Lotto.js';
 import randomNumberGenerator from '../utils/randomNumberGenerator.js';
-
 class LottoGame {
   #purchasedLotto;
 
@@ -22,13 +21,44 @@ class LottoGame {
   getPurchasedLotto() {
     return this.#purchasedLotto.map((lotto) => lotto.getSortedLotto());
   }
+
+  // 일치한 숫자에 따른 당첨등수
+  createEmptyStatistics() {
+    return {
+      firstPrize: 0,
+      secondPrize: 0,
+      thirdPrize: 0,
+      fourthPrize: 0,
+      fifthPrize: 0,
+    };
+  }
+
+  // 일치한 번호만큼 해당하는 등수를 카운팅해서 등수를 반환함
+  getStatistics(comparisonResults) {
+    const statistics = this.createEmptyStatistics();
+
+    comparisonResults.forEach(({ matchingCount, hasBonusNumber }) => {
+      if (matchingCount === 6) statistics.firstPrize += 1;
+      if (matchingCount === 5 && hasBonusNumber) statistics.secondPrize += 1;
+      if (matchingCount === 5 && !hasBonusNumber) statistics.thirdPrize += 1;
+      if (matchingCount === 4) statistics.fourthPrize += 1;
+      if (matchingCount === 3) statistics.fifthPrize += 1;
+    });
+
+    return statistics;
+  }
 }
 
 export default LottoGame;
 
-/*
-테스트용 콘솔
+/* 테스트용 콘솔
 const lotto = new LottoGame(2);
-console.log(lotto.getLottoTickets(), '로또티켓');
+console.log(lotto.getPurchasedLotto(), '로또티켓');
 console.log(lotto.getLottoComparisonResults([1, 2, 3, 4, 5, 6], 7), '일치여부');
+console.log(
+  lotto.getStatistics([
+    { matchingCount: 5, hasBonusNumber: false },
+    { matchingCount: 5, hasBonusNumber: true },
+  ])
+);
 */
