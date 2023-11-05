e363f69bb39495fcbb9aeb0f67204bd4dd7a50dd
yjh1008 <exesea12@gmail.com>
2023-11-05 23:18:48 +0900

    feat : 로또 뽑기 및 결과 출력 기능 구현.
    1. 로또를 뽑는 함수 생성.
    2. 당첨내역을 출력하는 함수 생성.
    3. 당첨 번호 및 보너스 번호를 입력받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Console, Random } from "@woowacourse/mission-utils";
-import { BUY_LOTTO, LOTTO_COUNT, PRINT_PURCHASE_LOTTO } from "./GameComment.";
+import { BUY_LOTTO, LOTTO_COUNT, PRINT_PURCHASE_LOTTO, WINNING_OUTCOME, INPUT_WINNING_NUMBER,  GENERATE_BONUS_NUM} from "./GameComment.";
 import Lotto from "./Lotto";
 class App {
   validPrice(price) {
@@ -12,22 +12,46 @@ class App {
   drawLotto(lottoContainer, count) {
     for(let i=0; i<count; i++) {
       const lottoNums = Random.pickUniqueNumbersInRange(1, 45, 6);
-      lottoContainer[i] = new Lotto(lottoNums);
+      lottoContainer[i] = new Lotto(lottoNums.sort((a,b)=>a-b));
     }
-    Console.print(PRINT_PURCHASE_LOTTO(lottoContainer));
+
+  }
+
+  matchNums (lotto, winningNums, bonusNum, result) {
+    let matches = -3;
+    winningNums.get().forEach(v=>{
+      if(lotto.includes(v)) matches++;
+    });
+    if(matches < 0) return;
+    if(matches === 5 && lotto.includes(bonusNum))  result[matches+1] += 1;
+    result[matches] += 1;
+    return;
+  }
+
+  lottoResult(lottoContainer, winningNums, bonusNum) {
+    const result = Array.from({length :5}).fill(0);
+    lottoContainer.forEach(v=> {
+      const lotto = v.get();
+      this.matchNums(lotto, winningNums, bonusNum, result);
+    });
+    Console.print(WINNING_OUTCOME(result));
+  }
+
+  async geterateWinNums() {
+    const input = await Console.readLineAsync(INPUT_WINNING_NUMBER);
+    return input.split(",").map(Number);
   }
 
   async play() {
     const price = await Console.readLineAsync(BUY_LOTTO);
-    console.log(price);
     const count = this.validPrice(price);
-    
     Console.print(LOTTO_COUNT(count));
-
     const lottoContainer = Array.from(count).fill(null);
-
-    this.drawLotto(lottoContainer, count)
-
+    this.drawLotto(lottoContainer, count);
+    Console.print(PRINT_PURCHASE_LOTTO(lottoContainer));
+    const winningNums = new Lotto(await this.geterateWinNums());
+    const bonusNum = await Console.readLineAsync(GENERATE_BONUS_NUM);
+    this.lottoResult(lottoContainer, winningNums, bonusNum);
   }
 }
 
