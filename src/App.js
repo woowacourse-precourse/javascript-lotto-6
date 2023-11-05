9b600d70db3d4311c207c75aac6315891317e51e
soi-ha <soy2302ten@gmail.com>
2023-11-05 18:01:55 +0900

    feat: 로또 구입 금액 입력 유효성 검사 BuyLotto.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { InputValue } from './InputValue.js';
 import Lotto from './Lotto.js';
+import { BuyLotto } from './BuyLotto.js';
 
 class App {
 	async play() {
 		try {
-			const inputValue = new InputValue();
-			const win = await inputValue.winningNumber();
-			new Lotto(win);
+			// const inputValue = new InputValue();
+			// const win = await inputValue.winningNumber();
+			new BuyLotto(-100);
+			// new Lotto(win);
 		} catch (error) {
 			console.log(error.message);
 		}
