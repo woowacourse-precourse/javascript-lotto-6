e36a1c8732896c63a252fc4742059fc55f350f8b
brgndy <brgndy96@gmail.com>
2023-11-05 09:49:19 +0900

    feat(lotto): 구매한 로또 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { asyncFnHandlerWithError } from './utils/asyncFnHandlerWithError.js';
 import { InputView } from './views/InputView.js';
 import { OutputView } from './views/OutputView.js';
 import Money from './domains/Money.js';
+import Lotto from './Lotto.js';
 
 class App {
   /**
@@ -11,13 +12,27 @@ class App {
 
   #lottoAmount;
 
+  /**
+   * @type { Lotto[] } 구입한 총 로또
+   */
+
+  #lottos;
+
   async play() {
     await asyncFnHandlerWithError(this.inputMoney, this);
+    this.printLottos();
   }
 
   async inputMoney() {
     const money = await InputView.readLine(LOTTO_MESSAGES.input_money);
     this.#lottoAmount = Money.getLottoAmount(money);
+    OutputView.divideLine();
+  }
+
+  printLottos() {
+    OutputView.printLine(LOTTO_MESSAGES.buy_lottos(this.#lottoAmount));
+    this.#lottos = Lotto.getPurchasedLottos(this.#lottoAmount);
+    this.#lottos.forEach((lotto) => OutputView.printLine(lotto.toStringFromNumbers()));
     OutputView.divideLine();
   }
 }
--- src/constants/lottoMessage.js
+++ src/constants/lottoMessage.js
@@ -1,3 +1,4 @@
 export const LOTTO_MESSAGES = Object.freeze({
   input_money: '구입금액을 입력해 주세요.\n',
+  buy_lottos: (amount) => `${amount}개를 구매했습니다.`,
 });
