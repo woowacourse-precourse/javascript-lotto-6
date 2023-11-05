a6c81dfa1aaa1cc28b95338d494862b1c10eba7f
ChanYong Park <krap5075@naver.com>
2023-11-06 02:36:22 +0900

    refactor: 사용자 입력 값 클래스 필드로 변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,19 +2,25 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 const Console = MissionUtils.console;
 class InputView {
+  money = 0;
+
+  winningnumbers = [];
+
+  bonusNumber = 0;
+
   async inputMoney() {
-    const money = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    this.money = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     // 금액 입력 예외 처리
   }
 
   async inputNumbers() {
-    const winningnumbers =
+    this.winningnumbers =
       await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
     // 당첨 번호 입력 예외처리
   }
 
-  async bonusNumber() {
-    const bonusNumber =
+  async inputBonusNumber() {
+    this.bonusNumber =
       await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
     // 보너스 번호 입력 예외 처리
   }
