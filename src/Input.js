00ea6bd4cc7a81acc2b4feae0af3f1b55c90a142
parkjonggyeong18 <gd122572@gmail.com>
2023-11-06 00:42:14 +0900

    feat: 입력문 작성

---
+++ src/Input.js
@@ -0,0 +1,34 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class Input {
+  static async getPurchaseAmount() {
+    try {
+      const input = await Console.readLineAsync('구입 금액을 입력해 주세요: ');
+      const purchaseAmount = parseInt(input, 10);
+
+      if (purchaseAmount % 1000 !== 0) {
+        throw new Error("[ERROR] 1000원 단위로 입력해야 합니다.");
+      }
+
+      return purchaseAmount;
+    } catch (error) {
+      throw new Error(`[ERROR] ${error.message}`);
+    }
+  }
+
+  static async getWinningNumbers() {
+    try {
+      const input = await Console.readLineAsync('당첨 번호를 입력해 주세요(쉼표로 구분): ');
+      const winningNumbers = input.split(',').map((num) => parseInt(num, 10));
+
+      if (winningNumbers.length !== 6 || !this.isValidRange(winningNumbers)) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 6개의 숫자여야 합니다.");
+      }
+
+      return winningNumbers;
+    } catch (error) {
+      throw new Error(`[ERROR] ${error.message}`);
+    }
+  }
+}
+export default Input;
