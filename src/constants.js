551f2fc6d28110abfeca356c45474b9face97072
Boeun <boeun0904@naver.com>
2023-11-05 23:24:02 +0900

    feat: 에러 메세지 상수화

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const MESSAGE = {
+export const MESSAGE = {
   INPUT_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
   PURCHASE_COUNT: (cnt) => `${cnt}개를 구매했습니다.`,
   INPUT_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.',
@@ -10,4 +10,16 @@ const MESSAGE = {
   TOTAL_ROR: (total) => `총 수익률은 ${total}%입니다.`,
 };
 
+export const ERROR = {
+  BLANK_INPUT: '값을 입력해 주세요ㄹ.',
+  NOT_A_NUMBER: '숫자를 입력해 주세요.',
+  NOT_A_NATURAL_NUMBER: '1 이상의 숫자를 입력해 주세요.',
+  NOT_DIVIDED_BY_THOUSAND: '1000 단위로 입력해 주세요.',
+  NOT_SIX_NUMBERS: '로또 번호는 6개여야 합니다.',
+  ALREADY_SELECTED: '당첨 번호에 존재하는 숫자입니다.',
+  OUT_OF_RANGE: (start, end) =>
+    `로또 번호는 ${start}부터 ${end} 사이의 숫자여야 합니다.`,
+};
+
 Object.freeze(MESSAGE);
+Object.freeze(ERROR);
