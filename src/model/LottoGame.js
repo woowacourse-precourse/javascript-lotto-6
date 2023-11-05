6cf64075b8276fb38f0d7271bb5abab6c5a0470a
노태균 <taegyunn123@gmail.com>
2023-11-05 17:12:11 +0900

    refactor: start 함수에서 같은 역할을 하는 코드들 메서드화

--- src/model/LottoGame.js
+++ src/model/LottoGame.js
@@ -8,25 +8,48 @@ class LottoGame {
   #winningResult = new Array(REWARD.length).fill(0);
 
   async start() {
+    // 로또 구매
+    const { purchaseAmount, lottos } = await this.#purchaseLottos();
+
+    // 당첨 번호 입력 받기 (당첨 번호 유효성 체크 포함)
+    const { answerNumbers, bonusNumbers } = await this.#getAnswerNumbers();
+
+    // 각 등수별 당첨 횟수 구하기 => #winningResult에 저장
+    lottos.forEach((lotto) => this.#addWinning(lotto, answerNumbers, bonusNumbers));
+
+    // 당첨 통계 출력
+    this.#printResult(purchaseAmount);
+  }
+
+  async #purchaseLottos() {
     // 구매 금액 입력 받기 (구매 금액 유효성 체크 포함)
     const purchaseAmount = await Read.purchaseAmount();
 
     // 구매 금액에 맞는 개수의 로또 발행
-    const lottos = this.#createLottos(purchaseAmount);
-    const purchaseLottos = lottos.map((lotto) => new Lotto(lotto));
+    const lottoArrs = this.#createLottos(purchaseAmount);
+    const lottos = lottoArrs.map((lotto) => new Lotto(lotto));
 
     // 구매 내역 출력
-    Print.purchaseLottos(lottos);
+    Print.purchaseLottos(lottoArrs);
 
+    return { purchaseAmount, lottos };
+  }
+
+  /**
+   * 당첨 번호 입력 받기
+   * @returns {{answerNumbers: number[], bonusNumbers: number[]}}
+   */
+  async #getAnswerNumbers() {
     // 당첨 번호 입력 받기 (로또 번호 유효성 체크 포함)
     const answerNumbers = await Read.answerNumbers();
 
     // 보너스 번호 입력 받기 (보너스 번호 유효성 체크 포함)
     const bonusNumbers = await Read.bonusNumbers(answerNumbers);
 
-    // 각 등수별 당첨 횟수 구하기
-    purchaseLottos.forEach((lotto) => this.#addWinning(lotto, answerNumbers, bonusNumbers));
+    return { answerNumbers, bonusNumbers };
+  }
 
+  #printResult(purchaseAmount) {
     // 당첨 통계 출력
     Print.lottoResult(this.#winningResult);
     Print.lottoProfit(this.#getProfit(purchaseAmount));
@@ -35,7 +58,7 @@ class LottoGame {
   /**
    * 로또 번호 생성
    * @param {number} purchaseAmount
-   * @returns {number[]}
+   * @returns {number[][]}
    */
   #createLottos(purchaseAmount) {
     const count = purchaseAmount / LOTTO.PRICE;
