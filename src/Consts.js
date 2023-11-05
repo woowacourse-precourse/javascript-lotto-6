e3cab664ed7976f1fb39eec877f738d0b9b9e0ab
youngchann <lych96@gmail.com>
2023-11-06 00:03:11 +0900

    feat: Consts.js 추
    - 출력하는 메세지 및 당첨 금액이 객체로 저장됨

---
+++ src/Consts.js
@@ -0,0 +1,21 @@
+const Message = {
+  tryPurchaseLotto: '구입금액을 입력해 주세요.',
+  winningNumbers: '당첨 번호를 입력해 주세요.',
+  bonusNumber: '보너스 번호를 입력해 주세요.',
+  winningStatistics: '당첨 통계\n---',
+  threeMatches: '3개 일치 (5,000원) - ',
+  fourMatches: '4개 일치 (50,000원) - ',
+  fiveMatches: '5개 일치 (1,500,000원) - ',
+  fiveBonusMatches: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  sixMatches: '6개 일치 (2,000,000,000원) - ',
+};
+
+const Money = {
+  threeMatches: 5000,
+  fourMatches: 50000,
+  fiveMatches: 1500000,
+  fiveBonusMatches: 30000000,
+  sixMatches: 2000000000,
+};
+
+export { Message, Money };
