e7839e09c4706d3c4cbe171917008ae6c7c7654e
j8won <joowoni99@gmail.com>
2023-11-05 18:41:14 +0900

    test(LottoPrizeManagerTest): 당첨 번호와 보너스 번호를 로또 번호와 비교해 올바른 등수를 반환하는지 확인하는 테스트 코드 작성

--- __tests__/LottoPrizeManagerTest.js
+++ __tests__/LottoPrizeManagerTest.js
@@ -1,7 +1,8 @@
 import LottoPrizeManager from "../src/LottoPrizeManager.js";
 import PRIZE from "../src/constant/PRIZE.js";
 
-const correctWinningNumberArray = ["1", "2", "3", "4", "5", "6"];
+const correctWinningNumber = ["1", "2", "3", "4", "5", "6"];
+const correctBonusNumber = "7";
 
 describe("LottoPrizeManager 클래스 테스트", () => {
   test("당첨 번호가 6개가 아니라면 예외 처리한다", () => {
@@ -35,7 +36,7 @@ describe("LottoPrizeManager 클래스 테스트", () => {
     "보너스 번호가 로또 숫자 범위 내의 숫자가 아니라면 예외 처리한다 - %s",
     (bonusNumber) => {
       expect(() => {
-        new LottoPrizeManager(correctWinningNumberArray, bonusNumber);
+        new LottoPrizeManager(correctWinningNumber, bonusNumber);
       }).toThrow(PRIZE.ERROR.BONUS_NUMBER_RANGE_NUMBER);
     }
   );
@@ -43,7 +44,35 @@ describe("LottoPrizeManager 클래스 테스트", () => {
   test("보너스 번호가 당첨 번호와 중복되면 예외 처리한다", () => {
     const bonusNumber = "1";
     expect(() => {
-      new LottoPrizeManager(correctWinningNumberArray, bonusNumber);
+      new LottoPrizeManager(correctWinningNumber, bonusNumber);
     }).toThrow(PRIZE.ERROR.BONUS_NUMBER_DUPLICATE);
   });
+
+  test.each([
+    ["FIRST", [1, 2, 3, 4, 5, 6]],
+    ["SECOND", [1, 2, 3, 4, 5, 7]],
+    ["THIRD", [1, 2, 3, 4, 5, 8]],
+    ["FOURTH", [1, 2, 3, 4, 8, 9]],
+    ["FIFTH", [1, 2, 3, 8, 9, 10]],
+  ])(
+    "당첨 번호와 보너스 번호를 로또 번호와 비교해 등수를 반환한다 - %s",
+    (expectedRank, numberArray) => {
+      const defaultResult = {
+        FIRST: 0,
+        SECOND: 0,
+        THIRD: 0,
+        FOURTH: 0,
+        FIFTH: 0,
+      };
+
+      const prizeManager = new LottoPrizeManager(
+        correctWinningNumber,
+        correctBonusNumber
+      );
+
+      const result = prizeManager.calculateAllLottoRank([numberArray]);
+      defaultResult[expectedRank]++;
+      expect(result).toEqual(defaultResult);
+    }
+  );
 });
--- src/constant/PRIZE.js
+++ src/constant/PRIZE.js
@@ -1,3 +1,11 @@
+const RANK = {
+  FIRST: { MATCHED_COUNT: 6, MONEY: 2_000_000_000 },
+  SECOND: { MATCHED_COUNT: 5, BONUS_MATCH: true, MONEY: 30_000_000 },
+  THIRD: { MATCHED_COUNT: 5, MONEY: 1_500_000 },
+  FOURTH: { MATCHED_COUNT: 4, MONEY: 50_000 },
+  FIFTH: { MATCHED_COUNT: 3, MONEY: 5_000 },
+};
+
 const ERROR = {
   WINNING_NUMBER_SIZE: "[ERROR] 당첨 번호는 6개여야 합니다",
   WINING_NUMBER_DUPLICATE: "[ERROR] 당첨 번호는 중복되면 안 됩니다",
@@ -10,5 +18,6 @@ const ERROR = {
 };
 
 Object.freeze(ERROR);
+Object.freeze(RANK);
 
-export default { ERROR };
+export default { ERROR, RANK };
