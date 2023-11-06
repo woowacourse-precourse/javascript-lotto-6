36f50be04c6708bc59583d82e2ad1ffb3ee8908a
swarvy <swarvy0826@naver.com>
2023-11-07 02:59:07 +0900

    refactor : test로직에 따라 validate를 view단에서 진행하지 않도록 변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,28 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 import { LOTTO_NOTIFICATION_MESSAGE } from '../constants/LottoMessage.js';
-import validateLottoBonusNumber from '../validator/LottoBonusNumberValidator.js';
-import validateLottoSeedMoney from '../validator/LottoSeedMoneyValidator.js';
-import validateLottoNumbers from '../validator/LottoNumbersValidator.js';
 
-const readLine = async (message, validate) => {
-  const input = await Console.readLineAsync(message);
-  validate(input);
-  return input;
-};
+const readLine = (message) => Console.readLineAsync(message);
 
 export const readLottoSeedMoney = () =>
-  readLine(
-    LOTTO_NOTIFICATION_MESSAGE.enterLottoSeedMoney,
-    validateLottoSeedMoney
-  );
+  readLine(LOTTO_NOTIFICATION_MESSAGE.enterLottoSeedMoney);
 
 export const readLottoMainNumbers = () =>
-  readLine(
-    LOTTO_NOTIFICATION_MESSAGE.enterLottoWinningNumber,
-    validateLottoNumbers
-  );
+  readLine(LOTTO_NOTIFICATION_MESSAGE.enterLottoWinningNumber);
 
-export const readLottoBonusNumber = (lottoNumbers) =>
-  readLine(LOTTO_NOTIFICATION_MESSAGE.enterLottoWinningBonusNumber, (input) =>
-    validateLottoBonusNumber(input, lottoNumbers)
-  );
+export const readLottoBonusNumber = () =>
+  readLine(LOTTO_NOTIFICATION_MESSAGE.enterLottoWinningBonusNumber);
