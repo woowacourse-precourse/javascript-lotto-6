8963e345cd8528687f7ab12cfd66e8ea4ef612ed
meunrud <113085963+meunrud@users.noreply.github.com>
2023-11-06 21:17:28 +0900

    feat : 사용자에게 로또 번호 입력 요청 기능 구현

---
+++ src/InputView.js
@@ -0,0 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const INPUT_PURCHASING_AMOUNT = '구입금액을 입력해 주세요.\n';
+const INPUT_WINNING_NUMBER = '당첨 번호를 입력해 주세요.\n';
+const INPUT_BONUST_NUMBER = '보너스 번호를 입력해 주세요.';
+
+class InputView {
+  async inputWinningNumber() {
+    const playerInput = await Console.readLineAsync(INPUT_WINNING_NUMBER);
+    return playerInput;
+  }
+}
