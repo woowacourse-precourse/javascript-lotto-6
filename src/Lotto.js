5a1c29a48265b37aaab268fc2479133e60a3c87d
JiSeong Kim <12180543@inha.edu>
2023-11-06 13:00:28 +0900

    feat(Lotto): add validation code
    중복된 수가 입력되었는지 추가적으로 검증하는 코드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,13 +5,16 @@ class Lotto	{
 
 	constructor(numbers) {
 		this.#validate(numbers);
-		WinningNumberArrayValidator.validate(numbers);
+		// WinningNumberArrayValidator.validate(numbers);
 		this.#numbers = numbers;
 	}
 
 	#validate(numbers) {
 		if (numbers.length !== 6) {
 			throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+		}
+		if (numbers.length !== (new Set(numbers)).size) {
+			throw new Error("[ERROR] 중복되지 않은 6개의 숫자를 입력해야 합니다.");
 		}
 	}
 
