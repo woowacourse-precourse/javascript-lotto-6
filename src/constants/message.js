c5b9e87aa0323404d8aa82c0ec81cabf9f452979
이호연 <ho991217@kakao.com>
2023-11-06 13:49:31 +0900

    refactor: 콘솔 프롬프트 파일명 변경

--- src/constants/message.js
+++
@@ -1,5 +0,0 @@
-export const COST_PROMPT = '구입금액을 입력해 주세요.\n';
-
-export default {
-  COST_PROMPT,
-};
---
+++ src/constants/prompt.js
@@ -0,0 +1,7 @@
+export const BUY_COST = '구입금액을 입력해 주세요.\n';
+
+const PROMPT = {
+  BUY_COST,
+};
+
+export default PROMPT;
