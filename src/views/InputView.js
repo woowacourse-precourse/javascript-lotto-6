a99fcc104e9b16eb2f1d798df750482d1543a78f
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 05:07:33 +0900

    :recycle: refactor: 보너스 번호 입력시 윗줄바꿈 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -10,7 +10,7 @@ const InputView = {
   },
 
   async getBonusNumber() {
-    return Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    return Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
   },
 };
 
