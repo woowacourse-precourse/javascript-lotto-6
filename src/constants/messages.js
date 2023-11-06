07339e91ad9e1a3e128122633a994fe10d110a72
ohgus <anakin1108@naver.com>
2023-11-06 17:31:51 +0900

    chore: 메세지 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,7 +3,7 @@ export const MESSAGES = {
   YOU_BOUGHT: "개를 구매했습니다.",
   INPUT_WINNING_NUMBERS: "당첨 번호를 입력해 주세요.",
   INPUT_BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
-  WINNING_STASTISTICS: `당첨 통계${"\n"}---`,
+  WINNING_STASTISTICS: "당첨 통계",
   THREE_COINCIDE: "3개 일치 (5,000원) - ",
   FOUR_COINCIDE: "4개 일치 (50,000원) - ",
   FIVE_COINCIDE: "5개 일치 (1,5000,000원) - ",
