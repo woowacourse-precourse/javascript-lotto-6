ff7c7b730fcb4a6f2ecc0ed07f0bf8bead33608e
Dobbymin <gmin0701@knu.ac.kr>
2023-11-06 11:23:10 +0900

    ✨ feat(getMaxLottoNumber): 금액에 따른  로또 구입

---
+++ src/components/getMaxLottoNumber.js
@@ -0,0 +1,11 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import money from './getNumber.js';
+
+export default async function getMaxLottoNumber(money) {
+    let count = money / 1000;
+    console.log(count);
+}
+
+console.log(money);
+
+getMaxLottoNumber(money);
