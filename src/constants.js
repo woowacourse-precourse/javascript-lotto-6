6519899c044d741199cfbb65884e7f09aa97e952
JeongSabibi <jsbb528@naver.com>
2023-11-07 02:23:57 +0900

    feat: 상수 처리

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+export const WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+export const BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+export const WINNING_STATISTICS = "당첨 통계";
+export const MIN_NUMBER = 1;
+export const MAX_NUMBER = 45;
+export const COUNT_NUMBER = 6;
+export const WINNING_INFOS = {
+  3: "3개 일치 (5,000원) - ",
+  4: "4개 일치 (50,000원) - ",
+  5: "5개 일치 (1,500,000원) - ",
+  "5+1": "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  6: "6개 일치 (2,000,000,000원) - "
+};
