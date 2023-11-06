bc41a057cedea7fbc334a7a43418975b674d7ad2
NSRBSG <nsrbsg@gmail.com>
2023-11-06 22:22:22 +0900

    feat(GameUtil): 에러 발생시 계속해서 물어보는 비동기 함수 추가

--- src/GameUtils.js
+++ src/GameUtils.js
@@ -5,6 +5,15 @@ class GameUtils {
     static getUniqueNumbersInRange(min, max, count) {
         return Random.pickUniqueNumbersInRange(min, max, count);
     }
+
+    static async asyncRetryUntilValidInput(func) {
+        try{
+            return await func();
+        } catch(error) {
+            console.error(error.message);
+            return await this.asyncRetryUntilValidInput(func);
+        }
+    }
 }
 
 export default GameUtils;
