2d3968fefaeb1ba5dacdfd58ddeed24b6626f32a
pakxe <pigkill40@naver.com>
2023-11-06 16:12:33 +0900

    feat: 로또 넘버 각각의 에러가 발생했을 때를 위한 에러 클래스 작성

---
+++ src/error/LottoNumberError.js
@@ -0,0 +1,9 @@
+import CustomError from './CustomError.js';
+
+class LottoNumberError extends CustomError {
+  // constructor(message) {
+  //   super(message);
+  // }
+}
+
+export default LottoNumberError;
