4cc7b299fd8be60d7f26de1503dfb7b5ef402ac7
yoovin <soo22839@gmail.com>
2023-11-06 23:21:08 +0900

    test: 로또 번호 받아오는 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -20,4 +20,14 @@ describe('로또 클래스 테스트', () => {
             new Lotto([1, 2, 3, 4, 5, 46]);
         }).toThrow('[ERROR]');
     });
+
+    test('로또 번호를 받아올 수 있다.', () => {
+        const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+        expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    test('로또 번호 배열의 내용은 오름차순으로 정렬되어 있다.', () => {
+        const lotto = new Lotto([6, 5, 4, 3, 2, 1]);
+        expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
+    });
 });
