2a841cfb8a1627d16101f7c6ba021e06d47cb550
unknown <pih9134@naver.com>
2023-11-07 02:54:25 +0900

    refactor: Validation Error메시지 단순화

--- src/Validation.js
+++ src/Validation.js
@@ -1,46 +1,21 @@
 import { ERROR_MESSAGE } from './constants.js';
 
 class Validation {
-  static cleanAndValidateInput({ input, errorMessage, checkRange = false }) {
-    const cleanedInput = Validation.cleanInput(input);
-    const intValue = Number(cleanedInput);
-
-    if (
-      !cleanedInput ||
-      !Validation.isSafeInteger(intValue) ||
-      (checkRange && !Validation.isInRange(intValue))
-    ) {
-      throw new Error(errorMessage);
-    }
-
-    return intValue;
-  }
-
-  static validateIntegerArray(numbers, errorMessage, checkRange = true) {
-    const cleanedNumbers = Validation.cleanInput(numbers);
-    const parsedNumbers = cleanedNumbers
-      .split(',')
-      .map((num) => Validation.cleanAndValidateInput({ input: num, errorMessage, checkRange }));
-
-    return parsedNumbers;
-  }
-
   static validatePurchaseAmount(purchaseAmount) {
-    if (purchaseAmount % 1000 !== 0 || purchaseAmount < 1000) {
-      Validation.cleanAndValidateInput({
-        input: purchaseAmount,
-        errorMessage: ERROR_MESSAGE.invalidPusrchaseAmount,
-        checkRange: true,
-      });
+    if (Number(purchaseAmount) % 1000 !== 0 || Number(purchaseAmount) < 1000) {
+      throw new Error(ERROR_MESSAGE.invalidPusrchaseAmount);
     }
 
     return purchaseAmount;
   }
 
   static validateWinningNumbers(winningNumbers) {
-    const numbers = Validation.validateIntegerArray(winningNumbers, ERROR_MESSAGE.duplicationWinningNumbers);
+    const numbers = winningNumbers.split(',').map((num) => Number(num.trim()));
 
-    if (numbers.length !== 6 || new Set(numbers).size !== 6) {
+    if (
+      new Set(numbers).size !== 6 ||
+      numbers.some((num) => !Validation.isSafeInteger(num) || !Validation.isInRange(num))
+    ) {
       throw new Error(ERROR_MESSAGE.duplicationWinningNumbers);
     }
 
@@ -48,12 +23,14 @@ class Validation {
   }
 
   static validateBonusNumber(bonusNumber, winningNumbers) {
-    const cleanedBonusNumber = Validation.cleanAndValidateInput({
-      input: bonusNumber,
-      errorMessage: ERROR_MESSAGE.invalidBonusNumber,
-      checkRange: true,
-    });
-
+    const cleanedBonusNumber = Number(bonusNumber);
+    if (
+      !cleanedBonusNumber ||
+      !Validation.isSafeInteger(cleanedBonusNumber) ||
+      !Validation.isInRange(cleanedBonusNumber)
+    ) {
+      throw new Error(ERROR_MESSAGE.invalidBonusNumber);
+    }
     if (winningNumbers.includes(cleanedBonusNumber)) {
       throw new Error(ERROR_MESSAGE.duplicationBonusNumber);
     }
@@ -61,16 +38,12 @@ class Validation {
     return cleanedBonusNumber;
   }
 
-  static cleanInput(input) {
-    return input.trim();
-  }
-
   static isSafeInteger(value) {
     return Number.isSafeInteger(Number(value));
   }
 
-  static isInRange(value, minValue = 1, maxValue = 45) {
-    return value >= minValue && value <= maxValue;
+  static isInRange(value) {
+    return value >= 1 && value <= 45;
   }
 }
 
