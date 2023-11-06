66f1bd7a92920e4649b247e29bc0f5ddbb91fe19
anttiey <hanuuny@gmail.com>
2023-11-07 00:13:07 +0900

    refactor: 사용자 입력 메시지 상수로 추출
    - 사용자 입력에 대한 안내 메시지 INPUT 상수 추가

--- src/Input.js
+++ src/Input.js
@@ -1,27 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
+import { INPUT } from './constants/Messages.js';
 
 class Input {
   async userMoney() {
-    const userMoneyInput = await Console.readLineAsync(
-      '구입 금액을 입력해 주세요.\n'
-    );
-
+    const userMoneyInput = await Console.readLineAsync(INPUT.userMoney);
     return userMoneyInput.trim();
   }
 
   async winningNumbers() {
     const winningNumbersInput = await Console.readLineAsync(
-      '당첨 번호를 입력해 주세요.\n'
+      INPUT.winningNumbers
     );
-
     return winningNumbersInput.trim();
   }
 
   async bonusNumbers() {
-    const bonusNumbersInput = await Console.readLineAsync(
-      '보너스 번호를 입력해 주세요.\n'
-    );
-
+    const bonusNumbersInput = await Console.readLineAsync(INPUT.bonusNumber);
     return bonusNumbersInput.trim();
   }
 }
--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,3 +1,9 @@
+export const INPUT = {
+  userMoney: '구입 금액을 입력해 주세요.\n',
+  winningNumbers: '당첨 번호를 입력해 주세요.\n',
+  bonusNumber: '보너스 번호를 입력해 주세요.\n',
+};
+
 export const INPUT_ERROR = {
   null: '[ERROR] 입력이 없습니다.',
   type: '[ERROR] 입력이 숫자가 아닙니다.',
