bdcacbd0d6fb93cff6a4f9d519494c3405ba88d3
abi-hong <flffjakfwm20@gmail.com>
2023-11-06 23:10:32 +0900

    feat(OutputView): 로또 게임 출력 기능 구현

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,19 +1,26 @@
 import { Console } from "@woowacourse/mission-utils";
 
-const Output = {
+export const OutputView = {
   count(number) {
-    Console.print(`${number}개를 구매했습니다.`);
+    Console.print(`\n${number}개를 구매했습니다.`);
   },
-  lotto(numbers) {
-    Console.print(`${numbers}`);
+  makeLottos(numbers) {
+    Console.print(`[${numbers.join(', ')}]`);
   },
-  history(number) {
-    Console.print(`3개 일치 (5,000원) - ${number}개`);
-    Console.print(`4개 일치 (50,000원) - ${number}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${number}개`);
-    Console.print(`6개 일치 (2,000,000,000원) - ${number}개`);
+  result(ranking) {
+    Console.print(`
+당첨 통계
+---
+3개 일치 (5,000원) - ${ranking[4]}개
+4개 일치 (50,000원) - ${ranking[3]}개
+5개 일치 (1,500,000원) - ${ranking[2]}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${ranking[1]}개
+6개 일치 (2,000,000,000원) - ${ranking[0]}개`);
   },
   profit(number) {
-    Console.print(`총 수익률은 ${number}%입니다.`)
-  }
+    Console.print(`총 수익률은 ${number}%입니다.`);
+  },
+  err(error) {
+    Console.print(error);
+  },
 }
