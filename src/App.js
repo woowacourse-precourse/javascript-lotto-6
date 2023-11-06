9192f40c8d62bb1330a62689773f9a01881119f4
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-11-07 00:20:43 +0900

    feat(App, Result) : 당첨통계 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // App.js
 import Input from "./Input.js";
+import Result from "./Result.js";
 
 class App {
   async play() {
     const purchaseAmount = await Input.inputMoney();
     const lottoNumbers = await Input.inputNumber();
     const bonusNumbers = await Input.bonusNumber();
+    Result.Result();
   }
 }
 
---
+++ src/Result.js
@@ -0,0 +1,10 @@
+//Result.js
+import { Console } from "@woowacourse/mission-utils";
+class Result {
+  static async Result() {
+    Console.print("당첨 통계");
+    Console.print("---");
+  }
+}
+
+export default Result;
