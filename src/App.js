a24cbb146247e649442816cfe58ecb9e6ee0cf2f
gillyongs <khs20010327@gmail.com>
2023-11-07 01:27:53 +0900

    feat: 당첨번호와 보너스번호 입력 받기

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App {
     let purchaseAmount;
     while (1) {
       try {
-        purchaseAmount = parseInt( await Console.readLineAsync("구입금액을 입력해 주세요."));
+        purchaseAmount = parseInt( await Console.readLineAsync("구입금액을 입력해 주세요.\n"));
         if (purchaseAmount % 1000 !== 0 || isNaN(purchaseAmount)) {
           throw new Error("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
         }
@@ -24,6 +24,9 @@ class App {
       Console.print(ticket.getNumbers().sort((a, b) => a - b));
     });
 
+    const winningNumbers = await this.inputWinningNumbers();
+    const bonusNumber = await this.inputBonusNumber();
+
   }
 
   generateLottoTickets(count) {
@@ -34,6 +37,50 @@ class App {
       lottoTickets.push(lottoTicket);
     }
     return lottoTickets;
+  }
+
+  async inputWinningNumbers() {
+    let winningNumbers;
+    let winningNumbersMap;
+    while (true) {
+      try {
+        winningNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n")
+        winningNumbersMap = winningNumbers.split(',').map(Number);
+        if (!this.validateNumbers(winningNumbersMap)) {
+          throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (winningNumbersMap.length !== 6) {
+          throw new Error("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return winningNumbersMap;
+  }
+
+  async  inputBonusNumber() {
+    let bonusNumber;
+    while (true) {
+      try {
+        bonusNumber = parseInt(await Console.readLineAsync("보너스 번호를 입력해 주세요.\n"));
+        if (!this.validateNumber(bonusNumber)) {
+          throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return bonusNumber;
+  }
+  validateNumbers(numbers) {
+    return numbers.every(this.validateNumber);
+  }
+  
+  validateNumber(number) {
+    return number >= 1 && number <= 45;
   }
 }
 
