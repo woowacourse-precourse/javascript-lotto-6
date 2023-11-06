fa44f2d29171d8324f1a5cec50649c34edf092e3
Dawon00 <dawon000203@gmail.com>
2023-11-07 01:44:52 +0900

    Feat: 구입금액 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+    async play() {}
+
+    async inputPurchaseAmount() {
+        const purchaseAmount = await Console.readLineAsync(
+            "구입금액을 입력해 주세요."
+        );
+        if (purchaseAmount % 1000 !== 0 || purchaseAmount < 1000) {
+            throw new Error(
+                "[ERROR] 구입금액은 1000원 이상이어야 하며, 1000원 단위로 입력해야 합니다."
+            );
+        }
+        return purchaseAmount;
+    }
 }
 
 export default App;
