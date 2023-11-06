c56f2fffac3de0177887c379d24608e4c8e489ea
wonkyum-kim <dodo12120@naver.com>
2023-11-06 17:54:12 +0900

    feat: 구입할 금액을 입력 받기

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const MONEY_INPUT_MESSAGE = '구입금액을 입력해 주세요.\n';
+
+export const TICKET_COUNT_MESSAGE = '개를 구매했습니다.';
+
+export const WINNING_NUMBER_INPUT_MESSAGE = '\n당첨 번호를 입력해 주세요.\n';
+
+export const BONUS_NUMBER_INPUT_MESSAGE = '\n보너스 번호를 입력해 주세요.\n';
+
+export const STATISTICS_INTRO = '\n당첨 통계\n---';
+
+export const TICKET_PRICE = 1000;
---
+++ src/io/Input.service.js
@@ -0,0 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+import {
+  BONUS_NUMBER_INPUT_MESSAGE,
+  MONEY_INPUT_MESSAGE,
+  WINNING_NUMBER_INPUT_MESSAGE,
+} from '../constants.js';
+
+export default class InputService {
+  async readAmount() {
+    return Console.readLineAsync(MONEY_INPUT_MESSAGE);
+  }
+}
