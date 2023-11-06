6e672e8c0c9df019b5b683a04b8b355214bc5bf3
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-11-06 17:26:57 +0900

    feat: 입력 처리 모듈 추가

---
+++ src/lib/View/InputView.js
@@ -0,0 +1,37 @@
+import InputValidator from "../Validator/InputValidator.js";
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "../Constants.js";
+
+class InputView {
+  static async ticketMoney() {
+    return InputView.#handleInput(
+      async () => await Console.readLineAsync(INPUT_MESSAGE.TICKET_MONEY),
+      InputValidator.ticketMoney,
+      Number,
+    );
+  }
+
+  static async winNumbers() {
+    return InputView.#handleInput(
+      async () => await Console.readLineAsync(INPUT_MESSAGE.WIN_NUMBERS),
+      InputValidator.winNumbers,
+      (s) => s.split(",").map((a) => Number(a)),
+    );
+  }
+
+  static async bonusNumber() {
+    return InputView.#handleInput(
+      async () => await Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER),
+      InputValidator.bonusNumber,
+      Number,
+    );
+  }
+
+  static async #handleInput(read, validate, parse = (e) => e) {
+    const response = await read();
+    validate(response);
+    return parse(response);
+  }
+}
+
+export default InputView;
