f8915319fccdd42a979afe3314613114f23337da
Narachan97 <qkrtkf3112@naver.com>
2023-11-06 16:41:01 +0900

    test(): buyLottoByAmount() 테스트 코드 추가
    __tests__/LottoTest.js 테스트 코드 추가
    
    LottoTests.js 추가점
    -buyLottoByAmount() 테스트 코드에 대한 추가
    -금액에 맞게 로또가 발행 및 금액이 알맞지 않을경우 예외처리 테스트 코드 추가
    -로또 번호가 1~45 사이에 있는지 확인 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,8 @@
 import Lotto from "../src/Lotto.js";
+import Functions from "../src/Functions.js";
 
 describe("로또 클래스 테스트", () => {
+  let functions;
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -14,5 +16,32 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+   beforeEach(() => {
+     functions = new Functions();
+   });
+
+   test("금액에 맞게 로또가 발행 및 예외처리 ", ()=> {
+    const validAmount = 5000;
+    const invalidAmout =5500;
+    const validLottoList = functions.buyLottoByAmount(validAmount);
+    expect(validLottoList.length).toBe(validAmount / 1000);
+
+    expect(()=> {
+      functions.buyLottoByAmount(invalidAmout);
+    }).toThrow("[ERROR]")
+   });
+
+   test("로또 번호가 1~45 사이에 있는지 확인",()=> {
+    const amout = 2000;
+    const lottoList = functions.buyLottoByAmount(amout);
+
+    for(const lottoNumbers of lottoList){
+      for(const number of lottoNumbers){
+        expect(number).toBeGreaterThanOrEqual(1);
+        expect(number).toBeLessThanOrEqual(45);
+      }
+    }
+   });
+
+
 });
