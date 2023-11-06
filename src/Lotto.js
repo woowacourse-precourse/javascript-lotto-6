643954cf3c3e9608c93438be071b0e98ea34464a
abi-hong <flffjakfwm20@gmail.com>
2023-11-06 23:08:03 +0900

    feat(Lotto): 로또 당첨 랭킹 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,5 @@
-// 제공된 Lotto 클래스를 활용해 구현해야 한다.
-// numbers의 # prefix를 변경할 수 없다.
-// Lotto에 필드를 추가할 수 없다.
+import { RANKING } from "./utils/Constant.js";
+
 class Lotto {
   #numbers;
 
@@ -10,17 +9,20 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    if (new Set(numbers).size !== 6) throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
   }
 
-  // TODO: 추가 기능 
   getNumbers() {
     return this.#numbers;
   }
 
-  
+  winningLotto(main, bonus) {
+    const hasBonus = this.#numbers.includes(bonus);
+    const matchMain = this.#numbers.filter((number) => main.includes(number)).length;
+    if (matchMain === 5) return RANKING[matchMain](hasBonus);
+    return RANKING[matchMain];
+  }
 }
 
 export default Lotto;
