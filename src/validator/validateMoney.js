db325a89d55dc0e513639f0dc89967e5d8b08e93
pakxe <pigkill40@naver.com>
2023-11-06 22:43:24 +0900

    feat: 돈이 유효 범위 이내인지와 음수인지를 판단하는 로직 추가

--- src/validator/validateMoney.js
+++ src/validator/validateMoney.js
@@ -7,8 +7,15 @@ const isInvalidMoneyPrice = (money) => {
   return money % LOTTO.price !== 0;
 };
 
+const isInvalidRange = (money) => {
+  return money > LOTTO.maxMoney || money < LOTTO.minMoney;
+};
+
 const validateMoney = (money) => {
   if (InputValidator.isNan(money)) throw new MoneyError(ERROR.isNan);
+  if (InputValidator.isNegativeNumber(money))
+    throw new MoneyError(ERROR.negativeMoney);
+  if (isInvalidRange(money)) throw new MoneyError(ERROR.moneyRange);
   if (isInvalidMoneyPrice(money)) throw new MoneyError(ERROR.price);
 };
 
