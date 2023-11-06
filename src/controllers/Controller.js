53d2163fe89f5b04510f676b74a889c41692c987
1eecan <busyx2modernsociety@gmail.com>
2023-11-07 04:12:46 +0900

    feat: Controller.js 작성

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,19 +1,70 @@
+import { Console } from '@woowacourse/mission-utils';
+import Person from '../models/Person.js';
+import LottoMachine from '../models/LottoMachine.js';
+import Lotto from '../Lotto.js';
+import LottoShop from '../models/LottoShop.js';
+
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
 
 class Controller {
-  #newGame;
+  #person;
+
+  #lottos;
+
+  #lottoShop;
 
   // eslint-disable-next-line no-useless-constructor, no-empty-function
   constructor() {}
 
+  async makePerson() {
+    while (true) {
+      try {
+        this.#person = new Person(await InputView.askPurchaseAmount());
+        break;
+      } catch (e) {
+        Console.print(e.message);
+      }
+    }
+  }
+
+  makeLottos() {
+    this.#lottos = Array.from(
+      { length: this.#person.purchasedLottoNumber() },
+      () => new Lotto(LottoMachine.createLotto()).getLotto()
+    );
+    this.#person.purchasedLottoInformation(this.#lottos);
+  }
+
+  async makeLottoShop() {
+    while (true) {
+      try {
+        // 당첨번호 보너스번호 분리필요
+        this.#lottoShop = new LottoShop(
+          await InputView.askWinningNumber(),
+          await InputView.askBonusNumber()
+        );
+        break;
+      } catch (e) {
+        Console.print(e.message);
+      }
+    }
+  }
+
   async run() {
-    await InputView.askPurchaseAmount();
-    OutputView.printPurchaseNumber(1, [[1, 2, 3, 4, 5, 6]]);
-    await InputView.askWinningNumber();
-    await InputView.askBonusNumber();
-    OutputView.printWinningLog([1, 2, 3, 4, 5]);
-    OutputView.printTotalReturn(1000);
+    await this.makePerson();
+    this.makeLottos();
+    OutputView.printPurchaseNumber(
+      this.#person.purchasedLottoNumber(),
+      this.#lottos
+    );
+    await this.makeLottoShop();
+    OutputView.printWinningLog(
+      this.#lottoShop.checkingTotalResult(this.#person.goToLottoShop())
+    );
+    OutputView.printTotalReturn(
+      this.#lottoShop.returnProfitRate(this.#person.getMoney())
+    );
   }
 }
 
