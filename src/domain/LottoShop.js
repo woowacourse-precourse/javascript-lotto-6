6725915df0c1ba093e8d7f16a9bd7c2cfe39ffa4
pakxe <pigkill40@naver.com>
2023-11-06 16:06:05 +0900

    feat: 로또 가게가 로또를 생성해 반환하도록 작성

--- src/domain/LottoShop.js
+++ src/domain/LottoShop.js
@@ -1,21 +1,24 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import LOTTO from '../constant/Lotto.js';
+import Lotto from './Lotto.js';
 
 class LottoShop {
   static buyLottos(money) {
-    const count = money.getMoney() / LOTTO.price;
+    const count = money.get() / LOTTO.price;
     const lottos = new Array(count).fill();
 
     // TODO: 새로운 배열을 만들어 반환하므로 효율성 문제가 있을 수도 있다.
-    return lottos.map(() => this.#generateLottoNumbers());
+    return lottos.map(() => this.#generateLotto());
   }
 
-  static #generateLottoNumbers() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(
+  static #generateLotto() {
+    const lotto = MissionUtils.Random.pickUniqueNumbersInRange(
       LOTTO.minLottoNum,
       LOTTO.maxLottoNum,
       LOTTO.count,
     );
+
+    return new Lotto(lotto);
   }
   // static create(money) {
   //   // 제일복권 또는 스포츠 토토인 경우를 위해 팩토리 메서드
