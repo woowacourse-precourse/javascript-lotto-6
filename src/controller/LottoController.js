baae99fb88585e0906d1b4f1e754ae321bea7d1a
swarvy <swarvy0826@naver.com>
2023-11-07 03:04:15 +0900

    refactor : try-catch를 통해 오류 발생시 컨트롤러에서 재입력을 요청하도록 변경

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,3 +1,4 @@
+import { Console } from '@woowacourse/mission-utils';
 import {
   LOTTO_PRICE,
   PROFIT_ROUND_UP_VALUE,
@@ -8,28 +9,54 @@ import {
   readLottoMainNumbers,
   readLottoSeedMoney,
 } from '../view/InputView.js';
-import { printLottoNumbers, printLottoResults } from '../view/OutputView.js';
+import {
+  printError,
+  printLottoNumbers,
+  printLottoResults,
+} from '../view/OutputView.js';
 
 export default class LottoController {
   #lottoService;
 
   async run() {
-    const lottoSeedMoney = await readLottoSeedMoney();
-    this.#lottoService = new LottoService(lottoSeedMoney);
-    this.#printLottoList();
+    try {
+      const lottoSeedMoney = await readLottoSeedMoney();
+
+      this.#lottoService = new LottoService(lottoSeedMoney);
+      await this.#printLottoList();
+    } catch (e) {
+      printError(e.message);
+      await this.run();
+    }
   }
 
   async #printLottoList() {
     const createdLottoList = this.#lottoService.getLottoList();
+
     printLottoNumbers(createdLottoList);
     await this.#readLottoNumbers();
   }
 
   async #readLottoNumbers() {
-    const lottoNumbers = await readLottoMainNumbers();
-    const lottoBonusNumber = await readLottoBonusNumber(lottoNumbers);
+    try {
+      const lottoNumbers = await readLottoMainNumbers();
+      await this.#readLottoBonusNumber(lottoNumbers);
+    } catch (e) {
+      printError(e.message);
+      await this.#readLottoNumbers();
+    }
+  }
 
-    this.#compare(lottoNumbers, lottoBonusNumber);
+  async #readLottoBonusNumber(lottoNumbers) {
+    try {
+      const lottoBonusNumber = await readLottoBonusNumber(lottoNumbers);
+
+      this.#compare(lottoNumbers, lottoBonusNumber);
+    } catch (e) {
+      printError(e.message);
+
+      await this.#readLottoBonusNumber(lottoNumbers);
+    }
   }
 
   #compare(lottoNumbers, lottoBonusNumber) {
@@ -38,8 +65,8 @@ export default class LottoController {
       lottoBonusNumber
     );
     const profit = (
-      lottoResults.prizeTotal /
-      (lottoAmount * LOTTO_PRICE)
+      (lottoResults.prizeTotal / (lottoAmount * LOTTO_PRICE)) *
+      100
     ).toFixed(PROFIT_ROUND_UP_VALUE);
 
     printLottoResults(lottoResults.prizeAmount, profit);
