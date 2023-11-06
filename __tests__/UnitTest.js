8a0f2b5d971eb236f8ecdc547b391c488a01f182
yoovin <soo22839@gmail.com>
2023-11-06 23:55:17 +0900

    test: 로또 수익률 확인 테스트 작성

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -50,4 +50,14 @@ describe('기능 테스트', () => {
             app.getLottoRank([1, 2, 3, 4, 5, 6], [1, 2, 3, 8, 9, 10], 45)
         ).toBe(5);
     });
+
+    test('로또의 수익률을 반환한다.', () => {
+        const lottoRanks = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 1 };
+        expect(app.getLottoRate(10000, lottoRanks)).toBe(50);
+    });
+
+    test('로또의 수익률은 소숫점 둘째 자리까지만 표시한다.', () => {
+        const lottoRanks = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 1 };
+        expect(app.getLottoRate(8000, lottoRanks)).toBe(62.5);
+    });
 });
