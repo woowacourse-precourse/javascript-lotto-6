feb94315762376ae5d0fc9c090475ef41ad4d261
신준혁 <oridori2705@naver.com>
2023-11-06 22:47:29 +0900

    commit: 로또 구입 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  async play() {}
+import { Console, Random } from "@woowacourse/mission-utils";
+
+function App() {
+  this.play = async () => {
+    const inputAmount = await InputPurchaseAmount();
+  };
+  const InputPurchaseAmount = async () => {
+    const inputAmount = await Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+    return inputAmount;
+  };
 }
 
 export default App;
