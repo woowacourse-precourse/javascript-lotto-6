ecf7463288b0c15f22f5660b357c1d7d97fac71e
thewronghand <penfreak77@gmail.com>
2023-11-06 15:37:00 +0900

    feat: 컨트롤러 및 입력값 핸들링 메서드 구현
    View의 메서드를 이용해 입력받은 값을 핸들링하는 메서드 / 에러 발생 시 재귀 호출 메서드를 구현한다.

---
+++ src/controllers/LottoGameController.js
@@ -0,0 +1,54 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import View from '../views/View.js';
+import LottoPublisher from '../models/LottoPublisher.js';
+import LottoService from '../models/LottoService.js';
+
+class LottoGameController {
+  #view = new View();
+
+  #lottoPublisher = new LottoPublisher();
+
+  #lottoService = new LottoService();
+
+  async start() {
+    await this.setMoneyAmountFromInput();
+    await this.setWinningNumbersFromInput();
+    await this.setBonusNumbersFromInput();
+  }
+
+  async #retryOnFailure(func) {
+    try {
+      return await func();
+    } catch (error) {
+      this.#view.printError(error);
+      return this.#retryOnFailure(func);
+    }
+  }
+
+  async setMoneyAmountFromInput() {
+    await this.#retryOnFailure(async () => {
+      const moneyAmount = await this.#view.readMoneyAmount();
+      this.#lottoPublisher.setMoneyAmount(moneyAmount);
+      Console.print('잘 됐음');
+    });
+  }
+
+  async setWinningNumbersFromInput() {
+    await this.#retryOnFailure(async () => {
+      const winningNumbers = await this.#view.readWinningNumbers();
+      this.#lottoService.setWinningNumbers(winningNumbers);
+      Console.print(this.#lottoService.getWinningNumbers());
+    });
+  }
+
+  async setBonusNumbersFromInput() {
+    await this.#retryOnFailure(async () => {
+      const bonusNumber = await this.#view.readBonusNumber();
+      this.#lottoService.setBonusNumber(bonusNumber);
+      Console.print(this.#lottoService.getBonusNumber());
+    });
+  }
+}
+
+export default LottoGameController;
