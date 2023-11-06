2b4c1bfc9e15704578de973f88659399b3188611
aaminha <dks4857@gmail.com>
2023-11-06 17:04:08 +0900

    feat: 입출력 관련 상수 생성

---
+++ src/constants/Mesaage.js
@@ -0,0 +1,23 @@
+import WINNING_AMOUNT_STRING from './WinningAmount';
+
+export const MESSAGE_GET = Object.freeze({
+  PURCHASE_AMOUNT: '구입금액을 입력해 주세요. \n',
+  WINNING_NUMBER: '당첨 번호를 입력해 주세요. \n',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요. \n',
+});
+
+export const MESSAGE_PRINT = Object.freeze({
+  LOTTO_AMOUNT: '개를 구매했습니다. \n',
+  STATISTICS: '당첨 통계 \n--- \n',
+  COUNT_UNIT: '개',
+  TOTAL_RATE_HEAD: '총 수익률은 ',
+  TOTAL_RATE_TAIL: '%입니다. \n',
+});
+
+export const MESSAGE_RESULT_STATISTICS = Object.freeze({
+  FIRST: `6개 일치 (${WINNING_AMOUNT_STRING.FIRST}원) - `,
+  SECOND: `5개 일치, 보너스 볼 일치 (${WINNING_AMOUNT_STRING.SECOND}원) - `,
+  THIRD: `5개 일치 (${WINNING_AMOUNT_STRING.THIRD}원) - `,
+  FOURTH: `4개 일치 (${WINNING_AMOUNT_STRING.FOURTH}원) - `,
+  FIFTH: `3개 일치 (${WINNING_AMOUNT_STRING.FIFTH}원) - `,
+});
