5aa7b7826c15639b669e83fab13face7ad1de98e
BK <3031011@naver.com>
2023-11-06 11:36:31 +0900

    fix: 사용자 입력 변환하지 않은 부분 수정
    콤마 기준으로 split 하지 않고 Lotto 클래스로 전달했던 부분 스플릿 해서 전달하도록 수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { PROPT_MESSAGE } from './constant/lottoConstants';
 import LottoMachine from './classes/LottoMachine';
 import LottoViewer from './classes/LottoViewer';
 import Lotto from './classes/Lotto';
-import LottoResultChecker from './classes/LottoResultChecker';
 
 class App {
   async play() {
@@ -12,7 +11,7 @@ class App {
     LottoViewer.purchasedLottos(lottoMachine.getLotto());
     const lottoWinningNumber = await Console.readLineAsync(PROPT_MESSAGE.INPUT_WINNING_NUMBER);
     const lottoBonusNumber = await Console.readLineAsync(PROPT_MESSAGE.INPUT_BONUS_NUMBER);
-    const lotto = new Lotto(lottoWinningNumber, lottoBonusNumber);
+    const lotto = new Lotto(lottoWinningNumber.split(','), lottoBonusNumber);
   }
 }
 
