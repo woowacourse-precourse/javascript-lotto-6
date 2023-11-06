2cf1ba6682a17f7c7a70982f070078e916f1b315
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-06 14:22:51 +0900

    :recycle: refactor: 에러메시지 상수처리 및 파일확장자 수정

--- __tests__/PurchaseAmountTest.js
+++ __tests__/PurchaseAmountTest.js
@@ -1,12 +1,11 @@
-import PurchaseAmount from '../src/domain/PurchaseAmount';
+import PurchaseAmount from '../src/domain/PurchaseAmount.js';
+import { ERROR_MESSAGE } from '../src/constants/messages.js';
 
 describe('PurchaseAmount', () => {
   test.each([2500, '', '구입금액'])(
     '구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.',
     ({ invalidAmount }) => {
-      expect(() => new PurchaseAmount(invalidAmount)).toThrowError(
-        '[ERROR] 구입 금액은 1,000원으로 나누어 떨어져야 합니다.'
-      );
+      expect(() => new PurchaseAmount(invalidAmount)).toThrowError(ERROR_MESSAGE.invalidAmount);
     }
   );
 
