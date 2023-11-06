6952751906212d188acd80227de1dd8fc8c8aab8
kwani6684 <jack0910@naver.com>
2023-11-06 10:30:08 +0900

    ✨feat: add calculate lottoCount

--- src/Money.js
+++ src/Money.js
@@ -2,18 +2,21 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class Money {
   payedMoney;
-  
+  lottoCount;
+  LOTTO_PRICE = 1000;
 
   constructor() {
     this.init();
   }
   init = async () => {
     await this.getPayedMoney();
-    
+    this.calLottoCount();
   };
   getPayedMoney = async () => {
     this.payedMoney = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
   };
-  
+  calLottoCount = () => {
+    this.lottoCount = this.payedMoney / this.LOTTO_PRICE;
+  };
 }
 export default Money;
