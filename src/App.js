c0d1e3082822b9837c1b57c1945504b9d0c2ff04
HarenKei <seungmin1266@gmail.com>
2023-11-06 23:32:43 +0900

    Feat: 추가 입력 기능 함수 구현
    사용자에게 금액 입력을 받는 함수 getInputPrice, 유효성 검사 함수 checkInputPrice를 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+
 class App {
-  async play() {}
+  #USER_PRICE;
+  #USER_LOTTOS;
+
+  async getInputPrice() {
+    const USER_INPUT =
+      await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+
+    try {
+      this.checkInputPrice(parseInt(USER_INPUT, 10));
+      this.#USER_PRICE = parseInt(USER_INPUT, 10);
+    } catch (e) {
+      Console.print(e);
+      this.getInputPrice();
+    }
+  }
+
+  checkInputPrice(priceInput) {
+    if (Number.isNaN(priceInput)) {
+      throw new Error('[ERROR] : 숫자를 입력해주세요.');
+    }
+
+    if (priceInput % 1000 !== 0) {
+      throw new Error('[ERROR] : 금액은 1,000원 단위로 입력해주세요.');
+    }
+  }
+
+  async play() {
+    await this.getInputPrice();
+    Console.print(this.#USER_PRICE);
+  }
 }
 
 export default App;
