abad8876bae44986582c052c3aa6e9547bc1b7af
anttiey <hanuuny@gmail.com>
2023-11-06 09:30:37 +0900

    feat: 로또 구입 금액 입력 기능 추가
    - 사용자에게 로또 구입 금액을 입력 받는 getUserPurchaseMoney 함수 추가
    - 입력의 유효성을 검사하는 validateUserPurchaseMoney 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  validateUserPurchaseMoney(input) {
+    if (input === '') {
+      throw new Error('[ERROR] 입력이 없습니다.');
+    }
+
+    const number = Number(input);
+
+    if (Number.isNaN(number)) {
+      throw new Error('[ERROR] 입력이 숫자가 아닙니다.');
+    }
+
+    if (number <= 0) {
+      throw new Error('[ERROR] 입력이 0 이하입니다');
+    }
+
+    if (number % 1000 !== 0) {
+      throw new Error('[ERROR] 입력이 1,000원 단위가 아닙니다.');
+    }
+  }
+
+  async getUserPurchaseMoney() {
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(
+          '구입 금액을 입력해 주세요.\n'
+        );
+        this.validateUserPurchaseMoney(input.trim());
+        return Number(input);
+      } catch (err) {
+        Console.print(err.message);
+      }
+    }
+  }
+
+  async play() {
+    const userMoney = await this.getUserPurchaseMoney();
+  }
 }
 
 export default App;
