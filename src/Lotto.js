f268512bc42699ee6a3b966c2e98dac637ed5183
hojkim77 <rlaghwnd4534@gmail.com>
2023-11-06 22:24:36 +0900

    [feat] lotto 클래스에 로또 결과 관련된 생성자 및 메소드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,32 @@
+import Generator from './Generator.js';
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers, bonus) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    this.bonus = bonus;
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  getResult(userNumbers) {
+    const lottoCount = Generator.lottoCountGenerator(
+      userNumbers,
+      this.#numbers
+    );
+
+    const isBonus = null;
+    if (lottoCount === 5)
+      isBonus = Generator.isBonusGenerator(userNumbers, this.bonus);
+
+    return { lottoCount, isBonus };
+  }
 }
 
 export default Lotto;
