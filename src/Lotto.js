dc18f8127f84e6a82af8f2397c440efd70b9f5c9
GimGayeon <kky1373@naver.com>
2023-11-06 11:23:55 +0900

    feat:Lotto 클래스에 당첨 번호 유효성 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+    if (Number.isNaN(Number(numbers))) {
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
