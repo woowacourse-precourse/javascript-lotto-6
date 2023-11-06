8c3ced4ec191d06aea95bd9e0ae214f4916b94a7
hzzzi <hji2004@naver.com>
2023-11-07 01:28:28 +0900

    feat: 플레이어에게 당첨 번호, 보너스 번호 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -15,6 +15,17 @@ function showAmountOfTickets(amountOfTickets) {
   Console.print(`${amountOfTickets}개를 구매했습니다.`);
 }
 
+async function getLottoNumbers() {
+  const lottoNumbers = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.');
+  const lottoNumbersArr = lottoNumbers.split(',').map(Number);
+  return lottoNumbersArr;
+}
+
+async function getBounusNumber() {
+  const bonusNumber = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.');
+  return bonusNumber;
+}
+
 class App {
   async play() {
     const purchasePrice = await getPurchasePrice();
@@ -28,6 +39,8 @@ class App {
       ticket.showNumbers();
       ticketArr.push(ticket);
     }
+    const winningNumbers = await getLottoNumbers();
+    const bonusNumber = await getBounusNumber();
   }
 }
 
