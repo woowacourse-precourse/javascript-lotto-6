0cffed28def41da7a0d8a196314136070249f2de
Sohye <saeyan2477@gmail.com>
2023-11-06 23:47:53 +0900

    feat: Constants 추가(입력값, 로또 구입금액)
    - 입력값 변수
    - 로또 구입금액 변수

--- src/Constants.js
+++ src/Constants.js
@@ -19,9 +19,20 @@ const GAME_MESSAGES = Object.freeze({
       NO_RANGE: (min, max) =>
         "로또 번호는 ${min}부터 ${max} 사이의 숫자여야 합니다.",
     },
+    INPUT:{
+      NO_NUMBER: "입력값은 숫자여야 합니다.", 
+      NO_POSITIVE: "입력값은 양수여야 합니다.",
+      NO_INTEGER: "입력값은 정수여야 합니다.",
+    },
     BUY: {
+      NO_NUMBER: "구매금액은 숫자여야 합니다.",
+      NO_POSITIVE:"구매금액은 양수여야 합니다.",
       BUY_UNIT: (unit) => "구매 금액은 ${unit}원 단위로 가능합니다.",
     },
+  },
+  BUY: {
+    START: "구입금액을 입력해주세요.",
+    RESULT: (count) => `${count}개를 구매했습니다.`,
   },
 });
 
