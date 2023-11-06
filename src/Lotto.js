a0ee8404e61dfcf3c796af6915bcc75fc4d7133f
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-11-07 03:46:14 +0900

    :sparkles: feat : LuckyNumber에 잘못 입력을 받았을 경우 다시 시도하는 로직을 추가한다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,7 +1,3 @@
-import { numberCheck, numbersCheck } from './validation.js';
-import { MESSAGE } from './constants/messages.js';
-import { LOTTO_NUMBER } from './constants/policy.js';
-
 class Lotto {
   #numbers;
 
@@ -14,16 +10,6 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
-    numbers.forEach((number) => {
-      numbersCheck.duplicate(numbers, number, MESSAGE.ERROR.duplicate);
-      numberCheck.number(number, MESSAGE.ERROR.number);
-      numberCheck.rangeCheck(
-        number,
-        LOTTO_NUMBER.startNumber,
-        LOTTO_NUMBER.endNumber,
-        MESSAGE.ERROR.lottoRange,
-      );
-    });
   }
 
   getNumber() {
--- src/LuckyNumber.js
+++ src/LuckyNumber.js
@@ -2,17 +2,31 @@ import Lotto from './Lotto.js';
 import { MESSAGE } from './constants/messages.js';
 import { numberCheck, numbersCheck } from './validation.js';
 import { LOTTO_NUMBER } from './constants/policy.js';
+import { Console } from '@woowacourse/mission-utils';
 
 class LuckyNumber extends Lotto {
   #bonusNumber = 0;
 
   constructor(luckyNumber, bonusNumber) {
     super(luckyNumber);
-    this.#validation(luckyNumber, bonusNumber);
     this.#bonusNumber = bonusNumber;
   }
 
-  #validation(luckyNumber, bonusNumber) {
+  #luckyNumberValidation(luckyNumber) {
+    numbersCheck.length(LOTTO_NUMBER.lottoLength, MESSAGE.ERROR.length);
+    luckyNumber.forEach((number) => {
+      numbersCheck.duplicate(luckyNumber, number, MESSAGE.ERROR.duplicate);
+      numberCheck.number(number, MESSAGE.ERROR.number);
+      numberCheck.rangeCheck(
+        number,
+        LOTTO_NUMBER.startNumber,
+        LOTTO_NUMBER.endNumber,
+        MESSAGE.ERROR.lottoRange,
+      );
+    });
+  }
+
+  #bonusNumberValidation(luckyNumber, bonusNumber) {
     numberCheck.rangeCheck(
       bonusNumber,
       LOTTO_NUMBER.startNumber,
@@ -28,6 +42,34 @@ class LuckyNumber extends Lotto {
 
   getBonusNumber() {
     return this.#bonusNumber;
+  }
+
+  static async readLuckyNumber() {
+    try {
+      const luckyNumber = await Console.readLineAsync(
+        MESSAGE.LOTTO.setLuckyNumber,
+      )
+        .split(',')
+        .map((v) => Number(v));
+      this.#luckyNumberValidation(luckyNumber);
+      return luckyNumber;
+    } catch (err) {
+      Console.print(err.message);
+      await LuckyNumber.readLuckyNumber();
+    }
+  }
+
+  static async readBonusNumber(luckyNumber) {
+    try {
+      const bonusNumber = Number(
+        await Console.readLineAsync(MESSAGE.LOTTO.setBonusNumber),
+      );
+      this.#bonusNumberValidation(luckyNumber, bonusNumber);
+      return bonusNumber;
+    } catch (err) {
+      Console.print(err.message);
+      await LuckyNumber.readBonusNumber(luckyNumber);
+    }
   }
 }
 
