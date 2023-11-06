1459df2f9150539da06cc7a1bfd97bb856fe74b4
heonq <gkagjsrb@naver.com>
2023-11-06 14:30:17 +0900

    test(User): 생성된 로또를 출력 형식에 맞게 문자열로 반환하는지 검사하는 테스트 작성 - 입력 : [2,3,4,12,25,38] 출력 : '[2, 3, 4, 12, 25, 38]'

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -14,6 +14,7 @@ describe('User 클래스 테스트', () => {
     const user = new User(1000);
     expect(user.lottos.length).toBe(1);
   });
+
   test('생성된 로또가 오름차순으로 정렬되는지 테스트', () => {
     const randoms = [[4, 12, 2, 25, 38, 3]];
     const expected = [[2, 3, 4, 12, 25, 38]];
@@ -23,5 +24,18 @@ describe('User 클래스 테스트', () => {
     const lottoNumbers = user.lottos.map((lotto) => lotto.getNumbers());
 
     lottoNumbers.forEach((lottoNumber, index) => expect(lottoNumber).toEqual(expected[index]));
+  });
+
+  test('생성된 로또를 출력 형식에 맞게 문자열로 반환하는지 테스트', () => {
+    const randoms = [[2, 3, 4, 12, 25, 38]];
+    const expected = ['[2, 3, 4, 12, 25, 38]'];
+
+    mockRandoms([...randoms]);
+    const user = new User(1000);
+    const lottoNumbersStringArray = user.getLottoStringArray();
+
+    lottoNumbersStringArray.forEach((lottoNumberString, index) =>
+      expect(lottoNumberString).toEqual(expected[index]),
+    );
   });
 });
