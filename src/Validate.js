fa40f9255ee3d4a67383689b658070aa56aff98b
hamo-o <yheel369@gmail.com>
2023-11-07 00:05:08 +0900

    refactor: 에러 문구 상수 분리

--- src/Validate.js
+++ src/Validate.js
@@ -1,33 +1,33 @@
+import ERROR from './constants/Error';
+
 const Validate = {
   checkPurchaseAmount(price) {
     const pattern = /^[0-9]+$/;
     if (!pattern.test(price)) {
-      throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
+      throw new Error(ERROR.purchaseIsNumber);
     } else if (price < 1000) {
-      throw new Error('[ERROR] 구입 금액은 최소 1000원 이상이어야 합니다.');
+      throw new Error(ERROR.purchaseMinNumber);
     } else if (price % 1000) {
-      throw new Error('[ERROR] 구입 금액은 1000원 단위여야 합니다.');
+      throw new Error(ERROR.purchaseThousands);
     }
   },
   checkDuplicateNumber(numbers) {
     const set = new Set(numbers);
     if (numbers.length !== set.size) {
-      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다');
+      throw new Error(ERROR.lottoIsNotDuplicate);
     }
   },
   checkNumberRange(number) {
     const pattern = /^[0-9]+$/;
     if (!pattern.test(number)) {
-      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+      throw new Error(ERROR.lottoIsNumber);
     } else if (number < 1 || number > 45) {
-      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+      throw new Error(ERROR.lottoRange);
     }
   },
   checkIncludeComma(numbers) {
     if (!numbers.includes(',')) {
-      throw new Error(
-        '[ERROR] 로또 번호는 쉼표(,)를 기준으로 구분해야 합니다.'
-      );
+      throw new Error(ERROR.lottoContainComma);
     }
   },
 };
