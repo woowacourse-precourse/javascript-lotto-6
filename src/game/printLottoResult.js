4462f7dcb4dc494cda799334feca52880787e1f6
Hchan2 <seongchan_@naver.com>
2023-11-06 22:35:42 +0900

    로또 최종 결과 출력 메소드

---
+++ src/game/printLottoResult.js
@@ -0,0 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import calculateProfit from "./calculateProfit";
+
+const printLottoResult = (lottoResult, userBuyMoney) => {
+    const calculateProfitPer = ((calculateProfit(lottoResult) / userBuyMoney) * 100).toFixed(1);
+    printLottoContent(lottoResult, calculateProfitPer);
+}
+
+const printLottoContent = (lottoResult, totalPer) => {
+    const contents = `
+        당첨 통계
+        ---
+        3개 일치 (5,000원) - ${lottoResult.three.count}개
+        4개 일치 (50,000원) - ${lottoResult.four.count}개
+        5개 일치 (1,500,000원) - ${lottoResult.five.count}개
+        5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult.fiveBonus.count}개
+        6개 일치 (2,000,000,000원) - ${lottoResult.six.count}개
+        총 수익률은 ${totalPer}%입니다.
+    `;
+
+    MissionUtils.Console.print(contents);
+}
+
+export default printLottoResult;
