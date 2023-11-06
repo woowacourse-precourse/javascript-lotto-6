9a7840deabda8d84cf988eeeb599c417d7395091
aaminha <dks4857@gmail.com>
2023-11-07 05:49:47 +0900

    fix: InputController에 input값 형식 변환 기능 추가

--- src/controllers/InputController.js
+++ src/controllers/InputController.js
@@ -7,6 +7,18 @@ class InputController {
     }
     return result;
   }
+
+  static convertNumber(numberString) {
+    return Number(numberString);
+  }
+
+  static converList(numbersString) {
+    const numberList = numbersString
+      .split(',')
+      .map(numberString => Number(numberString.trim()));
+
+    return numberList;
+  }
 }
 
 export default InputController;
