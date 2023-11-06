fdc8175c8907fbf31fa34cdd91a6ad31ecf73492
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-07 00:30:37 +0900

    chore: fix typo

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,7 +1,7 @@
 import Lotto from "../src/models/Lotto.js";
 
 describe("로또 클래스 테스트", () => {
-  test("구매한 로또 번호는 오름차순으로 정렬된다.", () => {
+  test("로또 번호는 오름차순으로 정렬된다.", () => {
     const lotto = new Lotto([3, 6, 4, 1, 2, 5]);
     expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
   });
