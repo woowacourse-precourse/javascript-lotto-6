1dbbf8fd634247869151c6e136ce4ba6ddaed8f2
Yeojin Kim <yeojin58@naver.com>
2023-11-07 02:59:02 +0900

    로또 구입 금액 입력 받기, LottoTest 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
+import { createLotto, getNumbersOfLotto } from './LottoUtils.js';
 
+const lottoInstance = createLotto([1, 2, 3, 4, 5, 6]);
+
+const numbers = getNumbersOfLotto(lottoInstance);
+console.log(numbers);
+
+function getLottoNumbers(lotto) {
+  return lotto._numbers;
+}
 
 class App {
-  async play() {
-    const lottoNumbers = this.#generateLottoNumbers();
-    const bonusNumber = this.#bonusNumber(lottoNumbers);
+  constructor() {
+    this.amount = 0;
   }
+
+  async play() {
+    this.amount = await this.#getAmount();
+    MissionUtils.Console.print (`${this.amount}`);
+    this.#generateLottos();
+    const lottoNumbers = new Lotto (this.#randomNumbers());
+    const bonusNumber = this.#bonusNumber(lottoNumbers.getNumbers());
+  }
+  
+  async #getAmount() {
+    let isValid = false;
+    let amount = 0;
+
+    while (!isValid) {
+      MissionUtils.Console.print("구입금액을 입력해 주세요.");
+      amount = await MissionUtils.Console.readLineAsync();
+      isValid = this.#validateAmount(amount);
+      if (isValid) {
+        MissionUtils.Console.print (`${this.amount}`);
+      }
+    }
+
+    return Number(amount);
+  }
+
+  #validateAmount (amount) {
+    if (isNaN(amount) || amount <= 0) {
+      MissionUtils.Console.print("[ERROR] 유효한 금액을 입력해주세요.");
+      return false;
+    }
+    return true;
+  }
+
+  #generateLottos () {
+    const lottoCount = Math.floor(this.amount / 1000);
+    const lottos = [];
+
+    for (let i=0; i < lottoCount; i++) {
+      lottos.push (createLotto(this.#randomNumbers()));
+    }
+
+    lottos.forEach (lotto => MissionUtils.Console.print(getNumbersOfLotto(lotto)));
+
+    MissionUtils.Console.print(`${numberOfLottos}개를 구매했습니다.`);
+  }
+  
+    #randomNumbers() {
+      const numbers = [];
+      while (numbers.length < 6) {
+        numbers.push (this.#uniqueNumber(numbers));
+      }
+      return numbers.sort ((a, b) => a - b);
+    }
 
     #isNumberUnique (number, numberHistory) {
       return !numberHistory.includes(number);
@@ -19,14 +80,6 @@ class App {
       } while (!this.#isNumberUnique(number, numberHistory));
 
       return number;
-    }
-
-    #generateLottoNumbers() {
-      const numbers = [];
-      while (numbers.length < 6) {
-        numbers.push(this.#uniqueNumber(numbers));
-      }
-      return numbers.sort((a, b) => a - b);
     }
 
     #bonusNumber(excludedNumber) {
