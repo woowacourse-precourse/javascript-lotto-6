6e256ca19c5b0f698a0356f106896d3367ac06ee
Sohye <saeyan2477@gmail.com>
2023-11-07 02:50:34 +0900

    feat: BuyLotto, LottoList 클래스 구현
    - LottoPrice에서 BuyLotto로 클래스명 변경
    - checkLotto, generateLotto, calcLottoAmount 는 LottoList로 분리
    - GAME_MESSAGE 대신 변수명 변경 적용

---
+++ src/LottoList.js
@@ -0,0 +1,42 @@
+import Lotto from './Lotto.js';
+import BuyLotto from './BuyLotto.js';
+import { LOTTO_RULE, INPUT_MESSAGES, ERROR_MESSAGES } from './Constants.js';
+
+const LENGTH = LOTTO_RULE.LENGTH;
+const { MIN, MAX } = LOTTO_RULE.RANGE;
+
+class LottoList {
+  #lottos;
+
+  constructor() {
+    this.#lottos = [];
+    this.buyLotto = new BuyLotto();
+  }
+  makeLotto(lottoNumbers) {
+    const lotto = new Lotto(lottoNumbers);
+    this.#lottos.push(lotto);
+  }
+
+  async lottoCount() {
+    await this.buyLotto.initialize();
+    return this.buyLotto.getLottoCount();
+  }
+
+  async generateLotto() {
+    const count = await this.lottoCount();
+
+    for (let i = 0; i < count; i++) {
+      let lottoNumbers = Random.pickUniqueNumbersInRange(MIN, MAX, LENGTH);
+      this.makeLotto(lottoNumbers);
+    }
+
+    return this.#lottos.map((lotto) => lotto.getNumbers());
+  }
+
+  async printLottoList() {
+    const lottoList = await this.generateLotto();
+
+    return lottoList.map((el) => `[${el.join(', ')}]`.join('\n'));
+  }
+}
+export default LottoList;
