5f6ae3baa53760550d81bfd82ea5f6cfd08902ed
Y.W KIM <kyw624@gmail.com>
2023-11-06 17:39:27 +0900

    Feat: 당첨 내역 정보를 반환하는 getWinningInfo 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,25 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  getWinningInfo(winningNumbers, bonusNumber) {
+    let matchCount = 0;
+    let isBonusMatched = false;
+
+    this.numbers.forEach((number) => {
+      if (winningNumbers.includes(number)) {
+        matchCount += 1;
+      } else if (number === bonusNumber) {
+        isBonusMatched = true;
+      }
+    });
+
+    return { matchCount, isBonusMatched };
+  }
 }
 
 export default Lotto;
