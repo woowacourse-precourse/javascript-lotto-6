0d82a42bf23f7c6427b616fe1773600822707bdb
woo7i <or2447221@gmail.com>
2023-11-06 20:18:27 +0900

    refector(LottoGame) : 로또의 출력과 사용자 로또 값을 입력 받기위한 클래스

---
+++ src/LottoGame.js
@@ -0,0 +1,33 @@
+import { PurchaseLotto } from './PurchaseLotto.js';
+import { LOTTO_NUMBERS } from './constants.js';
+import { randomNum } from './utils.js';
+
+class LottoGame {
+  constructor() {
+    this.purchaseLotto = new PurchaseLotto();
+  }
+
+  async lottoCount() {
+    await this.purchaseLotto.initialize(); // 비동기 초기화를 기다린 후에
+    return this.purchaseLotto.getLottoCount(); // 로또 개수를 반환합니다.
+  }
+
+  async createLotto() {
+    const count = await this.lottoCount(); // 비동기로 로또 개수를 가져온다
+    const numbers = new Set();
+
+    for (let i = 0; i < count; i++) {
+      const number = randomNum();
+      numbers.add(number.sort((numA, numB) => numA - numB));
+    }
+    return [...numbers];
+  }
+
+  async viewLottoList() {
+    const theLottoList = await this.createLotto();
+
+    return theLottoList.map((item) => `[${item.join(', ')}]`).join(`\n`);
+  }
+}
+
+export default LottoGame;
