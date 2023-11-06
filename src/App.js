fc8e557dfe4fba6d83ab840ea037289cd25da149
hs <tlsgustn93@naver.com>
2023-11-06 17:02:29 +0900

    feat: App Lotto 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import LottoMain from "./modules/LottoMain.js";
+
 class App {
-  async play() {}
+  async play() {
+    const lotto1 = new LottoMain();
+    await lotto1.start();
+  }
 }
 
 export default App;
