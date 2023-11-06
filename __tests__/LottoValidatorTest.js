b53cd1c2f5757160dc174757381f478454277d4a
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 05:17:32 +0900

    :white_check_mark: test: 보너스번호 유효성 테스트 코드 추가

--- __tests__/LottoValidatorTest.js
+++ __tests__/LottoValidatorTest.js
@@ -29,4 +29,20 @@ describe('로또번호 각 유효성 검증 테스트', () => {
     const expectedError = ERROR_MESSAGE.invalidUnique;
     expect(() => LottoValidator.validLottoNumber(numbers)).toThrow(expectedError);
   });
+
+  test('보너스 번호가 범위 1~45를 벗어나면 예외가 발생한다.', () => {
+    expect(() => LottoValidator.validBonusNumber(0, false)).toThrow(ERROR_MESSAGE.invalidRange);
+  });
+
+  test('보너스 번호가 하나의 숫자가 아니면 예외가 발생한다.', () => {
+    expect(() => LottoValidator.validBonusNumber('abc', false)).toThrow(
+      ERROR_MESSAGE.invalidBonusNumber
+    );
+  });
+
+  test('보너스 번호가 당첨번호에 포함되어 있는지의 여부에 따라 예외가 발생한다.', () => {
+    expect(() => LottoValidator.validBonusNumber(42, true)).toThrow(
+      ERROR_MESSAGE.invalidUniqueBonusNumber
+    );
+  });
 });
