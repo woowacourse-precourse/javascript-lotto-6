a240abac1c558f77a9ee3d0cc4448115161cc544
Baek <woonn97@gmail.com>
2023-11-06 10:31:37 +0900

    feat: 로또 개수 구하는 기능 구현
    * countOfLotto() : 로또 개수 구하는 메서드
    * 메서드로 개수를 구하고 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    const inputPrice = await Console.readLineAsync("구입금액을 입력해 주세요.");
+    const countOfLotto = this.countOfLotto(inputPrice); // 로또 개수 구하기
+    Console.print(`${countOfLotto}개를 구매했습니다.`);
+  }
+
+  countOfLotto(inputPrice) {
+    return inputPrice / 1000;
+  }
 }
 
 export default App;
