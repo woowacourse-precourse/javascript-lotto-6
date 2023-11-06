a2153180716036fe4e1d7e0c65a07c538a2e9edc
heonq <gkagjsrb@naver.com>
2023-11-06 09:52:53 +0900

    test(User): 생성된 로또가 오름차순으로 정렬되는지 확인하는 테스트 작성

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -1,8 +1,27 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import User from '../src/User.js';
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
+};
 
 describe('User 클래스 테스트', () => {
   test('구매 금액에 맞는 로또 수량이 생성됐는지 테스트', () => {
     const user = new User(1000);
     expect(user.lottos.length).toBe(1);
+  });
+  test('생성된 로또가 오름차순으로 정렬되는지 테스트', () => {
+    const randoms = [[4, 12, 2, 25, 38, 3]];
+    const expected = [[2, 3, 4, 12, 25, 38]];
+
+    mockRandoms([...randoms]);
+    const user = new User(1000);
+    const lottoNumbers = user.lottos.map((lotto) => lotto.getNumbers());
+
+    lottoNumbers.forEach((lottoNumber, index) => expect(lottoNumber).toEqual(expected[index]));
   });
 });
