605ec16f2022b68cb832e4e69a7d2761005d6f37
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 04:20:23 +0900

    :sparkles: feat: 컨트롤러에 당첨번호 입력 및 재입력 검증 연결

--- src/controllers/LottoGameController.js
+++ src/controllers/LottoGameController.js
@@ -2,6 +2,7 @@ import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
 import PurchaseAmount from '../domain/PurchaseAmount.js';
 import LottoGame from '../domain/LottoGame.js';
+import Lotto from '../Lotto.js';
 
 class LottoGameController {
   constructor() {}
@@ -17,9 +18,22 @@ class LottoGameController {
       const purchasedLotto = lottoGame.getPurchasedLotto();
 
       OutputView.printPurchasedLotto(purchasedLotto);
+      this.userWinningNumbers();
     } catch ({ message }) {
       OutputView.printStaticMessage(message);
       this.start();
+    }
+  }
+
+  async userWinningNumbers() {
+    try {
+      const winningNumbers = await InputView.getWinningNumbers();
+      const numbers = winningNumbers.split(',').map(Number);
+      const winningLotto = new Lotto(numbers).getSortedLotto();
+      // console.log(winningLotto.getSortedLotto());
+    } catch ({ message }) {
+      OutputView.printStaticMessage(message);
+      this.userWinningNumbers();
     }
   }
 }
