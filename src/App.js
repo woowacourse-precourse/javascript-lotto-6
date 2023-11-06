be610887f2a64bcad9e8650c3812b031e4b0c52b
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-07 05:51:46 +0900

    feat: App에서 LottoController 로직이 실행되도록 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import LottoController from './controller/LottoController.js';
+
 class App {
-  async play() {}
-}
+  async play() {
+    this.controller = new LottoController();
+    await this.controller.start();
+  }
+};
 
 export default App;
