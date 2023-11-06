d8a723afe72e844501664203f61f3d9da7fd7ca5
Kidprogramer1039 <bnpo5231@naver.com>
2023-11-06 15:40:59 +0900

    feat: App 파일 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Lotto } from "./Lotto.js";
+
 class App {
-  async play() {}
+  async play() {
+    
+
+    const winNumber = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    const bonusNumber = await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+
+    
+  }
+
+  async lottoNumber() {
+    const buyCount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    
+  }
+  
 }
 
 export default App;
