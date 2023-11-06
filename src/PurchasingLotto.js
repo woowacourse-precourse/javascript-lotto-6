b47e4d052a5887389ef448e4d7ab4a4fc34519ab
Jeongeun Lee <leeju1009@gmail.com>
2023-11-06 16:18:07 +0900

    feat: 유저에게 로또 구입 비용을 입력값으로 받는 기능 추가

---
+++ src/PurchasingLotto.js
@@ -0,0 +1,15 @@
+import Utils from './Utils.js';
+
+const QUESTIONS = {
+  purchaseLottoPrice: '구입금액을 입력해 주세요.',
+};
+
+class PurchasingLotto {
+  async getUserCost() {
+    const userCost = await Utils.ask(QUESTIONS.purchaseLottoPrice);
+    const costStringToNumber = Number(userCost);
+    return costStringToNumber;
+  }
+}
+
+export default PurchasingLotto;
