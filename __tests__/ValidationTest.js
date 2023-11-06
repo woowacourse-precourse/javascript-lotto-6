4d6097d689a5251625fee50a16bec3360ca83568
hong <ahi200@naver.com>
2023-11-06 19:38:25 +0900

    test: true, false 의미 전달 수정

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,11 +1,11 @@
 import Validation from '../src/Validation.js';
 describe('Validation.hasDuplication', () => {
-  test('중복된 숫자가 있는 경우 false를 반환한다', () => {
-    expect(Validation.hasDuplication([1, 1, 3, 4, 5, 6])).toBe(false);
-    expect(Validation.hasDuplication([1, 2, 3, 4, 5, 6, 7, 7])).toBe(false);
+  test('중복된 숫자가 있는 경우 true를 반환한다', () => {
+    expect(Validation.hasDuplication([1, 1, 3, 4, 5, 6])).toBe(true);
+    expect(Validation.hasDuplication([1, 2, 3, 4, 5, 6, 7, 7])).toBe(true);
   });
 
-  test('중복된 숫자가 없는 경우 true를 반환한다', () => {
-    expect(Validation.hasDuplication([1, 2, 3, 4, 5, 6, 7, 8])).toBe(true);
+  test('중복된 숫자가 없는 경우 false를 반환한다', () => {
+    expect(Validation.hasDuplication([1, 2, 3, 4, 5, 6, 7, 8])).toBe(false);
   });
 });
