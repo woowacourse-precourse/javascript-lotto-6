cedd8b4e92c0dc129c0ccbf582dd75f83d30ceed
Baek, Sehee <tpgmlqor@gmail.com>
2023-11-07 00:23:11 +0900

    feat(로또 게임): 구입 금액 1,000원 단위 아닌 경우 예외 처리

--- src/utils/purchaseAmountSave.js
+++ src/utils/purchaseAmountSave.js
@@ -1,9 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-
+import Purchase from "../Purchase.js";
 async function purchaseAmountSave() {
   try {
-    const purchaseAmount = await MissionUtils.Console.readLineAsync("");
-  } catch (error) {}
+    const AMOUNT = await MissionUtils.Console.readLineAsync("");
+    const PURCHASE_AMOUNT = Math.floor(AMOUNT);
+    new Purchase(PURCHASE_AMOUNT);
+    return PURCHASE_AMOUNT;
+  } catch (error) {
+    MissionUtils.Console.print(error);
+  }
 }
 
 export default purchaseAmountSave;
