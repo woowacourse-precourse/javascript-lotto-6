dbc6e842a63c21610b40a2f6272f0e5ae1b20b36
Oh Dong Joon <ohdoju0905@gmail.com>
2023-11-06 22:25:59 +0900

    feat(/src/Lotto.js): Lotto 객체를 생성할 때 검증로직을 추가
    <body>
    - 입력받은 당첨번호를 #numbers 필드에 저장
    - #numbers에 저장된 당첨 번호를 돌려주는 getNumbers 함수를 Lotto class 내에 추가
    
    <footer>

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,31 @@
 class Lotto {
-  #numbers;
+	#numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
+	constructor(numbers) {
+		this.#validate(numbers);
+		this.#numbers = numbers;
+	}
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-  }
+	#validate(numbers) {
+		if (numbers.some((e) => e < 1 || e > 45)) {
+			throw new Error('[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.');
+		} else if (numbers.length !== 6) {
+			throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+		} else if (new Set(numbers).size !== numbers.length) {
+			throw new Error('[ERROR] 로또 번호는 중복되지 않는 숫자들로 구성되어야 합니다.');
+		}
+		numbers.forEach((number) => {
+			if (Number.isNaN(number) || number < 0 || !Number.isInteger(number)) {
+				throw new Error(`[ERROR] 로또 번호는 자연수여야 합니다.`);
+			}
+		});
+		this.#numbers = numbers;
+	}
 
-  // TODO: 추가 기능 구현
+	// TODO: 추가 기능 구현
+	getNumbers() {
+		return this.#numbers;
+	}
 }
 
 export default Lotto;
