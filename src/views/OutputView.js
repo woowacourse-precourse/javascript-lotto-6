37dfacabd6b96dfedc6fb02ae419ca34968e41d7
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 06:01:53 +0900

    :sparkles: feat: 당첨통계 출력기능 추가 각 당첨등수에 맞는 개수를 출력한다

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -13,6 +13,22 @@ const OutputView = {
     const lottos = purchasedLotto.map((lotto) => `[${lotto.join(', ')}]`).join('\n');
     return Console.print(`${lottos}\n`);
   },
+
+  printWinsStatistics(winsStatistics) {
+    Console.print(`\n당첨통계\n---`);
+
+    const prizeLevels = [
+      { label: '3개 일치 (5,000원)', prizeKey: 'fifthPrize' },
+      { label: '4개 일치 (50,000원)', prizeKey: 'fourthPrize' },
+      { label: '5개 일치 (1,500,000원)', prizeKey: 'thirdPrize' },
+      { label: '5개 일치, 보너스 볼 일치 (30,000,000원)', prizeKey: 'secondPrize' },
+      { label: '6개 일치 (2,000,000,000원)', prizeKey: 'firstPrize' },
+    ];
+
+    prizeLevels.forEach((level) => {
+      Console.print(`${level.label} - ${winsStatistics[level.prizeKey]}개`);
+    });
+  },
 };
 
 export default OutputView;
