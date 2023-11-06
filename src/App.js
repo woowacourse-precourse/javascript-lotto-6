2b27678c7770f415a7b7a69572b512112f205c93
김다인 <dbslzhs1218@naver.com>
2023-11-06 22:11:39 +0900

    feat(lotto): 로또 당첨 기능 구현
    - 발행한 로또 번호와 입력받은 당첨번호, 보너스번호 비교하여 같은 값의
      개수에 따라 각각의 등수 값을 +1 해준다.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { Console } from '@woowacourse/mission-utils';
 import { prompt, result } from './constants/message.js';
 import BuyLotto from './BuyLotto.js';
 import validator from './Validator.js';
+import Compare from './Compare.js';
 
 class App {
   buyLotto = new BuyLotto();
 
+  lotteryTicket;
+
   winningNumber;
 
   bonusNumber;
+
+  resultLotto;
 
   async play() {
     const expense = await Console.readLineAsync(prompt.ASK_AMOUNT);
@@ -16,9 +21,9 @@ class App {
 
     Console.print(`\n${lotteryTicketCount}${result.PURCHASE}`);
 
-    const lotteryTicket = this.buyLotto.randomNumber(lotteryTicketCount);
+    this.lotteryTicket = this.buyLotto.randomNumber(lotteryTicketCount);
 
-    lotteryTicket.forEach((lottoElement) => {
+    this.lotteryTicket.forEach((lottoElement) => {
       Console.print(lottoElement);
     });
 
@@ -27,8 +32,21 @@ class App {
     const numberInput = await Console.readLineAsync(prompt.ASK_NUMBER);
     this.winningInput(numberInput);
 
+    Console.print('\n');
+
     const bonusInput = await Console.readLineAsync(prompt.ASK_BONUS_NUMBER);
     this.bonusNumberInput(bonusInput);
+
+    Console.print(result.STATISTIC);
+    Console.print(result.LINE);
+
+    const compare = new Compare(
+      this.lotteryTicket,
+      this.winningNumber,
+      this.bonusNumber,
+    );
+
+    this.resultLotto = compare.compareLotto();
   }
 
   winningInput(numberInput) {
--- src/Compare.js
+++ src/Compare.js
@@ -1,6 +1,67 @@
 class Compare {
+  sameNumber = {
+    firstPlace: 0,
+    secondPlace: 0,
+    thirdPlace: 0,
+    fourthPlace: 0,
+    fivePlace: 0,
+  };
+
+  constructor(lotto, winningNumber, bonusNumber) {
+    this.lotto = lotto;
+    this.winningNumber = winningNumber;
+    this.bonusNumber = bonusNumber;
+  }
+
   compareLotto() {
-    return this.null;
+    this.lotto.forEach((lottoElement) => {
+      const winningCounting = this.includesWinningNumber(lottoElement);
+      const bonusCounting = this.includesBonusNumber(lottoElement);
+
+      if (winningCounting === 3) {
+        this.sameNumber.fivePlace += 1;
+      }
+
+      if (winningCounting === 4) {
+        this.sameNumber.fourthPlace += 1;
+      }
+
+      if (winningCounting === 5 && bonusCounting !== 1) {
+        this.sameNumber.thirdPlace += 1;
+      }
+
+      if (winningCounting === 5 && bonusCounting === 1) {
+        this.sameNumber.secondPlace += 1;
+      }
+
+      if (winningCounting === 6) {
+        this.sameNumber.firstPlace += 1;
+      }
+    });
+
+    return this.sameNumber;
+  }
+
+  includesWinningNumber(lottoElement) {
+    let counting = 0;
+
+    lottoElement.forEach((numberElement) => {
+      if (this.winningNumber.includes(numberElement)) {
+        counting += 1;
+      }
+    });
+
+    return counting;
+  }
+
+  includesBonusNumber(lottoElement) {
+    let counting = 0;
+
+    if (lottoElement.includes(this.bonusNumber)) {
+      counting += 1;
+    }
+
+    return counting;
   }
 }
 export default Compare;
