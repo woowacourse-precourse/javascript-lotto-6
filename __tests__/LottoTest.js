e8e51bf68edce689cd033582395ded30fb4aa6e4
gouz7514 <gouz7514@gmail.com>
2023-11-06 19:49:41 +0900

    Remove(test) : LottoTest에서 당첨 결과 반환 로직 테스트 제거

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -38,52 +38,5 @@ describe("로또 클래스 테스트", () => {
       new Lotto(numbers);
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
-
-    test("Test 2-2. 로또가 자신의 번호와 당첨 번호, 보너스 번호를 비교하여 결과를 반환한다.", () => {
-      const numbers = [
-        [1, 2, 3, 4, 5, 6],
-        [1, 2, 3, 4, 5, 7],
-        [1, 2, 3, 4, 5, 8],
-        [1, 2, 3, 4, 7, 8],
-        [1, 2, 3, 7, 8, 9],
-        [1, 2, 7, 8, 9, 10]
-      ]
-
-      const results = [
-        {
-          cnt: 6,
-          bonus: false
-        },
-        {
-          cnt: 5,
-          bonus: true
-        },
-        {
-          cnt: 5,
-          bonus: false
-        },
-        {
-          cnt: 4,
-          bonus: false
-        },
-        {
-          cnt: 3,
-          bonus: false
-        },
-        {
-          cnt: 2,
-          bonus: false
-        }
-      ]
-
-      const luckyNumbers = [1, 2, 3, 4, 5, 6];
-      const bonusNumber = 7;
-
-      numbers.forEach((number, index) => {
-        const lotto = new Lotto(number);
-        const result = lotto.match(luckyNumbers, bonusNumber);
-        expect(result).toEqual(results[index]);
-      })
-    });
   });
 });
