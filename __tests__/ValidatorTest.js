fdffb05f4830ef4a1e40280d0a3cc42d10ae8873
heonq <gkagjsrb@naver.com>
2023-11-06 20:22:00 +0900

    test(Validator): 로또 당첨 번호에 대한 유효성 검사 테스트

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -2,6 +2,14 @@ import Validator from '../utils/Validator.js';
 
 const invalidPurchaseAmounts = [900, 1100, -1000, 1000.5, '천원'];
 const validPurchaseAmounts = [1000, 2000, 3000, 4000, 10000];
+const invalidMainNumbers = [
+  [0, 1, 2, 3, 4],
+  [-1, 1, 2, 3, 4],
+  [1.1, 2, 3, 4, 5],
+  [1, 2, 3, 4, 5, 46],
+  [1, 2, 3, 4, 5, 6, 7],
+];
+const validMainNumbers = [[1, 2, 3, 4, 5, 6]];
 
 describe('유효성 검사 기능 테스트', () => {
   test.each([
@@ -9,5 +17,11 @@ describe('유효성 검사 기능 테스트', () => {
     [validPurchaseAmounts, Validator.validatePurchaseAmount, true],
   ])('구입 금액에 대한 유효성 검사 테스트', (purchaseAmounts, validation, result) => {
     purchaseAmounts.forEach((purchaseAmount) => expect(validation(purchaseAmount)).toEqual(result));
+  });
+  test.each([
+    [invalidMainNumbers, Validator.validateLottoNumbers, false],
+    [validMainNumbers, Validator.validateLottoNumbers, true],
+  ])('로또 당첨 번호에 대한 유효성 검사 테스트', (mainNumbers, validation, result) => {
+    mainNumbers.forEach((mainNumber) => expect(validation(mainNumber)).toEqual(result));
   });
 });
