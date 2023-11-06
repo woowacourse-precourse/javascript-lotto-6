bff85332db37932e6e3763b74b7de416aec2d00a
jungmyungjin <myung2667@gmail.com>
2023-11-07 03:06:21 +0900

    feat(Utils) : 유틸 함수 추가
    로또 검증에 필요한 `IsNumberInRangeOfArray`, `IsNumber`, `IsNumberInRange` 함수 추가

--- src/Utils.js
+++ src/Utils.js
@@ -34,10 +34,36 @@ export const IsNumbersInArray = (list) => {
 };
 
 /**
- * 0-9까지의 숫자로만 이루어졌는지 확인하는 함수
- * @param {*} value
+ * 숫자 배열이 {min} ~ {max} 사이로 이루어졌는지 확인
+ * @param {number[]} list
+ * @param {number} min
+ * @param {number} max
  * @returns {boolean}
  */
-export const IsNumber = (value) => {
-  return /^[0-9]+$/.test(value);
+export const IsNumberInRangeOfArray = (list, min, max) => {
+  const result = list.every((num) => IsNumberInRange(num, min, max));
+  return result;
+};
+
+/**
+ * {target}숫자가 0-9까지의 숫자로만 이루어졌는지 확인
+ * @param {*} target
+ * @returns {boolean}
+ */
+export const IsNumber = (target) => {
+  return /^[0-9]+$/.test(target);
+};
+
+/**
+ * {target}숫자가 인자 값의 범위의 숫자인지 확인
+ * @param {number} target
+ * @param {number} min
+ * @param {number} max
+ * @returns {boolean}
+ */
+export const IsNumberInRange = (target, min, max) => {
+  if (min <= target && target <= max) {
+    return true;
+  }
+  return false;
 };
