56cf0a3dce9e5b259be3af5b7c5600181adfe922
songhee Kim <soonghee2@gmail.com>
2023-11-06 16:41:36 +0900

    [feat] 랜덤 로또 발행, 당첨번호, 보너스 번호 입력

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,11 +15,6 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  test("출력 확인", () => {
-    const rn = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-    console.log(rn);
-  });
-
   test("구입금액 예외 확인", () => {
     const price = "3067";
     let result = "GOOD";
@@ -29,7 +24,45 @@ describe("로또 클래스 테스트", () => {
     if (parseInt(price) % 1000 != 0) {
       result = "ERROR";
     }
-    console.log(result);
+    expect(result).toEqual("ERROR");
+  });
+
+  test("숫자 정렬 확인", () => {
+    let arr = [4, 35, 3, 12, 16, 6];
+    arr.sort((a, b) => a - b);
+    const compare = [3, 4, 6, 12, 16, 35];
+    expect(arr).toMatchObject(compare);
+  });
+
+  test("티켓 생성, 출력 확인", () => {
+    const ticket = 3;
+    const tickets = [];
+    for (let i = 0; i < ticket; i++) {
+      tickets.push(
+        MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+          (a, b) => a - b
+        )
+      );
+    }
+    const combinedString = tickets
+      .map((ticket) => "[" + ticket.join(", ") + "]")
+      .join("\n");
+    console.log(combinedString);
+  });
+
+  test("당첨 번호 확인", () => {
+    let status = "good";
+    const input = "2,3,4,5,6,45";
+    const winning_number = input.split(",").map((number) => parseInt(number));
+    winning_number.map((number) => {
+      if (isNaN(number)) {
+        status = "숫자ERROR";
+      }
+      if (number < 1 || number > 45) {
+        status = "범위ERROR";
+      }
+    });
+    expect(status).toEqual("good");
   });
 
   // 아래에 추가 테스트 작성 가능
--- src/App.js
+++ src/App.js
@@ -8,11 +8,46 @@ class App {
     if (parseInt(price) % 1000 != 0) {
       throw new Error("[ERROR] 천원 단위로 입력해 주세요");
     }
+    const ticket_num = parseInt(price);
+    // 발행한 로또 수량 및 번호를 출력
+    MissionUtils.Console.print(`${ticket_num}장을 구매했습니다.`);
+    const tickets = produce_ticket(ticket_num);
+
+    //당첨번호, 보너스 번호 입력
+    MissionUtils.Console.print("당첨 번호를 입력해 주세요.");
+    const input = await MissionUtils.Console.readLineAsync();
+    const winning_number = input.split(",").map((number) => parseInt(number));
+    winning_number.map((number) => {
+      check_number(number);
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 1~45까지의 번호를 입력해 주세요");
+      }
+    });
+
+    MissionUtils.Console.print("보너스 번호를 입력해 주세요.");
+    const bonus_number = await MissionUtils.Console.readLineAsync();
+    check_number(bonus_number);
 
     function check_number(input) {
       if (isNaN(input)) {
         throw new Error("[ERROR] 숫자를 입력해주세요");
       }
+    }
+
+    function produce_ticket(ticket) {
+      const tickets = [];
+      for (let i = 0; i < ticket; i++) {
+        tickets.push(
+          MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+            (a, b) => a - b
+          )
+        );
+      }
+      const combinedString = tickets
+        .map((ticket) => "[" + ticket.join(", ") + "]")
+        .join("\n");
+      MissionUtils.Console.print(combinedString);
+      return tickets;
     }
   }
 }
