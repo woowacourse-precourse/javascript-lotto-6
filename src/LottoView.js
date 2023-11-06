69ffe251f9da9c0934fe2bf2ab745cdc6d00b5c9
Yoos99 <yoosh2520@naver.com>
2023-11-07 02:20:36 +0900

    refactor: Console 사용 및 출력 문구 수정

--- src/LottoView.js
+++ src/LottoView.js
@@ -2,25 +2,27 @@ import { Console } from "@woowacourse/mission-utils";
 
 class LottoView {
   async askPayment() {
-    return Console.readLineAsync("구입금액을 입력해 주세요.");
+    return Console.readLineAsync("구입금액을 입력해 주세요.\n");
   }
 
-  showLottoNumbers(lottos) {
+  showLottoNumbers(countOfLotto, lottos) {
+    Console.print(`\n${countOfLotto}개를 구매했습니다.`);
     lottos.forEach((lotto) => Console.print(lotto.numbers));
   }
 
   async askWinningNumbers() {
-    const input = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
+    const input = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.");
     return input.split(",").map(Number);
   }
 
   async askBonusNumber() {
-    const input = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
+    const input = await Console.readLineAsync("\n보너스 번호를 입력해 주세요.");
     return Number(input);
   }
   showResults(results) {
+    Console.print("\n당첨 통계\n---");
     results.forEach((result) => {
-      console.log(
+      Console.print(
         `${result.count}개 일치${result.bonus ? ", 보너스 볼 일치" : ""}  ${
           result.lottoNumbers
         }`
