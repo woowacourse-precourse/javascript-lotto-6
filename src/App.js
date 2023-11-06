8a7e83b25df32d9700b68c3e46dcc605b8a99e62
Oh Dong Joon <ohdoju0905@gmail.com>
2023-11-06 22:00:31 +0900

    feat(/src/App.js): 구입금액을 입력받는 inputMoney 함수를 추가함.
    <body>
    
    <footer>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+	static async inputMoney() {
+		const money = await this.getValidMoney();
+		Console.print('');
+		return money;
+	}
+
+	static async getValidMoney() {
+		Console.print('구입금액을 입력해 주세요.');
+		const money = +(await Console.readLineAsync(''));
+
+		if (money % 1000) {
+			Console.print('[ERROR] 구입금액은 1000 단위로 나누어 떨어져야 합니다.');
+			return this.getValidMoney();
+		}
+		if (Number.isNaN(money) || money < 0 || !Number.isInteger(money)) {
+			Console.print('[ERROR] 구입금액은 자연수여야 합니다.');
+			return this.getValidMoney();
+		}
+		return money;
+	}
+
+	// async play() {}
 }
 
 export default App;
