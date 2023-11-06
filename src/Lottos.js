e641bc1fbcd82901041c50eb0dc30a4c70e18efa
조예진 <joyejin@joyejins-MacBook-Air.local>
2023-11-07 02:00:40 +0900

    랜덤 로또번호 생성 기능 추가

--- src/Lottos.js
+++ src/Lottos.js
@@ -1,6 +1,7 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 import { checkValue } from './libs/checkValue';
-import { LOTTO_NUMBER } from './libs/constants';
+import { LOTTO, LOTTO_NUMBER } from './libs/constants';
+import Lotto from './Lotto';
 
 class Lottos {
   constructor(purchaseAmount) {
@@ -13,5 +14,16 @@ class Lottos {
     if (errorMessage) {
       throwError(errorMessage);
     }
+  }
+
+  //랜덤 로또번호 생성
+  generateLotto() {
+    const lottoNumbers = Random.pickUniqueNumbersInRange(
+      LOTTO.MIN_NUMBER,
+      LOTTO.MAX_NUMBER,
+      LOTTO.NUMBERS_COUNT,
+    );
+
+    return new Lotto(lottoNumbers);
   }
 }
