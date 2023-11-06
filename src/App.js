4911b3ffba522758f2f7bd0af1434f0de2dbd292
anttiey <hanuuny@gmail.com>
2023-11-06 16:34:46 +0900

    feat: 당첨 내역 및 수익률 출력 기능 추가
    - 당첨 내역을 출력하는 printWinningResult 함수 추가
    - 수익률을 출력하는 printTotalReturn 함수 추가

--- src/App.js
+++ src/App.js
@@ -171,6 +171,23 @@ class App {
     return (total / userMoney) * 100;
   }
 
+  printWinningResult(results) {
+    Console.print('당첨 통계');
+    Console.print('---');
+    Console.print(`3개 일치 (5,000원) - ${results['5']}개`);
+    Console.print(`4개 일치 (50,000원) - ${results['4']}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${results['3']}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${results['2']}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${results['1']}개`);
+  }
+
+  printTotalReturn(totalReturn) {
+    Console.print(totalReturn);
+    Console.print(`총 수익률은 ${Math.round(totalReturn * 100) / 100}%입니다.`);
+  }
+
   async play() {
     const userMoney = await this.getUserPurchaseMoney();
     const userTickets = this.purchaseLottoTickets(userMoney);
@@ -187,7 +204,10 @@ class App {
       bonusNumber
     );
 
-    const totalReturn = this.calculateTotalReturn(results);
+    const totalReturn = this.calculateTotalReturn(results, userMoney);
+
+    this.printWinningResult(results);
+    this.printTotalReturn(totalReturn);
   }
 }
 
