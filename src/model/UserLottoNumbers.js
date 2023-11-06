e437e927d4af2982b5ff5ca7901cd5ef4aca5211
jonique98 <tnals9899@naver.com>
2023-11-06 22:52:24 +0900

    feat: 랜덤 배열로 로또 값을 세팅해주는 메소드 작성 랜덤한 6자리의 배열로 로또 값을 생성 woowa 라이브러리 Random 사용

--- src/model/UserLottoNumbers.js
+++ src/model/UserLottoNumbers.js
@@ -1,7 +1,14 @@
+import { Random } from '@woowacourse/mission-utils'
+
 class UserLottoNumber {
   #lottoNumber;
 
   constructor() {
     this.#lottoNumber = this.#generateLottoNumber();
+  }
+
+  #generateLottoNumber() {
+    const lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return lottoNumber.sort((a, b) => a - b);
   }
 }
