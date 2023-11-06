02721d493a15910f9623d67b97bb3228b136ef90
이호연 <ho991217@kakao.com>
2023-11-06 16:04:34 +0900

    feat: 에러 메시지 상수화

---
+++ src/constants/error.js
@@ -0,0 +1,21 @@
+export const ERROR_HEADER = '[ERROR]';
+
+export const LESS_THAN_MINIMUM = '1000원 이상의 금액을 입력해주세요.';
+export const NOT_DIVISIBLE_BY_1000 = '1000원 단위로 입력해주세요.';
+export const NOT_INTEGER = '정수를 입력해주세요.';
+export const EMPTY_STRING = '입력값이 없습니다.';
+export const NOT_POSITIVE_INTEGER = '양의 정수를 입력해주세요.';
+export const NOT_IN_RANGE = '1 ~ 45 사이의 숫자를 입력해주세요.';
+export const DUPLICATED_NUMBER = '중복된 숫자를 입력할 수 없습니다.';
+
+const ERROR_MESSAGE = {
+  LESS_THAN_MINIMUM,
+  NOT_DIVISIBLE_BY_1000,
+  NOT_INTEGER,
+  EMPTY_STRING,
+  NOT_POSITIVE_INTEGER,
+  NOT_IN_RANGE,
+  DUPLICATED_NUMBER,
+};
+
+export default ERROR_MESSAGE;
