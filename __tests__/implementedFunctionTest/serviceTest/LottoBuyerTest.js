ad52fe7946e8b0844e62884a21d6dd5a22dab84c
JiSeong Kim <12180543@inha.edu>
2023-11-06 15:22:35 +0900

    test(LottoBuyer): add test cases for LottoBuyer
    - 입력한 금액이 프로퍼티에 저장되어 getPurchaseAmount로 정상적으로 반환되는지 확인
    - 입력한 금액에 해당하는 로또 수가 발매되는지 확인
    - 주어진 조건에서 로또의 결과를 확인

---
+++ __tests__/implementedFunctionTest/serviceTest/LottoBuyerTest.js
@@ -0,0 +1,54 @@
+import LottoBuyer from '../../../src/LottoBuyer';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const mockRandoms = (numbers) => {
+	MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+	numbers.reduce((acc, number) => {
+		return acc.mockReturnValueOnce(number);
+	}, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+describe('LottoBuyer 테스트', () => {
+	test('입력한 금액이 프로퍼티에 저장이 됐는지 테스트', () => {
+		const purchaseAmount = 8000;
+		const lottoBuyer = new LottoBuyer(purchaseAmount);
+
+		expect(lottoBuyer.getPurchaseAmount()).toEqual(purchaseAmount);
+	});
+
+	test('입력한 금액 만큼의 로또가 발급되는지 테스트', () => {
+		const purchaseAmount = 8000;
+		const lottoBuyer = new LottoBuyer(purchaseAmount);
+
+		const expectedResult = Number.parseInt(purchaseAmount/1000);
+
+		expect(lottoBuyer.getAllLottoNumberArray().length).toEqual(expectedResult);
+	});
+
+	test('발행한 로또의 결과 테스트', () => {
+		// when
+		mockRandoms([
+			[1, 2, 3, 4, 5, 6],
+			[1, 2, 3, 4, 5, 16],
+			[1, 2, 3, 4, 5, 7],
+			[11, 12, 13, 14, 15, 16]
+		]);
+
+		// given
+		const purchaseAmount = 4000;
+		const lottoBuyer = new LottoBuyer(purchaseAmount);
+		const winningNumberArray = [1, 2, 3, 4, 5, 6];
+		const bonusNumber = 7;
+		
+		// then
+		const expectedResult = {
+			firstPlaceCount: 1,
+			secondPlaceCount: 1,
+			thirdPlaceCount: 1,
+			fourthPlaceCount: 0,
+			fifthPlaceCount: 0,
+		};
+
+		expect(lottoBuyer.checkResult(winningNumberArray, bonusNumber)).toEqual(expectedResult);
+	});
+});
