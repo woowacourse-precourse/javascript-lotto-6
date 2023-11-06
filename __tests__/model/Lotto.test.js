33650ef006239b53a6c917bad9bfde20bd6191a3
Trophy198 <spsp8426@naver.com>
2023-11-06 21:45:33 +0900

    test : Lotto 클래스에대한 테스트케이스 초안 작성

---
+++ __tests__/model/Lotto.test.js
@@ -0,0 +1,15 @@
+import Lotto from '../../src/model/Lotto';
+import { GAME_SETTINGS } from '../../src/constants/gameSettings';
+import { ERROR_MESSAGES } from '../../src/constants/ErrorMessages';
+
+describe('Lotto 클래스', () => {
+  test('유효한 번호들로 Lotto 인스턴스가 정상적으로 생성되어야 한다.', () => {});
+
+  test('번호의 개수가 올바르지 않으면 오류를 발생시켜야 한다.', () => {});
+
+  test('중복된 번호가 있으면 오류를 발생시켜야 한다.', () => {});
+
+  test('번호가 유효 범위를 벗어나면 오류를 발생시켜야 한다.', () => {});
+
+  test('당첨 번호와 일치하는 번호의 개수를 정확하게 계산한다', () => {});
+});
