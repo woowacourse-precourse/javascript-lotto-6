8929ac362d02d7feca3594b564931a7d676a9caf
hj7321 <oboki2001@naver.com>
2023-11-06 09:44:07 +0900

    feat: ValidationPrice 파일(구입 금액 유효성 검사) 기능 구현

---
+++ src/ValidationPrice.js
@@ -0,0 +1,14 @@
+const ValidationPrice = (price) => {
+  const priceToNumber = parseInt(price);
+  
+  if(Number.isNaN(priceToNumber))
+    throw new Error("[ERROR] 숫자를 입력해 주세요.");
+  if(priceToNumber <= 0)
+    throw new Error("[ERROR] 0보다 큰 숫자를 입력해 주세요.");
+  if(priceToNumber % 1000 != 0)
+    throw new Error("[ERROR] 1000원 단위로 나누어 떨어지는 숫자를 입력해 주세요.");
+  
+    return true;
+};
+
+export default ValidationPrice;
