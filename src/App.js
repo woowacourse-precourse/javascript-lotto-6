10e7c0e43035f65a5a7e085913eb3db9830bb254
gillyongs <khs20010327@gmail.com>
2023-11-07 02:43:02 +0900

    refactor: 함수 세분화

--- src/App.js
+++ src/App.js
@@ -3,27 +3,9 @@ import Lotto from './Lotto.js';
 
 class App {
   async play() {
-    let purchaseAmount;
-    while (1) {
-      try {
-        purchaseAmount = parseInt( await Console.readLineAsync("구입금액을 입력해 주세요.\n"));
-        if (purchaseAmount % 1000 !== 0 || isNaN(purchaseAmount)) {
-          throw new Error("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
-        }
-        break;
-      } catch (error) {
-        Console.print(error.message);
-      }
-    }
-    const numberOfLottoTickets = Math.floor(purchaseAmount / 1000);
-    const lottoTickets = this.generateLottoTickets(numberOfLottoTickets);
 
-    Console.print(`${numberOfLottoTickets}개를 구매했습니다.`);
-
-    lottoTickets.forEach((ticket, index) => {
-      Console.print('['+ticket.getNumbers().sort((a, b) => a - b).join(', ')+']');
-
-    });
+    const purchaseAmount = await this.inputPurchaseAmout();
+    const lottoTickets = this.getLottoTickets(purchaseAmount);
 
     const winningNumbers = await this.inputWinningNumbers();
     const bonusNumber = await this.inputBonusNumber();
@@ -31,6 +13,39 @@ class App {
     const result = this.calculateResults(lottoTickets, winningNumbers, bonusNumber);
     this.printResults(result);
 
+  }
+
+  async inputPurchaseAmout(){
+    let purchaseAmount;
+    while (1) {
+      try {
+        purchaseAmount = parseInt( await Console.readLineAsync("구입금액을 입력해 주세요.\n"));
+        if (this.isInvalidPurchaseAmount(purchaseAmount)) {
+          throw new Error("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
+        }
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return purchaseAmount;
+  }
+
+  isInvalidPurchaseAmount(purchaseAmount) {
+    return purchaseAmount % 1000 !== 0 || isNaN(purchaseAmount);
+  }
+
+
+  getLottoTickets(purchaseAmount){
+    const numberOfLottoTickets = Math.floor(purchaseAmount / 1000);
+    Console.print(`${numberOfLottoTickets}개를 구매했습니다.`);
+
+    const lottoTickets = this.generateLottoTickets(numberOfLottoTickets);
+    lottoTickets.forEach((ticket, index) => {
+      Console.print('['+ticket.getNumbers().sort((a, b) => a - b).join(', ')+']');
+    });
+
+    return lottoTickets;
   }
 
   generateLottoTickets(count) {
@@ -43,42 +58,52 @@ class App {
     return lottoTickets;
   }
 
+
   async inputWinningNumbers() {
     let winningNumbers;
     let winningNumbersMap;
     while (true) {
-      try {
-        winningNumbers = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n")
-        winningNumbersMap = winningNumbers.split(',').map(Number);
-        if (!this.validateNumbers(winningNumbersMap)) {
-          throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-        if (winningNumbersMap.length !== 6) {
-          throw new Error("[ERROR] 당첨 번호는 6개여야 합니다.");
-        }
-        break;
-      } catch (error) {
-        Console.print(error.message);
-      }
+      winningNumbers = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n")
+      winningNumbersMap = winningNumbers.split(',').map(Number);
+      this.validateWinningNumbers(winningNumbersMap);
+      break;
     }
     return winningNumbersMap;
+  }
+
+  async validateWinningNumbers(winningNumbersMap){
+    try {
+      if (!this.validateNumbers(winningNumbersMap)) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+      if (winningNumbersMap.length !== 6) {
+        throw new Error("[ERROR] 당첨 번호는 6개여야 합니다.");
+      }
+    } catch (error) {
+      Console.print(error.message);
+    }
   }
 
   async  inputBonusNumber() {
     let bonusNumber;
     while (true) {
-      try {
-        bonusNumber = parseInt(await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n"));
-        if (!this.validateNumber(bonusNumber)) {
-          throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-        break;
-      } catch (error) {
-        Console.print(error.message);
-      }
+      bonusNumber = parseInt(await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n"));
+      this.validateBonusNumbers(bonusNumber);
+      break;
     }
     return bonusNumber;
   }
+
+  async validateBonusNumbers(bonusNumber){
+    try {
+      if (!this.validateNumber(bonusNumber)) {
+        throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+
   validateNumbers(numbers) {
     return numbers.every(this.validateNumber);
   }
