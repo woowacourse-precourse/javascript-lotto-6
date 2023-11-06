8ecc031034bbc49cc0921499fc45aa91572d82f3
hongjimin00 <chunsim015@gmail.com>
2023-11-06 16:44:01 +0900

    feat: 구입할 금액 입력 받는 기능+1000원 단위 금액 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
-  async play() {}
+  async play() {
+    const lottoAmount = await Console.readLineAsync(
+      "구입금액을 입력해 주세요."
+    );
+    if (lottoAmount % 1000 !== 0)
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위입니다.");
+  }
 }
 
 export default App;
