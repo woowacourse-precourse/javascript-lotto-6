80bc95037a32257f152f3f37f46e23970b20e98c
23YUJIN <asdbnm066@daum.net>
2023-11-07 06:10:08 +0900

    feat: 로또 숫자 생성하기 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
@@ -13,6 +15,10 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+
+  static generateLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
 }
 
 export default Lotto;
