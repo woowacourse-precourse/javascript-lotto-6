0c6d530fd06307c7013b73607ddc00ced02c316a
serin-B <bangserin980302@gmail.com>
2023-11-07 06:17:38 +0900

    feat(UserInput): 당첨 번호 가져오는 함수 getWinningNumber 생성

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,6 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import MESSAGES from "./constants/Messages.js";
-import { isOnlyNumber } from "./utils/Validation";
+import { isOnlyNumber, isInRangeNumber } from "./utils/Validation";
 import { LOTTO } from "./constants/Standard.js";
 
 class UserInput {
@@ -22,6 +22,7 @@ class UserInput {
 
   #isVaildPrice(stringPrice) {
     const price = Number(stringPrice);
+
     if (
       !isOnlyNumber(stringPrice) ||
       stringPrice.length === 0 ||
@@ -32,11 +33,47 @@ class UserInput {
     if (price > LOTTO.MAX_PRICE) {
       return [false, MESSAGES.ERROR.EXCEED_PRICE, null];
     }
+
     return [true, "", price];
   }
 
   async getWinningNumber() {
-    //
+    try {
+      const winningNumber = await Console.readLineAsync(
+        MESSAGES.GET_WINNING_NUMBER
+      );
+      const [isVaild, message, result] = this.isVaildWinningNumber(
+        winningNumber.split(",")
+      );
+
+      if (!isVaild) {
+        Console.print(message);
+        throw new Error(message);
+      }
+
+      return result;
+    } catch (error) {
+      this.getWinningNumber();
+    }
+  }
+
+  isVaildWinningNumber(splitedNumber) {
+    const winningNumber = splitedNumber.map((el) => Number(el));
+
+    if (!isOnlyNumber(splitedNumber.join(""))) {
+      return [false, MESSAGES.ERROR.PLEASE_ONLY_NUMBER, null];
+    }
+    if (splitedNumber.length !== LOTTO.LENGTH) {
+      return [false, MESSAGES.ERROR.INVAILD_LOTTO_LENGTH, null];
+    }
+    if (!isInRangeNumber(winningNumber)) {
+      return [false, MESSAGES.ERROR.INVAILD_LOTTO_NUMBER, null];
+    }
+    if (new Set(winningNumber).size !== winningNumber.length) {
+      return [false, MESSAGES.ERROR.DUPLICATE_LOTTO_NUMBER, null];
+    }
+
+    return [true, "", winningNumber];
   }
 
   async getBonusNumber(winningNumber) {
