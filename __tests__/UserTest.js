421e36f48640c470c7709943d827c16ae1aa515f
teawon <xodnjsvkvk9@gmail.com>
2023-11-07 01:18:42 +0900

    test : User도메인 테스트 코드 작성

---
+++ __tests__/UserTest.js
@@ -0,0 +1,46 @@
+import User from "../src/domains/User.js";
+
+describe("사용자 클래스 테스트", () => {
+  function mockUserLotto(numbers) {
+    return {
+      getNumbers: () => numbers.sort((a, b) => a - b),
+    };
+  }
+
+  test("특정 금액이 제공되었을 때 사용자는 주어진 금액을 가지고 있어야한다", () => {
+    // given
+    const money = 1000;
+
+    // when
+    const user = new User(money);
+
+    // then
+    expect(user.getMoney()).toBe(money);
+  });
+
+  test("사용자는 음수의 금액을 가질 수 없다.", () => {
+    // given
+    const money = -1000;
+
+    // when & then
+    expect(() => new User(money)).toThrow("[ERROR] 금액은 양수여야 합니다.");
+  });
+
+  test("사용자는 로또를 구매할 수 있다.", () => {
+    // given
+    const money = 3000;
+    const user = new User(money);
+    const lotto1 = mockUserLotto([1, 2, 3, 4, 5, 6]);
+    const lotto2 = mockUserLotto([2, 3, 4, 5, 6, 7]);
+
+    // when
+    user.buyLotto(lotto1, 1000);
+    user.buyLotto(lotto2, 1000);
+    const leftMoney = user.getMoney();
+    const userLotto = user.getLottoList();
+
+    // then
+    expect(leftMoney).toBe(1000);
+    expect(userLotto).toEqual([lotto1, lotto2]);
+  });
+});
