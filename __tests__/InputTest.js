25d7e6c7b39ee5318857b5981d2146db8842b416
Seo San <dannysir@naver.com>
2023-11-07 05:48:20 +0900

    test : 보너스, 정답 예외 처리 테스트 추가 보너스와 정답 로또 번호 입력시 정수 숫자를 제외한 입력을 했을 때 예외처리하는 테스트 추가

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -4,6 +4,36 @@ describe("입력 예외 처리 테스트", () => {
 
     test('천원 단위 테스트', () => {
         const MONEY = "1500";
-        expect(()=>validator.moneyValidator(MONEY)).toThrow("[ERROR]");
+        expect(() => validator.moneyValidator(MONEY)).toThrow("[ERROR]");
+    });
+
+    test("보너스 - 숫자가 아닌값 입력 예외처리", () => {
+        const INPUT = "10a";
+        expect(() => validator.bonusValidator(INPUT)).toThrow("[ERROR]");
+    });
+
+    test("보너스 - 범위 예외처리", () => {
+        const INPUT = "47";
+        expect(() => validator.bonusValidator(INPUT)).toThrow("[ERROR]");
+    });
+
+    test("보너스 - 정수가 아닌 값", () => {
+        const INPUT = "10.5";
+        expect(() => validator.bonusValidator(INPUT)).toThrow("[ERROR]");
+    });
+
+    test("정답 - 범위 외의 숫자", () => {
+        const INPUT = "1,2,3,4,5,100";
+        expect(() => validator.bonusValidator(INPUT)).toThrow("[ERROR]");
+    });
+
+    test("정답 - 숫자가 아닌 값 입력", () => {
+        const INPUT = "1,2,3a,4,5,6";
+        expect(() => validator.bonusValidator(INPUT)).toThrow("[ERROR]");
+    });
+
+    test("정답 - 정수가 아닌값 포함", () => {
+        const INPUT = "1,2.2,3,4,5,6";
+        expect(() => validator.bonusValidator(INPUT)).toThrow("[ERROR]");
     });
 });
