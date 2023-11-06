df7aa16219d31b1f40e1f865d24bb6719e9e6994
starcradle101 <dreamerdev12@gmail.com>
2023-11-06 21:41:37 +0900

    🛠️Feat($Validation): 보너스 넘버 유효성 검사 구현 및 테스트 완료 보너스 넘버에 대한 유효성 검사 구현 및 테스트 코드 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -10,19 +10,43 @@ describe('Validation Tests', () => {
 
   test('1000원 단위가 아닐 경우 오류 발생', () => {
     expect(() => {
-      Validation.isPurchaseMoneyValidated(1501);
+      Validation.isPurchaseMoneyValidated('1501');
     }).toThrow(ERROR_MESSAGES.invalidLottoPurchaseAmount);
   });
 
   test('빈 값 혹은 1000원보다 작은 값을 입력했을 경우 오류 발생', () => {
     expect(() => {
-      Validation.isPurchaseMoneyValidated(0);
+      Validation.isPurchaseMoneyValidated('0');
     }).toThrow(ERROR_MESSAGES.invalidLottoPurchaseAmountZero);
   });
 
   test('올바른 입력에 대해서는 오류를 발생하지 않음', () => {
     expect(() => {
       Validation.isPurchaseMoneyValidated(6000);
+    }).not.toThrow();
+  });
+
+  test('보너스 번호가 범위를 벗어날 경우 오류 발생', () => {
+    expect(() => {
+      Validation.isBonusNumberValidated('51');
+    }).toThrow(ERROR_MESSAGES.invalidBonusNumberRange);
+  });
+
+  test('보너스 번호가 범위를 벗어날 경우 오류 발생', () => {
+    expect(() => {
+      Validation.isBonusNumberValidated('-1');
+    }).toThrow(ERROR_MESSAGES.invalidBonusNumberRange);
+  });
+
+  test('보너스 번호가 문자일 경우 오류 발생', () => {
+    expect(() => {
+      Validation.isBonusNumberValidated('a');
+    }).toThrow(ERROR_MESSAGES.invalidBonusNumberType);
+  });
+
+  test('올바른 입력에 대해서는 오류를 발생하지 않음', () => {
+    expect(() => {
+      Validation.isBonusNumberValidated(10);
     }).not.toThrow();
   });
 });
--- src/domain/Validation.js
+++ src/domain/Validation.js
@@ -1,4 +1,5 @@
-import { ERROR_MESSAGES, CustomError } from '../constants/Errors';
+import CONSTANTS from '../constants/Constants.js';
+import { ERROR_MESSAGES, CustomError } from '../constants/Errors.js';
 
 class Validation {
   static isPurchaseMoneyValidated(purchaseMoneyInput) {
@@ -12,6 +13,44 @@ class Validation {
 
     if (purchaseMoneyInput === '' || Number(purchaseMoneyInput) <= 0) {
       throw new CustomError(ERROR_MESSAGES.invalidLottoPurchaseAmountZero);
+    }
+  }
+
+  static validateLottoNumberLength(numbers) {
+    if (numbers.length !== 6) {
+      throw new CustomError(ERROR_MESSAGES.invalidLottoNumberLength);
+    }
+  }
+
+  static validateLottoNumberDuplicate(numbers) {
+    if (new Set(numbers).size !== numbers.length) {
+      throw new CustomError(ERROR_MESSAGES.invalidLottoNumberDuplicate);
+    }
+  }
+
+  static validateLottoNumberType(numbers) {
+    if (numbers.some(number => CONSTANTS.numberTypeRegex.test(number))) {
+      throw new CustomError(ERROR_MESSAGES.invalidLottoNumberType);
+    }
+  }
+
+  static validateLottoNumberRange(numbers) {
+    if (
+      numbers.some(
+        number => number > CONSTANTS.maxRange || number < CONSTANTS.minRange,
+      )
+    ) {
+      throw new CustomError(ERROR_MESSAGES.invalidLottoNumberRange);
+    }
+  }
+
+  static isBonusNumberValidated(bonusNumber) {
+    if (Number.isNaN(Number(bonusNumber))) {
+      throw new CustomError(ERROR_MESSAGES.invalidBonusNumberType);
+    }
+
+    if (bonusNumber > CONSTANTS.maxRange || bonusNumber < CONSTANTS.minRange) {
+      throw new CustomError(ERROR_MESSAGES.invalidBonusNumberRange);
     }
   }
 }
