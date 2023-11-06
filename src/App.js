54e738506375d5e62f119c3479c8fc1c98f2c667
LEEHYEYEONG <elto789@naver.com>
2023-11-06 17:28:12 +0900

    feat: 로또 구입 금액 입력 기능 - 1000으로 나누어 떨어지지 않는 경우와 0을 입력했을 경우, 문자열이 입력한 경우를 예외처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
-  async play() {}
+
+  // 로또 살 돈을 입력받는 함수
+  async lottoBudget() {
+    const budgetInput = await Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+    if (budgetInput % 1000 !== 0 || budgetInput <= 0) {
+      throw new Error("[ERROR]")
+    } else if (isNaN(parseInt(budgetInput))) {
+      throw new Error("[ERROR]")
+    }
+  }
+
+  //랜덤 로또 만드는 함수 
+  makeLotto() {
+    const computer = [];
+    while (computer.length < 6) {
+      const number = Random.pickNumberInRange(1, 45);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+  async play() {
+    this.lottoBudget();
+    const lottoNumber = this.makeLotto();
+  }
 }
 
 export default App;
+
+const app = new App()
+app.play()
