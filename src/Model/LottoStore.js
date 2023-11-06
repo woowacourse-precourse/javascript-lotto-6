6da0ebee5ef2dc0c1c6c4612525de13239922876
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-06 21:27:24 +0900

    feat: 당첨 여부 및 수익률을 계산하는 기능 구현

--- src/Model/LottoStore.js
+++ src/Model/LottoStore.js
@@ -1,8 +1,18 @@
 import Lotto from "../Lotto.js";
 import { Random } from "@woowacourse/mission-utils";
+import { LOTTO_WINNING_AMOUNT } from "../constants/lotto.js";
 
 class LottoStore {
   #lottos = [];
+
+  #LottoMatchResult = {
+    firstPlace: 0,
+    secondPlace: 0,
+    thirdPlace: 0,
+    fourthPlace: 0,
+    fifthPlace: 0,
+    returnRate: 0,
+  };
 
   constructor(purchaseQuantity) {
     const randomLottoNumbers = this.#createRandomLottoNumbers(purchaseQuantity);
@@ -10,9 +20,7 @@ class LottoStore {
   }
 
   #createRandomLottoNumbers(purchaseQuantity) {
-    const randomLottoNumbers = Array.from({ length: purchaseQuantity }, () =>
-      this.#pickRandomNumbers()
-    );
+    const randomLottoNumbers = Array.from({ length: purchaseQuantity }, () => this.#pickRandomNumbers());
 
     return randomLottoNumbers;
   }
@@ -23,14 +31,54 @@ class LottoStore {
   }
 
   #createLottos(randomLottoNumbers) {
-    this.#lottos = randomLottoNumbers.map(
-      (randomNumbers) => new Lotto(randomNumbers)
-    );
+    this.#lottos = randomLottoNumbers.map((randomNumbers) => new Lotto(randomNumbers));
   }
 
   getLottoNumbers() {
     const lottoNumbers = this.#lottos.map((lotto) => lotto.getNumbers());
     return lottoNumbers;
+  }
+
+  getLottoMatchResult({ lottoWinningNumbers, bonousNumber }) {
+    const matchCounts = this.#matchLottoNumbers({ lottoWinningNumbers, bonousNumber });
+    this.#calculateMatchResult(matchCounts);
+    this.#calculateReturnRate();
+    return this.#LottoMatchResult;
+  }
+
+  #matchLottoNumbers({ lottoWinningNumbers, bonousNumber }) {
+    const matchCounts = this.#lottos.map((lotto) => lotto.matchNumbers({ lottoWinningNumbers, bonousNumber }));
+    return matchCounts;
+  }
+
+  #calculateMatchResult(matchCounts) {
+    matchCounts.map((matchCount) => {
+      this.#compareMatchResult(matchCount);
+    });
+  }
+
+  #compareMatchResult({ lottoWinningNumbersMatchCount, bonousNumberMatchCount }) {
+    if (lottoWinningNumbersMatchCount === 6) this.#LottoMatchResult.firstPlace++;
+    if (lottoWinningNumbersMatchCount === 5 && bonousNumberMatchCount === 1) this.#LottoMatchResult.secondPlace++;
+    if (lottoWinningNumbersMatchCount === 5 && bonousNumberMatchCount === 0) this.#LottoMatchResult.thirdPlace++;
+    if (lottoWinningNumbersMatchCount === 4) this.#LottoMatchResult.fourthPlace++;
+    if (lottoWinningNumbersMatchCount === 3) this.#LottoMatchResult.fifthPlace++;
+  }
+
+  #calculateReturnRate() {
+    const totalWinningAmount = this.#calculateTotalWinningAmount();
+    const returnRate = (totalWinningAmount / (this.#lottos.length * 1000)) * 100;
+    this.#LottoMatchResult.returnRate = returnRate;
+  }
+
+  #calculateTotalWinningAmount() {
+    const totalWinningAmount =
+      this.#LottoMatchResult.firstPlace * LOTTO_WINNING_AMOUNT.firstPlace +
+      this.#LottoMatchResult.secondPlace * LOTTO_WINNING_AMOUNT.secondPlace +
+      this.#LottoMatchResult.thirdPlace * LOTTO_WINNING_AMOUNT.thirdPlace +
+      this.#LottoMatchResult.fourthPlace * LOTTO_WINNING_AMOUNT.fourthPlace +
+      this.#LottoMatchResult.fifthPlace * LOTTO_WINNING_AMOUNT.fifthPlace;
+    return totalWinningAmount;
   }
 }
 
---
+++ src/constants/lotto.js
@@ -0,0 +1,7 @@
+export const LOTTO_WINNING_AMOUNT = Object.freeze({
+  firstPlace: 2000000000,
+  secondPlace: 30000000,
+  thirdPlace: 1500000,
+  fourthPlace: 50000,
+  fifthPlace: 5000,
+});
