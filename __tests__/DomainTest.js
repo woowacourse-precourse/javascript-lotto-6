f2f0172e6ce88c245c58a2457433003abe87addf
dali1999 <dali1999@naver.com>
2023-11-06 20:41:48 +0900

    test(checkWin): 같은 번호의 갯수를 배열에 저장하는 기능 테스트

--- __tests__/DomainTest.js
+++ __tests__/DomainTest.js
@@ -53,4 +53,33 @@ describe('핵심 로직 테스트', () => {
     // then
     expect(arr).toEqual([{}, {}]);
   });
+
+  test('checkWin() - 당첨 번호와 로또 번호를 비교 후 같은 번호의 갯수를 배열에 저장한다.', () => {
+    // given
+    const arr = [];
+    const price = 7000;
+    const win = [1, 2, 3, 4, 5, 6];
+    const bonus = 7;
+    mockRandoms([
+      [1, 7, 8, 9, 10, 11], // 1개 일치
+      [1, 2, 7, 8, 9, 10], // 2개 일치
+      [1, 2, 3, 7, 8, 9], // 3개 일치
+      [1, 2, 3, 4, 7, 8], // 4개 일치
+      [1, 2, 3, 4, 5, 8], //5개 일치
+      [1, 2, 3, 4, 5, 7], //5개 + 보너스 일치
+      [1, 2, 3, 4, 5, 6], //6개 일치
+    ]);
+
+    // when
+    const controller = new LottoController();
+    for (let i = 0; i < price / 1000; i++) {
+      controller.generateAndStoreLotto(arr);
+    }
+    controller.checkWin(price, win, bonus, arr);
+
+    // then
+    expect(controller.sameNumCountArr).toEqual([1, 2, 3, 4, 5, 7, 6]);
+  });
+
+  
 });
