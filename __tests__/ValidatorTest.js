c2a97b17b9a4cb8d0cc5ab07df82c89a11d88af8
myeongin <mungin0926@naver.com>
2023-11-06 15:32:14 +0900

    [test] 로또 구매 금액이 최소 금액보다 작을경우 테스트

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -8,4 +8,8 @@ describe('유저 입력 유효성 검사', () => {
   test.each([[2500], [1200], [120003]])('로또 구매 금액이 1000원으로 안 나누어질 경우 테스트', (input) => {
     expect(() => Validator.validateDivisible(input)).toThrow();
   });
+
+  test.each([[999], [-200], [30]])('로또 구매 금액이 최소 금액보다 작을경우 테스트', (input) => {
+    expect(() => Validator.validateMinPrice(input)).toThrow();
+  });
 });
