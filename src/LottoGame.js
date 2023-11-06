cd4d47d1feb87a399ebee107b231296c8a676d85
NSRBSG <nsrbsg@gmail.com>
2023-11-06 16:27:37 +0900

    feat(LottoGame): 당첨 번호 입력 예외 처리 함수 추가

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -7,7 +7,26 @@ class LottoGame {
         return winningNumbers;
     }
 
-    #validateWinningNumbers(winningNumbers) {};
+    #validateWinningNumbers(winningNumbers) {
+        if(winningNumbers.split(",").length() !== 6) {
+            throw new Error("당첨 번호는 6개여야 합니다.");
+        }
+
+        if (new Set(winningNumbers.split(",")).size !== 6) {
+            throw new Error("당첨 번호에 중복된 번호가 있습니다.");
+        }
+
+        winningNumbers.split(",").forEach((number) => {
+            const winningNumber = Number(number);
+            if (isNaN(winningNumber)) {
+                throw new Error("당첨 번호는 숫자여야 합니다.");
+            }
+
+            if (winningNumber < 1 || winningNumber > 45) {
+                throw new Error("당첨 번호는 1과 45 사이여야 합니다.");
+            }
+        })
+    };
 
 
 
