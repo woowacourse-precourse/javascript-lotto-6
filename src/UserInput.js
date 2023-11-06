6bcc26fbfdf041dc6aa4dc5ba0e08b66ac33c7c4
serin-B <bangserin980302@gmail.com>
2023-11-07 05:03:55 +0900

    feat(UserInput): 구입금액을 가져오는 getPrice 함수 생성

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,6 +1,38 @@
+import { Console } from "@woowacourse/mission-utils";
+import MESSAGES from "./constants/Messages.js";
+import { isOnlyNumber } from "./utils/Validation";
+import { LOTTO } from "./constants/Standard.js";
+
 class UserInput {
   async getPrice() {
-    //
+    try {
+      const price = await Console.readLineAsync(MESSAGES.GET_PRICE);
+      const [isVaild, message, result] = this.#isVaildPrice(price);
+
+      if (!isVaild) {
+        Console.print(message);
+        throw new Error(message);
+      }
+
+      return result;
+    } catch (error) {
+      this.getPrice();
+    }
+  }
+
+  #isVaildPrice(stringPrice) {
+    const price = Number(stringPrice);
+    if (
+      !isOnlyNumber(stringPrice) ||
+      stringPrice.length === 0 ||
+      price % LOTTO.PRICE !== 0
+    ) {
+      return [false, MESSAGES.ERROR.INVALID_PRICE, null];
+    }
+    if (price > LOTTO.MAX_PRICE) {
+      return [false, MESSAGES.ERROR.EXCEED_PRICE, null];
+    }
+    return [true, "", price];
   }
 
   async getWinningNumber() {
