4f4e6b56c8f310bf25b52516fb8cb36ceb4d04fb
han-wo98 <99472476+han-wo98@users.noreply.github.com>
2023-11-06 20:17:21 +0900

    feat : Lotto.js에 중복숫자 경고 및 , 출력 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,24 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw "[ERROR] 로또 번호는 6개여야 합니다.";
+    }
+    if (new Set(numbers).size !== 6) {
+      throw "[ERROR] 중복되는 숫자가 있습니다.";
     }
   }
 
-  // TODO: 추가 기능 구현
+  toString() {
+    return this.#numbers.sort((a, b) => a - b).join(", ");
+  }
+
+  getMatchingNumbers(winningNumbers) {
+    return this.#numbers.filter((number) => winningNumbers.includes(number));
+  }
+
+  contains(number) {
+    return this.#numbers.includes(number);
+  }
 }
 
-export default Lotto;
+module.exports = Lotto;
