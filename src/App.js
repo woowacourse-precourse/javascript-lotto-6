2a247fea5c2dd9e049e915a288b8dce00567bc8f
sejineeee <sejinee0918@gmail.com>
2023-11-06 16:17:53 +0900

    feature: 구입금액 유효성 검사 함수 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ class App {
   async play() {
     const purchaseAmount = await input.askPurchaseAmount();
     const parsedpurchaseAmount = parseInt(purchaseAmount, 10);
+
+    this.validateAskPurchaseAmount(parsedpurchaseAmount);
+  }
+
+  validateAskPurchaseAmount(purchaseAmount) {
+    if (Number.isNaN(purchaseAmount) || purchaseAmount === 0) {
+      throw new Error('[ERROR] 입력된 값을 확인해주세요.');
+    }
+
+    if (purchaseAmount % CURRENCY_UNIT) {
+      throw new Error('[ERROR] 1,000원 단위로 입력해주세요.');
+    }
   }
 }
 
