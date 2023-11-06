6b28c37b2f5d767cf359a3a3091a7813c7380013
Dobbymin <gmin0701@knu.ac.kr>
2023-11-06 11:25:58 +0900

    ✨ feat(App): 로또 구입 구현중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { getNumber, getMaxLottoNumber } from './pages/modules.js';
+
 class App {
-  async play() {}
+    async play() {
+        let money = getNumber();
+        let MaxLottoNumber = getMaxLottoNumber(money);
+    }
 }
 
 export default App;
