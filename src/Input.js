328656d4a01b221b6a91dd10e84af9faa5f7cfe2
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-11-06 12:38:29 +0900

    feat(Input) : 라이브러리 변경

--- src/Input.js
+++ src/Input.js
@@ -1,62 +1,41 @@
-// Input.js
 import readline from "readline";
 import Lotto from "./Lotto.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Random, Console } from "@woowacourse/mission-utils";
 
 class Input {
   static async inputMoney() {
-    return new Promise((resolve, reject) => {
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      });
+    try {
+      const money = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+      const purchaseAmount = parseInt(money, 10);
 
-      rl.question("구입금액을 입력해 주세요.\n", (money) => {
-        const purchaseAmount = parseInt(money, 10);
+      if (isNaN(purchaseAmount) || purchaseAmount % 1000 !== 0) {
+        Console.print("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.");
+        return;
+      } else {
+        const count = purchaseAmount / 1000;
+        const message = `${count}개를 구매했습니다.`;
+        Console.print(message);
 
-        if (isNaN(purchaseAmount) || purchaseAmount % 1000 !== 0) {
-          console.error("[ERROR] 구입금액은 1,000원 단위로 입력해야 합니다.");
-          rl.close();
-          reject();
-        } else {
-          const count = purchaseAmount / 1000;
-          const message = `${count}개를 구매했습니다.`;
-          console.log(message);
-
-          for (let i = 0; i < count; i++) {
-            const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
-              1,
-              45,
-              6
-            );
-            console.log(`[${lottoNumbers.join(", ")}]`);
-          }
-          rl.close();
+        for (let i = 0; i < count; i++) {
+          const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+          Console.print(`[${lottoNumbers.join(", ")}]`);
         }
-      });
-    });
+      }
+    } catch (error) {
+      Console.print(`오류: ${error.message}`);
+    }
   }
 
   static async inputNumber() {
-    return new Promise((resolve, reject) => {
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      });
-
-      rl.question("당첨 번호를 입력해 주세요.\n", (number) => {
-        const lottoNumbers = number.split(",").map((num) => parseInt(num, 10));
-
-        try {
-          const lotto = new Lotto(lottoNumbers);
-          rl.close();
-          resolve(lottoNumbers);
-        } catch (error) {
-          rl.close();
-          reject(error);
-        }
-      });
-    });
+    try {
+      const number = await Console.readLineAsync(
+        "당첨 번호를 입력해 주세요.\n"
+      );
+      const lottoNumbers = number.split(",").map((num) => parseInt(num, 10));
+      const lotto = new Lotto(lottoNumbers);
+    } catch (error) {
+      Console.print(`오류: ${error.message}`);
+    }
   }
 }
 
