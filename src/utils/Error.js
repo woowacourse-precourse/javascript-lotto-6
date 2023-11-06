443a6f5f17e16a21a54ff4776be199ce945ba8d1
fecapark <fecapark@gmail.com>
2023-11-06 11:04:49 +0900

    feat: 보너스 번호 입력 에러 클래스 구현 완료

--- src/utils/Error.js
+++ src/utils/Error.js
@@ -51,15 +51,43 @@ export class WinningNumberError extends Error {
    */
   static #getMessage(type) {
     if (type === WinningNumberError.TYPE_NOT_AMOUNT_6) {
-      return `${DEFAULT_ERROR_MESSAGE} 올바르지 않은 숫자 형식입니다.`;
+      return `${DEFAULT_ERROR_MESSAGE} 입력된 당첨 번호가 6개가 아닙니다.`;
     }
 
     if (type === WinningNumberError.TYPE_OUT_OF_RANGE) {
-      return `${DEFAULT_ERROR_MESSAGE} 입력한 금액이 1000원 단위가 아닙니다.`;
+      return `${DEFAULT_ERROR_MESSAGE} 당첨 번호가 1~45 사이의 값이 아닙니다.`;
     }
 
     if (type === WinningNumberError.TYPE_DUPLICATED) {
-      return `${DEFAULT_ERROR_MESSAGE} 최소 1개 이상을 구입하셔야 합니다.`;
+      return `${DEFAULT_ERROR_MESSAGE} 중복된 당첨 번호가 있습니다.`;
+    }
+
+    return DEFAULT_ERROR_MESSAGE;
+  }
+}
+
+export class BonusNumberError extends Error {
+  static TYPE_OUT_OF_RANGE = 0;
+  static TYPE_DUPLICATED = 2;
+
+  /**
+   * @param {number} errorType
+   */
+  constructor(errorType = -1) {
+    super(BonusNumberError.#getMessage(errorType));
+  }
+
+  /**
+   *
+   * @param {number} type
+   */
+  static #getMessage(type) {
+    if (type === BonusNumberError.TYPE_OUT_OF_RANGE) {
+      return `${DEFAULT_ERROR_MESSAGE} 당첨 번호가 1~45 사이의 값이 아닙니다.`;
+    }
+
+    if (type === BonusNumberError.TYPE_DUPLICATED) {
+      return `${DEFAULT_ERROR_MESSAGE} 당첨 번호들과 중복되는 번호입니다.`;
     }
 
     return DEFAULT_ERROR_MESSAGE;
