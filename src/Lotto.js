1463935f1db79209f9ccf0706e9b9cf7ed030cbe
고세종 <fulfilled7418@gmail.com>
2023-11-07 00:03:55 +0900

    feat: 구현 getLottoNumbers 메소드

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import Validator from '../Controller/modules/Validator.js';
+
 class Lotto {
   #numbers;
 
@@ -7,9 +9,11 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    Validator.checkLottoNumberIsValid(numbers);
+  }
+
+  getLottoNumbers() {
+    return this.#numbers;
   }
 
   // TODO: 추가 기능 구현
