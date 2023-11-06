8a18b8c9ecaf5eacf4bc3c6659f6d7177d7c1738
swarvy <swarvy0826@naver.com>
2023-11-07 00:43:46 +0900

    feat : 출력부 기능 구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+import {
+  LOTTO_NOTIFICATION_FORMAT,
+  LOTTO_NOTIFICATION_MESSAGE,
+  LOTTO_RESULT_FORMANT,
+} from '../constants/LottoMessage.js';
+
+export const printLottoNumbers = ({ lottoList, lottoAmount }) => {
+  Console.print(LOTTO_NOTIFICATION_FORMAT.lottoAmount(lottoAmount));
+  lottoList.forEach((lotto) => Console.print(lotto));
+};
+
+export const printLottoResults = (lottoResult, profit) => {
+  Console.print(LOTTO_NOTIFICATION_MESSAGE.result);
+  Object.values(LOTTO_RESULT_FORMANT).forEach((resultFormat, idx, origin) => {
+    if (idx !== origin.length - 1) {
+      return Console.print(resultFormat(lottoResult[idx]));
+    }
+
+    return Console.print(resultFormat(profit));
+  });
+};
