6c91713f06c73d838688caf70be81f050ef53817
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-06 14:10:16 +0900

    Refactor: 당첨 번호 입력값에 대한 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,6 +11,23 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (
+      numbers.some(
+        (number) =>
+          isNaN(Number(number)) || number < 1 || number > 45 || number % 1 !== 0
+      )
+    ) {
+      throw new Error(
+        "[ERROR] 입력한 숫자는 1부터 45 사이의 자연수이어야 합니다."
+      );
+    }
+    if (numbers.length !== new Set(numbers).size) {
+      throw new Error("[ERROR] 중복된 숫자는 입력하면 안 됩니다.");
+    }
+  }
+
+  getNumbers() {
+    return this.#numbers;
   }
   print() {
     Console.print(this.#numbers);
