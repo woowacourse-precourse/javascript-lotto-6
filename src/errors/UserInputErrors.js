7fadca3e999b3c21298de3f37007c38c2c407ac6
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-11-07 05:03:09 +0900

    feat : 구매 금액 입력 요청시 발생하는 커스텀 에러 객체 구현
    PurchasePriceInputError : 구매금액 예외시 발생하는 에러객체

--- src/errors/UserInputErrors.js
+++ src/errors/UserInputErrors.js
@@ -1,3 +1,4 @@
 import GameError from './GameError.js';
 
 export class UserInputError extends GameError {}
+export class PurchasePriceInputError extends GameError {}
--- src/utils/validateUserInput.js
+++ src/utils/validateUserInput.js
@@ -4,7 +4,7 @@ import {
 } from '../constants/lotto.js';
 import { ERROR_MESSAGE } from '../constants/message.js';
 import paramType from '../lib/paramType/src/paramType.js';
-import { UserInputError } from '../errors/UserInputErrors.js';
+import { PurchasePriceInputError } from '../errors/UserInputErrors.js';
 
 export const validateUserInput = {
   purchasePrice(userInput, _ = paramType(userInput, 'string')) {
@@ -19,13 +19,15 @@ export const validateUserInput = {
   checkNegativeSign(userInput) {
     const minusSignRegExp = /^-.*/g;
     if (minusSignRegExp.test(userInput)) {
-      throw new UserInputError(ERROR_MESSAGE.USER_INPUT.HAVE_MINUS_SIGN);
+      throw new PurchasePriceInputError(
+        ERROR_MESSAGE.USER_INPUT.HAVE_MINUS_SIGN,
+      );
     }
   },
 
   checkMinimumPurchasePrice(userInput) {
     if (Number(userInput) < LOTTO.SELLING_PRICE) {
-      throw new UserInputError(
+      throw new PurchasePriceInputError(
         ERROR_MESSAGE.USER_INPUT.LOWER_THAN_MINIMUM_PURCHASE_PRICE,
       );
     }
@@ -34,14 +36,14 @@ export const validateUserInput = {
   checkSpacing(userInput) {
     const spacingRegExp = /\s/g;
     if (spacingRegExp.test(userInput)) {
-      throw new UserInputError(ERROR_MESSAGE.USER_INPUT.HAVE_SPACING);
+      throw new PurchasePriceInputError(ERROR_MESSAGE.USER_INPUT.HAVE_SPACING);
     }
   },
 
   checkNonNumberTypeInput(userInput) {
     const notNumberTypeRegExp = /[^0-9]/g;
     if (notNumberTypeRegExp.test(userInput)) {
-      throw new UserInputError(
+      throw new PurchasePriceInputError(
         ERROR_MESSAGE.USER_INPUT.IS_NOT_ONLY_NUMBER_TYPE,
       );
     }
@@ -49,7 +51,7 @@ export const validateUserInput = {
 
   checkSellingPriceMultiple(userInput) {
     if (!Number.isInteger(Number(userInput) / LOTTO.SELLING_PRICE)) {
-      throw new UserInputError(
+      throw new PurchasePriceInputError(
         ERROR_MESSAGE.USER_INPUT.IS_NOT_MULTIPLE_SELLING_PRICE,
       );
     }
@@ -57,7 +59,7 @@ export const validateUserInput = {
 
   checkOverBuying(userInput) {
     if (Number(userInput) > LIMIT_PER_DAY_PURCHASABLE_TOTAL_PRICE) {
-      throw new UserInputError(
+      throw new PurchasePriceInputError(
         ERROR_MESSAGE.USER_INPUT.IS_OVER_PURCHASABLE_PRICE_OF_PER_DAY,
       );
     }
