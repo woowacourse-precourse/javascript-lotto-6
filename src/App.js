2ba37ef4dffb53a5d903d9568f25f671452cc0d4
hyewon <hyewon.parkhw@gmail.com>
2023-11-06 15:41:39 +0900

    feat: 사용자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
-  async play() {}
+  async play() {
+    const budget = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+
+    const winningNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
+
+    const bonusNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
+    
+  }
+
+  
 }
 
 export default App;
