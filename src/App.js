9d9ef17fd7229c0c602f98b6c95bbd80f2b0bcd6
brgndy <brgndy96@gmail.com>
2023-11-06 11:00:34 +0900

    feat(winnigLotto): 당첨번호 및 보너스 번호 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { InputView } from './views/InputView.js';
 import { OutputView } from './views/OutputView.js';
 import Money from './domains/Money.js';
 import Lotto from './Lotto.js';
+import WinningLotto from './domains/WinningLotto.js';
 
 class App {
   /**
-   * @type { number }
+   * @type { number } 로또 구입 갯수
    */
 
   #lottoAmount;
@@ -18,12 +19,32 @@ class App {
 
   #lottos;
 
+  /**
+   * @type { Lotto } 당첨 로또 번호
+   */
+
+  #winningLottoNumbers;
+
+  /**
+   * @type { string } 보너스 번호
+   */
+
+  #bonusNumber;
+
+  /**
+   * @type { WinningLotto } 최종적인 당첨 번호
+   */
+
+  #totalWinningLotto;
+
   async play() {
-    await asyncFnHandlerWithError(this.inputMoney, this);
+    await asyncFnHandlerWithError(this.getLottoAmount, this);
     this.printLottos();
+    await asyncFnHandlerWithError(this.getWinningNumbers, this);
+    await asyncFnHandlerWithError(this.getTotalWinningLotto, this);
   }
 
-  async inputMoney() {
+  async getLottoAmount() {
     const money = await InputView.readLine(LOTTO_MESSAGES.input_money);
     this.#lottoAmount = Money.getLottoAmount(money);
     OutputView.divideLine();
@@ -33,6 +54,18 @@ class App {
     OutputView.printLine(LOTTO_MESSAGES.buy_lottos(this.#lottoAmount));
     this.#lottos = Lotto.getPurchasedLottos(this.#lottoAmount);
     this.#lottos.forEach((lotto) => OutputView.printLine(lotto.toStringFromNumbers()));
+    OutputView.divideLine();
+  }
+
+  async getWinningNumbers() {
+    const inputWinningNumbers = await InputView.readLine(LOTTO_MESSAGES.input_winning_numbers);
+    this.#winningLottoNumbers = Lotto.fromInputString(inputWinningNumbers);
+    OutputView.divideLine();
+  }
+
+  async getTotalWinningLotto() {
+    this.#bonusNumber = await InputView.readLine(LOTTO_MESSAGES.input_bonus_number);
+    this.#totalWinningLotto = new WinningLotto(this.#winningLottoNumbers, this.#bonusNumber);
     OutputView.divideLine();
   }
 }
