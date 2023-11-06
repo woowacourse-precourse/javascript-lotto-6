63cce73a042ec94a9a8c1ba07e26ec666b21cac9
8leamnk <silkviewrk@gmail.com>
2023-11-06 17:34:52 +0900

    refactor(validationUtils): validationUtils 를 리팩토링함
    변경 내용:
    - 리팩토링으로 인해 더 이상 사용하지 않는 메서드를 삭제
    - 함수를 선언한 후 객체에 담는 방식에서 객체 안의 메서드로 선언하는 방식으로 변경

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -1,30 +1,20 @@
 import MESSAGE from '../constants/message.js';
 import VALUE from '../constants/value.js';
 
-function checkNumber(number) {
-  if (!Number.isSafeInteger(number)) {
-    throw new Error(MESSAGE.error.notNumber);
-  }
-}
-
-function checkNumberRegExp(string) {
-  if (VALUE.notNumber.test(string)) {
-    throw new Error(MESSAGE.error.notNumber);
-  }
-}
-
-function checkRange(number) {
-  const { start, end } = VALUE.range;
-
-  if (number < start || number > end) {
-    throw new Error(MESSAGE.error.rangeLotto);
-  }
-}
-
 const validationUtils = {
-  checkNumber,
-  checkNumberRegExp,
-  checkRange,
+  checkNumber(number) {
+    if (!Number.isSafeInteger(number)) {
+      throw new Error(MESSAGE.error.notNumber);
+    }
+  },
+
+  checkRange(number) {
+    const { start, end } = VALUE.range;
+
+    if (number < start || number > end) {
+      throw new Error(MESSAGE.error.rangeLotto);
+    }
+  },
 };
 
 export default validationUtils;
