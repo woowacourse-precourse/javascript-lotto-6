c3697e8d97da11705f960a4caf7236341c3c422d
ChanYong Park <krap5075@naver.com>
2023-11-06 15:53:00 +0900

    feat: 에러메시지 상수로 추가

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
-const REWARD = Object.freeze({
+export const REWARD = Object.freeze({
   FIRST: 2000000000,
   SECOND: 30000000,
   THIRD: 1500000,
@@ -6,4 +6,11 @@ const REWARD = Object.freeze({
   FIFTH: 5000
 });
 
-export default REWARD;
+export const ERROR_MSG = Object.freeze({
+  IS_NOT_NUMBER: '[ERROR] 숫자로만 입력해주세요.',
+  INVALID_NUMBER: '[ERROR] 양의 정수로만 입력해주세요',
+  CAN_NOT_DIVIDE: '[ERROR] 1000원 단위로 금액을 입력해주세요',
+  INVALID_NUMBERS_LENGTH: '[ERROR] 당첨 번호는 6개의 숫자를 입력해주세요',
+  NUMBER_DUPLICATED: '[ERROR] 당첨 번호는 중복 없이 입력해주세요.',
+  INVALID_BONUS_NUMBER: '[ERROR] 보너스 번호는 1개만 입력해주세요'
+});
