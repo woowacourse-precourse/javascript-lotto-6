2f1d06ce38807fea6dc451f1c578e4dd8273fb67
ryan-dia <cjfdnjs158@naver.com>
2023-11-06 18:40:27 +0900

    test: PurchaseAmount 클래스 테스트 추가

---
+++ __tests__/model/PurchaseAmount.test.js
@@ -0,0 +1,25 @@
+import PurchaseAmount from '../../src/PurchaseAmount.js';
+
+describe('PurchaseAmount', () => {
+  test('유효한 input값을 넣었을 때 유요한 instance여야 합니다.', () => {
+    const validInput = '5000';
+    const purchaseAmount = PurchaseAmount.of(validInput);
+    expect(PurchaseAmount.of).toBeInstanceOf(Function);
+    expect(purchaseAmount).toBeInstanceOf(PurchaseAmount);
+  });
+
+  test('숫자가 아닌 문자가 포함된 값을 입력하였을 때 예외를 발생시켜야합니다.', () => {
+    const invalidInput = 'invalid';
+    expect(() => PurchaseAmount.of(invalidInput)).toThrow('[ERROR]');
+  });
+
+  test('1000원 단위로 구매하지 않았을 때 예외를 발생시켜야합니다.', () => {
+    const invalidInput = 999; //
+    expect(() => PurchaseAmount.of(invalidInput)).toThrow('[ERROR]');
+  });
+
+  test('최대 구입금액을 초과하였을 때 예외를 발생시켜야합니다.', () => {
+    const invalidInput = 100_500;
+    expect(() => PurchaseAmount.of(invalidInput)).toThrow('[ERROR]');
+  });
+});
