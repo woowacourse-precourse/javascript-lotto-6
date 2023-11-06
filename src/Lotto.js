5dd4e9edad2a68f87e5a1c228444511def1f6245
Kidprogramer1039 <bnpo5231@naver.com>
2023-11-06 15:41:15 +0900

    feat: 로또 파일 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { LOTTO_RANGE, ERROR_MESSAGE } from "./constants/constants";
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,12 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR_MESSAGE.lottoCount);
     }
   }
 
   // TODO: 추가 기능 구현
+
 }
 
 export default Lotto;
