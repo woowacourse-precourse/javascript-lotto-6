e8545b7899b73554d3b2072359910e7748f78136
JUNU <bokjunwoo@gmail.com>
2023-11-07 03:20:34 +0900

    add : 에러메세지 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -11,6 +11,11 @@ export const ERROR_MESSAGES = {
   MONEY_NOT_NUMERIC: '[ERROR] 금액은 숫자로 입력해야 합니다.',
   AMOUNT_LESS_THAN_1000: '[ERROR] 1000원보다 작은 수는 입력할 수 없습니다.',
   NOT_IN_1000_UNIT: '[ERROR] 1000원 단위로 입력해주세요.',
+  INVALID_LENGTH: '[ERROR] 정확히 6개의 숫자를 입력해주세요.',
+  NON_NUMERIC: '[ERROR] 숫자가 아닌 값이 포함되어 있습니다.',
+  OUT_OF_RANGE: '[ERROR] 1에서 45 사이의 숫자를 입력해주세요.',
+  DUPLICATE: '[ERROR] 중복된 숫자가 있습니다.',
+  DUPLICATE_BONUS: '[ERROR] 보너스 번호가 당첨 번호와 중복될 수 없습니다.',
 };
 
 export const LOTTO_PRICE = 1000;
