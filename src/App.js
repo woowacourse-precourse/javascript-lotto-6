5d71743bbcac22967bd8e9b35c9173effc39789a
ReturnReason <reasonz@daum.net>
2023-11-06 22:13:01 +0900

    feat(LottoGameFactory): 팩토리 클래스 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import LottoGameController from './controller/LottoGameController.js';
-import LottoTickets from './model/LottoTickets.js';
+import LottoGameFactory from './factories/LottoGameFactory.js';
 import RandomNumberStrategy from './model/strategies/RandomNumberStrategy.js';
-import InputView from './view/InputView.js';
-import OutputView from './view/OutputView.js';
 
 class App {
+  constructor(strategy) {
+    this.strategy = strategy || new RandomNumberStrategy();
+    this.factory = new LottoGameFactory();
+  }
+
   async play() {
-    const inputView = new InputView();
-    const outputView = new OutputView();
-    const lottoTickets = new LottoTickets();
-    const randomNumberGeneration = new RandomNumberStrategy();
-    const lottoGameController = new LottoGameController({
-      lottoTickets,
-      randomNumberGeneration,
-      inputView,
-      outputView,
-    });
-    await lottoGameController.start();
+    const lottoGame = this.factory.createLottoGame();
+    await lottoGame.start();
   }
 }
 
