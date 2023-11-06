ec79113192de83c9f835210a1505a0a15c67ccaf
hyzzzzy <shinhj4068@naver.com>
2023-11-06 22:45:00 +0900

    feat(App): 로또 생성과 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import { printMessage, readLineAsync } from './utils';
-import { 
-  isNumber,
-  isNaturalNumber,
-  isAboveMinCost,
-} from './utils';
-import { MESSAGE } from './constants';
+import { isDivideMinCost } from './utils';
+import { MESSAGE, SETTING } from './constants';
+import LottoShop from './LottoShop';
 
 class App {
+  #lottoShop = new LottoShop();
+  
   async play() {
-    await this.buyLotto();
+    const cost = await this.payLottoCost();
+    const lottos = this.getLotto(cost);
+    this.printLotto(lottos);
   }
 
-  async buyLotto() {
+  async payLottoCost() {
     try {
       const cost = await readLineAsync(MESSAGE.input_cost);
-      isNumber(cost);
-      isNaturalNumber(cost);
-      isAboveMinCost(cost);
+      isDivideMinCost(cost);
+      return cost;
     } catch (error) {
       printMessage(error.message);
-      await this.buyLotto();
+      await this.payLottoCost();
     }
+  }
+
+  getLotto(cost) {
+    const ticket = Number(cost) / SETTING.lotto_cost;
+    return this.#lottoShop.createLotto(ticket);
+  }
+
+  printLotto(lottos) {
+    printMessage(`${lottos.length}${MESSAGE.sell}`);
+    lottos.forEach((lotto) => {
+      const formatedLotto = lotto.join(', ');
+      printMessage(`[${formatedLotto}]`);
+    });
   }
 }
 
