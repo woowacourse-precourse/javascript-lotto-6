153eaead6440ea4f3905f00b35e60b8ead575efa
gyucheolshin <nsfsgc2295@gmail.com>
2023-11-07 02:57:59 +0900

    feat : 유틸 및 상수 초기화

---
+++ src/common/utils.js
@@ -0,0 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export const readLineAsync = (input) => {
+  return MissionUtils.Console.readLineAsync(input);
+};
+
+export const print = (message) => {
+  return MissionUtils.Console.print(message);
+};
+
+export const lottoRandomNumber = (min, max, length) => {
+  return MissionUtils.Random.pickUniqueNumbersInRange(min, max, length);
+};
