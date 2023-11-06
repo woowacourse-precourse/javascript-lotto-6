e93dbe9dc7b761c1bc4d67b477dc92c6ce58bd4a
laon2019 <laon5987@nate.com>
2023-11-06 21:32:06 +0900

    test(UserLottoModel.js): UserLottoModel 클래스 테스트 생성

---
+++ __tests__/UserLottoModel.js
@@ -0,0 +1,31 @@
+import UserLottoModel from '../src/domain/UserLottoModel';
+import Lotto from '../src/domain/Lotto';
+
+describe('UserLottoModel 클래스 테스트', () => {
+  let userLottoModel;
+
+  beforeEach(() => {
+    userLottoModel = new UserLottoModel();
+  });
+
+  test('함수가 로또 티켓을 생성 (5개)', () => {
+    userLottoModel.generateLottoTicket(1000 * 5);
+    const lottoTickets = userLottoModel.getLottoTickets();
+    expect(Array.isArray(lottoTickets)).toBe(true);
+    expect(lottoTickets.length).toBe(5); 
+  });
+
+  test('함수가 로또 티켓을 생성 (3개)', () => {
+    userLottoModel.generateLottoTicket(3000);
+    const lottoTickets = userLottoModel.getLottoTickets();
+    expect(Array.isArray(lottoTickets)).toBe(true);
+    expect(lottoTickets.length).toBe(3);
+  });
+
+  test('함수가 로또 티켓을 생성 (0개)', () => {
+    userLottoModel.generateLottoTicket(0);
+    const lottoTickets = userLottoModel.getLottoTickets();
+    expect(Array.isArray(lottoTickets)).toBe(true);
+    expect(lottoTickets.length).toBe(0);
+  });
+});
