3833c2549e221bc60009a56ab5315a9615051aa0
aaminha <dks4857@gmail.com>
2023-11-06 20:03:10 +0900

    fix: ErrorMessage 내부의 메시지 내용 및 상수명 수정

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -4,9 +4,9 @@ const ERROR_MESSAGE = Object.freeze({
   INVALID_NUMBER: `${ERROR_SYMBOL} 숫자만 입력해주세요. \n`,
   INVALID_PURCHASE_UNIT: `${ERROR_SYMBOL} 1000원 단위로 입력해주세요. \n`,
   INVALID_RANGE: `${ERROR_SYMBOL} 1~45 사이의 숫자를 입력해주세요. \n`,
-  INVALID_NUMBER_OF_WINNING_NUMBER: `${ERROR_SYMBOL} 6개의 숫자를 입력해주세요. \n`,
-  INVALID_DUPLICATE_WINNING_NUMBER: `${ERROR_SYMBOL} 숫자가 겹치지 않게 입력해주세요. \n`,
-  INVALID_DUPLICATE_BONUS_NUMBER: `${ERROR_SYMBOL} 당첨 번호와 겹치지 않는 값을 입력해주세요. \n`,
+  INVALID_NUMBER_OF_WINNING_NUMBERS: `${ERROR_SYMBOL} 6개의 숫자를 입력해주세요. \n`,
+  INVALID_DUPLICATE_WINNING_NUMBERS: `${ERROR_SYMBOL} 숫자가 중복되지 않게 입력해주세요. \n`,
+  INVALID_DUPLICATE_BONUS_NUMBER: `${ERROR_SYMBOL} 당첨 번호와 중복되지 않는 값을 입력해주세요. \n`,
 });
 
 export default ERROR_MESSAGE;
