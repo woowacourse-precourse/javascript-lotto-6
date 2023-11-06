2110b9f6759191c0998b49f999cf0946f731abeb
mieum12 <qpdlqltb1215@gmail.com>
2023-11-07 02:50:09 +0900

    fix: Lottos getter 추가

--- src/domain/Lottos.js
+++ src/domain/Lottos.js
@@ -1,46 +1,43 @@
-import {Lotto} from "./Lotto.js";
 import {LottosDto} from "./dto/LottosDto.js";
 
 export class Lottos {
-    /**
-     * @type {Lotto[]}
-     */
-    #manylotto
+  /**
+   * @type {Lotto[]}
+   */
+  #manyLottos;
 
-    /**
-     *
-     * @param {Lotto[]} boughtLottos
-     * @description - boughtLottos를 돌면서 Lotto로 보내 로또 한장을 생성해서 모두 가져옴
-     */
+  /**
+   *
+   * @param {Lotto[]} boughtLottos
+   * @description - boughtLottos를 돌면서 Lotto로 보내 로또 한장을 생성해서 모두 가져옴
+   */
 
-    constructor(boughtLottos) {
-        this.#manylotto = boughtLottos
-    }
+  constructor(boughtLottos) {
+    this.#manyLottos = boughtLottos;
+  }
 
-    /**
-     *
-     * @return {LottosDto}
-     * @description [LottosDto를 만들어주는 함수]
-     *
-     * manyLotto는 [Lotto, Lotto, Lotto ...]의 형태이다
-     * 그것을 [LottoDto, LottoDto, LottoDto ...]로 만들기! :manyLottoDto
-     * Lotto에 돌려서 만든 manyLottosDto를 LottosDto로 보내준다
-     */
+  /**
+   *
+   * @return {Lotto[]}
+   */
+  get lottos() {
+    //밖에선 lottos. 어쩌구로 사용
 
-    makeLottosDto() {
-        const manyLottoDto = this.#manylotto.map((lotto) => lotto.makeLottoDto())
-        return new LottosDto(manyLottoDto)
-    }
+    return this.#manyLottos;
+  }
 
-    /**
-     * @description - 추첨 기계로 로또 전부 추첨하는 메소드
-     * @param lottoMachine
-     */
-    drawAllBy(lottoMachine) {
-        //하나씩 돌면서 추첨보내기
-        this.#manylotto.forEach((lotto) => {
-            lotto.drawBy(lottoMachine)
-        })
-    }
+  /**
+   *
+   * @return {LottosDto}
+   * @description [LottosDto를 만들어주는 함수]
+   *
+   * manyLotto는 [Lotto, Lotto, Lotto ...]의 형태이다
+   * 그것을 [LottoDto, LottoDto, LottoDto ...]로 만들기! :manyLottoDto
+   * Lotto에 돌려서 만든 manyLottosDto를 LottosDto로 보내준다
+   */
 
+  makeLottosDto() {
+    const manyLottoDto = this.#manyLottos.map((lotto) => lotto.makeLottoDto());
+    return new LottosDto(manyLottoDto);
+  }
 }
