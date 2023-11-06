ab592a694733b80d4f044f4b61cffca1f3c379af
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-06 18:08:48 +0900

    [feat] : Add 로또 당첨 결과 확인 함수

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,13 +2,13 @@ import {
   LOTTO_NUMBER_COUNT,
   MAX_LOTTO_NUMBER,
   MIN_LOTTO_NUMBER,
-} from "./constants/standard";
+} from "./constants/standard.js";
 import {
   ERROR_NUMBER_COUNT,
   ERROR_NUMBER_DUPLICATION,
   ERROR_NUMBER_RANGE,
   ERROR_NUMBER_TYPE,
-} from "./constants/validate";
+} from "./constants/validate.js";
 
 class Lotto {
   #numbers;
@@ -59,16 +59,70 @@ class Lotto {
     return this.#numbers;
   }
 
-  compareResult(issuedNumbers, bonusNumber) {
-    const compareResult = [0, 0, 0, 0, 0];
+  async compareResult(issuedNumbers, bonusNumber) {
+    const compareResult = [];
+
     issuedNumbers.map((issuedNumber, index) => {
       compareResult.push(0);
-      issuedNumber.include(bonusNumber) && (compareResult[index] += 0.5);
+      issuedNumber.includes(+bonusNumber) && (compareResult[index] += 0.5);
       this.#numbers.map((winningNumber) => {
-        issuedNumber.include(winningNumber) && (compareResult[index] += 1);
+        issuedNumber.includes(+winningNumber) && (compareResult[index] += 1);
       });
     });
     return compareResult;
+  }
+
+  async winningResult(resultData, result) {
+    switch (result) {
+      case 3:
+      case 3.5: {
+        resultData.totalProfits += 5000;
+        resultData.lottoResult[0] += 1;
+        break;
+      }
+      case 4:
+      case 4.5: {
+        resultData.totalProfits += 50000;
+        resultData.lottoResult[1] += 1;
+        break;
+      }
+      case 5: {
+        resultData.totalProfits += 1500000;
+        resultData.lottoResult[2] += 1;
+        break;
+      }
+      case 5.5: {
+        resultData.totalProfits += 30000000;
+        resultData.lottoResult[3] += 1;
+        break;
+      }
+      case 6:
+      case 6.5: {
+        resultData.totalProfits += 2000000000;
+        resultData.lottoResult[4] += 1;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    return resultData;
+  }
+
+  async result(issuedNumbers, bonusNumber) {
+    let resultData = {
+      lottoResult: [0, 0, 0, 0, 0, 0], // 일치 개수 : [3개, 4개, 5개, 5개 + 보너스. 6개]
+      totalProfits: 0,
+    };
+
+    const comparedResult = await this.compareResult(issuedNumbers, bonusNumber);
+
+    comparedResult.forEach(async (result) => {
+      resultData = await this.winningResult(resultData, result);
+    });
+
+    return resultData;
   }
 }
 
