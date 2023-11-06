9a10f00f32d55a492f267f6c969bc51caddc91e2
starcradle101 <dreamerdev12@gmail.com>
2023-11-06 16:14:57 +0900

    🛠️Feat($Errors): Errors 모듈 구현 오류를 객체로 저장한 Errors.js 모듈 추가

---
+++ src/constants/Errors.js
@@ -0,0 +1,24 @@
+export class CustomError extends Error {
+  constructor(name, message) {
+    super(message);
+    this.name = name;
+  }
+}
+
+export const ERROR_MESSAGES = {
+  invalidLottoPurchaseAmount: '[ERROR] 로또 구입 금액은 1000원 단위입니다.',
+  invalidLottoPurchaseAmountZero:
+    '[ERROR] 로또 구입 금액은 최소 1000원 부터입니다.',
+  invalidLottoPurchaseAmountType:
+    '[ERROR] 로또 구입 금액에 숫자가 아닌 다른 문자가 입력될 수 없습니다.',
+  invalidLottoNumberRange:
+    '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+  invalidLottoNumberType:
+    '[ERROR] 로또 번호에 숫자가 아닌 다른 문자가 입력될 수 없습니다.',
+  invalidLottoNumberDuplicate: '[ERROR] 로또 번호는 중복될 수 없습니다.',
+  invalidLottoNumberLength: '[ERROR] 로또 번호의 개수는 6개입니다.',
+  invalidBonusNumberType:
+    '[ERROR] 보너스 번호에 숫자가 아닌 다른 문자가 입력될 수 없습니다.',
+  invalidBonusNumberRange:
+    '[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.',
+};
