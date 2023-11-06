d86ade61ac48bb205c65d83c0530b22db9f382d8
SU_VIN <pyotnqls25@naver.com>
2023-11-06 16:40:47 +0900

    Feat: 총수익 로직 구현

--- src/LottoEvaluator.js
+++ src/LottoEvaluator.js
@@ -3,50 +3,61 @@ class LottoEvaluator {
     this.lottoList = lottoList;
     this.winningNumber = winningNumber;
     this.bonusNumber = bonusNumber;
+    this.prizeMoney = {
+      first: 2000000000,
+      second: 30000000,
+      third: 1500000,
+      fourth: 50000,
+      fifth: 5000,
+    };
   }
 
   evaluateLotto() {
-    const result = { first: 0, second: 0, third: 0, fourth: 0, fifth: 0 };
-
-    this.lottoList.forEach((lotto) => {
-      const compareLotto = this.getCompareLotto(lotto);
-      this.updateResult(result, compareLotto);
-    });
-
+    const result = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+      earnings: 0,
+    };
+    this.lottoList.forEach((lotto) => this.evaluateSingleLotto(lotto, result));
     return result;
   }
 
+  evaluateSingleLotto(lotto, result) {
+    const compareLotto = this.getCompareLotto(lotto);
+    this.updateResult(result, compareLotto);
+  }
+
   getCompareLotto(lotto) {
-    const compareCount = lotto.filter((number) =>
+    const matchCount = lotto.filter((number) =>
       this.winningNumber.includes(number)
     ).length;
     const hasBonus = lotto.includes(this.bonusNumber);
-    return { compareCount, hasBonus };
+    return { matchCount, hasBonus };
   }
 
   updateResult(result, compareLotto) {
-    switch (compareLotto.compareCount) {
+    switch (compareLotto.matchCount) {
       case 6:
-        result.first += 1;
+        this.addWinning(result, 'first');
         break;
       case 5:
-        this.bonusOrNotBonus(result, compareLotto.hasBonus);
+        this.addWinning(result, compareLotto.hasBonus ? 'second' : 'third');
         break;
       case 4:
-        result.fourth += 1;
+        this.addWinning(result, 'fourth');
         break;
       case 3:
-        result.fifth += 1;
+        this.addWinning(result, 'fifth');
         break;
     }
   }
 
-  bonusOrNotBonus(result, hasBonus) {
-    if (hasBonus) {
-      result.second += 1;
-    } else {
-      result.third += 1;
-    }
+  addWinning(result, rank) {
+    result[rank] += 1;
+    result.earnings += this.prizeMoney[rank];
   }
 }
 
