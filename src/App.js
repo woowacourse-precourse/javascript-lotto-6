e1ed87d82820446d6f324be3209d97e3213cd260
gillyongs <khs20010327@gmail.com>
2023-11-07 01:55:18 +0900

    feat: 로또 결과 출력 구현

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App {
     const numberOfLottoTickets = Math.floor(purchaseAmount / 1000);
     const lottoTickets = this.generateLottoTickets(numberOfLottoTickets);
 
-    Console.print(`${numberOfLottoTickets}개를 구매했습니다.`);
+    Console.print(`\n${numberOfLottoTickets}개를 구매했습니다.`);
 
     lottoTickets.forEach((ticket, index) => {
       Console.print(ticket.getNumbers().sort((a, b) => a - b));
@@ -26,6 +26,9 @@ class App {
 
     const winningNumbers = await this.inputWinningNumbers();
     const bonusNumber = await this.inputBonusNumber();
+
+    const result = this.calculateResults(lottoTickets, winningNumbers, bonusNumber);
+    this.printResults(result);
 
   }
 
@@ -44,7 +47,7 @@ class App {
     let winningNumbersMap;
     while (true) {
       try {
-        winningNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n")
+        winningNumbers = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n")
         winningNumbersMap = winningNumbers.split(',').map(Number);
         if (!this.validateNumbers(winningNumbersMap)) {
           throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
@@ -64,7 +67,7 @@ class App {
     let bonusNumber;
     while (true) {
       try {
-        bonusNumber = parseInt(await Console.readLineAsync("보너스 번호를 입력해 주세요.\n"));
+        bonusNumber = parseInt(await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n"));
         if (!this.validateNumber(bonusNumber)) {
           throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
@@ -81,6 +84,73 @@ class App {
   
   validateNumber(number) {
     return number >= 1 && number <= 45;
+  }
+
+  calculateResults(lottoTickets, winningNumbers, bonusNumber) {
+    const results = {
+      0: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      5.1: 0,
+      6: 0,
+    };
+
+    for (const ticket of lottoTickets) {
+      const numbers = ticket.getNumbers();
+      const matchingNumbers = numbers.filter((number) => winningNumbers.includes(number));
+      const matchingCount = matchingNumbers.length;
+
+      if (matchingCount === 6) {
+        results[6]++;
+      } else if (matchingCount === 5 && numbers.includes(bonusNumber)) {
+        results[5.1]++;
+      } else {
+        results[matchingCount]++;
+      }
+    }
+
+    return results;
+  }
+
+  printResults(results) {
+    Console.print("\n당첨 통계");
+    Console.print("---");
+
+    for (const key in results) {
+      if (results.hasOwnProperty(key)) {
+        const count = results[key];
+        if(key === '5.1'){
+          Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`);
+        }
+        else if (key>=3) {
+          let prize = 0;
+          if (key === '3') prize = 5000;
+          else if (key === '4') prize = 50000;
+          else if (key === '5') prize = 1500000;
+          else if (key === '5.1') prize = 30000000;
+          else if (key === '6') prize = 2000000000;
+
+          Console.print(`${key}개 일치 (${prize}원) - ${count}개`);
+        }
+      }
+    }
+
+    const totalPrize = Object.keys(results).reduce((acc, key) => {
+      let prize = 0;
+      if (key === '3') prize = 5000;
+      else if (key === '4') prize = 50000;
+      else if (key === '5') prize = 1500000;
+      else if (key === '5.1') prize = 30000000;
+      else if (key === '6') prize = 2000000000;
+
+      return acc + results[key] * prize;
+    }, 0);
+    const totalPurchaseAmount = Object.keys(results).reduce((acc, key) => acc + results[key], 0) * 1000;
+    const profitRate = (totalPrize / totalPurchaseAmount) * 100;
+    Console.print(`총 수익률은 ${profitRate.toFixed(1)}%입니다.`);
   }
 }
 
