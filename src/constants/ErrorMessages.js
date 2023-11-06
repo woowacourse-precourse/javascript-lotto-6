a4ec11dc468bff094772a5faac769df7a3c999cc
Trophy198 <spsp8426@naver.com>
2023-11-07 00:27:49 +0900

    feat : 문자열이 들어오는 경우 상수 추가

--- src/constants/ErrorMessages.js
+++ src/constants/ErrorMessages.js
@@ -1,5 +1,6 @@
 export const ERROR_MESSAGES = {
-  INVALID_AMOUNT: '[ERROR] 유효하지 않은 금액입니다. 금액은 1000원 이상의 양수여야 합니다.',
+  INVALID_FORMAT:
+    '[ERROR] 입력값중 사용할 수 없는 문자열이 포함되어 있습니다. 숫자만 입력가능합니다.',
   AMOUNT_NOT_IN_UNITS: '[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.',
   EMPTY_NUMBER: '[ERROR] 모든 당첨 번호를 쉼표(,)로 구분하여 입력해야 합니다.',
   NOT_SIX_NUMBERS: '[ERROR] 당첨 번호는 쉼표(,)로 구분된 6개의 숫자여야 합니다.',
