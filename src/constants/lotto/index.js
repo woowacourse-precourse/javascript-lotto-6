734824aef41620754ea2c0f8e7dc3be498883aad
jang_yoonsu <shagrat8546@gmail.com>
2023-11-07 00:26:29 +0900

    feat : 로또룰에 로또 상금 상수 추가

--- src/constants/lotto/index.js
+++ src/constants/lotto/index.js
@@ -4,4 +4,12 @@ export const LOTTO_RULE = Object.freeze({
     MAX: 45,
   },
   LENGTH: 6,
+  PRICE: 1000,
+  WINNING_AMOUNT: {
+    1: 2_000_000_000,
+    2: 30_000_000,
+    3: 1_500_000,
+    4: 50_000,
+    5: 5_000,
+  },
 });
