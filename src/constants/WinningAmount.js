9c6868727b85ad241e1a4bece7dc6d779517ea49
aaminha <dks4857@gmail.com>
2023-11-07 05:49:12 +0900

    feat: 일치하는 숫자 개수에 따른 등수 관련 상수 추가

--- src/constants/WinningAmount.js
+++ src/constants/WinningAmount.js
@@ -1,4 +1,4 @@
-const WINNING_AMOUNT = Object.freeze({
+export const WINNING_AMOUNT = Object.freeze({
   FIRST: 2000000000,
   SECOND: 30000000,
   THIRD: 1500000,
@@ -6,7 +6,7 @@ const WINNING_AMOUNT = Object.freeze({
   FIFTH: 5000,
 });
 
-const WINNING_AMOUNT_STRING = Object.freeze({
+export const WINNING_AMOUNT_STRING = Object.freeze({
   FIRST: Number(WINNING_AMOUNT.FIRST).toLocaleString(),
   SECOND: Number(WINNING_AMOUNT.SECOND).toLocaleString(),
   THIRD: Number(WINNING_AMOUNT.THIRD).toLocaleString(),
@@ -14,4 +14,10 @@ const WINNING_AMOUNT_STRING = Object.freeze({
   FIFTH: Number(WINNING_AMOUNT.FIFTH).toLocaleString(),
 });
 
-export default WINNING_AMOUNT_STRING;
+export const MATCHING_COUNT = Object.freeze({
+  FIRST: 6,
+  SECOND: 5,
+  THIRD: 5,
+  FOURTH: 4,
+  FIFTH: 3,
+});
