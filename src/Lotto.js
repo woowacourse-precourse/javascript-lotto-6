df6d448ec0c118fa69336e6e57b674be0e1b3f4b
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-11-06 20:22:25 +0900

    feat: 로또 번호 생성

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,29 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  get numbers() {
+    return this.#numbers;
+  }
+}
+
+class LottoMachine {
+  #bonusNumber;
+
+  generateLotto() {
+    const number = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return new Lotto(number);
+  }
+  bonusGenerateLotto() {
+    this.bonusNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 1)[0];
+  }
+
+  get bonusNumber() {
+    return this.#bonusNumber;
+  }
 }
 
 export default Lotto;
