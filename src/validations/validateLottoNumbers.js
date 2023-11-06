1e05e14db3cfead47b9328925938b97847df7e81
JunSeok <ppu1234@naver.com>
2023-11-06 23:07:12 +0900

    fix: 확장자 에러 처리

--- src/validations/validateLottoNumbers.js
+++ src/validations/validateLottoNumbers.js
@@ -1,6 +1,6 @@
-import CustomError from '../errors/CustomError';
-import { ERROR_MESSAGES } from '../constants/messages';
-import LOTTO_CONSTANT from '../constants/lotto';
+import CustomError from '../errors/CustomError.js';
+import { ERROR_MESSAGES } from '../constants/messages.js';
+import LOTTO_CONSTANT from '../constants/lotto.js';
 
 const validateLottoNumbers = (numbers) => {
   if (numbers.length !== LOTTO_CONSTANT.count) throw new CustomError(ERROR_MESSAGES.lottoNumberLength);
