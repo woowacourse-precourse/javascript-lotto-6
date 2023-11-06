c7311674bbf5c1650e904525d1ada18809ccb0e5
aaminha <dks4857@gmail.com>
2023-11-07 05:51:25 +0900

    fix: InputView 내 재입력을 위한 try catch 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,17 +1,46 @@
 import { Console } from '@woowacourse/mission-utils';
-import { MESSAGE_GET } from '../constants/Mesaage';
+import { MESSAGE_GET } from '../constants/Message';
+import InputController from '../controllers/InputController';
+import ValidateController from '../controllers/ValidateController';
 
 const InputView = {
-  getPurchaseAmount() {
-    return Console.readLineAsync(MESSAGE_GET.PURCHASE_AMOUNT);
+  getPurchaseLottoAmount() {
+    try {
+      const inputAmountString = Console.readLineAsync(
+        MESSAGE_GET.PURCHASE_AMOUNT,
+      );
+      const inputAmount = InputController.convertNumber(inputAmountString);
+      ValidateController.validatePurchaseLottoAmount(inputAmount);
+      return inputAmount;
+    } catch (error) {
+      Console.print(error.message);
+      return undefined;
+    }
   },
 
   getWinningNumbers() {
-    return Console.readLineAsync(MESSAGE_GET.WINNING_NUMBER);
+    try {
+      const inputNumbersString = Console.readLineAsync(
+        MESSAGE_GET.WINNING_NUMBER,
+      );
+      const inputNumbers = InputController.converList(inputNumbersString);
+      ValidateController.validateWinningNumbers(inputNumbers);
+      return inputNumbers;
+    } catch (error) {
+      Console.print(error.message);
+      return undefined;
+    }
   },
 
   getBonusNumber() {
-    return Console.readLineAsync(MESSAGE_GET.BONUS_NUMBER);
+    try {
+      const inputNumberString = Console.readLineAsync(MESSAGE_GET.BONUS_NUMBER);
+      const inputNumber = InputController.convertNumber(inputNumberString);
+      return inputNumber;
+    } catch (error) {
+      Console.print(error.smessage);
+      return undefined;
+    }
   },
 };
 
