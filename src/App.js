16befa5b5f260db037c931898150075123616ad2
hyewon <hyewon.parkhw@gmail.com>
2023-11-06 16:40:44 +0900

    feat: 로또 티켓 발행 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 
 class App {
   async play() {
     const budget = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    const lottoCount = buyLotto(budget);
+    let lottoTickets = [];
+
+    for (let i=0; i<lottoCount; i++){
+      lottoTickets.push(getLottoTicket())
+    }
 
     const winningNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
 
     const bonusNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
-    
   }
-
-  
 }
 
 export default App;
+
+const buyLotto = (budget) => {
+  if (budget%1000>0) throw new Error("[Error] 구입금액은 1000단위여야 합니다.");
+  return budget/1000
+}
+
+const getLottoTicket = () => {
+  return Random.pickUniqueNumbersInRange(1, 45, 6);
+}
