5ba149b705662d950d0e9c5be2c5dde107227406
swarvy <swarvy0826@naver.com>
2023-11-07 00:40:00 +0900

    refactor : LOTTO_RANK에서 등수 정보와 금액을 관리하도록 변경

--- src/constants/LottoOption.js
+++ src/constants/LottoOption.js
@@ -6,10 +6,32 @@ export const LOTTO_MAGICNUMBER = Object.freeze({
 
 export const LOTTO_PRICE = 1000;
 
-export const LOTTO_PRIZEVALUE = Object.freeze({
-  first: 2_000_000_000,
-  second: 30_000_000,
-  third: 1_500_000,
-  fourth: 50_000,
-  fifth: 5_000,
+export const PROFIT_ROUND_UP_VALUE = 1;
+
+export const LOTTO_RANK = Object.freeze({
+  first: Object.freeze({
+    mainNumber: 6,
+    bonusNumber: false,
+    prizeValue: 2_000_000_000,
+  }),
+  second: Object.freeze({
+    mainNumber: 5,
+    bonusNumber: true,
+    prizeValue: 30_000_000,
+  }),
+  third: Object.freeze({
+    mainNumber: 5,
+    bonusNumber: false,
+    prizeValue: 1_500_000,
+  }),
+  fourth: Object.freeze({
+    mainNumber: 4,
+    bonusNumber: false,
+    prizeValue: 50_000,
+  }),
+  fifth: Object.freeze({
+    mainNumber: 3,
+    bonusNumber: false,
+    prizeValue: 5_000,
+  }),
 });
