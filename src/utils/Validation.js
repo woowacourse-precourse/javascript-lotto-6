6a0f238d09a6c7b42343916571f24c9088ea4336
kimjinwoo <rlawlsdn177@naver.com>
2023-11-06 21:40:06 +0900

    feat($checkPurchaseAmount): 구매 금액 예외 사항 추가
    - ($checkPurchaseAmount): 구매 금액이 0원일 경우 에러를 던진다.

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -24,7 +24,7 @@ export const checkBonusNumbers = (bonusNumber, winningNumbers) => {
 };
 
 export const checkPurchaseAmount = (amount) => {
-    if (isNaN(amount) || amount % LOTTO_PRICE !== ZERO) {
+    if (isNaN(amount) || amount === ZERO || amount % LOTTO_PRICE !== ZERO) {
         throw new Error(ERROR.INVALID_AMOUNT_MESSAGE);
     }
 };
