a85ef9f57ac39a6f8932fd7f67a94e7a307626f6
thewronghand <penfreak77@gmail.com>
2023-11-06 15:32:05 +0900

    feat: 상수 파일 작성

---
+++ src/utils/constants/string.js
@@ -0,0 +1,12 @@
+const read = Object.freeze({
+  moneyAmount: '구입금액을 입력해 주세요.',
+  lottoNumbers: '당첨 번호를 입력해 주세요.',
+  bonusNumber: '보너스 번호를 입력해 주세요.',
+});
+
+const MESSAGE = Object.freeze({
+  read,
+  statsHeader: '당첨 통계\n---',
+});
+
+export default MESSAGE;
