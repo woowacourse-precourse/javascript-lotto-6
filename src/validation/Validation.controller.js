e2df5bbe215c5078853982b563ee6cd87c81f42a
wonkyum-kim <dodo12120@naver.com>
2023-11-06 18:10:46 +0900

    feat: 입력 받은 구입 금액이 1000 단위인지 확인

--- src/validation/Validation.controller.js
+++ src/validation/Validation.controller.js
@@ -1,5 +1,10 @@
 import ValidationService from './Validation.service';
 
+const ERROR_MESSAGES = {
+  isNaN: '[ERROR] 입력한 금액이 숫자가 아닙니다.',
+  notDivided: '[ERROR] 1000 단위의 금액을 입력하세요.';
+}
+
 export default class ValidationController {
   #validationService;
 
@@ -10,7 +15,10 @@ export default class ValidationController {
   // num: string
   validateInputMoney(num) {
     if (!this.#validationService.isNumber(num)) {
-      throw new Error('[ERROR] 입력한 금액이 숫자가 아닙니다.');
+      throw new Error(ERROR_MESSAGES.isNaN);
+    }
+    if (!this.#validationService.isDividedBy1000(num)) {
+      throw new Error(ERROR_MESSAGES.notDivided);
     }
   }
 }
--- src/validation/Validation.service.js
+++ src/validation/Validation.service.js
@@ -2,4 +2,8 @@ export default class ValidationService {
   isNumber(num) {
     return !Number.isNaN(+num);
   }
+
+  isDividedBy1000(num) {
+    return num > 0 && +num % 1000 === 0;
+  }
 }
