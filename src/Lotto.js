ab96e2f399b42b61c7fdc14397224103abb067e4
kwani6684 <jack0910@naver.com>
2023-11-06 14:12:08 +0900

    ✨feat: add compae number with winNumber

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 class Lotto {
   #numbers;
@@ -11,12 +11,22 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
-  printNumbers(){
-    MissionUtils.Console.print(this.#numbers)
+  printNumbers() {
+    MissionUtils.Console.print(this.#numbers);
   }
+  compareWinNumbers(winNumber) {
+    let winCount=0;
+    winNumber.forEach((num) => {
+      if (this.#numbers.includes(Number(num))) {
+        winCount += 1;
+      }
+    });
+    return winCount;
+  }
+  
 
   // TODO: 추가 기능 구현
 }
