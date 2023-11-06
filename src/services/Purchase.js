e5d1b2de5c5933a31a4194a65b3c1bb28fdf84a3
skdbsqls <skdbsqls98@naver.com>
2023-11-06 19:05:58 +0900

    feat: 로또 구입 서비스 구현

---
+++ src/services/Purchase.js
@@ -0,0 +1,25 @@
+import inputs from "../domains/inputs.js";
+import MESSAGE from "../constants/message.js";
+import CONDITION from "../constants/condition.js";
+
+class Purchase {
+  // 로또 구입 금액 입력
+  async inputPrice() {
+    const price = await inputs.inputPrice();
+    this.#validate(price);
+    return Number(price);
+  }
+
+  #validate(price) {
+    // 숫자가 아닌 경우
+    if (!Number.isInteger(price)) {
+      throw new Error(MESSAGE.error.notNumber);
+    }
+    // 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우
+    if (price % CONDITION.unit.price !== 0) {
+      throw new Error(MESSAGE.error.priceUnit);
+    }
+  }
+}
+
+export default Purchase;
