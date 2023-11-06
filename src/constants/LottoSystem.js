6f269feecb847c2ac56681a19a8d02fbe3f81d55
Lee sang Yeop <mma7710@naver.com>
2023-11-06 17:21:17 +0900

    fix(constants): 우승 당첨 개수 상수 변경 - [우승에 필요한 당첨 갯수, 보너스 여부] 순서로 상수 변경

--- src/constants/LottoSystem.js
+++ src/constants/LottoSystem.js
@@ -5,13 +5,11 @@ const LOTTO_SYSTEM = Object.freeze({
 	maximum_number: 45,
 	lotto_numbers: 6,
 
-	first_winning_number: 6,
-	second_winning_number: 5,
-	second_winning_bonus: true,
-	third_winning_number: 5,
-	third_winning_bonus: true,
-	fourth_winning_number: 4,
-	fifth_winning_number: 3,
+	first_winning: [6, false],
+	second_winning: [5, true],
+	third_winning: [5, false],
+	fourth_winning: [4, false],
+	fifth_winning: [3, false],
 
 	first_place_prize: 2000000000,
 	second_place_prize: 30000000,
