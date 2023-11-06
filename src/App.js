0b5027349bb0a14ce1a60e6785a882915cea29bb
rkdcodus <rcd7275@naver.com>
2023-11-06 23:16:48 +0900

    feat: 당첨 번호 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import UserLotto from './UserLotto.js';
+import Lotto from './Lotto.js';
 import View from './View.js';
 
 class App {
   userLotto;
+
+  winningLotto;
 
   async getPrice(){
     const amount = await View.inputAmountOfMoney();
@@ -13,13 +16,19 @@ class App {
     const list = this.userLotto.getList();
     const count = this.userLotto.getCount();
     View.outputUserLottosList(list, count);
+  }
 
+  async getWinningNumber(){
+    let numbers = await View.inputSixWinningNumbers();
+    numbers = numbers.split(',');
+    this.winningLotto = new Lotto(numbers);
   }
 
   async play() {
     await this.getPrice();
     this.userLotto.createLottos();
     this.showLottos();
+    this.getWinningNumber();
   }
 }
 
--- src/View.js
+++ src/View.js
@@ -6,6 +6,11 @@ class View {
     const inputAmount = await Console.readLineAsync('구입 금액을 입력해 주세요.\n')
     return inputAmount;
   } 
+  
+  static async inputSixWinningNumbers(){
+    const numbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n')
+    return numbers;
+  }
 
   static async outputUserLottosList(list, count){
     Console.print(`\n${count}개를 구매했습니다.`)
