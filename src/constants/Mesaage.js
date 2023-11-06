647967e6a16989fb8f5b65d6a58263159d266edc
aaminha <dks4857@gmail.com>
2023-11-06 17:42:07 +0900

    fix: Message 내의 변수 추가

--- src/constants/Mesaage.js
+++ src/constants/Mesaage.js
@@ -9,15 +9,25 @@ export const MESSAGE_GET = Object.freeze({
 export const MESSAGE_PRINT = Object.freeze({
   LOTTO_AMOUNT: '개를 구매했습니다. \n',
   STATISTICS: '당첨 통계 \n--- \n',
-  COUNT_UNIT: '개',
+  COUNT_UNIT: '개 \n',
+  LOTTO_NUMBER_HEAD: '[',
+  LOTTO_NUMBER_TAIL: ']',
   TOTAL_RATE_HEAD: '총 수익률은 ',
   TOTAL_RATE_TAIL: '%입니다. \n',
 });
 
-export const MESSAGE_RESULT_STATISTICS = Object.freeze({
+const RESULT_STATISTICS = Object.freeze({
   FIRST: `6개 일치 (${WINNING_AMOUNT_STRING.FIRST}원) - `,
   SECOND: `5개 일치, 보너스 볼 일치 (${WINNING_AMOUNT_STRING.SECOND}원) - `,
   THIRD: `5개 일치 (${WINNING_AMOUNT_STRING.THIRD}원) - `,
   FOURTH: `4개 일치 (${WINNING_AMOUNT_STRING.FOURTH}원) - `,
   FIFTH: `3개 일치 (${WINNING_AMOUNT_STRING.FIFTH}원) - `,
 });
+
+export const MESSAGE_RESULT_STATISTICS = [
+  RESULT_STATISTICS.FIRST,
+  RESULT_STATISTICS.SECOND,
+  RESULT_STATISTICS.THIRD,
+  RESULT_STATISTICS.FOURTH,
+  RESULT_STATISTICS.FIFTH,
+];
