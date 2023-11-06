200ad6cc6da9ad17d74e987eab3b900b9237bdf8
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-06 17:14:21 +0900

    feat: 메세지 관련 상수파일 추가
    error.js : 에러메세지 상수화
    message : 입력메시지 상수화

---
+++ src/constants/error.js
@@ -0,0 +1,5 @@
+const ERROR = {
+  PURCHASE_AMOUNT: '금액을 1000원 단위로 입력해주세요.',
+};
+
+export default Object.freeze(ERROR);
---
+++ src/constants/message.js
@@ -0,0 +1,5 @@
+const MESSAGE = {
+  INPUT_MONEY: '구입금액을 입력해 주세요.',
+};
+
+export default Object.freeze(MESSAGE);
