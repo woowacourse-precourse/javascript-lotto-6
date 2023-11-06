740a0ed97f09387dcd919f33b5fa3b5ce5d86f72
swarvy <swarvy0826@naver.com>
2023-11-07 01:29:35 +0900

    feat : import문 간단화를 위한validator/index.js 생성

---
+++ src/validator/index.js
@@ -0,0 +1,3 @@
+export { default as validateLottoSeedMoney } from './LottoSeedMoneyValidator.js';
+export { default as validateLottoNumbers } from './LottoNumbersValidator.js';
+export { default as validateLottoBonusNumber } from './LottoBonusNumberValidator.js';
