a5bc2d645eb006a38022170e449da06c114245fd
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-11-06 17:28:23 +0900

    feat: 앱 모듈 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import Handler from "./lib/Handler/index.js";
+import OutputView from "./lib/View/OutputView.js";
+import { ERROR_MESSAGE } from "./lib/Constants.js";
+
 class App {
-  async play() {}
+  #tickets;
+
+  async play() {
+    try {
+      this.#tickets = await Handler.tickets();
+      this.printTickets();
+      const lotto = await Handler.lotto();
+      const bonusNumber = await Handler.bonusNumber(lotto);
+
+      const result = lotto.calcResult(this.#tickets.items, bonusNumber);
+      this.#tickets.result = result;
+      this.printResult();
+    } catch (err) {
+      OutputView.err({ message: err.message });
+    }
+  }
+
+  printTickets() {
+    try {
+      if (!this.#tickets || !this.#tickets.items) {
+        throw new Error(ERROR_MESSAGE.INSTANCE_NOT_INITIALIZED);
+      }
+      OutputView.tickets({ ticketItems: this.#tickets.items });
+    } catch (err) {
+      OutputView.err({ message: err.message });
+    }
+  }
+
+  printResult() {
+    try {
+      if (!this.#tickets || !this.#tickets.result) {
+        throw new Error(ERROR_MESSAGE.INSTANCE_NOT_INITIALIZED);
+      }
+      const { prizeMap, winRate } = this.#tickets.result;
+      OutputView.result({ prizeMap, winRate });
+    } catch (err) {
+      OutputView.err({ message: err.message });
+    }
+  }
 }
 
 export default App;
