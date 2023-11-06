88b289608a4ed6179473f3475c273323416ecb08
ChanYong Park <krap5075@naver.com>
2023-11-06 19:57:42 +0900

    feat: 사용자 입력 예외 처리 함수 추가

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,9 +1,60 @@
+import { ERROR_MSG } from './Constants.js';
+
+const winningNumbersSameCheck = (numbers) => {
+  const set = new Set(numbers);
+  if (set.size !== numbers.length) {
+    return true;
+  }
+  return false;
+};
+
+const winningNumbersCheck = (numbers) => {
+  if (
+    numbers.every(
+      (number) =>
+        !Number.isNaN(parseInt(number, 10)) &&
+        !Number.isInteger(parseInt(number, 10))
+    )
+  ) {
+    return false;
+  }
+  return true;
+};
+// 중복검사 + 양의 정수 검사
+const invalidInputNumbers = (numbers) => {
+  if (!winningNumbersCheck(numbers) && winningNumbersSameCheck(numbers)) {
+    return true;
+  }
+  return false;
+};
+
 export const validateMoney = (money) => {
+  if (!/^\d+$/.test(money)) {
+    throw new Error(ERROR_MSG.IS_NOT_NUMBER);
+  }
   if (parseInt(money, 10) % 1000 !== 0) {
-    throw new Error('[ERROR] 1000원 단위로 금액을 입력해주세요');
+    throw new Error(ERROR_MSG.CAN_NOT_DIVIDE);
   }
 };
 
-export const validateWinningNumbers = (winningNumbers) => {};
+export const validateWinningNumbers = (winningNumbers) => {
+  if (winningNumbers.length !== 6) {
+    throw new Error(ERROR_MSG.INVALID_NUMBERS_LENGTH);
+  }
+  if (invalidInputNumbers(winningNumbers)) {
+    throw new Error(ERROR_MSG.INVALID_NUMBER);
+  }
+};
 
-export const validateBonusNumber = () => {};
+export const validateBonusNumber = (bonusNumber) => {
+  const convertedBonusNumber = parseInt(bonusNumber, 10);
+  if (bonusNumber.length !== 1) {
+    throw new Error(ERROR_MSG.INVALID_BONUS_NUMBER);
+  }
+  if (Number.isNaN(convertedBonusNumber)) {
+    throw new Error(ERROR_MSG.IS_NOT_NUMBER);
+  }
+  if (convertedBonusNumber <= 0 || !Number.isInteger(convertedBonusNumber)) {
+    throw new Error(ERROR_MSG.INVALID_NUMBER);
+  }
+};
