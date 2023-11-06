ffb6149ff05830900721a60639a6ba8784bb6004
Sohye <saeyan2477@gmail.com>
2023-11-06 21:59:06 +0900

    feat: Lotto 클래스 구현
    - 케이스별 검증

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
 import { Validation } from "./Validation.js";
 import { LOTTO_RULE, GAME_MESSAGES } from "./Constants.js";
+import { CustomError } from "./CustomError.js";
+
+const LENGTH = LOTTO_RULE.LENGTH;
+const { MIN, MAX } = LOTTO_RULE.RANGE;
 
 class Lotto {
   #numbers;
@@ -11,31 +15,31 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (!Validation.isLength(numbers, LENGTH)) {
+      throw new CustomError(GAME_MESSAGES.ERROR.LOTTO.NO_LENGTH);
     }
 
-    if(!Validation.isUnique(numbers)){
-      throw new Error("[ERROR] 로또 번호는 유일해야 합니다.")
+    if (!Validation.isUnique(numbers)) {
+      throw new CustomError(GAME_MESSAGES.ERROR.LOTTO.NO_UNIQUE);
     }
 
-    numbers.every((number)=>{
-      if(!Validation.isNumber(number)){
-        throw new Errow("[ERROR] 로또 번호는 숫자여야 합니다.")
+    numbers.every((number) => {
+      if (!Validation.isNumber(number)) {
+        throw new CustomError(GAME_MESSAGES.ERROR.LOTTO.NO_NUMBER);
       }
 
-      if(!Validation.isPositive(number)){
-        throw new Errow("[ERROR] 로또 번호는 양수여야 합니다.")
+      if (!Validation.isPositive(number)) {
+        throw new CustomError(GAME_MESSAGES.ERROR.LOTTO.NO_POSITIVE);
       }
 
-      if(!Validation.isOnRange(number)){
-        throw new Errow("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
+      if (!Validation.isOnRange(number)) {
+        throw new CustomError(GAME_MESSAGES.ERROR.LOTTO.NO_RANGE);
       }
 
-      if(!Validation.isInteger(number)){
-        throw new Errow("[ERROR] 로또 번호는 정수여야 합니다.")
+      if (!Validation.isInteger(number)) {
+        throw new CustomError(GAME_MESSAGES.ERROR.LOTTO.NO_INTEGER);
       }
-    })
+    });
   }
 
   getNumbers() {
