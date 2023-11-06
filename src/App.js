0de96fb699a8e7fdc4180a142fc096d0a58e25f5
hongjimin00 <chunsim015@gmail.com>
2023-11-06 17:18:18 +0900

    feat: 구입 금액에 해당하는 만큼의 로또 발행

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ class App {
     );
     if (lottoAmount % 1000 !== 0)
       throw new Error("[ERROR] 구입 금액은 1,000원 단위입니다.");
+
+    const lottos = this.buyLotto(lottoAmount);
+  }
+
+  buyLotto(amount) {
+    const countLotto = amount / 1000;
+    const lottoList = [];
+
+    for (let i = 0; i < countLotto; i++) {
+      const lottoNum = Random.pickUniqueNumbersInRange(1, 45, 6);
+      const lotto = new Lotto(lottoNum);
+      lottoList.push(lotto);
+    }
+
+    return lottoList;
   }
 }
 
