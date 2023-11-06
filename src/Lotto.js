276d345d47952a46723a5c49a0d182f611142d71
hyzzzzy <shinhj4068@naver.com>
2023-11-06 22:40:48 +0900

    feat(Lotto): 로또 클래스 생성 6개의 요소가 있는지 확인하는 유효성 검사 로직 추가 중복된 요소가 있는지 확인하는 유효성 검사 로직 추가 numbers 필드 접근자 설정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,16 @@
+import { hasSixValues, isUnique } from './utils';
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    hasSixValues(numbers);
+    isUnique(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  get numbers() {
+    return this.#numbers;
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
