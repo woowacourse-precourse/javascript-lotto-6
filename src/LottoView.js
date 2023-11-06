81821dc23d2b5ffde6d697175b81f9fe70a5e2bc
hong <ahi200@naver.com>
2023-11-06 20:48:26 +0900

    feat: 당첨 통계를 출력하는 함수 구현

---
+++ src/LottoView.js
@@ -0,0 +1,26 @@
+import { Console } from '@woowacourse/mission-utils';
+import WinningStatistic from './WinningStatistic.js';
+const MESSAGE_WINNING_STATISTICS = '당첨 통계\n---';
+
+class LottoView {
+  static printStatistics(winningStatistics) {
+    Console.print(MESSAGE_WINNING_STATISTICS);
+    Console.print(
+      `3개 일치 (5,000원) - ${winningStatistics.getFifthPrize()}개`
+    );
+    Console.print(
+      `4개 일치 (50,000원) - ${winningStatistics.getFourthPrize()}개`
+    );
+    Console.print(
+      `5개 일치 (1,500,000원) - ${winningStatistics.getThirdPrize()}개`
+    );
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningStatistics.getSecondPrize()}개`
+    );
+    Console.print(
+      `6개 일치 (2,000,000,000원) - ${winningStatistics.getFirstPrize()}개`
+    );
+    Console.print(`총 수익률은 ${winningStatistics.getEaringRate()}%입니다.`);
+  }
+}
+export default LottoView;
