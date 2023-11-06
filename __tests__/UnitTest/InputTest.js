015fca2eee849b98294e837935adb2adeecb8f5c
lurgi <wahoo9040@naver.com>
2023-11-06 20:02:13 +0900

    test(InputTest): 당첨 번호 입력

--- __tests__/UnitTest/InputTest.js
+++ __tests__/UnitTest/InputTest.js
@@ -22,3 +22,24 @@ describe("로또 구입 금액 입력 테스트", () => {
     await expect(Input.readMoneyBuyingLotto()).rejects.toThrow("[ERROR]");
   });
 });
+
+// eslint-disable-next-line
+describe("당첨 번호 입력 테스트", () => {
+  const PASS_CASES = ["7,11,21,34,42,45", "1,3,9,21,33,34", "4,5,6,7,8,41"];
+  test.each(PASS_CASES)("당첨 번호 성공", async (input) => {
+    mockQuestions(input);
+    const answer = await Input.readLottoNumbers();
+    expect(answer).toStrictEqual(input);
+  });
+
+  const FAIL_CASES = [
+    "7,11,21,34,42,21",
+    "0,1,2,3,4,5",
+    "4,5,6,7,8,46",
+    "1,2,3",
+  ];
+  test.each(FAIL_CASES)("당첨 번호 실패", async (input) => {
+    mockQuestions(input);
+    await expect(Input.readLottoNumbers()).rejects.toThrow("[ERROR]");
+  });
+});
