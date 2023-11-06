da0c552abfa2e6ad06b7da2710a3dae025868012
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 03:53:03 +0900

    :sparkles: feat: 컨트롤러에 구입한 로또 출력 기능 연결

--- src/controllers/LottoGameController.js
+++ src/controllers/LottoGameController.js
@@ -1,6 +1,7 @@
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
 import PurchaseAmount from '../domain/PurchaseAmount.js';
+import LottoGame from '../domain/LottoGame.js';
 
 class LottoGameController {
   constructor() {}
@@ -9,7 +10,13 @@ class LottoGameController {
     try {
       const purchaseAmount = await InputView.getPurchaseAmount();
       const amount = new PurchaseAmount(purchaseAmount).getAmount();
-      console.log(amount);
+
+      OutputView.printPurchaseAmount(amount);
+
+      const lottoGame = new LottoGame(amount);
+      const purchasedLotto = lottoGame.getPurchasedLotto();
+
+      OutputView.printPurchasedLotto(purchasedLotto);
     } catch ({ message }) {
       OutputView.printStaticMessage(message);
       this.start();
