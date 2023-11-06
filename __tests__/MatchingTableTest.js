ada259d464d9eb9064f1f58655b1d266cba2d22c
PARK-CHANUK <doosan1224@naver.com>
2023-11-06 15:15:10 +0900

    refactor(MatchingTableTest): 가독성 개선

--- __tests__/MatchingTableTest.js
+++ __tests__/MatchingTableTest.js
@@ -1,33 +1,44 @@
 import MatchingTable from '../src/MatchingTable';
 
+const MATCHING_COUNT_INDEX = 1;
+
 describe('매칭 테이블 테스트', () => {
+  let mapTable;
+  beforeEach(() => {
+    mapTable = new MatchingTable();
+  });
+
   test('로또번호와 일치 갯수에 맞게 매칭 테이블을 업데이트합니다.', () => {
-    const mapTable = new MatchingTable();
-    const updatedTable = mapTable.updateTable(3);
+    const lottoMatchCount = 3;
+    const updatedTable = mapTable.updateTable(lottoMatchCount);
     const entry = [...updatedTable].find(
-      ([key, value]) => key.matchCount === 3,
+      ([{ matchCount }]) => matchCount === lottoMatchCount,
     );
 
-    expect(entry[1]).toBe(1);
+    expect(entry[MATCHING_COUNT_INDEX]).toBe(1);
   });
 
   test('5개가 일치하고 보너스 번호가 일치합니다.', () => {
-    const mapTable = new MatchingTable();
-    const updatedTable = mapTable.updateTable(5, true);
+    const lottoMatchCount = 5;
+    const hasBonus = true;
+    const updatedTable = mapTable.updateTable(lottoMatchCount, hasBonus);
     const entry = [...updatedTable].find(
-      ([key, value]) => key.matchCount === 5 && key.isBonusMatch,
+      ([{ matchCount, isBonusMatch }]) =>
+        matchCount === lottoMatchCount && isBonusMatch,
     );
 
-    expect(entry[1]).toBe(1);
+    expect(entry[MATCHING_COUNT_INDEX]).toBe(1);
   });
 
   test('5개가 일치하고 보너스 번호가 일치하지 않습니다.', () => {
-    const mapTable = new MatchingTable();
-    const updatedTable = mapTable.updateTable(5, false);
+    const lottoMatchCount = 5;
+    const hasBonus = false;
+    const updatedTable = mapTable.updateTable(lottoMatchCount, hasBonus);
     const entry = [...updatedTable].find(
-      ([key, value]) => key.matchCount === 5 && !key.isBonusMatch,
+      ([{ matchCount, isBonusMatch }]) =>
+        matchCount === lottoMatchCount && !isBonusMatch,
     );
 
-    expect(entry[1]).toBe(1);
+    expect(entry[MATCHING_COUNT_INDEX]).toBe(1);
   });
 });
