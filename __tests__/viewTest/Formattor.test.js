78041d072ae6a6854a5ddd907f87f6e39e620a7e
최나기 <choenagi@choenagiui-MacBookAir.local>
2023-11-06 20:10:03 +0900

    feat(View): Formattor에formatParseAmountToCount 함수 구현
    구입금액을 로또 구매량으로 파싱

--- __tests__/viewTest/Formattor.test.js
+++ __tests__/viewTest/Formattor.test.js
@@ -1,21 +1,22 @@
 import STRING_CONSTANT from "../test_constant/stringConstant";
+import NUMBER_CONSTANT from "../test_constant/numberConstant";
 import Formattor from "../../src/View/Formattor";
 import LottoValidationError from "../../src/Error/LottoValidationError";
 import ERROR_CONSTANT from "../../src/Constant/ErrorConstant";
 
 const factor1 = "123";
 
-describe('Formattor FormatParseAmountToNumber', () => {
+describe('Formattor formatParseAmountToNumber', () => {
   test('FormatParseAmountToNumber Function type이다 ', () => {
-    expect(typeof (Formattor.FormatParseAmountToNumber)).toBe('function');
+    expect(typeof (Formattor.formatParseAmountToNumber)).toBe('function');
   })
-  test('FormatParseAmountToNumber 인자 유효성 검사, string이 아니면 에러를 thorw한다 ', () => {
+  test('formatParseAmountToNumber 인자 유효성 검사, string이 아니면 에러를 thorw한다 ', () => {
     STRING_CONSTANT.errorStringTestCases.forEach((input) => {
       expect(() => {
-        Formattor.FormatParseAmountToNumber(input)
+        Formattor.formatParseAmountToNumber(input)
       }).toThrow(new LottoValidationError(ERROR_CONSTANT.IS_NOT_STRING));
     })
-    expect(() => Formattor.FormatParseAmountToNumber(factor1)).not.toThrow();
+    expect(() => Formattor.formatParseAmountToNumber(factor1)).not.toThrow();
   });
   const testCases = [
     { input: "2000", expected: 2000 },
@@ -24,9 +25,37 @@ describe('Formattor FormatParseAmountToNumber', () => {
     { input: "-1", expected: -1 },
     { input: "1.2", expected: 1.2 },
   ];
-  test('FormatParseAmountToNumber 기능 검사, 문자열을 숫자로 파싱한다 ', () => {
+  test('formatParseAmountToNumber 기능 검사, 문자열을 숫자로 파싱한다 ', () => {
     testCases.forEach (async ({ input, expected }) => {
-      expect(Formattor.FormatParseAmountToNumber(input)).toStrictEqual(expected);
+      expect(Formattor.formatParseAmountToNumber(input)).toStrictEqual(expected);
+    });
+  });
+});
+
+const factor2 = 1000;
+
+describe('Formattor formatParseAmountToCount', () => {
+  test('FormatParseAmountToNumber Function type이다 ', () => {
+    expect(typeof (Formattor.formatParseAmountToCount)).toBe('function');
+  })
+  test('formatParseAmountToCount 인자 유효성 검사, NaN을 제외한 number가 아니면 에러를 throw한다 ', () => {
+    NUMBER_CONSTANT.errorNumberTestCases.forEach((input) => {
+      expect(() => {
+        Formattor.formatParseAmountToCount(input)
+      }).toThrow(new LottoValidationError(`${input}${ERROR_CONSTANT.NOT_A_NUMBER}`));
+    })
+    expect(() => Formattor.formatParseAmountToCount(factor2)).not.toThrow();
+  });
+  const testCases = [
+    { input: 2000, expected: 2 },
+    { input: 3000, expected: 3 },
+    { input: 0, expected: 0 },
+    { input: -1, expected: -0.001 },
+    { input: 1.2, expected: 0.0012 },
+  ];
+  test(`formatParseAmountToCount 기능 검사, 입력받은 숫자를 조건에 따라 횟수로 파싱한다 `, () => {
+    testCases.forEach (async ({ input, expected }) => {
+      expect(Formattor.formatParseAmountToCount(input)).toStrictEqual(expected);
     });
   });
 });
--- src/View/Formattor.js
+++ src/View/Formattor.js
@@ -1,14 +1,25 @@
 import LottoValidationError from "../Error/LottoValidationError.js";
 import ERROR_CONSTANT from "../Constant/ErrorConstant.js";
-import DATA_TYPE_CONSTANT from "../Constant/DataTypeConstant.js";
+import DATATYPE_CONSTANT from "../Constant/DataTypeConstant.js";
+import RULE_CONSTANT from "../Constant/RuleConstant.js";
 
-const FormatParseAmountToNumber = (amount) => {
-  if (typeof amount !== DATA_TYPE_CONSTANT.STRING)
+const formatParseAmountToNumber = (amount) => {
+  if (typeof amount !== DATATYPE_CONSTANT.STRING)
     throw new LottoValidationError(`${ERROR_CONSTANT.IS_NOT_STRING}`);
 
   return (Number(amount));
 }
 
+const formatParseAmountToCount = (parsedAmount) => {
+  if (typeof parsedAmount !== DATATYPE_CONSTANT.NUMBER || Number.isNaN(parsedAmount))
+    throw new LottoValidationError(`${parsedAmount}${ERROR_CONSTANT.NOT_A_NUMBER}`);
+
+  const count = parsedAmount / RULE_CONSTANT.LOTTO_TICKET_PRICE;
+  return (count);
+}
+
 export default {
-  FormatParseAmountToNumber,
+  formatParseAmountToNumber,
+  formatParseAmountToCount,
+
 };
