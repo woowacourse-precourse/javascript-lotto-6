bd9ba0c53d5a6d322a84c987102f9284ea1caf80
Brokyeom <goindol40@gmail.com>
2023-11-06 16:24:49 +0900

    feat : 구입 금액 및 갯수 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await Console.readLineAsync(
+      "구입금액을 입력해 주세요. \n"
+    );
+    const lottoCount = this.calculateLottoCount(purchaseAmount);
+    Console.print(`${lottoCount}개를 구매했습니다.`);
+  }
+
+  calculateLottoCount(amount) {
+    if (amount % 1000 !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+    }
+    return amount / 1000;
+  }
 }
 
 export default App;
