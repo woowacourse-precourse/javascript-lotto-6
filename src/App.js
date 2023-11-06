abd6aaf9e63bb2c08984f011bc5ebf0367b9f5a8
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-11-07 02:31:42 +0900

    feat: 로또 발행 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import User from './User.js';
-import { Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
-  async play() {
+  issueLotto(theNumberOfLotto) {
+    const lottos = [];
+
+    let count = 0;
+    while (count < theNumberOfLotto) {
+      const lottoNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+
+      const lotto = new Lotto(lottoNumbers);
+      lottos.push(lotto);
+      
+      count++;
+    }
     
+    return lottos;
+  }
+
+  async play() {
+    const user = new User();
+    const theNumberOfLotto = await user.inputLottoPurchaseAmount();
+    this.issueLotto(theNumberOfLotto);
   }
 }
 
--- src/User.js
+++ src/User.js
@@ -3,7 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 class User {
   async inputLottoPurchaseAmount() {
     const purchaseAmount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
-    return purchaseAmount;
+    return parseInt(purchaseAmount) / 1000;
   }
 }
 
