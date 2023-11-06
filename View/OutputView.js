47f812d48cedc8fbe7d268514953c4dd6f134b38
simple0710 <thesimpleman@naver.com>
2023-11-06 21:42:01 +0900

    feat: 로또 구매 개수 출력 메세지 추가
    - 구매한 로또 수 출력

--- View/OutputView.js
+++ View/OutputView.js
@@ -7,6 +7,10 @@ const MESSAGE = {
 };
 
 const OutputView = {
+  outputPurchaseLottoCount(lottoCount) {
+    Console.print(`${lottoCount}개를 구매했습니다.`);
+  },
+
   outputRandomLottoNumbersList(RandomLottoNumbersList) {
     RandomLottoNumbersList.forEach(lottoList => {
       Console.print(`[${lottoList.join(', ')}]`);
