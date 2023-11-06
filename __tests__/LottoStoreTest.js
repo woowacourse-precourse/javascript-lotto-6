e56755684e60a471b940c575f2571b700c7fc121
zelkovaria <hyein396@gmail.com>
2023-11-06 18:14:12 +0900

    feat: askBonusNum() test 구현

--- __tests__/LottoStoreTest.js
+++ __tests__/LottoStoreTest.js
@@ -1,14 +1,26 @@
 import LottoStore from "../src/LottoStore.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 import Validator from "../src/Validate.js";
+import ERROR_MESSAGE from "../src/Errors.js";
 
 const mockReadLineAsync = jest.fn();
 MissionUtils.Console.readLineAsync = mockReadLineAsync;
-Validator.validateWinningNumbers = jest.fn();
+jest.mock("../src/Validate.js", () => ({
+  __esModule: true,
+  default: {
+    validateBonusNumber: jest.fn(),
+    validateWinningNumbers: jest.fn(),
+  },
+}));
 
 describe("LottoStoreTest", () => {
   beforeEach(() => {
     mockReadLineAsync.mockReset();
+    Validator.validateBonusNumber.mockReset();
+    Validator.validateWinningNumbers.mockReset();
+    Validator.validateBonusNumber.mockImplementation(() => {
+      throw new Error(ERROR_MESSAGE.invalidTicketNumbers);
+    });
   });
 
   test("당첨 번호는 1~45사이 정수를 중복없이 입력하지 않으면 예외가 발생한다", async () => {
@@ -19,5 +31,13 @@ describe("LottoStoreTest", () => {
     expect(Validator.validateWinningNumbers).toHaveBeenCalledWith(
       "1,2,3,4,5,6"
     );
+  });
+
+  test("보너스 번호는 1~45사이 정수를 입력하지 않으면 예외가 발생한다", async () => {
+    mockReadLineAsync.mockResolvedValue("49");
+    await expect(LottoStore.askBonusNum()).rejects.toThrow(
+      ERROR_MESSAGE.invalidTicketNumbers
+    );
+    expect(mockReadLineAsync).toHaveBeenCalledTimes(1);
   });
 });
