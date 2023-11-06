07c93bbabd8f9af91dbfa4fd0c5b0354d9c37ed0
cobocho <rlaalsrb1111@naver.com>
2023-11-07 02:42:32 +0900

    refactor: 컨트롤러 로직 재구성

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,9 +1,24 @@
 import { LottoPurchaseService, LottoRewardsService } from '../service/index.js';
-import { Lotto, LottoReward } from '../domain/index.js';
 
 import { InputView, OutputView } from '../view/index.js';
 
+import { Lotto, LottoReward } from '../domain/index.js';
+
 import SYSTEM from '../constants/system.js';
+
+import splitNumbersToArray from '../utils/splitNumbersToArray.js';
+
+/**
+ * @typedef PurchaseInfo 유저의 로또 구매 정보입니다.
+ * @property {number} money 유저의 구매 금액입니다.
+ * @property {Lotto[]} userLottos 유저의 구매 로또 목록입니다.
+ */
+
+/**
+ * @typedef LottoResult 로또 결과 정보입니다.
+ * @property {LottoReward[]} rewards 로또의 결과 목록입니다.
+ * @property {number} earningRate 유저의 수익률입니다.
+ */
 
 class Controller {
   /**
@@ -19,25 +34,51 @@ class Controller {
    */
   #service = {
     lottoPurchase: LottoPurchaseService,
-    lottoRewards: LottoRewardsService.of(),
+    lottoRewards: LottoRewardsService,
   };
 
   /**
    * 로또를 시작합니다.
    */
   async start() {
-    await this.#withRetry(() => this.#createUserLotto());
+    /** @type {PurchaseInfo} */
+    let purchaseInfo;
+
+    await this.#handleError(async () => {
+      purchaseInfo = await this.#createPurchaseInfo();
+      this.#printLottos(purchaseInfo.userLottos);
+    });
+
+    await this.#handleError(async () => {
+      const lottoResult = await this.#createLottoResult(purchaseInfo);
+      this.#printLottoResult(lottoResult);
+    });
   }
 
   /**
-   * 구매 금액을 입력받고 유저의 로또를 생성합니다.
+   * 구매 금액 만큼 유저의 로또를 생성합니다.
+   * @returns {Promise<PurchaseInfo>} 유저가 구매한 로또 목록입니다.
    */
-  async #createUserLotto() {
+  async #createPurchaseInfo() {
     const money = await this.#readPurchaseMoney();
-    const lottos = this.#service.lottoPurchase.buyLottos(money);
-    this.#printLottos(lottos);
+    const userLottos = this.#service.lottoPurchase.buyLottos(money);
 
-    await this.#withRetry(() => this.#createWinningLottoNumbers(lottos, money));
+    return { money, userLottos };
+  }
+
+  /**
+   * 우승 로또를 생성한 후 결과를 출력합니다.
+   * @param {PurchaseInfo} purchaseInfo 유저의 구매 정보입니다.
+   * @returns {Promise<LottoResult>} 로또 결과와 수익률입니다.
+   */
+  async #createLottoResult({ money, userLottos }) {
+    const winningNumbers = await this.#readWinningNumbers();
+    const bonus = await this.#readBonusNumber();
+
+    const rewards = this.#service.lottoRewards.getRewards(userLottos, { winningNumbers, bonus });
+    const earningRate = this.#service.lottoRewards.getEarningRate(money, rewards);
+
+    return { rewards, earningRate };
   }
 
   /**
@@ -50,6 +91,24 @@ class Controller {
   }
 
   /**
+   * 우승 번호를 입력받습니다.
+   * @returns {Promise<number[]>} 숫자로 이루어진 우승 번호 배열입니다.
+   */
+  async #readWinningNumbers() {
+    const winningNumbers = await this.#view.input.readWinningNumbers();
+    return splitNumbersToArray(winningNumbers, SYSTEM.lottoNumberSeparator);
+  }
+
+  /**
+   * 우승 로또의 보너스 번호를 입력받습니다.
+   * @returns {Promise<number>} 보너스 번호입니다.
+   */
+  async #readBonusNumber() {
+    const bonusNumber = await this.#view.input.readBonusNumber();
+    return Number(bonusNumber.trim());
+  }
+
+  /**
    * 유저가 구매한 로또를 출력합니다.
    * @param {Lotto[]} lottos 구매한 로또 목록입니다.
    */
@@ -59,56 +118,10 @@ class Controller {
   }
 
   /**
-   * 우승 로또 번호를 입력받고 우승 로또의 로또를 생성합니다.
-   * @param {Lotto[]} lottos 구매한 로또 목록입니다.
-   * @param {number} money 구매 금액입니다.
-   */
-  async #createWinningLottoNumbers(lottos, money) {
-    const winningLottoNumbers = await this.#readWinningLottoNumbers();
-    this.#service.lottoRewards.createWinningLotto(winningLottoNumbers);
-
-    await this.#withRetry(() => this.#createBonusNumber(lottos, money));
-  }
-
-  /**
-   * 우승 번호를 입력받습니다.
-   * @returns {Promise<number[]>} 숫자로 이루어진 우승 번호 배열입니다.
-   */
-  async #readWinningLottoNumbers() {
-    const winningLottoNumbers = await this.#view.input.readWinningNumbers();
-    return Array.from(winningLottoNumbers.split(SYSTEM.lottoNumberSeparator), Number);
-  }
-
-  /**
-   * 우승 로또 보너스 번호를 입력받고 우승 로또를 생성 후 계산합니다.
-   * @param {Lotto[]} lottos 구매한 로또 목록입니다.
-   * @param {number} money 구매 금액입니다.
-   */
-  async #createBonusNumber(lottos, money) {
-    const bonusNumber = await this.#readBonusNumber();
-    this.#service.lottoRewards.createBonus(bonusNumber);
-    const rewards = this.#service.lottoRewards.getRewards(lottos);
-
-    this.#printLottoResult(money, rewards);
-  }
-
-  /**
-   * 우승 로또의 보너스 번호를 입력받습니다.
-   * @returns {Promise<number>} 보너스 번호입니다.
-   */
-  async #readBonusNumber() {
-    const bonusNumber = await this.#view.input.readBonusNumber();
-    return Number(bonusNumber);
-  }
-
-  /**
    * 로또 결과를 출력합니다.
-   * @param {number} money 구매 금액입니다.
-   * @param {LottoReward[]} rewards 구매한 로또의 결과 목록입니다.
+   * @param {LottoResult} lottoResult 로또 결과와 수익률입니다.
    */
-  #printLottoResult(money, rewards) {
-    const earningRate = this.#service.lottoRewards.getEarningRate(money, rewards);
-
+  #printLottoResult({ rewards, earningRate }) {
     this.#view.output.winningStatistics();
     this.#view.output.rewards(rewards);
     this.#view.output.earningRate(earningRate);
@@ -118,12 +131,12 @@ class Controller {
    * 해당 콜백 함수 실행 중 에러가 발생할 시 함수를 다시 시작합니다.
    * @param {Function} action 에러 핸들링 대상이 될 함수입니다.
    */
-  async #withRetry(action) {
+  async #handleError(action) {
     try {
       await action();
     } catch ({ message }) {
       this.#view.output.error(message);
-      await this.#withRetry(action);
+      await this.#handleError(action);
     }
   }
 }
