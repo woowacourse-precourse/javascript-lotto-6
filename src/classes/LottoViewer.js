71c3f3d8fbd83edeb9b6d76d14c6ed85bd5677aa
BK <3031011@naver.com>
2023-11-06 18:31:01 +0900

    feat: 당첨 통계 출력 기능
    사용자가 입력한 로또번호와 구입한 로또 번호들을 비교해 로또 당첨 통계를 출력하는 기능

--- src/classes/LottoViewer.js
+++ src/classes/LottoViewer.js
@@ -1,5 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
-import { MESSAGE } from '../constant/lottoConstants';
+import { LOTTO_PRIZE_AMOUNT, MESSAGE } from '../constant/lottoConstants';
 
 class LottoViewer {
   static purchasedLottos(lottoTickets) {
@@ -8,6 +8,20 @@ class LottoViewer {
       .join('\n');
 
     Console.print(`${lottoTickets.length}${MESSAGE.PURCHASED}\n${printTickets}`);
+  }
+
+  static winningResult(result) {
+    const formatter = new Intl.NumberFormat('ko-KR');
+
+    // 개행 줄의 공백을 없애기 위해 replace 사용
+    Console.print(`당첨통계
+      ---
+      3개 일치 (${formatter.format(LOTTO_PRIZE_AMOUNT.LAST_PLACE)}원) - ${result.lastPrize}개
+      4개 일치 (${formatter.format(LOTTO_PRIZE_AMOUNT.FOURTY_PLACE)}원) - ${result.fourthPrize}개
+      5개 일치 (${formatter.format(LOTTO_PRIZE_AMOUNT.THIRD_PLACE)}원) - ${result.thirdPrize}개
+      5개 일치, 보너스 볼 일치 (${formatter.format(LOTTO_PRIZE_AMOUNT.SECOND_PLACE)}원) - ${result.secondPrize}개
+      6개 일치 (${formatter.format(LOTTO_PRIZE_AMOUNT.FIRST_PLACE)}원) - ${result.firstPrize}개
+    `.replace(/^ +/gm, ''));
   }
 }
 
