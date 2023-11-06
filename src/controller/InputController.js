e25e6ddd0aa5b4f9cdd89339cd46778f1dd5ffe7
rlaltj <we00425@gmail.com>
2023-11-07 02:29:52 +0900

    Fix: 수정-클래스 필드 접근 지정자 변경
    외부 클래스에서 사용하기 위해 private->public 변경

--- src/controller/InputController.js
+++ src/controller/InputController.js
@@ -1,31 +1,35 @@
 import Lotto from '../Lotto.js';
 import InputView from '../view/InputView.js';
+import PurchaseController from './PurchaseController.js';
 import ValidateController from './ValidateController.js';
 
 export default class InputController {
   inputView = new InputView();
   validateController = new ValidateController();
-  #amount = 0;
-  #winningNumbers = [];
-  #bonusNumber = 0;
+  purchaseController = new PurchaseController();
+  amount = 0;
+  winningNumbers = [];
+  bonusNumber = 0;
 
   async inputStart() {
-    await this.getBonusNumber();
+    await this.getAmount();
   }
 
   async getAmount() {
-    this.#amount = await this.inputView.readAmount();
-    this.validateController.validateAmount(this.#amount);
+    this.amount = await this.inputView.readAmount();
+    this.validateController.validateAmount(this.amount);
+
+    this.purchaseController.issueLottos(this.amount);
   }
 
   async getWinningNumbers() {
     const numberString = await this.inputView.readWinningNumbers();
-    this.#winningNumbers = numberString.split(',');
-    const lotto = new Lotto(this.#winningNumbers);
+    this.winningNumbers = numberString.split(',');
+    const lotto = new Lotto(this.winningNumbers);
   }
 
   async getBonusNumber() {
-    this.#bonusNumber = await this.inputView.readBonusNumber();
-    this.validateController.validateBonusNumber(this.#bonusNumber);
+    this.bonusNumber = await this.inputView.readBonusNumber();
+    this.validateController.validateBonusNumber(this.bonusNumber);
   }
 }
