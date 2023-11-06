0c6ea7cfc307de68327955ab017aac08f007fe48
Seungmani <dkelcncjs@naver.com>
2023-11-06 16:26:20 +0900

    [feat] 로또 발행 및 출력 기능

--- src/App.js
+++ src/App.js
@@ -21,11 +21,39 @@ const inputAmount = async () =>{
   return quotient;
 };
 
+const printProgressNumber = async (progressNumber) => {
+  print(`${progressNumber}${MESSAGE.TOTAL_PURCHASE}`);
+}
+
+
+// ----------- 로또 발행
+const totalLotto = []; // 전체 로또 번호 보관
+// 발행한 로또 수량 및 번호를 출력한다.
+const drawLottoNumber = async () => {
+  const lotto = Random.pickUniqueNumbersInRange(NUMBERS.LOTTO_MIN_NUMBER, NUMBERS.LOTTO_MAX_NUMBER, NUMBERS.LOTTO_LENGTH);
+  lotto.sort((a,b) => a-b);
+  print(lotto);
+  totalLotto.push(lotto);
+};
+
+// 발행한 로또 출력
+const printLotto = (totalLotto) => {
+  const lotto = totalLotto
+  .map((numbers) => `[${numbers.join(", ")}]`)
+  .join("\n");
+
+  print(lotto);
+}
 
 class App {
 
   async play() {
     const progressNumber = await inputAmount(); // 구입금액을 입력해 주세요.
+    await printProgressNumber(progressNumber);
+    for (let i=0; i < progressNumber; i++) {
+      await drawLottoNumber();
+    }
+    printLotto(totalLotto)
   }
 
 }
