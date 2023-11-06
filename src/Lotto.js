21cb5066c03c7553418f4e748c8824d31c57aff5
ssoferret <owosso1212@gmail.com>
2023-11-06 22:45:14 +0900

    feat : 당첨 결과 출력하는 기능 작업.

--- src/Lotto.js
+++ src/Lotto.js
@@ -111,7 +111,15 @@ const calculateRate = () => {
   return Math.round((rate * 10) / 10);
 }
 
-
+const printResults = () => {
+  Console.print('당첨 통계\n---');
+  Console.print(`3개 일치 (5,000원) - ${PublishedLottos.rank.fifth}개`);
+  Console.print(`4개 일치 (50,000원) - ${PublishedLottos.rank.fourth}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${PublishedLottos.rank.third}개`);
+  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${PublishedLottos.rank.second}개`);
+  Console.print(`6개 일치 (2,000,000,000원) - ${PublishedLottos.rank.first}개`);
+  Console.print(`총 수익률은 ${calculateRate()}입니다.`);
+}
 
 
 
