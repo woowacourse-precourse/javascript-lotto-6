4ada0ae8fa255598a60d3f157cad6b9945a0610a
Kidprogramer1039 <bnpo5231@naver.com>
2023-11-06 16:21:47 +0900

    feat: 메시지 상수 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,12 @@
 "use strict";
 
-export const LOTTO = Object.freeze({
+export const LOTTO_RANGE = Object.freeze({
 	lottoMaxRange: 45,
 	lottoMaxLength: 6,
 });
 
 export const ERROR_MESSAGE = Object.freeze({
-	lottoCount: "[ERROR] 로또 번호의 개수가 6개를 입력해주세요.",
+	lottoCount: "[ERROR] 로또 번호는 6개여야 합니다.",
 	lottoForm: "[ERROR] 숫자를 입력해주시기 바랍니다.",
 	lottoRange: "[ERROR] 로또 숫자는 1~45 범위 이내로 입력해주시기 바랍니다.",
     lottoDuplication: "[ERROR] 로또 숫자가 중복됩니다.",
