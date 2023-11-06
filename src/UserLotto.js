a9aaca854915e77e7b345b928e2678cbd08650d1
rkdcodus <rcd7275@naver.com>
2023-11-06 22:35:41 +0900

    feat: 수량에 따른 로또 발행하기

--- src/UserLotto.js
+++ src/UserLotto.js
@@ -8,7 +8,13 @@ class UserLotto {
   constructor(count){
     this.#list = [];
     this.#count = count;
-    console.log(this.#count)
+  }
+
+  createLottos(){
+    for(let i = 0;i < this.#count; i += 1){
+        const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort();
+        this.#list.push(lotto);
+    }
   }
 
 }
