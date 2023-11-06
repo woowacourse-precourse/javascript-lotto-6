61db2361148172792b63335291e8ea301aef5dbe
thewronghand <penfreak77@gmail.com>
2023-11-07 02:45:55 +0900

    refactor: NumberFormat 인스턴스 변수화, 함수명 변경

--- src/utils/MessageFormat.js
+++ src/utils/MessageFormat.js
@@ -4,23 +4,26 @@ import SYMBOL from './constants/symbol.js';
 const { statistics } = NUMBER;
 const { winningCriteria } = statistics;
 
+const prizeNumberFormatter = new Intl.NumberFormat('ko-KR');
+const profitRateFormatter = new Intl.NumberFormat('ko-KR', {
+  minimumFractionDigits: 1,
+  maximumFractionDigits: 1,
+});
+
 const MessageFormat = {
   lottoPurchaseHeader: count => `${count}개를 구매했습니다.`,
   lottoTicket: numbers => `[${numbers.join(SYMBOL.lottoNumberSeparator)}]`,
-  formatPrize: prize => new Intl.NumberFormat('ko-KR').format(prize),
-  formatProfitRate: profitRate =>
-    new Intl.NumberFormat('ko-KR', {
-      minimumFractionDigits: 1,
-      maximumFractionDigits: 1,
-    }).format(profitRate),
+  prize: prize => prizeNumberFormatter.format(prize),
+  profitRate: profitRate => profitRateFormatter.format(profitRate),
   lottoResultMessage: (category, prize, ticketCount) => {
     const { matchCount, bonusMatch } = winningCriteria[category];
-    return `${matchCount}개 일치${
-      bonusMatch ? ', 보너스 볼 일치' : ''
-    } (${MessageFormat.formatPrize(prize)}원) - ${ticketCount}개`;
+    const bonusMatchText = bonusMatch ? ', 보너스 볼 일치' : '';
+    return `${matchCount}개 일치${bonusMatchText} (${MessageFormat.prize(
+      prize,
+    )}원) - ${ticketCount}개`;
   },
   profitRateMessage: profitRate =>
-    `총 수익률은 ${MessageFormat.formatProfitRate(profitRate)}%입니다.`,
+    `총 수익률은 ${MessageFormat.profitRate(profitRate)}%입니다.`,
 };
 
 export default MessageFormat;
