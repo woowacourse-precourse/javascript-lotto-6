9d48d9365f12c8ff448547be9b0edfbb557786bd
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 03:51:34 +0900

    :sparkles: feat: 구입한 로또 출력템플릿 기능 구현 구입한 로또번호를 출력템플릿 형태값으로 출력한다. 일차원 배열로 각 배열마다 콤마없이 줄바꿈해서 출력

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,8 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
-  async printStaticMessage(messages) {
+  printStaticMessage(messages) {
     return Console.print(messages);
+  },
+
+  printPurchaseAmount(amount) {
+    return Console.print(`\n${amount}개를 구매했습니다.`);
+  },
+
+  printPurchasedLotto(purchasedLotto) {
+    const lottos = purchasedLotto.map((lotto) => `[${lotto.join(', ')}]`).join('\n');
+    return Console.print(lottos);
   },
 };
 
