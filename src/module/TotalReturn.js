2857bd5977e6f9bd113ea8eeebcaabfc3e6d5694
youngchann <lych96@gmail.com>
2023-11-06 21:59:38 +0900

    feat: 출력 기능 추가
    - Output.js: 발행한 로또 수량 및 번호, 당첨 내역, 수익률 출력 추가
    - Match.js: 당첨 통계를 계산하는 함수 추가
    - RandomLotto.js: 구매한 로또를 생산하는 함수 추가
    - TotalReturn.js: 총 수익률을 계산해주는 함수 추가

---
+++ src/module/TotalReturn.js
@@ -0,0 +1,14 @@
+import { money } from '../Consts';
+
+const totalReturn = (purchasedLotto, matchCount) => {
+  const totalWinning =
+    matchCount.threeMatches * money.threeMatches +
+    matchCount.fourMatches * money.fourMatches +
+    matchCount.fiveMatches * money.fiveMatches +
+    matchCount.fiveBonusMatches * money.fiveBonusMatches +
+    matchCount.sixMatches * money.sixMatches;
+  const tempReturn = totalWinning / purchasedLotto;
+  return Number(tempReturn.toFixed(2));
+};
+
+export default totalReturn;
