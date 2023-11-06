bdc6a943f45ffa88a4d5b1d640b86ed371a342df
Junseo <monstazo911@gmail.com>
2023-11-06 18:43:22 +0900

    Feat: 로또 숫자 생성 메소드 추가
    - 로또 숫자 랜덤 생성 메소드 generateLottoNumbers() 추가
    - pickUniqueNumbersInRange()를 통해 6자리 중복되지 않는 숫자 생성
    - sort를 통해 오름차순으로 정렬
    - 생성된 숫자 출력
    - 생성된 숫자 반환

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   purchaseAmount = 0;
+  lottos = [];
 
   async inputPurchaseAmount() {
     const inputPurchaseAmount = await MissionUtils.Console.readLineAsync(
@@ -17,6 +18,20 @@ class App {
       throw new Error("[ERROR] 구매 금액은 숫자여야 합니다.");
     if (purchaseAmount % 1000)
       throw new Error("[ERROR] 구매 금액은 1,000원 단위여야 합니다.");
+  }
+
+  static generateLottoNumbers() {
+    const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    const sortedLottoNumbers = lottoNumbers.sort((a, b) => a - b);
+    MissionUtils.Console.print("[" + sortedLottoNumbers.join(", ") + "]");
+    return sortedLottoNumbers;
+  }
+
+  purchaseLotto() {
+    let lottoAmount = this.purchaseAmount / 1000;
+    for (let i = 1; i <= lottoAmount; i++) {
+      const lottoNumbers = App.generateLottoNumbers();
+    }
   }
 
   async play() {
