20ada45c9405ffad007afcfba2228cb3dae2bbd6
Lee sang Yeop <mma7710@naver.com>
2023-11-06 21:28:08 +0900

    fix(UserInput): 사용자에게 입력 받을 때 에러발생 시 종료가 아닌 다시 값을 입력받을 수 있도록 수정 - 사용자 입력에 에러발생 시 화면에 보여주기 위해 printConsole 인스턴스 생성 추가 - 사용자 입력받았을 때 에러발생하면 재귀호출을 통해 값을 다시 받을 수 있도록 변경 - 로또 당첨번호, 보너스번호 입력하는 메서드를 private -> public 으로 다시 변경 (굳이 private 일 필요가 없기때문)

--- src/views/UserInput.js
+++ src/views/UserInput.js
@@ -1,39 +1,59 @@
 import { Console } from '@woowacourse/mission-utils';
 import SYSTEM_MESSAGES from '../constants/SystemMessages.js';
 import validators from '../utils/validators.js';
+import PrintConsole from './PrintConsole.js';
 
 class UserInput {
+	#printConsole;
+
+	constructor() {
+		this.#printConsole = new PrintConsole();
+	}
+
 	async lottoPurchase(question = SYSTEM_MESSAGES.input_purchase_amount) {
-		const userInput = await Console.readLineAsync(question);
-		const trimmedInput = userInput.trim();
-		validators.checkPurchasePrice(trimmedInput);
+		try {
+			const userInput = await Console.readLineAsync(question);
+			const trimmedInput = userInput.trim();
+			validators.checkPurchasePrice(trimmedInput);
 
-		return trimmedInput;
+			return trimmedInput;
+		} catch (error) {
+			this.#printConsole.print(error.message);
+			return this.lottoPurchase();
+		}
 	}
 
-	async #winningNumber(question = SYSTEM_MESSAGES.input_winning_numbers) {
-		const userInput = await Console.readLineAsync(question);
-		const splittedInput = userInput.split(',').map((input) => input.trim());
+	async winningNumber(question = SYSTEM_MESSAGES.input_winning_numbers) {
+		try {
+			const userInput = await Console.readLineAsync(question);
+			const splittedInput = userInput.split(',').map((input) => input.trim());
+			validators.checkLottoNumbers(splittedInput);
 
-		return splittedInput;
+			return splittedInput;
+		} catch (error) {
+			this.#printConsole.print(error.message);
+			return this.winningNumber();
+		}
 	}
 
-	async #winningBonusNumber(question = SYSTEM_MESSAGES.input_bonus_number) {
-		const userInput = await Console.readLineAsync(question);
+	async winningBonusNumber(winningNumber, question = SYSTEM_MESSAGES.input_bonus_number) {
+		try {
+			const userInput = await Console.readLineAsync(question);
+			const trimmedInput = userInput.trim();
 
-		if (typeof userInput === 'number') return userInput;
+			validators.checkWinningBonusNumber(winningNumber, trimmedInput);
 
-		const trimmedInput = userInput.trim();
-
-		return trimmedInput;
+			return trimmedInput;
+		} catch (error) {
+			this.#printConsole.print(error.message);
+			return this.winningBonusNumber(winningNumber);
+		}
 	}
 
 	async lottoWinningNumber() {
-		const winningNumber = await this.#winningNumber();
-		validators.checkLottoNumbers(winningNumber);
+		const winningNumber = await this.winningNumber();
 
-		const winningBonusNumber = await this.#winningBonusNumber();
-		validators.checkWinningBonusNumber(winningNumber, winningBonusNumber);
+		const winningBonusNumber = await this.winningBonusNumber(winningNumber);
 
 		return { winningNumber, winningBonusNumber };
 	}
