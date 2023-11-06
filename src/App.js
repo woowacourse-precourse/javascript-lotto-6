56a3495621ad79b425abc23d9678edcb8c7c9199
hzzzi <hji2004@naver.com>
2023-11-07 01:26:21 +0900

    feat: 구입 금액만큼 로또 티켓 발행하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { MissionUtils, Console } from '@woowacourse/mission-utils';
+import Ticket from './Ticket.js';
 
 async function getPurchasePrice() {
   const price = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.');
@@ -6,8 +7,28 @@ async function getPurchasePrice() {
   return purchasePrice;
 }
 
+function getAmountOfTickets(purchasePrice) {
+  return purchasePrice / 1000;
+}
+
+function showAmountOfTickets(amountOfTickets) {
+  Console.print(`${amountOfTickets}개를 구매했습니다.`);
+}
+
 class App {
-  async play() {}
+  async play() {
+    const purchasePrice = await getPurchasePrice();
+    const amountOfTickets = getAmountOfTickets(purchasePrice);
+    showAmountOfTickets(amountOfTickets);
+
+    const ticketArr = [];
+    for (let i = 0; i < amountOfTickets; i++) {
+      const ticket = new Ticket();
+      ticket.generateRandomNumbers();
+      ticket.showNumbers();
+      ticketArr.push(ticket);
+    }
+  }
 }
 
 export default App;
