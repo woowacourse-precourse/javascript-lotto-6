ad369100d8785263609b6e07a46f7a0158da8bb3
lurgi <wahoo9040@naver.com>
2023-11-06 21:22:17 +0900

    chore(Valid,ValidTest): 불필요한 Valid모듈 삭제
    fix(Lotto): Valid 삭제
    
    fix(Lotto): valid 이름 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,3 @@
-import { validLottoNumber } from "./Valid";
-
 class Lotto {
   #numbers;
 
@@ -12,10 +10,22 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    validLottoNumber(numbers);
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 중복된 숫자를 입력하셨습니다.");
+    }
+    this.#validLottoNumbers(numbers);
   }
 
   // TODO: 추가 기능 구현
+  #validLottoNumbers(numbers) {
+    numbers.forEach((number) => {
+      if (Number.isNaN(number)) throw new Error("[ERROR] 숫자만 입력해주세요.");
+      if (!number) throw new Error("[ERROR] 공백을 입력하셨습니다.");
+      if (number < 1 || number > 45)
+        throw new Error("[Error] 숫자의 범위는 1~45 이어야 합니다.");
+    });
+  }
+
   getLottoResult({ winNumbers, bonusNumber }) {
     let numberOfCorrect = 0;
     let isBonusCorrect = false;
