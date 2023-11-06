d843b4fe2295295adc3eed3176d4ea2b0da95066
soyoung125 <sypark041754@gmail.com>
2023-11-07 04:36:25 +0900

    feat: 보너스 번호 입력에 대한 예외 처리 테스트 추가

--- __tests__/validationTest.js
+++ __tests__/validationTest.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 import {
+  validateBonusNumber,
   validatePurchase,
   validateWinningNumber,
 } from "../src/utils/validation.js";
@@ -41,5 +42,30 @@ describe("validation 모듈 테스트", () => {
 
   test("당첨 번호에 중복이 포함되면 예외가 발생한다.", () => {
     expect(() => validateWinningNumber("1,2,3,4,5,5")).toThrow("[ERROR]");
+  });
+
+  // 보너스 번호 입력에 대한 예외 처리
+  test("당첨 번호에 빈 값을 입력하면 예외가 발생한다.", () => {
+    expect(() => validateBonusNumber([1, 2, 3, 4, 5, 6], "")).toThrow(
+      "[ERROR]",
+    );
+  });
+
+  test("당첨 번호에 숫자 외의 문자를 입력하면 예외가 발생한다.", () => {
+    expect(() => validateBonusNumber([1, 2, 3, 4, 5, 6], "a")).toThrow(
+      "[ERROR]",
+    );
+  });
+
+  test("당첨 번호에 범위(1~45)외의 숫자를 입력하면 예외가 발생한다.", () => {
+    expect(() => validateBonusNumber([1, 2, 3, 4, 5, 6], "0")).toThrow(
+      "[ERROR]",
+    );
+  });
+
+  test("당첨 번호에 중복이 포함되면 예외가 발생한다.", () => {
+    expect(() => validateBonusNumber([1, 2, 3, 4, 5, 6], "6")).toThrow(
+      "[ERROR]",
+    );
   });
 });
