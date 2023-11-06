f664153979275dc3b0639af4084b13d208a18073
rkdcodus <rcd7275@naver.com>
2023-11-07 00:01:58 +0900

    feat: ⚠ 당첨 번호 입력 시 발생하는 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,11 +4,23 @@ class Lotto {
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    console.log(numbers)
   }
 
   #validate(numbers) {
+    const numbersSet = [... new Set(numbers)];
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    numbers.forEach(number => {
+      if (!/\d/.test(number)){
+        throw new Error('[ERROR] 숫자를 입력해 주세요.')
+      } else if (number <= 0  || number > 45 ) {
+        throw new Error('[ERROR] 1 ~ 45 사이의 숫자여야 합니다.')
+      } 
+    });
+    if (numbersSet.length !== numbers){
+      throw new Error('[ERROR] 중복된 값이 있습니다.')
     }
   }
 
