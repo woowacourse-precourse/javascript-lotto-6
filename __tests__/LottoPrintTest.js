143e4866c1012e46ee3e358903be42f01f1fd4e5
whai2 <whai2125@gmail.com>
2023-11-07 00:30:46 +0900

    test(lotto): 로또 번호 발행 테스트 수정, 값 반환이 아닌, 모델 객체 저장 및 결과 출력

--- __tests__/LottoPrintTest.js
+++ __tests__/LottoPrintTest.js
@@ -1,16 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { Counter } from "../src/Counter.js";
 
-const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-
-    return Promise.resolve(input);
-  });
-};
-
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, "print");
   logSpy.mockClear();
--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import Lotto from "../src/Lotto.js";
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -21,18 +28,25 @@ describe("로또 클래스 테스트", () => {
   });
 
   test("올바른 로또 번호를 발행한다.", () => {
+    const logSpy = getLogSpy();
 
     const lotto = new Lotto([1, 2, 3, 4, 5, 6])
+    lotto.printNumbers();
 
-    expect(lotto).toEqual([1, 2, 3, 4, 5, 6]);
+    const log = "[1, 2, 3, 4, 5, 6]";
+
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log))
   });
 
   test("구매한 로또 번호를 오름차순으로 출력한다.", () => {
-    const INPUT = [21, 8, 23, 42, 43, 41];
-    const OUTPUT = [8, 21, 23, 41, 42, 43];
+    const logSpy = getLogSpy();
+    
+    const lotto = new Lotto([21, 8, 23, 42, 43, 41]);
+    lotto.printNumbers();
+    
+    const log = "[8, 21, 23, 41, 42, 43]";
   
-    const result = new Lotto(INPUT);
-  
-    expect(result).toEqual(OUTPUT);
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log))
   });
 });
