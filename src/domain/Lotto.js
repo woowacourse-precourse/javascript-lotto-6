05ffe75bf5a91973b9ce38871b843dd62debc43b
pakxe <pigkill40@naver.com>
2023-11-06 16:04:49 +0900

    feat: 중복 검사를 용이하게 하기 위해 보너스 넘버 생성 로직 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,15 +1,16 @@
+import LottoDto from '../dto/LottoDto.js';
 import Parser from '../parser/Parser.js';
+import calcMatchCount from '../utils/calcMatchCount.js';
+import validateBonusNumber from '../validator/validateBonusNumber.js';
 import validateLotto from '../validator/validateLotto.js';
 import LottoNumber from './LottoNumber.js';
 
 // 파서와 유효성 검사 진행
 class Lotto {
-  #lotto;
+  #lotto = [];
 
   constructor(lotto) {
-    const parsedLotto = Array.isArray(lotto)
-      ? lotto
-      : Parser.parseLotto(lotto).map((num) => new LottoNumber(num));
+    const parsedLotto = this.#parseLotto(lotto);
 
     Lotto.#validate(parsedLotto);
 
@@ -20,8 +21,31 @@ class Lotto {
     validateLotto(lotto);
   }
 
+  createBonusNumber(bonusNumberRequest) {
+    const bonusNumber = new LottoNumber(bonusNumberRequest);
+
+    validateBonusNumber(this.get(), bonusNumber.get());
+
+    return bonusNumber;
+  }
+
+  getMatchCount(winningLotto) {
+    return calcMatchCount(this.get(), winningLotto.get());
+  }
+
+  checkBonusMatch(bonusNumber) {
+    return this.#lotto.includes(bonusNumber.get());
+  }
+
+  #parseLotto(lotto) {
+    const lottoArr = Array.isArray(lotto) ? lotto : Parser.parseLotto(lotto);
+    const encapsuleLottoNumber = lottoArr.map((num) => new LottoNumber(num));
+
+    return encapsuleLottoNumber;
+  }
+
   get() {
-    return this.#lotto;
+    return LottoDto.getResponse(this.#lotto);
   }
 }
 
