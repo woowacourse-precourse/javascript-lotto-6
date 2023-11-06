a60710626c1bd1fb511bb8e8af59d0bca533a366
thewronghand <penfreak77@gmail.com>
2023-11-06 15:51:48 +0900

    feat(LottoPublisher): 금액 입력에 따른 로또 발행 기능 구현
    하나의 로또를 발행하는 메서드를 구현하고, 이를 활용해 금액만큼의 로또를 발행하여 배열로 반환하는 메서드를 구현한다.

--- src/models/LottoPublisher.js
+++ src/models/LottoPublisher.js
@@ -1,3 +1,10 @@
+import { Random } from '@woowacourse/mission-utils';
+import NUMBER from '../utils/constants/number';
+import Lotto from './Lotto';
+
+const { game } = NUMBER;
+const { lotto } = game;
+
 class LottoPublisher {
   #moneyAmount;
 
@@ -10,6 +17,24 @@ class LottoPublisher {
     if (moneyAmount % 1000 !== 0) {
       throw new Error('안돼임마');
     }
+  }
+
+  #generateRandomLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(
+      lotto.minNumber,
+      lotto.maxNumber,
+      lotto.length,
+    );
+  }
+
+  #publishSingleLotto() {
+    const lottoNumbers = this.#generateRandomLottoNumbers();
+    return new Lotto(lottoNumbers);
+  }
+
+  publishLottos() {
+    const count = this.#moneyAmount / game.money.unitAmount;
+    return Array.from({ length: count }, () => this.#publishSingleLotto());
   }
 }
 
