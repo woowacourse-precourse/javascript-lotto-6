04f3e4876c13deca8ccf530e14f409332868cf3c
swarvy <swarvy0826@naver.com>
2023-11-06 19:05:47 +0900

    feat : 랜덤으로 생성된 숫자를 오름차순 정렬하는 기능 구현

--- src/utils/generateRandomNumber.js
+++ src/utils/generateRandomNumber.js
@@ -3,4 +3,7 @@ import { Random } from '@woowacourse/mission-utils';
 const generateRandomNumbers = (startNum, endNum, amount) =>
   Random.pickUniqueNumbersInRange(startNum, endNum, amount);
 
-export default generateRandomNumbers;
+const getRandomNumbersByAscending = (startNum, endNum, amount) =>
+  generateRandomNumbers(startNum, endNum, amount).sort((a, b) => a - b);
+
+export default getRandomNumbersByAscending;
