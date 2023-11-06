58ead3f2cc838ef4cc02d8b4092c98ad17a5e6e4
Trophy198 <spsp8426@naver.com>
2023-11-06 23:00:20 +0900

    feat : 로또 숫자 일치 상수 추가

--- src/constants/GameSettings.js
+++ src/constants/GameSettings.js
@@ -4,7 +4,10 @@ export const GAME_SETTINGS = {
   MAX_LOTTO_NUMBER: 45,
   NUMBERS_PER_TICKET: 6,
   MATCH_TEXTS: {
+    3: '3개 일치',
+    4: '4개 일치',
+    5: '5개 일치',
     '5+1': '5개 일치, 보너스 볼 일치',
-    DEFAULT: '개 일치',
+    6: '6개 일치',
   },
 };
