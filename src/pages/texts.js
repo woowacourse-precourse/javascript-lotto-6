09be3932ce3c15119d00e26b46fb95ec9e2a4cc7
Dobbymin <gmin0701@knu.ac.kr>
2023-11-06 11:13:35 +0900

    📝 refactor(texts): 입력받은 금액 예외처리 추가

--- src/pages/texts.js
+++ src/pages/texts.js
@@ -1,8 +1,13 @@
 export const ERROR = {
     numeric: '[ERROR] 숫자만 입력해주세요!',
+    numeric_0: '[ERROR] 0원은 입력할 수 없습니다!',
+    numeric_null: '[ERROR] 금액을 입력해주세요!',
+    input_1000won: '[ERROR] 로또는 1000원단위로 구입가능합니다.',
+    input_less_1000won: '[ERROR] 1000원 이상의 금액을 입력해주세요!',
+
     include_0: '[ERROR] 1 ~ 45 사이의 숫자만 입력해주세요!',
     length: '[ERROR] 숫자는 6개를 입력해주세요!',
-    different_number: '[ERROR] ',
+    different_number: '[ERROR] 서로 다른 숫자를 입력해주세요!',
 };
 
 export const LOTTO = {
