68da3ce196f74d432aa98907581963c841af12c0
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-06 23:05:59 +0900

    test: 당첨 여부를 출력하는 기능에 대한 출력 문자열 수정

--- __tests__/View/OutputView.test.js
+++ __tests__/View/OutputView.test.js
@@ -4,6 +4,9 @@ import OutputView from "../../src/View/OutputView";
 Console.print = jest.fn();
 
 describe("OutputView 객체 테스트", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   test("printPurchaseResult 메서드가 존재해야 한다.", () => {
     expect(typeof OutputView.printPurchaseResult).toBe("function");
   });
@@ -25,23 +28,25 @@ describe("OutputView 객체 테스트", () => {
 
   test("printMatchResult 메서드를 호출하면 Console.print가 당첨 결과와 함께 호출되야 한다.", () => {
     const matchResult = {
-      firstPlace: 1,
-      secondPlace: 1,
-      thirdPlace: 0,
-      fourthPlace: 1,
       fifthPlace: 0,
+      fourthPlace: 1,
+      thirdPlace: 0,
+      secondPlace: 1,
+      firstPlace: 1,
       returnRate: 50751250,
     };
-    const matchResultMessage = `
-    당첨 통계
-    ---
-    3개 일치 (5,000원) - 1개
-    4개 일치 (50,000원) - 0개
-    5개 일치 (1,500,000원) - 0개
-    5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
-    6개 일치 (2,000,000,000원) - 0개
-    총 수익률은 62.5%입니다.
-    `;
+    const matchResultMessage = [
+      "",
+      "당첨 통계",
+      "---",
+      "3개 일치 (5,000원) - 0개",
+      "4개 일치 (50,000원) - 1개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+      "6개 일치 (2,000,000,000원) - 1개",
+      "총 수익률은 50751250%입니다.",
+    ].join("\n");
+
     OutputView.printMatchResult(matchResult);
     expect(Console.print).toBeCalledWith(matchResultMessage);
   });
