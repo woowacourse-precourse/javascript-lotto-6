6988b8ceb763f91e5621df0e3b7d4b2b123c3e2d
Yeojin Kim <yeojin58@naver.com>
2023-11-06 18:38:59 +0900

    로또 번호 숫자 뽑기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
+
 class App {
-  async play() {}
+  async play() {
+    const lottoNumbers = this.#generateLottoNumbers();
+    const bonusNumber = this.#bonusNumber(lottoNumbers);
+  }
+
+    #isNumberUnique (number, numberHistory) {
+      return !numberHistory.includes(number);
+    }
+
+    #uniqueNumber (numberHistory) {
+      let number;
+      do {
+        number = MissionUtils.Random.pickNumberInRange(1, 45);
+      } while (!this.#isNumberUnique(number, numberHistory));
+
+      return number;
+    }
+
+    #generateLottoNumbers() {
+      const numbers = [];
+      while (numbers.length < 6) {
+        numbers.push(this.#uniqueNumber(numbers));
+      }
+      return numbers.sort((a, b) => a - b);
+    }
+
+    #bonusNumber(excludedNumber) {
+      let number;
+      do {
+        number = MissionUtils.Random.pickNumberInRange(1, 45);
+      } while (excludedNumber.includes(number));
+      return number;
+    }
 }
 
 export default App;
