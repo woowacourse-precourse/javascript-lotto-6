e2f8d374965624702b7ac3020b09f9583442f201
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-06 15:39:52 +0900

    test(LottoMachine): 로또 구입 금액이 4000원이면 로또 4개 발행

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,6 +1,15 @@
 import LottoMachine from '../src/LottoMachine.js';
 
 describe('로또 기계 테스트', () => {
+  test('로또 구입 금액이 4000원이면 로또를 4개 발행한다.', () => {
+    const lottoMachine = new LottoMachine();
+    const money = 4000;
+
+    lottoMachine.insertMoney(money);
+
+    expect(lottoMachine.numberOfLottos).toBe(4);
+  });
+
   describe('예외 테스트', () => {
     test('로또 구입 금액이 양의 정수가 아니면 예외가 발생한다.', () => {
       const lottoMachine = new LottoMachine();
