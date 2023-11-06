2fec16fc3b4a7dfa74f12342ba0b75f40de1c87f
cobocho <rlaalsrb1111@naver.com>
2023-11-07 02:41:45 +0900

    refacor: `LottoRewardsService` 싱글톤으로 수정

--- src/service/LottoRewardsService.js
+++ src/service/LottoRewardsService.js
@@ -1,55 +1,26 @@
 import {
   Lotto,
-  LottoNumber,
   WinningLotto,
   LottoRewards,
   LottoReward,
   Calculator,
+  LottoNumber,
 } from '../domain/index.js';
 
-class LottoRewardsService {
-  /**
-   * @type {Lotto}
-   */
-  #lotto;
-
-  /**
-   * @type {LottoNumber}
-   */
-  #bonus;
-
-  static of() {
-    return new LottoRewardsService();
-  }
-
-  /**
-   * 우승로또에 들어갈 로또를 생성합니다.
-   * @param {number[]} numbers 우승 로또의 번호입니다.
-   */
-  createWinningLotto(numbers) {
-    this.#lotto = Lotto.of(numbers);
-  }
-
-  /**
-   * 우승로또에 들어갈 보너스 번호를 생성합니다.
-   * @param {number} bonus 우승 로또의 보너스 번호입니다.
-   */
-  createBonus(bonus) {
-    this.#bonus = LottoNumber.valueOf(bonus);
-  }
-
+const LottoRewardsService = Object.freeze({
   /**
    * 현재 우승 로또를 기반으로 결과를 반환합니다.
    * @param {Lotto[]} lottos 비교할 로또 배열입니다.
+   * @param {{ winningNumbers: number[], bonus: number }} winningLottoInfo 비교할 우승 로또 정보입니다.
    * @returns {LottoReward[]} 로또의 비교 결과들입니다.
    */
-  getRewards(lottos) {
-    const winningLotto = WinningLotto.of(this.#lotto, this.#bonus);
+  getRewards(lottos, { winningNumbers, bonus }) {
+    const winningLotto = WinningLotto.of(Lotto.of(winningNumbers), LottoNumber.valueOf(bonus));
     const rewards = LottoRewards.of(winningLotto);
     const result = rewards.getLottosResult(lottos);
 
     return result.reverse();
-  }
+  },
 
   /**
    * 입력받은 결과들을 기반으로 수익률을 체크합니다.
@@ -62,7 +33,7 @@ class LottoRewardsService {
     const result = calculator.earningRate(income, rewards);
 
     return result;
-  }
-}
+  },
+});
 
 export default LottoRewardsService;
