cd21c698777e4c1f13608302c982322ce22f62d2
soyoung125 <sypark041754@gmail.com>
2023-11-07 04:17:55 +0900

    feat: 로또 번호 getter 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -20,7 +20,7 @@ describe("로또 클래스 테스트", () => {
     const winningNumber = [1, 2, 3, 25, 43, 45];
     const bonusNumber = 5;
 
-    expect(lotto.match(winningNumber, bonusNumber)).toBe("fifth");
+    expect(lotto.match(winningNumber, bonusNumber)).toEqual("fifth");
   });
 
   test("당첨 번호와 로또 번호를 비교한다. - 보너스", () => {
@@ -28,6 +28,12 @@ describe("로또 클래스 테스트", () => {
     const winningNumber = [1, 2, 3, 4, 5, 45];
     const bonusNumber = 6;
 
-    expect(lotto.match(winningNumber, bonusNumber)).toBe("second");
+    expect(lotto.match(winningNumber, bonusNumber)).toEqual("second");
+  });
+
+  test("로또 번호 반환을 확인한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
