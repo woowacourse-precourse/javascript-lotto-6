fa2c509d1934ea514bcd99837db4cd2d3ddfa4b1
ReturnReason <reasonz@daum.net>
2023-11-06 23:37:07 +0900

    feat(LottoResult): 당첨된 번호의 개수에 해당하는 key값을 매핑, 키 값으로 당첨된 로또의 개수를 증가시키는 메서드 추가

--- src/model/LottoResult.js
+++ src/model/LottoResult.js
@@ -1,3 +1,6 @@
+import { GAME_REWARD } from '../constants/gameRule.js';
+import calcProfit from '../utils/calcProfit.js';
+
 class LottoResult {
   #matchCount = {
     three: 0,
@@ -8,8 +11,27 @@ class LottoResult {
   };
   #prizeMoney = 0;
 
-  // TODO 일치하는 당첨 번호/보너스 번호 계산 해서 출력하는 로직
-  // TODO 수익률 계산 후(유틸함수 사용) 출력 로직
+  setMatchCount(matchResults) {
+    matchResults.forEach(({ count, hasBonusNumber }) => {
+      const countKey = this.#getCountKey(count, hasBonusNumber);
+      if (!countKey) return;
+      this.#matchCount[countKey] += 1;
+    });
+  }
+
+  #getCountKey(winningNumberCount, hasBonusNumber) {
+    if (winningNumberCount === 5 && hasBonusNumber) {
+      return 'bonus';
+    }
+
+    const keyMap = {
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+    };
+    return keyMap[String(winningNumberCount)] || null;
+  }
 }
 
 export default LottoResult;
