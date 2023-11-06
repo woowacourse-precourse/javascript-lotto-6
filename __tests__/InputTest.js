f0d2f9bf51b0bec9f79f456074c5f31829cc9196
unknown <pih9134@naver.com>
2023-11-07 03:05:45 +0900

    test: 입력에 대한 test함수 작성
    구입금액, 당첨번호, 보너스번호에 대한 입력 test

---
+++ __tests__/InputTest.js
@@ -0,0 +1,74 @@
+import Validation from '../src/Validation.js';
+import { ERROR_MESSAGE } from '../src/constants.js';
+
+describe('입력에 대한 유효성 test', () => {
+  describe('구입금액에 대한 테스트', () => {
+    test('유효한 금액을 입력했을 경우', () => {
+      const result = Validation.validatePurchaseAmount(3000);
+      expect(result).toBe(3000);
+    });
+
+    test('1000보다 작은 금액을 입력했을 경우', () => {
+      expect(() => {
+        Validation.validatePurchaseAmount(999);
+      }).toThrow(Error(ERROR_MESSAGE.invalidPusrchaseAmount));
+    });
+
+    test('1,000원으로 나누어 떨어지지 않는 경우', () => {
+      expect(() => {
+        Validation.validatePurchaseAmount(10001);
+      }).toThrow(Error(ERROR_MESSAGE.invalidPusrchaseAmount));
+    });
+  });
+
+  describe('당첨 번호에 대한 테스트', () => {
+    test('유효한 당첨 번호를 입력했을 경우', () => {
+      const result = Validation.validateWinningNumbers('1,2,3,4,5,6');
+      expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    test('당첨 번호를 6개 입력하지 않았을 경우', () => {
+      expect(() => {
+        Validation.validateWinningNumbers('1,2,3,4,5');
+      }).toThrow(Error(ERROR_MESSAGE.duplicationWinningNumbers));
+    });
+
+    test('당첨 번호가 중복되었을 경우', () => {
+      expect(() => {
+        Validation.validateWinningNumbers('1,2,3,5,5,6');
+      }).toThrow(Error(ERROR_MESSAGE.duplicationWinningNumbers));
+    });
+
+    test('당첨 번호에 소수가 포함되었을 경우', () => {
+      expect(() => {
+        Validation.validateWinningNumbers('1,2,3,3.5,5,6');
+      }).toThrow(Error(ERROR_MESSAGE.duplicationWinningNumbers));
+    });
+
+    test('당첨 번호에 1~45사이가 아닌 숫자가 포함되었을 경우', () => {
+      expect(() => {
+        Validation.validateWinningNumbers('1,2,3,46,5,6');
+      }).toThrow(Error(ERROR_MESSAGE.duplicationWinningNumbers));
+    });
+  });
+
+  describe('보너스 번호에 대한 테스트', () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    test('유효한 보너스 번호를 입력했을 경우', () => {
+      const result = Validation.validateBonusNumber(7, winningNumbers);
+      expect(result).toBe(7);
+    });
+
+    test('당첨 번호와 중복되었을 경우', () => {
+      expect(() => {
+        Validation.validateBonusNumber(6, winningNumbers);
+      }).toThrow(Error(ERROR_MESSAGE.duplicationBonusNumber));
+    });
+
+    test('보너스 번호가 1~45사이가 아닌 숫자일 경우', () => {
+      expect(() => {
+        Validation.validateBonusNumber(46, winningNumbers);
+      }).toThrow(Error(ERROR_MESSAGE.invalidBonusNumber));
+    });
+  });
+});
