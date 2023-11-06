001a4a8aa3eadb03737d6552f6bd5a8832abff82
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-06 15:23:18 +0900

    test(LottoMachine): 구입 금액이 양의 정수가 아니면 예외 발생

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -2,6 +2,13 @@ import LottoMachine from '../src/LottoMachine.js';
 
 describe('로또 기계 테스트', () => {
   describe('예외 테스트', () => {
+    test('로또 구입 금액이 양의 정수가 아니면 예외가 발생한다.', () => {
+      const lottoMachine = new LottoMachine();
+      expect(() => {
+        lottoMachine.insertMoney(-1000);
+      }).toThrow('[ERROR]');
+    });
+
     test('로또 구입 금액이 1000원으로 나누어 떨어지지 않으면 예외가 발생한다.', () => {
       const lottoMachine = new LottoMachine();
       expect(() => {
