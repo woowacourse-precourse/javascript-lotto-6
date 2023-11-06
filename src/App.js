dc4eae46477319905f85ec153304be882a3cff03
doongeon <ndmb2012@gamil.com>
2023-11-06 13:26:47 +0900

    feat: print lotto order

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Lotto from "./Lotto.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  #orderPrice;
+  #orderQuantity;
+  #lottoOrder = [];
+
   getUserInput = async () => {
     let userInput = '';
 
@@ -30,6 +34,14 @@ class App {
     if(userInputNum % 1000 != 0) throw new Error('[ERROR] : cannot divide price by 1,000');
   }
 
+  setOrderPrice = (userInputPrice) => {
+    this.#orderPrice = userInputPrice;
+  }
+
+  setOrderQuantity = (userInputPrice) => {
+    this.#orderQuantity = userInputPrice/1000;
+  }
+
   generateLottoNumbers = () => {
     const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
     const sortedLottoNumbers = lottoNumbers.sort((o1, o2) => o1 - o2);
@@ -37,20 +49,38 @@ class App {
     return sortedLottoNumbers;
   }
 
+  printLottoOrder = () => {
+    MissionUtils.Console.print(`\n${this.#orderQuantity}개를 구매했습니다.`);
+
+    this.#lottoOrder.forEach((lottoNumbers) => {
+      MissionUtils.Console.print(lottoNumbers);
+    })
+  }
+
+
+
 
   async play() {
     try {
       MissionUtils.Console.print('구입금액을 입력해 주세요.');
 
-      // const userInput = await this.getUserInput();
+      const userInput = await this.getUserInput();
 
-      // const number = this.stringToNumber(userInput);
+      const userInputPrice = this.stringToNumber(userInput);
 
-      // this.checkIsValidPrice(number);
+      this.checkIsValidPrice(userInputPrice);
 
-      const lottoNumbers = this.generateLottoNumbers();
+      this.setOrderPrice(userInputPrice);
+      this.setOrderQuantity(userInputPrice);
 
-      console.log(lottoNumbers);
+      for(let i = 0; i < this.#orderQuantity; i++) {
+        const lottoNumbers = this.generateLottoNumbers();
+        
+        this.#lottoOrder.push(lottoNumbers);
+      }
+
+      this.printLottoOrder();
+
 
     } catch(err) {
       return Promise.reject(err);
