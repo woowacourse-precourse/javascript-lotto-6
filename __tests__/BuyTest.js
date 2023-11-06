e7c9553076f2b4bfa1f91a05c688c181422a75a9
JonghyunLEE12 <najo7609@gmail.com>
2023-11-07 04:16:02 +0900

    Test: [ BuyTest UI로직 단위 테스트 제외 ]

--- __tests__/BuyTest.js
+++ __tests__/BuyTest.js
@@ -3,12 +3,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import InputView from '../src/views/InputView.js';
 import LottoUtill from '../src/utils/LottoUtill.js';
 import LottoController from '../src/controllers/LottoController.js';
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
-  return logSpy;
-};
+import InputValidate from '../src/utils/InputValidate.js';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -57,50 +52,33 @@ describe('로또 구입 테스트', () => {
   });
 
   test('로또 번호(중복) 예외 테스트', async () => {
-    const FIRST = ['1000', '1,1,1,1,1,1'];
-    const INPUT_NUMBERS_TO_END = ['1,2,3,4,5,6', '7'];
-    mockQuestions([...FIRST, ...INPUT_NUMBERS_TO_END]);
-    const logSpy = getLogSpy();
+    const NUMBER = [1, 1, 1, 1, 1, 1];
+    const LottoVal = new InputValidate();
 
-    const LottoCon = new LottoController();
-    await LottoCon.inputPurchaseMoney();
-
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await expect(LottoVal.lottoNumber(NUMBER)).rejects.toThrow(Error);
   });
 
   test('로또 번호(문자 포함) 예외 테스트', async () => {
-    const FIRST = ['1000', '1,2,a,3,4,5'];
-    const INPUT_NUMBERS_TO_END = ['1,2,3,4,5,6', '7'];
-    mockQuestions([...FIRST, ...INPUT_NUMBERS_TO_END]);
-    const logSpy = getLogSpy();
+    const NUMBER = ['a', 1, 2, 3, 4, 5];
+    const LottoVal = new InputValidate();
 
-    const LottoCon = new LottoController();
-    await LottoCon.inputPurchaseMoney();
-
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await expect(LottoVal.lottoNumber(NUMBER)).rejects.toThrow(Error);
   });
 
   test('보너스 번호 예외 테스트', async () => {
-    const FIRST = ['1000', '1,2,3,4,5,6', '-1'];
-    const INPUT_NUMBERS_TO_END = ['7'];
-    mockQuestions([...FIRST, ...INPUT_NUMBERS_TO_END]);
-    const logSpy = getLogSpy();
+    const NUMBER = 'a';
+    const LottoVal = new InputValidate();
 
-    const LottoCon = new LottoController();
-    await LottoCon.inputPurchaseMoney();
-
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await expect(LottoVal.bonusNumber(NUMBER)).rejects.toThrow(Error);
   });
 
   test('당첨번호, 보너스번호 중복 예외 테스트', async () => {
-    const FIRST = ['1000', '1,2,3,4,5,6', '6'];
-    const INPUT_NUMBERS_TO_END = ['7'];
-    mockQuestions([...FIRST, ...INPUT_NUMBERS_TO_END]);
-    const logSpy = getLogSpy();
+    const BONUS_NUMBER = 1;
+    const WIN_NUMBER = [1, 2, 3, 4, 5];
+    const LottoVal = new InputValidate();
 
-    const LottoCon = new LottoController();
-    await LottoCon.inputPurchaseMoney();
-
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await expect(
+      LottoVal.bonusNumber(BONUS_NUMBER, WIN_NUMBER),
+    ).rejects.toThrow(Error);
   });
 });
