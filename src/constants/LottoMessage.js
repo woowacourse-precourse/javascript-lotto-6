d7dc676ae9ca6f6a12cb4de8394d30bdf8ec990d
swarvy <swarvy0826@naver.com>
2023-11-07 00:42:45 +0900

    refactor : 상수 메시지 수정 및 금액 쉼표단위 추가

--- src/constants/LottoMessage.js
+++ src/constants/LottoMessage.js
@@ -1,33 +1,33 @@
-import { LOTTO_PRIZEVALUE } from './LottoOption.js';
+import { LOTTO_RANK } from './LottoOption.js';
 
 export const LOTTO_NOTIFICATION_FORMAT = Object.freeze({
   lottoAmount(amount) {
-    return `${amount}개를 구입했습니다.`;
+    return `${amount}개를 구매했습니다.`;
   },
 });
 
 export const LOTTO_NOTIFICATION_MESSAGE = Object.freeze({
-  enterLottoSeedMoney: '구입 금액을 입력해 주세요.\n',
-  enterLottoWinningNumber: '당첨 번호를 입력해 주세요.\n',
-  enterLottoWinningBonusNumber: '보너스 번호를 입력해 주세요.\n',
-  result: '당첨 통계\n---',
+  enterLottoSeedMoney: '\n구입 금액을 입력해 주세요.\n',
+  enterLottoWinningNumber: '\n당첨 번호를 입력해 주세요.\n',
+  enterLottoWinningBonusNumber: '\n보너스 번호를 입력해 주세요.\n',
+  result: '\n당첨 통계\n---',
 });
 
 export const LOTTO_RESULT_FORMANT = Object.freeze({
   first(amount) {
-    return `6개 일치 (${LOTTO_PRIZEVALUE.first}원) - ${amount}개`;
+    return `6개 일치 (${LOTTO_RANK.first.prizeValue.toLocaleString()}원) - ${amount}개`;
   },
   second(amount) {
-    return `5개 일치, 보너스 볼 일치 (${LOTTO_PRIZEVALUE.second}원) - ${amount}개`;
+    return `5개 일치, 보너스 볼 일치 (${LOTTO_RANK.second.prizeValue.toLocaleString()}원) - ${amount}개`;
   },
   third(amount) {
-    return `5개 일치 (${LOTTO_PRIZEVALUE.third}원) - ${amount}개`;
+    return `5개 일치 (${LOTTO_RANK.third.prizeValue.toLocaleString()}원) - ${amount}개`;
   },
   fourth(amount) {
-    return `4개 일치 (${LOTTO_PRIZEVALUE.fourth}원) - ${amount}개`;
+    return `4개 일치 (${LOTTO_RANK.fourth.prizeValue.toLocaleString()}원) - ${amount}개`;
   },
   fifth(amount) {
-    return `3개 일치 (${LOTTO_PRIZEVALUE.fifth}원) - ${amount}개`;
+    return `3개 일치 (${LOTTO_RANK.fifth.prizeValue.toLocaleString()}원) - ${amount}개`;
   },
   result(profit) {
     return `총 수익률은 ${profit}% 입니다.`;
