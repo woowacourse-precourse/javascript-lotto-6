3a8dd45a1b1ba0c1f9f66d9b3fd71fa9e24987cb
swarvy <swarvy0826@naver.com>
2023-11-07 01:29:09 +0900

    feat : 보너스 유효성 검사를 위한 LottoBonusNumberValidator 구현

---
+++ src/validator/LottoBonusNumberValidator.js
@@ -0,0 +1,36 @@
+import { LOTTO_ERROR_MESSAGE } from '../constants/LottoMessage.js';
+import { LOTTO_MAGICNUMBER } from '../constants/LottoOption.js';
+import ValidationError from '../error/ValidationError.js';
+import validateCommon from './CommonValidator.js';
+
+const checkValidNumber = (input) => {
+  if (!Number.isInteger(input)) {
+    throw new ValidationError(LOTTO_ERROR_MESSAGE.notAValidNumber);
+  }
+};
+
+const checkValidRange = (input) => {
+  if (
+    input > LOTTO_MAGICNUMBER.maxValue ||
+    input < LOTTO_MAGICNUMBER.minValue
+  ) {
+    throw new ValidationError(LOTTO_ERROR_MESSAGE.notAValidNumberRange);
+  }
+};
+
+const checkDuplicationNumber = (input, lottoNumbers) => {
+  if (lottoNumbers.includes(input)) {
+    throw new ValidationError(LOTTO_ERROR_MESSAGE.notAValidLottoNumber);
+  }
+};
+
+const validateLottoBonusNumber = (input, lottoNumbers) => {
+  const number = Number(input);
+
+  validateCommon(input);
+  checkValidNumber(number);
+  checkValidRange(number);
+  checkDuplicationNumber(number, lottoNumbers);
+};
+
+export default validateLottoBonusNumber;
