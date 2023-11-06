31643427aa133c027f984acb0c5f5452754afcd7
hankyeol-dev <ddovlek@kakao.com>
2023-11-06 15:41:45 +0900

    test (Utils) - 유틸리티 함수 테스트
    - genRandomLottoNumber
    - splitInput
    - rankLotto
    - calculateProfit

--- src/Utils.js
+++ src/Utils.js
@@ -6,21 +6,18 @@ const Utils = {
       (numb1, numb2) => numb1 - numb2
     ),
   splitInput: (input) => input.split(",").filter((element) => element !== ""),
-  rankLotto: (winningMatch, bonusMatch) => {
-    const rank =
-      winningMatch === 6
-        ? 4
-        : winningMatch === 5 && bonusMatch === 1
-        ? 3
-        : winningMatch === 5
-        ? 2
-        : winningMatch === 4
-        ? 1
-        : winningMatch === 3
-        ? 0
-        : "";
-    return rank;
-  },
+  rankLotto: (winningMatch, bonusMatch) =>
+    winningMatch === 6
+      ? 4
+      : winningMatch === 5 && bonusMatch === 1
+      ? 3
+      : winningMatch === 5
+      ? 2
+      : winningMatch === 4
+      ? 1
+      : winningMatch === 3
+      ? 0
+      : "",
   calculateProfit: (lotteryResults, purchasedCount) =>
     (
       ((lotteryResults[0] * 5000 +
