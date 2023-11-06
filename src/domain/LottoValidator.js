bb227991e5a234116132ebb6ed11b37547f56c97
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 05:17:59 +0900

    :sparkles: feat: 보너스 번호 유효성 검증 기능추가

--- src/domain/LottoValidator.js
+++ src/domain/LottoValidator.js
@@ -16,6 +16,24 @@ const LottoValidator = {
     }
   },
 
+  validBonusNumber(number, isContainning) {
+    const numberRegExp = /^\d+$/;
+    const numberic = numberRegExp.test(number) && !Number.isNaN(number);
+    const range = number >= 1 && number <= 45;
+
+    if (!numberic) {
+      throw new Error(ERROR_MESSAGE.invalidBonusNumber);
+    }
+
+    if (!range) {
+      throw new Error(ERROR_MESSAGE.invalidRange);
+    }
+
+    if (isContainning) {
+      throw new Error(ERROR_MESSAGE.invalidUniqueBonusNumber);
+    }
+  },
+
   isValidLength(numbers) {
     return numbers.length === 6;
   },
@@ -30,6 +48,7 @@ const LottoValidator = {
 
   isNumberic(numbers) {
     const numberRegExp = /^\d+$/;
+
     return numbers.every((number) => numberRegExp.test(number));
   },
 };
