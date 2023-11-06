310d16e8b824e4793aae716ed93631450faea82e
Myomyoring <narcissuscat@naver.com>
2023-11-06 22:21:54 +0900

    [feat] 수량에 맞게 오름차 순으로 로또 번호 생성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
 import { MESSAGE } from './constants.js';
 import { validation } from './validation.js';
 
 class App {
 	async buy() {
 		const price = await Console.readLineAsync(MESSAGE.PRICE);
-		validation.buy(price);
+		validation.buy(Number(price));
 		return Number(price) / 1000;
+	}
+	createNumber() {
+		const numbers = [];
+		while (numbers.length < 6) {
+			const number = MissionUtils.Random.pickNumberInRange(1, 45);
+			!numbers.includes(number) ? numbers.push(number) : '';
+		}
+		return numbers;
+	}
+	sortNumber(numbers) {
+		numbers.sort(function (a, b) {
+			return a - b;
+		});
+	}
+	lottoPrinter(tickets) {
+		const lottos = [];
+		let numbers = [];
+		while (lottos.length < tickets) {
+			numbers = this.createNumber();
+			this.sortNumber(numbers);
+			lottos.push(numbers);
+		}
+		return lottos;
+	}
+	createLotto(tickets) {
+		const lottos = this.lottoPrinter(tickets);
 	}
 	async play() {
 		try {
 			const tickets = await this.buy();
+			this.createLotto(tickets);
 		} catch (error) {
 			throw error;
 		}
