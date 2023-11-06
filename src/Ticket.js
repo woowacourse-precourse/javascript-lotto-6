693fcee5420607339a39b9e86a68ed4be36626c2
hzzzi <hji2004@naver.com>
2023-11-07 01:37:15 +0900

    feat: 티켓 클래스에 티켓 번호와 당첨 번호 비교하는 메소드, 등수 업데이트하는 메소드 구현

--- src/Ticket.js
+++ src/Ticket.js
@@ -25,6 +25,36 @@ class Ticket {
   get Rank() {
     return this.rank;
   }
+
+  compareNumbers(winningNumbers, bonusNumber) {
+    let intersection = this.numbers.filter((x) => winningNumbers.includes(x));
+    let isBonusNumberMatch = false;
+
+    if (intersection.length >= 5) {
+      if (this.numbers.includes(bonusNumber)) {
+        isBonusNumberMatch = true;
+      }
+    }
+    return [intersection, isBonusNumberMatch];
+  }
+
+  updateRank(intersection, isBonusNumberMatch) {
+    if (intersection.length === 3) {
+      this.rank = 5;
+    }
+    if (intersection.length === 4) {
+      this.rank = 4;
+    }
+    if (intersection.length === 5) {
+      this.rank = 3;
+    }
+    if (intersection.length === 5 && isBonusNumberMatch === true) {
+      this.rank = 2;
+    }
+    if (intersection.length === 6) {
+      this.rank = 1;
+    }
+  }
 }
 
 export default Ticket;
