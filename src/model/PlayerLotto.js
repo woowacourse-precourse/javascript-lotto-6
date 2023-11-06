b03f4577096cbc71e5bb577a61773cc2604c29ac
swarvy <swarvy0826@naver.com>
2023-11-07 03:00:56 +0900

    refactor : 사용자의 생성된 로또를 관리하는 PlayerLotto 클래스 구현

---
+++ src/model/PlayerLotto.js
@@ -0,0 +1,29 @@
+export default class PlayerLotto {
+  #lottoNumbers;
+
+  constructor(numbers) {
+    this.#lottoNumbers = numbers;
+  }
+
+  getLottoNumbers() {
+    return this.#lottoNumbers;
+  }
+
+  compare(winningNumbers, winningBonusNumber) {
+    const compareResult = {
+      mainNumber: 0,
+      bonusNumber: false,
+    };
+
+    this.#lottoNumbers.forEach((number) => {
+      if (winningNumbers.includes(number)) {
+        compareResult.mainNumber += 1;
+      }
+      if (number === winningBonusNumber) {
+        compareResult.bonusNumber = true;
+      }
+    });
+
+    return compareResult;
+  }
+}
