cefad296d14f0d07d33f234276d0c5a3d52feb64
doongeon <ndmb2012@gamil.com>
2023-11-06 12:20:27 +0900

    feat: get user input by console

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import Lotto from "./Lotto.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  getUserInput = async () => {
+    let userInput = '';
+
+    await MissionUtils.Console.readLineAsync('')
+      .then((input) => {
+        userInput = input;
+      })
+      .catch((err) => {
+        throw new Error("[ERROR]");
+      })
+
+    return userInput;
+  }
+
+
+  async play() {
+    try {
+      const userInput = await this.getUserInput();
+
+      console.log(userInput);
+
+    } catch(err) {
+      return Promise.reject(err);
+    }
+  }
 }
 
 export default App;
