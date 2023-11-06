d011c65bcd6ca7ffe4c03635d50707b59172b49b
Oh Dong Joon <ohdoju0905@gmail.com>
2023-11-06 22:28:10 +0900

    test(/__tests__/FunctionTest.js): inputWinningNumber에 대한 테스트 코드 추가
    <body>
    - 정상적인 값을 입력받았을 때 당첨 번호가 Lotto 객체의 #numbers에 저장된 값이 예상한 값과 같은지 확인
    - 비정상적인 값을 입력받았을 때 [ERROR]가 포함된 문자열을 출력하고 한 번 더 입력받는지 확인
    
    <footer>

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -39,5 +39,25 @@ describe('함수 기능 단위 테스트', () => {
 				},
 			);
 		});
+		describe('inputWinningNumber', () => {
+			test.each([
+				{ input: '1,2,3,4,5,6', output: [1, 2, 3, 4, 5, 6] },
+				{ input: '1,10,20,30,40,45', output: [1, 10, 20, 30, 40, 45] },
+			])('정상적인 로또 번호가 입력된 경우', async ({ input, output }) => {
+				Console.readLineAsync = jest.fn().mockResolvedValueOnce(input);
+				const lotto = await App.inputWinningNumbers();
+				expect(lotto.getNumbers()).toEqual(expect.arrayContaining(output));
+			});
+			test.each([
+				'1, 2, 3, 4, 5',
+				'1, 2, 3, 4, 5, 6, 7',
+				'1, 2, 3, 4, 5, 100',
+				'1, 2, 3, 4, 5, -1',
+				'1, 2, 3, 4, 5, 5',
+			])('비정상적인 로또 번호가 입력된 경우', async (input) => {
+				Console.readLineAsync = jest.fn().mockResolvedValueOnce(input);
+				await expect(App.inputWinningNumbers()).rejects.toThrow(ERROR);
+			});
+		});
 	});
 });
