67b4e426fea8c0c7b29918aa821d5d3b19d01283
KYHH <kyhh93@naver.com>
2023-11-07 02:28:07 +0900

    refactor: 컨트롤러 메서드 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 import Input from './UserInput.js';
 import Controller from './LottoController.js';
+import OutPut from './ComputerOutput.js';
 class App {
   async play() {
     const lottoGame = new Controller();
+    lottoGame.checkNumber();
     lottoGame.gameStart();
-    lottoGame.userInput();
   }
 }
 
