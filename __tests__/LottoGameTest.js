c5c5c1b85965daaa6640de55daa39ebac0c45edc
sohyun <kshyun0215@gmail.com>
2023-11-07 00:09:10 +0900

    test: 구매 로또와 당첨 로또 비교하는 테스트 케이스 추가

--- __tests__/LottoGameTest.js
+++ __tests__/LottoGameTest.js
@@ -24,22 +24,53 @@ describe('LottoGame 클래스 테스트', () => {
   });
 
   describe('구매한 로또와 당첨 번호를 비교한다.', () => {
-    test('당첨된 로또와 비교해 각 등수에 몇 개가 당첨되었는지 확인할 수 있는 결과를 반환한다.', () => {
-      const PURCHASED_LOTTO = [
-        new Lotto([1, 2, 3, 4, 5, 6]),
-        new Lotto([1, 2, 3, 10, 20, 7]),
-      ];
-      const WINNING_LOTTO = [1, 2, 3, 10, 20, 35];
-      const BONUS_NUMBER = 7;
-      const game = new LottoGame();
-      const result = game.compareLotto(
-        PURCHASED_LOTTO,
-        WINNING_LOTTO,
-        BONUS_NUMBER,
-      );
+    test.each([
+      {
+        test: '1등 1개, 4등 1개, 5등 1개',
+        purchasedLotto: [
+          new Lotto([1, 2, 3, 4, 5, 6]),
+          new Lotto([1, 4, 5, 6, 10, 11]),
+          new Lotto([1, 2, 3, 7, 10, 20]),
+        ],
+        winningLotto: [1, 2, 3, 4, 5, 6],
+        bonusNumber: 7,
+        expectedResult: [1, 1, 0, 0, 1],
+      },
+      {
+        test: '2등 1개, 5등 1개',
+        purchasedLotto: [
+          new Lotto([8, 21, 23, 41, 42, 43]),
+          new Lotto([3, 5, 11, 16, 32, 36]),
+          new Lotto([7, 11, 16, 35, 36, 44]),
+          new Lotto([1, 8, 11, 31, 41, 42]),
+        ],
+        winningLotto: [11, 16, 35, 36, 41, 44],
+        bonusNumber: 7,
+        expectedResult: [1, 0, 0, 1, 0],
+      },
+      {
+        test: '모두 당첨안됨',
+        purchasedLotto: [
+          new Lotto([1, 2, 3, 4, 5, 6]),
+          new Lotto([1, 2, 3, 4, 5, 7]),
+        ],
+        winningLotto: [11, 12, 13, 14, 15, 16],
+        bonusNumber: 42,
+        expectedResult: [0, 0, 0, 0, 0],
+      },
+    ])(
+      '각 등수에 몇 개가 당첨되었는지 확인할 수 있는 결과를 반환한다. ($test)',
+      ({ purchasedLotto, winningLotto, bonusNumber, expectedResult }) => {
+        const game = new LottoGame();
+        const result = game.compareLotto(
+          purchasedLotto,
+          winningLotto,
+          bonusNumber,
+        );
 
-      expect(result).toEqual([1, 0, 0, 1, 0]);
-    });
+        expect(result).toEqual(expectedResult);
+      },
+    );
 
     test.each([
       {
