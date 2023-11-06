0d9693b77e9136cadbd01b5f62ca174128b1930c
JeongSabibi <jsbb528@naver.com>
2023-11-06 23:56:20 +0900

    feat(LottoGame): 오름차순 정렬 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -5,6 +5,10 @@ class LottoGame {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 
+  sortAscending(numbers) {
+    return numbers.sort((a, b) => a - b);
+  }
+
 }
 
 export default LottoGame;
