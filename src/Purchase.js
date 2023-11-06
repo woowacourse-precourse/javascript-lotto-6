9992e17a78eaa1edb974d4a6bf8bfc96efc7b475
박성진 <jin1401@bagseongjin-ui-MacBookAir.local>
2023-11-06 11:21:54 +0900

    feat(constants) Messages.js, Conditions.js 생성

---
+++ src/Purchase.js
@@ -0,0 +1,5 @@
+"use strict";
+import { Console } from "@woowacourse/mission-utils";
+import MESSAGES from "./constants/Messages";
+
+export default class Purchase {}
---
+++ src/constants/Conditions.js
@@ -0,0 +1,14 @@
+/**
+ * 입력 형식 등 여러 조건들을 정의한 객체
+ */
+
+const CONDITIONS = {
+    //ex)
+    RANGE: {
+        MIN: 1,
+        MAX: 9,
+    },
+    NUMBER_LENGTH: 3,
+};
+
+export default CONDITIONS;
