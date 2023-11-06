a26c20865dc71f24a07152e93dac5c55811e9b54
Hyunji Seok <alleyji126@gmail.com>
2023-11-06 10:10:52 +0900

    feat(messages, data): update constants files

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,4 +1,7 @@
 const ZERO = 0;
+const SIX = 6;
 const THOUSAND = 1000;
+const MIN = 1;
+const MAX = 45;
 
-export { ZERO, THOUSAND };
+export { ZERO, SIX, THOUSAND, MIN, MAX };
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,18 +1,25 @@
-const INPUT = {
-  price: '구입금액을 입력해 주세요. \n',
-  winning_numbers: '당첨 번호를 입력해 주세요. \n',
-  bonus_number: '보너스 번호를 입력해 주세요. \n',
-};
+const INPUT = Object.freeze({
+  price: '구입금액을 입력해 주세요.',
+  winning_numbers: '당첨 번호를 입력해 주세요.',
+  bonus_number: '보너스 번호를 입력해 주세요.',
+});
 
-const ERROR = {
+const ERROR = Object.freeze({
   not_a_valid_number: '[ERROR] 로또 구입금액은 자연수만 입력이 가능합니다.',
   not_a_thousand_unit: '[ERROR] 로또 구입금액은 1,000원 단위로 입력이 가능합니다.',
-};
+});
 
-const OUTPUT = {
+const OUTPUT = Object.freeze({
+  count: '개를 구매했습니다.',
+  winning_statistics: '당첨 통계',
+  three_numbers: '3개 일치 (5,000원)',
+  four_numbers: '4개 일치 (50,000원)',
+  five_plus_bonus_numbers: '5개, 보너스 볼 일치 (30,000,000원)',
+  six_numbers: '6개 일치 (2,000,000,000원)',
+});
 
-};
-
+const COUNT = '개';
+const HYPHEN = '-';
 const LINE_BREAK = '\n';
 
-export { INPUT, ERROR, OUTPUT, LINE_BREAK };
+export { INPUT, ERROR, OUTPUT, COUNT, HYPHEN, LINE_BREAK };
