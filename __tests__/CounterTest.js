e39dbf5bc784c44385286dfcf0c842ea18592a12
JunSeok <ppu1234@naver.com>
2023-11-06 23:02:41 +0900

    test: Counter 클래스 테스트 코드 작성

---
+++ __tests__/CounterTest.js
@@ -0,0 +1,33 @@
+import Counter from '../src/domains/Counter';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+describe('카운터 클래스 테스트', () => {
+  test('지불한 금액만큼 로또를 받아야 한다.', () => {
+    // Arrange
+    const inputAmount = 2000;
+
+    const input = [
+      [1, 2, 3, 4, 5, 6],
+      [3, 4, 5, 6, 7, 8],
+    ];
+    mockRandoms(input);
+
+    const output = [
+      [1, 2, 3, 4, 5, 6],
+      [3, 4, 5, 6, 7, 8],
+    ];
+
+    // Act
+    const myLottos = new Counter(inputAmount).giveLotto;
+
+    // Assert
+    expect(myLottos).toEqual(output);
+  });
+});
