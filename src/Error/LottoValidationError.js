0331e4574a302957b1307ab850187edfae04d710
최나기 <choenagi@choenagiui-MacBookAir.local>
2023-11-06 20:17:59 +0900

    feat(error): LottoValidationError 클래스 구현
    Error 클래스를 상속받아 Lotto 프로그램에서 쓰일 LottoValidationError 클래스를 구현

---
+++ src/Error/LottoValidationError.js
@@ -0,0 +1,13 @@
+import ERROR_CONSTANT from "../Constant/ErrorConstant.js";
+
+class LottoValidationError extends Error {
+  static name = 'LottoValidationError';
+
+  constructor(message) {
+    if (typeof (message) !== "string")
+      throw new Error (ERROR_CONSTANT.ERROR_PRIFIX + message);
+    super(ERROR_CONSTANT.ERROR_PRIFIX + message);
+  };
+}
+
+export default LottoValidationError;
