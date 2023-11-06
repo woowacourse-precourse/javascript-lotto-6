fb926344183f3f40b3f8a49955ee06db320435aa
teawon <xodnjsvkvk9@gmail.com>
2023-11-06 18:30:31 +0900

    feat : Lotto 도메인 기능 구현

--- src/domains/Lotto.js
+++ src/domains/Lotto.js
@@ -10,9 +10,15 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers.sort((a, b) => a - b);
+  }
 }
 
 export default Lotto;
