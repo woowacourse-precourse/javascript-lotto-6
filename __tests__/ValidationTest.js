416319643fdfc51717d39bda3cc904a42ad781ff
hong <ahi200@naver.com>
2023-11-06 21:53:31 +0900

    test: Validation.hasProperRange 테스트코드 구현

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -9,3 +9,19 @@ describe('Validation.hasDuplication', () => {
     expect(Validation.hasDuplication([1, 2, 3, 4, 5, 6, 7, 8])).toBe(false);
   });
 });
+
+describe('Validation.hasProperRange', () => {
+  test('1~45범위인 경우 true를 반환한다', () => {
+    expect(Validation.hasProperRange([1, 2, 3, 4, 5, 6])).toBe(true);
+    expect(Validation.hasProperRange([7, 8, 9, 10, 11, 12])).toBe(true);
+    expect(Validation.hasProperRange([13, 14, 15, 16, 17, 18])).toBe(true);
+    expect(Validation.hasProperRange([41, 42, 43, 44, 45, 1])).toBe(true);
+  });
+
+  test('1~45범위가 아닌경우 false를 반환한다', () => {
+    expect(Validation.hasProperRange([1, 2, 3, 4, 5, 46])).toBe(false);
+    expect(Validation.hasProperRange([7, 231, 9, 10, 11, 12])).toBe(false);
+    expect(Validation.hasProperRange([55, 14, 15, 16, 17, 18])).toBe(false);
+    expect(Validation.hasProperRange([0, 14, 15, 16, 17, 18])).toBe(false);
+  });
+});
