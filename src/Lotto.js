47ab585ff72200d13525847962803e1fccedcfd7
Sohye <saeyan2477@gmail.com>
2023-11-06 21:22:11 +0900

    feat: 케이스 구분하기

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+import { Console } from "@woowacourse/mission-utils";
+import { Validation } from "./Validation.js";
+import { LOTTO_RULE, GAME_MESSAGES } from "./Constants.js";
+
 class Lotto {
   #numbers;
 
@@ -10,9 +14,37 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if(!Validation.isUnique(numbers)){
+      throw new Error("[ERROR] 로또 번호는 유일해야 합니다.")
+    }
+
+    numbers.every((number)=>{
+      if(!Validation.isNumber(number)){
+        throw new Errow("[ERROR] 로또 번호는 숫자여야 합니다.")
+      }
+
+      if(!Validation.isPositive(number)){
+        throw new Errow("[ERROR] 로또 번호는 양수여야 합니다.")
+      }
+
+      if(!Validation.isOnRange(number)){
+        throw new Errow("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
+      }
+
+      if(!Validation.isInteger(number)){
+        throw new Errow("[ERROR] 로또 번호는 정수여야 합니다.")
+      }
+    })
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  printNumbers() {
+    return Console.print(this.#numbers.join(","));
+  }
 }
 
 export default Lotto;
