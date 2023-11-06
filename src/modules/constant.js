ed76ce3149a1190b98d3fd46e30cf156591d3889
hs <tlsgustn93@naver.com>
2023-11-06 18:02:55 +0900

    feat: 상수 정의

---
+++ src/modules/constant.js
@@ -0,0 +1,23 @@
+const GAME_PRIZE = {
+    1: 2000000000,
+    2: 30000000,
+    3: 1500000,
+    4: 50000,
+    5: 5000
+}
+
+const CORRECT_MESSAGE = {
+    FIRST: '6개 일치',
+    SECOND: '5개 일치, 보너스 볼 일치',
+    THIRD: '5개 일치',
+    FOURTH: '4개 일치',
+    FIFTH: '3개 일치'
+}
+
+const LOTTO_MESSAGE = {
+    RESULT_VIEW: '당첨 통계',
+    WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
+    BONUS_NUMBERS: '보너스 번호를 입력해 주세요.\n'
+}
+
+export { GAME_PRIZE, CORRECT_MESSAGE, LOTTO_MESSAGE } 
