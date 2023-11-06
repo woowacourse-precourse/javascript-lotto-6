7c258dd01d97c8ae22d2a19d6fc87b8a8d6b8825
innerstella <suji0509@naver.com>
2023-11-06 17:25:22 +0900

    feat : 2) 발매할 로또 수량 및 번호 출력

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 
 class App {
   constructor() {
     this.pay = 0; // 구입 개수
+    this.lottos = []; // 발행한 로또
   }
 
   async inputPay() {
     do {
       try {
-        const input = await Console.readLineAsync("구입금액을 입력해 주세요.");
+        const input = await Console.readLineAsync(
+          "구입금액을 입력해 주세요. \n"
+        );
         if (isNaN(+input)) {
           Console.print("[ERROR] 금액은 숫자로 입력해 주세요.");
           this.inputPay();
@@ -24,8 +27,20 @@ class App {
     } while (this.pay === 0);
   }
 
+  async makeLotto() {
+    Console.print(`\n ${this.pay}개를 구매했습니다.`);
+    for (let i = 0; i < this.pay; i++) {
+      const lotto = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+        (a, b) => a - b
+      );
+      Console.print(lotto);
+      this.lottos.push(lotto);
+    }
+  }
+
   async play() {
     await this.inputPay();
+    await this.makeLotto();
     // const lotto = new Lotto()
   }
 }
