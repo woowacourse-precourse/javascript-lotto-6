28d943415b5f1258dbb4cc759bc7063100c65163
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-06 18:31:56 +0900

    feat(사용자):당첨 번호/보너스 번호 입력 기능 추가

--- src/User.js
+++ src/User.js
@@ -4,15 +4,33 @@ import { Validator } from './validation/Validator.js';
 
 export class User {
   #money;
+  #winningNumbers;
+  #bonusNumber;
 
   getMoney() {
     return this.#money;
+  }
+
+  getWinningNumbers() {
+    return this.#winningNumbers;
   }
 
   async inputMoney() {
     const money = await Input.readInteger('구입금액을 입력해 주세요.');
 
     await this.#setMoney(money);
+  }
+
+  async inputWinningNumbers() {
+    const numbers = await Input.readMultipleValues('당첨 번호를 입력해 주세요.');
+
+    await this.#setWinningNumbers(numbers);
+  }
+
+  async inputBonusNumber() {
+    const number = await Input.readMultipleValues('보너스 번호를 입력해 주세요.');
+
+    await this.#setBonusNumber(number);
   }
 
   async #setMoney(money) {
@@ -23,6 +41,28 @@ export class User {
     } catch (e) {
       Console.print(`${e.name} ${e.message} `);
       return await this.inputMoney();
+    }
+  }
+
+  async #setWinningNumbers(numbers) {
+    try {
+      Validator.Lotto.validate(numbers);
+
+      this.#winningNumbers = numbers;
+    } catch (e) {
+      Console.print(`${e.name} ${e.message} `);
+      return await this.inputWinningNumbers();
+    }
+  }
+
+  async #setBonusNumber(number) {
+    try {
+      if (this.#winningNumbers.contain(number)) throw new Error('error');
+
+      this.#bonusNumber = number;
+    } catch (e) {
+      Console.print(`${e.name} ${e.message} `);
+      return await this.inputBonusNumber();
     }
   }
 }
