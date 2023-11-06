3d65d3fa29ad4b125bdb1eaac754c62f06b5795c
0jenn0 <jenn0.6n@gmail.com>
2023-11-06 14:13:57 +0900

    Feat: 당첨 통계 출력하기 추가, 구매한 로또 번호들 출력하기 추가

---
+++ src/Views/OutputView.js
@@ -0,0 +1,36 @@
+import { Console } from "@woowacourse/mission-utils";
+import LOTTO_SETTINGS from "../config/gameSetting.js";
+
+export default class OutputView {
+  static printPurchseResults(numOfLottos) {
+    Console.print(`${numOfLottos}개를 구매했습니다.`);
+  }
+
+  static printResultMessage(result) {
+    let resultMessages = "";
+    for (const [key, { prize, matchNum }] of Object.entries(
+      LOTTO_SETTINGS.WINNINGS
+    )) {
+      let text = `${matchNum}개 일치`;
+
+      if (key === "SECOND_PRIZE") text += ", 보너스 볼 일치";
+      resultMessages += `${text} (${prize.toLocaleString()}원) - ${
+        result[key]
+      }개\n`;
+    }
+
+    Console.print(resultMessages);
+  }
+
+  static printLottos(lottosArray) {
+    const output = lottosArray
+      .map((numbers) => `[${numbers.join(", ")}]`)
+      .join("\n");
+
+    Console.print(output);
+  }
+
+  static printYieldRate(yieldRate) {
+    Console.print(`총 수익률은 ${yieldRate}%입니다.`);
+  }
+}
