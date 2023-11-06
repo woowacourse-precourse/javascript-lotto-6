d27119ca0ca8d1ed69b02454e14f2f3122ca0cb5
8leamnk <silkviewrk@gmail.com>
2023-11-06 20:17:25 +0900

    test(randomModelTest): 로또 발행 기능을 테스트
    테스트 내용:
    - 발행된 로또 각각이 오름차순으로 잘 정렬되는지 테스트
    - 각각 오름차순으로 정렬된 로또가 n개 발행되어 이차원 배열에 담겼는지 테스트
    - 배열의 원소, 즉 로또 정보가 문자열로 잘 변환되는지 테스트

---
+++ __tests__/models/randomModelTest.js
@@ -0,0 +1,82 @@
+import randomModel from '../../src/models/randomModel';
+
+const mockOneOfRandom = (numbers) => {
+  randomModel.getOneOfRandom = jest.fn();
+  numbers.reduce(
+    (acc, random) => acc.mockReturnValueOnce(random),
+    randomModel.getOneOfRandom,
+  );
+};
+
+describe('오름차순 정렬', () => {
+  test('오름차순으로 정렬된다.', () => {
+    // given
+    const input = [100, 123, 77, 47, 96, 55, 45, 1, 3, 0];
+    const output = [0, 1, 3, 45, 47, 55, 77, 96, 100, 123];
+
+    // when
+    const result = randomModel.sortRandom(input);
+
+    // then
+    expect(result).toEqual(output);
+  });
+
+  test('오름차순으로 제대로 정렬이 되는가', () => {
+    // given
+    const input = [5, 1, 3, 2, 4];
+    const output = {
+      notEqual: [1, 2, 3, 5, 4],
+      equal: [1, 2, 3, 4, 5],
+    };
+
+    // when
+    const result = randomModel.sortRandom(input);
+
+    // then
+    expect(result).not.toEqual(output.notEqual);
+    expect(result).toEqual(output.equal);
+  });
+});
+
+test('오름차순으로 정렬된 배열이 n개 담겨 있는 이차원 배열이 반환된다.', () => {
+  // given
+  const INPUT_NUMBER = 3;
+  const random = [
+    [5, 4, 1, 3, 2, 7],
+    [7, 6, 9, 8, 10, 13],
+    [45, 37, 12, 25, 19, 8],
+  ];
+  const output = [
+    [1, 2, 3, 4, 5, 7],
+    [6, 7, 8, 9, 10, 13],
+    [8, 12, 19, 25, 37, 45],
+  ];
+
+  mockOneOfRandom([...random]);
+
+  // when
+  const result = randomModel.getRandom(INPUT_NUMBER);
+
+  // then
+  expect(result).toEqual(output);
+});
+
+test('배열의 원소가 문자열로 변환된다.', () => {
+  // given
+  const input = [
+    [1, 2, 3, 4, 5, 7],
+    [6, 7, 8, 9, 10, 3],
+    [11, 14, 12, 10, 8, 7],
+  ];
+  const output = [
+    `[1, 2, 3, 4, 5, 7]\n`,
+    `[6, 7, 8, 9, 10, 3]\n`,
+    `[11, 14, 12, 10, 8, 7]\n`,
+  ];
+
+  // when
+  const result = randomModel.getStringOfRandom(input);
+
+  // then
+  expect(result).toEqual(output);
+});
