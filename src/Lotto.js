dbb6ee23754ca6db0232204877f11758522952ce
JungHun98 <j7papa@naver.com>
2023-11-06 15:29:16 +0900

    feat: Lotto 클래스 번호 반환 메소드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,19 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    const set = new Set(numbers);
+    if (numbers.length !== set.size) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    const sortedNumbers = this.#numbers.sort((a, b) => a - b);
+    return sortedNumbers;
+  }
 }
 
 export default Lotto;
