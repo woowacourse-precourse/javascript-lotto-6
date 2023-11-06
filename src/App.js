4aa3bf7cb9a6f2e054d944af9f02d476e28a60c9
swJaNG12 <cbdjej38483@gmail.com>
2023-11-06 11:37:10 +0900

    refactor(App) : add try-catch in play method
    add try-catch block in play method to handle errors thrown by
    validateLottoAmount method in Lotto model.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { View } from './LottoView.js';
 import Lotto from './Lotto.js';
-import { Console } from '@woowacourse/mission-utils';
 
 class App {
   constructor() {
     this.view = new View();
   }
   async play() {
-    const lottoAmount = await this.view.getLottoPerchaseAmount();
-    let model = new Lotto(lottoAmount);
+    try {
+      const lottoAmount = await this.view.getLottoPerchaseAmount();
+      let model = new Lotto(lottoAmount);
 
-    const isValidateLottoAmount = model.validateLottoAmount();
+      model.validateLottoAmount();
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
