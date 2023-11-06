30b69cb08db843919e95d7cf36425d50bf85b325
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-06 16:02:37 +0900

    test: 당첨 번호와 로또 번호를 비교하는 기능에 대한 테스트 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -19,9 +19,10 @@ describe("로또 클래스 테스트", () => {
   // 아래에 추가 테스트 작성 가능
   describe("getNumbers 메서드 테스트", () => {
     beforeEach(() => {
-      randomNumbers = [1, 2, 3, 4, 5, 6, 7];
+      randomNumbers = [1, 2, 3, 4, 5, 6];
       lotto = new Lotto(randomNumbers);
     });
+
     test("getNumbers 메서드가 존재해야 한다.", () => {
       expect(typeof lotto.getNumbers).toBe("function");
     });
@@ -29,6 +30,30 @@ describe("로또 클래스 테스트", () => {
     test("getNumbers 메서드를 호출하면 생성할때 전달한 로또 숫자를 반환해야 한다.", () => {
       const receivedNumbers = lotto.getNumbers();
       expect(receivedNumbers).toBe(randomNumbers);
+    });
+  });
+
+  describe("matchNumbers 메서드 테스트", () => {
+    beforeEach(() => {
+      randomNumbers = [1, 2, 3, 4, 5, 6];
+      lotto = new Lotto(randomNumbers);
+    });
+
+    test("matchNumbers 메서드가 존재해야 한다.", () => {
+      expect(typeof lotto.matchNumbers).toBe("function");
+    });
+
+    test("matchNumbers 메서드를 호출하면 전달받은 번호와 몇 개가 일치하는지 반환하다.", () => {
+      const mockLottoWinningNumbers = {
+        lottoWinningNumbers: [2, 3, 4, 5, 6, 7],
+        bonousNumber: 1,
+      };
+      const mockMatchResult = {
+        lottoWinningNumbers: 5,
+        bonousNumber: 1,
+      };
+      const matchResult = lotto.matchNumbers(mockLottoWinningNumbers);
+      expect(matchResult).toEqual(mockMatchResult);
     });
   });
 });
