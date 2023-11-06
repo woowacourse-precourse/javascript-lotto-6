52257c43d86f52650941654fb9a48718c3fb6c78
Jaewon <wellbird@naver.com>
2023-11-06 14:00:30 +0900

    feat : 출력 메세지 파일 생성

---
+++ src/constant/Message.js
@@ -0,0 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export const userInputMessage = Object.freeze({
+  purchase: () => Console.print("구입금액을 입력해 주세요."),
+  winningNumber: () => Console.print("당첨 번호를 입력해주세요."),
+  bonusNumber: () => Console.print("보너스 번호를 입력해주세요."),
+});
+
+export const perchaseResultMessage = (amount) => {
+  Console.print(`${amount}개를 구매했습니다.`);
+};
+
+export const winningResultMessage = ([first, second, third, fourth, fifth]) => {
+  Console.print("당첨통계");
+  Console.print("---");
+  Console.print(`3개 일치 (5,000원) - ${fifth}개`);
+  Console.print(`4개 일치 (5,000원) - ${fourth}개`);
+  Console.print(`5개 일치 (5,000원) - ${third}개`);
+  Console.print(`5개 일치, 보너스 볼 일치 (5,000원) - ${second}개`);
+  Console.print(`6개 일치 (5,000원) - ${first}개`);
+};
+
+export const returnRateMessage = (rate) => {
+  Console.print(`총 수익률은 ${rate}% 입니다.`);
+};
+
+export const lineBreakMessage = () => Console.print("\n");
