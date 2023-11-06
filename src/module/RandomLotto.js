6bb4f526c4033da8f575153f343f15e77337a8b5
youngchann <lych96@gmail.com>
2023-11-07 01:24:08 +0900

    fix: 매개변수 수정
    - 인자로 받은 값을 로또 개수로 계산하도록 수정

--- src/module/RandomLotto.js
+++ src/module/RandomLotto.js
@@ -1,7 +1,8 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-const purchasedLottoArray = count => {
+const purchasedLottoArray = userInput => {
   const lottoArray = [];
+  const count = userInput / 1000;
   for (let i = 0; i < count; i += 1) {
     lottoArray.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
   }
