690d0cfc6b64dce7f5c6534548dc23a85c4e2ba8
baegyeong <beth0929@naver.com>
2023-11-06 20:23:20 +0900

    feat(BuyPrice): 구입 금액 클래스 구현
    - 유효성 검사
    - getter 정의

---
+++ src/BuyPrice.js
@@ -0,0 +1,31 @@
+import { validation } from "./utils/validate.js";
+import { BUY_PRICE_ERROR } from "./constant/ERROR.js";
+
+class BuyPrice {
+  #buyPrice;
+
+  constructor(buyPrice) {
+    this.#validate(buyPrice);
+    this.#buyPrice = buyPrice;
+  }
+
+  #validate(price) {
+    if (validation.isDivideThousand(price)) {
+      throw new Error(BUY_PRICE_ERROR.isDivideThousand);
+    }
+
+    if (validation.isZero(price)) {
+      throw new Error(BUY_PRICE_ERROR.isZero);
+    }
+
+    if (validation.isNumber(price)) {
+      throw new Error(BUY_PRICE_ERROR.isNaN);
+    }
+  }
+
+  get buyPrice() {
+    return this.#buyPrice;
+  }
+}
+
+export default BuyPrice;
