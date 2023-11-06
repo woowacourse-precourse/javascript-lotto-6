b52446e5c283c479ae71e2b4f5fda6a7bc1548b9
hs <tlsgustn93@naver.com>
2023-11-06 19:33:54 +0900

    refactor: App 함수 변경

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import LottoMain from "./modules/LottoMain.js";
 
 class App {
   async play() {
-    const lotto1 = new LottoMain();
-    await lotto1.start();
+    const lottoMain = new LottoMain();
+    await lottoMain.start();
   }
 }
 
