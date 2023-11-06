09470bd1ddc242528f5155cae4458d90925c6581
swarvy <swarvy0826@naver.com>
2023-11-07 03:00:34 +0900

    refactor : 상수 메시지 개행 수정

--- src/constants/LottoMessage.js
+++ src/constants/LottoMessage.js
@@ -3,12 +3,12 @@ import { DIVIDER } from './Symbol.js';
 
 export const LOTTO_NOTIFICATION_FORMAT = Object.freeze({
   lottoAmount(amount) {
-    return `${amount}개를 구매했습니다.`;
+    return `\n${amount}개를 구매했습니다.`;
   },
 });
 
 export const LOTTO_NOTIFICATION_MESSAGE = Object.freeze({
-  enterLottoSeedMoney: '\n구입 금액을 입력해 주세요.\n',
+  enterLottoSeedMoney: '구입 금액을 입력해 주세요.\n',
   enterLottoWinningNumber: '\n당첨 번호를 입력해 주세요.\n',
   enterLottoWinningBonusNumber: '\n보너스 번호를 입력해 주세요.\n',
   result: '\n당첨 통계\n---',
@@ -31,7 +31,7 @@ export const LOTTO_RESULT_FORMANT = Object.freeze({
     return `3개 일치 (${LOTTO_RANK.fifth.prizeValue.toLocaleString()}원) - ${amount}개`;
   },
   result(profit) {
-    return `총 수익률은 ${profit}% 입니다.`;
+    return `총 수익률은 ${profit}%입니다.`;
   },
 });
 
