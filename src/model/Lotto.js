ef34a055d3393aa48e460914e144ebd1f4261d68
Goonco <s07019@naver.com>
2023-11-06 22:16:40 +0900

    :sparkles: 당첨 로또와 보너스 번호를 바탕으로 점수를 채점하는 기능 추가

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -13,15 +13,32 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const dupCheck = new Set(numbers);
+    if(dupCheck.size !== numbers.length) throw new Error("[ERROR] 로또 번호 중복");
+  }
+
+  getNumbers(){
+    return this.#numbers;
   }
 
   #sortLotto(){
-    this.#numbers.sort();
+    this.#numbers.sort((a,b) => a-b);
   }
 
   printLotto(){
     const formatedMessage = this.#numbers.join(', ');
     Output.outputMessage(`[${formatedMessage}]`);
+  }
+
+  compareToWinningLotto(winningLotto, bonusNumber){
+    let score = 0;
+    winningLotto.getNumbers().forEach((num) => {
+      if(this.#numbers.includes(num)) score += 1;
+    })
+
+    if(score === 5 && this.#numbers.includes(bonusNumber)) score += 0.5;
+    return score;
   }
 }
 
