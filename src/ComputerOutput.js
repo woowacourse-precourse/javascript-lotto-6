e6e4c6541b230af2b1f93f5d44ebe53e0f06cd25
KYHH <kyhh93@naver.com>
2023-11-06 23:42:21 +0900

    feat: Ouput 클래스

---
+++ src/ComputerOutput.js
@@ -0,0 +1,12 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+import Input from './UserInput.js';
+import Lotto from './Lotto.js';
+
+class OutPut {
+
+  ticketPublish() {
+
+  }
+}
+
+export default OutPut;
