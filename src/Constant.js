b9e88f8194ca6cd06db9222a728ff8e936049ba0
yesiamok3 <jaehyunok@cau.ac.kr>
2023-11-07 00:38:14 +0900

    feat: 로또번호 중복 확인
    로또번호 생성 시 중복 확인.
    중복 시 에러 생성.

--- src/Constant.js
+++ src/Constant.js
@@ -1,4 +1,6 @@
 export const ERROR = {
     MONEY_IS_NAN: '[ERROR] 구입금액을 숫자로 입력해주세요.',
     MONEY_HAS_REMAINDER: '[ERROR] 구입금액을 1000원 단위로 입력해주세요',
+    LOTTO_NUMBER_NOT_SIX: '[ERROR] 로또 번호는 6개여야 합니다.',
+    LOTTO_NUMBER_IS_REDUNDANT: '[ERROR] 로또 번호가 중복됩니다.',
 };
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
-
+import { ERROR } from './Constant.js'
 
 class Lotto {
   #numbers;
@@ -11,7 +11,12 @@ class Lotto {
   
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR.LOTTO_NUMBER_NOT_SIX);
+    }
+
+    const numberSet = new Set(numbers);
+    if (numbers.length !== numberSet.length) {
+      throw new Error(ERROR.LOTTO_NUMBER_IS_REDUNDANT);
     }
   }
 
