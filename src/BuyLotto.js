b550823736a80d9f4ac58f8ac9024a27276355c6
GimGayeon <kky1373@naver.com>
2023-11-06 13:47:21 +0900

    feat: 1회 발행 시 1~45 사이 랜덤 숫자 6개 뽑기

--- src/BuyLotto.js
+++ src/BuyLotto.js
@@ -1,3 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+const { Random } = MissionUtils;
 class BuyLotto {
   #lottoCount;
   #money;
@@ -12,6 +14,9 @@ class BuyLotto {
     if (Number.isNaN(Number(money))) {
       throw new Error("[ERROR] 돈은 숫자여야 합니다.");
     }
+    if (money < 1000) {
+      throw new Error("[ERROR] 돈은 1000원 이상이여야 합니다.");
+    }
     if (money % 1000 !== 0) {
       throw new Error("[ERROR] 돈은 1000원으로 나누어 떨어져야 합니다.");
     }
@@ -23,6 +28,13 @@ class BuyLotto {
   get getMoney() {
     return this.#money;
   }
+
+  buyOneLotto() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
 }
+const buyLotto = new BuyLotto("3000");
+console.log(buyLotto.getLottoCount);
+console.log(buyLotto.buyLotto());
 
 export default BuyLotto;
