81d7b375a90bfe383660440d967d27245e1bf816
gouz7514 <gouz7514@gmail.com>
2023-11-06 18:07:25 +0900

    Fix : LottoTest descirbe문 위치 구별 및 테스트명 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -9,27 +9,28 @@ const getLogSpy = () => {
 
 describe("로또 클래스 테스트", () => {
   describe("Test 1. 로또 번호 테스트", () => {
-    test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+    test("Test 1-1. 로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
       expect(() => {
         new Lotto([1, 2, 3, 4, 5, 6, 7]);
       }).toThrow("[ERROR]");
     });
   
-    test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+    test("Test 1-2. 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
       expect(() => {
         new Lotto([1, 2, 3, 4, 5, 5]);
       }).toThrow("[ERROR]");
     });
   
     // 아래에 추가 테스트 작성 가능
-    test("로또 번호에 1 ~ 45 사이의 숫자가 아닌 숫자가 있으면 예외가 발생한다.", () => {
+    test("Test 1-3. 로또 번호에 1 ~ 45 사이의 숫자가 아닌 숫자가 있으면 예외가 발생한다.", () => {
       expect(() => {
         new Lotto([1, 2, 3, 4, 5, 46]);
       }).toThrow("[ERROR]");
     });
+  });
 
-  describe("로또 메소드 테스트", () => {
-    test("로또가 자기 자신의 번호를 출력한다.", () => {
+  describe("Test 2. 로또 메소드 테스트", () => {
+    test("Test 2-1. 로또가 자기 자신의 번호를 출력한다.", () => {
       const logSpy = getLogSpy();
       const numbers = [1, 2, 3, 4, 5, 6];
       const output = `[${numbers.join(", ")}]`;
@@ -38,7 +39,7 @@ describe("로또 클래스 테스트", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
 
-    test("로또가 자신의 번호와 당첨 번호, 보너스 번호를 비교하여 결과를 반환한다.", () => {
+    test("Test 2-2. 로또가 자신의 번호와 당첨 번호, 보너스 번호를 비교하여 결과를 반환한다.", () => {
       const numbers = [
         [1, 2, 3, 4, 5, 6],
         [1, 2, 3, 4, 5, 7],
@@ -85,5 +86,4 @@ describe("로또 클래스 테스트", () => {
       })
     });
   });
-  })
 });
