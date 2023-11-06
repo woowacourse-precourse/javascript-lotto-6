9b2371a143063046db320722503552feed0d5e1f
Majesty-jun <hkwns01@gmail.com>
2023-11-06 15:10:53 +0900

    FIX: 상수 줄바꿈 수정

--- src/constants.js
+++ src/constants.js
@@ -10,11 +10,11 @@ export const ASK_PRICE = '구매 금액을 입력해주세요.\n';
 
 export const BUY_LOTTO = '개를 구매했습니다.';
 
-export const ANSWER_LOTTO = '당첨 번호를 입력해주세요\n';
+export const ANSWER_LOTTO = '당첨 번호를 입력해주세요.\n';
 
 export const BONUS_LOTTO = '보너스 번호를 입력해주세요.\n';
 
-export const INFO_RESULT = '\n당첨 통계\n---';
+export const INFO_RESULT = '당첨 통계\n---';
 
 export const WINNING_PRICE = [5000, 50000, 1500000, 30000000, 2000000000];
 
