52a8a9a55215b4e49be3b832147be45802082344
hong <ahi200@naver.com>
2023-11-06 22:14:29 +0900

    test: Validation.isProperPurchaseAmount 테스트코드 구현

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -25,3 +25,24 @@ describe('Validation.hasProperRange', () => {
     expect(Validation.hasProperRange([0, 14, 15, 16, 17, 18])).toBe(false);
   });
 });
+
+describe('Validation.isProperPurchaseAmount', () => {
+  test('구입 금액이 1000원보다 작은 경우 false이다.', () => {
+    expect(Validation.isProperPurchaseAmount(999)).toBe(false);
+    expect(Validation.isProperPurchaseAmount(1)).toBe(false);
+    expect(Validation.isProperPurchaseAmount(0)).toBe(false);
+    expect(Validation.isProperPurchaseAmount(-1)).toBe(false);
+  });
+
+  test('구입 금액이 1000보다 크고 1000으로 나누어 떨어지면 true이다.', () => {
+    expect(Validation.isProperPurchaseAmount(1001000)).toBe(true);
+    expect(Validation.isProperPurchaseAmount(2000)).toBe(true);
+    expect(Validation.isProperPurchaseAmount(3000)).toBe(true);
+  });
+
+  test('구입 금액이 1000보다 크지만 1000으로 나누어 떨어지지 않으면 false이다.', () => {
+    expect(Validation.isProperPurchaseAmount(12300)).toBe(false);
+    expect(Validation.isProperPurchaseAmount(2010)).toBe(false);
+    expect(Validation.isProperPurchaseAmount(3001)).toBe(false);
+  });
+});
