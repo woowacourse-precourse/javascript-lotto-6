6851965e8d74630603c1d666e56245155ee3e453
dongbin420 <dongbin420@gmail.com>
2023-11-07 00:31:29 +0900

    fix: fix wrong string for winning money

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ export const OUTPUT_MESSAGE = Object.freeze({
 });
 
 export const PRIZES_MESSAGE = Object.freeze({
-  3: '3개 일치 (5000원)',
+  3: '3개 일치 (5,000원)',
   4: '4개 일치 (50,000원)',
   5: '5개 일치 (1,500,000원)',
   6: '5개 일치, 보너스 볼 일치 (30,000,000원)',
