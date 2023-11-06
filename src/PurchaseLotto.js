debe4cacac0b012457604c39282ee76dab1fa133
woo7i <or2447221@gmail.com>
2023-11-06 19:09:31 +0900

    fix(PurchaseLotto) : 초기화 했을 시 값 존재 유무 확인 기능 수정

--- src/PurchaseLotto.js
+++ src/PurchaseLotto.js
@@ -5,19 +5,27 @@ class PurchaseLotto {
   constructor() {
     this.inputPrice = null;
     this.lottoCount = 0;
+    this.initialized = false;
   }
 
   // 비동기로 처리해야 할 inputPrice 값을 다시 초기화 하기 위한 함수.
   async initialize() {
-    this.inputPrice = await inputLottoNum();
-    this.lottoCount = this.inputPrice / LOTTO_NUMBERS.PAY_LOTTO_MONEY;
+    if (!this.initialized) {
+      this.inputPrice = await inputLottoNum();
+      this.lottoCount = this.inputPrice / LOTTO_NUMBERS.PAY_LOTTO_MONEY;
+      this.initialized = true;
+    }
+  }
+
+  getLottoCount() {
+    return this.lottoCount;
   }
 
   async alertPurchaseLotto() {
     if (this.inputPrice % LOTTO_NUMBERS.PAY_LOTTO_MONEY !== 0) {
       return consoleError(ERROR_MESSAGE.UNIT);
     }
-    this.countingLotto(this.lottoCount);
+    return this.countingLotto(this.lottoCount);
   }
 
   countingLotto(lottoCount) {
@@ -25,14 +33,4 @@ class PurchaseLotto {
   }
 }
 
-const lotto = new PurchaseLotto();
-
-// 비동기 초기화 메서드 호출
-lotto.initialize().then(async () => {
-  // 초기화가 완료된 후에 추가 작업을 수행
-  console.log(lotto.lottoCount); // 따로 값이 빠지는 지 확인 콘솔.
-  const message = await lotto.alertPurchaseLotto();
-  console.log(message); // 반환된 메세지 출력하기 위한 콘솔.
-});
-
-export default PurchaseLotto;
+export { PurchaseLotto };
