4abc2df7a87a71af6bbcce3364f6a1e6a00368b8
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-11-07 03:27:49 +0900

    :lipstick: feat : User 클래스의 입력받는 부분을 추가합니다.

--- src/User.js
+++ src/User.js
@@ -1,6 +1,7 @@
 import { numberCheck } from './validation.js';
 import { TRY_COST } from './constants/policy.js';
 import { MESSAGE } from './constants/messages.js';
+import {Console} from "@woowacourse/mission-utils";
 
 class User {
   constructor() {
@@ -25,6 +26,11 @@ class User {
     this.#validation(money);
     this.money = money;
     this.count = money / TRY_COST;
+    Console.print(MESSAGE.USER.buyLotto(this.count))
+  }
+
+  async readUserMoney(){
+    return Number(await Console.readLineAsync(MESSAGE.USER.setMoney))
   }
 }
 
