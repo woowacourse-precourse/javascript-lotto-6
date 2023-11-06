4318fe99ec7a29c3929ece4745f6b491fac702ce
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-06 16:10:59 +0900

    ✨ Feat: 값을 입력하지 않았을 경우 예외 처리 추가

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,6 +1,7 @@
 const message = Object.freeze({
   remainderNotZero: '1,000 단위로 입력하세요.',
-  invalidNumber: '숫자를 입력하세요.'
+  invalidNumber: '숫자를 입력하세요.',
+  missingValue: '값을 입력해주세요.',
 });
 
 const ERROR = Object.freeze({ message });
--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -9,6 +9,9 @@ const Validator = {
   invalidNumber: purchaseAmount => {
     if (isNaN(purchaseAmount)) throw new Error(ERROR.message.invalidNumber);
   },
+  missingValue: value => {
+    if (!value) throw new Error(ERROR.message.missingValue);
+  },
 };
 
 export default Validator;
