41c0bc791abf4084e348180430b269a5762dac33
hyzzzzy <shinhj4068@naver.com>
2023-11-07 02:36:00 +0900

    feat(calculate): 로또 번호 매칭 계산 함수, 보너스 번호 유무확인 함수 구현

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -5,3 +5,13 @@ export const generateLottoNumber = () => {
   const { min_lotto_number, max_lotto_number, size } = SETTING;
   return Random.pickUniqueNumbersInRange(min_lotto_number, max_lotto_number, size);
 }
+
+export const calculateNumberMatch = (lotto, prize) => {
+  return lotto
+  .filter((num) => prize.includes(num))
+  .length;
+}
+
+export const isBonusMatch = (lotto, bonus) => {
+  return lotto.includes(bonus);
+}
