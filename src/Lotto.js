d6d557a7c850cbc5a5e4d202d4a37070959deda0
Jiwon Lee <giwon.yi339@gmail.com>
2023-11-07 00:23:02 +0900

    refactor: 유효성 검사 Lotto.js로 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,67 +1,74 @@
-import { Console, Random } from "@woowacourse/mission-utils";
-import { INPUT_MSG } from "./constants/InputMessage.js";
-import {
-  validateInputResults,
-  validatePurchaseFormat,
-  validateResultsLength,
-} from "./validation/validation.js";
+import { ERROR_MSG } from "./constants/ErrorMessage.js";
 
 class Lotto {
-  // #numbers;
+  #numbers;
 
-  // constructor(numbers) {
-  //   this.#validate(numbers);
-  //   this.#numbers = numbers;
-  // }
-
-  async play() {
-    const playLotto = new PurchaseLottery();
-    await playLotto.purchaseTicket();
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
   }
 
-  // #validate(numbers) {
-  //   if (numbers.length !== 6) {
-  //     throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-  //   }
-  // }
-
-  // TODO: 추가 기능 구현
-}
-
-class PurchaseLottery {
-  async purchaseTicket() {
-    let ticketsAmount = await Console.readLineAsync(INPUT_MSG.amount);
-    validatePurchaseFormat(ticketsAmount);
-
-    ticketsAmount = Math.floor(ticketsAmount / 1000);
-
-    const lottoNumbers = new LotteryNumbers();
-    await lottoNumbers.generateNumbers(ticketsAmount);
-    await lottoNumbers.inputLotteryResults();
-  }
-}
-
-class LotteryNumbers {
-  async generateNumbers(ticketQuantity) {
-    for (let i = 0; i < ticketQuantity; i++) {
-      const lottoNums = await Random.pickUniqueNumbersInRange(1, 45, 6);
-      Console.print(lottoNums);
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error(ERROR_MSG.length);
     }
-  }
 
-  async inputLotteryResults() {
-    const results = await Console.readLineAsync(INPUT_MSG.results);
-    const resultsArray = results.split(",").map(Number);
-    validateResultsLength(resultsArray);
+    // TODO: 추가 기능 구현
+    numbers.map((number) => {
+      // results 숫자 검사
+      if (isNaN(number)) {
+        throw new Error(ERROR_MSG.input);
+      }
 
-    resultsArray.map((num) => {
-      validateInputResults(num, resultsArray);
+      // results 숫자 검사
+      if (number < 1 || number > 45) {
+        throw new Error(ERROR_MSG.input);
+      }
+      // resuls 중복 검사
+      if (numbers.filter((n) => n === number).length > 1) {
+        throw new Error(ERROR_MSG.overlap);
+      }
     });
-    Console.print(resultsArray);
   }
 }
 
-const playLotto = new Lotto();
-playLotto.play();
+// class PurchaseLottery {
+//   async purchaseTicket() {
+//     let ticketsAmount = await Console.readLineAsync(INPUT_MSG.amount);
+//     validatePurchaseFormat(ticketsAmount);
+//     ticketsAmount = Math.floor(ticketsAmount / 1000);
+//     Console.print(`${ticketsAmount}개를 구매했습니다.`);
+
+//     const lottoNumbers = new LotteryNumbers();
+//     lottoNumbers.generateNumbers(ticketsAmount);
+//     lottoNumbers.inputLotteryResults();
+//   }
+// }
+
+// class LotteryNumbers {
+//   async generateNumbers(ticketQuantity) {
+//     for (let i = 0; i < ticketQuantity; i++) {
+//       const lottoNums = await Random.pickUniqueNumbersInRange(1, 45, 6);
+//       if (lottoNums.length > 6) {
+//         throw new Error("[ERROR]");
+//       }
+//       Console.print(lottoNums);
+//     }
+//   }
+
+//   async inputLotteryResults() {
+//     const results = await Console.readLineAsync(INPUT_MSG.results);
+//     const resultsArray = results.split(",").map(Number);
+//     validateResultsLength(resultsArray);
+
+//     resultsArray.map((num) => {
+//       validateInputResults(num, resultsArray);
+//     });
+//     Console.print(resultsArray);
+//   }
+// }
+
+// const playLotto = new Lotto();
+// playLotto.play();
 
 export default Lotto;
