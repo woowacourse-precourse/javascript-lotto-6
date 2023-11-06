40642a2b978920b2dfb5047f222fe152492efebc
Han <mayo3610@naver.com>
2023-11-06 18:43:11 +0900

    Feat : 발행한 로또 수량 계산하기

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -1,30 +1,34 @@
 import InputView from "../view/InputView.js";
 import InputValidator from "../utils/validator.js";
+import Lotto from "../Lotto.js";
 class LottoGameController {
+  #lotto;
+  #purchaseAmount;
+
   constructor() {}
 
   startGame() {
     this.readPurchaseAmount();
   }
 
+  getNumberOfLottoTickets(input) {
+    this.#purchaseAmount = +input % 1000;
+  }
+
   async readPurchaseAmount() {
     await InputView.purchaseAmount((input) => {
       this.handleError(input);
-      this.printPurchaseAmount(input);
     });
   }
 
-  handleError(input) {
+  async handleError(input) {
     try {
       InputValidator.purchaseAmount(input);
+      this.getNumberOfLottoTickets(input);
     } catch (error) {
       console.log(error);
       this.readPurchaseAmount();
     }
-  }
-
-  printPurchaseAmount(amount) {
-    console.log(amount);
   }
 }
 
