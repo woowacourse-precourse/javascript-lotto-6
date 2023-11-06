dc1db3e7c7e0ac24541e41935c74096b53dcb4ed
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-11-06 22:48:30 +0900

    feat: 구입금액 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -19,19 +19,68 @@ class Lotto {
   }
 }
 
+//로또 번호 생성
 class LottoMachine {
-  #bonusNumber;
+  constructor() {
+    this.lottos = [];
+  }
 
   generateLotto() {
-    const number = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-    return new Lotto(number);
-  }
-  bonusGenerateLotto() {
-    this.bonusNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 1)[0];
+    const lotto = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    this.lottos.push(lotto);
+    MissionUtils.Console.print(lotto);
   }
 
-  get bonusNumber() {
-    return this.#bonusNumber;
+  buyLottos(cash) {
+    const cashCount = new CashCount(cash).count;
+    MissionUtils.Console.print(`${cashCount}개를 구매했습니다.`);
+    for (let count = 1; count <= cashCount; count++) {
+      this.generateLotto();
+    }
+
+    return this.lottos;
+  }
+}
+
+// 현금->횟수
+class CashCount {
+  #count;
+
+  constructor(cash) {
+    this.#validate(cash);
+    this.#count = cash / 1000;
+  }
+
+  #validate(cash) {
+    if (isNaN(cash)) {
+      throw new Error('[ERROR] 숫자가 아닙니다.');
+    }
+    if (cash < 1000) {
+      throw new Error('[ERROR] 금액이 부족합니다.');
+    }
+    if (cash % 1000 !== 0) {
+      throw new Error('[ERROR] 1000원 단위가 아닙니다.');
+    }
+  }
+
+  get count() {
+    return this.#count;
+  }
+}
+
+class LottoGame {
+  constructor() {
+    this.playerLottos = [];
+  }
+
+  async buyLottos() {
+    const cash = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요');
+    const lottoMachine = new LottoMachine();
+    this.playerLottos = await lottoMachine.buyLottos(Number(cash));
+  }
+
+  start() {
+    this.buyLottos();
   }
 }
 
