31e2a34e96fe57d2b0b8182281a8609d7be60eec
kwani6684 <jack0910@naver.com>
2023-11-06 15:52:25 +0900

    ✨feat: add calculating totalEarned

--- src/Check.js
+++ src/Check.js
@@ -2,6 +2,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class Check {
   countArray = [0, 0, 0, 0, 0];
+  totalEarned = 0;
   checkCount(checkLotto) {
     switch (checkLotto.win) {
       case 3:
@@ -12,6 +13,7 @@ class Check {
         break;
       case 5:
         this.countArray[2] += 1;
+        break;
       case 6:
         this.countArray[4] += 1;
         break;
@@ -44,6 +46,21 @@ class Check {
   sixCorrect(count) {
     const SIX_CORRECT = `6개 일치 (2,000,000,000원) - ${count}개`;
     return SIX_CORRECT;
+  }
+  earnRate(countArray) {
+    const PRICE_MONEY = [5000, 50000, 1500000, 30000000, 2000000000];
+    countArray.forEach((count, index) => {
+      this.totalEarned += count * PRICE_MONEY[index];
+    });
+  }
+  printCount(countArray) {
+    const WINNING_MSG = '\n당첨 통계\n---';
+    MissionUtils.Console.print(WINNING_MSG);
+    MissionUtils.Console.print(this.threeCorrect(countArray.shift()));
+    MissionUtils.Console.print(this.fourCorrect(countArray.shift()));
+    MissionUtils.Console.print(this.fiveCorrect(countArray.shift()));
+    MissionUtils.Console.print(this.fiveBonusCorrect(countArray.shift()));
+    MissionUtils.Console.print(this.sixCorrect(countArray.shift()));
   }
 }
 export default Check;
