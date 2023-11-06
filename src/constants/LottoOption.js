efc34aab3365bf63bbfa84ce28f30eadcb6eb714
swarvy <swarvy0826@naver.com>
2023-11-06 18:57:03 +0900

    feat : 로또 매직넘버와 상금을 상수로 관리

---
+++ src/constants/LottoOption.js
@@ -0,0 +1,13 @@
+export const LOTTO_MAGICNUMBER = Object.freeze({
+  minValue: 1,
+  maxValue: 45,
+  selectAmount: 6,
+});
+
+export const LOTTO_PRIZEVALUE = Object.freeze({
+  first: 2_000_000_000,
+  second: 30_000_000,
+  third: 1_500_000,
+  fourth: 50_000,
+  fifth: 5_000,
+});
