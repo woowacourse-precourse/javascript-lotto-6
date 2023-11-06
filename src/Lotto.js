693cddfa0efea23e49b2c27940a9718c69b53ba4
soi-ha <soy2302ten@gmail.com>
2023-11-06 20:22:13 +0900

    chore: 들여쓰기 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,38 +1,38 @@
 class Lotto {
-	#numbers;
+  #numbers;
 
-	constructor(numbers) {
-		this.#validate(numbers);
-		this.#numbers = numbers;
-	}
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
 
-	#validate(numbers) {
-		this.#validateNumberQuantity(numbers);
-		this.#validateNumberRange(numbers);
-		this.#validateNumberRange(numbers);
-		this.#validateNumberDuplicate(numbers);
-	}
+  #validate(numbers) {
+    this.#validateNumberQuantity(numbers);
+    this.#validateNumberRange(numbers);
+    this.#validateNumberRange(numbers);
+    this.#validateNumberDuplicate(numbers);
+  }
 
-	#validateNumberQuantity(numbers) {
-		if (numbers.length !== 6) {
-			throw new Error('[ERROR] 로또 번호는 6개의 숫자를 입력해야 합니다.');
-		}
-	}
+  #validateNumberQuantity(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개의 숫자를 입력해야 합니다.');
+    }
+  }
 
-	#validateNumberRange(numbers) {
-		const REGEX = /^(?:[1-9]|[1-3][0-9]|4[0-5])$/;
-		const filterNumber = numbers.filter((num) => REGEX.test(num));
-		if (filterNumber.length !== 6) {
-			throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
-		}
-	}
+  #validateNumberRange(numbers) {
+    const REGEX = /^(?:[1-9]|[1-3][0-9]|4[0-5])$/;
+    const filterNumber = numbers.filter((num) => REGEX.test(num));
+    if (filterNumber.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+  }
 
-	#validateNumberDuplicate(numbers) {
-		const duplicateNumber = new Set(numbers);
-		if (duplicateNumber.size !== 6) {
-			throw new Error('[ERROR] 로또 번호는 중복된 숫자를 입력할 수 없습니다.');
-		}
-	}
+  #validateNumberDuplicate(numbers) {
+    const duplicateNumber = new Set(numbers);
+    if (duplicateNumber.size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복된 숫자를 입력할 수 없습니다.');
+    }
+  }
 }
 
 export default Lotto;
--- src/LottoIssuance.js
+++ src/LottoIssuance.js
@@ -1,10 +1,10 @@
 import { Random } from '@woowacourse/mission-utils';
 
 export default function LottoIssuance(lotto) {
-	const lottoIssuanceList = [];
+  const lottoIssuanceList = [];
 
-	for (let i = 0; i < lotto; i++) {
-		lottoIssuanceList.push(Random.pickUniqueNumbersInRange(1, 45, 6));
-	}
-	return lottoIssuanceList;
+  for (let i = 0; i < lotto; i++) {
+    lottoIssuanceList.push(Random.pickUniqueNumbersInRange(1, 45, 6));
+  }
+  return lottoIssuanceList;
 }
