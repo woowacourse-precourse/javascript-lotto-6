67343a880f61186d57ec9d6e1c09051c80a71125
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 05:08:02 +0900

    :sparkles: feat: 보너스번호 예외처리 메세지 상수화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,6 +4,8 @@ const ERROR_MESSAGE = Object.freeze({
   invalidRange: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
   invalidUnique: '[ERROR] 로또 번호는 중복되지 않아야 합니다.',
   invalidAmount: '[ERROR] 구입 금액은 1,000원으로 나누어 떨어져야 합니다.',
+  invalidBonusNumber: '[ERROR] 보너스 번호는 하나의 숫자만 가능합니다.',
+  invalidUniqueBonusNumber: '[ERROR] 보너스 번호는 로또 번호와 중복되지 않아야 합니다.',
 });
 
 export { ERROR_MESSAGE };
