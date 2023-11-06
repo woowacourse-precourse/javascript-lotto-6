507eb1b67c97073d86fdaf49ab9980a315bd4803
leeseohoo <sm01004@naver.com>
2023-11-06 17:33:24 +0900

    로또 번호 조건 처리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,65 @@
 import Lotto from './Lotto.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    let purchaseAmount;
+
+    // 구입 금액 입력
+    while (true) {
+      purchaseAmount = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+      if (purchaseAmount % 1000 !== 0) {
+        MissionUtils.Console.print('[ERROR] 1,000원 단위로 입력하세요.\n');
+      } else {
+        break;
+      }
+    }
+
+    const numberOfTickets = purchaseAmount / 1000;
+    MissionUtils.Console.print(`\n${numberOfTickets}개를 구매했습니다.`);
+
+    // 로또 티켓 생성
+    const lottoTickets = this.generateLottoTickets(numberOfTickets);
+
+    // 당첨 번호 입력
+    const winningNumbers = await MissionUtils.Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+    const winningNumbersArray = winningNumbers.split(',').map(number => parseInt(number, 10));
+    if (winningNumbersArray.some(number => isNaN(number) || number < 1 || number > 45)) {
+      MissionUtils.Console.print('[ERROR] 유효하지 않은 당첨 번호가 있습니다.\n');
+      return;
+    }
+    
+    // 보너스 번호 입력
+    const bonusNumber = await MissionUtils.Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
+    const parsedBonusNumber = parseInt(bonusNumber, 10);
+    if (isNaN(parsedBonusNumber) || parsedBonusNumber < 1 || parsedBonusNumber > 45) {
+      MissionUtils.Console.print('[ERROR] 유효하지 않은 보너스 번호입니다.\n');
+      return;
+    }
+
+    // 당첨 여부 확인
+    const winnings = this.checkLottoTickets(lottoTickets, winningNumbersArray, parsedBonusNumber);
+
+    // 결과 출력
+    this.displayWinnings(winnings, numberOfTickets);
+  }
+
+  generateLottoTickets(numberOfTickets) {
+    // 로또 티켓 생성
+
+  }  
+
+  checkLottoTickets(lottoTickets, winningNumbers, bonusNumber) {
+    // 로또 번호 확인
+  }  
+  
+  displayWinnings(winnings, numberOfTickets) {
+    // 당첨 통계 출력
+    MissionUtils.Console.print('\n당첨 통계');
+    MissionUtils.Console.print('---');
+    
+    // 수익률 계산
+  }
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -12,9 +12,22 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    
+    for (const number of numbers) {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    }
+
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }  
+
 }
 
 export default Lotto;
