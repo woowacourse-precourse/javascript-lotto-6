ceae8091084bebbd360141db7b52bf798b82c51d
lurgi <wahoo9040@naver.com>
2023-11-06 20:02:31 +0900

    feat(Input): 당첨 번호 입력

--- src/Input.js
+++ src/Input.js
@@ -15,6 +15,26 @@ class Input {
     if (INPUT_NUMBER % 1000 !== 0)
       throw new Error("[ERROR] 1,000원 단위로 입력해주세요.");
   }
+
+  static async readLottoNumbers() {
+    const INPUT = await MissionUtils.Console.readLineAsync();
+    const NUMBERS = INPUT.split(",").map(Number);
+    this.validLottoNumbers(NUMBERS);
+    return INPUT;
+  }
+
+  static validLottoNumbers(numbers) {
+    if (numbers.length !== 6)
+      throw new Error("[ERROR] 숫자는 6개를 입력해주세요.");
+    if (new Set(numbers).size !== numbers.length)
+      throw new Error("[ERROR] 중복된 숫자를 입력하셨습니다.");
+    numbers.forEach((number) => {
+      if (Number.isNaN(number)) throw new Error("[ERROR] 숫자만 입력해주세요.");
+      if (!number) throw new Error("[ERROR] 공백을 입력하셨습니다.");
+      if (number < 1 || number > 45)
+        throw new Error("[ERROR] 숫자의 범위는 1~45 이어야 합니다.");
+    });
+  }
 }
 
 export default Input;
