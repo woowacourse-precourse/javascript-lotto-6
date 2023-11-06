81c813756d985cfe3c982aace355e0d78c7d6c97
hyewon <hyewon.parkhw@gmail.com>
2023-11-06 20:16:51 +0900

    feat: 로또 번호 중복 예외 처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,3 @@
-import { Console } from "@woowacourse/mission-utils";
-
 class Lotto {
   #numbers;
   #bonusNumber;
@@ -12,6 +10,9 @@ class Lotto {
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    if (numbers.length != new Set(numbers).size){
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.")
     }
   }
 
