dad798af59845ef092ad452c46b1cf58b498a319
고세종 <fulfilled7418@gmail.com>
2023-11-07 00:06:14 +0900

    fix: 수정 일치하는 번호가 0개가 아닐경우 스코어보드 추가

--- Controller/modules/LottoResult.js
+++ Controller/modules/LottoResult.js
@@ -1,17 +1,16 @@
-import ResultUtils from '../../utils/ResultUtils';
+import ResultUtils from '../../utils/ResultUtils.js';
 
 const { compareNumberArr, addCountOnResultBoard, addProfit } = ResultUtils;
 
 const LottoResult = {
-  calculateResultBoard(lottoNumbers, winNumber, bonusNumber) {
+  calculateResultBoard(purchasedNumbers, lottoNumber, bonusNumber) {
     const resultBoard = { three: 0, four: 0, five: 0, fiveBonus: 0, six: 0 };
     let matchCount = 0;
-    let matchNumber = '';
 
-    lottoNumbers.forEach((numbers) => {
-      matchCount = compareNumberArr(numbers, winNumber);
-      matchNumber = addCountOnResultBoard(matchCount, numbers, bonusNumber);
-      resultBoard[matchNumber] += 1;
+    purchasedNumbers.forEach((numbers) => {
+      matchCount = compareNumberArr(numbers, lottoNumber);
+      let matchNumber = addCountOnResultBoard(matchCount, numbers, bonusNumber);
+      if (matchNumber !== 'zero') resultBoard[matchNumber] += 1;
     });
 
     return resultBoard;
