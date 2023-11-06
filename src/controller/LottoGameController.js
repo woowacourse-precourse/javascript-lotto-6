3d622302bf5c03bdc432648251d37fffcf67c5dc
parkgeonwoong <hk77kh@gmail.com>
2023-11-06 14:58:39 +0900

    feat : [구입기능] 로또 구입 금액과 로또 갯수 구현

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -1,9 +1,31 @@
+import { LOTTO_PRICE } from '../constants/GameSetting.js';
 import { inputBuyAmount } from '../view/View.js';
 
 export default class LottoGameController {
   #buyLottoAmount;
+  #buyLottoCnt;
 
   async play() {
-    inputBuyAmount();
+    await this.buyAmount();
+    this.buyLotto();
+
+    console.log('구입금액: ', this.getBuyLottoAmount());
+    console.log('로또 구매 갯수: ', this.getBuyLottoCnt());
+  }
+
+  async buyAmount() {
+    this.#buyLottoAmount = await inputBuyAmount();
+  }
+
+  buyLotto() {
+    this.#buyLottoCnt = this.#buyLottoAmount / LOTTO_PRICE;
+  }
+
+  getBuyLottoAmount() {
+    return this.#buyLottoAmount;
+  }
+
+  getBuyLottoCnt() {
+    return this.#buyLottoCnt;
   }
 }
--- src/view/View.js
+++ src/view/View.js
@@ -5,9 +5,8 @@ import { isValidBuyAmount } from '../utils/Validation.js';
 export async function inputBuyAmount() {
   try {
     const inputValue = await Console.readLineAsync(MESSAGE_NOTIFICATION.buyAmount);
-    if (isValidBuyAmount(inputValue)) {
-      return inputValue;
-    }
+    isValidBuyAmount(inputValue);
+    return inputValue;
   } catch (err) {
     Console.print(err);
     return inputBuyAmount();
