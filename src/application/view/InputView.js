d8faac609549722d8e93726a40b1224fb55a1763
dongbin420 <dongbin420@gmail.com>
2023-11-07 04:14:17 +0900

    feat: print error message and take input until user input correct

--- src/application/view/InputView.js
+++ src/application/view/InputView.js
@@ -8,28 +8,51 @@ class InputView {
   }
 
   async getUserInputPurchaseMoney() {
-    const USER_INPUT_PURCHASE_MONEY = await Console.readLineAsync(`${INPUT_MESSAGE.purchaseMoney}`);
-    this.validator.validateUserInputPurchaseMoney(Number(USER_INPUT_PURCHASE_MONEY));
+    while (true) {
+      try {
+        const USER_INPUT_PURCHASE_MONEY = await Console.readLineAsync(
+          `${INPUT_MESSAGE.purchaseMoney}`
+        );
+        this.validator.validateUserInputPurchaseMoney(Number(USER_INPUT_PURCHASE_MONEY));
 
-    return Number(USER_INPUT_PURCHASE_MONEY);
+        return Number(USER_INPUT_PURCHASE_MONEY);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 
   async getUserInputWinningNumbers() {
-    const USER_INPUT_WINNING_NUMBERS = await Console.readLineAsync(
-      `${INPUT_MESSAGE.winningNumbers}`
-    );
-    this.validator.validateUserInputWinningNumbers(
-      USER_INPUT_WINNING_NUMBERS.split(OTHERS.comma).map(Number)
-    );
+    while (true) {
+      try {
+        const USER_INPUT_WINNING_NUMBERS = await Console.readLineAsync(
+          `${INPUT_MESSAGE.winningNumbers}`
+        );
+        this.validator.validateUserInputWinningNumbers(
+          USER_INPUT_WINNING_NUMBERS.split(OTHERS.comma).map(Number)
+        );
 
-    return USER_INPUT_WINNING_NUMBERS.split(OTHERS.comma).map(Number);
+        return USER_INPUT_WINNING_NUMBERS.split(OTHERS.comma).map(Number);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 
   async getUserInputBonusNumber(winningNumbers) {
-    const USER_INPUT_BONUS_NUMBER = await Console.readLineAsync(`${INPUT_MESSAGE.bonusNumber}`);
-    this.validator.validateUserInputBonusNumber(Number(USER_INPUT_BONUS_NUMBER), winningNumbers);
+    while (true) {
+      try {
+        const USER_INPUT_BONUS_NUMBER = await Console.readLineAsync(`${INPUT_MESSAGE.bonusNumber}`);
+        this.validator.validateUserInputBonusNumber(
+          Number(USER_INPUT_BONUS_NUMBER),
+          winningNumbers
+        );
 
-    return Number(USER_INPUT_BONUS_NUMBER);
+        return Number(USER_INPUT_BONUS_NUMBER);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 }
 
