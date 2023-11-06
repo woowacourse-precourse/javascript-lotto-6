4018ce556a7928061c5c091d09f6a39f3e1b0ff1
skdbsqls <skdbsqls98@naver.com>
2023-11-06 16:27:32 +0900

    feat(condition, message): 로또 구입과 관련된 조건 및 메시지 상수화

---
+++ src/constants/condition.js
@@ -0,0 +1,7 @@
+const CONDITION = Object.freeze({
+  unit: {
+    price: 1000,
+  },
+});
+
+export default CONDITION;
---
+++ src/constants/message.js
@@ -0,0 +1,13 @@
+const MESSAGE = Object.freeze({
+  input: {
+    price: "구입금액을 입력해 주세요.\n",
+  },
+  output: {
+    amountOfLotto: "개를 구매했습니다.",
+  },
+  error: {
+    notNumber: "[ERROR] 구입금액은 숫자로 입력해 주세요.",
+    priceUnit: "[ERROR] 구입금액은 1000원 단위로 입력해 주세요.",
+  },
+});
+export default MESSAGE;
