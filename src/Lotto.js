3f2f03a5f69f0a1210e80de24c295525afd686f5
1eecan <busyx2modernsociety@gmail.com>
2023-11-07 04:30:12 +0900

    feat: Lotto 클래스 메소드 수정
    * 로또 생성시 겹치는 번호가 있을 경우 에러를 던지는 로직 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,9 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    if ([...new Set(numbers)].length !== 6) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있을 수 없습니다.');
+    }
   }
 
   // TODO: 추가 기능 구현
