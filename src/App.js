9a91cc2234ffd0d53fb0fe2d530aa8fc65349106
dxwwan <kkw1700@gmail.com>
2023-11-06 17:13:08 +0900

    feat: 에러 발생시 다시 로또를 실행시키는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Purchase from "./Credit.js";
 import Profit from "./Profit.js";
 import Lotto from "./Lotto.js";
+import { Console } from "@woowacourse/mission-utils";
+import { ERROR } from "./Message.js";
 class App {
   constructor() {
     this.Purchase = new Purchase();
@@ -8,19 +10,23 @@ class App {
     this.Profit = new Profit();
   }
   async play() {
-    const credit = await this.Purchase.getCredit();
-    const amountOfLotto = this.Purchase.getAmountOfLotto(credit);
-    this.Purchase.printAmountOfLotto(amountOfLotto);
-    const lottoNumbers = this.Lotto.getLottoNumbers(amountOfLotto);
-    this.Lotto.printLottoNumbers(lottoNumbers);
-    const drawNumbers = await this.Lotto.enterDrawNumbers();
-    const bonusNumber = await this.Lotto.enterBonusNumber();
-    const rankOfResult = this.Lotto.resultOfDraw(lottoNumbers, drawNumbers, bonusNumber);
-    const resultOfLotto = this.Lotto.lottoDrawResult(rankOfResult);
-    this.Lotto.lottoDrawPrinter(resultOfLotto);
-    const profitRate = this.Profit.profitRateAcc(resultOfLotto, credit);
-    this.Profit.profitRatePrinter(profitRate);
+    try {
+      const credit = await this.Purchase.getCredit();
+      const amountOfLotto = await this.Purchase.getAmountOfLotto(credit);
+      this.Purchase.printAmountOfLotto(amountOfLotto);
+      const lottoNumbers = this.Lotto.getLottoNumbers(amountOfLotto);
+      this.Lotto.printLottoNumbers(lottoNumbers);
+      const drawNumbers = await this.Lotto.enterDrawNumbers();
+      const bonusNumber = await this.Lotto.enterBonusNumber();
+      const rankOfResult = this.Lotto.resultOfDraw(lottoNumbers, drawNumbers, bonusNumber);
+      const resultOfLotto = this.Lotto.lottoDrawResult(rankOfResult);
+      this.Lotto.lottoDrawPrinter(resultOfLotto);
+      const profitRate = this.Profit.profitRateAcc(resultOfLotto, credit);
+      this.Profit.profitRatePrinter(profitRate);
+    } catch (error) {
+      Console.printError(error.message);
+      this.play();
+    }
   }
 }
-
 export default App;
