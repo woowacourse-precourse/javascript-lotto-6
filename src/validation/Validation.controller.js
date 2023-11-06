4d77fb981e729d61c44dcb801bff99d7555038a7
wonkyum-kim <dodo12120@naver.com>
2023-11-06 18:25:54 +0900

    feat: 구입한 티켓의 번호가 1부터 45 사이의 숫자인지 확인

--- src/validation/Validation.controller.js
+++ src/validation/Validation.controller.js
@@ -2,8 +2,9 @@ import ValidationService from './Validation.service';
 
 const ERROR_MESSAGES = {
   isNaN: '[ERROR] 입력한 금액이 숫자가 아닙니다.',
-  notDivided: '[ERROR] 1000 단위의 금액을 입력하세요.';
-}
+  notDivided: '[ERROR] 1000 단위의 금액을 입력하세요.',
+  overRange: '[ERROR] 숫자는 1부터 45사이여야 합니다.',
+};
 
 export default class ValidationController {
   #validationService;
@@ -20,5 +21,13 @@ export default class ValidationController {
     if (!this.#validationService.isDividedBy1000(num)) {
       throw new Error(ERROR_MESSAGES.notDivided);
     }
+  }
+
+  validatePurchasedTickets(tickets) {
+    tickets.forEach((ticket) => {
+      if (!this.#validationService.isInRange(ticket)) {
+        throw new Error(ERROR_MESSAGES.overRange);
+      }
+    });
   }
 }
--- src/validation/Validation.service.js
+++ src/validation/Validation.service.js
@@ -1,3 +1,6 @@
+const MIN = 1;
+const MAX = 45;
+
 export default class ValidationService {
   isNumber(num) {
     return !Number.isNaN(+num);
@@ -5,5 +8,14 @@ export default class ValidationService {
 
   isDividedBy1000(num) {
     return num > 0 && +num % 1000 === 0;
+  }
+
+  isInRange(ticket) {
+    return (
+      ticket.length ===
+      ticket.filter((num) => {
+        return num >= MIN && num <= MAX;
+      })
+    );
   }
 }
