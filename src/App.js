18cfac2530303e4955be02a5595c471579f0d85c
KmYongHwan <kyh5678@gmail.com>
2023-11-07 05:07:02 +0900

    get winning numbers and bonus number & exception case

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ class App {
   async play() {
     const LOTTO_NUM = await this.getLottoMoney();
     const LOTTO_ARRAY = await this.getLottoObject(LOTTO_NUM);
-
-
+    const WIN_NUM = await this.getWinningNum();
+    const BONUS_NUM = await this.getBonusNum(WIN_NUM);
   }
 
   async getLottoMoney(){
@@ -30,6 +30,35 @@ class App {
     });
 
     return ARR;
+  }
+
+  async getWinningNum(){
+    const WINNING_NUM = await MissionUtils.Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
+    const regex = /^\d+(,\d+){5}$/;
+    if(!regex.test(WINNING_NUM)) throw new Error("[ERROR] 당첨 번호는 숫자,숫자,숫자,숫자,숫자,숫자 의 형태로 입력해야 합니다.");
+
+    const WINNING_NUM_ARR = WINNING_NUM.split(',').map(Number);
+    WINNING_NUM_ARR.forEach(num => {
+      if(num < 1 || num > 45) throw new Error("[ERROR] 당첨 번호는 1 ~ 45 사이로 입력해야 합니다.");
+    });
+
+    const UNIQUEARR = [...new Set(WINNING_NUM_ARR)];
+    if(UNIQUEARR.length !== WINNING_NUM_ARR.length) throw new Error("[ERROR] 당첨 번호는 중복되지 않은 숫자로 입력해야 합니다.");
+
+    MissionUtils.Console.print(WINNING_NUM_ARR);
+
+    return WINNING_NUM_ARR;
+  }
+
+  async getBonusNum(WIN_NUM){
+    const BONUS = await MissionUtils.Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
+    const BONUS_NUMBER = parseInt(BONUS, 10);
+
+    if(isNaN(BONUS_NUMBER)) throw new Error("[ERROR] 숫자를 입력해야 합니다.");
+    if(BONUS_NUMBER < 1 || BONUS_NUMBER > 45) throw new Error("[ERROR] 보너스 번호는 1 ~ 45 사이의 숫자입니다.");
+    if(!WIN_NUM.includes(BONUS_NUMBER))throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복되어서는 안됩니다.");
+    
+    return BONUS_NUMBER;
   }
 }
 
