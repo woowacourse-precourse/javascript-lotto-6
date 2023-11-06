3d436cca0f7075f057ff80e0faf37348e6565f63
thewronghand <penfreak77@gmail.com>
2023-11-06 18:14:50 +0900

    refactor: MessageFormat에서 상금과 수익률 포맷 메서드 분리

--- src/utils/MessageFormat.js
+++ src/utils/MessageFormat.js
@@ -1,16 +1,26 @@
+import NUMBER from './constants/number.js';
 import SYMBOL from './constants/symbol.js';
+
+const { statistics } = NUMBER;
+const { winningCriteria } = statistics;
 
 const MessageFormat = {
   lottoPurchaseHeader: count => `${count}개를 구매했습니다.`,
   lottoTicket: numbers => `[${numbers.join(SYMBOL.lottoNumberSeparator)}]`,
-  formatNumber: number =>
-    new Intl.NumberFormat('ko-KR', { maximumFractionDigits: 1 }).format(number),
-  lottoResultMessage: (matchCount, bonusMatch, prize, ticketCount) =>
-    `${matchCount}개 일치${
-      bonusMatch && ', 보너스 볼 일치'
-    } (${MessageFormat.formatNumber(prize)}원) - ${ticketCount}개`,
+  formatPrize: prize => new Intl.NumberFormat('ko-KR').format(prize),
+  formatProfitRate: profitRate =>
+    new Intl.NumberFormat('ko-KR', {
+      minimumFractionDigits: 1,
+      maximumFractionDigits: 1,
+    }).format(profitRate),
+  lottoResultMessage: (category, prize, ticketCount) => {
+    const { matchCount, bonusMatch } = winningCriteria[category];
+    return `${matchCount}개 일치${
+      bonusMatch ? ', 보너스 볼 일치' : ''
+    } (${MessageFormat.formatPrize(prize)}원) - ${ticketCount}개`;
+  },
   profitRateMessage: profitRate =>
-    `총 수익률은 ${MessageFormat.formatNumber(profitRate)}%입니다.`,
+    `총 수익률은 ${MessageFormat.formatProfitRate(profitRate)}%입니다.`,
 };
 
 export default MessageFormat;
