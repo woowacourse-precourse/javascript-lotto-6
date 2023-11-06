bcc6c08bf209907af28ff39982fe83e967840fd1
abi-hong <flffjakfwm20@gmail.com>
2023-11-06 23:09:41 +0900

    feat(Input): 로또 게임 입력 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,16 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 
-const Input = {
+export const InputView = {
   async price() {
-    const price = await Console.readLineAsync('구입할 금액을 알려주세요.');
+    const price = await Console.readLineAsync('구입할 금액을 알려주세요.\n');
     return Number(price);
   },
   async win() {
-    const numbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.');
-    return numbers.map(Number(number)).split(',');
+    const numbers = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+    return numbers.split(',').map(Number);
   },
   async bonus() {
-    const number = await Console.readLineAsync('보너스 번호를 입력해 주세요.');
+    const number = await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
     return Number(number);
   },
 }
