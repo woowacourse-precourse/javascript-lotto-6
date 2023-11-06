ba3f961665b1d1149188809be3560d5437e28254
woo7i <or2447221@gmail.com>
2023-11-06 20:17:35 +0900

    refector(Lotto) : 사용자로 부터 입력 받은 값을 위한 클래스로 사용하기 위해 전면 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,29 +7,6 @@ class Lotto {
 
   constructor(numbers) {
     this.#numbers = numbers;
-    this.purchaseLotto = new PurchaseLotto();
-  }
-
-  async lottoCount() {
-    await this.purchaseLotto.initialize(); // 비동기 초기화를 기다린 후에
-    return this.purchaseLotto.getLottoCount(); // 로또 개수를 반환합니다.
-  }
-
-  async createLotto() {
-    const count = await this.lottoCount(); // 비동기로 로또 개수를 가져온다
-    const numbers = new Set();
-
-    for (let i = 0; i < count; i++) {
-      const number = randomNum();
-      numbers.add(number.sort((numA, numB) => numA - numB));
-    }
-    return [...numbers];
-  }
-
-  async viewLottoList() {
-    const theLottoList = await this.createLotto();
-
-    return theLottoList.map((item) => `[${item.join(', ')}]`).join(`\n`);
   }
 }
 
