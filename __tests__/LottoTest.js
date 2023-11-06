5289360c800ad3db0230697fe2f57c753589508f
6puritans9 <6puritans9@gmail.com>
2023-11-06 21:20:04 +0900

    test(LottoTest, README): Verifications for creating new lotto instance

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,5 +1,8 @@
 import Lotto from "../src/Lotto.js";
 import Printer from "../src/Game/Printer.js";
+import IO from "../src/Util/IOHandler.js";
+
+const amountResults = [10, 35, 4];
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -16,15 +19,35 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+});
+
+describe("프린터 테스트", () => {
   test.each([10000, 35000, 4000])(
     "구입 금액을 가격으로 나눈 몫만큼 구매 개수가 출력된다.",
-    () => {
-      const printer = new Printer();
+    async (input) => {
+      //given
+      const result = amountResults.shift();
 
-      const results = [10, 35, 4];
-      const result = results.shift();
+      //when
+      const printer = new Printer(input);
 
-      expect(printer.amount).toBe(result);
+      //then
+      await expect(printer.amount).toBe(result);
     }
   );
+
+  test("생성된 무작위 값과 동일한 번호 목록이 출력되어야 한다", () => {
+    //given
+    const logSpy = jest.spyOn(IO, "print");
+
+    //when
+    const printer = new Printer(1000);
+    printer.pack();
+    printer.print();
+
+    //then
+    expect(logSpy).toHaveBeenCalledWith(
+      `[${printer.getTicket()[0].show().join(", ")}]`
+    );
+  });
 });
