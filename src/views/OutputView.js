96c86823a4ab0122710026a82c11ab65f0a4fe45
at8in <at8in@naver.com>
2023-11-07 04:05:37 +0900

    refactor: return 제거

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -3,11 +3,11 @@ import { OUTPUT_MESSAGE } from '../constants/messages.js';
 
 const OutputView = {
   printPublishCount(count) {
-    return Console.print(OUTPUT_MESSAGE.publishCount(count));
+    Console.print(OUTPUT_MESSAGE.publishCount(count));
   },
 
   printLine() {
-    return Console.print('');
+    Console.print('');
   },
 
   printLottos(lottos) {
