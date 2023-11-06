fb94c0095d1208ce00bc367e93d2b9f6bdba4dad
OhHyukJun <kmh7277@naver.com>
2023-11-06 17:45:53 +0900

    Feat: 구매 금액 입력 기능

---
+++ src/InputPrice.js
@@ -0,0 +1,9 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export async function inputPrice() {
+    const userPrice = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
+    if (userPrice % 1000 !== 0) {
+        throw new Error("[ERROR] 입력 값이 1,000으로 나누어 떨어지지 않습니다.");
+    }
+    return userPrice/1000;
+}
