a7d34ff3d15488c7c62e01fb22ad31860923b051
OhHyukJun <kmh7277@naver.com>
2023-11-06 23:42:58 +0900

    Rename: 파일 삭제

--- src/Computer/LottoNum.js
+++
@@ -1,6 +0,0 @@
-import { getRandomNum } from "./Computer";
-import { inputPrice } from "../InputPrice";
-
-export function createNumbers(inputPrice) {
-    return Array.from({ length: inputPrice }, () => getRandomNum());
-}
