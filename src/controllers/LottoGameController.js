7e56305333bcdc87b883ddc6add8a83dd79d4046
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 07:59:07 +0900

    :sparkles: feat: 구입금액에 대한 컨트롤 분리 및 await 추가

--- src/controllers/LottoGameController.js
+++ src/controllers/LottoGameController.js
@@ -1,66 +1,68 @@
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
-import PurchaseAmount from '../domain/PurchaseAmount.js';
 import LottoGame from '../domain/LottoGame.js';
 import Lotto from '../Lotto.js';
 import LottoValidator from '../domain/LottoValidator.js';
 
 class LottoGameController {
   constructor() {
-    this.lottoGame = [];
+    this.lottoGame = null;
   }
 
   async start() {
     try {
-      const purchaseAmount = await InputView.getPurchaseAmount();
-      const amount = new PurchaseAmount(purchaseAmount).getAmount();
-
-      OutputView.printPurchaseAmount(amount);
-
+      const amount = await this.getValidPurchaseAmount();
       this.lottoGame = new LottoGame(amount);
+
       const purchasedLotto = this.lottoGame.getPurchasedLotto();
-
+      OutputView.printPurchaseAmount(amount);
       OutputView.printPurchasedLotto(purchasedLotto);
-      this.userWinningNumbers();
-    } catch ({ message }) {
-      OutputView.printErrorMessage(message);
-      this.start();
-    }
-  }
 
-  async userWinningNumbers() {
-    try {
-      const winningNumbers = await InputView.getWinningNumbers();
-      const numbers = winningNumbers.split(',').map(Number);
-      const winningLotto = new Lotto(numbers);
-      this.userBonusNumber(winningLotto);
-    } catch ({ message }) {
-      OutputView.printErrorMessage(message);
-      this.userWinningNumbers();
-    }
-  }
-
-  async userBonusNumber(winningLotto) {
-    try {
-      const bonusNumber = await InputView.getBonusNumber();
-      const isContainning = winningLotto.hasContainBonusNumber(Number(bonusNumber));
-
-      LottoValidator.validBonusNumber(bonusNumber, isContainning);
+      const winningLotto = await this.getValidWinningLotto();
+      const bonusNumber = await this.getValidBonusNumber(winningLotto);
 
       const comparisonResults = this.lottoGame.getLottoComparisonResults(
         winningLotto.getSortedLotto(),
-        Number(bonusNumber)
+        bonusNumber
       );
 
       const winsStatistics = this.lottoGame.getStatistics(comparisonResults);
-
       OutputView.printWinsStatistics(winsStatistics);
 
       const totalPrizeAmount = this.lottoGame.calcTotalPrizeAmount(winsStatistics);
       OutputView.printProfitRatio(this.lottoGame.getProfitRatio(totalPrizeAmount));
     } catch ({ message }) {
       OutputView.printErrorMessage(message);
-      this.userBonusNumber(winningLotto);
+      await this.start();
+    }
+  }
+
+  async getValidPurchaseAmount() {
+    const inputPurchaseAmount = await InputView.getPurchaseAmount();
+    LottoValidator.validPurchaseAmount(inputPurchaseAmount);
+    return inputPurchaseAmount / 1000;
+  }
+
+  async getValidWinningLotto() {
+    try {
+      const winningNumbers = await InputView.getWinningNumbers();
+      const numbers = winningNumbers.split(',').map(Number);
+      return new Lotto(numbers);
+    } catch ({ message }) {
+      OutputView.printErrorMessage(message);
+      return this.getValidWinningLotto();
+    }
+  }
+
+  async getValidBonusNumber(winningLotto) {
+    try {
+      const bonusNumber = await InputView.getBonusNumber();
+      const isContaining = winningLotto.hasContainBonusNumber(Number(bonusNumber));
+      LottoValidator.validBonusNumber(bonusNumber, isContaining);
+      return Number(bonusNumber);
+    } catch ({ message }) {
+      OutputView.printErrorMessage(message);
+      return this.getValidBonusNumber(winningLotto);
     }
   }
 }
