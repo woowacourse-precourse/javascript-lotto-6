fefd36faacc7c288ac5c558302fc11dd14433af8
JunSeok <ppu1234@naver.com>
2023-11-06 15:22:17 +0900

    refactor: 상수값 처리

--- src/domains/Counter.js
+++ src/domains/Counter.js
@@ -1,12 +1,6 @@
-import Lotto from '../Lotto.js';
+import Lotto from './Lotto.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
-import validateInputAmount from '../validations/validateInputAmount.js';
-
-const LOTTO_CONSTANT = Object.freeze({
-  min: 1,
-  max: 45,
-  count: 6,
-});
+import LOTTO_CONSTANT from '../constants/lotto.js';
 
 class Counter {
   #lottoTicketNumber;
@@ -18,12 +12,14 @@ class Counter {
   #calculateLottoTicketNumber(inputAmount) {
     this.#lottoTicketNumber = inputAmount / 1000;
   }
+
   #generateLotto() {
     const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(LOTTO_CONSTANT.min, LOTTO_CONSTANT.max, LOTTO_CONSTANT.count);
     lottoNumbers.sort((a, b) => a - b);
 
     return lottoNumbers;
   }
+
   get giveLotto() {
     const lottoBox = [];
     while (this.#lottoTicketNumber) {
