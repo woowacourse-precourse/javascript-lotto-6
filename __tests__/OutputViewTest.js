e6701647a31f4be33b5df957f263c287dd8b8052
osohyun0224 <osohyun0224@naver.com>
2023-11-06 14:55:21 +0900

    test(OutputViewTest): 당첨 결과 출력 로직에 대한 테스트 코드 작성
    - printResult는 당첨 통계와 ROI를 출력하는 기능을 담당합니다.
    - mockRoi와 mockResult를 사용하여 출력값에 대한 테스트 코드를 작성했습니다.
    - 각 결과의 문자열 포맷이 올바른지에 대한 검증을 진행하였습니다.

--- __tests__/OutputViewTest.js
+++ __tests__/OutputViewTest.js
@@ -29,4 +29,26 @@ describe("OutputView 테스트", () => {
     expect(Console.print).toHaveBeenCalledWith("[1, 2, 3, 4, 5, 6]");
     expect(Console.print).toHaveBeenCalledWith("[7, 8, 9, 10, 11, 12]");
   });
+
+  test("당첨 통계와 ROI가 올바르게 출력되는지 확인", () => {
+    const mockRoi = 150;
+    const mockResult = {
+      5: 3,
+      4: 2,
+      3: 1,
+      2: 0,
+      1: 1
+    };
+  
+    OutputView.printResult(mockRoi, mockResult);
+  
+    expect(Console.print).toHaveBeenCalledWith("당첨 통계");
+    expect(Console.print).toHaveBeenCalledWith("---");
+    expect(Console.print).toHaveBeenCalledWith("3개 일치 (5,000원) - 3개");
+    expect(Console.print).toHaveBeenCalledWith("4개 일치 (50,000원) - 2개");
+    expect(Console.print).toHaveBeenCalledWith("5개 일치 (1,500,000원) - 1개");
+    expect(Console.print).toHaveBeenCalledWith("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개");
+    expect(Console.print).toHaveBeenCalledWith("6개 일치 (2,000,000,000원) - 1개");
+    expect(Console.print).toHaveBeenCalledWith("총 수익률은 150.0%입니다.");
+  });  
 });
