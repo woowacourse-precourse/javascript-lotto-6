bba7183ce952b500cf61a0acd4f6bef3b430c1b2
hj7321 <oboki2001@naver.com>
2023-11-06 11:41:57 +0900

    feat: 당첨 번호 유효성 검사 기능을 Lotto 클래스의 validate() 함수에 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,38 @@ class Lotto {
   }
 
   #validate(numbers) {
+    const hasDuplicates = (numbers) => {
+      const uniqueNumbers = [...new Set(numbers)];
+      return numbers.length !== uniqueNumbers.length;
+    };
+    
+    numbers.every(num => {
+      if(Number.isNaN(num))
+        throw new Error("[ERROR] 숫자를 입력해야 합니다.");
+    });
+
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if(hasDuplicates(numbers)) {
+      throw new Error("[ERROR] 서로 다른 숫자를 입력해야 합니다.");
+    }
+
+    numbers.every(num => {
+      if(num <= 0 || num >= 46)
+        throw new Error("[ERROR] 1부터 45 사이의 숫자를 입력해야 합니다.");
+      if(num >= 1 && num <= 45 && Number.isInteger(num))
+        return true;
+    });
   }
 
   // TODO: 추가 기능 구현
+
+  async start() {
+    await this.getPrice();
+    await this.getSixNum();
+    await this.getBonusNum();
+  }
 }
 
 export default Lotto;
