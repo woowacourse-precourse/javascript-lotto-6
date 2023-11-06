ac6315eb87f5389b1574c0953cf4dd65f540d5a4
gunyongbok <qhrrjsdyd123@gmail.com>
2023-11-06 22:48:19 +0900

    feat: 당첨 결과 출력하는 ui 구성

--- src/common/messages.js
+++ src/common/messages.js
@@ -3,6 +3,7 @@ const Messages = {
   PURCHASE_COUNT_MESSAGE: "개를 구매했습니다.",
   GET_WINNING_LOTTO_MESSAGE: "\n당첨 번호를 입력해 주세요.\n",
   GET_BONUS_NUMBER_MESSAGE: "\n보너스 번호를 입력해 주세요.\n",
+  PRINT_WINNING_STATICS_MESSAGE: "\n당첨 통계\n---",
 };
 
 export default Messages;
---
+++ src/ui/PrintWinningStatic.js
@@ -0,0 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
+import Messages from "../common/messages.js";
+import winMessages from "../common/winMessage.js";
+
+const printWinningStatic = (count) => {
+  Console.print(`${Messages.PRINT_WINNING_STATICS_MESSAGE}`);
+  winMessages.forEach((msg, i) => {
+    Console.print(`${msg} ${count[i]}개`);
+  });
+};
+
+printWinningStatic([1, 0, 0, 0, 0]);
+
+export default printWinningStatic;
