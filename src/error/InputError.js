4f343eac0ba2483fe702cc982d5319fa7be1753d
BangDori <qudwns8865@naver.com>
2023-11-06 16:40:50 +0900

    feat: 커스텀 에러 name 추가

--- src/error/InputError.js
+++ src/error/InputError.js
@@ -3,6 +3,7 @@ import { ERROR_PREFIX } from '../constants/message/error.js';
 class InputError extends Error {
   constructor(error) {
     super(ERROR_PREFIX + error);
+    this.name = 'InputError';
   }
 }
 
