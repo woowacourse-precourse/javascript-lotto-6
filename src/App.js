fd5fd64508cefc8d01f2556a319bb2d5dba1310e
woo7i <or2447221@gmail.com>
2023-11-06 19:12:13 +0900

    feat(App) : App 클래스에 구현 된 기능들 하나씩 추가하여 출력 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+import Lotto from './Lotto.js';
+import { parchasedLotto } from './utils.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.lotto = new Lotto([]);
+  }
+
+  async play() {
+    await this.lotto.purchaseLotto.initialize();
+    // 구매한 로또 개수를 출력합니다.
+    parchasedLotto(await this.lotto.purchaseLotto.alertPurchaseLotto());
+    // 로또 리스트를 출력합니다.
+    MissionUtils.Console.print(await this.lotto.viewLottoList());
+  }
 }
 
 export default App;
