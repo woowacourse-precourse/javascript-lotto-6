2c6705eda2228d4092e1960c98832e03a47574ed
abi-hong <flffjakfwm20@gmail.com>
2023-11-06 23:12:24 +0900

    feat(Constant): 랭킹, 상금 상수화

---
+++ src/utils/Constant.js
@@ -0,0 +1,18 @@
+export const RANKING = {
+  0: null,
+  1: null,
+  2: null,
+  3: 5,
+  4: 4,
+  5: (hasBonus) => (hasBonus ? 2 : 3),
+  6: 1,
+}
+
+export const REWARD = {
+  1: 2000000000,
+  2: 30000000,
+  3: 1500000,
+  4: 50000,
+  5: 5000,
+  6: 0,
+}
