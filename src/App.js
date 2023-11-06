378f61bf8226e8c62295a3d84731c9ae83e3ca11
sejineeee <sejinee0918@gmail.com>
2023-11-06 16:16:58 +0900

    feature: 구입금액 입력받아 정수로 변환 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import Input from './Input.js';
+
+const CURRENCY_UNIT = 1000;
+const input = new Input();
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await input.askPurchaseAmount();
+    const parsedpurchaseAmount = parseInt(purchaseAmount, 10);
+  }
 }
 
 export default App;
