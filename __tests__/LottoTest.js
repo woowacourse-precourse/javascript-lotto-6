6ce2d796c15624e0b532740cb82b5f66d2aa8944
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-06 14:01:51 +0900

    test(refactor): 로또 번호 개수 관련 예외 상황을 명시
    기존: 번호 개수가 6개가 넘어갈 때 예외 발생 기대
    변경: 번호 개수가 6개가 아닐 때 예외 발생 기대

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -2,7 +2,7 @@ import Lotto from '../src/Lotto.js';
 
 describe('로또 클래스 테스트', () => {
   describe('예외 테스트', () => {
-    test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+    test('로또 번호의 개수가 6개가 아니면 예외가 발생한다.', () => {
       expect(() => {
         new Lotto([1, 2, 3, 4, 5, 6, 7]);
       }).toThrow('[ERROR]');
