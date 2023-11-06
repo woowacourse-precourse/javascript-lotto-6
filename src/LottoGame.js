8ff33cc0cf005641a8119839ad7a7a21d45f6e1b
djLee77 <dkdnl232@gmail.com>
2023-11-06 22:13:14 +0900

    refactor(LottoGame): 너무 많은 if문 개선, 매직넘버 포장
    -상금 액수 PRIZE_MONEY 상수 객체로 포장
    -foreach문 사용하여 if문 개수 개선

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,10 +1,14 @@
 import Lotto from "./Lotto.js";
 
+const PRIZE_MONEY = {
+  "3개 일치 (5,000원)": 5000,
+  "4개 일치 (50,000원)": 50000,
+  "5개 일치 (1,500,000원)": 1500000,
+  "5개 일치, 보너스 볼 일치 (30,000,000원)": 30000000,
+  "6개 일치 (2,000,000,000원)": 2000000000,
+};
+
 class LottoGame {
-  /*
-    myLotto == [[로또 번호 6개], [로또 번호 6개] , ~구입 개수]
-    winningLotto == {win : [당첨 번호 6개], bonus : 당첨번호}
-  */
   constructor(myLotto, winningLotto) {
     this.lottos = myLotto.map((numbers) => new Lotto(numbers));
     this.winningLotto = winningLotto;
@@ -16,7 +20,6 @@ class LottoGame {
       "6개 일치 (2,000,000,000원)": 0,
       None: 0,
     };
-    this.statistics = "";
     this.reward = 0;
   }
 
@@ -28,16 +31,12 @@ class LottoGame {
   }
 
   generateStatistics() {
+    this.statistics = "";
     Object.entries(this.results).forEach(([key, value]) => {
-      if (key !== "None") this.statistics += `\n${key} - ${value}개`;
-
-      if (key === "3개 일치 (5,000원)") this.reward += 5000 * value;
-      if (key === "4개 일치 (50,000원)") this.reward += 50000 * value;
-      if (key === "5개 일치 (1,500,000원)") this.reward += 1500000 * value;
-      if (key === "5개 일치, 보너스 볼 일치 (30,000,000원)")
-        this.reward += 30000000 * value;
-      if (key === "6개 일치 (2,000,000,000원)")
-        this.reward += 200000000 * value;
+      if (key !== "None") {
+        this.statistics += `\n${key} - ${value}개`;
+        this.reward += PRIZE_MONEY[key] * value;
+      }
     });
   }
 }
