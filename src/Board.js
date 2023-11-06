0c965b31b8e53a1aa24afdd8bc9fbf1e5772c1e0
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-11-07 03:24:00 +0900

    :lipstick: style : 코드포맷팅 누락을 수정한다.

--- src/Board.js
+++ src/Board.js
@@ -1,39 +1,39 @@
 import { Console } from '@woowacourse/mission-utils';
-import {MESSAGE} from "./constants/messages.js";
-import {REWORD, TRY_COST} from "./constants/policy.js";
+import { MESSAGE } from './constants/messages.js';
+import { REWORD, TRY_COST } from './constants/policy.js';
 
 class Board {
-    constructor(ranks) {
-        this.printResult(ranks)
-        this.printRevenue(ranks)
-    }
+  constructor(ranks) {
+    this.printResult(ranks);
+    this.printRevenue(ranks);
+  }
 
-    static getTotalReword(rank) {
-        let totalReword = 0;
-        rank.forEach((v) => {
-            if (v >= 1 && v < 6) {
-                totalReword += REWORD[`${v}th`];
-            }
-        });
-        return totalReword;
-    }
+  static getTotalReword(rank) {
+    let totalReword = 0;
+    rank.forEach((v) => {
+      if (v >= 1 && v < 6) {
+        totalReword += REWORD[`${v}th`];
+      }
+    });
+    return totalReword;
+  }
 
-    static getRevenue(tryCount, totalReword) {
-        return Math.floor((totalReword / tryCount) * TRY_COST * 1000) / 10;
-    }
+  static getRevenue(tryCount, totalReword) {
+    return Math.floor((totalReword / tryCount) * TRY_COST * 1000) / 10;
+  }
 
-    printResult(ranks) {
-        Console.print(MESSAGE.LOTTO.result);
-        for (let i = 1; i < 6; i++) {
-            Console.print(
-                MESSAGE.LOTTO[`${i}th`](ranks.filter((v) => v === i).length)
-            );
-        }
+  printResult(ranks) {
+    Console.print(MESSAGE.LOTTO.result);
+    for (let i = 1; i < 6; i++) {
+      Console.print(
+        MESSAGE.LOTTO[`${i}th`](ranks.filter((v) => v === i).length),
+      );
     }
+  }
 
-    printRevenue(ranks) {
-        Console.print(Board.getRevenue(ranks.length, Board.getTotalReword(ranks)));
-    }
+  printRevenue(ranks) {
+    Console.print(Board.getRevenue(ranks.length, Board.getTotalReword(ranks)));
+  }
 }
 
 export default Board;
