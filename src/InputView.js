26e511bd5ea97e2822b32ece9f91033a15e94753
useon <yoosk4986@naver.com>
2023-11-07 00:41:43 +0900

    refactor: 출력 형식에 알맞게 수정한다

--- src/InputView.js
+++ src/InputView.js
@@ -2,23 +2,17 @@ import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
   async readLottoPurchaseAmount() {
-    const inputValue = await Console.readLineAsync(
-      '구입금액을 입력해 주세요.\n',
-    );
+    const inputValue = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     return inputValue;
   },
 
   async readLottoWinningNumbers() {
-    const inputValue = await Console.readLineAsync(
-      '당첨 번호를 입력해 주세요.\n',
-    );
+    const inputValue = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
     return inputValue;
   },
 
   async readLottoBonusNumber() {
-    const inputValue = await Console.readLineAsync(
-      '보너스 번호를 입력해 주세요.\n',
-    );
+    const inputValue = await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
     return inputValue;
   },
 };
