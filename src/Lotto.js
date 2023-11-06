6d2b56ba2800fc274a5a1993b79bf54d7a6d1b73
greeneeya <suzythetravelerr@gmail.com>
2023-11-06 15:38:37 +0900

    feat: 일치하는 수의 통계를 가져온다

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
@@ -12,9 +14,33 @@ class Lotto {
     }
   }
 
-  #compareNumbers(userNumbers) {
+  #initializeResult() {
+    return {
+      three: 0,
+      four: 0,
+      five: 0,
+      bonusFive: 0,
+      six: 0,
+    };
+  }
+
+  #getResult(issuedTickets, bonus) {
+    const result = this.#initializeResult();
+    issuedTickets.forEach((issuedNumbers) => {
+      const totalMatched = this.#countMatchingNumbers(issuedNumbers);
+      const isBonus = issuedNumbers.includes(bonus);
+      if (totalMatched === 3) result.three += 1;
+      if (totalMatched === 4) result.four += 1;
+      if (totalMatched === 5 && !isBonus) result.five += 1;
+      if (totalMatched === 5 && isBonus) result.bonusFive += 1;
+      if (totalMatched === 6) result.six += 1;
+    });
+    return result;
+  }
+
+  #countMatchingNumbers(issuedNumbers) {
     let totalMatched = 0;
-    userNumbers.forEach((number) => {
+    issuedNumbers.forEach((number) => {
       if (this.#numbers.includes(number)) totalMatched += 1;
     });
     return totalMatched;
