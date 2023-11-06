dffe21af6d5c1a62f69d7d96114d69b081797d2c
BHyeonKim <rlaqhguse@gmail.com>
2023-11-07 02:42:20 +0900

    feat: 정해진 수량만큼 로또 발급

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { generateRandomLottoNumbers } from './utils/index.js';
+import LottoConsole from './view/LottoConsole.js';
+import Lotto from './Lotto.js';
+
 class App {
-  async play() {}
+  #lottos;
+
+  constructor() {
+    this.#lottos = [];
+  }
+
+  async play() {
+    const purchaseAmount = await LottoConsole.getPurchaseAmount();
+    const amount = purchaseAmount / 1000;
+    LottoConsole.printAmountOfLotto(amount);
+
+    // 로또 생성
+    for (let i = 0; i < amount; i++)
+      this.#lottos.push(new Lotto(generateRandomLottoNumbers()));
+
+    const lottoNumbers = await LottoConsole.getLottoNumbers();
+    const bonusNumber = await LottoConsole.getBonusNumber();
+  }
 }
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,42 @@
+import { Console } from '@woowacourse/mission-utils';
+/* eslint-disable no-plusplus */
 class Lotto {
   #numbers;
 
+  #isBonus;
+
+  #numOfCorrect;
+
   constructor(numbers) {
-    this.#validate(numbers);
+    Lotto.#printLottoNumber(numbers);
+    Lotto.#validate(numbers);
     this.#numbers = numbers;
+    this.#numOfCorrect = 0;
+    this.#isBonus = false;
   }
 
-  #validate(numbers) {
+  static #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복된 숫자가 있으면 안됩니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  checkLotto(winningNumbers, bonusNumber) {
+    winningNumbers.forEach(
+      (number) => this.#numbers.includes(number) && this.#numOfCorrect++
+    );
+    if (this.#numbers.includes(bonusNumber)) this.#isBonus = true;
+    return this;
+  }
+
+  static #printLottoNumber(numbers) {
+    Console.print(`[${numbers.join(', ')}]`);
+  }
 }
 
 export default Lotto;
