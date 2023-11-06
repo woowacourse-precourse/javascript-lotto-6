99e439c27983c81a705644fda971c19565e65a11
baegyeong <beth0929@naver.com>
2023-11-06 20:59:52 +0900

    feat(Lotto): 로또 예외 처리 문구 상수 데이터 지정

--- src/constant/ERROR.js
+++ src/constant/ERROR.js
@@ -3,3 +3,12 @@ export const BUY_PRICE_ERROR = {
   isBiggerThanThousand: "[ERROR] 구입 금액은 1000원 이상 입력해야 합니다.",
   isNumber: "[ERROR] 구입 금액은 숫자만 입력하셔야 합니다.",
 };
+
+export const LOTTO_ERROR = {
+  isSixSize: "[ERROR] 로또 번호는 6개여야 합니다.",
+  isOneToFourtyFive: "[ERROR]: 로또 번호는 1부터 45까지 입력 가능합니다.",
+  isNotDuplicate: "[ERROR] 로또 번호는 서로 중복될 수 없습니다.",
+  isNotBlank: "[ERROR] 입력을 쉼표로 마치면 안됩니다.",
+  isNotSpecialSymbol:
+    "[ERROR] 입력에 쉼표를 제외한 특수문자는 포함할 수 없습니다.",
+};
