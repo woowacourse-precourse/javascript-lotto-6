e3139f8ac1a95696dedc7590fe2643384b6e8a4c
Jaewon <wellbird@naver.com>
2023-11-06 22:04:56 +0900

    feat : 구매 금액 범위 에러 메세지 추가

--- src/constant/ErrorMessage.js
+++ src/constant/ErrorMessage.js
@@ -1,4 +1,7 @@
 const errorMessage = Object.freeze({
+  purchaseRangeError: () => {
+    throw new Error("[ERROR]구매 금액은 0보다 큰 숫자여야 합니다.");
+  },
   purchaseError: () => {
     throw new Error("[ERROR]구매 금액은 1,000원으로 나누어 떨어져야 합니다.");
   },
@@ -8,8 +11,8 @@ const errorMessage = Object.freeze({
   typeError: () => {
     throw new Error("[ERROR]입력이 잘못된 형식입니다.");
   },
-  rangeError: () => {
-    throw new Error("[ERROR]로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+  numberRangeError: () => {
+    throw new Error("[ERROR]로또 번호와 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
   },
   duplicateError: () => {
     throw new Error("[ERROR]중복 숫자가 존재합니다.");
