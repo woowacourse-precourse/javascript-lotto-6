ec94c14e3212f1709543bbc7d4646db9ff627d34
hj7321 <oboki2001@naver.com>
2023-11-06 12:07:07 +0900

    refactor: 당첨 번호 유효성 검사 기능의  코드 구조 개선

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,33 +3,32 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.#validateBonusNum(numbers, bonusNum);
     this.#numbers = numbers;
   }
 
+  hasDuplicates = (numbers) => {
+    const uniqueNumbers = [...new Set(numbers)];
+    return numbers.length !== uniqueNumbers.length;
+  };
+
   #validate(numbers) {
-    const hasDuplicates = (numbers) => {
-      const uniqueNumbers = [...new Set(numbers)];
-      return numbers.length !== uniqueNumbers.length;
-    };
-    
-    numbers.every(num => {
-      if(Number.isNaN(num))
-        throw new Error("[ERROR] 숫자를 입력해야 합니다.");
-    });
-
-    if (numbers.length !== 6) {
+    if (!numbers.isArray())
+      throw new Error("[ERROR] 콤마(,)로 구분해야 합니다.");
+    if (numbers.every(num => Number.isNaN(num)))
+      throw new Error("[ERROR] 숫자를 입력해야 합니다.");
+    if (numbers.length !== 6)
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-    if(hasDuplicates(numbers)) {
+    if (hasDuplicates(numbers))
       throw new Error("[ERROR] 서로 다른 숫자를 입력해야 합니다.");
-    }
+    if (numbers.some(num => num <= 0 || num >= 46))
+      throw new Error("[ERROR] 1부터 45 사이의 숫자를 입력해야 합니다.");
+    if (numbers.some(num => num >= 1 && num <= 45 && Number.isInteger(num)) === false)
+      throw new Error("[ERROR] 1부터 45 사이의 정수를 입력해야 합니다.");
+  }
 
-    numbers.every(num => {
-      if(num <= 0 || num >= 46)
-        throw new Error("[ERROR] 1부터 45 사이의 숫자를 입력해야 합니다.");
-      if(num >= 1 && num <= 45 && Number.isInteger(num))
-        return true;
-    });
+  #validateBonusNum(numbers, bonusNum) {
+    // 작성
   }
 
   // TODO: 추가 기능 구현
