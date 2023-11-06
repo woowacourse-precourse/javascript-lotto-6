a415b919110f1b2efeefdd3e76c2f0029ca06771
anttiey <hanuuny@gmail.com>
2023-11-06 12:52:58 +0900

    test: 당첨 번호 입력 유효성 테스트 추가
    - 입력된 값이 없는 케이스
    - 입력된 값이 쉼표로 구분되지 않는 케이스
    - 입력된 값이 6개가 아닌 케이스
    - 입력된 값에 중복이 있는 케이스
    - 입력된 값에 숫자가 아닌 값이 있는 케이스
    - 입력된 값에 범위를 만족하지 않는 값이 있는 케이스

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -36,3 +36,47 @@ describe('구입 금액 입력 유효성 테스트', () => {
     }).toThrow('[ERROR] 입력이 1,000원 단위가 아닙니다.');
   });
 });
+
+describe('당첨 번호 입력 유효성 테스트', () => {
+  test('입력된 값이 없을 때, 예외가 발생한다.', () => {
+    const input = '';
+    expect(() => {
+      new App().validateWinningNumbers(input);
+    }).toThrow('[ERROR] 입력이 없습니다.');
+  });
+
+  test('입력된 값이 쉼표로 구분되지 않을 때, 예외가 발생한다.', () => {
+    const input = '123456';
+    expect(() => {
+      new App().validateWinningNumbers(input);
+    }).toThrow('[ERROR] 입력이 쉼표로 구분되지 않습니다.');
+  });
+
+  test('입력된 값이 6개가 아닐 때, 예외가 발생한다.', () => {
+    const input = '1,2,3,4,5';
+    expect(() => {
+      new App().validateWinningNumbers(input);
+    }).toThrow('[ERROR] 입력이 6개가 아닙니다.');
+  });
+
+  test('입력된 값에 중복이 있을 때, 예외가 발생한다.', () => {
+    const input = '1,1,2,3,4,5';
+    expect(() => {
+      new App().validateWinningNumbers(input);
+    }).toThrow('[ERROR] 입력에 중복된 값이 있습니다.');
+  });
+
+  test('입력된 값에 숫자가 아닌 값이 있을 때, 예외가 발생한다.', () => {
+    const input = 'ㄱ,1,2,3,4,5';
+    expect(() => {
+      new App().validateWinningNumbers(input);
+    }).toThrow('[ERROR] 입력이 숫자가 아닙니다.');
+  });
+
+  test('입력된 값에 1 ~ 45 사이가 아닌 값이 있을 때, 예외가 발생한다.', () => {
+    const input = '46,1,2,3,4,5';
+    expect(() => {
+      new App().validateWinningNumbers(input);
+    }).toThrow('[ERROR] 입력이 1 ~ 45 사이가 아닙니다');
+  });
+});
