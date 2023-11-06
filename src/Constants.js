b767569c3579e2fc688bc5cb3620e0b05d184f9f
sehyun <e6d1fe@gmail.com>
2023-11-06 20:17:10 +0900

    feat: 상수 목록 객체 freeze

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const message = {
+export const message = Object.freeze({
   ENTER_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
   ENTER_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
   ENTER_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
@@ -14,9 +14,9 @@ export const message = {
   PRINT_GROSS_RETURN_END: '%입니다.',
   COUNT_UNIT: '개\n',
   ERROR: '[ERROR] 숫자가 잘못된 형식입니다.',
-};
+});
 
-export const num = {
+export const num = Object.freeze({
   LOTTO_NUMBERS_QUANTITY: 6,
   LOTTO_PRICE: 1000,
   LOTTO_LOWER_LIMIT: 1,
@@ -26,4 +26,4 @@ export const num = {
   MATCH_5: 1500000,
   MATCH_5_PLUS_BONUS: 30000000,
   MATCH_6: 2000000000,
-};
+});
