b4562ece74070e21a422f8c6a43ca944a8fb4bbf
gunyongbok <qhrrjsdyd123@gmail.com>
2023-11-06 15:38:35 +0900

    refactor: PurchasePriceInput 클래스를 함수로 변경

--- src/RunApp.js
+++ src/RunApp.js
@@ -1,5 +1,8 @@
 import Lotto from "./Lotto.js";
-import PurchasePriceInput from "./ui/purchasePriceInput.js";
+import ErrorMessages from "./common/errorMessages.js";
+import printBonusNumber from "./ui/PrintBonusNumber.js";
+import purchasePriceInput from "./ui/purchasePriceInput.js";
+import validateBonusNumber from "./util/validateBonusNumber.js";
 
 class RunApp {
   constructor() {
@@ -11,14 +14,21 @@ class RunApp {
   }
 
   async start() {
-    const purchasePriceInput = new PurchasePriceInput();
-    await purchasePriceInput.print();
+    await purchasePriceInput();
     await this.getLotto();
+    await this.getBonusNumber();
   }
 
   async getLotto() {
     const lottoInstance = new Lotto();
     this.numbers = await lottoInstance.main();
+  }
+
+  async getBonusNumber() {
+    const bonusNumber = await printBonusNumber();
+    if (!validateBonusNumber(this.numbers, bonusNumber))
+      throw new Error(`${ErrorMessages.BONUS_NUMBER_ERROR_MESSAGE}`);
+    this.numbers.push(bonusNumber);
     console.log(this.numbers);
   }
 }
--- src/ui/PurchasePriceInput.js
+++ src/ui/PurchasePriceInput.js
@@ -5,15 +5,11 @@ import validatePriceInput from "../util/validatePriceInput.js";
 import ErrorMessages from "../common/errorMessages.js";
 import printPurchaseLotto from "./PrintPurchaseLottos.js";
 
-let count = 0;
+const purchasePriceInput = async () => {
+  let price = await Console.readLineAsync(Messages.PURCHASE_INPUT_MESSAGE);
+  if (!validatePriceInput(price))
+    throw new Error(ErrorMessages.PRICE_INPUT_ERRORMESSAGE);
+  printPurchaseLotto(price / 1000);
+};
 
-class PurchasePriceInput {
-  async print() {
-    let price = await Console.readLineAsync(Messages.PURCHASE_INPUT_MESSAGE);
-    if (!validatePriceInput(price))
-      throw new Error(ErrorMessages.PRICE_INPUT_ERRORMESSAGE);
-    printPurchaseLotto(price / 1000);
-  }
-}
-
-export default PurchasePriceInput;
+export default purchasePriceInput;
