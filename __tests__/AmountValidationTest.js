91ab5619f649a63d6a5337e9b33c4632f56f6ac7
Yoonyesol <maniac970430@naver.com>
2023-11-06 22:43:10 +0900

    test: 코드 분리에 따른 테스트 케이스 실행 함수 변경

--- __tests__/AmountValidationTest.js
+++ __tests__/AmountValidationTest.js
@@ -1,10 +1,10 @@
-import UserInput from "../src/utils/UserInput.js";
+import InputValidate from "../src/utils/InputValidate";
 
 describe("로또 테스트: 금액 입력 예외 테스트", () => {
   test("공백 입력", () => {
     const emptyInput = " ";
 
-    expect(() => UserInput.validateAmount(emptyInput)).toThrow(
+    expect(() => InputValidate.validateAmount(emptyInput)).toThrow(
       "[ERROR] 입력값이 비어 있습니다."
     );
   });
@@ -12,15 +12,15 @@ describe("로또 테스트: 금액 입력 예외 테스트", () => {
   test("입력값이 숫자가 아닌 경우", () => {
     const nonNumericInput = "abc";
 
-    expect(() => UserInput.validateAmount(nonNumericInput)).toThrow(
+    expect(() => InputValidate.validateAmount(nonNumericInput)).toThrow(
       "[ERROR] 숫자가 아닌 값이 입력되었습니다."
     );
   });
 
-  test("0이하 또는 음수 입력", () => {
+  test("0 이하 또는 음수 입력", () => {
     const negativeInput = "0";
 
-    expect(() => UserInput.validateAmount(negativeInput)).toThrow(
+    expect(() => InputValidate.validateAmount(negativeInput)).toThrow(
       "[ERROR] 유효한 범위의 값을 입력해 주세요."
     );
   });
@@ -28,7 +28,7 @@ describe("로또 테스트: 금액 입력 예외 테스트", () => {
   test("0이하 또는 음수 입력", () => {
     const negativeInput = "-1000";
 
-    expect(() => UserInput.validateAmount(negativeInput)).toThrow(
+    expect(() => InputValidate.validateAmount(negativeInput)).toThrow(
       "[ERROR] 유효한 범위의 값을 입력해 주세요."
     );
   });
@@ -36,7 +36,7 @@ describe("로또 테스트: 금액 입력 예외 테스트", () => {
   test("1000원 단위가 아닌 수 입력", () => {
     const invalidAmount = "2500";
 
-    expect(() => UserInput.validateAmount(invalidAmount)).toThrow(
+    expect(() => InputValidate.validateAmount(invalidAmount)).toThrow(
       "[ERROR] 구입금액은 1000원 단위로 입력해야 합니다."
     );
   });
@@ -44,6 +44,6 @@ describe("로또 테스트: 금액 입력 예외 테스트", () => {
   test("유효한 입력", () => {
     const validInput = "3000";
 
-    expect(UserInput.validateAmount(validInput)).toBe(3000);
+    expect(InputValidate.validateAmount(validInput)).toBe(3000);
   });
 });
--- __tests__/LottoBonusNumbersValidationTest.js
+++ __tests__/LottoBonusNumbersValidationTest.js
@@ -1,16 +1,18 @@
-import UserInput from "../src/utils/UserInput.js";
+import InputValidate from "../src/utils/InputValidate.js";
 
 describe("로또 테스트: 보너스 번호 예외 테스트", () => {
   test("공백 입력", () => {
     const emptyInput = " ";
 
-    expect(() => UserInput.validateBonusNumber(emptyInput)).toThrow("[ERROR]");
+    expect(() => InputValidate.validateBonusNumber(emptyInput)).toThrow(
+      "[ERROR]"
+    );
   });
 
   test("입력값이 숫자가 아닌 경우", () => {
     const nonNumericInput = "a";
 
-    expect(() => UserInput.validateBonusNumber(nonNumericInput)).toThrow(
+    expect(() => InputValidate.validateBonusNumber(nonNumericInput)).toThrow(
       "[ERROR]"
     );
   });
@@ -18,15 +20,15 @@ describe("로또 테스트: 보너스 번호 예외 테스트", () => {
   test("0이하 또는 음수 입력", () => {
     const negativeInput = "0";
 
-    expect(() => UserInput.validateBonusNumber(negativeInput)).toThrow(
+    expect(() => InputValidate.validateBonusNumber(negativeInput)).toThrow(
       "[ERROR]"
     );
   });
 
-  test("0이하 또는 음수 입력", () => {
+  test("0 이하 또는 음수 입력", () => {
     const negativeInput = "-6";
 
-    expect(() => UserInput.validateBonusNumber(negativeInput)).toThrow(
+    expect(() => InputValidate.validateBonusNumber(negativeInput)).toThrow(
       "[ERROR]"
     );
   });
@@ -34,7 +36,7 @@ describe("로또 테스트: 보너스 번호 예외 테스트", () => {
   test("유효한 범위를 벗어나는 입력이 존재하는 경우", () => {
     const outOfRangeInput = "48";
 
-    expect(() => UserInput.validateBonusNumber(outOfRangeInput)).toThrow(
+    expect(() => InputValidate.validateBonusNumber(outOfRangeInput)).toThrow(
       "[ERROR]"
     );
   });
