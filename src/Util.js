79359cc1ef2e24f99ddcc532d12324593c1f585b
parkblo <badmuxi@gmail.com>
2023-11-06 15:46:20 +0900

    feat: 숫자 형식 여부를 판단하는 예외 처리 추가

--- src/Util.js
+++ src/Util.js
@@ -1,4 +1,7 @@
 export function validatePurchasePrice(price,basedAmount) {
+    if (isNaN(price)) {
+        throw new Error("[ERROR] 구매 금액이 올바른 숫자 형식이 아닙니다.")
+    }
     if (price % basedAmount != 0) {
         throw new Error("[ERROR] 구매 금액은 1,000원 단위 입니다.")
     }
@@ -10,11 +13,18 @@ export function validateLottoNumbers(numbers,bonusNumber) {
     }
 
     numbers.forEach(number => {
+        if (isNaN(number)) {
+            throw new Error("[ERROR] 당첨 번호가 올바른 숫자 형식이 아닙니다.")
+        }
         if (number < 1 && number > 45) {
             throw new Error("[ERROR] 당첨 번호는 1~45 사이의 번호여야 합니다.")
         }
+        
     })
 
+    if (isNaN(bonusNumber)) {
+        throw new Error("[ERROR] 보너스 당첨 번호가 올바른 숫자 형식이 아닙니다.")
+    }
     if (bonusNumber < 1 && bonusNumber > 45) {
         throw new Error("[ERROR] 보너스 당첨 번호는 1~45 사이의 번호여야 합니다.")
     }
