455d60a253164ef293a6ab135a787858eb48632f
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-11-06 12:22:39 +0900

    feat: 금액 입력 기능 작성 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const money = await buyLotto();
+      validateMoney(money);
+      const data = makeNumbers(money);
+      showNumbersList(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 export default App;
+
+const buyLotto = async () => {
+  const money = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  return parseInt(money);
+};
+
+const validateMoney = (money) => {
+  const input_money = money;
+  const isRight = input_money % 1000;
+  if (isNaN(isRight)) throw new Error('[ERROR] 숫자만 입력 가능 합니다.');
+  if (input_money % 1000 !== 0) throw new Error('[ERROR] 1,000원 단위로 입력 가능합니다.');
+};
+
+const makeNumbers = (money) => {
+  let cur_money = money;
+  const total_Numbers_arr = [];
+
+  while (cur_money > 0) {
+    const numbers = genRandomNumbers(6);
+    total_Numbers_arr.push(numbers);
+    cur_money -= 1000;
+  }
+  return total_Numbers_arr;
+};
+
+const genRandomNumbers = (length) => {
+  const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, length);
+  numbers.sort((a, b) => a - b);
+  return numbers;
+};
+
+const showNumbersList = (list) => {
+  let result = `${list.length}개를 구매했습니다.\n`;
+  list.forEach((numbers) => {
+    result += `[${numbers.join(', ')}]\n`;
+  });
+  Console.print(result);
+};
