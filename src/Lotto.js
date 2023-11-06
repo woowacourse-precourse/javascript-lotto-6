96ae2a1411f6050e0c533a2712b9ab8f73d38b60
serin-B <bangserin980302@gmail.com>
2023-11-06 19:30:46 +0900

    feat(Lotto): 로또 클래스 validate 추가 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+import MESSAGES from "./constants/Messages.js";
+import { LOTTO } from "./constants/Standard.js";
+import { isOnlyNumber, isInRangeNumber } from "./utils/Validation.js";
+
 class Lotto {
   #numbers;
 
@@ -7,12 +11,27 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers.length !== LOTTO.LENGTH) {
+      throw new Error(MESSAGES.ERROR.INVAILD_LOTTO_LENGTH);
     }
+    if (!isOnlyNumber(numbers.join(""))) {
+      throw new Error(MESSAGES.ERROR.PLEASE_ONLY_NUMBER);
+    }
+    if (!isInRangeNumber(numbers)) {
+      throw new Error(MESSAGES.ERROR.INVAILD_LOTTO_NUMBER);
+    }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error(MESSAGES.ERROR.DUPLICATE_LOTTO_NUMBER);
+    }
+    return true;
   }
 
-  // TODO: 추가 기능 구현
+  checkSameNumber(winningNumbers, bonusNumber) {
+    // 로또 번호는 외부에서 접근할 수 없는 필드, 내부에서 비교해야 함!
+    // 당첨 번호와 현재 로또 번호 비교하기
+    // 일치하는 숫자가 3개 이상이면 유의미
+    // 보너스 번호는 5개가 일치하는 경우에만 사용됨
+  }
 }
 
 export default Lotto;
