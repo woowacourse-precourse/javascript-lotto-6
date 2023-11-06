897e941f87ac2b1aafa2386c0f7475cbaa4859df
pakxe <pigkill40@naver.com>
2023-11-06 16:13:40 +0900

    chore: 수익률 계산이 클래스로 분리되어 기존 파일 제거

--- src/utils/calcTotalIncome.js
+++
@@ -1,9 +0,0 @@
-import PRIZES from '../constant/Prizes.js';
-
-const calcTotalIncome = (result) => {
-  return result.reduce((sum, count, i) => {
-    return (sum += PRIZES[i] * count);
-  }, 0);
-};
-
-export default calcTotalIncome;
--- src/utils/calcYieldPercent.js
+++
@@ -1,7 +0,0 @@
-const calcYieldPercent = (investment, totalIncome) => {
-  const yieldPercent = (totalIncome / investment) * 100;
-
-  return parseFloat(yieldPercent.toFixed(2));
-};
-
-export default calcYieldPercent;
