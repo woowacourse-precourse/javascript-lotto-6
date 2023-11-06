1504bcedd6568332502507797245b299073871f3
narinn-star <skfls2618@naver.com>
2023-11-07 01:42:04 +0900

    feat : InputView
    - inputPurchaseAmount
    - inputLottoNumbers
    - inputBonusNumber

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,10 +1,33 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from '../Constants/constants';
+
 class InputView {
   // 구매 금액 입력
-  async inputPurchase() {}
+  static async inputPurchaseAmount() {
+    const purchaseAmount = await Console.readLineAsync(
+      MESSAGE.INPUT_PURCHASE_AMOUNT,
+    );
+    // TODO : validation
+    return purchaseAmount;
+  }
 
   // 당첨 번호 입력
-  async inputLottoNumbers() {}
+  static async inputLottoNumbers() {
+    const inputNumbers = await Console.readLineAsync(
+      MESSAGE.INPUT_LOTTO_NUMBERS,
+    );
+    const lottoNumbers = inputNumbers.split(',');
+    // TODO : validation
+    return lottoNumbers;
+  }
 
-  async inputBonusNumber() {}
+  // 보너스 번호 입력
+  static async inputBonusNumber() {
+    const bonusNumber = await Console.readLineAsync(
+      MESSAGE.INPUT_BONUS_NUMBERS,
+    );
+    // TODO : validation
+    return bonusNumber;
+  }
 }
 export default InputView;
