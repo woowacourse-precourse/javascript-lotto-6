51aca313b49d2d5e49c40842fad8f31d72383db3
jungmyungjin <myung2667@gmail.com>
2023-11-07 02:19:52 +0900

    feat(ErrorMessage) : 에러메시지 상수 파일 추가

---
+++ src/Constructor/ErrorMessage.js
@@ -0,0 +1,8 @@
+import { LOTTO_NUMBER_COUNT } from "./Setting";
+
+const ERROR_PREFIX = "[ERROR]";
+const ERROR_MESSAGE = {
+  INVALID_LOTTO_NUMBER_COUNT: `${ERROR_PREFIX} 로또 번호는 ${LOTTO_NUMBER_COUNT}개여야 합니다.`,
+};
+
+export default ERROR_MESSAGE;
