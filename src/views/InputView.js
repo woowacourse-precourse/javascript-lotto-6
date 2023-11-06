212e1d845237bf737e11be301bc111a6122ef905
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 03:52:17 +0900

    :sparkles: feat: 당첨번호와 보너스번호 입력기능 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -4,6 +4,14 @@ const InputView = {
   async getPurchaseAmount() {
     return Console.readLineAsync('구입금액을 입력해 주세요.\n');
   },
+
+  async getWinningNumbers() {
+    return Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+  },
+
+  async getBonusNumber() {
+    return Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+  },
 };
 
 export default InputView;
