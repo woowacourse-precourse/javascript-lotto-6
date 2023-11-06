970e1408e1dbbf893d45ac03f2da9cecfebf6fbf
starcradle101 <dreamerdev12@gmail.com>
2023-11-06 16:15:53 +0900

    🛠️Feat($Logs): Logs 모듈 구현 UI를 위한 Log 모듈 추가

---
+++ src/constants/Logs.js
@@ -0,0 +1,17 @@
+export const USER_INPUT = {
+  purchaseAmountInputPrompt: '구입금액을 입력해 주세요.\n',
+  winningNumbersInputPrompt: '당첨 번호를 입력해 주세요.\n',
+  bonusNumberInputPrompt: '보너스 번호를 입력해 주세요.\n',
+};
+
+export const USER_OUTPUT = {
+  purchaseCountPrompt: '개를 구매했습니다.\n',
+  winningStatistics: '당첨 통계\n---',
+  match6Prize: '6개 일치 (2,000,000,000원) - ',
+  match5WithBonusPrize: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  match5Prize: '5개 일치 (1,500,000원) - ',
+  match4Prize: '4개 일치 (50,000원) - ',
+  match3Prize: '3개 일치 (5,000원) - ',
+  totalProfitRatePrefix: '총 수익률은',
+  totalProfitRateSuffix: '%입니다.\n',
+};
