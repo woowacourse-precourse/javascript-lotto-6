35445ed1ac649d6113c46783f474584e42867d51
mieum12 <qpdlqltb1215@gmail.com>
2023-11-07 02:54:09 +0900

    fix: 에러의 경우 해당 부분부터 재시작 로직 추가, 입력값으로 로또 머신 만들기 로직 추가

--- src/view/InputValidator.js
+++ src/view/InputValidator.js
@@ -1,36 +1,37 @@
 export class InputValidator {
-    /**
-     *
-     * @param input
-     * @description - [사용자가 입력한 구입 금액 검증]
-     */
-    validateMoneyInput(input) {
-        //숫자가 아닌 경우
-        if (isNaN(Number(input))) {
-            throw new Error("[ERROR] 구입 금액은 숫자만 가능합니다.")
-        }
+  /**
+   * @param {string} input
+   * @return {void}
+   * @description - [사용자가 입력한 구입 금액 검증]
+   */
+  validateMoneyInput(input) {
+    //숫자가 아닌 경우
+    if (isNaN(Number(input))) {
+      throw new Error("[ERROR] 구입 금액은 숫자만 가능합니다.");
     }
+  }
 
-    /**
-     *
-     * @param input
-     * @description - [사용자가 입력한 당첨 번호 검증]
-     */
-    validateWinningNumberInput(input) {
-        // 공백이 포함 된 경우
-        //
+  /**
+   *
+   * @param {string} input
+   * @return {void}
+   * @description - [사용자가 입력한 당첨 번호 검증]
+   */
+  validateWinningNumberInput(input) {
+    // 공백이 포함 된 경우
+    //
+  }
+
+  /**
+   *
+   * @param {string} input
+   * @return {void}
+   * @description - [사용자가 입력한 보너스 번호 검증]
+   */
+  validateBonusNumberInput(input) {
+    //숫자가 아닌 경우
+    if (isNaN(Number(input))) {
+      throw new Error("[ERROR] 구입 금액은 숫자만 가능합니다.");
     }
-
-    /**
-     *
-     * @param input
-     * @description - [사용자가 입력한 보너스 번호 검증]
-     */
-    validateBonusNumberInput(input) {
-        //숫자가 아닌 경우
-        if (isNaN(Number(input))) {
-            throw new Error("[ERROR] 구입 금액은 숫자만 가능합니다.")
-        }
-    }
-
+  }
 }
