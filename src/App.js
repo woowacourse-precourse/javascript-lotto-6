f6cdd424c7b3e3a04ee439b02c759c4bc90ba4eb
정민승 <jeongminseung@jeongminseung-ui-MacBookAir.local>
2023-11-06 09:07:20 +0900

    구입금액 입력 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils, Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    this.getUserNumbers();
+  }
+  async getUserNumbers() {
+    try {
+      const userNumbers = await Console.readLineAsync(
+        "구입금액을 입력해주세요."
+      );
+      if (userNumbers % 1000 !== 0) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+    } catch (error) {
+      console.error(error.message);
+      this.getUserNumbers();
+    }
+  }
 }
+const app = new App();
+app.play();
 
 export default App;
