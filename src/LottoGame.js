734d766369ca8a2265d4791b78ccd1cdb89dd6a0
JUNU <bokjunwoo@gmail.com>
2023-11-07 03:07:01 +0900

    feat : 생성자를 추가해 입력받은 구입 금액에 해당하는 만큼 로또를 발행 기능 구현
    - setupInputMoney 메서드가 돈을 입력받고 유효성을 검사한 후, count 생성자를 업데이트합니다.
    - generateRandomNumbers 메서드로 무작위 로또 티켓을 생성하고 lottos 생성자를 업데이트합니다.
    - generateRandomLotto 메서드를 사용하여 무작위 숫자 배열을 생성하며, 숫자를 오름차순으로 정렬합니다.

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,19 +1,49 @@
-import { Console } from '@woowacourse/mission-utils';
 import InputView from './InputView.js';
-import { GAME_MESSAGES } from './constants.js';
-import { validateMoney } from './utils.js';
+import { GAME_MESSAGES, LOTTO_PRICE, NUMBER_RANGE } from './constants.js';
+import { pickUniqueNumbersInRange, validateMoney } from './utils.js';
+import OutputView from './OutputView.js';
 
 class LottoGame {
+  #count;
+  #lottos;
+
+  constructor() {
+    this.#count = 0;
+    this.#lottos = [];
+  }
+
   async setupInputMoney() {
     while (true) {
-      const money = await InputView.inputCommon(GAME_MESSAGES.ENTER_PURCHASE_AMOUNT);
+      const money = await InputView.inputCommon(
+        GAME_MESSAGES.ENTER_PURCHASE_AMOUNT
+      );
       try {
         validateMoney(money);
+        this.#count = money / LOTTO_PRICE;
         break;
       } catch (error) {
         Console.print(error.message);
       }
     }
+    this.generateRandomNumbers();
+  }
+
+  generateRandomNumbers() {
+    for (let i = 0; i < this.#count; i++) {
+      const randomTicket = this.generateRandomLotto();
+      this.#lottos.push(randomTicket);
+    }
+    OutputView.printLottoNumbers(this.#lottos);
+    this.setupInputWinningNumbers();
+  }
+
+  generateRandomLotto() {
+    const randomNumbers = pickUniqueNumbersInRange(
+      NUMBER_RANGE.MIN,
+      NUMBER_RANGE.MAX,
+      NUMBER_RANGE.COUNT
+    );
+    return randomNumbers.sort((a, b) => a - b);
   }
 }
 
