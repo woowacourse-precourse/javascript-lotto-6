b487fc82935879b73329728cd989eb4455894bec
NSRBSG <nsrbsg@gmail.com>
2023-11-06 13:40:38 +0900

    feat(LottoMachine): 로또 구입 금액 입력 예외 처리 추가

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -3,12 +3,23 @@ import IOUtils from "./IOUties.js";
 class LottoMachine {
 
     async getLottoPurchaseAmount() {
-        const purchaseAmount = await IOUtils.input("구입금액을 입력해 주세요.\n");
+        const purchaseAmount = Number(await IOUtils.input("구입금액을 입력해 주세요.\n"));
         this.#validateLottoPurchaseAmount(purchaseAmount);
-        return Number(purchaseAmount);
+        return purchaseAmount;
     }
 
-    #validateLottoPurchaseAmount() {
+    #validateLottoPurchaseAmount(purchaseAmount) {
+        if (isNaN(purchaseAmount)) {
+            throw new Error("로또 구입 금액은 숫자여야 합니다.");
+        }
+
+        if (purchaseAmount < 1000) {
+            throw new Error("로또 구입 금액은 1000원 이상이어야 합니다.");
+        }
+
+        if (purchaseAmount % 1000 !== 0) {
+            throw new Error("로또 구입 금액은 1000원 단위여야 합니다.");
+        }
     }
 }
 
