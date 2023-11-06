d3b3cb07ea9db9408adf7c7209088253dd6aee81
[SW7] 박건우 <ceh2000212@gmail.com>
2023-11-07 03:10:06 +0900

    Feat 사용자 입력 받는 기능 오류 처리

--- src/App.js
+++ src/App.js
@@ -7,8 +7,42 @@ class App {
   }
 
   async play() {
+    // 사용자의 입력 받기 , 및 오류처리
+    await this.userInput();
+    console.log(this.userMoneyInput);
 
+    this.totalLottoListUser();
   }
+
+  async userInput() {
+    let userInput;
+    let isValidInput = false;
+
+    while (!isValidInput) {
+      userInput = await this.getUserMoney();
+
+      try {
+        this.checkUserMoney(userInput);
+        isValidInput = true;
+      } catch (error) {
+        // 에러 메시지 출력 및 다시 입력 받기
+        console.error(error.message);
+      }
+    }
+    this.userMoneyInput = Number(userInput);
+  }
+
+  async getUserMoney() {
+    return await Console.readLineAsync("구입금액을 입력해 주세요");
+  }
+
+  checkUserMoney(userInput) {
+    if (userInput % 1000 !== 0) {
+      throw new Error(ERROR.INVALID_INPUT);
+    }
+  }
+
+  
 }
 
 
