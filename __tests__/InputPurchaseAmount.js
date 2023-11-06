882fc18e0c59975f6497f544b1a4cdd7b88d3b41
Junseo <monstazo911@gmail.com>
2023-11-06 17:57:45 +0900

    feat: 구입 금액 입력 메소드 추가 및 테스트 코드 수정
    - 구입 금액 입력 메소드 inputPurchaseAmount 추가
    - 구입 금액 입력 후 Number()를 통해 숫자로 변환 시도
    - 유효성 검사 메소드 validatePurchaseAmount에서 유효성 확인
    - 입력된 내용이 숫자가 아니거나 1000원 단위가 아닐경우 예외 발생
    - 이후 App의 purchaseAmount 필드에 저장 (나중에 수익률 계산을 위해 사용될 예정)

--- __tests__/InputPurchaseAmount.js
+++ __tests__/InputPurchaseAmount.js
@@ -12,27 +12,36 @@ const mockQuestions = (inputs) => {
 };
 
 describe("로또 구매 금액 입력 테스트", () => {
-  test("1000원 단위로 입력할 경우 구매할 개수를 반환", () => {
+  test("1000원 단위로 입력할 경우 App인스턴스에 구매 금액 저장", async () => {
     const INPUT_PURCHASE_AMOUNT = ["11000"];
-    const RESULT = 11;
+    const RESULT = 11000;
+
     mockQuestions(INPUT_PURCHASE_AMOUNT);
 
     const app = new App();
 
-    const purchaseAmount = app.inputPurchaseAmount();
+    await app.inputPurchaseAmount();
 
-    expect(purchaseAmount).toEqual(RESULT);
+    expect(app.purchaseAmount).toEqual(RESULT);
   });
 
-  test("1000원 단위가 아닐경우 에러 발생", () => {
+  test("1000원 단위가 아닐경우 에러 발생", async () => {
     const INPUT_PURCHASE_AMOUNT = ["11500"];
 
     mockQuestions(INPUT_PURCHASE_AMOUNT);
 
     const app = new App();
 
-    expect(() => {
-      app.inputPurchaseAmount();
-    }).toThrow("[ERROR]");
+    await expect(app.inputPurchaseAmount()).rejects.toThrow("[ERROR]");
+  });
+
+  test("숫자가 아닐경우 에러 발생", async () => {
+    const INPUT_PURCHASE_AMOUNT = ["money"];
+
+    mockQuestions(INPUT_PURCHASE_AMOUNT);
+
+    const app = new App();
+
+    await expect(app.inputPurchaseAmount()).rejects.toThrow("[ERROR]");
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+  purchaseAmount = 0;
+
+  async inputPurchaseAmount() {
+    const inputPurchaseAmount = await MissionUtils.Console.readLineAsync(
+      "구입금액을 입력해 주세요."
+    );
+    const purchaseAmount = Number(inputPurchaseAmount);
+    App.validatePurchaseAmount(purchaseAmount);
+    this.purchaseAmount = purchaseAmount;
+  }
+
+  static validatePurchaseAmount(purchaseAmount) {
+    if (Number.isNaN(purchaseAmount))
+      throw new Error("[ERROR] 구매 금액은 숫자여야 합니다.");
+    if (purchaseAmount % 1000)
+      throw new Error("[ERROR] 구매 금액은 1,000원 단위여야 합니다.");
+  }
+
   async play() {}
 }
 
