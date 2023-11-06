b1d4d54b6d834c84b279ddf46f2a443284d16f9e
정민승 <jeongminseung@jeongminseung-ui-MacBookAir.local>
2023-11-06 10:32:20 +0900

    당첨번호 입력받기, 보너스 번호 입력받기.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,66 @@ import { MissionUtils, Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    this.getUserNumbers();
+    this.getUserBuyCount();
   }
-  async getUserNumbers() {
+  async getUserBuyCount() {
     try {
-      const userNumbers = await Console.readLineAsync(
-        "구입금액을 입력해주세요."
-      );
-      if (userNumbers % 1000 !== 0) {
+      const userBuy = await Console.readLineAsync("구입금액을 입력해주세요.");
+      if (userBuy % 1000 !== 0) {
         throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+      this.lottoCount(userBuy);
+    } catch (error) {
+      console.error(error.message);
+      this.getUserBuyCount();
+    }
+  }
+  lottoCount(userBuy) {
+    const userBuyLotto = userBuy / 1000;
+    Console.print(`${userBuyLotto}개를 구매했습니다`);
+    this.getUserNum();
+  }
+  async getUserNum() {
+    const userNums = await Console.readLineAsync("당첨번호를 입력해주세요.");
+    const userSixNums = userNums.split(",");
+    // Console.print(userSixNums);
+    this.checkUserNum(userSixNums);
+  }
+  checkUserNum(userSixNums) {
+    try {
+      const set = new Set(userSixNums);
+      for (let i = 0; i < userSixNums.length; i++) {
+        if (
+          userSixNums[i] > 45 ||
+          userSixNums[i] < 1 ||
+          userSixNums.length !== 6 ||
+          userSixNums.length !== set.size // 중복된 숫자 확인
+        ) {
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+      }
+      this.getUserBonusNum();
+    } catch (error) {
+      console.error(error.message);
+      this.getUserNum();
+    }
+  }
+  async getUserBonusNum() {
+    const userBonus = await Console.readLineAsync(
+      "보너스 번호를 입력해주세요."
+    );
+    this.checkBonusNum(userBonus);
+  }
+  checkBonusNum(userBonus) {
+    try {
+      for (let i = 0; i < userBonus.length; i++) {
+        if (userBonus[i] > 45 || userBonus[i] < 1) {
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
       }
     } catch (error) {
       console.error(error.message);
-      this.getUserNumbers();
+      this.getUserBonusNum();
     }
   }
 }
