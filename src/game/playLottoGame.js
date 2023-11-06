84e59d22de93cb1ad95c749a1aabf605c1a81cdb
Hchan2 <seongchan_@naver.com>
2023-11-06 22:35:14 +0900

    랜덤으로 생성된 로또 번호와 사용자가 입력한 로또 번호 비교 메소드

---
+++ src/game/playLottoGame.js
@@ -0,0 +1,22 @@
+import { RESULT_CHECK } from "../constant/constant";
+import { includedBonusNumber, includedLottoNumbers } from "./checkBonusNumber";
+import printLottoResult from "./printLottoResult";
+
+const playLottoGame = (inputNumber, lottoArray, bonusNumber, userBuyMoney) => {
+    const checkLottoResult = RESULT_CHECK;
+
+    lottoArray.forEach((numbers) => {
+        const lottoCount = includedLottoNumbers(inputNumber, numbers);
+        const bonusCheck = includedBonusNumber(numbers, bonusNumber);
+
+        if (lottoCount === 3) checkLottoResult.three.count += 1;
+        if (lottoCount === 4) checkLottoResult.four.count += 1;
+        if (lottoCount === 5) checkLottoResult.five.count += 1;
+        if (lottoCount === 5 && bonusCheck) checkLottoResult.fiveBonus.count += 1;
+        if (lottoCount === 6) checkLottoResult.six.count += 1;
+    });
+
+    printLottoResult(checkLottoResult, userBuyMoney);
+}
+
+export default playLottoGame;
