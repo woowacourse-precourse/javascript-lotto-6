b875fb982b891b0f81e6fda4df9c429ebe99a457
simple0710 <thesimpleman@naver.com>
2023-11-06 23:41:30 +0900

    chore: LottoController 코드 수정
    내용:
    - 생성자 및 메서드 수정
    - 사용자 입력 관련 메서드 수정
    - 출력 관련 메서드 수정
    - 구매 비용 관련 메서드 수정
    - 수익 관련 메서드 수정
    - 로또 일치 결과 관련 메서드 수정

--- src/Controller/LottoController.js
+++ src/Controller/LottoController.js
@@ -6,60 +6,62 @@ import OutputView from "../../View/OutputView.js";
 import { NUMBER } from "../../utils/Constant.js";
 
 class LottoController {
-  #lottoCost;
-  #pickLotto;
-  #bonusLotto;
-  #matchResult;
+  #userLottoCost;
+  #userPickLottoNumbers;
+  #userPickBonusLottoNumber;
+  #lottoMatchResult;
   #incomeResult;
-  constructor() {
-    this.#initialize();
+  constructor() {}
+
+  async play() {
+    await this.getPurchaseCost();
+    await this.getUserPickLottoNumbers();
+    await this.getUserPickBonusLottoNumber();
+    await this.getLottoMatchResultData();
+    await this.getIncomeResult();
+    await this.printLottoNumbersMatchCount();
   }
   
-  async #initialize() {
-    await this.#getDefaultInput();
-    this.printLottoNumbersMatchCount();
+  async getPurchaseCost() {
+    this.#userLottoCost = new PurchaseCost(await InputView.inputPurchaseCost());
+    await this.printPurChaseResult();
+  }
+  
+  async printPurChaseResult() {
+    await OutputView.outputPurchaseLottoCount(parseInt(await this.#userLottoCost.getPurchaseCost() / 1000));
+    await OutputView.outputRandomLottoNumbersList(await this.#userLottoCost.getRandomLottoNumbersList());
+  }
+
+  async getUserPickLottoNumbers() {
+    this.#userPickLottoNumbers = new Lotto(await InputView.inputLotto());
+  }
+
+  async getUserPickBonusLottoNumber() {
+    this.#userPickBonusLottoNumber = new BonusLotto(await InputView.inputBonusLotto(), this.#userPickLottoNumbers.getLottoNumbers());
   }
 
   async printLottoNumbersMatchCount() {
-    await this.getLottoMatchResultyData();
-    await this.getIncomeResult();
-    OutputView.outputLottoMatchResult(this.#matchResult, this.#incomeResult);
+    await OutputView.outputLottoMatchResult(this.#lottoMatchResult, this.#incomeResult);
   }
 
-  async getIncomeResult() {
-    this.#incomeResult = this.#incomeResult / await this.#lottoCost.getPurchaseCost() * 100 - 100;
-    if (this.#incomeResult < 0) this.#incomeResult += 100;
-  }
-
-  async #getDefaultInput() {
-    await this.getPurchaseCost();
-    this.#pickLotto = new Lotto(await InputView.inputLotto());
-    this.#bonusLotto = new BonusLotto(await InputView.inputBonusLotto(), this.#pickLotto.getLottoNumbers());
-  }
-
-  async getPurchaseCost() {
-    this.#lottoCost = new PurchaseCost(await InputView.inputPurchaseCost());
-    OutputView.outputRandomLottoNumbersList(await this.#lottoCost.getRandomLottoNumbersList());
-  }
-
-  async getLottoMatchResult() {
-    const matchCount = await this.#pickLotto.getLottoMathCount(await this.#lottoCost.getRandomLottoNumbersList());
-    const mathBonusCount = await this.#bonusLotto.getLottoMatchBonusCount(await this.#lottoCost.getRandomLottoNumbersList());
-    return matchCount.map((numberMatchCount, index) => [numberMatchCount, mathBonusCount[index]]);
-  }
-  
-  async getLottoMatchResultyData() {
-    this.#matchResult = {};
+  async getLottoMatchResultData() {
+    this.#lottoMatchResult = {};
     this.#incomeResult = 0;
     (await this.getLottoMatchResult()).forEach(matchData => {
       const [numberMatchCount, bonusCount] = matchData;
-      if (this.#matchResult[numberMatchCount] === undefined) {
-        this.#matchResult[numberMatchCount] = [0, 0];
+      if (this.#lottoMatchResult[numberMatchCount] === undefined) {
+        this.#lottoMatchResult[numberMatchCount] = [0, 0];
       };
-      this.#matchResult[numberMatchCount][0] += 1
-      if (bonusCount) this.#matchResult[numberMatchCount][1] += 1;
+      this.#lottoMatchResult[numberMatchCount][0] += 1
+      if (bonusCount) this.#lottoMatchResult[numberMatchCount][1] += 1;
       this.checkIncomeResult(numberMatchCount, bonusCount);
     });
+  }
+  
+  async getLottoMatchResult() {
+    const matchCount = await this.#userPickLottoNumbers.getLottoMathCount(await this.#userLottoCost.getRandomLottoNumbersList());
+    const mathBonusCount = await this.#userPickBonusLottoNumber.getLottoMatchBonusCount(await this.#userLottoCost.getRandomLottoNumbersList());
+    return matchCount.map((numberMatchCount, index) => [numberMatchCount, mathBonusCount[index]]);
   }
 
   async checkIncomeResult(numberMatchCount, bonusCount) {
@@ -69,6 +71,12 @@ class LottoController {
     };
     if (price !== undefined) this.#incomeResult += price;
   }
+
+  async getIncomeResult() {
+    this.#incomeResult = this.#incomeResult / await this.#userLottoCost.getPurchaseCost() * 100 - 100;
+    if (this.#incomeResult < 0) this.#incomeResult += 100;
+  }
+
 }
 
 export default LottoController;
