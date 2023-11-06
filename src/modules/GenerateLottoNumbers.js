a10b33ce8235c2a6ed906336e1ad725f4f9e3781
hs <tlsgustn93@naver.com>
2023-11-06 17:03:03 +0900

    feat: 로또 번호 생성 로직

---
+++ src/modules/GenerateLottoNumbers.js
@@ -0,0 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class GenerateLottoNumbers {
+  makeNumber() {
+    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return numbers;
+  }
+
+  lottoNumberInfo(lottoCost) {
+    const lottoArr = [];
+    const lottoCount = lottoCost / 1000;
+    for (let i = 0; i < lottoCount; i++) lottoArr.push(this.makeNumber());
+    return [lottoCount, lottoArr];
+  }
+}
+
+export default GenerateLottoNumbers;
