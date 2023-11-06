2643542a47a4876730bc8410daf5ab1691bfefc1
yu-heejin <jkgt0521@naver.com>
2023-11-07 01:44:01 +0900

    feat: 구입 금액에 따른 로또 번호 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
     const purchaseAmount = await Console.readLineAsync('구입 금액을 입력해 주세요.\n');
     const purchaseCount = purchaseAmount / 1000;
+
+    Console.print(`${purchaseCount}개를 구매했습니다.\n`);
+    const lottoRandomNumbers = [];
+    for (let i = 0; i < purchaseCount; i++) {
+      const randomNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottoRandomNumbers.push(randomNumber);
+    }
   }
 }
 
