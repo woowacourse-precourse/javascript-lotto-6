91ff90a51999ece5f114f1cf2f19991757cabb47
LEEHYEYEONG <elto789@naver.com>
2023-11-06 17:29:09 +0900

    feat: 로또 중복 검사 체크 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+import { Console, Random } from "@woowacourse/mission-utils";
 class Lotto {
   #numbers;
 
@@ -6,9 +7,15 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  getValidate() {
+    return this.#validate;
+  }
+
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    } else if (numbers.length !== new Set(numbers).size) {
+      throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
     }
   }
 
