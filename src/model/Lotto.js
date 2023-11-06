6def716f2d4d74da0c08ba585c43f3058969c4c8
jonique98 <tnals9899@naver.com>
2023-11-06 22:32:14 +0900

    feat: getLottoNumber 메소드 작성 getLottoNumber() numbers를 반환하는 메소드 작성

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -10,6 +10,10 @@ class Lotto {
     }
   }
 
+  getLottoNumber() {
+    return this.#numbers;
+  }
+
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
