752275da860bfe28e643b32d3cc24145c91d448d
sihyun10 <hyhy200170@gmail.com>
2023-11-06 18:30:20 +0900

    style: 사용자에게 입력받고, 출력하는 메시지 상수로 정의 constants 폴더에 messages.js파일을 생성하여 사용자에게 입력받는 부분과 출력해주는 부분을 상수로 정의해두었습니다.

---
+++ constants/messages.js
@@ -0,0 +1,9 @@
+const input = Object.freeze({
+  purchaseAmount: '구입금액을 입력해주세요.\n',
+});
+
+const output = Object.freeze({
+  lottoPurchased: '개를 구매했습니다.',
+});
+
+export const MESSAGES = Object.freeze({ input, output });
--- src/User.js
+++ src/User.js
@@ -1,19 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
 import LottoPurchase from '../validates/lottoPurchase.js';
+import { MESSAGES } from '../constants/messages.js';
 
 class User {
   async calculateLottoCount() {
     while (true) {
       try {
-        const inputAmount =
-          await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+        const inputAmount = await Console.readLineAsync(
+          MESSAGES.input.purchaseAmount,
+        );
         const amount = parseInt(inputAmount);
 
         LottoPurchase.validateFormat(amount);
         LottoPurchase.validateMinimumAmount(amount);
 
         const lottoCount = Math.floor(amount / 1000);
-        Console.print(`\n${lottoCount}개를 구매했습니다.`);
+        Console.print(`\n${lottoCount}${MESSAGES.output.lottoPurchased}`);
         return lottoCount;
       } catch (error) {
         Console.print(error.message);
