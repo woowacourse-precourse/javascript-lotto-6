e40a20b700658ffef76f069d375f0b4339e09991
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-11-07 02:41:15 +0900

    :lipstick: style : 코드 포맷팅 누락을 해결합니다.

--- src/User.js
+++ src/User.js
@@ -1,31 +1,31 @@
-import {numberCheck} from "./validation.js";
-import {LOTTO_NUMBER, TRY_COST} from "./constants/policy.js";
-import {MESSAGE} from "./constants/messages.js";
+import { numberCheck } from './validation.js';
+import { TRY_COST } from './constants/policy.js';
+import { MESSAGE } from './constants/messages.js';
 
 class User {
-    constructor() {
-        this.money = 0;
-        this.count = 0;
-    }
+  constructor() {
+    this.money = 0;
+    this.count = 0;
+  }
 
-    #validation(number) {
-        numberCheck.number(number, MESSAGE.ERROR.number)
-        numberCheck.unit(number, TRY_COST, MESSAGE.ERROR.unit)
-    }
+  #validation(number) {
+    numberCheck.number(number, MESSAGE.ERROR.number);
+    numberCheck.unit(number, TRY_COST, MESSAGE.ERROR.unit);
+  }
 
-    getCount() {
-        return this.count
-    }
+  getCount() {
+    return this.count;
+  }
 
-    getMoney() {
-        return this.money
-    }
+  getMoney() {
+    return this.money;
+  }
 
-    setUserMoney(money) {
-        this.#validation(money)
-        this.money = money
-        this.count = money / TRY_COST
-    }
+  setUserMoney(money) {
+    this.#validation(money);
+    this.money = money;
+    this.count = money / TRY_COST;
+  }
 }
 
-export default User
+export default User;
