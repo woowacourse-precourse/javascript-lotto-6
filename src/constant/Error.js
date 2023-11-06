e18b0ef6008258bcd4c9c93b20dfc62b3bf68826
pakxe <pigkill40@naver.com>
2023-11-06 16:06:49 +0900

    feat: 중복 로또 번호와 보너스 번호 중복에 대한 오류 메세지 추가

--- src/constant/Error.js
+++ src/constant/Error.js
@@ -7,6 +7,8 @@ const ERROR = Object.freeze({
   price: `금액은 ${LOTTO.price}원 단위로 입력해주세요.`,
   range: `번호는 ${LOTTO.minLottoNum} ~ ${LOTTO.maxLottoNum} 사이의 값으로 입력해주세요.`,
   count: `당첨 번호는 ${LOTTO.count}개로 입력해주세요.`,
+  duplicated: '로또 번호는 중복이 없도록 입력해주세요.',
+  bonusNumber: '보너스 번호가 중복되지 않도록 입력해주세요.',
 });
 
 export default ERROR;
