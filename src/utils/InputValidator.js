e8db9d6cbe30c38359dec6d92b4b8e67737427ea
eunsung <esk114477@gmail.com>
2023-11-06 09:37:36 +0900

    feat: 구입 금액 예외 처리

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -11,12 +11,24 @@ export default class InputValidator {
   }
 
   static validateNumbers(numbers) {
+    numbers.forEach(number => {
+      this.validateNumber(number);
+    });
     const setNumber = new Set(numbers);
     if (numbers.length !== 6) {
       throw new Error(ERROR_MESSAGES.ONLY_SIX_NUMBERS);
     }
-    if (setNumber.size != 6) {
+    if (setNumber.size !== 6) {
       throw new Error(ERROR_MESSAGES.DUPLICATE_NUMBER);
+    }
+  }
+
+  static validatePurchaseMoney(money) {
+    if (parseInt(money, 10) !== Number(money)) {
+      throw new Error(ERROR_MESSAGES.ONLY_NUMBERS);
+    }
+    if (money % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGES.PURCHASE_AMOUNT_THOUSANDS_ONLY);
     }
   }
 }
