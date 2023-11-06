29ee75455a74704bae8d2aad96772b047265cbaf
hyzzzzy <shinhj4068@naver.com>
2023-11-06 23:33:43 +0900

    feat(LottoShop): 당첨번호와 보너스 번호 접근자, 생성자 정의

--- src/LottoShop.js
+++ src/LottoShop.js
@@ -1,7 +1,45 @@
 import Lotto from './Lotto';
-import { generateLottoNumber } from './utils';
+import { 
+  generateLottoNumber, 
+  hasSixValues, 
+  isLottoNumberRange, 
+  isNaturalNumber, 
+  isNumber, 
+  isUnique, 
+  isUniqueBonus 
+} from './utils';
 
 class LottoShop {
+  #prizeNumbers;
+  #bonusNumber;
+
+  set prizeNumber(prize) {
+    hasSixValues(prize);
+    prize.forEach((num) => {
+      isNumber(num);
+      isNaturalNumber(num);
+      isLottoNumberRange(num);
+    });
+    isUnique(prize);
+    this.#prizeNumbers = prize.map(Number);
+  }
+
+  set bonusNumber(bonus) {
+    isNumber(bonus);
+    isNaturalNumber(bonus);
+    isLottoNumberRange(bonus);
+    isUniqueBonus(bonus);
+    this.#bonusNumber = Number(bonus);
+  }
+
+  get prizeNumber() {
+    return this.#prizeNumbers;
+  }
+
+  get bonusNumber() {
+    return this.#bonusNumber;
+  }
+
   createLotto(tickets) {
     const lottos = [];
     while (lottos.length < tickets) {
