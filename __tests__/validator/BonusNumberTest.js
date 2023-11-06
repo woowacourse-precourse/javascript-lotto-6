386dc8cd6c7869def54e6ff4320b1d0124466c68
chiman <rlaclghks123@naver.com>
2023-11-06 19:45:10 +0900

    test: 보너스 번호 테스트 수정
    test.each를 통해 코드 리팩토링

--- __tests__/validator/BonusNumberTest.js
+++ __tests__/validator/BonusNumberTest.js
@@ -1,76 +1,65 @@
 import bonusNumberValidator from '../../src/validator/bonusNumberValidator.js';
+import commonValidator from '../../src/validator/commonValidator.js';
 
 describe('보너스 번호 테스트', () => {
-  const winningNumbers = '1,2,3,4,5,6';
+  const BONUS_NUMBER_PRIFIX_MESSAGE = '보너스번호';
 
-  describe('보너스 번호가 숫자가 아닌 경우 테스트', () => {
-    test('올바른 보너스번호 테스트', () => {
+  describe('commonValidator.checkNumberType 숫자형태 테스트', () => {
+    test.each([['40'], ['45'], ['7']])('올바른 숫자형태 보너스 번호 테스트', (input) => {
       expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '40', winningNumbers });
+        commonValidator.checkNumberType(BONUS_NUMBER_PRIFIX_MESSAGE, input);
       }).not.toThrow();
     });
 
-    test('보너스 번호가 빈값인 경우', () => {
+    test.each([[''], ['        '], ['6j'], ['6 '], [' 12'], [' 45 '], ['-45'], ['+45']])(
+      '잘못된 숫자형태 보너스번호 테스트',
+      (input) => {
+        expect(() => {
+          commonValidator.checkNumberType(BONUS_NUMBER_PRIFIX_MESSAGE, input);
+        }).toThrow();
+      }
+    );
+  });
+
+  describe('commonValidator.checkLottoNumberRange 범위 테스트', () => {
+    const MAX = 45;
+    const MIN = 1;
+
+    test.each([['40'], ['45'], ['7']])('올바른 보너스 번호 범위 테스트', (input) => {
       expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '', winningNumbers });
-      }).toThrow();
+        commonValidator.checkLottoNumberRange(BONUS_NUMBER_PRIFIX_MESSAGE, input);
+      }).not.toThrow();
     });
 
-    test('보너스 번호에 문자가 포함된 경우', () => {
+    test.each([['0'], [MIN - 1], [MAX + 1]])('잘못된 보너스번호 범위 테스트', (input) => {
       expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '4j', winningNumbers });
-      }).toThrow();
-    });
-
-    test('보너스 번호에 문자가 포함된 경우', () => {
-      expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '4십', winningNumbers });
-      }).toThrow();
-    });
-
-    test('보너스 번호에 공백이 포함된 경우', () => {
-      expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: ' 6 ', winningNumbers });
-      }).toThrow();
-    });
-
-    test('보너스 번호에 음수가 포함된 경우', () => {
-      expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '-6', winningNumbers });
+        commonValidator.checkLottoNumberRange(BONUS_NUMBER_PRIFIX_MESSAGE, input);
       }).toThrow();
     });
   });
 
-  describe('보너스 번호 범위 테스트', () => {
-    test('보너스 번호가 올바른 범위안에 있는 경우 테스트', () => {
+  describe('bonusNumberValidator.checkDuplicate 보너스 번호가 당첨 번호와 중복된 경우 테스트', () => {
+    const winningNumbers = '10,13,25,32,42,45';
+    test.each([
+      ['40', winningNumbers],
+      ['43', winningNumbers],
+      ['7', winningNumbers],
+    ])('중복되지 않은 올바른 보너스 번호 테스트', (bonusNumber, winnigNumbers) => {
       expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '40', winningNumbers });
+        bonusNumberValidator.checkDuplicate(bonusNumber, winnigNumbers);
       }).not.toThrow();
     });
 
-    test('보너스 번호가 범위밖인 0인 경우 테스트', () => {
+    test.each([
+      ['10', winningNumbers],
+      ['13', winningNumbers],
+      ['25', winningNumbers],
+      ['32', winningNumbers],
+      ['42', winningNumbers],
+      ['45', winningNumbers],
+    ])('잘못된 보너스번호 테스트', (bonusNumber, winnigNumbers) => {
       expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '0', winningNumbers });
-      }).toThrow();
-    });
-
-    test('보너스 번호가 범위밖인 99인 경우 테스트', () => {
-      expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '99', winningNumbers });
-      }).toThrow();
-    });
-  });
-
-  describe('보너스 번호가 당첨번호와 중복 경우 테스트', () => {
-    test('보너스 번호가 당첨 번호와 중복되지 않은 경우 테스트', () => {
-      expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '40', winningNumbers });
-      }).not.toThrow();
-    });
-
-    test('보너스 번호가 당첨 번호와 중복될 경우 테스트', () => {
-      expect(() => {
-        bonusNumberValidator.checkBonusNumber({ bonusNumber: '5', winningNumbers });
+        bonusNumberValidator.checkDuplicate(bonusNumber, winnigNumbers);
       }).toThrow();
     });
   });
