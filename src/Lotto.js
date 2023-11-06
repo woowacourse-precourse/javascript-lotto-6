1c53dbe81eee9211d54d9c49b1a0b2710afad46b
chysis <chysiss@naver.com>
2023-11-06 23:44:01 +0900

    fix(Lotto): 수정된 Messages 객체에 맞게 변수 이름 변경

--- src/Lotto.js
+++ src/Lotto.js
@@ -19,7 +19,7 @@ class Lotto {
     }
     for (let number of numbers) {
       if (number < 1 || number > 45) {
-        throw new Error(Messages.ERROR_WINNING_NUMBER_OUT_OF_RANGE);
+        throw new Error(Messages.ERROR_NUMBER_OUT_OF_RANGE);
       }
     }
   }
