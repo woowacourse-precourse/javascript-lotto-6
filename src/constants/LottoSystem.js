c6bc624906c2cdcc7696f160d575ef746ba71b1e
Lee sang Yeop <mma7710@naver.com>
2023-11-06 18:47:11 +0900

    refactor(constants): 당첨 갯수 및 상금에 대한 타입 변경 최대한 자동화를 위해 당첨 갯수 및 보너스 여부가 필요한지와 상금에 대한 데이터를 저장하고 모든 데이터를 관리하는 배열 추가

--- src/constants/LottoSystem.js
+++ src/constants/LottoSystem.js
@@ -1,3 +1,15 @@
+const first_winning = { count: 6, hasBonus: false };
+const second_winning = { count: 5, hasBonus: true };
+const third_winning = { count: 5, hasBonus: false };
+const fourth_winning = { count: 4, hasBonus: false };
+const fifth_winning = { count: 3, hasBonus: false };
+
+const first_place_prize = 2000000000;
+const second_place_prize = 30000000;
+const third_place_prize = 1500000;
+const fourth_place_prize = 50000;
+const fifth_place_prize = 5000;
+
 const LOTTO_SYSTEM = Object.freeze({
 	lotto_price: 1000,
 
@@ -5,17 +17,15 @@ const LOTTO_SYSTEM = Object.freeze({
 	maximum_number: 45,
 	lotto_numbers: 6,
 
-	first_winning: [6, false],
-	second_winning: [5, true],
-	third_winning: [5, false],
-	fourth_winning: [4, false],
-	fifth_winning: [3, false],
+	winning_array: [fifth_winning, fourth_winning, third_winning, second_winning, first_winning],
 
-	first_place_prize: 2000000000,
-	second_place_prize: 30000000,
-	third_place_prize: 1500000,
-	fourth_place_prize: 50000,
-	fifth_place_prize: 5000,
+	prize_array: [
+		fifth_place_prize,
+		fourth_place_prize,
+		third_place_prize,
+		second_place_prize,
+		first_place_prize,
+	],
 });
 
 export default LOTTO_SYSTEM;
