bcfdc16dde9369167ebdff33427ec82f98e1cdf0
8leamnk <silkviewrk@gmail.com>
2023-11-06 17:05:04 +0900

    refactor(Random, App): 로또 발행 기능을 리팩토링함
    변경 내용:
    - UI를 담당하는 로직을 App에만 담당시키기 위해 Random 클래스를 리팩토링을 함
    - 변경된 Random 클래스를 App에 반영
    - 로또 발행 출력 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 import Price from './Price.js';
+import Random from './Random.js';
 import inputs from './View/inputs.js';
 import outputs from './View/outputs.js';
 
 class App {
   async play() {
     const { price, number } = await this.#executePrice();
+    const random = this.#executeRandom(number);
   }
 
   async #executePrice() {
@@ -21,6 +23,15 @@ class App {
       Console.print(error.message);
       return this.#executePrice();
     }
+  }
+
+  #executeRandom(number) {
+    const randomObject = new Random(number);
+    const { random, randomString } = randomObject.getRandomAndRandomString();
+
+    outputs.printRandoms(randomString);
+
+    return random;
   }
 }
 
--- src/Random.js
+++ src/Random.js
@@ -1,27 +1,17 @@
 import randomModel from './models/randomModel.js';
-import outputs from './View/outputs.js';
 
 class Random {
   #random;
 
-  constructor() {
-    this.#random = [];
-  }
+  #randomString;
 
-  #setRandom(number) {
+  constructor(number) {
     this.#random = randomModel.getRandom(number);
+    this.#randomString = randomModel.getStringOfRandom(this.#random);
   }
 
-  controlRandom(number) {
-    this.#setRandom(number);
-
-    const randomString = randomModel.getStringOfRandom(this.#random);
-
-    outputs.printRandoms(randomString);
-  }
-
-  getRandom() {
-    return this.#random;
+  getRandomAndRandomString() {
+    return { random: this.#random, randomString: this.#randomString };
   }
 }
 
