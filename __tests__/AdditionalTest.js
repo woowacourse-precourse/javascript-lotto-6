9b0e74f17bdb1ba108b14eb8447e4bdf70688c68
brgndy <brgndy96@gmail.com>
2023-11-07 08:36:02 +0900

    test: 추가적인 테스트코드 작성

---
+++ __tests__/AdditionalTest.js
@@ -0,0 +1,69 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import App from '../src/App.js';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('로또 테스트', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('기능 테스트', async () => {
+    // given
+    const logSpy = getLogSpy();
+
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 2, 3, 4, 7, 42],
+      [13, 14, 16, 38, 42, 45],
+    ]);
+    mockQuestions(['5000', '1,2,3,4,7,6', '42']);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    const logs = [
+      '5개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+      '[1, 2, 3, 4, 7, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '3개 일치 (5,000원) - 0개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 1개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 600000.0%입니다.',
+    ];
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+});
