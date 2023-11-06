58331cdc13425497b551f5e0eed5868736c1048f
chiman <rlaclghks123@naver.com>
2023-11-06 19:45:28 +0900

    refactor: commonValidator prefix 기본값 설정

--- src/validator/commonValidator.js
+++ src/validator/commonValidator.js
@@ -1,17 +1,17 @@
 const commonValidator = {
-  checkNumberType(prefixMessage, input) {
+  checkNumberType(prefixMessage = '', input) {
     if (!/^[0-9]+$/.test(input)) {
       throw new Error(`${prefixMessage} 숫자 형태로 입력해주세요`);
     }
   },
 
-  checkDuplicate(prefixMessage, input) {
+  checkDuplicate(prefixMessage = '', input) {
     if (new Set(input).size !== input.length) {
       throw new Error(`${prefixMessage} 번호가 중복되면 안됩니다.`);
     }
   },
 
-  checkLottoNumberRange(prefixMessage, input) {
+  checkLottoNumberRange(prefixMessage = '', input) {
     if (Number(input) < 1 || Number(input) > 45) {
       throw new Error(`${prefixMessage} 1~45의 숫자를 입력해주세요`);
     }
