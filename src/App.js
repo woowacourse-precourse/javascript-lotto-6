990eaa062205aecab1de3f80c2897c8c325d53b0
8leamnk <silkviewrk@gmail.com>
2023-11-06 14:32:05 +0900

    feat(App): 로또 게임을 총체적으로 관리하는 기능을 추가함
    추가 내용:
    - 가격과 구매 개수를 구하는 컨트롤러
    - 발행한 로또 정보를 구하는 컨트롤러
    - 사용자가 입력한 당첨 번호를 가져오는 컨트롤러
    - 사용자가 입력한 보너스 번호를 가져오는 컨트롤러
    - 당첨 금액을 가져오는 컨트롤러
    - 수익률을 구하는 컨트롤러

--- src/App.js
+++ src/App.js
@@ -1,5 +1,93 @@
+import Price from './Price.js';
+import Random from './Random.js';
+import Lotto from './Lotto.js';
+import Bonus from './Bonus.js';
+import Stats from './Stats.js';
+import Rate from './Rate.js';
+
 class App {
-  async play() {}
+  #price;
+
+  #number;
+
+  #random;
+
+  #lotto;
+
+  #bonus;
+
+  #revenues;
+
+  constructor() {
+    this.#price = 0;
+    this.#number = 0;
+    this.#random = [];
+    this.#bonus = 0;
+    this.#revenues = [];
+  }
+
+  async play() {
+    await this.#executePrice();
+
+    this.#executeRandom();
+
+    await this.#executeLotto();
+
+    await this.#executeBonus();
+
+    this.#executeStats();
+
+    this.#executeRate();
+  }
+
+  async #executePrice() {
+    const priceObject = new Price();
+
+    await priceObject.controlPrice();
+
+    const [price, number] = priceObject.getPriceAndNumber();
+
+    this.#price = price;
+    this.#number = number;
+  }
+
+  #executeRandom() {
+    const randomObject = new Random();
+
+    randomObject.controlRandom(this.#number);
+
+    this.#random = randomObject.getRandom();
+  }
+
+  async #executeLotto() {
+    const lottoObject = new Lotto();
+
+    await lottoObject.controlLotto();
+
+    this.#lotto = lottoObject.getLotto();
+  }
+
+  async #executeBonus() {
+    const bonusObject = new Bonus();
+
+    await bonusObject.controlBonus(this.#lotto);
+
+    this.#bonus = bonusObject.getBonus();
+  }
+
+  #executeStats() {
+    const statsObject = new Stats();
+
+    statsObject.controlCount(this.#random, this.#lotto, this.#bonus);
+
+    this.#revenues = statsObject.getRevenues();
+  }
+
+  #executeRate() {
+    const rateObject = new Rate();
+
+    rateObject.controlRate(this.#revenues, this.#price);
+  }
 }
 
 export default App;
