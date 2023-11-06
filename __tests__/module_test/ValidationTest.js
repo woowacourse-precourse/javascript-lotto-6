010bd593a4c14d4e6980e2e58739e7c58025f54d
고세종 <fulfilled7418@gmail.com>
2023-11-07 02:33:39 +0900

    fix: 수정 유효성 검사 의존성 변경에 대한 테스트 변경

--- __tests__/module_test/ValidationTest.js
+++ __tests__/module_test/ValidationTest.js
@@ -1,4 +1,6 @@
-import Validator from '../../Controller/modules/Validator.js';
+import BonusNumber from '../../Models/BonusNumber';
+import PurchaseMoney from '../../Models/PurchaseMoney';
+import Lotto from '../../src/Lotto';
 
 describe('사용자 입력 유효성 검사', () => {
   const insertedMoneyTestInput = ['1500', '1000k', 'abcd', '', ' '];
@@ -14,17 +16,15 @@ describe('사용자 입력 유효성 검사', () => {
   test.each(insertedMoneyTestInput)(
     '입력된 구입 금액 유효성 테스트.',
     (input) => {
-      expect(() => Validator.checkInsertedMoneyIsValid(input)).toThrow(
-        '[ERROR]'
-      );
+      expect(() => new PurchaseMoney(input)).toThrow('[ERROR]');
     }
   );
 
   test.each(lottoNumberInput)('입력된 당첨 번호 유효성 테스트.', (input) => {
-    expect(() => Validator.checkLottoNumberIsValid(input)).toThrow('[ERROR]');
+    expect(() => new Lotto(input)).toThrow('[ERROR]');
   });
 
   test.each(bonusNumberInput)('입력된 보너스 번호 유효성 테스트.', (input) => {
-    expect(() => Validator.checkBonusNumberIsValid(input)).toThrow('[ERROR]');
+    expect(() => new BonusNumber(input)).toThrow('[ERROR]');
   });
 });
