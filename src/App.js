b12118db7b127e1afe8a052607713db2d1514cbc
rlarlduf20 <kky8274@naver.com>
2023-11-06 22:40:25 +0900

    Refactor: 코드 가독성 향상

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import { getLottoCntFromInputMoney } from "./utils/getUserInput.js";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { getLottoCntFromInputMoney } from './utils/getUserInput.js';
 
 class App {
   async play() {
-    const inputMoney = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    const inputMoney = await MissionUtils.Console.readLineAsync(
+      '구입금액을 입력해 주세요.\n'
+    );
     const lottoCnt = getLottoCntFromInputMoney(inputMoney);
   }
 }
--- src/utils/getUserInput.js
+++ src/utils/getUserInput.js
@@ -1,26 +1,28 @@
 export const checkValueIsNumber = (value) => {
-  if(isNaN(Number(value))) {
+  if (isNaN(Number(value))) {
     return false;
   }
   return true;
-}
+};
 
 export const checkValueIsInteger = (value) => {
-  if(Number.isInteger(value)) {
+  if (Number.isInteger(value)) {
     return true;
   }
   return false;
-}
+};
 
 export const getLottoCntFromInputMoney = (inputMoney) => {
-  if(!checkValueIsNumber(inputMoney)) {
-      throw new Error('[ERROR] 구입금액은 숫자만 입력 가능합니다.');
+  if (!checkValueIsNumber(inputMoney)) {
+    throw new Error('[ERROR] 구입금액은 숫자만 입력 가능합니다.');
   }
 
-	const lottoCnt = Number(inputMoney) / 1000;
-  if(!checkValueIsInteger(lottoCnt)) {
-		throw new Error('[ERROR] 잔돈 반환이 불가능합니다. 로또 수의 딱 맞는 금액을 입력해주세요.');
-	}
+  const lottoCnt = Number(inputMoney) / 1000;
+  if (!checkValueIsInteger(lottoCnt)) {
+    throw new Error(
+      '[ERROR] 잔돈 반환이 불가능합니다. 로또 수의 딱 맞는 금액을 입력해주세요.'
+    );
+  }
 
   return lottoCnt;
-}
+};
