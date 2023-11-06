d54f25393a2f8e05bcd9f9af093a181d15817603
myeongin <mungin0926@naver.com>
2023-11-06 15:30:21 +0900

    [test] 로또 구매 금액이 1000원으로 안 나누어질 경우 테스트

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -4,4 +4,8 @@ describe('유저 입력 유효성 검사', () => {
   test.each([[''], ['  ']])('유저 입력 빈 값 테스트', (input) => {
     expect(() => Validator.validateEmptyInput(input)).toThrow();
   });
+
+  test.each([[2500], [1200], [120003]])('로또 구매 금액이 1000원으로 안 나누어질 경우 테스트', (input) => {
+    expect(() => Validator.validateDivisible(input)).toThrow();
+  });
 });
