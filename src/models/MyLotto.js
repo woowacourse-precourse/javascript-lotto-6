627270087ccca779fb8f5fdb68d1d4946a509621
aaminha <dks4857@gmail.com>
2023-11-07 05:52:01 +0900

    feat: MyLotto 클래스 구현

---
+++ src/models/MyLotto.js
@@ -0,0 +1,42 @@
+import Lotto from './Lotto';
+
+class MyLotto {
+  #purchaseAmount;
+
+  #lottoNumbersList = [];
+
+  constructor(purchaseLottoAmount, lottoCount) {
+    this.#purchaseAmount = purchaseLottoAmount;
+    this.#createLottoNumbersList(lottoCount);
+  }
+
+  #createLottoNumbersList(lottoCount) {
+    Array.from({ length: lottoCount }).forEach(() => {
+      this.#lottoNumbersList.push(new Lotto());
+    });
+  }
+
+  showMyLottoNumbers() {
+    this.#lottoNumbersList.forEach(lottoNumbers =>
+      lottoNumbers.printLottoNumbers(),
+    );
+  }
+
+  findMatching(winningNumbers, bonusNumber) {
+    const matchingResult = [];
+
+    this.#lottoNumbersList.forEach(numbers => {
+      const count = numbers.filter(num => winningNumbers.includes(num)).length;
+      const bonus = numbers.includes(bonusNumber);
+      matchingResult.push({ count, bonus });
+    });
+
+    return matchingResult;
+  }
+
+  getPurchaseAmount() {
+    return this.#purchaseAmount;
+  }
+}
+
+export default MyLotto;
