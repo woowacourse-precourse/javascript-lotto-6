f85508bef711ba6faee79a11f3bc373b7f9fd5ee
고세종 <fulfilled7418@gmail.com>
2023-11-07 02:30:46 +0900

    feat: 구현 PurchaseMoney 구입 금액 모델

---
+++ Models/PurchaseMoney.js
@@ -0,0 +1,26 @@
+import ValidationUtils from '../utils/ValidationUtils';
+
+const { checkIsNumber, checkCanDevideByThousand } = ValidationUtils;
+
+class PurchaseMoney {
+  #money;
+  constructor(money) {
+    this.#validatePurchaseMoney(money);
+    this.#money = money;
+  }
+
+  #validatePurchaseMoney(money) {
+    const moneyIsNumber = checkIsNumber(money);
+    if (!moneyIsNumber) throw new Error('[ERROR] 숫자를 입력해주세요.');
+
+    const moneyIsInThousands = checkCanDevideByThousand(money);
+    if (!moneyIsInThousands)
+      throw new Error('[ERROR] 천원단위로 입력해주세요.');
+  }
+
+  getInsertedMoney() {
+    return this.#money;
+  }
+}
+
+export default PurchaseMoney;
