f6050229687077b1e5c2a7dfce9da2f2e562fe98
Oh Dong Joon <ohdoju0905@gmail.com>
2023-11-06 22:07:02 +0900

    test(/__tests__/FunctionTest.js): inputMoney에 대한 test 코드 작성
    <body>
    - inputMoney에 정상적인 값을 입력했을 때 기대했던 값을 리턴하는지 확인
    - inputMoney에 비정상적인 값을 입력했을 때 [ERROR]로 시작하는 메세지를 출력 후 재입력받는지 확인
    
    <footer>

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,1 +1,35 @@
-describe('함수 기능 단위 테스트', () => {});
+import { Console } from '@woowacourse/mission-utils';
+import App from '../src/App';
+
+describe('함수 기능 단위 테스트', () => {
+	const ERROR = '[ERROR]';
+
+	describe('App', () => {
+		describe('inputMoney', () => {
+			test.each(['1000', '11000'])('정상적인 구입금액이 입력됐을 경우', async (input) => {
+				Console.readLineAsync = jest.fn().mockResolvedValueOnce(input);
+				const result = await App.inputMoney();
+				expect(result).toEqual(+input);
+			});
+
+			test.each([
+				{
+					invalid: '1010',
+					valid: '1000',
+				},
+				{
+					invalid: '-1',
+					valid: '1000',
+				},
+			])('비정상적인 구입금액이 입력됐을 경우', async ({ invalid, valid }) => {
+				Console.readLineAsync = jest.fn().mockResolvedValueOnce(invalid).mockResolvedValueOnce(valid);
+				Console.print = jest.fn();
+
+				const result = await App.inputMoney();
+				expect(Console.print).toHaveBeenCalledWith(expect.stringContaining(ERROR));
+				expect(result).toEqual(+valid);
+				expect(Console.readLineAsync).toHaveBeenCalledTimes(2);
+			});
+		});
+	});
+});
