734803c0c205808f8952d92a7b1a29506ec2309f
jonique98 <tnals9899@naver.com>
2023-11-06 22:44:02 +0900

    feat: 구매 금액의 유효성을 체크하는 메소드 구매 금액의 validate를 체크하는 메소드 작성 1. number 타입인지 체크 2. 100으로 나눠지는 지 체크

--- src/model/UserLotto.js
+++ src/model/UserLotto.js
@@ -1,18 +1,35 @@
 class UserLotto {
-	#purchaseAmount;
-	#numberOfPurchase;
-	#userLottoNumbers = [];
+  #purchaseAmount;
+  #numberOfPurchase;
+  #userLottoNumbers = [];
 
-	constructor(purchaseAmount) {
-		try {
-			this.#validate(purchaseAmount);
-			this.#setPurchaseVariable(purchaseAmount);
-		}catch (error) {
-			throw error;
-		}
+  constructor(purchaseAmount) {
+    try {
+      this.#validate(purchaseAmount);
+      this.#setPurchaseVariable(purchaseAmount);
+    } catch (error) {
+      throw error;
+    }
 
-		for (let i = 0; i < this.#numberOfPurchase; i++) {
-			this.#userLottoNumbers.push(new UserLottoNumber());
-		}
-	}
+    for (let i = 0; i < this.#numberOfPurchase; i++) {
+      this.#userLottoNumbers.push(new UserLottoNumber());
+    }
+  }
+
+  #validate(purchaseAmount) {
+    this.#typeCheck(purchaseAmount);
+    this.#divisionCheck(purchaseAmount);
+  }
+
+  #typeCheck(purchaseAmount) {
+    if (Number.isNaN(purchaseAmount)) {
+      throw new Error('[ERROR] 구입 금액은 숫자여야 합니다.');
+    }
+  }
+
+  #divisionCheck(purchaseAmount) {
+    if (purchaseAmount % 1000 !== 0) {
+      throw new Error('[ERROR] 구입 금액은 1000원 단위여야 합니다.');
+    }
+  }
 }
