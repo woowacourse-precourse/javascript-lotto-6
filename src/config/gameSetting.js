c9d1b7f7912cd11078d6d140e113236eac00eaf2
0jenn0 <jenn0.6n@gmail.com>
2023-11-06 14:10:19 +0900

    Feat: 로또 등수 이름 상수화 추가
    - 코드 내에 로또 등수 이름자체를 써야할 때가 있어서 이 때, 하드코딩하지 않고 오타로 인한 오류 발생 방지하기위해 이름도 상수화하여 타입처럼 강제함

--- src/config/gameSetting.js
+++ src/config/gameSetting.js
@@ -14,17 +14,22 @@ function deepFreeze(object) {
   return Object.freeze(object);
 }
 
-const LOTTO_SETTINGS = deepFreeze({
+export const LOTTO_SETTINGS = deepFreeze({
   NUMBER_RANGE: { MIN: 1, MAX: 45 },
   NUMBERS_PER_TICKET: 6,
   TICKET_PRICE: 1000,
+  PRIZE_RANKS: {
+    FIFTH_PRIZE: "FIFTH_PRIZE",
+    FOURTH_PRIZE: "FOURTH_PRIZE",
+    THIRD_PRIZE: "THIRD_PRIZE",
+    SECOND_PRIZE: "SECOND_PRIZE",
+    FIRST_PRIZE: "FIRST_PRIZE",
+  },
   WINNINGS: {
-    FIRST_PRIZE: { prize: 2000000000, matchNum: 6 },
-    SECOND_PRIZE: { prize: 30000000, matchNum: 5 },
-    THIRD_PRIZE: { prize: 1500000, matchNum: 5 },
-    FOURTH_PRIZE: { prize: 50000, matchNum: 4 },
     FIFTH_PRIZE: { prize: 5000, matchNum: 3 },
+    FOURTH_PRIZE: { prize: 50000, matchNum: 4 },
+    THIRD_PRIZE: { prize: 1500000, matchNum: 5 },
+    SECOND_PRIZE: { prize: 30000000, matchNum: 5 },
+    FIRST_PRIZE: { prize: 2000000000, matchNum: 6 },
   },
 });
-
-export default LOTTO_SETTINGS;
