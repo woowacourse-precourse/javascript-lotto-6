f1593bdbfa781f89990d204064132015a4df1cc9
BangDori <qudwns8865@naver.com>
2023-11-06 19:05:36 +0900

    test: 로또 번호 범위 테스트 코드 CommonTest로 이동

--- __tests__/validator/CommonTest.js
+++ __tests__/validator/CommonTest.js
@@ -1,29 +1,45 @@
 import CommonValidator from '../../src/validator/Common';
 
 describe('CommonTest 클래스 예외 테스트', () => {
-  test('문자가 입력되는 경우 예외가 발생한다.', () => {
-    const numbers = ['a', 'b', '{}', '[]'];
+  describe('문자 입력 테스트', () => {
+    test('문자가 입력되는 경우 예외가 발생한다.', () => {
+      const numbers = ['a', 'b', '{}', '[]'];
 
-    numbers.forEach(number => {
-      expect(() => CommonValidator.validateIsNumber(Number(number))).toThrow(
-        '[ERROR]',
-      );
+      numbers.forEach(number => {
+        expect(() => CommonValidator.validateIsNumber(Number(number))).toThrow(
+          '[ERROR]',
+        );
+      });
+    });
+
+    test('NaN이 입력되는 경우 예외가 발생한다.', () => {
+      const number = NaN;
+
+      expect(() => CommonValidator.validateIsNumber(number)).toThrow('[ERROR]');
+    });
+
+    test('특수 문자가 입력되는 경우 예외가 발생한다.', () => {
+      const numbers = ['!', '@', '.', '-'];
+
+      numbers.forEach(number => {
+        expect(() => CommonValidator.validateIsNumber(Number(number))).toThrow(
+          '[ERROR]',
+        );
+      });
     });
   });
 
-  test('NaN이 입력되는 경우 예외가 발생한다.', () => {
-    const number = NaN;
+  describe('로또 번호 범위 테스트', () => {
+    test('로또 번호가 0 이하라면 예외가 발생한다.', () => {
+      expect(() => {
+        CommonValidator.validateLottoNumberInRange(0);
+      }).toThrow('[ERROR]');
+    });
 
-    expect(() => CommonValidator.validateIsNumber(number)).toThrow('[ERROR]');
-  });
-
-  test('특수 문자가 입력되는 경우 예외가 발생한다.', () => {
-    const numbers = ['!', '@', '.', '-'];
-
-    numbers.forEach(number => {
-      expect(() => CommonValidator.validateIsNumber(Number(number))).toThrow(
-        '[ERROR]',
-      );
+    test('로또 번호가 46 이상이라면 예외가 발생한다.', () => {
+      expect(() => {
+        CommonValidator.validateLottoNumberInRange(46);
+      }).toThrow('[ERROR]');
     });
   });
 });
--- __tests__/validator/LottoTest.js
+++ __tests__/validator/LottoTest.js
@@ -13,37 +13,4 @@ describe('LottoValidator 클래스 예외 테스트', () => {
       LottoValidator.validateDuplication([1, 2, 3, 4, 5, 5]);
     }).toThrow('[ERROR]');
   });
-
-  // 아래에 추가 테스트 작성 가능
-  describe('로또 번호 범위 테스트', () => {
-    test('로또 번호에 0 이하의 숫자가 있으면 예외가 발생한다.', () => {
-      expect(() => {
-        LottoValidator.validateLottoNumbers([0, 1, 2, 3, 4, 5]);
-      }).toThrow('[ERROR]');
-    });
-
-    test('로또 번호에 46 이상의 숫자가 있으면 예외가 발생한다.', () => {
-      expect(() => {
-        LottoValidator.validateLottoNumbers([1, 2, 3, 4, 5, 47]);
-      }).toThrow('[ERROR]');
-    });
-  });
-
-  describe('보너스 번호 예외 테스트', () => {
-    test('보너스 번호가 0 이하라면 예외가 발생한다.', () => {
-      const number = 0;
-
-      expect(() => LottoValidator.validateLottoNumber(number)).toThrow(
-        '[ERROR]',
-      );
-    });
-
-    test('보너스 번호가 46 이상이라면 예외가 발생한다.', () => {
-      const number = 46;
-
-      expect(() => LottoValidator.validateLottoNumber(number)).toThrow(
-        '[ERROR]',
-      );
-    });
-  });
 });
