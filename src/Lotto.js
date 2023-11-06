c567359c618f2a4b762d4a2926033bd30aed5881
GimGayeon <kky1373@naver.com>
2023-11-06 11:32:50 +0900

    test:당첨 번호 문자일 경우 예외, 옳게 입력된경우 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,12 +9,12 @@ class Lotto {
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-    if (new Set(numbers).size !== 6) {
+    } else if (new Set(numbers).size !== 6) {
       throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
-    }
-    if (Number.isNaN(Number(numbers))) {
+    } else if (Number.isNaN(Number(numbers))) {
       throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    } else {
+      return true;
     }
   }
 
