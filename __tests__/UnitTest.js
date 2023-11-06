7293eb79e2d4e4f5424d8f09341a143e870da149
yoovin <soo22839@gmail.com>
2023-11-06 23:31:48 +0900

    test: 당첨 번호 개수 테스트 작성

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -10,4 +10,8 @@ describe('기능 테스트', () => {
     test('당첨 번호에 1부터 45사이의 숫자가 아니면 예외가 발생한다.', () => {
         expect(app.getWinNumbers('1,2,3,4,5,46')).rejects.toThrow('[ERROR]');
     });
+
+    test('당첨 번호가 6개가 아니면 예외가 발생한다.', () => {
+        expect(app.getWinNumbers('1,2,3,4,5')).rejects.toThrow('[ERROR]');
+    });
 });
