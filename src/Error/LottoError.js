5142763ac92b36bf3aafb2e1bc1adb508b7cd98f
0jenn0 <jenn0.6n@gmail.com>
2023-11-06 14:41:31 +0900

    Style: 메시지 스타일 수정

--- src/Error/LottoError.js
+++ src/Error/LottoError.js
@@ -2,7 +2,7 @@ export default class LottoError extends Error {
   static PREFIX = "[ERROR]";
 
   constructor(message) {
-    super(`\n${LottoError.PREFIX} : ${message}\n`);
+    super(`\n${LottoError.PREFIX} ${message}\n`);
     this.name = this.constructor.name;
   }
 }
