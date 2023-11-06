4d7b5f9e3f60fcb67e26446a92c5917a40f0b4c9
hong <ahi200@naver.com>
2023-11-06 23:18:12 +0900

    test: 로또 생성 함수 메소드 테스트코드

---
+++ __tests__/LottoMachineTest.js
@@ -0,0 +1,35 @@
+import LottoMachine from '../src/LottoMachine.js';
+import { Random } from '@woowacourse/mission-utils';
+
+const mockRandoms = (numbers) => {
+  Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickUniqueNumbersInRange);
+};
+
+describe('LottoMachine 클래스 테스트', () => {
+  test('8000원으로 8개의 로또가 발행되어야 한다.', () => {
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+    const lottoMachine = new LottoMachine();
+    expect(lottoMachine.issueLotto(8000)).toStrictEqual([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+  });
+});
