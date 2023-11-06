32676702079f381cab0d6957153ea9b33c801da4
ReturnReason <reasonz@daum.net>
2023-11-06 23:32:24 +0900

    feat(OutputView): 일치한 로또 결과를 출력하는 메서드 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,8 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
+import formatGamePrizes from '../utils/formatGamePrizes.js';
 
 class OutputView {
   print(message = '') {
-    return Console.print(message);
+    Console.print(message);
+  }
+
+  /**
+   * 당첨금을 받을 수 있는 로또의 오브젝트를 인자로 받아 템플릿 형식에 맞춰 출력한다.
+   * @param {{ three: number, four: number, five: number, bonus: number, six: number }} matchLottoCount -각 key는 일치하는 숫자의 개수를 의미한다.
+   */
+  printLottoResult(matchLottoCount) {
+    this.print(formatGamePrizes(matchLottoCount));
   }
 }
 
