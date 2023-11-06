8b1f375ae05515e39c6da735f83e0ec026d4e5ef
mieum12 <qpdlqltb1215@gmail.com>
2023-11-07 02:57:05 +0900

    feat: Lotto 내부 데이터를 사용해서 당첨 번호와 얼마나 맞는지, 보너스 번호가 포함됐는지 판단하는 로직 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,56 +1,69 @@
 import {LottoDto} from "./dto/LottoDto.js";
 
-export class Lotto {
-    /**
-     * 로또 가격이 인상되더라도 상수로 만들었기 때문에
-     * PRICE 한번만 바꾸면 된다!
-     *
-     * 1000이라는 리터럴 그대로 사용하지 않기~
-     *
-     * @type {number}
-     */
-    static PRICE = 1000;
-    /**
-     * @type {number[]}
-     */
-    #numbers;
+class Lotto {
+  /**
+   * 로또 가격이 인상되더라도 상수로 만들었기 때문에
+   * PRICE 한번만 바꾸면 된다!
+   *
+   * 1000이라는 리터럴 그대로 사용하지 않기~
+   *
+   * @type {number}
+   */
+  static PRICE = 1000;
+  /**
+   * @type {number[]}
+   */
+  #numbers;
 
-    /**
-     * @param {number[]} numbers
-     * @description - 구매한 로또 한장 + 로또 용지에 적힌 번호 저장
-     */
+  /**
+   * @param {number[]} numbers
+   * @description - 구매한 로또 한장 + 로또 용지에 적힌 번호 저장
+   */
 
-    constructor(numbers) {
-        this.#validate(numbers);
-        this.#numbers = numbers;
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
+
+  /**
+   *
+   * @param {number[]} numbers
+   */
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-
-    get numbers() {
-        return this.#numbers
+    const lottoNums = new Set(numbers);
+    if (lottoNums.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복 될 수 없습니다.");
     }
+  }
 
-    /**
-     *기
-     * @param {number[]} numbers
-     */
-    #validate(numbers) {
-        if (numbers.length !== 6) {
-            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-        const lottoNums = new Set(numbers)
-        if (lottoNums.size !== numbers.length) {
-            throw new Error("[ERROR] 로또 번호는 중복 될 수 없습니다.")
-        }
-    }
+  /**
+   * @param {number} bonusNumber
+   * @return {boolean}
+   * @description 보너스 번호를 갖고있는지 여부 판단
+   */
+  hasBonusNumber(bonusNumber) {
+    return this.#numbers.includes(bonusNumber);
+  }
 
-    /**
-     * @description - 추첨 기계에 의해 로또 한 개 추첨하기,
-     */
-    drawBy(lottoMachine) {
+  /**
+   * @param {number[]} winningNumbers
+   * @return {number}
+   * @description 당첨 번호랑 몇개나 맞는지 계산
+   */
+  countIncludingNumbers(winningNumbers) {
+    return this.#numbers.filter((n) => winningNumbers.includes(n)).length;
+  }
 
-    }
-
-    makeLottoDto() {
-        return new LottoDto(this.#numbers)
-    }
+  makeLottoDto() {
+    return new LottoDto(this.#numbers);
+  }
 }
+
+export default Lotto;
