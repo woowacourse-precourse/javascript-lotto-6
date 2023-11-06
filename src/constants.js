13601ee70767a7316a4d47452bd758370daf4763
Majesty-jun <hkwns01@gmail.com>
2023-11-06 14:31:12 +0900

    FIX: 상수 오타 수정

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const ERROR_MSG = {
   CURRENCY_UNIT_ERR: '[ERROR] 1000원 단위의 숫자를 입력해주세요.',
   NOT_SIX_NUMBER: '[ERROR] 로또 번호는 6개여야 합니다.',
   NOT_NUMBER_ERR: '[ERROR] 숫자를 입력해주세요.',
-  ONE_TO_FOURTYFIVE_ERR: '[ERROR] 1~45 사이의 숫자를 입력해주세요',
+  ONE_TO_FOURTYFIVE_ERR: '[ERROR] 1~45 사이의 숫자를 입력해주세요.',
   DUPLICATE_ERR: '[ERROR] 중복되지 않은 숫자를 입력해주세요.',
 };
 
@@ -14,7 +14,7 @@ export const ANSWER_LOTTO = '당첨 번호를 입력해주세요\n';
 
 export const BONUS_LOTTO = '보너스 번호를 입력해주세요.\n';
 
-export const INFO_RESULT = '당첨 통계\n---';
+export const INFO_RESULT = '\n당첨 통계\n---';
 
 export const WINNING_PRICE = [5000, 50000, 1500000, 30000000, 2000000000];
 
