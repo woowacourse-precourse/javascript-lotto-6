740592c199ffa325aad4bcec0665daad86ec27ab
zelkovaria <hyein396@gmail.com>
2023-11-06 21:37:37 +0900

    feat: askBonusNum() test 예외 케이스 추가 구현

--- __tests__/LottoStoreTest.js
+++ __tests__/LottoStoreTest.js
@@ -41,6 +41,16 @@ describe("LottoStoreTest", () => {
     expect(mockReadLineAsync).toHaveBeenCalledTimes(1);
   });
 
+  test("당첨 번호는 입력 시 쉼표가 아닌 다른 구분자를 사용하면 에러가 발생한다", async () => {
+    mockReadLineAsync.mockResolvedValue("1;2!3@4#5$6");
+    Validator.validateWinningNumbers.mockImplementation(() => {
+      throw new Error(ERROR_MESSAGE.invalidTicketNumbers);
+    });
+    await expect(LottoStore.askWinningNum()).rejects.toThrow(
+      ERROR_MESSAGE.invalidTicketNumbers
+    );
+  });
+
   test("보너스 번호는 1~45사이 정수를 입력하지 않으면 예외가 발생한다", async () => {
     mockReadLineAsync.mockResolvedValue("49");
     Validator.validateBonusNumber.mockImplementation(() => {
