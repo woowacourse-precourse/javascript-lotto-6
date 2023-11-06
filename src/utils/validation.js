5a2dac4b8dfd26df35270dd0f677e3ecddc1ccb9
hyzzzzy <shinhj4068@naver.com>
2023-11-06 16:37:52 +0900

    refactor(validation): 에러 반환하는 유효성 검사로 변경

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,21 +1,40 @@
 import { SETTING } from '../constants';
+import { ERROR_MESSAGE } from '../constants';
+const { 
+  not_above_min_cost,
+  not_natural_number,
+  not_number,
+  not_range,
+  not_six_numbers,
+} = ERROR_MESSAGE;
 
 export const isNumber = (num) => {
-  return Number.isNaN(num);
+  if (Number.isNaN(num)) {
+    throw new Error(not_number);
+  }
 }
 
 export const isNaturalNumber = (num) => {
-  return Number(num) % 1 === 0;
+  if (Number(num) % 1 !== 0) {
+    throw new Error(not_natural_number);
+  }
 }
 
 export const isAboveMinNumber = (num) => {
-  return Number(num) > SETTING.min_cost;
+  if (Number(num) < SETTING.min_cost) {
+    throw new Error(not_above_min_cost);
+  }
 }
 
 export const hasSixValues = (arr) => {
-  return arr.length === SETTING.input_length;
+  if (arr.length !== SETTING.input_length) {
+    throw new Error(not_six_numbers);
+  }
 }
 
-export const isLottoNumber = (num) => {
-  return Number(num) >= SETTING.min_lotto_number || Number(num) <= SETTING.max_lotto_number;
+export const isLottoNumberRange = (num) => {
+  if (Number(num) < SETTING.min_lotto_number 
+  || Number(num) > SETTING.max_lotto_number) {
+    throw new Error(not_range);
+  }
 }
