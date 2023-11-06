fa139c297f3d2e52613b3fde4186a988b1186533
yu-retel <dbdudals100@naver.com>
2023-11-06 20:22:44 +0900

    feat(LOttoSimulator): 입력한 당첨번호와 발행된 로또 번호 비교 기능구현

--- src/LottoSimulator.js
+++ src/LottoSimulator.js
@@ -3,9 +3,11 @@ import NumberGenerator from './util/NumberGenerator.js';
 
 class LottoSimulator {
   #lottos
+  #rankMap
 
   constructor() {
     this.#lottos = [];
+    this.#rankMap = Array(5).fill(0);
   }
 
   issueLotto(quantity) {
@@ -13,6 +15,51 @@ class LottoSimulator {
       const lotto = new Lotto(NumberGenerator.generator(6));
       this.#lottos.push(lotto.getNumbers());
     })
+  }
+
+  compareWinningNumber(winningNumber) {
+    const result = [];
+
+    this.#lottos.map((lotto) => {
+      const winning = lotto.filter((number) => winningNumber.includes(number));
+      result.push(winning.length);
+    });
+
+    return result;
+  }
+
+  compareBonusNumber(bonusNumber) {
+    const result = [];
+
+    this.#lottos.map((lotto) => {
+      const bonus = lotto.includes(bonusNumber);
+      result.push(bonus);
+    })
+
+    return result;
+  }
+  
+  rank(winningNumber, bonusNumber) {
+    if (winningNumber === 6) return 1;
+    if (winningNumber === 5 && bonusNumber) return 2;
+    if (winningNumber === 5) return 3;
+    if (winningNumber === 4) return 4;
+    if (winningNumber === 3) return 5;
+
+    return false;
+  }
+
+  drawingLotto(winningNumber, bonusNumber) {
+    const result = [];
+    const winnings = this.compareWinningNumber(winningNumber);
+    const bonusNumbers = this.compareBonusNumber(bonusNumber);
+
+    winnings.map((winning, ticket) => {
+      const rank = this.rank(winning, bonusNumbers[ticket]);
+      if (rank) result.push(rank);
+    })
+
+    return result;
   }
 
   getLottos() {
