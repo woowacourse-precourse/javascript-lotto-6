2a7691b7b98a0fb37a347bc749493585fb7e254d
JeongSabibi <jsbb528@naver.com>
2023-11-06 23:20:23 +0900

    feat(Lotto): 로또 번호를 반환하는 함수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,15 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 중복된 숫자가 입력되었습니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
