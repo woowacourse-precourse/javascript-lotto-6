3a8b120100874c6f1d5c8bcf84eb3ce2f68036de
kimjaehyuk <jhkim0097@gmail.com>
2023-11-06 13:39:57 +0900

    fix: 출력 양식 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 import GameError from './LottoError.js';
 import Lotto from './Lotto.js';
+import { LOTTO_PRIZE } from './constants.js';
 
 class App {
   #generatRandomLottos(purchaseAmount) {
-    if (purchaseAmount % 1000 != 0)
-      throw new GameError('로또 구입 금액은 1000원 단위입니다.');
+    if (purchaseAmount % LOTTO_PRIZE != 0)
+      throw new GameError(`로또 구입 금액은 ${LOTTO_PRIZE}원 단위입니다.`);
 
     const lottoAmount = purchaseAmount / 1000;
-    Console.print(lottoAmount + '개를 구매했습니다.');
+    Console.print(`${lottoAmount}개를 구매했습니다.`);
 
     const lottos = [];
     for (let i = 0; i < lottoAmount; i++) {
@@ -23,7 +24,7 @@ class App {
 
   async play() {
     const purchaseAmount = await Console.readLineAsync(
-      '로또 구입 금액을 입력해 주세요: '
+      '로또 구입 금액을 입력해 주세요.\n'
     );
     const lottos = this.#generatRandomLottos(Number(purchaseAmount));
   }
