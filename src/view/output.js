747c008467673c362c278356cf2635e5fbd67d93
Goonco <s07019@naver.com>
2023-11-06 15:55:31 +0900

    :sparkles: 메시지를 받아 출력하는 기능 추가

---
+++ src/view/output.js
@@ -0,0 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class Output{
+    constructor(){}
+
+    static outputMessage(message){
+        Console.print(message);
+    }
+}
+
+export default Output;
