5b46fea139900561303212bad9908e65e731d701
yesiamok3 <jaehyunok@cau.ac.kr>
2023-11-07 01:51:51 +0900

    test: 당첨번호 테스트
    당첨번호의 수가 6개가 아니거나 중복되는 경우 예외처리

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,6 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
+import WinningNumbers from "../src/WinningNumbers.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -110,3 +111,16 @@ describe("로또 테스트", () => {
   });
 });
 
+describe("당첨번호 클래스 테스트", () => {
+  test("당첨번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+    expect(() => {
+      new WinningNumbers([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("당첨번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new WinningNumbers([1, 2, 3, 4, 5, 5]);
+    }).toThrow("[ERROR]");
+  });
+});
