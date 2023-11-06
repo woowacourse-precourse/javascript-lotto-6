1a13819e13e3f8d1881c1f413f9b657fe8b70d85
Boeun <boeun0904@naver.com>
2023-11-06 22:02:35 +0900

    refactor(InputValidation): 유효성 검증 클래스 분리

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,88 +1,83 @@
-import UserInput from '../src/UserInput.js';
+import InputValidation from '../src/InputValidation.js';
 
-describe('유저 입력 테스트', () => {
-  describe('로또 구입 금액 입력 테스트', () => {
-    test('빈 값을 입력할 경우 예외를 발생시킨다.', () => {
-      expect(() => UserInput.checkPurchaseAmountValidation('')).toThrow('[ERROR]');
+describe('입력 유효성 검증 테스트', () => {
+  describe('로또 구입 금액 유효성 테스트', () => {
+    test('빈 값일 경우 예외를 발생시킨다.', () => {
+      expect(() => InputValidation.checkPurchaseAmount('')).toThrow('[ERROR]');
     });
 
-    test.each(['abc', 'ㄱㄴㄷ'])('숫자가 아닌 값을 입력할 경우 예외를 발생시킨다.', (input) => {
-      expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow('[ERROR]');
+    test.each(['abc', 'ㄱㄴㄷ'])('숫자가 아닐 경우 예외를 발생시킨다.', (input) => {
+      expect(() => InputValidation.checkPurchaseAmount(input)).toThrow('[ERROR]');
     });
 
-    test.each(['-1000', '0'])('0 이하의 숫자를 입력할 경우 예외를 발생시킨다.', (input) => {
-      expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow('[ERROR]');
+    test.each(['-1000', '0'])('0 이하의 숫자일 경우 예외를 발생시킨다.', (input) => {
+      expect(() => InputValidation.checkPurchaseAmount(input)).toThrow('[ERROR]');
     });
 
     test.each(['1', '100', '1234'])(
-      '1000으로 나누어떨어지지 않는 숫자를 입력할 경우 예외를 발생시킨다.',
+      '1000으로 나누어떨어지지 않는 숫자일 경우 예외를 발생시킨다.',
       (input) => {
-        expect(() => UserInput.checkPurchaseAmountValidation(input)).toThrow('[ERROR]');
+        expect(() => InputValidation.checkPurchaseAmount(input)).toThrow('[ERROR]');
       },
     );
 
-    test.each(['1000', '10000'])('값을 올바르게 입력할 경우 예외를 발생시키지 않는다.', (input) => {
-      expect(() => UserInput.checkPurchaseAmountValidation(input)).not.toThrow('[ERROR]');
+    test.each(['1000', '10000'])('값이 올바를 경우 예외를 발생시키지 않는다.', (input) => {
+      expect(() => InputValidation.checkPurchaseAmount(input)).not.toThrow('[ERROR]');
     });
   });
 
-  describe('당첨 번호 입력 테스트', () => {
-    test('빈 값을 입력할 경우 예외를 발생시킨다.', () => {
-      expect(() => UserInput.checkWinningNumbersValidation('')).toThrow('[ERROR]');
+  describe('당첨 번호 유효성 테스트', () => {
+    test('빈 값일 경우 예외를 발생시킨다.', () => {
+      expect(() => InputValidation.checkWinningNumbers('')).toThrow('[ERROR]');
     });
 
     test.each(['abc,1,2,3,4,5', '1,2,3,4,5,ㄱㄴㄷ'])(
-      '숫자가 아닌 값을 입력할 경우 예외를 발생시킨다.',
+      '숫자가 아닐 경우 예외를 발생시킨다.',
       (input) => {
-        expect(() => UserInput.checkWinningNumbersValidation(input)).toThrow('[ERROR]');
+        expect(() => InputValidation.checkWinningNumbers(input)).toThrow('[ERROR]');
       },
     );
 
     test.each(['-1000,1,2,3,4,5', '1,2,3,4,5,0'])(
-      '0 이하의 숫자를 입력할 경우 예외를 발생시킨다.',
+      '0 이하의 숫자일 경우 예외를 발생시킨다.',
       (input) => {
-        expect(() => UserInput.checkWinningNumbersValidation(input)).toThrow('[ERROR]');
+        expect(() => InputValidation.checkWinningNumbers(input)).toThrow('[ERROR]');
       },
     );
 
     test.each(['1,2,3,4', '1,2,3,4,5,6,7'])(
-      '입력한 숫자가 6개가 아닐 경우 예외를 발생시킨다.',
+      '숫자가 6개가 아닐 경우 예외를 발생시킨다.',
       (input) => {
-        expect(() => UserInput.checkWinningNumbersValidation(input)).toThrow('[ERROR]');
+        expect(() => InputValidation.checkWinningNumbers(input)).toThrow('[ERROR]');
       },
     );
 
-    test.each(['1,2,3,4,4,4', '1,1,1,1,1,1'])(
-      '같은 숫자를 여러 번 입력할 경우 예외를 발생시킨다.',
-      (input) => {
-        expect(() => UserInput.checkWinningNumbersValidation(input)).toThrow('[ERROR]');
-      },
-    );
+    test.each(['1,2,3,4,4,4', '1,1,1,1,1,1'])('숫자가 중복될 경우 예외를 발생시킨다.', (input) => {
+      expect(() => InputValidation.checkWinningNumbers(input)).toThrow('[ERROR]');
+    });
 
-    test('값을 올바르게 입력할 경우 예외를 발생시키지 않는다.', () => {
-      expect(() => UserInput.checkWinningNumbersValidation('1,2,3,4,5,6')).not.toThrow('[ERROR]');
+    test('값이 올바를 경우 예외를 발생시키지 않는다.', () => {
+      expect(() => InputValidation.checkWinningNumbers('1,2,3,4,5,6')).not.toThrow('[ERROR]');
     });
   });
 
-  describe('보너스 번호 입력 테스트', () => {
+  describe('보너스 번호 유효성 테스트', () => {
     const winningNumbers = [1, 2, 3, 4, 5, 6];
 
-    test('빈 값을 입력할 경우 예외를 발생시킨다.', () => {
-      expect(() => UserInput.checkBonusNumberValidation(winningNumbers, '')).toThrow('[ERROR]');
+    test('빈 값일 경우 예외를 발생시킨다.', () => {
+      expect(() => InputValidation.checkBonusNumber(winningNumbers, '')).toThrow('[ERROR]');
     });
 
-    test.each(['abc', 'ㄱㄴㄷ'])('숫자가 아닌 값을 입력할 경우 예외를 발생시킨다.', (input) => {
-      expect(() => UserInput.checkBonusNumberValidation(winningNumbers, input)).toThrow('[ERROR]');
+    test.each(['abc', 'ㄱㄴㄷ'])('숫자가 아닐 경우 예외를 발생시킨다.', (input) => {
+      expect(() => InputValidation.checkBonusNumber(winningNumbers, input)).toThrow('[ERROR]');
     });
 
-    test('당첨 번호에 존재하는 번호를 입력할 경우 예외를 발생시킨다.', () => {
-      expect(() => UserInput.checkBonusNumberValidation(winningNumbers, '6')).toThrow('[ERROR]');
+    test('당첨 번호에 존재하는 번호일 경우 예외를 발생시킨다.', () => {
+      expect(() => InputValidation.checkBonusNumber(winningNumbers, '6')).toThrow('[ERROR]');
     });
 
-    test('값을 올바르게 입력할 경우 예외를 발생시키지 않는다.', () => {
-      expect(() => UserInput.checkBonusNumberValidation(winningNumbers, '7')).not.toThrow(
-        '[ERROR]',
-      );
+    test('값이 올바를 경우 예외를 발생시키지 않는다.', () => {
+      expect(() => InputValidation.checkBonusNumber(winningNumbers, '7')).not.toThrow('[ERROR]');
     });
   });
 });
--- src/UserInput.js
+++ src/UserInput.js
@@ -1,12 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
-import { MESSAGE, ERROR, LOTTERY } from './constants.js';
-import { InputError } from './utils/error.js';
+import { MESSAGE } from './constants.js';
+import InputValidation from './InputValidation.js';
 
 export default class UserInput {
   static async getPurchaseAmount() {
     const input = await Console.readLineAsync(MESSAGE.INPUT_PURCHASE_AMOUNT);
     try {
-      this.checkPurchaseAmountValidation(input);
+      InputValidation.checkPurchaseAmount(input);
       return Number(input);
     } catch (exception) {
       this.printError(exception.message);
@@ -14,18 +14,10 @@ export default class UserInput {
     }
   }
 
-  static checkPurchaseAmountValidation(input) {
-    if (input.length === 0) throw new InputError(ERROR.BLANK_INPUT);
-    const num = Number(input);
-    if (Number.isNaN(num)) throw new InputError(ERROR.NOT_A_NUMBER);
-    if (num <= 0) throw new InputError(ERROR.NOT_A_NATURAL_NUMBER);
-    if (num % LOTTERY.PRICE !== 0) throw new InputError(ERROR.NOT_DIVIDED_BY_THOUSAND);
-  }
-
   static async getWinningNumbers() {
     const input = await Console.readLineAsync(MESSAGE.INPUT_WINNING_NUMBERS);
     try {
-      this.checkWinningNumbersValidation(input);
+      InputValidation.checkWinningNumbers(input);
       return input.split(',').map(Number);
     } catch (exception) {
       this.printError(exception.message);
@@ -33,36 +25,15 @@ export default class UserInput {
     }
   }
 
-  static checkWinningNumbersValidation(input) {
-    if (input.length === 0) throw new InputError(ERROR.BLANK_INPUT);
-    const arr = input.split(',').map(Number);
-    if (arr.some((num) => Number.isNaN(num))) throw new InputError(ERROR.NOT_A_NUMBER);
-    if (
-      !arr.every((num) => {
-        return num >= LOTTERY.MIN_NUM && num <= LOTTERY.MAX_NUM;
-      })
-    )
-      throw new InputError(ERROR.OUT_OF_RANGE(LOTTERY.MIN_NUM, LOTTERY.MAX_NUM));
-    if (arr.length !== LOTTERY.NUM_COUNT) throw new InputError(ERROR.NOT_SIX_NUMBERS);
-    if (new Set(arr).size !== LOTTERY.NUM_COUNT) throw new InputError(ERROR.NOT_SIX_NUMBERS);
-  }
-
   static async getBonusNumber(winningNumbers) {
     const input = await Console.readLineAsync(MESSAGE.INPUT_BONUS_NUMBER);
     try {
-      this.checkBonusNumberValidation(winningNumbers, input);
+      InputValidation.checkBonusNumber(winningNumbers, input);
       return Number(input);
     } catch (exception) {
       this.printError(exception.message);
       return this.getBonusNumber(winningNumbers);
     }
-  }
-
-  static checkBonusNumberValidation(winningNumbers, input) {
-    if (input.length === 0) throw new InputError(ERROR.BLANK_INPUT);
-    const num = Number(input);
-    if (Number.isNaN(num)) throw new InputError(ERROR.NOT_A_NUMBER);
-    if (winningNumbers.includes(num)) throw new InputError(ERROR.ALREADY_SELECTED);
   }
 
   static printError(error) {
