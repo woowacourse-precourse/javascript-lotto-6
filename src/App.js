e14a6473a6d2ae04ea0a59ebce788b780818a375
Jaewon <wellbird@naver.com>
2023-11-06 22:07:45 +0900

    feat : App클래스에 컨트롤러 실행 구문 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import LottoController from "./controller/LottoController";
+
 class App {
-  async play() {}
+  async play() {
+    const controller = new LottoController();
+    await controller.process();
+  }
 }
 
 export default App;
