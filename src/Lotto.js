f5a73c42d8b3ec9ef60fa6b47a92442c79c0598f
JUNU <bokjunwoo@gmail.com>
2023-11-07 03:26:43 +0900

    refactor : validateWinnigNumber유효성 검사 함수 사용
    #validate 메소드를 수정하여 validateWinnigNumber 함수를 통해 유효성 검사를 진행하도록 했습니다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { validateWinnigNumber } from './utils.js';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,8 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    validateWinnigNumber(numbers);
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
