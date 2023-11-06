1c0ef0e05c7ec3ec8a6402629cfd1405f12e2d69
Rilee-0320 <go13209@naver.com>
2023-11-07 01:05:56 +0900

    feat: 로또 구입 금액 입력값 검증

--- src/Inputs.js
+++ src/Inputs.js
@@ -5,6 +5,14 @@ class Inputs {
     const price = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
     return price;
   }
+
+  validPrice(price) {
+    const numericPrice = Number(price);
+    if (!(numericPrice % 1000 === 0) || isNaN(numericPrice) || !numericPrice) {
+      throw new Error("[ERROR] 1,000원 단위의 숫자를 입력해주세요.");
+    }
+    return numericPrice;
+  }
 }
 
 export default Inputs;
