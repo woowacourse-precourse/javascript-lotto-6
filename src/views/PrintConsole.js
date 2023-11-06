f677ea037a194597ce5f016f69cf54200f5bfb5c
Lee sang Yeop <mma7710@naver.com>
2023-11-06 18:48:16 +0900

    feat(PrintConsole): 당첨 통계를 출력하는 메서드 추가 몇개 일치하면 얼마의 상금이 있는지에 대한 정보와 유저는 몇개의 로또를 맞혔는지 볼 수 있도록 추가

--- src/views/PrintConsole.js
+++ src/views/PrintConsole.js
@@ -1,8 +1,35 @@
 import { Console } from '@woowacourse/mission-utils';
+import SYSTEM_MESSAGES from '../constants/SystemMessages.js';
+import LOTTO_SYSTEM from '../constants/LottoSystem.js';
 
 class PrintConsole {
+	showWinningStatistics(lottoResult, totalReturn) {
+		this.print(SYSTEM_MESSAGES.winning_statistics);
+
+		LOTTO_SYSTEM.winning_array.forEach((winning, idx) => {
+			const winningCount = winning.count;
+			const prize = LOTTO_SYSTEM.prize_array[idx];
+			const userWinningCount = lottoResult.filter((result) => result === idx).length;
+
+			if (winning.hasBonus)
+				this.print(
+					SYSTEM_MESSAGES.number_of_match(winningCount, prize.toLocaleString(), userWinningCount)
+				);
+			else
+				this.print(
+					SYSTEM_MESSAGES.number_of_match_bonusball(
+						winningCount,
+						prize.toLocaleString(),
+						userWinningCount
+					)
+				);
+		});
+
+		this.print(SYSTEM_MESSAGES.rate_of_return(totalReturn));
+	}
+
 	showLottoNumbers(numbers) {
-		this.print(`${numbers.length}개를 구매했습니다.`);
+		this.print(SYSTEM_MESSAGES.number_of_purchase(numbers.length));
 		numbers.forEach((number) => this.print(number));
 	}
 
