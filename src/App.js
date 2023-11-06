96699cc603dc955088a20cceb5fa96d3ffb74d06
yu-heejin <jkgt0521@naver.com>
2023-11-07 01:42:18 +0900

    feat: 구입 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await Console.readLineAsync('구입 금액을 입력해 주세요.\n');
+    const purchaseCount = purchaseAmount / 1000;
+  }
 }
 
 export default App;
