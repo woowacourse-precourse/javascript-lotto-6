627fbaa88b57b7a5453dc68fd25498fe90e4a8b6
baegyeong <beth0929@naver.com>
2023-11-06 20:25:36 +0900

    feat(App): 구입금액 입력을 받는 클래스 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { input } from "./input.js";
+import BuyPrice from "./BuyPrice.js";
+
 class App {
-  async play() {}
+  price = 0;
+
+  constructor() {
+    this.price = 0;
+  }
+
+  async play() {
+    this.price = await input.getInputBuyPrice();
+    const buyPrice = await new BuyPrice(this.price);
+  }
 }
 
 export default App;
