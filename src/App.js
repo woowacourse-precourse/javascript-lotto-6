405a91aa43c6bd8614852da16f8baf4e37e726d6
Dawon00 <dawon000203@gmail.com>
2023-11-07 02:10:19 +0900

    Feat: 발행한 로또 수량 및 번호 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import { MissionUtils, Console } from "@woowacourse/mission-utils";
-
+import Lotto from "./Lotto.js";
 class App {
-    async play() {}
+    async play() {
+        const purchaseAmount = await this.inputPurchaseAmount();
+        Console.print("\n" + purchaseAmount / 1000 + "개를 구매했습니다.");
+
+        const lottoTickets = [];
+        for (let i = 0; i < purchaseAmount / 1000; i++) {
+            const lottoNumbers = this.generateLottoNumbers();
+            const lottoTicket = new Lotto(lottoNumbers);
+            lottoTickets.push(lottoTicket);
+        }
+
+        for (const lottoTicket of lottoTickets) {
+            Console.print(lottoTicket.getNumbers());
+        }
+    }
 
     async inputPurchaseAmount() {
         const purchaseAmount = await Console.readLineAsync(
-            "구입금액을 입력해 주세요."
+            "구입금액을 입력해 주세요.\n"
         );
         if (purchaseAmount % 1000 !== 0 || purchaseAmount < 1000) {
             throw new Error(
@@ -13,6 +27,15 @@ class App {
             );
         }
         return purchaseAmount;
+    }
+
+    generateLottoNumbers() {
+        const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
+            1,
+            45,
+            6
+        );
+        return lottoNumbers.sort((a, b) => a - b);
     }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,20 @@
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    constructor(numbers) {
+        this.#validate(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  // TODO: 추가 기능 구현
+    #validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    getNumbers() {
+        return this.#numbers;
+    }
 }
 
 export default Lotto;
