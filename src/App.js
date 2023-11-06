390f0643e28dbb3d9b8f4f690bb2234a74babae4
youngchann <lych96@gmail.com>
2023-11-07 01:32:37 +0900

    feat: App.js에 로또 게임 기능 추가
    - 입력 및 출력 기능들을 종합하여 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import UserInput from './module/UserInput.js';
+import Output from './module/Output.js';
+import Lotto from './Lotto.js';
+import ValidateBonus from './module/ValidateBonus.js';
+import ValidatePurchase from './module/ValidatePurchase.js';
+import purchasedLottoArray from './module/RandomLotto.js';
+import matchLotto from './module/Match.js';
+
 class App {
-  async play() {}
+  async play() {
+    const purchasedLotto = await UserInput.purchasedLotto();
+    const validatePurchase = new ValidatePurchase(purchasedLotto);
+    const lottoArray = purchasedLottoArray(purchasedLotto);
+    Output.printUserLottos(lottoArray);
+    const winningNumbers = await UserInput.winningNumbers();
+    const lotto = new Lotto(winningNumbers);
+    const bonus = await UserInput.bonusNumber();
+    const validateBonus = new ValidateBonus(bonus);
+    Output.printWinningStatistics(
+      matchLotto(lottoArray, winningNumbers, bonus),
+    );
+    Output.printTotalReturn(
+      purchasedLotto,
+      matchLotto(lottoArray, winningNumbers, bonus),
+    );
+  }
 }
 
 export default App;
