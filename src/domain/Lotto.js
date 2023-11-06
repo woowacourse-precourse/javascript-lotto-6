e6fbb18c502303ef1ec06c8b47509ab17d20a80d
starcradle101 <dreamerdev12@gmail.com>
2023-11-06 14:38:32 +0900

    ✍️Rename($Lotto): Lotto.js 파일 이동 Lotto.js 파일을 domain 폴더 내부로 이동

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -8,11 +8,13 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  // TODO: 로또 등수별 확인 메서드
+  // TODO: 로또 번호 확인 가능 메서드
 }
 
 export default Lotto;
