39b14ae08587a2620822cda320f11d1459098b2d
swarvy <swarvy0826@naver.com>
2023-11-07 00:43:31 +0900

    feat : 컨트롤러에서 당첨번호를 입력받고 비교 결과를 출력하도록 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,5 +1,14 @@
+import {
+  LOTTO_PRICE,
+  PROFIT_ROUND_UP_VALUE,
+} from '../constants/LottoOption.js';
 import LottoService from '../service/LottoService.js';
-import { readLottoSeedMoney } from '../view/InputView.js';
+import {
+  readLottoBonusNumber,
+  readLottoMainNumbers,
+  readLottoSeedMoney,
+} from '../view/InputView.js';
+import { printLottoNumbers, printLottoResults } from '../view/OutputView.js';
 
 export default class LottoController {
   #lottoService;
@@ -7,10 +16,32 @@ export default class LottoController {
   async run() {
     const lottoSeedMoney = await readLottoSeedMoney();
     this.#lottoService = new LottoService(lottoSeedMoney);
-    this.#test();
+    this.#printLottoList();
   }
 
-  #test() {
-    console.log(this.#lottoService.getLottoes());
+  async #printLottoList() {
+    const createdLottoList = this.#lottoService.getLottoList();
+    printLottoNumbers(createdLottoList);
+    await this.#readLottoNumbers();
+  }
+
+  async #readLottoNumbers() {
+    const lottoNumbers = await readLottoMainNumbers();
+    const lottoBonusNumber = await readLottoBonusNumber();
+
+    this.#compare(lottoNumbers, lottoBonusNumber);
+  }
+
+  #compare(lottoNumbers, lottoBonusNumber) {
+    const { lottoResults, lottoAmount } = this.#lottoService.getCompareResults(
+      lottoNumbers,
+      lottoBonusNumber
+    );
+    const profit = (
+      lottoResults.prizeTotal /
+      (lottoAmount * LOTTO_PRICE)
+    ).toFixed(PROFIT_ROUND_UP_VALUE);
+
+    printLottoResults(lottoResults.prizeAmount, profit);
   }
 }
