355e8f4913542d134eb7f223e8017151c28898af
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-06 23:46:06 +0900

    test: lotto consist of numbers passed as parameters

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,11 @@
 import Lotto from "../src/models/Lotto.js";
 
 describe("로또 클래스 테스트", () => {
+  test("로또 번호를 넘기면 로또는 그 번호로 구성되야한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getNumbers()).toContain(1, 2, 3, 4, 5, 6);
+  });
+
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
