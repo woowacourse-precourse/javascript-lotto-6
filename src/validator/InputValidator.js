d871e768cb6b9aa02cef2632ae5078b6a01d15fb
pakxe <pigkill40@naver.com>
2023-11-06 16:17:36 +0900

    feat: 유효성 검사를 간단하게 하기 위한 메서드를 모은 유틸 클래스 작성

--- src/validator/InputValidator.js
+++ src/validator/InputValidator.js
@@ -1,8 +1,33 @@
-// 자주 사용되는 검사 로직을 여기에 둔다
+import InputError from '../error/InputError.js';
+
 // 각 인풋의 유효성 검사는 이 클래스의 메서드 조합으로 한다.
 class InputValidator {
+  static isArrayHaveNan(arr) {
+    return arr.some((el) => InputValidator.isNan(el));
+  }
+
   static isNan(input) {
     return Number.isNaN(input);
+  }
+
+  static checkLength(length, arr) {
+    if (arr.length !== length) throw new InputError();
+  }
+
+  static isDuplicated(arr) {
+    const set = new Set(arr);
+
+    return set.size !== arr.length;
+  }
+
+  /**
+   * 숫자가 범위 이내인지 확인하는 함수
+   * @param {Number []} range
+   * @param {Number} num
+   * @returns Boolean
+   */
+  static isNotInRange([start, end], num) {
+    return start > num || end < num;
   }
 }
 
