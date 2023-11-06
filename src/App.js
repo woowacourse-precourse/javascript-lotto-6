d58d0830219ae2fcdb42e58b43f86e5150ec31ea
badahertz52 <badahertz52@gmail.com>
2023-11-06 19:28:15 +0900

    feat<App>: App 클래스 수정
    - 로또 구매,당첨 번호와 보너스 번호 뽑기, 로또 당첨 통계를 진행하는 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,68 @@
+import Calculator from "./Calculator";
+import Cashier from "./Cashier";
+import Checker from "./Checker";
+import DrawingMachine from "./DrawingMachine";
+import Statistics from "./Statistic";
+import User from "./User";
+
 class App {
-  async play() {}
+  paymentAmount;
+  userLottos;
+
+  async buyLottos(){
+    const user = new User();
+    const paymentAmount = await user.pay();
+    const cashier = new Cashier(paymentAmount);
+    const userLottos = cashier.issueLottos();
+    user.getLottos(userLottos);
+
+    return {paymentAmount:paymentAmount, userLottos:userLottos}
+  }
+
+  async drawWinningLottoAndBonus(){
+    const drawingMachine = new DrawingMachine();
+
+    await drawingMachine.drawWinningLotto();
+    await drawingMachine.drawBonusBall();
+
+    const {lotto,bonusBall} = drawingMachine.getWinningLottoAndBonusBall();
+
+    return {
+      winningLotto :lotto,
+      bonusBall :bonusBall
+    }
+  }
+
+  checkLottos(winnigLotto, bonusBall, userLottos ,paymentAmount){
+    const checker = new Checker(winnigLotto, bonusBall, userLottos);
+
+    const winningResult = checker.calculateWinningResult();
+
+    const calculator = new Calculator(winningResult);
+
+    const rateOfReturn = calculator.getRateOfReturn(winningResult,paymentAmount);
+
+    return {
+      winningResult:winningResult,
+      rateOfReturn: rateOfReturn
+    }
+  }
+  printResult(winningResult, rateOfReturn){
+    const statistics = new Statistics();
+
+    statistics.print(winningResult, rateOfReturn);
+  }
+
+  async play() {
+
+    const {paymentAmount, userLottos} = await this.buyLottos();
+
+    const {winningLotto, bonusBall} = await this.drawWinningLottoAndBonus();
+
+    const {winningResult, rateOfReturn} =this.checkLottos(winningLotto,bonusBall,userLottos,paymentAmount);
+
+    this.printResult(winningResult, rateOfReturn);
+  }
 }
 
 export default App;
