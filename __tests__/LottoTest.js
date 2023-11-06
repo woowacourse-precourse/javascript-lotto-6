a77c11928f266bd730cc36570d150676dbc902ad
JUN <gurwns232@naver.com>
2023-11-06 23:28:33 +0900

    test: Lotto.js 단위 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,39 @@
-import Lotto from "../src/Lotto.js";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from '../src/domains/Lotto.js';
+import { EXCEPTION } from '../src/constants/constants.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(EXCEPTION.LOTTO_MUST_SIX_NUMBERS);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(EXCEPTION.LOTTO_DUPLICATE);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호가 1~45까지의 정수가 아니면 에러 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 500]);
+    }).toThrow(EXCEPTION.LOTTO_MUST_IN_RANGE);
+  });
+
+  test('6자리 로또 번호 생성 테스트(오름차순 정렬)', () => {
+    const RANDOM_NUMBERS = [1, 3, 5, 2, 4, 6];
+    mockRandoms([RANDOM_NUMBERS]);
+
+    const input = Lotto.buyLotto();
+    const output = new Lotto(input).getLotto();
+    expect(output).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const EXCEPTION = Object.freeze({
+  LOTTO_MUST_SIX_NUMBERS: '[ERROR] 로또 번호는 6개여야 합니다.',
+  LOTTO_MUST_IN_RANGE: '[ERROR] 로또 번호는 1부터 45까지의 정수입니다.',
+  LOTTO_DUPLICATE: '[ERROR] 로또 번호에 중복된 숫자가 있으면 안된다.',
+});
