df1eae73e1445e53e1930ff667a2919ac15f3e89
unknown <pih9134@naver.com>
2023-11-07 02:58:39 +0900

    refactor: constants 수정

--- src/constants.js
+++ src/constants.js
@@ -5,10 +5,10 @@ export const PROMPT_MESSAGE = {
 };
 
 export const ERROR_MESSAGE = {
-  invalidPusrchaseAmount: '[ERROR] 1000원 단위로 구입 금액을 입력해주세요.',
-  duplicationWinningNumbers: '[ERROR] 1부터 45 사이의 6개 중복되지 않는 자연수를 입력해주세요.',
-  invalidBonusNumber: '[ERROR] 1부터 45 사이의 보너스 번호를 입력해주세요.',
-  duplicationBonusNumber: '[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.',
+  invalidPusrchaseAmount: '1000원 단위로 구입 금액을 입력해주세요.',
+  duplicationWinningNumbers: '1부터 45 사이의 6개 중복되지 않는 자연수를 입력해주세요.',
+  invalidBonusNumber: '1부터 45 사이의 보너스 번호를 입력해주세요.',
+  duplicationBonusNumber: '보너스 번호는 당첨 번호와 중복되면 안됩니다.',
 };
 
 export default { PROMPT_MESSAGE, ERROR_MESSAGE };
