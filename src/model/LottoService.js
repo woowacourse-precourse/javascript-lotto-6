15763cbf6445f1310ecf7f8a313121dc0b091e66
abi-hong <flffjakfwm20@gmail.com>
2023-11-06 23:11:19 +0900

    feat(LottoService): 로또 구매, 당첨 랭킹 개수, 수익률 기능 구현

---
+++ src/model/LottoService.js
@@ -0,0 +1,27 @@
+import { Random } from "@woowacourse/mission-utils";
+import { REWARD } from '../utils/Constant.js';
+import { Validator } from "../utils/Validator.js";
+
+export const LottoService = {
+  purchase(price) {
+    Validator.checkMoney(price)
+    return price / 1000;
+  },
+
+  calculateRanking(lottos, winningNumber, bonusNumber) {
+    const rankingCount = Array(6).fill(0);
+    return lottos.reduce((acc, lotto) => {
+      const ranking = lotto.winningLotto(winningNumber, bonusNumber);
+      acc[ranking - 1] += 1;
+      return acc;
+    }, rankingCount);
+  },
+
+  calculateProfit(price, rankingResult) {
+    const totalPrice = rankingResult.reduce((acc, ranking, index) => {
+      acc += ranking * REWARD[index+1];
+      return acc;
+    }, 0);
+    return (totalPrice / price) * 100;
+  }
+}
