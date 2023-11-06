dd6ece21f75848b465c19b03e84e60e239fe6db7
swarvy <swarvy0826@naver.com>
2023-11-07 01:29:55 +0900

    feat : InputView에서 유효성 검사를 진행하도록 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,13 +1,28 @@
 import { Console } from '@woowacourse/mission-utils';
 import { LOTTO_NOTIFICATION_MESSAGE } from '../constants/LottoMessage.js';
+import validateLottoBonusNumber from '../validator/LottoBonusNumberValidator.js';
+import validateLottoSeedMoney from '../validator/LottoSeedMoneyValidator.js';
+import validateLottoNumbers from '../validator/LottoNumbersValidator.js';
 
-const readLine = (message) => Console.readLineAsync(message);
+const readLine = async (message, validate) => {
+  const input = await Console.readLineAsync(message);
+  validate(input);
+  return input;
+};
 
 export const readLottoSeedMoney = () =>
-  readLine(LOTTO_NOTIFICATION_MESSAGE.enterLottoSeedMoney);
+  readLine(
+    LOTTO_NOTIFICATION_MESSAGE.enterLottoSeedMoney,
+    validateLottoSeedMoney
+  );
 
 export const readLottoMainNumbers = () =>
-  readLine(LOTTO_NOTIFICATION_MESSAGE.enterLottoWinningNumber);
+  readLine(
+    LOTTO_NOTIFICATION_MESSAGE.enterLottoWinningNumber,
+    validateLottoNumbers
+  );
 
-export const readLottoBonusNumber = () =>
-  readLine(LOTTO_NOTIFICATION_MESSAGE.enterLottoWinningBonusNumber);
+export const readLottoBonusNumber = (lottoNumbers) =>
+  readLine(LOTTO_NOTIFICATION_MESSAGE.enterLottoWinningBonusNumber, (input) =>
+    validateLottoBonusNumber(input, lottoNumbers)
+  );
