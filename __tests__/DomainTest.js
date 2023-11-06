ab7fe39f5245240a698186631dc9db0efeda8c3f
dali1999 <dali1999@naver.com>
2023-11-06 22:30:01 +0900

    test(checkIsBonus): 번호 5개 일치 시 보너스 번호와 비교하는 기능 테스트

--- __tests__/DomainTest.js
+++ __tests__/DomainTest.js
@@ -54,7 +54,26 @@ describe('핵심 로직 테스트', () => {
     expect(arr).toEqual([{}, {}]);
   });
 
-  test('checkWin() - 당첨 번호와 로또 번호를 비교 후 같은 번호의 갯수를 배열에 저장한다.', () => {
+  test('checkIsBonus() - 당첨 번호에 보너스 번호와 일치하는 번호가 있으면 배열에 `7`을 저장하거나 없으면 `5`를 저장한다.', () => {
+    // given
+    const arr = [
+      [1, 2, 3, 4, 5, 8],
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 5, 9],
+    ];
+    const bonus = 7;
+
+    // when
+    const controller = new LottoController();
+    for (let i = 0; i < arr.length; i++) {
+      controller.checkIsBonus(arr[i], bonus);
+    }
+
+    // then
+    expect(controller.sameNumCountArr).toEqual([5, 7, 5]);
+  });
+
+  test('checkWin() - 당첨 번호와 로또 번호 비교 후 일치하는 번호의 갯수를 배열에 저장한다.', () => {
     // given
     const arr = [];
     const price = 7000;
@@ -65,9 +84,9 @@ describe('핵심 로직 테스트', () => {
       [1, 2, 7, 8, 9, 10], // 2개 일치
       [1, 2, 3, 7, 8, 9], // 3개 일치
       [1, 2, 3, 4, 7, 8], // 4개 일치
-      [1, 2, 3, 4, 5, 8], //5개 일치
-      [1, 2, 3, 4, 5, 7], //5개 + 보너스 일치
-      [1, 2, 3, 4, 5, 6], //6개 일치
+      [1, 2, 3, 4, 5, 8], // 5개 일치
+      [1, 2, 3, 4, 5, 7], // 5개 + 보너스 일치
+      [1, 2, 3, 4, 5, 6], // 6개 일치
     ]);
 
     // when
@@ -80,6 +99,4 @@ describe('핵심 로직 테스트', () => {
     // then
     expect(controller.sameNumCountArr).toEqual([1, 2, 3, 4, 5, 7, 6]);
   });
-
-  
 });
