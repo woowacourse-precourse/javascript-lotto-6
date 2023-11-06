12f3777a35ddd32a301786e11f5d3210153d60de
rkdrjsrks <rkdrjs71@naver.com>
2023-11-06 10:45:04 +0900

    TODO List 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 class App {
-  async play() {
+    async play() {
 
-  }
+    }
+
+    async buyLottoTickets() {
+        //TODO : 로또 구입 금액을 입력 받는다.
+    }
+
+    async inputWinningNumbers() {
+        //TODO : 당첨 번호를 입력 받는다.
+    }
+
+    async inputBonusNumber() {
+        //TODO : 보너스 번호를 입력 받는다.
+    }
+
+    async displayLottoNumbers() {
+        //TODO : 발행한 로또 수량 및 번호를 출력한다.
+    }
+
+    async printResults() {
+        //TODO : 당첨 내역을 출력한다.
+    }
+
+    async printTotalEarningsRate() {
+        //TODO : 총 수익률을 출력한다.
+    }
+
 }
 
 export default App;
