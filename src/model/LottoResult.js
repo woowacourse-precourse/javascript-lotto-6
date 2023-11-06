47212a3c5c3fa483a432c927f51ee6eaa471fdd9
ReturnReason <reasonz@daum.net>
2023-11-06 22:16:42 +0900

    feat(LottoResult): 로또 게임 결과 데이터를 담당할 클래스 추가

---
+++ src/model/LottoResult.js
@@ -0,0 +1,15 @@
+class LottoResult {
+  #matchCount = {
+    three: 0,
+    four: 0,
+    five: 0,
+    six: 0,
+    bonus: 0,
+  };
+  #prizeMoney = 0;
+
+  // TODO 일치하는 당첨 번호/보너스 번호 계산 해서 출력하는 로직
+  // TODO 수익률 계산 후(유틸함수 사용) 출력 로직
+}
+
+export default LottoResult;
