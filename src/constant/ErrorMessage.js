49d00e5013c9e50b1002ae36f2ab1e2bbc63f5d8
Jaewon <wellbird@naver.com>
2023-11-06 14:00:16 +0900

    feat : 에러 메세지 파일 생성

---
+++ src/constant/ErrorMessage.js
@@ -0,0 +1,19 @@
+const errorMessage = Object.freeze({
+  purchaseError: () => {
+    throw new Error("[ERROR]구매 금액은 1,000원으로 나누어 떨어져야 합니다.");
+  },
+  lengthError: () => {
+    throw new Error("[ERROR]로또 번호는 6개여야 합니다.");
+  },
+  typeError: () => {
+    throw new Error("[ERROR]입력이 잘못된 형식입니다.");
+  },
+  rangeError: () => {
+    throw new Error("[ERROR]로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+  },
+  duplicateError: () => {
+    throw new Error("[ERROR]중복 숫자가 존재합니다.");
+  },
+});
+
+export default errorMessage;
