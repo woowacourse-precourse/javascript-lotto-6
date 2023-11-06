1be11bf5f2d9fd304f7a7a8523823328dfc16b73
noodle <noodle>
2023-11-06 14:35:20 +0900

    comment: 주석 수정

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,19 +1,28 @@
 const message = Object.freeze({
   prefix: '[ERROR]',
-  // 공통
+
+  /**
+   * common message
+   */
   invalidInput: '공백 혹은 쉼표가 입력되었습니다.',
   invalidNumberRange: '번호는 1부터 45 사이의 숫자여야 합니다.',
 
-  // 구매 가격 입력
+  /**
+   * 구매 가격 입력
+   */
   invalidUnit: '1,000원 단위로 입력해주세요.',
   invalidPurchase: '구매 가격에 문자열이 포함되어 있습니다. 숫자만 입력해주세요.',
 
-  // 로또 번호 입력 winningNumbers
+  /**
+   * 로또 번호 입력 winningNumbers
+   */
   invalidNumberLimit: '로또 번호는 6개여야 합니다.',
   duplicateNumber: '중복된 숫자가 있습니다. 6개의 서로 다른 숫자를 입력해주세요.',
   noZero: '0은 허용되지 않습니다.',
 
-  // 보너스 번호 입력 bonusNumber
+  /**
+   * 보너스 번호 입력 bonusNumber
+   */
   invalidBonusNumberLimit: '1개의 보너스 번호를 입력해주세요.',
   duplicateBonusNumber: '보너스 번호가 당첨 번호와 중복됩니다. 다른 번호를 입력해주세요.',
 });
