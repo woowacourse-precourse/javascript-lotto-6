f1efea1e4bada26ceccd3a02b7e1c0a058f6a499
Sohye <saeyan2477@gmail.com>
2023-11-07 03:31:17 +0900

    feat: App 클래스 구현
    - 당첨번호 입력받기
    - 보너스 번호 입력받기
    - 구매한 로또 번호 출력하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import { Validation } from './Validation.js';
+import { LOTTO_RULE, INPUT_MESSAGES, ERROR_MESSAGES } from './Constants.js';
+import { CustomError } from './CustomError.js';
+import LottoList from './LottoList.js';
+
+const LENGTH = LOTTO_RULE.LENGTH;
+const { MIN, MAX } = LOTTO_RULE.RANGE;
+
 class App {
-  async play() {}
+  #lottoApp;
+
+  constructor() {
+    this.#lottoApp = new LottoList();
+  }
+
+  async play() {
+    await this.printLottoList();
+    const inputNumbers = await this.inputNumbers();
+    await this.inputBonusNumber(inputNumbers);
+  }
+
+  async printLottoList() {
+    const lottoList = await this.#lottoApp.makeLottoList();
+    Console.print(lottoList);
+  }
+
+  async inputNumbers() {
+    const input = await Console.readLineAsync(INPUT_MESSAGES.LOTTO_NUMBER);
+    return input.split(',').map((num) => Number(num.trim()));
+  }
+
+  async inputBonusNumber() {
+    const inputBonusNumber = await Console.readLineAsync(
+      INPUT_MESSAGES.BONUS_NUMBER,
+    );
+    const bonusNumber = Number(inputBonusNumber);
+
+    return bonusNumber;
+  }
 }
 
 export default App;
