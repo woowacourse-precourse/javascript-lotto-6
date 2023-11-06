12f8d76ea198bea850aadbb2564c86db0ecb4dd8
kwani6684 <jack0910@naver.com>
2023-11-06 11:45:13 +0900

    ✨feat: add print lottonumber

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,15 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    this.printNumbers();
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+  printNumbers(){
+    MissionUtils.Console.print(this.#numbers)
   }
 
   // TODO: 추가 기능 구현
