c8033c8d5708a8a89f96ac49c7c9f92fa261adf9
laon2019 <laon5987@nate.com>
2023-11-06 19:25:25 +0900

    test(LottoTest.js) : 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,27 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+  // 아래에 추가 테스트 작성 가능
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호가 6개가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 1부터 45 범위에 있어야 한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 46, 47]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 1부터 45 범위에 있는 유효한 배열이면 객체를 생성한다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getLottoNumber()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
