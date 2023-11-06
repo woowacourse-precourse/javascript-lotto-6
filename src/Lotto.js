b838163656504a3e82eafb27146f74a0f2027ab8
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-06 16:22:07 +0900

    style: prettier 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,7 +8,7 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
