54bf3ae060089b18e71848776c0ad496f45b609b
bada308 <203190@jnu.ac.kr>
2023-11-06 14:44:10 +0900

    feat: NUMBER 상수 선언 - LOTTO, PURCHASE

---
+++ src/constants/NUMBER.js
@@ -0,0 +1,13 @@
+const LOTTO = {
+  MIN: 1,
+  MAX: 45,
+  COUNT: 6,
+};
+
+const PURCHASE = {
+  MIN: 1000,
+  UNIT: 1000,
+};
+
+Object.freeze(LOTTO, PURCHASE);
+export default { LOTTO, PURCHASE };
