0772288a7106f74ba3e16a9ac246365a06706ddd
OhHyukJun <kmh7277@naver.com>
2023-11-06 23:43:09 +0900

    Rename: 파일 삭제

--- src/InputPrice.js
+++
@@ -1,9 +0,0 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-
-export async function inputPrice() {
-    const userPrice = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.");
-    if (userPrice % 1000 !== 0) {
-        throw new Error("[ERROR] 입력 값이 1,000으로 나누어 떨어지지 않습니다.");
-    }
-    return userPrice/1000;
-}
