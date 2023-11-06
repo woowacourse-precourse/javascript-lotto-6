e87669a52f2edc6479277330a3f7e020dc712309
8leamnk <silkviewrk@gmail.com>
2023-11-06 20:14:37 +0900

    test(priceModelTest): 발행한 로또 개수를 구하는 로직을 테스트
    테스트 내용:
    - 발행한 로또 개수가 알맞게 계산되는 경우
    - 발행한 로또 개수가 다르게 계산되는 경우

---
+++ __tests__/models/priceModelTest.js
@@ -0,0 +1,19 @@
+import priceModel from '../../src/models/priceModel.js';
+
+test('발행한 로또 개수가 알맞게 계산된다.', () => {
+  const inputs = [10000, 5000, 17000, 200000, 7000];
+  const outputs = [10, 5, 17, 200, 7];
+
+  outputs.forEach((output, index) => {
+    expect(priceModel.calculateNumberOfLotto(inputs[index])).toBe(output);
+  });
+});
+
+test('발행한 로또 개수가 다르게 계산된다.', () => {
+  const inputs = [15000, 50000, 170000, 2000, 70000];
+  const outputs = [10, 5, 17, 200, 7];
+
+  outputs.forEach((output, index) => {
+    expect(priceModel.calculateNumberOfLotto(inputs[index])).not.toBe(output);
+  });
+});
