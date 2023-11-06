ae745e9a026315c70839d52f19b708e6570d1d74
Y.W KIM <kyw624@gmail.com>
2023-11-06 17:56:48 +0900

    Chore: MESSAGES 상수 변수 export 구문 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const MESSAGES = {
+import Game from './Game';
+
+export const MESSAGES = {
   INPUT_AMOUNT: '구입금액을 입력해 주세요.\n',
   INPUT_WINNING_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
   INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
