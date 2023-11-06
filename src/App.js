26067eac252697cfc4950d67552938219ad93650
KYHH <kyhh93@naver.com>
2023-11-06 15:00:10 +0900

    feat : 로또 번호 6개와 보너스 번호 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+import Lotto from "./Lotto.js";
 class App {
-  async play() {}
+  async play() {
+    const { numbers, bonusNumber } = Lotto.createRandomNumbers();
+    const lotto = new Lotto(numbers, bonusNumber);
+    const lottoNumber = lotto.getNumbers();
+    const lottoBonusNumber = lotto.getBonusNumber();
+
+    Console.print(`로또 번호: ${lottoNumber}`);
+    Console.print(`보너스 번호: ${lottoBonusNumber}`);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,12 @@
+import { Random, Console } from '@woowacourse/mission-utils';
 class Lotto {
   #numbers;
+  #bonusNumber;
 
-  constructor(numbers) {
+  constructor(numbers, bonusNumber) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    this.#bonusNumber = bonusNumber;
   }
 
   #validate(numbers) {
@@ -13,6 +16,26 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+
+  static createRandomNumbers() {
+    const uniqueNumbers = new Set();
+    while (uniqueNumbers.size < 6) {
+      uniqueNumbers.add(Random.pickNumberInRange(1, 45));
+    }
+
+    const remainingNumbers = Array.from({ length: 45 }, (_, index) => index + 1).filter(number => !uniqueNumbers.has(number));
+    const bonusNumber = remainingNumbers[Math.floor(Math.random() * remainingNumbers.length)];
+
+    return { numbers: Array.from(uniqueNumbers), bonusNumber };
+  }
+
+  getNumbers() {
+    return [...this.#numbers];
+  }
+
+  getBonusNumber() {
+    return [this.#bonusNumber];
+  }
 }
 
-export default Lotto;
+export default Lotto; 
