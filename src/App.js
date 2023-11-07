e4593bee659c78516b861bc6fa87eeebf172eb92
hyj5076 <hyj5076@hanmail.net>
2023-11-08 06:17:26 +0900

    feat(App): 티켓 수에 따른 로또 번호 생성 및 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App {
   async play() {
     try {
       await this.setLotto();
+      this.createLotto();
     } catch (error) {
       Console.print(error.message);
       return Promise.reject(error);
@@ -26,12 +27,10 @@ class App {
   }
 
   async setLotto() {
-    // 사용자 입력값 호출
     const pay = await ask.payment();
     const baseNums = await ask.baseNumbers();
     const bonusNum = await ask.bonusNumber();
 
-    // 로또 유효성 검사와 함께 저장
     this.payment.setUserPayment(pay);
     this.base.setBaseNumbers(baseNums);
     this.bonus.setBonusNumber(bonusNum);
@@ -39,6 +38,21 @@ class App {
     Console.print(`지불금액: ${pay}`);
     Console.print(`기본숫자: ${baseNums}`);
     Console.print(`보너스숫자: ${bonusNum}`);
+  }
+
+  createLotto() {
+    const ticketCount = this.payment.numberOfTickets();
+    print.howManyBuyTickets(ticketCount);
+    const userTickets = [];
+
+    for (let i = 0; i < ticketCount; i++) {
+      const randomLottoNumbers = runLotteryMachine();
+      const myLotto = new Lotto(randomLottoNumbers);
+      myLotto.ascendingOrder();
+      print.formattedNumbers(myLotto.getNumbers());
+      userTickets.push(myLotto.getNumbers());
+    }
+    return userTickets;
   }
 }
 
