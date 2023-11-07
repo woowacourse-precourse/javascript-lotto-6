25cd8c2a4084f3ab737aa146927c36e23663a54d
Lim-shweety <smile70200@naver.com>
2023-11-07 22:30:09 +0900

    :sparkles: feat: 로또 금액 입력받기 로또 금액 입력 받고 입력 예외처리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import Lotto from "./Lotto.js";
-import {MissionUtils} from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+// eslint-disable-next-line import/extensions
+import Lotto from './Lotto.js';
+
 class App {
   async play() {
-    const MONEY= await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.")
-    const INPUT_NUMBERS = await MissionUtils.Console.readLineAsync("당첨 번호를 입력해 주세요.")
-    const LOTTO_NUMBERS=new Lotto(INPUT_NUMBERS);
+    const MONEY = await this.inputMoney();
+  }
 
+  async inputMoney() {
+    const MONEY = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    this.checkMoney(MONEY);
+    return Math.floor(Number(MONEY) / 1000);
+  }
 
-
-
+  // eslint-disable-next-line class-methods-use-this
+  checkMoney(money) {
+    if (!Number.isInteger(Number(money))) {
+      throw new Error('[ERROR] 로또 금액은 정수여야 합니다.');
+    }
+    if (Number(money) % 1000 !== 0) {
+      throw new Error('[ERROR] 로또 금액은 1000원 단위어야 합니다.');
+    }
   }
 }
 
