3f37d6f4cb4dd63aee7e15ca6a32c2f2b76d0b64
HyunJaeyeon <bomul0109@gmail.com>
2023-11-08 00:22:16 +0900

    feat: 랜덤숫자 6개 받기 [로또 발행]

--- src/controllers/LottoDrawing.js
+++ src/controllers/LottoDrawing.js
@@ -1,9 +1,14 @@
 // [로또 발행]
+import { Random } from '@woowacourse/mission-utils';
+
 export default class LottoDrawing {
   #line;
 
+  #getRandomNumbers;
+
   constructor(line) {
     this.#line = line;
+    this.#getRandomNumbers = () => Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 
   //  로또 구입 수량 체크
