298929c852ae08232a07be97030efb7a3bd07920
ohgus <anakin1108@naver.com>
2023-11-07 14:28:44 +0900

    fix: money 예외처리 조건문 수정

--- src/Input.js
+++ src/Input.js
@@ -33,7 +33,7 @@ export class Input {
       throw new Error(ERROR_MESSAGES.IT_IS_TO_SMALL);
     } else if (money.includes(".") || money.includes(" ")) {
       throw new Error(ERROR_MESSAGES.ONLY_NUMBER);
-    } else if (!Number(money) % 1000 !== 0) {
+    } else if (Number(money) % 1000 !== 0) {
       throw new Error(ERROR_MESSAGES.ONLY_THOUSANDWON_UNIT);
     }
   }
