ea2365dc60ed8ccd1aa6bb05f1c5752ea4a95ebb
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-07 16:29:18 +0900

    style: LOTTO_PRICE => this.LOTTO_PRICE 코드 수정

--- src/LottoBudget.js
+++ src/LottoBudget.js
@@ -3,7 +3,7 @@ class LottoBudget {
     lottoBudget;
     LOTTO_NUMBER;
     ERROR_MESSEGE="[ERROR] 숫자가 잘못된 형식입니다.";
-    GET_LOTTO_BUDGET_INPUT="구입금액을 입력해 주세요.\n";
+    GET_LOTTO_BUDGET_INPUT_MESSEGE="구입금액을 입력해 주세요.\n";
 
     constructor(MIN_LOTTO_NUM,MAX_LOTTO_NUM,LOTTO_PRICE) {
         this.MIN_LOTTO_NUM=MIN_LOTTO_NUM;
@@ -16,8 +16,8 @@ class LottoBudget {
             throw new Error(this.ERROR_MESSEGE);
         }
 
-        this.LOTTO_NUMBER = lottoBudget/LOTTO_PRICE;
-        const CHANGE = lottoBudget%LOTTO_PRICE
+        this.LOTTO_NUMBER = lottoBudget/this.LOTTO_PRICE;
+        const CHANGE = lottoBudget%this.LOTTO_PRICE
 
         if(CHANGE!==0){
             throw new Error(this.ERROR_MESSEGE);
@@ -25,10 +25,10 @@ class LottoBudget {
     }
 
     async getInput() {
-        this.lottoBudget = parseInt(await Console.readLineAsync(this.GET_LOTTO_BUDGET_INPUT));
+        this.lottoBudget = parseInt(await Console.readLineAsync(this.GET_LOTTO_BUDGET_INPUT_MESSEGE));
         this.#isValid(this.lottoBudget);
+        Console.print('\n');   
     }
-
 }
 
 export default LottoBudget;
