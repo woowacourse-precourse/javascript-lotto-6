f368e3b0fbda9863fde7d15a9fc612bcfa6ac4ca
hojkim77 <rlaghwnd4534@gmail.com>
2023-11-07 16:03:51 +0900

    [refactor] 출력 관련 오타 수정

--- src/views/Output.js
+++ src/views/Output.js
@@ -19,12 +19,12 @@ class Output {
     Console.print('당첨 통계');
     Console.print('---');
     Console.print(`3개 일치 (5,000원) - ${threeEqual}개`);
-    Console.print(`4개 일치 (5,000원) - ${fourEqual}개`);
-    Console.print(`5개 일치 (5,000원) - ${fiveEqual}개`);
+    Console.print(`4개 일치 (50,000원) - ${fourEqual}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${fiveEqual}개`);
     Console.print(
       `5개 일치, 보너스 볼 일치 (30,000,000원) - ${fiveEqualAndBonus}개`
     );
-    Console.print(`6개 일치 (5,000원) - ${sixEqual}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${sixEqual}개`);
   }
 
   static printYeild(userYeild) {
