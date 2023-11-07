d168e7dca74876d63a2ecb3145e6b9230b6a9be2
ymj307 <dymj307@gmail.com>
2023-11-07 17:42:57 +0900

    feat:write features for src/PurchasedLotto.js exceptions

--- src/PurchasedLotto.js
+++ src/PurchasedLotto.js
@@ -1,3 +1,24 @@
-class PurchasedLotto {}
+import Input from './Input';
+
+class PurchasedLotto {
+  #cash;
+
+  constructor(cash) {
+    this.#validate(cash);
+    this.#cash = cash;
+  }
+
+  #validate(cash) {
+    if (cash === null || isNaN(cash)) {
+      throw new Error('[ERROR] 숫자의 형태로 금액을 입력해주세요.');
+    }
+    if (cash <= 0) {
+      throw new Error('[ERROR] 0원 초과의 금액을 입력해주세요.');
+    }
+    if (cash % 1000 !== 0) {
+      throw new Error('[ERROR] 1000원 단위로 금액을 입력해주세요.');
+    }
+  }
+}
 
 export default PurchasedLotto;
