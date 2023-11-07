07960c705252251fa278a4a4a22be9f750846e32
gilgor2 <misby0327@gmail.com>
2023-11-07 18:41:32 +0900

    feature(Input.js): 질문자 객체 구현
    askUser , askUserUntilValid 구현

--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,23 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class Input {
-    async askUser(question) {
+	#console = Console;
 
-    }
+	async askUser(question) {
+		const reply = await this.#console.readLineAsync(question);
+		return reply;
+	}
 
-    async askUserUntilValid(question, validatorCallBack = () => true) {
-
-    }
+	async askUserUntilValid(question, validatorCallBack = () => true) {
+		let reply = await this.askUser(question);
+		const isReplyValid = validatorCallBack(reply);
+		if (!isReplyValid) {
+			reply = await this.askUserUntilValid(
+				question,
+				validatorCallBack,
+			);
+		}
+		return reply;
+	}
 }
 export default Input;
