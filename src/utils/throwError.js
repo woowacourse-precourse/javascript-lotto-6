c57a9bbeea292a48f48e299f97992305b5181df3
pjc0179 <pjc0179@gmail.com>
2023-11-07 23:24:50 +0900

    refactor(utils): throwError.js 간단한 이름변경

--- src/utils/throwError.js
+++ src/utils/throwError.js
@@ -1,7 +1,7 @@
-import MESSAGES from '../constants/message.js';
+import ERROR_MESSAGES from '../constants/errorMessage.js';
 
 const throwError = (message) => {
-  throw new Error(`${MESSAGES.error} ${message}`);
+  throw new Error(`${ERROR_MESSAGES.error} ${message}`);
 };
 
 export default throwError;
