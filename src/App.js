3bfde0b09f4e0c986f66b472cca75e3bc0c38c7d
visvaden7 <visvaden7@naver.com>
2023-11-08 01:56:53 +0900

    feat: buyAmountOfLotto() 로또 발행수 추가

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ class App {
             throw new Error("[ERROR] 잘못된 숫자를 입력하셨습니다.");
     }
 
+    async buyAmountOfLotto() {
+        while (true) {
+            try {
+                const purchasedAmount = await MissionUtils.Console.readLineAsync("구입금액을 입력해주세요.\n");
+                this.checkInputPurchasedAmount(purchasedAmount);
+                return purchasedAmount / 1000;
+            } catch (e) {
+                console.log(e.message);
+            }
+        }
+    }
+
+
 
     async play() {
+        const purchasedLottoCount = await this.buyAmountOfLotto()
 
     }
 }
