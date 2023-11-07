6d81a28403177aeafb0d218f2b743acd267e64e4
ss0526100 <ss0526100@gmail.com>
2023-11-07 23:23:58 +0900

    test: remove extra comment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-//import Fucntional from './functinoal/Functional.js';
 import MVC from './mvc/MVC.js';
 
 class App {
   async play() {
-    //await Fucntional.play();
-
     await MVC.play();
   }
 }
