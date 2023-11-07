7f2390d851465f1847ac40a74ac94442277c1f49
donggu <zzzjdg@naver.com>
2023-11-07 15:49:49 +0900

    Test : Add LottoTest
    로또 클래스에 대한 테스트 함수들 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,5 +14,32 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("숫자 범위는 1~45", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 46, 5, 7]);
+    }).toThrow("[ERROR] 당첨 번호의 범위는 1~45 입니다.");
+  });
+
+  test("숫자만 입력 받았는지", () => {
+    expect(() => {
+      new Lotto([1, 'b', 3, 'a', 5, 'c']);
+    }).toThrow("[ERROR] 숫자가 아닌 값이 존재합니다.");
+  }); 
+  
+  test("matchLotto 2", () => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      const otherLotto = [1, 2, 10, 14, 22, 45];
+      const count = lotto.matchLotto(otherLotto);
+      expect(count).toBe(2); 
+  });
+
+  test("matchLotto 5", () => {
+    const lotto = new Lotto([11, 12, 13, 14, 15, 34]);
+    const otherLotto = [11, 12, 14, 15, 34, 35];
+    const count = lotto.matchLotto(otherLotto);
+    expect(count).toBe(5); 
 });
+
+  
+});
+
