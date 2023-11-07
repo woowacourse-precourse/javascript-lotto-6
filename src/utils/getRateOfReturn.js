f9d86c50aad7e74f6bcd1bbfdaff5f5467a4111c
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-08 02:27:42 +0900

    🐞 Fix: getRateOfReturn 당첨금이 없을 경우 처리

--- src/utils/getRateOfReturn.js
+++ src/utils/getRateOfReturn.js
@@ -1,4 +1,7 @@
+import CONSTANTS from '../constants/constants.js';
+
 const getRateOfReturn = (purchaseAmount, returns) => {
+  if (returns.length === CONSTANTS.number.zero) return '0%';
   const finalValue =
     Number(purchaseAmount) + returns.reduce((acc, currentReturn) => acc + currentReturn);
   const rateOfReturn = (((finalValue - purchaseAmount) / purchaseAmount) * 100).toFixed(1);
