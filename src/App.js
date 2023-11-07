03de6986cda63904420dc720772eb4fb7741aa5d
GloomyType <rldnrdl12@naver.com>
2023-11-07 20:57:07 +0900

    feat: Implemented feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,118 @@
+import Lotto from "./Lotto"; // Lotto 클래스의 정의가 있는 파일 경로에 따라 임포트 경로를 수정해야 합니다.
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const MESSAGE1 = '구입금액을 입력해 주세요.';
+    const MESSAGE2 = '당첨 번호를 입력해 주세요.';
+    const MESSAGE3 = '보너스 번호를 입력해 주세요.';
+    const MESSAGE4 = '당첨 통계';
+    
+    let purchaseAmountStatus = true;
+    let lottoNumberStatus = true;
+    let bonusNumberStatus = true;
+    let purchaseAmount;
+    let lottoNumber;
+    let bonusNumber;
+    let purchaseCount;
+    let lottoNumbersArray;
+
+    do {
+      try {
+        purchaseAmount = await MissionUtils.Console.readLineAsync(MESSAGE1);
+        const parsedAmount = parseInt(purchaseAmount, 10);
+        if (isNaN(purchaseAmount) || parsedAmount % 1000 !== 0) {
+          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+        
+        purchaseAmount = parsedAmount;
+        purchaseCount = purchaseAmount/1000;
+        MissionUtils.Console.print(`${purchaseCount}개를 구매했습니다.`);
+        purchaseAmountStatus = false;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    } while (purchaseAmountStatus);
+
+    let randomLottoArray = [];
+    
+    for (let i = 0 ; i < purchaseCount ; i++ ) {
+      randomLottoArray[i] = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);   
+    }
+
+    randomLottoArray.sort((a, b) => a - b);
+    randomLottoArray.forEach((v) => {
+      const stringValue = `[${v.join(', ') }]`;
+      MissionUtils.Console.print(stringValue);
+    });
+
+    do {
+      try {
+        lottoNumber = await MissionUtils.Console.readLineAsync(MESSAGE2);
+        const lotto = new Lotto(lottoNumber.split(",").map(Number));
+        lottoNumbersArray = lotto.getNumbers();
+        
+        lottoNumberStatus = false;
+      } catch (error) {
+        MissionUtils.Console.print(error);
+      }
+    } while (lottoNumberStatus);
+
+    do {
+      try {
+        bonusNumber = await MissionUtils.Console.readLineAsync(MESSAGE3);
+
+        if (!(bonusNumber >= 1 && bonusNumber <= 45)) {
+          throw new Error("[ERROR] 보너스 번호는 1부터 45사이의 숫자여야 합니다.");
+        }
+
+        bonusNumberStatus = false;
+      } catch (error) {
+        MissionUtils.Console.print(error);
+      }
+    } while (bonusNumberStatus);
+
+    
+    // 당첨 티켓 개수를 초기화
+    let win3 = 0;
+    let win4 = 0;
+    let win5 = 0;
+    let win5WithBonus = 0;
+    let win6 = 0;
+
+    for (const ticket of randomLottoArray) {
+      const matchedNumbers = ticket.filter(num => lottoNumbersArray.includes(num));
+      const matchedCount = matchedNumbers.length;
+
+      if (matchedCount === 3) {
+        win3++;
+      } else if (matchedCount === 4) {
+        win4++;
+      } else if (matchedCount === 5) {
+        if (matchedNumbers.includes(bonusNumber)) {
+          win5WithBonus++;
+        } else {
+          win5++;
+        }
+      } else if (matchedCount === 6) {
+        win6++;
+      }
+    }
+
+    // 당첨 통계 출력
+    MissionUtils.Console.print(MESSAGE4);
+    MissionUtils.Console.print(`3개 일치 (5,000원) - ${win3}개`);
+    MissionUtils.Console.print(`4개 일치 (50,000원) - ${win4}개`);
+    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${win5}개`);
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${win5WithBonus}개`);
+    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${win6}개`);
+
+    // 총 수익률 계산
+    const totalPrize = win3 * 5000 + win4 * 50000 + win5 * 1500000 + win5WithBonus * 30000000 + win6 * 2000000000;
+    const totalCost = purchaseAmount;
+    const profitRate = ((totalPrize / totalCost) * 100).toFixed(1);
+    MissionUtils.Console.print(`총 수익률은 ${profitRate}%입니다.`);    
+  }
 }
 
 export default App;
