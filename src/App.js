c2732ad0968ebebfe59c034e0908b1ca84d64d5d
starcradle101 <dreamerdev12@gmail.com>
2023-11-08 05:46:17 +0900

    ✅Test($App): 전체 기능 테스트 통과 로또 프로그램 테스트 전체 통과

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { Console } from '@woowacourse/mission-utils';
+import createLottoTickets from './domain/LottoSetup';
 import {
   getBonusNumber,
   getLottoPurchaseAmount,
   getWinningLottoNumbers,
-} from './ui/Input.js';
-import Lotto from './domain/Lotto.js';
+} from './ui/Input';
+import {
+  printLottoTickets,
+  analyzeProfitRate,
+  printLottoTotalResult,
+} from './ui/Output';
 
 class App {
   constructor() {
@@ -23,7 +27,34 @@ class App {
   }
 
   async play() {
+    await this.purchaseLottoTickts();
+    await this.checkWinningTickets();
+    this.displayLottoProfit();
+  }
+
+  async purchaseLottoTickts() {
     this.lottoPurchaseAmount = await getLottoPurchaseAmount();
+    this.lottoTickets = createLottoTickets(this.lottoPurchaseAmount);
+    printLottoTickets(this.lottoTickets);
+  }
+
+  async checkWinningTickets() {
+    const lotto = await getWinningLottoNumbers();
+    this.winningLotto = lotto.getLottoNumbers();
+    this.bonusNumber = await getBonusNumber(this.winningLotto);
+    this.lottoResult = lotto.evaluateLottoTickets(
+      this.lottoTickets,
+      this.bonusNumber,
+      this.lottoResult,
+    );
+  }
+
+  displayLottoProfit() {
+    this.profitRate = analyzeProfitRate(
+      this.lottoPurchaseAmount,
+      this.lottoResult,
+    );
+    printLottoTotalResult(this.lottoResult, this.profitRate);
   }
 }
 
