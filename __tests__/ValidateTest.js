7765deab13b08d3ce4c6f8d64f4fbf9ced13cdaa
youngchann <lych96@gmail.com>
2023-11-08 03:51:33 +0900

    test: 상수 스네이크 표기법으로 수정
    - 테스트에 필요한 값을 상수로 선언하여 사용하도록 수정

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -3,46 +3,54 @@ import ValidatePurchase from '../src/module/ValidatePurchase.js';
 
 describe('구입금액 입력 예외 테스트', () => {
   test('구입금액이 숫자가 아니면 예외가 발생한다.', () => {
+    const USER_INPUT = '가나다';
     expect(() => {
-      new ValidatePurchase('가나다');
+      new ValidatePurchase(USER_INPUT);
     }).toThrow('[ERROR] 구입금액을 숫자로 입력해 주세요.');
   });
 
   test('구입금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.', () => {
+    const USER_INPUT = '1500';
     expect(() => {
-      new ValidatePurchase('1500');
+      new ValidatePurchase(USER_INPUT);
     }).toThrow('[ERROR] 1000원 단위로 입력해 주세요.');
   });
 
   test('구입금액에 빈 공간이 입력되면 예외가 발생한다.', () => {
+    const USER_INPUT = '1500 ';
     expect(() => {
-      new ValidatePurchase('1500 ');
+      new ValidatePurchase(USER_INPUT);
     }).toThrow('[Error] 입력에 빈 공간이 있으면 안됩니다.');
   });
 });
 
 describe('보너스 입력 예외처리 테스트', () => {
   test('보너스 번호가 숫자가 아니면 예외가 발생한다.', () => {
+    const USER_INPUT = '가';
     expect(() => {
-      new ValidateBonus('가');
+      new ValidateBonus(USER_INPUT);
     }).toThrow('[Error] 보너스 번호는 숫자여야 합니다.');
   });
 
   test('보너스 번호의 범위가 1~45가 아니면 예외가 발생한다.', () => {
+    const USER_INPUT = '99';
     expect(() => {
-      new ValidateBonus('99');
+      new ValidateBonus(USER_INPUT);
     }).toThrow('[Error] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.');
   });
 
   test('보너스 번호가 당첨 번호가 중복되면 예외가 발생한다.', () => {
+    const USER_INPUT = '4';
+    const WINNING_NUMBERS = '1,2,3,4,5,6';
     expect(() => {
-      new ValidateBonus('4','1,2,3,4,5,6');
+      new ValidateBonus(USER_INPUT, WINNING_NUMBERS);
     }).toThrow('[Error] 보너스 번호는 로또 번호와 중복되지 않아야 합니다.');
   });
 
   test('보너스 번호에 빈 공간이 입력되면 예외가 발생한다.', () => {
+    const USER_INPUT = ' 3';
     expect(() => {
-      new ValidateBonus(' 3');
+      new ValidateBonus(USER_INPUT);
     }).toThrow('[Error] 입력에 빈 공간이 있으면 안됩니다.');
   });
 });
