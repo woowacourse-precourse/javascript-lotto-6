ddaeaab98466ae53ccfcdc4a61828587553517e7
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-07 23:31:18 +0900

    test: 로또 번호 생성 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,30 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../src/Lotto.js';
+import { LOTTO } from '../src/config.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // 아래에 추가 테스트 작성 가능
+  describe('로또 번호 테스트', () => {
+    test('생성한 로또 번호 중복 테스트', () => {
+      const numbers = Lotto.generateLottoNumbers();
+      const set = new Set(numbers);
+      expect(set.size).toBe(numbers.length);
+    });
+    test('생성한 로또 번호 길이 테스트', () => {
+      const numbers = Lotto.generateLottoNumbers();
+      expect(numbers.length).toBe(LOTTO.COUNT);
+    });
+  });
 });
