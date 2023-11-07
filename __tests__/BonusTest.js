9848f5b538f6979eadfad7bd70f2c7c878e961e9
PARK-CHANUK <doosan1224@naver.com>
2023-11-07 10:41:35 +0900

    test(BonusTest): 테스트 케이스 추가 case: 1부터 45사이의 당첨번호와 중복되지 않는 숫자만 유효합니다.

--- __tests__/BonusTest.js
+++ __tests__/BonusTest.js
@@ -4,8 +4,17 @@ import ERROR_MESSAGE from '../src/constants/error';
 describe('보너스 번호 테스트', () => {
   let winningLotto;
   beforeEach(() => {
-    winningLotto = [1, 2, 3, 4, 5, 6];
+    winningLotto = [10, 11, 12, 13, 14, 15];
   });
+
+  test.each(['1', '7', '8', '9', '45'])(
+    '1부터 45사이의 당첨번호와 중복되지 않는 숫자만 유효합니다.',
+    (input) => {
+      expect(() => {
+        new Bonus(input, winningLotto);
+      }).not.toThrow();
+    },
+  );
 
   test.each(['', ' ', 'a', 'abc', '1f'])(
     '숫자를 입력하지 않으면 예외처리한다.',
@@ -26,7 +35,7 @@ describe('보너스 번호 테스트', () => {
   );
 
   test('당첨번호와 보너스번호가 중복되면 예외처리한다.', () => {
-    const bonus = '1';
+    const bonus = '10';
     expect(() => {
       new Bonus(bonus, winningLotto);
     }).toThrow(ERROR_MESSAGE.duplicatedWinningLotto);
