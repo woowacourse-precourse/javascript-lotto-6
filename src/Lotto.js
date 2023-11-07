fff381e4b1f404c5d93d3b92577b08bc8bb9fa27
soyunim <togksk123@naver.com>
2023-11-07 09:20:08 +0900

    feat: 당첨 복권 번호에 대한 예외를 Lotto클래스에 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,35 @@
+import { ERROR_MESSAGE } from "./constants/errorMessage";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.#validate2(numbers);
+    this.#validate3(numbers);
     this.#numbers = numbers;
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR_MESSAGE.NO_WINNING_NUMBERS);
     }
   }
 
   // TODO: 추가 기능 구현
+  #validate2(numbers) {
+    if (new Set(numbers).size !== 6) {
+      throw new Error(ERROR_MESSAGE.INPUT_DUPLICATION);
+    }
+  }
+
+  #validate3(number) {
+    for(let i=0; i<6; i++){
+      if(parseInt(number[i])>45 || parseInt(number[i])<1){
+          throw new Error(ERROR_MESSAGE.OUT_OF_RANGE);
+      }
+    }
+  }
 }
 
 export default Lotto;
