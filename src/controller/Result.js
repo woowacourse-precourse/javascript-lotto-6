6dc33dd742aff99293edaec8fbed778131731f14
soominnnn <marisol03@naver.com>
2023-11-08 01:27:25 +0900

    ✨feat: printWinningStastics 메서드 구현

--- src/controller/Result.js
+++ src/controller/Result.js
@@ -14,8 +14,19 @@ class Result {
     return this.winMoney;
   }
   static calculrateRateOfReturn(userPurchaseAmount) {
-    const RateOfReturn = (this.winMoney - userPurchaseAmount) / userPurchaseAmount * 100;
+    this.RateOfReturn = (this.winMoney - userPurchaseAmount) / userPurchaseAmount * 100;
     return RateOfReturn;
+  }
+  static printWinningStastics(winningInFive) {
+    Console.print(`
+      당첨통계
+      ---
+      3개 일치 (5,000원) - ${this.winningThird}개
+      4개 일치 (50,000원) - ${this.winningForth}개
+      5개 일치 (1,500,000원) - ${winningInFive[0]}개
+      5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningInFive[1]}개
+      6개 일치 (2,000,000,000원) - ${this.winningSix}개
+      총 수익률은 ${(100-(-this.winMoney)).toFixed(1)}%입니다.`)
   }
 }
 
