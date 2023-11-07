4ef8eb9ecb18955ce56151846e165a5a746e9890
MinGu Jeong <mingu830@gmail.com>
2023-11-07 12:28:19 +0900

    feat: 로또 게임 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Console } from '@woowacourse/mission-utils';
 import { Input } from './domain/Input.js';
+import Output from './domain/Output.js';
+import GenerateLotto from './domain/GenerateLotto.js';
+import isWinningLotto from './domain/CheckLotto.js';
+import ROI from './domain/CalculateROI.js';
+import Lotto from './domain/Lotto.js';
 
 class App {
   async play() {
-    const amount = new Input();
+    const lotto = new Input();
+    const print = new Output();
+    const amount = await lotto.getAmount();
+    new Lotto(amount);
+    const ticketCount = amount / 1000;
+    const lottoNumbers = GenerateLotto(ticketCount);
+    print.printLotto(ticketCount, lottoNumbers);
+    const winningLotto = await lotto.getWinningNumber();
+    new Lotto(winningLotto);
+    const bonusNumber = await lotto.getBonusNumber();
+    new Lotto(bonusNumber);
+    const winningNumber = isWinningLotto(
+      winningLotto,
+      bonusNumber,
+      lottoNumbers
+    );
+    const roi = ROI(amount, winningNumber);
+    print.printResult(winningNumber, roi);
   }
 }
-
 export default App;
