19aa8343667911ed3b454b776079cc7895f5d968
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-11-07 18:27:30 +0900

    test: 당첨번호 유효범위 테스트 코드 구현
    입력받은 당첨 번호 숫자들의 유효범위를 테스트하는 코드를 구현하였습니다.

--- __tests__/GenerateWinningNumberTest.js
+++ __tests__/GenerateWinningNumberTest.js
@@ -30,9 +30,7 @@ describe("당첨 번호 생성", () => {
     const app = new App();
 
     // then
-    await expect(app.play()).rejects.toThrow(
-      "[ERROR] 로또 번호는 6개여야 합니다."
-    );
+    await expect(app.play()).rejects.toThrow("[ERROR] 로또 번호는 6개여야 합니다.");
   });
 
   test("당첨 번호가 숫자가 아닌 경우", async () => {
@@ -42,8 +40,16 @@ describe("당첨 번호 생성", () => {
     const app = new App();
 
     // then
-    await expect(app.play()).rejects.toThrow(
-      "[ERROR] 로또 번호는 숫자로만 이루어져야 합니다."
-    );
+    await expect(app.play()).rejects.toThrow("[ERROR] 로또 번호는 숫자로만 이루어져야 합니다.");
+  });
+
+  test("당첨 번호가 유효 범위 숫자가 아닌 경우", async () => {
+    mockRandoms([[1, 2, 3, 4, 5, 6]]);
+    mockQuestions(["1000", "1,2,3,4,5,55", "7"]);
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR] 1에서 45 사이에 숫자만 입력이 가능합니다.");
   });
 });
