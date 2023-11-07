a883bb0fbc39f7286c516996cef3fdf5a53af785
sunub <bsc5672@gmail.com>
2023-11-08 01:11:14 +0900

    feat : 로또 티켓 테스트를 통과하기 위해 Lotto 클래스에 추가 메소드 작성
    로또 클래스에 numbers에 대한 정보에 접근 가능할 수 있는 getter와 더 자세한 validate 메소드 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,17 +2,42 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+    try {
+      this.#validate(numbers);
+      this.#numbers = numbers;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (!this.isValidSize(numbers) || this.isOverlapped(numbers) || !this.isEveryNumsInRange(numbers)) {
+      throw new Error('[ERROR] : 조건에 맞는 로또 번호가 생성되지 않았습니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  get numbers() {
+    return Array.from(this.#numbers);
+  }
+
+  isOverlapped(numbers) {
+    const set = new Set(numbers);
+    return set.size !== 6;
+  }
+
+  isValidSize(numbers) {
+    if (numbers.length !== 6) return false;
+    return true;
+  }
+
+  isValidRangeNum(num) {
+    return num >= 1 && num <= 45;
+  }
+
+  isEveryNumsInRange(numbers) {
+    return numbers.every((num) => this.isValidRangeNum(num));
+  }
 }
 
 export default Lotto;
