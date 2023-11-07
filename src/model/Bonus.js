5e46c8146d02f1a8a8d61477e3646f576ef6b387
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-07 12:20:22 +0900

    feat(validation): 로또 번호 범위 유효성 검사 추가

--- src/model/Bonus.js
+++ src/model/Bonus.js
@@ -1,4 +1,6 @@
 import { ERROR_MESSAGE } from "../constants/errors.js";
+import { LOTTO_NUMBERS } from "../constants/numbers.js";
+import { stringToNumber } from "../utils/conversion.js";
 
 class Bonus {
   // Bonus 예시: "3" 과 같은 문자열
@@ -8,6 +10,7 @@ class Bonus {
     this.#string = string;
     this.#isEmpty(string);
     this.#isNotNumber(string);
+    this.#isNumberInRange(string);
   }
 
   #isEmpty(string) {
@@ -19,8 +22,15 @@ class Bonus {
 
   #isNotNumber(string) {
     // 숫자인지 체크
-    if (isNaN(Number(string))) {
+    if (isNaN(stringToNumber(string))) {
       throw new Error(ERROR_MESSAGE.typeDismatching);
+    }
+  }
+
+  #isNumberInRange(string) {
+    const number = stringToNumber(string);
+    if (!(LOTTO_NUMBERS.minimum <= number && number <= LOTTO_NUMBERS.maximum)) {
+      throw new Error(ERROR_MESSAGE.outOfRange);
     }
   }
 }
--- src/model/Winning.js
+++ src/model/Winning.js
@@ -1,6 +1,7 @@
 import { ERROR_MESSAGE } from "../constants/errors.js";
 import { SEPARATOR } from "../constants/messages.js";
-import { COUNT } from "../constants/numbers.js";
+import { COUNT, LOTTO_NUMBERS } from "../constants/numbers.js";
+import { divideIntoCommas, stringToNumber } from "../utils/conversion.js";
 
 class Winning {
   // Winning 예시: "1,2,3,4,5,6" 과 같은 문자열
@@ -13,6 +14,7 @@ class Winning {
     this.#isArrayHasEmpty(string);
     this.#isNotAllNumbers(string);
     this.#isDuplicated(string);
+    this.#isNumberInRange(string);
   }
 
   #isEmpty(string) {
@@ -24,14 +26,14 @@ class Winning {
 
   #mismatchedCount(string) {
     // ,를 기준으로 나누었을 때 길이가 COUNT.winningNumbers가 아닌 경우
-    if (string.split(SEPARATOR).length !== COUNT.winningNumbers) {
+    if (divideIntoCommas(string).length !== COUNT.winningNumbers) {
       throw new Error(ERROR_MESSAGE.winningCount);
     }
   }
 
   #isArrayHasEmpty(string) {
     // 배열이 되었을 때 공백이 포함되는 경우
-    const stringArr = string.split(SEPARATOR);
+    const stringArr = divideIntoCommas(string);
     for (const string of stringArr) {
       if (!string.trim().length) {
         throw new Error(ERROR_MESSAGE.hasEmpty);
@@ -41,9 +43,9 @@ class Winning {
 
   #isNotAllNumbers(string) {
     // 배열에 다른 타입의 값이 들어있는지 검사
-    const stringArr = string.split(SEPARATOR);
+    const stringArr = divideIntoCommas(string);
     for (const string of stringArr) {
-      const number = Number(string.trim()); // 공백을 제거하고 숫자로 변환
+      const number = stringToNumber(string.trim()); // 공백을 제거하고 숫자로 변환
       if (isNaN(number)) {
         throw new Error(ERROR_MESSAGE.arrHasTypeDismatching);
       }
@@ -53,12 +55,23 @@ class Winning {
   #isDuplicated(string) {
     // 각 숫자들이 중복되었을 경우
     let checkDuplicateSet = new Set();
-    const numbers = string.split(SEPARATOR);
+    const numbers = divideIntoCommas(string);
     for (const number of numbers) {
       if (checkDuplicateSet.has(number)) {
         throw new Error(ERROR_MESSAGE.isNumberDuplicated);
       }
       checkDuplicateSet.add(number);
+    }
+  }
+
+  #isNumberInRange(string) {
+    const numbers = divideIntoCommas(string);
+    for (const number of numbers) {
+      if (
+        !(LOTTO_NUMBERS.minimum <= number && number <= LOTTO_NUMBERS.maximum)
+      ) {
+        throw new Error(ERROR_MESSAGE.outOfRange);
+      }
     }
   }
 }
