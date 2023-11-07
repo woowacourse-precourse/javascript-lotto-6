bf63fe2c972b59db8bc343461509a54a1fe496b5
skdbsqls <skdbsqls98@naver.com>
2023-11-07 19:04:59 +0900

    feat(message): 보너스 번호 입력 메시지 및 오류 메시지 상수화

--- src/constants/message.js
+++ src/constants/message.js
@@ -2,6 +2,7 @@ const MESSAGE = Object.freeze({
   input: {
     price: "구입금액을 입력해 주세요.\n",
     winningNum: "당첨 번호를 입력해 주세요.\n",
+    bonusNum: "\n보너스 번호를 입력해 주세요.\n",
   },
   output: {
     amount: "개를 구매했습니다.",
@@ -13,6 +14,9 @@ const MESSAGE = Object.freeze({
     notLength: "[ERROR] 당첨 번호는 6개의 숫자여야 합니다.",
     notRange: "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.",
     repeatNum: "[ERROR] 당첨 번호는 중복되지 않는 숫자여야 합니다.",
+    notBonusRange: "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.",
+    repeatBonusNum:
+      "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 숫자여야 합니다.",
   },
 });
 export default MESSAGE;
