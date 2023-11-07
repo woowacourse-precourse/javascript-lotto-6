800f40b635788f1b4a468c3f9c48f094c1b70993
eunchong <suicas@naver.com>
2023-11-08 04:35:06 +0900

    로또 구입 금액 입력 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from '../src/Lotto.js';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const purchaseAmount = await this.inputPurchaseAmount();
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      this.play();
+    }
+  }
+
+  async inputPurchaseAmount() {
+    const input = await MissionUtils.Console.readLineAsync(
+      '구입 금액을 입력해 주세요.'
+    );
+    const amount = Number(input);
+    if (Number.isNaN(amount) || amount % 1000 !== 0) {
+      throw new Error(
+        '[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.'
+      );
+    }
+    return amount;
+  }
 }
 
 export default App;
