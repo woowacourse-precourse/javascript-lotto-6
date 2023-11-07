96a0a367a62914966d519d08f48fe3adb748ff46
yeokyoung song <spolice0324@gmail.com>
2023-11-07 20:46:11 +0900

    feat : 사용자의 입력을 받는 입력 메세지 및 에러 메세지 출력 상수화
    - Input Message 및 Error Message 정의

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,5 +14,15 @@ const LOTTO_INFO = Object.freeze({
   },
 });
 
+const INPUT_MESSAGE = Object.freeze({
+  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
+});
 
-export {LOTTO_INFO};
+const ERROR_MESSAGE = Object.freeze({
+  INVALID_INPUT_FORMAT: `[ERROR] 입력 형태가 맞지 않습니다. 게임을 종료합니다.`,
+  TYPE_ERROR: `[ERROR] 정수가 아닙니다. 다시 입력해주세요.`,
+});
+
+export { LOTTO_INFO, INPUT_MESSAGE, ERROR_MESSAGE };
