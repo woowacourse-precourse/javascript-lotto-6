c102dd3fe79b4168c70d6c8165085ad2aedf1f44
yongZin-MAC <>
2023-11-07 13:24:34 +0900

    [로또 게임] 로또 게임 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.#addValidate(numbers);
     this.#numbers = numbers;
   }
 
@@ -12,7 +13,38 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  #addValidate(numbers){
+    if (
+      numbers.some((number, index) => 
+        number < 1 ||
+        number > 45 ||
+        numbers.indexOf(number) !== index
+      )
+    ) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 중복되지 않는 숫자여야 합니다.");
+    }
+  }
+
+  checkLotto(WINNING_NUMBERS, BONUS_NUMBER) {
+    const MATCHING_LOTTO = this.#numbers.filter((number) => WINNING_NUMBERS.includes(number));
+    const MATCHING_COUNT = MATCHING_LOTTO.length;
+
+    if (MATCHING_COUNT === 6) {
+      return "6개 일치";
+    } else if (MATCHING_COUNT === 5 && this.#numbers.includes(BONUS_NUMBER)) {
+      return "5개 일치, 보너스 볼 일치";
+    } else if (MATCHING_COUNT === 5) {
+      return "5개 일치";
+    } else if (MATCHING_COUNT === 4) {
+      return "4개 일치";
+    } else if (MATCHING_COUNT === 3) {
+      return "3개 일치";
+    }
+  }
+
+  toString() {
+    return `[${this.#numbers.join(", ")}]`;
+  }
 }
 
 export default Lotto;
