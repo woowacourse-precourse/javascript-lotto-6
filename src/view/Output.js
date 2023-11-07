afe7a9d129becb7b3695631ed447c2ef6bbe49e7
soominnnn <marisol03@naver.com>
2023-11-08 00:39:50 +0900

    ✨feat: printRandomLottoNumber 메서드 구현

--- src/view/Output.js
+++ src/view/Output.js
@@ -1,8 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class Output {
-  printPurchaseCount(purchaseAmount) {
+  static printPurchaseCount(purchaseAmount) {
     Console.print(`\n${purchaseAmount}개를 구매했습니다.`);
+  }
+  static printRandomLottoNumber(RandomLottoNumber) {
+    const FORMATTED_ARRAY = RandomLottoNumber.sort((a,b) => a - b).join(', ');
+    Console.print(`[${FORMATTED_ARRAY}]`);
   }
 }
 
