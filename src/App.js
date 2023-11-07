2f76e3e867a156428780c0912af05e02b0d2f824
minseok cho <whalstjr199927@gmail.com>
2023-11-07 17:40:18 +0900

    printLottos 메서드 구현

--- src/App.js
+++ src/App.js
@@ -24,6 +24,13 @@ class App {
             return new Lotto(numbers);
         });
     }
+
+    #printLottos(lottos) {
+        Console.print(`${lottos.length}개를 구매했습니다.`);
+        lottos.forEach((lotto) =>
+            Console.print(`[${lotto.numbers.join(", ")}]`)
+        );
+    }
 }
 
 export default App;
