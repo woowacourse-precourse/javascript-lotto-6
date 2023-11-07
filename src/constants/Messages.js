1b3225a9135f9ed19017577564a7dc3d366b9e1e
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-11-08 00:11:21 +0900

    Refactor : 상수 메세지 통일

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -7,11 +7,11 @@ export const INPUT_MESSAGES = {
 export const OUTPUT_MESSAGES = {
   PURCHASE_QUANTITY: "개를 구매했습니다.",
   WINNING_STATISTICS: "\n당첨 통계\n---",
-  CORRECT_COUNT_OUTPUT_3: "3개 일치 (5,000원) - ",
-  CORRECT_COUNT_OUTPUT_4: "4개 일치 (50,000원) - ",
-  CORRECT_COUNT_OUTPUT_5: "5개 일치 (1,500,000원) - ",
+  CORRECT_COUNT_3: "3개 일치 (5,000원) - ",
+  CORRECT_COUNT_4: "4개 일치 (50,000원) - ",
+  CORRECT_COUNT_5: "5개 일치 (1,500,000원) - ",
   CORRECT_COUNT_BONUS: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
-  CORRECT_COUNT_OUTPUT_6: "6개 일치 (2,000,000,000원) - ",
+  CORRECT_COUNT_6: "6개 일치 (2,000,000,000원) - ",
   RATE_OF_RETURN: "총 수익률은 ",
 };
 
