e81573e00ea0a95c5bd83300f34afd187617c20c
Brokyeom <goindol40@gmail.com>
2023-11-07 10:05:54 +0900

    feat : 로또 생성 로직 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,28 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
+  }
+
+  get numbers() {
+    return this.#numbers;
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
   }
-
-  // TODO: 추가 기능 구현
 }
-
 export default Lotto;
