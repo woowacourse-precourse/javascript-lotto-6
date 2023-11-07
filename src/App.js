f4539421ca3d06c350cbc99b5d69aaa623f4cd2a
limdumb <dlaruddls@nate.com>
2023-11-07 23:12:46 +0900

    feat: Play 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+import { getPayLottoAmount } from "./function/getPayLottoAmount.js";
+import { getWinningCount } from "./function/getWinningCount.js";
+import { calculateLottoCount } from "./function/util/calculateLottoCount.js";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const userPayAmount = await getPayLottoAmount();
+      const lottoCount = calculateLottoCount(userPayAmount);
+      const lottos = Lotto.buyLotto(userPayAmount, lottoCount);
+      const winNumbers = await getWinningCount();
+      const resultLotto = new Lotto(winNumbers);
+      await resultLotto.printLottoResult(userPayAmount, lottos);
+    } catch (err) {
+      Console.print(err.message);
+    }
+  }
 }
 
 export default App;
