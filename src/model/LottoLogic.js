050219fffc83dd90b3ce9f2a1be5097d4508f20f
Jiwon kang <kk3012002@gmail.com>
2023-11-07 14:06:03 +0900

    add: 로또 로직 구현, 로또 클래스 내부 메서드 추가, README 업데이트

---
+++ src/model/LottoLogic.js
@@ -0,0 +1,104 @@
+import Lotto from "./Lotto.js";
+import BonusLotto from "./BonusLotto.js";
+import Constants from "../utils/Constants.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class LottoLogic {
+  // 로또 로직을 돌린 후에 결과를 반환
+  // 결과는 배열 형태로 반환
+
+  #lotto; // 당첨번호
+  #bonusLotto; // 보너스번호
+  #count; // 구매한 로또 개수
+
+  constructor(lotto, bonusLotto, count) {
+    this.#lotto = lotto;
+    this.#bonusLotto = bonusLotto;
+    this.#count = count;
+  }
+
+  async start() {
+    const result = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      miss: 0,
+    };
+
+    const generatedLottos = await this.#generateLottos(this.#count);
+    const lotto = this.#lotto;
+    const bonusNum = await this.#bonusLotto.getNumber();
+
+    for (let i = 0; i < this.#count; i++) {
+      await this.#getResult(lotto, generatedLottos, i, result, bonusNum);
+    }
+
+    return result;
+  }
+
+  async #getResult(lotto, generatedLottos, i, result, bonusNum) {
+    const matchCount = await lotto.compare(generatedLottos[i]);
+    if (matchCount === 6) {
+      result[1]++;
+      return;
+    }
+    if (matchCount === 5) {
+      this.#checkBonusNum(generatedLottos, i, result, bonusNum);
+      return;
+    }
+    if (matchCount === 4) {
+      result[4]++;
+      return;
+    }
+    if (matchCount === 3) {
+      result[5]++;
+      return;
+    }
+    result["miss"]++;
+    return;
+  }
+
+  async #checkBonusNum(generatedLottos, i, result, bonusNum) {
+    if (await generatedLottos[i].isBonusInclude(bonusNum)) {
+      result[2]++;
+      return;
+    } else {
+      result[3]++;
+      return;
+    }
+  }
+
+  async #generateLotto() {
+    const randomArr = await this.#randomArr();
+    await randomArr.sort((a, b) => a - b);
+    const lotto = new Lotto(randomArr);
+    return lotto;
+  }
+
+  async #generateLottos(count) {
+    const result = [];
+    for (let i = 0; i < count; i++) {
+      result.push(await this.#generateLotto());
+    }
+    return result;
+  }
+
+  async #randomArr() {
+    const constants = new Constants();
+    return MissionUtils.Random.pickUniqueNumbersInRange(
+      constants.getLottoNumberMin(),
+      constants.getLottoNumberMax(),
+      constants.getLottoNumberCount()
+    );
+  }
+}
+
+// test done!
+// const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+// const bonusLotto = new BonusLotto(7);
+// const lottoLogic = new LottoLogic(lotto, bonusLotto, 100000);
+// console.log(await lottoLogic.start());
+
+export default LottoLogic;
