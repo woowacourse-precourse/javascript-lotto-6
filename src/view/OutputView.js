1953a99a607c9ea5227e288e52b544beed7620b5
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-07 13:39:09 +0900

    feat: 에러메시지 출력 통합 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -24,13 +24,8 @@ const OutputView = {
   printRateOfReturn(rateOfReturn) {
     Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
   },
-  printPurchasePriceError(error) {
-    if (error.message.includes(MESSAGE_ERROR.purchasePrice)) {
-      Console.print(error.message);
-    }
-    if (error.message.includes(MESSAGE_ERROR.blank)) {
-      Console.print(error.message);
-    }
+  printErrorMessage(error) {
+    Console.print(error.message);
   },
 };
 
