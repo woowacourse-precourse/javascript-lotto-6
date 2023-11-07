d69d23b661fd7264dc0589301dc96a1e9371ce0d
yoovin <soo22839@gmail.com>
2023-11-07 23:35:32 +0900

    feat: 로또 당첨번호 validate 구현

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ class App {
 
     buyLotto(pay) {
         if (pay.match(/\D/)) {
-            throw '[ERROR] 숫자만 입력해주세요.';
+            throw new Error('[ERROR] 숫자만 입력해주세요.');
         }
 
         pay = Number(pay);
 
         if (pay < 1000) {
-            throw '[ERROR] 1000원 이상의 금액을 입력해주세요.';
+            throw new Error('[ERROR] 1000원 이상의 금액을 입력해주세요.');
         }
 
         if (pay % 1000 !== 0) {
-            throw '[ERROR] 1000원 단위로 입력해주세요.';
+            throw new Error('[ERROR] 1000원 단위로 입력해주세요.');
         }
 
         const count = pay / 1000;
@@ -32,6 +32,28 @@ class App {
         return new Lotto(
             MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
         );
+    }
+
+    getWinNumbers(numbers) {
+        numbers = numbers.split(',').map((number) => Number(number));
+        if (numbers.some((number) => isNaN(number))) {
+            throw new Error('[ERROR] 숫자가 아닌 문자가 있습니다.');
+        }
+
+        if (numbers.length !== 6) {
+            throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+        }
+
+        if (new Set(numbers).size !== 6) {
+            throw new Error('[ERROR] 당첨 번호에 중복된 숫자가 있습니다.');
+        }
+
+        numbers.forEach((number) => {
+            if (number < 1 || number > 45) {
+                throw new Error('[ERROR] 당첨 번호는 1부터 45사이여야 합니다.');
+            }
+        });
+        return numbers;
     }
 }
 
