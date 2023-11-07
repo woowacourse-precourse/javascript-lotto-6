5044ba2d957a666842387e2b592070939032bd27
mmeat512 <wndus0958@naver.com>
2023-11-07 17:41:56 +0900

    feat: 중복숫자체크 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { LOTTO } from './constants/lotto';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +9,19 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers.length !== LOTTO.length) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+
+    if (new Set(numbers).size !== LOTTO.length) {
+      throw new Error('[ERROR] 중복된 숫자가 존재합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
