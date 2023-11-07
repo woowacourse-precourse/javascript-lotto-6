26ddb9c961820c6c030cfdfc1a32857bfba9e40c
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 13:30:24 +0900

    :recycle: refactor: 기능 메서드별 주석추가 및 변수명 수정, 상수연결

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -5,6 +5,7 @@ import {
   PRICE_PER_TICKET,
   INITIAL_STATISTICS,
   PRIZE_KEY,
+  RANK,
 } from '../constants/constants.js';
 
 class LottoGame {
@@ -14,21 +15,36 @@ class LottoGame {
     this.#purchasedLotto = this.#generateLottoTickets(amount);
   }
 
+  /**
+   * 랜덤값을 담은 로또 인스턴스를 수량만큼 생성하여 배열로 반환한다.
+   * @param {number} amount
+   * @returns { Lotto[] }
+   */
   #generateLottoTickets(amount) {
     return Array.from({ length: amount }, () => new Lotto(randomNumberGenerator()));
-  }
-
-  // 티켓 수만큼 로또 비교 결과를 얻는 메서드 [{matchingCount, hasBonusNumber}]
-  getLottoComparisonResults(winningNumbers, bonusNumber) {
-    return this.#purchasedLotto.map((lotto) =>
-      lotto.getLottoComparisonResults(winningNumbers, bonusNumber)
-    );
   }
 
   getPurchasedLotto() {
     return this.#purchasedLotto.map((lotto) => lotto.getSortedLotto());
   }
 
+  /**
+   * 모든 로또들의 당첨번호 일치카운트와 보너스번호 포함여부를 반환한다.
+   * @param { number[] } winningNumbers
+   * @param { number } bonusNumber
+   * @returns { [{ matchingCount: number, hasBonusNumber: boolean }] } comparisonResults
+   */
+  getComparisonResults(winningNumbers, bonusNumber) {
+    return this.#purchasedLotto.map((lotto) =>
+      lotto.getLottoComparisonResults(winningNumbers, bonusNumber)
+    );
+  }
+
+  /**
+   * 모든 로또들의 각 당첨등수를 카운팅한 통계를 반환한다.
+   * @param { [{ matchingCount: number, hasBonusNumber: boolean }] } comparisonResults
+   * @returns { { [PRIZE_KEY] : number } } statistics
+   */
   getStatistics(comparisonResults) {
     const statistics = { ...INITIAL_STATISTICS };
 
@@ -40,22 +56,32 @@ class LottoGame {
   }
 
   #updateStatistics(statistics, matchingCount, hasBonusNumber) {
-    if (matchingCount === 6) statistics[PRIZE_KEY.firstPrize] += 1;
-    if (matchingCount === 5 && hasBonusNumber) statistics[PRIZE_KEY.secondPrize] += 1;
-    if (matchingCount === 5 && !hasBonusNumber) statistics[PRIZE_KEY.thirdPrize] += 1;
-    if (matchingCount === 4) statistics[PRIZE_KEY.fourthPrize] += 1;
-    if (matchingCount === 3) statistics[PRIZE_KEY.fifthPrize] += 1;
+    if (matchingCount === RANK.first) statistics[PRIZE_KEY.firstPrize] += 1;
+    if (matchingCount === RANK.second && hasBonusNumber) statistics[PRIZE_KEY.secondPrize] += 1;
+    if (matchingCount === RANK.third && !hasBonusNumber) statistics[PRIZE_KEY.thirdPrize] += 1;
+    if (matchingCount === RANK.fourth) statistics[PRIZE_KEY.fourthPrize] += 1;
+    if (matchingCount === RANK.fifth) statistics[PRIZE_KEY.fifthPrize] += 1;
   }
 
-  calcTotalPrizeAmount(statistics) {
-    return Object.entries(statistics).reduce((acc, [prize, count]) => {
-      return acc + PRIZE_AMOUNT[prize] * count;
+  /**
+   * 모든 당첨 등수의 상금을 합산한 값을 반환한다.
+   * @param { { [PRIZE_KEY] : number } } statistics
+   * @returns { number } - totalPrizeAmount
+   */
+  getTotalPrizeAmount(statistics) {
+    return Object.entries(statistics).reduce((acc, [prizeKey, count]) => {
+      return acc + PRIZE_AMOUNT[prizeKey] * count;
     }, 0);
   }
 
+  /**
+   * 소수점 첫번째 자리까지 반올림한 총 수익률을 계산하여 반환한다.
+   * @param { number } - totalPrizeAmount
+   * @returns { number } - 총 수익률
+   */
   getProfitRatio(totalPrizeAmount) {
-    const profitRatio = (totalPrizeAmount / (this.#purchasedLotto.length * PRICE_PER_TICKET)) * 100;
-    return profitRatio.toFixed(1);
+    const totalTicketCost = this.#purchasedLotto.length * PRICE_PER_TICKET;
+    return ((totalPrizeAmount / totalTicketCost) * 100).toFixed(1);
   }
 }
 
