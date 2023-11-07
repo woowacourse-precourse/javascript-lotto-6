3a72679adc4630e81d1149f2eb904a416427de21
yeokyoung song <spolice0324@gmail.com>
2023-11-08 03:49:35 +0900

    refactor : inputValidator 로직 개선
    - InputCheck와 InputValidator로 나누어 선언, 로직 분리

--- src/Validator/inputValidator.js
+++ src/Validator/inputValidator.js
@@ -1,34 +1,34 @@
 import { ERROR_MESSAGE } from '../Utils/constants';
 
-const checkInputFormat = input => {
-  if (input === undefined) throw new Error(ERROR_MESSAGE.INVALID_INPUT_FORMAT);
+const InputCheck = {
+  checkInputFormat: input => {
+    if (input === undefined)
+      throw new Error(ERROR_MESSAGE.INVALID_INPUT_FORMAT);
+  },
+
+  checkInputType: input => {
+    if (Number(input).toString() !== input) {
+      throw new Error(ERROR_MESSAGE.TYPE_ERROR);
+    }
+  },
+};
+const InputValidator = {
+  purchaseInput: input => {
+    InputCheck.checkInputFormat(input);
+    InputCheck.checkInputType(input);
+  },
+
+  winningNumbers: winningNumbers => {
+    InputCheck.checkInputFormat(winningNumbers);
+    winningNumbers.split(',').forEach(e => {
+      InputCheck.checkInputType(e);
+    });
+  },
+
+  bonusNumber: bonusNumber => {
+    InputCheck.checkInputFormat(bonusNumber);
+    InputCheck.checkInputType(bonusNumber);
+  },
 };
 
-const checkInputType = input => {
-  if (Number(input).toString() !== input) {
-    throw new Error(ERROR_MESSAGE.TYPE_ERROR);
-  }
-};
-
-const purchaseInput = input => {
-  checkInputFormat(input);
-  checkInputType(input);
-};
-
-const winningNumbers = winningNumbers => {
-  checkInputFormat(winningNumbers);
-  winningNumbers.split(',').forEach(e => {
-    checkInputType(e);
-  });
-};
-
-const bonusNumber = bonusNumber => {
-  checkInputFormat(bonusNumber);
-  checkInputType(bonusNumber);
-};
-
-export default {
-  purchaseInput,
-  winningNumbers,
-  bonusNumber,
-};
+export default InputValidator;
