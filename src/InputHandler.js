f586a43f170e583e77c55be6a9449b5dcd1dc039
iamreum <102291571+iamreum@users.noreply.github.com>
2023-11-08 01:38:13 +0900

    로또 구입 메서드 while문으로 수정

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -3,11 +3,16 @@ import Lotto from "./Lotto.js";
 
 export default class InputHandler {
     static async getPurchaseAmount() {
-        const input = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
-        const amount = parseInt(input, 10);
+        let amount;
+        while (true) {
+            const input = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+            const sanitizedInput = input.replace(/,/g, '');
+            amount = parseInt(sanitizedInput, 10);
 
-        if (isNaN(amount) || amount <= 0 || amount % Lotto.PRICE !== 0) {
-            throw new Error("[ERROR] 로또 구입 금액은 1,000원 단위입니다.");
+            if (!isNaN(amount) && amount > 0 && amount % Lotto.PRICE === 0) {
+                break;
+            }
+            MissionUtils.Console.print("[ERROR] 로또 구입 금액은 1,000원 단위입니다.")
         }
         return amount;
     }
