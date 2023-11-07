fdc13fa38e259a6de6b7ee8ee7f544486dacfae7
ohgus <anakin1108@naver.com>
2023-11-07 17:30:24 +0900

    feat: 올바른 값을 입력할 때까지 반복하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { PICK_NUMBERS, ONE_LOTTO_PRICE } from "./constants/lottoNumbers.js";
+import { MODE } from "./constants/mode.js";
+import { Random, Console } from "@woowacourse/mission-utils";
+import { Input } from "./Input.js";
 
 class App {
   #money;
@@ -16,7 +19,20 @@ class App {
     };
   }
 
-  async play() {}
+  async play() {
+    this.#money = await this.untilValueAvailable(this.#money, MODE.MONEY);
+
+    this.#winningNumbers = await this.untilValueAvailable(
+      this.#winningNumbers,
+      MODE.WINNING_NUMBERS
+    );
+
+    this.#bonusNumber = await this.untilValueAvailable(
+      this.#bonusNumber,
+      MODE.BONUS_NUMBER,
+      this.#winningNumbers
+    );
+  }
 
   makeLotto(money) {
     const howManyLotto = money / ONE_LOTTO_PRICE;
@@ -33,6 +49,39 @@ class App {
     }
 
     return lottos;
+  }
+
+  async assignValue(mode, winningNumbers) {
+    const input = new Input();
+    let temp = "";
+
+    if (mode === MODE.MONEY) {
+      temp = await input.getMoney();
+      input.validateMoney(temp);
+      temp = input.convertToNumber(temp);
+      return temp;
+    } else if (mode === MODE.WINNING_NUMBERS) {
+      temp = await input.getWinningNumbers();
+      input.validateWinningNumbers(temp);
+      temp = input.convertToArray(temp);
+      return temp;
+    } else if (mode === MODE.BONUS_NUMBER) {
+      temp = await input.getBonusNumber();
+      input.validateBonusNumber(temp, winningNumbers);
+      temp = input.convertToNumber(temp);
+      return temp;
+    }
+  }
+
+  async untilValueAvailable(value, mode, winningNumbers) {
+    while (!value) {
+      try {
+        value = await this.assignValue(mode, winningNumbers);
+        return value;
+      } catch (e) {
+        Console.print(e.message + "\n");
+      }
+    }
   }
 }
 
