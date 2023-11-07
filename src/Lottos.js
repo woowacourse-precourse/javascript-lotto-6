00ee50e816624b313156344bf9bec5f79e2c4a72
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-07 11:15:40 +0900

    rename(컴퓨터->n 개의 로또):Computer 클래스 이름 을 Lottos 로 수정
    * n 개의 로또 를 의미하는 Lottos 가 해당 객체의 성질을 더 잘 나타낸다고 생각되어 수정
    * 메서드 이름 도 클래스 이름의 수정사항을 반영해 함께 변경

---
+++ src/Lottos.js
@@ -0,0 +1,33 @@
+import Lotto from './Lotto.js';
+import { Print } from './interface/Output.js';
+import { generateRandomNumbers } from './utils/generateRandomNumbers.js';
+import { sortAscending } from './utils/sort.js';
+import { getArrayLikeString } from './utils/typeCasters.js';
+
+export class Lottos {
+  #list;
+  #count;
+  constructor(count) {
+    this.#count = count;
+    this.#setList();
+  }
+
+  get() {
+    return this.#list;
+  }
+
+  #setList() {
+    this.#list = Array.from({ length: this.#count }).map(() =>
+      this.makeNewLotto(generateRandomNumbers(1, 45, 6)),
+    );
+  }
+  makeNewLotto(numbers) {
+    return new Lotto(numbers);
+  }
+
+  print() {
+    Print(`${this.#count}개를 구매했습니다.`);
+
+    this.#list.forEach((lotto) => Print(getArrayLikeString(sortAscending(lotto.getNumbers()))));
+  }
+}
