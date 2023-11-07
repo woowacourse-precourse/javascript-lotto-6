87be484dd55d453f4a5b59a760f50b87af567783
apfp77 <apfp77@naver.com>
2023-11-07 15:45:40 +0900

    feat: 출력할 메세지 상수화

--- src/constants/message/view.js
+++ src/constants/message/view.js
@@ -1,10 +1,7 @@
-export const input = Object.freeze({
+export const INPUT = {
   purchaseAmount: '구입 금액을 입력해 주세요.',
   winningNumber: '당첨 번호를 입력해 주세요.',
   bonusNumber: '보너스 번호를 입력해 주세요.',
-});
+};
 
-export const output = Object.freeze({
-  buy: '개를 구매했습니다.',
-  winningStatistics: '당첨 통계',
-});
+export const WINNING_STATISTICS = '당첨 통계';
