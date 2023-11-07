03e3f9332ab6147d3e26921f5ce2f87701a43401
yui880 <kimyuna521@gmail.com>
2023-11-07 14:16:54 +0900

    test(Lotto): 로또 클래스 기능 부분 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,7 +1,7 @@
 import Lotto from '../src/model/Lotto.js';
-import { ERROR } from '../src/constants/Constant.js';
+import { ERROR, RANK } from '../src/constants/Constant.js';
 
-describe('로또 클래스 테스트', () => {
+describe('로또 클래스 검증 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -61,6 +61,108 @@ describe('로또 클래스 테스트', () => {
       expect(() => {
         new Lotto(input);
       }).not.toThrow();
+    },
+  );
+});
+
+describe('로또 클래스 기능 테스트', () => {
+  test.each([
+    { input: [6, 5, 4, 3, 2, 1], output: [1, 2, 3, 4, 5, 6] },
+    { input: [43, 21, 33, 11, 5, 17], output: [5, 11, 17, 21, 33, 43] },
+  ])('로또 생성시 번호가 오름차순으로 정렬되는지 테스트', ({ input, output }) => {
+    // given
+    const lotto = new Lotto(input);
+
+    // when
+    const lottoNumbers = lotto.getNumbers();
+
+    // then
+    expect(lottoNumbers).toEqual(output);
+  });
+
+  test.each([
+    {
+      lottoNumber: [1, 2, 3, 4, 5, 6],
+      winningNumber: [1, 2, 3, 4, 5, 6],
+      bonusNumber: 7,
+      matchingCount: RANK.first.match,
+    },
+    {
+      lottoNumber: [6, 5, 4, 3, 2, 1],
+      winningNumber: [1, 2, 3, 4, 5, 7],
+      bonusNumber: 6,
+      matchingCount: RANK.bonus.match,
+    },
+    {
+      lottoNumber: [13, 14, 15, 16, 17, 18],
+      winningNumber: [7, 8, 9, 10, 11, 12],
+      bonusNumber: 13,
+      matchingCount: 0,
+    },
+  ])(
+    '로또 번호와 당첨 번호를 비교해서 일치하는 개수를 구하는 기능 테스트',
+    ({ lottoNumber, winningNumber, bonusNumber, matchingCount }) => {
+      // given
+      const lotto = new Lotto(lottoNumber);
+      const winning = new Lotto(winningNumber);
+
+      // when
+      const result = lotto.getMatchingCount(winning, bonusNumber);
+
+      // then
+      expect(result).toBe(matchingCount);
+    },
+  );
+
+  test.each([
+    { matchingCount: RANK.first.match, rank: 'first' },
+    { matchingCount: RANK.bonus.match, rank: 'bonus' },
+    { matchingCount: 2, rank: null },
+  ])('로또의 순위를 구하는 기능 테스트', ({ matchingCount, rank }) => {
+    // given
+    const mockGetMatchingCount = jest.fn();
+    mockGetMatchingCount.mockReturnValue(matchingCount);
+
+    // when
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    lotto.getMatchingCount = mockGetMatchingCount;
+    const result = lotto.getRank(); // winningNumber와 bonusNumber가 필요하지만 matchingCount 계산을 mock으로 대체해서 입력하지 않음
+
+    // then
+    expect(result).toBe(rank);
+  });
+
+  test.each([
+    {
+      lottoNumber: [1, 2, 3, 4, 5, 6],
+      winningNumber: [1, 2, 3, 4, 5, 6],
+      bonusNumber: 7,
+      rank: 'first',
+    },
+    {
+      lottoNumber: [6, 5, 4, 3, 2, 1],
+      winningNumber: [1, 2, 3, 4, 5, 7],
+      bonusNumber: 6,
+      rank: 'bonus',
+    },
+    {
+      lottoNumber: [13, 14, 15, 16, 17, 18],
+      winningNumber: [7, 8, 9, 10, 11, 12],
+      bonusNumber: 13,
+      rank: null,
+    },
+  ])(
+    '로또의 매칭 카운트를 구해서 순위를 구하는 기능 테스트',
+    ({ lottoNumber, winningNumber, bonusNumber, rank }) => {
+      // given
+      const lotto = new Lotto(lottoNumber);
+      const winningLotto = new Lotto(winningNumber);
+
+      // when
+      const result = lotto.getRank(winningLotto, bonusNumber);
+
+      // then
+      expect(result).toBe(rank);
     },
   );
 });
