3f234d2d7c3d5bba06459e475529e0eba86b7af7
ss0526100 <ss0526100@gmail.com>
2023-11-07 22:34:10 +0900

    test: delete mvc play comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import Fucntional from './functinoal/Functional.js';
-import MVC from './mvc/MVC.js';
 
 class App {
   async play() {
     await Fucntional.play();
-
-    //await MVC.play();
   }
 }
 
