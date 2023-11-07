849eacc95ad396fa84978216e36d886fec45cacf
Hong sung soo <sow9707@daum.net>
2023-11-07 23:11:46 +0900

    feat: 구매금액 입력받는 메소드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const Random = MissionUtils.Random;
+const Console = MissionUtils.Console;
 
 class Lotto {
   #numbers;
@@ -11,6 +15,11 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+  }
+
+  static async userMoney() {
+    const userMoney = await Console.readLineAsync("구입 금액을 입력해주세요.");
+    return userMoney;
   }
 }
 
