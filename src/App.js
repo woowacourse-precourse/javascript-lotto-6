c6f63c52541bdb1cfffe5c69ed1d494a127c343c
minSsan <msp1125@hanyang.ac.kr>
2023-11-07 20:43:00 +0900

    feat: App.js 코드 추가 및 로또 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+import LottoController from "./Controller/LottoController";
+
 class App {
-  async play() {}
+  #controller;
+
+  constructor() {
+    this.#controller = new LottoController();
+  }
+
+  async play() {
+    try {
+      await this.#controller.lotto();
+    } catch (error) {
+      Console.print(`${error.message}`);
+    }
+  }
 }
 
 export default App;
