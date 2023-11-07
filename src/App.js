b6735cc30af7a963604fb551ace837efc800f558
LEEHYEYEONG <elto789@naver.com>
2023-11-07 16:04:48 +0900

    feat: 로또 숫자 맞춘 개수 저장, 결과 출력 - 5개가 맞는 경우에 bonus가 맞는 경우를 빼서 3등을 계산

--- src/App.js
+++ src/App.js
@@ -24,6 +24,11 @@ class App {
     const bonusInput = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
     const bonus = new InputBonus(bonusInput, lottoNumber);
     bonus.validate(bonusInput, lottoNumber);
+    Console.print("");
+
+    const matchList = matchLotto(randomLottoArr, lottoNumber, bonusInput);
+    const resultList = matchResult(matchList);
+    printResult(resultList, budgetInput);
   }
 }
 
@@ -45,6 +50,56 @@ function printLotto(numLotto) {
   }
   Console.print("");
   return randomLottoArr;
+}
+
+function matchLotto(randomLottoArr, lotto, bonus) {
+  const matchList = []
+  for (let i = 0; i < randomLottoArr.length; i++) {
+    matchList.push(matchLottoNumber(randomLottoArr[i], lotto, bonus))
+  }
+  return matchList;
+}
+
+function matchLottoNumber(randomLottoArr, lotto, bonus) {
+  const match = { lottoMatch: 0, bonusMatch: 0 }
+  for (let i = 0; i < 6; i++) {
+    if (randomLottoArr.includes(parseInt(lotto[i]))) {
+      match["lottoMatch"]++;
+    }
+  }
+  if (match["lottoMatch"] === 5) {
+    if (randomLottoArr.includes(parseInt(bonus))) {
+      match["bonusMatch"] = 1;
+    }
+  }
+  return match;
+}
+
+const getLottoMatch = (arr, el) => arr.filter(value => value["lottoMatch"] === el).length;
+const getBonusMatch = (arr, el) => arr.filter(value => value["bonusMatch"] === el).length;
+
+function matchResult(matchList) {
+  const matchFifth = getLottoMatch(matchList, 3);
+  const matchFourth = getLottoMatch(matchList, 4);
+  const matchSecond = getBonusMatch(matchList, 1);
+  const matchThird = getLottoMatch(matchList, 5) - matchSecond;
+  const matchFirst = getLottoMatch(matchList, 6);
+
+  return [matchFifth, matchFourth, matchThird, matchSecond, matchFirst];
+}
+
+function printResult(resultList, budget) {
+  Console.print("당첨 통계");
+  Console.print("---");
+  Console.print("3개 일치 (5,000원) - " + resultList[0] + "개");
+  Console.print("4개 일치 (50,000원) - " + resultList[1] + "개");
+  Console.print("5개 일치 (1,500,000원) - " + resultList[2] + "개");
+  Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + resultList[3] + "개");
+  Console.print("6개 일치 (2,000,000,000원) - " + resultList[4] + "개");
+
+  const profit = 5000 * resultList[0] + 50000 * resultList[1] + 1500000 * resultList[2] + 30000000 * resultList[3] + 2000000000 * resultList[4];
+  const profitRate = profit / budget * 100;
+  Console.print("총 수익률은 " + profitRate + "%입니다.");
 }
 
 export default App;
