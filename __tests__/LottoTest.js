287f0b73006242334b1285ae8af994ee1103ff19
Taeyooooon <eric568@naver.com>
2023-11-07 16:24:15 +0900

    test: Lotto 클래스 테스트 중복제거

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -2,23 +2,17 @@ import { ERROR } from '../src/constant/constant.js';
 import Lotto from '../src/model/Lotto.js';
 
 describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow(ERROR.length);
-  });
+  const testCases = [
+    { numbers: [1, 2, 3, 4, 5, 6, 7], expectedError: ERROR.length },
+    { numbers: [1, 2, 3, 4, 5, 5], expectedError: ERROR.duplicated },
+    { numbers: [1, 2, 3, 4, 5, 99], expectedError: ERROR.range },
+    { numbers: [1, 2, 3, 4, 5, 'asdf'], expectedError: ERROR.numberOnly },
+  ];
 
-  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow(ERROR.duplicated);
-  });
-
-  test('로또 번호가 범위를 벗어나면 예외가 발생한다.', () => {
-    expect(() => new Lotto([1, 2, 3, 4, 5, 99])).toThrow(ERROR.range);
-  });
-
-  test('로또 번호가 숫자가 아니면 예외가 발생한다.', () => {
-    expect(() => new Lotto([1, 2, 3, 4, 5, 'asdf'])).toThrow(ERROR.numberOnly);
-  });
+  test.each(testCases)(
+    '예외 테스트 numbers: $numbers, toThrow: $expectedError',
+    ({numbers,expectedError}) => {
+      expect(()=>new Lotto(numbers)).toThrow(expectedError);
+    }
+  );
 });
