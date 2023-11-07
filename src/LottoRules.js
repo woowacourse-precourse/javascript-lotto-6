d68fd8c23888cfb741238e129fa2f4b4f3e57867
gilgor2 <misby0327@gmail.com>
2023-11-07 22:08:44 +0900

    feat(LottoRules.js): 로또 규칙 객체 구현
    README의 기능 명세에 따라 로또 규칙 구현.
    단일 객체로 구현할 것인지 클래스로 구현할 것인지 차후 리팩토링 필요

--- src/LottoRules.js
+++ src/LottoRules.js
@@ -1,4 +1,26 @@
 class LottoRules {
+	#winningRule = [
+		{ correctCnt: 3, bonusCnt: 0, winnings: 5000 },
+		{ correctCnt: 4, bonusCnt: 0, winnings: 50000 },
+		{ correctCnt: 5, bonusCnt: 0, winnings: 1500000 },
+		{ correctCnt: 5, bonusCnt: 1, winnings: 30000000 },
+		{ correctCnt: 6, bonusCnt: 0, winnings: 2000000000 },
+	];
 
+	getWinnings(correctCnt, bonusCnt) {
+		const correctRank = this.#winningRule.find(
+			(condition) =>
+				condition.correctCnt === correctCnt &&
+				condition.bonusCnt === bonusCnt,
+		);
+		if (!correctRank) {
+			return -1;
+		}
+		return correctRank.winnings;
+	}
+
+	getTotalRule() {
+		return this.#winningRule;
+	}
 }
 export default LottoRules;
