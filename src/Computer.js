d12fb62b30575e0f31849c95f122a74c06d6950d
JungHun98 <j7papa@naver.com>
2023-11-07 13:52:53 +0900

    feat: 컴퓨터 클래스 당첨 결과 반영 메소드 구현

--- src/Computer.js
+++ src/Computer.js
@@ -1,3 +1,20 @@
+const FIRST = 'first';
+const SECOND = 'second';
+const THIRD = 'third';
+const FOURTH = 'fourth';
+const FIFTH = 'fifth';
+
+const prizeConditionArr = [
+  { key: { mathNumber: 6, isMatchBonus: false }, value: FIRST },
+  { key: { mathNumber: 5, isMatchBonus: true }, value: SECOND },
+  { key: { mathNumber: 5, isMatchBonus: false }, value: THIRD },
+  { key: { mathNumber: 4, isMatchBonus: false }, value: FOURTH },
+  { key: { mathNumber: 3, isMatchBonus: false }, value: FIFTH },
+];
+
+const map = new Map();
+prizeConditionArr.forEach((elem) => map.set(JSON.stringify(elem.key), elem.value));
+
 class Computer {
   #winnerNumbers;
   #bonusNumber;
@@ -15,7 +32,7 @@ class Computer {
     };
   }
 
-  getPrizeResult(lottos) {
+  setPrizeResult(lottos) {
     const mathResult = lottos.map((lotto) => {
       const lottoNumbers = lotto.getNumbers();
       const mathNumbers = this.#winnerNumbers.filter((number) => lottoNumbers.includes(number));
@@ -25,7 +42,18 @@ class Computer {
       return { mathNumber, isMatchBonus };
     });
 
-    return mathResult;
+    mathResult.forEach((elem) => {
+      const keyString = JSON.stringify(elem);
+      const prize = map.get(keyString);
+
+      if (prize !== undefined) {
+        this.#prizeResult[prize] += 1;
+      }
+    });
+  }
+
+  getPrizeResult() {
+    return this.#prizeResult;
   }
 }
 
