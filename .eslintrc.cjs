d157586c50c048ff912c12b86c54906191e1f89e
sryung <sryung1225@gmail.com>
2023-11-08 01:00:16 +0900

    style(eslintrc): eslint-import 규칙 비활성화
    에어비엔비 스타일 가이드에서 제시하는 import 중 확장자 생략은 eslint 규칙에 위배됨
    하지만 현재 package.json은 "type": "module" 로 ESM 표준 모듈을 사용 중. 이로 인해 확장자 생략 시 오류가 발생 (Cannot find module)
    
    결론적으로 모든 import 하는 모듈에는 확장자를 붙이고 eslint에서는 해당 규칙을 비활성화하는 방식을 채택

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
     commonjs: true,
-    es2021: true,
     node: true,
     jest: true,
   },
@@ -14,5 +12,6 @@ module.exports = {
     'operator-linebreak': ['error', 'before'], // 할당 연산자 줄바꿈 설정
     'max-depth': ['error', 2], // 들여쓰기 깊이 제한
     'max-lines-per-function': ['error', { max: 15 }], // 함수의 길이 제한
+    'import/extensions': 'off', // eslint-import/extensions 규칙 비활성화
   },
 };
