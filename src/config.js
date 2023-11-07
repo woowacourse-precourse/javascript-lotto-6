e9cda5ee287cd2b189c512acb81626b44ba7efcf
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-07 23:23:28 +0900

    feat(view): 금액 입력 기능에 lotto price 유효성 검증 추가

--- src/config.js
+++ src/config.js
@@ -9,4 +9,6 @@ export const LOTTO = {
 export const ERROR = Object.freeze({
   IS_EMPTY: '[ERROR] 값을 입력해주세요.\n',
   IS_NOT_INTEGER: '[ERROR] 정수를 입력해주세요.\n',
+  IS_LESS_THAN_LOTTO_PRICE: `[ERROR] 로또 구입 금액은 ${LOTTO.PRICE}원 이상이어야 합니다.\n`,
+  IS_NOT_MULTIPLY_OF_LOTTO_PRICE: `[ERROR] 로또 구입 금액은 ${LOTTO.PRICE}원 단위로 가능합니다.\n`,
 });
--- src/view/input.js
+++ src/view/input.js
@@ -1,6 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import { isEmpty, isPositiveInteger } from '../utils.js';
-import { ERROR } from '../config.js';
+import { ERROR, LOTTO } from '../config.js';
 
 const MESSAGE = Object.freeze({
   AMOUNT_TO_BUY: '구입금액을 입력해 주세요.\n',
@@ -13,6 +13,8 @@ export default class Input {
       amount = await Input.readTrimmedLineAsync(MESSAGE.AMOUNT_TO_BUY);
       if (isEmpty(amount)) throw new Error(ERROR.IS_EMPTY);
       if (!isPositiveInteger(amount)) throw new Error(ERROR.IS_NOT_INTEGER);
+      if (amount < LOTTO.PRICE) throw new Error(ERROR.IS_LESS_THAN_LOTTO_PRICE);
+      if (amount % LOTTO.PRICE !== 0) throw new Error(ERROR.IS_NOT_MULTIPLY_OF_LOTTO_PRICE);
     } catch (e) {
       Console.print(e.message);
       amount = await Input.amountToBuy();
