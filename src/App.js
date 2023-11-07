92e658772f59c3a5943973b2767f812ef7715306
Y.W KIM <kyw624@gmail.com>
2023-11-07 18:43:40 +0900

    Refactor: 유효성 검사 로직 분리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 
 import Game, { RANK } from './Game.js';
+import {
+  validateAmountInput,
+  validateWinningInput,
+  validateBonusInput,
+} from './util/validateInput.js';
 
 export const MESSAGES = Object.freeze({
   INPUT_AMOUNT: '구입금액을 입력해 주세요.',
@@ -37,49 +42,9 @@ class App {
     }
   }
 
-  validateAmountInput(input) {
-    if (input === '') {
-      throw new Error('[ERROR] 입력값이 없습니다.');
-    }
-
-    if (isNaN(input)) {
-      throw new Error('[ERROR] 숫자를 입력해주세요.');
-    }
-
-    if (input % 1000 !== 0 || input < 1000) {
-      throw new Error('[ERROR] 1,000원 단위로 입력해주세요.');
-    }
-  }
-
-  validateWinningInput(input) {
-    if (input === '') {
-      throw new Error('[ERROR] 입력값이 없습니다.');
-    }
-
-    const numbers = input.split(',');
-
-    if (input.split(',').length !== 6) {
-      throw new Error('[ERROR] 6개의 번호를 입력해주세요.');
-    }
-
-    if (new Set(numbers).size !== numbers.length) {
-      throw new Error('[ERROR] 중복된 숫자가 있습니다.');
-    }
-
-    if (numbers.some((num) => num < 1 || num > 45)) {
-      throw new Error('[ERROR] 1~45 사이의 숫자여야 합니다.');
-    }
-  }
-
-  validateBonusInput(input, winningInfo) {
-    if (winningInfo.split(',').includes(input)) {
-      throw new Error('[ERROR] 당첨 번호에 이미 있는 숫자입니다.');
-    }
-  }
-
   async getAmountInput() {
     const amountInput = await Console.readLineAsync(MESSAGES.INPUT_AMOUNT);
-    this.validateAmountInput(amountInput);
+    validateAmountInput(amountInput);
 
     return amountInput;
   }
@@ -88,12 +53,12 @@ class App {
     const winningNumbersInput = await Console.readLineAsync(
       MESSAGES.INPUT_WINNING_NUMBERS
     );
-    this.validateWinningInput(winningNumbersInput);
+    validateWinningInput(winningNumbersInput);
 
     const bonusNumberInput = await Console.readLineAsync(
       MESSAGES.INPUT_BONUS_NUMBER
     );
-    this.validateBonusInput(bonusNumberInput, winningNumbersInput);
+    validateBonusInput(bonusNumberInput, winningNumbersInput);
 
     return {
       winningNumbers: winningNumbersInput.split(',').map(Number),
