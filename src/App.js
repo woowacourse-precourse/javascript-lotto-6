c15a59616a4c9878033fc3ada660752010e758ed
hojkim77 <rlaghwnd4534@gmail.com>
2023-11-07 15:48:28 +0900

    [feat] 게임 컨트롤 관련된 메서드 생성 및 연결: 입력, 결과생성, 출력

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
+import Lotto from './Lotto.js';
+import User from './User.js';
 import Input from './views/Input.js';
+import Output from './views/Output.js';
 
 class App {
+  constructor() {
+    this.user;
+    this.lotto;
+  }
+
   async play() {
-    await Input.readPurchaseAmout();
+    await this.userInputController();
+    this.userNumbersOutputController();
 
-    await Input.readLottoNumber();
+    await this.lottoInputController();
+    this.resultController();
+    this.resultOutputController();
+  }
 
-    await Input.readBonusNumber();
+  async userInputController() {
+    const purchaseAmount = await Input.readPurchaseAmout();
+    this.user = new User(purchaseAmount);
+  }
+
+  userNumbersOutputController() {
+    const userNumbersToString = this.user.userNumbersList.map((numbers) =>
+      numbers.join(', ')
+    );
+
+    Output.printUserNumbers(this.user.purchaseNumber, userNumbersToString);
+  }
+
+  async lottoInputController() {
+    const lottoNumber = await Input.readLottoNumber();
+    const bonusNumber = await Input.readBonusNumber();
+    this.lotto = new Lotto(lottoNumber, bonusNumber);
+  }
+
+  resultController() {
+    this.user.userNumbersList.map((userNumbers) => {
+      const { lottoCount, isBonus } = this.lotto.getResult(userNumbers);
+      this.user.setResult(lottoCount, isBonus);
+      this.user.setProfit(lottoCount, isBonus);
+    });
+
+    this.user.setYeild();
+  }
+
+  resultOutputController() {
+    Output.printLottoResult(this.user.results);
+    Output.printYeild(this.user.yeild);
   }
 }
 
