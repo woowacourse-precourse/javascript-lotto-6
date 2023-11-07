0a5f02f14dbb7e7f148583663973ae1615e60807
OAO <ashgrayblue0@gmail.com>
2023-11-08 04:26:23 +0900

    feat(Inputs): implement user output

---
+++ src/module/view/Outputs.js
@@ -0,0 +1,38 @@
+import {MissionUtils} from "@woowacourse/mission-utils";
+
+class Outputs {
+  #lottoList;
+  #score;
+  #message = {
+    three: '3개 일치 (5,000원) - ',
+    four: '4개 일치 (50,000원) - ',
+    five: '5개 일치 (1,500,000원) - ',
+    bonus: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+    six: '6개 일치 (2,000,000,000원) - ',
+  };
+
+  constructor(lottoList, score) {
+    this.#lottoList = lottoList;
+    this.#score = score;
+  }
+
+  printLottoList() {
+    const leng = this.#lottoList.length;
+
+    MissionUtils.Console.print(`${leng}개를 구매했습니다.`);
+
+    for (let i = 0; i < leng; i += 1) {
+      MissionUtils.Console.print(this.#lottoList[i].numbers.sort((a, b) => a - b));
+    }
+  }
+
+  printScore() {
+    MissionUtils.Console.print('당첨 통계\n---');
+    const data = ['three', 'four', 'five', 'bonus', 'six'];
+    for (let i = 0; i < 5; i += 1) {
+      MissionUtils.Console.print(`${this.#message[data[i]]}${this.#score[data[i]]}개`);
+    }
+  }
+}
+
+export default Outputs;
