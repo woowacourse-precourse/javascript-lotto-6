eb1985c1b7d38bddc9d95a6d7efbd7c51ace3e0e
Y.W KIM <kyw624@gmail.com>
2023-11-07 18:39:48 +0900

    Feat: 당첨 통계를 출력하는 printResult 메서드 구현

--- src/App.js
+++ src/App.js
@@ -100,6 +100,20 @@ class App {
       bonusNumber: Number(bonusNumberInput),
     };
   }
+
+  printResult(result) {
+    const values = Object.values(result);
+
+    Console.print('\n당첨 통계');
+    Console.print('---');
+    Console.print(`3개 일치 (5,000원) - ${values[0].count}개`);
+    Console.print(`4개 일치 (50,000원) - ${values[1].count}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${values[2].count}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${values[3].count}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${values[4].count}개`);
+  }
 }
 
 export default App;
