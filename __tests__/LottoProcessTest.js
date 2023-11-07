71d5548041443b641dc66178d46fa7cf420d3e51
today-is-first <diligent0644@gmail.com>
2023-11-08 02:13:10 +0900

    test: 로또 당첨 순위 테스트

--- __tests__/LottoProcessTest.js
+++ __tests__/LottoProcessTest.js
@@ -1,16 +1,11 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import LottoProcess from '../src/domain/LottoProcess';
 import Lotto from '../src/domain/Lotto';
-import { PRINT_MESSAGES } from '../src/constant/Constants';
 
 jest.mock('../src/domain/Lotto');
 
 describe('xp', () => {
   let lottoProcess;
-  const spyOnUserLottoNumbers = jest.spyOn(
-    LottoProcess.prototype,
-    'getUserLottoNumbers',
-  );
 
   const getSpyOnConsolePrint = () => {
     const spyOnConsolePrint = jest.spyOn(MissionUtils.Console, 'print');
@@ -51,6 +46,51 @@ describe('xp', () => {
       expect(spyOnConsolePrint).toHaveBeenCalledWith(
         expect.stringContaining(log),
       );
+    });
+  });
+
+  describe('로또 당첨 순위 확인 테스트', () => {
+    const accordList = [2, 1, 0, 3, 3, 0, 0, 5, 5, 6];
+    beforeEach(() => {
+      lottoProcess = new LottoProcess();
+      lottoProcess.lottos = [
+        [1, 3, 11, 38, 43, 45],
+        [4, 25, 26, 39, 44, 45],
+        [8, 10, 11, 19, 28, 30],
+        [1, 4, 5, 31, 36, 43],
+        [1, 5, 6, 11, 13, 19],
+        [7, 8, 21, 37, 39, 42],
+        [10, 11, 23, 32, 38, 41],
+        [1, 2, 3, 4, 5, 12],
+        [1, 2, 3, 4, 5, 11],
+        [1, 2, 3, 4, 5, 6],
+      ];
+    });
+    test('당첨 개수 테스트', () => {
+      lottoProcess.winningNumbers = '1,2,3,4,5,6';
+
+      const result = lottoProcess.checkAccord();
+
+      expect(result).toEqual(accordList);
+    });
+
+    test('맞춘 개수로 당첨 순위 반환 테스트', () => {
+      lottoProcess.bonusNumber = 12;
+      const ranklist = [
+        'nothing',
+        'nothing',
+        'nothing',
+        'fifth',
+        'fifth',
+        'nothing',
+        'nothing',
+        'second',
+        'third',
+        'first',
+      ];
+      const result = lottoProcess.checkRanking(accordList);
+
+      expect(result).toEqual(ranklist);
     });
   });
 });
