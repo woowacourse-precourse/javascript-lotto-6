5437fa14167369d509d6da3643d099a6ec2b18a2
Sunghyun627 <kksscc4@gmail.com>
2023-11-07 14:26:58 +0900

    feat: 구입금액이 0으로 시작하는 경우 에러처리 구현

--- src/Validation.js
+++ src/Validation.js
@@ -4,9 +4,16 @@ export const validatePurchaseAmountInput = async (purchaseAmountInput) => {
   if (!isNumber(purchaseAmountInput)) {
     throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGES.NOT_NUMBER);
   }
+  if (isStartWithZero(purchaseAmountInput)) {
+    throw new Error(PURCHASE_AMOUNT_ERROR_MESSAGES.START_WITH_ZERO);
+  }
 };
 
 const isNumber = (input) => {
   const regex = /^[0-9]+$/;
   return regex.test(input);
+};
+
+const isStartWithZero = (input) => {
+  return input[0] === '0';
 };
