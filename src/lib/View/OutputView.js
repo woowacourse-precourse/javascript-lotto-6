3566197ba8452ba8336e81251d7b9a85b6c78f93
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-11-07 18:42:15 +0900

    refactor: OutputView의 데이터 가공 메서드들을 분리

--- src/lib/View/OutputView.js
+++ src/lib/View/OutputView.js
@@ -3,35 +3,40 @@ import { ERROR_MESSAGE, GAME_RULE } from "../Constants.js";
 
 class OutputView {
   static lottoBundle({ purchaseHistory }) {
-    Console.print(
-      [
-        "",
-        `${purchaseHistory.length}개를 구매했습니다.`,
-        ...purchaseHistory.map((numbers) => `[${numbers.join(", ")}]`),
-        "",
-      ].join("\n"),
+    Console.print(OutputView.lottoBundleMessage({ purchaseHistory })).join(
+      "\n",
     );
   }
 
   static result({ prizeMap, winRate }) {
-    Console.print(
-      [
-        "",
-        "당첨 통계",
-        "---",
-        ...OutputView.#prizeMessage(prizeMap),
-        `총 수익률은 ${winRate.toFixed(GAME_RULE.DECIMAL_POINT)}%입니다.`,
-      ].join("\n"),
-    );
+    Console.print(OutputView.resultMessage({ prizeMap, winRate }).join("\n"));
   }
 
   static err({ message }) {
-    // else 사용이 적절하다고 판단됨
     if (Object.values(ERROR_MESSAGE).includes(message)) Console.print(message);
     else Console.print(ERROR_MESSAGE.UNKNOWN_ERROR);
   }
 
-  static #prizeMessage(prizeMap) {
+  static lottoBundleMessage({ purchaseHistory }) {
+    return [
+      "",
+      `${purchaseHistory.length}개를 구매했습니다.`,
+      ...purchaseHistory.map((numbers) => `[${numbers.join(", ")}]`),
+      "",
+    ];
+  }
+
+  static resultMessage({ prizeMap, winRate }) {
+    return [
+      "",
+      "당첨 통계",
+      "---",
+      ...OutputView.prizeMessage(prizeMap),
+      `총 수익률은 ${winRate.toFixed(GAME_RULE.DECIMAL_POINT)}%입니다.`,
+    ];
+  }
+
+  static prizeMessage(prizeMap) {
     return [
       `3개 일치 (5,000원) - ${prizeMap.get(5) ?? 0}개`,
       `4개 일치 (50,000원) - ${prizeMap.get(4) ?? 0}개`,
