1e2e91e2190d280147c98a0cb25769ef3fb0aef5
intersoom <sallybig@naver.com>
2023-11-08 02:31:20 +0900

    feat: 에러 문구 상수 추가

---
+++ src/constant/errorDate.js
@@ -0,0 +1,11 @@
+const ERROR_MESSAGE = Object.freeze({
+  PAYMENT_INVALID: "[ERROR] 1000원 단위로 입력해주세요.",
+  NUMBER_FORM: "[ERROR] 숫자만으로 입력해주세요.",
+  NUMBER_COMMA_FORM: "[ERROR] 숫자와 쉼표만으로 입력해주세요.",
+  BLANK_FORM: "[ERROR] 공백 없이 입력해주세요.",
+  RANGE_INVALID: "[ERROR] 1부터 45 사이의 숫자로 입력해주세요",
+  SIZE_INVALID: "[ERROR] 총 6개의 숫자만을 입력해주세요.",
+  DUPLICATION: "[ERROR] 6개의 숫자를 중복없이 입력해주세요.",
+});
+
+export default ERROR_MESSAGE;
