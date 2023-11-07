5237dd15d0c029dbb8c1f4a0d997151c613a00d8
JungHun98 <j7papa@naver.com>
2023-11-07 16:14:53 +0900

    feat: App 클래스 반복 입력 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import input from './utils/input.js';
+import print from './utils/print.js';
+
 class App {
   #lottos;
+
+  async getInput(prompt, validateFunc) {
+    while (true) {
+      try {
+        const inputValue = await input(prompt);
+        validateFunc(inputValue);
+        return inputValue;
+      } catch (error) {
+        print(error.message);
+      }
+    }
+  }
 
   async play() {}
 }
