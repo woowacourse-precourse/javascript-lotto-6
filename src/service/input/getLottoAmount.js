5c93950b308747ef3dc96752d305088ee10d0fce
yeong-hwan <jghff700@naver.com>
2023-11-08 05:36:33 +0900

    refactor: 로또 발행 횟수 구하는 함수를 로또 구매 금액 입력받는 함수로 변경
    구매 금액 입력과 발행 횟수 산출을 나누어 구분할 필요 있어 수정

--- src/service/input/getLottoAmount.js
+++ src/service/input/getLottoAmount.js
@@ -3,13 +3,12 @@ import InputValidator from '../../domain/validator/InputValidator.js';
 import MESSAGE from '../../constants/message.js';
 import NUMBER from '../../constants/number.js';
 
-export default async function getLottoCount() {
+export default async function getLottoAmount() {
   const userInput = await Console.readLineAsync(MESSAGE.purchaseAmount + '\n');
   const inputValidator = new InputValidator();
 
   inputValidator.validateUnitCost(userInput);
 
-  const lottoCount = parseInt(userInput / NUMBER.unitCost);
-
-  return lottoCount;
+  const lottoAmount = userInput;
+  return lottoAmount;
 }
