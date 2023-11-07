2e852128442475a16775754eceb47eca27398d20
OhHyukJun <kmh7277@naver.com>
2023-11-07 15:56:06 +0900

    Refactor: try cath 문 작성

--- src/LottoResult/BonusNumber.js
+++ src/LottoResult/BonusNumber.js
@@ -8,19 +8,22 @@ export class BonusNumber {
     async bonusNumber() {
         let bonusInput = '';
         while(true) {
+        try {
             bonusInput = await MissionUtils.Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
             bonusInput = Number(bonusInput);
 
             if(bonusInput < 1 || bonusInput > 45){
-                MissionUtils.Console.print("[ERROR] 입력 값은 1~45 까지의 숫자입니다");
-                continue;
-            }
+                throw new Error("[ERROR] 입력 값은 1~45 까지의 숫자입니다");
+            } 
             else if(this.winNumbers.includes(bonusInput)){
-                MissionUtils.Console.print("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
-                continue;
+                throw new Error("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
             }
             break;
+        } catch (error) {
+            MissionUtils.Console.print(error.message);
+            continue;
         }
-        return bonusInput;
     }
+    return bonusInput;
+  }
 }
--- src/LottoResult/WinNumber.js
+++ src/LottoResult/WinNumber.js
@@ -4,24 +4,29 @@ export class WinNumber {
     async winnumber() {
         let winnum = [];
         while(winnum.length !== 6) {
-            const winnumInput = await MissionUtils.Console.readLineAsync("당첨 번호를 입력해주세요.\n");
-            winnum = winnumInput.split(',').map(num => Number(num.trim()));
-            if(winnum.some(num => num < 1 || num > 45)){
-                MissionUtils.Console.print("[ERROR] 입력 값은 1~45 까지의 숫자입니다");
-                winnum = [];
-                continue;
+            try {
+                const winnumInput = await MissionUtils.Console.readLineAsync("당첨 번호를 입력해주세요.\n");
+                winnum = winnumInput.split(',').map(num => Number(num.trim()));
+
+                if(winnum.some(num => num < 1 || num > 45)){
+                    throw new Error("[ERROR] 입력 값은 1~45 까지의 숫자입니다");
+                }
+
+                if(winnum.length !== 6 ){
+                    throw new Error("[ERROR] 입력 값은 6개의 숫자입니다.");
+                }
+
+                const deferentNum = new Set(winnum);
+
+                    if(deferentNum.size !== winnum.length){
+                        throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+                    }
+                } catch (error) {
+                    MissionUtils.Console.print(error.message);
+                    winnum = []; 
+                    continue; 
+                }
             }
-            else if(winnum.length !== 6 ){
-                MissionUtils.Console.print("[ERROR] 입력 값은 6개의 숫자입니다.");
-                winnum = [];
-                continue;
-            }
-            const deferentNum = new Set(winnum);
-            if(deferentNum.size !== winnum.length){
-                MissionUtils.Console.print("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
-                winnum = [];
-            }
-        }
         return winnum;
     }
 }
