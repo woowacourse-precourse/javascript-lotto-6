e9952c1479731fb5ba3722b0014ac33f36aa1a86
Elisha0510 <sjeong35@knu.ac.kr>
2023-11-08 02:53:20 +0900

    style: 테스트 코드 완료한 함수에 대한 주석 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-import { ERROR_MESSAGE } from "./message";
+import { ERROR_MESSAGE } from './message';
 
 class Lotto {
   #numbers;
@@ -14,13 +14,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error(ERROR_MESSAGE.SIX_NUMBERS);
     }
-    if (this.checkRange(numbers))
-      throw new Error(ERROR_MESSAGE.LOTTO_RANGE);
+    if (this.checkRange(numbers)) throw new Error(ERROR_MESSAGE.LOTTO_RANGE);
     const numbersSet = this.getNumbersSet(numbers);
     if ([...numbersSet].length !== 6)
       throw new Error(ERROR_MESSAGE.SAME_NUMBERS);
   }
-
+  // test code
   checkRange(numbers) {
     return numbers.filter(value => 1 > value || value > 45).length;
   }
@@ -28,52 +27,60 @@ class Lotto {
   getNumbersSet(numbers) {
     return new Set(numbers.map(Number));
   }
-
-  countEqualNumbers(randomNumberArray, userLottoNumber){
-    return randomNumberArray.filter(number => userLottoNumber.includes(number)).length;
+  // test code
+  countEqualNumbers(randomNumberArray, userLottoNumber) {
+    return randomNumberArray.filter(number => userLottoNumber.includes(number))
+      .length;
   }
-
-  compareBonusNumber(randomNumberArray, userBonusNumber){
+  // test code
+  compareBonusNumber(randomNumberArray, userBonusNumber) {
     return randomNumberArray.includes(userBonusNumber);
   }
-
-  getLottoResult(equalNumber, bonusResult){
-    let result = [0,0,0,0,0];
-    if(equalNumber === 3) result[0] += 1;
-    if(equalNumber === 4) result[1] += 1;
-    if(equalNumber === 5 && !bonusResult) result[2] += 1;
-    if(equalNumber === 5 && bonusResult) result[3] += 1;
-    if(equalNumber === 6) result[4] += 1;
+  // test code
+  getLottoResult(equalNumber, bonusResult) {
+    let result = [0, 0, 0, 0, 0];
+    if (equalNumber === 3) result[0] += 1;
+    if (equalNumber === 4) result[1] += 1;
+    if (equalNumber === 5 && !bonusResult) result[2] += 1;
+    if (equalNumber === 5 && bonusResult) result[3] += 1;
+    if (equalNumber === 6) result[4] += 1;
     return result;
   }
-
-  checkBonusNumber(randomNumberArray, equalNumber, userBonusNumber){
-    if(equalNumber === 5){
+  // test code
+  checkBonusNumber(randomNumberArray, equalNumber, userBonusNumber) {
+    if (equalNumber === 5) {
       return this.compareBonusNumber(randomNumberArray, userBonusNumber);
     }
   }
 
-  compareLottoNumbers(lottoRandomNumber, userLottoNumber, userBonusNumber){
+  compareLottoNumbers(lottoRandomNumber, userLottoNumber, userBonusNumber) {
     let lottoResult = [];
     let bonusResult = 0;
     lottoRandomNumber.forEach(randomNumberArray => {
-      const equalNumber = this.countEqualNumbers(randomNumberArray, userLottoNumber);
-      bonusResult = this.checkBonusNumber(randomNumberArray, equalNumber, userBonusNumber);
+      const equalNumber = this.countEqualNumbers(
+        randomNumberArray,
+        userLottoNumber,
+      );
+      bonusResult = this.checkBonusNumber(
+        randomNumberArray,
+        equalNumber,
+        userBonusNumber,
+      );
       lottoResult = this.getLottoResult(equalNumber, bonusResult);
     });
     return lottoResult;
   }
 
-  getLottoRate(lottoResult, lottoPrice){
+  getLottoRate(lottoResult, lottoPrice) {
     const money = [5000, 50000, 1500000, 30000000, 2000000000];
     let prizeMoney = 0;
     let investment = Number(lottoPrice);
     let rate = 0;
     lottoResult.forEach((amount, index) => {
-      if(amount) prizeMoney += amount * money[index];
-    })
-    rate = ((prizeMoney - investment)/ investment)*100;
-    rate = 100 + (Math.round(rate * 100) / 100);
+      if (amount) prizeMoney += amount * money[index];
+    });
+    rate = ((prizeMoney - investment) / investment) * 100;
+    rate = 100 + Math.round(rate * 10) / 10;
     return rate;
   }
 }
