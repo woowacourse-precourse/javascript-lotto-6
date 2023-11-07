cf8c1f432c6e8e11dd6179597bd34a562274f82a
OAO <ashgrayblue0@gmail.com>
2023-11-08 00:50:36 +0900

    feat(LottoGenerator): delete converting method from money to count

--- src/module/model/LottoGenerator.js
+++ src/module/model/LottoGenerator.js
@@ -3,31 +3,18 @@ import {MissionUtils} from "@woowacourse/mission-utils";
 
 class LottoGenerator {
   #lottos = [];
-  #count;
 
-  constructor(money) {
-    this.#validateMoney(money);
-    this.#count = this.#changeMoneyToCount(money);
-    this.#generateLottos();
+  constructor(count) {
+    this.#generateLottos(count);
   }
 
-  #generateLottos() {
+  #generateLottos(count) {
     let lotto;
 
     for (let i = 0; i < this.#count; i += 1) {
       lotto = new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
       this.#lottos.push(lotto);
     }
-  }
-
-  #changeMoneyToCount(money) {
-    return money / 1000;
-  }
-
-  #validateMoney(money) {
-    if (money % 1000 !== 0) {
-      throw new Error('[ERROR] 구입 금액이 1,000원 단위가 아닙니다.');
-    };
   }
 }
 
