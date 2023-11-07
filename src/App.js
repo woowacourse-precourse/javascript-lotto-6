65f734c3387574cb769a31ce6f43874a9ee4c7da
minseok cho <whalstjr199927@gmail.com>
2023-11-07 17:38:44 +0900

    기본 변수 및 getPurchaseAmount 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
+    #lottoPrice = 1000;
+    #prizeMoney = [2000000000, 30000000, 1500000, 50000, 5000];
+    #winningCounts = [0, 0, 0, 0, 0];
     async play() {}
+
+    async #getPurchaseAmount() {
+        const input = await Console.readLineAsync("구입금액을 입력해 주세요.");
+        const amount = parseInt(input, 10);
+        if (isNaN(amount) || amount % this.#lottoPrice !== 0) {
+            throw new Error(
+                "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다."
+            );
+        }
+        return amount;
+    }
 }
 
 export default App;
