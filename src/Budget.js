9677dcc423356fc8b6a29974c009b5663f5f3cad
Stendhalsynd <qmflf556@naver.com>
2023-11-08 07:55:33 +0900

    feat: 로또 구입 금액의 유효성을 판단한다. Budget#validate()

---
+++ src/Budget.js
@@ -0,0 +1,31 @@
+class Budget {
+  #lottery_num;
+
+  constructor(budget) {
+    this.#validate(budget);
+    this.#lottery_num = this.calcLotteryNumbers(this.#lottery_num);
+  }
+
+  #validate(budget) {
+    this.validateBudgetType(budget);
+    this.validateBudgetUnit(budget);
+  }
+
+  validateBudgetType(budget) {
+    const numericBudget = parseFloat(budget);
+
+    if (Number.isNaN(numericBudget)) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+  }
+
+  validateBudgetUnit(budget) {
+    if (budget % 1000 !== 0) {
+      throw new Error(
+        "[ERROR] 로또 구입 금액은 1000원 단위로 입력할 수 있습니다.",
+      );
+    }
+  }
+}
+
+export default Budget;
