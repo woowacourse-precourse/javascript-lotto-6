080277222c6c60b3d35cf6b1094b286629beb6bf
BangDori <qudwns8865@naver.com>
2023-11-07 15:48:45 +0900

    feat: 당첨 통계에서 사용되는 상수 파일 추가

---
+++ src/constants/statistics.js
@@ -0,0 +1,24 @@
+export const MATCH_COUNT = Object.freeze({
+  sixth: 2,
+  fifth: 3,
+  fourth: 4,
+  third: 5,
+  second: 5,
+  first: 6,
+});
+
+export const RANK = Object.freeze({
+  fifth: 'fifth',
+  fourth: 'fourth',
+  third: 'third',
+  second: 'second',
+  first: 'first',
+});
+
+export const PRIZE = Object.freeze({
+  fifth: 5000,
+  fourth: 50000,
+  third: 1500000,
+  second: 30000000,
+  first: 2000000000,
+});
