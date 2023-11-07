ca7ef0b9f0e0c6c1a8be4b310205fb3fc10a2f66
kwoncha <rnjsckgus@gmail.com>
2023-11-08 00:29:44 +0900

    feat: 상수 모듈 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -25,4 +25,22 @@ const ERROR = Object.freeze({
   includedBonusNumber: '[ERROR] 보너스 번호가 중복되어서는 안됩니다.',
 });
 
-export { LOTTO_PRICE, RESULT, PRIZE_MONEY, ERROR };
+const MESSAGE = Object.freeze({
+  PURCHASE: '구입금액을 입력해 주세요.\n',
+  BLINK: '\n',
+  NUMBER_OF_LOTTO: '개를 구매했습니다.',
+  WINNING_NUMBER: '\n당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
+  TITLE: '\n당첨 통계',
+  DIVIDER: '---',
+  UNIT: '개',
+  FIFTH_RESULT: '3개 일치 (5,000원) - ',
+  FOURTH_RESULT: '4개 일치 (50,000원) - ',
+  THIRD_RESULT: '5개 일치 (1,500,000원) - ',
+  SECOND_RESULT: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  FIRST_RESULT: '6개 일치 (2,000,000,000원) - ',
+  PRICE_RATE_PRE: '총 수익률은 ',
+  PRICE_RATE_POST: '%입니다.',
+});
+
+export { LOTTO_PRICE, RESULT, PRIZE_MONEY, ERROR, MESSAGE };
