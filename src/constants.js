889695242bd1be508a40372e937075612b4f8481
mywhzy <cmj2393a@naver.com>
2023-11-07 19:18:28 +0900

    chore: 로또 당첨 등수별 정보 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const RANK_INFO = [
+  { ranking: 5, matchNumbers: 3, winnings: 5000 },
+  { ranking: 4, matchNumbers: 4, winnings: 50000 },
+  { ranking: 3, matchNumbers: 5, winnings: 1500000 },
+  { ranking: 2, matchNumbers: 5, winnings: 30000000 },
+  { ranking: 1, matchNumbers: 6, winnings: 2000000000 },
+];
