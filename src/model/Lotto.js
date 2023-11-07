2abb8b3eda9f50077990ae457e01da29a8abafd5
jonique98 <tnals9899@naver.com>
2023-11-07 23:03:23 +0900

    refactor: 상속 관계로 구조 변경 winningLotto에서 lotto를 상속받는 구조로 변경

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -2,9 +2,9 @@ import { ERRORMESSAGE } from '../constants/constants.js';
 
 class Lotto {
   #numbers;
-  #bonusNumber;
 
   constructor(numbers) {
+
     this.#validate(numbers);
     this.#numbers = numbers;
   }
@@ -13,34 +13,11 @@ class Lotto {
     return this.#numbers;
   }
 
-  getBonusNumber() {
-    return this.#bonusNumber;
-  }
-
-  setBonusNumber(bonusNumber) {
-    this.#bonusValidate(bonusNumber);
-    this.#bonusNumber = bonusNumber;
-  }
-
   #validate(numbers) {
     this.#numberTypeCheck(numbers);
     this.#numberLengthCheck(numbers);
     this.#numberRangeCheck(numbers);
     this.#numberDuplicateCheck(numbers);
-  }
-
-  #bonusValidate(bonusNumber) {
-    if (Number.isNaN(bonusNumber)) {
-      throw new Error(ERRORMESSAGE.bonusType);
-    }
-
-    if (bonusNumber < 1 || bonusNumber > 45) {
-      throw new Error(ERRORMESSAGE.bonusInput);
-    }
-
-    if (this.#numbers.includes(bonusNumber)) {
-      throw new Error(ERRORMESSAGE.bonusDuplicate);
-    }
   }
 
   #numberTypeCheck(numbers) {
---
+++ src/model/WinningLotto.js
@@ -0,0 +1,39 @@
+import { ERRORMESSAGE } from '../constants/constants.js';
+import Lotto from './Lotto.js';
+
+class WinningLotto extends Lotto {
+  #bonusNumber;
+
+  constructor(numbers) {
+    super(numbers);
+  }
+
+  setBonusNumber(bonusNumber) {
+    this.#bonusValidate(bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  getLottoNumber() {
+    return super.getLottoNumber();
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+
+  #bonusValidate(bonusNumber) {
+    if (Number.isNaN(bonusNumber)) {
+      throw new Error(ERRORMESSAGE.bonusType);
+    }
+
+    if (bonusNumber < 1 || bonusNumber > 45) {
+      throw new Error(ERRORMESSAGE.bonusInput);
+    }
+
+    if (super.getLottoNumber().includes(bonusNumber)) {
+      throw new Error(ERRORMESSAGE.bonusDuplicate);
+    }
+  }
+}
+
+export default WinningLotto;
