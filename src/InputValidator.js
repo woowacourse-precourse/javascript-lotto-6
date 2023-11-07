201875e1e6f65fff8c41d641fc00072b11db70c0
장호빈 <devhobin@gmail.com>
2023-11-07 16:35:28 +0900

    refactor<InputValidator, Lotto>: 매직 넘버 상수화

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,62 +1,63 @@
 import AppError, { ERROR_TYPE } from "./AppError.js";
 
 const ROTTO_NUMBER_LENGTH = 6;
+const PRICE_SUFFIX = "000";
+
+const INPUT_REGEX = {
+  number: /^[0-9]+$/,
+  lottoNumber: /^(?:[1-9]|[1-3][0-9]|4[0-5])$/,
+};
+
+const INPUT_ERROR_MESSAGE = {
+  amount: "구입 금액은 숫자만 가능합니다.",
+  unit: "금액은 1000원 단위로 입력 가능합니다.",
+  invalidLottoNumber: "로또 번호는 1 ~ 45 사이의 숫자여야 합니다.",
+  invalidLottoLength: "로또는 6개의 숫자를 선택해야합니다.",
+  duplication: "중복된 로또 번호가 존재합니다.",
+};
 
 class InputValidator {
-  #REGEX = {
-    number: /^[0-9]+$/,
-    lottoNumber: /^(?:[1-9]|[1-3][0-9]|4[0-5])$/,
-  };
-
-  #ERROR_MESSAGE = {
-    amount: "구입 금액은 숫자만 가능합니다.",
-    unit: "금액은 1000원 단위로 입력 가능합니다.",
-    invalidLottoNumber: "로또 번호는 1 ~ 45 사이의 숫자여야 합니다.",
-    invalidLottoLength: "로또는 6개의 숫자를 선택해야합니다.",
-    duplication: "중복된 로또 번호가 존재합니다.",
-  };
-
   validateAmount(input) {
-    if (!this.#REGEX.number.test(input)) {
-      throw new AppError(this.#ERROR_MESSAGE.amount, ERROR_TYPE.inputError);
+    if (!INPUT_REGEX.number.test(input)) {
+      throw new AppError(INPUT_ERROR_MESSAGE.amount, ERROR_TYPE.inputError);
     }
-    if (input.slice(-3) !== "000") {
-      throw new AppError(this.#ERROR_MESSAGE.unit, ERROR_TYPE.inputError);
+    if (input.slice(-PRICE_SUFFIX.length) !== PRICE_SUFFIX) {
+      throw new AppError(INPUT_ERROR_MESSAGE.unit, ERROR_TYPE.inputError);
     }
   }
 
   validateWinningNumbers(input) {
     const nums = input.split(",");
-    if (nums.findIndex((num) => !this.#REGEX.lottoNumber.test(num)) !== -1) {
+    if (nums.findIndex((num) => !INPUT_REGEX.lottoNumber.test(num)) !== -1) {
       throw new AppError(
-        this.#ERROR_MESSAGE.invalidLottoNumber,
+        INPUT_ERROR_MESSAGE.invalidLottoNumber,
         ERROR_TYPE.inputError,
       );
     }
     if (nums.length !== ROTTO_NUMBER_LENGTH) {
       throw new AppError(
-        this.#ERROR_MESSAGE.invalidLottoLength,
+        INPUT_ERROR_MESSAGE.invalidLottoLength,
         ERROR_TYPE.inputError,
       );
     }
     if (new Set(nums).size < ROTTO_NUMBER_LENGTH) {
       throw new AppError(
-        this.#ERROR_MESSAGE.duplication,
+        INPUT_ERROR_MESSAGE.duplication,
         ERROR_TYPE.inputError,
       );
     }
   }
 
   validateBonusNumber(input, winningNumbers) {
-    if (!this.#REGEX.lottoNumber.test(input)) {
+    if (!INPUT_REGEX.lottoNumber.test(input)) {
       throw new AppError(
-        this.#ERROR_MESSAGE.invalidLottoNumber,
+        INPUT_ERROR_MESSAGE.invalidLottoNumber,
         ERROR_TYPE.inputError,
       );
     }
     if (winningNumbers.includes(Number(input))) {
       throw new AppError(
-        this.#ERROR_MESSAGE.duplication,
+        INPUT_ERROR_MESSAGE.duplication,
         ERROR_TYPE.inputError,
       );
     }
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,12 @@
 import AppError from "./AppError.js";
 
+const LOTTO_NUMBER_LENGTH = 6;
+
+const ERROR_MESSAGE = {
+  invalidRottoLength: "로또 번호는 6개여야 합니다.",
+  duplication: "중복된 로또 번호가 존재합니다.",
+};
+
 class Lotto {
   #numbers;
 
@@ -9,11 +16,11 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new AppError("로또 번호는 6개여야 합니다.");
+    if (numbers.length !== LOTTO_NUMBER_LENGTH) {
+      throw new AppError(ERROR_MESSAGE.invalidRottoLength);
     }
-    if (new Set(numbers).size !== 6) {
-      throw new AppError("중복된 로또 번호가 존재합니다.");
+    if (new Set(numbers).size !== LOTTO_NUMBER_LENGTH) {
+      throw new AppError(ERROR_MESSAGE.duplication);
     }
   }
 
