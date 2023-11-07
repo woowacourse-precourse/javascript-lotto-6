4a562c0972dbf8649430f6dca139313dc22e7eb0
visvaden7 <visvaden7@naver.com>
2023-11-08 01:49:00 +0900

    feat: checkInputPurchasedAmount() 입력된 금액을 검증하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import {MissionUtils} from "@woowacourse/mission-utils";
 
 class App {
 
-
+    checkInputPurchasedAmount(purchasedAmount) {
+        if (isNaN(purchasedAmount) || purchasedAmount % 1000 !== 0)
+            throw new Error("[ERROR] 잘못된 숫자를 입력하셨습니다.");
+    }
 
 
     async play() {
