96e6d3b51bda50c1a780fd515b4df9c5deb4af7b
jjinheeWorld <wlsgml450@naver.com>
2023-11-07 21:21:24 +0900

    ✨ Feat: 로또 당첨 번호 유효성 검사 추가

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,4 +6,26 @@ function isValidLottoAmountInput(amount) {
   return true;
 }
 
-export { isValidLottoAmountInput };
+function isValidLottoWinningNumbersInput(winningNumbers) {
+  if (winningNumbers.length !== 6) {
+    throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+  }
+
+  if (new Set(winningNumbers).size !== 6) {
+    throw new Error('[ERROR] 중복되지 않는 6개의 숫자를 뽑아주세요.');
+  }
+
+  winningNumbers.forEach((number) => {
+    if (isNaN(number)) {
+      throw new Error('[ERROR] 빈칸 없이 숫자만 입력해 주세요.');
+    }
+
+    if (number < 1 || number > 45) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+  });
+
+  return true;
+}
+
+export { isValidLottoAmountInput, isValidLottoWinningNumbersInput };
