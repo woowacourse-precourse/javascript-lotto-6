269046ca1f8411c0adb36d73aebee8d24257fa23
wonkyum-kim <dodo12120@naver.com>
2023-11-07 17:21:11 +0900

    test: 당첨 번호가 6개가 아닌 경우

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -116,4 +116,19 @@ describe('로또 테스트', () => {
     // then
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
   });
+
+  test('예외 테스트 - 당첨 번호가 6개가 아닌 경우', async () => {
+    const logSpy = getLogSpy();
+    const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
+    const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5', '1,2,3,4,5,6', '7'];
+    mockRandoms([RANDOM_NUMBERS_TO_END]);
+    mockQuestions([...INPUT_NUMBERS_TO_END]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+  });
 });
