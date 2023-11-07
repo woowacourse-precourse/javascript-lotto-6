15b5f7f2086079f6db576c94d45001e0f65077ac
zelkovaria <hyein396@gmail.com>
2023-11-07 16:28:15 +0900

    feat: WinLotto 생성자 test 구현

---
+++ __tests__/WinLottoTest.js
@@ -0,0 +1,14 @@
+import WinLotto from "../src/WinLotto.js";
+
+describe("WinLottoTest", () => {
+  test("1~45사이 당첨 번호 6개와 보너스 번호 형식이 주어질 경우 객체를 성공적으로 생성한다", () => {
+    const winningNumbers = ["1", "2", "3", "4", "5", "6"];
+    const bonusNumber = "7";
+    const ticketNumbers = [["8", "9", "10", "11", "12", "13"]];
+
+    const winLotto = new WinLotto(winningNumbers, bonusNumber, ticketNumbers);
+    expect(winLotto.winningNumbers).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(winLotto.bonusNumber).toBe(7);
+    expect(winLotto.ticketNumbers).toEqual([[8, 9, 10, 11, 12, 13]]);
+  });
+});
