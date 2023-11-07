7b6126fbd74ecddc0b134b6f419fac9b441a30de
JunSeok <ppu1234@naver.com>
2023-11-08 01:42:03 +0900

    test: printLottoNumbers 함수 테스트 코드 작성

--- __tests__/PrintTest.js
+++ __tests__/PrintTest.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import printEachLotto from '../src/print/printEachLotto';
+import printLottoNumbers from '../src/print/printLottoNumbers';
 
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
@@ -19,5 +20,24 @@ describe('printEachLotto() 함수 테스트', () => {
 
     // Assert
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+  });
+});
+
+describe('printLottoNumbers() 함수 테스트', () => {
+  test('로또 개수와 로또 번호를 출력한다.', () => {
+    // Arrange
+    const logSpy = getLogSpy();
+    const lottoTicketNumber = 2;
+    const lottoBox = [
+      [1, 2, 3, 4, 5, 6],
+      [2, 3, 4, 5, 6, 7],
+    ];
+    const outputs = ['2개를 구매했습니다', '[1, 2, 3, 4, 5, 6]', '[2, 3, 4, 5, 6, 7]'];
+
+    // Act
+    printLottoNumbers(lottoTicketNumber, lottoBox);
+
+    // Assert
+    outputs.forEach((output) => expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output)));
   });
 });
