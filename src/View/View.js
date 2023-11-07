fd847951865ca02f38b88a8df52e2d6dc60ee97d
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-11-08 00:28:32 +0900

    :bug: Fix : 로또 번호 출력 방식 수정
    로또 번호 출력 방식 수정

--- src/View/View.js
+++ src/View/View.js
@@ -24,7 +24,7 @@ const View = {
   },
 
   printLottoNumbers(lottoArray) {
-    lottoArray.forEach((lotto) => Console.print(lotto.getNumbers()));
+    lottoArray.forEach((lotto) => Console.print(`[${lotto.getNumbers().join(', ')}]`));
   },
 
   printResult(resultArray, rateOfReturn) {
