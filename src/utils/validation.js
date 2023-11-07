34279adf34f54c480eee9a0f27eb70ddbc35927b
yeonkyoung <keep.ykk@gmail.com>
2023-11-08 04:07:19 +0900

    feat: 중복 숫자 확인 함수 추가

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -3,3 +3,8 @@ export const isNumber = (amount) => {
   if (isNaN(amount)) throw new Error(NOT_NUMBER_ERROR_MESSAGE);
   return Number(amount);
 };
+
+export const isDuplicate = (lottoArray) => {
+  if (new Set(lottoArray).size !== SIX_NUMBERS)
+    throw new Error(NOT_ALLOWED_DUPLICATED_NUMBERS);
+};
