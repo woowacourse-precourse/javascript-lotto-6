be41d75abb23c7612f3f99e9b866bb8b45b68617
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-08 02:24:22 +0900

    🐞 Fix: message 오타 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -14,8 +14,8 @@ const winningStatistics = Object.freeze({
   [CONSTANTS.match.threeMatch]: `3개 일치 (5,000원) - `,
   [CONSTANTS.match.fourMatch]: `4개 일치 (50,000원) - `,
   [CONSTANTS.match.fiveMatch]: `5개 일치 (1,500,000원) - `,
-  [CONSTANTS.match.fiveMatchWithBonus]: `5개 일치, 보너스 볼 일치 (30,000,0000원) - `,
-  [CONSTANTS.match.sixMatch]: `6개 일치, (2,000,000,000원) - `,
+  [CONSTANTS.match.fiveMatchWithBonus]: `5개 일치, 보너스 볼 일치 (30,000,000원) - `,
+  [CONSTANTS.match.sixMatch]: `6개 일치 (2,000,000,000원) - `,
 });
 
 const MESSAGE = Object.freeze({
