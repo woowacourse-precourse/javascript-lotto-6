b2511cda8835c7ec5b58f20ed246bc78e621a73c
yeokyoung song <spolice0324@gmail.com>
2023-11-08 01:01:52 +0900

    feat : LottoCalculator 클래스 구현
    - Lotto 클래스 상속
    - 로또 수익률과 결과를 계산하기 위한 로직들을 구현
    - 수익률 정보를 매핑하여 값을 누적하는 형태로 구현

---
+++ src/domain/LottoCalculator.js
@@ -0,0 +1,23 @@
+import Lotto from '../Lotto';
+import { LOTTO_INFO } from '../utils/constants';
+
+class LottoCalculator extends Lotto {
+  calculateResult(lottos) {
+    const prizeResultMap = new Map();
+    lottos.forEach(lotto => {
+      const prize = lotto.calculatePrize(this);
+      prizeResultMap.set(prize, (prizeResultMap.get(prize) || 0) + 1);
+    });
+
+    const profits = Array.from(prizeResultMap.entries())
+      .map(([prize, count]) => LOTTO_INFO.PRIZE[prize] * count)
+      .reduce((a, b) => a + b);
+    const lottoSize = Array.from(prizeResultMap.values()).reduce(
+      (a, b) => a + b,
+    );
+    const earningRate = (profits / (lottoSize * LOTTO_INFO.PRICE)) * 100;
+    return { prizeResultMap, earningRate };
+  }
+}
+
+export default LottoCalculator;
