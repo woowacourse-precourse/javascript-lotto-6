145037931aa578a972d7462528855a99ad6af413
sejineeee <sejinee0918@gmail.com>
2023-11-07 14:50:47 +0900

    feature: 당첨번호 유효성 검사 함수 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,31 @@
+const MAX_NUMBER = 45;
+const MIN_NUMBER = 1;
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.validateNumbers(numbers);
     this.#numbers = numbers;
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
-}
+  validateNumbers(numbers) {
+    numbers.forEach((number) => {
+      if (Number.isNaN(Number(number))) {
+        throw new Error('[ERROR] 숫자를 입력해주세요.');
+      }
 
+      if (Number(number) < MIN_NUMBER || Number(number) > MAX_NUMBER) {
+        throw new Error('[ERROR] 1부터 45 사이의 숫자를 입력해주세요.');
+      }
+    });
+  }
+}
 export default Lotto;
