a07714946910e7aff3cea34460cc7d6d77bd54f0
songudeum <songudeum@gmail.com>
2023-11-07 17:44:57 +0900

    feat: 로또 자판기 테스트코드명 변경 및 로또 내역 출력해주는 메소드 테스트 추가

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -13,7 +13,7 @@ describe("app test", () => {
         }
     );
 
-    test("로또 자판기는 6개의 숫자를 로또 개수만큼 출력해야 합니다.", async () => {
+    test("로또 자판기는 6개의 숫자가 든 배열을 로또 개수만큼 출력해야 합니다.", async () => {
         const input = 3000;
         const result = [
             new Lotto([1, 2, 3, 4, 5, 6]),
@@ -24,5 +24,18 @@ describe("app test", () => {
         const app = new App();
 
         await expect(app.lottoVendingMachine(input).length).toBe(result.length);
+    });
+
+    test("로또 내역에 대한 문구를 형식에 맞게 출력해야 합니다.", async () => {
+        const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
+        const lotto2 = new Lotto([1, 2, 3, 4, 5, 7]);
+
+        const input = [lotto1, lotto2];
+        const result =
+            `${input.length}개를 구매했습니다.\n[${input.map((el) => el.printLottoNumber()).join("]\n[")}]`;
+
+        const app = new App();
+
+        await expect(app.renderLottoTickets(input)).toBe(result);
     });
 });
