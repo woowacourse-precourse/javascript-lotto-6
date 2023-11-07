4fd5eb5c47a0d0b37ce9dc6d5e5c2bcdeea7f5f9
Hong sung soo <sow9707@daum.net>
2023-11-07 16:38:53 +0900

    refactor: 로또 당첨금 구하는 메소드 삭제

---
+++ src/classes/Lotto.js
@@ -0,0 +1,73 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { validationUserMoney } from "../validation.js";
+import { lottoNumbers } from "../lotto-number.js";
+import {
+  inputMoney,
+  lottoWinnerNumbers,
+  lottoBonusNumber,
+} from "../user-input.js";
+
+const Console = MissionUtils.Console;
+const LOTTOPRICE = 1000;
+
+const PRIZE = {
+  FIRST: 2000000000,
+  SECOND: 30000000,
+  THIRD: 1500000,
+  FOURTH: 50000,
+  FIFTH: 5000,
+};
+
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+    this.COUNT = 0;
+    this.BONUS = false;
+    this.PRIZE;
+  }
+
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+  }
+
+  compareNumber(winnerNumbers) {
+    this.#numbers.forEach((number) =>
+      this.qulificationWinNumber(winnerNumbers, number)
+    );
+  }
+
+  qulificationWinNumber(winnerNumbers, number) {
+    if (winnerNumbers.includes(number)) {
+      this.COUNT++;
+    }
+  }
+
+  compareBonusNumber(bonusNumber) {
+    if (this.#numbers.includes(+bonusNumber)) {
+      this.BONUS = true;
+    }
+  }
+
+  static async buy() {
+    let userMoney;
+    let whileBreak = true;
+    while (whileBreak) {
+      try {
+        userMoney = await inputMoney();
+        whileBreak = false;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    const manyLottos = userMoney / LOTTOPRICE;
+    const lottoArray = Array(manyLottos).fill();
+    return lottoArray;
+  }
+}
+
+export default Lotto;
