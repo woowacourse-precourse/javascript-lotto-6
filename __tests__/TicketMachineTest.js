d6f333ed79138c45965d89008421cd9ebe34b75b
working-zima <working.zima@gmail.com>
2023-11-07 23:22:36 +0900

    docs(docs/README.md): 게임 실행, 테스트 방법 추가

---
+++ __tests__/TicketMachineTest.js
@@ -0,0 +1,43 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import TicketMachine from '../src/components/TicketMachine';
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+describe('TicketMachine 테스트', () => {
+  test(`발행한 로또 번호 테스트`, async () => {
+    // given
+    const purchaseAmount = 8000;
+    const tickets = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
+
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+
+    // when
+    const ticketMachine = new TicketMachine(purchaseAmount);
+
+    //then
+    expect(ticketMachine.getNumberOfGame()).toEqual(tickets);
+  });
+});
