8414923a4d9fe326cfea677e3317507b87e9390c
ReturnReason <reasonz@daum.net>
2023-11-07 18:11:03 +0900

    chore(LottoNumberGenerationStrategy): 에러 메세지 수정

--- src/model/strategies/LottoNumberGenerationStrategy.js
+++ src/model/strategies/LottoNumberGenerationStrategy.js
@@ -1,6 +1,8 @@
 class LottoNumberGenerationStrategy {
   generateLottoNumber() {
-    throw new Error('generate 메서드를 오버라이드하여 구현해야 합니다.');
+    throw new Error(
+      'generateLottoNumber 메서드를 오버라이드하여 구현해야 합니다.',
+    );
   }
 }
 
