c6cd139a86d4e677d10ee2a8aa160578538c8528
yoovin <soo22839@gmail.com>
2023-11-07 22:24:14 +0900

    feat: 로또 클래스 validate 및 number 반환 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,22 @@ class Lotto {
         if (numbers.length !== 6) {
             throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
         }
+
+        if (new Set(numbers).size !== 6) {
+            throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
+        }
+
+        numbers.forEach((number) => {
+            if (number < 1 || number > 45) {
+                throw new Error('[ERROR] 로또 번호는 1부터 45사이여야 합니다.');
+            }
+        });
     }
 
     // TODO: 추가 기능 구현
+    get numbers() {
+        return this.#numbers.sort((a, b) => a - b);
+    }
 }
 
 export default Lotto;
