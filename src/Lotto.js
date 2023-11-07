ab7976482d99c2d1c6443d154016cbc013817fff
mywhzy <cmj2393a@naver.com>
2023-11-07 18:58:57 +0900

    feat: 로또 등수 구하기 기능 구현 feat: 로또 번호 validate 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,6 @@
+import checkNumbersRange from './utils/checkNumbersRange.js';
+import isDuplicateNumbers from './utils/isDuplicateNumbers.js';
+
 class Lotto {
   #numbers;
 
@@ -9,11 +12,39 @@ class Lotto {
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    } else if (!checkNumbersRange(numbers)) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45까지의 숫자여야 합니다.');
+    } else if (isDuplicateNumbers(numbers)) {
+      throw new Error(
+        '[ERROR] 로또 번호는 중복되지 않는 숫자로 이뤄져야 합니다.'
+      );
     }
   }
 
   get numbers() {
     return this.#numbers;
+  }
+
+  matchNumbers(winningNumbers) {
+    return this.#numbers.filter((number) => winningNumbers.includes(number))
+      .length;
+  }
+
+  getRanking(winningNumbers, bonusNumber) {
+    if (this.matchNumbers(winningNumbers) === 6) {
+      return 1;
+    } else if (
+      this.matchNumbers(winningNumbers) === 5 &&
+      this.#numbers.includes(bonusNumber)
+    ) {
+      return 2;
+    } else if (this.matchNumbers(winningNumbers) === 5) {
+      return 3;
+    } else if (this.matchNumbers(winningNumbers) === 4) {
+      return 4;
+    } else if (this.matchNumbers(winningNumbers) === 3) {
+      return 5;
+    }
   }
 }
 
