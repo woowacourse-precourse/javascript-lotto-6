23e3e9dc7228b26a68608005ee16fcb49d4d5448
publdaze <publdaze@naver.com>
2023-11-07 15:24:20 +0900

    feat: 랜덤 로또 번호 선택 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,9 +1,12 @@
+import { Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
 import LOTTO_INFO from './constants/lottoInfo.js';
 import InputView from './view/InputView.js';
 import OutputView from './view/OutputView.js';
 
 class LottoGame {
   #purchaseAmount;
+  #lottoList;
 
   async play() {
     await this.inputPurchaseAmount();
@@ -18,6 +21,19 @@ class LottoGame {
   purchaseLotto() {
     const lottoCount = this.#purchaseAmount / LOTTO_INFO.purchase.unit;
     OutputView.printPurchaseLottoCount(lottoCount);
+
+    this.#lottoList = Array.from(
+      { length: lottoCount },
+      () => new Lotto(LottoGame.getRandomLottoNumbers()),
+    );
+  }
+
+  static getRandomLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(
+      LOTTO_INFO.lottoNumber.min,
+      LOTTO_INFO.lottoNumber.max,
+      LOTTO_INFO.lottoNumber.count,
+    );
   }
 }
 
--- src/constants/lottoInfo.js
+++ src/constants/lottoInfo.js
@@ -2,6 +2,12 @@ const purchase = Object.freeze({
   unit: 1000,
 });
 
-const LOTTO_INFO = Object.freeze({ purchase });
+const lottoNumber = Object.freeze({
+  min: 1,
+  max: 45,
+  count: 6,
+});
+
+const LOTTO_INFO = Object.freeze({ purchase, lottoNumber });
 
 export default LOTTO_INFO;
