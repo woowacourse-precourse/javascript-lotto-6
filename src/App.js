13db8bdd8376c4a1b28ec7c7955a6f3dc5dd8f5b
eunchong <suicas@naver.com>
2023-11-08 04:47:58 +0900

    로또 티켓 생성 메서드 구현

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ class App {
   async play() {
     try {
       const purchaseAmount = await this.inputPurchaseAmount();
+      const tickets = this.generateLottoTickets(purchaseAmount);
     } catch (error) {
       MissionUtils.Console.print(error.message);
       this.play();
     }
   }
 
+  // 로또 구입 금액 입력
   async inputPurchaseAmount() {
     const input = await MissionUtils.Console.readLineAsync(
       '구입 금액을 입력해 주세요.'
@@ -22,6 +24,19 @@ class App {
       );
     }
     return amount;
+  }
+
+  // 로또 구입 금액에 따라 티켓 생성
+  generateLottoTickets(amount) {
+    const tickets = [];
+    const ticketCount = amount / 1000;
+
+    for (let i = 0; i < ticketCount; i++) {
+      const ticket = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      tickets.push(ticket);
+    }
+
+    return tickets;
   }
 }
 
