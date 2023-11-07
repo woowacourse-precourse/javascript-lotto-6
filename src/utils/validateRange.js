d72d6675e565fbb0b5a4c5473a80a21360716013
songudeum <songudeum@gmail.com>
2023-11-08 01:42:56 +0900

    feat: 문자열이 1~45까지의 범위인지 판단하는 함수 추가

--- src/utils/validateRange.js
+++ src/utils/validateRange.js
@@ -1,4 +1,4 @@
-const isValidRange = (arr) => {
+const isValidRangeArr = (arr) => {
     const invalidRangeArr = arr.filter((el) => el < 1 || el > 45);
     if (invalidRangeArr.length !== 0) {
         return false;
@@ -6,4 +6,9 @@ const isValidRange = (arr) => {
     return true;
 };
 
-export default isValidRange;
+const isValidRange = (num) => {
+    const validRange = /^[1-9]{1}$|^[1-3]{1}[0-9]{1}$|^[4]{1}[0-5]{1}$/;
+    return validRange.test(num);
+};
+
+export { isValidRangeArr, isValidRange };
