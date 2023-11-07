333f5c19648fcd12ad62bc26d17148c8b047c819
dongsuseo <poopoo0515@naver.com>
2023-11-07 16:51:51 +0900

    feat: 로또 클래스 리스트에 저장 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import AmountInput from "./View/AmountInput";
+import Lotto from "./Lotto";
 import makeRandomNumbers from "./makeRandomNumbers";
 import calculateLottoNumber from "./calculateLottoNumber";
 class App {
@@ -8,9 +9,11 @@ class App {
     const lottoNumber = calculateLottoNumber(amount);
     Console.print(`${lottoNumber}개를 구매했습니다.`);
 
+    const lottoList = [];
     for (let i = 0; i < lottoNumber; i++) {
       const sortedRandomNumbers = makeRandomNumbers().sort((a, b) => a - b);
       Console.print(`[${sortedRandomNumbers.join(", ")}]`);
+      lottoList.push(new Lotto(sortedRandomNumbers));
     }
   }
 }
