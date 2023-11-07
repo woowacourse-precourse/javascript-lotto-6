7c99f08ac90d1f82e7554d8c23d536c2c2c2db8c
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-11-07 22:42:53 +0900

    Feat : Lotto 클래스 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,27 +1,43 @@
-/*
-Lotto 클래스
-  제공된 Lotto 클래스를 활용해 구현해야 한다.
-  numbers의 # prefix를 변경할 수 없다.
-  Lotto에 필드를 추가할 수 없다.
+import { MissionUtils } from "@woowacourse/mission-utils";
 
-*/
-
+//입력받은 값을 검증하고, 해당 값을 가지고 연산을 수행해서 결과값을 도출하는 것을 도메인 로직으로 함.
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
     this.#numbers = numbers;
+    this.#validate(numbers);
   }
 
-  //내가 입력한 로또 당첨 번호
   #validate(numbers) {
+    const NUMBER_SET = new Set(numbers);
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (NUMBER_SET.size !== numbers.length) {
+      console.log(NUMBER_SET.length);
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  printLottoNumber() {
+    MissionUtils.Console.print(this.#numbers.sort((a, b) => a - b));
+  }
+
+  compareLottoNumber(winningNumber, bonusNumber) {
+    let correctCount = 0;
+    let bonusFlag = false;
+    const MATCHING_NUMBER = winningNumber.filter((element) =>
+      this.#numbers.includes(element)
+    );
+    if (MATCHING_NUMBER.length >= 3) {
+      correctCount += MATCHING_NUMBER.length;
+    }
+    if (MATCHING_NUMBER.length === 5 && this.#numbers.includes(bonusNumber)) {
+      bonusFlag = true;
+    }
+    return { correctCount, bonusFlag };
+  }
 }
 
 export default Lotto;
