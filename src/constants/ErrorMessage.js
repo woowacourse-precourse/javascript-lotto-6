28dcf20b00f557773d6bd8bb0eb4f2271a1c38ab
aaminha <dks4857@gmail.com>
2023-11-07 18:40:30 +0900

    fix: 상수 내용 수정

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -1,10 +1,12 @@
+import NUMBER from './Number';
+
 const ERROR_SYMBOL = '[ERROR]';
 
 const ERROR_MESSAGE = Object.freeze({
   INVALID_NUMBER: `${ERROR_SYMBOL} 숫자만 입력해주세요. \n`,
-  INVALID_PURCHASE_UNIT: `${ERROR_SYMBOL} 1000원 단위로 입력해주세요. \n`,
-  INVALID_RANGE: `${ERROR_SYMBOL} 1~45 사이의 숫자를 입력해주세요. \n`,
-  INVALID_NUMBER_OF_WINNING_NUMBERS: `${ERROR_SYMBOL} 6개의 숫자를 입력해주세요. \n`,
+  INVALID_PURCHASE_UNIT: `${ERROR_SYMBOL} ${NUMBER.PURCHASE_AMOUNT_UNIT}원 단위로 입력해주세요. \n`,
+  INVALID_RANGE: `${ERROR_SYMBOL} ${NUMBER.MIN_IN_RANGE}~${NUMBER.MAX_IN_RANGE} 사이의 숫자를 입력해주세요. \n`,
+  INVALID_NUMBER_OF_WINNING_NUMBERS: `${ERROR_SYMBOL} ${NUMBER.LOTTO_NUMBER_OF_NUMBERS}개의 숫자를 입력해주세요. \n`,
   INVALID_DUPLICATE_WINNING_NUMBERS: `${ERROR_SYMBOL} 숫자가 중복되지 않게 입력해주세요. \n`,
   INVALID_DUPLICATE_BONUS_NUMBER: `${ERROR_SYMBOL} 당첨 번호와 중복되지 않는 값을 입력해주세요. \n`,
 });
