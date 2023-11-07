04352caec445d3f9f20dfda9bcc6be9d3c73ae2e
dante0214 <rn_chic@naver.com>
2023-11-07 23:49:47 +0900

    feat(getMoney): 입력이 유효하지 않을 때 반복되는 기능 추가

--- src/Game.js
+++ src/Game.js
@@ -2,15 +2,24 @@ import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 
 export async function getMoney() {
-  let moneyInput = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
-  if (isNaN(Number(moneyInput))) {
-    throw new Error("[ERROR]");
-  }
+  let validateGetMoney = false;
+  let moneyInput = 0;
 
-  if (Number(moneyInput) % 1000 !== 0) {
-    throw new Error("[ERROR]");
-  }
+  while (!validateGetMoney) {
+    try {
+      moneyInput = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+      if (isNaN(Number(moneyInput))) {
+        throw new Error("[ERROR]");
+      }
 
+      if (Number(moneyInput) % 1000 !== 0) {
+        throw new Error("[ERROR]");
+      }
+      validateGetMoney = true;
+    } catch (error) {
+      Console.print("[ERROR]");
+    }
+  }
   return Number(moneyInput) / 1000;
 }
 
@@ -28,12 +37,42 @@ export async function getUserLottoInput() {
     "\n당첨 번호를 입력해 주세요.\n"
   );
   const userLottoNumbers = userLottoInput.split(",");
+  validateUserLottoInput(userLottoNumbers);
   const userLottoBonusNum = await Console.readLineAsync(
     "보너스 번호를 입력해 주세요.\n"
   );
+  validateUserBonusInput(userLottoBonusNum);
 
   return {
     numbers: userLottoNumbers.map(Number),
     bonusNumber: Number(userLottoBonusNum),
   };
+}
+export function validateUserLottoInput(numbers) {
+  const parsedNumbers = numbers.map(Number);
+  if (parsedNumbers.length !== 6) {
+    throw new Error("[ERROR] 로또 번호는 6개여야 합니다");
+  }
+  if (new Set(parsedNumbers).size !== 6) {
+    throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다");
+  }
+  for (let number of parsedNumbers) {
+    if (number < 1 || number > 45) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+  }
+}
+
+export function validateUserBonusInput(number) {
+  const parsedNumber = Number(number); // 문자열을 숫자로 변환
+  if (String(parsedNumber) !== number) {
+    // 입력된 값이 숫자로 제대로 변환되었는지 확인
+    throw new Error("[ERROR] 숫자를 입력해 주세요.");
+  }
+  if (number.length !== 1) {
+    throw new Error("[ERROR] 보너스 번호는 1개여야 합니다.");
+  }
+  if (parsedNumber < 1 || parsedNumber > 45) {
+    throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+  }
 }
