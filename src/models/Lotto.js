5b4951195b93538d4c7ff6d219b26d7ffba1ddf0
zxxng <jeayeong123@naver.com>
2023-11-07 19:54:20 +0900

    refactor: 에러메세지 상수화

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -1,41 +1,46 @@
+import { ERROR } from '../constants/messages.js';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#numbers = numbers;
-    this.#validateSize(numbers);
-    this.#validateDuplicate(numbers);
-    this.#validateRange(numbers);
-    this.#sortedLotto(numbers);
+    this.#validateLotto();
+    this.#sortedLotto(this.#numbers);
+  }
+
+  #validateLotto() {
+    this.#validateSize(this.#numbers);
+    this.#validateDuplicate(this.#numbers);
+    this.#validateRange(this.#numbers);
   }
 
   getLotto() {
     return [...this.#numbers];
   }
 
-  // TODO: this.number 수정
   #validateSize(numbers) {
     if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+      throw new Error(ERROR.length);
     }
   }
 
   #validateDuplicate(numbers) {
     numbers.forEach((number) => {
-      if (this.duplicateCount(numbers, number) >= 2) {
-        throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+      if (this.#duplicateCount(numbers, number) >= 2) {
+        throw new Error(ERROR.duplicate);
       }
     });
   }
 
-  duplicateCount(numbers, number) {
+  #duplicateCount(numbers, number) {
     return numbers.filter((current) => current === number).length;
   }
 
   #validateRange(numbers) {
     numbers.forEach((number) => {
       if (1 < number && number > 45) {
-        throw new Error('[ERROR] 로또 번호의 숫자 범위는 1~45 입니다.');
+        throw new Error(ERROR.notInRange);
       }
     });
   }
