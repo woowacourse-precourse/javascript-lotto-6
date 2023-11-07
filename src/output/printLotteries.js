a26a876571f3b87e8c1d357315e7d7325db41020
Majesty-jun <hkwns01@gmail.com>
2023-11-07 12:32:04 +0900

    FIX: 출력 오타 수정

--- src/output/printLotteries.js
+++ src/output/printLotteries.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 function printLotteries(lottery) {
   for (let i = 0; i < lottery.length; i += 1) {
-    Console.print(lottery[i]);
+    Console.print(`[${lottery[i].join(', ')}]`);
   }
 }
 
--- src/output/printResult.js
+++ src/output/printResult.js
@@ -4,9 +4,9 @@ import { INFO_RESULT, RESULT_COUNT, WINNING_PRICE } from '../constants.js';
 function printResult(result, returnRate) {
   Console.print(INFO_RESULT);
   for (let i = 0; i < result.length; i += 1) {
-    Console.print(`${RESULT_COUNT[i]} (${WINNING_PRICE[i].toLocaleString('ko-KR')}원) - ${result[i]}`);
+    Console.print(`${RESULT_COUNT[i]} (${WINNING_PRICE[i].toLocaleString('ko-KR')}원) - ${result[i]}개`);
   }
-  Console.print(`총 수익률은 ${returnRate}% 입니다.`);
+  Console.print(`총 수익률은 ${returnRate}%입니다.`);
 }
 
 export default printResult;
