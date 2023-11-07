2cbc6bde9e4f4547d8c96343dc9528c6c2b25b55
heonq <gkagjsrb@naver.com>
2023-11-07 17:04:45 +0900

    style(Messages): MESSAGES 객체 수정 - 다른 클래스에서 사용하는 이름으로 통일하기 위해 drawStatistics에서 raffleStatistics로 수정 - drawResult를 printRaffleStatistic으로 수정 - earningRate를 printEarningRate로 수정 메서드임을 드러내기 위해 이름 앞에 동사 추가

--- utils/Messages.js
+++ utils/Messages.js
@@ -3,13 +3,13 @@ const MESSAGES = Object.freeze({
   purchaseResult: '개를 구매했습니다.',
   mainNumberQuery: '당첨 번호를 입력해 주세요.\n',
   bonusNumberQuery: '보너스 번호를 입력해 주세요.\n',
-  drawStatistics: '당첨 통계\n---',
+  raffleStatistic: '당첨 통계\n---',
   blank: '',
   printLottoNumberDelimiter: ', ',
   printLottoBracket(lottoNumbers) {
     return `[${lottoNumbers}]`;
   },
-  drawResult([fifth, fourth, third, second, first]) {
+  printRaffleStatistic([fifth, fourth, third, second, first]) {
     return [
       `3개 일치 (5,000원) - ${fifth}개`,
       `4개 일치 (50,000원) - ${fourth}개`,
@@ -18,7 +18,7 @@ const MESSAGES = Object.freeze({
       `6개 일치 (2,000,000,000원) - ${first}개`,
     ];
   },
-  earningRate(earningRate) {
+  printEarningRate(earningRate) {
     return `총 수익률은 ${earningRate}%입니다.`;
   },
 });
