c57aa6f3b9a234b08eedf1319e254dee0ce7ab64
rlarlduf20 <kky8274@naver.com>
2023-11-07 16:26:48 +0900

    Test: 구매금액 입력 함수 테스트 코드 작성
    쉼표를 기준으로 올바른 배열을 생성하는지만 테스트 한 이유는 나머지 예외 상황에 대해서는 Lotto 클래스에서 예외 처리 후 테스트 진행할 예정이기 때문

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,4 +1,7 @@
-import { getLottoCntFromInputMoney } from '../src/utils/getUserInput';
+import {
+  getLottoCntFromInputMoney,
+  getWinningLottoArray,
+} from '../src/utils/getUserInput';
 
 describe('구매금액 입력 함수 테스트', () => {
   test('입력한 구매금액의 값이 숫자가 아니라면 예외가 발생한다.', () => {
@@ -15,5 +18,11 @@ describe('구매금액 입력 함수 테스트', () => {
 
   test('구매 금액의 1/1000에 해당하는 로또의 갯수가 정상적으로 출력된다.', () => {
     expect(getLottoCntFromInputMoney('8000')).toBe(8);
+  });
+});
+
+describe('당첨번호 입력 함수 테스트', () => {
+  test('쉼표를 기준으로 구분하여 올바른 배열을 생성한다.', () => {
+    expect(getWinningLottoArray('1,2,3,4,5,6')).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
