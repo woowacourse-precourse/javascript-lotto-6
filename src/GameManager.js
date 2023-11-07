1ff7f38abb284c7149e8d99c386a0df2d03df477
shgusgh12 <https://github.com/shgusgh12>
2023-11-08 04:56:00 +0900

    feat: 비교한 결과로 등수를 출력하는 기능 구현

--- src/GameManager.js
+++ src/GameManager.js
@@ -1,5 +1,7 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 
+import RANK from './Constant.js';
+
 class GameManager {
   constructor() {}
 
@@ -12,6 +14,36 @@ class GameManager {
   static splitWinNumber(winNumberArray) {
     const splitNumber = winNumberArray.split(',').map((input) => Number(input));
     return splitNumber;
+  }
+
+  static checkResult(lottos, winNumbers, bonusNumber) {
+    let earn = 0;
+    const totalResult = lottos.map((lotto) =>
+      this.changeToRank(lotto.checkMatchNumber(winNumbers, bonusNumber))
+    );
+    RANK.forEach((rank, index) => {
+      const count = totalResult.filter(
+        (lottoRank) => lottoRank === rank.result
+      ).length;
+      Console.print(
+        `${rank.match}개 일치${index === 3 ? `, 보너스 볼 일치` : ' '}(${
+          rank.printMoney
+        }원) - ${count}개`
+      );
+      earn += count * rank.money;
+    });
+    return earn;
+  }
+
+  static changeToRank({ match, bonus }) {
+    if (match === 6) return 'one';
+    if (match === 5 && bonus) return 'two';
+    if (match === 5) {
+      return 'three';
+    }
+    if (match === 4) return 'four';
+    if (match === 3) return 'five';
+    return null;
   }
 }
 
