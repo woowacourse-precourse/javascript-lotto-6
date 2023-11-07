994d8b4f4ae5d9bcf1110884f91c6c193b186d70
OhHyukJun <kmh7277@naver.com>
2023-11-07 12:21:19 +0900

    Feat: 로또 번호 생성, 당첨 번호 생성 실행 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { Computer } from "./Computer/Computer.js";
+import { CreateLottoNum } from "./LottoGame/CreateLottoNum.js";
+import { CreateWinNum } from "./LottoGame/CreateWinnum.js";
 
 class App {
   constructor() {
     this.computer = new Computer();
+    this.createLottoNum = new CreateLottoNum();
+    this.createWinNum = new CreateWinNum();
   }
   
   async play() {
-    MissionUtils.Console.print("dsds");
-    MissionUtils.Console.print(`ddd${this.computer.purchaseLotto}`);
+    await this.createLottoNum.start();
+    await this.createWinNum.start();
   }
 }
 
