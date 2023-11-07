49ca775b4999e4b88e0bd5e47d8e9831d2e492ad
nari <naringst@gmail.com>
2023-11-08 00:58:59 +0900

    feat : 구매 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 class App {
-  async play() {}
+  async play() {
+    //구입 금액 입력받기
+    MissionUtils.Console.print("구입금액을 입력해 주세요.");
+    const totalCost = await MissionUtils.Console.readLineAsync("");
+  }
 }
 
 export default App;
