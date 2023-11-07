df1f0c886158d1b84465746aee0b6c979f68c126
bmj0053 <bmj05378@gmail.com>
2023-11-07 21:21:30 +0900

    refactor: 반복문 수정

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { MESSAGE, MESSAGE_INPUT, ERROR_MESSAGE } from "./constants/constant.js";
 class App {
   async play() {
     const userMoney = await this.getUserMoney();
-    
+
     const randomCount = (userMoney / 1000);
     Console.print(MESSAGE_INPUT(randomCount).COUNT+'\n');
     
-    const userRandomListNums = this.makeRandomNumbers(randomCount);
+    let userRandomList = [];
+    const userRandomListNums = this.makeRandomNumbers(userRandomList);
 
     const numRangePattern = /^(?:[1-9]|[1-3][0-9]|4[0-5])$/;
     const winningNumsList = await this.getWinningNumbers(numRangePattern);
@@ -18,29 +19,27 @@ class App {
   }
 
   async getUserMoney() {
-    while (1) {
-      try {
-        // 보유 금액 입력
-        const inputMoney = await Console.readLineAsync(MESSAGE.START);
-        
-        if (isNaN(inputMoney) || inputMoney === null) {
-          throw new Error(ERROR_MESSAGE.INPUT_USERMONEY_ERROR);
-        }
-        const userMoney = Number(inputMoney)
-        if (userMoney % 1000 > 0 || userMoney <= 0) {
-          throw new Error(ERROR_MESSAGE.INPUT_USERMONEY_ERROR);
-        }
-        return userMoney;
-      } catch (error) {
-        Console.print(ERROR_MESSAGE.INPUT_USERMONEY_ERROR)
+    try {
+      const inputMoney = await Console.readLineAsync(MESSAGE.START);
+      
+      if (isNaN(inputMoney) || inputMoney === null) {
+        throw new Error(ERROR_MESSAGE.INPUT_USERMONEY_ERROR);
       }
+      this.userMoney = Number(inputMoney)
+      if (this.userMoney % 1000 > 0 || this.userMoney <= 0) {
+        throw new Error(ERROR_MESSAGE.INPUT_USERMONEY_ERROR);
+      }
+      return this.userMoney;
+    } catch (error) {
+      Console.print(ERROR_MESSAGE.INPUT_USERMONEY_ERROR)
+      await this.getUserMoney();
     }
   };
 
-  makeRandomNumbers(randomCount) {
-    const userRandomListNums = []; // 배열 저장
+  makeRandomNumbers() {
+    // this.userRandomList = []; // 배열 저장
 
-    while (userRandomListNums.length < randomCount) {
+    // while (this.userRandomList.length < this.randomCount) {
       let randomNums = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
       randomNums.sort((a, b) => a - b); // 오름차순 정렬
       
@@ -58,62 +57,66 @@ class App {
       }
 
       printedString = '[' + printedString + ']'
-      userRandomListNums.push(randomNums); // 숫자 배열 저장
+      this.userRandomList.push(randomNums); // 숫자 배열 저장
       Console.print(printedString)
-    }
-    return userRandomListNums;
+
+      if (this.userRandomList.length < 6) {
+        this.makeRandomNumbers()
+      }
+    // }
+    return this.userRandomListNums;
   };
 
   async getWinningNumbers(numRangePattern) {
     let winningNumsList;
 
-    while (1) {
-      try {
-        const winningNums = await Console.readLineAsync(MESSAGE.WINNING_INPUT);
-        
-        if (
-          winningNums.includes(',,') ||
-          winningNums[0] === ',' ||
-          winningNums[winningNums.length-1] === ',' ||
-          winningNums === null
-          ) {
-          throw new Error(ERROR_MESSAGE.INPUT_ERROR);
-        }
-
-        winningNumsList = winningNums.split(',').map(Number);
-        
-        if (winningNumsList.length !== 6 || winningNumsList.length !== new Set(winningNumsList).size) {
-          throw new Error(ERROR_MESSAGE.INVALID_ERROR)
-        }
-
-        for (let i = 0; i < winningNumsList.length; i++) {
-          if (!numRangePattern.test(winningNumsList[i])) {
-            throw new Error(ERROR_MESSAGE.INPUT_ERROR)
-          }
-        }
-        return winningNumsList;
-      } catch (error) {
-        Console.print(ERROR_MESSAGE.INPUT_ERROR)
+    try {
+      const winningNums = await Console.readLineAsync(MESSAGE.WINNING_INPUT);
+      
+      if (
+        winningNums.includes(',,') ||
+        winningNums[0] === ',' ||
+        winningNums[winningNums.length-1] === ',' ||
+        winningNums === null
+        ) {
+        throw new Error(ERROR_MESSAGE.INPUT_ERROR);
       }
+
+      winningNumsList = winningNums.split(',').map(Number);
+      
+      if (winningNumsList.length !== 6 || winningNumsList.length !== new Set(winningNumsList).size) {
+        throw new Error(ERROR_MESSAGE.INVALID_ERROR)
+      }
+
+      for (let i = 0; i < winningNumsList.length; i++) {
+        if (!numRangePattern.test(winningNumsList[i])) {
+          throw new Error(ERROR_MESSAGE.INPUT_ERROR)
+        }
+      }
+      return this.winningNumsList;
+    } catch (error) {
+      Console.print(ERROR_MESSAGE.INPUT_ERROR);
+      await getWinningNumbers(numRangePattern);
     }
   };
 
   async getBonus(winningNumsList, numRangePattern) {
-    while (1) {
+    // while (1) {
       try {
         const inputBonus = await Console.readLineAsync(MESSAGE.BONUS_INPUT);
 
         if (inputBonus === null || isNaN(inputBonus) || winningNumsList.includes(Number(inputBonus))) {
           throw new Error(ERROR_MESSAGE.INPUT_ERROR)
         }
-        if (!numRangePattern.test(inputBonus)) {
+        if (!numRangePattern.test(Number(inputBonus))) {
           throw new Error(ERROR_MESSAGE.INPUT_ERROR)
         }
         return Number(inputBonus);
       } catch (error) {
-        Console.print(ERROR_MESSAGE.INPUT_ERROR)
+        Console.print(ERROR_MESSAGE.INPUT_ERROR);
+        await this.getBonus(winningNumsList, numRangePattern);
       }
-    }
+    // }
   };
 
   checkWinningResults(userRandomListNums, userMoney, winningNumsList, bonus) {
