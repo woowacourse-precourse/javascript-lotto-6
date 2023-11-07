115f79eec172e7ccf18acf09feb7472dbec36874
zxxng <jeayeong123@naver.com>
2023-11-07 13:46:52 +0900

    test: Lottos 클래스 compareLotto 메서드 테스트 코드 추가

--- __tests__/LottosTest.js
+++ __tests__/LottosTest.js
@@ -1,4 +1,5 @@
 import Lottos from '../src/models/Lottos';
+import WinningLotto from '../src/models/WinnigLotto';
 
 describe('Lottos 클래스 테스트', () => {
   const amount = [3, 5, 10];
@@ -7,9 +8,23 @@ describe('Lottos 클래스 테스트', () => {
     const lottos = new Lottos(amount);
     const lottoList = lottos.getLottos();
 
-    lottoList.forEach((lotto) => {
-      console.log(lotto.getLotto());
-    });
     expect(lottoList.length).toBe(amount);
   });
+
+  const winningNumbers = [1, 2, 3, 4, 5, 6];
+  const bonusNumber = 7;
+  const winningLotto = new WinningLotto({ winningNumbers, bonusNumber });
+
+  test.each(amount)(
+    '추첨 번호와 당첨번호를 비교하여 당첨갯수를 반환한다.',
+    (amount) => {
+      const lottos = new Lottos(amount);
+      const winnigResult = [];
+
+      lottos.compareLotto(winningLotto, winnigResult);
+
+      expect(Array.isArray(winnigResult)).toBe(true);
+      expect(winnigResult.every(Array.isArray)).toBe(true);
+    },
+  );
 });
