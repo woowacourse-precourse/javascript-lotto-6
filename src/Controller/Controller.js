be85ddd543bdcbd4bc3c51dc61c2e58ef789a4fa
육종호 <dbrwhdgh1203@naver.com>
2023-11-08 02:36:31 +0900

    feat: Implement controller functionality for MVC

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -1,27 +1,50 @@
-import Input from '../View/Input';
-import Output from '../View/Output';
+import Input from '../View/Input.js';
+import Output from '../View/Output.js';
+import LottoService from '../Model/LottoService.js';
 
 class Controller {
   #amount;
-
+  #lottoList = [];
   #winnginNum;
-
   #bonusNum;
+  #matchHistory;
+  #rateOfReturn;
 
   constructor() {
     this.input = new Input();
     this.output = new Output();
+    this.lottoService = new LottoService();
   }
 
   async buyLotto() {
     this.#amount = await this.input.inputPurchase();
-    // 구입금액만큼 로또사기
-    // 로또 출력
+    while (this.#amount > 0) {
+      this.#lottoList.push(this.lottoService.createLotto());
+      this.#amount -= 1000;
+    }
+    this.output.purchaseHistory(this.#lottoList);
   }
 
   async inputWinningNum() {
     this.#winnginNum = await this.input.inputWinningNum();
     this.#bonusNum = await this.input.inputBonus();
+  }
+
+  winningStatement() {
+    this.#matchHistory = this.lottoService.getWinningList(
+      this.#lottoList,
+      this.#winnginNum,
+      this.#bonusNum,
+    );
+    this.output.winningDetails(
+      this.#matchHistory,
+      this.#winnginNum,
+      this.#bonusNum,
+    );
+  }
+
+  rateReturn() {
+    this.#rateOfReturn = this.lottoService.getrateReturn(this.#amount);
   }
 }
 
