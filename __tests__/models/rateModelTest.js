3a3e9755f9c0321a39f51197e5ad8a9c3a8401c7
8leamnk <silkviewrk@gmail.com>
2023-11-07 11:08:43 +0900

    test(rateModelTest): 총 수익 금액과 수익률 계산 기능에 대한 테스트 코드를 수정함
    수정 내용:
    - describe로 각각의 테스트 대상을 그룹화
    - test 제목을 보다 알기 쉽게 변경(변경 전: 화폐 단위로 확률 값이 문자열로 반환된다., 변경 후: 확률 값이 화폐 단위의 문자열로 반환된다.)

--- __tests__/models/rateModelTest.js
+++ __tests__/models/rateModelTest.js
@@ -1,52 +1,56 @@
 import rateModel from '../../src/models/rateModel.js';
 
-test('합계를 구한다.', () => {
-  // given
-  const numbers = [1, 2, 3, 4, 5];
-  const OUTPUT = 15;
+describe('총 수익 금액 구하기', () => {
+  test('합계를 구한다.', () => {
+    // given
+    const numbers = [1, 2, 3, 4, 5];
+    const OUTPUT = 15;
 
-  // when
-  const sum = rateModel.getTotal(numbers);
+    // when
+    const sum = rateModel.getTotal(numbers);
 
-  // then
-  expect(sum).toBe(OUTPUT);
+    // then
+    expect(sum).toBe(OUTPUT);
+  });
 });
 
-test('소수점 둘째 자리에서 반올림한 확률 값을 문자열로 반환한다.', () => {
-  // given
-  const TOTAL = 5000;
-  const PRICE = 9000;
-  const OUTPUT = '55.56';
+describe('수익률 계산', () => {
+  test('소수점 둘째 자리에서 반올림한 확률 값을 문자열로 반환한다.', () => {
+    // given
+    const TOTAL = 5000;
+    const PRICE = 9000;
+    const OUTPUT = '55.56';
 
-  // when
-  const result = rateModel.getRate(TOTAL, PRICE);
+    // when
+    const result = rateModel.getRate(TOTAL, PRICE);
 
-  // then
-  expect(result).toBe(OUTPUT);
-});
+    // then
+    expect(result).toBe(OUTPUT);
+  });
 
-test('소수점이 없으면 없는 상태의 확률 값이 문자열로 반환된다.', () => {
-  // given
-  const TOTAL = 5000;
-  const PRICE = 10000;
-  const OUTPUT = '50';
+  test('소수점이 없으면 없는 상태의 확률 값이 문자열로 반환된다.', () => {
+    // given
+    const TOTAL = 5000;
+    const PRICE = 10000;
+    const OUTPUT = '50';
 
-  // when
-  const result = rateModel.getRate(TOTAL, PRICE);
+    // when
+    const result = rateModel.getRate(TOTAL, PRICE);
 
-  // then
-  expect(result).toBe(OUTPUT);
-});
+    // then
+    expect(result).toBe(OUTPUT);
+  });
 
-test('화폐 단위로 확률 값이 문자열로 반환된다.', () => {
-  // given
-  const TOTAL = 1500000;
-  const PRICE = 5000;
-  const OUTPUT = '30,000';
+  test('확률 값이 화폐 단위의 문자열로 반환된다.', () => {
+    // given
+    const TOTAL = 1500000;
+    const PRICE = 5000;
+    const OUTPUT = '30,000';
 
-  // when
-  const result = rateModel.getRate(TOTAL, PRICE);
+    // when
+    const result = rateModel.getRate(TOTAL, PRICE);
 
-  // then
-  expect(result).toBe(OUTPUT);
+    // then
+    expect(result).toBe(OUTPUT);
+  });
 });
