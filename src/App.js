6bf8cba7341914f33dd3e2e3aa358571d934a753
seoye0ng <bsy0313@gmail.com>
2023-11-07 19:53:43 +0900

    Feat: 로또 구입 금액 입력받기 및 입력 받은 금액 예외처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { Console } from '@woowacourse/mission-utils';
+import {
+  LOTTO_PRICE,
+  REQUEST_MESSAGE,
+  VALIDATION_ERRORS_MESSAGE,
+  regexNumber,
+} from './Constants.js';
+
 class App {
-  async play() {}
+  async play() {
+    const money = await this.getMoney();
+  }
+
+  // 1. 로또 구입금액 입력받기
+  async getMoney() {
+    while (true) {
+      try {
+        const money = await Console.readLineAsync(REQUEST_MESSAGE.PUT_MONEY);
+        return this.isValidMoney(money) && money;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  isValidMoney(money) {
+    const moneyNumber = Number(money);
+    if (!money) throw new Error(VALIDATION_ERRORS_MESSAGE.EMPTY_INPUT);
+    if (!regexNumber.test(money))
+      throw new Error(VALIDATION_ERRORS_MESSAGE.NOT_ONLY_NUMBER);
+    if (moneyNumber < LOTTO_PRICE)
+      throw new Error(VALIDATION_ERRORS_MESSAGE.INCORRECT_AMOUNT);
+    if (moneyNumber % LOTTO_PRICE !== 0) {
+      throw new Error(VALIDATION_ERRORS_MESSAGE.INCORRECT_AMOUNT);
+    }
+
+    return true;
+  }
 }
 
 export default App;
