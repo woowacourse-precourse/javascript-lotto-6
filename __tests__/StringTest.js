133d7ce3c1791f069da07bf59e2ed81666392e7d
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-08 00:03:10 +0900

    test: use stringContaining to check error

--- __tests__/StringTest.js
+++ __tests__/StringTest.js
@@ -12,9 +12,15 @@ const mockQuestions = (inputs) => {
   });
 };
 
-describe("구매금액 입력 테스트", () => {
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("정상 입력 테스트", () => {
   beforeAll(() => {
-    const input = ["1000", "", "abc"];
+    const input = ["1000", "1,2,3", "1"];
     mockQuestions(input);
   });
 
@@ -23,51 +29,83 @@ describe("구매금액 입력 테스트", () => {
     expect(amount).toEqual(1000);
   });
 
-  test("구매금액을 입력하지 않으면 예외가 발생한다.", async () => {
-    await expect(inputView.enterPurchaseAmount()).rejects.toThrow("[ERROR]");
-  });
-
-  test("구매금액이 숫자인 문자가 아니면 예외가 발생한다", async () => {
-    await expect(inputView.enterPurchaseAmount()).rejects.toThrow("[ERROR]");
-  });
-});
-
-describe("로또번호 입력 테스트", () => {
-  beforeAll(() => {
-    const input = ["1,2,3", "1,2,", "1,2,a"];
-    mockQuestions(input);
-  });
-
   test("로또번호를 입력하면 숫자가 담긴 배열로 반환한다.", async () => {
     const numbers = await inputView.enterWinningNumber();
     expect(numbers).toContain(1, 2, 3);
-  });
-
-  test("로또번호를 입력하지 않으면 예외가 발생한다.", async () => {
-    await expect(inputView.enterWinningNumber()).rejects.toThrow("[ERROR]");
-  });
-
-  test("로또번호가 숫자인 문자가 아니면 예외가 발생한다.", async () => {
-    await expect(inputView.enterWinningNumber()).rejects.toThrow("[ERROR]");
-  });
-});
-
-describe("보너스 번호 입력 테스트", () => {
-  beforeAll(() => {
-    const input = ["1", "", "abc"];
-    mockQuestions(input);
   });
 
   test("보너스 번호를 입력하면 숫자로 반환한다.", async () => {
     const number = await inputView.enterBonusNumber();
     expect(number).toEqual(1);
   });
+});
+
+describe("구매금액 입력 예외 테스트", () => {
+  beforeAll(() => {
+    const input = ["", "1000", "abc", "1000"];
+    mockQuestions(input);
+  });
+
+  let logSpy;
+  beforeEach(() => {
+    logSpy = getLogSpy();
+  });
+
+  afterEach(() => {
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
+  });
+
+  test("구매금액을 입력하지 않으면 예외가 발생한다.", async () => {
+    await inputView.enterPurchaseAmount();
+  });
+
+  test("구매금액이 숫자인 문자가 아니면 예외가 발생한다", async () => {
+    await inputView.enterPurchaseAmount();
+  });
+});
+
+describe("로또번호 입력 예외 테스트", () => {
+  beforeAll(() => {
+    const input = ["1,2,", "1,2,3", "1,2,a", "1,2,3"];
+    mockQuestions(input);
+  });
+
+  let logSpy;
+  beforeEach(() => {
+    logSpy = getLogSpy();
+  });
+
+  afterEach(() => {
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
+  });
+
+  test("로또번호를 입력하지 않으면 예외가 발생한다.", async () => {
+    await inputView.enterWinningNumber();
+  });
+
+  test("로또번호가 숫자인 문자가 아니면 예외가 발생한다.", async () => {
+    await inputView.enterWinningNumber();
+  });
+});
+
+describe("보너스 번호 입력 예외 테스트", () => {
+  beforeAll(() => {
+    const input = ["", "1", "abc", "1"];
+    mockQuestions(input);
+  });
+
+  let logSpy;
+  beforeEach(() => {
+    logSpy = getLogSpy();
+  });
 
   test("보너스 번호를 입력하지 않으면 예외가 발생한다.", async () => {
-    await expect(inputView.enterBonusNumber()).rejects.toThrow("[ERROR]");
+    await inputView.enterBonusNumber();
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
   });
 
   test("보너스 번호가 숫자인 문자가 아니면 예외가 발생한다.", async () => {
-    await expect(inputView.enterBonusNumber()).rejects.toThrow("[ERROR]");
+    await inputView.enterBonusNumber();
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
   });
 });
