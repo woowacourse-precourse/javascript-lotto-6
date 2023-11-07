1714fd32d19a223572ded7ddab734c89815f4341
heonq <gkagjsrb@naver.com>
2023-11-07 13:44:02 +0900

    feat: 로또 등수 관련 정보를 저장하는 Prize 모듈 생성 - PRIZE 객체엔 등수를 나타내는 문자열을 저장 - PRIZE_REWARD엔 각 등수 별 상금을 저장

---
+++ utils/Prize.js
@@ -0,0 +1,17 @@
+const PRIZE = Object.freeze({
+  3: 'fifthPrize',
+  4: 'fourthPrize',
+  6: 'firstPrize',
+  thirdPrize: 'thirdPrize',
+  secondPrize: 'secondPrize',
+});
+
+const PRIZE_REWARD = Object.freeze({
+  fifthPrize: 5_000,
+  fourthPrize: 50_000,
+  thirdPrize: 1_500_000,
+  secondPrize: 30_000_000,
+  firstPrize: 2_000_000_000,
+});
+
+export { PRIZE, PRIZE_REWARD };
