7128f9106866f64e88128b8a20475e3af5a4554e
wonkyum-kim <dodo12120@naver.com>
2023-11-07 17:29:41 +0900

    refactor: test 코드 재사용으로 길이를 줄임
    - runExeceptionByInput을 재사용하여 테스트 코드의 길이를 줄였습니다

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -42,6 +42,24 @@ const runException = async (input) => {
   expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
 };
 
+const runExceptionByInput = async (input) => {
+  // given
+  const logSpy = getLogSpy();
+
+  const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
+  const INPUT_NUMBERS_TO_END = input;
+
+  mockRandoms([RANDOM_NUMBERS_TO_END]);
+  mockQuestions([input, ...INPUT_NUMBERS_TO_END]);
+
+  // when
+  const app = new App();
+  await app.play();
+
+  // then
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+};
+
 describe('로또 테스트', () => {
   beforeEach(() => {
     jest.restoreAllMocks();
@@ -103,104 +121,32 @@ describe('로또 테스트', () => {
   );
 
   test('예외 테스트 - 당첨 번호가 숫자로 이루어지지 않은 경우', async () => {
-    const logSpy = getLogSpy();
-    const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-    const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5,a', '1,2,3,4,5,6', '7'];
-    mockRandoms([RANDOM_NUMBERS_TO_END]);
-    mockQuestions([...INPUT_NUMBERS_TO_END]);
-
-    // when
-    const app = new App();
-    await app.play();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await runExceptionByInput(['1000', '1,2,3,4,5,a', '1,2,3,4,5,6', '7']);
   });
 
   test('예외 테스트 - 당첨 번호가 6개가 아닌 경우', async () => {
-    const logSpy = getLogSpy();
-    const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-    const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5', '1,2,3,4,5,6', '7'];
-    mockRandoms([RANDOM_NUMBERS_TO_END]);
-    mockQuestions([...INPUT_NUMBERS_TO_END]);
-
-    // when
-    const app = new App();
-    await app.play();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await runExceptionByInput(['1000', '1,2,3,4,5', '1,2,3,4,5,6', '7']);
   });
 
   test('예외 테스트 - 당첨 번호가 1부터 45사이의 숫자가 아닌 경우', async () => {
-    const logSpy = getLogSpy();
-    const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-    const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5,99', '1,2,3,4,5,6', '7'];
-    mockRandoms([RANDOM_NUMBERS_TO_END]);
-    mockQuestions([...INPUT_NUMBERS_TO_END]);
-
-    // when
-    const app = new App();
-    await app.play();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await runExceptionByInput(['1000', '1,2,3,4,5,99', '1,2,3,4,5,6', '7']);
   });
 
   test('예외 테스트 - 당첨 번호에 중복된 숫자가 있는 경우', async () => {
-    const logSpy = getLogSpy();
-    const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-    const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5,5', '1,2,3,4,5,6', '7'];
-    mockRandoms([RANDOM_NUMBERS_TO_END]);
-    mockQuestions([...INPUT_NUMBERS_TO_END]);
-
-    // when
-    const app = new App();
-    await app.play();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await runExceptionByInput(['1000', '1,2,3,4,5,5', '1,2,3,4,5,6', '7']);
   });
 
   test('예외 테스트 - 보너스 번호가 숫자가 아닌 경우', async () => {
-    const logSpy = getLogSpy();
-    const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-    const INPUT_NUMBERS_TO_END = [
-      '1000',
-      '1,2,3,4,5,5',
-      '1,2,3,4,5,6',
-      'a',
-      '7',
-    ];
-    mockRandoms([RANDOM_NUMBERS_TO_END]);
-    mockQuestions([...INPUT_NUMBERS_TO_END]);
-
-    // when
-    const app = new App();
-    await app.play();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    await runExceptionByInput(['1000', '1,2,3,4,5,5', '1,2,3,4,5,6', 'a', '7']);
   });
 
   test('예외 테스트 - 보너스 번호가 1부터 45 사이의 숫자가 아닌 경우', async () => {
-    const logSpy = getLogSpy();
-    const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
-    const INPUT_NUMBERS_TO_END = [
+    await runExceptionByInput([
       '1000',
       '1,2,3,4,5,5',
       '1,2,3,4,5,6',
       '66',
       '7',
-    ];
-    mockRandoms([RANDOM_NUMBERS_TO_END]);
-    mockQuestions([...INPUT_NUMBERS_TO_END]);
-
-    // when
-    const app = new App();
-    await app.play();
-
-    // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+    ]);
   });
 });
