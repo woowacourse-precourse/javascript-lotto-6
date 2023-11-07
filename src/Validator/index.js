4065556e3a3e35633fdd2acf881b0a01c383cd57
Collection50 <fkaus65@naver.com>
2023-11-08 01:43:02 +0900

    refactor(Validator): Validator 클래스의 책임을 분산하기 위하여 클래스 분리

--- src/Validator/index.js
+++ src/Validator/index.js
@@ -1,9 +1,8 @@
-import Formatter from '../Formatter/index.js';
 import ValidationError from './ValidationError/index.js';
 import { NUMBER, ERROR, RANDOM } from '../constants/index.js';
 
 class Validator {
-  static #isNotNaturalNumber(value) {
+  static isNotNaturalNumber(value) {
     const num = Number(value);
 
     if (Number.isNaN(value) || !Number.isSafeInteger(num) || num <= 0) {
@@ -12,9 +11,9 @@ class Validator {
     return false;
   }
 
-  static #isOutOfRange(number) {
+  static isOutOfRange(number) {
     if (
-      this.#isNotNaturalNumber(number) ||
+      this.isNotNaturalNumber(number) ||
       number > RANDOM.max ||
       number < RANDOM.min
     ) {
@@ -22,49 +21,33 @@ class Validator {
     }
   }
 
-  static #validateRangeOfNumbers(numbers) {
-    const formattedNumbers = Formatter.formatInputNumbers(numbers);
-    formattedNumbers.forEach((value) => this.#isOutOfRange(value));
+  static validateRangeOfNumbers(numbers) {
+    if (!numbers.length) {
+      throw new ValidationError(ERROR.outOfRange);
+    }
+    numbers.forEach((number) => this.isOutOfRange(number));
   }
 
-  static validateDuplication(numbers, number) {
-    if (new Set(numbers).size !== NUMBER.lottoCount) {
-      throw new ValidationError(ERROR.invalidWinningNumbers);
-    }
-
-    if (number && numbers.includes(Number(number))) {
-      throw new ValidationError(ERROR.invalidBonusNumber);
+  static validateDuplication(numbers) {
+    if (
+      new Set(numbers).size !== NUMBER.lottoCount ||
+      numbers.length !== NUMBER.lottoCount
+    ) {
+      throw new ValidationError(ERROR.invalidLottoNumbers);
     }
   }
 
   static validatePurchaseAmount(amount) {
     if (
-      this.#isNotNaturalNumber(amount) ||
+      this.isNotNaturalNumber(amount) ||
       Number(amount) % NUMBER.lottoPurchaseUnit
     ) {
       throw new ValidationError(ERROR.invalidPurchaseAmount);
     }
   }
 
-  static validateWinningNumbers(numbers) {
-    const formattedNumbers = Formatter.formatInputNumbers(numbers);
-
-    this.#validateRangeOfNumbers(numbers);
-    this.validateDuplication(formattedNumbers);
-  }
-
-  static validateBonusNumber(numbers, number) {
-    this.#validateRangeOfNumbers(number);
-    this.validateDuplication(numbers, number);
-  }
-
   static validateLottoNumbers(numbers) {
-    if (
-      numbers.length !== NUMBER.lottoCount ||
-      this.validateDuplication(numbers)
-    ) {
-      throw new ValidationError(ERROR.invalidLottoNumbers);
-    }
+    this.validateDuplication(numbers);
   }
 }
 
