87dfdff47c73958be448c026a1308c680ba7797b
dongbin420 <dongbin420@gmail.com>
2023-11-07 23:57:49 +0900

    refactor: change GameController structure for modularity

--- src/application/controller/GameController.js
+++ src/application/controller/GameController.js
@@ -12,35 +12,48 @@ class GameController {
     this.randomNumberGenerator = new RandomNumberGenerator();
     this.gameMessageGenerator = new GameMessageGenerator();
     this.gameCalculator = null;
+    this.purchaseLottos = null;
+    this.purchaseMoney = null;
   }
 
   async start() {
-    const PURCHASE_MONEY = await this.inputView.getUserInputPurchaseMoney();
+    await this.getLottoTryCount();
 
-    const LOTTO_TRY_COUNT = this.getLottoTryCount(PURCHASE_MONEY);
+    await this.getNumbersForWinner();
+  }
 
-    const PURCHASE_LOTTOS = this.randomNumberGenerator.getRandomNumberArray(LOTTO_TRY_COUNT);
-    const PURCHASE_LOTTOS_MESSAGES = this.gameMessageGenerator.getPurchaseLottosMessages(PURCHASE_LOTTOS);
+  async getLottoTryCount() {
+    this.purchaseMoney = await this.inputView.getUserInputPurchaseMoney();
+    const LOTTO_TRY_COUNT = this.purchaseMoney / NUMBER.purchaseMoneyDivisor;
 
-    this.outputView.showPurchaseLottosOutput(LOTTO_TRY_COUNT, PURCHASE_LOTTOS_MESSAGES);
-    
+    this.printPurchaseLottos(LOTTO_TRY_COUNT);
+  }
+
+  printPurchaseLottos(lottoTryCount) {
+    this.purchaseLottos = this.randomNumberGenerator.getRandomNumberArray(lottoTryCount);
+    const PURCHASE_LOTTOS_MESSAGES = this.gameMessageGenerator.getPurchaseLottosMessages(
+      this.purchaseLottos
+    );
+
+    this.outputView.showPurchaseLottosOutput(lottoTryCount, PURCHASE_LOTTOS_MESSAGES);
+  }
+
+  async getNumbersForWinner() {
     const WINNING_NUMBERS = await this.inputView.getUserInputWinningNumbers();
     const BONUS_NUMBER = await this.inputView.getUserInputBonusNumber(WINNING_NUMBERS);
 
-    this.gameCalculator = new GameCalculator(PURCHASE_LOTTOS, WINNING_NUMBERS, BONUS_NUMBER);
+    this.printGameResult(WINNING_NUMBERS, BONUS_NUMBER);
+  }
+
+  printGameResult(winningNumbers, bonusNumber) {
+    this.gameCalculator = new GameCalculator(this.purchaseLottos, winningNumbers, bonusNumber);
     const WINNING_RESULT = this.gameCalculator.calculate();
     const WINNING_RESULT_MESSAGE = this.gameMessageGenerator.getResultMessage(
       WINNING_RESULT,
-      PURCHASE_MONEY
+      this.purchaseMoney
     );
-    
+
     this.outputView.showGameResultOutput(WINNING_RESULT_MESSAGE);
-  }
-
-  getLottoTryCount(purchaseMoney) {
-    const LOTTO_TRY_COUNT = purchaseMoney / NUMBER.purchaseMoneyDivisor;
-
-    return LOTTO_TRY_COUNT;
   }
 }
 
