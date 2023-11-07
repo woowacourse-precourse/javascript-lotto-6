f8f419df09806d32caadfbd0cadd9060292a3c16
KimRosa <108081283+KimRosa@users.noreply.github.com>
2023-11-07 15:04:02 +0900

    Feat(App.js): 티켓 수량 확인
    로또 티켓 수량 확인

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ class App {
         "구입금액을 입력해 주세요."
       );
       await this.checkUserMoneyPossible(userMoney);
+
+      const countTicket = this.countLottoTicket(userMoney);
     }
   }
   async checkUserMoneyPossible(userMoney) {
     if (isNaN(userMoney) || userMoney % 1000 !== 0 || userMoney < 1000) {
       throw new Error("[ERROR] 1000단위의 숫자를 입력해주세요");
     }
+  }
+  countLottoTicket(userMoney) {
+    const countTicket = userMoney / 1000;
+    MissionUtils.Console.print(`${countTicket}개를 구매했습니다.`);
+    return countTicket;
   }
 }
 
