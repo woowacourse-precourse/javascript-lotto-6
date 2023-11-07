d61c06ff2b512ef33c55c9dfb8c5c996dec5dc9d
yeokyoung song <spolice0324@gmail.com>
2023-11-08 01:19:58 +0900

    feat : 메인 게임 로직 App에 구현
    - play 메소드 내 전체적인 게임 진행 로직 구현
    - 로또 형태 및 로또 결과 출력 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { bonusNumberHandler, lottoCalculatorHandler, lottoGeneratorHandler } from "./utils/handlers";
+import OutputView from "./view/OutputView";
+
 class App {
-  async play() {}
+  #lottoGenerator;
+  #result;
+
+  async play() {
+    try {
+      this.#lottoGenerator = await lottoGeneratorHandler();
+      this.printLotto();
+      const LottoCalculator = await lottoCalculatorHandler();
+      await bonusNumberHandler(LottoCalculator);
+      this.#result = LottoCalculator.calculateResult(this.#lottoGenerator.lottos);
+      this.printResult();
+    } catch (err) {
+      if (err) throw err;
+      OutputView.err({ message: err.message });
+    }
+  }
+
+  printLotto() {
+    try {
+      OutputView.lottoGenerator({
+        purchasedList: this.#lottoGenerator.purchasedList,
+      });
+    } catch (err) {
+      OutputView.err({ message: err.message });
+    }
+  }
+
+  printResult() {
+    try {
+      const { prizeResultMap, earningRate } = this.#result;
+      OutputView.result({ prizeResultMap, earningRate });
+    } catch (err) {
+      OutputView.err({ message: err.message });
+    }
+  }
 }
 
 export default App;
