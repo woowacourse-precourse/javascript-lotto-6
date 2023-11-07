160d6e8e20f44792893ff87495abfbf722117dcf
kwani6684 <jack0910@naver.com>
2023-11-07 16:08:41 +0900

    ✨feat: add isZeroError exception in getPayedMoney

--- src/Money.js
+++ src/Money.js
@@ -14,6 +14,7 @@ class Money {
     this.payedMoney = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
     this.noInputError(this.payedMoney);
     this.isNaNError(this.payedMoney);
+    this.isZeroError(this.payedMoney);
     this.notThousandError(this.payedMoney);
   };
   noInputError = (input) => {
@@ -22,16 +23,20 @@ class Money {
     }
   };
   isNaNError = (input) => {
-    if (Number.isNaN(input)) {
+    if (Number.isNaN(Number(input))) {
       throw new Error('[ERROR] 입력값이 숫자가 아닙니다.');
+    }
+  };
+  isZeroError = (input) => {
+    if (input === '0') {
+      throw new Error('[ERROR] 0원으로는 복권을 구매할 수 없습니다.');
     }
   };
   notThousandError = (input) => {
     if (input % 1000 !== 0) {
-      throw new Error('[ERROR] 복권은 천원단위로 구매할 수 있습니다.');
+      throw new Error('[ERROR] 복권은 천원단위로만 구매할 수 있습니다.');
     }
   };
-
   calLottoCount = async () => {
     this.lottoCount = this.payedMoney / this.LOTTO_PRICE;
   };
