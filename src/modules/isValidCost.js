8ac7f00b34587d9ebaaf2cdb7f403a80d85f83b5
JungHun98 <j7papa@naver.com>
2023-11-07 14:51:02 +0900

    refactor: 구입 금액 유효성 함수 검증 알고리즘 수정

--- src/modules/isValidCost.js
+++ src/modules/isValidCost.js
@@ -1,10 +1,30 @@
 const DIVISOR = 1000;
 const ZERO = 0;
 
+const checkList = [
+  {
+    check: (costString) => {
+      const isNumber = /^[0-9]+$/.test(costString);
+      return isNumber;
+    },
+    errorMessage: '[ERROR] 구입 금액은 숫자로 입력해야 합니다.',
+  },
+  {
+    check: (costString) => {
+      const isDividedUp = Number(costString) % DIVISOR === ZERO;
+      return isDividedUp;
+    },
+    errorMessage: `[ERROR] 구입 금액은 ${DIVISOR}단위로 입력해야 합니다.`,
+  },
+];
+
 const isValidCost = (costString) => {
-  const isNumber = /^[0-9]+$/.test(costString);
-  const isDividedUp = Number(costString) % DIVISOR === ZERO;
-  const isValid = isNumber && isDividedUp;
+  const inValidObject = checkList.find((elem) => !elem.check(costString));
+  const isValid = inValidObject === undefined;
+
+  if (!isValid) {
+    throw new Error(inValidObject.errorMessage);
+  }
 
   return isValid;
 };
