156a209e1db455f00848f4744e6fe9b0e0b5cfc4
Su Hwan CHOI <hap_hazard@naver.com>
2023-11-07 16:38:28 +0900

    refactor(LottoMachineTest):️ 결제금액 유효성 관련 테스트 추가
    - 결제금액을 받는 LottoMachine 내 유효성 검사 관련 테스트 추가
    - 기존의 PurchaseAmountValidationTest 제거

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,22 +1,40 @@
 import LottoMachine from '../src/LottoMachine.js';
+import CustomError from '../src/CustomError.js';
+import ERROR from '../src/constants/Error.js';
 
 describe('LottoMachine 클래스 테스트', () => {
-  const lottoMachineInstance = new LottoMachine();
-  test.each([
-    {
-      input: 8000,
-      expectedResult: 8,
-    },
-    {
-      input: 120000,
-      expectedResult: 120,
-    },
-  ])(
-    '로또 반환 갯수 확인 테스트 (입력금액:$input/로또 갯수:$expectedResult개)',
-    ({ input, expectedResult }) => {
-      expect(lottoMachineInstance.getLotto(input).length).toEqual(
-        expectedResult,
-      );
-    },
-  );
+  describe('결제 금액 유효성 검사 테스트', () => {
+    test('결제금액 양의 정수 예외값 테스트', () => {
+      expect(() => {
+        new LottoMachine('positivetest');
+      }).toThrow(new CustomError(ERROR.PAYMENT_NUMBER));
+    });
+
+    test('결제금액 천 단위 예외 테스트', () => {
+      expect(() => {
+        new LottoMachine('12345');
+      }).toThrow(new CustomError(ERROR.PAYMENT_THOUSAND));
+    });
+  });
+  describe('로또 반환 갯수 확인 테스트', () => {
+    const lottoMachineInstance = (input) => new LottoMachine(input);
+
+    test.each([
+      {
+        input: 8000,
+        expectedResult: 8,
+      },
+      {
+        input: 120000,
+        expectedResult: 120,
+      },
+    ])(
+      '로또 반환 갯수 (입력금액:$input/로또 갯수:$expectedResult개)',
+      ({ input, expectedResult }) => {
+        expect(lottoMachineInstance(input).getLotto().length).toEqual(
+          expectedResult,
+        );
+      },
+    );
+  });
 });
