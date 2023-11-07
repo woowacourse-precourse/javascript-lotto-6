c9bf1068d00c97d33d40afd035ca2df098d8af65
jungmyungjin <myung2667@gmail.com>
2023-11-08 04:54:23 +0900

    refactor(App, Lotto) : JSDOC 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,10 @@ class Lotto {
     this.#numbers = numbers.sort((a, b) => a - b);
   }
 
+  /**
+   * 로또 번호가 유효한지 검증한다.
+   * @param {number[]} numbers
+   */
   #validate(numbers) {
     validateLottoNumbersCountMismatch(numbers);
     validateLottoNumbersNotAllNumbers(numbers);
@@ -20,6 +24,10 @@ class Lotto {
     validateLottoNumberIsUniq(numbers);
   }
 
+  /**
+   * 로또 번호가 있는 배열을 반환한다.
+   * @returns this.#numbers;
+   */
   getNumbers() {
     return this.#numbers;
   }
