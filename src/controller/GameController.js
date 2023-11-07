c46b1ae1530cef694f24df758ebfb59753954bdb
sryung <sryung1225@gmail.com>
2023-11-08 02:57:33 +0900

    feat: 구매한 로또 수량 구하기
    구매 금액에 맞춰서 로또 수량 계산
    구매 금액과 로또 수량을 각각 GameController 클래스의 private 속성으로 지정하여 관리

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,12 +1,22 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Validator from '../utils/validation.js';
 import UserInputView from '../view/UserInputView.js';
+import { MESSAGE } from '../constants/Constants.js';
 
 class GameController {
+  #purchasePrice;
+
+  #tickectsCount;
+
   async startGame() {
-    const purchasePrice = Number(await UserInputView.inputPrice());
+    const inputPurchasePrice = Number(await UserInputView.inputPrice());
     try {
-      Validator.purchasePrice(purchasePrice);
+      Validator.purchasePrice(inputPurchasePrice);
+      this.#purchasePrice = inputPurchasePrice;
+      this.#tickectsCount = inputPurchasePrice / 1000;
+      MissionUtils.Console.print(
+        `${this.#tickectsCount}${MESSAGE.purchaseAmount}`,
+      );
     } catch (error) {
       MissionUtils.Console.print(error.message);
       await this.startGame();
