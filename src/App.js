5636839d8343fdbac2433b859bcdd75d921bf9aa
DESKTOP-55LJG2P\교육생47 <onmer99@gmail.com>
2023-11-07 22:10:14 +0900

    feat: validate구현, 로또 구매 및 당첨 결과 출력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-class App {
-  async play() {}
-}
+import Lotto from "./Lotto.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
+class App {
+  #lottos = [];
+
+  // TODO: 로또 구매 및 당첨 결과 출력
+  async play() {
+    try {
+      const purchaseAmount = await this.#askPurchaseAmount();
+      this.#lottos = this.#purchaseLottos(purchaseAmount);
+      this.#printLottos(this.#lottos);
+
+      const winningNumbers = await this.#askWinningNumbers();
+      const bonusNumber = await this.#askBonusNumber();
+      const results = this.#checkResults(winningNumbers, bonusNumber);
+      const profitRate = this.#calculateProfitRate(purchaseAmount, results);
+
+      this.#printResults(results);
+      this.#printProfitRate(profitRate);
+    } catch (error) {
+      console.error("[ERROR]", error); // 두 번째 인수로 에러 객체를 전달
+    }
+  }
+
+}
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,24 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
+  }
+
+  get numbers() {
+    return this.#numbers;
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+    if (numbers.some(number => number < 1 || number > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
