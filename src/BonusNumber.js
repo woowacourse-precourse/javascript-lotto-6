e03e0aebf8b3a8b10b5daa6326681cae2f160212
inhwa <dlsghk0711@naver.com>
2023-11-07 21:21:51 +0900

    보너스 번호 예외 처리 수정

--- src/BonusNumber.js
+++ src/BonusNumber.js
@@ -5,8 +5,16 @@ class BonusNumber {
   }
   validate(bonusNumber){
     if(isNaN(bonusNumber)!=false){
-      throw new Error('[ERROR] 올바른 숫자를 입력해주세요.');
+      throw new Error('[ERROR] 보너스 번호는 숫자만 입력할 수 있습니다.');
     } 
+
+    if(bonusNumber.includes(' ')){
+      throw new Error('[ERROR] 보너스 번호는 공백을 포함할 수 없습니다.');
+    }
+
+    if (bonusNumber>45 || bonusNumber<0){
+      throw new Error('[ERROR] 보너스 번호는 1~45 범위 내에서만 가능합니다.');
+    }
   }
 }
 export default BonusNumber;
