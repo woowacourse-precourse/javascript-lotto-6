9af688960ed0ab2e67bb93df3cff488ad5915941
Goonco <s07019@naver.com>
2023-11-07 22:27:56 +0900

    :pencil2: 로또 구매 - 바뀐 상수명에 대한 수정

--- src/validator/moneyValidate.js
+++ src/validator/moneyValidate.js
@@ -1,10 +1,10 @@
 import { LOTTO_PRICE } from "../constants/LottoConstants.js";
-import { ERRMSG_LOTTO_NOT_MONEY, ERRMSG_LOTTO_NOT_DIVIDED_BY_LOTTO_PRICE } from "../constants/ErrorMessage.js";
+import { ERRMSG_MONEY_NOT_MONEY, ERRMSG_MONEY_NOT_DIVIDED_BY_LOTTO_PRICE } from "../constants/ErrorMessage.js";
 
 export const isMoney = (money) => {
-    if(!Number.isInteger(money)) throw new Error(ERRMSG_LOTTO_NOT_MONEY);
+    if(!Number.isInteger(money)) throw new Error(ERRMSG_MONEY_NOT_MONEY);
 }
 
 export const isDividedByLottoPrice = (money) => {
-    if(money % LOTTO_PRICE !== 0) throw new Error(ERRMSG_LOTTO_NOT_DIVIDED_BY_LOTTO_PRICE);
+    if(money % LOTTO_PRICE !== 0) throw new Error(ERRMSG_MONEY_NOT_DIVIDED_BY_LOTTO_PRICE);
 }
