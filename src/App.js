07e86c6960ca81533b2e0cd8b2f10f6faeb18378
Myomyoring <narcissuscat@naver.com>
2023-11-07 23:31:03 +0900

    [feat] 당첨 내역 출력

--- src/App.js
+++ src/App.js
@@ -77,6 +77,12 @@ class App {
 					ranks.first += 1;
 				}
 			});
+			Console.print(`\n당첨 통계\n---`);
+			Console.print(`3개 일치 (5,000원) - ${ranks.fifth}개`);
+			Console.print(`4개 일치 (50,000원) - ${ranks.fourth}개`);
+			Console.print(`5개 일치 (1,500,000원) - ${ranks.third}개`);
+			Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${ranks.second}개`);
+			Console.print(`6개 일치 (2,000,000,000원) - ${ranks.first}개`);
 		} catch (error) {
 			throw error;
 		}
