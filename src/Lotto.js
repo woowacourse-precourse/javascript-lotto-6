1ec8cd0cda64133ee37ef1fd552acb11acb8b39e
Rilee-0320 <go13209@naver.com>
2023-11-08 00:10:51 +0900

    feat: 로또 당첨 결과 계산 및 출력

--- src/Lotto.js
+++ src/Lotto.js
@@ -33,4 +33,37 @@ function printBuyingLottoNumbers(numberList) {
   }
 }
 
+function calculator(buyingNumberList, winningNumbers, bonusNumber) {
+  const result = [0, 0, 0, 0, 0];
+
+  for (const oneLotto of buyingNumberList) {
+    const intersection = [oneLotto.filter((x) => winningNumbers.includes(x))];
+    const bonusMatch = oneLotto.includes(bonusNumber);
+
+    if (intersection.length === 6) {
+      result[0]++;
+    } else if (intersection.length === 5) {
+      if (bonusMatch) {
+        result[1]++;
+      } else {
+        result[2]++;
+      }
+    } else if (intersection.length === 4) {
+      result[3]++;
+    } else if (intersection.length === 3) {
+      result[4]++;
+    }
+  }
+  return result;
+}
+
+function printResult(result) {
+  Console.print("당첨 통계\n---");
+  Console.print(`3개 일치 (5,000원) - ${result[4]}개`);
+  Console.print(`4개 일치 (50,000원) - ${result[3]}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${result[2]}개`);
+  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[1]}개`);
+  Console.print(`6개 일치 (2,000,000,000원) - ${result[0]}개`);
+}
+
 export default Lotto;
