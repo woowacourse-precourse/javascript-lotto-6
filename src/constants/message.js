204a59c44eb960f90bb9cac4c0183f2ebaaa3fe5
pjc0179 <pjc0179@gmail.com>
2023-11-07 20:49:04 +0900

    refactor(constansts):"message.js 프로퍼티 수정"

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,8 +1,9 @@
 const MESSAGES = Object.freeze({
-  USER_INPUT_LOTTO_TOTAL_BUY_PRICE: '구입금액을 입력해 주세요.\n',
-  USER_INPUT_LOTTO_CORRECT_NUMBER: '당첨 번호를 입력해 주세요.\n',
-  USER_INPUT_LOTTO_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
-  BUY_MESSAGE: '개를 구매했습니다.',
+  userInputLottoBuyTotalPrice: '구입금액을 입력해 주세요.\n',
+  userInputLottoCorrectNumber:
+    '당첨 번호를 입력해 주세요 번호의 구분은 ,로 해주세요.\n',
+  userInputLottoBonusNumber: '보너스 번호를 입력해 주세요.\n',
+  buyMessage: '개를 구매했습니다.',
   totalWinning: '당첨 통계 \n---\n',
 });
 
