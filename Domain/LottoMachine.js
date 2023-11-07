e9e93d49cfe686cc52d413ed04501451943a1380
jeongin98 <eon7500@gmail.com>
2023-11-07 09:01:35 +0900

    Feat(Controller): 금액만큼 로또 구매 후 저장

--- Domain/LottoMachine.js
+++ Domain/LottoMachine.js
@@ -1,2 +1,23 @@
-class LottoMachine {}
+import Lotto from './Lotto.js';
+import Random from '';
+
+class LottoMachine {
+  #random;
+
+  constructor(Random) {
+    this.#random = Random;
+  }
+
+  buyLotto(money) {
+    // 유효성 검사 추가
+    const buyCount = Math.floor(money / 1000);
+
+    const lottoList = [];
+    for (let i = 0; i < buyCount; i++) {
+      lottoList.push(new Lotto(this.#random.getRandomLotto()));
+    }
+
+    return lottoList;
+  }
+}
 export default LottoMachine;
