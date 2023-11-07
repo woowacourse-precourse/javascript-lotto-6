cd41e3f16bc3dda80b89907ea843ff7827b506d6
Jihyun_Kim <jihyun3313@daum.net>
2023-11-07 21:31:16 +0900

    style: number 상수 객체화

--- src/constatns/number.js
+++ src/constatns/number.js
@@ -1,4 +1,19 @@
 export const LOTTO_PRICE = 1000;
 export const LOTTO_COUNT = 6;
 export const LOTTO_START_NUM = 1;
-export const LOTTO_END_NUM= 45;
+export const LOTTO_END_NUM = 45;
+
+export const LOTTO = Object.freeze({
+  PRICE: 1000,
+  COUNT: 6,
+  START_NUM: 1,
+  END_NUM: 45,
+});
+
+export const RESULT = Object.freeze({
+  THREE: 0,
+  FOUR: 1,
+  FIVE: 2,
+  FIVE_BONUS: 3,
+  SIX: 4
+});
--- src/validator.js
+++ src/validator.js
@@ -1,15 +1,15 @@
 import { ERROR_MESSAGE } from "./constatns/message.js"
-import { LOTTO_PRICE, LOTTO_COUNT, LOTTO_START_NUM, LOTTO_END_NUM } from "./constatns/number.js"
+import { LOTTO } from "./constatns/number.js"
 
 export const validAmountCheck = async (amount) => {
-  if ((amount % LOTTO_PRICE) !== 0)
+  if ((amount % LOTTO.PRICE) !== 0)
     throw new Error(ERROR_MESSAGE.AMOUNT);
 }
 
 export const validBonusNumberCheck = async (number) => {
   if (number.includes(","))
     throw new Error(ERROR_MESSAGE.BONUS_NUM_COUNT);
-  if (number < LOTTO_START_NUM || number > LOTTO_END_NUM)
+  if (number < LOTTO.START_NUM || number > LOTTO.END_NUM)
     throw new Error(ERROR_MESSAGE.NUM_RANGE);
   return number;
 }
