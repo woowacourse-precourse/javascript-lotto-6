3e890cd38cf0a2b74e99332ae7c66975d0450242
Goonco <s07019@naver.com>
2023-11-07 22:25:30 +0900

    :sparkles: 로또 발급 - 필요한 로또 수 만큼 로또를 생성하는 기능 추가

--- src/utils/generateLottos.js
+++ src/utils/generateLottos.js
@@ -1,13 +1,15 @@
+import { Random } from "@woowacourse/mission-utils";
 import Lotto from "../model/Lotto.js";
-import generateLottoNumbers from "./generateLottoNumbers.js";
+import { MAXIMUM_LOTTO_NUMBER, MINIMUM_LOTTO_NUMBER, LOTTO_LENGTH } from "../constants/LottoConstants.js"
+
+const generateLottoNumbers = () => Random.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER, LOTTO_LENGTH)
 
 const generateLottos = (neededLotto) => {
     const lottoSet = [];
 
-    let newLottoNumbers;
     let lottoMadded = 0;
     while(lottoMadded !== neededLotto){
-        newLottoNumbers = generateLottoNumbers();
+        const newLottoNumbers = generateLottoNumbers();
         lottoSet.push(new Lotto(newLottoNumbers));
         lottoMadded += 1;
     }
