c110a8ec236041438fa3cbf77033d0842dcf3d10
kimyonghui <kyhui1115@gmail.com>
2023-11-07 18:23:38 +0900

    style: 4. 당첨 내역 및 수익률 관련 UI 구현

--- src/WinningStatistics.js
+++ src/WinningStatistics.js
@@ -92,6 +92,17 @@ class WinningStatistics {
     this.#totalYield = TOTAL_YIELD;
   }
 
+  ui() {
+    Console.print('\n당첨통계\n---');
+    Console.print(`3개 일치 (5,000원) - ${this.#winningThree}개`);
+    Console.print(`4개 일치 (50,000원) - ${this.#winningFour}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.#winningFive}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.#winningFiveBonus}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.#winningSix}개`);
+    Console.print(`총 수익률은 ${this.#totalYield}%입니다.`);
+  }
 }
 
 export default WinningStatistics;
