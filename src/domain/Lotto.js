ac774d0a7527d673cb204efca0d25179d4808131
MinGu Jeong <mingu830@gmail.com>
2023-11-07 12:27:06 +0900

    feat: 로또 예외처리

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -7,12 +7,31 @@ class Lotto {
   }
 
   #validate(numbers) {
+    // this.#validateNumberCount(numbers);
+    // this.#validateNumberRange(numbers);
+    // this.#validateNumberDuplicate(numbers);
+  }
+
+  #validateNumberCount(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateNumberRange(numbers) {
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error('[ERROR] 로또 번호는 1~45 사이여야 합니다.');
+      }
+    });
+  }
+
+  #validateNumberDuplicate(numbers) {
+    const numberSet = new Set(numbers);
+    if (numberSet.size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    }
+  }
 }
 
 export default Lotto;
