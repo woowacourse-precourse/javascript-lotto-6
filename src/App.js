55a137ec86a03038a41e461ce683e291dac382f5
LEEHYEYEONG <elto789@naver.com>
2023-11-07 09:15:33 +0900

    feat: 로또 구익 금액 기능 구현 - 클래스로 구현 - 예외처리(1000으로 나누어지지 않는 경우 0이하인 경우, 문자열인 경우)

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
+import InputPrice from "./InputPrice.js";
 
 class App {
-
-  // 로또 살 돈을 입력받는 함수
-  async lottoBudget() {
+  async play() {
     const budgetInput = await Console.readLineAsync(
       "구입금액을 입력해 주세요.\n"
     );
-    if (budgetInput % 1000 !== 0 || budgetInput <= 0) {
-      throw new Error("[ERROR]")
-    } else if (isNaN(parseInt(budgetInput))) {
-      throw new Error("[ERROR]")
-    }
-  }
-
-  //랜덤 로또 만드는 함수 
-  makeLotto() {
-    const computer = [];
-    while (computer.length < 6) {
-      const number = Random.pickNumberInRange(1, 45);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
-    }
-    return computer;
-  }
-  async play() {
-    this.lottoBudget();
-    const lottoNumber = this.makeLotto();
+    const budgetPrice = new InputPrice(budgetInput);
+    budgetPrice.validate(budgetInput)
+    const numLotto = budgetPrice.calculateLottoCount(budgetInput);
+    printLotto(numLotto);
   }
 }
 
-export default App;
+function makeLotto() {
+  const computer = [];
+  while (computer.length < 6) {
+    const number = Random.pickNumberInRange(1, 45);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer.sort();
+}
 
-const app = new App()
-app.play()
+function printLotto(numLotto) {
+  Console.print("");
+  Console.print(String(numLotto) + "개를 구매했습니다.")
+  for (let i = 0; i < numLotto; i++) {
+    Console.print(makeLotto())
+  }
+  Console.print("");
+}
+
+export default App;
---
+++ src/InputPrice.js
@@ -0,0 +1,19 @@
+class InputPrice {
+  constructor(price) {
+    this.validate(price);
+    this.price = price;
+  }
+  validate(price) {
+    if (price % 1000 !== 0 || price <= 0) {
+      throw new Error("[ERROR]");
+    }
+    else if (isNaN(parseInt(price))) {
+      throw new Error("[ERROR]");
+    }
+  }
+  calculateLottoCount(price) {
+    return price / 1000;
+  }
+}
+
+export default InputPrice
