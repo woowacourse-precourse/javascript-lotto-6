1615324abd19baf65b9d36628d6b7068eac34c43
BangDori <qudwns8865@naver.com>
2023-11-07 21:12:24 +0900

    feat: 어플리케이션 실행 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import LottoController from './controller/LottoController.js';
+import retryOnError from './utils/retry.js';
+import InputView from './views/InputView.js';
+
 class App {
-  async play() {}
+  async play() {
+    const money = await retryOnError(InputView.readMoney);
+    const lottoController = new LottoController(money);
+
+    const winningNumbers = await retryOnError(InputView.readWinningNumbers);
+    const bonusNumber = await retryOnError(
+      InputView.readBonusNumber,
+      winningNumbers,
+    );
+
+    lottoController.compareLottos(winningNumbers, bonusNumber);
+    lottoController.showLottoResult();
+  }
 }
 
 export default App;
