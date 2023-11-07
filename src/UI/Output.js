118c529ca5cce33a8544fc197a993f8f92e750ba
youjeong Jeong <wjddbwjd1205@naver.com>
2023-11-08 05:43:49 +0900

    refactor: 출력하는는 로직 분리
    UI를 담당하는 로직 중 출력하는 부분

---
+++ src/UI/Output.js
@@ -0,0 +1,25 @@
+class Output {
+  boughtTickets(lottoTickets, count) {
+    Console.print(`${count}개를 구매했습니다.`);
+    lottoTickets.forEach(lottoTicket => {
+      Console.print(`[${lottoTicket.join(', ')}]`);
+    });
+  }
+
+  winningResults(winningResult) {
+    const prizeMoney = ['5,000', '50,000', '1,500,000', '30,000,000', '2,000,000,000'];
+    const matchCountList = [3, 4, 5, 5, 6];
+    const reversedWinningResult = [...winningResult].reverse();
+    reversedWinningResult.forEach((result, index) => {
+      let bonusText = '';
+      if (index === 3) {
+        bonusText = ', 보너스 볼 일치';
+      }
+      Console.print(`${matchCountList[index]}개 일치${bonusText} (${prizeMoney[index]}원) - ${result}개`)
+    });
+  }
+
+  profitRate(profitRate) {
+    Console.print(`총 수익률은 ${profitRate}%입니다.`);
+  }
+}
