1346e1240ccf0ea3caf89fefc9f35809b473bdbb
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-11-07 19:40:02 +0900

    test: 보너스 번호 중복 여부 테스트 구현
    입력받은 보너스 번호가 당첨 번호와 중복되지 않는지 확인하는 테스트 코드를 구현하였습니다.

--- __tests__/GenerateBonusNumber.js
+++ __tests__/GenerateBonusNumber.js
@@ -44,6 +44,16 @@ describe("보너스 번호 생성", () => {
     await expect(app.play()).rejects.toThrow("[ERROR] 1에서 45 사이에 숫자만 입력이 가능합니다.");
   });
 
+  test("보너스 번호가 중복된 숫자의 경우", async () => {
+    mockRandoms([[1, 2, 3, 4, 5, 6]]);
+    mockQuestions(["1000", "1,2,3,4,5,6", "5"]);
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR] 당첨 번호와 중복되지 않은 숫자만 입력이 가능한니다.");
+  });
+
   test("BONUS_NUMBER 상수 확인", async () => {
     const winningNumberMockData = [1, 2, 3, 4, 5, 6];
     const bonusNumberMockData = [2];
