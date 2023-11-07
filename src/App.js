d72f038dee91e85ea9a4223957e42044189c4eba
j8won <joowoni99@gmail.com>
2023-11-08 03:34:27 +0900

    feat(App): Lotto, LottoController, LottoPrizeManager, InputHandler, OutputHandler를 활용해 전체 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,63 @@
+import LottoController from "./LottoController.js";
+import Lotto from "./Lotto.js";
+import LottoPrizeManager from "./LottoPrizeManager.js";
+import InputHandler from "./InputHandler.js";
+import OutputHandler from "./OutputHandler.js";
+
 class App {
-  async play() {}
+  #totalMoney;
+  #lottoController;
+  #prizeManager;
+
+  async initializeLottoController() {
+    this.#totalMoney = await InputHandler.getUserTotalMoney();
+    this.#lottoController = new LottoController(this.#totalMoney, (number) => {
+      return new Lotto(number);
+    });
+  }
+
+  printLottoTickets() {
+    const ticketCount = this.#lottoController.lottoArray.length;
+    OutputHandler.printLottoTicketCount(ticketCount);
+    OutputHandler.printAllLottoNumbers(
+      this.#lottoController.lottoArray.map((lotto) => lotto.numbers)
+    );
+  }
+
+  async initializePrizeManager() {
+    const winningNumberString = await InputHandler.getWinningNumberString();
+    const bonusNumberString = await InputHandler.getBonusNumberString();
+
+    this.#prizeManager = new LottoPrizeManager(
+      winningNumberString,
+      bonusNumberString
+    );
+  }
+
+  calculateLottoPrizeResult() {
+    const rankResult = this.#prizeManager.calculateAllLottoRank(
+      this.#lottoController.lottoArray.map((lotto) => lotto.numbers)
+    );
+
+    const profitRate = LottoPrizeManager.calculateProfitRate({
+      rankResult,
+      totalMoney: +this.#totalMoney,
+    });
+
+    OutputHandler.printPrizeResult({ rankResult, profitRate });
+  }
+
+  async play() {
+    try {
+      await this.initializeLottoController();
+      this.printLottoTickets();
+
+      await this.initializePrizeManager();
+      this.calculateLottoPrizeResult();
+    } catch (e) {
+      OutputHandler.printErrorMessage(e);
+    }
+  }
 }
 
 export default App;
