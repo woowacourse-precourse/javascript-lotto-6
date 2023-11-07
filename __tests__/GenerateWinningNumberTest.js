ae532ec095229ed828ecceffa3c9f18ab5b3d1bb
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-11-07 18:31:51 +0900

    test: 당첨 번호 중복 여부 테스트 코드 구현
    입력 받은 당첨 번호 중에 중복 여부에 대한 예외 상황 테스트 코드를 구현하였습니다.

--- __tests__/GenerateWinningNumberTest.js
+++ __tests__/GenerateWinningNumberTest.js
@@ -52,4 +52,14 @@ describe("당첨 번호 생성", () => {
     // then
     await expect(app.play()).rejects.toThrow("[ERROR] 1에서 45 사이에 숫자만 입력이 가능합니다.");
   });
+
+  test("당첨 번호가 중복 된 경우", async () => {
+    mockRandoms([[1, 2, 3, 4, 5, 6]]);
+    mockQuestions(["1000", "1,2,3,4,5,5", "7"]);
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR] 중복되지 않는 6개의 숫자로 입력해 주세요.");
+  });
 });
