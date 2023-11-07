d2a74b1bc8a40c7f39da85d02e447d86342316ed
coolIlive <galilee155@naver.com>
2023-11-08 00:45:59 +0900

    style: 주석 정리

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,7 +15,6 @@ class Lotto {
     this.sameNumber();
   }
 
-  // 쉼표 외의 문자가 들어간 경우
   notNumberElement() {
     this.#numbers.forEach(number => {
       return this.notNumber(number);
@@ -28,14 +27,12 @@ class Lotto {
     }
   }
 
-  // 6개가 아닌 경우
   notSixNumber() {
     if (this.#numbers.length !== NUMBERS.lottoLength) {
       throw ERROR.notSixNumber;
     }
   }
 
-  // 1~45사이의 정수가 아닌 경우
   notRangeNumber() {
     this.#numbers.forEach(number => {
       return this.numberListRangeCheck(Number(number));
@@ -48,7 +45,6 @@ class Lotto {
     }
   }
 
-  // 중복되는 숫자가 있는 경우
   sameNumber() {
     const deleteSameNumber = new Set(this.#numbers).size;
 
