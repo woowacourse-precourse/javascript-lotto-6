3d76fd6e8760d01e7ecfaf50322ed18b55e17142
wonkyum-kim <dodo12120@naver.com>
2023-11-07 17:25:20 +0900

    test: 보너스 번호가 1부터 45 사이의 숫자가 아닌 경우

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -182,4 +182,25 @@ describe('로또 테스트', () => {
     // then
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
   });
+
+  test('예외 테스트 - 보너스 번호가 1부터 45 사이의 숫자가 아닌 경우', async () => {
+    const logSpy = getLogSpy();
+    const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
+    const INPUT_NUMBERS_TO_END = [
+      '1000',
+      '1,2,3,4,5,5',
+      '1,2,3,4,5,6',
+      '66',
+      '7',
+    ];
+    mockRandoms([RANDOM_NUMBERS_TO_END]);
+    mockQuestions([...INPUT_NUMBERS_TO_END]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+  });
 });
