860fdc8f7915d21490f3049b63ae9eee2eeaad6f
hyj5076 <hyj5076@hanmail.net>
2023-11-08 00:21:54 +0900

    refactor: return 추가

--- src/UI/inputView.js
+++ src/UI/inputView.js
@@ -3,14 +3,14 @@ import { ASK } from "../const/Messages.js";
 
 export const ask = {
   async payment() {
-    await Console.readLineAsync(ASK.INPUT_MONEY);
+    return await Console.readLineAsync(ASK.INPUT_MONEY);
   },
 
   async baseNumbers() {
-    await Console.readLineAsync(ASK.INPUT_NUMBERS);
+    return await Console.readLineAsync(ASK.INPUT_NUMBERS);
   },
 
   async bonusNumber() {
-    await Console.readLineAsync(ASK.INPUT_BONUS);
+    return await Console.readLineAsync(ASK.INPUT_BONUS);
   },
 };
