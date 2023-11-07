b5c0ac93bf46ad6ab52bbbf60d2b98a7d945ebbe
00kang <kdb070970@gmail.com>
2023-11-07 18:35:18 +0900

    feat: eslint와 prettier 설정

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true, // 쌍따옴표 대신 홑따옴표 사용
+  semi: true, // 모든 구문 끝에 세미콜론 출력
+  useTabs: false, // 탭 대신 공백으로 들여쓰기
+  tabWidth: 2, // 들여쓰기 공백 수
+  trailingComma: 'all', // 가능하면 후행 쉼표 사용
+  printWidth: 80, // 줄 바꿈할 길이
+  bracketSpacing: true, // 객체 괄호에 공백 삽입
+  arrowParens: 'always', // 항상 화살표 함수의 매개 변수를 괄호로 감쌈
+  endOfLine: 'auto', // OS에 따른 코드라인 끝 처리 방식 사용
+};
