530018d75d2e820a9f9c6dbc5414f316e8b7d64c
djLee77 <dkdnl232@gmail.com>
2023-11-08 01:59:04 +0900

    feat(InputConstatns): UserInput 입력 메시지 상수 클래스 생성

---
+++ src/Constants/InputConstants.js
@@ -0,0 +1,9 @@
+class InputConstants {
+  static INPUT_AMOUNT = "구입금액을 입력해주세요.\n";
+
+  static INPUT_WINNING_NUMBERS = "당첨 번호를 입력해주세요.\n";
+
+  static INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해주세요.\n";
+}
+
+export default InputConstants;
