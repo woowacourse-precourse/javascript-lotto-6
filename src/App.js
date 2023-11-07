aa9a24497405a1e0cba1f07ccba42ed99ce7a4c0
nari <naringst@gmail.com>
2023-11-08 01:31:55 +0900

    feat : 랜덤 숫자로 로또 발행 및 결과 출력

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class App {
     //구입 금액 입력받기
     MissionUtils.Console.print("구입금액을 입력해 주세요.");
     const totalCost = await MissionUtils.Console.readLineAsync("");
-    MissionUtils.Console.print("구입금액을 입력해 주세요.");
 
     //구입 금액 예외 처리 - 1) 숫자가 아닐 때
     if (isNaN(totalCost) === true) {
@@ -15,6 +14,20 @@ class App {
     //구입 금액 예외 처리 - 2) 1000원 단위가 아닐 때
     if (Number(totalCost) % 1000 !== 0) {
       throw new Error("[ERROR] 1000원 단위 금액을 입력해야 합니다.");
+    }
+
+    // 구매 개수 만큼 당첨 번호 생성 및 출력
+    const boughtLottoCount = totalCost / 1000;
+    const totalLottoNumList = [];
+    MissionUtils.Console.print(`${boughtLottoCount}개를 구매했습니다.`);
+    for (let i = 0; i < boughtLottoCount; i++) {
+      const nowLottoNumList = MissionUtils.Random.pickUniqueNumbersInRange(
+        1,
+        45,
+        6
+      );
+      MissionUtils.Console.print(nowLottoNumList);
+      totalLottoNumList.push(nowLottoNumList);
     }
   }
 }
