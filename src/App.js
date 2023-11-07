39bf1d68738094e1aa340700386fdcade3b6d58c
BaeDongWoo <ehddn5410@naver.com>
2023-11-07 22:44:20 +0900

    feat:입력 금액만큼 로또 번호 발행

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Console, Random } from '@woowacourse/mission-utils';
 class App {
   async play() {
     const input = await this.priceInput();
+    this.getLottoNumbers(input);
   }
   async priceInput() {
     const input = await Console.readLineAsync('구입금액을 입력해 주세요.');
@@ -9,15 +10,36 @@ class App {
   }
   async validatePriceInput(input) {
     try {
-      if (isNaN(input)) throw new Error('[ERROR] 숫자만 입력해 주세요');
-      if (input % 1000 !== 0 || input <= 0)
+      if (isNaN(input)) {
+        throw new Error('[ERROR] 숫자만 입력해 주세요');
+      }
+      if (input % 1000 !== 0 || input <= 0) {
         throw new Error('[ERROR] 1000원 단위로 입력해주세요');
+      }
+      return input;
     } catch (error) {
       Console.print(error);
-      // this.priceInput();
+      this.priceInput();
     }
+  }
+  getLottoNumbers(input) {
+    const lottoNumbers = Number(input) / 1000;
+    let lotto = [];
+    for (let i = 0; i < lottoNumbers; i++) {
+      lotto.push(this.getLotto());
+    }
+    Console.print(lotto);
+    return lotto;
+  }
+  getLotto() {
+    const number = Random.pickUniqueNumbersInRange(1, 45, 6);
+    number.sort(this.compare);
+    return number;
+  }
+  compare(a, b) {
+    return a - b;
   }
 }
 const app = new App();
-app.validatePriceInput('12345');
+app.play();
 export default App;
