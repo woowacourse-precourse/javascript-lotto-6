de4b33a280c8c65f76671f5bf745d4e3a5437cf7
yeonkyoung <keep.ykk@gmail.com>
2023-11-08 04:35:37 +0900

    feat: 구매한 로또

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ class App {
     const amount = await userLottoInputAmount();
     const purchasedLottoCounts = getPurchasedLottoCounts(amount);
     printMessage(`${purchasedLottoCounts}${PURCHASE_AMOUNT_GUIDE_MESSAGE}`);
+
+    const lottoArray = getPurchasedLottoArray(purchasedLottoCounts);
     printPurchasedLottoNumbers(lottoArray);
 }
 
