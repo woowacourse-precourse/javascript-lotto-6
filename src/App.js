e36601f32490c9bc033cf16c0ad795e3585d8074
skdbsqls <skdbsqls98@naver.com>
2023-11-07 17:31:58 +0900

    feat(App): 로또 발행 로직 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 import purchaseService from "./services/purchaseService.js";
+import publishService from "./services/publishService.js";
+import outputs from "./domains/outputs.js";
 
 class App {
   #price;
+  #amount;
+  #lottos;
 
   async play() {
     await this.#executePurchaseLotto();
+    this.#executePublishLotto();
   }
 
   // 로또 구입
@@ -16,6 +21,14 @@ class App {
       Console.print(error.message);
       return this.#executePurchaseLotto();
     }
+  }
+
+  // 로또 발행
+  #executePublishLotto() {
+    this.#amount = publishService.calculateAmount(this.#price);
+    outputs.printAmountOfLotto(this.#amount);
+    this.#lottos = publishService.publishLottos(this.#amount);
+    outputs.printLottos(this.#lottos);
   }
 }
 
