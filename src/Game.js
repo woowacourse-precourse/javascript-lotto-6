1c5b440e2e9cb8e4eefdd5be5f0938151929a1ff
puranium <puranium235@gmail.com>
2023-11-07 16:35:16 +0900

    feat: 로또 구매 금액을 입력받는다.

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,18 @@
-import { readInput, printMessage } from './User.js';
-import Validation from './Validation.js'
+import { Console } from '@woowacourse/mission-utils';
+import User from './User.js';
+import Validation from './Validation.js';
+import MESSAGE from './MESSAGE.js';
 
-const 
+class Game {
+  async play() {
+    try {
+      const priceInput = await User.readInput(MESSAGE.enterPrice);
+      Console.print(Validation.price(priceInput));
+    } catch (e) {
+      Console.print(e);
+      throw e;
+    }
+  }
+}
+
+export default Game;
--- src/Validation.js
+++ src/Validation.js
@@ -1,3 +1,12 @@
-export default {
-  price: (input) => {},
+import MESSAGE from './MESSAGE.js';
+
+const Validation = {
+  price: (input) => {
+    const price = Number(input);
+    if (price === 0) {
+      throw new Error(MESSAGE.formatError('에러입니다!'));
+    }
+    return price;
+  },
 };
+export default Validation;
