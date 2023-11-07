b6685649ebbb98efaa22a49316f5ed2e2f1bb4c4
greeneeya <suzythetravelerr@gmail.com>
2023-11-07 18:32:05 +0900

    feat: 보너스 번호가 당첨번호와 중복일 경우, 예외처리한다.

--- src/Host.js
+++ src/Host.js
@@ -10,10 +10,13 @@ export default class Host {
     return winningNumbers;
   }
 
-  static async getBonusNumber() {
+  static async getBonusNumber(winningNumbers) {
     const bonus = await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
     if (!Number.isInteger(bonus) || bonus > 45 || bonus < 1) {
       throw new Error('[ERROR] 1~45 사이의 정수만 입력 가능합니다.');
+    }
+    if (winningNumbers.includes(bonus)) {
+      throw new Error('[ERROR] 보너스 번호는 로또 번호와 중복이 불가합니다.');
     }
   }
 }
