280ba3a55c88fc386887a4e0bad8b0aaa4fb6bd3
BaeDongWoo <ehddn5410@naver.com>
2023-11-07 20:20:25 +0900

    feat:로또 번호 중복 검사 에러처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,12 @@ class Lotto {
   }
 
   #validate(numbers) {
+    const numberSet = new Set(numbers);
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numberSet.length !== 6) {
+      throw new Error('[ERROR] 중복된 번호는 입력할 수 없습니다.');
     }
   }
 
