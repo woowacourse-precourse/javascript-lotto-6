f2e7af914bd92b3f9fa134dccdbf07a1fa8f6cd2
KimRosa <108081283+KimRosa@users.noreply.github.com>
2023-11-07 15:27:39 +0900

    Feat(App.js): 당첨 결과 계산
    당첨 결과 출력을 위한 계산 매서드

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ class App {
       const winNum = await this.getLuckyNumber();
       const bonusNumber = await this.getBonusNumber();
       this.checkBonusNumber(bonusNumber, winNum);
+      const winningStats = this.calculateWinningStats(
+        lottoNumbers,
+        winNum,
+        bonusNumber
+      );
     }
   }
 
@@ -83,6 +88,57 @@ class App {
     }
     if (winNum.includes(Number(bonusNumber))) {
       throw new Error("[ERROR] 보너스 번호와 당첨 번호 중복");
+    }
+  }
+  calculateWinningStats(lottoNumbers, winNum, bonusNumber) {
+    const winConditions = {
+      6: { count: 0, amount: 2000000000 },
+      "5+bonus": { count: 0, amount: 30000000 },
+      5: { count: 0, amount: 1500000 },
+      4: { count: 0, amount: 50000 },
+      3: { count: 0, amount: 5000 },
+    };
+
+    this.calculateWinningStatsForLottoNumbers(
+      lottoNumbers,
+      winNum,
+      bonusNumber,
+      winConditions
+    );
+
+    return winConditions;
+  }
+
+  calculateWinningStatsForLottoNumbers(
+    lottoNumbers,
+    winNum,
+    bonusNumber,
+    winConditions
+  ) {
+    for (const lotto of lottoNumbers) {
+      const matchingNumbers = winNum.filter((num) =>
+        lotto.includes(num)
+      ).length;
+      const hasBonusNumber = lotto.includes(bonusNumber);
+
+      switch (matchingNumbers) {
+        case 6:
+          winConditions[6].count++;
+          break;
+        case 5:
+          if (hasBonusNumber) {
+            winConditions["5+bonus"].count++;
+          } else {
+            winConditions[5].count++;
+          }
+          break;
+        case 4:
+          winConditions[4].count++;
+          break;
+        case 3:
+          winConditions[3].count++;
+          break;
+      }
     }
   }
 }
