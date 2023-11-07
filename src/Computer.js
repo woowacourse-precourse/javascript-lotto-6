1cbac9040bd5569c4ab42c110e8dc924bb6a472b
JungHun98 <j7papa@naver.com>
2023-11-07 12:46:20 +0900

    feat: 컴퓨터 생성자 구현

--- src/Computer.js
+++ src/Computer.js
@@ -1,6 +1,11 @@
 class Computer {
   #winnerNumbers;
   #bonusNumber;
+
+  constructor(winnnersNumbers, bonusNumber) {
+    this.#winnerNumbers = winnnersNumbers;
+    this.#bonusNumber = bonusNumber;
+  }
 }
 
 export default Computer;
