3c508db7724071b6017a0ebb9306543c81983075
jungmyungjin <myung2667@gmail.com>
2023-11-08 04:00:37 +0900

    fix(LottoValidation) : 로또 클래스 검증 로직 수정, 위치 변경

--- src/Validation/LottoValidation.js
+++ src/Validation/LottoValidation.js
@@ -1,6 +1,10 @@
-import SETTING from "./Constructor/Setting";
-import ERROR_MESSAGE from "./Constructor/ErrorMessage";
-import { IsNumbersInArray, IsNumberInRangeOfArray } from "./Utils";
+import SETTING from "../Constructor/Setting.js";
+import ERROR_MESSAGE from "../Constructor/ErrorMessage.js";
+import {
+  IsNumbersInArray,
+  IsNumberInRangeOfArray,
+  IsUniqueInArray,
+} from "../Utils.js";
 
 export const validateLottoNumbersCountMismatch = (numbers) => {
   if (numbers.length !== SETTING.LOTTO_NUMBER_COUNT) {
@@ -9,19 +13,25 @@ export const validateLottoNumbersCountMismatch = (numbers) => {
 };
 
 export const validateLottoNumbersNotAllNumbers = (numbers) => {
-  if (IsNumbersInArray(numbers)) {
+  if (!IsNumbersInArray(numbers)) {
     throw new Error(ERROR_MESSAGE.LOTTO.NUMBERS_NOT_ALL_NUMBERS(numbers));
   }
 };
 
 export const validateLottoNumbersOutOfRange = (numbers) => {
   if (
-    IsNumberInRangeOfArray(
+    !IsNumberInRangeOfArray(
       numbers,
       SETTING.LOTTO_NUMBER_RANGE.MIN,
       SETTING.LOTTO_NUMBER_RANGE.MAX
     )
   ) {
     throw new Error(ERROR_MESSAGE.LOTTO.NUMBERS_OUT_OF_RANGE(numbers));
+  }
+};
+
+export const validateLottoNumberIsUniq = (numbers) => {
+  if (!IsUniqueInArray(numbers)) {
+    throw new Error(ERROR_MESSAGE.LOTTO.NUMBERS_DUPLICATED(numbers));
   }
 };
