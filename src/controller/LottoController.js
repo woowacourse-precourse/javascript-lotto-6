01c59a52418c1bf40f9c874c17b1d77106faa973
Jihyun_Kim <jihyun3313@daum.net>
2023-11-07 22:12:07 +0900

    style: 랜덤숫자배열생성메소드 utils로 분리

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -2,30 +2,20 @@ import Lotto from "../model/Lotto.js"
 import { getBonusNumber, getUserAmount, getWinningNumber } from "../view/input.js"
 import { printAttempt, printRandomNumbersArray } from "../view/output.js";
 import { LOTTO } from "../constatns/number.js";
-import { getRandomNumbers } from "../utils.js";
+import { getRandomNumbersArray } from "../utils.js";
 
 class LottoController {
 
   async lottoStart() {
     const attempt = await getUserAmount() / LOTTO.PRICE;
-    const randomNumbersArray = this.getRandomNumbersArray(attempt);
+    const randomNumbersArray = getRandomNumbersArray(attempt);
     printAttempt(attempt);
     printRandomNumbersArray(randomNumbersArray);
 
     const lotto = new Lotto(await getWinningNumber());
     const bonusNumber = await getBonusNumber();
-    lotto.lottoMatchStart(randomNumbersArray);
-  }
+    const lottoResult = lotto.lottoMatchStart(randomNumbersArray, bonusNumber);
 
-  // 랜덤 번호 저장할 2차원 배열 생성
-  getRandomNumbersArray(attempt) {
-    const array = Array(attempt);
-    const randomNumbersArray = array.fill();
-    randomNumbersArray.forEach((e, index) => {
-      randomNumbersArray[index] = getRandomNumbers();
-    })
-    return randomNumbersArray;
   }
-
 }
 export default LottoController;
--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,17 @@ export const print = (message) => {
   MissionUtils.Console.print(message);
 }
 
-export const getRandomNumbers = () => {
+// 랜덤 번호 저장할 2차원 배열 생성
+export const getRandomNumbersArray = (attempt) => {
+  const array = Array(attempt);
+  const randomNumbersArray = array.fill();
+  randomNumbersArray.forEach((e, index) => {
+    randomNumbersArray[index] = getRandomNumbers();
+  })
+  return randomNumbersArray;
+}
+
+const getRandomNumbers = () => {
   const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
   randomNumbers.sort((a, b) => a - b); // 오름차순 정렬
   return randomNumbers;
