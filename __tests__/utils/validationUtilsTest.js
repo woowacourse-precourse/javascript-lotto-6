b2834552becb0613cbe466d4afa1c474ca10c44e
8leamnk <silkviewrk@gmail.com>
2023-11-07 10:34:51 +0900

    test(validationUtilsTest): 테스트 코드를 수정함
    수정 내용:
    - 상수에 대한 컨벤션을 준수하지 않아 이를 수정 (SNAKE_CASE로 작성)
    - expect 안에 있는 함수에 중괄호를 추가

--- __tests__/utils/validationUtilsTest.js
+++ __tests__/utils/validationUtilsTest.js
@@ -2,32 +2,40 @@ import validationUtils from '../../src/utils/validationUtils.js';
 
 describe('숫자형 체크', () => {
   test('숫자가 아닌 값이 들어가면 예외를 발생시킨다.', () => {
-    const notANumber = NaN;
-    const undefinedValue = undefined;
+    const NOT_A_NUMBER = NaN;
+    const UNDEFINED_VALUE = undefined;
 
-    expect(() => validationUtils.checkNumber(notANumber)).toThrow('[ERROR]');
-    expect(() => validationUtils.checkNumber(undefinedValue)).toThrow(
-      '[ERROR]',
-    );
+    expect(() => {
+      validationUtils.checkNumber(NOT_A_NUMBER);
+    }).toThrow('[ERROR]');
+    expect(() => {
+      validationUtils.checkNumber(UNDEFINED_VALUE);
+    }).toThrow('[ERROR]');
   });
 
   test('숫자값이 들어가면 예외를 발생시키지 않는다.', () => {
-    const number = 1;
+    const NUMBER = 1;
 
-    expect(() => validationUtils.checkNumber(number)).not.toThrow('[ERROR]');
+    expect(() => {
+      validationUtils.checkNumber(NUMBER);
+    }).not.toThrow('[ERROR]');
   });
 });
 
 describe('범위 체크', () => {
   test('1부터 45까지가 아닌 값이 들어가면 예외를 발생시킨다.', () => {
-    const number = 46;
+    const NUMBER = 46;
 
-    expect(() => validationUtils.checkRange(number)).toThrow('[ERROR]');
+    expect(() => {
+      validationUtils.checkRange(NUMBER);
+    }).toThrow('[ERROR]');
   });
 
   test('1부터 45까지의 값이 들어가면 예외를 발생시키지 않는다.', () => {
-    const number = 1;
+    const NUMBER = 1;
 
-    expect(() => validationUtils.checkRange(number)).not.toThrow('[ERROR]');
+    expect(() => {
+      validationUtils.checkRange(NUMBER);
+    }).not.toThrow('[ERROR]');
   });
 });
