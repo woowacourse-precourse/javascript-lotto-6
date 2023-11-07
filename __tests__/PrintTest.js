6028b9b79cd7df8e934b532c082d9c4344500b28
JunSeok <ppu1234@naver.com>
2023-11-08 01:52:47 +0900

    test: printLottoResult 함수 테스트 코드 작성

--- __tests__/PrintTest.js
+++ __tests__/PrintTest.js
@@ -1,6 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import printEachLotto from '../src/print/printEachLotto';
 import printLottoNumbers from '../src/print/printLottoNumbers';
+import printLottoResult from '../src/print/printLottoResult';
 
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
@@ -36,6 +37,36 @@ describe('printLottoNumbers() 함수 테스트', () => {
 
     // Act
     printLottoNumbers(lottoTicketNumber, lottoBox);
+
+    // Assert
+    outputs.forEach((output) => expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output)));
+  });
+});
+
+describe('printLottoResult() 함수 테스트', () => {
+  test('로또 결과를 출력한다.', () => {
+    // Arrange
+    const logSpy = getLogSpy();
+    const lottoTicketNumber = 2;
+    const ranks = new Map([
+      [3, 1],
+      [4, 0],
+      [5, 0],
+      [5.5, 0],
+      [6, 0],
+    ]);
+    const outputs = [
+      '\n당첨 통계\n---',
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 250%입니다.',
+    ];
+
+    // Act
+    printLottoResult(lottoTicketNumber, ranks);
 
     // Assert
     outputs.forEach((output) => expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output)));
