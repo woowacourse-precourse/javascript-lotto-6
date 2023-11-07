5e052efbe5b2cb3e3ec003403a26189928f43535
PARKJONGGYEONG <gd30220@o.cnu.ac.kr>
2023-11-08 02:49:33 +0900

    feat: 리팩토링

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -32,11 +32,11 @@ class LottoGame {
   async playLotteryGame() {
     const amount = await this.input.Price();
     const lotteries = this.computer.sell(amount);
-    this.output.PurchaseInformation(lotteries.map(Lotto => Lotto.getInformation()));
+    this.output.PurchaseInformation(lotteries.map((Lotto) => Lotto.getInformation()));
 
     const Number = await this.input.Number();
     const bonusNumber = await this.input.BonusNumber(Number);
-    const lottoResult = lotteries.map(lotto => lotto.checkNumber(Number, bonusNumber));
+    const lottoResult = lotteries.map((lotto) => lotto.checkNumber(Number, bonusNumber));
 
     const finalResult = this.calculate.calculateResult(lottoResult);
     const Rate = this.calculate.calculateRate(finalResult, amount);
