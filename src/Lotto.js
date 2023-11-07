165b22bb4cf0c9928e71c3a3c3cf71ee44b15b03
Baek, Sehee <tpgmlqor@gmail.com>
2023-11-08 00:59:54 +0900

    feat: 로또 번호 중복된 숫자 예외

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,14 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const numberSet = new Set();
+    for (let i = 0; i < numbers.length; i++) {
+      if (numberSet.has(numbers[i])) {
+        throw new Error("[ERROR] 중복된 숫자가 있습니다.");
+      }
+      numberSet.add(numbers[i]);
+    }
   }
 
   // TODO: 추가 기능 구현
