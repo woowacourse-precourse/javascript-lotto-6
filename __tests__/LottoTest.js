8012a2163352549940eec220a0087abcdfd8dd3a
OhJeongHwan <wjdghks0316@naver.com>
2023-11-07 14:24:52 +0900

    test: lottoTest 테스트 추가: 1~45 범위 넘어가는 경우, 로또 번호에 잘못된 숫자가 있는 경우

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -18,6 +18,20 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 55]);
     }).toThrow("[ERROR]");
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 0]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 숫자나 정수가 아닌 것이 있으면 예외가 발생한다..", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, "a"]);
+    }).toThrow("[ERROR]");
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 1.1]);
+    }).toThrow("[ERROR]");
   });
 
   // 아래에 추가 테스트 작성 가능
