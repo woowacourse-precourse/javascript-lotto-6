62ac63480c089f68a90e888489fa13d2e51ebccb
euncherry <csmo2642@naver.com>
2023-11-08 03:04:42 +0900

    feat: 프로그램 실행시 게임 설정 초기화 및 입력값 세팅 구현

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import InputView from './view/inputView.js';
+import LottoGame from './domain/LottoGame.js';
 class App {
+  constructor() {
+    this.lottoGame = new LottoGame();
+  }
   async play() {
     //Input : 구입 금액을 입력받기
-
     const purchaseMoney = await InputView.readPurchaseMoney();
     console.log('  1️⃣   purchaseMoney', purchaseMoney);
 
-    //로또 발행 후 발행한 로또 수량 및 번호 출력하기
-    //Input : 당첨 번호 입력받기
+    // 로또 발행 후 발행한 로또 수량 및 번호 출력하기
+    this.lottoGame.generateLottoTickets(purchaseMoney);
+
+    this.lottoGame.consoleField();
+
+    // Input : 당첨 번호 입력받기
     const winningNumber = await InputView.readWinningNumber();
     console.log('2️⃣   winningNumber', winningNumber);
 
     //Input : 보너스 번호 입력받기
     const bonusNumber = await InputView.readBonusNumber(winningNumber);
-
     console.log('3️⃣   bonusNumber', bonusNumber);
+
+    this.lottoGame.setWinningNumbers(winningNumber, bonusNumber);
+
+    this.lottoGame.consoleField();
+
     //[당첨 내역, 수익률] 계산
+
     //[당첨 내역, 수익률] 출력하기
   }
 }
