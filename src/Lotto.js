509b635c9768815c739e28228c6123f36e276bd3
Kidprogramer1039 <bnpo5231@naver.com>
2023-11-08 06:27:47 +0900

    feat: 로또 함수 throw문 조건 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,13 +9,26 @@ class Lotto {
   }
 
   #validate(numbers) {
+    // 입력받은 로또가 6개 이상일 경우 오류 출력
     if (numbers.length !== 6) {
       throw new Error(ERROR_MESSAGE.lottoCount);
     }
+    // 입력받은 로또가 45이상일 경우 오류 출력
+    numbers.map((x) => {
+      if (x > 45) {
+        throw new Error(ERROR_MESSAGE.lottoRange);
+      }
+    });
+    // 입력받은 로또 중 중복이 있을 경우 오류 출력
+    const set = new Set(numbers);
+    if(numbers.length !== set.size) {
+      throw new Error(ERROR_MESSAGE.lottoDuplication);
+    }
+
   }
 
   // TODO: 추가 기능 구현
-
+  
 }
 
 export default Lotto;
