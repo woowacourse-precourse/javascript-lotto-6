a8525102392c3d3a48f9bcbefb36f7aa97c23570
yhyem <yoonhyemin0120@gmail.com>
2023-11-08 00:46:55 +0900

    fix: 당첨 번호, 보너스 번호 예외 처리 추가

--- src/Validate.js
+++ src/Validate.js
@@ -9,12 +9,16 @@ class Validate {
 
     if (prizeNumber.map((prize) => isNaN(prize)).includes(true))
       throw "[ERROR] 숫자가 아닌 값이 포함 되어있습니다.";
-    if (stringList.length !== 6)
+    else if (stringList.length !== 6)
       throw "[ERROR] 당첨 번호 숫자는 6개여야 합니다.";
+    else if (prizeNumber.map((prize) => prize < 1 || prize > 45).includes(true))
+      throw "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
   }
 
   bonusValidate(bonus) {
     if (isNaN(Number(bonus))) throw "[ERROR] 보너스 번호가 숫자가 아닙니다.";
+    else if (Number(bonus) < 1 || Number(bonus) > 46)
+      throw "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
   }
 }
 
