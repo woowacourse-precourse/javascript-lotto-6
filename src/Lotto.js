361a41563fde2785daf50f3c0237466b58034f8c
leey00nsu <dbstndla1212@naver.com>
2023-11-07 19:04:47 +0900

    refactor: Lotto에 LottoTest에 추가된 예외 처리 반영

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,6 @@
+import OPTION from './constants/option.js';
+import InputError from './error/InputError.js';
+
 class Lotto {
   #numbers;
 
@@ -7,8 +10,11 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    if (numbers.length !== OPTION.lottoLength) {
+      throw new InputError(`로또 번호는 ${OPTION.lottoLength}개여야 합니다.`);
+    }
+    if (numbers.length !== new Set(numbers).size) {
+      throw new InputError('로또 번호는 중복되지 않아야 합니다.');
     }
   }
 
