449b942377414e9249d7d8b5e34e254c6c7c61d4
shgusgh12 <https://github.com/shgusgh12>
2023-11-07 18:42:09 +0900

    feat: 구입 금액에 맞춰 Lotto 생성하는 기능 구현

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,19 +1,32 @@
 import { Console, Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+import GameManager from './GameManager.js';
 
 class LottoGame {
   money;
 
   numberOfLotto;
 
+  purchasedTicket = [];
+
   constructor() {}
 
   async gameStart() {
     await this.getUserMoney();
+    this.buyLottoTicket();
   }
 
   async getUserMoney() {
     this.money = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     this.numberOfLotto = Number(this.money) / 1000;
+  }
+
+  buyLottoTicket() {
+    Console.print(`${this.numberOfLotto}개 구매했습니다.`);
+    for (let i = 0; i < Number(this.numberOfLotto); i++) {
+      const lotto = new Lotto(GameManager.getRandomNumber());
+      this.purchasedTicket.push(lotto);
+    }
   }
 }
 
