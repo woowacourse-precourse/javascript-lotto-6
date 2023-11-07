0c577a5c5b0134f48c3e5e2b1d656428f089019f
nijuy <youjin6325@naver.com>
2023-11-07 17:33:12 +0900

    test(App): generateLottos() 테스트 수정
    - 로또 1개를 생성할 때마다 print()를 호출하니까, 로또 수량만큼 호출되는게 맞는지 검사

--- __tests__/GenerateLottosTest.js
+++ __tests__/GenerateLottosTest.js
@@ -1,9 +1,15 @@
 import App from '../src/App';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 describe('App - generateLottos() 테스트', () => {
   test('주어진 수량에 맞는 로또를 생성한다.', () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+    logSpy.mockClear();
+
     const amount = 4;
     const app = new App();
     app.generateLottos(amount);
+
+    expect(logSpy).toHaveBeenCalledTimes(amount);
   });
 });
