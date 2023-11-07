74958b70782cbafb4593f81ff6ee2ec8b17047fb
intersoom <sallybig@naver.com>
2023-11-08 05:21:28 +0900

    fix: 에러 문구 수정

--- src/constant/errorData.js
+++ src/constant/errorData.js
@@ -1,11 +1,11 @@
 const ERROR_MESSAGE = Object.freeze({
-  PAYMENT_INVALID: "[ERROR] 1000원 단위로 입력해주세요.",
-  NUMBER_FORM: "[ERROR] 숫자만으로 입력해주세요.",
-  NUMBER_COMMA_FORM: "[ERROR] 숫자와 쉼표만으로 입력해주세요.",
-  BLANK_FORM: "[ERROR] 공백 없이 입력해주세요.",
-  RANGE_INVALID: "[ERROR] 1부터 45 사이의 숫자로 입력해주세요",
-  SIZE_INVALID: "[ERROR] 총 6개의 숫자만을 입력해주세요.",
-  DUPLICATION: "[ERROR] 6개의 숫자를 중복없이 입력해주세요.",
+  PAYMENT_INVALID: "[ERROR] 1000원 단위로 입력해주세요.\n",
+  NUMBER_FORM: "[ERROR] 숫자만으로 입력해주세요.\n",
+  NUMBER_COMMA_FORM: "[ERROR] 숫자와 쉼표만으로 입력해주세요.\n",
+  BLANK_FORM: "[ERROR] 6개의 숫자를 입력해주세요.\n",
+  RANGE_INVALID: "[ERROR] 1부터 45 사이의 숫자로 입력해주세요\n",
+  SIZE_INVALID: "[ERROR] 총 6개의 숫자를 입력해주세요.\n",
+  DUPLICATION: "[ERROR] 6개의 숫자를 중복없이 입력해주세요.\n",
 });
 
 export default ERROR_MESSAGE;
