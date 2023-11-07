6a2b81cc62aba29c67bbc33ddc66dbf5d55edafd
PARK-CHANUK <doosan1224@naver.com>
2023-11-07 22:42:59 +0900

    refactor(RateOfReturnTest): 테스트 코드 수정 수익율이 100인 경우에는 100.0이 출력되도록 테스트 코드 수정

--- __tests__/RateOfReturnTest.js
+++ __tests__/RateOfReturnTest.js
@@ -1,18 +1,36 @@
 import RateOfReturn from '../src/RateOfReturn';
 
 describe('로또 수익율 계산 테스트', () => {
-  test('구입한 로또 갯수와 당첨내역을 전달하면 수익율을 계산합니다.', () => {
-    const purchaseAmount = 5;
-    const matchingTable = {
-      three: 2,
-      four: 0,
-      fiveNotBonus: 0,
-      fiveAndBonus: 0,
-      all: 0,
-    };
-    const rateOfReturn = new RateOfReturn();
-    expect(rateOfReturn.getRateOfReturn(matchingTable, purchaseAmount)).toBe(
-      200.0,
-    );
-  });
+  test.each([
+    {
+      purchaseAmount: 5,
+      matchingTable: {
+        three: 1,
+        four: 0,
+        fiveNotBonus: 0,
+        fiveAndBonus: 0,
+        all: 0,
+      },
+      expected: '100.0',
+    },
+    {
+      purchaseAmount: 7,
+      matchingTable: {
+        three: 1,
+        four: 0,
+        fiveNotBonus: 0,
+        fiveAndBonus: 0,
+        all: 0,
+      },
+      expected: '71.4',
+    },
+  ])(
+    '구입한 로또 갯수와 당첨내역을 전달하면 수익율을 계산합니다.',
+    ({ purchaseAmount, matchingTable, expected }) => {
+      const rateOfReturn = new RateOfReturn();
+      expect(rateOfReturn.getRateOfReturn(matchingTable, purchaseAmount)).toBe(
+        expected,
+      );
+    },
+  );
 });
