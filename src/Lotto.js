bd85b8da77fd5632f726aacdc53066d7327219b6
aaminha <dks4857@gmail.com>
2023-11-08 04:42:24 +0900

    feat: Lotto 클래스에 유효성 검사 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,12 +1,21 @@
 import { MESSAGE_PRINT } from './constants/Message';
+import Validate from './utils/Validate';
 import OutputView from './views/OutputView';
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.#validate(numbers);
     this.#numbers = numbers;
     this.#sortNumbers();
+  }
+
+  #validate(numbers) {
+    numbers.map(number => Validate.isNumberInRange(number));
+    Validate.isWinningNumbersLength(numbers);
+    Validate.isDuplicateWinningNumbers(numbers);
+    numbers.map(number => Validate.isNumberInRange(number));
   }
 
   #sortNumbers() {
