22ebab4e560c8c176fffb5cbd05d193912d60312
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-07 20:56:01 +0900

    Fix: 잘못된 입력값 입력시, 재입력 받음

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    let income = await MissionUtils.Console.readLineAsync(
-      "구입금액을 입력해 주세요."
-    );
-    let lotto_num = await this.getIncomeError(income);
+    let lotto_num = await this.getIncomeAndCheck();
     lotto_num = Math.floor(lotto_num / 1000);
     MissionUtils.Console.print(`${lotto_num}개를 구매했습니다.`);
   }
 
   //함수: 로또구입금액을 입력받고, 1000단위가 아닐 경우 예외처리
-  async getIncomeError(income) {
-    let income_num = Number(income);
-    if (isNaN(income_num))
-      throw new Error("[ERROR] 구입 금액은 숫자를 입력해주세요");
-    if (income_num % 1000 != 0)
-      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+  async getIncomeAndCheck() {
+    let income_num;
+    while (true) {
+      let income = await MissionUtils.Console.readLineAsync(
+        "구입금액을 입력해 주세요."
+      );
+      income_num = Number(income);
+      try {
+        console.log(income_num, income, typeof income_num, "dd");
+        if (isNaN(income_num))
+          throw new Error("[ERROR] 구입 금액은 숫자를 입력해주세요");
+        if (income_num % 1000 != 0)
+          throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+        break;
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
     return income_num;
   }
 }
--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,12 @@ class Lotto {
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+
+    for (let num of numbers) {
+      if (new Set(numbers).size !== numbers.length) {
+        throw new Error("[ERROR] 당첨 번호엔 중복이 없어야 합니다.");
+      }
+    }
   }
 
   #validate(numbers) {
@@ -11,8 +17,6 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
