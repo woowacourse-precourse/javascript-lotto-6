cb12f544bb1eb099ac190e5c554862876cbed6bd
도예 <kmdy125@gmail.com>
2023-11-08 00:40:35 +0900

    Feat: Lotto class validate 추가, getLottoNumbers 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,21 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const set = new Set(numbers);
+    if (set.size !== numbers.length) {
+      throw new Error("[ERROR] 중복된 숫자가 있습니다.");
+    }
+    numbers.map((number) => {
+      if (number > 45 || number < 1 || isNaN(number)) {
+        throw new Error(
+          "[ERROR] 로또 번호는 1 이상 45 이하의 숫자여야 합니다."
+        );
+      }
+    });
   }
-
-  // TODO: 추가 기능 구현
+  getLottoNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
