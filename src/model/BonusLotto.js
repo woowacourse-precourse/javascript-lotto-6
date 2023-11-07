c81b802c18149b58d26d274a3a6e9881216f4906
jonique98 <tnals9899@naver.com>
2023-11-07 22:50:35 +0900

    feat: bonusLotto 유효성 검사 보너스 로또에 대한 유효성 검사 메소드 작성

--- src/model/BonusLotto.js
+++ src/model/BonusLotto.js
@@ -1,5 +1,30 @@
-class BonusLotto {
-	#bonusNumber;
+import { ERRORMESSAGE } from '../utils/const.js';
 
-	
+class BonusLotto {
+  #bonusNumber;
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+
+  setBonusNumber(bonusNumber) {
+    this.#bonusValidate(bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  #bonusValidate(bonusNumber, lottoNumber) {
+    if (Number.isNaN(bonusNumber)) {
+      throw new Error(ERRORMESSAGE.bonusType);
+    }
+
+    if (bonusNumber < 1 || bonusNumber > 45) {
+      throw new Error(ERRORMESSAGE.bonusInput);
+    }
+
+    if (lottoNumber.includes(bonusNumber)) {
+      throw new Error(ERRORMESSAGE.bonusDuplicate);
+    }
+  }
 }
+
+export default BonusLotto;
