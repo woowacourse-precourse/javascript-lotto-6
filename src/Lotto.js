f64236746742dffa12a61a5b1772f0be060acd15
lcy <8410294@naver.com>
2023-11-07 16:53:01 +0900

    feat: winning numbers validate

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,10 @@
+import {
+  amountType,
+  duplicateNum,
+  numberCounter,
+  numberRange,
+} from "./Validation";
+
 class Lotto {
   #numbers;
 
@@ -7,9 +14,10 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    numberCounter(numbers);
+    numbers.forEach((v) => amountType(v));
+    numbers.forEach((v) => numberRange(v));
+    duplicateNum(numbers);
   }
 
   // TODO: 추가 기능 구현
