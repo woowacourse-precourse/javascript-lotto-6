b86dc4e9b9e3b16b0c546877646f29a750c9a382
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-11-07 23:13:41 +0900

    feat : user의 입력이 잘못됐을때 throw 하고 재입력 받는 기능 구현
    try... catch로 입력부분들을 감싸서 error 발생시 message를 print하고 에러가 발생한 입력을 재요청
    
    꼬리재귀로 최적화 그러나 overflow 위험으로 while문으로 수정하는 방향이 좋아보임
    
    requestPurchasePrice() : 구매금액 입력에 예외 발생시 재귀적 호출
    requestWinningNumbers() : 당첨숫자 입력에 예외 발생시 재귀적 호출
    requestBonusNumber() : 보너스 번호 입력에 예외 발생시 재귀적 호출
    
    // README.md
    [check]
    잘못된 입력시에 입력 재요청 기능 : class App의 request로 시작하는 메서드 내부의 try ... catch 로직

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import InputReader from './view/InputReader.js';
-import { validateUserInput } from './utils/ValidateUserInput.js';
+import PromptPrinter from './view/promptPrinter.js';
+import {
+  ValidateWinningNumbersUserInput,
+  ValidatePurchasePriceUserInput,
+  ValidateBonusNumberUserInput,
+} from './utils/ValidateUserInput.js';
 
 class App {
   #inputReader;
+  #promptPrinter;
 
   constructor() {
     this.#inputReader = new InputReader();
+    this.#promptPrinter = new PromptPrinter();
   }
+
   async play() {
     const purchasePrice = await this.requestPurchasePrice();
+    const winningNumbers = await this.requestWinningNumbers();
+    const bonusNumber = await this.requestBonusNumber(winningNumbers);
   }
 
   async requestPurchasePrice() {
-    const purchasePrice = await this.#inputReader.purchasePrice();
-    validateUserInput.purchasePrice(purchasePrice);
+    try {
+      const purchasePrice = await this.#inputReader.purchasePrice();
+      new ValidatePurchasePriceUserInput(purchasePrice);
 
-    return purchasePrice;
+      return purchasePrice;
+    } catch (error) {
+      this.#promptPrinter.userInputErrorMessage(error);
+      return await this.requestPurchasePrice();
+    }
+  }
+
+  async requestWinningNumbers() {
+    try {
+      const winningNumbers = await this.#inputReader.winningNumbers();
+      new ValidateWinningNumbersUserInput(winningNumbers);
+
+      return winningNumbers;
+    } catch (error) {
+      this.#promptPrinter.userInputErrorMessage(error);
+      return await this.requestWinningNumbers();
+    }
+  }
+
+  async requestBonusNumber(winningNumbers) {
+    try {
+      const bonusNumber = await this.#inputReader.bonusNumber();
+      new ValidateBonusNumberUserInput(bonusNumber, winningNumbers);
+
+      return bonusNumber;
+    } catch (error) {
+      this.#promptPrinter.userInputErrorMessage(error);
+      return await this.requestBonusNumber(winningNumbers);
+    }
   }
 }
 
