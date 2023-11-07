ff378c8fc0db987cd75f938ac6730914b906ec36
PARKJONGGYEONG <gd30220@o.cnu.ac.kr>
2023-11-08 02:55:08 +0900

    feat: 리팩토링

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -5,16 +5,21 @@ import AppError from './constants/AppError.js';
 import CALCULATE from './Calculate.js';
 
 class LottoGame {
+  #input;
+  #output;
+  #computer;
+  #calculate;
+
   constructor() {
-    this.input = new INPUT();
-    this.output = new OUTPUT();
-    this.computer = new COMPUTER();
-    this.calculate = new CALCULATE();
+    this.#input = new INPUT();
+    this.#output = new OUTPUT();
+    this.#computer = new COMPUTER();
+    this.#calculate = new CALCULATE();
   }
 
   async handleAppError(error) {
     if (error instanceof AppError) {
-      this.output.print(error.message);
+      this.#output.print(error.message);
       await this.play();
     }else{
       throw error;
@@ -30,17 +35,17 @@ class LottoGame {
   }
 
   async playLotteryGame() {
-    const amount = await this.input.Price();
-    const lotteries = this.computer.sell(amount);
-    this.output.PurchaseInformation(lotteries.map((Lotto) => Lotto.getInformation()));
+    const amount = await this.#input.Price();
+    const lotteries = this.#computer.sell(amount);
+    this.#output.PurchaseInformation(lotteries.map((Lotto) => Lotto.getInformation()));
 
-    const Number = await this.input.Number();
-    const bonusNumber = await this.input.BonusNumber(Number);
+    const Number = await this.#input.Number();
+    const bonusNumber = await this.#input.BonusNumber(Number);
     const lottoResult = lotteries.map((lotto) => lotto.checkNumber(Number, bonusNumber));
 
-    const finalResult = this.calculate.calculateResult(lottoResult);
-    const Rate = this.calculate.calculateRate(finalResult, amount);
-    this.output.Statistics(finalResult, Rate);
+    const finalResult = this.#calculate.calculateResult(lottoResult);
+    const Rate = this.#calculate.calculateRate(finalResult, amount);
+    this.#output.Statistics(finalResult, Rate);
   }
 }
 
