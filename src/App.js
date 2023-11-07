4f5ef209c70cf1ec5e6bb5921dcf691e993fe168
anr4876 <dustjdanr@naver.com>
2023-11-07 14:15:33 +0900

    feat: 로또 구입 금액 입력 기능 로또 1장의 가격을 1,000원으로 설정 1,000원으로 나누어 떨어지지 않는 경우 예외 처리 해당 입력 금액에 숫자를 제외한 문자가 있으면 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const PRINT = new lottoPrint();
+    const LOTTO_COUNT = PRINT.lottoMoneyInput();
+
+  }
+}
+
+class lottoPrint {
+  async lottoMoneyInput(){
+    const LOTTO_MONEY = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    if(LOTTO_MONEY % 1000 !== 0){
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    if(LOTTO_MONEY.match(/[^0-9]/)){
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    return LOTTO_MONEY/1000;
+  }
+
+
+
+
 }
 
 export default App;
