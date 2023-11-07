ae07d24599a1b8293e9cd1a6158158a189a024c7
byunjiin <jibyun5808@naver.com>
2023-11-08 08:50:22 +0900

    feat: 테스트 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,10 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { checkbonus } from '../src/Exception/CheckBonus.js';
+import { checkbuy } from '../src/Exception/CheckBuy.js';
+import { checkduplication } from '../src/Exception/Checkduplication.js';
+import { winningnum } from '../src/Game/WinningNum.js';
+import { lottoprint } from '../src/Output/LottoPrint.js';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -19,7 +24,7 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
@@ -28,8 +33,8 @@ const runException = async (input) => {
   // given
   const logSpy = getLogSpy();
 
-  const RANDOM_NUMBERS_TO_END = [1,2,3,4,5,6];
-  const INPUT_NUMBERS_TO_END = ["1000", "1,2,3,4,5,6", "7"];
+  const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
+  const INPUT_NUMBERS_TO_END = ['1000', '1,2,3,4,5,6', '7'];
 
   mockRandoms([RANDOM_NUMBERS_TO_END]);
   mockQuestions([input, ...INPUT_NUMBERS_TO_END]);
@@ -39,15 +44,15 @@ const runException = async (input) => {
   await app.play();
 
   // then
-  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
-}
+  expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
+};
 
-describe("로또 테스트", () => {
+describe('로또 테스트', () => {
   beforeEach(() => {
     jest.restoreAllMocks();
-  })
+  });
 
-  test("기능 테스트", async () => {
+  test('기능 테스트', async () => {
     // given
     const logSpy = getLogSpy();
 
@@ -61,7 +66,7 @@ describe("로또 테스트", () => {
       [2, 13, 22, 32, 38, 45],
       [1, 3, 5, 14, 22, 45],
     ]);
-    mockQuestions(["8000", "1,2,3,4,5,6", "7"]);
+    mockQuestions(['8000', '1,2,3,4,5,6', '7']);
 
     // when
     const app = new App();
@@ -69,21 +74,21 @@ describe("로또 테스트", () => {
 
     // then
     const logs = [
-      "8개를 구매했습니다.",
-      "[8, 21, 23, 41, 42, 43]",
-      "[3, 5, 11, 16, 32, 38]",
-      "[7, 11, 16, 35, 36, 44]",
-      "[1, 8, 11, 31, 41, 42]",
-      "[13, 14, 16, 38, 42, 45]",
-      "[7, 11, 30, 40, 42, 43]",
-      "[2, 13, 22, 32, 38, 45]",
-      "[1, 3, 5, 14, 22, 45]",
-      "3개 일치 (5,000원) - 1개",
-      "4개 일치 (50,000원) - 0개",
-      "5개 일치 (1,500,000원) - 0개",
-      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-      "6개 일치 (2,000,000,000원) - 0개",
-      "총 수익률은 62.5%입니다.",
+      '8개를 구매했습니다.',
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+      '[1, 8, 11, 31, 41, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '[7, 11, 30, 40, 42, 43]',
+      '[2, 13, 22, 32, 38, 45]',
+      '[1, 3, 5, 14, 22, 45]',
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 0개',
+      '5개 일치 (1,500,000원) - 0개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
+      '6개 일치 (2,000,000,000원) - 0개',
+      '총 수익률은 62.5%입니다.',
     ];
 
     logs.forEach((log) => {
@@ -91,8 +96,55 @@ describe("로또 테스트", () => {
     });
   });
 
-  test("예외 테스트", async () => {
-    await runException("1000j");
+  test('예외 테스트', async () => {
+    await runException('1000j');
+  });
+
+  // prettier-ignore
+  test('당첨 기능 테스트', () => {
+    //given
+    const arrary = [
+      [1, 2, 3, 4, 5, 6],
+      [11, 12, 13, 14, 15, 16],
+    ];
+    const bonusnum = { 'numbers': [1, 2, 3, 4, 5, 6], 'bonus': 7 };
+    const output = { '5': 0, '4': 0, '3': 0, '2': 0, '1': 1 };
+    //when
+    const result = winningnum(arrary, bonusnum);
+
+    //then
+    expect(result).toEqual(output);
+  });
+
+  test('오름차순', () => {
+    //given
+    const buyinput = 1;
+    const RANDOM_NUMBERS_TO_END = [3, 4, 5, 2, 9, 1];
+    const output = '[1, 2, 3, 4, 5, 9]';
+    const logSpy = getLogSpy();
+    mockRandoms([RANDOM_NUMBERS_TO_END]);
+    //when
+    lottoprint(buyinput);
+    //then
+    expect(logSpy).toHaveBeenCalledWith(output);
+  });
+
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+    expect(() => {
+      checkbonus('60');
+    }).toThrow('[ERROR]');
+  });
+
+  test('금액은 1000원 단위의 숫자가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      checkbuy('1001');
+    }).toThrow('[ERROR]');
+  });
+
+  // prettier-ignore
+  test('당첨 번호와 보너스 번호가 일치하면 예외가 발생한다.', () => {
+    expect(() => {
+      checkduplication({ 'numbers': [1, 2, 3, 4, 5, 6]},5);
+    }).toThrow('[ERROR]');
   });
 });
-
