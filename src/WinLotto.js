b2f36b8ce9eb3cfb7869fc7a9932a003b591d60b
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-07 17:38:39 +0900

    feat: 로또 당첨 번호에 대한 WinLotto 클래스 및 유효성 검사 추가

---
+++ src/WinLotto.js
@@ -0,0 +1,53 @@
+/**
+ * @typedef {import('./Lotto').default} Lotto
+ */
+
+import Lotto from './Lotto.js';
+
+class WinLotto {
+    /**
+     * @type {Lotto}
+     */
+    #lotto;
+
+    /**
+     * @type {number}
+     */
+    #bonusNumber;
+
+    /**
+     * @param {Lotto} lotto
+     * @param {number} bonus
+     */
+    constructor(lotto, bonus) {
+        this.#validate(lotto, bonus);
+        this.#lotto = lotto;
+        this.#bonusNumber = bonus;
+    }
+
+    /**
+     * @param {Lotto} lotto
+     * @param {number} bonus
+     */
+    #validate(lotto, bonus) {
+        if (!(typeof bonus === 'number' && bonus)) {
+            throw new Error('[ERROR] 보너스 번호는 1부터 45 사이의 숫자만 사용 가능합니다.');
+        }
+        if (!(bonus >= 1 && bonus <= 45)) {
+            throw new Error('[ERROR] 보너스 번호는 1부터 45 사이의 숫자만 사용 가능합니다.');
+        }
+        if (lotto.getNumbers().includes(bonus)) {
+            throw new Error('[ERROR] 당첨 번호로 사용된 번호는 보너스 번호로 사용할 수 없습니다.');
+        }
+    }
+
+    getLotto() {
+        return this.#lotto;
+    }
+
+    getBonusNumber() {
+        return this.#bonusNumber;
+    }
+}
+
+export default WinLotto;
