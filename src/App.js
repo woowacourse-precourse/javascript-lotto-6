bfa4dc5aae68cb7082e2b6807b3395c3d7c7d2d5
iamreum <102291571+iamreum@users.noreply.github.com>
2023-11-08 04:58:08 +0900

    play 메서드 길이 수정을 위한 메서드 분리

--- src/App.js
+++ src/App.js
@@ -6,37 +6,46 @@ import LottoStatistics from "./LottoStatistics.js";
 import Lotto from "./Lotto.js";
 
 class App {
+  constructor() {
+    this.lottoPurchase = null;
+    this.lottoStatistics = null;
+    this.purchasedLottos = [];
+  }
+
   async play() {
     try {
-      // 구입 금액
-      const purchaseAmount = await InputHandler.getPurchaseAmount();
-      const lottoPurchase = new LottoPurchase(purchaseAmount);
-      lottoPurchase.purchase();
-
-      const purchasedLottos = lottoPurchase.getLottos();
-
-      // 구매한 로또 티켓 출력
-      OutputHandler.numberOfLottoPurchase(purchasedLottos);
-
-      // 당첨 번호와 보너스 번호 입력 받기
-      const winningNumbers = await InputHandler.getWinningNumbers();
-      const bonusNumber = await InputHandler.getBonusNumbers(winningNumbers);
-
-      const lottoStatistics = new LottoStatistics(winningNumbers, bonusNumber, Lotto.PRICE);
-      lottoStatistics.calculate(purchasedLottos);
-      const statistics = lottoStatistics.getStatistics();
-
-      // 당첨 통계 출력하기
-      OutputHandler.winningStatistics(statistics);
-
-      // 수익률 계산하기
-      const profitRate = lottoStatistics.calculateProfitRate(purchasedLottos);
-      // 수익률 출력하기
-      OutputHandler.totalProfitRate(profitRate);
-
+      await this.handlePurchase();
+      await this.handleWinningNumbersInput();
+      this.handleStatistics();
+      this.handleProfitRateOutput();
     } catch (error) {
       console.error(`[ERROR] ${error.message}`);
     }
+  }
+
+  async handlePurchase() {
+    const purchaseAmount = await InputHandler.getPurchaseAmount();
+    this.lottoPurchase = new LottoPurchase(purchaseAmount);
+    this.lottoPurchase.purchase();
+    this.purchasedLottos = this.lottoPurchase.getLottos();
+    OutputHandler.numberOfLottoPurchase(this.purchasedLottos);
+  }
+
+  async handleWinningNumbersInput() {
+    const winningNumbers = await InputHandler.getWinningNumbers();
+    const bonusNumber = await InputHandler.getBonusNumbers(winningNumbers);
+    this.lottoStatistics = new LottoStatistics(winningNumbers, bonusNumber, Lotto.PRICE);
+  }
+
+  handleStatistics() {
+    this.lottoStatistics.calculate(this.purchasedLottos);
+    const statistics = this.lottoStatistics.getStatistics();
+    OutputHandler.winningStatistics(statistics);
+  }
+
+  handleProfitRateOutput() {
+    const profitRate = this.lottoStatistics.calculateProfitRate(this.purchasedLottos);
+    OutputHandler.totalProfitRate(profitRate);
   }
 }
 
