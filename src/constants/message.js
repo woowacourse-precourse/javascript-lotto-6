230b3e60c326e5e02a64686dddcced0a67685cda
Collection50 <fkaus65@naver.com>
2023-11-07 22:21:30 +0900

    refactor(number): 상금과 관련된 REWARD 객체

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,8 +1,8 @@
-import { NUMBER, RANDOM } from './number.js';
+import { RANK, NUMBER, RANDOM } from './number.js';
 
 export const SYMBOLS = {
-  nameDivider: ',',
-  numberDivider: ', ',
+  numberDivider: ',',
+  printDivider: ', ',
   boundary: '---',
   hyphen: '-',
   lineBreak: '\n',
@@ -11,28 +11,38 @@ export const SYMBOLS = {
   closeSquareBracket: ']',
 };
 
-export const REWARD = {
-  first: '2,000,000,000',
-  second: '30,000,000',
-  third: '1,500,000',
-  fourth: '50,000',
+export const REWARD_MESSAGE = {
   fifth: '5,000',
+  fourth: '50,000',
+  third: '1,500,000',
+  second: '30,000,000',
+  first: '2,000,000,000',
+};
+
+export const RANK_MESSAGE = {
+  first: 'first',
+  second: 'second',
+  third: 'third',
+  fourth: 'fourth',
+  fifth: 'fifth',
+  none: 'none',
 };
 
 export const MESSAGE = {
   askPurchaseAmount: `구입 급액을 입력해주세요.${SYMBOLS.lineBreak}`,
-  askWinningNumbers: `당첨 번호를 입력해 주세요.${SYMBOLS.lineBreak}`,
-  askBonusNumber: `보너스 번호를 입력해 주세요.${SYMBOLS.lineBreak}`,
+  askWinningNumbers: `${SYMBOLS.lineBreak}당첨 번호를 입력해 주세요.${SYMBOLS.lineBreak}`,
+  askBonusNumber: `${SYMBOLS.lineBreak}보너스 번호를 입력해 주세요.${SYMBOLS.lineBreak}`,
   winningStatistics: `${SYMBOLS.lineBreak}당첨 통계${SYMBOLS.lineBreak}`,
-  result: '총 수익률은',
+  purchase: `개를 구매했습니다.${SYMBOLS.lineBreak}`,
+  result: '총 수익률은 ',
 };
 
 export const RESULT = {
-  first: `${NUMBER.first}개 일치 (${REWARD.first}원) ${SYMBOLS.hyphen} `,
-  second: `${NUMBER.second}개 일치, 보너스 볼 일치 (${REWARD.second}원) ${SYMBOLS.hyphen} `,
-  third: `${NUMBER.third}개 일치 (${REWARD.third}원) ${SYMBOLS.hyphen} `,
-  fourth: `${NUMBER.fourth}개 일치 (${REWARD.fourth}원) ${SYMBOLS.hyphen} `,
-  fifth: `${NUMBER.fifth}개 일치 (${REWARD.fifth}원) ${SYMBOLS.hyphen} `,
+  first: `${RANK.first}개 일치 (${REWARD_MESSAGE.first}원) ${SYMBOLS.hyphen} `,
+  second: `${RANK.second}개 일치, 보너스 볼 일치 (${REWARD_MESSAGE.second}원) ${SYMBOLS.hyphen} `,
+  third: `${RANK.third}개 일치 (${REWARD_MESSAGE.third}원) ${SYMBOLS.hyphen} `,
+  fourth: `${RANK.fourth}개 일치 (${REWARD_MESSAGE.fourth}원) ${SYMBOLS.hyphen} `,
+  fifth: `${RANK.fifth}개 일치 (${REWARD_MESSAGE.fifth}원) ${SYMBOLS.hyphen} `,
 };
 
 export const ERROR = {
@@ -41,4 +51,5 @@ export const ERROR = {
   invalidWinningNumbers: `${NUMBER.lottoCount}개 숫자를 중복 없이 쉼표로 구분하여 입력해주세요.${SYMBOLS.lineBreak}`,
   invalidBonusNumber: `당첨 번호와 중복되지 않는 숫자를 입력해주세요.${SYMBOLS.lineBreak}`,
   outOfRange: `${RANDOM.min} ~ ${RANDOM.max} 사이의 자연수를 입력해주세요.${SYMBOLS.lineBreak}`,
+  invalidLottoNumbers: `로또 번호는 중복없이 ${NUMBER.lottoCount}개 자연수로 이뤄져야 합니다.${SYMBOLS.lineBreak}`,
 };
--- src/constants/number.js
+++ src/constants/number.js
@@ -1,14 +1,25 @@
 export const NUMBER = {
+  lottoCount: 6,
+  lottoPurchaseUnit: 1000,
+};
+
+export const RANK = {
   first: 6,
   second: 5,
   third: 5,
   fourth: 4,
   fifth: 3,
-  lottoCount: 6,
-  lottoPurchaseUnit: 1000,
 };
 
 export const RANDOM = {
   min: 1,
   max: 45,
+};
+
+export const REWARD = {
+  fifth: 5000,
+  fourth: 50000,
+  third: 1500000,
+  second: 30000000,
+  first: 2000000000,
 };
