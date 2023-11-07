7b6d9d75a6d3b49d4c428a878548cbe5101a1f03
yoovin <soo22839@gmail.com>
2023-11-07 23:34:17 +0900

    test: throw 확인하는 테스트 expect에서 함수 익명함수로 실행

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -32,31 +32,39 @@ describe('기능 테스트', () => {
     });
 
     test('당첨 번호에 중복이 있으면 예외가 발생한다.', () => {
-        expect(app.getWinNumbers('1,2,3,4,5,5')).toThrow('[ERROR]');
+        expect(() => {
+            app.getWinNumbers('1,2,3,4,5,5');
+        }).toThrow('[ERROR]');
     });
 
     test('당첨 번호에 1부터 45사이의 숫자가 아니면 예외가 발생한다.', () => {
-        expect(app.getWinNumbers('1,2,3,4,5,46')).toThrow('[ERROR]');
+        expect(() => {
+            app.getWinNumbers('1,2,3,4,5,46');
+        }).toThrow('[ERROR]');
     });
 
     test('당첨 번호가 6개가 아니면 예외가 발생한다.', () => {
-        expect(app.getWinNumbers('1,2,3,4,5')).toThrow('[ERROR]');
+        expect(() => {
+            app.getWinNumbers('1,2,3,4,5');
+        }).toThrow('[ERROR]');
     });
 
     test('보너스 번호에 당첨 번호가 있으면 예외가 발생한다.', () => {
-        expect(app.getBonusNumbers([1, 2, 3, 4, 5, 6], '6')).toThrow('[ERROR]');
+        expect(() => {
+            app.getBonusNumbers([1, 2, 3, 4, 5, 6], '6');
+        }).toThrow('[ERROR]');
     });
 
     test('보너스 번호가 1부터 45사이의 숫자가 아니면 예외가 발생한다.', () => {
-        expect(app.getBonusNumbers([1, 2, 3, 4, 5, 6], '46')).toThrow(
-            '[ERROR]'
-        );
+        expect(() => {
+            app.getBonusNumbers([1, 2, 3, 4, 5, 6], '46');
+        }).toThrow('[ERROR]');
     });
 
     test('보너스 번호가 1개가 아닌 경우 예외가 발생한다.', () => {
-        expect(app.getBonusNumbers([1, 2, 3, 4, 5, 6], '1,2')).toThrow(
-            '[ERROR]'
-        );
+        expect(() => {
+            app.getBonusNumbers([1, 2, 3, 4, 5, 6], '1,2');
+        }).toThrow('[ERROR]');
     });
 
     test('당첨 번호를 받아올 수 있다.', () => {
