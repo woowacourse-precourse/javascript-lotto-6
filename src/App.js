ccce2d76005c36a8bc04b0732497dd18c52d6d8d
nari <naringst@gmail.com>
2023-11-08 01:04:27 +0900

    feat : 구입 금액이 숫자 아닐 때 예외 처리

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ class App {
     //구입 금액 입력받기
     MissionUtils.Console.print("구입금액을 입력해 주세요.");
     const totalCost = await MissionUtils.Console.readLineAsync("");
+
+    //구입 금액 예외 처리 - 1) 숫자가 아닐 때
+    if (Number.isNaN(totalCost) === true) {
+      throw new Error("[ERROR] 숫자를 입력해야 합니다.");
+    }
   }
 }
 
