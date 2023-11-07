cfa76508aa7cd9ea3338b2a7b9eaffe4f95f175b
yeong-hwan <jghff700@naver.com>
2023-11-08 06:57:42 +0900

    feat: 구입 금액을 입력받아 로또 발행 및 출력하는 기능 달성
    App.js의 앞단에 구축

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import getLottoAmount from './service/input/getLottoAmount.js';
+import LottoGenerator from './service/LottoGenerator.js';
+import IssuedLottInfo from './domain/info/IssuedLottoInfo.js';
+import display from './domain/utils/display.js';
+
 class App {
-  async play() {}
+  #lottoAmount;
+  #lottoGenerator;
+
+  constructor() {
+    this.#lottoAmount = null;
+    this.#lottoGenerator = null;
+  }
+
+  async #issueLotto() {
+    this.#lottoAmount = await getLottoAmount();
+
+    this.#lottoGenerator = new LottoGenerator(this.#lottoAmount);
+    this.#lottoGenerator.generateLottos();
+
+    display.issedLottoInfo({
+      count: this.#lottoGenerator.getDrawCount(),
+      lottos: this.#lottoGenerator.getLottos(),
+    });
+  }
+
+  async play() {
+    this.#issueLotto();
+  }
 }
 
 export default App;
