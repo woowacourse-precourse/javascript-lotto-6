d9844274a0e87ff5b13aa335c898e08fb3de6ef2
hwangheetae <tejaia@naver.com>
2023-11-07 18:35:55 +0900

    feat: 보너스 번호 입력

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ class App {
 		const WINNING_NUMBERS_List = WINNING_NUMBERS.split(',').map((str) =>
 			Number(str.trim()),
 		);
+		const BONUS_NUMBER_INPUT =
+			await Console.readLineAsync('보너스 번호를 입력해 주세요.');
+
+		const BONUS_NUMBER = Number(BONUS_NUMBER_INPUT);
 	}
 }
 
