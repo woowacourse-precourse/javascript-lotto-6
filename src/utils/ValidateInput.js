e8cf80284b950d2f6a03ba811657db8f512275ce
djLee77 <dkdnl232@gmail.com>
2023-11-08 01:14:09 +0900

    refactor(ValidateInput): 숫자 범위 에러 출력 문자 통합

--- src/utils/ValidateInput.js
+++ src/utils/ValidateInput.js
@@ -2,13 +2,13 @@ const MIN_WINNING_NUMBER = 1;
 const MAX_WINNING_NUMBER = 45;
 const ERROR_MESSAGES = {
   NATURAL_NUMBER: (subject) => `[ERROR] ${subject} 자연수로 입력해주세요.\n`,
+  NUMBER_RANGE: (subject) =>
+    `[ERROR] ${subject} ${MIN_WINNING_NUMBER}부터 ${MAX_WINNING_NUMBER}사이의 값을 입력해주세요.\n`,
   MIN_AMOUNT: "[ERROR] 구입금액은 최소 1000원부터 가능합니다.\n",
   AMOUNT_UNIT: "[ERROR] 구입금액은 1,000원 단위만 가능합니다.\n",
   WINNING_NUMBER_COUNT:
     "[ERROR] 당첨 번호는 쉼표(,)로 구분하여 6개 입력해주세요.\n",
   DUPLICATE_NUMBER: "[ERROR] 번호들 중 중복된 값이 존재합니다.\n",
-  WINNING_NUMBER_RANGE: `[ERROR] 당첨 번호는 ${MIN_WINNING_NUMBER}부터 ${MAX_WINNING_NUMBER}사이의 값을 입력해주세요.\n`,
-  BONUS_NUMBER_RANGE: `[ERROR] 보너스 번호는 ${MIN_WINNING_NUMBER}부터 ${MAX_WINNING_NUMBER}사이의 값을 입력해주세요.\n`,
   BONUS_NUMBER_DUPLICATE:
     "[ERROR] 당첨 번호들 중 보너스 번호와 중복된 값이 존재합니다.\n",
 };
@@ -33,6 +33,7 @@ class ValidateInput {
   }
 
   static validateWinningNumber(numbers) {
+    const subject = "당첨 번호는";
     if (numbers.length !== 6) {
       throw new Error(ERROR_MESSAGES.WINNING_NUMBER_COUNT);
     }
@@ -43,7 +44,7 @@ class ValidateInput {
     }
 
     numbers.forEach((number) => {
-      ValidateInput.validateNaturalNumber(number, "당첨 번호는");
+      ValidateInput.validateNaturalNumber(number, subject);
     });
 
     if (
@@ -51,15 +52,16 @@ class ValidateInput {
         (number) => number < MIN_WINNING_NUMBER || number > MAX_WINNING_NUMBER,
       )
     ) {
-      throw new Error(ERROR_MESSAGES.WINNING_NUMBER_RANGE);
+      throw new Error(ERROR_MESSAGES.NUMBER_RANGE(subject));
     }
   }
 
   static validateBonusNumber(number, winningNumber) {
-    ValidateInput.validateNaturalNumber(number, "보너스 번호는");
+    const subject = "보너스 번호는";
+    ValidateInput.validateNaturalNumber(number, subject);
 
     if (number < MIN_WINNING_NUMBER || number > MAX_WINNING_NUMBER) {
-      throw new Error(ERROR_MESSAGES.BONUS_NUMBER_RANGE);
+      throw new Error(ERROR_MESSAGES.NUMBER_RANGE(subject));
     }
 
     if (winningNumber.some((item) => item === number)) {
