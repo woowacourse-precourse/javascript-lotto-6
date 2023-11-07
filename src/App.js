2e9bd80f45d0e7d58a93e6a7c95dbc5b45624dfb
박소희 <sthgml@naver.com>
2023-11-08 03:05:04 +0900

    ✨ feat($입력받기): 당첨번호 검사
    $입력 받은 번호 검사
    - Inspector.getSplited(string) 쉼표(,)를 기준으로 나눈 배열의 길이가 6이 아니면 false
    - Inspector.getIsAble(splited) 다음 경우 Error 호출
      - 나눈 배열의 요소중 빈 문자열이 있는 경우
      - 숫자가 아닌 요소가 있는 경우
      - 1-45 사이 숫자가 있는 경우
    
    Breaks 입력받은 당첨번호의 유효성 확인

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Prompter from "./utils/Prompter.js";
 import Inspector from "./utils/Inspector.js";
 import LottoGenerator from "./utils/LottoGenerator.js";
+import Lotto from "./Lotto.js";
 
 class App {
   async play() {
     // 구매 금액 입력받기
     const prompter = new Prompter();
-    const question = '';
-    const availableMoney = await prompter.getUserInput(question);
+    const QUESTION_MONEY = '로또 구입 금액을 입력 받는다. 구입 금액은 1,000원 단위로 입력 받으며 1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.';
+    const availableMoney = await prompter.getUserInput(QUESTION_MONEY);
 
     // 입력 예외 검사
     const inspector = new Inspector();
@@ -17,6 +18,13 @@ class App {
       throw new Error("[ERROR] 구입 금액은 1000으로 나누어떨어지는 숫자여야 합니다.");
     };
     
+    // 당첨 번호 입력받기
+    const QUESTION_LOTTO = '당첨 번호를 입력 받는다. 번호는 쉼표(,)를 기준으로 구분한다.';
+    const winnerLotto = await prompter.getUserInput(QUESTION_LOTTO);
+    const splited = inspector.getSplited(winnerLotto);
+    const ableWinnerLotto = splited ? inspector.getIsAble(splited) : null;
+    const lotto = new Lotto(ableLotto);
+
     // 반복 수행 횟수 정하기
     const lottoCount = availableMoney / 1000;
     
--- src/utils/Inspector.js
+++ src/utils/Inspector.js
@@ -1,4 +1,5 @@
 class Inspector {
+  // 구입금액
   containNumberOnly (number) {
     const regExpNumber = /^\d+$/g;
     if (number.match(regExpNumber)) {
@@ -10,6 +11,31 @@ class Inspector {
 
   getIsThousand (number) {
     return number % 1000 === 0;
+  }
+
+  // 당첨번호
+  getSplited (string) {
+    const splitedString = string.split(',').map((_, i, arr) => {
+      return arr[i] = arr[i].trim();
+    });
+    return splitedString.length === 6 ? splitedString : false;
+  }
+
+  getIsAble (splited) {
+    return splited.map((_, i, arr) => {
+      if (arr[i] === '') {
+        throw new Error('[ERROR] 로또 번호는 1,2,3,4,5,6 와 같은 구조로 이루어져야 합니다.');
+      };
+      const isNumber = this.containNumberOnly(arr[i]);
+      if (!isNumber) {
+        throw new Error('[ERROR] 쉼표 사이는 숫자만 들어가야 합니다.')
+      };
+      const numberEl = Number(arr[i]);
+      if(numberEl < 1 && numberEl > 45) {
+        throw new Error('[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.')
+      };
+      return arr[i];
+    });
   }
 }
 
