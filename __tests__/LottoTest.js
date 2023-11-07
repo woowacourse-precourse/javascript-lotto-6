28803dd32c77582c47254d3250cdc9dfb1101cf7
woo7i <or2447221@gmail.com>
2023-11-07 19:50:03 +0900

    test(LottoTest) : 도메인 클래스 로직인 Lotto 클래스에 해당하는 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,49 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../src/Lotto.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+test('getMatchCount메서드 실행 시 일치하는 번호의 갯수를 반환한다.', () => {
+  const lottoNumbers = [1, 2, 3, 4, 5, 6];
+  const lotto = new Lotto(lottoNumbers);
+
+  const inputNumbers = [1, 2, 3, 4, 5, 6];
+
+  const matchCount = lotto.getMatchCount(inputNumbers);
+
+  const expected = 6;
+
+  expect(matchCount).toBe(expected);
+});
+
+describe('lotto.hasBonusNumber 메서드', () => {
+  test('hasBonusNumber메서드는 로또 번호 베열에 보너스 번호가 포함되어 있으면, true를 반환한다.', () => {
+    const lottoNumbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(lottoNumbers);
+
+    const bonusNumber = 1;
+    const hasBonusNumber = lotto.hasBonusNumber(bonusNumber);
+
+    expect(hasBonusNumber).toBe(true);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
-  });
+  test('hasBonusNumber메서드는 로또 번호 베열에 보너스 번호가 포함되어 있지 않으면, false를 반환한다.', () => {
+    const lottoNumbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(lottoNumbers);
 
-  // 아래에 추가 테스트 작성 가능
+    const bonusNumber = 7;
+    const hasBonusNumber = lotto.hasBonusNumber(bonusNumber);
+
+    expect(hasBonusNumber).toBe(false);
+  });
+});
+
+test('getCompareResult메서드는 일치하는 번호의 개수와 보너스번호 포함여부를 담은 객체를 반환한다.', () => {
+  const lottoNumbers = [1, 2, 3, 4, 5, 6];
+  const lotto = new Lotto(lottoNumbers);
+
+  const winningNumbers = [1, 2, 3, 4, 5, 6];
+  const bonusNumber = 7;
+  const compareResult = lotto.getCompareResult(winningNumbers, bonusNumber);
+
+  const expected = { matchCount: 6, hasBonusNumber: false };
+
+  expect(compareResult).toEqual(expected);
 });
