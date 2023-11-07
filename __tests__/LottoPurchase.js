4651dac4b2dabf4050ec9a6a7450afd3f16bf93d
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-11-07 22:22:20 +0900

    test : 사용자 로또 구매 가격 입력 유효성 검사 테스트 추가

--- __tests__/LottoPurchase.js
+++ __tests__/LottoPurchase.js
@@ -40,3 +40,33 @@ descirbe('LottoPurchase.getLottoPurchase 테스트', () => {
     expect(amount).toBe(3000);
   });
 });
+
+descirbe('LottoPurchase.validateAmount', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('금액이 숫자가 아닌 경우에는 에러를 발생시켜야 한다.', () => {
+    expect(() => {
+      LottoPurchase.validateAmount(NaN);
+    }).toThrow('[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.');
+  });
+
+  test('금액이 1000원 미만인 경우에는 에러를 발생시켜야 한다.', () => {
+    expect(() => {
+      LottoPurchase.validateAmount(500);
+    }).toThrow('[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.');
+  });
+
+  test('금액이 1000원 단위가 아닌 경우에는 에러를 발생시켜야 한다.', () => {
+    expect(() => {
+      LottoPurchase.validateAmount(2500);
+    }).toThrow('[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.');
+  });
+
+  test('올바른 금액이 입력된 경우에는 에러를 발생시키지 않는다.', () => {
+    expect(() => {
+      LottoPurchase.validateAmount(NaN);
+    }).not.toThrow('[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.');
+  });
+});
