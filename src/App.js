a24693c4f74b64bce88002279b02d4eca01bf644
hwangheetae <tejaia@naver.com>
2023-11-07 18:35:13 +0900

    feat: 당첨번호 입력 (쉼표(,)를 기준으로 구분)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ class App {
 	async play() {
 		const PURCHASE_AMOUNT =
 			await Console.readLineAsync('구입금액을 입력해 주세요.');
+
+		const WINNING_NUMBERS =
+			await Console.readLineAsync('당첨 번호를 입력해 주세요.');
+
+		const WINNING_NUMBERS_List = WINNING_NUMBERS.split(',').map((str) =>
+			Number(str.trim()),
+		);
 	}
 }
 
