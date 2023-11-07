af37de7000187211bcbca0ce94b4154a6bb7cd87
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-07 18:00:30 +0900

    feat: play 메소드 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import * as lottoUtil from './lottoUtil.js';
+
 class App {
-  async play() {}
+    async play() {
+        const lottoPay = await lottoUtil.inputLottoPay();
+        lottoUtil.validateLottoPay(lottoPay);
+
+        const lottos = lottoUtil.lottoGenerate(lottoPay / 1000);
+        lottoUtil.printLottoNumbers(lottos);
+
+        const winLotto = await lottoUtil.winLottoGenerate();
+
+        const rankMap = lottoUtil.runCalculate(lottos, winLotto);
+        lottoUtil.printStatistics(lottoPay, rankMap);
+    }
 }
 
 export default App;
