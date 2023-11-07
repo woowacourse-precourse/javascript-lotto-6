fe8a0b36cb1df0970f43514120458bf6b015d187
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-07 20:40:37 +0900

    test: LottoValidator 테스트와 LottoTest를 하나로 통합

--- __tests__/unit/LottoTest.js
+++ __tests__/unit/LottoTest.js
@@ -2,11 +2,11 @@
 import { ERROR } from '../../src/common/constants.js';
 import LottoValidator from '../../src/service/LottoValidator.js';
 
-describe('LottoValidator 클래스 테스트', () => {
+describe('로또 번호 테스트', () => {
   test('로또가 유효한 형식이라면 성공', () => {
     const validInput = [1, 2, 3, 4, 5, 6];
     const lottoValidator = new LottoValidator(validInput);
-    expect(() => lottoValidator.validate()).not.toThrow();
+    expect(() => lottoValidator.validate()).toThrow();
   });
 
   test('로또가 배열 형태가 아니면 실패', () => {
@@ -25,5 +25,17 @@ describe('LottoValidator 클래스 테스트', () => {
     const invalidInput = [1, 2, 3, 4, 5, 46];
     const lottoValidator = new LottoValidator(invalidInput);
     expect(() => lottoValidator.validate()).toThrow(ERROR.range);
+  });
+
+  test('로또의 번호가 중복되었다면 실패', () => {
+    const invalidInput = [1, 2, 3, 4, 5, 5];
+    const lottoValidator = new LottoValidator(invalidInput);
+    expect(() => lottoValidator.validate()).toThrow(ERROR.lotto_duplicate);
+  });
+
+  test('로또 번호가 6개가 아니라면 실패', () => {
+    const validInput = [1, 2, 3, 4, 5, 6, 7];
+    const lottoValidator = new LottoValidator(validInput);
+    expect(() => lottoValidator.validate()).toThrow(ERROR.lotto_length);
   });
 });
