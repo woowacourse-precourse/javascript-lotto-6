3dc03c07f92a9cb2b341973d4c093c3054713bee
hanyoung <phanyoung@naver.com>
2023-11-07 21:04:56 +0900

    refactor: 로또 구매 내역 출력 로직 분리

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,14 +1,29 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const OutputView = {
+  printLottoLength(lottos) {
+    Console.print(`${lottos.length}개를 구매했습니다.`);
+  },
+
+  printLottos(lottos) {
+    lottos.forEach((lotto) => {
+      Console.print(`[${lotto.getNumbers().join(", ")}]`);
+    });
+  },
+
+  printBlankLine() {
+    Console.print("");
+  },
+
   printPrizes(prizes) {
-    Console.print(`
-    3개 일치 (5,000원) - ${this.countElementInArray("5등", prizes)}개  
-    4개 일치 (50,000원) - ${this.countElementInArray("4등", prizes)}개
-    5개 일치 (1,500,000원) - ${this.countElementInArray("3등", prizes)}개
-    5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.countElementInArray("2등", prizes)}개
-    6개 일치 (2,000,000,000원) - ${this.countElementInArray("1등", prizes)}개
-    `);
+    Console.print(`\
+당첨 통계
+---
+3개 일치 (5,000원) - ${this.countElementInArray("5등", prizes)}개  
+4개 일치 (50,000원) - ${this.countElementInArray("4등", prizes)}개
+5개 일치 (1,500,000원) - ${this.countElementInArray("3등", prizes)}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.countElementInArray("2등", prizes)}개
+6개 일치 (2,000,000,000원) - ${this.countElementInArray("1등", prizes)}개`);
   },
 
   countElementInArray(target, array) {
