fad978c4bfd7471fc3408481530ab995b8c66cd6
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-07 11:49:59 +0900

    feat: validation hook 구현
    입력 유효성 검사하는 hook 구현:
    - checkNull()
    - checkBlank()
    - checkChar()
    - checkIndivisible()
    - checkEmpty()
    - checkWrong()
    - checkOutOfRange()
    - checkDuplicate()

---
+++ src/Validation.js
@@ -0,0 +1,53 @@
+import { INPUT_ERROR, NUMBER_CRITERIA, UNIT } from "./Constants.js";
+
+const Validation = {
+  checkNull(number) {
+    if (number.length === 0) {
+      throw new Error(INPUT_ERROR.null);
+    }
+  },
+
+  checkBlank(number) {
+    if (number.replaceAll(" ", "").length !== number.length) {
+      throw new Error(INPUT_ERROR.blank);
+    }
+  },
+
+  checkChar(number) {
+    if (Number.isNaN(Number(number))) {
+      throw new Error(INPUT_ERROR.char);
+    }
+  },
+
+  checkIndivisible(money) {
+    if (Number(money) % UNIT !== 0) {
+      throw new Error(INPUT_ERROR.indivisible);
+    }
+  },
+
+  checkEmpty(number) {
+    if (number === "") {
+      throw new Error(INPUT_ERROR.empty);
+    }
+  },
+
+  checkWrong(numberList) {
+    if (numberList.length !== NUMBER_CRITERIA) {
+      throw new Error(INPUT_ERROR.wrong);
+    }
+  },
+
+  checkOutOfRange(number) {
+    if (number < 1 || number > 45) {
+      throw new Error(INPUT_ERROR.outOfRange);
+    }
+  },
+
+  checkDuplicate(numberList) {
+    if (new Set(numberList).length !== NUMBER_CRITERIA) {
+      throw new Error(INPUT_ERROR.duplicate);
+    }
+  },
+};
+
+export default Validation;
