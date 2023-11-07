dc91ec2468f64edddcfbdda27867392c4d497ec0
anttiey <hanuuny@gmail.com>
2023-11-07 11:22:39 +0900

    refactor: 입력 처리 중복 코드 함수로 추출
    - 입력을 처리하는 중복된 코드를 read 함수로 추출

--- src/Input.js
+++ src/Input.js
@@ -1,22 +1,27 @@
+import Validation from './Validation.js';
 import { Console } from '@woowacourse/mission-utils';
 import { INPUT } from './constants/Messages.js';
 
 class Input {
   async userMoney() {
-    const userMoneyInput = await Console.readLineAsync(INPUT.userMoney);
+    const userMoneyInput = await this.read(INPUT.userMoney);
     return userMoneyInput.trim();
   }
 
   async winningNumbers() {
-    const winningNumbersInput = await Console.readLineAsync(
-      INPUT.winningNumbers
-    );
+    const winningNumbersInput = await this.read(INPUT.winningNumbers);
     return winningNumbersInput.trim();
   }
 
   async bonusNumbers() {
-    const bonusNumbersInput = await Console.readLineAsync(INPUT.bonusNumber);
+    const bonusNumbersInput = await this.read(INPUT.bonusNumber);
     return bonusNumbersInput.trim();
+  }
+
+  async read(message) {
+    const input = await Console.readLineAsync(message);
+    Validation.validateInputEmpty(input);
+    return input;
   }
 }
 
