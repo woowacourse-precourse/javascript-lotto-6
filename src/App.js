22499743aea94ddb5918602b6008db717cefaa56
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-08 02:20:36 +0900

    refactor: handle error in controller not in model

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import draw from "./models/Draw.js";
-import purchase from "./models/Purchase.js";
-import reward from "./models/Reward.js";
-import inputView from "./views/Input.js";
-import outputView from "./views/Output.js";
+import lottoGame from "./controller/LottoGame.js";
 
 class App {
   async play() {
-    /* 로또 구매 */
-    const purchaseAmount = await inputView.enterPurchaseAmount();
-    const lottoCount = purchase.countMoney(purchaseAmount);
-
-    const lottos = purchase.getLottos(lottoCount);
-    const lottoNumbersArr = lottos.map((lotto) => lotto.getNumbers());
-    outputView.printPurchasedLottos(lottoNumbersArr);
-
-    const winningNumber = await inputView.enterWinningNumber();
-    const bonusNumber = await inputView.enterBonusNumber();
-    const matches = draw.getResult(lottoNumbersArr, winningNumber, bonusNumber);
-    outputView.printWinningStatistics(matches);
-
-    const winningAmount = reward.computeWinningAmount(matches);
-    const totalReturn = reward.computeTotalReturn(
-      winningAmount,
-      purchaseAmount
-    );
-    outputView.printTotalReturn(totalReturn);
+    await lottoGame.start();
   }
 }
 
--- src/views/Input.js
+++ src/views/Input.js
@@ -5,13 +5,7 @@ import { throwErrorIf } from "../utils/index.js";
 const inputView = {
   enterPurchaseAmount: async function () {
     const amount = await Console.readLineAsync(message.ENTER_PURCHASE_AMOUNT);
-
-    try {
-      this.validate(inputType.PURCHASE, amount);
-    } catch (error) {
-      Console.print(error.message);
-      await this.enterPurchaseAmount();
-    }
+    this.validate(inputType.PURCHASE, amount);
 
     return Number(amount);
   },
@@ -19,32 +13,15 @@ const inputView = {
   enterWinningNumber: async function () {
     const numbers = await Console.readLineAsync(message.ENTER_WINNING_NUMBER);
 
-    return await Promise.all(
-      numbers
-        .split(",")
-        .map(async (number) => await this.returnValidNumber(number))
-    );
-  },
-
-  returnValidNumber: async function (number) {
-    try {
+    return numbers.split(",").map((number) => {
       this.validate(inputType.LOTTO, number);
       return Number(number);
-    } catch (error) {
-      Console.print(error.message);
-      await this.enterWinningNumber();
-    }
+    });
   },
 
   enterBonusNumber: async function () {
     const number = await Console.readLineAsync(message.ENTER_BONUS_NUMBER);
-
-    try {
-      this.validate(inputType.LOTTO, number);
-    } catch (error) {
-      Console.print(error.message);
-      await this.enterBonusNumber();
-    }
+    this.validate(inputType.LOTTO, number);
 
     return Number(number);
   },
