34b22791f838ae6a1ad44ba32810bc7fb6a5e3bb
정민규 <jmg081005@naver.com>
2023-11-07 15:14:10 +0900

    fix) 예외처리 수정
    1. 에러 예외처리수정
    2. 반올림 제대로 안되던 버그 fix

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App {
       const input = await MissionUtils.Console.readLineAsync(message);
       return input;
     } catch (error) {
-      throw "[ERROR] 입력을 받는 중 실패하였습니다.";
+      throw new Error("[ERROR] 입력을 받는 중 실패하였습니다.");
     }
   }
 
@@ -29,8 +29,10 @@ class App {
 
   async #getUserMoney() {
     const input = await this.#getInput("구입금액을 입력해 주세요.");
+    if (!/^[0-9]+$/.test(input))
+      throw new Error("[ERROR] 숫자를 입력해 주세요!!");
     if (+input % 1000 !== 0)
-      throw new error("[ERROR] 돈은 천원 단위로 입력해주세요");
+      throw new Error("[ERROR] 돈은 천원 단위로 입력해주세요");
     return this.#changeMoneyToLottoCnt(input);
   }
 
@@ -39,7 +41,7 @@ class App {
     const winningNumbers = input.split(",").map((v) => +v);
     const validateNumbers = winningNumbers.every((num) => 0 < num && num <= 45);
     if (!validateNumbers || winningNumbers.length !== 6)
-      throw new error("[ERROR] 로또 형식에 부합하지 않습니다.");
+      throw new Error("[ERROR] 로또 형식에 부합하지 않습니다.");
     return winningNumbers;
   }
 
@@ -47,17 +49,18 @@ class App {
     const input = await this.#getInput("보너스 번호를 입력해 주세요.");
     const bonusNum = +input;
     if (!/^[0-9]+$/.test(input))
-      throw new error("[ERROR] 숫자를 입력해 주세요");
+      throw new Error("[ERROR] 숫자를 입력해 주세요");
     if (bonusNum < 1 || 45 < bonusNum)
-      throw new error("[ERROR] 1~45 사이 수를 입력하세요");
+      throw new Error("[ERROR] 1~45 사이 수를 입력하세요");
     if (winningNumbers.includes(bonusNum))
-      throw new error("[ERROR] 보너스 번호는 로또번호와 달라야 합니다.");
+      throw new Error("[ERROR] 보너스 번호는 로또번호와 달라야 합니다.");
     return bonusNum;
   }
 
   #makeLottoAry(lottoCnt) {
     const ary = [...Array(lottoCnt)].map((_) => {
-      const lotto = new Lotto();
+      const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      const lotto = new Lotto(numbers);
       lotto.printLottoNum();
       return lotto;
     });
@@ -76,8 +79,8 @@ class App {
       (a, c, idx) => a + c * this.#winMoney[idx],
       0
     );
-    const winAmount = Math.round((amount / money) * 100, 2);
-    MissionUtils.Console.print(`총 수익률은 ${winAmount}% 입니다.`);
+    const winAmount = Math.round((amount / money) * 10000) / 100;
+    MissionUtils.Console.print(`총 수익률은 ${winAmount}%입니다.)`);
   }
 
   async play() {
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,20 +3,25 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class Lotto {
   #numbers;
 
-  constructor() {
-    this.#numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
     this.#numbers.sort((a, b) => a - b);
-    this.#validate(this.#numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const set = new Set(numbers);
+    if (set.size !== numbers.length) {
+      throw new Error("[ERROR] 중복된 로또번호는 있을 수 없습니다.");
+    }
   }
 
   printLottoNum() {
-    MissionUtils.Console.print(this.#numbers);
+    let str = this.#numbers.join(", ");
+    MissionUtils.Console.print("[" + str + "]");
   }
 
   checkLottoLank(winningNumbers, bonusNum) {
