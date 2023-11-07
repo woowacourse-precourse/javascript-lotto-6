aa10b4dae3162612f723041699dd20a1e6987c01
OAO <ashgrayblue0@gmail.com>
2023-11-07 23:36:18 +0900

    feat(Lotto): implement Error check and number getter method

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,28 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.#checkDuplication(numbers);
     this.#numbers = numbers;
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  #checkDuplication(numbers) {
+    numbers.forEach((e) => {
+      if (numbers.includes(e)) {
+        throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
+      }
+    });
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
