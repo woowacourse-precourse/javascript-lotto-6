afd53989e2f5f1bcbc52bbbba4110d67f9f3b603
BaeDongWoo <ehddn5410@naver.com>
2023-11-07 20:17:49 +0900

    feat:사용자에게 구입 금액을 입력받고 검사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console, Random } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    const input = await this.priceInput();
+  }
+  async priceInput() {
+    const input = await Console.readLineAsync('구입금액을 입력해 주세요.');
+    return this.validatePriceInput(input);
+  }
+  async validatePriceInput(input) {
+    try {
+      if (isNaN(input)) throw new Error('[ERROR] 숫자만 입력해 주세요');
+      if (input % 1000 !== 0 || input <= 0)
+        throw new Error('[ERROR] 1000원 단위로 입력해주세요');
+    } catch (error) {
+      Console.print(error);
+      // this.priceInput();
+    }
+  }
 }
-
+const app = new App();
+app.validatePriceInput('12345');
 export default App;
