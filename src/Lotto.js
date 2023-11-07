a0e6928711dc8ddf253f08d6a8e3d65aa9e7f086
inhwa <dlsghk0711@naver.com>
2023-11-07 19:18:48 +0900

    Lotto 클래스

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,6 @@
+import { Console,Random } from "@woowacourse/mission-utils";
+
+
 class Lotto {
   #numbers;
 
@@ -7,12 +10,53 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    const num = (Object.values(numbers)).length;
+    if(num!=6){
+      throw new Error('[ERROR] 6자리 숫자여야 합니다.');
+    }
+    
+    const str = String(Object.values(numbers));
+    const inputToSet = new Set(str.split(',').map(Number));
+    if ([...inputToSet].length !== 6) {
+      throw new Error('[ERROR] 숫자는 중복이 없어야 합니다.');
     }
   }
-
+  
+ 
   // TODO: 추가 기능 구현
+  sortNumbers() {
+    this.#numbers.sort((a, b) => a - b);
+  }
+
+  printNumbers() {
+    this.sortNumbers();
+
+    Console.print(`[${this.#numbers.join(', ')}]`);
+  } 
+
+  getBonusRank(bonusNumber){
+    this.#numbers.forEach((number) => {
+      if (bonusNumber.includes(number)) {
+        return true;
+      }
+    });
+  }
+
+  getRank(winningNumbers, bonusNumber) {
+    let count = 0;
+    this.#numbers.forEach((number) => {
+      if (winningNumbers.includes(String(number))) {
+        count+=1;      
+      }
+    });
+
+    if (count===5){
+      if(this.getBonusRank(String(bonusNumber)));{
+        count+=1;
+      }
+    }
+    return count;
+  }
 }
 
 export default Lotto;
