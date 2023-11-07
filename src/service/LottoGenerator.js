636b0b6c010a0cd263b0c7d0f3ee2e846a3576b9
yeong-hwan <jghff700@naver.com>
2023-11-08 05:53:12 +0900

    feat: 로또 발행 기능 구현

---
+++ src/service/LottoGenerator.js
@@ -0,0 +1,46 @@
+import { Random } from '@woowacourse/mission-utils';
+import NUMBER from '../constants/number.js';
+import Lotto from '../Lotto.js';
+import sortNumberArray from '../domain/utils/sortNumberArray.js';
+
+/**
+ * 입력된 금액만큼 로또를 발행한다.
+ */
+class LottoGenerator {
+  #lottoAmount;
+  #drawCount;
+
+  constructor(amount) {
+    this.#lottoAmount = amount;
+    this.#drawCount = 0;
+  }
+
+  #setDrawCount(amount) {
+    this.#drawCount = parseInt(this.#lottoAmount / NUMBER.unitCost);
+  }
+
+  #generateSingleLotto() {
+    return Random.pickUniqueNumbersInRange(
+      NUMBER.minNumber,
+      NUMBER.maxNumber,
+      NUMBER.drawCount,
+    );
+  }
+
+  generateLotto() {
+    this.#setDrawCount();
+    const lotto = new Array();
+
+    for (let count = 0; count < this.#drawCount; count++) {
+      const singleLotto = this.#generateSingleLotto();
+      lotto.push(sortNumberArray(singleLotto));
+    }
+
+    return lotto;
+  }
+}
+
+export default LottoGenerator;
+
+// const lottoGenerator = new LottoGenerator(5000);
+// console.log(lottoGenerator.generateLotto());
