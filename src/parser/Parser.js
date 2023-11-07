07ff61efa2abf7a681acc68d3c9eac544d513a62
pakxe <pigkill40@naver.com>
2023-11-08 01:19:08 +0900

    feat: 원시값인 배열의 요소를 로또 넘버로 포장하는 로직 추가

--- src/parser/Parser.js
+++ src/parser/Parser.js
@@ -1,4 +1,5 @@
 import LOTTO from '../constant/Lotto.js';
+import LottoNumber from '../domain/LottoNumber.js';
 
 class Parser {
   static parseInt(input) {
@@ -7,11 +8,17 @@ class Parser {
   }
 
   static parseLotto(input) {
-    return input.split(LOTTO.delimiter).map(Number);
+    const numArr = Array.isArray(input) ? input : input.split(LOTTO.delimiter);
+
+    return this.parseArrToLottoNumber(numArr);
   }
 
   static parseToFixed(input, limit) {
     return parseFloat(input.toFixed(limit));
+  }
+
+  static parseArrToLottoNumber(arr) {
+    return arr.map((num) => new LottoNumber(num));
   }
 }
 
