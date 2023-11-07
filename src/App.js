144290e20d6cb5fbbed3e29aab4cd2399ab4689a
dongsuseo <poopoo0515@naver.com>
2023-11-07 16:49:58 +0900

    feat: 로또번호 오름차순 정렬 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
 import AmountInput from "./View/AmountInput";
-import CalculateLottoNumber from "./View/CalculateLottoNumber";
+import makeRandomNumbers from "./makeRandomNumbers";
+import calculateLottoNumber from "./calculateLottoNumber";
 class App {
   async play() {
-    const lottoList = [];
-    const amount = AmountInput();
-    const lottoNumber = CalculateLottoNumber(amount);
+    const amount = await AmountInput();
+    const lottoNumber = calculateLottoNumber(amount);
+    Console.print(`${lottoNumber}개를 구매했습니다.`);
+
+    for (let i = 0; i < lottoNumber; i++) {
+      const sortedRandomNumbers = makeRandomNumbers().sort((a, b) => a - b);
+      Console.print(`[${sortedRandomNumbers.join(", ")}]`);
+    }
   }
 }
 
