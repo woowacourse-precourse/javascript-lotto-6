048ce229199971c295c03b00f86a335b3c15da04
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-08 04:09:53 +0900

    test: check input data validate

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,6 @@
 import Lotto from "../src/Lotto.js";
+import LottoGame from "../src/LottoGame.js";
+import { validateInputMoney } from "../src/Validator.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -14,5 +16,11 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+  test("로또 구입 가격이 1000원 단위가 아니면 예외가 발생한다.", () => {
+    const invalidInputMoney = 1501;
+    expect(() => {
+      new validateInputMoney(invalidInputMoney);
+    }).toThrow("[ERROR]");
+  });
 });
