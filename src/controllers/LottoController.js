04fa1d0ce237cc09359434b84df9554e6b0d58c4
JonghyunLEE12 <najo7609@gmail.com>
2023-11-07 16:49:25 +0900

    Chore : [ 예기치 않은 오류 찾기 #1 ]

--- src/controllers/LottoController.js
+++ src/controllers/LottoController.js
@@ -17,66 +17,67 @@ class LottoController {
     if (this.LOTTO_DATA.userMoney === CONSTANTS.IS_ERROR) {
       return this.inputPurchaseMoney();
     }
-    return this.#buyLotto();
+    // return this.#buyLotto();
+    return true;
   }
 
-  async #buyLotto() {
-    const CALC_BUY = new LottoUtill(this.LOTTO_DATA.userMoney);
-    await this.#printLottoNumber(CALC_BUY.howManyToBuy());
-  }
+  // async #buyLotto() {
+  //   const CALC_BUY = new LottoUtill(this.LOTTO_DATA.userMoney);
+  //   await this.#printLottoNumber(CALC_BUY.howManyToBuy());
+  // }
 
-  async #printLottoNumber(canBuy) {
-    this.OUTPUT_VIEW.userCanBuy(canBuy);
-    await this.#lottoGenerator(canBuy);
-    await this.#inputWinNumber();
-  }
+  // async #printLottoNumber(canBuy) {
+  //   this.OUTPUT_VIEW.userCanBuy(canBuy);
+  //   await this.#lottoGenerator(canBuy);
+  //   await this.#inputWinNumber();
+  // }
 
-  async #lottoGenerator(canBuy) {
-    if (canBuy > 0) {
-      const lottoNumber = await new Lotto();
-      this.LOTTO_DATA.userNumber.push(await lottoNumber.generator());
-      this.OUTPUT_VIEW.userLottoNumber(await lottoNumber.sortingNumber());
-      await this.#lottoGenerator(canBuy - 1);
-    }
-  }
+  // async #lottoGenerator(canBuy) {
+  //   if (canBuy > 0) {
+  //     const lottoNumber = await new Lotto();
+  //     this.LOTTO_DATA.userNumber.push(await lottoNumber.generator());
+  //     this.OUTPUT_VIEW.userLottoNumber(await lottoNumber.sortingNumber());
+  //     await this.#lottoGenerator(canBuy - 1);
+  //   }
+  // }
 
-  async #inputWinNumber() {
-    this.LOTTO_DATA.getWinNumber(await this.INPUT_VIEW.winLotteryNumber());
-    try {
-      await Lotto.createLottoInstance(this.LOTTO_DATA.winNumber);
-      // await new Lotto(this.LOTTO_DATA.winNumber);
-      return this.#inputBonusNumber();
-    } catch (error) {
-      this.OUTPUT_VIEW.printError(error.message);
-      return this.#inputWinNumber();
-    }
-  }
+  // async #inputWinNumber() {
+  //   this.LOTTO_DATA.getWinNumber(await this.INPUT_VIEW.winLotteryNumber());
+  //   try {
+  //     await Lotto.createLottoInstance(this.LOTTO_DATA.winNumber);
+  //     // await new Lotto(this.LOTTO_DATA.winNumber);
+  //     return this.#inputBonusNumber();
+  //   } catch (error) {
+  //     this.OUTPUT_VIEW.printError(error.message);
+  //     return this.#inputWinNumber();
+  //   }
+  // }
 
-  async #inputBonusNumber() {
-    this.LOTTO_DATA.getBonusNumber(
-      await this.INPUT_VIEW.bonusNumber(this.LOTTO_DATA.winNumber),
-    );
-    if (this.LOTTO_DATA.bonusNumber === CONSTANTS.IS_ERROR) {
-      return this.#inputBonusNumber();
-    }
-    return this.#printStatistic();
-  }
+  // async #inputBonusNumber() {
+  //   this.LOTTO_DATA.getBonusNumber(
+  //     await this.INPUT_VIEW.bonusNumber(this.LOTTO_DATA.winNumber),
+  //   );
+  //   if (this.LOTTO_DATA.bonusNumber === CONSTANTS.IS_ERROR) {
+  //     return this.#inputBonusNumber();
+  //   }
+  //   return this.#printStatistic();
+  // }
 
-  async #printStatistic() {
-    const clacStatic = new LottoUtill();
-    const lottoStatic = await clacStatic.checkLottoCorrect(
-      this.LOTTO_DATA.userNumber,
-      this.LOTTO_DATA.winNumber,
-      this.LOTTO_DATA.bonusNumber,
-    );
-    this.OUTPUT_VIEW.lottoStatic(lottoStatic);
-    this.#printRate(lottoStatic);
-  }
+  // async #printStatistic() {
+  //   const clacStatic = new LottoUtill();
+  //   const lottoStatic = await clacStatic.checkLottoCorrect(
+  //     this.LOTTO_DATA.userNumber,
+  //     this.LOTTO_DATA.winNumber,
+  //     this.LOTTO_DATA.bonusNumber,
+  //   );
+  //   this.OUTPUT_VIEW.lottoStatic(lottoStatic);
+  //   this.#printRate(lottoStatic);
+  // }
 
-  async #printRate(lottoStatic) {
-    const rateUtill = new LottoUtill(this.LOTTO_DATA.userMoney, lottoStatic);
-    this.OUTPUT_VIEW.totalRevenue(rateUtill.getRate());
-  }
+  // async #printRate(lottoStatic) {
+  //   const rateUtill = new LottoUtill(this.LOTTO_DATA.userMoney, lottoStatic);
+  //   this.OUTPUT_VIEW.totalRevenue(rateUtill.getRate());
+  // }
 }
 
 export default LottoController;
