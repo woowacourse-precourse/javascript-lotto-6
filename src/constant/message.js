a0f2e2c3eda60b34894d78ff48c96d0095d46617
Hchan2 <seongchan_@naver.com>
2023-11-07 22:18:45 +0900

    feat: 에러 메시지 상수 선언

---
+++ src/constant/message.js
@@ -0,0 +1,11 @@
+export const ERROR_MESSAGES = Object.freeze({
+    prefix: "[ERROR]",
+    invalidSingle: "1자리 숫자를 입력해주세요.",
+    invalidNumber: "숫자를 입력해주세요.",
+    invalidOnlyNumber: "숫자만 입력해주세요.",
+    invalidBetweenOneAndFortyFive: "숫자 1부터 45까지 입력해주세요.",
+    invalidNotIncludedLottoNumbers: "입력한 로또 번호와 다른 번호를 입력해주세요.",
+    invalidNotOverlap: "중복되지 않은 숫자를 입력해주세요.",
+    invalidThousandWonUnit: "1000원 단위로 입력해주세요. (예. 1000, 5000, 10000)",
+    invalidMoreThousand: "1000원(1장) 이상 입력해주세요.",
+})
