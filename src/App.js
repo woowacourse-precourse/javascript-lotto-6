1b9d0fdc27083103be25ad9379e83f10e82b44ea
Seungbo Shim <fodo9898@gmail.com>
2023-11-08 04:16:47 +0900

    feat: 함수 구조 정립

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@ class App {
   async play() {
     
   }
+
+  /** 입력 받기 */
+  async inputLotto() {
+    try {
+      const purchase = await this.getPurchase();
+      const lottoTicket = this.getLottoTicket(purchase);
+      const lottoNumber = await this.getLottoNumber();
+      const bonusNumber = await this.getBonusNumber();
+
+      const result = this.getResult(lottoTicket, lottoNumber, bonusNumber);
+    } catch(error) {
+
+    }
+  }
+
+  /** 구입 금액 입력 */
+  async getPurchase() {
+
+  }
+
+  /** 로또 생성 */
+  getLottoTicket(purchase) {
+
+  }
+
+  /** 당첨 번호 받기 */
+  async getLottoNumber() {
+
+  }
+
+  /** 보너스 번호 받기 */
+  async getBonusNumber() {
+
+  }
+
+  /** 결과 출력 */
+  getResult(lottoTicket, lottoNumber, bonusNumber) {
+
+  }
 }
 
 export default App;
