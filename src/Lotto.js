310eb2a2dba7b710d928af3be36fd0928f724428
yeonkyoung <keep.ykk@gmail.com>
2023-11-08 04:21:02 +0900

    feat: 정렬이 되었는지 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -16,6 +16,10 @@ class Lotto {
       throw new Error(NOT_AVAILABLE_NUMBER_ERROR_MESSAGE);
     if ([...new Set(numbers)].length !== SIX_NUMBERS)
       throw new Error(NOT_ALLOWED_DUPLICATED_NUMBERS);
+    for (let index = 0; index < SIX_NUMBERS - 1; index += 1) {
+      if (numbers[index] >= numbers[index + 1])
+        throw new Error(NOT_SORTED_ASCENDING_ORDER);
+    }
   }
 
   // TODO: 추가 기능 구현
