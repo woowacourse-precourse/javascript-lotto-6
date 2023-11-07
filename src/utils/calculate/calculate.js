8b5c31306b781509edf2550828ebb131decfb3a1
kwoncha <rnjsckgus@gmail.com>
2023-11-08 00:36:21 +0900

    feat: 구입 로또 당첨 확인 기능 구현

--- src/utils/calculate/calculate.js
+++ src/utils/calculate/calculate.js
@@ -1,3 +1,5 @@
+import { RESULT } from '../../constants/constants.js';
+
 const filterLottoNumbers = (winningNumbers, lottoNumbers) => {
   const filteredNumber = lottoNumbers.filter(number => {
     return winningNumbers.includes(number);
@@ -5,3 +7,30 @@ const filterLottoNumbers = (winningNumbers, lottoNumbers) => {
 
   return filteredNumber;
 };
+
+const matchedLottoNumbers = (lottoResults, matchedNumbers, matchedBonus) => {
+  const updatedResults = { ...lottoResults };
+
+  if (matchedNumbers.length === 6) updatedResults[RESULT.FIRST] += 1;
+  if (matchedNumbers.length === 5 && matchedBonus) updatedResults[RESULT.SECOND] += 1;
+  if (matchedNumbers.length === 5) updatedResults[RESULT.THIRD] += 1;
+  if (matchedNumbers.length === 4) updatedResults[RESULT.FOURTH] += 1;
+  if (matchedNumbers.length === 3) updatedResults[RESULT.FIFTH] += 1;
+
+  return updatedResults;
+};
+
+const countIncludeNumbers = (lottoResults, winningNumbers, bonusNumber, lottoNumbersArray) => {
+  const updatedLottoResults = { ...lottoResults };
+
+  lottoNumbersArray.forEach(lottoNumbers => {
+    const matchedNumbers = filterLottoNumbers(winningNumbers, lottoNumbers);
+    const matchedBonus = lottoNumbers.includes(bonusNumber);
+    const updatingLottoResults = matchedLottoNumbers(updatedLottoResults, matchedNumbers, matchedBonus);
+    Object.assign(updatedLottoResults, updatingLottoResults);
+  });
+
+  return updatedLottoResults;
+};
+
+export { countIncludeNumbers };
