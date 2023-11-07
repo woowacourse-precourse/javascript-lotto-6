c297cb98524cf8977d24f84a58d615808fdbd6bc
Boeun <boeun0904@naver.com>
2023-11-08 01:33:02 +0900

    feat(Lotto): 등수 반환 함수 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -26,21 +26,39 @@ export default class Lotto {
     return this.#numbers.includes(bonusNumber);
   }
 
+  getWinningPlace(winningNumbers, bonusNumber) {
+    const matchCnt = this.getWinningNumbersMatchCount(winningNumbers);
+    const hasBonus = this.hasBonusNumber(bonusNumber);
+
+    if (matchCnt === LOTTERY.FIFTH_CNT) return LOTTERY.FIFTH_PLACE;
+    if (matchCnt === LOTTERY.FOURTH_CNT) return LOTTERY.FOURTH_PLACE;
+    if (matchCnt === LOTTERY.SECOND_CNT && hasBonus) return LOTTERY.SECOND_PLACE;
+    if (matchCnt === LOTTERY.THIRD_CNT) return LOTTERY.THIRD_PLACE;
+    if (matchCnt === LOTTERY.FIRST_CNT) return LOTTERY.FIRST_PLACE;
+    return LOTTERY.DEFAULT_PLACE;
+  }
+
   static getLottoWinnings(matchCnt, hasBonus) {
     const result = {
+      place: LOTTERY.DEFAULT_PLACE,
       label: `${MESSAGE.MATCH_NUMBERS(matchCnt)}`,
       winnings: LOTTERY.DEFAULT_WINNINGS,
     };
 
-    if (matchCnt === LOTTERY.FIFTH_CNT) return { ...result, winnings: LOTTERY.FIFTH_WINNINGS };
-    if (matchCnt === LOTTERY.FOURTH_CNT) return { ...result, winnings: LOTTERY.FOURTH_WINNINGS };
+    if (matchCnt === LOTTERY.FIFTH_CNT)
+      return { ...result, place: LOTTERY.FIFTH_PLACE, winnings: LOTTERY.FIFTH_WINNINGS };
+    if (matchCnt === LOTTERY.FOURTH_CNT)
+      return { ...result, place: LOTTERY.FOURTH_PLACE, winnings: LOTTERY.FOURTH_WINNINGS };
     if (matchCnt === LOTTERY.SECOND_CNT && hasBonus === true)
       return {
         label: result.label + `, ${MESSAGE.MATCH_BONUS_NUMBER}`,
+        place: LOTTERY.SECOND_PLACE,
         winnings: LOTTERY.SECOND_WINNINGS,
       };
-    if (matchCnt === LOTTERY.THIRD_CNT) return { ...result, winnings: LOTTERY.THIRD_WINNINGS };
-    if (matchCnt === LOTTERY.FIRST_CNT) return { ...result, winnings: LOTTERY.FIRST_WINNINGS };
+    if (matchCnt === LOTTERY.THIRD_CNT)
+      return { ...result, place: LOTTERY.THIRD_PLACE, winnings: LOTTERY.THIRD_WINNINGS };
+    if (matchCnt === LOTTERY.FIRST_CNT)
+      return { ...result, place: LOTTERY.FIRST_PLACE, winnings: LOTTERY.FIRST_WINNINGS };
     return result;
   }
 }
