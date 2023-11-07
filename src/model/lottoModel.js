479b6ea5a65a9bf7f4d9b52c9cd8d88decad36cf
suwonthugger <ddww1029wwdd@gmail.com>
2023-11-08 04:55:42 +0900

    Feat: 모델에 상수를 설정하는 기능 추가

--- src/model/lottoModel.js
+++ src/model/lottoModel.js
@@ -1,7 +1,37 @@
+import generateLotto from "../utils/generateLotto";
+
 export default class lottoModel {
   #purchaseAmount;
   #purchaseCount;
   #lottoArray;
   #winningNumber;
   #bonusNumber;
+
+  constructor(purchaseAmount) {
+    this.#purchaseAmount = purchaseAmount;
+    this.#purchaseCount;
+    this.#lottoArray;
+    this.#winningNumber;
+    this.#bonusNumber;
+  }
+
+  setPurchaseCount() {
+    this.#purchaseCount = this.#purchaseAmount / 1000;
+  }
+
+  getPurchaseCount() {
+    return this.#purchaseCount;
+  }
+
+  setLottoArray() {
+    this.#lottoArray = generateLotto(this.#purchaseCount);
+  }
+
+  setLottoArray() {
+    this.#lottoArray;
+  }
+
+  setBonusNumber() {
+    this.#bonusNumber;
+  }
 }
