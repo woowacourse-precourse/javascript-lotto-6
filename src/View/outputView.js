b9c0d3a8eebdf972f378db1a41ee8255ac3b28cf
coding-frog117 <syeshe2@gmail.com>
2023-11-07 21:04:56 +0900

    Refactor : 로또 번호 출력 형식 변경

--- src/View/outputView.js
+++ src/View/outputView.js
@@ -4,7 +4,10 @@ import { OUTPUT_MESSAGE } from '../constants/messages.js';
 const outputView = {
 	printLottoNumbers(amount, lottoNumbers) {
 		Console.print(`\n${amount}${OUTPUT_MESSAGE.infoLottoNumbers}`);
-		Console.print(lottoNumbers);
+
+		for (let i = 0; i < lottoNumbers.length; i++) {
+			Console.print(lottoNumbers[i]);
+		}
 	},
 };
 
