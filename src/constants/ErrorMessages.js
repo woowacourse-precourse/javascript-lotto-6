a0106d3cdbfc9c8cd0573815c22d64f1da4e8316
Trophy198 <spsp8426@naver.com>
2023-11-07 21:03:32 +0900

    style : ErrorMessages 상수에서 불필요한 백틱 변경

--- src/constants/ErrorMessages.js
+++ src/constants/ErrorMessages.js
@@ -7,6 +7,6 @@ export const ERROR_MESSAGES = {
   NUMBER_OUT_OF_RANGE: '[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.',
   DUPLICATE_NUMBER: '[ERROR] 당첨 번호에 중복된 숫자가 있습니다.',
   BONUS_NUMBER_DUPLICATE: '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
-  INVALID_NUMBERS_COUNT: `[ERROR] 로또 번호는 6개여야 합니다.`,
+  INVALID_NUMBERS_COUNT: '[ERROR] 로또 번호는 6개여야 합니다.',
   DUPLICATE_NUMBERS: '[ERROR] 로또 번호는 중복될 수 없습니다.',
 };
