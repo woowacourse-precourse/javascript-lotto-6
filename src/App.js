3ac1951be225a5bc7d9a887dddf2714f51d4a293
KimRosa <108081283+KimRosa@users.noreply.github.com>
2023-11-07 15:07:31 +0900

    Feat(App.js): 당첨 번호 입력
    당첨번호 입력 콘솔
    입력된 당첨 번호 오류 확인

--- src/App.js
+++ src/App.js
@@ -14,17 +14,40 @@ class App {
       await this.checkUserMoneyPossible(userMoney);
 
       const countTicket = this.countLottoTicket(userMoney);
+      const winNum = await this.getLuckyNumber();
     }
   }
+
   async checkUserMoneyPossible(userMoney) {
     if (isNaN(userMoney) || userMoney % 1000 !== 0 || userMoney < 1000) {
       throw new Error("[ERROR] 1000단위의 숫자를 입력해주세요");
     }
   }
+
   countLottoTicket(userMoney) {
     const countTicket = userMoney / 1000;
     MissionUtils.Console.print(`${countTicket}개를 구매했습니다.`);
     return countTicket;
+  }
+
+  async getLuckyNumber() {
+    const luckyNumber = await MissionUtils.Console.readLineAsync(
+      `당첨 번호를 입력해 주세요.`
+    );
+    const winNum = this.checkLuckyNumber(luckyNumber);
+    return winNum;
+  }
+
+  checkLuckyNumber(luckyNumber) {
+    const winNum = luckyNumber.split(",").map(Number);
+    if (winNum.length !== 6 || winNum.some((num) => num < 1 || num > 45)) {
+      throw new Error("[ERROR] 올바른 당첨 번호를 입력해주세요.");
+    }
+    if (winNum.some((num, index) => winNum.indexOf(num) !== index)) {
+      throw new Error("[ERROR]");
+    }
+
+    return winNum;
   }
 }
 
