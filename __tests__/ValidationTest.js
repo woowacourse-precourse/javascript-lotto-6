c3ccbd05d217276e7c5ca64f5312d84bf0c1f6e1
JunSeok <ppu1234@naver.com>
2023-11-08 00:43:58 +0900

    test: 로또 당첨 번호 유효성 검사 테스트 코드 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,5 @@
 import validateInputAmount from '../src/validations/validateInputAmount';
+import validateWinningNumbers from '../src/validations/validateWinningNumbers';
 
 const ERROR_PREFIX = '[ERROR]';
 
@@ -53,6 +54,74 @@ describe('로또 금액 유효성 검사', () => {
 
     // Act
     const resultFn = () => validateInputAmount(input);
+
+    // Assert
+    expect(resultFn).toThrow(ERROR_PREFIX);
+  });
+});
+
+describe('로또 당첨 번호 유효성 검사', () => {
+  test('1 ~ 45 사이의 정수이면서 중복되지 않은 정수 6자리일 경우 true 반환', () => {
+    // Arrange
+    const input = [1, 2, 3, 4, 5, 6];
+
+    // Act
+    const result = validateWinningNumbers(input);
+
+    // Assert
+    expect(result).toBe(true);
+  });
+
+  test('정수가 아닌 값이 포함되어 있는 경우 예외 발생', () => {
+    // Arrange
+    const input = [1, 2, 3, 4, 5, 't'];
+
+    // Act
+    const resultFn = () => validateWinningNumbers(input);
+
+    // Assert
+    expect(resultFn).toThrow(ERROR_PREFIX);
+  });
+
+  test('중복값이 포함되어 있는 경우 예외 발생', () => {
+    // Arrange
+    const input = [1, 2, 3, 4, 5, 5];
+
+    // Act
+    const resultFn = () => validateWinningNumbers(input);
+
+    // Assert
+    expect(resultFn).toThrow(ERROR_PREFIX);
+  });
+
+  test('6개의 정수가 아닌 경우 예외 발생', () => {
+    // Arrange
+    const input = [1, 2, 3, 4, 5, 6, 7];
+
+    // Act
+    const resultFn = () => validateWinningNumbers(input);
+
+    // Assert
+    expect(resultFn).toThrow(ERROR_PREFIX);
+  });
+
+  test('1 미만의 정수가 포함되어 있는 경우 예외 발생', () => {
+    // Arrange
+    const input = [-2, 1, 2, 3, 4, 5];
+
+    // Act
+    const resultFn = () => validateWinningNumbers(input);
+
+    // Assert
+    expect(resultFn).toThrow(ERROR_PREFIX);
+  });
+
+  test('45 초과의 정수가 포함되어 있는 경우 예외 발생', () => {
+    // Arrange
+    const input = [1, 2, 3, 4, 5, 50];
+
+    // Act
+    const resultFn = () => validateWinningNumbers(input);
 
     // Assert
     expect(resultFn).toThrow(ERROR_PREFIX);
