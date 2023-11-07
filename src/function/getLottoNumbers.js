8acd3248b9c3ddb170146bcaf2f048669f85d00a
limdumb <dlaruddls@nate.com>
2023-11-07 23:11:51 +0900

    fix: 출력값 Test Code에 맞춰 수정

--- src/function/getLottoNumbers.js
+++ src/function/getLottoNumbers.js
@@ -2,7 +2,7 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 export const getLottoNumbers = (count) => {
   const result = [];
-
+  Console.print(`${count}개를 구매했습니다.`);
   for (let i = 1; i <= count; i++) {
     const lottoRandomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
       1,
@@ -11,7 +11,7 @@ export const getLottoNumbers = (count) => {
     ).sort((a, b) => a - b);
 
     result.push(lottoRandomNumber);
-    Console.print(lottoRandomNumber);
+    Console.print(`[${lottoRandomNumber.join(", ")}]`);
   }
 
   return result;
