2318bf99127c180335bdcd4fb1159a278b64d7e8
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 17:40:39 +0900

    :recycle: refactor: 유효성 검증 모델 클래스화 클래스 내부에서만 사용되는 로직 분리, 변수명 및 메서드명 수정, 게임 조건에 관련된 매직넘버 상수로 연결, 주석 추가 및 문서화

--- src/domain/LottoValidator.js
+++ src/domain/LottoValidator.js
@@ -1,73 +1,80 @@
+import { LOTTO_CONDITION, PRICE_PER_TICKET } from '../constants/constants.js';
 import { ERROR_MESSAGE } from '../constants/messages.js';
 
-const LottoValidator = {
-  validLottoNumber(numbers) {
-    if (!this.isValidLength(numbers)) {
-      throw new Error(ERROR_MESSAGE.invalidLength);
-    }
-    if (!this.isNumberic(numbers)) {
-      throw new Error(ERROR_MESSAGE.invalidNumberic);
-    }
-    if (!this.isValidRangeNumber(numbers)) {
-      throw new Error(ERROR_MESSAGE.invalidRange);
-    }
-    if (!this.isValidUniqueName(numbers)) {
-      throw new Error(ERROR_MESSAGE.invalidUnique);
-    }
-  },
-
-  validPurchaseAmount(number) {
-    const numberRegExp = /^\d+$/;
-    const numberic = numberRegExp.test(number) && !Number.isNaN(number);
-
-    if (!numberic) {
+class LottoValidator {
+  // 구입금액의 유효성을 검증하고 예외처리한다. (타입, 조건, 범위)
+  validatePurchaseAmount(amount) {
+    if (!this.#isNumberic(amount)) {
       throw new Error(ERROR_MESSAGE.invalidNumberic);
     }
 
-    if (number % 1000 !== 0) {
+    if (amount % PRICE_PER_TICKET !== 0 || amount < 0) {
       throw new Error(ERROR_MESSAGE.invalidAmount);
     }
+  }
 
-    if (number < 0) {
+  // 6자리 로또 번호배열을 검증하고 예외처리한다. (길이, 타입, 범위, 중복)
+  validateLottoNumber(numbers) {
+    if (!this.#isValidLength(numbers)) {
+      throw new Error(ERROR_MESSAGE.invalidLength);
+    }
+
+    if (!this.#isEveryNumberic(numbers)) {
       throw new Error(ERROR_MESSAGE.invalidNumberic);
     }
-  },
 
-  validBonusNumber(number, isContainning) {
-    const numberRegExp = /^\d+$/;
-    const numberic = numberRegExp.test(number) && !Number.isNaN(number);
-    const range = number >= 1 && number <= 45;
+    if (!this.#isValidEveryRange(numbers)) {
+      throw new Error(ERROR_MESSAGE.invalidRange);
+    }
 
-    if (!numberic) {
+    if (!this.#isValidUniqueName(numbers)) {
+      throw new Error(ERROR_MESSAGE.invalidUnique);
+    }
+  }
+
+  /**
+   * 보너스번호의 유효성을 검증하고 예외처리한다. (타입, 범위, 중복)
+   * @param {number} bonusNumber
+   * @param {boolean} isContainning
+   */
+  validateBonusNumber(bonusNumber, isContainning) {
+    if (!this.#isNumberic(bonusNumber)) {
       throw new Error(ERROR_MESSAGE.invalidBonusNumber);
     }
 
-    if (!range) {
+    if (!this.#isValidRange(bonusNumber)) {
       throw new Error(ERROR_MESSAGE.invalidRange);
     }
 
     if (isContainning) {
       throw new Error(ERROR_MESSAGE.invalidUniqueBonusNumber);
     }
-  },
+  }
 
-  isValidLength(numbers) {
-    return numbers.length === 6;
-  },
+  #isValidLength(numbers) {
+    return numbers.length === LOTTO_CONDITION.length;
+  }
 
-  isValidRangeNumber(numbers) {
-    return numbers.every((number) => number >= 1 && number <= 45);
-  },
+  #isValidRange(number) {
+    return number >= LOTTO_CONDITION.startRange && number <= LOTTO_CONDITION.endRange;
+  }
 
-  isValidUniqueName(numbers) {
-    return new Set(numbers).size === 6;
-  },
+  #isValidEveryRange(numbers) {
+    return numbers.every((number) => this.#isValidRange(number));
+  }
 
-  isNumberic(numbers) {
-    const numberRegExp = /^\d+$/;
+  #isValidUniqueName(numbers) {
+    return new Set(numbers).size === LOTTO_CONDITION.length;
+  }
 
-    return numbers.every((number) => numberRegExp.test(number));
-  },
-};
+  #isNumberic(number) {
+    const numberRegExp = /^\d+/;
+    return numberRegExp.test(number) && !Number.isNaN(Number(number));
+  }
+
+  #isEveryNumberic(numbers) {
+    return numbers.every((number) => this.#isNumberic(number));
+  }
+}
 
 export default LottoValidator;
