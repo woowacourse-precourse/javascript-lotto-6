f5e5f569d882ffc704de55d5bdaef8f21af361a3
sehyun <e6d1fe@gmail.com>
2023-11-07 15:43:46 +0900

    validateBonusNumber 중복 검사 시 문자열 인수 받도록 수정

--- src/Input/enterBonusNumber.js
+++ src/Input/enterBonusNumber.js
@@ -3,10 +3,10 @@ import { message } from '../Constants.js';
 
 import validateBonusNumber from '../Validation/validateBonusNumber.js';
 
-async function enterBonusNumber() {
+async function enterBonusNumber(winningNumbers) {
   const bonusNumberInput = await Console.readLineAsync(message.ENTER_BONUS_NUMBER);
 
-  if (validateBonusNumber(bonusNumberInput)) {
+  if (validateBonusNumber(bonusNumberInput, winningNumbers)) {
     return Number(bonusNumberInput);
   }
 
