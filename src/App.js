68ce41dd3d8ddd2277c0fc7f15b4eea86f18a82b
hongjimin00 <chunsim015@gmail.com>
2023-11-07 14:18:10 +0900

    feat: 통계 결과 출력

--- src/App.js
+++ src/App.js
@@ -12,20 +12,23 @@ class App {
 
     const lottoList = this.buyLotto(lottoAmount);
 
-    Console.print(`${lottoList.length}개를 구매했습니다`);
+    Console.print(`${lottoList.length}개를 구매했습니다.`);
     for (let i = 0; i < lottoList.length; i++) {
       Console.print(lottoList[i].getLottoNumbers().sort((a, b) => a - b));
     }
 
     const winningNumber = (
       await Console.readLineAsync("당첨 번호를 입력해 주세요.")
-    ).split(",");
+    )
+      .split(",")
+      .map(Number);
 
     const bonusNumber = await Console.readLineAsync(
       "보너스 번호를 입력해 주세요."
     );
 
     const result = this.getResult(lottoList, winningNumber, bonusNumber);
+    this.getStatistics(lottoAmount, winningNumber, bonusNumber, result);
   }
 
   buyLotto(amount) {
@@ -71,6 +74,46 @@ class App {
     }
 
     return result;
+  }
+
+  getStatistics(lottoAmount, winningNumber, bonusNumber, result) {
+    const prizeList = {
+      n3: 5000,
+      n4: 50000,
+      n5: 1500000,
+      n5b: 30000000,
+      n6: 200000000,
+    };
+
+    let totalPrize = 0;
+
+    Console.print("당첨 통계");
+    Console.print("---");
+
+    for (const key in result) {
+      const count = result[key];
+      const prize = prizeList[key];
+
+      switch (key) {
+        case "n3":
+        case "n4":
+        case "n5":
+        case "n6":
+          Console.print(`${key}개 일치 (${prize}원 - ${count}개)`);
+          totalPrize += count * prize;
+          break;
+        case "n5b":
+          Console.print(
+            `${key}개 일치, 보너스 불 일치 (${prize}원 - ${count}개)`
+          );
+          totalPrize += count * prize;
+          break;
+      }
+    }
+
+    const returnRate = ((totalPrize - lottoAmount) / lottoAmount) * 100;
+
+    Console.print(`총 수익률은 ${returnRate.toFixed(1)}%입니다.`);
   }
 }
 
