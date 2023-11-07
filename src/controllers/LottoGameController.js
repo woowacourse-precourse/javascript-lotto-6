99a001ec7e2530f6a1f42bec73d45a1215dbf210
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 18:43:11 +0900

    :recycle: refactor: 컨트롤러 게임의 제어흐름 분리 초기에 필요한 사용자의 입력을 받는 부분과 결과표시에 해당하는 기능메서드로 분리함

--- src/controllers/LottoGameController.js
+++ src/controllers/LottoGameController.js
@@ -14,24 +14,33 @@ class LottoGameController {
   }
 
   async startGame() {
-    const amount = await this.requestValidPurchaseAmount();
+    const amount = await this.#requestValidPurchaseAmount();
     this.#lottoGame.initializeLotto(amount);
+
     const purchasedLotto = this.#lottoGame.getPurchasedLotto();
+
     this.#outputView.printPurchaseAmount(amount);
     this.#outputView.printPurchasedLotto(purchasedLotto);
-    const winningLotto = await this.requestValidWinningLotto();
-    const bonusNumber = await this.requestValidBonusNumber(winningLotto);
+
+    const winningLotto = await this.#requestValidWinningLotto();
+    const bonusNumber = await this.#requestValidBonusNumber(winningLotto);
+
+    this.#finishGame(winningLotto, bonusNumber);
+  }
+
+  #finishGame(winningLotto, bonusNumber) {
     const comparisonResults = this.#lottoGame.getComparisonResults(
       winningLotto.getSortedNumbers(),
       bonusNumber
     );
     const winsStatistics = this.#lottoGame.getStatistics(comparisonResults);
-    this.#outputView.printWinsStatistics(winsStatistics);
     const totalPrizeAmount = this.#lottoGame.getTotalPrizeAmount(winsStatistics);
+
+    this.#outputView.printWinsStatistics(winsStatistics);
     this.#outputView.printProfitRatio(this.#lottoGame.getProfitRatio(totalPrizeAmount));
   }
 
-  async requestValidPurchaseAmount() {
+  async #requestValidPurchaseAmount() {
     try {
       const inputPurchaseAmount = await this.#inputView.getPurchaseAmount();
       this.#validator.validatePurchaseAmount(inputPurchaseAmount);
@@ -40,11 +49,11 @@ class LottoGameController {
     } catch ({ message }) {
       this.#outputView.printError(message);
 
-      return await this.requestValidPurchaseAmount();
+      return await this.#requestValidPurchaseAmount();
     }
   }
 
-  async requestValidWinningLotto() {
+  async #requestValidWinningLotto() {
     try {
       const winningNumbers = await this.#inputView.getWinningNumbers();
       const numbers = winningNumbers.split(',').map(Number);
@@ -53,11 +62,11 @@ class LottoGameController {
     } catch ({ message }) {
       this.#outputView.printError(message);
 
-      return await this.requestValidWinningLotto();
+      return await this.#requestValidWinningLotto();
     }
   }
 
-  async requestValidBonusNumber(winningLotto) {
+  async #requestValidBonusNumber(winningLotto) {
     try {
       const bonusNumber = await this.#inputView.getBonusNumber();
       const isContaining = winningLotto.hasContainBonusNumber(parseInt(bonusNumber, 10));
@@ -67,7 +76,7 @@ class LottoGameController {
     } catch ({ message }) {
       this.#outputView.printError(message);
 
-      return await this.requestValidBonusNumber(winningLotto);
+      return await this.#requestValidBonusNumber(winningLotto);
     }
   }
 }
