9c9d27651980de1f81face305658ed13778d96c9
mmeat512 <wndus0958@naver.com>
2023-11-08 03:43:05 +0900

    refactor: initialResults 추가 필요없는 함수 제거

--- src/service/LottosResults.js
+++ src/service/LottosResults.js
@@ -4,54 +4,59 @@ import LottoCompare from './LottoCompare.js';
 class LottosResults {
   #lottoCompare;
   #lottos;
+  #initialResults;
 
   constructor(lottos, winningNumbers, bonusNumber) {
     this.#lottos = lottos;
     this.#lottoCompare = new LottoCompare(winningNumbers, bonusNumber);
+    this.#initialResults = this.#getInitialResults();
   }
 
-  #getCountLottoRanks(rank) {
-    const lottosRanks = this.getLottosRanks();
-
-    return lottosRanks.filter((lottoRank) => lottoRank === rank).length;
-  }
-
-  getLottoRank(includesCount, hasBonusNumber) {
-    const { fifth, fourth, second, first } = LOTTO_RESULT;
-    const [firstRank, secondRank, thirdRank, fourthRank, fifthRank] = Object.keys(LOTTO_RESULT);
-
-    if (fifth.includesCount === includesCount) return fifthRank;
-
-    if (fourth.includesCount === includesCount) return fourthRank;
-
-    if (second.includesCount === includesCount) {
-      if (hasBonusNumber) return secondRank;
-
-      return thirdRank;
-    }
-
-    if (first.includesCount === includesCount) return firstRank;
-  }
-
-  getLottosRanks() {
-    return this.#lottos.map((lotto) => {
-      const includesCount = this.#lottoCompare.getCountIncludesWinningNumbers(lotto);
-      const hasBonusNumber = this.#lottoCompare.hasBonusNumber(lotto);
-
-      return this.getLottoRank(includesCount, hasBonusNumber);
-    });
-  }
-
-  getLottosResultsCount() {
-    const ranks = Object.keys(LOTTO_RESULT);
-
-    return ranks.reduce(
-      (acc, cur) => ({
+  #getInitialResults() {
+    return Object.keys(LOTTO_RESULT).reduce(
+      (acc, key) => ({
         ...acc,
-        [cur]: this.#getCountLottoRanks(cur),
+        [key]: 0,
       }),
       {},
     );
+  }
+
+  getLottoRank(includesCount, hasBonusNumber) {
+    const [first, second, third, fourth, fifth] = Object.keys(LOTTO_RESULT);
+
+    if (LOTTO_RESULT[fifth].includesCount === includesCount) return fifth;
+
+    if (LOTTO_RESULT[fourth].includesCount === includesCount) return fourth;
+
+    if (LOTTO_RESULT[second].includesCount === includesCount) {
+      if (hasBonusNumber) return second;
+
+      return third;
+    }
+
+    if (LOTTO_RESULT[first].includesCount === includesCount) return first;
+  }
+
+  getLottosRanks() {
+    return this.#lottos
+      .map((lotto) => {
+        const includesCount = this.#lottoCompare.getCountIncludesWinningNumbers(lotto);
+        const hasBonusNumber = this.#lottoCompare.hasBonusNumber(lotto);
+
+        return this.getLottoRank(includesCount, hasBonusNumber);
+      })
+      .filter((lottoRank) => lottoRank);
+  }
+
+  getLottosResultsCount() {
+    const lottosRanks = this.getLottosRanks();
+
+    lottosRanks.forEach((lottoRank) => {
+      this.#initialResults[lottoRank] += 1;
+    });
+
+    return this.#initialResults;
   }
 }
 
