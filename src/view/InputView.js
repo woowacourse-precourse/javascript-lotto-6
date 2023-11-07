a45d9a87df429febfe8aa0bc161d68f9f20bdff3
jjinheeWorld <wlsgml450@naver.com>
2023-11-07 21:20:35 +0900

    ✨ Feat: 로또 당첨 번호 입력 기능 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -4,6 +4,20 @@ class InputView {
   static async readLottoAmount() {
     return Console.readLineAsync('구입금액을 입력해 주세요.\n');
   }
+
+  static async readLottoWinningNumbers() {
+    const winningNumbers = await Console.readLineAsync(
+      '당첨 번호를 입력해 주세요.\n'
+    );
+
+    const winningNumbersList = winningNumbers
+      .split(',')
+      .map((number) => parseInt(number.trim()));
+
+    console.log(winningNumbersList);
+
+    return winningNumbersList;
+  }
 }
 
 export default InputView;
