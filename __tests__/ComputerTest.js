db0314cda071b9443dcbcfddf22e3a3fda1c08a6
JungHun98 <j7papa@naver.com>
2023-11-07 14:07:05 +0900

    test: 컴퓨터 클래스 생성자 인자 변경 사항 반영

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -14,9 +14,10 @@ describe('로또 컴퓨터 클래스 테스트', () => {
     [2, 13, 22, 32, 38, 45],
     [1, 3, 5, 14, 22, 45],
   ];
+  const cost = 8000;
 
   test('생성된 로또들의 번호와 당첨 번호, 보너스 번호를 비교한 결과를 최종 산출에 반영한다.', () => {
-    const computer = new Computer(winnnersNumbers, bonusNumber);
+    const computer = new Computer(winnnersNumbers, bonusNumber, cost);
     const lottos = numbers.map((elem) => new Lotto(elem));
     computer.setPrizeResult(lottos);
 
@@ -30,7 +31,7 @@ describe('로또 컴퓨터 클래스 테스트', () => {
   });
 
   test('최종 산출된 등수의 결과를 바탕으로 수익률을 계산한다.', () => {
-    const computer = new Computer(winnnersNumbers, bonusNumber);
+    const computer = new Computer(winnnersNumbers, bonusNumber, cost);
     const lottos = numbers.map((elem) => new Lotto(elem));
     computer.setPrizeResult(lottos);
 
