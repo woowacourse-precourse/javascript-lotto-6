6cdea90765b3d174056b123584928104a07857ba
p0cari <pocari3434@gmail.com>
2023-11-07 22:44:07 +0900

    시작점인 App.js에 부모 컨트롤러 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import GameManage from './mvc/controller/parent/game_manage';
+
 class App {
-  async play() {}
+  async play() {
+    const GAME_MANAGE = new GameManage();
+    await GAME_MANAGE.gameManage();
+  }
 }
 
 export default App;
