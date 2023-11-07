51e838a4a3e6cd60f301bced8f7b21d12eccb738
chorong <chorong4641@naver.com>
2023-11-08 00:32:48 +0900

    feat(statistics): 당첨 통계 출력 기능 구현

--- src/LottoOutput.js
+++ src/LottoOutput.js
@@ -2,6 +2,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { MESSAGES } from './Messages.js';
 
+
 export function printLottoAmount(amount) {
   MissionUtils.Console.print(`\n${amount}${MESSAGES.LOTTO_BUY_AMOUNT_OUTPUT}`);
 }
@@ -10,4 +11,23 @@ export function printLottoNumbers(numbers) {
   for (let index = 0; index < numbers.length; index += 1) {
     MissionUtils.Console.print(numbers[index]);
   }
+}
+
+export function printLottoStatistics(matchedCounts) {
+  const winningCountData = {
+    [MESSAGES.MATCH_THREE]: { text: "3개 일치 (5,000원)", value: 0 },
+    [MESSAGES.MATCH_FOUR]: { text: "4개 일치 (50,000원)", value: 0 },
+    [MESSAGES.MATCH_FIVE]: { text: "5개 일치 (1,500,000원)", value: 0 },
+    [MESSAGES.MATCH_BONUS]: { text: "5개 일치, 보너스 볼 일치 (30,000,000원)", value: 0 },
+    [MESSAGES.MATCH_SIX]: { text: "6개 일치 (2,000,000,000원)", value: 0 },
+  };
+
+  MissionUtils.Console.print(`\n${MESSAGES.LOTTO_STATISTICS}`);
+  MissionUtils.Console.print('---');
+
+  matchedCounts.forEach((count) => {
+    if (winningCountData[count.toString()]) winningCountData[count.toString()].value += 1;
+  });
+  const result = Object.keys(winningCountData).sort().map(key => `${winningCountData[key].text} - ${winningCountData[key].value}개`);
+  MissionUtils.Console.print(result);
 }
