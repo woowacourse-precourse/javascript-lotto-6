dc768228c2dcab2ae327f8060781a001447bdc76
iamreum <102291571+iamreum@users.noreply.github.com>
2023-11-07 18:54:12 +0900

    예외 분리 및 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,38 +1,30 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
 
-/**
- * 사용자에게 당첨 번호 입력받기
- */
-
 class Lotto {
   #numbers;
+
+  static PRICE = 1000;
 
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  /**
-   * 사용자에게 입력받는 당첨 번호 에러 처리
-   */
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (numbers.some(isNaN) || numbers.some(number => number < 1 || number > 45)) {
+    const isNotInRange = number => number < 1 || number > 45;
+    if (numbers.some(isNaN)) {
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
+    if (numbers.some(isNotInRange)) {
       throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
-    const uniqueNumbers = new Set(numbers);
-    if (uniqueNumbers.size !== numbers.length) {
-      throw new Error("[ERROR] 중복된 숫자는 입력할 수 없습니다.")
+    const hasDuplicates = new Set(numbers).size !== numbers.length;
+    if (hasDuplicates) {
+      throw new Error("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
     }
-  }
-
-  /**
-   * 1에서 45 사이의 중복되지 않는 숫자 6개 출력
-   */
-  getRandomLottoNumbers() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
   }
 }
 
