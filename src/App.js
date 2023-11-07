6e9ed3161d883bd7bed8c3b891d4952dea5ad5fd
JeongSabibi <jsbb528@naver.com>
2023-11-08 02:30:35 +0900

    feat: 기능 목록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console } from '@woowacourse/mission-utils';
+import LottoGame from './LottoGame.js';
+import Lotto from './Lotto.js';
+
 class App {
-  async play() {}
+  async play() {
+      const lottoGame = new LottoGame();
+      await lottoGame.buyLotto();
+
+      Console.print(`\n${lottoGame.lottoPieces}개를 구매했습니다.`);
+
+      for (let i = 1; i <= lottoGame.lottoPieces; i++)
+        Console.print(lottoGame.makeRandomNumber());
+
+      const winningNumber = await lottoGame.inputLottoNumber();
+
+      const lotto = new Lotto(winningNumber);
+
+      const winningBonus = await lottoGame.inputBonusNumber();
+
+      lotto.win(winningBonus, lottoGame.lottoNumbers);
+  }
 }
 
 export default App;
