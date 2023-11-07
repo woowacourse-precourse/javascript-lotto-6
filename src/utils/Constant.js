56e90b4395332e2143b29e6c2548ce13d3ff1c4a
abi-hong <flffjakfwm20@gmail.com>
2023-11-07 15:09:16 +0900

    refactor(Constant): 입력, 예외 값 상수화

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,7 +1,7 @@
 export const RANKING = {
-  0: null,
-  1: null,
-  2: null,
+  0: 0,
+  1: 0,
+  2: 0,
   3: 5,
   4: 4,
   5: (hasBonus) => (hasBonus ? 2 : 3),
@@ -15,4 +15,16 @@ export const REWARD = {
   4: 50000,
   5: 5000,
   6: 0,
+}
+
+export const MESSAGE = {
+  price: '구입할 금액을 알려주세요.\n',
+  win: '\n당첨 번호를 입력해 주세요.\n',
+  bonus: '\n보너스 번호를 입력해 주세요.\n',
+}
+
+export const EXCEPTION = {
+  money: '[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.',
+  sixNumber: '[ERROR] 당첨 번호는 중복되지 않는 숫자 6개를 입력해야 합니다.',
+  isNumber: '[ERROR] 보너스 번호는 1 ~ 45사이의 숫자 1개를 입력해야 합니다.',
 }
