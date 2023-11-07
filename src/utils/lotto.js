3a1f75975692ccd732d12d57385de23ec19276e5
intersoom <sallybig@naver.com>
2023-11-08 04:40:30 +0900

    feat: 로또 수량만큼 발행 및 오름차순 정렬

--- src/utils/lotto.js
+++ src/utils/lotto.js
@@ -1,5 +1,19 @@
+import { Random } from "@woowacourse/mission-utils";
 import LOTTO from "../constant/lottoData.js";
+import Lotto from "../class/Lotto.js";
 
 export const paymentToLottoCount = (payment) => {
   return payment / LOTTO.COST;
+};
+
+export const generateLottos = (lottoCount) => {
+  const lottos = [];
+
+  Array.from({ length: lottoCount }).map((_) => {
+    const lotto = Random.pickUniqueNumbersInRange(1, 45, 6);
+    lotto.sort((a, b) => a - b);
+    lottos.push(new Lotto(lotto));
+  });
+
+  return lottos;
 };
