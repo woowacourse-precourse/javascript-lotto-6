ae8fcff24adf5119171f0e61cfc3b203ba8f97e2
youjeong Jeong <wjddbwjd1205@naver.com>
2023-11-08 04:42:25 +0900

    refactor: 예외사항 처리를 객체를 생성할 때 할 수 있도록 리펙터링

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,11 +10,21 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    
+
     const termNumbers = [];
     numbers.forEach(number => {
+      number = Number(number);
+      if (isNaN(number)) {
+        throw new Error("[ERROR] 숫자가 아닌 입력이 있습니다.")
+      }
+      if (!Number.isInteger(number)) {
+        throw new Error("[ERROR] 정수가 아닌 입력이 있습니다.")
+      }
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 1부터 45 사이의 숫자가 아닌 입력이 있습니다.")
+      }
       if (termNumbers.includes(number)) {
-        throw new Error("[ERROR]");
+        throw new Error("[ERROR] 중복된 숫자가 있습니다.")
       }
 
       termNumbers.push(number);
