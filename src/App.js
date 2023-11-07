3b5b5858ccae91748765c934889f2d4f0fed8ac5
osohyun0224 <osohyun0224@naver.com>
2023-11-08 08:43:01 +0900

    refactor(LottoController): 게임 로직을 LottoController로 분리
    - App 클래스는 애플리케이션의 진입점 역할만 수행하고, 게임의 전체 흐름은 LottoController가 관리하도록 하였습니다.
    - LottoController를 생성하여 게임의 전체적인 흐름을 진행하도록 책임을 부여하였습니다.
    -

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import InputView from "./views/InputView.js";
-import OutputView from "./views/OutputView.js";
-import GenerateLottoNumbers from "./models/GenerateLottoNumbers.js";
-import LottoResultCalculator from "./models/LottoResultCalculator.js";
-import ProfitCalculator from "./models/ProfitCalculator.js";
+import LottoController from "./controller/LottoController"
 
 class App {
   async play() {
-    OutputView.printInsertMoneyMessage();
-    const purchasePrice = await InputView.getPurchasePrice();
-    
-    const lottoCount = parseInt(purchasePrice / 1000);
-    OutputView.printLottoCount(lottoCount);
-
-    const lottos = GenerateLottoNumbers.getLottos(lottoCount);
-    OutputView.printLottos(lottos);
-
-    OutputView.printInsertWinningNumbersMessage();
-    const winningNumbers = await InputView.getWinningNumbers(); 
-
-    OutputView.printInsertBonusNumberMessage();
-    const bonusNumber = await InputView.getBonusNumber(winningNumbers);
-
-    const game = new LottoResultCalculator(lottos);
-    const result = game.getResult(winningNumbers, bonusNumber);
-
-    const totalPrize = ProfitCalculator.calculateTotalPrize(result);
-    const roi = ProfitCalculator.calculateROI(totalPrize, purchasePrice);
-
-    OutputView.printResult(roi, result);
-}
-
+    const lottoController = new LottoController();
+    await lottoController.playGame();
+  }
 }
 
 export default App;
---
+++ src/controller/LottoController.js
@@ -0,0 +1,58 @@
+import InputView from "../views/InputView.js"
+import OutputView from "../views/OutputView.js";
+import GenerateLottoNumbers from "../models/GenerateLottoNumbers.js";
+import LottoResultCalculator from "../models/LottoResultCalculator.js";
+import ProfitCalculator from "../models/ProfitCalculator.js";
+
+class LottoController {
+  #purchasePrice;
+  #lottos;
+  #winningNumbers;
+  #bonusNumber;
+  #result;
+  #roi;
+
+  async playGame() {
+    await this.#inputPurchasePrice();
+    this.#generateLottos();
+    await this.#inputWinningNumbers();
+    await this.#inputBonusNumber();
+    this.#calculateResult();
+    this.#displayResult();
+  }
+
+  async #inputPurchasePrice() {
+    OutputView.printInsertMoneyMessage();
+    this.#purchasePrice = await InputView.getPurchasePrice();
+  }
+
+  #generateLottos() {
+    const lottoCount = parseInt(this.#purchasePrice / 1000);
+    OutputView.printLottoCount(lottoCount);
+    this.#lottos = GenerateLottoNumbers.getLottos(lottoCount);
+    OutputView.printLottos(this.#lottos);
+  }
+
+  async #inputWinningNumbers() {
+    OutputView.printInsertWinningNumbersMessage();
+    this.#winningNumbers = await InputView.getWinningNumbers();
+  }
+
+  async #inputBonusNumber() {
+    OutputView.printInsertBonusNumberMessage();
+    this.#bonusNumber = await InputView.getBonusNumber(this.#winningNumbers);
+  }
+
+  #calculateResult() {
+    const game = new LottoResultCalculator(this.#lottos);
+    this.#result = game.getResult(this.#winningNumbers, this.#bonusNumber);
+    const totalPrize = ProfitCalculator.calculateTotalPrize(this.#result);
+    this.#roi = ProfitCalculator.calculateROI(totalPrize, this.#purchasePrice);
+  }
+
+  #displayResult() {
+    OutputView.printResult(this.#roi, this.#result);
+  }
+}
+
+export default LottoController;
