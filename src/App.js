9c47994af0fa1646439da993bbce9990424561e3
stupidJoon <junsang.yu3@gmail.com>
2023-11-07 19:38:58 +0900

    feat: add get lottoMatches and print

--- src/App.js
+++ src/App.js
@@ -28,6 +28,26 @@ class App {
     Console.print('');
     Console.print('보너스 번호를 입력해 주세요.');
     const bonus = Number(await Console.readLineAsync(''));
+
+    Console.print('당첨 통계');
+    Console.print('---');
+
+    const lottoMatches = Object.fromEntries(['3', '4', '5', '5-b', '6'].map(k => [k, 0]));
+    lottos.forEach((lotto) => {
+      let lottoMatch = lotto.filter((lottoNumber) => winningNumbers.includes(lottoNumber)).length;
+
+      if (lottoMatch === 5 && lotto.includes(bonus)) {
+        lottoMatch = '5-b';
+      }
+
+      lottoMatches[lottoMatch] += 1;
+    });
+
+    Console.print(`3개 일치 (5,000원) - ${lottoMatches[3]}개`);
+    Console.print(`4개 일치 (50,000원) - ${lottoMatches[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${lottoMatches[5]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoMatches['5-b']}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${lottoMatches[6]}개`);
   }
 }
 
