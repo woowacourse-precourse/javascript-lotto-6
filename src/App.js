26aa9f53e70b51e6cd842431f3172360622256f4
육종호 <dbrwhdgh1203@naver.com>
2023-11-08 03:04:48 +0900

    feat: Add funct buylotto & inputwinningNum to controller

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Controller from './Controller/Controller.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.controller = new Controller();
+    // this.play();
+  }
+
+  async play() {
+    await this.controller.buyLotto();
+    await this.controller.inputWinningNum();
+  }
 }
+
+const app = new App();
+await app.play();
 
 export default App;
