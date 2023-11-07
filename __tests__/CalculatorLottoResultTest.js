7a286c7f7f7d829b5e01e355141488db06420e75
teawon <xodnjsvkvk9@gmail.com>
2023-11-08 04:33:00 +0900

    test : CalculatorLottoResultTest 도메인의 수익률 계산 로직 추가

--- __tests__/CalculatorLottoResultTest.js
+++ __tests__/CalculatorLottoResultTest.js
@@ -11,7 +11,7 @@ describe("로또 결과 계산 클래스 테스트", () => {
     const lotto2 = new Lotto([2, 3, 4, 5, 6, 7]);
     user.buyLotto(lotto1, 1000);
     user.buyLotto(lotto2, 1000);
-    const targetLotto = new TargetLotto([1, 2, 3, 4, 5, 6], 7);
+    const targetLotto = new TargetLotto([1, 2, 3, 40, 41, 42], 7);
 
     calCulatorLottoResult = new CalculatorLottoResult(user, targetLotto);
   });
@@ -21,7 +21,7 @@ describe("로또 결과 계산 클래스 테스트", () => {
     const lottoWinningCounts = calCulatorLottoResult.getLottoWinningCount();
 
     //then
-    expect(lottoWinningCounts).toEqual([1, 1, 0, 0, 0]);
+    expect(lottoWinningCounts).toEqual([0, 0, 0, 0, 1]);
   });
 
   test("로또 당첨금액의 총액을 반환한다.", () => {
@@ -29,9 +29,21 @@ describe("로또 결과 계산 클래스 테스트", () => {
     const lottoWinningCounts = calCulatorLottoResult.getLottoWinningCount();
 
     //when
-    const reulst = calCulatorLottoResult.getTotalProfit(lottoWinningCounts);
+    const profit = calCulatorLottoResult.getTotalProfit(lottoWinningCounts);
 
     //then
-    expect(reulst).toEqual(2030000000);
+    expect(profit).toEqual(5000);
+  });
+
+  test("사용금액에 대한 당첨금액의 수익률을 소수점 둘째 자리에서 반올림해 반환한다.", () => {
+    //given
+    const lottoWinningCounts = calCulatorLottoResult.getLottoWinningCount();
+    const profit = calCulatorLottoResult.getTotalProfit(lottoWinningCounts);
+
+    //when
+    const totalProfitRate = calCulatorLottoResult.getTotalProfitRate(profit);
+
+    //then
+    expect(totalProfitRate).toEqual(250);
   });
 });
