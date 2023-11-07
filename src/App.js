a6c4e24c6b651af291da0faa1bf5cb5a675f5193
정민규 <jmg081005@naver.com>
2023-11-07 14:04:39 +0900

    feat) 로또생성 & 출력

--- src/App.js
+++ src/App.js
@@ -11,20 +11,27 @@ class App {
     }
   }
 
-  async getLottoCnt(money) {
-    return ~~(+money / 1000);
+  async changeMoneyToLottoCnt(money) {
+    const lottoCnt = ~~(+money / 1000);
+
+    MissionUtils.Console.print(`${lottoCnt}개를 구매했습니다.`);
+    return lottoCnt;
   }
 
   async getUserMoney() {
     const input = await this.getInput("구입금액을 입력해 주세요.");
     if (+input % 1000 !== 0)
       throw new error("[ERROR] 돈은 천원 단위로 입력해주세요");
-    return this.getLottoCnt(input);
+    return this.changeMoneyToLottoCnt(input);
   }
 
   async play() {
     const lottoCnt = await this.getUserMoney();
-    console.log(lottoCnt);
+
+    for (let i = 0; i < lottoCnt; i++) {
+      const lotto = new Lotto();
+      lotto.printLottoNum();
+    }
   }
 }
 
