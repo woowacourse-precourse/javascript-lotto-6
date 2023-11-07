d18d93ca91933ad6d8dc2558ea1b462f035eafe4
suyeon <moa0823@naver.com>
2023-11-08 01:07:48 +0900

    test : 로또 구매 테스트
    - 구입 금액이 1000원 단위가 아닌 경우/양수가 아닌 경우/숫자가 아닌 경우 예외 테스트 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 import Lotto from "../src/Lotto.js";
+import User from "../src/User.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -7,12 +8,32 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
+});
 
-  // 아래에 추가 테스트 작성 가능
+describe("로또 구매 테스트", () => {
+  test("구입 금액이 1000원 단위가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      const user = new User();
+      user.validate(500);
+    }).toThrow("[ERROR]");
+  });
+
+  test("구입 금액이 양수가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      const user = new User();
+      user.validate(-1000);
+    }).toThrow("[ERROR]");
+  });
+
+  test("구입 금액이 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      const user = new User();
+      user.validate("hi");
+    }).toThrow("[ERROR]");
+  });
 });
