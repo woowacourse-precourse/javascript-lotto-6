3ca584d5f1715936c80c9b67e5b14700827bf30e
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-07 20:45:52 +0900

    test: 보너스 번호 입력 테스트 코드 작성
    InputViewTest.js 내에 구현:
    - 아무 입력을 안하고 엔터 누르면 예외 발생
    - 숫자 내 공백 존재할 때 예외 발생
    - 문자(공백 포함) 입력할 때 예외 발생
    - 1 ~ 45 숫자가 아닌 경우 예외 발생

--- __tests__/view/InputViewTest.js
+++ __tests__/view/InputViewTest.js
@@ -1,4 +1,3 @@
-import { Console } from "@woowacourse/mission-utils";
 import InputView from "../../src/view/InputView.js";
 import { INPUT_ERROR } from "../../src/model/Constants.js";
 
@@ -14,8 +13,8 @@ const mockQuestions = (inputs) => {
 
 describe("로또 구입 금액 입력 테스트", () => {
   const nullMoney = "";
-  const emptyMoney = ",";
   const blankMoney = "14 000";
+  const charMoney = "14a00";
   const indivisibleMoney = "14999";
   const validMoney = "14000";
 
@@ -28,15 +27,15 @@ describe("로또 구입 금액 입력 테스트", () => {
   });
 
   test("숫자 내 공백 존재할 때 예외 발생", async () => {
-    mockQuestions([emptyMoney]);
+    mockQuestions([blankMoney]);
 
     await expect(InputView.receiveMoney()).rejects.toThrow(
-      `${INPUT_ERROR.empty}`
+      `${INPUT_ERROR.blank}`
     );
   });
 
   test("문자(공백 포함) 입력할 때 예외 발생", async () => {
-    mockQuestions([blankMoney]);
+    mockQuestions([charMoney]);
 
     await expect(InputView.receiveMoney()).rejects.toThrow(
       `${INPUT_ERROR.char}`
@@ -141,5 +140,53 @@ describe("당첨 번호 입력 테스트", () => {
     const result = await InputView.receiveNumbers();
 
     expect(result).toEqual(validNumbers);
+  });
+});
+
+describe("보너스 번호 입력 테스트", () => {
+  const nullNumber = "";
+  const blankNumber = "3 4";
+  const charNumber = "3a";
+  const outOfRangeNumber = "56";
+  const validNumber = "34";
+
+  test("아무 입력을 안하고 엔터 누르면 예외 발생", async () => {
+    mockQuestions([nullNumber]);
+
+    await expect(InputView.receiveBonusNumber()).rejects.toThrow(
+      `${INPUT_ERROR.null}`
+    );
+  });
+
+  test("숫자 내 공백 존재할 때 예외 발생", async () => {
+    mockQuestions([blankNumber]);
+
+    await expect(InputView.receiveBonusNumber()).rejects.toThrow(
+      `${INPUT_ERROR.blank}`
+    );
+  });
+
+  test("문자(공백 포함) 입력할 때 예외 발생", async () => {
+    mockQuestions([charNumber]);
+
+    await expect(InputView.receiveBonusNumber()).rejects.toThrow(
+      `${INPUT_ERROR.char}`
+    );
+  });
+
+  test("1 ~ 45 숫자가 아닌 경우 예외 발생", async () => {
+    mockQuestions([outOfRangeNumber]);
+
+    await expect(InputView.receiveBonusNumber()).rejects.toThrow(
+      `${INPUT_ERROR.outOfRange}`
+    );
+  });
+
+  test("올바른 보너스 번호를 입력", async () => {
+    mockQuestions([validNumber]);
+
+    const result = await InputView.receiveBonusNumber();
+
+    expect(result).toEqual(validNumber);
   });
 });
