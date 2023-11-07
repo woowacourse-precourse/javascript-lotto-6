b4e7f7d26113d0ddad80a39093b9beeaed4064ef
Jiwon Lee <giwon.yi339@gmail.com>
2023-11-07 22:51:15 +0900

    feat: 로또 생성 번호 정렬

--- src/LottoPlay.js
+++ src/LottoPlay.js
@@ -3,26 +3,33 @@ import { ConsoleInput } from "./ConsoleInput.js";
 import { ConsoleOutput } from "./ConsoleOutput.js";
 import Lotto from "./Lotto.js";
 import { validateLottoPrice } from "./validation/validation.js";
+import { LOTTO_RULES } from "./constants/constants.js";
 
 class LottoPlay {
   async startGame() {
     // 로또 금액 입력 함수
     await this.inputLottoPrice();
     await this.inputLottoResult();
+    await this.inputBonus();
   }
 
   async inputLottoPrice() {
     let lottoPrice = await ConsoleInput.inputLottoPrice();
     // 1000 단위 검사, 1000부터 입력 가능
     validateLottoPrice(lottoPrice);
-    const price = lottoPrice / 1000;
+    const price = lottoPrice / LOTTO_RULES.PRICE;
     await ConsoleOutput.lottoPurchasedMessage(price);
     await this.generateLotto(price);
   }
 
   async generateLotto(quantity) {
     for (let i = 0; i < quantity; i++) {
-      const generate = await Random.pickUniqueNumbersInRange(1, 45, 6);
+      let generate = await Random.pickUniqueNumbersInRange(
+        LOTTO_RULES.START,
+        LOTTO_RULES.END,
+        LOTTO_RULES.LENGTH
+      );
+      generate.sort((a, b) => a - b);
       const stringifyNums = `[${generate.join(", ")}]`;
       await ConsoleOutput.generatedLotto(stringifyNums);
     }
@@ -33,6 +40,10 @@ class LottoPlay {
     // NaN, 범위, 중복 검사
     new Lotto(result);
     await ConsoleOutput.lottoNumberList(result);
+  }
+
+  async inputBonus() {
+    await ConsoleInput.inputBonusNumber();
   }
 }
 export default LottoPlay;
