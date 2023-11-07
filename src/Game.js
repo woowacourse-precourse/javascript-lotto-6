dd1eface7077834be0d606fb35665c1be2ded2a0
Y.W KIM <kyw624@gmail.com>
2023-11-07 18:34:14 +0900

    Refactor: 반환된 Lotto 인스턴스를 직접 담도록 수정

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,30 @@
-import { Random } from '@woowacourse/mission-utils';
+import { Random, Console } from '@woowacourse/mission-utils';
 
 import App, { MESSAGES } from './App.js';
 import Lotto from './Lotto.js';
+
+export const RANK = Object.freeze({
+  _5th: '5th',
+  _4th: '4th',
+  _3rd: '3rd',
+  _2nd: '2nd',
+  _1st: '1st',
+});
 
 class Game {
   constructor(amount) {
     this.amount = amount;
     this.tickets = [];
+    this.drawInfo = {
+      [RANK._5th]: { count: 0, reward: 5000 },
+      [RANK._4th]: { count: 0, reward: 50000 },
+      [RANK._3rd]: { count: 0, reward: 1500000 },
+      [RANK._2nd]: { count: 0, reward: 30000000 },
+      [RANK._1st]: { count: 0, reward: 2000000000 },
+    };
   }
 
-  async getLottoTickets() {
+  getLottoTickets() {
     const ticketNumber = this.amount / 1000;
 
     for (let i = 0; i < ticketNumber; i++) {
@@ -18,7 +33,7 @@ class Game {
       );
       const ticket = new Lotto(randomNumbers);
 
-      this.tickets.push(ticket.printNumbers());
+      this.tickets.push(ticket);
     }
   }
 }
