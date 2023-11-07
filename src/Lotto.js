4b802cc2d40f994a1cfce5d8a2f2d4e7d9927cd7
songudeum <songudeum@gmail.com>
2023-11-07 15:42:08 +0900

    feat: 로또 클래스 validate항목 추가 완료

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,26 @@
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    constructor(numbers) {
+        this.#validate(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  // TODO: 추가 기능 구현
+    #validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        const invalidLottoNumberArr = numbers.filter((el) => el > 45 || el < 1);
+        if (invalidLottoNumberArr.length !== 0) {
+            throw new Error("[ERROR] 로또 번호는 1~45의 숫자여야 합니다.");
+        }
+        const isLottoNumberUnique = numbers.every(
+            (el, idx, arr) => arr.indexOf(el) === idx
+        );
+        if (!isLottoNumberUnique) {
+            throw new Error("[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.");
+        }
+    }
 }
 
 export default Lotto;
