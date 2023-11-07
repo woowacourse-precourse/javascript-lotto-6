d147d14d7d697455e1576cdbc7cbc07a84eb34e9
starcradle101 <dreamerdev12@gmail.com>
2023-11-08 05:23:29 +0900

    ♻️Refactor($Lotto): Lotto 클래스 리팩토링 가독성 개선을 위해서 통합 유효성 검사 메서드로 수정

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,4 +1,4 @@
-import Validation from './Validation.js';
+import Validation from './Validation';
 
 class Lotto {
   #numbers;
@@ -9,40 +9,12 @@ class Lotto {
   }
 
   #validate(numbers) {
-    Validation.validateLottoNumberType(numbers);
-    Validation.validateLottoNumberLength(numbers);
-    Validation.validateLottoNumberDuplicate(numbers);
-    Validation.validateLottoNumberRange(numbers);
+    Validation.isLottoNumbersValidated(numbers);
   }
 
   getLottoNumbers() {
     return [...this.#numbers];
   }
-
-  // TODO: 추가 기능 구현
-
-  // createLottoTickets(purchaseAmount) {
-  //   for (let ticketCount = 0; ticketCount < purchaseAmount; ticketCount += 1) {
-  //     this.lottoTickets.push(this.#createLottoTicket());
-  //   }
-  // }
-
-  // #createLottoTicket() {
-  //   const lottoTicket = Random.pickUniqueNumbersInRange(
-  //     CONSTANTS.minRange,
-  //     CONSTANTS.maxRange,
-  //     6,
-  //   ).sort((a, b) => a - b);
-  //   return lottoTicket;
-  // }
-
-  // printLottoTickets() {
-  //   const lottoPurchaseStatus = `${this.lottoTickets.length}${USER_OUTPUT.purchaseCountPrompt}`;
-  //   const ticketLines = this.lottoTickets
-  //     .map(ticket => `[${ticket.join(', ')}]`)
-  //     .join('\n');
-  //   Console.print(`${lottoPurchaseStatus}\n${ticketLines}`);
-  // }
 }
 
 export default Lotto;
