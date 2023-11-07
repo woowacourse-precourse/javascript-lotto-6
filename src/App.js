d6e6772e66ce47fd46f71456532d3274aeb739c2
jjo-mi <whdal2636@naver.com>
2023-11-07 20:03:52 +0900

    [feat] 로또 생성 하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ class App {
     let lottoTicket = printTiceks(money);
   }
 }
+
+// 4. 로또 한개 만들기_6개의 중복되지 않는 수 반복으로 만들기
+const generateLottoNumbers = () => {
+  const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+  return numbers.sort((a, b) => a - b);
+};
+
 export default App;
 const app = new App();
 app.play();
