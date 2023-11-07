cbf9cb1b77aab62bafc90dece4497d5d6b9a3786
KimRosa <108081283+KimRosa@users.noreply.github.com>
2023-11-07 15:00:53 +0900

    Feat(App.js): 구매 금액 입력
    로또 구매 금액 입력 콘솔 생성
    입력받은 금액 조건 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto";
+
 class App {
-  async play() {}
+  constructor() {
+    this.gameStatus = true;
+  }
+
+  async play() {
+    while (this.gameStatus) {
+      const userMoney = await MissionUtils.Console.readLineAsync(
+        "구입금액을 입력해 주세요."
+      );
+      await this.checkUserMoneyPossible(userMoney);
+    }
+  }
+  async checkUserMoneyPossible(userMoney) {
+    if (isNaN(userMoney) || userMoney % 1000 !== 0 || userMoney < 1000) {
+      throw new Error("[ERROR] 1000단위의 숫자를 입력해주세요");
+    }
+  }
 }
 
 export default App;
