da15324ba06d6506ddc96fe21a1b27dd580bd67e
pjc0179 <pjc0179@gmail.com>
2023-11-07 20:49:50 +0900

    refactor(constants): message.js에서 관리하던 error 프로퍼티 errorMessage.js로 이관

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,6 +1,7 @@
 import CONFIG from './config.js';
 
 const ERROR_MESSAGES = Object.freeze({
+  error: '[ERROR]',
   buyLottoPriceError: `로또 구매 금액은 ${CONFIG.lottoPrice} 단위로 입력해주세요`,
   numberTypeError: '숫자가 잘못된 형식입니다',
   numberOutOfRange: `로또번호 및 보너스번호는 ${CONFIG.minLottoNumber} ~ ${CONFIG.maxLottoNumber} 사이의 값을 입력해주세요`,
