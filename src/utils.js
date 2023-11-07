f057e986563618ca946bfe700ac6cf10ee713e94
Jihyun_Kim <jihyun3313@daum.net>
2023-11-07 21:33:26 +0900

    feat: winningNumber 정수형 변경 메소드 추가

--- src/utils.js
+++ src/utils.js
@@ -5,8 +5,15 @@ export const print = (message) => {
   MissionUtils.Console.print(message);
 }
 
-export const getRandomNumber = () => {
-  const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-  randomNumber.sort((a, b) => a - b); // 오름차순 정렬
-  return randomNumber;
+export const getRandomNumbers = () => {
+  const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  randomNumbers.sort((a, b) => a - b); // 오름차순 정렬
+  return randomNumbers;
+}
+
+export const changeArrayStringIntoNumber = (array) => {
+  array.forEach((str, index) => {
+    array[index] = Number(str);
+  })
+  return array;
 }
