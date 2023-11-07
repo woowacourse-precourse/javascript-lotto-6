56a2c5ca4159ef6425b5206de8d97270e288f6ab
lee juyoung <juyung0903@naver.com>
2023-11-07 22:51:52 +0900

    refactor: 게임과 직접적인 관련 없고, 도와주는 역할의 메소드들은 GameUtils로 분리

--- src/lotto/GameUtils.js
+++ src/lotto/GameUtils.js
@@ -1,25 +1,58 @@
-import { WINNIG_PROFITS } from '../Constants.js';
-
+import { WINNIG_PROFITS, LOTTO_LENGTH } from "../Constants.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+import LottoGame from "./LottoGame.js";
 class GameUtils {
-  static splitComma(input) {
-    return input.split(',');
+  constructor(lottos) {
+    this.lottos = lottos;
   }
-  static sortLottoNumbers(arr) {
-    // 각 행을 오름차순으로 정렬
-    arr.forEach((row) => row.sort((a, b) => a - b));
-    return arr;
+  generateRandomNumbers() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, LOTTO_LENGTH);
   }
 
-  static removeItemsWithNumericKeysLessThanThree(obj) {
+  matchingCountsWithArr(winningNumbers) {
+    const MATCHING_COUNTS = [];
+    for (let index = 0; index < this.lottos.length; index++) {
+      MATCHING_COUNTS.push(this.lottos[index].getMatchingCount(winningNumbers));
+    }
+    return MATCHING_COUNTS;
+  }
+
+  matchingCountsWithObj(matchingCountsArr, bonusNumber) {
+    let matchingCountsObj = {};
+    for (let index = 0; index < matchingCountsArr.length; index++) {
+      let matchingCount = matchingCountsArr[index];
+      if (matchingCount === 5) {
+        const BONUS_LOTTO = this.lottos[index].getNumbers();
+        matchingCount = this.checkBonusNumberMatch(BONUS_LOTTO, bonusNumber);
+      }
+      matchingCountsObj = this.addOrUpdatePropertyInObj(
+        matchingCountsObj,
+        matchingCount
+      );
+    }
+    return matchingCountsObj;
+  }
+
+  addOrUpdatePropertyInObj(obj, matchingCount) {
+    let newObj = { ...obj };
+    if (!obj[matchingCount]) {
+      newObj[matchingCount] = 1;
+      return newObj;
+    }
+    newObj[matchingCount] += 1;
+    return newObj;
+  }
+
+  removeItemsWithNumericKeysLessThanThree(obj) {
     const OBJ = { ...obj };
     for (const KEY in OBJ) {
-      if (typeof Number(KEY) === 'number' && KEY < 3) {
+      if (typeof Number(KEY) === "number" && KEY < 3) {
         delete OBJ[KEY];
       }
     }
     return OBJ;
   }
-  static addMissingElements(obj) {
+  addMissingElements(obj) {
     const OBJ = { ...obj };
     for (const WINNING_NUMBER in WINNIG_PROFITS) {
       const key = WINNING_NUMBER.toString();
@@ -29,11 +62,19 @@ class GameUtils {
     }
     return OBJ;
   }
-  static processMatchingNumbersToResult(obj) {
+
+  processMatchingNumbersToResult(obj) {
     let result = { ...obj };
     result = this.removeItemsWithNumericKeysLessThanThree(result);
     result = this.addMissingElements(result);
     return result;
+  }
+
+  checkBonusNumberMatch(bonusLotto, bonusNumber) {
+    if (bonusLotto.includes(Number(bonusNumber))) {
+      return "bonus";
+    }
+    return "5";
   }
 }
 export default GameUtils;
--- src/lotto/LottoGame.js
+++ src/lotto/LottoGame.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Lotto from './Lotto.js';
 import GameUtils from './GameUtils.js';
-import { LOTTO_LENGTH, LOTTO_PRICE, WINNIG_PROFITS } from '../Constants.js';
+import { LOTTO_PRICE, WINNIG_PROFITS } from '../Constants.js';
 import Validation from '../validation/Validation.js';
 class LottoGame {
   #lottos;
@@ -14,11 +14,13 @@ class LottoGame {
     this.#purchaseAmount = purchaseAmount;
     this.#purchaseNumber = purchaseAmount / LOTTO_PRICE;
     this.#issueLotto();
+    this.gameUtils = new GameUtils(this.#lottos);
   }
 
   #issueLotto() {
+    const gameUtils = new GameUtils();
     for (let i = 0; i < this.#purchaseNumber; i++) {
-      const LOTTO = new Lotto(this.generateRandomNumbers());
+      const LOTTO = new Lotto(gameUtils.generateRandomNumbers());
       this.#lottos.push(LOTTO);
     }
   }
@@ -31,49 +33,15 @@ class LottoGame {
     return LOTTO_NUMBERS;
   }
 
-  generateRandomNumbers() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, LOTTO_LENGTH);
-  }
-
   getWinningStatus(winningNumbers, bonusNumber) {
-    const MATHING_COUNTS = [];
     Validation.validateLottoNumbers(winningNumbers);
     Validation.validateBonusNumber(bonusNumber);
-    for (let index = 0; index < this.#lottos.length; index++) {
-      MATHING_COUNTS.push(this.#lottos[index].getMatchingCount(winningNumbers));
-    }
-    const MATHING_COUNT_OBJ = this.matchingCountsWithObj(MATHING_COUNTS,bonusNumber);
-    const WINNING_STATUS = GameUtils.processMatchingNumbersToResult(MATHING_COUNT_OBJ);
+    const MATCHING_COUNT_ARR = this.gameUtils.matchingCountsWithArr(winningNumbers);
+    const MATCHING_COUNT_OBJ = this.gameUtils.matchingCountsWithObj(MATCHING_COUNT_ARR,bonusNumber);
+    const WINNING_STATUS = this.gameUtils.processMatchingNumbersToResult(MATCHING_COUNT_OBJ);
     return WINNING_STATUS;
   }
 
-  checkBonusNumberMatch(index, bonusNumber) {
-    const LOTTO_NUMBERS = this.#lottos[index].getNumbers();
-    if (LOTTO_NUMBERS.includes(Number(bonusNumber))) {
-      return 'bonus';
-    }
-    return '5';
-  }
-  addOrUpdatePropertyInObj(obj, matchingCount) {
-    let newObj = { ...obj };
-    if (!obj[matchingCount]) {
-      newObj[matchingCount] = 1;
-      return newObj;
-    }
-    newObj[matchingCount] += 1;
-    return newObj;
-  }
-  matchingCountsWithObj(matchingCounts, bonusNumber) {
-    let matchingCountsObj = {};
-    for (let index = 0; index < matchingCounts.length; index++) {
-      let matchingCount = matchingCounts[index];
-      if (matchingCount === 5) {
-        matchingCount = this.checkBonusNumberMatch(index, bonusNumber);
-      }
-      matchingCountsObj = this.addOrUpdatePropertyInObj(matchingCountsObj, matchingCount);
-    }
-    return matchingCountsObj;
-  }
   calculateRateOfReturn(winnigStatus) {
     let sumProfit = 0
     for (let winningNumber in winnigStatus) {
