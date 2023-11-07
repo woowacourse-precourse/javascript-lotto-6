e2108f87c8a90c29b0ec1a008d97f506a2a5fe53
Hong sung soo <sow9707@daum.net>
2023-11-07 23:35:59 +0900

    feat: 로또 게임 인트로 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+import WinningLotto from "./classes/WinningLotto.js";
+
+const Console = MissionUtils.Console;
+
 class App {
-  async play() {}
+  async intro() {
+    const userMoney = await Lotto.validateUserMoney();
+    const purchaseNumber = userMoney / 1000;
+    Console.print(purchaseNumber + "개를 구매했습니다.");
+    const lottoArray = Array(purchaseNumber)
+      .fill()
+      .map(() => Lotto.createNumber());
+    const lottoClasses = lottoArray.map((lotto) => new Lotto(lotto));
+    lottoClasses.forEach((lottoClass) => lottoClass.printNumber());
+    return lottoClasses;
+  }
+
+  async play() {
+  }
 }
 
 export default App;
