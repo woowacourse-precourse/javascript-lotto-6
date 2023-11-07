eb76cc42718700ce818f1be14f216a57e52f4bd3
djLee77 <dkdnl232@gmail.com>
2023-11-08 01:48:21 +0900

    feat(ValidateConstants):  로또 객체 에러 메시지 객체 추가
    :변수명 변경
    NUMBER_RANGE => OUT_OF_RANGE

--- src/ValidateConstants.js
+++ src/ValidateConstants.js
@@ -3,9 +3,9 @@ class Const {
 
   static MAX_WINNING_NUMBER = 45;
 
-  static ERROR_MESSAGES = {
+  static INPUT_ERROR_MESSAGES = {
     NATURAL_NUMBER: (subject) => `[ERROR] ${subject} 자연수로 입력해주세요.\n`,
-    NUMBER_RANGE: (subject) =>
+    OUT_OF_RANGE: (subject) =>
       `[ERROR] ${subject} ${Const.MIN_WINNING_NUMBER}부터 ${Const.MAX_WINNING_NUMBER}사이의 값을 입력해주세요.\n`,
     MIN_AMOUNT: "[ERROR] 구입금액은 최소 1000원부터 가능합니다.\n",
     AMOUNT_UNIT: "[ERROR] 구입금액은 1,000원 단위만 가능합니다.\n",
@@ -14,6 +14,13 @@ class Const {
     DUPLICATE_NUMBER: "[ERROR] 번호들 중 중복된 값이 존재합니다.\n",
     BONUS_NUMBER_DUPLICATE:
       "[ERROR] 당첨 번호들 중 보너스 번호와 중복된 값이 존재합니다.\n",
+  };
+
+  static LOTTO_ERROR_MESSAGES = {
+    INVALID_NUMBER_COUNT: "[ERROR] 로또 번호는 6개여야 합니다.",
+    DUPLICATE_VALUES: "[ERROR] 로또 번호중 중복된 값이 존재합니다.\n",
+    NOT_AN_INTEGER: "[ERROR] 로또 번호는 자연수여야 합니다.",
+    OUT_OF_RANGE: "[ERROR] 로또 번호는 1과 45사이의 값이어야 합니다.",
   };
 }
 
