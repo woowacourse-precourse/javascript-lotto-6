9a560f3934571a38f52c1d915e4fa3350a4d3435
Trophy198 <spsp8426@naver.com>
2023-11-08 00:20:36 +0900

    refactor : 에러메시지 상수 네이밍 개선

--- src/constants/ErrorMessages.js
+++ src/constants/ErrorMessages.js
@@ -1,12 +1,10 @@
 export const ERROR_MESSAGES = {
-  INVALID_FORMAT:
-    '[ERROR] 입력값중 사용할 수 없는 문자열이 포함되어 있습니다. 숫자만 입력가능합니다.',
-  AMOUNT_NOT_IN_UNITS: '[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.',
-  EMPTY_NUMBER: '[ERROR] 모든 당첨 번호를 쉼표(,)로 구분하여 입력해야 합니다.',
-  NOT_SIX_NUMBERS: '[ERROR] 당첨 번호는 쉼표(,)로 구분된 6개의 숫자여야 합니다.',
+  INVALID_FORMAT: '[ERROR] 입력값은 숫자만 가능합니다.',
+  INVALID_AMOUNT: '[ERROR] 구입 금액은 숫자여야 하며, 1000원 단위로 입력해야 합니다.',
+  INVALID_NUMBER: '[ERROR] 로또 번호는 쉼표(,)로 구분되어야 하며, 1부터 45 사이의 숫자여야 합니다.',
+  INVALID_NUMBERS: '[ERROR] 로또 번호는 쉼표(,)로 구분된 6개의 숫자여야 하며, 중복될 수 없습니다.',
+  INVALID_BONUS_NUMBER:
+    '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없으며, 1부터 45 사이의 숫자여야 합니다.',
   NUMBER_OUT_OF_RANGE: '[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.',
-  DUPLICATE_NUMBER: '[ERROR] 당첨 번호에 중복된 숫자가 있습니다.',
   BONUS_NUMBER_DUPLICATE: '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
-  INVALID_NUMBERS_COUNT: '[ERROR] 로또 번호는 6개여야 합니다.',
-  DUPLICATE_NUMBERS: '[ERROR] 로또 번호는 중복될 수 없습니다.',
 };
