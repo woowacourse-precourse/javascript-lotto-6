41567620147554bf16fff55d84ce6fa69b7e0d10
dongsuseo <poopoo0515@naver.com>
2023-11-07 20:46:24 +0900

    style: 주석 추가
    작동 흐름의 가독성을 위해 주석을 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ import getStatistics from "./getStatistics";
 import calculateProfit from "./calculateProfit";
 class App {
   async play() {
+    // 구입 금액 입력
     const amount = await AmountInput();
     const lottoNumber = calculateLottoNumber(amount);
     Console.print(`${lottoNumber}개를 구매했습니다.`);
-
+    // 로또 번호 생성
     const lottoList = [];
     for (let i = 0; i < lottoNumber; i++) {
       const sortedRandomNumbers = makeRandomNumbers().sort((a, b) => a - b);
       Console.print(`[${sortedRandomNumbers.join(", ")}]`);
       lottoList.push(new Lotto(sortedRandomNumbers));
     }
+    // 당첨 번호 입력
     const [winningNumbers, bonusNumber] = await numberInput();
+    // 로또 결과 계산
     const lottoResult = { 3: 0, 4: 0, 5: 0, "5bonus": 0, 6: 0 };
     lottoList.forEach((lotto) => {
       const [result, bonusResult] = lotto.compareNumbers(
@@ -28,6 +31,7 @@ class App {
       );
       getStatistics(lottoResult, result, bonusResult);
     });
+    // 최종 결과 출력
     const profit = calculateProfit(amount, lottoResult);
     printResult(lottoResult, profit);
   }
