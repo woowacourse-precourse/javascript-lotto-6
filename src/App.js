57b6cbae4696c4ece9dd2481b3d7b9f48a3c938d
ss0526100 <ss0526100@gmail.com>
2023-11-07 22:23:32 +0900

    refactor: restore App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import Fucntional from './functinoal/Functional.js';
 import MVC from './mvc/MVC.js';
 
-import ConvertInputTo from './functinoal/modules/ConvertInputTo.js';
-import Print from './functinoal/modules/Print.js';
-import Get from './functinoal/modules/Get.js';
-
 class App {
   async play() {
-    //await Fucntional.play();
+    await Fucntional.play();
 
     //await MVC.play();
-
-    const lottoArray = await ConvertInputTo.lottoArray();
-    Print.purchasedLotto(lottoArray);
-    Print.lineBreak();
-
-    const lottoBoard = await ConvertInputTo.lottoBoard();
-    Print.lineBreak();
-
-    const lottoResult = Get.lottoResult(lottoArray, lottoBoard);
-    Print.lottoResult(lottoResult);
-    Print.lottoReturnRatio(
-      Get.lottoReturnRatio(lottoResult, lottoArray.length)
-    );
   }
 }
 
