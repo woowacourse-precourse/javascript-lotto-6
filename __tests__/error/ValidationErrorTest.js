3ff8a12caa987fd5cbce0234fc05f0c450fa6f90
swarvy <swarvy0826@naver.com>
2023-11-07 10:58:24 +0900

    test : 커스텀 에러 테스트로직 작성

---
+++ __tests__/error/ValidationErrorTest.js
@@ -0,0 +1,14 @@
+import ValidationError from '../../src/error/ValidationError.js';
+import DefaultError from '../../src/error/DefaultError.js';
+
+describe('커스텀 에러 테스트', () => {
+  const throwError = () => {
+    throw new ValidationError('test');
+  };
+
+  // given
+  test(`에러는 ${DefaultError.ERROR_PREFIX}를 포함해야한다.`, () => {
+    // when & then
+    expect(throwError).toThrow(DefaultError.ERROR_PREFIX);
+  });
+});
