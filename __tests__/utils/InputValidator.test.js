09014f228c0884abcef0801d8656cb2a45969040
Trophy198 <spsp8426@naver.com>
2023-11-08 00:23:23 +0900

    test : 입력값 검증 테스트 케이스에서 에러메시지 상수를 이용해 명확한 값으로 변경

--- __tests__/utils/InputValidator.test.js
+++ __tests__/utils/InputValidator.test.js
@@ -1,3 +1,4 @@
+import { ERROR_MESSAGES } from '../../src/constants/ErrorMessages';
 import InputValidator from '../../src/utils/InputValidator';
 
 describe('InputValidator 클래스', () => {
@@ -5,51 +6,31 @@ describe('InputValidator 클래스', () => {
     test('금액이 비어 있을 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validatePurchaseAmount('');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_FORMAT);
     });
 
     test('금액이 숫자가 아닐 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validatePurchaseAmount('1000원');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_FORMAT);
     });
 
     test('금액이 1000원 단위가 아닐 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validatePurchaseAmount('2500');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_AMOUNT);
     });
 
     test('금액이 음수일 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validatePurchaseAmount('-1000');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_FORMAT);
     });
 
     test('금액이 1000원 이상이 아닐 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validatePurchaseAmount('999');
-      }).toThrow('[ERROR]');
-    });
-  });
-
-  describe('parseAndValidateNumbers 메서드', () => {
-    test('숫자 문자열이 비어 있을 때 예외가 발생한다.', () => {
-      expect(() => {
-        InputValidator.parseAndValidateNumbers('1,2, ,4,5,6');
-      }).toThrow('[ERROR]');
-    });
-
-    test('숫자가 범위 밖일 때 예외가 발생한다.', () => {
-      expect(() => {
-        InputValidator.parseAndValidateNumbers('0,2,3,4,5,6');
-      }).toThrow('[ERROR]');
-    });
-
-    test('문자열에 숫자가 아닌 문자가 포함되어 있을 때 예외가 발생한다.', () => {
-      expect(() => {
-        InputValidator.parseAndValidateNumbers('1,2,three,4,5,6');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_AMOUNT);
     });
   });
 
@@ -57,13 +38,31 @@ describe('InputValidator 클래스', () => {
     test('당첨 번호가 6개가 아닐 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validateWinningNumbers('1,2,3,4,5');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_NUMBERS);
     });
 
     test('당첨 번호에 중복이 있을 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validateWinningNumbers('1,1,2,3,4,5');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_NUMBERS);
+    });
+
+    test('숫자 문자열이 비어 있을 때 예외가 발생한다.', () => {
+      expect(() => {
+        InputValidator.validateWinningNumbers('1,2, ,4,5,6');
+      }).toThrow(ERROR_MESSAGES.INVALID_NUMBER);
+    });
+
+    test('숫자가 범위 밖일 때 예외가 발생한다.', () => {
+      expect(() => {
+        InputValidator.validateWinningNumbers('0,2,3,4,5,6');
+      }).toThrow(ERROR_MESSAGES.INVALID_NUMBER);
+    });
+
+    test('문자열에 숫자가 아닌 문자가 포함되어 있을 때 예외가 발생한다.', () => {
+      expect(() => {
+        InputValidator.validateWinningNumbers('1,2,three,4,5,6');
+      }).toThrow(ERROR_MESSAGES.INVALID_NUMBER);
     });
   });
 
@@ -71,19 +70,19 @@ describe('InputValidator 클래스', () => {
     test('보너스 번호가 범위 밖일 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validateBonusNumber('46', '1,2,3,4,5,6');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_NUMBER);
     });
 
     test('보너스 번호가 당첨 번호와 중복될 때 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validateBonusNumber('1', '1,2,3,4,5,6');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.BONUS_NUMBER_DUPLICATE);
     });
 
     test('보너스 번호가 숫자가 아닌 문자열이 들어올경우 예외가 발생한다.', () => {
       expect(() => {
         InputValidator.validateBonusNumber('string', '1,2,3,4,5,6');
-      }).toThrow('[ERROR]');
+      }).toThrow(ERROR_MESSAGES.INVALID_NUMBER);
     });
   });
 });
