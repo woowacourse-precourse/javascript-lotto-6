1c402d461faf60d415d299f13ad8edfa3b11ab74
shgusgh12 <https://github.com/shgusgh12>
2023-11-08 05:00:16 +0900

    feat: 등수 출력에 사용되는 객체 추가

---
+++ src/Constant.js
@@ -0,0 +1,34 @@
+const RANK = [
+  {
+    result: 'five',
+    match: 3,
+    printMoney: '5,000',
+    money: 5000,
+  },
+  {
+    result: 'four',
+    match: 4,
+    printMoney: '50,000',
+    money: 50000,
+  },
+  {
+    result: 'three',
+    match: 5,
+    printMoney: '1,500,000',
+    money: 1500000,
+  },
+  {
+    result: 'two',
+    match: 5,
+    printMoney: '30,000,000',
+    money: 30000000,
+  },
+  {
+    result: 'one',
+    match: 6,
+    printMoney: '2,000,000,000',
+    money: 2000000000,
+  },
+];
+
+export default RANK;
