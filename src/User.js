1ad023d4b18c32a6599dd9841c3cb014ec3a79ff
hojkim77 <rlaghwnd4534@gmail.com>
2023-11-07 15:51:10 +0900

    [refactor] 변수명 수정

--- src/User.js
+++ src/User.js
@@ -2,15 +2,18 @@ import Generator from './Generator.js';
 
 class User {
   constructor(purchaseAmout) {
-    this.purchaseAmout = purchaseAmout / 1000;
-    this.userNumbersList = Generator.randomNumbersGenerator(this.purchaseAmout);
+    this.purchaseAmout = purchaseAmout;
+    this.purchaseNumber = purchaseAmout / 1000;
+    this.userNumbersList = Generator.randomNumbersGenerator(
+      this.purchaseNumber
+    );
 
     this.results = {
-      '3Equal': 0,
-      '4Equal': 0,
-      '5Equal': 0,
-      '5EqualAndBonus': 0,
-      '6Equal': 0,
+      threeEqual: 0,
+      fourEqual: 0,
+      fiveEqual: 0,
+      fiveEqualAndBonus: 0,
+      sixEqual: 0,
     };
     this.profits = 0;
     this.yeild;
@@ -19,15 +22,15 @@ class User {
   setResult(lottoCount, isBonus) {
     switch (lottoCount) {
       case 3:
-        return (this.results['3Equal'] += 1);
+        return (this.results['threeEqual'] += 1);
       case 4:
-        return (this.results['4Equal'] += 1);
+        return (this.results['fourEqual'] += 1);
       case 5:
         return isBonus
-          ? (this.results['5EqualAndBonus'] += 1)
-          : (this.results['5Equal'] += 1);
+          ? (this.results['fiveEqualAndBonus'] += 1)
+          : (this.results['fiveEqual'] += 1);
       case 6:
-        return (this.results['6Equal'] += 1);
+        return (this.results['sixEqual'] += 1);
     }
   }
 
