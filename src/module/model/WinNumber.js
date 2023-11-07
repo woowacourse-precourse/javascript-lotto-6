8fad4ad565a0d9d6a3d17c9df28887353aad800c
OAO <ashgrayblue0@gmail.com>
2023-11-08 02:24:01 +0900

    fix(WinNumber): fix checkDuplication not working properly

--- src/module/model/WinNumber.js
+++ src/module/model/WinNumber.js
@@ -20,11 +20,13 @@ class WinNumber {
   }
 
   #checkDuplication(numbers) {
-    numbers.forEach((e) => {
-      if (numbers.includes(e)) {
-        throw new Error('[ERROR] 당첨 번호는 중복되면 안됩니다.');
+    for (let i = 0; i < numbers.length; i += 1) {
+      for (let j = i + 1; j < numbers.length; j += 1) {
+        if (numbers[i] === numbers[j]) {
+          throw new Error('[ERROR] 당첨 번호는 중복되면 안됩니다.');
+        }
       }
-    });
+    }
   }
 
   #checkNumberBound(numbers) {
