1fadd37144192c94f210e6b5eeb49e06326fc58d
hyj5076 <hyj5076@hanmail.net>
2023-11-08 00:43:15 +0900

    feat(App): 구입 금액, 사용자 로또 번호, 보너스 숫자 생성 및 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import Lotto from "./Lotto.js";
+import UserBonusNumber from "./domain/UserBonusNumber.js";
+import UserPayment from "./domain/UserPayment.js";
+import UserBaseNumbers from "./domain/UserBaseNumbers.js";
+import LottoEvaluator from "./domain/LottoEvaluator.js";
+import { runLotteryMachine } from "./domain/RunLotteryMachine.js";
+import { ask } from "./UI/inputView.js";
+import { print, prize, counts } from "./UI/outputView.js";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.base = new UserBaseNumbers();
+    this.bonus = new UserBonusNumber();
+    this.payment = new UserPayment();
+    this.lottoEvaluator = null;
+  }
+
+  async play() {
+    try {
+      await this.setLotto();
+    } catch (error) {
+      Console.print(error.message);
+      return Promise.reject(error);
+    }
+  }
+
+  async setLotto() {
+    // 사용자 입력값 호출
+    const pay = await ask.payment();
+    const baseNums = await ask.baseNumbers();
+    const bonusNum = await ask.bonusNumber();
+
+    // 로또 유효성 검사와 함께 저장
+    this.payment.setUserPayment(pay);
+    this.base.setBaseNumbers(baseNums);
+    this.bonus.setBonusNumber(bonusNum);
+
+    Console.print(`지불금액: ${pay}`);
+    Console.print(`기본숫자: ${baseNums}`);
+    Console.print(`보너스숫자: ${bonusNum}`);
+  }
 }
 
 export default App;
