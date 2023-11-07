fa3e4c43b2e2d1e10d5cc8a8a919098f51d17dac
hongikAn <adh30000@gmail.com>
2023-11-07 20:28:20 +0900

    test: 로또 클래스 관련 테스트코드 수정

--- __tests__/LotteryPrizeTest.js
+++ __tests__/LotteryPrizeTest.js
@@ -1,9 +1,10 @@
-import LotteryPrize from '../src/LotteryPrize.js';
-import WinningStatistic from '../src/WinningStatistic.js';
+import LotteryPrize from "../src/LotteryPrize.js";
+import WinningStatistic from "../src/WinningStatistic.js";
+import Lotto from "../src/Lotto.js";
 
-describe('LotteryPrize.calculatePrize', () => {
-  test('1등 당첨, 수익률 200,000,000%', () => {
-    const lottoBundle = [[1, 2, 3, 4, 5, 6]];
+describe("LotteryPrize.calculatePrize", () => {
+  test("1등 당첨, 수익률 200,000,000%", () => {
+    const lottoBundle = [new Lotto([1, 2, 3, 4, 5, 6])];
     const winningNumbers = [1, 2, 3, 4, 5, 6];
 
     const lotteryPrize = new LotteryPrize(lottoBundle, winningNumbers, 7);
@@ -17,16 +18,16 @@ describe('LotteryPrize.calculatePrize', () => {
     expect(winningStatistic.getEaringRate()).toBe(200000000);
   });
 
-  test('3등 당첨, 수익률 62.5%', () => {
+  test("3등 당첨, 수익률 62.5%", () => {
     const lottoBundle = [
-      [8, 21, 23, 41, 42, 43],
-      [3, 5, 11, 16, 32, 38],
-      [7, 11, 16, 35, 36, 44],
-      [1, 8, 11, 31, 41, 42],
-      [13, 14, 16, 38, 42, 45],
-      [7, 11, 30, 40, 42, 43],
-      [2, 13, 22, 32, 38, 45],
-      [1, 3, 5, 14, 22, 45],
+      new Lotto([8, 21, 23, 41, 42, 43]),
+      new Lotto([3, 5, 11, 16, 32, 38]),
+      new Lotto([7, 11, 16, 35, 36, 44]),
+      new Lotto([1, 8, 11, 31, 41, 42]),
+      new Lotto([13, 14, 16, 38, 42, 45]),
+      new Lotto([7, 11, 30, 40, 42, 43]),
+      new Lotto([2, 13, 22, 32, 38, 45]),
+      new Lotto([1, 3, 5, 14, 22, 45]),
     ];
     const winningNumbers = [1, 2, 3, 4, 5, 6];
 
@@ -41,16 +42,16 @@ describe('LotteryPrize.calculatePrize', () => {
     expect(winningStatistic.getEaringRate()).toBe(62.5);
   });
 
-  test('4등 1개, 5등 3개당첨 수익률 812.5%', () => {
+  test("4등 1개, 5등 3개당첨 수익률 812.5%", () => {
     const lottoBundle = [
-      [1, 34, 2, 3, 4, 42],
-      [1, 3, 4, 7, 8, 9],
-      [1, 2, 3, 42, 43, 44],
-      [1, 2, 3, 12, 13, 14],
-      [13, 14, 16, 38, 42, 45],
-      [7, 11, 30, 40, 42, 43],
-      [2, 13, 22, 32, 38, 45],
-      [31, 32, 355, 14, 22, 45],
+      new Lotto([1, 34, 2, 3, 4, 42]),
+      new Lotto([1, 3, 4, 7, 8, 9]),
+      new Lotto([1, 2, 3, 42, 43, 44]),
+      new Lotto([1, 2, 3, 12, 13, 14]),
+      new Lotto([13, 14, 16, 38, 42, 45]),
+      new Lotto([7, 11, 30, 40, 42, 43]),
+      new Lotto([2, 13, 22, 32, 38, 45]),
+      new Lotto([31, 32, 35, 14, 22, 45]),
     ];
     const winningNumbers = [1, 2, 3, 4, 5, 6];
 
