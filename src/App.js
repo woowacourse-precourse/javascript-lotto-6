b21e599e1ce6118b44816468179bfd762b56d0d2
yoovin <soo22839@gmail.com>
2023-11-07 23:00:58 +0900

    feat: 로또 구매 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
 class App {
     async play() {}
+
+    buyLotto(pay) {
+        if (pay.match(/\D/)) {
+            throw '[ERROR] 숫자만 입력해주세요.';
+        }
+
+        pay = Number(pay);
+
+        if (pay < 1000) {
+            throw '[ERROR] 1000원 이상의 금액을 입력해주세요.';
+        }
+
+        if (pay % 1000 !== 0) {
+            throw '[ERROR] 1000원 단위로 입력해주세요.';
+        }
+
+        const count = pay / 1000;
+        const lottos = [];
+
+        for (let i = 0; i < count; i++) {
+            lottos.push(this.getLotto());
+        }
+
+        return lottos;
+    }
+
+    getLotto() {
+        return new Lotto(
+            MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+        );
+    }
 }
 
 export default App;
