63325bd04f85fd3f38444152ad3a795670e0494d
useon <yoosk4986@naver.com>
2023-11-07 17:14:57 +0900

    refactor: Lotto 클래스 수정됨에 따라 같은 로직으로 작동하도록 수정한다

--- src/Bonus.js
+++ src/Bonus.js
@@ -3,19 +3,19 @@ class Bonus {
 
   constructor(winningNumbers, inputBonusNumber) {
     this.#validate(winningNumbers, inputBonusNumber);
+    this.#number = inputBonusNumber;
   }
 
   #validate(winningNumbers, inputBonusNumber) {
     if (Number.isNaN(inputBonusNumber)) {
       throw new Error('[ERROR] 보너스 번호는 숫자여야 합니다.');
     }
-    if (Number(inputBonusNumber) < 1 || Number(inputBonusNumber) > 45) {
+    if (inputBonusNumber < 1 || inputBonusNumber > 45) {
       throw new Error('[ERROR] 보너스 번호는 1 이상 45 이하여야 합니다.');
     }
     if (winningNumbers.includes(inputBonusNumber)) {
       throw new Error('[ERROR] 보너스 번호는 당첨 번호 외의 숫자여야 합니다.');
     }
-    this.#number = Number(inputBonusNumber);
   }
 
   get bonusNumber() {
