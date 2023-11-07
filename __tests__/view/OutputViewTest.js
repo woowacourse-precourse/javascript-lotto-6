d8b7e0fee3abf6fa84c14aec993aeaea133c4bc4
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-08 01:42:53 +0900

    test: OutputView 출력 테스트 구현
    OutputView.js 내에 작성

--- __tests__/view/OutputViewTest.js
+++ __tests__/view/OutputViewTest.js
@@ -1,71 +1,75 @@
 import { Random } from "@woowacourse/mission-utils";
 import OutputView from "../../src/view/OutputView.js";
 
-const mockRandoms = (numbers) => {
-  Random.pickUniqueNumbersInRange = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickUniqueNumbersInRange);
-};
+describe("OutputView 객체 테스트", () => {
+  const mockRandoms = (numbers) => {
+    Random.pickUniqueNumbersInRange = jest.fn();
+    numbers.reduce((acc, number) => {
+      return acc.mockReturnValueOnce(number);
+    }, MissionUtils.Random.pickUniqueNumbersInRange);
+  };
 
-const mockLottoGame = {
-  money: 8000,
-  amount: 8,
-  lottos: [
-    [8, 21, 23, 41, 42, 43],
-    [3, 5, 11, 16, 32, 38],
-    [7, 11, 16, 35, 36, 44],
-    [1, 8, 11, 31, 41, 42],
-    [13, 14, 16, 38, 42, 45],
-    [7, 11, 30, 40, 42, 43],
-    [2, 13, 22, 32, 38, 45],
-    [1, 3, 5, 14, 22, 45],
-  ],
-  winningNumbers: [1, 2, 3, 4, 5, 6],
-  bonusNumber: 7,
-  winningResults: [
-    { price: 5000, ticket: 1 },
-    { price: 50000, ticket: 0 },
-    { price: 1500000, ticket: 0 },
-    { price: 30000000, ticket: 0 },
-    { price: 2000000000, ticket: 0 },
-  ],
-  totalProfit: 5000,
-  profitRatio: 62.5,
-};
+  const mockLottoGame = {
+    money: 8000,
+    amount: 8,
+    lottos: [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ],
+    winningNumbers: [1, 2, 3, 4, 5, 6],
+    bonusNumber: 7,
+    winningResults: [
+      { price: 5000, ticket: 1 },
+      { price: 50000, ticket: 0 },
+      { price: 1500000, ticket: 0 },
+      { price: 30000000, ticket: 0 },
+      { price: 2000000000, ticket: 0 },
+    ],
+    totalProfit: 5000,
+    profitRatio: 62.5,
+  };
 
-describe("로또 구매 내역 출력 테스트", () => {
-  test("발행한 로또 수량 출력", () => {
-    const expected = `${mockLottoGame.amount}개를 구매했습니다.`;
-    const result = OutputView.printAmount(mockLottoGame.money);
+  describe("로또 구매 내역 출력 테스트", () => {
+    test("발행한 로또 수량 출력", () => {
+      const expected = `${mockLottoGame.amount}개를 구매했습니다.`;
+      const result = OutputView.printAmount(mockLottoGame.money);
 
-    expect(result).toHaveBeenCalledWith(expected);
+      expect(result).toHaveBeenCalledWith(expected);
+    });
+
+    test("수량에 따른 발행한 여러 로또 출력", () => {
+      mockRandoms(mockLottoGame.lottos);
+
+      const expected =
+        "[8, 21, 23, 41, 42, 43]\n[3, 5, 11, 16, 32, 38]\n[7, 11, 16, 35, 36, 44]\n[1, 8, 11, 31, 41, 42]\n[13, 14, 16, 38, 42, 45]\n[7, 11, 30, 40, 42, 43]\n[2, 13, 22, 32, 38, 45]\n[1, 3, 5, 14, 22, 45]";
+      const result = OutputView.printLottos(mockLottoGame.lottos);
+
+      expect(result).toHaveBeenCalledWith(expected);
+    });
   });
 
-  test("수량에 따른 발행한 여러 로또 출력", () => {
-    mockRandoms(mockLottoGame.lottos);
+  describe("당첨 통계 출력 테스트", () => {
+    test("당첨 내역 출력", () => {
+      const expected =
+        "3개 일치 (5,000원) - 1개\n4개 일치 (50,000원) - 0개\n5개 일치 (1,500,000원) - 0개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n6개 일치 (2,000,000,000원) - 0개\n";
+      const result = OutputView.printWinningResult(
+        mockLottoGame.winningResults
+      );
 
-    const expected =
-      "[8, 21, 23, 41, 42, 43]\n[3, 5, 11, 16, 32, 38]\n[7, 11, 16, 35, 36, 44]\n[1, 8, 11, 31, 41, 42]\n[13, 14, 16, 38, 42, 45]\n[7, 11, 30, 40, 42, 43]\n[2, 13, 22, 32, 38, 45]\n[1, 3, 5, 14, 22, 45]";
-    const result = OutputView.printLottos(mockLottoGame.lottos);
+      expect(result).toHaveBeenCalledWith(`당첨 통계\n---\n${expected}`);
+    });
 
-    expect(result).toHaveBeenCalledWith(expected);
-  });
-});
+    test("수익률 출력", () => {
+      const expected = `총 수익률은 ${mockLottoGame.profitRatio}%입니다.`;
+      const result = OutputView.printProfitRatio(mockLottoGame.totalProfit);
 
-describe("당첨 통계 출력 테스트", () => {
-  test("당첨 내역 출력", () => {
-    const expected =
-      "3개 일치 (5,000원) - 1개\n4개 일치 (50,000원) - 0개\n5개 일치 (1,500,000원) - 0개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n6개 일치 (2,000,000,000원) - 0개\n";
-    const result = OutputView.printWinningResult(mockLottoGame.winningResults);
-
-    expect(result).toHaveBeenCalledWith(`당첨 통계\n---\n${expected}`);
-  });
-
-  test("수익률 출력", () => {
-    const expected = `총 수익률은 ${mockLottoGame.profitRatio}%입니다.`;
-    const result = OutputView.printProfitRatio(mockLottoGame.totalProfit);
-
-    expect(result).toHaveBeenCalledWith(expected);
+      expect(result).toHaveBeenCalledWith(expected);
+    });
   });
 });
