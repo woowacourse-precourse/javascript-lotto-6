2f0efe868b601561c6dbc48cb61e69e074bf04ee
Jiwon Lee <giwon.yi339@gmail.com>
2023-11-07 21:29:55 +0900

    refactor : 폴더 및 파일 정리

--- src/constants/ErrorMessage.js
+++
@@ -1,7 +0,0 @@
-export const ERROR_MSG = Object.freeze({
-  number: "[ERROR] 숫자가 잘못된 형식입니다.",
-  input: "[ERROR] 로또 번호의 숫자 범위는 1~45까지입니다.",
-  price: "[ERROR] 로또 1장의 가격은 1,000원입니다",
-  overlap: "[ERROR] 중복되지 않는 숫자를 입력해주세요.",
-  length: "[ERROR] 로또 번호는 6개여야 합니다.",
-});
--- src/constants/ResultMessage.js
+++
@@ -1,4 +0,0 @@
-export const RESULT_MSG = Object.freeze({
-  purchased: "개를 구매했습니다.",
-  lottoResult: `당첨 통계`,
-});
