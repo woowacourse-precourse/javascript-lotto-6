5314c0e7a0e58c5be120ff5ea8730141a32e5669
yeokyoung song <spolice0324@gmail.com>
2023-11-07 20:47:07 +0900

    feat : 사용자 입력에 대한 유효성 검사 로직 추가
    - 사용자의 입력 ( 구매금액, 당첨 번호, 보너스 번호) 에 대한 유효성 검사 로직 구현

---
+++ src/Validator/InputValidator.js
@@ -0,0 +1,34 @@
+import { ERROR_MESSAGE } from '../utils/constants';
+
+const checkInputFormat = input => {
+  if (input === undefined) throw new Error(ERROR_MESSAGE.INVALID_INPUT_FORMAT);
+};
+
+const checkInputType = input => {
+  if (Number(input).toString() !== input) {
+    throw new Error(ERROR_MESSAGE.TYPE_ERROR);
+  }
+};
+
+const purchaseInput = input => {
+  checkInputFormat(input);
+  checkInputType(input);
+};
+
+const winningNumbers = winningNumbers => {
+  checkInputFormat(winningNumbers);
+  winningNumbers.split(',').forEach(e => {
+    checkInputType(e);
+  });
+};
+
+const bonusNumber = bonusNumber => {
+  checkInputFormat(bonusNumber);
+  checkInputType(bonusNumber);
+};
+
+export default {
+  purchaseInput,
+  winningNumbers,
+  bonusNumber,
+};
