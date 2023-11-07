e4b9d7c29ad54a2f82364773781278e38ebfa419
kwoncha <rnjsckgus@gmail.com>
2023-11-07 23:49:33 +0900

    feat: 출력 모듈 구현

---
+++ src/utils/inputOutput/output.js
@@ -0,0 +1,9 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class Output {
+  static print(outputText) {
+    MissionUtils.Console.print(outputText);
+  }
+}
+
+export default Output;
