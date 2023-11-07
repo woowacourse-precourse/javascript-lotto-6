29a06e93ec0d9b7fa5d6e7d5f85b246196f5e848
simorimi <hori5020@gmail.com>
2023-11-07 16:13:15 +0900

    feat(Lotto): 구매장수 출력 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import InputMoney from "./UI/InputMoney.js";
+import PrintNumberOfLotto from "./UI/PrintNumberOfLotto.js";
 
 class App {
   async play() {
     const INPUT_MONEY = new InputMoney();
+    const PRINT_NUMBER_OF_LOTTO = new PrintNumberOfLotto();
 
     const NUMBER_OF_LOTTO = await INPUT_MONEY.inputMoney();
+
+    PRINT_NUMBER_OF_LOTTO.printNumberOfLotto(NUMBER_OF_LOTTO);
   }
 }
 
