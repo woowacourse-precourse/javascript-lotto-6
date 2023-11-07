12c75bfbb9f395c49cf457ef8bf0d96b6db26ebc
coldok <2coldok@gmail.com>
2023-11-07 12:56:56 +0900

    refactor : 랜덤 로또 번호 생성 과정에 좀 더 간결한 문법 적용

--- src/domain/LotteryMachine.js
+++ src/domain/LotteryMachine.js
@@ -2,35 +2,28 @@ import { Random } from "@woowacourse/mission-utils";
 import { LOTTO_RULE } from "../constants/BusinessNumber.js";
 
 class LotteryMachine {
-  #lottoTicket = [];
+  #lottoTicket;
 
-  #issuanceTime = 0;
-
-  #issuanceLimit = 0;
+  #issuanceLimit;
 
   constructor(purchaseAmount) {
     this.#issuanceLimit = purchaseAmount / LOTTO_RULE.buyUnit;
-    this.#ticketMaker()
-  }
-
-  #ticketMaker() {
-    const lottoArray = Random.pickUniqueNumbersInRange(
-      LOTTO_RULE.minNumber,
-      LOTTO_RULE.maxNumber,
-      LOTTO_RULE.selectTime
-    );
-    this.#lottoTicket.push(this.#ascendingOrder(lottoArray));
-    this.#issuanceTime += 1;
-
-    if (this.#issuanceTime === this.#issuanceLimit) return;
-
     this.#ticketMaker();
   }
 
-  #ascendingOrder(lottoArray) {
-    return lottoArray.sort((frontLotto, backLotto) => frontLotto - backLotto);
+  #ticketMaker() {
+    this.#lottoTicket = Array.from({ length : this.#issuanceLimit }, () => 
+    this.#ascendingOrder(Random.pickUniqueNumbersInRange(
+      LOTTO_RULE.minNumber,
+      LOTTO_RULE.maxNumber,
+      LOTTO_RULE.selectTime
+    )));
   }
-
+  
+  #ascendingOrder(lottoArray) {
+    return lottoArray.sort((a, b) => a - b);
+  }
+  
   getTiket() {
     return this.#lottoTicket;
   }
