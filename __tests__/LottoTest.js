53ebd8ff32112ed7bfc082764d4c97f0b990b28f
JonghyunLEE12 <najo7609@gmail.com>
2023-11-07 16:54:10 +0900

    Chore : [ 예기치 않은 오류 찾기 2 ]

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,34 +1,18 @@
-/* eslint-disable max-lines-per-function */
-import Lotto from '../src/Lotto.js';
+class Lotto {
+  #numbers;
 
-describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', async () => {
-    const numbers = [1, 2, 3, 4, 5, 6, 7];
-    await expect(Lotto.createLottoInstance(numbers)).rejects.toThrow('[ERROR]');
-  });
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
 
-  test('로또 번호가 중복됐을 때, 예외처리', async () => {
-    const numbers = [1, 2, 3, 4, 6, 6];
-    await expect(Lotto.createLottoInstance(numbers)).rejects.toThrow('[ERROR]');
-  });
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
 
-  test('로또 번호에 문자가 포함됐을 때, 예외처리', async () => {
-    const numbers = [1, 2, 3, 4, 6, 'a'];
-    await expect(Lotto.createLottoInstance(numbers)).rejects.toThrow('[ERROR]');
-  });
+  // TODO: 추가 기능 구현
+}
 
-  test('로또 번호의 개수가 6개가 안되면 예외가 발생한다.', async () => {
-    const numbers = [1, 2, 3, 4, 5];
-    await expect(Lotto.createLottoInstance(numbers)).rejects.toThrow('[ERROR]');
-  });
-
-  test('로또 번호의 범위는 1부터 45 안되면 예외가 발생한다.', async () => {
-    const numbers = [0, 2, 3, 4, 5, 6];
-    await expect(Lotto.createLottoInstance(numbers)).rejects.toThrow('[ERROR]');
-  });
-
-  test('로또 번호의 범위는 1부터 45 안되면 예외가 발생한다.', async () => {
-    const numbers = [1, 2, 3, 4, 5, 46];
-    await expect(Lotto.createLottoInstance(numbers)).rejects.toThrow('[ERROR]');
-  });
-});
+export default Lotto;
