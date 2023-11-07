0031ecef51dca8a90bde53255032580c85f32bd2
hwangheetae <tejaia@naver.com>
2023-11-07 18:37:55 +0900

    feat: 당첨내역 출력

--- src/App.js
+++ src/App.js
@@ -23,6 +23,45 @@ class App {
 			await Console.readLineAsync('보너스 번호를 입력해 주세요.');
 
 		const BONUS_NUMBER = Number(BONUS_NUMBER_INPUT);
+
+		let match_three = 0;
+		let match_four = 0;
+		let match_five = 0;
+		let match_five_plus_bonus = 0;
+		let match_six = 0;
+
+		for (let list of LOTTERY_TICKET_LIST) {
+			let count = 0;
+			for (let item of list) {
+				if (WINNING_NUMBERS.includes(item)) {
+					count++;
+				}
+			}
+
+			if (count === 3) {
+				match_three++;
+			}
+			if (count === 4) {
+				match_four++;
+			}
+			if (count === 5 && !list.includes(BONUS_NUMBER)) {
+				match_five++;
+			}
+			if (count === 5 && list.includes(BONUS_NUMBER)) {
+				match_five_plus_bonus++;
+			}
+			if (count === 6) {
+				match_six++;
+			}
+		}
+
+		Console.print(`3개 일치 (5,000원) - ${match_three}개`);
+		Console.print(`4개 일치 (50,000원) - ${match_four}개`);
+		Console.print(`5개 일치 (1,500,000원) - ${match_five}개`);
+		Console.print(
+			`5개 일치, 보너스 볼 일치 (30,000,000원) - ${match_five_plus_bonus}개`,
+		);
+		Console.print(`6개 일치 (2,000,000,000원) - ${match_six}개`);
 	}
 }
 
