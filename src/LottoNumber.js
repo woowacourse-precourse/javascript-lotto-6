6dfad72e6546d25624ea47a457efef20d1358fea
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-07 18:43:37 +0900

    feat: LottoNumber 클래스 구현
    로또 번호 각각의 번호 클래스로, 1 이상 45 이하의 숫자인지 확인하는 메서드를 포함한다

---
+++ src/LottoNumber.js
@@ -0,0 +1,19 @@
+class LottoNumber {
+    LOTTO_NUMBER;
+    ERROR_MESSEGE_NON_NUMBER = "[ERROR] 숫자를 입력해 주세요.";
+    ERROR_MESSEGE_NOT_IN_RANGE = "[ERROR] 숫자는 1 이상 45 이하여야 합니다."
+    constructor(number) {
+        this.#validate(number);
+        this.LOTTO_NUMBER = number;
+    }
+
+    #validate(number){
+        if(!isNaN(number)){
+            throw new Error(this.ERROR_MESSEGE_NON_NUMBER);
+        }
+        if(number < 1 || number > 45){
+            throw new Error(this.ERROR_MESSEGE_NOT_IN_RANGE);
+        }
+    }
+}
+export default LottoNumber;
