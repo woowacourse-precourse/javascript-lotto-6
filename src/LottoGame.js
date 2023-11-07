63c8fa5e2ed40dd308981f29c835de501fce8a68
SSY1203 <tlstjsdud566@naver.com>
2023-11-07 10:17:34 +0900

    [feat] 당첨 번호와 로또 일치 개수 계산

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -8,6 +8,8 @@ class LottoGame {
   #lottoList;
   #winningLotto;
   #bonusNumber;
+  #result;
+  #prize;
 
   async gameStart() {
     this.#lottoList = [];
@@ -17,10 +19,10 @@ class LottoGame {
       const randomNumbers = this.getRandomNumbers();
       this.#lottoList.push(new Lotto(randomNumbers));
     }
-    OutputView.printLottoCount(this.#lottoList);
-    this.#lottoList.map(lotto => {
-      OutputView.printLotto(lotto.numbers);
-    });
+    // OutputView.printLottoCount(this.#lottoList);
+    // this.#lottoList.map(lotto => {
+    //   OutputView.printLotto(lotto.numbers);
+    // });
     this.#winningLotto = new Lotto(await InputView.readWinningNumbers());
 
     try {
@@ -28,6 +30,7 @@ class LottoGame {
       if (this.#winningLotto.numbers.includes(this.#bonusNumber)) {
         throw new Error(ERROR_LOTTO_REPEAT);
       }
+      this.checkResult();
     } catch (error) {
       Console.print(error.message);
     }
@@ -44,6 +47,35 @@ class LottoGame {
     return sortedList;
   }
 
+  checkResult() {
+    this.#result = {
+      1: { count: 0, prize: 2_000_000_000 },
+      2: { count: 0, prize: 30_000_000 },
+      3: { count: 0, prize: 1_500_000 },
+      4: { count: 0, prize: 50_000 },
+      5: { count: 0, prize: 5_000 },
+    };
+    this.#lottoList.forEach(ticket => {
+      const matchingNumbers = ticket.numbers.filter(number =>
+        this.#winningLotto.numbers.includes(number),
+      );
+      console.log(matchingNumbers);
+
+      if (matchingNumbers.length === 6) {
+        this.#result[1].count++;
+      } else if (matchingNumbers.length === 5 && ticket.numbers.includes(this.#bonusNumber)) {
+        this.#result[2].count++;
+      } else if (matchingNumbers.length === 5) {
+        this.#result[3].count++;
+      } else if (matchingNumbers.length === 4) {
+        this.#result[4].count++;
+      } else if (matchingNumbers.length === 3) {
+        this.#result[5].count++;
+      }
+    });
+    console.log(this.#result);
+  }
+
   get lottoList() {
     return this.#lottoList;
   }
@@ -54,6 +86,10 @@ class LottoGame {
 
   get bonusNumber() {
     return this.#bonusNumber;
+  }
+
+  get result() {
+    return this.#result;
   }
 }
 
