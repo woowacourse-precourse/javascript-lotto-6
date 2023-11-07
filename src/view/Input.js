f090d65ad6496045a480102130c4063b6bd99cee
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-07 20:40:57 +0900

    ✨feat($Input): 구입 금액, 당첨 번호, 보너스 번호를 입력받는 기능 추가

--- src/view/Input.js
+++ src/view/Input.js
@@ -1,9 +1,28 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { GUIDE_MESSAGE } from "../constants/constants";
 
 class Input {
   async getValue(message) {
     const INPUT = await MissionUtils.Console.readLineAsync(message)
     return String(INPUT)
+  }
+
+  async getPurchaseAmount(){
+    const money = await MissionUtils.Console.readLineAsync(GUIDE_MESSAGE.insertMoney)
+
+    return money.trim()
+  }
+
+  async getWinningNumbers() {
+    const numbers = await MissionUtils.Console.readLineAsync(GUIDE_MESSAGE.insertWinnerNumbers)
+
+    return numbers.trim()
+  }
+
+  async getBonusNumber() {
+    const number = await MissionUtils.Console.readLineAsync(GUIDE_MESSAGE.insertBonusNumber)
+
+    return number.trim();
   }
 }
 
