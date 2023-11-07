c19bbaa4def1c6203655bd1992ca210988675e26
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-07 17:37:17 +0900

    feat: Lotto 클래스 유효성 검사 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,41 @@
 class Lotto {
-  #numbers;
+    /**
+     * @type {number[]}
+     */
+    #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    /**
+     * @param {number[]} numbers
+     */
+    constructor(numbers) {
+        this.#validate(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  // TODO: 추가 기능 구현
+    /**
+     * @param {number[]} numbers
+     */
+    #validate(numbers) {
+        if (!(numbers instanceof Array && numbers.length === 6)) {
+            throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+        }
+        if (!numbers.every(num => typeof num === 'number' && num)) {
+            throw new Error('[ERROR] 로또 번호는 1부터 45의 숫자만 사용하실 수 있습니다.');
+        }
+        if (!numbers.every(num => num >= 1 && num <= 45)) {
+            throw new Error('[ERROR] 로또 번호는 1부터 45의 숫자만 사용하실 수 있습니다.');
+        }
+        if (new Set(numbers).size !== 6) {
+            throw new Error('[ERROR] 중복된 번호는 사용하실 수 없습니다.');
+        }
+        if (!numbers.every((num, i) => i === 0 || num > numbers[i - 1])) {
+            throw new Error('[ERROR] 오름차순 정렬이 되어있지 않은 배열입니다.');
+        }
+    }
+
+    getNumbers() {
+        return this.#numbers;
+    }
 }
 
 export default Lotto;
