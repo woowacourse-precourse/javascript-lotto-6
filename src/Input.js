42ea5e20b1bee27899634f721a2f6e7cb0913805
sejineeee <sejinee0918@gmail.com>
2023-11-07 14:53:55 +0900

    refactor: 필요없는 console 제거

--- src/Input.js
+++ src/Input.js
@@ -13,7 +13,6 @@ class Input {
     const lotteryNumbers = await Console.readLineAsync(
       '당첨 번호를 입력해 주세요.'
     );
-    console.log(lotteryNumbers.split(','));
 
     return lotteryNumbers.split(',');
   }
