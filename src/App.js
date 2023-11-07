1945835c9d23e99ead94d6fa120ddb1b13ed2774
skdbsqls <skdbsqls98@naver.com>
2023-11-07 16:59:59 +0900

    feat(App): 로또 구입 로직 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+import purchaseService from "./services/purchaseService.js";
+
 class App {
-  async play() {}
+  #price;
+
+  async play() {
+    await this.#executePurchaseLotto();
+  }
+
+  // 로또 구입
+  async #executePurchaseLotto() {
+    try {
+      this.#price = await purchaseService();
+    } catch (error) {
+      Console.print(error.message);
+      return this.#executePurchaseLotto();
+    }
+  }
 }
 
 export default App;
