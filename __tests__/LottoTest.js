64e5e95133a966a0210658259c39107448618440
nijuy <youjin6325@naver.com>
2023-11-07 17:16:57 +0900

    test(Lotto): 로또 번호 출력 테스트
    - Lotto 객체의 numbers를 형식에 맞게 출력하는지 검사하기 위함

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,5 +1,6 @@
 import ERROR_MESSAGE from '../src/constant/ErrorMessage.js';
 import Lotto from '../src/Lotto.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -24,5 +25,16 @@ describe('로또 클래스 테스트', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 1.234]);
     }).toThrow(ERROR_MESSAGE.outOfRange);
+  });
+
+  test('로또 번호를 형식에 맞게 출력하기', () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+    logSpy.mockClear();
+
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const result = '[1, 2, 3, 4, 5, 6]';
+    lotto.print();
+
+    expect(logSpy).toHaveBeenCalledWith(result);
   });
 });
