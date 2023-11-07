9360b95e29901e71734aa170032b2f5f7c536aac
junghanlee <dlwjdgks126@hansung.ac.kr>
2023-11-07 23:17:02 +0900

    금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, Random } from "woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 class App {
-  async play() {}
+  async play() {
+    const PURCHASE_AMOUNT = this.inputPurchaseAmount();
+  }
+  async inputPurchaseAmount() {
+    const MONEY = await Console.readLineAsync("구입금액을 입력해 주세요.")
+
+    return MONEY / 1000;
+  }
 }
 
 export default App;
