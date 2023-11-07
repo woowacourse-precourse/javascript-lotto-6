89c08ba73a87a7b5149c6b25d810e1847655d504
djLee77 <dkdnl232@gmail.com>
2023-11-08 01:22:32 +0900

    feat(Constants): 상수들 클래스로 분리

--- src/utils/ValidateInput.js
+++ src/utils/ValidateInput.js
@@ -1,22 +1,13 @@
-const MIN_WINNING_NUMBER = 1;
-const MAX_WINNING_NUMBER = 45;
-const ERROR_MESSAGES = {
-  NATURAL_NUMBER: (subject) => `[ERROR] ${subject} 자연수로 입력해주세요.\n`,
-  NUMBER_RANGE: (subject) =>
-    `[ERROR] ${subject} ${MIN_WINNING_NUMBER}부터 ${MAX_WINNING_NUMBER}사이의 값을 입력해주세요.\n`,
-  MIN_AMOUNT: "[ERROR] 구입금액은 최소 1000원부터 가능합니다.\n",
-  AMOUNT_UNIT: "[ERROR] 구입금액은 1,000원 단위만 가능합니다.\n",
-  WINNING_NUMBER_COUNT:
-    "[ERROR] 당첨 번호는 쉼표(,)로 구분하여 6개 입력해주세요.\n",
-  DUPLICATE_NUMBER: "[ERROR] 번호들 중 중복된 값이 존재합니다.\n",
-  BONUS_NUMBER_DUPLICATE:
-    "[ERROR] 당첨 번호들 중 보너스 번호와 중복된 값이 존재합니다.\n",
-};
+import Constants from "../Constants.js";
 
 class ValidateInput {
   static validateNaturalNumber(number, subject) {
-    if (isNaN(number) || number < MIN_WINNING_NUMBER || number % 1 !== 0) {
-      throw new Error(ERROR_MESSAGES.NATURAL_NUMBER(subject));
+    if (
+      isNaN(number) ||
+      number < Constants.MIN_WINNING_NUMBER ||
+      number % 1 !== 0
+    ) {
+      throw new Error(Constants.ERROR_MESSAGES.NATURAL_NUMBER(subject));
     }
   }
 
@@ -24,23 +15,23 @@ class ValidateInput {
     ValidateInput.validateNaturalNumber(amount, "구입 금액은");
 
     if (amount < 1000) {
-      throw new Error(ERROR_MESSAGES.MIN_AMOUNT);
+      throw new Error(Constants.ERROR_MESSAGES.MIN_AMOUNT);
     }
 
     if (amount > 1000 && amount % 1000 !== 0) {
-      throw new Error(ERROR_MESSAGES.AMOUNT_UNIT);
+      throw new Error(Constants.ERROR_MESSAGES.AMOUNT_UNIT);
     }
   }
 
   static validateWinningNumber(numbers) {
     const subject = "당첨 번호는";
     if (numbers.length !== 6) {
-      throw new Error(ERROR_MESSAGES.WINNING_NUMBER_COUNT);
+      throw new Error(Constants.ERROR_MESSAGES.WINNING_NUMBER_COUNT);
     }
 
     const set = new Set(numbers);
     if (numbers.length !== set.size) {
-      throw new Error(ERROR_MESSAGES.DUPLICATE_NUMBER);
+      throw new Error(Constants.ERROR_MESSAGES.DUPLICATE_NUMBER);
     }
 
     numbers.forEach((number) => {
@@ -49,10 +40,12 @@ class ValidateInput {
 
     if (
       numbers.some(
-        (number) => number < MIN_WINNING_NUMBER || number > MAX_WINNING_NUMBER,
+        (number) =>
+          number < Constants.MIN_WINNING_NUMBER ||
+          number > Constants.MAX_WINNING_NUMBER,
       )
     ) {
-      throw new Error(ERROR_MESSAGES.NUMBER_RANGE(subject));
+      throw new Error(Constants.ERROR_MESSAGES.NUMBER_RANGE(subject));
     }
   }
 
@@ -60,12 +53,15 @@ class ValidateInput {
     const subject = "보너스 번호는";
     ValidateInput.validateNaturalNumber(number, subject);
 
-    if (number < MIN_WINNING_NUMBER || number > MAX_WINNING_NUMBER) {
-      throw new Error(ERROR_MESSAGES.NUMBER_RANGE(subject));
+    if (
+      number < Constants.MIN_WINNING_NUMBER ||
+      number > Constants.MAX_WINNING_NUMBER
+    ) {
+      throw new Error(Constants.ERROR_MESSAGES.NUMBER_RANGE(subject));
     }
 
     if (winningNumber.some((item) => item === number)) {
-      throw new Error(ERROR_MESSAGES.BONUS_NUMBER_DUPLICATE);
+      throw new Error(Constants.ERROR_MESSAGES.BONUS_NUMBER_DUPLICATE);
     }
   }
 }
