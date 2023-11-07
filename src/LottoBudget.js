fc8d1efe4370094778c07bf0c783984d8987d1e9
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-07 15:23:30 +0900

    Rename: isValid의 inputNumber 인수를 lottoBudget 인수로 이름 변경

--- src/LottoBudget.js
+++ src/LottoBudget.js
@@ -10,12 +10,12 @@ class LottoBudget {
         this.LOTTO_PRICE=LOTTO_PRICE;
     }
 
-    isValid(inputNumber){
-        if(inputNumber < this.MIN_LOTTO_NUM*this.LOTTO_PRICE || inputNumber > this.MAX_LOTTO_NUM*this.LOTTO_PRICE){
+    isValid(lottoBudget){
+        if(lottoBudget < this.MIN_LOTTO_NUM*this.LOTTO_PRICE || lottoBudget > this.MAX_LOTTO_NUM*this.LOTTO_PRICE){
             throw new Error(this.ERROR_MESSEGE);
         }
 
-        if(inputNumber % this.LOTTO_PRICE!==0){
+        if(lottoBudget % this.LOTTO_PRICE!==0){
             throw new Error(this.ERROR_MESSEGE);
         }
     }
