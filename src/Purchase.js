fc61fae72db1d91357b769ef4215429c41d30de2
Dawon00 <dawon000203@gmail.com>
2023-11-08 03:11:55 +0900

    Refactor: 구입금액 예외처리 분리

--- src/Purchase.js
+++ src/Purchase.js
@@ -2,22 +2,40 @@ class Purchase {
     #amount;
 
     constructor(amount) {
+        this.#validate(amount);
         this.#amount = amount;
     }
 
     #validate(amount) {
-        if (!this.isValidAmount(amount)) {
-            throw new Error("[ERROR] 유효한 구입금액을 입력해주세요.");
+        if (!this.isNotNaN(amount)) {
+            throw new Error("[ERROR] 숫자를 입력해주세요.");
         }
+        if (!this.isOver1000(amount)) {
+            throw new Error("[ERROR] 1000원 이상의 금액을 입력해주세요.");
+        }
+        if (!this.isUnder1000000(amount)) {
+            throw new Error("[ERROR] 1000000원 이하의 금액을 입력해주세요.");
+        }
+        if (!this.isMultipleOf1000(amount)) {
+            throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
+        }
+        this.#amount = amount;
     }
 
-    isValidAmount(amount) {
-        return (
-            !isNaN(amount) &&
-            amount >= 1000 &&
-            amount <= 1000000 &&
-            amount % 1000 === 0
-        );
+    isNotNaN(amount) {
+        return !isNaN(amount);
+    }
+
+    isOver1000(amount) {
+        return amount >= 1000;
+    }
+
+    isUnder1000000(amount) {
+        return amount <= 1000000;
+    }
+
+    isMultipleOf1000(amount) {
+        return amount % 1000 === 0;
     }
 
     getAmount() {
