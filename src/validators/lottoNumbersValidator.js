72fea9d56899dd92b1ebb877d2dbaa2f3a51100f
hwinkr <dnddl8280@naver.com>
2023-11-07 21:57:39 +0900

    feat(lottoNumbersValidator): 로또 번호 유효성 검사 함수 구현
    - 모든 번호들이 숫자가 아닌 경우
    - 번호들의 갯수가 6이 아닌 경우
    - 1 ~ 45 사이의 숫자가 아닌 경우
    - 중복된 숫자가 있을 경우 InvalidInputException을 발생시킴

---
+++ src/validators/lottoNumbersValidator.js
@@ -0,0 +1,37 @@
+import { ERROR_MESSAGES, LOTTO_RULES } from '../constants/lotto.js';
+
+import InvalidInputException from '../exceptions/InvalidInputError.js';
+
+const validateType = (numbers) => {
+  if (!numbers.every((number) => typeof number === 'number' && !Number.isNaN(number))) {
+    throw new InvalidInputException(ERROR_MESSAGES.invalidLottoType);
+  }
+};
+
+const validateCount = (numbers) => {
+  if (numbers.length !== LOTTO_RULES.lottoNumberCount) {
+    throw new InvalidInputException(ERROR_MESSAGES.invalidLottoCount);
+  }
+};
+
+const validateRange = (numbers) => {
+  if (numbers.some((number) => number < LOTTO_RULES.minLottoNumber || number > LOTTO_RULES.maxLottoNumber)) {
+    throw new InvalidInputException(ERROR_MESSAGES.invalidLottoRange);
+  }
+};
+
+const validateUniqueness = (numbers) => {
+  const uniqueNumbers = new Set(numbers);
+  if (uniqueNumbers.size !== numbers.length) {
+    throw new InvalidInputException(ERROR_MESSAGES.duplicatedWinningNumber);
+  }
+};
+
+const lottoNumberValidator = {
+  validateType,
+  validateCount,
+  validateRange,
+  validateUniqueness,
+};
+
+export default lottoNumberValidator;
