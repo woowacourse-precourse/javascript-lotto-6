4bf128d4b994a5c201b272f379dc02114bc578ee
yjh1008 <exesea12@gmail.com>
2023-11-08 00:50:41 +0900

    fixed : airbnb 코드 컨벤션 적용

--- src/GameComment..js
+++ src/GameComment..js
@@ -1,34 +1,36 @@
-const BUY_LOTTO = "구맥 금액을 입력해주세요.";
+const BUY_LOTTO = '구맥 금액을 입력해주세요.';
 
 const LOTTO_COUNT = (count) => `${count}개를 구매했습니다.`;
 
-const PRINT_PURCHASE_LOTTO = (lotto) => `[${lotto.join(", ")}]\n`;
+const PRINT_PURCHASE_LOTTO = (lotto) => `[${lotto.join(', ')}]\n`;
 
-const INPUT_WINNING_NUMBER = "당첨 번호를 입력해주세요";
+const INPUT_WINNING_NUMBER = '당첨 번호를 입력해주세요';
 
-const WINNING_OUTCOME = (arr, outcomes) => {
+const WINNING_OUTCOME = (arr) => {
   return `3개 일치 (5,000원) - ${arr[0]}개\n
 4개 일치 (50,000원) - ${arr[1]}개\n
 5개 일치 (1,500,000원) - ${arr[2]}개\n
 5개 일치, 보너스 볼 일치 (30,000,000원) - ${arr[3]}개\n
-6개 일치 (2,000,000,000원) - ${arr[4]}개\n`
-}
+6개 일치 (2,000,000,000원) - ${arr[4]}개\n`;
+};
 
-const GENERATE_BONUS_NUM = "보너스 번호를 입력해 주세요.";
+const GENERATE_BONUS_NUM = '보너스 번호를 입력해 주세요.';
 
-const RATIO_OF_RETURN = (result, price, outcomes) =>{
+const RATIO_OF_RETURN = (result, price, outcomes) => {
   let totalOutcome = 0;
-  result.forEach((v, i) => totalOutcome += v * outcomes[i]);
-  const ratio = Math.round((totalOutcome/price * 100) *10)/10;
-  return `총 수익률은 ${ratio}%입니다.`
-} 
+  result.forEach((v, i) => {
+    totalOutcome += v * outcomes[i];
+  });
+  const ratio = Math.round((totalOutcome / price) * 100 * 10) / 10;
+  return `총 수익률은 ${ratio}%입니다.`;
+};
 
 export {
-  BUY_LOTTO, 
+  BUY_LOTTO,
   LOTTO_COUNT,
   PRINT_PURCHASE_LOTTO,
   INPUT_WINNING_NUMBER,
   WINNING_OUTCOME,
   GENERATE_BONUS_NUM,
-  RATIO_OF_RETURN
-}
+  RATIO_OF_RETURN,
+};
--- src/Lotto.js
+++ src/Lotto.js
@@ -8,35 +8,35 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
     this.isNumber(numbers);
     const set = new Set([...numbers]);
-    if(set.size !== numbers.length) {
-      throw new Error("[ERROR]");
+    if (set.size !== numbers.length) {
+      throw new Error('[ERROR]');
     }
     this.rangeException(numbers);
   }
 
   rangeException(numbers) {
-    for(let i=0;i<numbers.length;i++) {
-      if(numbers[i] < 1 || numbers[i] > 45) {
-        throw new Error("[ERROR] 1이상 45이하의 값을 입력해주세요.");
+    for (let i = 0; i < numbers.length; i += 1) {
+      if (numbers[i] < 1 || numbers[i] > 45) {
+        throw new Error('[ERROR] 1이상 45이하의 값을 입력해주세요.');
       }
     }
   }
 
   isNumber(numbers) {
     const REX = /[0-9]/;
-    for(let i=0;i<numbers.length;i++) {
-      if(!REX.test(numbers[i])) {
-        throw new Error("[ERROR] 숫자를 입력해주세요.");
+    for (let i = 0; i < numbers.length; i += 1) {
+      if (!REX.test(numbers[i])) {
+        throw new Error('[ERROR] 숫자를 입력해주세요.');
       }
     }
   }
 
   showLotto() {
-    return `${this.#numbers.sort((a,b)=>a-b).join(",")}\n`;
+    return `${this.#numbers.sort((a, b) => a - b).join(',')}\n`;
   }
   // TODO: 추가 기능 구현
 
@@ -44,16 +44,15 @@ class Lotto {
     return this.#numbers;
   }
 
-  matchNums ( winningNums, bonusNum ) {
+  matchNums(winningNums, bonusNum) {
     let matches = -3;
-    winningNums.forEach(v=>{
-      if(this.#numbers.includes(v)) matches++;
-    });   
-    if(matches < 0) return -1;
-    if(matches === 5 && this.#numbers.includes(bonusNum))  return matches+1;
+    winningNums.forEach((v) => {
+      if (this.#numbers.includes(v)) matches += 1;
+    });
+    if (matches < 0) return -1;
+    if (matches === 5 && this.#numbers.includes(bonusNum)) return matches + 1;
     return matches;
   }
-
 }
 
 export default Lotto;
