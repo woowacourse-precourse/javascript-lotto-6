da7f1440356f1139d908dd116d83e729a7cf5cfe
Collection50 <fkaus65@naver.com>
2023-11-07 22:39:11 +0900

    feat(Calculator): 로또와 당첨번호, 보너스 번호를 토대로 당첨 순위를 계산하는 countMatch, getRank, calculateRanks 메서드 구현

---
+++ src/Caculator/index.js
@@ -0,0 +1,50 @@
+import { RANK, RANK_MESSAGE } from '../constants/index.js';
+
+class Calculator {
+  #lottos;
+
+  #winningNumbers;
+
+  #bonusNumber;
+
+  constructor(lottos, winningNumbers, bonusNumber) {
+    this.#lottos = lottos;
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNumber = bonusNumber;
+  }
+
+  #countMatch(lotto) {
+    return lotto.filter((number) => this.#winningNumbers.includes(number))
+      .length;
+  }
+
+  #getRank(lotto) {
+    const matchCount = this.#countMatch(lotto, this.#winningNumbers);
+    const hasBonusNumber = lotto.includes(this.#bonusNumber);
+    if (matchCount === RANK.first) {
+      return RANK_MESSAGE.first;
+    }
+    if (matchCount === RANK.second) {
+      return hasBonusNumber ? RANK_MESSAGE.second : RANK_MESSAGE.third;
+    }
+    if (matchCount === RANK.fourth) {
+      return RANK_MESSAGE.fourth;
+    }
+    return matchCount === RANK.fifth ? RANK_MESSAGE.fifth : RANK_MESSAGE.none;
+  }
+
+  calculateRanks() {
+    const ranks = { fifth: 0, fourth: 0, third: 0, second: 0, first: 0 };
+
+    this.#lottos.forEach((lotto) => {
+      const rank = this.#getRank(lotto.getLotto());
+
+      if (rank !== RANK_MESSAGE.none) {
+        ranks[rank] += 1;
+      }
+    });
+    return ranks;
+  }
+}
+
+export default Calculator;
