b5a9fac72b26621d0d7f8c5a42afd43ccdca5047
JungHun98 <j7papa@naver.com>
2023-11-07 14:50:26 +0900

    test: 유효성 검사 테스트 코드 수정 구입 금액 유효성 테스트

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -8,13 +8,17 @@ describe('입력값 유효성 검사 테스트', () => {
       const cost = '14000';
       expect(isValidCost(cost)).toBeTruthy();
     });
-    test('입력 금액이 1000원 단위로 나누어 떨어지지 않으면 false를 반환한다.', () => {
+    test('입력 금액이 1000원 단위로 나누어 떨어지지 않으면 throw Error', () => {
       const cost = '14010';
-      expect(isValidCost(cost)).toBeFalsy();
+      expect(() => {
+        isValidCost(cost);
+      }).toThrow();
     });
-    test('입력 금액은 0-9까지의 숫자로 이루어지지 않는다면 false를 반환한다.', () => {
+    test('입력 금액은 0-9까지의 숫자로 이루어지지 않는다면 throw Error', () => {
       const cost = '140e10';
-      expect(isValidCost(cost)).toBeFalsy();
+      expect(() => {
+        isValidCost(cost);
+      }).toThrow();
     });
   });
 
