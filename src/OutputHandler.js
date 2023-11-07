5ac99eb7b3cd029a7597d8501e9eb2429873ad0f
iamreum <102291571+iamreum@users.noreply.github.com>
2023-11-08 00:30:51 +0900

    출력 클래스 및 메서드 구현

---
+++ src/OutputHandler.js
@@ -0,0 +1,39 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export default class OutputHandler {
+    static lottoPurchaseAmount() {
+        MissionUtils.Console.print("구입금액을 입력해 주세요.");
+    }
+
+    static numberOfLottoPurchase(lottos) {
+        MissionUtils.Console.print(`${lottos.length}개를 구매했습니다.`);
+        lottos.forEach(lotto => {
+            MissionUtils.Console.print(`[${lotto.numbers.join(', ')}]`);
+        });
+    }
+
+    static getWinningNumbers() {
+        MissionUtils.Console.print("당첨 번호를 입력해 주세요.");
+    }
+
+    static getBonusNumbers() {
+        MissionUtils.Console.print("보너스 번호를 입력해 주세요.");
+    }
+
+    static winningStatistics(statistics) {
+        const statisticsMessage = [
+            "당첨 통계",
+            "---",
+            `3개 일치 (5,000원) - ${statistics.three}개`,
+            `4개 일치 (50,000원) - ${statistics.four}개`,
+            `5개 일치 (1,500,000원) - ${statistics.five}개`,
+            `5개 일치, 보너스 볼 일치 (30,000,000원) - ${statistics.fiveWithBonus}개`,
+            `6개 일치 (2,000,000,000원) - ${statistics.six}개`,
+        ].join('\n');
+        MissionUtils.Console.print(statisticsMessage);
+    }
+
+    static totalProfitRate(profitRate) {
+        MissionUtils.Console.print(`총 수익률은 ${profitRate}%입니다.`);
+    }
+}
