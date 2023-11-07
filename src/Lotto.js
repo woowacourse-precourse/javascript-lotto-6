708feb972a53a6726c40f5fc9503ec20195e4aee
dante0214 <rn_chic@naver.com>
2023-11-07 14:52:15 +0900

    feat: 증복에 관한 예외사항 추가
    발행된 로또의 번호가 6개가 넘어가지 않게 검증부에 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,9 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다");
+    }
   }
   #printLottoNumbers() {
     Console.print(`[${this.#numbers.join(", ")}]`);
