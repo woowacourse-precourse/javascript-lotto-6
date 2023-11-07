9fa109b955ab5732bcf4748cc6daa61a0176d13d
Dobbymin <gmin0701@knu.ac.kr>
2023-11-07 22:04:57 +0900

    ✨ feat(texts): 에러 출력문과 로또 게임 관련 출력문

---
+++ src/pages/texts.js
@@ -0,0 +1,13 @@
+export const ERROR = {
+    numeric: '[ERROR] 숫자만 입력해주세요!',
+    include_0: '[ERROR] 1 ~ 45 사이의 숫자만 입력해주세요!',
+    length: '[ERROR] 숫자는 6개를 입력해주세요!',
+    different_number: '[ERROR] ',
+};
+
+export const LOTTO = {
+    buy: '구입금액을 입력해 주세요.\n',
+    winning_number: '당첨 번호를 입력해 주세요.\n',
+    bonus_number: '보너스 번호를 입력해 주세요.\n',
+    total_winner: '당첨 통계\n---\n',
+};
