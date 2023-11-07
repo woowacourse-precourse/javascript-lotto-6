6bce63cebf0f7595b88cf0a34aa343219c1f079c
6puritans9 <6puritans9@gmail.com>
2023-11-07 15:38:10 +0900

    chore(Lotto): Modified method to match the original code

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,12 +12,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    // TODO: 추가 기능 구현
     if (new Set(numbers).size < numbers.length) {
       throw new Error("[ERROR] 로또 번호에 중복된 숫자가 포함될 수 없습니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
   show() {
     return this.#numbers;
   }
