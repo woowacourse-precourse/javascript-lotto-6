dde75efcd20da55067ebcc87c8df1e4552ddaf3b
Goonco <s07019@naver.com>
2023-11-07 16:37:07 +0900

    :sparkles: 예외의 형식에 맞게 출력하는 기능 추가

--- src/view/output.js
+++ src/view/output.js
@@ -3,6 +3,10 @@ import { Console } from "@woowacourse/mission-utils";
 class Output{
     constructor(){}
 
+    static outputError(errorMessage){
+        Console.print(`[ERROR] ${errorMessage}`);
+    }
+
     static outputMessage(message){
         Console.print(message);
     }
