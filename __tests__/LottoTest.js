b52251f5141cc62431a17f3f8355452b4bb5927d
suyeon <moa0823@naver.com>
2023-11-08 01:49:53 +0900

    test : 로또 발행 테스트
    - 8000원을 입력한 경우 8개의 로또 발행 메시지를 출력하는지 확인하는 테스트 구현
    - 2개의 랜덤 로또 발행 테스트 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,5 +1,6 @@
 import Lotto from "../src/Lotto.js";
 import User from "../src/User.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -37,16 +38,37 @@ describe("로또 구매 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // describe("로또 발행 테스트", () => {
-  //   test("8000원은 입력한 경우 8개의 로또를 발행한다.", () => {
-  //     const consoleSpy = jest.spyOn(console, "log");
+  describe("로또 발행 테스트", () => {
+    test("8000원을 입력하면 8개의 로또 발행 메시지를 출력한다.", () => {
+      const consoleSpy = jest.spyOn(console, "log");
 
-  //     const user = new User();
-  //     user.calculateCount(8000);
+      const user = new User();
+      user.calculateCount(8000);
 
-  //     expect(consoleSpy).toHaveBeenCalledWith("\n8개를 구매했습니다.");
+      expect(consoleSpy).toHaveBeenCalledWith("\n8개를 구매했습니다.");
 
-  //     consoleSpy.mockRestore();
-  //   });
-  // });
+      consoleSpy.mockRestore();
+    });
+
+    test("2개의 랜덤 로또를 발행한다.", () => {
+      const mockPickUniqueNumbersInRange = jest.spyOn(
+        MissionUtils.Random,
+        "pickUniqueNumbersInRange"
+      );
+      mockPickUniqueNumbersInRange
+        .mockReturnValueOnce([1, 2, 3, 4, 5, 6])
+        .mockReturnValueOnce([7, 8, 9, 10, 11, 12]);
+
+      const consoleSpy = jest.spyOn(console, "log");
+
+      const user = new User();
+      user.createLotto(2);
+
+      expect(consoleSpy).toHaveBeenCalledWith("[1, 2, 3, 4, 5, 6]");
+      expect(consoleSpy).toHaveBeenCalledWith("[7, 8, 9, 10, 11, 12]");
+
+      consoleSpy.mockRestore();
+      mockPickUniqueNumbersInRange.mockRestore();
+    });
+  });
 });
