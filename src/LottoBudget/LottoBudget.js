b329541441bf9c477ddecb4e0f86bb2c8677c91e
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-07 14:51:42 +0900

    feat: LottoBudget 클래스 구현
    구입 금액 입력을 요구하는 문구 출력 기능, 구입 금액을 입력받고 구입 금액이 유효한지 확인하는 기능 구현

---
+++ src/LottoBudget/LottoBudget.js
@@ -0,0 +1,29 @@
+import { Console } from "@woowacourse/mission-utils";
+class LottoBudget {
+    lottoBudget;
+    MIN_LOTTO_NUM=0;
+    MAX_LOTTO_NUM=10;
+    LOTTO_PRICE=1000;
+    ERROR_MESSEGE="[ERROR] 숫자가 잘못된 형식입니다.";
+    GET_LOTTO_BUDGET_INPUT="구입금액을 입력해 주세요.\n";
+    constructor() {
+    }
+
+    isValid(inputNumber){
+        if(inputNumber < this.MIN_LOTTO_NUM*this.LOTTO_PRICE || inputNumber > this.MAX_LOTTO_NUM*this.LOTTO_PRICE){
+            throw new Error(this.ERROR_MESSEGE);
+        }
+
+        if(inputNumber % this.LOTTO_PRICE!==0){
+            throw new Error(this.ERROR_MESSEGE);
+        }
+    }
+
+    async getInput() {
+        this.lottoBudget = parseInt(await Console.readLineAsync(this.GET_LOTTO_BUDGET_INPUT));
+        this.isValid(this.lottoBudget);
+    }
+
+}
+
+export default LottoBudget.js
