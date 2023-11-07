9ce5d1cac88f79719fe181c809bc57e75d9aba1a
sehyun <e6d1fe@gmail.com>
2023-11-07 23:05:49 +0900

    style: 상수 뒤 newline 삭제

--- src/Constants.js
+++ src/Constants.js
@@ -3,8 +3,8 @@ export const message = Object.freeze({
   ENTER_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
   ENTER_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
   PRINT_PURCHASE_AMOUNT: '개를 구매했습니다.\n',
-  PRINT_WIN_STATISTICS: '당첨 통계\n',
-  PRINT_DIVIDER: '---\n',
+  PRINT_WIN_STATISTICS: '당첨 통계',
+  PRINT_DIVIDER: '---',
   PRINT_MATCH_3: '3개 일치 (5,000원) - ',
   PRINT_MATCH_4: '4개 일치 (50,000원) - ',
   PRINT_MATCH_5: '5개 일치 (1,500,000원) - ',
@@ -12,7 +12,7 @@ export const message = Object.freeze({
   PRINT_MATCH_6: '6개 일치 (2,000,000,000원) - ',
   PRINT_GROSS_RETURN_START: '총 수익률은 ',
   PRINT_GROSS_RETURN_END: '%입니다.',
-  COUNT_UNIT: '개\n',
+  COUNT_UNIT: '개',
   ERROR: '[ERROR] 숫자가 잘못된 형식입니다.',
 });
 
