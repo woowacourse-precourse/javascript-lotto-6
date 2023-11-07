64d20f554e1b6d7bd4bbc8975b6dfcf9e5d13337
Jihyun_Kim <jihyun3313@daum.net>
2023-11-07 22:12:29 +0900

    feat: 로또 일치 개수 판별 구현

--- src/model/Lotto.js
+++ src/model/Lotto.js
@@ -1,4 +1,5 @@
 import { ERROR_MESSAGE } from "../constatns/message.js";
+import { RESULT } from "../constatns/number.js";
 
 class Lotto {
   #numbers;
@@ -6,6 +7,7 @@ class Lotto {
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    this.result = [0, 0, 0, 0, 0];  // 당첨 결과 저장 배열
   }
 
   #validate(numbers) {
@@ -19,7 +21,38 @@ class Lotto {
     })
   }
 
-  // TODO: 추가 기능 구현
+  lottoMatchStart(randomNumbersArray, bonusNumber) {
+    randomNumbersArray.forEach((randomNumbers) => {
+      let count = this.getMatchNumberCount(randomNumbers);
+      let isBonus = this.isBonusNumber(bonusNumber, randomNumbers);
+      this.storeResult(count, isBonus);
+    })
+    return this.result;
+  }
+
+  getMatchNumberCount(randomNumbers) {
+    let count = randomNumbers.filter((num) =>
+      this.#numbers.includes(num)
+    ).length;
+    return count;
+  }
+
+  isBonusNumber(bonusNumber, randomNumbers) {
+    return randomNumbers.includes(Number(bonusNumber));
+  }
+
+  storeResult(count, isBonus) {
+    if (count === 3) this.result[RESULT.THREE] += 1;
+
+    if (count === 4) this.result[RESULT.FOUR] += 1;
+
+    if (count === 5)
+      isBonus
+        ? this.result[RESULT.FIVE_BONUS] += 1
+        : this.result[RESULT.FIVE] += 1;
+
+    if (count === 6) this.result[RESULT.SIX] += 1
+  }
 }
 
 export default Lotto;
