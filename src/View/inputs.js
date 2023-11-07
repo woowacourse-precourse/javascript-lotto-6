e0898a62c10e9d6950336c4b7eda40935a8693b0
8leamnk <silkviewrk@gmail.com>
2023-11-07 17:43:05 +0900

    style(inputs): 공백이 필요한 부분에 공백 처리

--- src/View/inputs.js
+++ src/View/inputs.js
@@ -4,16 +4,19 @@ import MESSAGE from '../constants/message.js';
 const inputs = {
   async enterPrice() {
     const priceAnswer = await Console.readLineAsync(MESSAGE.input.price);
+
     return priceAnswer;
   },
 
   async enterLotto() {
     const lottoAnswer = await Console.readLineAsync(MESSAGE.input.lotto);
+
     return lottoAnswer;
   },
 
   async enterBonus() {
     const bonusAnswer = await Console.readLineAsync(MESSAGE.input.bonus);
+
     return bonusAnswer;
   },
 };
