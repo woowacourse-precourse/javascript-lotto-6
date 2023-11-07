2d8d6750e7127fa161a1ebcca9b1afda24418777
0jenn0 <jenn0.6n@gmail.com>
2023-11-07 17:29:27 +0900

    Refactor: LottoPurchaseController에서 랜덤으로 번호 만들기 부분 함수로 분리
    - 분리하면서 1,45,6 하드코딩되어있던 숫자들도 상수사용 함

--- src/Controllers/LottoPurchaseController.js
+++ src/Controllers/LottoPurchaseController.js
@@ -1,6 +1,7 @@
 import { Lotto, PurchaseAmount } from "../Models/index.js";
 import { Random } from "@woowacourse/mission-utils";
 import { InputView, OutputView } from "../Views/index.js";
+import { LottoSettings } from "../config/gameSetting.js";
 
 export default class LottoPurchaseController {
   static async purchaseLottos() {
@@ -29,10 +30,22 @@ export default class LottoPurchaseController {
   static #createLottos(numberOfLottos) {
     const lottos = [];
     Array.from({ length: numberOfLottos }, () => {
-      lottos.push(
-        new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6)).getNumbers()
-      );
+      const randomLottoNumbers = this.#createRandomLottoNumbers();
+      lottos.push(new Lotto(randomLottoNumbers).getNumbers());
     });
     return lottos;
+  }
+
+  static #createRandomLottoNumbers() {
+    const setting = new LottoSettings();
+    const { minOfLottoNumberRange, maxOfLottoNumberRange } =
+      setting.getLottoNumberRange();
+
+    const numberPerLotto = setting.getNumberPerLotto();
+    return Random.pickUniqueNumbersInRange(
+      minOfLottoNumberRange,
+      maxOfLottoNumberRange,
+      numberPerLotto
+    );
   }
 }
