2f5dab6bf4604a6a0920e93232a046cdd5e990b0
leejaejae <leesengjie@naver.com>
2023-11-07 16:25:15 +0900

    fix(출력 메세지 형태): 로또 배열 출력 형태 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   async play() {
     const lottoMoney = await this.getLottoMoney(); // 로또 구매할 금액 입력
     const lottoNumbers = this.generateLottoNumbers(lottoMoney / 1000);  // 갯수에 맞게 랜덤 생성되는 로또
-    this.showLottoNumbers(lottoNumbers)  // 랜던 생성된 로또 출력
+    this.showLottoNumbers(lottoNumbers)  // 랜던 생성된 로또 출력 
     const { mainNumbers, bonusNumber } = await this.getMyNumbers();  // 당첨 번호 입력 받기
     const matchCounts = this.checkResult(lottoNumbers, mainNumbers, bonusNumber);  // 당첨 번호와 보너스 번호 전달, 우승 값 계산
     this.printResults(matchCounts);  // 우승값 출력
@@ -36,22 +36,20 @@ class App {
   }
 
   showLottoNumbers(lottoNumbers) {  // 랜덤 생성된 로또 배열 출력
-    Console.print(`${lottoNumbers.length}개를 구매했습니다.`);
+    Console.print(`\n${lottoNumbers.length}개를 구매했습니다.`);
     for (const numbers of lottoNumbers) {
-      Console.print(`[${numbers.join(",")}]`);
+      Console.print(`[${numbers.join(", ")}]`);
     }
-    Console.print("\n")
   }
 
   async getMyNumbers() {  // 당첨 번호 입력 받기
     try {
-      const winningInput = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+      const winningInput = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
       const winningNumbers = winningInput.split(",").map(num => parseInt(num.trim()));
       if (winningNumbers.length !== 6 || winningNumbers.some(num => num < 1 || num > 45)) {
         throw new Error("[ERROR] 1부터 45까지의 6개 번호를 입력해 주세요.");
       }
-      Console.print("\n");
-      const bonusInput = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+      const bonusInput = await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
       const bonusNumber = parseInt(bonusInput.trim());
       if (bonusNumber < 1 || bonusNumber > 45 || winningNumbers.includes(bonusNumber)) {
         throw new Error("[ERROR] 1부터 45까지의 번호 중에서 중복되지 않게 선책해 주세요.");
