6955bb46b955a4c13680dd2a080775d020252ed5
today-is-first <diligent0644@gmail.com>
2023-11-07 15:54:00 +0900

    fix: 테스트 오류로 인한 수정

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -12,7 +12,7 @@ import Lotto from '../Lotto.js';
 class LottoGame {
   async lottoProcess() {
     this.money = await getInputPurchasingMoney();
-    this.generateUserLottos();
+    await this.generateUserLottos();
     this.winningNumbers = await getInputWinningNumbers();
     this.bonusNumber = await getInputBonusNumber(this.winningNumbers);
     this.checkWinning();
