f1dcaa4dee50ee5daa6172d95cdbb188374f62da
djLee77 <dkdnl232@gmail.com>
2023-11-08 01:36:53 +0900

    feat(LottoConstants): LottoGame 문자열 상수 클래스 생성

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,12 +1,5 @@
 import Lotto from "./Lotto.js";
-
-const PRIZE_MONEY = {
-  "3개 일치 (5,000원)": 5000,
-  "4개 일치 (50,000원)": 50000,
-  "5개 일치 (1,500,000원)": 1500000,
-  "5개 일치, 보너스 볼 일치 (30,000,000원)": 30000000,
-  "6개 일치 (2,000,000,000원)": 2000000000,
-};
+import LottoConstants from "./LottoConstants.js";
 
 class LottoGame {
   constructor(myLotto) {
@@ -33,9 +26,9 @@ class LottoGame {
   generateStatistics() {
     this.statistics = "";
     Object.entries(this.results).forEach(([key, value]) => {
-      if (key !== "None") {
+      if (key !== LottoConstants.NONE) {
         this.statistics += `\n${key} - ${value}개`;
-        this.reward += PRIZE_MONEY[key] * value;
+        this.reward += LottoConstants.PRIZE_MONEY[key] * value;
       }
     });
   }
