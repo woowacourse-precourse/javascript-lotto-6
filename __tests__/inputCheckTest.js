f5c06c4e0a699a9c88578145aa8268f58afd49a8
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-11-08 00:32:43 +0900

    test: refacotring message
    constants.js was used for test refactoring. added some new test codes.

--- __tests__/inputCheckTest.js
+++ __tests__/inputCheckTest.js
@@ -1,7 +1,7 @@
-import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
+import CONSTANT from "../src/constant/constants.js";
+import App from "../src/App.js";
 
-// MissionUtils.Console.readLineAsync를 Mock 함수로 대체합니다.
 jest.mock("@woowacourse/mission-utils", () => ({
   MissionUtils: {
     Console: {
@@ -10,36 +10,40 @@ jest.mock("@woowacourse/mission-utils", () => ({
   },
 }));
 
-describe("Input check test", () => {
-  test("Input amount must be a multiple of 1000", async () => {
-    // Mock 함수가 특정 값을 반환하도록 설정합니다.
-    MissionUtils.Console.readLineAsync.mockResolvedValue("2500");
-
-    const app = new App();
-    // async 함수이므로 await를 사용해야 합니다.
-    // 비동기 함수 내에서 예외가 발생하는지 확인하려면, 비동기 로직을 await하는 Promise를 expect에 전달합니다.
-    await expect(app.getInputPurchaseAmount()).rejects.toThrow(
-      "구입 금액은 1000원 단위로 입력해야 합니다."
-    );
-  });
-
-  test("구입 금액은 숫자여야 합니다.", async () => {
-    MissionUtils.Console.readLineAsync.mockResolvedValue("asdf");
-
-    const app = new App();
-    await expect(app.getInputPurchaseAmount()).rejects.toThrow(
-      "구입 금액은 숫자여야 합니다."
-    );
-  });
-
+describe("Input check test for winning numbers", () => {
   test("당첨 번호는 6개여야 합니다.", async () => {
-    MissionUtils.Console.readLineAsync
-      .mockResolvedValueOnce("1,2,3,4,5")
-      .mockResolvedValueOnce("45");
-
+    MissionUtils.Console.readLineAsync.mockResolvedValueOnce("1,2,3,4,5");
     const app = new App();
     await expect(app.getInputWinningNumbers()).rejects.toThrow(
-      "당첨 번호는 6개여야 합니다."
+      CONSTANT.ERR_LOG.WIN_LENGTH
+    );
+  });
+
+  test("당첨 번호는 숫자여야 합니다", async () => {
+    MissionUtils.Console.readLineAsync.mockResolvedValueOnce("1,2,3,4,5,A");
+    const app = new App();
+    await expect(app.getInputWinningNumbers()).rejects.toThrow(
+      CONSTANT.ERR_LOG.NAN
+    );
+  });
+
+  test("보너스 번호가 올바르지 않습니다.", async () => {
+    MissionUtils.Console.readLineAsync
+      .mockResolvedValueOnce("1,2,3,4,5,6")
+      .mockResolvedValueOnce("A");
+    const app = new App();
+    await expect(app.getInputWinningNumbers()).rejects.toThrow(
+      CONSTANT.ERR_LOG.WRONG_BONUS
+    );
+  });
+
+  test("보너스 번호는 당첨 번호와 중복될 수 없습니다.", async () => {
+    MissionUtils.Console.readLineAsync
+      .mockResolvedValueOnce("1,2,3,4,5,6")
+      .mockResolvedValueOnce("6");
+    const app = new App();
+    await expect(app.getInputWinningNumbers()).rejects.toThrow(
+      CONSTANT.ERR_LOG.REPEATED_BONUS
     );
   });
 });
