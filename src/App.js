85f400cebee38b2c04e8e4b0cda2f4bc2e361fb3
JungHun98 <j7papa@naver.com>
2023-11-07 16:18:09 +0900

    feat: App 클래스 입력 단계 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import input from './utils/input.js';
 import print from './utils/print.js';
+import getRandomUniqueNumbers from './utils/getRandomUniqueNumbers.js';
 import Lotto from './Lotto.js';
+import isValidCost from './modules/isValidCost.js';
+import isValidWinnerNumber from './modules/isValidWinnerNumber.js';
+import isValidBonusNumber from './modules/isValidBonusNumber.js';
+
+const inputStep = {
+  cost: {
+    prompt: '구입금액을 입력해 주세요.\n',
+    validate: isValidCost,
+  },
+  winnerNumbers: {
+    prompt: '당첨 번호를 입력해 주세요\n',
+    validate: isValidWinnerNumber,
+  },
+  bonusNumber: {
+    prompt: '보너스번호를 입력해 주세요\n',
+    validate: isValidBonusNumber,
+  },
+};
 
 class App {
   #lottos;
@@ -25,7 +44,26 @@ class App {
     });
   }
 
-  async play() {}
+  async play() {
+    const cost = await this.getInput(inputStep.cost.prompt, inputStep.cost.validate);
+    this.createLotto(cost);
+    print(`${cost / 1000}개를 구매했습니다.`);
+
+    this.#lottos.forEach((lotto) => {
+      print(`[${lotto.getNumbers().join(', ')}]`);
+    });
+
+    let winnerNumbers = await this.getInput(
+      inputStep.winnerNumbers.prompt,
+      inputStep.winnerNumbers.validate,
+    );
+    winnerNumbers = winnerNumbers.split(',').map((elem) => Number(elem));
+
+    let bonusNumber = await this.getInput(inputStep.bonusNumber.prompt, (input) => {
+      inputStep.bonusNumber.validate(input, winnerNumbers);
+    });
+    bonusNumber = Number(bonusNumber);
+  }
 }
 
 export default App;
