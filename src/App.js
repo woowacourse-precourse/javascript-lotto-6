2252f01fe2d09f4d731620c5b85da12953170cc5
aaapark <parksc1534.dev@gmail.com>
2023-11-07 21:56:13 +0900

    feat: 구매금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lottos from './Lottos.js'
+
 class App {
-  async play() {}
+  async play() {
+    await this.userInputMoney();
+  }
+
+  async userInputMoney() {
+    const userValue = await MissionUtils.Console.readLineAsync("구입금액을 입력해주세요.\n")
+    this.lottos = new Lottos(userValue)
+    console.log(this.lottos)
+  }
+
 }
 
 export default App;
---
+++ src/Lottos.js
@@ -0,0 +1,7 @@
+
+export default class Lottos {
+    constructor(money) {
+        this.value = money
+        this.count = money / 1000
+    }
+}
