8c54022617eead32c5b32c1e649b7feadabeba69
sehyun <e6d1fe@gmail.com>
2023-11-07 15:17:13 +0900

    feat: App.js에 보너스 번호 입력까지 게임 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import LottosList from './LottosList.js';
+
+import enterPurchaseAmount from './Input/enterPurchaseAmount.js';
+import enterWinningNumbers from './Input/enterWinningNumbers.js';
+import enterBonusNumber from './Input/enterBonusNumber.js';
+import printPurchasedLottoAmount from './Output/printPurchasedLottoAmount.js';
+import printLottosList from './Output/printLottosList.js';
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await enterPurchaseAmount();
+
+    const newLottosList = new LottosList(purchaseAmount);
+    const lottosList = newLottosList.lottosList;
+    printPurchasedLottoAmount(newLottosList.lottoAmount);
+    printLottosList(lottosList);
+
+    const winningNumbers = await enterWinningNumbers();
+
+    const bonusNumber = await enterBonusNumber();
+  }
 }
 
 export default App;
