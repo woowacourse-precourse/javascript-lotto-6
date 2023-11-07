00cf5275047f5645914167bc7956a6bd612ab37d
손진영 <sm099@naver.com>
2023-11-07 21:01:34 +0900

    test(systemErrorHandler): expect를 하나의 test로 분리
    1. 함수 호출 횟수, 첫 호출 에러 발생, 두번째 호출 성공 반환에 대한 case를 각각의 test로 분리하여 관리하도록 변경

--- src/error/systemErrorHandler/SystemErrorHandler.test.js
+++ src/error/systemErrorHandler/SystemErrorHandler.test.js
@@ -8,23 +8,35 @@ jest.mock('@woowacourse/mission-utils', () => ({
 }));
 
 describe('입력 관련 예외 처리 테스트', () => {
-  afterEach(() => {
+  let executeTest;
+
+  beforeEach(() => {
     jest.clearAllMocks();
+    // given
+    executeTest = jest
+      .fn()
+      .mockRejectedValueOnce(new Error('Test Error'))
+      .mockResolvedValueOnce('Success');
   });
 
-  describe('retryOnErrors 테스트', () => {
-    test('첫 번째 실행은 에러가 발생하여 다시 함수가 호출되며, 두 번째 실행은 정상적으로 실행 후 값을 반환한다.', async () => {
-      // given
-      const executeTest = jest.fn();
-      executeTest.mockRejectedValueOnce(new Error('Test Error')).mockResolvedValueOnce('Success');
+  test('함수가 두 번 호출된다.', async () => {
+    // when
+    await systemErrorHandler.retryOnErrors(executeTest);
+    // then
+    expect(executeTest).toHaveBeenCalledTimes(2);
+  });
 
-      // when
-      const result = await systemErrorHandler.retryOnErrors(executeTest);
+  test('첫 번째 호출은 실패 후 에러 로깅이 발생한다.', async () => {
+    // when
+    await systemErrorHandler.retryOnErrors(executeTest);
+    // then
+    expect(Console.print).toHaveBeenCalledWith('Test Error');
+  });
 
-      // then
-      expect(executeTest).toHaveBeenCalledTimes(2);
-      expect(Console.print).toHaveBeenCalledWith('Test Error');
-      expect(result).toMatch('Success');
-    });
+  test('두 번째 호출은 성공적인 결과를 반환한다.', async () => {
+    // when
+    const result = await systemErrorHandler.retryOnErrors(executeTest);
+    // then
+    expect(result).toMatch('Success');
   });
 });
