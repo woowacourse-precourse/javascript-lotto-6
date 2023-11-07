a7d35a2a6960ed4e3aa4d3578ddefece1d1a4ef5
starcradle101 <dreamerdev12@gmail.com>
2023-11-08 05:48:48 +0900

    🐛Fix($Logs): 미션 프로그램 문구 수정 미션 프로그램 문구 수정 사항 미 반영으로 인한 테스트 오류 해결

--- src/constants/Logs.js
+++ src/constants/Logs.js
@@ -1,18 +1,18 @@
 const USER_INPUT = {
   purchaseAmountInputPrompt: '구입금액을 입력해 주세요.\n',
-  winningNumbersInputPrompt: '당첨 번호를 입력해 주세요.\n',
-  bonusNumberInputPrompt: '보너스 번호를 입력해 주세요.\n',
+  winningNumbersInputPrompt: '\n당첨 번호를 입력해 주세요.\n',
+  bonusNumberInputPrompt: '\n보너스 번호를 입력해 주세요.\n',
 };
 
 const USER_OUTPUT = {
   purchaseCountPrompt: '개를 구매했습니다.',
-  winningStatistics: '당첨 통계\n---',
-  match6Prize: '6개 일치 (2,000,000,000원) - ',
-  match5WithBonusPrize: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
-  match5Prize: '5개 일치 (1,500,000원) - ',
-  match4Prize: '4개 일치 (50,000원) - ',
-  match3Prize: '3개 일치 (5,000원) - ',
-  totalProfitRatePrefix: '총 수익률은',
+  winningStatistics: '\n당첨 통계\n---\n',
+  fifthPrize: '3개 일치 (5,000원) - ',
+  forthPrize: '4개 일치 (50,000원) - ',
+  thirdPrize: '5개 일치 (1,500,000원) - ',
+  secondPrize: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  firstPrize: '6개 일치 (2,000,000,000원) - ',
+  totalProfitRatePrefix: '총 수익률은 ',
   totalProfitRateSuffix: '%입니다.\n',
 };
 
