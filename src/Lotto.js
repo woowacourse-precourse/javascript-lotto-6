e326a9eb63c666a399e9556f2bb9d49d42d5167f
minki3 <86992006+minki3@users.noreply.github.com>
2023-11-08 03:46:59 +0900

    예제 통과 테스트

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,11 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
     for (let i = 0; i < numbers.length; i++) {
       if (numbers.indexOf(numbers[i]) !== i) {
-        throw new Error("[ERROR]: 중복된 숫자가 있습니다.");
+        throw new Error('[ERROR]: 중복된 숫자가 있습니다.');
       }
     }
   }
