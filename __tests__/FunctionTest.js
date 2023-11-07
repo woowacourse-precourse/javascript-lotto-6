d6c739697406f1aa18f2976c7de24d015d707e0a
anttiey <hanuuny@gmail.com>
2023-11-07 23:40:55 +0900

    test: 당첨 내역 계산 기능 테스트
    - 로또가 1개의 등수에 당첨된 케이스 테스트 생성
    - 로또가 2개의 등수에 당첨된 케이스 테스트 생성
    - 로또가 모든 등수에 당첨된 케이스 테스트 생성
    - 로또가 당첨되지 않은 케이스 테스트 생성

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,5 +1,6 @@
 import { Random } from '@woowacourse/mission-utils';
 import Game from '../src/Game.js';
+import Lotto from '../src/Lotto.js';
 
 const mockRandoms = (numbers) => {
   Random.pickUniqueNumbersInRange = jest.fn();
@@ -42,3 +43,111 @@ describe('로또 생성 기능 테스트', () => {
     expect(ticket.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
+
+describe('당첨 내역 계산 기능 테스트', () => {
+  const game = new Game();
+
+  game.setWinningNumbers('1,2,3,4,5,6');
+  game.setBonusNumber(7);
+
+  test('로또가 1등에 당첨됐을 때', () => {
+    const tickets = [new Lotto([1, 2, 3, 4, 5, 6])];
+    const results = { 0: 0, 1: 1, 2: 0, 3: 0, 4: 0, 5: 0 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 2등에 당첨됐을 때', () => {
+    const tickets = [new Lotto([1, 2, 3, 4, 5, 7])];
+    const results = { 0: 0, 1: 0, 2: 1, 3: 0, 4: 0, 5: 0 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 3등에 당첨됐을 때', () => {
+    const tickets = [new Lotto([1, 2, 3, 4, 5, 8])];
+    const results = { 0: 0, 1: 0, 2: 0, 3: 1, 4: 0, 5: 0 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 4등에 당첨됐을 때', () => {
+    const tickets = [new Lotto([1, 2, 3, 4, 8, 9])];
+    const results = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 1, 5: 0 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 5등에 당첨됐을 때', () => {
+    const tickets = [new Lotto([1, 2, 3, 8, 9, 10])];
+    const results = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 1 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 1등, 2등에 당첨됐을 때', () => {
+    const tickets = [
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([1, 2, 3, 4, 5, 7]),
+    ];
+    const results = { 0: 0, 1: 1, 2: 1, 3: 0, 4: 0, 5: 0 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 1등, 3등에 당첨됐을 때', () => {
+    const tickets = [
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([1, 2, 3, 4, 5, 8]),
+    ];
+    const results = { 0: 0, 1: 1, 2: 0, 3: 1, 4: 0, 5: 0 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 1등, 4등에 당첨됐을 때', () => {
+    const tickets = [
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([1, 2, 3, 4, 8, 9]),
+    ];
+    const results = { 0: 0, 1: 1, 2: 0, 3: 0, 4: 1, 5: 0 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 1등, 5등에 당첨됐을 때', () => {
+    const tickets = [
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([1, 2, 3, 8, 9, 10]),
+    ];
+    const results = { 0: 0, 1: 1, 2: 0, 3: 0, 4: 0, 5: 1 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 1등, 2등, 3등, 4등, 5등에 당첨됐을 때', () => {
+    const tickets = [
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([1, 2, 3, 4, 5, 7]),
+      new Lotto([1, 2, 3, 4, 5, 8]),
+      new Lotto([1, 2, 3, 4, 8, 9]),
+      new Lotto([1, 2, 3, 8, 9, 10]),
+    ];
+    const results = { 0: 0, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+
+  test('로또가 당첨되지 않았을 때', () => {
+    const tickets = [new Lotto([8, 9, 10, 11, 12, 13])];
+    const results = { 0: 1, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+
+    expect(game.calculateTotalWinningResults(tickets)).toEqual(results);
+  });
+});
+
+// describe('총 수익률 계산 기능 테스트');
+
+// describe('당첨 내역 출력 기능 테스트');
+
+// describe('총 수익률 출력 기능 테스트');
