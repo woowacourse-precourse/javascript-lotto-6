b52301d910ac54d3e60e5c6dbe0de636974b1ccb
yeeun426 <thsudkcla7@naver.com>
2023-11-07 11:20:09 +0900

    feat : 로또 구입 금액 입력 (구입 금액 예외 처리)

---
+++ src/LottoInput.js
@@ -0,0 +1,12 @@
+class LottoInput {
+  async racingCarInput() {
+    const price = await Console.readLineAsync("구입금액을 입력해 주세요.");
+
+    this.checkValidPrice(Number(price));
+  }
+
+  checkValidPrice(price) {
+    if (price % 1000 !== 0)
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+  }
+}
