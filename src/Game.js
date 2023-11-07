c68afac2fb2f2538e61eed41bd405dae7a67ed23
puranium <puranium235@gmail.com>
2023-11-08 01:05:57 +0900

    feat: 중복되지 않는 1~45 숫자 6개 뽑기

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import { Console, Random } from '@woowacourse/mission-utils';
 import User from './User.js';
 import Validation from './Validation.js';
 import MESSAGE from './MESSAGE.js';
+import Lotto from './Lotto.js';
 
 class Game {
   #lottoAmount;
@@ -22,9 +23,21 @@ class Game {
     this.#lottoAmount = Game.#priceToAmount(price);
   }
 
+  static #pickLottoNumbers() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  #setMyLotto() {
+    let count = this.#lottoAmount;
+    while (count > 0) {
+      this.#myLotto.push(new Lotto(Game.#pickLottoNumbers()));
+      count -= 1;
+    }
+  }
+
   async play() {
     await this.#setLottoAmount();
-    Console.print(this.#lottoAmount);
+    Console.print(Game.#pickLottoNumbers());
   }
 }
 
