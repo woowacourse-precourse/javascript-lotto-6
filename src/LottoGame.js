e64c6677ff48c0089e6917861c2d66320391f4b9
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-11-07 20:53:53 +0900

    refactor: #getPurchaseAmount(), #getWinningNumbers(), #getBonusNumber() 함수 추출
    한 함수가 한 가지 기능만 담당하게 하기 위해서 함수 분리

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -3,41 +3,57 @@ import View from './View.js';
 import WinningLotto from './WinningLotto.js';
 
 class LottoGame {
-  #purchaser;
+  #lottoPurchaser;
   #winningLotto;
 
   async play() {
-    await this.#determinePurchaseAmount();
+    await this.#createPurchaser();
     this.#purchaseLottos();
     await this.#createWinningLotto();
-    this.#purchaser.check(this.#winningLotto);
+    this.#lottoPurchaser.check(this.#winningLotto);
   }
 
-  async #determinePurchaseAmount() {
+  async #createPurchaser() {
     try {
-      const purchaseAmount = await View.askPurchaseAmount();
-      this.#purchaser = new LottoPurchaser(purchaseAmount);
+      const purchaseAmount = await this.#getPurchaseAmount();
+      this.#lottoPurchaser = new LottoPurchaser(purchaseAmount);
     } catch (error) {
       View.print(error.message);
-      await this.#determinePurchaseAmount();
+      await this.#createPurchaser();
     }
   }
 
+  async #getPurchaseAmount() {
+    const purchaseAmount = await View.askPurchaseAmount();
+    return purchaseAmount;
+  }
+
   #purchaseLottos() {
-    this.#purchaser.purchase();
-    View.printLottoCount(this.#purchaser.getLottoCount());
-    View.printSortedLottos(this.#purchaser.getSortedLottos());
+    this.#lottoPurchaser.purchase();
+    View.printLottoCount(this.#lottoPurchaser.getLottoCount());
+    View.printSortedLottos(this.#lottoPurchaser.getSortedLottos());
   }
 
   async #createWinningLotto() {
     try {
-      const winningNumbers = await View.askWinningNumbers();
-      const bonusNumber = await View.askBonusNumber();
-      this.#winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+      this.#winningLotto = new WinningLotto(
+        await this.#getWinningNumbers(),
+        await this.#getBonusNumber(),
+      );
     } catch (error) {
       View.print(error.message);
       await this.#createWinningLotto();
     }
+  }
+
+  async #getWinningNumbers() {
+    const winningNumbers = await View.askWinningNumbers();
+    return winningNumbers;
+  }
+
+  async #getBonusNumber() {
+    const bonusNumber = await View.askBonusNumber();
+    return bonusNumber;
   }
 }
 
