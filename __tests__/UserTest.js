57ae8fb62919ee7188ebc0ec540871ea13e4146f
teawon <xodnjsvkvk9@gmail.com>
2023-11-08 04:09:20 +0900

    test : User도메인 테스트 코드 추가

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -18,6 +18,30 @@ describe("사용자 클래스 테스트", () => {
     expect(user.getMoney()).toBe(money);
   });
 
+  test("사용자가 사용한 금액은 처음에 0원이여야 한다.", () => {
+    // given
+    const money = 1000;
+
+    // when
+    const user = new User(money);
+
+    // then
+    const usedMoney = user.getUsedMoney();
+    expect(usedMoney).toBe(0);
+  });
+
+  test("사용자는 처음에 어떠한 로또도 가지고 있지 않다.", () => {
+    // given
+    const money = 1000;
+
+    // when
+    const user = new User(money);
+
+    // then
+    const lottoList = user.getLottoList();
+    expect(lottoList).toEqual([]);
+  });
+
   test("사용자는 음수의 금액을 가질 수 없다.", () => {
     // given
     const money = -1000;
@@ -36,11 +60,13 @@ describe("사용자 클래스 테스트", () => {
     // when
     user.buyLotto(lotto1, 1000);
     user.buyLotto(lotto2, 1000);
+    const usedMoney = user.getUsedMoney();
     const leftMoney = user.getMoney();
     const userLotto = user.getLottoList();
 
     // then
     expect(leftMoney).toBe(1000);
     expect(userLotto).toEqual([lotto1, lotto2]);
+    expect(usedMoney).toBe(2000);
   });
 });
