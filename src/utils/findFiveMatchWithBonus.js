690867e79b01ba5016be6a800238bdcb9a16f287
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-07 22:40:30 +0900

    ✨ Feat: 당첨번호 5개와 보너스 넘버가 일치하는 여부를 계산하는 기능 추가

---
+++ src/utils/findFiveMatchWithBonus.js
@@ -0,0 +1,16 @@
+import CONSTANTS from '../constants/constants.js';
+
+const findFiveMatchWithBonus = (lottos, matchCounts, bonusNumber) => {
+  const matchWithBonuscCounts = [];
+  matchCounts.forEach((element, index) => {
+    if (element === CONSTANTS.match.fiveMatch) {
+      matchWithBonuscCounts.push(index);
+    }
+  });
+  matchWithBonuscCounts.forEach(indice => {
+    if (lottos[indice].getNumbers().includes(bonusNumber))
+      matchCounts[indice] = CONSTANTS.match.fiveMatchWithBonus;
+  });
+};
+
+export default findFiveMatchWithBonus;
