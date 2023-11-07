e52438a60ad44e45eb38a7a3ba27e39639f6346c
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-07 17:25:00 +0900

    test: LottoValidator 클래스의 기능 검증을 위한 테스트코드 추가

---
+++ __tests__/unit/LottoValidatorTest.js
@@ -0,0 +1,29 @@
+/* eslint-disable max-lines-per-function */
+import { ERROR } from '../../src/common/constants.js';
+import LottoValidator from '../../src/service/LottoValidator.js';
+
+describe('LottoValidator 클래스 테스트', () => {
+  test('로또가 유효한 형식이라면 성공', () => {
+    const validInput = [1, 2, 3, 4, 5, 6];
+    const lottoValidator = new LottoValidator(validInput);
+    expect(() => lottoValidator.validate()).not.toThrow();
+  });
+
+  test('로또가 배열 형태가 아니면 실패', () => {
+    const invalidInput = '1, 2, 3, 4, 5, 6';
+    const lottoValidator = new LottoValidator(invalidInput);
+    expect(() => lottoValidator.validate()).toThrow(ERROR.not_lotto);
+  });
+
+  test('로또에 문자열이 포함되면 실패', () => {
+    const invalidInput = [1, 2, 3, '문자', 5, 6];
+    const lottoValidator = new LottoValidator(invalidInput);
+    expect(() => lottoValidator.validate()).toThrow(ERROR.numeric);
+  });
+
+  test('로또의 번호가 1 이상 45 이하가 아니면 실패', () => {
+    const invalidInput = [1, 2, 3, 4, 5, 46];
+    const lottoValidator = new LottoValidator(invalidInput);
+    expect(() => lottoValidator.validate()).toThrow(ERROR.range);
+  });
+});
