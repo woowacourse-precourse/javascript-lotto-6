88f2a2fb5729e0ef06d6b45cfce03b3170cb2da9
yeokyoung song <spolice0324@gmail.com>
2023-11-08 01:18:29 +0900

    feat : 에러 처리 및 재시도 핸들러 추가
    - 에러의 경우 메세지를 출력하고, 입력 받을 수 있도록 재시도 하는 핸들러 정의
    - 로또를 생성하고, 인풋에 대한 에러, 재입력 처리 핸들러 추가
    - 로또 결과를 계산하고 인풋에 대한 에러, 재입력 처리 핸들러 추가
    - 보너스 번호 입풋에 대한 에러, 재입력 처리 핸들러 추가

---
+++ src/utils/handlers.js
@@ -0,0 +1,37 @@
+import LottoGenerator from '../domain/LottoGenerator';
+import LottoCalculator from '../domain/LottoCalculator';
+import InputView from '../view/InputView';
+import OutputView from '../view/OutputView';
+
+const ErrorHandlerAndRetry = async (handler, retry) => {
+  try {
+    return await handler();
+  } catch (err) {
+    OutputView.err({ message: err.message });
+    if (err) return await retry();
+    throw err;
+  }
+};
+
+const lottoGeneratorHandler = async () =>
+  ErrorHandlerAndRetry(async () => {
+    const response = await InputView.purchaseAmount();
+    return new LottoGenerator(response);
+  }, lottoGeneratorHandler);
+
+const lottoCalculatorHandler = async () =>
+  ErrorHandlerAndRetry(async () => {
+    const response = await InputView.winningNumbers();
+    return new LottoCalculator(response);
+  }, lottoCalculatorHandler);
+
+const bonusNumberHandler = async (lottoCalculator) =>
+  ErrorHandlerAndRetry(
+    async () => {
+      const response = await InputView.bonusNumber();
+      lottoCalculator.bonus = response;
+    },
+    () => bonusNumberHandler(lottoCalculator),
+  );
+
+export { lottoGeneratorHandler, lottoCalculatorHandler, bonusNumberHandler };
