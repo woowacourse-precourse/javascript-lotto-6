f71b40b8204b0a81f770ae0664fa7d1a7bc062a8
Collection50 <fkaus65@naver.com>
2023-11-07 22:30:40 +0900

    feat(Formatter): 출력 결과를 포맷팅하는 Formatter 클래스 구현

---
+++ src/Formatter/index.js
@@ -0,0 +1,19 @@
+import { MESSAGE, RESULT, SYMBOLS } from '../constants/index.js';
+
+class Formatter {
+  static formatResult(ranks, revenu) {
+    const results = Object.entries(ranks)
+      .map(([rank, value]) => `${RESULT[rank]}${value}개${SYMBOLS.lineBreak}`)
+      .join('');
+    return `${MESSAGE.winningStatistics}${SYMBOLS.boundary}${SYMBOLS.lineBreak}${results}${MESSAGE.result}${revenu}${SYMBOLS.percent}입니다.`;
+  }
+
+  static formatInputNumbers(numbers) {
+    return numbers
+      .replace(/\s/g, '')
+      .split(SYMBOLS.numberDivider)
+      .filter(Boolean);
+  }
+}
+
+export default Formatter;
