bc8b88500c9430b773857cacfbab6142297b3514
wonkyum-kim <dodo12120@naver.com>
2023-11-07 11:45:52 +0900

    test: 번호가 1에서 45사이에 있지 않으면 예외가 발생

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,25 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../src/Lotto.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test.each([
+    [1, 2, 3, 4, 5, 0],
+    [1, 2, 3, 4, 5, 46],
+  ])('번호가 1에서 45사이에 있지 않으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 0]);
+    }).toThrow('[ERROR]');
+  });
 });
