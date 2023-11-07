1f13639d44ef19d0a47bcfd4143f3e74daca8f7a
JunSeok <ppu1234@naver.com>
2023-11-08 01:37:32 +0900

    style: 공백 제거

--- src/print/printEachLotto.js
+++ src/print/printEachLotto.js
@@ -2,18 +2,15 @@ import { Console } from '@woowacourse/mission-utils';
 
 const printEachLotto = (lotto) => {
   let str = ``;
-
   lotto.forEach((number, idx, arr) => {
     if (idx === 0) {
       str += `[${number}, `;
       return;
     }
-
     if (idx === arr.length - 1) {
       str += `${number}]`;
       return;
     }
-
     str += `${number}, `;
   });
 
