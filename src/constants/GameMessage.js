3cb506e4511adaab6f369346211e486e786421f9
Goonco <s07019@naver.com>
2023-11-08 00:36:44 +0900

    :pencil2: Winning Lotto와 Winning Number의 모호성 제거를 위한 typo 수정

--- src/constants/GameMessage.js
+++ src/constants/GameMessage.js
@@ -1,5 +1,5 @@
 export const GAMEMSG_INPUT_MONEY = "구입금액을 입력해 주세요.\n";
-export const GAMEMSG_INPUT_WINNING_LOTTO = "당첨 번호를 입력해 주세요.\n";
+export const GAMEMSG_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.\n";
 export const GAMEMSG_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.\n";
 
 export const GAMEMST_OUTPUT_BOUGHT_LOTTO_NUMBERS = "개를 구매했습니다.";
