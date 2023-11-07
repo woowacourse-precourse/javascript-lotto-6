8720cbed00c121b0e0c7e58c07f71d0a1bdc1752
kang <kangju2000@naver.com>
2023-11-07 18:43:26 +0900

    :white_check_mark: Lotto 테스트코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,38 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]')
   })
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5])
     }).toThrow('[ERROR]')
   })
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호가 6개가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5])
+    }).toThrow('[ERROR]')
+  })
+
+  test('로또 번호가 6개이고 중복된 숫자가 없으면 생성된다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6])
+    expect(lotto).toBeInstanceOf(Lotto)
+  })
+
+  test('로또 번호의 숫자가 1 ~ 45 사이가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46])
+    }).toThrow('[ERROR]')
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 0])
+    }).toThrow('[ERROR]')
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, -1])
+    }).toThrow('[ERROR]')
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 'a'])
+    }).toThrow('[ERROR]')
+  })
 })
