f5c4a9ad303af14ce2262958a0e0ba7d32601b52
kim-young-dong <opa9904@gmail.com>
2023-11-07 17:42:40 +0900

    feat: Lotto class method update
    - validate 예외 상황 추가
    - getNumbers method 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,23 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (numbers.some(number => number < 1 || number > 45)) {
+      throw new Error('[ERROR] 로또 번호는 1~45 사이여야 합니다.');
+    }
+    if (numbers.some(number => Number.isNaN(number))) {
+      throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+    }
+    if (numbers.some((number, index) => numbers.indexOf(number) !== index)) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
     }
   }
 
   // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
