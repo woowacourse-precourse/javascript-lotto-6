02251f09e23097b7887076a33319b1e59a0b5ead
Goonco <s07019@naver.com>
2023-11-08 00:33:55 +0900

    :sparkles: 당첨 로또 생성 - 로또 번호 문자열 쪼개기 및 번호들의 정수 변환 기능 추가

---
+++ src/utils/generateWinningLotto.js
@@ -0,0 +1,7 @@
+export const generateWinningNumbers = (winnigNumbersString) => {
+    const parsedWinningNumbersString = winnigNumbersString.split(",");
+    const winningNumbers = parsedWinningNumbersString.map((numberString)=>Number(numberString));
+    return winningNumbers;
+}
+
+export const generateBonusNumber = (bonusNumberString) => Number(bonusNumberString)
