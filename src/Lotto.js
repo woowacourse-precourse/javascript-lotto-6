fb1852af715fdccbec870b269664a4d7ccee4f48
최진욱 <jinwook.fe@gmail.com>
2023-11-07 17:20:36 +0900

    [fix] jest bug fix

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,21 +9,20 @@ class Lotto {
 
   #validateForm(numbers) {
     const regexNumber = /^\d+$/;
-    const numberArray = numbers.split(",");
 
-    if (numberArray.length !== 6) {
-      throw new Error(
-        "[ERROR] 쉼표(,)로 구분된 숫자 형식의 입력만 가능합니다.",
-      );
-    }
-
-    numberArray.forEach((number) => {
-      if (!regexNumber.test(number)) {
+    if (Array.isArray(numbers)) {
+      if (numbers.length !== 6) {
         throw new Error(
-          "[ERROR] 쉼표(,)로 구분된 숫자 형식의 입력만 가능합니다.",
+          "[ERROR] 로또 번호는 쉼표(,)로 구분된 6자리여야 합니다.",
         );
       }
-    });
+
+      this.isValid(regexNumber, numbers);
+    } else if (numbers.split(",").length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 쉼표(,)로 구분된 6자리여야 합니다.");
+    }
+
+    this.isValid(regexNumber, numbers);
   }
 
   #validateDuplication(numbers) {
@@ -34,6 +33,28 @@ class Lotto {
         "[ERROR] 1~45 사이의 중복되지 않는 6개의 숫자를 입력해주세요.",
       );
     }
+  }
+
+  isValid(regexNumber, numbers) {
+    if (Array.isArray(numbers)) {
+      numbers.forEach((number) => {
+        if (!regexNumber.test(number)) {
+          throw new Error(
+            "[ERROR] 쉼표(,)로 구분된 숫자 형식의 입력만 가능합니다.",
+          );
+        }
+      });
+
+      return;
+    }
+
+    numbers.split(",").forEach((number) => {
+      if (!regexNumber.test(number)) {
+        throw new Error(
+          "[ERROR] 쉼표(,)로 구분된 숫자 형식의 입력만 가능합니다.",
+        );
+      }
+    });
   }
 
   getNumbers() {
