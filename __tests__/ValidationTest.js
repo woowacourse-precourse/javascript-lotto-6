6623466977c100ef2bf06684914cc2595767d223
yeong-hwan <jghff700@naver.com>
2023-11-08 02:01:52 +0900

    test: 음수를 입력한 경우에 대한 테스트케이스 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,15 +1,18 @@
 import ErrorMessage from '../src/domain/errors/ErrorMessage.js';
 import InputValidator from '../src/domain/validator/InputValidator.js';
 
-// jest.mock('../src/domain/validator/InputValidator.js');
-const testModel = require('../src/domain/validator/InputValidator.js');
-testModel.default.create = jest.fn();
-
 describe('Number Validation', () => {
+  const inputValidator = new InputValidator();
+
   test('숫자가 아닌 문자를 입력', () => {
-    const inputValidator = new InputValidator();
     expect(() => {
       inputValidator.validateNumber('string');
+    }).toThrow(ErrorMessage);
+  });
+
+  test('음수를 입력', () => {
+    expect(() => {
+      inputValidator.validatePositiveNumber('-1');
     }).toThrow(ErrorMessage);
   });
 });
