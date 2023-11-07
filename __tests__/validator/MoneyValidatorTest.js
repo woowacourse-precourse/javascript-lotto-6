78629f2b375c7b43e9be8327df750346cbab5afb
BangDori <qudwns8865@naver.com>
2023-11-07 19:19:14 +0900

    test: 구입금액 테스트 코드 리팩토링

--- __tests__/validator/MoneyValidatorTest.js
+++ __tests__/validator/MoneyValidatorTest.js
@@ -1,7 +1,8 @@
+import { MONEY_ERROR } from '../../src/constants/message/error';
 import MoneyValidator from '../../src/validator/MoneyValidator';
 
 describe('Money Validator 클래스 테스트', () => {
-  describe('로또 구매 여부 테스트', () => {
+  describe('로또 구매', () => {
     test('숫자가 1000 이상이라면 에러가 발생하지 않는다.', () => {
       const money = 1000;
 
@@ -9,25 +10,17 @@ describe('Money Validator 클래스 테스트', () => {
         MoneyValidator.validateLottoPurchaseAmount(money),
       ).not.toThrow();
     });
+  });
 
+  describe('로또 구매 예외 상황', () => {
     test('숫자가 1000보다 작다면 예외가 발생한다.', () => {
       const moneyList = [-1, 0, 999];
 
       moneyList.forEach(money =>
         expect(() => MoneyValidator.validateLottoPurchaseAmount(money)).toThrow(
-          '[ERROR]',
+          MONEY_ERROR.purchaseAmount,
         ),
       );
-    });
-  });
-
-  describe('구매금액이 로또가격으로 정확히 나누어 떨어지는지 테스트', () => {
-    test('구매금액이 로또가격으로 정확히 나누어 떨어진다.', () => {
-      const money = 1000;
-
-      expect(() =>
-        MoneyValidator.validateLottoAmountExactness(money),
-      ).not.toThrow();
     });
 
     test('구매금액이 로또가격으로 정확히 나누어 떨어지지 않는다면 예외 처리한다.', () => {
@@ -36,7 +29,7 @@ describe('Money Validator 클래스 테스트', () => {
       moneyList.forEach(money => {
         expect(() =>
           MoneyValidator.validateLottoAmountExactness(money),
-        ).toThrow('[ERROR]');
+        ).toThrow(MONEY_ERROR.amountExactness);
       });
     });
   });
