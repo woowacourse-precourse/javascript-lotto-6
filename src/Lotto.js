d1dec2e41166f478d362df347ad9c5a511c93170
aaapark <parksc1534.dev@gmail.com>
2023-11-08 00:25:02 +0900

    fix: 테스트코드에 따라 lotto 클래스 유효성 검사 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,6 +12,11 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const set = new Set(numbers)
+    if(set.size !== numbers.length) {
+      throw new Error ('[ERROR] 중복된 숫자가 있습니다.')
+    }
   }
 
   printLottoNumber() {
