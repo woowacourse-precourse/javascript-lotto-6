e70912c818dd648b30731d77369bff8a2856ea4a
77JY <kwn03111@naver.com>
2023-11-07 15:02:25 +0900

    feat: 보너스 번호의 중복 및 범위 검증을 위한 isBonusNumber 메소드 추가

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -28,4 +28,19 @@ export const Validator = {
       throw new Error(Errors.duplication_Number);
     }
   },
+
+  isBonusNumber(bonus_Number, lottery_Numbers) {
+    if (isNaN(bonus_Number) || !bonus_Number) {
+      throw new Error(Errors.is_Number);
+    }
+    if (bonus_Number < GameSetting.lotto_Min_Number) {
+      throw new Error(ErroErrors.min_Number);
+    }
+    if (bonus_Number > GameSetting.lotto_Max_Number) {
+      throw new Error(Errors.max_Number);
+    }
+    if (lottery_Numbers.some((lottery_Number) => bonus_Number === lottery_Number)) {
+      throw new Error(Errors.duplication_Number);
+    }
+  },
 };
