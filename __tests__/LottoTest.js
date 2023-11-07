27f7d66c2ad970dd6f1072c390c146d150669f1e
rustandbone <dlehdgh14@naver.com>
2023-11-08 02:02:52 +0900

    테스트: 예외 처리 클래스로 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,23 +1,24 @@
 import Lotto from '../src/Lotto.js';
+import LottoError from '../src/error/LottoError.js';
 
 describe('로또 클래스 테스트', () => {
   describe('로또 번호 유효성 검사', () => {
     test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
       expect(() => {
         new Lotto([1, 2, 3, 4, 5, 6, 7]);
-      }).toThrow('[ERROR]');
+      }).toThrow(LottoError);
     });
 
     test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
       expect(() => {
         new Lotto([1, 2, 3, 4, 5, 5]);
-      }).toThrow('[ERROR]');
+      }).toThrow(LottoError);
     });
 
     test('로또 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다.', () => {
       expect(() => {
         new Lotto([1, 2, 3, 4, 5, 50]);
-      }).toThrow('[ERROR]');
+      }).toThrow(LottoError);
     });
   });
 
