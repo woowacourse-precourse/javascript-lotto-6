06e40784a5ced76380f8337c3f4f3f1657dc052f
dongsuseo <poopoo0515@naver.com>
2023-11-07 17:47:13 +0900

    feat: 로또 번호 예외 처리 추가
    1~45사이 숫자가 아니거나 번호가 중복될 때 예외 처리를 하였습니다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,13 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (numbers[0] < 1 || numbers[5] > 45) {
+      throw new Error("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
+    }
+    const filterArray = new Set(numbers);
+    if (numbers.length != filterArray.size) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
