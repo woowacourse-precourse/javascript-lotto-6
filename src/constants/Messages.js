3083835be581842492b5cd75112e4234629db168
kimmoonju-102 <kimmun3549@naver.com>
2023-11-07 15:15:10 +0900

    feat : 게임에 사용되는 문자열을 상수화하여 저장

---
+++ src/constants/Messages.js
@@ -0,0 +1,11 @@
+const LOTTO_MESSAGES = {
+  inputPurchaseAmount: "\n구입금액을 입력해 주세요.\n",
+  inputLottoNumbers: "\n당첨 번호를 입력해 주세요.\n",
+  inputBonusNumber: "\n보너스 번호를 입력해 주세요.\n",
+  outputLottoAmount: "개를 구매했습니다.",
+  outputResultTitle: "당첨 통계\n---\n",
+};
+
+module.exports = {
+  LOTTO_MESSAGES,
+};
