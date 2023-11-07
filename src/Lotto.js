890a7996b5f989b6347ef57359049ba9d866b15e
minseok cho <whalstjr199927@gmail.com>
2023-11-07 17:37:25 +0900

    Lotto.js 기능 완성

--- src/Lotto.js
+++ src/Lotto.js
@@ -6,9 +6,17 @@ class Lotto {
         this.#numbers = numbers;
     }
 
+    get numbers() {
+        return this.#numbers;
+    }
+
     #validate(numbers) {
         if (numbers.length !== 6) {
             throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        if (new Set(numbers).size !== 6) {
+            throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
