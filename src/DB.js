bab81ba349382629722e95cf8c4ee1f93536f2ef
damdam6 <dambi626626@gmail.com>
2023-11-07 15:51:29 +0900

    refactor: TEXTVIEW 객체화

--- src/DB.js
+++ src/DB.js
@@ -9,20 +9,23 @@ const INITIALIZE_NUM = 0;
 const INITIALIZE_ARR = [];
 const INITIALIZE_OBJECT = {};
 
-const TEXTVIEW_REQUEST_MONEYINPUT = '구입금액을 입력해 주세요';
-const TEXTVIEW_LOTTO_AMOUNT = '개를 구매했습니다.';
-const TEXTVIEW_REQUEST_LOTTOWIN = '당첨 번호를 입력해주세요.';
-const TEXTVIEW_REQUEST_LOTTOBONUS = '보너스 번호를 입력해주세요.';
-const TEXTVIEW_RESULT = ([win3, win4, win5, win5Bonus, win6, percent]) => `
-당첨 통계\n
----\n
-3개 일치 (5,000원) - ${win3}\n
-4개 일치 (50,000원) - ${win4}\n
-5개 일치 (1,500,000원) - ${win5}\n
-5개 일치, 보너스 볼 일치 (30,000,000원) - ${win5Bonus}\n
-6개 일치 (2,000,000,000원) - ${win6}\n
-총 수익률은 ${percent}%입니다.
-`;
+// textViews.js
+const TEXTVIEW = {
+  REQUEST_MONEYINPUT: '구입금액을 입력해 주세요',
+  LOTTO_AMOUNT: '개를 구매했습니다.',
+  REQUEST_LOTTOWIN: '당첨 번호를 입력해주세요.',
+  REQUEST_LOTTOBONUS: '보너스 번호를 입력해주세요.',
+  RESULT: ([win3, win4, win5, win5Bonus, win6, percent]) => `
+    당첨 통계\n
+    ---\n
+    3개 일치 (5,000원) - ${win3}\n
+    4개 일치 (50,000원) - ${win4}\n
+    5개 일치 (1,500,000원) - ${win5}\n
+    5개 일치, 보너스 볼 일치 (30,000,000원) - ${win5Bonus}\n
+    6개 일치 (2,000,000,000원) - ${win6}\n
+    총 수익률은 ${percent}%입니다.
+  `,
+};
 
 class Data {
   #dataResetNeed = false;
@@ -109,5 +112,5 @@ class Data {
 }
 
 export {
-  Data, STAGE_NUM_1, STAGE_NUM_2, STAGE_NUM_3, STAGE_NUM_4, STAGE_NUM_5, STAGE_NUM_6,
+  TEXTVIEW, Data, STAGE_NUM_1, STAGE_NUM_2, STAGE_NUM_3, STAGE_NUM_4, STAGE_NUM_5, STAGE_NUM_6,
 };
