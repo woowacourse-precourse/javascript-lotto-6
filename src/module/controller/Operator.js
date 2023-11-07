05236084666e0ffa59f4a7e6bf6dc10d9fa9e870
OAO <ashgrayblue0@gmail.com>
2023-11-08 03:53:40 +0900

    feat(Operator): implement controller to operate models

---
+++ src/module/controller/Operator.js
@@ -0,0 +1,35 @@
+import LottoGenerator from "../model/LottoGenerator.js";
+import WinNumber from "../model/WinNumber.js";
+import WinnerDecider from "../model/WinnerDecider.js";
+
+class Operator {
+  #LG
+  #WN
+  #WD
+  #lottoList
+  #score
+
+  doLottoGenerator(count) {
+    this.#LG = new LottoGenerator(count);
+    this.#lottoList = LG.lottoList;
+  }
+
+  doWinNumber(numbers, bonus) {
+    this.#WN = new WinNumber(numbers, bonus);
+  }
+
+  doWinnerDecider(WN, LG) {
+    this.#WD = new WinnerDecider(this.#WN, this.#LG);
+    this.#score = WD.score;
+  }
+
+  get lottoList() {
+    return this.#lottoList;
+  }
+
+  get score() {
+    return this.#score;
+  }
+}
+
+export default Operator;
