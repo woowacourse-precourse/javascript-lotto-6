150e0f5478acae87988c8ad45fd159a210161fb3
LEEHYEYEONG <elto789@naver.com>
2023-11-07 10:23:19 +0900

    fix: 1~45 사이의 수 인지 체크 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,3 @@
-import { Console, Random } from "@woowacourse/mission-utils";
 class Lotto {
   #numbers;
 
@@ -13,12 +12,23 @@ class Lotto {
     } else if (numbers.length !== new Set(numbers).size) {
       throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
     }
+    this.numberValidate(numbers);
+  }
+
+  numberValidate(numbers) {
+    for (let i = 0; i < 6; i++) {
+      if (numbers[i] > 45) {
+        throw new Error("[ERROR] 1~45 사이의 값을 입력해주세요. ");
+      }
+      else if (numbers[i] <= 0) {
+        throw new Error("[ERROR] 1~45 사이의 값을 입력해주세요. ");
+      }
+    }
   }
 
   getValidate() {
     return this.#validate;
   }
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
