2fcb3528f6d31bcd52077b64ed3b5f1e0f97ea57
yeong-hwan <jghff700@naver.com>
2023-11-08 08:30:49 +0900

    refactor: 로또 구매 입력 받는 함수의 공백 개행 상수로 변경

--- src/service/input/getLottoAmount.js
+++ src/service/input/getLottoAmount.js
@@ -1,9 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 import InputValidator from '../../domain/validator/InputValidator.js';
 import MESSAGE from '../../constants/message.js';
+import SYMBOL from '../../constants/symbol.js';
 
 export default async function getLottoAmount() {
-  const userInput = await Console.readLineAsync(MESSAGE.purchaseAmount + '\n');
+  const userInput = await Console.readLineAsync(
+    MESSAGE.purchaseAmount + SYMBOL.blankDivider,
+  );
   const inputValidator = new InputValidator();
 
   inputValidator.validateUnitCost(userInput);
