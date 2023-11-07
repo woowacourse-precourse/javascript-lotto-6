01411a74c4f257e4362f23f50c2e434c7392cde0
Seongjin Hong <hongseongjin.to@gmail.com>
2023-11-07 22:05:52 +0900

    style: 주요 설정값 및 메시지 상수화

---
+++ src/constants/OutputMessage.js
@@ -0,0 +1,12 @@
+const OUTPUT_MESSAGE = Object.freeze({
+  boughtNumber: '개를 구매했습니다.',
+  winningResultHeader: '당첨 통계\n---',
+  winningResultFirstPrize: '6개 일치 (2,000,000,000원)',
+  winningResultSecondPrize: '5개 일치, 보너스 볼 일치 (30,000,000원)',
+  winningResultThirdPrize: '5개 일치 (1,500,000원)',
+  winningResultFourthPrize: '4개 일치 (50,000원)',
+  winningResultFifthPrize: '3개 일치 (5,000원)',
+  earningRatio: `총 수익률은 ${earningRatioNumber}% 입니다.`,
+});
+
+export { OUTPUT_MESSAGE };
---
+++ src/constants/Setting.js
@@ -0,0 +1,23 @@
+const LOTTO_SETTING = Object.freeze({
+  minNumber: 1,
+  maxNumber: 45,
+  maxNumbersLength: 6,
+});
+
+const WINNING_CRETERIA = Object.freeze({
+  firstPrize: 6,
+  secondPrize: 6,
+  thirdPrize: 5,
+  fourthPrize: 4,
+  fifthPrize: 3,
+});
+
+const WINNING_PRIZE = Object.freeze({
+  firstPrize: 2000000000,
+  secondPrize: 30000000,
+  thirdPrize: 1500000,
+  fourthPrize: 50000,
+  fifthPrize: 5000,
+});
+
+export { LOTTO_SETTING, WINNING_CRETERIA, WINNING_PRIZE };
