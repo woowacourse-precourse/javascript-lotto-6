dcbecde4be581d3b626e7175e718116cee979c79
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-07 22:12:05 +0900

    feat: play lotto game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import draw from "./modules/Draw";
+import purchase from "./modules/Purchase";
+import reward from "./modules/Reward";
+import inputView from "./views/Input";
+import outputView from "./views/Output";
+
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await inputView.enterPurchaseAmount();
+    const lottoCount = purchase.countMoney(purchaseAmount);
+    const lottos = purchase.getLottos(lottoCount);
+    const lottoNumbersArr = lottos.map((lotto) => lotto.getNumbers());
+    outputView.printPurchasedLottos(lottoNumbersArr);
+
+    const winningNumber = await inputView.enterWinningNumber();
+    const bonusNumber = await inputView.enterBonusNumber();
+    const matches = draw.getResult(lottoNumbersArr, winningNumber, bonusNumber);
+    outputView.printWinningStatistics(matches);
+
+    const winningAmount = reward.computeWinningAmount(matches);
+    const totalReturn = reward.computeTotalReturn(
+      winningAmount,
+      purchaseAmount
+    );
+    outputView.printTotalReturn(totalReturn);
+  }
 }
 
 export default App;
