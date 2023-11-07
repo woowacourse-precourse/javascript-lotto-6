c5fb896a8908be9d7cf658681d1bf1f0862391d0
OAO <ashgrayblue0@gmail.com>
2023-11-08 00:29:46 +0900

    feat(LottoGenerator): implement some methods
    - add constructor
    - add #changeMoneyToCount
    - add #validateMoney
    - add #generateLottos - repeat count times and push #lottos array in this methods

---
+++ src/module/model/LottoGenerator.js
@@ -0,0 +1,30 @@
+import Lotto from '../../Lotto.js';
+import {MissionUtils} from "@woowacourse/mission-utils";
+
+class LottoGenerator {
+  #lottos = [];
+  #count;
+
+  constructor(money) {
+    this.#validateMoney(money);
+    this.#count = this.#changeMoneyToCount(money);
+    this.#generateLottos();
+  }
+
+  #generateLottos() {
+    let lotto;
+
+    for (let i = 0; i < this.#count; i += 1) {
+      lotto = new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6));
+      this.#lottos.push(lotto);
+    }
+  }
+
+  #changeMoneyToCount(money) {
+    return money / 1000;
+  }
+
+  #validateMoney(money) {
+    if (money % 1000 !== 0) throw new Error('[ERROR] 구입 금액이 1,000원 단위가 아닙니다.');
+  }
+}
