97616307e9366669f958c0c51ff8921d1a1f94ac
Mijin Sim <202110106@bssm.hs.kr>
2023-11-07 15:05:57 +0900

    feat(generateLottoNumber) : 로또 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import Money from "./Money.js";
+import GAME_MESSAGES from "./constants/GameMessages.js";
 
 class App {
   async play() {
     const money = new Money();
     const userMoney = await money.userMoney();
-    Console.print(userMoney)
+    const lottoCount = userMoney / 1000;
+    Console.print(`\n${lottoCount}${GAME_MESSAGES.COUNT_LOTTO}`);
+    Console.print(this.generateLottoNumber(lottoCount))
+  }
+
+  generateLottoNumber(lottoCount) {
+    let lottos = [lottoCount];
+    for (let i = 0; i < lottoCount; i += 1) {
+      lottos[i] = Random.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    for (let i = 0; i < lottos.length; i += 1) {
+      lottos[i].sort((a, b) => {
+        return a - b;
+      });
+    }
+
+    return lottos;
   }
 }
 
--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -1,6 +1,10 @@
 const ERROR_MESSAGES = Object.freeze({
-    IS_NUMBER : '[ERROR] 금액은 숫자여야 합니다.',
-    IS_ONE_THOUSAND_WON : '[ERROR] 금액은 1000원 단위여야 합니다.'
-})
+  IS_NUMBER: "[ERROR] 금액은 숫자여야 합니다.",
+  IS_ONE_THOUSAND_WON: "[ERROR] 금액은 1000원 단위여야 합니다.",
+  IS_LOTTO_NUMBER: "[ERROR] 로또 번호는 숫자여야 합니다.",
+  IS_LOTTO_COUNT: "[ERROR] 로또 번호는 6개여야 합니다.",
+  IS_LOTTO_RANGE: "[ERROR] 로또의 범위는 1~45까지 입니다.",
+  IS_DUPLICATION: "[ERROR] 로또의 숫자가 중복됩니다.",
+});
 
 export default ERROR_MESSAGES;
