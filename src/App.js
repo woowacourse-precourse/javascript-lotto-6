c01786590c2c67224b4e338a690a8f3fbeeb3dbb
jang_yoonsu <shagrat8546@gmail.com>
2023-11-07 14:19:11 +0900

    feat : App파일 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { LottoService } from "./service";
+
 class App {
-  async play() {}
+  #lottoService = new LottoService();
+
+  async play() {
+    await this.#lottoService.buyLottos();
+    await this.#lottoService.drawLotto();
+    this.#lottoService.printResult();
+  }
 }
 
 export default App;
