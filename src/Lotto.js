edb7d3dc0dbb03ed18bf3018e193ffb316bc027b
charchar111 <3hjab@naver.com>
2023-11-07 17:42:58 +0900

    doc/README.md 수정(최초)

--- src/Lotto.js
+++ src/Lotto.js
@@ -4,6 +4,9 @@ class Lotto {
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    //  변경 불가
+    // numbers : 사용자가 뽑은 당첨 번호
+    // 필드 추가 금지
   }
 
   #validate(numbers) {
@@ -11,7 +14,7 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
+  // 메소드 추가 가능
   // TODO: 추가 기능 구현
 }
 
