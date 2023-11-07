802f4047186c493c088b7109515d73943f6358e1
Hong sung soo <sow9707@daum.net>
2023-11-07 23:41:18 +0900

    feat: 로또들의 등수를 이용하여 출력하는 메소드 구현

--- src/App.js
+++ src/App.js
@@ -46,6 +46,16 @@ class App {
     return rank;
   }
 
+  printResult(rank) {
+    Console.print("3개 일치 (5,000원) - " + rank.fifth + "개");
+    Console.print("4개 일치 (50,000원) - " + rank.fourth + "개");
+    Console.print("5개 일치 (1,500,000원) - " + rank.third + "개");
+    Console.print(
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - " + rank.second + "개"
+    );
+    Console.print("6개 일치 (2,000,000,000원) - " + rank.first + "개");
+  }
+
   async play() {
   }
 }
