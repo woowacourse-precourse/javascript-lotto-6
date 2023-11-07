edd0b7a37a820f06237d0889b872e3c0828024ea
Sunghyun627 <kksscc4@gmail.com>
2023-11-07 18:32:27 +0900

    feat: Lotto 클래스에서 로또번호를 반환하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,8 +8,12 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+  }
+
+  getLottoNumbers() {
+    return this.#numbers;
   }
 
   // TODO: 추가 기능 구현
