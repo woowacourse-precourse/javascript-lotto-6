dd5d93863b1cb1f23c71178110616f846746c6d6
greeneeya <suzythetravelerr@gmail.com>
2023-11-07 18:26:47 +0900

    feat: 보너스 번호가 1~45 사이의 정수가 아닐 경우, 예외처리한다.

--- src/Host.js
+++ src/Host.js
@@ -9,4 +9,11 @@ export default class Host {
       .sort((a, b) => a - b);
     return winningNumbers;
   }
+
+  static async getBonusNumber() {
+    const bonus = await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    if (!Number.isInteger(bonus) || bonus > 45 || bonus < 1) {
+      throw new Error('[ERROR] 1~45 사이의 정수만 입력 가능합니다.');
+    }
+  }
 }
