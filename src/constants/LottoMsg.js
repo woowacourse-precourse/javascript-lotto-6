968829bc738861fddc85ef80d45b50f9989e9651
JonghyunLEE12 <najo7609@gmail.com>
2023-11-07 16:11:02 +0900

    Test : [ TestCase 패키지 구조 변경 ]

--- src/constants/LottoMsg.js
+++ src/constants/LottoMsg.js
@@ -1,19 +1,19 @@
 export const INPUT_MSG = {
-  PURCHASE_MONEY: '구입금액을 입력해 주세요.',
-  WIN_LOTTERY_NUMBER: '당첨 번호를 입력해 주세요.',
-  BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  PURCHASE_MONEY: '구입금액을 입력해 주세요.\n',
+  WIN_LOTTERY_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
 };
 
 export const OUTPUT_MSG = {
   userCanBuyLotto: (amount) => `${amount}개를 구매했습니다.`,
   userLottoNumber: (number) => `[${number}]`,
-  LOTTO_STATIC_OUT: '당첨 통계---',
+  LOTTO_STATIC_OUT: '당첨 통계\n---',
   userLottoResult: (result) => `
   3개 일치 (5,000원) - ${result[3]}개
   4개 일치 (50,000원) - ${result[4]}개
   5개 일치 (1,500,000원) - ${result[5]}개
   5개 일치, 보너스 볼 일치 (30,000,000원) - ${result.bonus}개
-  6개 일치 (2,000,000,000원) - ${result[6]}개`,
+  6개 일치 (2,000,000,000원) - ${result[6]}개\n`,
   LOTTO_REVENUE_IS: (revenue) => `총 수익률은 ${revenue}%입니다.`,
 };
 
