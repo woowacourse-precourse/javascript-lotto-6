905f38d5dc0c9ac1582fc10853ca623f89a168fe
23YUJIN <asdbnm066@daum.net>
2023-11-08 02:16:19 +0900

    test: 로또 발행하기 테스트

--- __tests__/StoreTest.js
+++ __tests__/StoreTest.js
@@ -1,4 +1,18 @@
+import { Console, Random } from "@woowacourse/mission-utils";
 import Store from "../src/Store.js";
+
+const mockRandoms = (numbers) => {
+  Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe("스토어 클래스 테스트", () => {
   test("구입 금액에 따른 로또 개수를 올바르게 구하는지 테스트", async () => {
@@ -6,5 +20,25 @@ describe("스토어 클래스 테스트", () => {
     Store.inputMoney = jest.fn().mockReturnValue(5000);
     await store.receivePayment();
     expect(store.calculateLottoAmount()).toBe(5);
+  });
+
+  test("로또 발행 테스트", () => {
+    const store = new Store();
+    const logSpy = getLogSpy();
+    const outputs = ["[8, 21, 23, 41, 42, 43]", "[3, 5, 11, 16, 32, 38]"];
+
+    const LOTTO_AMOUNT = 2;
+    const RANDOM_INPUT = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+    ];
+
+    mockRandoms(RANDOM_INPUT);
+    store.calculateLottoAmount = jest.fn().mockReturnValue(LOTTO_AMOUNT);
+    store.issueLotto();
+
+    for (const output of outputs) {
+      expect(logSpy).toHaveBeenCalledWith(output);
+    }
   });
 });
