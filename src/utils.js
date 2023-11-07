52ac16a1b64dd7d5107c8f10db3e141e62479c55
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-07 22:26:37 +0900

    feat(utils): isInteger(), isEmpty() 생성

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+export const isInteager = (value) => {
+  const regex = /^[0-9]*$/;
+
+  return regex.test(value);
+};
+
+export const isEmpty = (value) => {
+  if (value === null || value === undefined || value.length === 0) return true;
+
+  return false;
+};
