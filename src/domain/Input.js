97550708dc6e9d51ca59f4db9ea9777ba5e7b20f
MinGu Jeong <mingu830@gmail.com>
2023-11-08 01:06:52 +0900

    feat: 입력 예외처리

--- src/domain/Input.js
+++ src/domain/Input.js
@@ -1,16 +1,36 @@
 import { Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+import { validAmount, validBonusNumber } from './ValidationInput.js';
 
 export class Input {
   async getAmount() {
-    const amount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
-    return amount;
+    while (true) {
+      try {
+        const amount = await Console.readLineAsync(
+          '구입금액을 입력해 주세요.\n'
+        );
+        validAmount(amount);
+        return amount;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 
   async getWinningNumber() {
-    const winningNumber = await Console.readLineAsync(
+    const winningNumberInput = await Console.readLineAsync(
       '\n당첨 번호를 입력해 주세요.\n'
     );
-    return winningNumber.split(',').map((number) => Number(number));
+    const winningNumbers = winningNumberInput
+      .split(',')
+      .map((number) => Number(number.trim()));
+
+    try {
+      new Lotto(winningNumbers);
+    } catch (error) {
+      throw error;
+    }
+    return winningNumbers;
   }
 
   async getBonusNumber() {
