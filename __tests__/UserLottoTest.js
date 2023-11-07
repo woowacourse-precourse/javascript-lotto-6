62a488d8cef25d59f33079a3909a8782c5a4ebc8
jonique98 <tnals9899@naver.com>
2023-11-08 00:02:40 +0900

    test: UserLotto 클래스 기능 단위 테스트 SAFE INTEGER에 관한 체크 음수 혹은 40억 이상에 관해서 체크

--- __tests__/UserLottoTest.js
+++ __tests__/UserLottoTest.js
@@ -10,17 +10,39 @@ describe('UserLotto 클래스', () => {
     }).not.toThrow();
   });
 
-	test('유효하지 않은 숫자로 구매 금액이 주어지면 예외가 발생.', () => {
+  test('유효하지 않은 숫자로 구매 금액이 주어지면 예외가 발생.', () => {
     expect(() => {
       new UserLotto(NaN);
     }).toThrow(ERRORMESSAGE.purchaseInput);
   });
 
-	test('1,000 단위로 나누어지지 않는 구매 금액이 주어지면 예외가 발생.', () => {
+  test('1,000 단위로 나누어지지 않는 구매 금액이 주어지면 예외가 발생.', () => {
     expect(() => {
       new UserLotto(2500);
     }).toThrow(ERRORMESSAGE.purchaseAmount);
   });
 
-  
+  test('Number.MAX_SAFE_INTEGER를 초과하는 구매 금액이 주어지면 예외가 발생.', () => {
+    expect(() => {
+      new UserLotto(Number.MAX_SAFE_INTEGER + 1);
+    }).toThrow(ERRORMESSAGE.purchaseToBig);
+  });
+
+  test('Number.MIN_SAFE_INTEGER 미만의 구매 금액이 주어지면 예외가 발생.', () => {
+    expect(() => {
+      new UserLotto(Number.MIN_SAFE_INTEGER - 1);
+    }).toThrow(ERRORMESSAGE.purchaseToSmall);
+  });
+
+  test('음수 구매 금액이 주어지면 예외가 발생.', () => {
+    expect(() => {
+      new UserLotto(-1000);
+    }).toThrow(ERRORMESSAGE.purchaseRange1);
+  });
+
+  test('4,000,000,000을 초과하는 구매 금액이 주어지면 예외가 발생.', () => {
+    expect(() => {
+      new UserLotto(4000000001);
+    }).toThrow(ERRORMESSAGE.purchaseRange2);
+  });
 });
