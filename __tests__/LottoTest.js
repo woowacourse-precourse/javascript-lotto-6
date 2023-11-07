c8c3f93da569376194bd9c49554d43f0e8c3b909
lucaseunchae <lucaskim.dev@gmail.com>
2023-11-07 23:08:50 +0900

    test(Lotto): add calculate lotto quantity from price test

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,17 @@
-import Lotto from "../src/Lotto.js";
+import Lotto, { LOTTO_PRICE } from "../src/Lotto.js";
 
 describe("로또 클래스 테스트", () => {
+  test("로또의 갯수는 (구매 금액 / 로또 가격) 이다.", () => {
+    // given
+    const purchasePrice = 3000;
+
+    // when
+    const lottoQuantity = Lotto.calculateQuantityFromPrice(purchasePrice);
+
+    // then
+    expect(lottoQuantity).toBe(purchasePrice / LOTTO_PRICE);
+  });
+
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -13,6 +24,4 @@ describe("로또 클래스 테스트", () => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
-
-  // 아래에 추가 테스트 작성 가능
 });
