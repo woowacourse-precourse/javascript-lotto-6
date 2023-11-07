33147133a4ff27ca0052890a87431b1f86e68735
77JY <kwn03111@naver.com>
2023-11-07 15:12:36 +0900

    feat: 로또 티켓 출력 기능 구현
    - 사용자가 구매한 로또 번호들을 티켓 형태로 출력
    - 각 티켓은 정렬된 번호들을 포함

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -2,7 +2,9 @@ import { InputView } from "../views/InputView.js";
 import { OutputView } from "../views/OutputView.js";
 import { Validator } from "../utils/Validator.js";
 import { LottoPrice } from "../utils/LottoPrice.js";
+import { formatLottoNumber } from "../utils/formatLottoNumber.js";
 import { Print } from "../constants/constant.js";
+import { RandomLottoGenerator } from "../utils/RandomLottoGenerator.js";
 
 export const GameController = {
   async getLottoTiket() {
@@ -16,5 +18,16 @@ export const GameController = {
       OutputView.print(error.message);
       return this.getLottoTiket();
     }
+  },
+
+  async getLottoList(tikets) {
+    const LOTTO_BOX = [];
+    for (let i = 0; i < tikets; i++) {
+      let lotto_Number = RandomLottoGenerator();
+      Validator.isLotteryNumber(lotto_Number);
+      LOTTO_BOX.push(formatLottoNumber(lotto_Number));
+    }
+    OutputView.printLottoTikets(LOTTO_BOX);
+    return LOTTO_BOX;
   },
 };
