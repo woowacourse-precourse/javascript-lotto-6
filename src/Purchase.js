bf130b16d49148056e34ef7e039fb0c6859f5c27
kimyonghui <kyhui1115@gmail.com>
2023-11-07 18:08:18 +0900

    5. 잘못된 값을 입력할 경우 예외 처리(로또 구입)

--- src/Purchase.js
+++ src/Purchase.js
@@ -5,8 +5,25 @@ class Purchase {
   lottoList = [];
 
   constructor(amount) {
+    this.#validateIsString(amount);
+    this.#validateIsDivideThousand(amount);
     this.#amount = amount;
   }
+
+  #validateIsString(amount) {
+    if (/[^0-9]/.test(amount)) {
+      throw new Error('[ERROR] 로또 구입 금액은 숫자만 입력 가능합니다.');
+    }
+  }
+
+  #validateIsDivideThousand(amount) {
+    if (amount % 1000 !== 0) {
+      throw new Error(
+        '[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.'
+      );
+    }
+  }
+
   list() {
     const COUNT = this.#amount / 1000;
 
