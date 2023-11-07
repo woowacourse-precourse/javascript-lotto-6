9112f05f2bf1d55ca958a3a9a729ce99664bd460
kimyonghui <kyhui1115@gmail.com>
2023-11-07 18:14:44 +0900

    feat: 5. 예외처리 기능 구현(당첨 번호)

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,17 +2,46 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    this.#validateIsNumberLengthSix(numbers);
+    this.#validateIsNumberDuplicated(numbers);
+    this.#validateIsString(numbers);
+    this.#validateIsLottoNumber(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+  #validateIsNumberLengthSix(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateIsNumberDuplicated(numbers) {
+    const IS_DUPLICATED = numbers.some(
+      (number, index) => numbers.indexOf(number) !== index
+    );
+
+    if (IS_DUPLICATED) {
+      throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    }
+  }
+
+  #validateIsString(numbers) {
+    const IS_STRING = numbers.some((number) => /[^0-9]/.test(number));
+
+    if (IS_STRING) {
+      throw new Error('[ERROR] 로또 번호는 숫자만 입력 가능합니다.');
+    }
+  }
+
+  #validateIsLottoNumber(numbers) {
+    const IS_LOTTO_NUMBER = numbers.some(
+      (number) => Number(number) < 1 || Number(number) > 45
+    );
+
+    if (IS_LOTTO_NUMBER) {
+      throw new Error('[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+  }
 }
 
 export default Lotto;
