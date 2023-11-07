d852a8d7d96d481272d425166f46c3329a79940c
leedbswo107 <ljyu1@naver.com>
2023-11-07 18:19:50 +0900

    style: 코드 포맷 변경

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,11 +10,11 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) throw new Error(`${ERROR_MESSAGE.NUMBER_COUNT}`);
-    if (numbers.length !== new Set(numbers).size) throw new Error(`${ERROR_MESSAGE.NUMBER_DUPLICATE}`);
     numbers.forEach(e => {
       if (isNaN(e)) throw new Error(`${ERROR_MESSAGE.NUMBER_TYPE}`);
       if (e > 46 || e < 0) throw new Error(`${ERROR_MESSAGE.NUMBER_RANGE}`);
     });
+    if (numbers.length !== new Set(numbers).size) throw new Error(`${ERROR_MESSAGE.NUMBER_DUPLICATE}`);
   }
   // TODO: 추가 기능 구현
 }
