7f7a9acf10c42d066c4ed3c7201f2879ed6ba13f
Tomyo Kim <gihyun17@naver.com>
2023-11-07 21:14:10 +0900

    feat: error 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,14 +5,25 @@ class Lotto {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
-
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    for (const NUMBER of numbers) {
+      if (NUMBER < 1 || NUMBER > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    }
+
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
