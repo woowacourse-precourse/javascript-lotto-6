3dc82feea1c05a9d5b74c6a30732b47ab276d01c
[SW7] 박건우 <ceh2000212@gmail.com>
2023-11-08 04:21:23 +0900

    Feat indent depth 줄이기

--- src/App.js
+++ src/App.js
@@ -22,18 +22,18 @@ class App {
   }
 
   async user(){
-    await this.userInput();
+    await this.getUserInput();
     this.totalLottoListUser();
   }
 
   async lottoMachine(){
-    await this.settingLottoNumber();
-    await this.settingLottoBonusNumber();
+    await this.setLottoNumber();
+    await this.setLottoBonusNumber();
     this.checkLottoResult();
   }
 
 
-  async userInput() {
+  async getUserInput() {
     while (true) {
       const userInput = await this.getUserMoney();
       if (this.checkUserMoney(userInput)) {
@@ -65,18 +65,18 @@ class App {
     return true;
   }
 
-  userByLotto() {
+  lottoQuantity() {
     const count = this.userMoneyInput / 1000;
     Console.print(`${count}개를 구매했습니다.`);
     return count;
   }
 
   totalLottoListUser() {
-    const num = this.userByLotto();
+    const num = this.lottoQuantity();
     let lottoList = [];
 
     for (let i = 0; i < num; i++) {
-      let randomNumbers = this.generateRandomNumber();
+      let randomNumbers = this.generateLotto();
       randomNumbers.sort(function (a, b) {
         return a - b;
       })
@@ -87,11 +87,12 @@ class App {
     this.userByLottoList = lottoList;
   }
 
-  generateRandomNumber() {
+  generateLotto() {
     const lottoNumber = Random.pickUniqueNumbersInRange(1, 45, 7);
     return lottoNumber;
   }
-  async settingLottoNumber() {
+
+  async setLottoNumber() {
     while (true) {
       try {
         const result = await Console.readLineAsync("당첨 번호를 입력해 주세요");
@@ -101,16 +102,16 @@ class App {
         if (!isDuplicate && numbers.length === 6) {
           this.winningLotto = numbers;
           break;
-        } else {
-          // Console.print(ERROR.INVALID_INPUT_IS_DUPLICATED);
-        }
+        } 
       } catch (error) {
-        // Console.print(ERROR.INVALID_INPUT_LOTTO);
+        Console.print(ERROR.INVALID_INPUT_LOTTO);
       }
     }
   }
 
-  async settingLottoBonusNumber() {
+
+
+  async setLottoBonusNumber() {
     while (true) {
       try {
         const result = await Console.readLineAsync("보너스 번호를 입력해 주세요");
@@ -118,11 +119,9 @@ class App {
         if (!isDuplicate) {
           this.winningLotto.push(Number(result));
           break;
-        } else {
-          // Console.print(ERROR.INVALID_INPUT_IS_DUPLICATED);
         }
       } catch (error) {
-        // Console.print(ERROR.INVALID_INPUT_LOTTO);
+        Console.print(ERROR.INVALID_INPUT_LOTTO);
       }
     }
   }
@@ -132,25 +131,7 @@ class App {
     const bonusNumber = this.winningLotto.slice(6);
 
     this.userByLottoList.forEach((lotto, index) => {
-      let checkNumber = mainNumber.filter(x => lotto.includes(x));
-      let countScore = checkNumber.length;
-      if (countScore === 3) {
-        this.countWinner[5]++;
-      } else if (countScore === 4) {
-        this.countWinner[4]++;
-      } else if (countScore === 5) {
-        // 3등부터는 보너스 유무 중요
-        const bonusMatch = lotto.includes(bonusNumber[0]);
-        if (bonusMatch) {
-          this.countWinner[3]++; // 보너스 포함 5개
-        } else {
-          this.countWinner[2]++; // 보너스 제외 5개
-        }
-      } else if (countScore === 6) {
-        this.countWinner[1]++;
-      } else {
-        this.countWinner[6]++;
-      }
+        this.checkUserLotto(mainNumber,bonusNumber,lotto);
     });
 
     // 아무도 걸리지 않은 등수 값 0 처리 
@@ -178,6 +159,31 @@ class App {
     Console.print(`6개 일치 (2,000,000,000원) - ${this.countWinner[1]}개`);
     Console.print(`총 수익률은 ${profitRate}%입니다.`);
   }
+
+  checkUserLotto(mainNumber,bonusNumber,lotto){
+    let checkNumber = mainNumber.filter(x => lotto.includes(x));
+    let countScore = checkNumber.length;
+    if (countScore === 3) {
+      this.countWinner[5]++;
+    } else if (countScore === 4) {
+      this.countWinner[4]++;
+    } else if (countScore === 5) {
+      // 3등부터는 보너스 유무 중요
+      const bonusMatch = lotto.includes(bonusNumber[0]);
+      if (bonusMatch) {
+        this.countWinner[3]++; // 보너스 포함 5개 3등
+      } else {
+        this.countWinner[2]++; // 보너스 제외 5개 2등 
+      }
+    } else if (countScore === 6) {
+      this.countWinner[1]++;
+    } else {
+      this.countWinner[6]++;
+    }
+  }
+
+
+  
 }
 
 export default App;
