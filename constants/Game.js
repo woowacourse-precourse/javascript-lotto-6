30ffb43627c3744bdabc083d741fde00a30e8dcf
FirstBright <mipo8890@gmail.com>
2023-11-07 23:23:24 +0900

    :recycle: refactor: 게임 조건 상수화

---
+++ constants/Game.js
@@ -0,0 +1,10 @@
+const GAME = Object.freeze({
+  lottoLength: 6,
+  threeMatchWinnings: 5000,
+  fourMatchWinnings: 50000,
+  fiveMatchWinnigs: 1500000,
+  fiveMatchWinnigsWithBonus: 30000000,
+  sixMatchWinnigs: 2000000000,
+});
+
+export default GAME;
