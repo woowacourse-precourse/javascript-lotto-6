ea161d9a9767dde8335b8ccdbf62e4b23296f7ac
LEEHYEYEONG <elto789@naver.com>
2023-11-07 10:22:33 +0900

    feat: 당첨 번호, 보너스 번호 입력 기능 구현 - 예외처리(1~45 사이의 수 인지 체크, 문자인지 체크, 중복체크)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 import InputPrice from "./InputPrice.js";
+import InputBonus from "./InputBonus.js";
 
 class App {
   async play() {
@@ -10,28 +11,45 @@ class App {
     const budgetPrice = new InputPrice(budgetInput);
     budgetPrice.validate(budgetInput)
     const numLotto = budgetPrice.calculateLottoCount(budgetInput);
-    printLotto(numLotto);
+    const randomLottoArr = printLotto(numLotto);
+
+    const lottoInput = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
+    const lottoNumber = lottoInput.split(",");
+    const lotto = new Lotto(lottoNumber);
+    lotto.getValidate();
+
+    Console.print("");
+    const bonusInput = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+    const bonus = new InputBonus(bonusInput, lottoNumber);
+    bonus.validate(bonusInput, lottoNumber);
   }
 }
 
+// 랜덤 로또 번호 생성
 function makeLotto() {
-  const computer = [];
-  while (computer.length < 6) {
+  const randomLotto = [];
+  while (randomLotto.length < 6) {
     const number = Random.pickNumberInRange(1, 45);
-    if (!computer.includes(number)) {
-      computer.push(number);
+    if (!randomLotto.includes(number)) {
+      randomLotto.push(number);
     }
   }
-  return computer.sort();
+  // 배열을 오름차순으로 정렬
+  randomLotto.sort((a, b) => a - b)
+  return randomLotto;
 }
 
+// 랜덤 로또 번호 출력과 저장
 function printLotto(numLotto) {
+  const randomLottoArr = []
   Console.print("");
   Console.print(String(numLotto) + "개를 구매했습니다.")
   for (let i = 0; i < numLotto; i++) {
     Console.print(makeLotto())
+    randomLottoArr.push(makeLotto())
   }
   Console.print("");
+  return randomLottoArr;
 }
 
 export default App;
---
+++ src/InputBonus.js
@@ -0,0 +1,18 @@
+class InputBonus {
+  constructor(bonus, lottoArr) {
+    this.validate(bonus, lottoArr);
+    this.price = bonus;
+    this.lottoArr = lottoArr // 로또 배열을 전달하여 이 숫자가 로또 숫자와 중복된지 체크
+  }
+  validate(bonus, lottoArr) {
+    if (isNaN(parseInt(bonus))) {
+      throw new Error("[ERROR] 숫자를 입력해주세요. ");
+    } else if (lottoArr.includes(bonus)) {
+      throw new Error("[ERROR] 중복되지 않는 값으로 입력해주세요. ");
+    } else if (bonus > 45 || bonus <= 0) {
+      throw new Error("[ERROR] 1~45 사이의 수를 입력해주세요. ");
+    }
+  }
+}
+
+export default InputBonus
