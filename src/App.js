f773e7ba5bc8cf9dfa38f943916532574a23ea5f
leejaejae <leesengjie@naver.com>
2023-11-07 14:34:43 +0900

    feat(user-input): 사용자로부터 로또 구입 금액 입력 기능 추가 (로또 1장 가격: 1,000원)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const lottoMoney = await this.getLottoMoney(); // 로또 구매할 금액 입력
+  }
+
+  async getLottoMoney() {  // 로또 구매할 금액 입력
+    while(true){
+      try{
+        const input = await Console.readLineAsync("로또 구입 금액을 입력하세요.\n");
+        const lottoMoney = parseInt(input);
+        if(lottoMoney % 1000 != 0) {
+          throw new Error("[ERROR] 1,000원 단위로 입력해주세요.")
+        }
+        this.lottoMoney = lottoMoney;
+        return lottoMoney;
+      } catch (error){
+        Console.print(error.message);
+      }
+    }
+  }
 }
+
+
 
 export default App;
