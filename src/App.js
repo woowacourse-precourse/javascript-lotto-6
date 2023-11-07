e0276833e2926d4a4343d0a384f3a1047e5338d1
Lim-shweety <smile70200@naver.com>
2023-11-07 22:32:31 +0900

    :sparkles: feat: 로또 번호 입력받기 로또 번호 입력받고, 예외처리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Lotto from './Lotto.js';
 class App {
   async play() {
     const MONEY = await this.inputMoney();
+    const INPUT_NUMBERS = await this.getLottoNumber();
+    const LOTTO_NUMBERS = new Lotto(INPUT_NUMBERS);
   }
 
   async inputMoney() {
@@ -21,6 +23,16 @@ class App {
     if (Number(money) % 1000 !== 0) {
       throw new Error('[ERROR] 로또 금액은 1000원 단위어야 합니다.');
     }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async getLottoNumber() {
+    const INPUT = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    const INPUT_ARRAY = INPUT.split(',');
+    for (let i = 0; i < INPUT_ARRAY.length; i += 1) {
+      INPUT_ARRAY[i] = Number(INPUT_ARRAY[i]);
+    }
+    return INPUT_ARRAY;
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,27 +1,26 @@
 class Lotto {
-    #numbers;
+  #numbers;
 
-    constructor(numbers) {
-        this.#validate(numbers);
-        this.#validateDuplicate(numbers);
-        this.#numbers = numbers;
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
 
+  // eslint-disable-next-line class-methods-use-this
+  #validate(numbers) {
+    const SET_NUMBERS = new Set(numbers);
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
-
-    #validate(numbers) {
-        if (numbers.length !== 6) {
-            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    if (SET_NUMBERS.size !== numbers.length) {
+      throw new Error('[ERROR] 중복되는 숫자가 존재합니다.');
     }
-
-    #validateDuplicate(numbers) {
-        const SET_NUMBERS = new Set(numbers);
-        if (SET_NUMBERS.length !== numbers.length) {
-            throw new Error("[ERROR] 중복되는 로또 번호가 입력 되었습니다.");
-        }
-
+    for (let i = 0; i < numbers.length; i += 1) {
+      if (numbers[i] > 45 || numbers[i] < 0) {
+        throw new Error('[ERROR] 로또 숫자 범위는 1~45 까지 입니다.');
+      }
     }
-
+  }
 }
 
 export default Lotto;
