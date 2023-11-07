e2d9d621e794ba6e139b63e23d5da3c89585b06e
damdam6 <dambi626626@gmail.com>
2023-11-07 15:44:05 +0900

    refactor: textview분리 수정

--- src/DB.js
+++ src/DB.js
@@ -9,6 +9,21 @@ const INITIALIZE_NUM = 0;
 const INITIALIZE_ARR = [];
 const INITIALIZE_OBJECT = {};
 
+const TEXTVIEW_REQUEST_MONEYINPUT = '구입금액을 입력해 주세요';
+const TEXTVIEW_LOTTO_AMOUNT = '개를 구매했습니다.';
+const TEXTVIEW_REQUEST_LOTTOWIN = '당첨 번호를 입력해주세요.';
+const TEXTVIEW_REQUEST_LOTTOBONUS = '보너스 번호를 입력해주세요.';
+const TEXTVIEW_RESULT = ([win3, win4, win5, win5Bonus, win6, percent]) => `
+당첨 통계\n
+---\n
+3개 일치 (5,000원) - ${win3}\n
+4개 일치 (50,000원) - ${win4}\n
+5개 일치 (1,500,000원) - ${win5}\n
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${win5Bonus}\n
+6개 일치 (2,000,000,000원) - ${win6}\n
+총 수익률은 ${percent}%입니다.
+`;
+
 class Data {
   #dataResetNeed = false;
 
