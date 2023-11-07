b43a7e703fe67437fca7f25b259a09ae07ee6f12
hyunyeee <5004ohy@naver.com>
2023-11-07 21:59:30 +0900

    feat: 구입 금액을 입력 받는다. 1000원으로 나누어 떨어지지 않는 경우 예외 처리한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const PRICE_MESSAGE = "구입금액을 입력해 주세요.\n";
+const ERROR_PRICE_INPUT = "[ERROR] 숫자가 잘못된 형식입니다.\n";
+
 class App {
-  async play() {}
+  async play() {
+    await this.#controller();
+  }
+  async #controller() {
+    while (true) {
+      try {
+        await this.#readPrice();
+        break;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
+  }
+
+  async #readPrice() {
+    const price = await MissionUtils.Console.readLineAsync(PRICE_MESSAGE);
+
+    if (price % 1000 !== 0) {
+      MissionUtils.Console.print(price);
+      throw new Error(ERROR_PRICE_INPUT);
+    }
+  }
 }
 
 export default App;
