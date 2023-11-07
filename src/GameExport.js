566cdadd21c5ade7b3037b2bd50d589d5f73e7f0
woo7i <or2447221@gmail.com>
2023-11-07 17:02:42 +0900

    feat(GameExport) : 전반적인 게임로직을 다루는 클래스로 파일명 변경

--- src/GameExport.js
+++ src/GameExport.js
@@ -2,7 +2,7 @@ import Lotto from './Lotto.js';
 import { PurchaseLotto } from './PurchaseLotto.js';
 import { randomNum } from './utils.js';
 
-class LottoList {
+class GameExport {
   #lottos;
 
   constructor() {
@@ -12,31 +12,33 @@ class LottoList {
 
   makeLotto(lottoNumbers) {
     const lotto = new Lotto(lottoNumbers);
-
     this.#lottos.push(lotto);
   }
 
   async lottoCount() {
-    await this.purchaseLotto.initialize(); // 비동기 초기화를 기다린 후에
-    return this.purchaseLotto.getLottoCount(); // 로또 개수를 반환.
+    await this.purchaseLotto.initialize();
+    return this.purchaseLotto.getLottoCount();
   }
 
   async createLotto() {
     const count = await this.lottoCount();
+
     for (let i = 0; i < count; i++) {
-      let lottoNumbers = randomNum(); // 로또 번호를 생성하는 함수 호출
+      let lottoNumbers = randomNum();
       lottoNumbers = lottoNumbers.sort((numA, numB) => numA - numB);
-      this.makeLotto(lottoNumbers); // 수정된 부분
+      this.makeLotto(lottoNumbers);
     }
-    // Set을 사용하지 않고, 바로 #lottos 배열에 저장
-    return this.#lottos.map((lotto) => lotto.getNumbers()); // Lotto 객체의 번호를 반환
+    return this.#lottos.map((lotto) => lotto.getNumbers());
   }
 
   async viewLottoList() {
     const theLottoList = await this.createLotto();
-
     return theLottoList.map((item) => `[${item.join(', ')}]`).join(`\n`);
+  }
+
+  getLottos() {
+    return this.#lottos;
   }
 }
 
-export default LottoList;
+export default GameExport;
