b50a6e1568d531185e9263c6b6deea5ac8e17aee
Tomyo Kim <gihyun17@naver.com>
2023-11-07 21:08:45 +0900

    feat: 구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Lotto from "./Lotto.js";
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const SUM_OF_PURCHASE = await this.inputAmount();
+
+    if (SUM_OF_PURCHASE % 1000 !== 0) {
+      Console.print("[ERROR] 1,000원 단위로 입력하세요.");
+      return;
+    }
+    this.SHOW_WINNING(WINNING);
+  }
+
+  async inputAmount() {
+    while (true) {
+      const SUM_OF_PURCHASE = await Console.readLineAsync(
+        "구입금액을 입력해 주세요.\n"
+      );
+      if (SUM_OF_PURCHASE % 1000 === 0) {
+        return parseInt(SUM_OF_PURCHASE, 10);
+      } else {
+        Console.print("[ERROR] 1,000원 단위로 입력하세요.");
+      }
+    }
+  }
 }
 
 export default App;
