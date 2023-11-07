8b057664c1aab27e787a6d31520e65e5a55b198c
starcradle101 <dreamerdev12@gmail.com>
2023-11-08 05:35:17 +0900

    🛠️Feat($Output): 발행한 로또 출력 기능 구현

--- src/ui/Output.js
+++ src/ui/Output.js
@@ -1,1 +1,11 @@
-import { Console, Random } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
+import { USER_OUTPUT } from '../constants/Logs';
+import CONSTANTS from '../constants/Constants';
+
+const printLottoTickets = lottoTickets => {
+  const lottoPurchaseStatus = `${lottoTickets.length}${USER_OUTPUT.purchaseCountPrompt}`;
+  const ticketLines = lottoTickets
+    .map(ticket => `[${ticket.join(', ')}]`)
+    .join('\n');
+  Console.print(`\n${lottoPurchaseStatus}\n${ticketLines}`);
+};
