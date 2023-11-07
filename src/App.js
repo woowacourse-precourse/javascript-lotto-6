4316a65d17565c72ed0261bef023157cf9b33c1b
junghanlee <dlwjdgks126@hansung.ac.kr>
2023-11-07 23:28:01 +0900

    입력 받은 돈 예외처리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Console, Random } from "woowacourse/mission-utils";
-import Lotto from "./Lotto.js";
-import {INPUT} from "./text.js";
+import {INPUT, OUTPUT_ERROR} from "./text.js";
 class App {
   async play() {
     const PURCHASE_AMOUNT = this.inputPurchaseAmount();
   }
   async inputPurchaseAmount() {
     const MONEY = await Console.readLineAsync(INPUT.input_money);
-    
+    if (MONEY % 1000 !== 0) {
+      throw new Error(OUTPUT_ERROR.money_err);
+    }
     return MONEY / 1000;
   }
 }
--- src/text.js
+++ src/text.js
@@ -1,3 +1,6 @@
 export const INPUT = {
   input_money: '구입금액을 입력해 주세요 : ',
 }
+export const OUTPUT_ERROR = {
+  money_err: '[ERROR] 1000원 단위로 입력해주세요.',
+}
