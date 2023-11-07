aac69824050becc195685647e4968f23189b7eb3
SongInJae <pan0663@naver.com>
2023-11-08 00:04:31 +0900

    feat: 구입기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+  async payAmount() {
+    const amount = await MissionUtils.Console.readLineAsync(
+      "구입금액을 입력해 주세요."
+    );
+    // TODO: 숫자인지 확인
+    return amount;
+  }
   async play() {}
 }
 
