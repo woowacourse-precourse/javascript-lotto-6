581cfbd5adca28ee9da0b613636991d094debc6c
j8won <joowoni99@gmail.com>
2023-11-08 03:12:44 +0900

    test(LottoTest): 로또 숫자를 오름차순으로 정렬하는지 확인하는 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -8,12 +8,14 @@ describe("로또 클래스 테스트", () => {
     }).toThrow(LOTTO.ERROR.SIZE);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow(LOTTO.ERROR.DUPLICATE);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호는 오름차순으로 정렬한다.", () => {
+    const lotto = new Lotto([6, 5, 4, 3, 2, 1]);
+    expect(lotto.numbers).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
