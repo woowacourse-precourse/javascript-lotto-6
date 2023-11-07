b4512f78761242f71560c577e292631cb58ac649
leey00nsu <dbstndla1212@naver.com>
2023-11-07 18:59:45 +0900

    test: 발행한 로또 숫자 n개의 각 로또의 번호를 표시한다 테스트 추가 LottoTest 테스트 케이스 추가

--- __tests__/services/LottoService.js
+++ __tests__/services/LottoService.js
@@ -8,11 +8,11 @@ describe('LottoService 테스트', () => {
     lottoService = new LottoService();
   });
 
-  test(`Account 객체를 반환할 수 있다.`, () => {
-    expect(lottoService.getAccount()).toBeInstanceOf(Account);
+  test(`초기화 되었을 때 구입 금액 0원을 반환할 수 있다.`, () => {
+    expect(lottoService.getPurchaseAmount()).toBe(0);
   });
 
-  test(`입력한 금액 1000원에 대해 1000단위로 나눈 1개의 로또를 구매할 수 있다.`, () => {
+  test(`입력한 금액 1000원에 대해 1000원 단위로 나눈 1개의 로또를 구매할 수 있다.`, () => {
     lottoService.setPurchaseAmount(1000);
 
     lottoService.buyLottos();
@@ -20,7 +20,7 @@ describe('LottoService 테스트', () => {
     expect(lottoService.getLottos()).toHaveLength(1);
   });
 
-  test(`입력한 금액 2000원에 대해 1000단위로 나눈 2개의 로또를 구매할 수 있다.`, () => {
+  test(`입력한 금액 2000원에 대해 1000원 단위로 나눈 2개의 로또를 구매할 수 있다.`, () => {
     lottoService.setPurchaseAmount(2000);
 
     lottoService.buyLottos();
