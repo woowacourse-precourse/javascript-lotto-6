d125f39b5255ec828f76616f439a7f7bedd7161f
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-08 02:35:37 +0900

    refactor: 일부 상수 이름 및 형식 변경

--- src/common/constants.js
+++ src/common/constants.js
@@ -4,7 +4,6 @@ const LOTTO = Object.freeze({
   max_number: 45,
   min_match: 3,
   max_match: 6,
-  winning_length: 6,
   bonus_length: 1,
 });
 
@@ -52,31 +51,31 @@ const RANKING = {
     match: 3,
     bonus: false,
     prize: 5000,
-    message: `3개 일치 (5,000원) - `,
+    message: '3개 일치 (5,000원) - ',
   },
   'FOURTH': {
     match: 4,
     bonus: false,
     prize: 50000,
-    message: `4개 일치 (50,000원) - `,
+    message: '4개 일치 (50,000원) - ',
   },
   'THIRD': {
     match: 5,
     bonus: false,
     prize: 1500000,
-    message: `5개 일치 (1,500,000원) - `,
+    message: '5개 일치 (1,500,000원) - ',
   },
   'SECOND': {
     match: 5,
     bonus: true,
     prize: 30000000,
-    message: `5개 일치, 보너스 볼 일치 (30,000,000원) - `,
+    message: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
   },
   'FIRST': {
     match: 6,
     bonus: false,
     prize: 2000000000,
-    message: `6개 일치 (2,000,000,000원) - `,
+    message: '6개 일치 (2,000,000,000원) - ',
   },
 };
 
