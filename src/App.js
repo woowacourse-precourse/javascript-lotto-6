c323cf5f7be5e591a9cca603d892bdcc640bd5b4
mingyeong:D <63152086+Kim-Mingyeong@users.noreply.github.com>
2023-11-08 00:50:16 +0900

    feat: 발행한 로또 수량 및 로또 번호 오름차순으로 출력

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 import { GAME, ERROR } from "./message";
-import { Lotto } from "./Lotto";
+import Lotto from "./Lotto";
 
 class App {
-  #amount;
   constructor() {
-    this.#amount = 0;
+    this.amount = 0;
+    this.lottos = [];
   }
 
-  #validate(amount) {
+  validateAmount = (amount) => {
     if (isNaN(amount) || amount < 0) {
       throw new Error(ERROR.NOT_NUMBER);
     }
     if (amount % 1000 !== 0) {
       throw new Error(ERROR.INVALID_AMOUNT);
     }
-  }
+  };
 
   inputPurchaseAmount = async () => {
     try {
@@ -23,15 +23,30 @@ class App {
         await Console.readLineAsync(GAME.INPUT.AMOUNT),
         10
       );
-      this.#validate(amount);
-      this.#amount = amount;
+      this.validateAmount(amount);
+      this.amount = amount / 1000;
     } catch (error) {
       console.error(error.message);
       this.inputPurchaseAmount();
     }
+    return this.amount;
   };
 
-  async play() {}
+  generateLottosList = (amount) => {
+    Console.print(`${this.amount}개를 구입했습니다.\n`);
+    for (let i = 0; i < amount; i += 1) {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      numbers.sort((a, b) => a - b);
+      const lotto = new Lotto(numbers);
+      this.lottos.push(lotto);
+      Console.print(lotto.toString());
+    }
+  };
+
+  async play() {
+    const amount = await this.inputPurchaseAmount();
+    this.generateLottosList(amount);
+  }
 }
 
 export default App;
