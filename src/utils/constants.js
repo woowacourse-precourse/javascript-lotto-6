5fb0154451e7ffd84f8229e3b661bbc42c5a90cb
yeokyoung song <spolice0324@gmail.com>
2023-11-07 17:50:54 +0900

    feat : 난수 생성 함수에 정렬 추가, 상수 선언
    - 오름차순으로 랜덤으로 생성된 로또 번호들을 정렬하는 로직 추가
    - 매직 넘버 상수로 선언 및 대체

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+const LOTTO_INFO = Object.freeze({
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  PRICE: 1000,
+  LENGTH: 6,
+  MIN_LOTTO_MONEY: 1000,
+  PRIZE: {
+    1: 2000000000,
+    2: 30000000,
+    3: 1500000,
+    4: 50000,
+    5: 5000,
+    6: 0,
+  },
+});
+
+
+export {LOTTO_INFO};
--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,7 +1,15 @@
 import { Random } from '@woowacourse/mission-utils';
+import { LOTTO_INFO } from './constants';
+
+const [min, max, size] = [
+  LOTTO_INFO.MIN_NUMBER,
+  LOTTO_INFO.MAX_NUMBER,
+  LOTTO_INFO.LENGTH,
+];
 
 const getRandomNumber = () => {
-  return Random.pickUniqueNumbersInRange(1, 45, 6);
+  const lottoNumbers = Random.pickUniqueNumbersInRange(min, max, size);
+  return lottoNumbers.sort((a, b) => a - b);
 };
 
 export default getRandomNumber;
