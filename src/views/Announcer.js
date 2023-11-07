7f6c99de1a93d559aeaa90700662f7f68e1d345f
zxxng <jeayeong123@naver.com>
2023-11-08 01:25:06 +0900

    refactor: Announcer 상수 분리 및 출력 로직 수정

--- src/views/Announcer.js
+++ src/views/Announcer.js
@@ -1,27 +1,30 @@
 import { Console } from '@woowacourse/mission-utils';
+import { PROMPT_MESSEGE, RESULT_MESSEGE } from '../constants/messages.js';
+
+const purchase = (amount) => `\n${amount}개를 구매했습니다.`;
+const formatLotto = (lotto) => `[${lotto.join(', ')}]\n`;
+const profitRate = (rate) => `총 수익률은 ${rate}%입니다.`;
 
 class Announcer {
-  printLottos(lottos, amount) {
-    const lottoList = lottos.getLottos();
-    Console.print(`${amount}개를 구매했습니다.\n`);
+  printLottoBundle(lottoBundle, amount) {
+    const lottoList = lottoBundle.getLottoBundle();
+
+    Console.print(purchase(amount));
     lottoList.forEach((lotto) => {
-      const lottoNumbers = lotto.getLotto();
-      Console.print(`[${lottoNumbers.join(', ')}]\n`);
+      Console.print(formatLotto(lotto.getLotto()));
     });
-    Console.print('\n');
+    Console.print(PROMPT_MESSEGE.newLine);
   }
 
   printPrizeInfo(analyzer) {
-    const prizeInfo = analyzer.getPrizeInfo();
-    Console.print('당첨 통계\n---\n');
-    Console.print(`3개 일치 (5,000원) - ${prizeInfo.fifthPlace}개\n`);
-    Console.print(`4개 일치 (50,000원) - ${prizeInfo.fourthPlace}개\n`);
-    Console.print(`5개 일치 (1,500,000원) - ${prizeInfo.thirdPlace}개\n`);
-    Console.print(
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${prizeInfo.secondPlace}개\n`,
-    );
-    Console.print(`6개 일치 (2,000,000,000원) - ${prizeInfo.firstPlace}개\n`);
-    Console.print(`총 수익률은 ${prizeInfo.prizeYield}%입니다.`);
+    Console.print(PROMPT_MESSEGE.lottoResult);
+
+    const rankedLotto = analyzer.getRankedLotto();
+    const rank = ['fifth', 'forth', 'third', 'second', 'first'];
+    RESULT_MESSEGE.forEach((messege, idx) => {
+      Console.print(`${messege + rankedLotto[rank[idx]]}개`);
+    });
+    Console.print(profitRate(analyzer.calculateProfitRate()));
   }
 }
 
