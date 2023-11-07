be5caf8b124d0a0b45f8615f152b85683e427feb
Jeongeun Lee <leeju1009@gmail.com>
2023-11-08 03:15:26 +0900

    feat: 당첨 로또의 class를 생성

---
+++ src/WinningLotto.js
@@ -0,0 +1,42 @@
+import Lotto from './Lotto.js';
+import Validation from './Validation.js';
+
+const NUMBER_NAME = {
+  winning: '당첨',
+  bonus: '보너스',
+};
+
+class WinningLotto extends Lotto {
+  #bonusNumber;
+
+  constructor(numbers, bonusNumber) {
+    super(numbers);
+
+    this.#validateBonus(bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  #validateBonus(bonusNumber) {
+    Validation.isNumber(bonusNumber);
+    Validation.isNumberInRange(bonusNumber);
+
+    const winningNumbers = this.getNumbers();
+    Validation.isNonDuplicatedBonusNumber(bonusNumber, winningNumbers);
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+
+  getTotalNumbers() {
+    const winningNumbers = this.getNumbers();
+    const totalNumbers = new Map([
+      [NUMBER_NAME.winning, winningNumbers],
+      [NUMBER_NAME.bonus, this.#bonusNumber],
+    ]);
+
+    return totalNumbers;
+  }
+}
+
+export default WinningLotto;
