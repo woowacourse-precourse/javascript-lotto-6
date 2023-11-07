a05fb0a65f179e784fb116f05b5c09a7dd1c844d
intersoom <sallybig@naver.com>
2023-11-08 03:54:50 +0900

    feat: 로또 구입 금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+import { validatePayment } from "./ValidateInput";
+
 class App {
-  async play() {}
+  async play() {
+    await this.readPayment();
+  }
+
+  async readPayment() {
+    while (true) {
+      try {
+        const payment = Input.readPayment();
+        validatePayment(payment);
+        return Number(payment);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
 }
 
 export default App;
