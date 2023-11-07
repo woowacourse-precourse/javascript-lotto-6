5abe60c4a7b4dc639f5780936f54f999183c2b27
minSsan <msp1125@hanyang.ac.kr>
2023-11-07 16:11:30 +0900

    feat: constants.js 추가

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+/**
+ * 안내 문구
+ */
+export const INFO_MSG = Object.freeze({
+  AMOUNT_INPUT: "구입금액을 입력해주세요.\n",
+  WINNING_INPUT: "당첨 번호를 입력해 주세요.\n",
+  BONUS_INPUT: "보너스 번호를 입력해 주세요.\n",
+});
+
+export const MIN_NUMBER = 1;
+export const MAX_NUMBER = 45;
+export const NUM_OF_LOTTO = 6;
+
+export const LOTTO_VALUE = 1000;
