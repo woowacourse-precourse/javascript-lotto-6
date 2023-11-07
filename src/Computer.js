7f1c2759fa9b966c17321117dc94b1fc9a021592
JungHun98 <j7papa@naver.com>
2023-11-07 13:12:22 +0900

    feat: 컴퓨터 클래스 getPrizeResult 메소드 구현

--- src/Computer.js
+++ src/Computer.js
@@ -6,6 +6,19 @@ class Computer {
     this.#winnerNumbers = winnnersNumbers;
     this.#bonusNumber = bonusNumber;
   }
+
+  getPrizeResult(lottos) {
+    const mathResult = lottos.map((lotto) => {
+      const lottoNumbers = lotto.getNumbers();
+      const mathNumbers = this.#winnerNumbers.filter((number) => lottoNumbers.includes(number));
+      const mathNumber = mathNumbers.length;
+      const isMatchBonus = lottoNumbers.includes(this.#bonusNumber);
+
+      return { mathNumber, isMatchBonus };
+    });
+
+    return mathResult;
+  }
 }
 
 export default Computer;
