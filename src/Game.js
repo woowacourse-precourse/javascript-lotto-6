ff0228185fd0ff342e77b15e0e6038de95c4de67
sihyun10 <hyhy200170@gmail.com>
2023-11-07 22:13:11 +0900

    feat: 당첨내역과 수익률 출력하는 기능 구현 사용자가 구매한 로또 번호와 당첨번호, 보너스번호를 서로 비교해서 일치하는 개수를 확인해, 1등~5등까지의 경우를 출력하는 기능을 구현하였습니다. 또한 수익률도 출력해주었습니다. 당첨내역 및 수익률을 출력한뒤에 로또 게임을 종료시킵니다.

--- src/Game.js
+++ src/Game.js
@@ -5,32 +5,105 @@ import { Console } from '@woowacourse/mission-utils';
 class Game {
   #user;
   #lottoCount;
+  #lottoList;
 
   constructor() {
     this.#user = new User();
     this.#lottoCount = null;
+    this.#lottoList = [];
   }
 
   async playGame() {
     this.#lottoCount = await this.#user.calculateLottoCount();
+    this.generateLottoNumbers();
     this.printLottoNumbers();
-    this.compareWinningNumbers();
+    const winningNumbers = await this.#user.getWinningNumbers();
+    const bonusNumber = await this.#user.getBonusNumber();
+    const stats = this.compareWinningNumbers(winningNumbers, bonusNumber);
+    this.printWinningResult(stats);
   }
 
-  async printLottoNumbers() {
+  generateLottoNumbers() {
     const initialLottoNumbers = [1, 2, 3, 4, 5, 6];
 
     for (let i = 0; i < this.#lottoCount; i += 1) {
       const lotto = new Lotto(initialLottoNumbers);
       lotto.generateRandomNumbers();
-      const lottoNumbers = lotto.getNumbers();
-      Console.print(`[${lottoNumbers.join(', ')}]`);
+      this.#lottoList.push(lotto.getNumbers());
     }
   }
 
-  async compareWinningNumbers() {
-    const winningNumbers = await this.#user.getWinningNumbers();
-    const bonusNumber = await this.#user.getBonusNumber();
+  printLottoNumbers() {
+    this.#lottoList.forEach((lottoNumbers) => {
+      Console.print(`[${lottoNumbers.join(', ')}]`);
+    });
+  }
+
+  compareWinningNumbers(winningNumbers, bonusNumber) {
+    const stats = {
+      3: { prize: '5,000원', count: 0 },
+      4: { prize: '50,000원', count: 0 },
+      5: { prize: '1,500,000원', count: 0 },
+      five_b: { prize: '30,000,000원', count: 0 },
+      6: { prize: '2,000,000,000원', count: 0 },
+    };
+
+    this.#lottoList.forEach((userLotto) => {
+      const matchCount = this.countMatchingNumbers(userLotto, winningNumbers);
+      const hasBonusNumber = userLotto.includes(bonusNumber);
+
+      if (matchCount === 5 && hasBonusNumber) {
+        stats['five_b'].count += 1;
+      } else if (matchCount >= 3) {
+        stats[matchCount].count += 1;
+      }
+    });
+
+    return stats;
+  }
+
+  countMatchingNumbers(lotto, winningNumbers) {
+    return lotto.filter((num) => winningNumbers.includes(num)).length;
+  }
+
+  printWinningResult(stats) {
+    Console.print('당첨 통계');
+    Console.print('---');
+
+    const prizeKeys = [3, 4, 5, 'five_b', 6];
+
+    prizeKeys.forEach((key) => {
+      const prize = stats[key].prize;
+      const count = stats[key].count;
+      const message = count === 0 ? `0개` : `${count}개`;
+      const matchMessage =
+        key === 'five_b' ? '5개 일치, 보너스 볼 일치' : `${key}개 일치`;
+      Console.print(`${matchMessage} (${prize}) - ${message}`);
+    });
+
+    const totalPrize = this.calculateTotalPrize(stats);
+    if (totalPrize === 0) {
+      Console.print('총 수익률은 0%입니다.');
+      Console.print('\n당첨되지 않았습니다. 다음 기회에 참여해주세요.');
+    } else {
+      const revenueRate = (
+        (totalPrize / (this.#lottoCount * 1000)) *
+        100
+      ).toFixed(1);
+      Console.print(`총 수익률은 ${revenueRate}%입니다.`);
+    }
+  }
+
+  calculateTotalPrize(stats) {
+    let totalPrize = 0;
+
+    [3, 4, 5, 'five_b', 6].forEach((key) => {
+      const count = stats[key].count;
+      const prize = stats[key].prize;
+      totalPrize += count * parseInt(prize.replace(/,/g, ''));
+    });
+
+    return totalPrize;
   }
 }
 
