3780971b8cdecef89c47d976fc8c6c250b2845ec
BHyeonKim <rlaqhguse@gmail.com>
2023-11-07 20:46:04 +0900

    test: 로또 클래스 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,5 +1,4 @@
 import Lotto from '../src/Lotto.js';
-import { parseNumber, parseNumbers } from '../src/utils/index.js';
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -16,21 +15,16 @@ describe('로또 클래스 테스트', () => {
   });
 
   // 아래에 추가 테스트 작성 가능
-});
-
-describe('로또 번호 파싱 테스트', () => {
-  test.each([
-    ['8,21,34,하,23'],
-    ['6,3,42,3,2,,,,,'],
-    [',,,,5,3,4,2'],
-    ['5,2,,,24,3,2'],
-  ])('입력에 숫자가 아닌게 올 경우', (input) => {
-    expect(() => parseNumbers(input)).toThrow('[ERROR]');
+  test('로또를 채점하기 전에 로또 순위를 가져오려고 하면 예외가 발생한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(() => lotto.getRank()).toThrow('[ERROR]');
   });
-});
 
-describe('보너스 번호 파싱 테스트', () => {
-  test.each([['가'], ['다']])('입력애 숫자가 아닌게 올 경우', (input) => {
-    expect(() => parseNumber(input)).toThrow('[ERROR]');
+  test('로또를 채점하고 로또 순위를 가져오려고 하면 정상적으로 로또 순위를 가져온다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(() => lotto.getRank()).toThrow('[ERROR]');
+
+    lotto.checkLotto([1, 2, 3, 4, 5, 8], 6);
+    expect(lotto.getRank()).toEqual('2등');
   });
 });
