8c6228e6a362d4fad523f0e7e85947999aced0af
8leamnk <silkviewrk@gmail.com>
2023-11-07 11:27:19 +0900

    test(priceModelTest): 로또 발행 개수에 대한 테스트 코드를 수정함
    수정 내용:
    - describe로 그룹화

--- __tests__/models/priceModelTest.js
+++ __tests__/models/priceModelTest.js
@@ -1,19 +1,21 @@
 import priceModel from '../../src/models/priceModel.js';
 
-test('발행한 로또 개수가 알맞게 계산된다.', () => {
-  const inputs = [10000, 5000, 17000, 200000, 7000];
-  const outputs = [10, 5, 17, 200, 7];
+describe('발행할 로또 개수를 계산', () => {
+  test('발행한 로또 개수가 알맞게 계산된다.', () => {
+    const inputs = [10000, 5000, 17000, 200000, 7000];
+    const outputs = [10, 5, 17, 200, 7];
 
-  outputs.forEach((output, index) => {
-    expect(priceModel.calculateNumberOfLotto(inputs[index])).toBe(output);
+    outputs.forEach((output, index) => {
+      expect(priceModel.calculateNumberOfLotto(inputs[index])).toBe(output);
+    });
   });
-});
 
-test('발행한 로또 개수가 다르게 계산된다.', () => {
-  const inputs = [15000, 50000, 170000, 2000, 70000];
-  const outputs = [10, 5, 17, 200, 7];
+  test('발행한 로또 개수가 다르게 계산된다.', () => {
+    const inputs = [15000, 50000, 170000, 2000, 70000];
+    const outputs = [10, 5, 17, 200, 7];
 
-  outputs.forEach((output, index) => {
-    expect(priceModel.calculateNumberOfLotto(inputs[index])).not.toBe(output);
+    outputs.forEach((output, index) => {
+      expect(priceModel.calculateNumberOfLotto(inputs[index])).not.toBe(output);
+    });
   });
 });
