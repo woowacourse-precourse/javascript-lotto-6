4cfbe2096553e46cb469dc6285908fb29fac668c
sejineeee <sejinee0918@gmail.com>
2023-11-07 10:40:02 +0900

    feature: 구입 금액만큼 로또를 생성하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import Input from './Input.js';
 
 const CURRENCY_UNIT = 1000;
 const input = new Input();
 
 class App {
+  purchaseAmount;
+  myLottoLits;
+
+  constructor() {
+    this.purchaseAmount = 0;
+    this.myLottoLits = [];
+  }
+
   async play() {
-    const purchaseAmount = await input.askPurchaseAmount();
-    const parsedpurchaseAmount = parseInt(purchaseAmount, 10);
+    const inputPurchaseAmount = await input.askPurchaseAmount();
+    const parsedpurchaseAmount = parseInt(inputPurchaseAmount, 10);
 
     this.validateAskPurchaseAmount(parsedpurchaseAmount);
   }
@@ -20,6 +28,19 @@ class App {
     if (purchaseAmount % CURRENCY_UNIT) {
       throw new Error('[ERROR] 1,000원 단위로 입력해주세요.');
     }
+  }
+
+  createLottos(parsedPurchaseAmount) {
+    this.purchaseAmount = parsedPurchaseAmount / CURRENCY_UNIT;
+
+    let count = 0;
+    while (count < this.purchaseAmount) {
+      this.myLottoLits.push(Random.pickUniqueNumbersInRange(1, 45, 6));
+
+      count += 1;
+    }
+
+    return this.myLottoLits;
   }
 }
 
