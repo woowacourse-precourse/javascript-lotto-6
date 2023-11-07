b6a529e07f1aa9b5e31e7952e07f7668694f2ebd
p0cari <pocari3434@gmail.com>
2023-11-07 22:16:03 +0900

    당첨 번호가 유효한지 검사하는 함수

--- src/mvc/controller/child/winning_lotto_manage.js
+++ src/mvc/controller/child/winning_lotto_manage.js
@@ -1,3 +1,4 @@
+import Lotto from '../../../utils/error/type/Lotto.js';
 import printError from '../../../utils/error/print_error.js';
 import Question from '../../../utils/input/question.js';
 import userInput from '../../../utils/input/user_input.js';
@@ -14,6 +15,15 @@ class WinningLottoManage {
   async inputWinningNum() {
     this.#winningNum = await userInput(Question.winningNum());
     this.#winningNum = this.#winningNum.split(',');
+  }
+
+  async #checkWinningNum() {
+    try {
+      new Lotto(this.#winningNum);
+    } catch (error) {
+      printError(error);
+      await this.inputWinningNum();
+    }
   }
 }
 
