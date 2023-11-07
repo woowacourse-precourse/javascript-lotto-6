d38875acb532f9cc5790c53f3e42269359740c72
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-11-07 20:38:11 +0900

    :recycle: refactor: 컨트롤러의 기능분리 및 주석추가

--- src/controllers/LottoGameController.js
+++ src/controllers/LottoGameController.js
@@ -1,4 +1,5 @@
 import Lotto from '../Lotto.js';
+import { PRICE_PER_TICKET } from '../constants/constants.js';
 
 class LottoGameController {
   #lottoGame;
@@ -13,71 +14,108 @@ class LottoGameController {
     this.#outputView = outputView;
   }
 
+  // 로또게임의 전체 흐름을 담당하는 메서드
   async startGame() {
     const amount = await this.#requestValidPurchaseAmount();
+
     this.#lottoGame.initializeLotto(amount);
-
-    const purchasedLotto = this.#lottoGame.getPurchasedLotto();
-
-    this.#outputView.printPurchaseAmount(amount);
-    this.#outputView.printPurchasedLotto(purchasedLotto);
+    this.#printPurchaseInfo(amount);
 
     const winningLotto = await this.#requestValidWinningLotto();
     const bonusNumber = await this.#requestValidBonusNumber(winningLotto);
+    const winsStatistics = this.#calculateWinsStatistics(winningLotto, bonusNumber);
 
-    this.#finishGame(winningLotto, bonusNumber);
+    this.#printResults(winsStatistics);
   }
 
-  #finishGame(winningLotto, bonusNumber) {
+  /**
+   * 유효한 구입 수량을 요청하고 컨트롤한다.
+   * @returns { number } - 구입한 로또 티켓의 수량
+   */
+  async #requestValidPurchaseAmount() {
+    while (true) {
+      try {
+        const inputPurchaseAmount = await this.#inputView.getPurchaseAmount();
+        this.#validator.validatePurchaseAmount(inputPurchaseAmount);
+
+        return inputPurchaseAmount / PRICE_PER_TICKET;
+      } catch ({ message }) {
+        this.#outputView.printError(message);
+      }
+    }
+  }
+
+  /**
+   * 유효한 당첨 번호를 요청하고 컨트롤한다.
+   * @returns { Lotto } - 유효한 당첨 로또
+   */
+  async #requestValidWinningLotto() {
+    while (true) {
+      try {
+        const winningNumbers = await this.#inputView.getWinningNumbers();
+        const numbers = winningNumbers.split(',').map(Number);
+
+        return new Lotto(numbers);
+      } catch ({ message }) {
+        this.#outputView.printError(message);
+      }
+    }
+  }
+
+  /**
+   * 유효한 보너스 번호를 요청하고 컨트롤한다.
+   * @param { Lotto[number] } winningLotto - 당첨 로또
+   * @returns { number } - 유효한 보너스 번호
+   */
+  async #requestValidBonusNumber(winningLotto) {
+    while (true) {
+      try {
+        const bonusNumber = await this.#inputView.getBonusNumber();
+        const isContaining = winningLotto.hasContainBonusNumber(parseInt(bonusNumber, 10));
+        this.#validator.validateBonusNumber(bonusNumber, isContaining);
+
+        return parseInt(bonusNumber, 10);
+      } catch ({ message }) {
+        this.#outputView.printError(message);
+      }
+    }
+  }
+
+  /**
+   * 구입한 수량과 구매한 로또목록을 출력한다.
+   * @param {number} amount 구매한 장 수
+   */
+  #printPurchaseInfo(amount) {
+    this.#outputView.printPurchaseAmount(amount);
+    this.#outputView.printPurchasedLotto(this.#lottoGame.getPurchasedLotto());
+  }
+
+  /**
+   * 당첨 통계를 계산해서 반환한다.
+   * @param {number[]} winningLotto
+   * @param {number} bonusNumber
+   * @returns { { [PRIZE_KEY] : number } } winsStatistics 당첨된 등수와 카운트
+   */
+  #calculateWinsStatistics(winningLotto, bonusNumber) {
     const comparisonResults = this.#lottoGame.getComparisonResults(
       winningLotto.getSortedNumbers(),
       bonusNumber
     );
-    const winsStatistics = this.#lottoGame.getStatistics(comparisonResults);
-    const totalPrizeAmount = this.#lottoGame.getTotalPrizeAmount(winsStatistics);
 
+    return this.#lottoGame.getStatistics(comparisonResults);
+  }
+
+  /**
+   * 계산된 당첨 통계와 총 수익률을 출력한다.
+   * @param { { [PRIZE_KEY] : number } } winsStatistics
+   */
+  #printResults(winsStatistics) {
     this.#outputView.printWinsStatistics(winsStatistics);
-    this.#outputView.printProfitRatio(this.#lottoGame.getProfitRatio(totalPrizeAmount));
-  }
 
-  async #requestValidPurchaseAmount() {
-    try {
-      const inputPurchaseAmount = await this.#inputView.getPurchaseAmount();
-      this.#validator.validatePurchaseAmount(inputPurchaseAmount);
+    const totalPrizeAmount = this.#lottoGame.getTotalPrizeAmount(winsStatistics);
+    const totalProfitRatio = this.#lottoGame.getProfitRatio(totalPrizeAmount);
 
-      return inputPurchaseAmount / 1000;
-    } catch ({ message }) {
-      this.#outputView.printError(message);
-
-      return await this.#requestValidPurchaseAmount();
-    }
-  }
-
-  async #requestValidWinningLotto() {
-    try {
-      const winningNumbers = await this.#inputView.getWinningNumbers();
-      const numbers = winningNumbers.split(',').map(Number);
-
-      return new Lotto(numbers);
-    } catch ({ message }) {
-      this.#outputView.printError(message);
-
-      return await this.#requestValidWinningLotto();
-    }
-  }
-
-  async #requestValidBonusNumber(winningLotto) {
-    try {
-      const bonusNumber = await this.#inputView.getBonusNumber();
-      const isContaining = winningLotto.hasContainBonusNumber(parseInt(bonusNumber, 10));
-      this.#validator.validateBonusNumber(bonusNumber, isContaining);
-
-      return parseInt(bonusNumber, 10);
-    } catch ({ message }) {
-      this.#outputView.printError(message);
-
-      return await this.#requestValidBonusNumber(winningLotto);
-    }
+    this.#outputView.printProfitRatio(totalProfitRatio);
   }
 }
 
