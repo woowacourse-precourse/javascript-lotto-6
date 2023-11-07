7b8828fb0a4c1abb14d69693894c6a51051ec1bb
abi-hong <flffjakfwm20@gmail.com>
2023-11-07 15:10:34 +0900

    refactor(Validator): 예외 값 상수로 변경

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,17 +1,18 @@
-import { OutputView } from "../views/OutputView.js";
+import { OutputView } from '../views/OutputView.js';
+import { EXCEPTION } from './Constant.js';
 
 export const Validator = {
   checkMoney(price) {
     const strPrice = String(price);
     if (!/^[0-9]+$/g.test(strPrice) || price % 1000 !== 0) {
-      OutputView.err('[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.');
+      OutputView.err(EXCEPTION.money);
       return false;
     }
     return true;
   },
   checkSixNumber(numbers) {
     if (numbers.length !== 6) {
-      OutputView.err('[ERROR] 당첨 번호는 중복되지 않는 숫자 6개를 입력해야 합니다.');
+      OutputView.err(EXCEPTION.sixNumber);
       return false;
     }
     return true;
@@ -19,7 +20,7 @@ export const Validator = {
   checkIsNumber(number) {
     const strNumber = String(number);
     if (!/^([1-9]|[1-3][0-9]|4[0-5])$/.test(strNumber)) {
-      OutputView.err('[ERROR] 보너스 번호는 1 ~ 45사이의 숫자 1개를 입력해야 합니다.')
+      OutputView.err(EXCEPTION.isNumber);
       return false;
     }
     return true;
