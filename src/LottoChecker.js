4dcc0866f81e8a2701ddf44c6db6af1f5afd3477
chorong <chorong4641@naver.com>
2023-11-08 00:30:38 +0900

    fix(checker): 보너스 번호가 여러 장에 해당할 경우 판단하지 못하는 로직 수정
    - 일치 개수 변수에 보너스 번호가 일치하는지도 판단하여 저장

--- src/LottoChecker.js
+++ src/LottoChecker.js
@@ -1,13 +1,15 @@
+import { MESSAGES } from "./Messages";
 
 export function lottoChecker(randomNumbers, winningNumbers, bonusNumber) {
   const matchedCounts = [];
-  let matchedBonusPosition = -1;
+
   for (let index = 0; index < randomNumbers.length; index += 1) {
-    matchedCounts.push(randomNumbers[index].reduce((acc, number) => acc + (winningNumbers.includes(number)), 0));
-    if (randomNumbers[index].includes(bonusNumber)) {
-      matchedBonusPosition = index;
+    const count = randomNumbers[index].reduce((acc, number) => acc + (winningNumbers.includes(number)), 0);
+    if (count === 5 && randomNumbers[index].includes(bonusNumber)) {
+      matchedCounts[index] = MESSAGES.MATCH_BONUS;
+    } else {
+      matchedCounts[index] = count;
     }
   }
-
-  return { matchedCounts, matchedBonusPosition };
+  return matchedCounts;
 }
