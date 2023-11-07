49471d21b094c940fa08a7c2b3a1758fa0a048b1
최진욱 <jinwook.fe@gmail.com>
2023-11-07 15:24:27 +0900

    [feat] 당첨 번호 에러 핸들링 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,17 +2,43 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    this.#validateForm(numbers);
+    this.#validateDuplication(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  #validateForm(numbers) {
+    const regexNumber = /^\d+$/;
+    const numberArray = numbers.split(",");
+
+    if (numberArray.length !== 6) {
+      throw new Error(
+        "[ERROR] 쉼표(,)로 구분된 숫자 형식의 입력만 가능합니다.",
+      );
+    }
+
+    numberArray.forEach((number) => {
+      if (!regexNumber.test(number)) {
+        throw new Error(
+          "[ERROR] 쉼표(,)로 구분된 숫자 형식의 입력만 가능합니다.",
+        );
+      }
+    });
+  }
+
+  #validateDuplication(numbers) {
+    const regexDuplicatedInLottoRange =
+      /^(?!.*(\d+)(?=.*\b\1\b))(?:(?:[1-9]|[1-3]\d|4[0-5])(?:,|$)){6}$/;
+    if (!regexDuplicatedInLottoRange.test(numbers)) {
+      throw new Error(
+        "[ERROR] 1~45 사이의 중복되지 않는 6개의 숫자를 입력해주세요.",
+      );
     }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
