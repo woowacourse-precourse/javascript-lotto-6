0998704dcacd89db737a4d445efcd666886b895d
leedbswo107 <ljyu1@naver.com>
2023-11-07 18:20:38 +0900

    fix: 에러 발생 지점 이어서 시작 불가 해결

--- view/InputAndOutput.js
+++ view/InputAndOutput.js
@@ -10,32 +10,46 @@ class InputAndOutput{
         this.validate = new Validate();
         this.lottoPlay = new LottoPlay();
     }
-
     async inputPurchasePrice(){
-        const PURCHASE_PRICE = await Console.readLineAsync(`${MESSAGE.PURCHASE_NUMBER_INPUT}\n`);   
-        const VALIDATE_PURCHASE_PRICE = this.validate.validatePrice(PURCHASE_PRICE);
-        return VALIDATE_PURCHASE_PRICE;
+        while(true){
+            try {
+                const PURCHASE_PRICE = await Console.readLineAsync(`${MESSAGE.PURCHASE_NUMBER_INPUT}\n`);   
+                const VALIDATE_PURCHASE_PRICE = await this.validate.validatePrice(PURCHASE_PRICE);
+                return VALIDATE_PURCHASE_PRICE; 
+            } catch (error) {
+                Console.print(`${error}`);
+            }
+        }
     }
     async inputWinNumber(){
-        const WIN_NUMBER = await Console.readLineAsync(`${MESSAGE.WIN_NUMBER_INPUT}\n`);
-        const SPLIT_WIN_NUMBER = WIN_NUMBER.split(',').map(Number);
-        Console.print('');
-        // TODO: 입력된 로또 번호의 유효성 검사를 진행
-        new Lotto(SPLIT_WIN_NUMBER);
-        return SPLIT_WIN_NUMBER;
+        while(true){
+            try {
+                const WIN_NUMBER = await Console.readLineAsync(`${MESSAGE.WIN_NUMBER_INPUT}\n`);
+                const SPLIT_WIN_NUMBER = WIN_NUMBER.split(',').map(Number);
+                Console.print('');
+                new Lotto(SPLIT_WIN_NUMBER);
+                return SPLIT_WIN_NUMBER;       
+            } catch (error) {
+                Console.print(`${error}`);
+            }
+        }
     }
     async inputBonusNumber(numbers){
-        const BONUS_NUMBER = parseInt(await Console.readLineAsync(`${MESSAGE.BONUS_NUMBER_INPUT}\n`));
-        this.validate.validateBonusNumber(BONUS_NUMBER,numbers);
-        Console.print('');
-        return BONUS_NUMBER;     
+        while(true) {
+            try {
+                const BONUS_NUMBER = parseInt(await Console.readLineAsync(`${MESSAGE.BONUS_NUMBER_INPUT}\n`));
+                await this.validate.validateBonusNumber(BONUS_NUMBER,numbers);
+                Console.print('');
+                return BONUS_NUMBER;     
+            } catch (error) {
+                Console.print(`${error}`);
+            }
+        }
     }
     async purchasedLottoOutput(number, amount){
-        // const PURCHASE_LOTTO_NUMBER = this.lottoPlay.autoLottoGenerator(amount);
         Console.print(`${amount}${MESSAGE.NUMBER_OF_PURCHASE}`);
         number.forEach(e => {
            Console.print(`[${e.join(', ')}]`);
-        //    Console.print(JSON.stringify(e));
         });
         Console.print('');
     }
