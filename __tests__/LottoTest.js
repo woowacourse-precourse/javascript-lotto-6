fbde0dc6dbf089db3ed8ed5bfb0d292d91e6ca8a
BHyeonKim <rlaqhguse@gmail.com>
2023-11-07 19:26:18 +0900

    style: 린터 설정에 따른 코드 포맷팅

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,36 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../src/Lotto.js';
+import { parseNumber, parseNumbers } from '../src/utils/index.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // 아래에 추가 테스트 작성 가능
+});
+
+describe('로또 번호 파싱 테스트', () => {
+  test.each([
+    ['8,21,34,하,23'],
+    ['6,3,42,3,2,,,,,'],
+    [',,,,5,3,4,2'],
+    ['5,2,,,24,3,2'],
+  ])('입력에 숫자가 아닌게 올 경우', (input) => {
+    expect(() => parseNumbers(input)).toThrow('[ERROR]');
+  });
+});
+
+describe('보너스 번호 파싱 테스트', () => {
+  test.each([['가'], ['다']])('입력애 숫자가 아닌게 올 경우', (input) => {
+    expect(() => parseNumber(input)).toThrow('[ERROR]');
+  });
 });
