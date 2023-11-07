89f511dc0f33eb8e95f6698f3cb1377a0ba05a0d
donggu <zzzjdg@naver.com>
2023-11-07 15:49:02 +0900

    Feat : Add Lotto 유효성 함수들
    당첨 번호의 유효성 검사하는 함수들 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,9 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.#isNumber(numbers);
+    this.#isDuplicate(numbers);
+    this.#isRange(numbers);
     this.#numbers = numbers;
   }
 
@@ -12,7 +15,40 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  #isNumber(numbers){
+    for(let i = 0; i< 6 ; i++){
+      if(isNaN(Number(numbers[i]))){
+        throw new Error("[ERROR] 숫자가 아닌 값이 존재합니다.");
+      }
+    }
+  }
+
+  #isDuplicate(numbers){
+    const setNumbers = new Set(numbers);
+    if(setNumbers.size !== 6){
+      throw new Error("[ERROR] 중복된 값이 존재합니다");
+    }
+  }
+
+  #isRange(numbers){
+    for(let i = 0; i< 6 ; i++){
+      if (Number(numbers[i]) > 45 
+      || Number(numbers[i]) < 1) {
+        throw new Error("[ERROR] 당첨 번호의 범위는 1~45 입니다.");
+      }
+    }
+  }
+
+  matchLotto(lotto){
+    let count = 0;
+    for(let i= 0 ; i< 6 ; i++){
+      if(this.#numbers.includes(lotto[i])){
+        count += 1;
+      }
+    }
+    return count;
+  }
+  
 }
 
 export default Lotto;
