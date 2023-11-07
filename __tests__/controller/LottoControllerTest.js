2eac8846a2f9026368de3b24a4a6020b498760f5
BangDori <qudwns8865@naver.com>
2023-11-07 19:59:08 +0900

    refactor: 로또 구매 테스트 LottoShopTest로 분리

--- __tests__/controller/LottoControllerTest.js
+++ __tests__/controller/LottoControllerTest.js
@@ -1,22 +1,7 @@
 import LottoController from '../../src/controller/LottoController';
-import { LOTTO_PRICE } from '../../src/constants/setting';
-import LottoShop from '../../src/domain/LottoShop';
 import Lotto from '../../src/domain/Lotto';
 
 describe('LottoController 클래스 테스트', () => {
-  describe('로또 발급 테스트', () => {
-    test.each([
-      [1000, 1000 / LOTTO_PRICE],
-      [8000, 8000 / LOTTO_PRICE],
-      [12000, 12000 / LOTTO_PRICE],
-    ])('로또 %d원치 구매시 %d개의 로또 발급', (money, lottoCount) => {
-      LottoShop.generateLotto = jest.fn(() => new Lotto([1, 2, 3, 4, 5, 6]));
-      const lottoController = new LottoController(money);
-
-      expect(LottoShop.generateLotto).toHaveBeenCalledTimes(lottoCount);
-    });
-  });
-
   describe('로또 비교 테스트, 보너스 번호 불일치', () => {
     const lotto = new Lotto([7, 14, 21, 28, 35, 42]);
     let lottoController;
