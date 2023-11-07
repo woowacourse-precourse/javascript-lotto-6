00ea970dc928a5be0ae28f4f2564ee2fe76ba161
simorimi <hori5020@gmail.com>
2023-11-08 03:18:39 +0900

    feat(Lotto): final version

--- __tests__/BonusNumberValidityTest.js
+++ __tests__/BonusNumberValidityTest.js
@@ -2,7 +2,7 @@ import BonusNumberValidity from "../src/Domain/BonusNumberValidity.js";
 
 describe("보너스 번호 테스트", () => {
   test("숫자를 입력했는지 테스트", () => {
-    const INPUT = 'a';
+    const INPUT = "a";
     const INPUT_NUMBER = new BonusNumberValidity();
     expect(() => INPUT_NUMBER.numberNaNvalidity(INPUT)).toThrow("[ERROR]");
   });
@@ -15,8 +15,10 @@ describe("보너스 번호 테스트", () => {
 
   test("중복된 수가 있는지 테스트", () => {
     const INPUT = 1;
-    const ARRAY = [1,2,3,4,5,6];
+    const ARRAY = [1, 2, 3, 4, 5, 6];
     const INPUT_NUMBER = new BonusNumberValidity();
-    expect(() => INPUT_NUMBER.numberDuplicateValidity(INPUT, ARRAY)).toThrow("[ERROR]");
+    expect(() => INPUT_NUMBER.numberDuplicateValidity(INPUT, ARRAY)).toThrow(
+      "[ERROR]"
+    );
   });
 });
--- src/UI/PrintRateOfReturn.js
+++ src/UI/PrintRateOfReturn.js
@@ -1,9 +1,9 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-class PrintRateOfReturn{
-    async print(rateOfReturn){
-MissionUtils.Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
-}
+class PrintRateOfReturn {
+  async print(rateOfReturn) {
+    MissionUtils.Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
+  }
 }
 
 export default PrintRateOfReturn;
