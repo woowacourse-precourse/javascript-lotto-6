dbb5133ae5bee975100d12e2d4bcdb35266e292c
p0cari <pocari3434@gmail.com>
2023-11-07 22:42:10 +0900

    부모 컨트롤러에 자식 컨트롤러 연결

--- src/mvc/controller/parent/game_manage.js
+++ src/mvc/controller/parent/game_manage.js
@@ -1,5 +1,14 @@
+import RandomLottoManage from '../child/random_lotto_manage';
+import WinningLottoManage from '../child/winning_lotto_manage';
+
 class GameManage {
-  async gameManage() {}
+  async gameManage() {
+    const RANDOM_LOTTO_MANAGE = new RandomLottoManage();
+    await RANDOM_LOTTO_MANAGE.inputPurchaseAmount();
+    const RANDOM_LOTTO = RANDOM_LOTTO_MANAGE.randomLotto;
+    const WINNING_LOTTO_MANAGE = new WinningLottoManage(RANDOM_LOTTO);
+    await WINNING_LOTTO_MANAGE.inputWinningNum();
+  }
 }
 
 export default GameManage;
