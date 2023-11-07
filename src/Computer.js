fb65aca8627ef13005ef0c4a857ede3025cfcaf8
JungHun98 <j7papa@naver.com>
2023-11-07 13:50:18 +0900

    feat: 컴퓨터 클래스 필드 값 추가 prizeResult

--- src/Computer.js
+++ src/Computer.js
@@ -6,6 +6,13 @@ class Computer {
   constructor(winnnersNumbers, bonusNumber) {
     this.#winnerNumbers = winnnersNumbers;
     this.#bonusNumber = bonusNumber;
+    this.#prizeResult = {
+      [FIRST]: 0,
+      [SECOND]: 0,
+      [THIRD]: 0,
+      [FOURTH]: 0,
+      [FIFTH]: 0,
+    };
   }
 
   getPrizeResult(lottos) {
