62e16289daa646e7238267225340dc98f42dd0bf
sryung <sryung1225@gmail.com>
2023-11-08 00:02:27 +0900

    feat(Constants): 출력에 사용할 메세지 상수 선언
    - MESSAGE : 게임 진행 중 출력문으로 사용할 메세지
    - ERROR : thorw 할 에러 메세지

---
+++ src/constants/Constants.js
@@ -0,0 +1,17 @@
+export const MESSAGE = Object.freeze({
+  enterPurchasePrice: `구입금액을 입력해 주세요.\n`,
+  purchaseAmount: `개를 구매했습니다.\n`,
+  enterWinningNumbers: `당첨 번호를 입력해 주세요.\n`,
+  enterBonusNumber: `보너스 번호를 입력해 주세요.\n`,
+  winningResult: `당첨 통계\n---\n`,
+  totalProfitPercentage: `총 수익률은`,
+});
+
+export const ERROR = Object.freeze({
+  invalidPriceNumber: `[ERROR] 구입 금액은 유효한 숫자여야 합니다.`,
+  invalidPriceLeast: `[ERROR] 구입 금액은 1,000원 이상이여야 합니다.`,
+  invalidPriceUnit: `[ERROR] 구입 금액은 1,000원 단위여야 합니다.`,
+  invalidNumberCount: `[ERROR] 로또 번호는 6개의 숫자여야 합니다.`,
+  invalidNumberRange: `[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.`,
+  duplicateNumbers: `[ERROR] 로또 번호는 중복이 없어야 합니다.`,
+});
