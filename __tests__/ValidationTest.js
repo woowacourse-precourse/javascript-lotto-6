bb17cc6278de4fb9a154197797e53beec122b134
yeong-hwan <jghff700@naver.com>
2023-11-08 02:18:07 +0900

    test: 잘못된 단위를 입력한 경우에 대한 테스트케이스 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -4,21 +4,31 @@ import InputValidator from '../src/domain/validator/InputValidator.js';
 describe('Number Validation', () => {
   const inputValidator = new InputValidator();
 
-  test('숫자가 아닌 문자를 입력', () => {
+  test('숫자가 아닌 문자를 입력하면 예외가 발생한다.', () => {
     expect(() => {
       inputValidator.validateNumber('string');
     }).toThrow(ErrorMessage);
   });
 
-  test('음수를 입력', () => {
+  test('음수를 입력하면 예외가 발생한다.', () => {
     expect(() => {
       inputValidator.validatePositiveNumber('-1');
     }).toThrow(ErrorMessage);
   });
 
-  test('정수가 아닌 소수를 입력', () => {
+  test('정수가 아닌 소수를 입력하면 예외가 발생한다.', () => {
     expect(() => {
       inputValidator.validateInteger('0.5');
+    }).toThrow(ErrorMessage);
+  });
+});
+
+describe('Unit Cost Validation', () => {
+  const inputValidator = new InputValidator();
+
+  test('잘못된 단위를 입력하면 예외가 발생한다.', () => {
+    expect(() => {
+      inputValidator.validateUnitCost('1001');
     }).toThrow(ErrorMessage);
   });
 });
