f44159f01ee0f587cefda09e791fbc7a7361f2ca
jomo34 <trillion32@naver.com>
2023-11-08 00:31:24 +0900

    feature: 사용자로부터 구매할 금액을 입력받는 getPurchaseAmount 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 class App {
-  async play() {}
+  async play() {
+    const PRICE = await getPurchaseAmount();
+  }
+
+  async getPurchaseAmount(){
+    const INPUTPRICE = parseInt(Console.readLineAsync("구입금액을 입력해 주세요."));
+    if(INPUTPRICE % 1000 !== 0)
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위여야 합니다.")
+    return INPUTPRICE;
+  }
+
 }
 
 export default App;
