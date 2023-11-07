5ee9574e84f1b758c84c2f15079be0a94749c4cd
myireland8125 <iloveyouyk88@gmail.com>
2023-11-08 01:50:26 +0900

    [feature/refactor] const 상수화 객체 동결

--- src/constants/propmtMessage.js
+++ src/constants/propmtMessage.js
@@ -1,7 +1,7 @@
-const PROPMT_MESSAGE = {
+const PROPMT_MESSAGE = Object.freeze({
   bonusNumber: '보너스 번호를 입력해 주세요.',
   winningNumber: '당첨 번호를 입력해 주세요.',
   purchasePrice: '구입금액을 입력해 주세요.',
-};
+});
 
 export default PROPMT_MESSAGE;
