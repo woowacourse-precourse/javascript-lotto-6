9df067ae219486004afb3725a6e05fedab487db7
yeonkyoung <keep.ykk@gmail.com>
2023-11-08 04:34:53 +0900

    feat: 구매한 개수 출력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 class App {
   async play() {
     const amount = await userLottoInputAmount();
+    const purchasedLottoCounts = getPurchasedLottoCounts(amount);
+    printMessage(`${purchasedLottoCounts}${PURCHASE_AMOUNT_GUIDE_MESSAGE}`);
+    printPurchasedLottoNumbers(lottoArray);
 }
 
 export default App;
