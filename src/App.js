6ae7778a7a25d038ba68c42e738af4bbf78632c9
조예진 <joyejin@joyejins-MacBook-Air.local>
2023-11-08 00:30:28 +0900

    feat: 사용자가 구매금액 입력하고 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 import Lotto from './Lotto';
+import { MESSAGE } from './libs/constants';
+import Lottos from './Lottos';
+import WinningNumbers from './WinningNumbers';
 
 class App {
   constructor() {
     this.lottos = null;
+    this.winningNumbers = null;
+    this.bonusNumbers = null;
   }
 
   async play() {}
+
+  inputAmount() {
+    Console.readLineAsync(MESSAGE.INPUT_AMOUNT, amount => {
+      this.lottos = new Lottos(amount);
+      this.lottos.printTicketCount();
+      this.lottos.printTickets();
+
+      this.inputWinningNumbers;
+    });
+  }
 }
 
 export default App;
--- src/libs/constants.js
+++ src/libs/constants.js
@@ -7,7 +7,7 @@ export const ERROR_MESSAGE = {
 
 export const MESSAGE = {
   INPUT_AMOUNT: '구입금액을 입력해 주세요.\n',
-  INPUT_WINNGING_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  INPUT_WINNGING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
   INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
   WINNING_STATS: '\n당첨 통계\n---',
 };
