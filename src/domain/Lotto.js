f955abf509050e7b01b80ce4edc6b4066c1ca86f
starcradle101 <dreamerdev12@gmail.com>
2023-11-08 05:29:34 +0900

    🛠️Feat($Lotto): 당첨 번호와 발행 로또 비교 기능 구현 당첨 번호와 발행 로또를 비교하고 추첨 결과를 반환하는 로직 구현

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -15,6 +15,36 @@ class Lotto {
   getLottoNumbers() {
     return [...this.#numbers];
   }
+
+  #matchCount(ticket) {
+    return ticket.filter(number => this.#numbers.includes(number)).length;
+  }
+
+  #evaluateEachLottoTicket(ticket, bonusNumber, lottoResult) {
+    const updatedResult = { ...lottoResult };
+    const matchingNumber = this.#matchCount(ticket);
+    const bonusMatch = ticket.includes(bonusNumber);
+
+    if (matchingNumber === 6) updatedResult.first += 1;
+    if (matchingNumber === 5 && bonusMatch) updatedResult.second += 1;
+    if (matchingNumber === 5 && !bonusMatch) updatedResult.third += 1;
+    if (matchingNumber === 4) updatedResult.forth += 1;
+    if (matchingNumber === 3) updatedResult.fifth += 1;
+
+    return updatedResult;
+  }
+
+  evaluateLottoTickets(lottoTickets, bonusNumber, lottoResult) {
+    let evaluatedResult = { ...lottoResult };
+    lottoTickets.forEach(ticket => {
+      evaluatedResult = this.#evaluateEachLottoTicket(
+        ticket,
+        bonusNumber,
+        evaluatedResult,
+      );
+    });
+    return evaluatedResult;
+  }
 }
 
 export default Lotto;
