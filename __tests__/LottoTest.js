9c756ed73e73ffe9ee4be48374c2dc7b1d3eb53a
teawon <xodnjsvkvk9@gmail.com>
2023-11-07 12:30:03 +0900

    test : Lotto 도메인에 대한 정수 및 정수 범위에 대한 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -13,11 +13,30 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
+  });
+
+  test("로또 정보의 각 숫자가 정수가 아니라면 예외가 발생한다.", () => {
+    // given
+    const wrongLottoNumber = ["a", "b", "c", "d", "e", "f"];
+
+    // when & then
+    expect(() => new Lotto(wrongLottoNumber)).toThrow(
+      "[ERROR] 로또의 각 번호는 정수여야 합니다."
+    );
+  });
+
+  test("로또 정보의 각 숫자가 1~45 사이의 값을 가지지 않으면 예외가 발생한다.", () => {
+    // given
+    const wrongLottoNumber = [-1, 2, 3, 4, 5, 46];
+
+    // when & then
+    expect(() => new Lotto(wrongLottoNumber)).toThrow(
+      "[ERROR] 로또는 1~45 사이의 숫자로 구성되어야 합니다."
+    );
   });
 
   test("로또 정보는 오름차순으로 정렬된 정수를 반환해야 한다.", () => {
