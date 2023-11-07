a7eac94e20b1c1d9e2d05e6a5199bc089ccf1430
kwoncha <rnjsckgus@gmail.com>
2023-11-07 23:37:45 +0900

    feat: 숫자가 아닐시 throw 기능 구현

--- src/utils/validation/validation.js
+++ src/utils/validation/validation.js
@@ -5,3 +5,11 @@ const trimmedNumber = number => {
 const parsedNumber = number => {
   return parseInt(trimmedNumber(number), 10);
 };
+
+const validateNumber = number => {
+  const trimmed = trimmedNumber(number);
+  const parsed = parsedNumber(number);
+
+  if (trimmed === '' || Number.isNaN(parsed)) throw new Error();
+  if (trimmed !== parsed.toString()) throw new Error();
+};
