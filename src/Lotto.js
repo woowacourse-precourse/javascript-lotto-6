4341a68f85dd1b96d3d3f67490dd9503a12498b6
byunjiin <jibyun5808@naver.com>
2023-11-07 18:14:34 +0900

    feat: 초기 설정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+// 기능3의 당첨 번호 입력 조건
 class Lotto {
   #numbers;
 
@@ -8,7 +9,7 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
   }
 
---
+++ src/output/LottoPrint.js
@@ -0,0 +1,5 @@
+// 기능2. 로또 발행
+import { Console } from '@woowacourse/mission-utils';
+import { checkbuy } from '../Exception/CheckBuy.js';
+
+export function lottoprint(buyinput) {}
