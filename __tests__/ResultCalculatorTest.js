8062f871182287d4a26d0b2077cbec9ca44c0655
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-08 04:05:16 +0900

    test(ResultCalculator): 로또 당첨 테스트 중복 제거

--- __tests__/ResultCalculatorTest.js
+++ __tests__/ResultCalculatorTest.js
@@ -7,15 +7,23 @@ describe('당첨 결과 테스트', () => {
   winningLotto.bonusNumber = 7;
 
   describe('발행한 로또의 번호가 당첨 로또 번호', () => {
-    test('6개와 일치하면, 1등 당첨이다.', () => {
-      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-      const resultCalculator = new ResultCalculator();
+    test.each([
+      [6, 1, [1, 2, 3, 4, 5, 6], 'first'],
+      [5, 3, [1, 2, 3, 4, 5, 45], 'third'],
+      [4, 4, [1, 2, 3, 4, 44, 45], 'fourth'],
+      [3, 5, [1, 2, 3, 43, 44, 45], 'fifth'],
+    ])(
+      '%d개와 일치하면, %d등 당첨이다.',
+      (matchingNumber, rankNumber, numbers, rankString) => {
+        const lotto = new Lotto(numbers);
+        const resultCalculator = new ResultCalculator();
 
-      resultCalculator.compareLottos([lotto], winningLotto);
-      const output = resultCalculator.cntRank.first;
+        resultCalculator.compareLottos([lotto], winningLotto);
+        const output = resultCalculator.cntRank[rankString];
 
-      expect(output).toBe(1);
-    });
+        expect(output).toBe(1);
+      },
+    );
 
     test('5개, 보너스 번호와 일치하면, 2등 당첨이다.', () => {
       const lotto = new Lotto([1, 2, 3, 4, 5, 7]);
@@ -23,36 +31,6 @@ describe('당첨 결과 테스트', () => {
 
       resultCalculator.compareLottos([lotto], winningLotto);
       const output = resultCalculator.cntRank.second;
-
-      expect(output).toBe(1);
-    });
-
-    test('5개와 일치하면, 3등 당첨이다.', () => {
-      const lotto = new Lotto([1, 2, 3, 4, 5, 45]);
-      const resultCalculator = new ResultCalculator();
-
-      resultCalculator.compareLottos([lotto], winningLotto);
-      const output = resultCalculator.cntRank.third;
-
-      expect(output).toBe(1);
-    });
-
-    test('4개와 일치하면, 4등 당첨이다.', () => {
-      const lotto = new Lotto([1, 2, 3, 4, 44, 45]);
-      const resultCalculator = new ResultCalculator();
-
-      resultCalculator.compareLottos([lotto], winningLotto);
-      const output = resultCalculator.cntRank.fourth;
-
-      expect(output).toBe(1);
-    });
-
-    test('3개와 일치하면, 5등 당첨이다.', () => {
-      const lotto = new Lotto([1, 2, 3, 43, 44, 45]);
-      const resultCalculator = new ResultCalculator();
-
-      resultCalculator.compareLottos([lotto], winningLotto);
-      const output = resultCalculator.cntRank.fifth;
 
       expect(output).toBe(1);
     });
