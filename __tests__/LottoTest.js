7bf5f4ca78e3ddcc9461d2a468459477c2fab584
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-07 17:25:00 +0900

    feat : 로또 구입 입력받기& 예외처리

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 import Lotto from "../src/Lotto.js";
+import App from "../src/App.js";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -14,5 +15,15 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 구입 금액이 1000 단위가 아니면 예외가 발생한다.", () => {
+    const app = new App();
+    const value = "1100";
+    expect(() => app.getIncomeError(value)).toThrow("[ERROR]");
+  });
+
+  test("로또 구입 금액이 숫자가 아니면 아니면 예외가 발생한다.", () => {
+    const app = new App();
+    const value = "2000";
+    expect(() => app.getIncomeError(value)).toThrow("[ERROR]");
+  });
 });
--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    let income = await MissionUtils.Console.readLineAsync(
+      "구입금액을 입력해 주세요."
+    );
+    this.getIncomeError(income);
+  }
+
+  //함수: 로또구입금액을 입력받고, 1000단위가 아닐 경우 예외처리
+  async getIncomeError(income) {
+    let income_num = Number(income);
+    if (isNaN(income_num))
+      throw new Error("[ERROR] 구입 금액은 숫자를 입력해주세요");
+    if (income_num % 1000 != 0)
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+  }
 }
 
 export default App;
