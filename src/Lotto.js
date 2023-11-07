1d2f5356c3eb19feb56868d1cee2577412b6bff6
OhHyukJun <kmh7277@naver.com>
2023-11-07 15:55:17 +0900

    Feat: 예외 처리 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,10 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
   }
 
   getNumbers() {
