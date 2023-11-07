5a1c91788c75dd04c3285742d0848209b74a7338
leejaejae <leesengjie@naver.com>
2023-11-07 14:46:53 +0900

    feat(배열 출력): 랜덤 생성된 로또 배열 출력 및 로또 구매 갯수 출력

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App {
   async play() {
     const lottoMoney = await this.getLottoMoney(); // 로또 구매할 금액 입력
     const lottoNumbers = this.generateLottoNumbers(lottoMoney / 1000);  // 갯수에 맞게 랜덤 생성되는 로또
+    this.showLottoNumbers(lottoNumbers)  // 랜던 생성된 로또 출력
   }
 
   async getLottoMoney() {  // 로또 구매할 금액 입력
@@ -29,6 +30,14 @@ class App {
       lottoNumbers.push(numbers);
     }
     return lottoNumbers;
+  }
+
+  showLottoNumbers(lottoNumbers) {  // 랜덤 생성된 로또 배열 출력
+    Console.print(`${lottoNumbers.length}개를 구매했습니다.`);
+    for(const numbers of lottoNumbers){
+      Console.print(`[${numbers.join(",")}]`);
+    }
+    Console.print("\n")
   }
 }
 
