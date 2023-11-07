72c195c320ed9c5514e262def5d4bb4a253462fa
OhHyukJun <kmh7277@naver.com>
2023-11-07 10:33:49 +0900

    Feat: 생성자 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { Computer } from "./Computer/Computer.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this.computer = new Computer();
+  }
+  
+  async play() {
+    MissionUtils.Console.print("dsds");
+    MissionUtils.Console.print(`ddd${this.computer.purchaseLotto}`);
+  }
 }
 
 export default App;
