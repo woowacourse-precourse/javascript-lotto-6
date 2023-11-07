9719a00334886fcc28067c733c289abb2585a8e0
mingyeong:D <63152086+Kim-Mingyeong@users.noreply.github.com>
2023-11-07 22:08:49 +0900

    feat: 사용자 입력 받고 입력에 따라 예외 처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+import { GAME, ERROR } from "./message";
+import { Lotto } from "./Lotto";
+
 class App {
+  #amount;
+  constructor() {
+    this.#amount = 0;
+  }
+
+  #validate(amount) {
+    if (isNaN(amount) || amount < 0) {
+      throw new Error(ERROR.NOT_NUMBER);
+    }
+    if (amount % 1000 !== 0) {
+      throw new Error(ERROR.INVALID_AMOUNT);
+    }
+  }
+
+  inputPurchaseAmount = async () => {
+    try {
+      const amount = parseInt(
+        await Console.readLineAsync(GAME.INPUT.AMOUNT),
+        10
+      );
+      this.#validate(amount);
+      this.#amount = amount;
+    } catch (error) {
+      console.error(error.message);
+      this.inputPurchaseAmount();
+    }
+  };
+
   async play() {}
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+import { GAME, ERROR } from "./message";
+import { App } from "./App";
+
 class Lotto {
   #numbers;
 
@@ -8,11 +12,45 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR.INVALID_ARRAY_LENGTH);
+    }
+
+    for (const number of numbers) {
+      if (number < 1 || number > 45) {
+        throw new Error(ERROR.OUT_OF_RANGE);
+      }
+    }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error(ERROR.DUPLICATE_NUMBER);
     }
   }
 
   // TODO: 추가 기능 구현
+  inputWinningNumbers = async () => {
+    try {
+      const numbers = await Console.readLineAsync(GAME.INPUT.WINNING_NUMBER);
+      const numbersArray = numbers.split(",");
+      this.#validate(numbersArray);
+    } catch (error) {
+      console.error(error.message);
+      this.inputWinningNumbers();
+    }
+  };
+
+  inputBonusNumber = async () => {
+    try {
+      const number = parseInt(
+        await Console.readLineAsync(GAME.INPUT.BONUS_NUMBER),
+        10
+      );
+      this.#validate(number);
+    } catch (error) {
+      console.error(error.message);
+      this.inputBonusNumber();
+    }
+  };
 }
 
 export default Lotto;
