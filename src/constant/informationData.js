a4439ab2982c5045e9ea3eaea70bb61dbd934a71
intersoom <sallybig@naver.com>
2023-11-08 02:31:26 +0900

    feat: 안내 문구 상수 추가

---
+++ src/constant/informationData.js
@@ -0,0 +1,14 @@
+export const INFORMATION_MESSAGE = Object.freeze({
+  PURCHASE: "개를 구매했습니다.\n",
+  WINNING_STATISTICS: "\n당첨 통계\n---\n",
+  FIFTH_WINNING: "3개 일치 (5,000원) - ",
+  FOURTH_WINNING: "4개 일치 (50,000원) - ",
+  THIRD_WINNING: "5개 일치 (1,500,000원) - ",
+  SECOND_WINNING: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  FIRST_WINNING: "6개 일치 (2,000,000,000원) - ",
+  NUMERAL: "개\n",
+  RETURN_RATE: "총 수익률은",
+  PERCENT: "%입니다.",
+});
+
+export default INPUT_MESSAGE;
