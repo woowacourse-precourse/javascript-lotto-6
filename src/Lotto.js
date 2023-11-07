9aee981319da8bd632df00f7190cc7ce125577a8
Hong sung soo <sow9707@daum.net>
2023-11-07 14:53:24 +0900

    refactor: 로또 갯수 구하는 함수를 구매한 로또 번호를 배열에 담는 함수로 리팩토링

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { validationUserMoney } from "./validation.js";
+import { lottoNumbers } from "./lotto-number.js";
+import {
+  inputMoney,
+  lottoWinnerNumbers,
+  lottoBonusNumber,
+} from "./user-input.js";
+
+const Console = MissionUtils.Console;
+const LOTTOPRICE = 1000;
+
 const PRIZE = {
   FIRST: 2000000000,
   SECOND: 30000000,
   THIRD: 1500000,
   FOURTH: 50000,
-  FIFTH: 5000
+  FIFTH: 5000,
 };
 
 class Lotto {
@@ -24,45 +36,62 @@ class Lotto {
   }
 
   compareNumber(winnerNumbers) {
-    this.#numbers.forEach((number) => this.qulificationWinNumber(winnerNumbers, number));
+    this.#numbers.forEach((number) =>
+      this.qulificationWinNumber(winnerNumbers, number)
+    );
   }
 
   qulificationWinNumber(winnerNumbers, number) {
-    if(winnerNumbers.includes(number)) {
+    if (winnerNumbers.includes(number)) {
       this.COUNT++;
     }
   }
 
   compareBonusNumber(bonusNumber) {
-    if(this.#numbers.includes(bonusNumber)) {
+    if (this.#numbers.includes(+bonusNumber)) {
       this.BONUS = true;
     }
   }
 
   qulificationSecond() {
-    if(this.Bonus) {
-      return this.PRIZE = PRIZE.SECOND;
+    if (this.Bonus) {
+      this.PRIZE = PRIZE.SECOND;
     }
-    if(!this.BONUS) {
-      return this.PRIZE = PRIZE.THIRD;
+    if (!this.BONUS) {
+      this.PRIZE = PRIZE.THIRD;
     }
   }
 
   getPrize() {
-    if(this.COUNT === 6) {
-      return this.PRIZE = PRIZE.FIRST;
+    if (this.COUNT === 6) {
+      return (this.PRIZE = PRIZE.FIRST);
     }
-    if(this.COUNT === 5 && this.BONUS) {
+    if (this.COUNT === 5 && this.BONUS) {
       return this.qulificationSecond();
     }
-    if(this.COUNT === 4) {
-      return this.PRIZE = PRIZE.FOURTH;
+    if (this.COUNT === 4) {
+      return (this.PRIZE = PRIZE.FOURTH);
     }
-    if(this.COUNT === 3) {
-      return this.PRIZE = PRIZE.FIFTH;
+    if (this.COUNT === 3) {
+      return (this.PRIZE = PRIZE.FIFTH);
     }
   }
 
+  static async buy() {
+    let userMoney;
+    let whileBreak = true;
+    while (whileBreak) {
+      try {
+        userMoney = await inputMoney();
+        whileBreak = false;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    const manyLottos = userMoney / LOTTOPRICE;
+    const lottoArray = Array(manyLottos).fill();
+    return lottoArray;
+  }
 }
 
 export default Lotto;
--- src/lotto-util.js
+++ src/lotto-util.js
@@ -1,16 +1,9 @@
-const LOTTOPRICE = 1000;
-
 function getTotalPrize(lottoArray) {
-    let totalPrize = 0;
-    for(let i = 0; i < lottoArray.length; i++) {
-        totalPrize += lottoArray[i].Prize;
-    }
-    return totalPrize;
+  let totalPrize = 0;
+  for (let i = 0; i < lottoArray.length; i++) {
+    totalPrize += lottoArray[i].Prize;
+  }
+  return totalPrize;
 }
 
-function lottoPurchasesNumber(userMoney) {
-    const lottoNumber = userMoney/LOTTOPRICE;
-    return lottoNumber;
-}
-
-export { getTotalPrize, lottoPurchasesNumber };
+export { getTotalPrize };
