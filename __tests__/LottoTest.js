4ebc4523225ac9388e61155e73e5631458c885cf
yu-retel <dbdudals100@naver.com>
2023-11-08 20:36:52 +0900

    test(Lotto): 로또의 단위테스트 구현

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,12 +7,25 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또번호가 1 ~ 45가 아니면 예외가 발생한다.', () => {
+    const numbers = [0, 46, 121, 1000];
+
+    numbers.map((number) => expect(() => {
+      Lotto.validateLottoNumber(number);
+    }).toThrow("[ERROR]"));
+  });
+
+  test('보너스 번호가 당첨번호와 중복된다면 예외가 발생한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]).getNumbers();
+
+    expect(() => {
+      Lotto.validateBonusNumberInWinningNumber(lotto, 1);
+    }).toThrow('[ERROR]');
+  })
 });
