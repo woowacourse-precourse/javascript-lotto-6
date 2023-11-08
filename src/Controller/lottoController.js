ce7fbd5f9d5b1a3987b3f6d87bf76f0cfa1bd787
yunji <dbswl330@gmail.com>
2023-11-08 17:14:02 +0900

    refactor: 잘못된 금액 입력 시 다시 입력받도록 수정

--- src/Controller/lottoController.js
+++ src/Controller/lottoController.js
@@ -2,6 +2,7 @@ import Input from "../view/Input.js";
 import Output from "../View/Output.js";
 import Lottos from "../Model/Lottos.js";
 import Calculate from "../Model/Calculate.js";
+import { Console } from "@woowacourse/mission-utils";
 
 class LottoController {
   constructor() {
@@ -11,7 +12,18 @@ class LottoController {
   }
 
   async lottoGame() {
-    const purchaseAmount = await this.input.getPurchaseAmount();
+    let purchaseAmount;
+    while (true) {
+      try {
+        purchaseAmount = await this.input.getPurchaseAmount();
+        if (!this.lottos.validatePurchaseAmount(purchaseAmount)) {
+          throw new Error("[ERROR] 유효한 구매 금액을 입력하세요.");
+        }
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
     this.lottos.createLottos(purchaseAmount);
     this.output.printPurchaseQuantity(this.lottos.getLottoQuantity());
     this.output.printLottoTickets(this.lottos.getLottoTickets());
--- src/Model/Lottos.js
+++ src/Model/Lottos.js
@@ -19,17 +19,18 @@ class Lottos {
   }
 
   calculateLottoQuantity(purchaseAmount) {
-    this.validatePurchaseAmount(purchaseAmount);
+    // this.validatePurchaseAmount(purchaseAmount);
     this.#lottoQuantity = purchaseAmount / 1000;
   }
 
   validatePurchaseAmount(purchaseAmount) {
     if (!Number.isInteger(Number(purchaseAmount))) {
-      throw new Error("[ERROR] 1000원 단위의 정수인 숫자를 입력해 주세요.");
+      return false;
     }
     if (purchaseAmount <= 0 || purchaseAmount % 1000 !== 0) {
-      throw new Error("[ERROR] 1000원 단위의 유효한 구매 금액을 입력하세요.");
+      return false;
     }
+    return true;
   }
 
   createLottos(amount) {
