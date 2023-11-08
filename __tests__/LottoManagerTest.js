af26e19829ecdc9e0da4ebad59cb2e16318bc96a
fecapark <fecapark@gmail.com>
2023-11-08 15:40:12 +0900

    test: 로또 번호를 개수만큼 만들었는지 체크하는 테스트 구현

---
+++ __tests__/LottoManagerTest.js
@@ -0,0 +1,27 @@
+import LottoManager from "../src/core/managers/LottoManager.js";
+
+describe("LottoManager 클래스 테스트", () => {
+  /** @type {LottoManager} */
+  let lottoManager;
+
+  beforeEach(() => {
+    lottoManager = new LottoManager();
+  });
+
+  test("로또 번호들을 개수만큼 만드는 기능 테스트", () => {
+    function isAllLottos(lottos) {
+      const realLottos = lottos.filter((lotto) => lotto instanceof Lotto);
+      return realLottos.length === lottos.length;
+    }
+
+    const inputs = [4, 6];
+
+    inputs.forEach((input) => {
+      const lottos = lottoManager.makeLottos(input);
+      const makeCorrectly = isAllLottos(lottos);
+
+      expect(makeCorrectly).toBe(true);
+      expect(lottos.length === input).toBe(true);
+    });
+  });
+});
