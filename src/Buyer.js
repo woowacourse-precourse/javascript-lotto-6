0c47d99891f6af4030f547edce34d20202a92b3e
이동주 <leednj0113@naver.com>
2023-11-08 10:05:50 +0900

    feat(Buyer): 구입 금액에 대한 유효성 검사

--- src/Buyer.js
+++ src/Buyer.js
@@ -10,6 +10,7 @@ class Buyer {
   async inputMoney() {
     try {
       const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+      this.vaildateMoney(input);
 
       this.#purchaseMoney = Number(input);
     } catch (error) {
@@ -19,6 +20,13 @@ class Buyer {
     }
 
     return this.#purchaseMoney;
+  }
+
+  vaildateMoney(input) {
+    if (isNaN(input)) throw new Error("[ERROR] 숫자만 입력 가능합니다.");
+    if (input < 1000) throw new Error("[ERROR] 최소 구입금액은 1,000원입니다.");
+    if (input % 1000 !== 0)
+      throw new Error("[ERROR] 1,000원 단위로 입력해 주세요.");
   }
 }
 
