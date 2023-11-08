299d6318bdffce36e62fb384a7ff36cbf09d4971
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2023-11-08 15:49:20 +0900

    feat: 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 class App {
-  async play() {}
+  // 구입 금액 입력받기
+  async inputMoney() {
+    const money = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    if (money % 1000 !== 0) {
+      throw new Error("[ERROR] 1000원 단위로 입력해 주세요.");
+    }
+    return money;
+  }
 }
 
 export default App;
