036e89fa60ff05845fe805a1e91dfec119019750
yura <yuraa0302@gmail.com>
2023-11-08 22:00:20 +0900

    style : printBuyResult로 함수명 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,7 +4,7 @@ const OutputView = {
   /**
    * @param {numbers: number[][]} lottoes
    */
-  outputBuyResult: (numbers) => {
+  printBuyResult: (numbers) => {
     Console.print(`\n${numbers.length}개를 구매했습니다.`);
     numbers.forEach((number) => {
       Console.print(`[${number.join(", ")}]`);
