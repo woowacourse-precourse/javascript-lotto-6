398041ed09f5dd104400de4df2e80f63d4e3aa37
OhHyukJun <kmh7277@naver.com>
2023-11-08 15:44:25 +0900

    Refactor: 코드 분리

--- src/LottoGame/CreateWinnum.js
+++ src/LottoGame/CreateWinnum.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { WinNumber } from "../LottoResult/WinNumber.js";
-import Lotto from "../Lotto.js";
-import { BonusNumber } from "../LottoResult/BonusNumber.js";
+import { WinNumber } from "../LottoInput/WinNumber.js";
+import { BonusNumber } from "../LottoInput/BonusNumber.js";
+import { LottoChecker } from "../LottoResult/LottoCheck.js";
 
 export class CreateWinNum {
     constructor(lottoNum) {
@@ -13,43 +13,18 @@ export class CreateWinNum {
         const winNumbers = await this.winNumber.winnumber();
         const bonusNumber = new BonusNumber(winNumbers);
         const bonusNum = await bonusNumber.bonusNumber();
-        const winResult = this.checkWinning(this.lottoNum, { normal: winNumbers, bonus: bonusNumber });
 
+        const lottoChecker = new LottoChecker(this.lottoNum);
+        const winResult = lottoChecker.checkWinning({ normal: winNumbers, bonus: bonusNum });
         this.printWinResult(winResult);
     }
 
-    checkWinning(lottoNum, winNumbers) {
-        let winResult = {
-          3: 0,
-          4: 0,
-          5: 0,
-          '5+bonus': 0,
-          6: 0,
-        };
-      
-        lottoNum.forEach(lotto => {
-          const matchCount = lotto.match(winNumbers.normal);
-          if (matchCount < 3) return;
-      
-          if (matchCount === 5 && lotto.getNumbers().includes(winNumbers.bonus)) {
-            winResult['5+bonus']++;
-          } else {
-            winResult[matchCount]++;
-          }
-        });
-      
-        return winResult;
-      }
-
     printWinResult(winResult) {
+        MissionUtils.Console.print("당첨 통계\n---");
         MissionUtils.Console.print(`3개 일치 (5,000원) - ${winResult[3]}개`);
         MissionUtils.Console.print(`4개 일치 (50,000원) - ${winResult[4]}개`);
         MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${winResult[5]}개`);
-        MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winResult['5+bonus']}개`);
-        MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${winResult[6]}개`);
+        MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winResult[6]}개`);
+        MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${winResult[7]}개`);
     }
 }
-/*
-const lottoGame = new CreateWinNum();
-lottoGame.start();
-*/
