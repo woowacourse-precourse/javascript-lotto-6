2d0098d52e14b703a8b5a396c5610b65d4163fb8
Klomachenko <leekyumin0901@naver.com>
2023-11-08 21:47:23 +0900

    feat(lottoTest): 로또 번호 생성 테스트 구현
    Description: Lotto 클래스의 생성자를 호출하여 정상적인 로또 번호 배열을
    전달하였을 때, Lotto 클래스의 인스턴스가 반환되는지 확인하는 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,11 +7,15 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호를 정상적으로 생성할 수 있어야 합니다.", () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto).toBeInstanceOf(Lotto);
   });
 
   // 아래에 추가 테스트 작성 가능
