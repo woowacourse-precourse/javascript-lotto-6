430474f47a01160de6e52e9f82ee56a235c9ea13
flowersayo <flowersayo0811@ewhain.net>
2023-11-08 10:54:36 +0900

    feat: 누락된 구매금액 유효성 검증 코드 추가

--- src/domain/Buyer.js
+++ src/domain/Buyer.js
@@ -5,24 +5,21 @@ class Buyer {
   #money;
 
   constructor(money) {
+    this.#validate(money);
     this.#money = money;
   }
 
   #validate(money) {
     const REGEX_NUMERIC = /^\d+$/;
-
     if (!REGEX_NUMERIC.test(money)) {
       throw new CustomError(ERROR.MONEY_NOT_A_NUMBER);
     }
-
     if (money <= 0) {
       throw new CustomError(ERROR.MONEY_NOT_A_POSITIVE);
     }
-
     if (money > LOTTO.MONEY_LIMIT) {
       throw new CustomError(ERROR.EXCEED_MONEY_LIMIT);
     }
-
     if (money % LOTTO.MONEY_UNIT !== 0) {
       throw new CustomError(ERROR.INVALID_MONEY_UNIT);
     }
