a384c25b2c5b0dbd47930bff445a2685b0447f73
ji-dawn <wlgy33@naver.com>
2023-11-08 23:48:27 +0900

    feat: 로또 구매 금액 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    let money = await this.inputMoney();
+  }
+  async inputMoney() {
+    let money = await Console.readLineSync;
+    ("구입할 로또 금액을 입력해주세요");
+  }
 }
 
 export default App;
