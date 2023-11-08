440119689168f034c660000abbdc611bc663ce21
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-08 17:19:46 +0900

    feat(view): 통계 출력 기능

--- src/view/output.js
+++ src/view/output.js
@@ -1,8 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
+import { LOTTO } from '../config.js';
 
 const MESSAGE = Object.freeze({
   TOTAL_LOTTO_COUNT: (count) => `\n${count}개를 구매했습니다.`,
   LOTTO_NUMBER: (numbers) => `[${numbers.join(', ')}]`,
+  WINNING_STATISTICS: '당첨 통계',
+  WINNING_COUNT: (amount, count) => `(${amount.toLocaleString()}원) - ${count}개`,
 });
 export default class Output {
   static purchasedLottoNumbers(lottos) {
@@ -14,5 +17,17 @@ export default class Output {
     const output = MESSAGE.LOTTO_NUMBER(lottoNumbers);
 
     Console.print(output);
+  }
+
+  static winningStatistics(result) {
+    const { FIRST, SECOND, THIRD, FOURTH, FIFTH } = LOTTO.WIN;
+
+    Console.print('\n당첨 통계');
+    Console.print('---------');
+    Console.print(`3개 일치 ${MESSAGE.WINNING_COUNT(FIFTH, result[FIFTH])}`);
+    Console.print(`4개 일치 ${MESSAGE.WINNING_COUNT(FOURTH, result[FOURTH])}`);
+    Console.print(`5개 일치 ${MESSAGE.WINNING_COUNT(THIRD, result[THIRD])}`);
+    Console.print(`5개 일치, 보너스 볼 일치 ${MESSAGE.WINNING_COUNT(SECOND, result[SECOND])}`);
+    Console.print(`6개 일치 ${MESSAGE.WINNING_COUNT(FIRST, result[FIRST])}`);
   }
 }
