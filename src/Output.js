378a8a815e7dac17c4b6e1b95e6f4cc473be032a
gilgor2 <misby0327@gmail.com>
2023-11-08 20:53:41 +0900

    feat(Output.js): 발표자 객체 구현

--- src/Output.js
+++ src/Output.js
@@ -1,14 +1,37 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class Output {
-    printLotoList(lottoList) {
+	static printLottoList(lottoList) {
+		Console.print(`${lottoList.length}개를 구매했습니다.`);
 
-    }
+		lottoList.forEach((lotto) => {
+			const numbers = lotto.getLottoNumber();
+			Console.print(`[${numbers.join(', ')}]`);
+		});
+		Console.print('');
+	}
 
-    printWinningResult(winningLottoList) {
+	static printWinningResult(winningLottoList) {
+		Console.print('당첨 통계\n---');
+		winningLottoList.forEach((statisticObj) => {
+			const { correctCnt, bonusCnt, winnings } =
+				statisticObj.condition.getWinningCondition();
+			Console.print(
+				`${correctCnt}개 일치${
+					bonusCnt > 0 ? ', 보너스 볼 일치' : ''
+				} (${Output.numberWithCommas(winnings)}원) - ${
+					statisticObj.count
+				}개`,
+			);
+		});
+	}
 
-    }
+	static printProfitRate(profitRate) {
+		Console.print(`총 수익률은 ${profitRate}입니다.`);
+	}
 
-    printProfitRate(profitRate) {
-
-    }
+	static numberWithCommas(x) {
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	}
 }
 export default Output;
