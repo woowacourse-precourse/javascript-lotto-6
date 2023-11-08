ebb6e9219314b0f4813fb4857a0f1fb1edca66df
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2023-11-08 19:24:41 +0900

    chore: 함수 순서 변경

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -14,8 +14,34 @@ const InputView = {
                 return Number(input)
             }
             catch(error){
-                MissionUtils.Console.print('[ERROR]')
+                MissionUtils.Console.print('[ERROR] 금액 입력받기 오류')
             } 
+        }
+    },
+
+    // 사용자로부터 당첨번호 받기
+    async getWinningLotto(){
+        try{
+            const input = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.')
+            const splitInput = input.split(',').map(Number)        
+            const sortedInput = splitInput.sort((a, b) => a - b)
+            return sortedInput
+        }
+        catch(error){
+            MissionUtils.Console.print('[ERROR] 당첨번호 입력받기 오류')
+        }
+    },
+
+    // 사용자로부터 보너스번호 받기
+    async getBonus(){
+        try{
+            const input = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.')
+            this.checkIsNumber(Number(input))
+
+            return Number(input)
+        }
+        catch(error){
+            MissionUtils.Console.print('[ERROR] 보너스번호 입력받기 오류')
         }
     },
 
@@ -40,34 +66,6 @@ const InputView = {
     checkMoneyRange(number){
         if(number < 1000){
             throw new Error('[ERROR] 구입 금액은 1000원 이상이어야 합니다.')
-        }
-    },
-
-    // 사용자로부터 당첨번호 받기
-    async getWinningLotto(){
-        while(true){
-            try{
-                const input = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.')
-                const splitInput = input.split(',').map(Number)        
-                const sortedInput = splitInput.sort((a, b) => a - b)
-                return sortedInput
-            }
-            catch(error){
-                MissionUtils.Console.print('[ERROR] getWinningLotto Error')
-            }
-        }
-    },
-
-    // 사용자로부터 보너스번호 받기
-    async getBonus(){
-        try{
-            const input = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.')
-            this.checkIsNumber(Number(input))
-
-            return Number(input)
-        }
-        catch(error){
-            MissionUtils.Console.print('[ERROR]')
         }
     }
 }
