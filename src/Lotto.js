7cc3a7f447cd59f4dec3f502534cb334deaee916
j-nary <dlwls0921@naver.com>
2023-11-08 22:12:36 +0900

    feat: 로또 단위 테스트 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+    if (
+      numbers.some((n) => {
+        typeof n !== "number" || number < 1 || number > 45;
+      })
+    ) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
 
   // TODO: 추가 기능 구현
