d7b0cc1dee68bc45e67e9256d664f7b206a120f3
Klomachenko <leekyumin0901@naver.com>
2023-11-08 20:56:02 +0900

    feat: isLottoNumbersFormat 구현
    Description: 로또 번호 배열이 올바른 형식인지 확인

--- src/Validate.js
+++ src/Validate.js
@@ -6,6 +6,12 @@ class Validate {
   static isMultipleOf1000(input) {
     return +input % 1000 === 0;
   }
+
+  static isLottoNumbersFormat(winningNumbers) {
+    if (winningNumbers.length !== 6) return false;
+    const set = new Set(winningNumbers);
+    return set.size === winningNumbers.length;
+  }
 }
 
 export default Validate;
