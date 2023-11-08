5f473269b63356ec17cfab72377fa792c3ed34ae
hyzzzzy <shinhj4068@naver.com>
2023-11-08 21:28:51 +0900

    fix(constants): 변경된 변수명 적용

--- src/constants.js
+++ src/constants.js
@@ -39,7 +39,7 @@ export const ERROR_MESSAGE = Object.freeze({
   not_number: `${PREFIX.error} 숫자가 아닙니다.`,
   not_natural_number: `${PREFIX.error} 자연수가 아닙니다.`,
   not_lotto_cost: `${PREFIX.error} ${SETTING.lotto_cost} 단위의 금액을 입력하세요.`,
-  not_six_numbers: `${PREFIX.error} 쉼표를 기준으로 ${SETTING.input_length}개의 숫자를 입력하세요.`,
+  not_six_numbers: `${PREFIX.error} 쉼표를 기준으로 ${SETTING.size}개의 숫자를 입력하세요.`,
   not_range: `${PREFIX.error} ${SETTING.min_lotto_number} 이상 ${SETTING.max_lotto_number} 이하의 숫자를 입력하세요.`,
   not_unique: `${PREFIX.error} 중복된 숫자가 존재합니다.`,
   not_unique_bonus: `${PREFIX.error} 이미 존재하는 당첨번호 입니다.`,
