055b8b5f426c877f095b8761f9c353a26122ceac
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-08 18:57:46 +0900

    chores: lint 적용

--- src/Validation.js
+++ src/Validation.js
@@ -1,48 +1,48 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import { STRINGS } from './constants/STRINGS';
 class Validation {
-  static validateInputPrice(inputPrice){
-    if (inputPrice % 1000 !== 0){
+  static validateInputPrice(inputPrice) {
+    if (inputPrice % 1000 !== 0) {
       throw new Error(STRINGS.ERROR_UNIT);
     }
   }
 
-  static validateAnswerNumbersComma(answerNumbers){
-    if (!answerNumbers.includes(",")){
+  static validateAnswerNumbersComma(answerNumbers) {
+    if (!answerNumbers.includes(',')) {
       throw new Error(STRINGS.ERROR_COMMA);
     }
   }
 
-  static validateRepeatedNumbers(numbers){
+  static validateRepeatedNumbers(numbers) {
     const SET_ANSWERS = new Set(numbers);
-    if (numbers.length !== SET_ANSWERS){
+    if (numbers.length !== SET_ANSWERS) {
       throw new Error(STRINGS.ERROR_REPEATED);
     }
   }
 
-  static repeatedAnswerNumbers(answerNumbers){
-    const PARSED_ANSWERS = answerNumbers.split(",");
+  static repeatedAnswerNumbers(answerNumbers) {
+    const PARSED_ANSWERS = answerNumbers.split(',');
     this.validateRepeatedNumbers(PARSED_ANSWERS);
   }
 
-  static validateNumbersLength(numbers){
+  static validateNumbersLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error(STRINGS.ERROR_LENGTH);
     }
   }
 
-  static validateRandomNumbers(randomNumbers){
+  static validateRandomNumbers(randomNumbers) {
     this.validateNumbersLength(randomNumbers);
     this.validateRepeatedNumbers(randomNumbers);
   }
 
-  static validateRangeNumbers(bonusNumber){
-    if (bonusNumber < 1 || bonusNumber > 45){
+  static validateRangeNumbers(bonusNumber) {
+    if (bonusNumber < 1 || bonusNumber > 45) {
       throw new Error(STRINGS.ERROR_RANGE);
     }
   }
 
-  static validateBonusNumber(answerNumbers, inputBonusNumber){
+  static validateBonusNumber(answerNumbers, inputBonusNumber) {
     const compareNumberArray = answerNumbers.push(inputBonusNumber);
     this.validateRepeatedNumbers(compareNumberArray);
     this.validateRangeNumbers(inputBonusNumber);
