87663d0fbaafac267d9ca154705d4a8a1ec5d397
Sohye <saeyan2477@gmail.com>
2023-11-08 21:29:11 +0900

    feat: SetGame 클래스 구현(당첨현황 확인 및 수익률 계산)
    - 기존 로직 연결
    - 당첨현황 확인
    - 수익률 계산

--- src/SetGame.js
+++ src/SetGame.js
@@ -1,5 +1,6 @@
-import { LOTTO_RULE } from './Constants.js';
+import { LOTTO_RULE, WINNING_PROFITS } from './Constants.js';
 import Lotto from './Lotto.js';
+import LottoUtils from './LottoUtils.js';
 import Utils from './Utils.js';
 import { Validation } from './Validation/Validation.js';
 
@@ -14,6 +15,7 @@ class SetGame {
     this.#buyAmount = buyAmount;
     this.#buyNumber - buyAmount / LOTTO_RULE.UNIT;
     this.#issueLotto();
+    this.lottoUtils = new LottoUtils(this.#lottos);
   }
   // 구매금액 검증
   #validate(numbers) {
@@ -34,6 +36,29 @@ class SetGame {
       lottoList.push(this.#lottos[i].getNumbers());
     }
     return lottoList;
+  }
+
+  // 당첨 번호 및 보너스 번호 비교 후, 당첨현황 확인
+  getWinningStatus(winningNumbers, bonusNumber) {
+    Validation.validateLottoNumbers(winningNumbers);
+    Validation.validateBonusNumber(bonusNumber);
+    const matchingArray = this.lottoUtils.matchingCountToArray(winningNumbers);
+    const matchingObject = this.lottoUtils.matchingCountToObject(
+      matchingArray,
+      bonusNumber,
+    );
+    const winningStatus = this.lottoUtils.checkGameResult(matchingObject);
+
+    return winningStatus;
+  }
+  // 수익률 계산
+  calcProfit(winningStatus) {
+    let sumProfit = 0;
+    for (let i in winningStatus) {
+      sumProfit += WINNING_PROFITS[i] * winningStatus[i];
+    }
+    const profit = ((sumProfit / this.#buyAmount) * 100).toFixed(1);
+    return profit;
   }
 }
 
