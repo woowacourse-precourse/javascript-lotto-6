f5e9ef2449682ed42913097b24b161be0c4fe3a5
lottt105 <hyobin0601@naver.com>
2023-11-08 11:06:53 +0900

    feat: 공통 예외처리 기능 추가

---
+++ src/utils/Validator.js
@@ -0,0 +1,27 @@
+import { error, condition } from '../consts';
+
+const Validator = {
+  validateInput(input) {
+    if (input.trim().length === condition.inputEmptyLength) {
+      throw error.inputEmptyError;
+    }
+  },
+
+  validateNumbers(numbers) {
+    numbers.forEach((number) => this.validateNumber(number));
+  },
+
+  validateNumber(number) {
+    if (!Number.isInteger(number)) {
+      throw error.numberTypeError;
+    }
+    if (
+      number < condition.lottoMinNumber ||
+      number > condition.lottoMaxNumber
+    ) {
+      throw error.numberRangeError;
+    }
+  },
+};
+
+export default Validator;
