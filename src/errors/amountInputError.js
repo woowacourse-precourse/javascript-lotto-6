fd23d89d91571bd20f8ae83d9caa709e9d4feb09
dongsuseo <poopoo0515@naver.com>
2023-11-08 13:02:00 +0900

    feat: 구입 금액이 1000원 단위가 아닌 경우 예외 처리 추가

--- src/errors/amountInputError.js
+++ src/errors/amountInputError.js
@@ -4,6 +4,10 @@ const amountInputError = (amount) => {
     Console.print("[ERROR] 숫자가 잘못된 형식입니다.");
     throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
+  if (amount % 1000 !== 0) {
+    Console.print("[ERROR] 1000원 단위로 입력해주세요.");
+    throw new Error("[ERROR] 1000원 단위로 입력해주세요.");
+  }
 };
 
 export default amountInputError;
