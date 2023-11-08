1c6eee7f31b0ec9f02f71241139696f2af0b9668
publdaze <publdaze@naver.com>
2023-11-08 20:40:40 +0900

    refactor: 오타 수정

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,14 +1,10 @@
 import LOTTO_INFO from '../constants/lottoInfo.js';
 
 export const isEmptyString = (str) => str.trim() === '';
-export const isIntegarString = (str) => Number.isInteger(Number(str));
-export const isIntegarNumList = (strList) => strList.every(isIntegarString);
-export const hasDuplicateNumber = (numList) =>
-  numList.some((num) => numList.indexOf(num) !== numList.lastIndexOf(num));
+export const isIntegerString = (str) => Number.isInteger(Number(str));
+export const isIntegerNumList = (strList) => strList.every(isIntegerString);
+export const hasDuplicateNumber = (numList) => numList.some((num) => numList.indexOf(num) !== numList.lastIndexOf(num));
 
-export const isValidPurchaseUnit = (num) =>
-  num % LOTTO_INFO.purchase.unit === 0 && num >= LOTTO_INFO.purchase.unit;
-export const isInvalidNumberRange = (num) =>
-  num < LOTTO_INFO.lottoNumber.min || num > LOTTO_INFO.lottoNumber.max;
-export const hasInvalidNumberRange = (numList) =>
-  numList.some(isInvalidNumberRange);
+export const isValidPurchaseUnit = (num) => num % LOTTO_INFO.purchase.unit === 0 && num >= LOTTO_INFO.purchase.unit;
+export const isInvalidNumberRange = (num) => num < LOTTO_INFO.lottoNumber.min || num > LOTTO_INFO.lottoNumber.max;
+export const hasInvalidNumberRange = (numList) => numList.some(isInvalidNumberRange);
