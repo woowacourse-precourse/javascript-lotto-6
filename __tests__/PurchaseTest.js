e704c49d723f0081dc9c4c9bc286dd352673f54f
dxwwan <kkw1700@gmail.com>
2023-11-08 20:29:05 +0900

    test: 숫자가 아닌 경우 예외 테스트 추가

--- __tests__/PurchaseTest.js
+++ __tests__/PurchaseTest.js
@@ -2,7 +2,15 @@ import Purchase from "../src/Credit.js";
 import Lotto from "../src/Lotto.js";
 
 describe("로또 구매 테스트", () => {
-  test("로또 구매 예외 테스트", () => {
+  test("로또 구매 예외 테스트 - 숫자가 아닐 경우", () => {
+    expect(() => {
+      const purchase = new Purchase();
+      const result = purchase.getAmountOfLotto("3000j");
+      expect(result).toThrow("[ERROR]");
+    });
+  });
+
+  test("로또 구매 예외 테스트 - 1000단위가 아닐 경우", () => {
     expect(() => {
       const purchase = new Purchase();
       const result = purchase.getAmountOfLotto(3001);
