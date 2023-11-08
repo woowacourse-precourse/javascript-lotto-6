b7fa67b00e45ff4c1488b6403b96781fb62e993f
j-nary <dlwls0921@naver.com>
2023-11-08 20:38:37 +0900

    feat: 구입 금액 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      let [count, change] = await this.userPriceInput();
+      
+    } catch (error) {
+      [count, change] = await this.userPriceInput();
+    }
+  }
+  async userPriceInput() {
+    try {
+      const price = await MissionUtils.Console.readLineAsync (
+        "구입금액을 입력해 주세요.\n"
+      );
+      if (!this.notNumber(price)) {
+        throw new Error("[ERROR] 잘못된 형식입니다. 숫자를 입력해주세요.")
+      }
+      const cnt = price / 1000;
+      const change = price % 1000;
+      return [cnt, change];
+    } catch(error) {
+      throw error;
+    }
+  }
+
+  notNumber(input) {
+    return typeof input === 'number' && !isNaN(input);
+  }
+
+
 }
+  
 
 export default App;
