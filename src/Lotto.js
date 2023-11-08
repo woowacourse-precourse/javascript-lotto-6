2dc24cd8a811afa89ca481f45705ef2018a98174
23YUJIN <asdbnm066@daum.net>
2023-11-08 16:50:55 +0900

    feat: 번호 비교하기 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -25,6 +25,23 @@ class Lotto {
   printNumbers() {
     Console.print(`[${this.#numbers.map((number) => number).join(", ")}]`);
   }
+
+  compareNumbers(winningNumbers, bonusNumber) {
+    const doesNumberExist = new Map();
+    const compareResult = { sameNumberCount: 0, bonusIsSame: false };
+    for (const number of this.#numbers) {
+      doesNumberExist.set(number, true);
+    }
+    for (const number of winningNumbers) {
+      if (doesNumberExist.has(number)) {
+        compareResult.sameNumberCount++;
+      }
+    }
+    if (doesNumberExist.has(bonusNumber)) {
+      compareResult.bonusIsSame = true;
+    }
+    return compareResult;
+  }
 }
 
 export default Lotto;
