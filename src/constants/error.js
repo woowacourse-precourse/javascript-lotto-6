4c51ad040ba6c332aa6999f4e8278119364a8e66
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 15:46:12 +0900

    refacor: 상수 관련 구현에 따라 사용하지 않는 파일 삭제

--- src/constants/error.js
+++
@@ -1,8 +0,0 @@
-const ERROR = {
-  none: '[ERROR] 금액을 입력하지 않았습니다.',
-  type: '[ERROR] 숫자 타입의 금액을 입력하세요.',
-  devide1000: '[ERROR] 천 단위로 나누어지는 금액을 입력하세요.',
-  over1000: '[ERROR] 1000 이상의 숫자를 입력하세요.',
-};
-
-export default ERROR;
