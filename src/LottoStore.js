8f4f04021bc8abe5486fe82bf49cb7df893c4d1e
이동주 <leednj0113@naver.com>
2023-11-08 10:07:45 +0900

    feat(LottoStore): 로또 개수에 맞춰 로또를 발행

--- src/LottoStore.js
+++ src/LottoStore.js
@@ -3,6 +3,19 @@ import { Random, Console } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 
 class LottoStore {
+  sellLotto(money) {
+    const lottoTickets = money / 1000;
+
+    const lottos = [];
+    for (let i = 0; i < lottoTickets; i++) {
+      const randomNumbers = this.makeRandomNumbers();
+      const lotto = new Lotto(randomNumbers);
+
+      lottos.push(lotto);
+    }
+
+    return lottos;
+  }
 }
 
 export default LottoStore;
