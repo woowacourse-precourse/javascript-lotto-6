5a4959849f322039ab2817bfac7e9c035d0ee920
won0104 <98508955+won0104@users.noreply.github.com>
2023-11-08 16:35:35 +0900

    chore: 테스트 코드에 따른 리턴 값 추가

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -5,17 +5,20 @@ const purchaseAmount = (input) => {
   if (!/^\d000$/.test(String(input))) {
     throw CustomError.userInputError(ERROR_MESSAGE.invalidAmountError);
   }
+  return true;
 };
 
 const lottoNumbers = (input) => {
   if (!/^(\d+)(,\d+)*$/.test(String(input))) {
     throw CustomError.lottoValidateError(ERROR_MESSAGE.invalidInputNumbers);
   }
+  return true;
 };
 const bonusNumber = (input) => {
   if (!Number(input)) {
     throw CustomError.lottoValidateError(ERROR_MESSAGE.invalidInputBonusNumber);
   }
+  return true;
 };
 
 const InputValidator = {
