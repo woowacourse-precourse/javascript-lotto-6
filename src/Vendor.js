7672a2f7a0b2f7a0f5946bc59a08b9c0ba99f1c5
greeneeya <suzythetravelerr@gmail.com>
2023-11-08 13:38:33 +0900

    feat: 금액이 숫자가 아닐 경우, 예외처리한다.

--- src/Vendor.js
+++ src/Vendor.js
@@ -4,6 +4,9 @@ export default class Vendor {
   static async buyTickets() {
     const user = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     const paid = parseInt(user, 10);
+    if (!Number.isInteger(paid) || paid < 0) {
+      throw new Error('[ERROR] 금액은 숫자만 입력가능합니다.');
+    }
     return paid;
   }
 }
