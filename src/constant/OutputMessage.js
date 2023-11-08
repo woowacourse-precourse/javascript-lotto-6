77b7620d102227b454844afa5465b46b95202ba5
choemangi <choemangi@gmail.com>
2023-11-08 23:45:36 +0900

    final

---
+++ src/constant/OutputMessage.js
@@ -0,0 +1,2 @@
+export const OUTPUT_LOTTO_COUNT = '개를 구매했습니다.';
+export const OUTPUT_WINNING_STATISTICS = '\n당첨 통계\n---';
---
+++ src/view/Output.js
@@ -0,0 +1,45 @@
+import { Console } from '@woowacourse/mission-utils';
+import * as OUTPUT from '../constant/OutputMessage.js';
+
+const Output = {
+  async printLottoCount(lottoCount) {
+    await Console.print(`\n${lottoCount}${OUTPUT.OUTPUT_LOTTO_COUNT}`);
+  },
+  async printLottoList(lottoList) {
+    lottoList.map((lotto) => {
+      lotto.sort((pre, next) => {
+        return pre - next;
+      });
+    });
+
+    lottoList.forEach((lotto) => {
+      Console.print(
+        `[${lotto.map((num, idx) => {
+          return idx > 0 && idx < lotto.length ? ` ${num}` : `${num}`;
+        })}]`
+      );
+    });
+  },
+  printWinnerStatistics(resultMap) {
+    Console.print(OUTPUT.OUTPUT_WINNING_STATISTICS);
+    const price = ['', '', '5,000', '50,000', '1,500,000', '2,000,000,000'];
+
+    resultMap.forEach((totalCnt, sameNum) => {
+      if (sameNum > 2 && sameNum !== 6) {
+        if (sameNum === 7) sameNum = 6;
+        Console.print(
+          `${sameNum}개 일치 (${price[sameNum - 1]}원) - ${totalCnt}개`
+        );
+      } else if (sameNum === 6) {
+        Console.print(
+          `5개 일치, 보너스 볼 일치 (30,000,000원) - ${totalCnt}개`
+        );
+      }
+    });
+  },
+  printTotalReturn(totalReturn) {
+    Console.print(`총 수익률은 ${totalReturn}%입니다.`);
+  },
+};
+
+export default Output;
