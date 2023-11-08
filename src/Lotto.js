f6f0e0f3f09aaa6a234af26c0e57772ccb1d8f30
soosoo22 <lllllh00@naver.com>
2023-11-08 19:42:59 +0900

    chore: 보너스 번호 예외 처리 추가
    보너스 번호가 당첨 번호와 중복되는지 확인
    보너스 번호가 1에서 45사이의 숫자인지 확인

--- src/Lotto.js
+++ src/Lotto.js
@@ -5,8 +5,9 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.invalidNumberFormat(numbers);
     this.outOfRange(numbers);
-    this.#duplicate(numbers);
+    this.duplicate(numbers);
     this.#numbers = numbers;
   }
 
@@ -15,12 +16,23 @@ class Lotto {
       throw ERROR.INVALID_LENGTH;
     }
   }
+
+  invalidNumberFormat(numbers) {
+    const regex = /^[0-9]+$/;
+    if (numbers.some((num) => !regex.test(num))) throw ERROR.INVALID_NUMBER_FORMAT;
+  }
+
   outOfRange(numbers) {
     if (numbers.some((num) => !(num >= 1 && num <= 45))) throw ERROR.OUT_OF_RANGE;
   }
-  #duplicate(numbers) {
+  duplicate(numbers) {
     const set = new Set(numbers);
     if (set.size !== numbers.length) throw ERROR.DUPLICATE_NUMBER;
+  }
+
+  checkBonusNumber(bonus) {
+    if (!(bonus >= 1 && bonus <= 45)) throw ERROR.BONUS_NUMBER_IS_OUT_OF_RANGE;
+    if (this.#numbers.includes(bonus)) throw ERROR.DUPLICATE_WINNING_NUMBER;
   }
 
   getNumbers() {
