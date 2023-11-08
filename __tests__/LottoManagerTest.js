665c7187aa53eea64437befe76b2b56852af6c1c
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 20:10:14 +0900

    test: 로또 매니저의 getMoney 테스트 코드 작성

--- __tests__/LottoManagerTest.js
+++ __tests__/LottoManagerTest.js
@@ -1,4 +1,5 @@
 import LottoManager from '../src/LottoManager';
+import { MONEY_CONSTANTS } from '../src/constants/constants';
 
 describe('LottoManager 테스트', () => {
   const lottoManager = new LottoManager();
@@ -10,5 +11,21 @@ describe('LottoManager 테스트', () => {
     expect(generatedLottoNumbers.length).toBe(6);
     expect(isUnique(generatedLottoNumbers)).toBe(true);
     expect(generatedLottoNumbers.every((num) => num >= 1 && num <= 45)).toBe(true);
+  });
+
+  test('generateLottoTickets 확인, 금액과 카운트 계산', () => {
+    const money = 3000;
+    lottoManager.generateLottoTickets(money);
+
+    expect(lottoManager.getMoney()).toBe(money);
+    expect(lottoManager.getCount()).toBe(parseInt(money / MONEY_CONSTANTS.countUnit));
+  });
+
+  test('getMoney 확인, 같은 금액 반환', () => {
+    const money1 = 3000;
+    lottoManager.generateLottoTickets(money1);
+
+    const money2 = lottoManager.getMoney();
+    expect(money2).toBe(money1);
   });
 });
