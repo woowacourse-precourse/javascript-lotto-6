7b8608b61eacef05a2e26b575ea57590a3e7d7b7
hyewon <hyewon.parkhw@gmail.com>
2023-11-08 16:09:00 +0900

    refactor: 결과 출력 메서드 분리

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
 
 class App {
+  #lottoTickets = [];
+  #lottoCount = 0;
+
   async play() {
-    let lottoCount = await getUserInput("구입금액을 입력해 주세요.\n", (answer)=>{return buyLotto(Number(answer))});
+    this.#lottoCount = await getUserInput("구입금액을 입력해 주세요.\n", (answer)=>{return buyLotto(Number(answer))});
     
-    let lottoTickets = [];
-    for (let i=0; i<lottoCount; i++){
-      lottoTickets.push(getLottoTicket())
+    this.#lottoTickets = [];
+    for (let i=0; i<this.#lottoCount; i++){
+      this.#lottoTickets.push(getLottoTicket())
     }
 
-    Console.print(`\n${lottoCount}개를 구매했습니다.`)
-    for (let lottoTicket of lottoTickets){
-      Console.print(`[${lottoTicket.join(', ')}]`)
+    Console.print(`\n${this.#lottoCount}개를 구매했습니다.`)
+    for (let lottoTicket of this.#lottoTickets){
+      Console.print(`[${lottoTicket.sort((a,b)=>a-b).join(', ')}]`);
     }
     
-    const lotto = await getUserInput("\n당첨 번호를 입력해 주세요.\n", (winningNumbers) => new Lotto(winningNumbers.split(",").map(number=>Number(number))))
-    
+    const lotto = await getUserInput("\n당첨 번호를 입력해 주세요.\n", (winningNumbers) => new Lotto(winningNumbers.split(",").map(number=>Number(number))));
     await getUserInput("\n보너스 번호를 입력해 주세요.\n",(bonusNumber) => lotto.addBonusNumber(bonusNumber));
     
+    this.printResult(lotto);
+  }
 
+  printResult (lotto) {
     let result = {
       "3개 일치 (5,000원)":0, 
       "4개 일치 (50,000원)":0, 
@@ -27,21 +32,20 @@ class App {
       "5개 일치, 보너스 볼 일치 (30,000,000원)":0, 
       "6개 일치 (2,000,000,000원)":0
     }
-
-    for (let ticket of lottoTickets){
+  
+    for (let ticket of this.#lottoTickets){
       const count = lotto.checkResult(ticket);
       recordResult(result, count);
     }
-
+  
     Console.print("\n당첨 통계")
     let income = 0;
     Object.entries(result).forEach((entry, index)=>{
       income += [5000,50000,1500000,30000000,2000000000][index]*entry[1]
       Console.print(`${entry[0]} - ${entry[1]}개`)
     })
-
-    
-    Console.print(`\n총 수익률은 ${income/(lottoCount*10)}%입니다.`)
+  
+    Console.print(`\n총 수익률은 ${income/(this.#lottoCount*10)}%입니다.`)
   }
 }
 
@@ -86,3 +90,4 @@ const getUserInput = async (message, callback) => {
     }
   }
 }
+
