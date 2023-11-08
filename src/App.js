c40ac1b0eabd44503890a290135ae06690182a38
irenee-14 <ireneee145@gmail.com>
2023-11-08 22:56:47 +0900

    feature: 당첨번호, 보너스 번호 입력받기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-//
-import { Console } from "@woowacourse/mission-utils";
-//
+
 import UserInput from "./UserInput.js";
 import LottoGenerator from "./LottoGenerator.js";
-
+import { checkWinning, printResult } from "./checkLotto.js";
 
 class App {
   async play() {
 	const inputs = new UserInput();
+
 	const price = await inputs.inputPrice();
+
 	const lottos = new LottoGenerator(price);
+
+	const winNumbers = await inputs.inputWinNumber();
+	const bonusNumber = await inputs.inputBonusNumber(winNumbers);
 
   }
 }
