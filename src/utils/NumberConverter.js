1f8cb54d168d6d11aa07ffafb4d0827d764a1110
Boeun <boeun0904@naver.com>
2023-11-08 19:09:28 +0900

    feat(NumberConverter): 소수점 포함해서 콤마 찍는 메소드 추가

--- src/utils/NumberConverter.js
+++ src/utils/NumberConverter.js
@@ -2,4 +2,10 @@ export class NumberConverter {
   static splitIntoThreeDigitWithComma(inputNum) {
     return inputNum.toLocaleString('ko-KR');
   }
+  static splitIntoThreeDigitWithCommaContainingDecimalPoint(inputNum, decimal) {
+    return inputNum.toLocaleString('ko-KR', {
+      minimumFractionDigits: decimal,
+      maximumFractionDigits: decimal,
+    });
+  }
 }
