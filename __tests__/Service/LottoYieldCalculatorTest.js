80ada9faa1c847b770cf10f7d027ff3d2c5d38c5
0jenn0 <jenn0.6n@gmail.com>
2023-11-08 17:58:26 +0900

    Test: LottoYieldCalculator 클래스 테스트 코드 추가
    - 총 상금계산,수익률 계산,당첨된 로또 결과 리턴 기능 테스트 코드 추가

--- __tests__/Service/LottoYieldCalculatorTest.js
+++ __tests__/Service/LottoYieldCalculatorTest.js
@@ -1,82 +1,74 @@
-import DrawnLotto from "../../src/Models/DrawnLotto.js";
-import Lotto from "../../src/Models/Lotto.js";
-import LOTTO_SETTINGS from "../../src/config/gameSetting.js";
 import LottoYieldCalculator from "../../src/service/LottoYieldCalculator.js";
 
-describe("LottoYeildCaculator 클래스 테스트", () => {
-  const { PRIZES } = LOTTO_SETTINGS;
-  test("5개 맞으면 보너스 번호도 체크해야한다.", () => {
-    const lotto = new Lotto(["1", "2", "3", "4", "5", "8"]);
-    const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "5", "6"], "8");
-    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
-      PRIZES.SECOND
+describe("LottoYieldCalculator 클래스 테스트", () => {
+  let calculator;
+  let settingsMock;
+  let resultMock;
+  let purchaseAmountMock;
+
+  beforeEach(() => {
+    settingsMock = {
+      getPrizeForRank: jest.fn().mockImplementation((rank) => {
+        const prizes = {
+          FIRST_PRIZE: 2000000000,
+          SECOND_PRIZE: 30000000,
+          THIRD_PRIZE: 1500000,
+          FOURTH_PRIZE: 50000,
+          FIFTH_PRIZE: 5000,
+        };
+        return prizes[rank];
+      }),
+      getAllPrizeDetails: jest.fn().mockReturnValue({
+        FIRST_PRIZE: { prize: 2000000000, matchNum: 6 },
+      }),
+    };
+
+    calculator = new LottoYieldCalculator(settingsMock);
+
+    resultMock = {
+      FIRST_PRIZE: 1,
+      SECOND_PRIZE: 0,
+      THIRD_PRIZE: 0,
+      FOURTH_PRIZE: 0,
+      FIFTH_PRIZE: 0,
+    };
+
+    purchaseAmountMock = {
+      getAmount: jest.fn().mockReturnValue(1000),
+    };
+  });
+
+  test("총 상금 계산이 올바른지 테스트", () => {
+    const expectedTotalPrize = 2000000000;
+    const totalPrize = calculator.getTotalPrize(resultMock);
+
+    expect(totalPrize).toBe(expectedTotalPrize);
+  });
+
+  test("수익률 계산이 정확한지 테스트", () => {
+    const expectedYieldRate = Number(((2000000000 / 1000) * 100).toFixed(2));
+
+    const yieldRate = calculator.calculateYieldRate(
+      resultMock,
+      purchaseAmountMock.getAmount()
     );
+
+    expect(yieldRate).toBe(expectedYieldRate);
   });
 
-  test("1등하는 케이스.", () => {
-    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
-    const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "5", "6"], "8");
-    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
-      PRIZES.FIRST
-    );
-  });
-
-  test("3등하는 케이스", () => {
-    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
-    const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "5", "7"], "8");
-    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
-      PRIZES.THIRD
-    );
-  });
-
-  test("4등하는 케이스", () => {
-    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
-    const drawnLotto = new DrawnLotto(["1", "2", "3", "4", "7", "8"], "9");
-    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
-      PRIZES.FOURTH
-    );
-  });
-
-  test("5등하는 케이스", () => {
-    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
-    const drawnLotto = new DrawnLotto(["1", "2", "3", "7", "8", "9"], "10");
-    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(
-      PRIZES.FIFTH
-    );
-  });
-
-  test("당첨금을 못받는 경우", () => {
-    const lotto = new Lotto(["1", "2", "3", "4", "5", "6"]);
-    const drawnLotto = new DrawnLotto(["1", "2", "7", "8", "9", "10"], "11");
-    expect(LottoYieldCalculator.calculate(lotto, drawnLotto)).toBe(0);
-  });
-
-  test("로또가 여러개 때 총 당첨금액 구하기", () => {
-    const lottos = [
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "7", "8", "9", "10"]), // 1등
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "7"]), // 5등
+  test("당첨된 로또들 결과를 리턴해야한다.", () => {
+    const lottosNumbers = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
     ];
-    const drawnLotto = new DrawnLotto(["1", "2", "7", "8", "9", "10"], "11");
-    expect(LottoYieldCalculator.getTotalPrize(lottos, drawnLotto)).toBe(
-      PRIZES.FIFTH + PRIZES.FIRST
-    );
-  });
-
-  test("로또가 여러개 때 수익률 구하기", () => {
-    const lottos = [
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "6"]),
-      new Lotto(["1", "2", "3", "4", "5", "7"]), // 5등
-    ];
-    const drawnLotto = new DrawnLotto(["1", "2", "7", "8", "9", "10"], "11");
-    expect(LottoYieldCalculator.calculateYield(lottos, drawnLotto)).toBe(62.5);
+    const drawnLotto = {
+      getFullNumbers: () => ({
+        drawnLottoNumbers: [1, 2, 3, 4, 5, 6],
+        bonusNumber: 7,
+      }),
+    };
+    const result = calculator.getResult(lottosNumbers, drawnLotto);
+    expect(result.SECOND_PRIZE).toBe(1);
+    expect(result.FIRST_PRIZE).toBe(1);
   });
 });
