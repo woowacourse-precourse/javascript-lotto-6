c619b9642db587cc4daef1438124484850ca8481
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-08 17:50:11 +0900

    refactor: 예외 처리 따로 빼서 관리

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,15 +12,11 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
     Validation.validateNumbersLength(numbers);
     Validation.validateRepeatedNumbers(numbers);
 
   }
 
-  // TODO: 추가 기능 구현
   #printLottos(numbers){
     Console.print(`[${numbers}]\n`);
   }
