2f046c48c20a88119b8112eb5eaa9a6f30f57437
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 16:03:49 +0900

    refactor: 금액 유효성 검사에서 통일된 [ERROR] 문구를 개별 상황에 맞게 수정

--- src/validation/validateMoney.js
+++ src/validation/validateMoney.js
@@ -1,15 +1,15 @@
-import validateMoneyError from '../constants/error';
+import { MONEY_ERROR } from "../errorMessages/errorMessage";
 
 const regexOnlyNumbers = /^\d+$/;
 
 const validateMoney = (money) => {
-  if (!regexOnlyNumbers.test(money)) throw new Error('[ERROR]');
+  if (!regexOnlyNumbers.test(money)) throw new Error(MONEY_ERROR.type);
 
   const moneyToNumber = parseInt(money);
 
-  if (!moneyToNumber) throw new Error('[ERROR]');
-  if (moneyToNumber < 1000) throw new Error('[ERROR]');
-  if (moneyToNumber % 1000 !== 0) throw new Error('[ERROR]');
+  if (!moneyToNumber) throw new Error(MONEY_ERROR.none);
+  if (moneyToNumber < 1000) throw new Error(MONEY_ERROR.over1000);
+  if (moneyToNumber % 1000 !== 0) throw new Error(MONEY_ERROR.divide1000);
 };
 
 export default validateMoney;
