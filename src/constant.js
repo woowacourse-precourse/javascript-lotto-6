c0670285061a1ad4a93a19f9c24fe07452ceef09
Moon Sojeong <sjeong35@naver.com>
2023-11-08 18:26:54 +0900

    feat: 숫자를 상수로 정의하여 CONSTANT 객체 생성

---
+++ src/constant.js
@@ -0,0 +1,11 @@
+const CONSTANT = Object.freeze({
+    ZERO : 0,
+    ONE : 1,
+    MATCH_FIVE : 5,
+    NUMBER_MAX_LENGTH : 6,
+    NUMBER_MIN_RANGE : 1,
+    NUMBER_MAX_RANGE : 45,
+    THOUSAND : 1000,
+});
+
+export {CONSTANT};
