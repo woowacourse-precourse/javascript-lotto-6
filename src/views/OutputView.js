5035f0c3491ef7098340c5f2234d5608adaaf74f
Seongjin Hong <hongseongjin.to@gmail.com>
2023-11-08 23:26:10 +0900

    fix: 등수별 당첨 개수 및 수익률 출력양식 변경

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -11,14 +11,17 @@ class OutputView {
 
   static outputLottoGameResult(result) {
     Console.print(`\n${OUTPUT_MESSAGE.winningResultHeader}`);
-    Console.print(`${OUTPUT_MESSAGE.winningResult.fifthPrize} - ${result.fifthPrize}`);
-    Console.print(`${OUTPUT_MESSAGE.winningResult.fourthPrize} - ${result.fourthPrize}`);
-    Console.print(`${OUTPUT_MESSAGE.winningResult.thirdPrize} - ${result.thirdPrize}`);
-    Console.print(`${OUTPUT_MESSAGE.winningResult.secondPrize} - ${result.secondPrize}`);
-    Console.print(`${OUTPUT_MESSAGE.winningResult.firstPrize} - ${result.firstPrize}`);
-    Console.print(
-      `${OUTPUT_MESSAGE.profitRatioHeader}${result.profitRate}${OUTPUT_MESSAGE.profitRatioFooter}`,
-    );
+    Console.print(`${OUTPUT_MESSAGE.winningResult.fifthPrize} - ${result.fifthPrize}개`);
+    Console.print(`${OUTPUT_MESSAGE.winningResult.fourthPrize} - ${result.fourthPrize}개`);
+    Console.print(`${OUTPUT_MESSAGE.winningResult.thirdPrize} - ${result.thirdPrize}개`);
+    Console.print(`${OUTPUT_MESSAGE.winningResult.secondPrize} - ${result.secondPrize}개`);
+    Console.print(`${OUTPUT_MESSAGE.winningResult.firstPrize} - ${result.firstPrize}개`);
+    if (result.profitRate === '')
+      Console.print(`${OUTPUT_MESSAGE.profitRatioHeader}0.0%${OUTPUT_MESSAGE.profitRatioFooter}`);
+    if (result.profitRate !== '')
+      Console.print(
+        `${OUTPUT_MESSAGE.profitRatioHeader}${result.profitRate}${OUTPUT_MESSAGE.profitRatioFooter}`,
+      );
   }
 
   static outputMessage(message) {
