66444066ec29cd2da299ddd88801f584a6d917dc
ImxYJL <allensain14@gmail.com>
2023-11-08 15:09:41 +0900

    feat: 보너스 번호 입력 기능 구현

--- src/lotto/LottoGameHost.js
+++ src/lotto/LottoGameHost.js
@@ -12,6 +12,11 @@ class LottoGameHost {
     this.#isWinningNumberDuplicate,
   ];
 
+  #bonusNumberValidationList = [
+    (input) => Validator.isMatchingRegex(input, LOTTO_REGEX.bonusNumberInput),
+    (input) => this.#isBonusNumberDuplicate(Number(input)),
+  ];
+
   #isWinningNumberDuplicate(input) {
     const numbers = input.split(',').map(Number);
     const uniqueNumbers = new Set(numbers);
@@ -21,16 +26,32 @@ class LottoGameHost {
     }
   }
 
+  #isBonusNumberDuplicate(bonusNumber) {
+    if (this.#winningNumbers.includes(bonusNumber)) {
+      throw new Error(ERROR_MESSAGE.wrongBonusNumberInput);
+    }
+  }
+
   async setLottoWinningNumbers() {
     const winningNumbers = await getAndValidateInput(
       MESSAGE.winningNumbersInput,
       this.#winningNumbersValidationList,
     );
     this.#winningNumbers = winningNumbers.split(',').map(Number);
+
+    const bonusNumber = await getAndValidateInput(
+      MESSAGE.bonusNumberInput,
+      this.#bonusNumberValidationList,
+    );
+    this.#bonusNumber = Number(bonusNumber);
   }
 
   getWinningNumbers() {
     return [...this.#winningNumbers];
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
   }
 }
 
