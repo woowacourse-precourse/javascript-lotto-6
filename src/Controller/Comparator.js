8c2eff06beb49954da814596d97ff4cf692d443f
sunub <bsc5672@gmail.com>
2023-11-08 18:31:17 +0900

    refactor : Comparator의 전역 함수, 변수를 클래스 내부로 이전

--- src/Controller/Comparator.js
+++ src/Controller/Comparator.js
@@ -1,47 +1,52 @@
-import LottoTicket from '../Model/LottoTicket';
-
-const lottoTicket = LottoTicket.getInstance();
-
-function isBonusNumCase(matchesCount, lottoNum) {
-  const { bonusNum } = lottoTicket;
-  return matchesCount === 5 && lottoNum.includes(bonusNum);
-}
+import LottoTicket from '../Model/LottoTicket.js';
 
 class Comparator {
+  #lottoTicket;
+  #numOfWinningNumMatches;
   constructor() {
-    this.numOfWinningNumMatches = new Map();
+    this.#lottoTicket = LottoTicket.getInstance();
+    this.#numOfWinningNumMatches = new Map();
+  }
+
+  get numOfWinningNumMatches() {
+    return new Map(this.#numOfWinningNumMatches);
   }
 
   matchWinNumbersWithTicket() {
-    const { ticket } = lottoTicket;
+    const { ticket } = this.#lottoTicket;
     ticket.forEach((lotto) => {
       const matchesCount = this.countMatches(lotto.numbers);
 
       this.increaseMatchesCount(matchesCount);
       this.increaseBonusCount(matchesCount, lotto.numbers);
     });
-    return this.numOfWinningNumMatches;
+    return this.#numOfWinningNumMatches;
   }
 
   countMatches(lotto) {
-    const { winNums } = lottoTicket;
+    const { winNums } = this.#lottoTicket;
     return winNums.filter((number) => lotto.includes(number)).length;
   }
 
   increaseMatchesCount(matchesCount) {
-    this.numOfWinningNumMatches.set(
+    this.#numOfWinningNumMatches.set(
       matchesCount,
-      (this.numOfWinningNumMatches.get(matchesCount) || 0) + 1,
+      (this.#numOfWinningNumMatches.get(matchesCount) || 0) + 1,
     );
   }
 
   increaseBonusCount(matchesCount, lottoNums) {
-    if (isBonusNumCase(matchesCount, lottoNums)) {
-      this.numOfWinningNumMatches.set(
+    if (this.isBonusNumCase(matchesCount, lottoNums)) {
+      this.#numOfWinningNumMatches.set(
         'bonus',
-        (this.numOfWinningNumMatches.get('bonus') || 0) + 1,
+        (this.#numOfWinningNumMatches.get('bonus') || 0) + 1,
       );
     }
+  }
+
+  isBonusNumCase(matchesCount, lottoNum) {
+    const { bonusNum } = this.#lottoTicket;
+    return matchesCount === 5 && lottoNum.includes(bonusNum);
   }
 }
 
