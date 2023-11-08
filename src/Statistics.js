f0f84463fe9963ea1d01aac300d6df2ce22bc1eb
이호연 <ho991217@kakao.com>
2023-11-08 23:49:50 +0900

    docs: Statistics 주석 추가

--- src/Statistics.js
+++ src/Statistics.js
@@ -1,3 +1,4 @@
+import { LOTTO_PRICE } from './constants/number.js';
 import Message from './utils/Message.js';
 
 class Statistics {
@@ -12,11 +13,17 @@ class Statistics {
     this.#calculate();
   }
 
+  /**
+   * 통계를 출력하는 메서드
+   */
   show() {
     const profitRate = this.#getProfitRate();
     Message.showStatistics(this.#rankCount, profitRate);
   }
 
+  /**
+   * 등수 별 당첨 횟수를 계산하는 메서드
+   */
   #calculate() {
     this.results.forEach((result) => {
       const rank = result.getRank();
@@ -24,9 +31,13 @@ class Statistics {
     });
   }
 
+  /**
+   * 수익률을 계산하는 메서드
+   * @returns {number} 수익률
+   */
   #getProfitRate() {
     const totalPrize = this.results.reduce((acc, result) => acc + result.getPrize(), 0);
-    const totalPurchase = this.results.length * 1000;
+    const totalPurchase = this.results.length * LOTTO_PRICE;
 
     return (totalPrize / totalPurchase) * 100;
   }
