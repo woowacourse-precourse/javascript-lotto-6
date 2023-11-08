d1581fca99150ca4088ac39501aa7ea09d2a2167
yeong-hwan <jghff700@naver.com>
2023-11-08 15:47:42 +0900

    test: 메세지에 소괄호를 감싸주는 함수에 대한 테스트케이스 추가

--- __tests__/UtilsTest.js
+++ __tests__/UtilsTest.js
@@ -1,4 +1,5 @@
 import sortNumberArray from '../src/domain/utils/sortNumberArray';
+import wrapParenthesis from '../src/domain/utils/wrapParentheses';
 
 describe('Sorting Test', () => {
   test('숫자로 이루어진 배열 정렬 테스트', () => {
@@ -6,5 +7,14 @@ describe('Sorting Test', () => {
     const result = [-3, -1, 0, 5, 9];
 
     expect(sortNumberArray(input)).toEqual(result);
+  });
+});
+
+describe('String Test', () => {
+  test('괄호를 씌워주는 함수 테스트', () => {
+    const input = '내용물';
+    const result = '(내용물)';
+
+    expect(wrapParenthesis(input)).toEqual(result);
   });
 });
