d0cccbe799f8d34ddaa0e4fbe29cfc14d1364411
fecapark <fecapark@gmail.com>
2023-11-08 10:52:05 +0900

    feat: 로또 구입 금액 검증 로직 완성

--- src/core/const.js
+++ src/core/const.js
@@ -1,1 +1,11 @@
+// Error
 export const DEFAULT_ERROR_MESSAGE = "[ERROR]";
+
+// Money Value
+export const MONEY_UNIT = 1000;
+
+// Lotto Number
+export const LOTTO_NUMBER_RANGE = {
+  min: 1,
+  max: 45,
+};
--- src/utils/Assert.js
+++ src/utils/Assert.js
@@ -1,1 +1,58 @@
-export default class Assert {}
+//@ts-check
+
+import { LOTTO_NUMBER_RANGE, MONEY_UNIT } from "../core/const";
+import { PurchaseValueError } from "./Error";
+
+export default class Assert {
+  constructor() {}
+
+  assertPurchaseValue(money) {
+    if (!this.#isPositiveFiniteInteger(money))
+      throw new PurchaseValueError(PurchaseValueError.TYPE_NOT_NUMBER);
+    if (!this.#isEnoughMoneyToBuy(money))
+      throw new PurchaseValueError(PurchaseValueError.TYPE_NOT_PURCHASED);
+    if (!this.#isMoneyHasNoChange(money))
+      throw new PurchaseValueError(PurchaseValueError.TYPE_HAS_MONEY_CHANGE);
+  }
+
+  /**
+   *
+   * @param {number} money
+   * @returns {boolean}
+   */
+  #isEnoughMoneyToBuy(money) {
+    return money >= MONEY_UNIT;
+  }
+
+  /**
+   *
+   * @param {number} money
+   * @returns {boolean}
+   */
+  #isMoneyHasNoChange(money) {
+    return money % MONEY_UNIT === 0;
+  }
+
+  /**
+   *
+   * @param {number} num
+   * @returns {boolean}
+   */
+  #isPositiveFiniteInteger(num) {
+    if (!isFinite(num)) return false;
+    if (!Number.isInteger(num)) return false;
+    if (num < 0) return false;
+    return true;
+  }
+
+  /**
+   *
+   * @param {number} num
+   * @returns {boolean}
+   */
+  #isInRange(num) {
+    if (num < LOTTO_NUMBER_RANGE.min) return false;
+    if (LOTTO_NUMBER_RANGE.max < num) return false;
+    return true;
+  }
+}
