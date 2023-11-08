5e3891096c35bcf3fd0bc6122b258666bba9d61c
thgus5335 <thgus5335@naver.com>
2023-11-08 23:03:56 +0900

    feat: 로또 랜덤 번호 뽑기 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import inputView from './InputView.js';
 import validation from './validation.js';
 import Lotto from './Lotto.js';
+import { LOTTO } from './constant.js';
 
 class App {
   async play() {
@@ -11,6 +12,27 @@ class App {
   async gameStart() {
     const purchasePrice = await inputView.purchaseInput();
     validation.checkPurchasePrice(purchasePrice);
+    
+    this.pickLottoNum(purchasePrice);
+  }
+
+  pickLottoNum(purchasePrice){
+    const lottos = [];
+    const lottoCount = purchasePrice / LOTTO.UNIT;
+
+    while (lottos.length < lottoCount) {
+      const lottoNums = arraySort(this.pickNum());
+      lottos.push(new Lotto(lottoNums));
+    }
+  }
+
+  static arraySort(arr) {
+    return [...arr].sort((a, b) => a - b);
+  }
+
+  pickNum() {
+    const lottoNum = MissionUtils.Random.pickUniqueNumbersInRange(LOTTO.MIN_RANGE, LOTTO.MAX_RANGE, LOTTO.LENGTH);
+    return lottoNum;
   }
 }
 
