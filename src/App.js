4d36e712bcaeb1e1fdffa0a9b772d0487c800b91
Ellie-kang <kya6873@hanmail.net>
2023-11-08 20:43:25 +0900

    fix : 당첨통계 & 총 수익률 버그 수정

--- src/App.js
+++ src/App.js
@@ -20,13 +20,10 @@ class App {
     const bonusNumberInt = parseInt(bonusNumber);
 
     const result = this.calculateResult(lottos, winningNumbersArr, bonusNumberInt);
-    const totalPrize = Object.entries(result).reduce((sum, [match, count]) => {
-      const prize = this.calculatePrize(match);
-      return sum + prize * count;
-    }, 0);
 
-    const totalRateOfReturn = ((totalPrize / purchaseAmount) * 100)
-    MissionUtils.Console.print(`총 수익률은 ${totalRateOfReturn.toFixed(1)}%입니다.`);
+    MissionUtils.Console.print("\n 당첨 통계 \n --- ");
+
+    this.printResult(result, purchaseAmount);
   }
 
   // 로또 구입 금액 유효성 검사
@@ -47,9 +44,9 @@ class App {
   }
 
   printLottos(lottos) {
-    MissionUtils.Console.print(`${lottos.length}개를 구매했습니다.`)
+    MissionUtils.Console.print(`${lottos.length}개를 구매했습니다.`);
     lottos.forEach((lotto) => {
-      MissionUtils.Console.print(lotto.getNumbers());
+      MissionUtils.Console.print(`[${lotto.getNumbers().join(', ')}]`);
     });
   }
 
@@ -59,12 +56,12 @@ class App {
 
   calculateResult(lottos, winningNumbers, bonusNumber) {
     const results = {
-      3 : 0, 4 : 0, 5 : 0, 5.1 : 0, 6 : 0
+      3 : 0, 4 : 0, 5 : 0, 5.1 : 0, 6 : 0,
     };
 
     lottos.forEach((lotto) => {
       const matchedNumbers = lotto.matchedNumbers(winningNumbers);
-      const matchedBonus = lotto.matchedBonusNumber(winningNumbers, bonusNumber);
+      const matchedBonus = lotto.matchedBonusNumber(bonusNumber);
 
       if(matchedNumbers === 6) {
         results[6]++;
@@ -79,28 +76,29 @@ class App {
     return results;
   }
 
-  printResult(results) {
-    MissionUtils.Console.print("당첨 통계");
-    MissionUtils.Console.print("---");
+  printResult(results, purchaseAmount) {
+    MissionUtils.Console.print("3개 일치 (5,000원) - " + results[3] + "개");
+    MissionUtils.Console.print("4개 일치 (50,000원) - " + results[4] + "개");
+    MissionUtils.Console.print("5개 일치 (1,500,000원) - " + results[5] + "개");
+    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${results[5.1]}개`);
+    MissionUtils.Console.print("6개 일치 (2,000,000,000원) - " + results[6] + "개");
 
-    const resultEntries = Object.entries(results);
-    resultEntries.forEach(([match, count]) => {
-      const prize = this.calculatePrize(match);
-      MissionUtils.Console.print(`${match}개 일치 (${prize}) - ${count}개`);
-    });
-  }
+    const totalPrize =
+      results[3] * 5000 +
+      results[4] * 50000 +
+      results[5] * 1500000 +
+      results[5.1] * 30000000 +
+      results[6] * 2000000000;
+  
+      const totalRateOfReturn = ((totalPrize / purchaseAmount) * 100).toFixed(1);
+      MissionUtils.Console.print(`총 수익률은 ${totalRateOfReturn}%입니다.`);
 
-  calculatePrize(match) {
-    const prizeMap = {
-      3: "5,000원",
-      4: "50,000원",
-      5: "1,500,000원",
-      5.1: "30,000,000원",
-      6: "2,000,000,000원",
-    };
-    return prizeMap[match];
   }
 
 }
+
+
+
+
 
 export default App;
