8f23f7459da36e735f34ca5ab3fcaf53c3a01af7
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2023-11-08 11:22:17 +0900

    [Feat] 구입금액을 입력할 수 있다.
    * test: 구입금액 검증 테스트
    
    * feat: 구입 금액 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    this.inputAmount();
+  }
+
+  async inputAmount() {
+    const amount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    if (!this.validPurchase(amount))
+      throw new Error('[ERROR] invalid input amount.');
+  }
+
+  validPurchase(amount) {
+    if (isNaN(amount)) return false;
+    if (Number(amount) % 1000 !== 0) return false;
+
+    return true;
+  }
 }
 
 export default App;
