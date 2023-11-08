48587602696ae97d4f1a350ad26ae940b89bdb84
Moon Sojeong <sjeong35@naver.com>
2023-11-08 21:08:04 +0900

    refactor: 상수 값 추가

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,10 @@
 const CONSTANT = Object.freeze({
     ZERO : 0,
     ONE : 1,
+    MATCH_THREE : 3,
+    MATCH_FOUR : 4,
     MATCH_FIVE : 5,
+    MATCH_SIX : 6,
     NUMBER_MAX_LENGTH : 6,
     NUMBER_MIN_RANGE : 1,
     NUMBER_MAX_RANGE : 45,
