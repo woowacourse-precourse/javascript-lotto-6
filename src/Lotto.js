2b9c7d3189128a5cf45cf921ca6a29cd7569d2cb
parkblo <badmuxi@gmail.com>
2023-11-08 21:42:17 +0900

    style: 코드 컨벤션 적용

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,16 +12,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    
+
     if (new Set(numbers).size !== numbers.length) {
       throw new Error("[ERROR] 로또 번호는 중복되어서는 안됩니다.");
     }
 
-    numbers.forEach(number => {
+    numbers.forEach((number) => {
       if (isNaN(number)) {
-        throw new Error("[ERROR] 로또 번호가 올바른 숫자 형식이 아닙니다.")
+        throw new Error("[ERROR] 로또 번호가 올바른 숫자 형식이 아닙니다.");
       }
-    })
+    });
   }
 
   printNumbers() {
@@ -29,19 +29,26 @@ class Lotto {
   }
 
   #ranking(winningCount, doesBonusMatch) {
-    if (winningCount == 3) { return 5; }
-    else if (winningCount == 4) { return 4; }
-    else if (winningCount == 5 && !doesBonusMatch) { return 3; }
-    else if (winningCount == 5 && doesBonusMatch) { return 2; }
-    else if (winningCount == 6) { return 1;}
-    else { return 0; }
+    if (winningCount == 3) {
+      return 5;
+    } else if (winningCount == 4) {
+      return 4;
+    } else if (winningCount == 5 && !doesBonusMatch) {
+      return 3;
+    } else if (winningCount == 5 && doesBonusMatch) {
+      return 2;
+    } else if (winningCount == 6) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
-  confirmWinning(winningNumbers,winningBonusNumber) {
+  confirmWinning(winningNumbers, winningBonusNumber) {
     let winningCount = 0;
     let doesBonusMatch = false;
 
-    this.#numbers.forEach(number => {
+    this.#numbers.forEach((number) => {
       if (winningNumbers.includes(number)) {
         winningCount += 1;
       }
@@ -49,7 +56,7 @@ class Lotto {
       if (number == winningBonusNumber) {
         doesBonusMatch = true;
       }
-    })
+    });
 
     return this.#ranking(winningCount, doesBonusMatch);
   }
--- src/Util.js
+++ src/Util.js
@@ -1,39 +1,40 @@
-export function validatePurchasePrice(price,basedAmount) {
-    if (isNaN(price)) {
-        throw new Error("[ERROR] 구매 금액이 올바른 숫자 형식이 아닙니다.")
-    }
-    if (price % basedAmount != 0) {
-        throw new Error("[ERROR] 구매 금액은 1,000원 단위 입니다.")
-    }
-};
+export function validatePurchasePrice(price, basedAmount) {
+  if (isNaN(price)) {
+    throw new Error("[ERROR] 구매 금액이 올바른 숫자 형식이 아닙니다.");
+  }
+  if (price % basedAmount != 0) {
+    throw new Error("[ERROR] 구매 금액은 1,000원 단위 입니다.");
+  }
+}
 
-export function validateLottoNumbers(numbers,bonusNumber) {
-    if (numbers.length != 6) {
-        throw new Error("[ERROR] 당첨 번호는 6개여야 합니다.")
-    }
+export function validateLottoNumbers(numbers, bonusNumber) {
+  if (numbers.length != 6) {
+    throw new Error("[ERROR] 당첨 번호는 6개여야 합니다.");
+  }
 
-    numbers.forEach(number => {
-        if (isNaN(number)) {
-            throw new Error("[ERROR] 당첨 번호가 올바른 숫자 형식이 아닙니다.")
-        }
-        if (number < 1 && number > 45) {
-            throw new Error("[ERROR] 당첨 번호는 1~45 사이의 번호여야 합니다.")
-        }
-        
-    })
+  numbers.forEach((number) => {
+    if (isNaN(number)) {
+      throw new Error("[ERROR] 당첨 번호가 올바른 숫자 형식이 아닙니다.");
+    }
+    if (number < 1 && number > 45) {
+      throw new Error("[ERROR] 당첨 번호는 1~45 사이의 번호여야 합니다.");
+    }
+  });
 
-    if (isNaN(bonusNumber)) {
-        throw new Error("[ERROR] 보너스 당첨 번호가 올바른 숫자 형식이 아닙니다.")
-    }
-    if (bonusNumber < 1 || bonusNumber > 45) {
-        throw new Error("[ERROR] 보너스 당첨 번호는 1~45 사이의 번호여야 합니다.")
-    }
-    if (numbers.includes(bonusNumber)) {
-        throw new Error("[ERROR] 보너스 당첨 번호는 기존 6개 로또 번호와 달라야 합니다.")
-    }
+  if (isNaN(bonusNumber)) {
+    throw new Error("[ERROR] 보너스 당첨 번호가 올바른 숫자 형식이 아닙니다.");
+  }
+  if (bonusNumber < 1 || bonusNumber > 45) {
+    throw new Error("[ERROR] 보너스 당첨 번호는 1~45 사이의 번호여야 합니다.");
+  }
+  if (numbers.includes(bonusNumber)) {
+    throw new Error(
+      "[ERROR] 보너스 당첨 번호는 기존 6개 로또 번호와 달라야 합니다.",
+    );
+  }
 }
 
 export function create2DArray(rows, columns) {
-    let arr = Array.from(Array(rows), () => new Array(columns));
-    return arr;
-};
+  let arr = Array.from(Array(rows), () => new Array(columns));
+  return arr;
+}
