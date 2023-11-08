cf5df4c78b3ae4482a93bf63281da1ca0adce39d
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-08 17:57:40 +0900

    feat: check winning validate

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,34 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
+    Console.print(this.#numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const SET_NUMBERS = new Set(numbers);
+    if (SET_NUMBERS.size !== 6) {
+      throw new Error("[ERROR] 중복된 숫자를 가질 수 없습니다.");
+    }
+
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.");
+      }
+    });
   }
 
-  // TODO: 추가 기능 구현
+  get numbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
