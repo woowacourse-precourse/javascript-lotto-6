f4942bae67aeb205c64c6bae23072582f7b5f72f
youjeong Jeong <wjddbwjd1205@naver.com>
2023-11-08 21:19:37 +0900

    test: Lotto 클래스에 대한 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -14,5 +14,37 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호가 6개 이하인 경우 예외가 발생한다", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  })
+
+  test("로또 번호에 숫자가 아닌 입력이 있으면 예외가 발생한다", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, "H", 5, 6]);
+    }).toThrow("[ERROR]");
+  })
+
+  test("로또 번호에 정수가 아닌 입력이 있으면 예외가 발생한다", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 0.7, 5, 6]);
+    }).toThrow("[ERROR]");
+  })
+
+  test("로또 번호에 1부터 45 사이의 숫자가 아닌 입력이 있으면 예외가 발생한다", () => {
+    expect(() => {
+      new Lotto([1, 2, 33, , -5, 6]);
+    }).toThrow("[ERROR]");
+
+    expect(() => {
+      new Lotto([1, 2, 3, 50, 5, 6]);
+    }).toThrow("[ERROR]");
+  })
+
+  test("로또 번호를 오름차순으로 정렬", () => {
+    const lottoInstance = new Lotto([6, 5, 4, 3, 2, 1]);
+    const sortedNumbers = lottoInstance.getSortNumbers();
+    expect(sortedNumbers).toEqual([1, 2, 3, 4, 5, 6]);
+  })
 });
