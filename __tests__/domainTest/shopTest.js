2a63653af40aa26e159e25e03f099fe4af6bd86c
apfp77 <apfp77@naver.com>
2023-11-08 20:33:51 +0900

    test(shop): 테스트 함수 추가
    - 판매함수 반환값 타입 체크
    - 판매함수 원소 타입 체크
    - 로또 번호에 중복값이 있는지 확인

--- __tests__/domainTest/shopTest.js
+++ __tests__/domainTest/shopTest.js
@@ -1,10 +1,21 @@
-/* eslint-disable */
-
+import Lotto from '../../src/Lotto';
 import shop from '../../src/domain/shop';
 
 describe('shop 테스트', () => {
-  test('판매 테스트', () => {
-    const money = 3000;
-    shop.lotterySales(money);
+  const money = 3000;
+  const lottos = shop.lotterySales(money);
+
+  test('판매함수 반환값이 배열인지 체크', () => {
+    expect(Array.isArray(lottos)).toBe(true);
+  });
+
+  test('판매함수의 반환값의 원소들의 타입이 Lotto클래스인지 체크', () => {
+    lottos.forEach((lotto) => expect(lotto).toBeInstanceOf(Lotto));
+  });
+
+  test('로또 뽑은 수 중에 중복이 있는지 테스트', () => {
+    const random = shop.drawLottoNumbers();
+    const set = new Set(random);
+    expect(set.size).toBe(random.length);
   });
 });
