0a94c1314e3867ed3d79f64a1cbdda0999b0e49e
Dawon00 <dawon000203@gmail.com>
2023-11-08 16:27:18 +0900

    Refactor: 코드 단순화

--- src/App.js
+++ src/App.js
@@ -33,9 +33,7 @@ class App {
                 const purchaseAmount = await Console.readLineAsync(
                     "구입금액을 입력해 주세요.\n"
                 );
-                if (!new Purchase(purchaseAmount)) {
-                    throw new Error("[ERROR] 유효한 구입금액을 입력해주세요.");
-                }
+                new Purchase(purchaseAmount);
                 return purchaseAmount;
             } catch (error) {
                 Console.print(error.message);
@@ -77,9 +75,7 @@ class App {
                 const winningNumbers = await Console.readLineAsync(
                     "당첨 번호를 입력해 주세요.\n"
                 ).then((numbers) => numbers.split(",").map(Number));
-                if (!new Lotto(winningNumbers)) {
-                    throw new Error("[ERROR] 유효한 당첨 번호를 입력해주세요.");
-                }
+                new Lotto(winningNumbers);
                 return winningNumbers;
             } catch (error) {
                 Console.print(error.message);
@@ -90,16 +86,10 @@ class App {
     async inputBonusNumber(winningNumbers) {
         while (true) {
             try {
-                console.log(winningNumbers);
                 const bonusNumber = await Console.readLineAsync(
                     "보너스 번호를 입력해 주세요.\n"
                 ).then(Number);
-                console.log(bonusNumber);
-                if (!new WinningNumbers(winningNumbers, bonusNumber)) {
-                    throw new Error(
-                        "[ERROR] 유효한 보너스 번호를 입력해주세요."
-                    );
-                }
+                new WinningNumbers(winningNumbers, bonusNumber);
                 return bonusNumber;
             } catch (error) {
                 Console.print(error.message);
