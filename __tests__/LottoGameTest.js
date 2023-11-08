e6c9307305c4e223f4486e7e483da88754255430
publdaze <publdaze@naver.com>
2023-11-08 16:55:26 +0900

    test: LottoGame - purchaseLotto 메소드 테스트

---
+++ __tests__/LottoGameTest.js
@@ -0,0 +1,47 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import LottoGame from '../src/LottoGame.js';
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => acc.mockReturnValueOnce(number), Random.pickUniqueNumbersInRange);
+};
+
+describe('로또 게임 클래스 테스트', () => {
+  let lottoGame;
+
+  beforeEach(() => {
+    lottoGame = new LottoGame();
+  });
+
+  test('로또를 구입하면, 구입한 개수만큼의 로또가 발행된다. - purchaseLotto()', async () => {
+    const MOCK_PURCHASE_AMOUNT = ['5000'];
+    const RANDOM_NUMBERS = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
+
+    mockQuestions(MOCK_PURCHASE_AMOUNT);
+    mockRandoms(RANDOM_NUMBERS);
+
+    await lottoGame.inputPurchaseAmount();
+    lottoGame.purchaseLotto();
+
+    expect(Random.pickUniqueNumbersInRange).toHaveBeenCalledTimes(5);
+  });
+});
