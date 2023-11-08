cbe205dcac157abdb0450919e4cb35c19e9e6291
useon <yoosk4986@naver.com>
2023-11-08 16:20:22 +0900

    refactor: 불필요한 필드를 삭제하고 클래스명,필드명을 수정한다

--- src/Issuer.js
+++ src/Issuer.js
@@ -1,45 +1,41 @@
 import RandomNumberGenerator from './utils/RandomNumberGenerator.js';
 
-class LottoTicket {
-  #purchaseAmount;
+class Issuer {
+  #lottoCount;
 
-  #totalIssueLotto;
-
-  #lottoList;
+  #tickets;
 
   constructor(inputValue) {
+    const purchaseAmount = Number(inputValue);
     this.#validate(inputValue);
-    this.#purchaseAmount = inputValue;
-    this.#totalIssueLotto = this.#purchaseAmount / 1000;
+    this.#lottoCount = purchaseAmount / 1000;
     this.#generate();
   }
 
-  #validate(inputValue) {
-    const valueToNumber = Number(inputValue);
-    if (Number.isNaN(valueToNumber)) {
+  #validate(purchaseAmount) {
+    if (Number.isNaN(purchaseAmount)) {
       throw new Error('[ERROR] 구매 금액은 숫자여야 합니다.');
     }
-    if (valueToNumber < 1000) {
+    if (purchaseAmount < 1000) {
       throw new Error('[ERROR] 최소 구매 금액은 1000원입니다.');
     }
-    if (valueToNumber % 1000 !== 0) {
+    if (purchaseAmount % 1000 !== 0) {
       throw new Error('[ERROR] 구매 금액은 1000원 단위여야 합니다.');
     }
-    this.#purchaseAmount = valueToNumber;
   }
 
   #generate() {
     const issuedLottoList = [];
-    while (issuedLottoList.length < this.#totalIssueLotto) {
+    while (issuedLottoList.length < this.#lottoCount) {
       const sortedRandomNumbers = RandomNumberGenerator().sort((a, b) => a - b);
       issuedLottoList.push(sortedRandomNumbers);
     }
-    this.#lottoList = issuedLottoList;
+    this.#tickets = issuedLottoList;
   }
 
-  get lottoList() {
-    return this.#lottoList;
+  get tickets() {
+    return this.#tickets;
   }
 }
 
-export default LottoTicket;
+export default Issuer;
