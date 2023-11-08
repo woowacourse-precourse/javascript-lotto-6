c13906114264ca5808c1a9f45a94886d58ac215d
yonainthefish <yonaisgood@gmail.com>
2023-11-08 23:43:19 +0900

    feat: 최종점검 및 readme 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import LottoController from './constroller/LottoController';
+
 class App {
-  async play() {}
+  constructor() {
+    this.lottoController = new LottoController();
+  }
+
+  async play() {
+    await this.lottoController.playGame();
+  }
 }
 
 export default App;
