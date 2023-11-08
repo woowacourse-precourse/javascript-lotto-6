d8af4ffbdc658e9141c5d551abb5bb66af069a19
bada308 <203190@jnu.ac.kr>
2023-11-08 15:57:27 +0900

    feat: Input 앞뒤 공백 문자 제거 - 폴더 이동    - from View to utils - 입력 공백 문자 제거

--- src/utils/Input.js
+++ src/utils/Input.js
@@ -1,20 +1,23 @@
 import { Console } from "@woowacourse/mission-utils";
-import MESSAGE from "../constants/MESSAGE";
+import MESSAGE from "../constants/MESSAGE.js";
 
 const Input = {
   async purchase() {
     const input = await Console.readLineAsync(MESSAGE.INPUT.PURCHASE_AMOUNT);
-    return input;
+    return input.trim();
   },
 
   async winningNumber() {
     const input = await Console.readLineAsync(MESSAGE.INPUT.WINNING_NUMBER);
-    return input.split(MESSAGE.SIGNAL.SEPERATOR);
+    return input
+      .trim()
+      .split(MESSAGE.SIGNAL.SEPERATOR)
+      .map((input) => input.trim());
   },
 
   async bonusNumber() {
     const input = await Console.readLineAsync(MESSAGE.INPUT.BONUS_NUMBER);
-    return input;
+    return input.trim();
   },
 };
 
