6c4718777da87c1c69b34441255cf45b32a551af
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-11-08 20:53:10 +0900

    test: Lotto 클래스에서 includes() 함수에 대한 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -2,34 +2,13 @@ import Lotto from '../src/Lotto.js';
 import { ERROR } from '../src/LottoMessage.js';
 
 describe('로또 클래스 테스트', () => {
-  describe('예외 관련 테스트', () => {
-    test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
-      expect(() => {
-        new Lotto([1, 2, 3, 4, 5, 6, 7]);
-      }).toThrow(ERROR.count);
+  describe('includes', () => {
+    const lotto = new Lotto([45, 1, 44, 10, 16, 19]);
+    test('주어진 숫자가 로또 번호 중에 있으면 true', () => {
+      expect(lotto.includes(45)).toBeTruthy();
     });
-    test('로또 번호의 개수가 6개보다 적으면 예외가 발생한다.', () => {
-      expect(() => {
-        new Lotto([1, 2, 3, 4, 5]);
-      }).toThrow(ERROR.count);
-    });
-
-    test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
-      expect(() => {
-        new Lotto([1, 2, 3, 4, 5, 5]);
-      }).toThrow(ERROR.unique);
-    });
-
-    test('1에서 45 사이가 아닌 숫자가 있으면 예외가 발생한다.', () => {
-      expect(() => {
-        new Lotto([0, 1, 2, 3, 4, 5]);
-      }).toThrow(ERROR.between);
-    });
-
-    test('예외가 발생하지 않는 경우', () => {
-      expect(() => {
-        new Lotto([1, 2, 3, 4, 5, 6]);
-      }).not.toThrow();
+    test('주어진 숫자가 로또 번호 중에 없으면 false', () => {
+      expect(lotto.includes(2)).toBeFalsy();
     });
   });
 
@@ -58,6 +37,34 @@ describe('로또 클래스 테스트', () => {
     });
     test('6개 일치하면 6이 나온다.', () => {
       expect(countMatchingWith([1, 2, 3, 4, 5, 6])).toBe(6);
+    });
+  });
+
+  describe('예외 관련 테스트', () => {
+    test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      }).toThrow(ERROR.count);
+    });
+    test('로또 번호의 개수가 6개보다 적으면 예외가 발생한다.', () => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5]);
+      }).toThrow(ERROR.count);
+    });
+    test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, 5]);
+      }).toThrow(ERROR.unique);
+    });
+    test('1에서 45 사이가 아닌 숫자가 있으면 예외가 발생한다.', () => {
+      expect(() => {
+        new Lotto([0, 1, 2, 3, 4, 5]);
+      }).toThrow(ERROR.between);
+    });
+    test('예외가 발생하지 않는 경우', () => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, 6]);
+      }).not.toThrow();
     });
   });
 });
--- __tests__/WinningResultsTest.js
+++ __tests__/WinningResultsTest.js
@@ -1,9 +1,6 @@
 import WinningResults from '../src/WinningResults';
 
 describe('당첨 결과 클래스 테스트', () => {
-  const winningResults = new WinningResults();
-  winningResults.saveResultBy(3);
-
   describe('총 수익률에 대한 테스트', () => {
     test('구입금액이 8000원이고 로또 당첨 결과가 5등 1개라면 수익률은 62.5%', () => {
       const winningResults = new WinningResults();
