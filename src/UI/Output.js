11a95e4071e348261b6c449d1ffe06688a79388c
youjeong Jeong <wjddbwjd1205@naver.com>
2023-11-08 21:46:00 +0900

    refactor: 상수를 사용하여 Output 클래스 리팩터링

--- src/UI/Output.js
+++ src/UI/Output.js
@@ -1,5 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const FIRST_PRIZE = '2000000000';
+const SECOND_PRIZE = '30000000';
+const THIRD_PRIZE = '1500000';
+const FOURTH_PRIZE = '50000';
+const FIFTH_PRIZE = '5000';
+const THREE_MATCH = 3;
+const FOUR_MATCH = 4;
+const FIVE_MATCH = 5;
+const SIX_MATCH = 6;
+const BONUS_MATCH_INDEX = 3;
+
 class Output {
   boughtTickets(lottoTickets, count) {
     Console.print(`${count}개를 구매했습니다.`);
@@ -9,12 +20,12 @@ class Output {
   }
 
   winningResults(winningResult) {
-    const prizeMoney = ['5,000', '50,000', '1,500,000', '30,000,000', '2,000,000,000'];
-    const matchCountList = [3, 4, 5, 5, 6];
+    const prizeMoney = [FIFTH_PRIZE, FOURTH_PRIZE, THIRD_PRIZE, SECOND_PRIZE, FIRST_PRIZE];
+    const matchCountList = [THREE_MATCH, FOUR_MATCH, FIVE_MATCH, FIVE_MATCH, SIX_MATCH];
     const reversedWinningResult = [...winningResult].reverse();
     reversedWinningResult.forEach((result, index) => {
       let bonusText = '';
-      if (index === 3) {
+      if (index === BONUS_MATCH_INDEX) {
         bonusText = ', 보너스 볼 일치';
       }
       Console.print(`${matchCountList[index]}개 일치${bonusText} (${prizeMoney[index]}원) - ${result}개`)
