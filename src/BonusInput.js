08cc915c96e0b250d9d61d1fb4291df2dc6de191
kwani6684 <jack0910@naver.com>
2023-11-08 21:10:08 +0900

    ✅test: add BonusInputTest

---
+++ src/BonusInput.js
@@ -0,0 +1,20 @@
+import ExceptionList from "./ExceptionList";
+
+ExceptionList
+
+class BonusInput {
+  bonus;
+  win;
+  constructor(bonus) {
+    this.validateBonusInput(bonus)
+    this.bonus = bonus;
+  }
+  validateBonusInput = (bonus) => {
+    let exception = new ExceptionList();
+    exception.noInputError(bonus);
+    exception.isNaNError(bonus);
+    exception.numberRangeError(bonus);
+  };
+}
+
+export default BonusInput;
