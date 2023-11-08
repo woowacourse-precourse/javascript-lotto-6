eda7e4eb4c9b1bf91ea45e2e5561f61992adea31
KangYunJi <dbswl701@ajou.ac.kr>
2023-11-08 16:45:05 +0900

    feat: 구입금액 입력 및 예외처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const money = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+
+    // 예외처리
+    if (isNaN(money)) throw new Error("[ERROR] 구입금액은 숫자여야 합니다.");
+    if (parseInt(money / 1000) !== money / 1000) throw new Error("[ERROR] 구입금액은 1000원 단위여야 합니다.");
+    if (money == 0) throw new Error("[ERROR] 구입금액은 0원 이상이어야 합니다.");
+  }
 }
 
 export default App;
