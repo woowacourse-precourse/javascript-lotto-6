d955cdb9b627e2fc6f69fa7201a38c517a559148
SungChul Hong <hsc6263@gmail.com>
2023-11-08 14:57:38 +0900

    test: 로또 클래스 단위 테스트 코드 수정
    수정내용:
    변경 전
    - [ERROR] 문구가 포함된지를 판별
    
    변경 후:
    - 해당 예외에 맞는 문구가 출력되는지를 판별

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,33 +1,34 @@
 import Lotto from '../src/models/Lotto';
+import { ErrorMessage } from '../src/constants/ErrorMessage';
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ErrorMessage.INVALID_LOTTO_NUMBERS_RANGE);
   });
 
   test('로또 번호의 개수가 6개보다 적으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ErrorMessage.INVALID_LOTTO_NUMBERS_RANGE);
   });
 
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ErrorMessage.INVALID_LOTTO_NUMBERS_UNIQUENESS);
   });
 
   test('로또 번호에 숫자가 아닌 입력이 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 'a', 3, 4, 5]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ErrorMessage.INVALID_LOTTO_NUMBERS_UNIT);
   });
 
   test('로또 번호가 1~45 사이의 숫자가 아닐 경우 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 60]);
-    }).toThrow('[ERROR]');
+    }).toThrow(ErrorMessage.INVALID_LOTTO_NUMBERS_UNIT);
   });
 });
