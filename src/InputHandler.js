d4737c40399d9482d9b10262354d1567ce2e4cd4
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-08 23:35:34 +0900

    feat: 로또 구매 금액이 유효하지 않는 경우 예외 발생 및 재입력 기능 구현

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -9,9 +9,25 @@ class InputHandler {
     this.#numOfLottos = 0;
   }
 
+  async #getUserInput(promptMessage, validateFunction) {
+    try {
+      const initialInput = await Console.readLineAsync(promptMessage);
+      const userInput = initialInput.trim();
+      validateFunction(userInput);
+
+      return userInput;
+    } catch (error) {
+      Console.print(error.message);
+
+      return this.#getUserInput(promptMessage, validateFunction);
+    }
+  }
+
   async inputAmount() {
-    const userAmount = await Console.readLineAsync(INPUT_MESSAGES.INPUT_AMOUNT);
-    LottoValidator.validateAmount(userAmount);
+    const userAmount = await this.#getUserInput(
+      INPUT_MESSAGES.INPUT_AMOUNT,
+      LottoValidator.validateAmount
+    );
     this.#numOfLottos = parseInt(userAmount / LOTTO_GAME.PRICE_UNIT);
 
     Console.print(INPUT_MESSAGES.PURCHASED_LOTTOS(this.#numOfLottos));
