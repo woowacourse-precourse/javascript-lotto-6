ceb5f3198014b1c8c46fc313deddb3b7f5588ba2
flowersayo <flowersayo0811@ewhain.net>
2023-11-08 09:46:34 +0900

    feat: 메시지 입력 InputView 추가

---
+++ src/Constant.js
@@ -0,0 +1,5 @@
+export const MESSAGE = {
+  GET_MONEY_INPUT: '구입금액을 입력해 주세요.',
+  GET_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.',
+  GET_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+};
---
+++ src/view/InputView.js
@@ -0,0 +1,26 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from '../Constant.js';
+
+const InputView = {
+  async readMoney() {
+    const money = await Console.readLineAsync(`${MESSAGE.GET_MONEY_INPUT}\n`);
+    return Number(money);
+  },
+  async readWinningNumbers() {
+    const readlottoNumbers = await Console.readLineAsync(
+      `${MESSAGE.GET_WINNING_NUMBERS}\n`
+    );
+
+    const lottoNumbers = readlottoNumbers
+      .split(',')
+      .map((num) => Number(num.trim()));
+
+    return lottoNumbers;
+  },
+  async readBonusNumber() {
+    const bonusNumber = await Console.readLine(MESSAGE.GET_BONUS_NUMBER);
+    return Number(bonusNumber);
+  },
+};
+
+export default InputView;
