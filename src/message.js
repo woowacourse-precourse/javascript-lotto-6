469d5d4f2717d199c1996b2875cb6dcf78d08f04
Moon Sojeong <sjeong35@naver.com>
2023-11-08 18:29:57 +0900

    refactor: 에러 메세지 출력 형식 통일

--- src/message.js
+++ src/message.js
@@ -7,7 +7,7 @@ const ERROR_MESSAGE = Object.freeze({
     LOTTO_EMPTY : '[ERROR] 당첨 번호를 입력하세요.',
     SIX_NUMBERS : '[ERROR] 당첨 번호는 6개여야 합니다.',
     LOTTO_RANGE : '[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.',
-    SAME_NUMBERS : '[ERROR] 중복되지 않는 숫자를 입력하세요',
+    SAME_NUMBERS : '[ERROR] 중복되지 않는 숫자를 입력하세요.',
 });
 
 const PRINT_MESSAGE = Object.freeze({
