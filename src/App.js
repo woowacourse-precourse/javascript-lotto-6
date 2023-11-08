a42a6aa4a60b437a870d658e5d3d88ab18536fc4
kim-young-dong <opa9904@gmail.com>
2023-11-08 17:37:37 +0900

    refactor: 당첨 결과 및 수익률 통계 출력 로직 리펙토링
    - 당첨 결과 출력 로직 분리
    - 반복문을 사용하는 것으로 반복 줄임

--- src/App.js
+++ src/App.js
@@ -43,8 +43,8 @@ class App {
     this.#buyLottos();
     await this.#inputWinningNumbers();
     await this.#inputBonusNumber();
-    this.setStatistics();
-    this.outputStatistics();
+    this.#setStatistics();
+    this.#outputStatistics();
   }
 
   async #inputPaymentAmount() {
@@ -109,60 +109,7 @@ class App {
     });
   }
 
-    try {
-      const bonusNumber =
-        await Console.readLineAsync('보너스 번호를 입력해 주세요.');
-      this.#validateBonusNumber(Number(bonusNumber));
-      this.#bonusNumber = Number(bonusNumber);
-    } catch (error) {
-      Console.print(error.message);
-      this.#inputBonusNumber();
-    }
-  }
-
-  #validatePaymentAmount(amount) {
-    if (!Number.isInteger(Number(amount))) {
-      throw new Error('[ERROR] 정수를 입력해주세요.');
-    }
-    if (amount % 1000 !== 0) {
-      throw new Error('[ERROR] 로또 구입 금액은 1000원 단위로만 가능합니다.');
-    }
-  }
-
-  #validateWinningNumber(winningNumbers) {
-    if (winningNumbers.length !== 6) {
-      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
-    }
-    if (
-      winningNumbers.some(number => number < MIN_NUMBER || number > MAX_NUMBER)
-    ) {
-      throw new Error('[ERROR] 당첨 번호는 1~45 사이여야 합니다.');
-    }
-    if (winningNumbers.some(number => !Number.isInteger(Number(number)))) {
-      throw new Error('[ERROR] 당첨 번호는 정수여야 합니다.');
-    }
-    if (
-      winningNumbers.some(
-        (number, index) => winningNumbers.indexOf(number) !== index
-      )
-    ) {
-      throw new Error('[ERROR] 당첨 번호는 중복될 수 없습니다.');
-    }
-  }
-
-  #validateBonusNumber(bonusNumber) {
-    if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
-      throw new Error('[ERROR] 보너스 번호는 1~45 사이여야 합니다.');
-    }
-    if (!Number.isInteger(Number(bonusNumber))) {
-      throw new Error('[ERROR] 보너스 번호는 정수여야 합니다.');
-    }
-    if (this.#winningNumbers.includes(bonusNumber)) {
-      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
-    }
-  }
-
-  setStatistics() {
+  #setStatistics() {
     this.#statistics = new LottoStatistics(
       this.#paymentAmount,
       this.#winningNumbers,
@@ -171,34 +118,28 @@ class App {
     );
   }
 
-  outputStatistics() {
-    const winningRate = this.#statistics.getWinningRate();
+  #outputWinningRate(winningRate) {
+    const rates = Object.keys(winningRate).reverse();
+    let matchedCount = 3;
+    rates.forEach(rate => {
+      const money = WINNING_MONEY[rate].toLocaleString();
+      if (rate === 'second') {
+        Console.print(
+          `5개 일치, 보너스 볼 일치 (${money}원) - ${winningRate.second}개`
+        );
+      } else {
+        Console.print(
+          `${matchedCount}개 일치 (${money}원) - ${winningRate[rate]}개`
+        );
+        matchedCount += 1;
+      }
+    });
+  }
+
+  #outputStatistics() {
     const earningRate = this.#statistics.getEarningRate();
-    Console.print(
-      `3개 일치 (${WINNING_MONEY.fifth.toLocaleString()}원) - ${
-        winningRate.fifth
-      }개`
-    );
-    Console.print(
-      `4개 일치 (${WINNING_MONEY.fourth.toLocaleString()}원) - ${
-        winningRate.fourth
-      }개`
-    );
-    Console.print(
-      `5개 일치 (${WINNING_MONEY.third.toLocaleString()}원) - ${
-        winningRate.third
-      }개`
-    );
-    Console.print(
-      `5개 일치, 보너스 볼 일치 (${WINNING_MONEY.second.toLocaleString()}원) - ${
-        winningRate.second
-      }개`
-    );
-    Console.print(
-      `6개 일치 (${WINNING_MONEY.first.toLocaleString()}원) - ${
-        winningRate.first
-      }개`
-    );
+    const winningRate = this.#statistics.getWinningRate();
+    this.#outputWinningRate(winningRate);
     Console.print(`총 수익률은 ${earningRate}%입니다.`);
   }
 }
