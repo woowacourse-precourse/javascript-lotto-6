99649c88467bfb29cfa497549eb08252a30ae007
sehyun <e6d1fe@gmail.com>
2023-11-08 11:59:12 +0900

    feat: 구입 금액 입력 다시 받는 기능 구현

---
+++ src/Input/tryEnterBonusNumber.js
@@ -0,0 +1,20 @@
+import enterPurchaseAmount from './enterPurchaseAmount.js';
+import { Console } from '@woowacourse/mission-utils';
+
+async function tryEnterBonusNumber() {
+  let purchaseAmount;
+
+  async function tryEnter() {
+    try {
+      purchaseAmount = await enterPurchaseAmount();
+      return purchaseAmount;
+    } catch (e) {
+      Console.print(e.message);
+      return tryEnter();
+    }
+  }
+
+  return tryEnter();
+}
+
+export default tryEnterBonusNumber;
