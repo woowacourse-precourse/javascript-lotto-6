aa71e4513faa0af12bed0fb80d54066e8fac6f28
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-11-08 23:20:28 +0900

    test: 로또 메서드 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -18,4 +18,21 @@ describe('로또 클래스 테스트', () => {
       new Lotto([0, 1, 3, 4, 45, 46]);
     }).toThrow('[ERROR]');
   });
+
+  test('contain 함수 테스트', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.contains(1)).toBeTruthy();
+    expect(lotto.contains(7)).toBeFalsy();
+  });
+
+  test('matchCount 함수 테스트', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const target = new Lotto([1, 2, 3, 7, 8, 9]);
+    expect(lotto.matchCount(target)).toBe(3);
+  });
+
+  test('toString 함수 테스트', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.toString()).toBe('[1, 2, 3, 4, 5, 6]');
+  });
 });
