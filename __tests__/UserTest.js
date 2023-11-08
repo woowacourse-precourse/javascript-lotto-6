7c8812c21c35da6d917d6bd0d2084081d2f34855
sihyun10 <hyhy200170@gmail.com>
2023-11-08 22:41:57 +0900

    test: 사용자 구입금액 입력 테스트 수정 Console.readLineAsync 로직에 대한 단위 테스트는 제외하다보니, 1,000원 미만 구입금액 입력 시 에러가 발생하는지 확인해보는 테스트를 작성해보았습니다.

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -7,20 +7,14 @@ describe('로또 구입금액 입력 테스트', () => {
     user = new User();
   });
 
-  test('1,000원 미만 구입금액 입력 시 에러가 발생한다.', () => {
-    const inputAmount = 500;
-    expect(() => user.calculateLottoCount(inputAmount)).toThrow('[ERROR]');
-  });
+  test('1,000원 미만 구입금액 입력 시 에러가 발생한다.', async () => {
+    user.calculateLottoCount = jest.fn().mockResolvedValue('500');
 
-  test('1,000원 이상 구입금액 입력 시 올바른 로또 발행 수가 계산된다.', () => {
-    const inputAmount = 3000;
-    const lottoCount = user.calculateLottoCount(inputAmount);
-    expect(lottoCount).toBe(3);
-  });
-
-  test('1,000원 이상이면서 1,000원으로 딱 떨어지지 않는 경우 계산이 정확히 이루어진다.', () => {
-    const inputAmount = 2500;
-    const lottoCount = user.calculateLottoCount(inputAmount);
-    expect(lottoCount).toBe(2);
+    try {
+      await user.calculateLottoCount();
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+    }
   });
 });
