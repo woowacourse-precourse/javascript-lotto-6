4984e3c39e8e1017d7e8b95a47cbda0895550d28
dev-yenya <dev.yenya@gmail.com>
2023-11-08 16:44:41 +0900

    ✨ feat(amount): Add functionality to handle lotto purchase amount
    Introduce a constant LOTTO_UNIT_PRICE to represent the unit price for a lotto.
    Modify the validateLottoAmount method to use the constant LOTTO_UNIT_PRICE.
    Create a new function calculateLottoCount to calculate the number of lotto tickets to be purchased.
    Implement the printLottoCount function to display the number of lotto tickets purchased.
    
    This enables the user to input the purchase amount for the lotto and receive the number of tickets purchased based on the input amount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
+const LOTTO_UNIT_PRICE = 1000;
+
 class App {
-  async play() {}
+  async getLottoAmount() {
+    const lottoAmount = parseInt(await Console.readLineAsync('구입금액을 입력해주세요.\n'));
+    return lottoAmount;
+  }
+
+  validateLottoAmount(lottoAmount) {
+    if (lottoAmount % 1000 !== 0) {
+      throw new Error('[ERROR] 구입금액은 1000원 단위로 입력되어야 합니다.');
+    }
+    return lottoAmount;
+  }
+
+  calculateLottoCount(lottoAmount) {
+    return lottoAmount / LOTTO_UNIT_PRICE;
+  }
+
+  printLottoCount(lottoCount) {
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+  }
+
+  async play() {
+    const lottoAmount = await this.getLottoAmount();
+    this.validateLottoAmount(lottoAmount);
+    const lottoCount = this.calculateLottoCount(lottoAmount);
+    this.printLottoCount(lottoCount);
+  }
 }
 
 export default App;
