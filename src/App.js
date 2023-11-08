f38bfe311f2c53145c99ca227598c78d7d7b5115
KangYunJi <dbswl701@ajou.ac.kr>
2023-11-08 18:25:12 +0900

    feat: 로또 구입금액에 해당하는 만큼 로또 발행

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ class App {
     if (isNaN(money)) throw new Error("[ERROR] 구입금액은 숫자여야 합니다.");
     if (parseInt(money / 1000) !== money / 1000) throw new Error("[ERROR] 구입금액은 1000원 단위여야 합니다.");
     if (money == 0) throw new Error("[ERROR] 구입금액은 0원 이상이어야 합니다.");
+
+    // 구입금액에 해당하는 만큼 로또 발행
+    let count = money / 1000;
+    MissionUtils.Console.print(`${count}개를 구매했습니다.`);
+    const lottoList = [];
+    while(count--) {
+      lottoList.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b));
+    }
+
+    console.log(lottoList);
   }
 }
 
