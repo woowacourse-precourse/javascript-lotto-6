7850d4fcb150ce9a41cf09135fa14d5c8c1390f5
jinsil <jinsil378900@gmail.com>
2023-11-08 20:12:06 +0900

    Feat: 로또 일치 개수 찾는 것 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,38 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../src/components/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+describe('로또 클래스 테스트', () => {
+  test('로또 번호가 주어진 다른 번호 배열과 0개 일치', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const otherNumbers = [7, 8, 9, 10, 11, 12];
+    const count = lotto.matchCount(otherNumbers);
+    expect(count).toBe(0);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+  test('로또 번호가 주어진 다른 번호 배열과 3개 일치', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const otherNumbers = [4, 5, 6, 7, 8, 9];
+    const count = lotto.matchCount(otherNumbers);
+    expect(count).toBe(3);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호가 주어진 다른 번호 배열과 6개 일치', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const otherNumbers = [1, 2, 3, 4, 5, 6];
+    const count = lotto.matchCount(otherNumbers);
+    expect(count).toBe(6);
+  });
+
+  test('로또 번호가 주어진 숫자를 포함하는 경우', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const number = 4;
+    const contains = lotto.contains(number);
+    expect(contains).toBe(true);
+  });
+
+  test('로또 번호가 주어진 숫자를 포함하지 않는 경우', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const number = 7;
+    const contains = lotto.contains(number);
+    expect(contains).toBe(false);
+  });
 });
