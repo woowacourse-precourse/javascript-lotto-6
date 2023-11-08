5e88df7e539982adf4966e017d183bb9b22dc043
Nevacat <nevacattery217@naver.com>
2023-11-08 17:28:31 +0900

    feat: Lotto 클래스를 이용한 validation 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { ERROR } from './Constant/Constants';
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,25 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ERROR.INPUT_WINNING_NUM);
+    }
+    numbers.forEach((num) => {
+      if (num < 1 || num > 45) {
+        throw new Error(ERROR.INPUT_RANGE_NUM);
+      }
+    });
+    if (new Set(numbers).size !== 6) {
+      throw new Error(ERROR.INPUT_DUPLICATE_NUM);
+    }
+    if (numbers.includes('')) {
+      throw new Error(ERROR.INPUT_EMPTY);
     }
   }
 
   // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
