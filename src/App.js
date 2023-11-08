e0a27e02b9e70c7698db495a24b663f3f930927f
JUMTBOX <yjey12@naver.com>
2023-11-08 23:11:09 +0900

    feat.(,): App.js, Lotto.js내의 기능 구현 >> App.js내 App class의 play() 메서드 구현: >> - 로또번호 생성 기능 generateNumber 메서드 구현현 >> - 한줄의 로또 번호에서 당첨 번호의 갯수 카운터 기능 checkNumbersHasWinNum 메서드 구현 >> - 총 발행한 로또에서 번호가 3개 이상 일치하는 로또 갯수를 반환하는 setCounter 메서드 구현 >> - 총 수익률을 계산하는 putOutYeild 메서드 구현 >> - 당첨 결과를 프린트하는 printResult 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,119 @@
-class App {
-  async play() {}
-}
+import { MissionUtils } from "@woowacourse/mission-utils";
 
+class App {
+  #prize = {
+    "3개": 5000,
+    "4개": 50000,
+    "5개": 1500000,
+    "5개-보너스": 30000000,
+    "6개": 2000000000,
+  };
+  #generatedNumbers = [];
+  #winningNums;
+  #bonusNum;
+
+  async play() {
+    try {
+      const QUESTION = await MissionUtils.Console.readLineAsync(
+        "구입금액을 입력해 주세요.\n"
+      );
+
+      if (QUESTION % 1000 !== 0) {
+        throw new Error("[ERROR] 구입금액은 천원 단위로 입력해주세요!");
+      }
+
+      let ticketNum = QUESTION / 1000;
+
+      MissionUtils.Console.print(`\n${ticketNum}개를 구매했습니다.`);
+
+      while (ticketNum > 0) {
+        const LOTTONUMS = this.generateNumber();
+        ticketNum === 1
+          ? MissionUtils.Console.print(`[${LOTTONUMS}]\n`)
+          : MissionUtils.Console.print(`[${LOTTONUMS}]`);
+        ticketNum -= 1;
+      }
+
+      this.#winningNums = await MissionUtils.Console.readLineAsync(
+        "당첨 번호를 입력해 주세요.\n"
+      );
+      this.#bonusNum = await MissionUtils.Console.readLineAsync(
+        "\n보너스 번호를 입력해 주세요.\n"
+      );
+
+      this.printResult(QUESTION);
+    } catch (err) {
+      MissionUtils.Console.print(`${err}`);
+    }
+  }
+  /**로또번호 생성 */
+  generateNumber() {
+    let rand = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+      (a, b) => a - b
+    );
+    rand = rand.toString().replaceAll(",", ", ").split(",");
+    this.#generatedNumbers.push(rand);
+    return rand;
+  }
+
+  /**한줄의 로또 번호에서 당첨 번호를 몇개 가지고 있는지 세는 기능*/
+  checkNumbersHasWinNum(winningNums, lottoNumbers) {
+    let count = 0;
+    lottoNumbers = lottoNumbers.map((el) => el.trim());
+    for (let num of winningNums) {
+      lottoNumbers.includes(num) && (count += 1);
+    }
+    if (count === 5 && lottoNumbers.includes(this.#bonusNum)) count = "_5";
+    return count;
+  }
+
+  /**총 발행한 로또에서 번호가 3개 이상 일치하는 로또가 몇개인지 세는 기능*/
+  setCounter() {
+    let counter = {
+      "3개": 0,
+      "4개": 0,
+      "5개": 0,
+      "5개-보너스": 0,
+      "6개": 0,
+    };
+    let winningNums = this.#winningNums.split(",");
+    for (let lottoNumbers of this.#generatedNumbers) {
+      let res = this.checkNumbersHasWinNum(winningNums, lottoNumbers);
+      Number(res) > 2 && counter[`${res}개`]++;
+      res === "_5" && counter["5개-보너스"]++;
+    }
+    return counter;
+  }
+
+  /**총 수익률을 계산하는 기능*/
+  putOutYeild(QUESTION) {
+    let counter = this.setCounter();
+    let overAll = 0;
+    for (let key in counter) {
+      overAll += this.#prize[key] * counter[key];
+    }
+    const YEILD = ((overAll / Number(QUESTION)) * 100).toFixed(1);
+    return [YEILD, counter];
+  }
+
+  /** 당첨 결과를 프린트하는 기능*/
+  printResult(QUESTION) {
+    const RES = this.putOutYeild(QUESTION);
+    let yeild = RES[0];
+    let counter = RES[1];
+    MissionUtils.Console.print("\n당첨 통계\n ---");
+    for (let key in counter) {
+      let text = `${key} 일치 (${this.#prize[key].toLocaleString(
+        "ko-KR"
+      )}원) - ${counter[key]}개`;
+      if (key.includes("보너스")) {
+        text = `5개 일치, 보너스 볼 일치 (${this.#prize[key].toLocaleString(
+          "ko-KR"
+        )}원) - ${counter[key]}개`;
+      }
+      MissionUtils.Console.print(text);
+    }
+    MissionUtils.Console.print(`총 수익률은 ${yeild}%입니다.`);
+  }
+}
 export default App;
--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,27 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    this.#checkDuplication(numbers);
+    this.#checkIsthereNoneDigit(numbers);
   }
 
   // TODO: 추가 기능 구현
+  /**중복 숫자 예외처리 */
+  #checkDuplication(numbers) {
+    let counter = {};
+    for (let el of numbers) {
+      counter[el] = (counter[el] || 0) + 1;
+    }
+    if (Object.values(counter).includes(2)) {
+      throw new Error("[ERROR] 로또 번호는 서로 중복이 아닌 숫자여야 합니다!");
+    }
+  }
+  #checkIsthereNoneDigit(numbers) {
+    for (let el of numbers) {
+      if (el.match(/D/g)) {
+        throw new Error("[ERROR] 로또 번호는 숫자로만 이루어져야 합니다!");
+      }
+    }
+  }
 }
-
 export default Lotto;
