d8bb014cc4fe474b69b0ae502d1e6beb9e713f12
Hchan2 <seongchan_@naver.com>
2023-11-08 21:36:06 +0900

    Feat: 로또 게임 결과 계산을 위한 상수 선언

---
+++ src/constant/winning.js
@@ -0,0 +1,7 @@
+export const LOTTO_RESULT_CHECK = {
+    three: { count: 0, winnings: 5000 },
+    four: { count: 0, winnings: 50000 },
+    five: { count: 0, winnings: 1500000 },
+    fiveBonus: { count: 0, winnings: 30000000 },
+    six: { count: 0, winnings: 2000000000 }
+};
