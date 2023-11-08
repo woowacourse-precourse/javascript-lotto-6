4a1b4cfdf8cccc8d0936b206d13a6d9344699bd9
irenee-14 <ireneee145@gmail.com>
2023-11-08 19:55:36 +0900

    feature: lotto 클래스 내에서 출력

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+import { Console } from "@woowacourse/mission-utils";
 class Lotto {
   #numbers;
 
@@ -8,21 +9,33 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.\n");
     }
 	for (let i = 0; i < numbers.length ; i++)
 	{
 		if (isNaN(numbers[i]))
-			throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+			throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.\n");
 		if (numbers.indexOf(numbers[i]) != numbers.lastIndexOf(numbers[i]))
-			throw new Error("[ERROR] 로또 번호는 중복되지 않은 숫자여야 합니다.");
+			throw new Error("[ERROR] 로또 번호는 중복되지 않은 숫자여야 합니다.\n");
 		if (numbers[i] < 1 || numbers[i] > 45)
-			throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
-		if (Number.isInteger(numbers[i]))
-			throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+			throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.\n");
+		if (!Number.isInteger(numbers[i]))
+			throw new Error("[ERROR] 로또 번호는 정수여야 합니다.\n");
 	}
   }
   // TODO: 추가 기능 구현
+  getNumbers()
+  {
+	return this.#numbers;
+  }
+
+  printNumbers()
+  {
+	for (let i = 0; i < this.#numbers.length; i++)
+		Console.print(this.#numbers[i]);
+        //Console.print(`[${this.#numbers[i].join(", ")}]`);
+		Console.print("");
+  }
 }
 
 export default Lotto;
