ac6571023f783c20e086ad5cb3e542e224be815e
pakxe <pigkill40@naver.com>
2023-11-08 16:41:47 +0900

    chore: 혼동을 줄 수 있는 테스트 문구 명확하게 수정

--- __tests__/utils/CalcMatchCountTest.js
+++ __tests__/utils/CalcMatchCountTest.js
@@ -1,6 +1,6 @@
 import calcMatchCount from '../../src/utils/calcMatchCount.js';
 
-describe('배열에 일치하는 요소의 개수를 반환하는 유틸 함수 테스트', () => {
+describe('두 배열에 일치하는 요소의 개수를 반환하는 유틸 함수 테스트', () => {
   test('테스트 1', () => {
     const targetArr = [3, 4, 5, 6, 7, 8];
     const arr = [1, 2, 3, 4, 5, 6]
