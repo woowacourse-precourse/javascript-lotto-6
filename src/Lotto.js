b75a7563956421f78c596ae79fc1dba5570d768c
jujube12 <gyubam1@gmail.com>
2023-11-08 21:47:25 +0900

    기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,40 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
+    let arr = numbers.map(Number)
+    if (arr.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    arr = [...new Set(arr)]
+    if (arr.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+    }
+    arr.map((a)=> {
+      if(a < 1 || a > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45사이의 값이어야 합니다.");
+      }
+    })
+    if (arr.indexOf(NaN) !== -1) {
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
   }
-
-  // TODO: 추가 기능 구현
+  
+  validateNumbers(numbers) {
+    let arr = numbers.map(Number)
+    arr = [...new Set(arr)]
+    if (arr.indexOf(NaN) !== -1) {
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
+    if (arr.length !== 7) {
+      throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+    }
+    arr.map((a)=> {
+      if(a < 1 || a > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45사이의 값이어야 합니다.");
+      }
+    })
+    return arr
+  }
 }
 
 export default Lotto;
