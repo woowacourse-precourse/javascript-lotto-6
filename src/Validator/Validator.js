494e020b64b0d69f1a62a3a7c602fb264d8a78b7
MAGHC <windowsapple95@gmail.com>
2023-11-08 12:58:21 +0900

    feat: 유저의 구매 관련 검증 함수를 하나로 묶는 함수

--- src/Validator/Validator.js
+++ src/Validator/Validator.js
@@ -5,6 +5,12 @@ import CustomError from '../CustomError/CustonError.js';
 // 추후 테스트하기 좋게 변경하기
 
 const Validator = class {
+  static checkPurchaseAmount(money) {
+    Validator.checkIsNaN(money);
+    Validator.checkPositiveInt(money);
+    Validator.checkMoneyUnit(money);
+  }
+
   static checkMoneyUnit(money) {
     if (+money % MONEY_UNIT !== 0) {
       throw new CustomError(ERROR_MEESAGE.NOT_VALID_MONEY);
