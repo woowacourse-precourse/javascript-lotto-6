98e75feb20aec369f96cd6dd4a30a7b05aa5e980
Brokyeom <goindol40@gmail.com>
2023-11-08 20:47:04 +0900

    refactor : 추상화한 함수 기반의 클래스 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,29 +1,74 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
+import { getUserInput } from "./utils/getUserInput";
+import { handleResult } from "./utils/handleResult.js";
 
 class App {
+  #lottoTickets = [];
+  #lottoCount = 0;
+
   async play() {
-    const purchaseAmount = await Console.readLineAsync(
-      "구입금액을 입력해 주세요. \n"
+    this.#lottoCount = await getUserInput(
+      "구입금액을 입력해 주세요.\n",
+      (answer) => {
+        return this.purchaseLotto(Number(answer));
+      }
     );
-    const lottoCount = this.calculateLottoCount(purchaseAmount);
-    Console.print(`${lottoCount}개를 구매했습니다.`);
-    const lottos = this.createLottoNumbers(lottoCount);
-    lottos.forEach((lotto) => Console.print(lotto.numbers));
-  }
 
-  calculateLottoCount(amount) {
-    if (amount % 1000 !== 0) {
-      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+    this.#lottoTickets = [];
+    for (let i = 0; i < this.#lottoCount; i++) {
+      this.#lottoTickets.push(this.createLottoNumbers());
     }
-    return amount / 1000;
+
+    Console.print(`\n${this.#lottoCount}개를 구매했습니다.`);
+    for (let lottoTicket of this.#lottoTickets) {
+      Console.print(`[${lottoTicket.sort((a, b) => a - b).join(", ")}]`);
+    }
+
+    const lotto = await getUserInput(
+      "\n당첨 번호를 입력해 주세요.\n",
+      (winningNumbers) =>
+        new Lotto(winningNumbers.split(",").map((number) => Number(number)))
+    );
+    await getUserInput("\n보너스 번호를 입력해 주세요.\n", (bonusNumber) =>
+      lotto.addBonusNumber(bonusNumber)
+    );
+
+    this.printResult(lotto);
   }
 
-  createLottoNumbers(count) {
-    return Array.from({ length: count }, () => {
-      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
-      return new Lotto(numbers);
+  purchaseLotto = (price) => {
+    if (price % 1000 !== 0)
+      throw new Error("[ERROR] 구입금액은 1000단위 숫자여야 합니다.");
+    return price / 1000;
+  };
+
+  createLottoNumbers = () => {
+    return Random.pickUniqueNumbersInRange(1, 45, 6);
+  };
+
+  printResult(lotto) {
+    let result = {
+      "3개 일치 (5,000원)": 0,
+      "4개 일치 (50,000원)": 0,
+      "5개 일치 (1,500,000원)": 0,
+      "5개 일치, 보너스 볼 일치 (30,000,000원)": 0,
+      "6개 일치 (2,000,000,000원)": 0,
+    };
+
+    for (let ticket of this.#lottoTickets) {
+      const count = lotto.checkResult(ticket);
+      handleResult(result, count);
+    }
+
+    Console.print("\n당첨 통계");
+    let income = 0;
+    Object.entries(result).forEach((entry, index) => {
+      income += [5000, 50000, 1500000, 30000000, 2000000000][index] * entry[1];
+      Console.print(`${entry[0]} - ${entry[1]}개`);
     });
+
+    Console.print(`\n총 수익률은 ${income / (this.#lottoCount * 10)}%입니다.`);
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,30 +1,39 @@
 class Lotto {
   #numbers;
+  #bonusNumber;
 
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers.sort((a, b) => a - b);
   }
-
-  get numbers() {
-    return this.#numbers;
-  }
-
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-
     numbers.forEach((number) => {
-      if (number < 1 || number > 45) {
+      if (isNaN(number) || number > 45 || number < 0) {
         throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
       }
     });
-
-    const uniqueNumbers = new Set(numbers);
-    if (uniqueNumbers.size !== numbers.length) {
+    if (numbers.length != new Set(numbers).size) {
       throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
   }
+
+  addBonusNumber(number) {
+    if (isNaN(number) || number > 45 || number < 0)
+      throw new Error("[ERROR] 보너스 번호는 1-45 사이의 숫자여야 합니다.");
+    this.#bonusNumber = number;
+  }
+
+  checkResult(ticket) {
+    let count = 0;
+    for (let lottoNumber of ticket) {
+      if (this.#numbers.includes(lottoNumber)) count++;
+      else if (lottoNumber == this.#bonusNumber) count += 0.5;
+    }
+    return count;
+  }
 }
+
 export default Lotto;
