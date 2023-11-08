66af145a756dccf2c0c735f8b045fd53b57f4408
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-08 13:12:38 +0900

    refactor: LottoController의 일부 로직을 LottoService로 이동

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,140 +1,99 @@
+import { LOTTO, UTILS } from '../common/constants.js';
+
+import { printMessage } from '../common/utils.js';
 import Lotto from '../model/Lotto.js';
 import Money from '../model/Money.js';
+import BonusNumberValidator from '../service/BonusNumberValidator.js';
+import LottoService from '../service/LottoService.js';
+import WinningNumberValidator from '../service/WinningNumberValidator.js';
 import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
-import WinningNumberValidator from '../service/WinningNumberValidator.js';
-import BonusNumberValidator from '../service/BonusNumberValidator.js'
-import { LOTTO, UTILS, RANKING, STATISTICS } from '../common/constants.js';
-import { generateRandomNumber, printMessage } from '../common/utils.js';
 
 class LottoController {
   
   #money;
 
-  #lottos;
-
   #winningNumbers;
 
   #bonusNumber;
 
-  #ranking;
+  #lottoService;
 
-  #profit;
-
-  constructor () {
+  constructor() {
     this.#money = 0;
-    this.#lottos = [];
-    this.#winningNumbers = []; 
+    this.#winningNumbers = [];
     this.#bonusNumber = 0;
-    this.#ranking = new Map();
-    this.#profit = 0;
-    this.#initRankingMap();
-  };
+    this.#lottoService = new LottoService();
+  }
 
-  // eslint-disable-next-line max-lines-per-function
-  async start () {
+  async start() {
     try {
-      await this.#getInputMoney();
-      const lottoCount = this.#calculateLottoCount();
-      this.#createLottos(lottoCount);
-      this.#printLottos();
-      
+      await this.#initGame();
       await this.#runGame();
     } catch (error) {
       printMessage(error.message);
     }
-  };
+  }
 
-  async #getInputMoney () {
+  async #initGame() {
+    await this.#getInputMoney();
+
+    const lottoCount = this.#calculateLottoCount();
+    this.#lottoService.generateLottos(lottoCount);
+    
+    this.#printLottos();
+  }
+
+  async #getInputMoney() {
     this.#money = new Money(await InputView.getMoney());
   };
 
-  #calculateLottoCount () {
+  #calculateLottoCount() {
     const lottoCount = this.#money.getAmount() / LOTTO.money_unit;
     OutputView.printLottoCount(lottoCount);
     return lottoCount;
   };
 
-  #createLottos (lottoCount) {
-    for (let count = 0; count < lottoCount; count += 1) {
-      const generatedNumber = generateRandomNumber(LOTTO.min_number, LOTTO.max_number, LOTTO.max_match);
-      this.#lottos.push(new Lotto(generatedNumber));
-    }
-  };
-  
   #printLottos() {
-    OutputView.printLottoNumbers(this.#lottos);
+    OutputView.printLottoNumbers(this.#lottoService.getLottos());
   };
 
   async #runGame() {
     await this.#getWinningNumbers();
     await this.#getBonusNumber();
 
-    this.#compareLotto();
+    this.#lottoService.compareLotto(this.#winningNumbers, this.#bonusNumber);
     this.#printStatistics();
-    this.#calculateProfit();
-    this.#printProfit(); 
+    
+    this.#lottoService.calculateProfit(this.#money.getAmount());
+    this.#printProfit();
   };
 
-  async #getWinningNumbers () {
+  async #getWinningNumbers() {
     const inputNumber = await InputView.getWinningNumbers();
     const winningNumberValidator = new WinningNumberValidator(inputNumber);
     winningNumberValidator.validate();
-    
+
     const lotto = new Lotto(inputNumber.split(UTILS.comma).map(Number));
     this.#winningNumbers = lotto.getNumbers();
   };
 
-  async #getBonusNumber () {
+  async #getBonusNumber() {
     const inputNumber = await InputView.getBonusNumber();
-    const bonusNumbervalidator = new BonusNumberValidator(inputNumber);
-    bonusNumbervalidator.validateUnique(this.#winningNumbers);
-
-    this.#bonusNumber = bonusNumbervalidator.validate();
-  };
-
-  #compareLotto () {
-    this.#lottos.forEach(lotto => {
-      const matchedCount = this.#winningNumbers.filter(number => lotto.getNumbers().includes(number)).length;
-      const bonus = lotto.getNumbers().includes(this.#bonusNumber);
-
-      this.#calculateRanking(matchedCount, bonus);
-    })
-  };
-
-  #initRankingMap() {
-    Object.keys(RANKING).forEach(key => {
-      this.#ranking.set(key, 0)
-    })
-  };
-
-  #calculateRanking(matchedCount, bonus) {
-    Object.entries(RANKING).forEach(([key, rank]) => {
-      if (rank.match === matchedCount && rank.bonus === bonus) {
-        const existingValue = this.#ranking.get(key);
-        this.#ranking.set(key, existingValue + 1);
-      }
-    });
-  };
+    const bonusNumberValidator = new BonusNumberValidator(inputNumber);
   
-  #printStatistics () {
-    const statisticsArray = Array.from(this.#ranking).map(([key, count]) => RANKING[key].message + count + STATISTICS.count);
-    const statistics = statisticsArray.join(UTILS.line_break)
+    this.#bonusNumber = bonusNumberValidator.validate(this.#winningNumbers);
+  };
 
+  #printStatistics() {
+    const statisticsArray = this.#lottoService.getStatisticsArray();
+    const statistics = statisticsArray.join(UTILS.line_break);
+    
     OutputView.printStatistics(statistics);
   };
 
-  #calculateProfit() {
-    const moneyAmount = this.#money.getAmount();
-    const totalPrize = Array.from(this.#ranking).reduce((total, [key, count]) => {
-      const rank = RANKING[key];
-      return total + rank.prize * count;
-    }, 0);
-    this.#profit = (totalPrize / moneyAmount) * 100;
-  };  
-
   #printProfit() {
-    OutputView.printProfit(this.#profit);
+    OutputView.printProfit(this.#lottoService.getProfit());
   };
 };
 
---
+++ src/service/LottoService.js
@@ -0,0 +1,82 @@
+import { LOTTO, RANKING, STATISTICS } from '../common/constants.js';
+
+import { generateRandomNumber } from '../common/utils.js';
+import Lotto from '../model/Lotto.js';
+
+class LottoService {
+  
+  #lottos;
+  
+  #ranking;
+  
+  #profit;
+
+  constructor() {
+    this.#lottos = [];
+    this.#ranking = new Map();
+    this.#profit = 0;
+
+    this.#initRankingMap();
+  }
+
+  calculateProfit(moneyAmount) {
+    const totalPrize = this.#calculateTotalPrize();
+    this.#profit = (totalPrize / moneyAmount) * 100;
+  }
+
+  compareLotto(winningNumbers, bonusNumber) {
+    this.#lottos.forEach(lotto => {
+      const { matchedCount, bonus } = this.#calculateMatchAndBonus(lotto, winningNumbers, bonusNumber);
+      this.#calculateRanking(matchedCount, bonus);
+    });
+  }
+
+  generateLottos(lottoCount) {
+    for (let count = 0; count < lottoCount; count += 1) {
+      const generatedNumber = generateRandomNumber(LOTTO.min_number, LOTTO.max_number, LOTTO.max_match);
+      this.#lottos.push(new Lotto(generatedNumber));
+    }
+  }
+
+  getLottos() {
+    return this.#lottos;
+  }
+
+  getProfit() {
+    return this.#profit;
+  }
+
+  getStatisticsArray() {
+    return Array.from(this.#ranking).map(([key, count]) => RANKING[key].message + count + STATISTICS.count);
+  }
+
+  #initRankingMap() {
+    Object.keys(RANKING).forEach(key => {
+      this.#ranking.set(key, 0);
+    });
+  }
+
+  #calculateTotalPrize() {
+    return Array.from(this.#ranking).reduce((total, [key, count]) => {
+      const rank = RANKING[key];
+      return total + rank.prize * count;
+    }, 0);
+  }
+
+  #calculateMatchAndBonus(lotto, winningNumbers, bonusNumber) {
+    const matchedCount = winningNumbers.filter(number => lotto.getNumbers().includes(number)).length;
+    const bonus = lotto.getNumbers().includes(bonusNumber);
+    return { matchedCount, bonus };
+  }
+
+  #calculateRanking(matchedCount, bonus) {
+    Object.entries(RANKING).forEach(([key, rank]) => {
+      if (rank.match === matchedCount && rank.bonus === bonus) {
+        const existingValue = this.#ranking.get(key);
+        this.#ranking.set(key, existingValue + 1);
+      }
+    });
+  }
+}
+
+export default LottoService;
