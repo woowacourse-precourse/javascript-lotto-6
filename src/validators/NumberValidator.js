e13a944f8f06e64f0649f13255a2c2b626427007
이호연 <ho991217@kakao.com>
2023-11-08 23:48:27 +0900

    docs: NumberValidator 주석 추가

--- src/validators/NumberValidator.js
+++ src/validators/NumberValidator.js
@@ -16,6 +16,12 @@ class NumberValidator {
     return TypeValidator.isNumber(value) && value % divisor === 0;
   }
 
+  /**
+   * value가 from과 to 사이의 정수인지 검사하는 메서드
+   * @param {number} value 검사할 값
+   * @param {{ from: number, to: number }} range 범위의 시작과 범위의 끝
+   * @returns {boolean} value가 from과 to 사이의 정수인지 여부
+   */
   static isInRange(value, { from, to }) {
     return TypeValidator.isNumber(value) && value >= from && value <= to;
   }
