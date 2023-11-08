a1794203d23aaffe46a8d94eaaae225c031b3196
byunjiin <jibyun5808@naver.com>
2023-11-08 09:07:19 +0900

    feat: 기능7. 수익률

--- src/output/RatePrint.js
+++ src/output/RatePrint.js
@@ -1,6 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 //기능7. 수익률
 export function rateprint(output, buyinput) {
+  // prettier-ignore
   Console.print(
     `총 수익률은 ${
       Math.round(
@@ -9,10 +10,5 @@ export function rateprint(output, buyinput) {
           1500000 * output['3'] +
           30000000 * output['2'] +
           2000000000 * output['1']) /
-          (buyinput * 1000)) *
-          1000,
-        2,
-      ) / 10
-    }%입니다.`,
-  );
+          (buyinput * 1000)) *1000,2) / 10}%입니다.`);
 }
