2ad4cb55489480f20e0becc7a99a172db1260b57
yonainthefish <yonaisgood@gmail.com>
2023-11-08 23:34:01 +0900

    feat: 에러메세지 및 필요수 상수화

---
+++ src/constants/Constants.js
@@ -0,0 +1,11 @@
+export const LOTTO_START = 1;
+export const LOTTO_END = 45;
+export const LOTTO_LENGTH = 6;
+export const LOTTO_PRICE = 1000;
+export const WINNIG_PROFITS = {
+  3: 5000,
+  4: 50000,
+  5: 1500000,
+  bonus: 30000000,
+  6: 2000000000,
+};
