66dc0696e9c6b270b1ca50eaa1cca58cb0152e32
junghanlee <dlwjdgks126@hansung.ac.kr>
2023-11-08 17:46:15 +0900

    로또 생성 수정 및
    구매 수 출력, 당첨 번호 입력

--- src/App.js
+++ src/App.js
@@ -3,27 +3,37 @@ import {INPUT, OUTPUT, OUTPUT_ERROR} from "./text.js";
 import Lotto from "./Lotto.js";
 class App {
   constructor() {
+    this.purchase=0;
     this.lotto = [];
+    this.winning_num = [];
   }
   async play() {
-    const PURCHASE_AMOUNT = await this.inputPurchaseAmount();
-    await this.generateLottos(PURCHASE_AMOUNT);
+    await this.inputPurchaseAmount();
+    await this.generateLottos();
+    await this.inputWinningNum();
   }
   async inputPurchaseAmount() {
     const MONEY = await Console.readLineAsync(INPUT.input_money);
     if (MONEY % 1000 !== 0) {
       throw new Error(OUTPUT_ERROR.money_err);
     }
-    return MONEY / 1000;
+    this.purchase=MONEY/1000;
   }
-  async generateLottos(PURCHASE_AMOUNT) {
-    for (let i =0; i < PURCHASE_AMOUNT; i++) {
+  async generateLottos() {
+    await this.printPurchaseLottoCount();
+    for (let i =0; i < this.purchase; i++) {
       const LOTTO_NUMS = Random.pickUniqueNumbersInRange(1,45,6);
       this.lotto.push(LOTTO_NUMS);
+      Console.print(LOTTO_NUMS);
     }
-    console.log(this.lotto)
   }
-
+  async printPurchaseLottoCount() {
+    Console.print(`\n${this.purchase}${OUTPUT.lottos_cnt}`);
+  }
+  async inputWinningNum() {
+    const USER_INPUT = (await Console.readLineAsync(`\n${INPUT.input_winning}\n`)).split(',');
+    this.winning_num = USER_INPUT;
+  }
 }
 
 export default App;
--- src/text.js
+++ src/text.js
@@ -1,5 +1,6 @@
 export const INPUT = {
   input_money: '구입금액을 입력해 주세요.\n',
+  input_winning: '당첨 번호를 입력해 주세요.',
 }
 export const OUTPUT = {
   lottos_cnt: '개를 구매했습니다.',
