436579a917db3f75c9bac57d14a82c2ba6a19f55
MinYoungWI <young1y@nate.com>
2023-11-08 18:59:05 +0900

    Feat: 구입 금액을 입력해주세요 출력 후 사용자에게 입력값 받기

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,8 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
+  purchaseAmount;
 
   constructor(numbers) {
     this.#validate(numbers);
@@ -13,6 +16,25 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
-}
+  async buyTicket() {
+    while (true) {
+      try {
+        this.purchaseAmount = await MissionUtils.Console.readLineAsync(
+          "구입 금액을 입력해 주세요."
+        );
+        const amount = parseInt(this.purchaseAmount);
 
+        if (amount % 1000 !== 0) {
+          throw new Error("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+        }
+
+        this.purchaseAmount = amount;
+        break;
+      } catch (error) {
+        console.error(error.message);
+        throw error;
+      }
+    }
+  }
+}
 export default Lotto;
