9a06bb358bd10c2ac265fad2e3e17d83a730cfaa
yu-retel <dbdudals100@naver.com>
2023-11-08 16:57:57 +0900

    feat(App): 전체적인 로또 실행구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,83 @@
+import InputView from './view/InputView.js'
+import OutputView from './view/OutputView.js';
+
+import Lotto from './Lotto.js';
+import LottoManager from './LottoManager.js';
+import PrizeMoneyManager from './PrizeMoneyManager.js';
+import LottoTypeConversion from './util/LottoTypeConversion.js';
+import Validator from './util/validator.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.LottoManager = new LottoManager();
+  }
+
+  async play() {
+    try {
+      const buyLotto = await InputView.buyLotto();
+      Validator.numberType(buyLotto);
+      Validator.buyLottoMinimumOrder(buyLotto);
+      Validator.buyLottoUnit(buyLotto);
+
+      return this.issueLotto(buyLotto);
+    } catch(error) {
+      OutputView.error(error);
+
+      return this.play();
+    }
+  }
+
+  issueLotto(buyLotto) {
+    const lottoTicket = this.LottoManager.lottoTicketExchange(buyLotto);
+    OutputView.buyLotto(lottoTicket);
+
+    this.LottoManager.issueLotto(Lotto.generateLottos(lottoTicket));
+    this.LottoManager.getLottos().map((lotto) => OutputView.issuedLotto(LottoTypeConversion.issuedLotto(lotto)));
+    
+    return this.getWinningNumbers(buyLotto);
+  }
+  
+  async getWinningNumbers(buyLotto) {
+    try {
+      const userWinningNumbers = await InputView.winningNumber();
+      Validator.winningNumbersType(userWinningNumbers);
+      Validator.commaSeparatedWinningNumbers(userWinningNumbers);
+
+      const winningNumbers = new Lotto(LottoTypeConversion.winningNumbers(userWinningNumbers)).getNumbers();
+
+      return this.getBounsNumber(buyLotto, winningNumbers);
+    } catch(error) {
+      OutputView.error(error);
+
+      return this.getWinningNumbers(buyLotto);
+    }
+  }
+
+  async getBounsNumber(buyLotto, winningNumbers) {
+    try {
+      const bonusNumber = await InputView.bonusNumber();
+      Validator.numberType(bonusNumber);
+      Lotto.validateLottoNumber(bonusNumber);
+      Lotto.validateBonusNumberInWinningNumber(winningNumbers, bonusNumber);
+
+      return this.result(buyLotto, winningNumbers, bonusNumber);
+    } catch(error) {
+      OutputView.error(error);
+
+      return this.getBounsNumber(buyLotto, winningNumbers);
+    }
+  }
+
+  result(buyLotto, winningNumbers, bonusNumber) {
+    const prizeMoneyManager = new PrizeMoneyManager();
+
+    this.LottoManager.drawingLotto(LottoTypeConversion.Numbers(winningNumbers, bonusNumber));
+
+    const totalMoney = prizeMoneyManager.calculateTotalPrizeMoney(this.LottoManager.getRanks());
+    const earningsPercen = prizeMoneyManager.calculateEarningsPercent(totalMoney, buyLotto);
+
+    OutputView.result(this.LottoManager.getRanks(), LottoTypeConversion.NumberCommas(earningsPercen));
+  }
 }
 
 export default App;
