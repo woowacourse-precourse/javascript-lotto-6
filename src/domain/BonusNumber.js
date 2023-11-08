4cc788bfbefe61ef93dda487d7e61f09d696f3c5
HyunseokLEE <unducklee@gmail.com>
2023-11-08 22:05:52 +0900

    feat: 보너스 번호를 반환하는 기능인 BonusNumber 생성

---
+++ src/domain/BonusNumber.js
@@ -0,0 +1,13 @@
+class BonusNumber {
+  #bonusNumber;
+
+  constructor(bonusNumber) {
+    this.#bonusNumber = bonusNumber;
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+}
+
+export default BonusNumber;
