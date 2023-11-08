f3d91c8f3d39bca3c3bb009bcd85d5375381b18f
ymj307 <dymj307@gmail.com>
2023-11-08 14:38:48 +0900

    feat:write features for src/Lotto.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,55 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Input from './Input.js';
 import PurchasedLotto from './PurchasedLotto.js';
+import IssuedLotto from './IssuedLotto.js';
+import Lotto from './Lotto.js';
 
 class App {
   async play() {
-    const input = new Input();
+    try {
+      const input = new Input();
+      const issuedLottos = await this.buyAndIssueLottos(input);
+      const winningLotto = await this.getWinningLotto(input);
+    } catch (err) {
+      throw new Error(`[ERROR] ${err}`);
+    }
+  }
+
+  async getCashFromUser(input) {
     const cash = await input.simpleInput('금액을 입력해주세요\n');
+    return cash;
+  }
+
+  async issuedLotto(cash) {
     const purchasedLotto = new PurchasedLotto(cash);
-    MissionUtils.Console.print(purchasedLotto.numOfPurchasedLotto());
-    const winningLotto =
-      await input.inputChangeToArr('당첨번호를 입력해주세요.\n');
-    MissionUtils.Console.print(winningLotto);
+    const numOfPurchasedLotto = purchasedLotto.numOfPurchasedLotto();
+    const issuedLotto = new IssuedLotto(numOfPurchasedLotto);
+    return issuedLotto.lottoIssuance();
+  }
+
+  printIssuedLottos(lottos) {
+    const message = `${lottos.length}개를 구매했습니다.`;
+    MissionUtils.Console.print(message);
+    lottos.forEach((lotto) => MissionUtils.Console.print(lotto));
+  }
+
+  async buyAndIssueLottos(input) {
+    const cash = await this.getCashFromUser(input);
+    const returnLotto = await this.issueLottos(cash);
+    this.printIssuedLottos(returnLotto);
+    return returnLotto;
+  }
+
+  async getWinningNumbersFromUser(input) {
+    const winningLottoNumbers =
+      await input.inputChangeToArr('당첨번호를 입력해주세요\n');
+    return winningLottoNumbers;
+  }
+
+  async getWinningLotto(input) {
+    const winningLottoNumbers = await this.getWinningNumbersFromUser(input);
+    const winningLotto = new Lotto(winningLottoNumbers);
+    return winningLotto;
   }
 }
 
