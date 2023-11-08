696631adf0fd3da2f5891f6d657f956a64afb73d
usablechan <lch8910@naver.com>
2023-11-08 19:55:21 +0900

    feat : 당첨 번호 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App {
     const purchaseAmount = await this.getPurchaseAmount();
     const lottos = this.makeLottos(purchaseAmount);
     this.printLottos(lottos);
+    const winningNumbers = await this.getFullWinningNumbers();
   }
   async getPurchaseAmount() {
     const purchaseAmount = await MissionUtils.Console.readLineAsync(
@@ -13,7 +14,7 @@ class App {
     return parseInt(purchaseAmount);
   }
   makeLottos(purchaseAmount) {
-    const lottoCount = purchaseAmount / 1000;
+    const lottoCount = parseInt(purchaseAmount / 1000);
     const lottos = [];
     for (let i = 0; i < lottoCount; i++) {
       const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
@@ -27,6 +28,50 @@ class App {
     for (const lotto of lottos) {
       MissionUtils.Console.print(lotto.getNumbers());
     }
+  }
+  async getFullWinningNumbers() {
+    const formattingWinningNumbers = await this.getWinningNumbers();
+    const formattingFullWinningNumbers = await this.getBonusNumber(
+      formattingWinningNumbers
+    );
+
+    return formattingFullWinningNumbers;
+  }
+  async getWinningNumbers() {
+    const winningNumbers = await MissionUtils.Console.readLineAsync(
+      "지난 주 당첨 번호를 입력해 주세요.\n"
+    );
+    const formattingWinningNumbers = this.isValidWinningNumbers(
+      winningNumbers.split(",").map((num) => parseInt(num)),
+      6
+    );
+    return formattingWinningNumbers;
+  }
+  async getBonusNumber(formattingWinningNumbers) {
+    const bonusNumber = await MissionUtils.Console.readLineAsync(
+      "보너스 번호를 입력해 주세요.\n"
+    );
+    const formattingFullWinningNumbers = [
+      ...formattingWinningNumbers,
+      parseInt(bonusNumber),
+    ];
+    return this.isValidWinningNumbers(formattingFullWinningNumbers, 7);
+  }
+
+  isValidWinningNumbers(numbers, lotttoLength) {
+    if (numbers.every((num) => !isNaN(num)) === false) {
+      throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
+    if (numbers.length !== lotttoLength) {
+      throw new Error(`[ERROR] 로또 번호는 ${lotttoLength}개여야 합니다.`);
+    }
+    if (new Set(numbers).size !== lotttoLength) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+    if (numbers.some((num) => num < 1 || num > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1과 45 사이여야 합니다.");
+    }
+    return numbers;
   }
 }
 
