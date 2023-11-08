755524ecdd592c3021a5f9c5f883de8b0a46027c
[NDCC] Park Jee_Hoon <jh.m02h.park@misumi.co.kr>
2023-11-08 16:53:09 +0900

    test: 로또 번호에 관한 유효성 검사 테스트 항목 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,21 @@
 import Lotto from "../src/Lotto.js";
-import Lottos from "../src/model/Lottos.js";
-import WinningNumbers from "../src/model/WinningNumbers.js";
 
-describe("로또 클래스 테스트", () => {
+describe("Lotto 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 6개가 아닐 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
     }).toThrow("[ERROR]");
   });
   
