c6ed2cb21250af3024fa540e64d8be1dad894d24
gyucheolshin <nsfsgc2295@gmail.com>
2023-11-08 21:51:24 +0900

    feat : 당첨 내역 반환

--- src/App.js
+++ src/App.js
@@ -79,8 +79,73 @@ class App {
       matchLottoCount.push(lotto.matchLotto(winningNumber));
       matchLottoBonus.push(lotto.matchLottoBonus(inputBonusNumber));
     });
-    console.log(matchLottoCount);
-    console.log(matchLottoBonus);
+    this.lottoResult(matchLottoCount, matchLottoBonus);
+  }
+
+  async lottoResult(matchLottoCount, matchLottoBonus) {
+    // 3,4,5,5(보너스), 수익률 총 6개
+
+    const lottoResultArray = Array(6).fill(0);
+    for (let i = 0; i < matchLottoCount.length; i++) {
+      if (matchLottoCount[i] === LOTTO.SIX) {
+        lottoResultArray[LOTTO.FIRST_PLACE] += 1;
+      } else if (
+        matchLottoCount[i] === LOTTO.FIVE &&
+        matchLottoBonus[i] === true
+      ) {
+        lottoResultArray[LOTTO.SECOND_PLACE] += 1;
+      } else if (
+        matchLottoCount[i] === LOTTO.FIVE &&
+        matchLottoBonus[i] === false
+      ) {
+        lottoResultArray[LOTTO.THIRD_PLACE] += 1;
+      } else if (matchLottoCount[i] === LOTTO.FOUR) {
+        lottoResultArray[LOTTO.FOURTH_PLACE] += 1;
+      } else if (matchLottoCount[i] === LOTTO.THREE) {
+        lottoResultArray[LOTTO.FIFTH_PLACE] += 1;
+      }
+    }
+    this.totalLottoSum(lottoResultArray);
+  }
+
+  async totalLottoSum(lottoResultArray) {
+    let lottoSum = 0;
+    lottoSum += lottoResultArray[LOTTO.FIRST_PLACE] * LOTTO.FIRST_PLACE_PRIZE;
+    lottoSum += lottoResultArray[LOTTO.SECOND_PLACE] * LOTTO.SECOND_PLACE_PRIZE;
+    lottoSum += lottoResultArray[LOTTO.THIRD_PLACE] * LOTTO.THIRD_PLACE_PRIZE;
+    lottoSum += lottoResultArray[LOTTO.FOURTH_PLACE] * LOTTO.FOURTH_PLACE_PRIZE;
+    lottoSum += lottoResultArray[LOTTO.FIFTH_PLACE] * LOTTO.FIFTH_PLACE_PRIZE;
+    this.printLootoResult(lottoResultArray);
+  }
+
+  async printLootoResult(lottoResultArray) {
+    print(MESSAGE.WINNING_STATISTICS);
+    print(MESSAGE.UNDERLINE);
+    print(
+      `${MESSAGE.FIFTH_PLACE}${lottoResultArray[LOTTO.FIFTH_PLACE]}${
+        MESSAGE.EA
+      }`
+    );
+    print(
+      `${MESSAGE.FOURTH_PLACE}${lottoResultArray[LOTTO.FOURTH_PLACE]}${
+        MESSAGE.EA
+      }`
+    );
+    print(
+      `${MESSAGE.THIRD_PLACE}${lottoResultArray[LOTTO.THIRD_PLACE]}${
+        MESSAGE.EA
+      }`
+    );
+    print(
+      `${MESSAGE.SECOND_PLACE}${lottoResultArray[LOTTO.SECOND_PLACE]}${
+        MESSAGE.EA
+      }`
+    );
+    print(
+      `${MESSAGE.FIRST_PLACE}${lottoResultArray[LOTTO.FIRST_PLACE]}${
+        MESSAGE.EA
+      }`
+    );
   }
 }
 
