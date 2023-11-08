011e6a30264611ac960959e12da0c5f74b664173
seoyeon-jung <jungluchia1204@gmail.com>
2023-11-08 17:59:43 +0900

    refactor: Constan.js에 상수 추가

--- src/Constant.js
+++ src/Constant.js
@@ -1,11 +1,15 @@
-const GAME_MESSAGE = {
+export const RANGE_MIN = 1;
+export const RANGE_MAX = 45;
+export const LOTTO_LENGTH = 6;
+
+export const GAME_MESSAGE = {
   MONEY: "구입금액을 입력해주세요.\n",
   WINNING_NUM: "당첨 번호를 입력해주세요.\n",
   BONUS_NUM: "보너스 번호를 입력해주세요\n",
   STATIC: "당첨 통계\n---",
 };
 
-const ERROR_MESSAGE = {
+export const ERROR_MESSAGE = {
   NUMBER: "[ERROR] 숫자만 입력할 수 있습니다",
   MIN_MONEY: "[ERROR] 최소 구입 금액은 1000원입니다.",
   DIVISION: "[ERROR] 1000원 단위의 로또를 구입해주세요.",
@@ -13,8 +17,4 @@ const ERROR_MESSAGE = {
   DUPLICATE: "[ERROR] 로또 번호는 중복되지 않은 숫자로 이루어져야 합니다.",
   RANGE: "[ERROR] 로또 번호의 범위는 1~45입니다.",
   ALREADY_INCLUDE: "[ERROR] 보너스 번호가 이미 당첨 번호에 포함되어 있습니다.",
-  BONUS_NUMBER: "[ERROR] 보너스 번호는 숫자여야 합니다.",
-  BONUS_RANGE: "[ERROR] 보너스 번호의 범위는 1~45입니다.",
 };
-
-module.exports = { GAME_MESSAGE, ERROR_MESSAGE };
