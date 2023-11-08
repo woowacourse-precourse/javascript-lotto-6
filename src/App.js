c0dc60e3cc02c4484c76e566d68960f4365103ec
j-nary <dlwls0921@naver.com>
2023-11-08 21:36:57 +0900

    feat: 수익률 출력하기

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App {
     });
 
     this.printWinningList(winList);
+    this.printROI(winList, count);
   }
   async userPriceInput() {
     try {
@@ -155,6 +156,17 @@ class App {
     MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${winList[2]}개\n`);
     MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winList[3]}개\n`);
     MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${winList[4]}개\n`);
+  }
+
+  printROI(winList, count) {
+    const price = [5000, 50000, 1500000, 30000000, 2000000000];
+    let winnings = 0;
+    winList.map((w, idx) => {
+      winnings = winnings + w * price[idx];
+    })
+    count = count * 1000 / 100;
+    const result = winnings / count;
+    MissionUtils.Console.print('총 수익률은 ${Number(result.toFixed(2))%입니다.\n}');
   }
 }
   
