3c3dbb0b16befb4b8f322d03c7605a63a52dd337
Y.W KIM <kyw624@gmail.com>
2023-11-08 15:08:10 +0900

    Refactor: printResult 메서드의 통계 출력 방식 개선

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 
-import Game, { TICKET_PRICE } from './Game.js';
+import Game from './Game.js';
 import {
   validateAmountInput,
   validateWinningInput,
   validateBonusInput,
 } from './util/validateInput.js';
-
-export const MESSAGES = Object.freeze({
-  INPUT_AMOUNT: '구입금액을 입력해 주세요.\n',
-  INPUT_WINNING_NUMBERS: '\n당첨 번호를 입력해 주세요.\n',
-  INPUT_BONUS_NUMBER: '\n보너스 번호를 입력해 주세요.\n',
-
-  OUTPUT_BUY_TICKETS: (num) => `\n${num}개를 구매했습니다.`,
-  OUTPUT_RETURN: (num) => `총 수익률은 ${num}%입니다.`,
-});
+import { MESSAGES, RANK, TICKET_PRICE } from './util/constants.js';
 
 class App {
   async play() {
     try {
       const amount = await this.getAmountInput();
-
       const game = new Game(amount);
+
       game.getLottoTickets();
       game.printTickets();
 
@@ -66,17 +58,12 @@ class App {
   }
 
   printResult(result) {
-    const values = Object.values(result);
-
     Console.print('\n당첨 통계');
     Console.print('---');
-    Console.print(`3개 일치 (5,000원) - ${values[0].count}개`);
-    Console.print(`4개 일치 (50,000원) - ${values[1].count}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${values[2].count}개`);
-    Console.print(
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${values[3].count}개`
-    );
-    Console.print(`6개 일치 (2,000,000,000원) - ${values[4].count}개`);
+
+    for (let i = 5; i >= 1; i--) {
+      Console.print(MESSAGES.OUTPUT_RESULT_DETAILS(i, result[RANK[i]]));
+    }
   }
 }
 
