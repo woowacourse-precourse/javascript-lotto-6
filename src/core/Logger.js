7412208fba9ed6490c2019097594a460516b8b83
fecapark <fecapark@gmail.com>
2023-11-08 14:41:07 +0900

    fix: 파라미터 타입 오류 정정

--- src/core/Logger.js
+++ src/core/Logger.js
@@ -30,7 +30,6 @@ export default class Logger {
    *    five: number;
    *    bonusFive: number;
    *    six: number;
-   *    returnRate: number;
    * }} board
    * @param {number} returnRate
    */
