156673805ba3945cda4004b144bf78ca6ce3a77c
whai2 <whai2125@gmail.com>
2023-11-08 14:19:08 +0900

    test(results): 개별 출력에 대한 단위 테스트 진행

--- __tests__/ResultsTest.js
+++ __tests__/ResultsTest.js
@@ -44,6 +44,36 @@ describe("당첨 통계 테스트", () => {
     });
   });
 
+  test.each([
+    [[1, 2, 3, 7, 8, 9], "3개 일치 (5,000원) - 3개"],
+    [[7, 8, 9, 10, 1, 2], "4개 일치 (50,000원) - 1개"],
+    [[13, 14, 15, 16, 17, 19], "5개 일치 (1,500,000원) - 1개"],
+    [[19, 20, 21, 22, 23, 25], "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개"],
+    [[37, 38, 39, 40, 41, 42], "6개 일치 (2,000,000,000원) - 1개"]
+  ])("개별 당첨 출력을 확인한다.", (winningLotto, log) => {
+    //given
+    const logSpy = getLogSpy();
+
+    const lottos = [
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18],
+      [19, 20, 21, 22, 23, 24],
+      [25, 26, 27, 28, 29, 30],
+      [31, 32, 33, 34, 35, 36],
+      [37, 38, 39, 40, 41, 42],
+      [43, 44, 45, 1, 2, 3],
+    ];
+    const BONUS_NUMBER = 24;
+    const COUNTS = 8;
+
+    //when
+    new Results(lottos, winningLotto, BONUS_NUMBER, COUNTS)
+
+    //then
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
+
   test("총 수익률을 모두 출력한다.", () => {
     //given
     const logSpy = getLogSpy();
