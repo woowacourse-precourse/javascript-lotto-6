11642cf61fc75442c3ed4f7460ea71d0075be7d2
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 15:59:25 +0900

    refactor: 로또 유효성 검사에서 통일된 [ERROR] 문구를 개별 상황에 맞게 수정

--- src/validation/validateLotto.js
+++ src/validation/validateLotto.js
@@ -1,17 +1,19 @@
+import { LOTTO_ERROR } from "../errorMessages/errorMessage";
+
 const validateLotto = (lottos) => {
   const lottoToNumbers = lottos.map((lotto) => parseInt(lotto, 10));
 
-  if (!lottoToNumbers.length) throw new Error('[ERROR]');
+  if (!lottoToNumbers.length) throw new Error(LOTTO_ERROR.none);
 
-  if (lottoToNumbers.some(isNaN)) throw new Error('[ERROR]');
+  if (lottoToNumbers.some(isNaN)) throw new Error(LOTTO_ERROR.type);
 
-  if (lottoToNumbers.length > 6) throw new Error('[ERROR]');
+  if (lottoToNumbers.length > 6) throw new Error(LOTTO_ERROR.digits);
 
   if (lottoToNumbers.length !== new Set(lottoToNumbers).size)
-    throw new Error('[ERROR]');
+    throw new Error(LOTTO_ERROR.duplication);
 
   if (!lottoToNumbers.every((number) => number >= 1 && number <= 45))
-    throw new Error('[ERROR]');
+    throw new Error(LOTTO_ERROR.range);
 };
 
 export default validateLotto;
