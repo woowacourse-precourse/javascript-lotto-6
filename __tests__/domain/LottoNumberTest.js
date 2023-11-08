e1bc515c0b11f297120506a937f68f02f7397c32
pakxe <pigkill40@naver.com>
2023-11-08 16:38:03 +0900

    test: 로또 번호가 올바른 번호인지 테스트하는 코드 작성

---
+++ __tests__/domain/LottoNumberTest.js
@@ -0,0 +1,16 @@
+import ERROR from '../../src/constant/Error.js';
+import LOTTO from '../../src/constant/Lotto.js';
+import LottoNumber from '../../src/domain/LottoNumber.js';
+
+describe('로또 클래스 테스트', () => {
+  test('로또 번호에 nan이 있으면 예외가 발생한다.', () => {
+    expect(() => {
+    }).toThrow(ERROR.isNan);
+  });
+
+  test(`로또 번호가 ${LOTTO.minLottoNum} ~ ${LOTTO.maxLottoNum} 사이가 아니면 예외가 발생한다.`, () => {
+    expect(() => {
+      new LottoNumber(0);
+    }).toThrow(ERROR.range);
+  });
+});
