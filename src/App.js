a05c4bfc274dd35ea4461742ff3d4d9c38a4f4b5
kelvin3476 <winrain03226@gmail.com>
2023-11-08 14:39:52 +0900

    refactor(*): App.js 시작점 간결하게 리팩터링

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import LottoController from "./controller/LottoController";
-import LottoHandler from "./lottoHandler/LottoHandler";
-import View from "./view/View";
 
 class App {
-  #controller;
-
-  constructor() {
-    this.#controller = new LottoController({
-      view: new View(),
-      lottoHandler: new LottoHandler(),
-    });
-  }
-
   async play() {
-    await this.#controller.startLotto();
+    const lottoController = new LottoController();
+    await lottoController.startLotto();
   }
 }
 
