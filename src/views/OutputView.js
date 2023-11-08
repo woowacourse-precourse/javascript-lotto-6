4febca5d65fe7d06cfa04b5a24136fc351b3b94d
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-08 16:58:54 +0900

    feat: OutputView 구현
    메서드:
    - printPayment
        - printAmount
        - printLottos
    - printStatics
        - printWinningResult
        - printProfitRatio

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,21 +1,46 @@
+import { Console } from "@woowacourse/mission-utils";
+import { IN_GAME_MESSAGE } from "../models/Constants.js";
+
 const OutputView = {
-  printPayment() {
-    this.printAmount();
-    this.printLottos();
+  printPayment(amount, lottoList) {
+    this.printAmount(amount);
+    this.printLottos(lottoList);
   },
 
-  printStatics() {
-    this.printWinningResult();
-    this.printProfitRatio();
+  printStatics(winningResultList, profitRatio) {
+    Console.print(IN_GAME_MESSAGE.lottoResults);
+
+    this.printWinningResult(winningResultList);
+    this.printProfitRatio(profitRatio);
   },
 
-  printAmount() {},
+  printAmount(amount) {
+    Console.print(`\n${amount}${IN_GAME_MESSAGE.amount}`);
+  },
 
-  printLottos() {},
+  printLottos(lottoList) {
+    lottoList.forEach((lotto) => this.printLotto(lotto));
+  },
 
-  printWinningResult() {},
+  printLotto(lotto) {
+    const numbers = lotto.map((number) => number).join(", ");
 
-  printProfitRatio() {},
+    Console.print(`[${numbers}]`);
+  },
+
+  printWinningResult(winningResultList) {
+    winningResultList.forEach((result) =>
+      Console.print(
+        `${result.history} (${result.price.toLocaleString()}원) - ${
+          result.ticket
+        }개`
+      )
+    );
+  },
+
+  printProfitRatio(profitRatio) {
+    Console.print(`총 수익률은 ${profitRatio}%입니다.`);
+  },
 };
 
 export default OutputView;
