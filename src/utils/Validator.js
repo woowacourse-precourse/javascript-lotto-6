36abfe18f85497df1db3dbb43c09a8c86748dbbe
HyunseokLEE <unducklee@gmail.com>
2023-11-08 18:18:19 +0900

    refact: 에러문구 상수로 관리

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,20 +1,18 @@
+import ERROR_MESSAGE from "../constant/ErrorMessage";
+
 const Validator = {
   InputPurchaseAmount(purchaseAmount) {
     if (purchaseAmount.replace(/\d/g, "").length > 0)
-      throw new Error("[ERROR] 구입 금액은 숫자만 입력할 수 있습니다.");
+      throw new Error(ERROR_MESSAGE.PURCHASE_AMOUNT_NUMBER);
 
     if (purchaseAmount < 1000)
-      throw new Error(
-        "[ERROR] 구입 금액은 1000원 이상부터 로또 구입이 구입 가능합니다."
-      );
+      throw new Error(ERROR_MESSAGE.PURCHASE_AMOUNT_UNDER);
 
     if (purchaseAmount % 1000 !== 0)
-      throw new Error(
-        "[ERROR] 구입 금액은 1000원 단위로 로또를 구입해야 합니다."
-      );
+      throw new Error(ERROR_MESSAGE.PURCHASE_AMOUNT_UNIT);
 
     if (purchaseAmount.replace(/0/g, "").length === 0)
-      throw new Error("[ERROR] 구입 금액은 0원이 될 수 없습니다.");
+      throw new Error(ERROR_MESSAGE.PURCHASE_AMOUNT_ZERO);
   },
 };
 
