a77bb10f5fe5d0419a82e5ff56560b444b66cd3c
[NDCC] Park Jee_Hoon <jh.m02h.park@misumi.co.kr>
2023-11-08 16:50:05 +0900

    chore: 중복체크에 관한 예외처리 보충 수정

--- src/validate/LottoValidate.js
+++ src/validate/LottoValidate.js
@@ -18,6 +18,9 @@ class LottoValidate{
         if(winningNumbers.length !== 6){
             throw Error(`${ERROR_MESSAGE.INPUT_MORE_WINNING_NUMBERS_ERROR}`);
         }
+        if(winningNumbers.filter((value, index) => winningNumbers.indexOf(value) !== index).length > 0){
+            throw Error(`${ERROR_MESSAGE.INPUT_NUMBER_DUPLICATION_ERROR}`);
+        }
         winningNumbers.forEach((number) => {
             if (number < 1 || number > 45) {
                 throw new Error(`${ERROR_MESSAGE.INPUT_WINNING_NUMBERS_ERROR}`);
@@ -43,6 +46,9 @@ class LottoValidate{
     randomNumberValidate = (lottoNumber) => {
         if(lottoNumber.length !== 6){
             throw Error(`${ERROR_MESSAGE.INPUT_MORE_WINNING_NUMBERS_ERROR}`);
+        }
+        if(lottoNumber.filter((value, index) => lottoNumber.indexOf(value) !== index).length > 0){
+            throw Error(`${ERROR_MESSAGE.INPUT_NUMBER_DUPLICATION_ERROR}`);
         }
         lottoNumber.forEach((number) => {
             if(number < 1 || number > 45) {
