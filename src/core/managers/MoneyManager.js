e1ad4ebcefa91272c42ad7d5796dce1403434830
fecapark <fecapark@gmail.com>
2023-11-08 14:50:29 +0900

    feat: 총 수익금 계산 로직 구현 완료

--- src/core/managers/MoneyManager.js
+++ src/core/managers/MoneyManager.js
@@ -1,3 +1,7 @@
+//@ts-check
+
+import { WINNING_PRICES } from "../const.js";
+
 export default class MoneyManager {
   /** @type {number} */
   #purchaseValue;
@@ -12,5 +16,23 @@ export default class MoneyManager {
    */
   setPurchaseValue(purchaseValue) {
     this.#purchaseValue = purchaseValue;
+  }
+
+  /**
+   *
+   * @param {{
+   *    three: number;
+   *    four: number;
+   *    five: number;
+   *    bonusFive: number;
+   *    six: number;
+   * }} board
+   */
+  calculateTotalReturns(board) {
+    const entries = Object.entries(board);
+
+    return entries
+      .map(([fieldName, amount]) => WINNING_PRICES[fieldName] * amount)
+      .reduce((acc, cur) => acc + cur, 0);
   }
 }
