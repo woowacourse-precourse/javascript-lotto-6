c2281bc7037310b4881ed21bd23ce3a3e4706608
dev-yenya <dev.yenya@gmail.com>
2023-11-08 21:47:20 +0900

    test(Lotto): Add test for numbers getter and exception with fewer than 6 numbers
    - Add a test to verify that the numbers getter in the Lotto class works correctly.
    - Ensure that the numbers retrieved from the Lotto instance match the provided numbers array.
    - Also, add a test to check if an exception is thrown when the number of lotto numbers is less than 6.

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -7,7 +7,6 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
@@ -15,4 +14,15 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호의 개수가 6개보다 작으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("numbers getter가 올바르게 작동하는지 확인한다.", () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const lotto = new Lotto(numbers);
+    expect(lotto.numbers).toEqual(numbers);
+  });
 });
