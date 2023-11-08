0c0622f845bca8d67aa109eadf38d27b44ea8d27
pakxe <pigkill40@naver.com>
2023-11-08 16:52:15 +0900

    fix: 삭제되어 오류가 발생하던 라인 복구

--- __tests__/domain/LottoNumberTest.js
+++ __tests__/domain/LottoNumberTest.js
@@ -5,6 +5,7 @@ import LottoNumber from '../../src/domain/LottoNumber.js';
 describe('로또 클래스 테스트', () => {
   test('로또 번호에 nan이 있으면 예외가 발생한다.', () => {
     expect(() => {
+      new LottoNumber('빢빢쎄');
     }).toThrow(ERROR.isNan);
   });
 
