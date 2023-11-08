3737cae2c2d8a1f1cc51a7b5f1cebc5f8ffcd50b
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-11-08 21:41:57 +0900

    feat : 일치하는 번호 개수 식별하는 변수명 선언

---
+++ src/static/static.js
@@ -0,0 +1,5 @@
+export const MATCHED_NUMBER_3 = '3';
+export const MATCHED_NUMBER_4 = '4';
+export const MATCHED_NUMBER_5 = '5';
+export const MATCHED_NUMBER_5_BONUS = '5_BONUS';
+export const MATCHED_NUMBER_6 = '6';
