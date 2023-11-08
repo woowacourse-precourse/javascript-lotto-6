5369a8d1e02d8f978a1d935170ffb384c1bb4069
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-08 10:51:06 +0900

    chores: 상수 파일 작성

---
+++ src/constants/STRINGS.js
@@ -0,0 +1,20 @@
+const STRINGS = {
+  PAY_INPUT: '구입금액을 입력해 주세요.',
+  PAY_RESULT: '개를 구매했습니다.',
+  ANSWER_NUMBERS: '당첨 번호를 입력해 주세요.',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  RESULT_TITLE: '당첨 통계 \n---',
+  RESULT_THREE_CORRECT: '3개 일치 (5,000원) - ',
+  RESULT_FOUR_CORRECT: '4개 일치 (50,000원) - ',
+  RESULT_FIVE_CORRECT: '5개 일치 (1,500,000원) - ',
+  RESULT_FIVE_BONUS_CORRECT: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
+  RESULT_SIX_CORRECT: '6개 일치 (2,000,000,000원) - ',
+  RATE_OF_RETURN: '총 수익률은 62.5%입니다.',
+
+  ERROR_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
+  ERROR_NAN: '[ERROR] 숫자만 입력 가능합니다.',
+  ERROR_RANGE: '[ERROR] 1부터 45까지의 숫자만 입력 가능합니다.',
+  ERROR_REPEATED: '[ERROR] 각 로또에서 번호는 중복 입력 불가합니다.',
+};
+
+export default STRINGS;
