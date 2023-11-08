b026c95a84afe7f3f902a580097f97ceeef44b5e
LEEHYEYEONG <elto789@naver.com>
2023-11-08 15:41:07 +0900

    test: 테스트 코드 수정 - 파일 이름 변경으로 인한 코드 수정

--- __tests__/InputMoneyTest.js
+++ __tests__/InputMoneyTest.js
@@ -1,21 +1,21 @@
-import InputPrice from "../src/InputPrice.js";
+import InputPriceValidation from "../src/InputPriceValidation.js";
 
 describe("로또 구입 금액 테스트", () => {
     test("1000으로 나누어지지 않으면 예외처리", () => {
         expect(() => {
-            new InputPrice("1500");
+            new InputPriceValidation("1500");
         }).toThrow("[ERROR]");
     });
 
     test("금액이 0이하이면 예외처리", () => {
         expect(() => {
-            new InputPrice("0");
+            new InputPriceValidation("0");
         }).toThrow("[ERROR]");
     });
 
     test("금액이 숫자가 아니면 예외처리", () => {
         expect(() => {
-            new InputPrice("1000j");
+            new InputPriceValidation("1000j");
         }).toThrow("[ERROR]");
     });
 });
