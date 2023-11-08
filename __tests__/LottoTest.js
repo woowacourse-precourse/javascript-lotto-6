e350c1e083acf2652ef19e03c4d571fc51cf3a0d
Y.W KIM <kyw624@gmail.com>
2023-11-08 17:14:42 +0900

    Test: 테스트 코드 설명이 중복되던 문제와 변수명 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -18,27 +18,26 @@ describe('로또 클래스 테스트', () => {
   // 아래에 추가 테스트 작성 가능
 
   // getWinningInfo
-  test('로또가 정상적으로 발행되는지 테스트', () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+  test('당첨 번호와 비교해 결과를 반환하는 메서드 테스트', () => {
+    const LottoInstance = new Lotto([1, 2, 3, 4, 5, 6]);
     const winning = [1, 2, 3, 4, 5, 6];
     const bonus = 7;
 
     const output = { matchCount: 6, isBonusMatched: false };
 
-    expect(lotto.getWinningInfo(winning, bonus)).toEqual(output);
+    expect(LottoInstance.getWinningInfo(winning, bonus)).toEqual(output);
   });
 
-  // getLottoTickets
-  test('로또가 정상적으로 발행되는지 테스트', () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+  test('로또 클래스 테스트', () => {
+    const LottoInstance = new Lotto([1, 2, 3, 4, 5, 6]);
 
-    expect(lotto).toBeInstanceOf(Lotto);
-    expect(lotto.info).toBe('[1, 2, 3, 4, 5, 6]');
+    expect(LottoInstance).toBeInstanceOf(Lotto);
+    expect(LottoInstance.info).toBe('[1, 2, 3, 4, 5, 6]');
   });
 
-  // // getRank
+  // // getDrawRank
   test('순위를 반환하는 메서드 테스트', () => {
-    const game = new Game();
+    const LottoGame = new Game();
 
     const inputs = [
       { matchCount: 6, isBonusMatched: false },
@@ -51,16 +50,16 @@ describe('로또 클래스 테스트', () => {
     const output = Object.values(RANK);
 
     inputs.forEach((input, index) => {
-      expect(game.getRank(input)).toEqual(output[index]);
+      expect(LottoGame.getDrawRank(input)).toEqual(output[index]);
     });
   });
 
   // // getProfitRate
   test('총 수익을 구하는 메서드 테스트', () => {
     const amount = 2000;
-    const game = new Game(amount);
+    const LottoGame = new Game(amount);
 
-    game.drawInfo = {
+    LottoGame.drawInfo = {
       [RANK[5]]: { winningCount: 1, reward: REWARDS[5], matchCount: 3 },
       [RANK[4]]: { winningCount: 1, reward: REWARDS[4], matchCount: 4 },
       [RANK[3]]: { winningCount: 0, reward: REWARDS[3], matchCount: 5 },
@@ -71,6 +70,6 @@ describe('로또 클래스 테스트', () => {
     const totalReward = 55000;
     const output = (totalReward / amount) * 100;
 
-    expect(game.getProfitRate()).toEqual(output);
+    expect(LottoGame.getProfitRate()).toEqual(output);
   });
 });
