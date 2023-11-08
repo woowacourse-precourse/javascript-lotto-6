b4cbf16d6a96b7cbe8f8aef8b07de6f2093c8421
Geeehyun <wkdwl578@gmail.com>
2023-11-08 18:17:02 +0900

    refactor: input 부분 함수 수정

--- src/input.js
+++ src/input.js
@@ -1,20 +1,21 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Validation from "./Validation";
 
 
 class Input {
   async getMoney () {
     const input = await MissionUtils.Console.readLineAsync('구매금액을 입력해 주세요.');
-    return input;
+    return Validation.checkMoney(input);
   }
 
   async getNumbers () {
     const input = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.');
-    return input;
+    return Validation.checkNumbers(input);
   }
 
   async getBonus () {
     const input = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해주세요.');
-    return input;
+    return Validation.checkBonus(input);
   }
 }
   
