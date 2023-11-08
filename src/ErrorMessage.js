14790259770509e866d09cde814bd3c2d9d3383b
haazzero <wwwjungha@gmail.com>
2023-11-08 22:43:43 +0900

    feat : 에러메시지 상수화

---
+++ src/ErrorMessage.js
@@ -0,0 +1,9 @@
+const ErrorMessage = Object.freeze({
+    INVALID_PURCHASE_COST: '구입 금액은 숫자여야 합니다.',
+    INVALID_PURCAHSE_COST_RANGE: '구입 금액은 0 이상의 숫자여야 합니다.',
+    INVALID_PURCHASE_COST_UNIT: '구입 금액은 1000원 단위여야 합니다.',
+    INVALID_LOTTO_NUMBERS_UNIT: '당첨 번호는 1 ~ 45 사이의 숫자여야 합니다.',
+    INVALID_LOTTO_NUMBERS_RANGE: '당첨 번호는 총 6개의 숫자여야 합니다.',
+  });
+  
+  export default ErrorMessage;
