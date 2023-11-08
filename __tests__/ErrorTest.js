889d7ac7a9bc83130c081a4ee15791dea4a4e834
Klomachenko <leekyumin0901@naver.com>
2023-11-08 21:18:58 +0900

    feat(errorTest): lottoNumbersDuplicate 테스트 구현

--- __tests__/ErrorTest.js
+++ __tests__/ErrorTest.js
@@ -6,4 +6,9 @@ describe("ErrorMessages 클래스 테스트", () => {
     const result = ErrorMessages.invalidInput(message);
     expect(result).toBe(`[ERROR] ${message}`);
   });
+
+  it("lottoNumbersDuplicate 메서드가 올바른 에러 메시지를 반환해야 합니다.", () => {
+    const result = ErrorMessages.lottoNumbersDuplicate();
+    expect(result).toBe("[ERROR] 중복된 로또번호는 있을 수 없습니다.");
+  });
 });
