5d9a6c4432a226311c53dc91bd848225524e8a18
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-11-08 19:46:57 +0900

    test: a모든 로또 당첨 확인 기능 테스트 코드 구현

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -1,7 +1,6 @@
 import App from "../src/App.js";
 import Lotto from "../src/Lotto.js";
 
-// - 로또 당첨 확인 기능
 // - 모든 로또 당첨 확인 기능
 // - 로또 수익률 계산 기능
 
@@ -43,3 +42,63 @@ describe('로또 당첨 확인 테스트', () => {
     expect(app.compareToLottoNumbers(myLotto, lottoWinningNumber, lottoBonusNumber)).toEqual(result);
   });
 });
+
+describe('모든 로또 당첨 확인 테스트', () => {
+  test('사용자가 발행한 모든 로또의 당첨 결과를 확인한다.', () => {
+    const myLottos = [
+      new Lotto([1,2,3,4,5,6]),
+      new Lotto([1,2,3,23,28,45]),
+      new Lotto([1,2,3,10,11,12]),
+      new Lotto([1,2,3,4,34,36]),
+      new Lotto([1,2,3,4,5,45])
+    ];
+    const lottoWinningNumber = [1,2,3,4,5,6];
+    const lottoBonusNumber = 45;
+    const allLottoResult = {
+      Ranking5: {
+        rankingDetail: {
+          theNumberOfMatches: 3,
+          haveBonusNumber: false
+        },
+        reward: 5000,
+        count: 2
+      },
+      Ranking4: {
+        rankingDetail: {
+          theNumberOfMatches: 4,
+          haveBonusNumber: false
+        },
+        reward: 50000,
+        count: 1
+      },
+      Ranking3: {
+        rankingDetail: {
+          theNumberOfMatches: 5,
+          haveBonusNumber: false
+        },
+        reward: 1500000,
+        count: 0
+      },
+      Ranking2: {
+        rankingDetail: {
+          theNumberOfMatches: 5,
+          haveBonusNumber: true
+        },
+        reward: 30000000,
+        count: 1
+      },
+      Ranking1: {
+        rankingDetail: {
+          theNumberOfMatches: 6,
+          haveBonusNumber: false
+        },
+        reward: 2000000000,
+        count: 1
+      }
+    }
+
+    const app = new App();
+
+    expect(app.checkAllLottoResult(myLottos, lottoWinningNumber, lottoBonusNumber)).toEqual(allLottoResult);
+  })
+})
