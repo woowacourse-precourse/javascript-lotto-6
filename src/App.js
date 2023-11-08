19b4ae3e729b1c1d76c0ec2d0dd4a5612e285215
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-11-08 23:27:49 +0900

    refactor(App.js) : 로직 정리 및 분리

--- src/App.js
+++ src/App.js
@@ -1,123 +1,151 @@
 import { Console, Random } from "@woowacourse/mission-utils";
-import Money from "./Money.js";
-import GAME_MESSAGES from "./constants/GameMessages.js";
 import Lotto from "./Lotto.js";
 import ERROR_MESSAGES from "./constants/ErrorMessage.js";
+import GAME_MESSAGES from "./constants/GameMessages.js";
+import LOTTO_MESSAGES from "./constants/LottoMessages.js";
 
 class App {
-  async play() {
-    const money = new Money();
-    const userMoney = await money.userMoney();
-    const lottoCount = parseInt(userMoney / 1000, 10);
-    let lottos = this.generateLottoNumber(lottoCount);
-    this.getWinningNumbers(lottos);
+  constructor() {
+    this.pay = 0;
+    this.lottos = [];
+    this.winning = [];
+    this.bonus = 0;
+    this.result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
   }
 
-  async getWinningNumbers(lottos) {
-    const winningNumber = await Console.readLineAsync(
-      GAME_MESSAGES.INPUT_WINNING_NUMBER,
-    );
-    let winningNumberArray = winningNumber
-      .split(",")
-      .map((x) => parseInt(x, 10));
+  async inputMoney() {
+    let isValid = false;
 
-    try {
-      const winning = new Lotto(winningNumberArray);
-      this.getBonusNumber(lottos, winning);
-    } catch (error) {
-      Console.print(`${error.message}`);
+    while (!isValid) {
+      try {
+        const input = await Console.readLineAsync(GAME_MESSAGES.INPUT_MONEY);
+
+        this.validate(input);
+        this.pay = +input / 1000;
+        isValid = true;
+      } catch (error) {
+        Console.print(error.message);
+      }
     }
   }
 
-  async getBonusNumber(lottos, winning) {
-    const bonusNumber = await Console.readLineAsync(
-      GAME_MESSAGES.INPUT_BONUS_NUMBER,
-    );
+  validate(input) {
+    if (isNaN(+input)) throw new Error(ERROR_MESSAGES.IS_NUMBER);
 
-    if (isNaN(bonusNumber)) throw new Error(ERROR_MESSAGES.IS_BONUS_NUMBER);
-    if (winning.getNumber().includes(parseInt(bonusNumber, 10)))
-      throw new Error(ERROR_MESSAGES.IS_BONUS_DUPLICATION);
-    if (bonusNumber < 1 || bonusNumber > 45)
-      throw new Error(ERROR_MESSAGES.IS_BONUS_RANGE);
-    let winningNumber = winning.getNumber();
-    this.winningLotto(winningNumber, bonusNumber, lottos);
-  }
-
-  winningLotto(winningNumber, bonusNumber, lottos) {
-    const winningResultArray = new Array(6).fill(0);
-    for (let i = 0; i < lottos.length; i += 1) {
-      winningResultArray[
-        this.isCorrectNumber(winningNumber, bonusNumber, lottos[i])
-      ] += 1;
+    if (+input % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGES.IS_ONE_THOUSAND_WON);
     }
 
-    this.printWinningResult(winningResultArray);
-    this.printRateOfReturn(winningResultArray, lottos.length);
+    return true;
   }
 
-  printRateOfReturn(winningResultArray, quantity) {
-    let money = 0;
-    money =
-      winningResultArray[1] * 2000000000 +
-      winningResultArray[2] * 30000000 +
-      winningResultArray[3] * 1500000 +
-      winningResultArray[4] * 50000 +
-      winningResultArray[5] * 5000;
-    let rateOfReturn = Math.round((money / (quantity * 1000)) * 1000) / 10;
-    Console.print(`총 수익률은 ${rateOfReturn}%입니다.`);
-  }
-
-  printWinningResult(winningResultArray) {
-    Console.print(GAME_MESSAGES.WINNING_STATISTICS);
-    Console.print(`3개 일치 (5,000원) - ${winningResultArray[5]}개`);
-    Console.print(`4개 일치 (50,000원) - ${winningResultArray[4]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${winningResultArray[3]}개`);
-    Console.print(
-      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningResultArray[2]}개`,
-    );
-    Console.print(`6개 일치 (2,000,000,000원) - ${winningResultArray[1]}개`);
-  }
-
-  isCorrectNumber(winningNumber, bonusNumber, lotto) {
-    let count = winningNumber.filter((x) => lotto.includes(x)).length;
-
-    if (count === 6) return 1;
-    if (count === 5 && lotto.includes(bonusNumber)) return 2;
-    if (count === 5) return 3;
-    if (count === 4 || count === 3) return 8 - count;
-
-    return 0;
-  }
-
-  generateLottoNumber(lottoCount) {
-    Console.print(`\n${lottoCount}${GAME_MESSAGES.COUNT_LOTTO}`);
-
-    const lottos = [];
-
-    for (let i = 0; i < lottoCount; i += 1) {
-      const lottoNumbers = this.generateUniqueLottoNumbers();
-      lottos.push(lottoNumbers);
+  async generateLottoNumber() {
+    Console.print(`${this.pay}${GAME_MESSAGES.COUNT_LOTTO}`);
+    for (let i = 0; i < this.pay; i += 1) {
+      const lotto = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+        (a, b) => a - b,
+      );
+      const formattedLotto = `[${lotto.join(", ")}]`;
+      Console.print(formattedLotto);
+      this.lottos.push(lotto);
     }
-
-    this.printLottoNumbers(lottos);
-    return lottos;
   }
 
-  generateUniqueLottoNumbers() {
-    const lottoNumbers = new Set();
+  async inputWinning() {
+    let isValid = false;
 
-    while (lottoNumbers.size < 6) {
-      const randomNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
-      randomNumber.forEach((num) => lottoNumbers.add(num));
+    while (!isValid) {
+      try {
+        const input = await Console.readLineAsync(
+          GAME_MESSAGES.INPUT_WINNING_NUMBER,
+        );
+        const winning = input.split(",").map((elem) => +elem);
+        const lotto = new Lotto(winning);
+        this.winning = winning;
+        isValid = true;
+      } catch (error) {
+        Console.print(error.message);
+      }
     }
-
-    return Array.from(lottoNumbers).sort((a, b) => a - b);
   }
 
-  printLottoNumbers(lottos) {
-    lottos.forEach((lotto) => {
-      Console.print(`[${lotto.join(", ")}]`);
+  async inputBonus() {
+    let isValid = false;
+
+    while (!isValid) {
+      try {
+        const input = await Console.readLineAsync(
+          GAME_MESSAGES.INPUT_BONUS_NUMBER,
+        );
+        const bonusNum = +input;
+        this.bonusValidate(bonusNum);
+        this.bonus = bonusNum;
+        isValid = true;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  bonusValidate(bonusNum) {
+    if (this.winning.includes(bonusNum)) {
+      throw new Error("[ERROR] 당첨 번호와 중복되지 않는 숫자를 입력해주세요.");
+    }
+    if (bonusNum < 1 || bonusNum > 45) {
+      throw new Error("[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요.");
+    }
+  }
+
+  async calWinning() {
+    this.lottos.forEach((lotto) => {
+      let basicNumber = 0;
+      let bonusNumber = 0;
+
+      lotto.forEach((ball) => {
+        if (this.winning.includes(ball)) basicNumber += 1;
+        if (this.bonusNumber === ball) bonusNumber += 1;
+      });
+
+      if (basicNumber === 6 && bonusNumber === 0) this.result[1] += 1;
+      if (basicNumber === 5 && bonusNumber === 1) this.result[2] += 1;
+      if (basicNumber + bonusNumber === 5) this.result[3] += 1;
+      if (basicNumber + bonusNumber === 4) this.result[4] += 1;
+      if (basicNumber + bonusNumber === 3) this.result[5] += 1;
     });
+  }
+
+  async printResult() {
+    const total = this.resultRateOfReturn();
+
+    Console.print(GAME_MESSAGES.WINNING_STATISTICS);
+    Console.print(`${LOTTO_MESSAGES.COINCIDE_THREE_NUMBER}${this.result[5]}개`);
+    Console.print(`${LOTTO_MESSAGES.COINCIDE_FOUR_NUMBER}${this.result[4]}개`);
+    Console.print(`${LOTTO_MESSAGES.COINCIDE_FIVE_NUMBER}${this.result[3]}개`);
+    Console.print(
+      `${LOTTO_MESSAGES.COINCIDE_FIVE_BONUS_NUMBER}${this.result[2]}개`,
+    );
+    Console.print(`${LOTTO_MESSAGES.COINCIDE_SIX_NUMBER}${this.result[1]}개`);
+    Console.print(`총 수익률은 ${total}%입니다.\n`);
+  }
+
+  resultRateOfReturn() {
+    const money =
+      this.result[1] * 2000000000 +
+      this.result[2] * 30000000 +
+      this.result[3] * 1500000 +
+      this.result[4] * 50000 +
+      this.result[5] * 5000;
+    const buy = this.pay * 1000;
+    return Number((money / buy) * 100).toFixed(1);
+  }
+
+  async play() {
+    await this.inputMoney();
+    await this.generateLottoNumber();
+    await this.inputWinning();
+    await this.inputBonus();
+    await this.calWinning();
+    await this.printResult();
   }
 }
 
