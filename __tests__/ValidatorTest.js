36d1c36170b6f2660ee26629191399809e7e7390
Jihyun_Kim <jihyun3313@daum.net>
2023-11-08 19:44:03 +0900

    test: validator 테스트

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -18,3 +18,21 @@ describe("금액 테스트", () => {
     expect(await validAmountCheck(wrong_input)).toBe(ERROR_MESSAGE.AMOUNT);
   });
 });
+
+describe("보너스 숫자 테스트", () => {
+  test("1에서 45 사이의 숫자인지 테스트", async () => {
+    const input = '30';
+    const wrong_input = '9780';
+
+    expect(await validBonusNumberCheck(input)).toBe(input);
+    expect(await validBonusNumberCheck(wrong_input)).toBe(ERROR_MESSAGE.NUM_RANGE);
+  });
+
+  test("1개의 숫자인지 테스트", async () => {
+    const input = '30';
+    const wrong_input = "32,25";
+
+    expect(await validBonusNumberCheck(input)).toBe(input);
+    expect(await validBonusNumberCheck(wrong_input)).toBe(ERROR_MESSAGE.BONUS_NUM_COUNT);
+  });
+});
