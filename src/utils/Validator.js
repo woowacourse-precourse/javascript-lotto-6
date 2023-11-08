36e1aae8533741af6e41e857be79527d6466b521
HyunseokLEE <unducklee@gmail.com>
2023-11-08 19:55:00 +0900

    feat: 당첨 번호에 대한 유효성 검사

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -20,10 +20,29 @@ const Validator = {
     const number = numbers.join("");
     if (number.replace(/\d/g, "").length > 0)
       throw new Error(ERROR_MESSAGE.LOTTO_NUMBER);
+
     if (numbers.length !== new Set(numbers).size)
       throw new Error(ERROR_MESSAGE.LOTTO_DUPLICATE);
+
     if (numbers.length !== 6) throw new Error(ERROR_MESSAGE.LOTTO_COUNT);
+
     if (RangeTest(numbers)) throw new Error(ERROR_MESSAGE.LOTTO_RANGE);
+  },
+
+  InputWinningNumber(input) {
+    const inputWinninNumbers = input.split(",");
+    if (input.replace(/\d|\,/g, "").length > 0)
+      throw new Error("[ERROR] 로또 번호는 숫자만 가능합니다.");
+
+    if (inputWinninNumbers.length !== 6)
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+
+    if (inputWinninNumbers.length !== new Set(inputWinninNumbers).size)
+      throw new Error(
+        "[ERROR] 로또 번호는 중복되지 않은 숫자로 이루어져야 합니다."
+      );
+    if (RangeTest(inputWinninNumbers))
+      throw new Error("[ERROR] 당첨 번호는 1과 45 사이의 값이어야 합니다.");
   },
 };
 
