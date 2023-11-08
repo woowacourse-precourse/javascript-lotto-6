740f911762a33b8329140ab6f3bf6921e24f78ea
Moon Sojeong <sjeong35@naver.com>
2023-11-08 18:31:04 +0900

    refactor: constant.js에 따라 상수로 변경

--- src/Validate.js
+++ src/Validate.js
@@ -1,17 +1,18 @@
-import { ERROR_MESSAGE} from './message';
+import {ERROR_MESSAGE} from './message';
+import { CONSTANT } from './constant';
 class Validate {
     checkLottoPrice(lottoPrice) { //Validate
         lottoPrice = Number(lottoPrice);
         if (isNaN(lottoPrice)) throw new Error(ERROR_MESSAGE.NOT_NUMBER);
-        if (lottoPrice < 1000) throw new Error(ERROR_MESSAGE.MIN_PRICE);
-        if (lottoPrice % 1000 !== 0) throw new Error(ERROR_MESSAGE.THOUSAND_UNIT);
+        if (lottoPrice < CONSTANT.THOUSAND) throw new Error(ERROR_MESSAGE.MIN_PRICE);
+        if (lottoPrice % CONSTANT.THOUSAND !== 0) throw new Error(ERROR_MESSAGE.THOUSAND_UNIT);
     }
 
     checkBonusNumber(userBonusNumber) { //Validate
         if (userBonusNumber === '') throw new Error(ERROR_MESSAGE.NUMBER_EMPTY);
         userBonusNumber = Number(userBonusNumber);
         if (isNaN(userBonusNumber)) throw new Error(ERROR_MESSAGE.NOT_NUMBER);
-        if (1 > userBonusNumber || userBonusNumber > 45) throw new Error(ERROR_MESSAGE.RANGE_BONUS);
+        if (CONSTANT.NUMBER_MIN_RANGE > userBonusNumber || userBonusNumber > CONSTANT.NUMBER_MAX_RANGE) throw new Error(ERROR_MESSAGE.RANGE_BONUS);
     }
 }
 
