7511e36060d69a574afa7caa772c86276e2c0fce
Hchan2 <seongchan_@naver.com>
2023-11-08 22:39:44 +0900

    Fix: 게임 결과 메소드를 활용하여 결과 내용 출력

--- src/game/printLottoResult.js
+++ src/game/printLottoResult.js
@@ -7,18 +7,8 @@ const printLottoResult = (lottoResult, userBuyMoney) => {
 }
 
 const printLottoContent = (lottoResult, totalPer) => {
-    const contents = `
-        당첨 통계
-        ---
-        3개 일치 (5,000원) - ${lottoResult.three.count}개
-        4개 일치 (50,000원) - ${lottoResult.four.count}개
-        5개 일치 (1,500,000원) - ${lottoResult.five.count}개
-        5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult.fiveBonus.count}개
-        6개 일치 (2,000,000,000원) - ${lottoResult.six.count}개
-        총 수익률은 ${totalPer}%입니다.
-    `;
-
-    OutputView.printResult(contents);
+    const getLottoResult = OutputView.printLottoGameResult(lottoResult, totalPer);
+    OutputView.printResult(getLottoResult);
 }
 
 export default printLottoResult;
