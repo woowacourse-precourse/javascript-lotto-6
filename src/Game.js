0542c2ce09a81ee576d96dd46a5d1d91ac313212
yongjae95 <64951@hanmail.net>
2023-11-08 20:32:45 +0900

    feat: 보너스 번호 및 수익률 계산 함수 생성

--- src/Game.js
+++ src/Game.js
@@ -32,6 +32,55 @@ class Game {
 
     this.gameResult();
   }
+
+  compareLotto(purchasedLotto) {
+    let sameNumCount = 0;
+
+    this.#winnerLotto.forEach((winnerLottoNum) => {
+      if (purchasedLotto.indexOf(winnerLottoNum) !== -1) sameNumCount++;
+    });
+
+    if (
+      sameNumCount === 5 &&
+      purchasedLotto.indexOf(Number(this.#bonusNum)) !== -1
+    ) {
+      sameNumCount = 10;
+    }
+
+    return sameNumCount;
+  }
+
+  gameResult() {
+    this.#sameNumCountList.forEach((sameNumCount) => {
+      if (sameNumCount === 3) this.#winningLottoCount.three++;
+      if (sameNumCount === 4) this.#winningLottoCount.four++;
+      if (sameNumCount === 5) this.#winningLottoCount.five++;
+      if (sameNumCount === 6) this.#winningLottoCount.six++;
+      if (sameNumCount === 10) this.#winningLottoCount.fiveNBonus++;
+    });
+
+    for (const key in this.#winningLottoCount) {
+      Console.print(
+        `${GAME_MESSAGE.winningLottoPrice[key][0]} (${formatAmount(
+          GAME_MESSAGE.winningLottoPrice[key][1]
+        )}원) - ${this.#winningLottoCount[key]}개`
+      );
+    }
+
+    let totalIncome = 0;
+    for (const key in this.#winningLottoCount) {
+      totalIncome +=
+        GAME_MESSAGE.winningLottoPrice[key][1] * this.#winningLottoCount[key];
+    }
+    const rateOfReturn = Number(
+      ((totalIncome / this.#purchaseAmount) * 100).toFixed(1)
+    );
+    Console.print(
+      `${GAME_MESSAGE.totalReturn[0]} ${formatAmount(rateOfReturn)}%${
+        GAME_MESSAGE.totalReturn[1]
+      }`
+    );
+  }
 }
 
 export default Game;
