c7e1482192c0ce74a325cb76596ccce6c7949b7f
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-11-08 13:56:09 +0900

    test: statistics클래스 테스트 추가 및 테스트 케이스 파일 분리

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,20 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { CashCount } from '../src/CashCount.js';
 import { LottoMachine } from '../src/LottoMachine.js';
-
-//CashCount 클래스 테스트
-describe('cashcount 클래스 테스트', () => {
-  test('1000원 나누어 떨어지나', () => {
-    expect(() => {
-      new CashCount(123);
-    }).toThrow('[ERROR]');
-  });
-  test('숫자인가', () => {
-    expect(() => {
-      new CashCount(NaN);
-    }).toThrow('[ERROR]');
-  });
-});
+import { CashCount } from '../src/CashCount.js';
 
 describe('LottoMachine 클래스 테스트', () => {
   test('로또를 구매하고 배열로 반환한다.', () => {
