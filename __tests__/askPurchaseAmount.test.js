10609413c3a0b184d575b7c28feaf36b7ef5f4a5
소인섭 <sis9745@gachon.ac.kr>
2023-11-08 17:10:31 +0900

    test: 구매 금액 입력 기능 테스트 코드
    - 정상 테스트: 정상 값 8000 입력 시, 값 반환
    - 예외 테스트: 각 예외 상황마다 에러 발생
      - 아무것도 입력하지 않은 경우
      - 문자를 포함하여 입력한 경우
      - '0' 또는 음수를 입력한 경우
      - 1000으로 나누어 떨어지지 않은 수를 입력한 경우

---
+++ __tests__/askPurchaseAmount.test.js
@@ -0,0 +1,33 @@
+import { mockReadLineAsync } from './mockMissionUtils';
+import { askPurchaseAmount } from '../src/askPurchaseAmount';
+
+describe('askPurchaseAmount', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('8000 입력', async () => {
+    // given
+    const purchaseAmount = ['8000'];
+    mockReadLineAsync(purchaseAmount);
+
+    // when
+    const result = await askPurchaseAmount();
+
+    // then
+    expect(result).toBe(8000);
+  });
+
+  test.each([[''], ['1000j'], ['-8000'], ['8080']])(
+    '"%s" 입력',
+    async (purchaseAmount) => {
+      // given
+      mockReadLineAsync([purchaseAmount, 8000]);
+
+      // when & then
+      await expect(askPurchaseAmount()).rejects.toThrow(
+        '[ERROR] Invalid purchase amount.'
+      );
+    }
+  );
+});
