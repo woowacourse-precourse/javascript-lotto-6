2846ff72087305bb9fa64b01fd989d5818bbf4a2
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 14:31:24 +0900

    refactor: 일치한 갯수라는 의미에 맞게 변수명을 CASE -> MATCH로 수정

--- src/LottoResult.js
+++ src/LottoResult.js
@@ -1,31 +1,31 @@
 class LottoResult {
-  static CASE_3 = {
+  static MATCH_3 = {
     description: '3개 일치',
     prize: 5000,
   };
-  static CASE_4 = {
+  static MATCH_4 = {
     description: '4개 일치',
     prize: 50000,
   };
-  static CASE_5 = {
+  static MATCH_5 = {
     description: '5개 일치',
     prize: 1500000,
   };
-  static CASE_5_BONUS = {
+  static MATCH_5_BONUS = {
     description: '5개 일치, 보너스 볼 일치',
     prize: 30000000,
   };
-  static CASE_6 = {
+  static MATCH_6 = {
     description: '6개 일치',
     prize: 2000000000,
   };
 
   static getPrize = (count, isBonus) => {
-    if (count == 3) return LottoResult.CASE_3;
-    if (count == 4) return LottoResult.CASE_4;
-    if (count == 5 && !isBonus) return LottoResult.CASE_5;
-    if (count == 5 && isBonus) return LottoResult.CASE_5_BONUS;
-    if (count == 6) return LottoResult.CASE_6;
+    if (count == 3) return LottoResult.MATCH_3;
+    if (count == 4) return LottoResult.MATCH_4;
+    if (count == 5 && !isBonus) return LottoResult.MATCH_5;
+    if (count == 5 && isBonus) return LottoResult.MATCH_5_BONUS;
+    if (count == 6) return LottoResult.MATCH_6;
   };
 }
 
