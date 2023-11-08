152b2583accbad6c5b3a3f4a844c67432f82a6b7
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-08 15:09:24 +0900

    refactor: LottoBudget 클래스로부터 Budget 클래스 분리
    Budget 유효성 검사 메서드와 budget 자체만을 갖고 있는 Budget 클래스를 구현함으로써 LottoBudget 클래스가 budget 유효성 검사와 기타 다른 역할을 통합적으로 하지 않도록 리팩토링

--- src/LottoBudget.js
+++ src/LottoBudget.js
@@ -1,33 +1,52 @@
 import { Console } from "@woowacourse/mission-utils";
+import Budget from "./Budget.js";
 class LottoBudget {
-    lottoBudget;
+
     LOTTO_NUMBER;
-    ERROR_MESSEGE="[ERROR] 숫자가 잘못된 형식입니다.";
-    GET_LOTTO_BUDGET_INPUT_MESSEGE="구입금액을 입력해 주세요.\n";
+
+    MIN_LOTTO_NUM;
+    MAX_LOTTO_NUM;
+    LOTTO_PRICE;
+
+    BUDGET;
+    ERROR_MESSAGE="[ERROR] 숫자가 잘못된 형식입니다.";
+    GET_LOTTO_BUDGET_INPUT_MESSAGE="구입금액을 입력해 주세요.\n";
 
     constructor(MIN_LOTTO_NUM,MAX_LOTTO_NUM,LOTTO_PRICE) {
         this.MIN_LOTTO_NUM=MIN_LOTTO_NUM;
         this.MAX_LOTTO_NUM=MAX_LOTTO_NUM;
         this.LOTTO_PRICE=LOTTO_PRICE;
+        console.log(this.LOTTO_PRICE);
     }
 
-    #isValid(lottoBudget){
-        if(lottoBudget < this.MIN_LOTTO_NUM*this.LOTTO_PRICE || lottoBudget > this.MAX_LOTTO_NUM*this.LOTTO_PRICE){
-            throw new Error(this.ERROR_MESSEGE);
-        }
-
-        this.LOTTO_NUMBER = lottoBudget/this.LOTTO_PRICE;
-        const CHANGE = lottoBudget%this.LOTTO_PRICE
-
-        if(CHANGE!==0){
-            throw new Error(this.ERROR_MESSEGE);
+    #isNumberCharacter(ch){
+        if( ch < '0' || ch > '9') throw new Error(this.ERROR_MESSAGE);
+    }
+    #isAllCharacter(str) {
+        for(let i = 0 ; i < str.length ; i++) {
+            this.#isNumberCharacter(str[i]);
         }
     }
-
     async getInput() {
-        this.lottoBudget = parseInt(await Console.readLineAsync(this.GET_LOTTO_BUDGET_INPUT_MESSEGE));
-        this.#isValid(this.lottoBudget);
-        Console.print('\n');   
+        while(true){
+            try{
+            const LOTTO_BUDGET_INPUT = await Console.readLineAsync(this.GET_LOTTO_BUDGET_INPUT_MESSAGE);
+            this.#isAllCharacter(LOTTO_BUDGET_INPUT);
+            this.lottoBudget = parseInt(LOTTO_BUDGET_INPUT);
+            console.log(this.LOTTO_PRICE);
+            this.BUDGET = new Budget(
+                this.lottoBudget,
+                this.MIN_LOTTO_NUM,
+                this.MAX_LOTTO_NUM,
+                this.LOTTO_PRICE
+                );
+            this.LOTTO_NUMBER = (this.BUDGET.budget)/this.LOTTO_PRICE;
+            }catch(e){
+                Console.print(e.message);
+                continue;
+            }
+            break;
+        }
     }
 }
 
