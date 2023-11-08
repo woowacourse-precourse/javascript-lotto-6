104bdd30b0d930a7a3220db4c3bf80a9719b549c
Yoos99 <yoosh2520@naver.com>
2023-11-08 16:59:51 +0900

    feat: 에러 메시지 종류 추가

--- src/ErrorMessages.js
+++ src/ErrorMessages.js
@@ -1,10 +1,13 @@
 const ErrorMessages = {
-  NOT_DIVISIBLE_AMOUNT:
-    "[ERROR] 구매 금액은 1,000원으로 나누어 떨어져야 합니다.",
-  INVALID_NUMBER_INPUT: "[ERROR] 유효하지 않은 숫자 입력입니다.",
-  DUPLICATE_NUMBER: "[ERROR] 번호에 중복이 발생했습니다.",
-  TOO_MANY_WINNING_NUMBERS: "[ERROR] 당첨 번호는 6개를 초과할 수 없습니다.",
-  NUMBER_OUT_OF_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  EMPTY_INPUT: "[ERROR] 공백을 입력할 수 없습니다.",
+  INVALID_NUMBER: "[ERROR] 유효하지 않은 금액입니다.",
+  ONLY_ONE_NUMBER: "[ERROR] 하나의 숫자만 입력해야 합니다.",
+  UNDIVIDED_AMOUNT: "[ERROR] 금액은 1,000원으로 나누어 떨어져야 합니다.",
+  NUMBER_OUT_OF_RANGE: "[ERROR] 1~45 사이의 숫자를 입력해야 합니다.",
+  DUPLICATE_NUMBER: "[ERROR] 중복된 숫자를 입력할 수 없습니다.",
+  TOO_MANY_NUMBERS: "[ERROR] 개수를 초과하여 입력하였습니다",
+  TOO_LESS_NUMBERS: "[ERROR] 더 많은 숫자를 입력해야 합니다.",
+  DUPLICATE_BONUS_NUMBER: "[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.",
 };
 
 export default ErrorMessages;
