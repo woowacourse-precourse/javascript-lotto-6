8a45b1e18e133006a4c56549b334569f2de866b6
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-11-08 22:41:50 +0900

    refactor: 숫자, 에러메세지 상수로 분리

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -1,7 +1,8 @@
 import App from '../src/App.js';
+import { LOTTO_PRICE } from '../src/constant/constants.js';
 
 describe('로또 발행 테스트', () => {
-  test('로또 하나의 금액은 1,000원이며 입력 받은 금액에 맞게 로또를 발행해야 한다.', () => {
+  test(`로또 하나의 금액은 ${LOTTO_PRICE.toLocaleString('ko-KR')}원이며 입력 받은 금액에 맞게 로또를 발행해야 한다.`, () => {
     
     const app = new App();
 
--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,7 +1,8 @@
 import Lotto from '../src/Lotto.js';
+import { LOTTO_NUMBER_MAX, LOTTO_NUMBER_MIN, THE_NUMBER_OF_LOTTO_NUMBER } from '../src/constant/constants.js';
 
 describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
+  test(`로또 번호의 개수가 ${THE_NUMBER_OF_LOTTO_NUMBER}개가 넘어가면 예외가 발생한다.`, () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow('[ERROR]');
@@ -13,7 +14,7 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  test('로또 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다.', () => {
+  test(`로또 번호가 ${LOTTO_NUMBER_MIN}부터 ${LOTTO_NUMBER_MAX} 사이의 숫자가 아니면 예외가 발생한다.`, () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 50]);
     }).toThrow('[ERROR]');
