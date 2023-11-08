d1731717d56ca4a9cb44697efef1e5c137aceed2
김미르 <kimmireu@gimmileuui-iMac.local>
2023-11-08 15:43:00 +0900

    chore : LottoModel -> LottoGame

---
+++ __tests__/LottoGameTest.js
@@ -0,0 +1,128 @@
+import LottoGame from "../src/model/LottoGame";
+import SETTINGS from "../src/constants/settings";
+import MESSAGES from "../src/constants/messages";
+
+describe("lottoGame 테스트", () => {
+  let lottoGame;
+
+  describe("사용자 입력 처리 메서드", () => {
+    test("사용자가 입력한 로또 구매 금액을 설정하고 가져오기", () => {
+      const price = 10000;
+      lottoGame = new LottoGame(price);
+
+      expect(lottoGame.getPrice()).toBe(price);
+    });
+
+    test("사용자가 입력한 당첨 번호를 설정하고 가져오기", () => {
+      const numbers = [3, 7, 12, 18, 22, 45];
+
+      lottoGame.setTargetNumbers(numbers);
+
+      expect(lottoGame.getTargetNumbers()).toEqual(numbers);
+    });
+
+    test("사용자가 입력한 당첨 번호가 중복된 경우 예외 처리", () => {
+      const invalidNumbers = [1, 2, 3, 4, 5, 5];
+
+      expect(() => lottoGame.validateTargetNumbers(invalidNumbers)).toThrow(
+        MESSAGES.error.notDuplicateTargetNumbers
+      );
+    });
+
+    test("사용자가 입력한 당첨 번호 중 범위를 벗어나는 번호가 있는 경우 예외 처리", () => {
+      const invalidNumber = SETTINGS.targetNumber.maximum * 2;
+      const invalidNumbers = [1, 2, 3, 4, 5, invalidNumber];
+
+      expect(() => lottoGame.validateTargetNumbers(invalidNumbers)).toThrow(
+        MESSAGES.error.invalidRange
+      );
+    });
+
+    test("사용자가 입력한 당첨 번호가 유효한 경우 검증 통과", () => {
+      const numbers = [10, 15, 20, 25, 30, 35];
+
+      expect(() => lottoGame.validateTargetNumbers(numbers)).not.toThrow();
+    });
+
+    test("사용자가 입력한 보너스 번호가 범위를 벗어날 경우 예외 처리", () => {
+      const number = SETTINGS.targetNumber.maximum * 2;
+
+      expect(() => lottoGame.validateBonusNumbers(number)).toThrowError(
+        MESSAGES.error.invalidRange
+      );
+    });
+
+    test("사용자가 입력한 보너스가 번호가 이미 당첨 번호에 있는 번호인 경우 예외 처리", () => {
+      const numbers = [10, 15, 20, 25, 30, 35];
+      const number = 25;
+
+      lottoGame.setTargetNumbers(numbers);
+
+      expect(() => lottoGame.validateBonusNumbers(number)).toThrowError(
+        MESSAGES.error.notDuplicateTargetNumbers
+      );
+    });
+
+    test("사용자가 입력한  보너스 번호가 유효한 경우 검증 통과", () => {
+      const numbers = [10, 15, 20, 25, 30, 35];
+      const number = 42;
+
+      lottoGame.setTargetNumbers(numbers);
+
+      expect(() => lottoGame.validateBonusNumbers(number)).not.toThrow();
+    });
+  });
+
+  describe("get 메서드", () => {
+    test("lottoGame은 올바른 price, count 값을 반환한다.", () => {
+      const price = 1000;
+      const lottoGame = new LottoGame(price);
+
+      expect(lottoGame.getPrice()).toEqual(price);
+      expect(lottoGame.getCount()).toEqual(price / SETTINGS.priceUnit);
+    });
+  });
+
+  describe("calculateCorrect 메서드", () => {
+    const lottoGame = new LottoGame(1000);
+    const cases = [
+      { userNumbers: [1, 2, 3, 4, 5, 6], expected: 6 },
+      { userNumbers: [1, 2, 3, 4, 5, 7], expected: 5 },
+      { userNumbers: [1, 2, 3, 4, 7, 8], expected: 4 },
+      { userNumbers: [1, 2, 3, 7, 8, 9], expected: 3 },
+      { userNumbers: [1, 2, 7, 8, 9, 10], expected: 2 },
+    ];
+    const targetNumbers = [1, 2, 3, 4, 5, 6];
+
+    lottoGame.setTargetNumbers(targetNumbers);
+
+    test.each(cases)(
+      "주어진 사용자 로또 번호와 일치하는 당첨 번호의 개수를 반환합니다",
+      ({ userNumbers, expected }) => {
+        const target = lottoGame.calculateCorrect(userNumbers)[0];
+        expect(target).toEqual(expected);
+      }
+    );
+  });
+
+  describe("addCount 메서드", () => {
+    test("수익을 계산할 때 올바른 결과를 반환해야 합니다", () => {
+      const dummyResult = [0, 5, 3, 2, 1, 0];
+
+      dummyResult.forEach((count, prize) => {
+        for (let i = 0; i < count; i++) {
+          lottoGame.addCount(prize);
+        }
+      });
+      lottoGame.setIncome();
+
+      const expectedIncome =
+        dummyResult[1] * SETTINGS.income.first +
+        dummyResult[2] * SETTINGS.income.second +
+        dummyResult[3] * SETTINGS.income.third +
+        dummyResult[4] * SETTINGS.income.fourth +
+        dummyResult[5] * SETTINGS.income.fifth;
+      expect(lottoGame.getIncome()).toBe(expectedIncome);
+    });
+  });
+});
---
+++ src/model/LottoGame.js
@@ -0,0 +1,142 @@
+import MESSAGES from "../constants/messages";
+import SETTINGS from "../constants/settings";
+import Lotto from "../Lotto";
+import { Random } from "@woowacourse/mission-utils";
+
+class LottoGame {
+  #price;
+  #count;
+  #lottos = [];
+
+  #targetNumbers = [];
+  #bonusNumber;
+  #result = Array(SETTINGS.targetNumber.count).fill(SETTINGS.default);
+  #income;
+
+  constructor(price) {
+    this.#validatePrice(price);
+    this.#price = price;
+    this.#count = parseInt(price / SETTINGS.priceUnit);
+  }
+
+  #validatePrice(price) {
+    if (Number.isNaN(price)) throw new Error(MESSAGES.error.notNumber);
+    if (price % SETTINGS.priceUnit) {
+      throw new Error(MESSAGES.error.invalidPricUnit);
+    }
+  }
+
+  getPrice() {
+    return this.#price;
+  }
+
+  getCount() {
+    return this.#count;
+  }
+
+  buyLottos() {
+    const { minimum, maximum } = SETTINGS.targetNumber;
+
+    for (let count = 0; count < this.#count; count++) {
+      const numbers = Random.pickUniqueNumbersInRange(count, minimum, maximum);
+      this.#lottos.push(new Lotto(numbers));
+    }
+
+    return [this.#price, this.#count, this.#lottos];
+  }
+
+  setTargetNumbers(numbers) {
+    this.validateTargetNumbers(numbers);
+    this.#targetNumbers = numbers;
+  }
+
+  validateTargetNumbers(numbers) {
+    const { count, minimum, maximum } = SETTINGS.targetNumber;
+    const numbersSet = new Set(numbers);
+
+    if (numbers.length !== count) {
+      throw new Error(MESSAGES.error.invalidTargetNumbersLength);
+    }
+    if (numbers.length !== numbersSet.size) {
+      throw new Error(MESSAGES.error.notDuplicateTargetNumbers);
+    }
+    if (numbers.some((number) => number > maximum || number < minimum)) {
+      throw new Error(MESSAGES.error.invalidRange);
+    }
+  }
+
+  getTargetNumbers() {
+    return this.#targetNumbers;
+  }
+
+  setBonusNumber(number) {
+    this.validateBonusNumbers(number);
+    this.#bonusNumber = number;
+  }
+
+  validateBonusNumbers = (number) => {
+    const { minimum, maximum } = SETTINGS.targetNumber;
+
+    if (number > maximum || number < minimum) {
+      throw new Error(MESSAGES.error.invalidRange);
+    }
+    if (this.#targetNumbers.includes(number)) {
+      throw Error(MESSAGES.error.notDuplicateTargetNumbers);
+    }
+  };
+
+  checkNumbers() {
+    for (let lotto of this.#lottos) {
+      const numbers = lotto.getNumbers();
+      const [correctTarget, correctBonus] = this.calculateCorrect(numbers);
+      this.calculateResult(correctTarget, correctBonus);
+    }
+
+    this.setIncome();
+    return [this.#result, this.#income];
+  }
+
+  calculateCorrect(numbers) {
+    let correctTarget = 0;
+    let correctBonus = numbers.includes(Number(this.#bonusNumber)) ? 1 : 0;
+
+    for (let targetNumber of this.#targetNumbers) {
+      if (numbers.includes(Number(targetNumber))) correctTarget += 1;
+    }
+    return [correctTarget, correctBonus];
+  }
+
+  calculateResult(correctTarget, correctBonus) {
+    const { count } = SETTINGS.targetNumber;
+    let ranking = 0;
+
+    if (correctTarget === count) ranking = 1;
+    if (correctTarget === count - 1 && correctBonus) ranking = 2;
+    if (correctTarget === count - 1 && !correctBonus) ranking = 3;
+    if (correctTarget === count - 2) ranking = 4;
+    if (correctTarget === count - 3) ranking = 5;
+
+    this.addCount(ranking);
+  }
+
+  addCount(ranking) {
+    this.#result[ranking] += 1;
+  }
+
+  setIncome() {
+    const { first, second, third, fourth, fifth } = SETTINGS.income;
+
+    this.#income =
+      this.#result[1] * first +
+      this.#result[2] * second +
+      this.#result[3] * third +
+      this.#result[4] * fourth +
+      this.#result[5] * fifth;
+  }
+
+  getIncome() {
+    return this.#income;
+  }
+}
+
+export default LottoGame;
