4445e2a64ee9a4e769bd78aeedb4c55153620822
hamo-o <yheel369@gmail.com>
2023-11-08 19:58:56 +0900

    refactor: 에러 헤더 상수 변환

--- src/constants/Error.js
+++ src/constants/Error.js
@@ -1,12 +1,14 @@
-const ERROR = {
-  purchaseIsNumber: '[ERROR] 구입 금액은 숫자여야 합니다.',
-  purchaseMinNumber: '[ERROR] 구입 금액은 최소 1000원 이상이어야 합니다.',
-  purchaseThousands: '[ERROR] 구입 금액은 1000원 단위여야 합니다.',
+const ERROR_HEADER = '[ERROR]';
 
-  lottoIsNotDuplicate: '[ERROR] 로또 번호는 중복될 수 없습니다',
-  lottoIsNumber: '[ERROR] 로또 번호는 숫자여야 합니다.',
-  lottoRange: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
-  lottoContainComma: '[ERROR] 로또 번호는 쉼표(,)를 기준으로 구분해야 합니다.',
+const ERROR = {
+  purchaseIsNumber: `${ERROR_HEADER} 구입 금액은 숫자여야 합니다.`,
+  purchaseMinNumber: `${ERROR_HEADER} 구입 금액은 최소 1000원 이상이어야 합니다.`,
+  purchaseThousands: `${ERROR_HEADER} 구입 금액은 1000원 단위여야 합니다.`,
+
+  lottoIsNotDuplicate: `${ERROR_HEADER} 로또 번호는 중복될 수 없습니다`,
+  lottoIsNumber: `${ERROR_HEADER} 로또 번호는 숫자여야 합니다.`,
+  lottoRange: `${ERROR_HEADER} 로또 번호는 1부터 45 사이의 숫자여야 합니다.`,
+  lottoContainComma: `${ERROR_HEADER} 로또 번호는 쉼표(,)를 기준으로 구분해야 합니다.`,
 };
 Object.freeze(ERROR);
 
