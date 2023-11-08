600144f7e98f9b0b4847eb004659a52febabcc35
이동주 <leednj0113@naver.com>
2023-11-08 10:20:30 +0900

    docs: 당첨 로또 생성 기능명 유효성 검사로 변경

--- src/LottoDrawing.js
+++ src/LottoDrawing.js
@@ -5,6 +5,42 @@ import Lotto from "./Lotto.js";
 class LottoDrawing {
   #winnigLotto;
   #bonusNumber;
+
+  getWinningLotto() {
+    return this.#winnigLotto;
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+
+
+  async inputBonusNumber() {
+    try {
+      const input = await Console.readLineAsync(
+        "\n보너스 번호를 입력해 주세요.\n"
+      );
+
+      this.vaildateBonusNumber(input);
+      this.#bonusNumber = Number(input);
+    } catch (error) {
+      Console.print(error.message);
+
+      return this.inputBonusNumber();
+    }
+  }
+
+  vaildateBonusNumber(input) {
+    if (isNaN(input)) throw new Error("[ERROR] 숫자만 입력 가능합니다.");
+
+    const number = Number(input);
+    if (!Number.isInteger(number))
+      throw new Error("[ERROR] 정수만 입력 가능합니다.");
+    if (number < 1 || number > 45)
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    if (this.#winnigLotto.getNumbers().includes(number))
+      throw new Error("[ERROR] 보너스 번호는 당첨 번호에 포함될 수 없습니다.");
+  }
 }
 
 export default LottoDrawing;
