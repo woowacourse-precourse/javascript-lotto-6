a58f8b51814f51eac290e22e188bc552a647e323
KYHH <kyhh93@naver.com>
2023-11-08 21:06:19 +0900

    refactor: 계속되는 에러발생으로 코드를 수정하여 다르게 시도

--- src/LottoController.js
+++ src/LottoController.js
@@ -15,17 +15,17 @@ class Controller {
   async gameStart() {
     const input = new Input();
     this.purchaseAmount = await input.userPurchase();
-    // this.lottoTickets = Output.ticketPrint(this.purchaseAmount);
+    this.lottoTickets = Output.ticketPrint(this.purchaseAmount);
     this.lottoNumbers = await input.userNumber();
     this.lottoBonusNumber = await input.userBonusNumber();
     this.gameResult();
   }
 
   async gameResult() {
-    Console.print(`\n구입금액을 입력해 주세요.\n${this.purchaseAmount}`);
-    this.lottoTickets = Output.ticketPrint(this.purchaseAmount);
-    Console.print(`\n당첨 번호를 입력해 주세요.\n${this.lottoNumbers}`);
-    Console.print(`\n보너스 번호를 입력해 주세요.\n${this.lottoBonusNumber}`);
+    // Console.print(`\n구입금액을 입력해 주세요.\n${this.purchaseAmount}`);
+    // this.lottoTickets = Output.ticketPrint(this.purchaseAmount);
+    // Console.print(`\n당첨 번호를 입력해 주세요.\n${this.lottoNumbers}`);
+    // Console.print(`\n보너스 번호를 입력해 주세요.\n${this.lottoBonusNumber}`);
 
     const matchingCounts = Output.compareTickets(this.lottoTickets, this.lottoNumbers, this.lottoBonusNumber);
 
--- src/UserInput.js
+++ src/UserInput.js
@@ -5,64 +5,60 @@ class Input {
     this.lottoNumbers = [];
   }
 
-
   async userPurchase() {
-    while (true) {
+    try {
+      Console.print('구입금액을 입력해 주세요.');
+      const input = await Console.readLineAsync('');
+      const purchaseAmount = parseInt(input);
 
-      try {
-        Console.print('구입금액을 입력해 주세요.');
-        const input = await Console.readLineAsync('');
-        const purchaseAmount = parseInt(input);
-
-        if (isNaN(purchaseAmount) || purchaseAmount <= 0 || purchaseAmount % 1000 !== 0) {
-          throw new Error("[ERROR] 올바른 구입금액을 입력해 주세요 (1,000원 단위, 양수).");
-        }
-
-        if (input.split('').some(char => !'0123456789'.includes(char))) {
-          throw new Error("[ERROR] 구입금액은 숫자만 입력할 수 있습니다.");
-        }
-        return input;
-      } catch (error) {
-        Console.print(error.message);
+      if (isNaN(purchaseAmount) || purchaseAmount <= 0 || purchaseAmount % 1000 !== 0) {
+        throw new Error("[ERROR] 올바른 구입금액을 입력해 주세요 (1,000원 단위, 양수).");
       }
+
+      if (input.split('').some(char => !'0123456789'.includes(char))) {
+        throw new Error("[ERROR] 구입금액은 숫자만 입력할 수 있습니다.");
+      }
+      return input;
+    } catch (error) {
+      Console.print(error.message);
+      return this.userPurchase(); // 재귀 호출
     }
   }
 
-
   async userNumber() {
-    while (true) {
-      try {
-        Console.print(`\n당첨 번호를 입력해 주세요.`);
-        const lottoNumbers = await Console.readLineAsync('');
-        this.lottoNumbers = lottoNumbers.split(',').map(num => parseInt(num.trim()));
-        if (this.lottoNumbers.length !== 6 || this.lottoNumbers.some(isNaN) || this.lottoNumbers.some(num => num < 1 || num > 45)) {
-          throw new Error('[ERROR]올바른 로또 번호를 입력해 주세요 (1부터 45까지의 숫자, 쉼표로 구분된 6개 숫자).');
-        }
-        this.checkForDuplicates(this.lottoNumbers);
-
-        return this.lottoNumbers;
-      } catch (error) {
-        Console.print(error.message);
+    try {
+      Console.print(`\n당첨 번호를 입력해 주세요.`);
+      const lottoNumbers = await Console.readLineAsync('');
+      if (!/^\d+(,\d+){5}$/.test(lottoNumbers)) {
+        throw new Error('[ERROR] 올바른 로또 번호를 입력해 주세요 (1부터 45까지의 숫자, 쉼표로 구분된 6개 숫자).');
       }
+      this.lottoNumbers = lottoNumbers.split(',').map(num => parseInt(num.trim()));
+      this.checkForDuplicates(this.lottoNumbers);
+
+      return this.lottoNumbers;
+    } catch (error) {
+      Console.print(error.message);
+      return this.userNumber(); // 재귀 호출
     }
   }
 
   async userBonusNumber() {
-    while (true) {
-      try {
-        Console.print(`\n보너스 번호를 입력해 주세요.`);
-        const input = await Console.readLineAsync('');
-        const bonusNumber = parseInt(input);
-        if (isNaN(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
-          throw new Error('[ERROR]올바른 보너스 번호를 입력해 주세요 (1부터 45까지의 숫자).');
-        }
-        const allNumbers = [...this.lottoNumbers, bonusNumber];
-        this.checkForDuplicates(allNumbers);
-
-        return bonusNumber;
-      } catch (error) {
-        Console.print(error.message);
+    try {
+      Console.print(`\n보너스 번호를 입력해 주세요.`);
+      const input = await Console.readLineAsync('');
+      const bonusNumber = parseInt(input);
+      if (isNaN(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
+        throw new Error('[ERROR]올바른 보너스 번호를 입력해 주세요 (1부터 45까지의 숫자).');
       }
+      const allNumbers = [...this.lottoNumbers, bonusNumber];
+      this.checkForDuplicates(allNumbers);
+      if (!/^\d+$/.test(input) || parseInt(input) < 1 || parseInt(input) > 45) {
+        throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
+      }
+      return bonusNumber;
+    } catch (error) {
+      Console.print(error.message);
+      return this.userBonusNumber(); // 재귀 호출
     }
   }
 
@@ -72,7 +68,6 @@ class Input {
       throw new Error('[ERROR]로또 번호에 중복된 숫자가 있습니다.');
     }
   }
-
 }
 
 export default Input;
