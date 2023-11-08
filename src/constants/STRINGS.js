5f55b7c2a4fe292a70502a357877fe5b28b9d4af
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-08 13:56:08 +0900

    chores: 상수 파일 수정

--- src/constants/STRINGS.js
+++ src/constants/STRINGS.js
@@ -1,5 +1,5 @@
 const STRINGS = Object.freeze({
-  PAY_INPUT: '구입금액을 입력해 주세요.',
+  PAY_INPUT: '구입금액을 입력해 주세요.\n',
   PAY_RESULT: (quantity) => `\n${quantity}개를 구매했습니다.`,
   ANSWER_NUMBERS: '당첨 번호를 입력해 주세요. \n',
   BONUS_NUMBER: '보너스 번호를 입력해 주세요. \n',
@@ -12,6 +12,7 @@ const STRINGS = Object.freeze({
   RATE_OF_RETURN: (rateOfReturn) => `총 수익률은 ${rateOfReturn}%입니다.\n`,
 
   ERROR_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
+  ERROR_UNIT: '[ERROR] 구입 금액은 1,000원 단위만 가능합니다.',
   ERROR_NAN: '[ERROR] 숫자만 입력 가능합니다.',
   ERROR_RANGE: '[ERROR] 1부터 45까지의 숫자만 입력 가능합니다.',
   ERROR_REPEATED: '[ERROR] 각 로또에서 번호는 중복 입력 불가합니다.',
