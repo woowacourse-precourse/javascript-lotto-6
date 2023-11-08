3ae61da8149c362a44c685f8b8669c194f187273
j-nary <dlwls0921@naver.com>
2023-11-08 22:29:42 +0900

    fix: 함수 15줄 넘기지 않기

--- src/App.js
+++ src/App.js
@@ -1,52 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { userPriceInput, userWinningInput, userBonusInput } from './input.js';
-import { printCnt, printLottoNumber } from './print.js';
-import { printWinningList, printROI } from './result.js';
-import { checkWinning } from './utils.js';
+import { userPriceInput, userWinningInput, userBonusInput } from "./input.js";
+import { printCnt, printLottoNumber } from "./print.js";
+import { printWinningList, printROI } from "./result.js";
+import { checkWinning } from "./utils.js";
 import Lotto from "./Lotto.js";
 
 class App {
   async play() {
-    let count, change, winNumber;
-    while (true) {
-      try {
-        [count, change] = await userPriceInput();
-        break;
-      } catch (error) {
-        MissionUtils.Console.print(error.message);
-        // throw error;
-      }
-    }
-
+    const [count, change] = await userPriceInput();
     printCnt(count, change);
     const lottos = generateLottos(count);
     printLottoNumber(lottos);
-
-    while (true) {
-      try {
-        winNumber = await userWinningInput();
-        break;
-      } catch (error) {
-        MissionUtils.Console.print(error.message);
-        // throw error;
-      }
-    }
-
-    let bonusNumber;
-    while (true) {
-      try {
-        bonusNumber = await userBonusInput();
-        break;
-      } catch (error) {
-        MissionUtils.Console.print(error.message);
-      }
-    }
-
+    const winNumber = await userWinningInput();
+    const bonusNumber = await userBonusInput();
     const winList = checkAllLottos(lottos, winNumbers, bonusNumber);
     printWinningList(winList);
     printROI(winList, count);
   }
 }
-  
 
 export default App;
