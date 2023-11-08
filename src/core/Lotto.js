5fd17e6ba5e1c1bb9123e73b710ebde4a9dfde59
fecapark <fecapark@gmail.com>
2023-11-08 15:14:35 +0900

    feat: 로또 번호를 리턴하는 메소드 구현 완료

--- src/core/Lotto.js
+++ src/core/Lotto.js
@@ -1,18 +1,35 @@
+//@ts-check
+
+import Assert from "./Assert";
+
 class Lotto {
   #numbers;
 
+  /**
+   *
+   * @param {number[]} numbers
+   */
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort();
   }
 
+  /**
+   *
+   * @param {number[]} numbers
+   */
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    const assert = new Assert();
+    assert.assertWinningNumber(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  /**
+   *
+   * @returns {number[]}
+   */
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
