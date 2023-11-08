18b65d660c443c58cef037d519408fe829c8c515
skiende74 <skiende74@gmail.com>
2023-11-08 18:18:31 +0900

    feat(App): GameResult 등 클래스 사용방법에따라 App.play() 작성
    - LottoPurchaseInput, WinningNumberInput, GameResult를 사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import GameResult from "./GameResult.js";
 import LottoPurchase from "./LottoPurchase.js";
+import LottoPurchaseInput from "./LottoPurchaseInput.js";
 import WinningNumberInput from "./WinningNumberInput.js";
 
 class App {
   async play() {
-    const lottos = await LottoPurchase.buyWithUserInput();
+    const lottoPurchase = await LottoPurchaseInput.collectMoney();
+    const lottos = lottoPurchase.generateLottos();
     LottoPurchase.print(lottos);
-    await WinningNumberInput.collect();
+
+    const [winningNumberStr, bonusNumberStr] =
+      await WinningNumberInput.collect();
+    const winningNumber = App.parseWinningNumber(winningNumberStr);
+    const bonusNumber = Number(bonusNumberStr);
+
+    const gameResult = new GameResult(lottos, winningNumber, bonusNumber);
+    gameResult.show();
+  }
+
+  static parseWinningNumber(numbersStr) {
+    return numbersStr.split(",").map((strNum) => Number(strNum));
   }
 }
 
