e9d002e204b7736892e7a40597179a0b8ffec914
puranium <puranium235@gmail.com>
2023-11-08 12:33:54 +0900

    style: 출력에 줄바꿈 추가

--- src/MESSAGE.js
+++ src/MESSAGE.js
@@ -1,12 +1,12 @@
 const MESSAGE = Object.freeze({
-  enterPrice: '구입금액을 입력해 주세요.',
-  enterWinNumbers: '당첨 번호를 입력해 주세요.',
-  enterBonusNumber: '보너스 번호를 입력해 주세요.',
+  enterPrice: '구입금액을 입력해 주세요.\n',
+  enterWinNumbers: '\n당첨 번호를 입력해 주세요.\n',
+  enterBonusNumber: '\n보너스 번호를 입력해 주세요.\n',
   purchasedLotto(amount) {
-    return `${amount}개를 구매했습니다.`;
+    return `\n${amount}개를 구매했습니다.`;
   },
   winningDetails(match) {
-    return `당첨 통계
+    return `\n당첨 통계
 ---
 3개 일치 (5,000원) - ${match[3]}개
 4개 일치 (50,000원) - ${match[4]}개
