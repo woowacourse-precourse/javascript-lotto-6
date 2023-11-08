ef8a23b00407e73b8834b93b82558b058fca2d7c
gilgor2 <misby0327@gmail.com>
2023-11-08 13:54:02 +0900

    refactor(Validator.js): 단일 객체를 클래스와 정적 메서드로 변경
    문법적 통일과 확장성을 고려해 validator객체를 클래스와 정적 메소드로 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,53 +1,57 @@
-const checkIsNaturalNumber = (str) => {
-	return /r'[1-9]\d*'/.test(str);
-};
+class Validator {
+	static checkIsNaturalNumber(str) {
+		return /r'[1-9]\d*'/.test(str);
+	}
 
-const checkIsDividedByLottoPrice = (str) => {
-	return parseInt(str, 10) % 1000 === 0;
-};
+	static checkIsDividedByLottoPrice(str) {
+		return parseInt(str, 10) % 1000 === 0;
+	}
 
-const checkNoDupSize = (array, expectedSize) => {
-	const set = new Set(array);
-	return set.size === expectedSize && array.length === expectedSize;
-};
+	static checkNoDupSize(array, expectedSize) {
+		const set = new Set(array);
+		return set.size === expectedSize && array.length === expectedSize;
+	}
 
-const checkAllNumberInRange = (array, start, end) => {
-	const isNotValid = array.some((str) => {
-		if (!checkIsNaturalNumber(str)) {
-			return true;
-		}
-		return parseInt(str, 10) < start || parseInt(str, 10) > end;
-	});
+	static checkAllNumberInRange(array, start, end) {
+		const isNotValid = array.some((str) => {
+			if (!Validator.checkIsNaturalNumber(str)) {
+				return true;
+			}
+			return parseInt(str, 10) < start || parseInt(str, 10) > end;
+		});
 
-	return !isNotValid;
-};
+		return !isNotValid;
+	}
 
-const Validator = {
-	checkBudgetValidity(str) {
-		const isStrNaturalNumber = checkIsNaturalNumber(str);
+	static checkBudgetValidity(str) {
+		const isStrNaturalNumber = Validator.checkIsNaturalNumber(str);
 		if (!isStrNaturalNumber) {
 			throw new Error('[ERROR] 자연수를 입력해주세요.');
 		}
 
-		const isStrDivideBy1000 = checkIsDividedByLottoPrice(str);
+		const isStrDivideBy1000 = Validator.checkIsDividedByLottoPrice(str);
 		if (!isStrDivideBy1000) {
 			throw new Error('[ERROR] 1000의 배수를 입력해주세요.');
 		}
 
 		return true;
-	},
+	}
 
-	checkWinningNumberValidity(str) {
+	static checkWinningNumberValidity(str) {
 		const winningNumberArray = str?.split(',');
 
-		const isArraySizeCorrect = checkNoDupSize(winningNumberArray, 6);
+		const isArraySizeCorrect = Validator.checkNoDupSize(winningNumberArray, 6);
 		if (!isArraySizeCorrect) {
 			throw new Error(
 				`[ERROR] ${6}개의 숫자를 입력해주세요. 중복은 허용되지 않습니다.`,
 			);
 		}
 
-		const isAllNumberInRange = checkAllNumberInRange(winningNumberArray, 1, 45);
+		const isAllNumberInRange = Validator.checkAllNumberInRange(
+			winningNumberArray,
+			1,
+			45,
+		);
 		if (!isAllNumberInRange) {
 			throw new Error(
 				`[ERROR] ${1}에서 ${45}사이의 숫자를 입력해주세요.`,
@@ -55,15 +59,15 @@ const Validator = {
 		}
 
 		return true;
-	},
+	}
 
-	checkBonusNumberValidity(str) {
-		const isStrNaturalNumber = checkIsNaturalNumber(str);
+	static checkBonusNumberValidity(str) {
+		const isStrNaturalNumber = Validator.checkIsNaturalNumber(str);
 		if (!isStrNaturalNumber) {
 			throw new Error('[ERROR] 자연수를 입력해주세요.');
 		}
 
 		return true;
-	},
-};
+	}
+}
 export default Validator;
