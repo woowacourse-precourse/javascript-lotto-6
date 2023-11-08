240bf7695f9365fddfb402683026a22f104c61b1
sunub <bsc5672@gmail.com>
2023-11-08 18:21:47 +0900

    refactor : 기존의 코드에서 가독성을 높이고 try catch를 통해서 에러 핸들링이 이루어지는지 App을 통해서도 알 수 있게끔 리펙토링 했다.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TicketMachine from './Controller/TicketMachine.js';
 import WinStastics from './Controller/WinStastics.js';
 import Printer from './View/Printer.js';
 import LottoTicket from './Model/LottoTicket.js';
+import Interface from './View/Interface.js';
 
 class App {
   constructor() {
@@ -13,15 +14,63 @@ class App {
   }
 
   async play() {
-    await this.ticketMachine.buyTicket();
-    Printer.printLottoTicket(this.lottoTicket.ticket);
+    // 로또 티켓 구매
+    await this.buyTicket();
 
-    await this.lottoMachine.createWinNum();
-    await this.lottoMachine.createBonusNum();
+    // 당첨번호와 보너스 번호 설정
+    await this.setWinngNumber();
+    await this.setBonusNumber();
 
+    // 티켓과 당첨번호를 토대로 당첨 통계를 설정한다.
+    this.setWinningstatistics();
+    Printer.printStatistics(this.winStastics.statistics);
+  }
+
+  async buyTicket() {
+    try {
+      await this.ticketMachine.buyTicket();
+      Printer.printLottoTicket(this.lottoTicket.ticket);
+    } catch (error) {
+      await this.buyTicketErrorHandler(error);
+    }
+  }
+
+  async buyTicketErrorHandler(error) {
+    Printer.printBuyTicketErrorMessage(error);
+    await this.buyTicket();
+  }
+
+  async setWinngNumber() {
+    try {
+      const winNums = await Interface.requestWinNums();
+      this.lottoMachine.createWinNum(winNums);
+    } catch (error) {
+      await this.winNumsErrorHandler(error);
+    }
+  }
+
+  async winNumsErrorHandler(error) {
+    Printer.printWinNumsErrorMessage(error);
+    await this.setWinngNumber();
+  }
+
+  async setBonusNumber() {
+    try {
+      const bonusNum = await Interface.requestBonusNum();
+      this.lottoMachine.createBonusNum(bonusNum);
+    } catch (error) {
+      await this.bonusNumErrorHandler(error);
+    }
+  }
+
+  async bonusNumErrorHandler(error) {
+    Printer.printBonusNumErrorMessage(error);
+    await this.setBonusNumber();
+  }
+
+  setWinningstatistics() {
     this.winStastics.setMatchesCount();
     this.winStastics.setEarningRate();
-    Printer.printStastics(this.winStastics.statics);
   }
 }
 
