bcb35b3bbba6ea3c43e84d4424abd696f3f47cec
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-11-08 22:05:32 +0900

    refactor: App 클래스 메서드 분리

--- src/App.js
+++ src/App.js
@@ -3,32 +3,38 @@ import System from "./System";
 import ConsolePrint from "./views/ConsolePrint";
 
 class App {
+  constructor() {
+    this.moneyInstance = new Money();
+    this.system = new System();
+    this.consolePrint = new ConsolePrint();
+  }
+
   async play() {
-    const moneyInstance = new Money();
-    await moneyInstance.initialize();
-    const money = moneyInstance.getMoney();
-
-    const system = new System();
-    const consolePrint = new ConsolePrint();
-
-    const lottoCount = await moneyInstance.getLottoCount();
-    const lottos = await system.purchaseLottos(lottoCount);
-
-    // 로또 구매결과 출력
-    consolePrint.printLottos(lottos);
-    // 당첨 번호 입력
-    const lotto = await system.getLotto();
-    const bonusNumber = await system.getBonusNumber(lotto);
-    const winningNumbers = lotto.getNumbers();
-    const winningCounts = system.getWinningCounts(
+    await this.moneyInstance.initialize();
+    const money = this.moneyInstance.getMoney();
+    const lottoCount = await this.moneyInstance.getLottoCount();
+    const lottos = await this.system.purchaseLottos(lottoCount);
+    const { lotto, bonusNumber } = await this.getLottoAndBonusNumber();
+    const winningCounts = this.system.getWinningCounts(
       lottos,
-      winningNumbers,
+      lotto.getNumbers(),
       bonusNumber
     );
-    consolePrint.printPrize(winningCounts);
-    const prize = system.calculateTotalWinningMoney(winningCounts);
-    const percentage = system.calculateProfitPercentage(money, prize);
-    consolePrint.printPercentage(percentage);
+    const prize = this.system.calculateTotalWinningMoney(winningCounts);
+    const percentage = this.system.calculateProfitPercentage(money, prize);
+    this.printResults(lottos, winningCounts, percentage);
+  }
+
+  async getLottoAndBonusNumber() {
+    const lotto = await this.system.getLotto();
+    const bonusNumber = await this.system.getBonusNumber(lotto);
+    return { lotto, bonusNumber };
+  }
+
+  printResults(lottos, winningCounts, percentage) {
+    this.consolePrint.printLottos(lottos);
+    this.consolePrint.printPrize(winningCounts);
+    this.consolePrint.printPercentage(percentage);
   }
 }
 
