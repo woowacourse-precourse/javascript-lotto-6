7c40d2b8cb74fb421a323d9a2b9c16387f5e5229
areumH <94692566+areumH@users.noreply.github.com>
2023-11-08 17:55:22 +0900

    test: 로또 클래스 테스트 케이스 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from "../src/domain/Lotto";
+import WinningLotto from "../src/domain/WinningLotto";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -21,13 +22,13 @@ describe("로또 클래스 테스트", () => {
 
   test("보너스 번호가 로또 번호와 중복이면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6]).getBonus(6);
+      new WinningLotto([1, 2, 3, 4, 5, 6]).getBonus(6);
     }).toThrow("[ERROR]");
   });
 
   test("보너스 번호가 로또 번호의 범위가 아니면 예외가 발생한다.", () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6]).getBonus(46);
+      new WinningLotto([1, 2, 3, 4, 5, 6]).getBonus(46);
     }).toThrow("[ERROR]");
   });
 });
