4b485d71ccb2aa010a30c4545b2b3b9f16b7f906
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-08 14:04:45 +0900

    feat: toFixed 추가

--- __tests__/LottoStatisticsTest.js
+++ __tests__/LottoStatisticsTest.js
@@ -13,8 +13,8 @@ const mockRandoms = (numbers) => {
 
 describe('당첨 내역 및 통계 출력 테스트', () => {
   test.each([
-    [1000, [7,8,9,10,11,13], [1,2,3,4,5,6], 12,  0, 0.0],
-    [1000, [1,2,3,4,8,9], [1,2,3,4,5,6], 12, 50000, 50.0],
+    [1000, [7,8,9,10,11,13], [1,2,3,4,5,6], 12, 0, 0],
+    [1000, [1,2,3,4,8,9], [1,2,3,4,5,6], 12, 50000, 50],
   ])(
     "로또 한 장 테스트 - 이 테스트에 대한 구입 금액은 %s원, 발행된 로또는 %s, 당첨 번호는 %s, 보너스 번호는 %s, 당첨 금액은 %s, 수익률은 %s%여야한다.",
     (money, randoms, winnings, bonus, expectedPrizeMoney, expectedStats) => {
@@ -27,11 +27,12 @@ describe('당첨 내역 및 통계 출력 테스트', () => {
       const stats = new Lotto(winnings).calculateWinningStats(tickets, [...winningNumbers, bonusNumber]);
       const profit = new Lotto(winnings).getProfits(stats)
       const rate = new Lotto(winnings).calculateRate(profit, money)
+      console.log(`rate: ${rate}`)
 
       expect(winningNumbers).toEqual(winnings)
       expect(bonusNumber).toEqual(bonus)
       expect(profit).toEqual(expectedPrizeMoney)
-      expect(rate).toEqual(expectedStats)
+      expect(String(rate.toFixed(1))).toEqual(String(expectedStats.toFixed(1)))
     },
   );
 
@@ -52,6 +53,7 @@ describe('당첨 내역 및 통계 출력 테스트', () => {
       const stats = new Lotto(winnings).calculateWinningStats(tickets, [...winningNumbers, bonusNumber]);
       const profit = new Lotto(winnings).getProfits(stats)
       const rate = new Lotto(winnings).calculateRate(profit, money)
+      console.log(`rate: ${rate}`)
 
       expect(winningNumbers).toEqual(winnings)
       expect(bonusNumber).toEqual(bonus)
