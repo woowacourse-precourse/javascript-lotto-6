c6eeea0da1a981b54f4addcae5abfe9a86a07d90
Hchan2 <seongchan_@naver.com>
2023-11-08 22:42:53 +0900

    Fix: 로또 게임 결과 출력 들어쓰기 정리

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -19,15 +19,15 @@ const OutputView = {
 
     printLottoGameResult(lottoResult, totalPer) {
         return `
-        당첨 통계
-        ---
-        3개 일치 (5,000원) - ${lottoResult.three.count}개
-        4개 일치 (50,000원) - ${lottoResult.four.count}개
-        5개 일치 (1,500,000원) - ${lottoResult.five.count}개
-        5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult.fiveBonus.count}개
-        6개 일치 (2,000,000,000원) - ${lottoResult.six.count}개
-        총 수익률은 ${totalPer}%입니다.
-    `;
+            당첨 통계
+            ---
+            3개 일치 (5,000원) - ${lottoResult.three.count}개
+            4개 일치 (50,000원) - ${lottoResult.four.count}개
+            5개 일치 (1,500,000원) - ${lottoResult.five.count}개
+            5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult.fiveBonus.count}개
+            6개 일치 (2,000,000,000원) - ${lottoResult.six.count}개
+            총 수익률은 ${totalPer}%입니다.
+        `;
     }
 }
 
