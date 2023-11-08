1e536efa34d43e018c8c87fb28c0282d95a8833c
ChoiYongWon <yongwon0824@naver.com>
2023-11-08 18:00:41 +0900

    feat: 로또 번호 입력 예외처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,9 +7,11 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) throw Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers.filter(n=>!Number.isNaN(n)).length != 6) throw Error("[ERROR] 로또 번호는 숫자 형식이어야 합니다.");
+    if (numbers.filter(n=>Number.isInteger(n)).length != 6) throw Error("[ERROR] 로또 번호는 정수 형식이어야 합니다.");
+    if (numbers.filter(n=>n <= 45 && n >= 1).length != 6) throw Error("[ERROR] 로또 번호는 1에서 45사이여야 합니다.");
+    if (new Set(numbers).size != 6) throw Error("[ERROR] 로또 번호는 중복되면 안됩니다.");
   }
 
   // TODO: 추가 기능 구현
