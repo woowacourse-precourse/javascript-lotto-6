73b1730357a8a416f49305e26aee33e8ef0fed45
yeong-hwan <jghff700@naver.com>
2023-11-08 10:20:48 +0900

    refactor: 안내 메세지 출력 객체에 당첨 결과 object를 추가

--- src/domain/utils/display.js
+++ src/domain/utils/display.js
@@ -1,8 +1,14 @@
 import IssuedLottInfo from '../info/IssuedLottoInfo.js';
+import WinningStatisticsInfo from '../info/WinningStatisticsInfo.js';
 
 const display = {
   issedLottoInfo: ({ count, lottos }) => {
     const info = new IssuedLottInfo({ count, lottos });
+    info.printInfoMessage();
+  },
+
+  winningStatisticsInfo: ({ lottoGenerator, lotto, bonus }) => {
+    const info = new WinningStatisticsInfo({ lottoGenerator, lotto, bonus });
     info.printInfoMessage();
   },
 };
