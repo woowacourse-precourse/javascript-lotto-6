631b5d37bc0b8dcbbfd4cc789a0846d49df0ba04
FordangIT <fordang0819@gmail.com>
2023-11-08 23:37:12 +0900

    docs: 파일 이름 조정

---
+++ __tests__/CustomerTest.js
@@ -0,0 +1,48 @@
+import Customer from "../src/game/Customer";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("고객 클래스 테스트", () => {
+  // TEST 이후, cntLottoTicekts변수를 private로 변경함.
+  /*
+  test("지불한 금액만큼 로또를 구입", () => {
+    mockRandoms([[1, 2, 3, 4, 5, 6], [11, 12, 13, 14, 15, 16]]);
+    const customer = new Customer(2000);
+    const result = customer.cntLottoTicekts;
+
+    expect(result).toEqual(2);
+  });
+  */
+
+  test("구입한 로또만큼 리스트에 저장", () => {
+    const logSpy = getLogSpy();
+
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [11, 12, 13, 14, 15, 16],
+    ]);
+    const customer = new Customer(2000);
+    customer.buyLottoTickets();
+    const logs = [
+      "2개를 구매했습니다.",
+      "[1, 2, 3, 4, 5, 6]",
+      "[11, 12, 13, 14, 15, 16]",
+    ];
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+});
---
+++ __tests__/FunctionTest.js
@@ -0,0 +1,25 @@
+describe("내장함수 테스트", () => {
+  test("문자열을 split했을 때 리스트 원소 타입 확인", () => {
+    const input = "1,2,3,4,5,6";
+    const list = input.split(",");
+    const result = list[1] + list[2];
+
+    expect(result).toEqual("23");
+  });
+
+  test("문자열을 split했을 때 숫자로 인식", () => {
+    const input = "1,2,3,4,5,6";
+    const list = input.split(",").map((str) => parseInt(str));
+    const result = list[1] + list[2];
+
+    expect(result).toEqual(5);
+  });
+
+  test("리스트 합치기", () => {
+    const input1 = [1, 2, 3, 4];
+    const input2 = 5;
+    const result = [...input1, input2];
+
+    expect(result).toContainEqual(1, 2, 3, 4, 5);
+  });
+});
