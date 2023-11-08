e109ad481b8bdf51285bfdacbdf0a2ea2020b717
ImxYJL <allensain14@gmail.com>
2023-11-08 15:20:07 +0900

    feat: 로또 당첨 결과 출력 기능 구현

--- src/lotto/LottoGame.js
+++ src/lotto/LottoGame.js
@@ -1,6 +1,12 @@
 import LottoCenter from './LottoCenter.js';
 import LottoGameHost from './LottoGameHost.js';
 import LottoStore from './LottoStore.js';
+import {
+  LOTTO_CONSTANT,
+  LOTTO_RANK,
+  FORMATTER,
+  MESSAGE,
+} from '../constant/constant.js';
 
 class LottoGame {
   lottoStore = null;
@@ -18,10 +24,41 @@ class LottoGame {
     this.lottoCenter.tryPrintAllLottoNumbers();
   }
 
+  printResults(lottoResultsList) {
+    LOTTO_CONSTANT.reverseRankList.forEach((rank) => {
+      const matchedNumber = LOTTO_RANK[rank].matchedNumber;
+      const bonudNumberText = LOTTO_RANK[rank].bonudNumberText;
+      const reward = LOTTO_RANK[rank].rewardText;
+      const count = lottoResultsList[rank];
+
+      print(
+        FORMATTER.resultsPrintFormatter(
+          matchedNumber,
+          bonudNumberText,
+          reward,
+          count,
+        ),
+      );
+    });
+  }
+
+  getAndPrintResults() {
+    this.lottoCenter.inspectLottoWinningStatus(
+      this.lottoGameHost.getWinningNumbers(),
+      this.lottoGameHost.getBonusNumber(),
+    );
+
+    const lottoResultsList = this.lottoCenter.getLottoResultsList();
+    print(MESSAGE.titleForResults);
+    this.printResults(lottoResultsList);
+  }
+
   async playLottoGame() {
     await this.buyAndIssueLottos();
-    
+
     await this.lottoGameHost.setLottoWinningNumbers();
+
+    this.getAndPrintResults();
   }
 }
 
