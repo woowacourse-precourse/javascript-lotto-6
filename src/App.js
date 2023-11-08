20670e1e8b7da0bb1f81a98cefc801dbfe918df0
rkdrjsrks <rkdrjs71@naver.com>
2023-11-08 14:04:28 +0900

    MVC 패턴을 적용하여 리팩토링을 진행함.

--- src/App.js
+++ src/App.js
@@ -1,157 +1,16 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
 import Lotto from "./Lotto.js";
+import LottoController from "./LottoController.js";
+import LottoView from "./LottoView.js";
+import LottoModel from "./LottoModel.js";
+
 
 class App {
     async play() {
-        const purchaseAmount = await this.buyLottoTickets();
-
-        const lottos = this.generateLottos(purchaseAmount);
-        this.displayLottoNumbers(lottos);
-
-        const winningNumbers = await this.inputWinningNumbers();
-        const bonusNumber = await this.inputBonusNumber();
-
-        const results = this.calculateResults(lottos, winningNumbers, bonusNumber);
-
-        this.printResults(results);
-        this.printTotalEarnings(purchaseAmount, results);
-    }
-
-    async buyLottoTickets() {
-        //TODO : 로또 구입 금액을 입력 받는다.
-        try {
-            const userInput = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
-            if (isNaN(userInput) || userInput === null || userInput.includes(" ")) {
-                throw new Error(`[ERROR] 잘못된 숫자 입력입니다.`);
-            }
-
-            const purchaseAmount = parseInt(userInput);
-            if (purchaseAmount % 1000 !== 0 || purchaseAmount <= 0) {
-                throw new Error(`[ERROR] 숫자는 1000이상 입력해야 합니다.`);
-            }
-            MissionUtils.Console.print("");
-            return purchaseAmount;
-        } catch (error) {
-            MissionUtils.Console.print(`[ERROR] 잘못된 구입금액 입력입니다.`);
-            //throw error;
-        }
-    }
-
-    generateLottos(purchaseAmount) {
-        //TODO : 로또 티켓의 최대 구매 가능 개수만큼 랜덤한 로또 번호 배열을 만들고 반환한다.
-        const maxTickets = this.getMaxPurchasedTickets(purchaseAmount);
-        return Array.from({length: maxTickets}, () => new Lotto(this.generateRandomNumbers()));
-    }
-
-    getMaxPurchasedTickets(purchaseAmount) {
-        return Math.floor(purchaseAmount / 1000);
-    }
-
-    generateRandomNumbers() {
-        //TODO : 랜덤한 값을 반환
-        return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-    }
-
-    async inputWinningNumbers() {
-        try {
-            //TODO : 당첨 번호를 입력 받는다.
-            let userInput = await MissionUtils.Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
-            const winningNumbers = userInput.split(',').map(Number);
-
-            if (!Array.isArray(winningNumbers) || winningNumbers.length !== 6 || winningNumbers.includes(null) || winningNumbers.includes("") || winningNumbers.includes(" ") || winningNumbers.includes(NaN)) {
-                throw new Error(`[ERROR] 잘못된 당첨 번호 입력입니다.`);
-            }
-
-            MissionUtils.Console.print("");
-            return winningNumbers;
-        } catch (error) {
-            MissionUtils.Console.print(`[ERROR] 잘못된 당첨 번호 입력입니다.`);
-            //throw error;
-        }
-    }
-
-    async inputBonusNumber() {
-        try {
-            //TODO : 보너스 번호를 입력 받는다.
-            const userInput = await MissionUtils.Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
-            const bonusNumber = parseInt(userInput);
-
-            if (isNaN(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
-                throw new Error("[ERROR] 잘못된 보너스 번호 입력입니다.");
-            }
-
-            MissionUtils.Console.print("");
-            return bonusNumber;
-        } catch (error) {
-            MissionUtils.Console.print(`[ERROR] 잘못된 보너스 번호 입력입니다.`);
-            //throw error;
-        }
-    }
-
-    displayLottoNumbers(lottos) {
-        //TODO : 발행한 로또 수량 및 번호를 출력한다.
-        MissionUtils.Console.print(`${lottos.length}개를 구매했습니다.`);
-        lottos.map(lotto => lotto.displayNumbers());
-        MissionUtils.Console.print("");
-    }
-
-    calculateResults(lottos, winningNumbers, bonusNumber) {
-        const results = lottos.map(lotto => this.calculateSingleLottoResult(lotto, winningNumbers, bonusNumber));
-        return this.countResults(results);
-    }
-
-    calculateSingleLottoResult(lotto, winningNumbers, bonusNumber) {
-        const matchCount = lotto.winningNumbersCount(winningNumbers);
-        const hasBonusMatch = lotto.bonusMatch(bonusNumber);
-
-        if (matchCount === 5 && hasBonusMatch) {
-            return 5.1;
-        } else if (matchCount >= 3) {
-            return matchCount;
-        } else {
-            return 0;
-        }
-    }
-
-    countResults(results) {
-        const countResults = {3: 0, 4: 0, 5: 0, 5.1: 0, 6: 0};
-
-        for (const result of results) {
-            countResults[result] += 1;
-        }
-
-        return countResults;
-    }
-
-    printResults(countResults) {
-        //TODO : 당첨 내역을 출력한다.
-        MissionUtils.Console.print("당첨 통계\n---");
-        MissionUtils.Console.print("3개 일치 (5,000원) - " + countResults[3] + "개");
-        MissionUtils.Console.print("4개 일치 (50,000원) - " + countResults[4] + "개");
-        MissionUtils.Console.print("5개 일치 (1,500,000원) - " + countResults[5] + "개");
-        MissionUtils.Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countResults[5.1] + "개");
-        MissionUtils.Console.print("6개 일치 (2,000,000,000원) - " + countResults[6] + "개");
-    }
-
-    calculateTotalPrize(countResults) {
-        return countResults[3] * 5000 + countResults[4] * 50000 + countResults[5] * 1500000 + countResults[5.1] * 30000000 + countResults[6] * 2000000000;
-    }
-
-    calculateTotalEarningsRate(totalPrize, purchaseAmount) {
-        return (totalPrize / purchaseAmount) * 100;
-    }
-
-    printTotalEarningsRate(totalEarningsRate) {
-        MissionUtils.Console.print(`총 수익률은 ${totalEarningsRate.toFixed(1)}%입니다.`);
-    }
-
-    printTotalEarnings(purchaseAmount, countResults) {
-        //TODO : 총 수익률을 출력한다.
-
-        const totalPrize = this.calculateTotalPrize(countResults);
-        const totalEarningsRate = this.calculateTotalEarningsRate(totalPrize, purchaseAmount);
-
-        this.printTotalEarningsRate(totalEarningsRate);
+        const model = new LottoModel();
+        const view = new LottoView();
+        const controller = new LottoController(model, view);
+        await controller.play();
     }
 }
 
