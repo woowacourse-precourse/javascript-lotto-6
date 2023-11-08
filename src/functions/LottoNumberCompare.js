61f1c7c875a8c1cd067111dc5e215c4dc3504ba2
김태희 <>
2023-11-08 21:46:48 +0900

    fix: LottoNumberCompare 수정
    LottoNumberCompare class를 각 함수로 분리하여 Lotto class에서 호출할 수 있도록 수정함.

--- src/functions/LottoNumberCompare.js
+++ src/functions/LottoNumberCompare.js
@@ -1,40 +1,51 @@
-class LottoNumberCompare {
+import { Console } from "@woowacourse/mission-utils";
 
-	constructor(lotto, winNumber, bonusNumber) {
-		this.lotto = lotto;
-		this.winNumber = winNumber;
-		this.bonusNumber = bonusNumber;
-	}
+function compare(lotto, winNumber) {
+	let NumberCount = 0;
 
-	compare() {
-		const winNumberIncludeBonus = [...this.winNumber];
-
-		let winRank = [];
-
-		this.lotto.forEach(innerArr => {
-			let intersect = innerArr.filter(x => winNumberIncludeBonus.includes(x));
-			let intersectLength = intersect.length;
-
-			if (innerArr.includes(this.bonusNumber) && intersectLength === 5) winRank[1] += 1;
-			else if (intersectLength === 5) winRank[2] += 1;
-
-			switch (intersectLength) {
-				case 3:
-					winRank[4] += 1;
-					break;
-
-				case 4:
-					winRank[3] += 1;
-					break;
-
-				case 6:
-					winRank[0] += 1;
-					break;
-			}
-		});
-
-		return winRank;
-	}
+	winNumber.forEach(number => {
+		if (lotto.includes(Number(number))) {
+			NumberCount += 1;
+		}
+	});
+	return NumberCount
 }
 
-export default LottoNumberCompare;
+function rank(bonusNumber, lotto, NumberCount, winRank) {
+	switch (NumberCount) {
+		case 3:
+			winRank[4] += 1;
+			break;
+
+		case 4:
+			winRank[3] += 1;
+			break;
+
+		case 5:
+			if (lotto.includes(bonusNumber)) {
+				winRank[1] += 1;
+			}
+			winRank[2] += 1;
+			break;
+
+		case 6:
+			winRank[0] += 1;
+			break;
+	}
+
+	return winRank;
+}
+
+export function lottoNumberCompare(lotto, winNumber, bonusNumber) {
+
+	let winRank = new Array(5).fill(0);
+
+	lotto.map(innerArr => {
+		const numberCount = compare(lotto, winNumber);
+		winRank = rank(bonusNumber, innerArr, numberCount, winRank);
+	});
+
+	Console.print(winRank);
+
+	return winRank;
+};
