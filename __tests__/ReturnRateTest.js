51e8b3e33636fb97ed15f6513018c078479c00d3
이용성 <ukal5006@kakao.com>
2023-11-08 21:45:33 +0900

    test(ReturnRateTest): 테스트 입력값 수정

--- __tests__/ReturnRateTest.js
+++ __tests__/ReturnRateTest.js
@@ -2,9 +2,9 @@ import ReturnRate from '../src/models/ReturnRate';
 
 describe('수익률 클래스 테스트', () => {
   test('작동 테스트', () => {
-    const lottoCount = 8;
+    const purchaseAmount = 8000;
     const lottoTicketResult = [1, 0, 0, 0, 0]; // 5등 1개
-    const returnRate = new ReturnRate(lottoCount, lottoTicketResult);
+    const returnRate = new ReturnRate(purchaseAmount, lottoTicketResult);
     const result = returnRate.getReturnRate();
     const answer = 62.5;
     expect(result).toEqual(answer);
