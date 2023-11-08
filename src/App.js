941d8bc49c22410037125ac453fee94e30f68d44
shimeeuisuk <euisuk95@gmail.com>
2023-11-08 22:07:02 +0900

    refactor: Input 함수 및 유효성 검사 로직 파일 분리

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 class App {
   async play() {}
-
-  async #getMoney() {
-    const input = Console.readLineAsync("구입금액을 입력해 주세요.");
-    const money = Number(input);
-    if (Number.isNaN(money)) {
-      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-    }
-    if (money % 1000 !== 0) {
-      throw new Error("1000원 단위로 입력해 주세요.");
-    }
-    return money;
-  }
 }
 
 export default App;
