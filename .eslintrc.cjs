4580fe982d5b7f4298e3b2bef14ab66547af3824
Lim-shweety <smile70200@naver.com>
2023-11-08 15:48:20 +0900

    :memo: docs: package.json 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,29 @@
 module.exports = {
-	env: {
-		es2021: true,
-		node: true,
-	},
-	extends: 'airbnb-base',
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: ['.eslintrc.{js,cjs}'],
-			parserOptions: {
-				sourceType: 'script',
-			},
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	rules: {
-		'max-depth': ['error', 2],
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'max-depth': ['error', 2],
 
-		'max-lines-per-function': ['error', { max: 15 }],
-	},
+    'max-lines-per-function': ['error', { max: 15 }],
+  },
 };
