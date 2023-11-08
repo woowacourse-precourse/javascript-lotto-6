576d61eb82ee36e80fa1c2b1cb0f622e70b6210e
donggyun <donggyunkim12@gmail.com>
2023-11-08 14:22:35 +0900

    test(DomainTest): 결과생성 - 당첨통계 계산 테스트 구현

---
+++ __tests__/ResultTest.spec.js
@@ -0,0 +1,41 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import LottoGame from '../src/controller/LottoGame.js';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+describe('결과 생성', () => {
+  test('당첨 통계 계산', async () => {
+    mockQuestions(['5000', '1,2,3,4,5,6', '7']);
+
+    mockRandoms([
+      [1, 2, 3, 4, 5, 43], // 5개 일치
+      [1, 2, 3, 4, 42, 43], // 4개 일치
+      [1, 2, 3, 4, 5, 7], // 5개+보너스번호 일치
+      [4, 5, 6, 35, 36, 44], // 3개 일치
+      [1, 2, 3, 4, 5, 6], // 6개 일치
+    ]);
+
+    const lottoGame = new LottoGame();
+
+    await lottoGame.buyLotto();
+    await lottoGame.drawLotto();
+    await lottoGame.result();
+
+    const rewardCount = lottoGame.getRewardCount();
+    expect(rewardCount).toEqual({ 3: 1, 4: 1, 5: 1, 6: 1, '5b': 1 });
+  });
+});
