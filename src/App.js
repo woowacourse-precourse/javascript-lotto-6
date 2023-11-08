6a94652f5f6da9c0b62e1b00f7ca0b36508dacd3
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-08 22:23:44 +0900

    feat: 구매 금액 입력 및 티켓 구매

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export async function purchaseTickets() {
+  try {
+    let purchaseAmount = await MissionUtils.Console.readLineAsync("구입 금액을 입력해 주세요.\n");
+    purchaseAmount = Number(purchaseAmount);
+    if (purchaseAmount % 1000 !== 0) {
+      throw new Error("[ERROR] 금액은 1000으로 나누어 떨어져야 합니다.");
+    }
+    return purchaseAmount;
+  } catch (error) {
+    MissionUtils.Console.print(error.message);
+    return await purchaseTickets();
+  }
+}
+
 class App {
   async play() {}
 }
