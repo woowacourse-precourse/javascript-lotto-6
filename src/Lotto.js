874590ead584080926eecf18291c54428c635781
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-08 22:41:53 +0900

    feat: 로또 번호 유효성 검사

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if(numbers.length !== new Set(numbers).size){
+      throw new Error("[ERROR] 1~45 사이의 중복되지 않는 6개의 숫자를 입력해주세요.");
+    }
+    if(numbers.some(number=>number<1) || numbers.some(number=>number>45)){
+      throw new Error("[ERROR] 1~45 사이의 숫자 형식의 입력만 가능합니다.")
+    }
   }
 
   // TODO: 추가 기능 구현
