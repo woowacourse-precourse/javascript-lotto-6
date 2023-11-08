7a85625980257c5dfaaf96a8699e68a24143e5d9
irenee-14 <ireneee145@gmail.com>
2023-11-08 22:56:58 +0900

    feature: 당첨확인 및 출력 init

---
+++ src/checkLotto.js
@@ -0,0 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
+function checkWinning(lottos, winNumbers, bonusNumber)
+{
+
+}
+
+function printResult(lottos, winNumbers, bonusNumber)
+{
+
+}
+
+export {checkWinning, printResult};
