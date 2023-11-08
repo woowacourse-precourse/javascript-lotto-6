3a6bcb15c1f8a5ad64f29e67280f506256aaf5d2
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-08 16:55:27 +0900

    feat: Lotto 클래스 구현
    필드:
    - numbers
    생성자:
    - constructor(numbers)
    메서드:
    - getLotto()

--- src/models/Lotto.js
+++ src/models/Lotto.js
@@ -1,18 +1,16 @@
+import LottoValiation from "../validations/LottoValidation.js";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    LottoValiation.checkLotto(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+  getLotto() {
+    return this.#numbers;
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
