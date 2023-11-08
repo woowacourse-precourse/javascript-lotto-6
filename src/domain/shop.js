5047653365c2860e95c328824554bb7be11e3007
apfp77 <apfp77@naver.com>
2023-11-08 20:41:50 +0900

    feat(shop): import 상수 파일 변경 및 로또 번호추출  함수 추가

--- src/domain/shop.js
+++ src/domain/shop.js
@@ -1,7 +1,8 @@
-import { LOTTO_PRICE, ZERO } from '../constants/lotto/numbers';
-import ValidationError from '../error/Validation';
+import { MissionUtils } from '@woowacourse/mission-utils';
 import validationUtils from '../services/utils/validation';
-import Lotto from './Lotto';
+import Lotto from '../Lotto';
+import { ZERO } from '../constants/validate';
+import { LOTTO_PRICE, PICK_NUMBERS, RANGE } from '../constants/lotto';
 
 const ERROR_PURCHASE_AMOUNT_ZERO = `[ERROR] Amount is zero or not divisible by ${LOTTO_PRICE}`;
 
@@ -11,23 +12,36 @@ const ERROR_PURCHASE_AMOUNT_ZERO = `[ERROR] Amount is zero or not divisible by $
  */
 export const isDivisibleByLottoPrice = (purchaseAmount) => {
   if (purchaseAmount === ZERO || purchaseAmount % LOTTO_PRICE !== ZERO) {
-    throw new ValidationError(`${ERROR_PURCHASE_AMOUNT_ZERO}`);
+    throw new Error(`${ERROR_PURCHASE_AMOUNT_ZERO}`);
   }
 };
 
 const shop = {
   /**
+   * 무작위 로또 번호를 반환한다
+   * @returns {number[]} - 무작위로 선별된 6자리 로또 번호
+   */
+  drawLottoNumbers: () => {
+    return MissionUtils.Random.pickUniqueNumbersInRange(
+      RANGE.smallNumber,
+      RANGE.largestNumber,
+      PICK_NUMBERS,
+    );
+  },
+
+  /**
    * 로또 구입금액을 입력받아 구입 가능한 개수만큼 생성하여 반환합니다
    * @param {number} purchaseAmount
-   * @returns {number[]}
+   * @returns {[Object, Object, ...]}
    */
   lotterySales: (purchaseAmount) => {
-    validationUtils.isInteger(purchaseAmount);
+    validationUtils.isIntegerToThrow(purchaseAmount);
     isDivisibleByLottoPrice(purchaseAmount);
     const count = purchaseAmount / LOTTO_PRICE;
     const lottos = [];
     while (lottos.length !== count) {
-      lottos.push(new Lotto(Lotto.drawLottoNumbers()));
+      const draw = shop.drawLottoNumbers();
+      lottos.push(new Lotto(draw));
     }
     return lottos;
   },
