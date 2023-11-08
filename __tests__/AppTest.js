faf84ca4169786ac31a6adad90ae56bf4693411c
songudeum <songudeum@gmail.com>
2023-11-08 18:36:38 +0900

    feat: 로또 결과 출력 기능 테스트 추가

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -87,4 +87,21 @@ describe("app test", () => {
             app.calculateProfitRate(winningArr, purchaseAmount)
         ).toEqual(result);
     });
+
+    test("로또 최종 결과 텍스트를 출력해야 합니다.", async () => {
+        const winningArr = [0, 0, 0, 1, 0];
+        const purchaseAmount = 1000;
+        const result = `당첨 통계\n---\n
+        3개 일치 (5,000원) - 0개\n
+        4개 일치 (50,000원) - 0개\n
+        5개 일치 (1,500,000원) - 0개\n
+        5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n
+        6개 일치 (2,000,000,000원) - 1개\n
+        총 수익률은 200000000.0%입니다.`;
+        const app = new App();
+
+        await expect(
+            app.renderLottoResultAndProfit(winningArr, purchaseAmount)
+        ).toEqual(result);
+    });
 });
