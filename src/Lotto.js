ab1aa29de7140977c215124b87551ea271f9bf40
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-11-08 23:08:04 +0900

    feat: 로또가 다른 로또와 일치하는 개수를 구하는 함수

--- src/Lotto.js
+++ src/Lotto.js
@@ -19,6 +19,14 @@ class Lotto {
   contains(number) {
     return this.#numbers.includes(number);
   }
+
+  /**
+   * 다른 로또와 일치하는 개수를 반환합니다
+   * @param {Lotto} target
+   * @return {number} 일치하는 개수 */
+  matchCount(target) {
+    return this.#numbers.filter((number) => target.contains(number)).length;
+  }
 }
 
 export default Lotto;
