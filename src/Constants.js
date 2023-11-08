8ce4afb1ddf683c5782aeef4e413ea798067c7a6
김경주 <popo505@naver.com>
2023-11-08 23:20:56 +0900

    feat: 기본 출력 메세지 변수로 저장

---
+++ src/Constants.js
@@ -0,0 +1,12 @@
+export const GAME_MESSAGE = {
+  PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  INPUT_WINNGNUMBERS: '당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUSNUMBER: '보너스 번호를 입력해 주세요.\n',
+  WINNING_STATISTICS: '당첨통계\n---',
+};
+
+export const ERROR_MESSAGE = {
+  AMOUNT_ERROR: '[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.',
+  NUMBER_ERROR: '[ERROR] 숫자를 입력해야 합니다.',
+  RANGE_ERROR: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+};
