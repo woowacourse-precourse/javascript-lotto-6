c083644315d150dba9cf716bf0de3c66e979bd8c
visvaden7 <visvaden7@naver.com>
2023-11-08 15:16:01 +0900

    feat: printingBuyingLottoNumbers() 로또 번호 출력 구현

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ class App {
             throw new Error("[ERROR] 잘못된 숫자를 입력하셨습니다.");
     }
 
-    generatingLottoNumbers(count) {
-        let lottoNumbers = []
-        for (let i = 0; i < count; i++) {
-            const lottoNumber = new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6))
-            lottoNumbers.push(lottoNumber.getNumbers())
-        }
-        return lottoNumbers
-    }
+
     async buyAmountOfLotto() {
         while (true) {
             try {
@@ -28,13 +21,27 @@ class App {
         }
     }
 
+    generatingLottoNumbers(count) {
+        let lottoNumbers = []
+        for (let i = 0; i < count; i++) {
+            const lottoNumber = new Lotto(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6))
+            lottoNumbers.push(lottoNumber.getNumbers())
+        }
+        return lottoNumbers
+    }
 
-
+    printingBuyingLottoNumbers(lottoNumbers) {
+        MissionUtils.Console.print(`${lottoNumbers.length}개를 구매했습니다.`)
+        lottoNumbers.forEach((lottoNumber) => {
+            lottoNumber.sort((a, b) => a - b)
+            MissionUtils.Console.print(`[${lottoNumber[0]}, ${lottoNumber[1]}, ${lottoNumber[2]}, ${lottoNumber[3]}, ${lottoNumber[4]}, ${lottoNumber[5]}]`)
+        })
+    }
 
     async play() {
         const purchasedLottoCount = await this.buyAmountOfLotto()
         const buyLottoNumbers = this.generatingLottoNumbers(purchasedLottoCount)
-        console.log(buyLottoNumbers)
+        this.printingBuyingLottoNumbers(buyLottoNumbers)
     }
 }
 
