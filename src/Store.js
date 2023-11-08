366cc7dd94064906e4632dee3e1e0d27c0c5c475
23YUJIN <asdbnm066@daum.net>
2023-11-08 20:42:41 +0900

    feat: 수익률 계산하기 구현

--- src/Store.js
+++ src/Store.js
@@ -153,6 +153,47 @@ class Store {
     }
     return rankCountMap;
   }
+
+  static generateWinningMessage(rankCountMap) {
+    let winningMessage = "";
+    const messageByRank = new Map([
+      [1, "6개 일치 (2,000,000,000원)"],
+      [2, "5개 일치, 보너스 볼 일치 (30,000,000원)"],
+      [3, "5개 일치 (1,500,000원)"],
+      [4, "4개 일치 (50,000원)"],
+      [5, "3개 일치 (5,000원)"],
+    ]);
+
+    for (let rank = 5; rank > 0; rank--) {
+      winningMessage += `${messageByRank.get(rank)} - ${rankCountMap.get(
+        rank
+      )}개\n`;
+    }
+    return winningMessage;
+  }
+
+  static #caculateProfit(rankCountMap) {
+    let lottoProfit = 0;
+    const profitByRank = new Map([
+      [1, 2000000000],
+      [2, 30000000],
+      [3, 1500000],
+      [4, 50000],
+      [5, 5000],
+    ]);
+    for (let rank = 5; rank > 0; rank--) {
+      const rankCount = rankCountMap.get(rank);
+      lottoProfit += profitByRank.get(rank) * rankCount;
+    }
+    return lottoProfit;
+  }
+
+  calculateProfitRate(rankCountMap) {
+    let profitRate;
+    const lottoProfit = Store.#caculateProfit(rankCountMap);
+    profitRate = (lottoProfit / this.#money) * 100;
+    return profitRate;
+  }
 }
 
 export default Store;
