d9b92df2a756f9088e52bf3fe6a6395395e8da62
sunub <bsc5672@gmail.com>
2023-11-08 18:30:25 +0900

    refactor : ProfitCalculator의 전역 변수를 클래스 내부로 이전

--- src/Controller/ProfitCalculator.js
+++ src/Controller/ProfitCalculator.js
@@ -1,18 +1,22 @@
 import LottoTicket from '../Model/LottoTicket.js';
 
-const lottoTicket = LottoTicket.getInstance();
 const WIN_PRICES = {
   3: 5000, 4: 50000, 5: 1500000, bonus: 30000000, 6: 2000000000,
 };
 
 class ProfitCalculator {
+  #lottoTicket;
+  constructor() {
+    this.#lottoTicket = LottoTicket.getInstance();
+  }
+
   calculateTotalIncome(numOfWinningNumMatches) {
     const totalIncome = Object.entries(numOfWinningNumMatches).reduce((acc, [matchesCount, count]) => acc + (WIN_PRICES[matchesCount] * count), 0);
     return totalIncome;
   }
 
   calculateEarningRate(totalIncome) {
-    const purchasePrice = lottoTicket.ticket.length * 1000;
+    const purchasePrice = this.#lottoTicket.ticket.length * 1000;
     const earningRate = Math.round((totalIncome / purchasePrice) * 1000) / 10;
     return earningRate;
   }
