4e808b8b76ab9021158ad9942af8fdae758c52e6
sunub <bsc5672@gmail.com>
2023-11-08 18:28:54 +0900

    refactor : LottoMachine의 Controller로서의 역할 강화
    Interface를 통해 값을 읽는 기능을 App으로 옮겨 LottoMachine이 Controller로써의 역할만 수행하게끔 리펙토링 하였다.

--- src/Controller/LottoMachine.js
+++ src/Controller/LottoMachine.js
@@ -1,30 +1,17 @@
 import LottoTicket from '../Model/LottoTicket.js';
-import Interface from '../View/Interface.js';
-import Printer from '../View/Printer.js';
 
 class LottoMachine {
-  async createWinNum() {
-    try {
-      const lottoTicket = LottoTicket.getInstance();
-      const winNums = await Interface.requestWinNums();
-      lottoTicket.saveSpecificTypeData('win', winNums);
-    } catch (error) {
-      Printer.print('[ERROR]');
-      Printer.print(error);
-      await this.createWinNum();
-    }
+  #lottoTicket;
+  constructor() {
+    this.#lottoTicket = LottoTicket.getInstance();
   }
 
-  async createBonusNum() {
-    try {
-      const lottoTicket = LottoTicket.getInstance();
-      const bonusNum = await Interface.requestBonusNum();
-      lottoTicket.saveSpecificTypeData('bonus', bonusNum);
-    } catch (error) {
-      Printer.print('[ERROR]');
-      Printer.print(error);
-      await this.createBonusNum();
-    }
+  createWinNum(winNums) {
+    this.#lottoTicket.saveSpecificTypeData('win', winNums);
+  }
+
+  createBonusNum(bonusNum) {
+    this.#lottoTicket.saveSpecificTypeData('bonus', bonusNum);
   }
 }
 export default LottoMachine;
