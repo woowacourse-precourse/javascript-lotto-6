6e577b412bd084694eafb48dfa865c0eada743dc
hjs0522 <91768822+hjs0522@users.noreply.github.com>
2023-11-08 17:18:00 +0900

    Feat: "구입금액 입력, 로또번호 생성 구현" 구입금액을 입력받는 함수 inputPurchaseAmount 로또번호를 생성하는 함수 generateLottoNumber 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import {Console,MissionUtils} from '@woowacourse/mission-utils'; 
+import Lotto from './Lotto.js';
+const lottos = []
+const inputPurchaseAmount = async()=>{
+  const amount = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+  if (isNaN(amount) || amount % 1000 !== 0) {
+    throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+  }
+  console.log();
+  const count = amount/1000;
+  Console.print(`${count}개를 구매했습니다.`);
+  for(let i=0;i<count;i++){
+    const number = generateLottoNumber();
+    Console.print(number);
+    const lotto = new Lotto(number);
+    lottos.push(lotto);
+  }
+  console.log();
+}
+
+const generateLottoNumber = ()=>{
+  const count = 6;
+  const numbers = new Set();
+  while(numbers.size < count){
+    const random = MissionUtils.Random.pickNumberInRange(1,45);
+    numbers.add(random);
+  }
+  return [...numbers];
+}
 class App {
-  async play() {}
+  async play() {
+    try{
+      await inputPurchaseAmount();
+    }
+    catch (error){
+      throw new Error(`${error}`);
+    }
+  }
 }
 
 export default App;
