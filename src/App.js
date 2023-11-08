d388abac6c9bcf61ee11be75c97e3e22bf04da6d
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-08 22:23:26 +0900

    fix(App): 리팩토링으로 인한 함수 및 코드 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { Console } from '@woowacourse/mission-utils';
-import Input from './view/input.js';
-import Lotto from './Lotto.js';
-import Output from './view/output.js';
+import { INPUT_MESSAGE, WINNING_NUMBER_DELIMITER } from './constant/index.js';
+import { Lotto, LottoCalculator, LottoValidation } from './domain/index.js';
+import { Input, Output } from './view/index.js';
 
 class App {
+  #winningNumbers;
+  #bonusNumber;
+  #lottos;
   async play() {
-    const amount = await Input.amountToBuy();
+    const amount = await Input.readLineLoop(INPUT_MESSAGE.AMOUNT_TO_BUY, LottoValidation.amount);
     const lottos = Lotto.createLottos(amount);
+    this.#lottos = lottos;
+    Output.purchasedLottosNumber(lottos);
 
-    Output.purchasedLottoNumbers(lottos);
-    const winningLotto = await Input.winningNumbers();
-    await Input.bonusNumber(winningLotto);
+    const winningNumbers = await Input.readLineLoop(INPUT_MESSAGE.WINNING_NUMBER, LottoValidation.winningNumbers);
+    this.#winningNumbers = winningNumbers.split(WINNING_NUMBER_DELIMITER).map(Number);
 
-    const statistics = winningLotto.getStatistics(lottos);
-    Output.winningStatistics(statistics);
-    const profitRate = winningLotto.getProfitRate(statistics, amount);
+    const validateBonus = (input) => LottoValidation.bonus(this.#winningNumbers, Number(input));
+    const bonusNumber = await Input.readLineLoop(INPUT_MESSAGE.BONUS_NUMBER, validateBonus);
+    this.#bonusNumber = Number(bonusNumber);
+
+    const statistics = LottoCalculator.calculateStatistics(lottos, this.#winningNumbers, this.#bonusNumber);
+    Output.printStatistics(statistics);
+    const profitRate = LottoCalculator.calculateProfitRate(statistics, amount);
     Output.profitRate(profitRate);
   }
 }
