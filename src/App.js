a1eb49ec78f981aebdfde195b54b5b703079ada0
KangYunJi <dbswl701@ajou.ac.kr>
2023-11-08 21:34:35 +0900

    feat: 보너스 번호 입력 및 예외 처리

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ class App {
     // 당첨 번호 입력
     const win = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
     const lotto = new Lotto(win.split(','));
+
+    // 보너스 번호 입력
+    const bonus = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    if (isNaN(parseInt(bonus))) throw new Error("[ERROR] 보너스 번호는 숫자여야 합니다.");
+    if (1 > bonus || bonus > 45) throw new Error("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
+    if (win.includes(bonus)) throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
   }
 }
 
