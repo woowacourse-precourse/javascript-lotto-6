c6532bf05bc88e15e06047e9a7cd4290bc1b3173
kkanghh <attsky2@gmail.com>
2023-11-08 15:14:07 +0900

    test: 수익률 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -28,7 +28,7 @@ const runException = async (input) => {
   // given
   const logSpy = getLogSpy();
 
-  const RANDOM_NUMBERS_TO_END = [1,2,3,4,5,6];
+  const RANDOM_NUMBERS_TO_END = [1, 2, 3, 4, 5, 6];
   const INPUT_NUMBERS_TO_END = ["1000", "1,2,3,4,5,6", "7"];
 
   mockRandoms([RANDOM_NUMBERS_TO_END]);
@@ -40,12 +40,12 @@ const runException = async (input) => {
 
   // then
   expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
-}
+};
 
 describe("로또 테스트", () => {
   beforeEach(() => {
     jest.restoreAllMocks();
-  })
+  });
 
   test("기능 테스트", async () => {
     // given
@@ -91,8 +91,44 @@ describe("로또 테스트", () => {
     });
   });
 
+  test("수익률 테스트", async () => {
+    const logSpy = getLogSpy();
+
+    mockRandoms([
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 5, 8],
+      [1, 2, 3, 4, 11, 12],
+      [1, 2, 3, 7, 8, 9],
+    ]);
+    mockQuestions(["5000", "1,2,3,4,5,6", "7"]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    const logs = [
+      "5개를 구매했습니다.",
+      "[1, 2, 3, 4, 5, 6]",
+      "[1, 2, 3, 4, 5, 7]",
+      "[1, 2, 3, 4, 5, 8]",
+      "[1, 2, 3, 4, 11, 12]",
+      "[1, 2, 3, 7, 8, 9]",
+      "3개 일치 (5,000원) - 1개",
+      "4개 일치 (50,000원) - 1개",
+      "5개 일치 (1,500,000원) - 1개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+      "6개 일치 (2,000,000,000원) - 1개",
+      "총 수익률은 40,631,100%입니다.",
+    ];
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
   test("예외 테스트", async () => {
     await runException("1000j");
   });
 });
-
