a5b22f71ca6674a5bfa171bcd8fb221e9c6e4504
KYHH <kyhh93@naver.com>
2023-11-08 14:06:37 +0900

    feat: 잘못된 입력값일 때 예외를 발생시키고 발생부분부터 다시 입력

--- src/UserInput.js
+++ src/UserInput.js
@@ -7,49 +7,62 @@ class Input {
 
 
   async userPurchase() {
-    try {
-      Console.print('구입금액을 입력해 주세요');
-      const input = await Console.readLineAsync('');
-      const purchaseAmount = parseInt(input);
-      if (isNaN(purchaseAmount) || purchaseAmount <= 0 || purchaseAmount % 1000 !== 0) {
-        throw new Error('[ERROR] 올바른 구입금액을 입력해 주세요 (1,000원 단위, 양수).');
+    while (true) {
+
+      try {
+        Console.print('구입금액을 입력해 주세요');
+        const input = await Console.readLineAsync('');
+        const purchaseAmount = parseInt(input);
+
+        if (isNaN(purchaseAmount) || purchaseAmount <= 0 || purchaseAmount % 1000 !== 0) {
+          throw new Error("[ERROR] 올바른 구입금액을 입력해 주세요 (1,000원 단위, 양수).");
+        }
+
+        if (input.split('').some(char => !'0123456789'.includes(char))) {
+          throw new Error("[ERROR] 구입금액은 숫자만 입력할 수 있습니다.");
+        }
+        return input;
+      } catch (error) {
+        Console.print(error.message);
       }
-      return input;
-    } catch (error) {
-      throw error;
     }
   }
 
-  async userNumber() {
-    try {
-      Console.print('당첨 번호를 입력해 주세요.');
-      const lottoNumbers = await Console.readLineAsync('');
-      this.lottoNumbers = lottoNumbers.split(',').map(num => parseInt(num.trim()));
-      if (this.lottoNumbers.length !== 6 || this.lottoNumbers.some(isNaN) || this.lottoNumbers.some(num => num < 1 || num > 45)) {
-        throw new Error('[ERROR]올바른 로또 번호를 입력해 주세요 (1부터 45까지의 숫자, 쉼표로 구분된 6개 숫자).');
-      }
-      this.checkForDuplicates(this.lottoNumbers);
 
-      return this.lottoNumbers;
-    } catch (error) {
-      throw error;
+  async userNumber() {
+    while (true) {
+      try {
+        Console.print(`\n당첨 번호를 입력해 주세요.`);
+        const lottoNumbers = await Console.readLineAsync('');
+        this.lottoNumbers = lottoNumbers.split(',').map(num => parseInt(num.trim()));
+        if (this.lottoNumbers.length !== 6 || this.lottoNumbers.some(isNaN) || this.lottoNumbers.some(num => num < 1 || num > 45)) {
+          throw new Error('[ERROR]올바른 로또 번호를 입력해 주세요 (1부터 45까지의 숫자, 쉼표로 구분된 6개 숫자).');
+        }
+        this.checkForDuplicates(this.lottoNumbers);
+
+        return this.lottoNumbers;
+      } catch (error) {
+        Console.print(error.message);
+      }
     }
   }
 
   async userBonusNumber() {
-    try {
-      Console.print('보너스 번호를 입력해 주세요.')
-      const input = await Console.readLineAsync('');
-      const bonusNumber = parseInt(input);
-      if (isNaN(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
-        throw new Error('[ERROR]올바른 보너스 번호를 입력해 주세요 (1부터 45까지의 숫자).');
-      }
-      const allNumbers = [...this.lottoNumbers, bonusNumber];
-      this.checkForDuplicates(allNumbers);
+    while (true) {
+      try {
+        Console.print(`\n보너스 번호를 입력해 주세요.`);
+        const input = await Console.readLineAsync('');
+        const bonusNumber = parseInt(input);
+        if (isNaN(bonusNumber) || bonusNumber < 1 || bonusNumber > 45) {
+          throw new Error('[ERROR]올바른 보너스 번호를 입력해 주세요 (1부터 45까지의 숫자).');
+        }
+        const allNumbers = [...this.lottoNumbers, bonusNumber];
+        this.checkForDuplicates(allNumbers);
 
-      return bonusNumber;
-    } catch (error) {
-      throw error;
+        return bonusNumber;
+      } catch (error) {
+        Console.print(error.message);
+      }
     }
   }
 
