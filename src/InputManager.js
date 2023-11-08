c9d5c0d2a533dec0ae63d4f0de4ad634f73c0c00
lucia0929 <thenum1developer@gamil.com>
2023-11-08 19:39:55 +0900

    feat : (C-1-3) 금액 입력 예외 처리 기능 구현

--- src/InputManager.js
+++ src/InputManager.js
@@ -3,8 +3,24 @@ import { Console } from "@woowacourse/mission-utils";
 class InputManager {
     async enterAmount() {
         const inputAmount = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
-        const cleanedAmount = inputAmount.replace(/\s+/g, ''); 
-        return cleanedAmount;    
+        const cleanedAmount = this.cleanInput(inputAmount);
+        this.validateAmount(cleanedAmount);
+        return parseInt(cleanedAmount, 10);
+    }
+
+    cleanInput(input) {
+        return input.replace(/\s+/g, '');
+    }
+
+    validateAmount(amount) {
+        if (!/^\d+$/.test(amount)) {
+            throw new Error("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+
+        const numericAmount = parseInt(amount, 10);
+        if (numericAmount % 1000 !== 0) {
+            throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
     }
 }
   
