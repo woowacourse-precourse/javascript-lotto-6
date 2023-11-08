91365ca088d4fe74055a1f049ff7b5a3361986f6
OhHyukJun <kmh7277@naver.com>
2023-11-08 16:49:40 +0900

    Refactor: try catch 구문 삭제

--- src/LottoInput/PurChasePrice.js
+++ src/LottoInput/PurChasePrice.js
@@ -2,16 +2,10 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 export class PurChasePrice {
     async inputPrice() {
-        while (true) {
-            try {
-                const userPrice = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
-                if(userPrice % 1000 !== 0){
-                    throw new Error("[ERROR] 입력 값이 1,000으로 나누어 떨어지지 않습니다.");
-                }
-                return userPrice/1000;
-            } catch (error) {
-                MissionUtils.Console.print(error.message);
-            }
+        const userPrice = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+        if (userPrice % 1000 !== 0) {
+            throw new Error("[ERROR] 입력 값이 1,000으로 나누어 떨어지지 않습니다.");
         }
+        return userPrice / 1000;
     }
 }
