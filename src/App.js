e65b16f295414e2256f7f10aa5804c86e5e1ecaf
koblmom <nayun9191@gamil.com>
2023-11-08 22:22:18 +0900

    feat: 수익률 계산과 출력기능 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ class App {
     this.winningNumbers = null;
     this.bonusNumber = null;
   }
+  calculateRateOfReturn(winningStatistics, ticketCount) {
+    const totalPrizeMoney =
+      winningStatistics[1] * 2000000000 +
+      winningStatistics[2] * 50000000 +
+      winningStatistics[3] * 1500000 +
+      winningStatistics[4] * 50000 +
+      winningStatistics[5] * 5000;
+
+    const totalInvestment = ticketCount * 1000;
+
+    return ((totalPrizeMoney - totalInvestment) / totalInvestment) * 100 + 100;
+  }
+
   async play() {
     const price = await InputManager.inputPrice();
     const generatedLottoNumbers = LottoPurchaser.lottoList(price);
@@ -18,8 +31,8 @@ class App {
       lottoTicket.sort((a, b) => a - b);
       Console.print(`[${lottoTicket.join(", ")}]`);
     }
-    Console.print("");
     this.winningNumbers = await InputManager.getWinningNumbers();
+
     this.bonusNumber = await InputManager.getBonusNumbers();
 
     const myLotto = new Lotto(this.winningNumbers);
@@ -32,12 +45,18 @@ class App {
 
     Console.print("당첨 통계:\n---");
     Console.print(`3개 일치 (5,000원) - ${winningStatistics[5]}개`);
-    Console.print(`4개 일치 (5,000원) - ${winningStatistics[4]}개`);
-    Console.print(`5개 일치 (5,000원) - ${winningStatistics[3]}개`);
+    Console.print(`4개 일치 (50,000원) - ${winningStatistics[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${winningStatistics[3]}개`);
     Console.print(
-      `5개 일치, 보너스 볼 일치 (5,000원) - ${winningStatistics[2]}개`
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningStatistics[2]}개`
     );
     Console.print(`6개 일치 (2,000,000,000원) - ${winningStatistics[1]}개`);
+
+    const rateOfReturn = this.calculateRateOfReturn(
+      winningStatistics,
+      price / 1000
+    );
+    Console.print(`총 수익률은 ${rateOfReturn.toFixed(1)}%입니다.`);
   }
 }
 
