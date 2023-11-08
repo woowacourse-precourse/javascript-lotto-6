61a9814ea9eda83bc93ed45ddb05e01a60d22c9a
yheseul_kang <kangyheseul@naver.com>
2023-11-08 20:02:51 +0900

    Set : 텍스트 수정

--- src/validator/winningNumberValidation.js
+++ src/validator/winningNumberValidation.js
@@ -1,14 +1,11 @@
 export const winningNumberLength = (winningNumber) => {
-  const winningNumberArray = winningNumber.split(',');
-  if (winningNumberArray.length !== 6) {
-    throw new Error('6개의 숫자를 입력해 주세요.');
+  if (winningNumber.split(',').length !== 6) {
+    throw new Error('[ERROR] 6개의 숫자를 입력해 주세요.');
   }
 };
 
 export const duplicate = (winningNumber) => {
-  const duplicateArray = winningNumber.split(',');
-  const setDuplicateArray = new Set(duplicateArray);
-  if(duplicateArray.length !== setDuplicateArray.size) {
-    throw new Error('로또 번호에 중복된 숫자가 있습니다.');
+  if(winningNumber.split(',').length !== new Set(winningNumber.split(',')).size) {
+    throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
   }
 };
