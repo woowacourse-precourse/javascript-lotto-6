f735dd633c58b40b2438397b13f874c8d809ee74
whai2 <whai2125@gmail.com>
2023-11-08 13:50:32 +0900

    refactor(results): 메서드 프라이빗화, 메서드 호출 순서 체계화

--- src/Results.js
+++ src/Results.js
@@ -8,20 +8,19 @@ export class Results {
     this.winning = winning;
     this.bonus = bonus;
     this.count = count;
-    this.lottosReader(lotto, winning, bonus, count);
-    this.lottoResultsCounter(model.winningResults);
-    this.lottoResultComment();
-    this.lottoResultsPrinter(model.winning);
+    this.#lottosReader(lotto, winning, bonus, count);
   }
 
-  lottosReader(lotto, winning, bonus, count) {
+  #lottosReader(lotto, winning, bonus, count) {
     for (let i = 0; i < count; i++) {
-      const result = this.lottoReader(lotto[i], winning, bonus)
+      const result = this.#lottoReader(lotto[i], winning, bonus)
       model.winningResults.push(result);
     }
+
+    this.#lottoResultsCounter(model.winningResults);
   }
 
-  lottoReader(lotto, winning, bonus) {
+  #lottoReader(lotto, winning, bonus) {
     const sameNumbers = winning.filter(number => lotto.includes(number))
     const sameBonus = lotto.includes(bonus);
     if (sameBonus === false) {
@@ -30,13 +29,15 @@ export class Results {
     return [sameNumbers.length, 1];
   }
 
-  lottoResultsCounter(results) {
+  #lottoResultsCounter(results) {
     for (let i = 0; i < results.length; i++) {
-      this.lottoResultCounter(results[i]);
+      this.#lottoResultCounter(results[i]);
     }
+
+    this.#lottoResultsPrinter(model.winning);
   }
 
-  lottoResultCounter(result) {
+  #lottoResultCounter(result) {
     for (let i = 3; i < 7; i++) {
       if (result[0] === i && i !== 5) {
         model.winning[i-3] += 1;
@@ -44,11 +45,11 @@ export class Results {
       }
     }
     if (result[0] === 5) {
-      this.lottoResultBonusCounter(result);
+      this.#lottoResultBonusCounter(result);
     }
   }
 
-  lottoResultBonusCounter(result) {
+  #lottoResultBonusCounter(result) {
     if (result[1] === 0) {
       model.winning[2] += 1;
       model.totalPrize += model.prize[2];
@@ -56,27 +57,30 @@ export class Results {
     model.totalPrize += model.prize[3];
   }
 
-  lottoResultsPrinter(results) {
+  #lottoResultsPrinter(results) {
+    this.#lottoResultComment();
+
     for (let i = 0; i < results.length; i++) {
       const prize = model.prize[i]
-      const currencyPrize = this.formatCurrency(prize);
-      this.lottoResultPrinter(currencyPrize, results[i], i);
+      const currencyPrize = this.#formatCurrency(prize);
+      this.#lottoResultPrinter(currencyPrize, results[i], i);
     }
+
     this.totalRate(model.totalMoney, model.totalPrize);
   }
 
   totalRate(money, prize) {
-    const roundedRate = this.calculateRate(money, prize);
+    const roundedRate = this.#calculateRate(money, prize);
     MissionUtils.Console.print(`총 수익률은 ${roundedRate}%입니다.`);
   }
   
-  calculateRate(money, prize) {
+  #calculateRate(money, prize) {
     const rate = (prize / money) * 100;
     const roundedRate = rate.toFixed(2);
     return Number(roundedRate);
   }
 
-  formatCurrency(number) {
+  #formatCurrency(number) {
     return new Intl.NumberFormat("ko-KR", {
       style: "currency",
       currency: "KRW",
@@ -84,7 +88,7 @@ export class Results {
     }).format(number).replace(/₩/g, '');
   }
 
-  lottoResultPrinter(prize, result, i) {
+  #lottoResultPrinter(prize, result, i) {
     if (i === 3) {
       MissionUtils.Console.print(
         `${model.same[i]}개 일치, 보너스 볼 일치 (${prize}원) - ${result}개`
@@ -96,7 +100,7 @@ export class Results {
     );
   }
 
-  lottoResultComment() {
+  #lottoResultComment() {
     MissionUtils.Console.print("");
     MissionUtils.Console.print("당첨 통계");
     MissionUtils.Console.print("---");
