bdd9b8ec92eb4a218d69c92ceac2a93c85c0b698
seoye0ng <bsy0313@gmail.com>
2023-11-08 22:13:16 +0900

    Comment: 주석 수정 및 불필요한 주석 삭제

--- src/App.js
+++ src/App.js
@@ -15,25 +15,20 @@ class App {
   async play() {
     const money = await this.getMoney();
     const lottoCount = money / LOTTO_PRICE;
-
-    // 2.
     const lottos = Lotto.generateLotto(lottoCount);
     this.printLotto(lottos, lottoCount);
-
     const winningNumbers = await this.getWinningNumber();
     const bonusNumber = await this.getBonusNumber(winningNumbers);
-
     const winningRank = this.winningRankResult(
       lottos,
       winningNumbers,
       bonusNumber,
     );
-    // 7.
     const winningProfit = Lotto.calculateProfit(money, winningRank);
     this.printResult(winningRank, winningProfit);
   }
 
-  // 1. 로또 구입금액 입력받기
+  // 로또 구입 금액 입력
   async getMoney() {
     while (true) {
       try {
@@ -59,26 +54,24 @@ class App {
     return true;
   }
 
-  // 3. 발행한 로또 수량 및 번호를 출력
+  // 발행한 로또 출력
   printLotto(lottos, lottoCount) {
     const message = PRINT_MESSAGE(lottoCount);
     Console.print(message.lottoQuantity);
 
     lottos.forEach((lotto) => {
-      const formattedLotto = `[${lotto.join(', ')}]`; // 배열을 문자열로 변환, 공백 추가
+      const formattedLotto = `[${lotto.join(', ')}]`;
       Console.print(formattedLotto);
     });
   }
 
-  // 4. 당첨 번호를 입력받기
+  // 당첨 번호 입력
   async getWinningNumber() {
     while (true) {
       try {
         const numbers = await Console.readLineAsync(
           REQUEST_MESSAGE.PUT_WINNING_NUMBER,
         );
-
-        console.log('numbers', numbers);
         const winningNumbers = numbers.split(',');
         const lotto = new Lotto(winningNumbers);
         return lotto.lottoNumbers();
@@ -88,7 +81,7 @@ class App {
     }
   }
 
-  // 5. 보너스 번호를 입력받기
+  // 보너스 번호를 입력
   async getBonusNumber(winningNumbers) {
     while (true) {
       try {
@@ -116,7 +109,7 @@ class App {
     return true;
   }
 
-  // 6. 사용자가 구매한 로또 번호와 당첨 번호를 비교
+  // 구매한 로또 번호와 당첨 번호를 비교
   winningRankResult(lottos, winningNumbers, bonusNumber) {
     const winningRank = {
       first: 0,
@@ -127,11 +120,10 @@ class App {
     };
 
     Lotto.compareLottoNumber(winningRank, lottos, winningNumbers, bonusNumber);
-
     return winningRank;
   }
 
-  // 8. 당첨 내역 및 수익률 출력
+  // 당첨 내역 및 수익률 출력
   printResult(winningRank, winningProfit) {
     const message = PRINT_MESSAGE(winningProfit);
 
