e8a12c65eff79c4dabe76a432146143a9d33f0a5
Klomachenko <leekyumin0901@naver.com>
2023-11-08 21:31:15 +0900

    feat(inputOutputTest): getInput 에러 발생 테스트 구현
    Description: getInput 매서드에서 입력이 실패할 경우 에러가 발생하는지
    확인하는 테스트 추가

--- __tests__/InputOutputTest.js
+++ __tests__/InputOutputTest.js
@@ -14,5 +14,21 @@ describe("InputOutput 클래스 테스트", () => {
 
       expect(mockReadLineAsync).toHaveBeenCalledWith("테스트 입력 메시지");
     });
+
+    it("입력이 실패할 경우 에러를 던져야 합니다.", async () => {
+      const mockReadLineAsync = jest.fn().mockRejectedValue(new Error("Test Error"));
+      MissionUtils.Console.readLineAsync = mockReadLineAsync;
+
+      try {
+        await InputOutput.getInput("테스트 입력 메시지");
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error.message).toBe("[ERROR] 입력을 받는 중 실패하였습니다.");
+      }
+
+      expect(mockReadLineAsync).toHaveBeenCalledTimes(1);
+
+      expect(mockReadLineAsync).toHaveBeenCalledWith("테스트 입력 메시지");
+    });
   });
 });
