6d6b55f6c2389b0f6c29255f22f73cb973d6a5d7
yeokyoung song <spolice0324@gmail.com>
2023-11-08 17:04:39 +0900

    test : ValidateTest 케이스 추가
    - lottoValidator 내 유효성을 검사하는 로직 테스트 추가

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,5 +1,6 @@
 import { ERROR_MESSAGE } from '../src/Utils/constants';
 import InputValidator from '../src/Validator/inputValidator';
+import lottoValidator from '../src/Validator/lottoValidator';
 
 describe('InputValidateTest', () => {
   describe('구매 금액 입력 테스트', () => {
@@ -75,5 +76,79 @@ describe('InputValidateTest', () => {
         },
       );
     });
+  });
+});
+
+const pickRandomInRanage = (min, max) =>
+  parseInt(Math.random() * (max - min)) + min;
+
+describe('OutputValidateTest', () => {
+  describe('lottoValidator내 purchaseAmount 출력 테스트 ', () => {
+    test.each([
+      ...Array.from(
+        { length: 100 },
+        () => pickRandomInRanage(1, 800000) * 1000,
+      ),
+      ('구입 금액이 %s와 같을 때, 결과는 정상처리',
+      input => {
+        expect(() => {
+          lottoValidator.validatePurchaseAmount(input);
+        }).not.toThrow('[ERROR]');
+      }),
+    ]);
+  });
+
+  describe('lottoValidator내 winningNumbers 출력 테스트 ', () => {
+    test.each([
+      [[1, 2, 3, 4, 5, 6]],
+      [[41, 42, 43, 44, 45, 40]],
+      [[14, 3, 2, 6, 43, 9]],
+    ])('당첨 번호가 %s와 같을 때, 결과는 정상처리', input => {
+      expect(() => {
+        lottoValidator.validateWinningNumbers(input);
+      }).not.toThrow('[ERROR]');
+    });
+  });
+
+  describe('lottoValidator내 bonusNumber 출력 테스트 ', () => {
+    test.each([
+      [[1, 2, 3, 4, 5, 6], 7],
+      [[1, 2, 3, 4, 5, 6], 45],
+    ])('보너스 번호가 %s와 같을 때, 결과는 정상처리', input => {
+      expect(() => {
+        lottoValidator.validateBonusNumber(input);
+      }).not.toThrow('[ERROR]');
+    });
+  });
+
+  describe('lottoValidator내 winningNumbers 출력 예외 테스트 ', () => {
+    test.each([
+      [[1, 2, 3, 4, 5]], // 로또 번호 개수 예외 (미만)
+      [[41, 42, 43, 44, 45, 40, 39]], // 로또 번호 개수 예외 (초과)
+      [[3, 3, 2, 6, 43, 9]], // 로또 번호 중복 예외 (미만)
+      [[1, 2, 3, 4, 5, 46]], // 로또 번호 범위 예외 (초과)
+      [[1, 2, 3, 4, 5, 0]], // 로또 번호 비정상 예외 (0)
+      [[1, 2, 3, 4, 5, -1]], // 로또 번호 비정상 예외 (음수)
+    ])('당첨 번호가 %s와 같을 때, 결과는 예외처리', input => {
+      expect(() => {
+        lottoValidator.validateWinningNumbers(input);
+      }).toThrow('[ERROR]');
+    });
+  });
+
+  describe('lottoValidator내 bonusNumber 출력 예외 테스트 ', () => {
+    test.each([
+      [[1, 2, 3, 4, 5, 6], 1], // 당첨 번호 중복 예외
+      [[1, 2, 3, 4, 5, 6], 46], // 보너스 번호 범위 예외
+      [[1, 2, 3, 4, 5, 6], 0], // 보너스 번호 비정상 예외 (0)
+      [[1, 2, 3, 4, 5, 6], -1], // 보너스 번호 비정상 예외 (음수)
+    ])(
+      '일반 당첨 번호 %s가 있고, 보너스 번호가 %s와 같을 때, 결과는 예외처리',
+      (winningNumbers, bonus) => {
+        expect(() => {
+          lottoValidator.validateBonusNumber(winningNumbers, bonus);
+        }).toThrow('[ERROR]');
+      },
+    );
   });
 });
