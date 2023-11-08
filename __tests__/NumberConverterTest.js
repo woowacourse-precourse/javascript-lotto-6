186d7187129f92c48e9cec472c8f0522b974257c
Boeun <boeun0904@naver.com>
2023-11-08 19:37:47 +0900

    test: NumberConverter 소수점 포함 콤마 출력 테스트

--- __tests__/NumberConverterTest.js
+++ __tests__/NumberConverterTest.js
@@ -12,6 +12,8 @@ describe('숫자 변환 테스트', () => {
     { input: 1000.0, expected: '1,000.0' },
     { input: 1000.12, expected: '1,000.1' },
     { input: 1000.16, expected: '1,000.2' },
+    { input: 37500.0, expected: '37,500.0' },
+    { input: 25000000.0, expected: '25,000,000.0' },
   ])('$input에 1000단위로 콤마를 찍으면, $expected가 반환된다.', ({ input, expected }) => {
     expect(NumberConverter.splitIntoThreeDigitWithCommaContainingDecimalPoint(input, 1)).toBe(
       expected,
