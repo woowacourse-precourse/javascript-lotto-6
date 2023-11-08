aac38f990bac3222644e80048ab0ce53bf0ed243
thgus5335 <thgus5335@naver.com>
2023-11-08 14:37:43 +0900

    fix: 상수 메시지 수정

--- src/constant.js
+++ src/constant.js
@@ -34,6 +34,6 @@ export const ERROR_MSG = {
     INPUT_NAN: '[ERROR] 입력된 숫자가 잘못된 형식입니다.',
     NUM_UNIT: '[ERROR] 1,000원 단위로만 입력 가능합니다.',
     NUM_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
-    NUM_COUNT: '[ERROR] 당첨 번호는 6개의 숫자만 입력 가능합니다.',
+    NUM_COUNT: '[ERROR] 로또 번호는 6개여야 합니다.',
     NUM_DUPE: '[ERROR] 중복된 숫자는 입력할 수 없습니다.',
 };
