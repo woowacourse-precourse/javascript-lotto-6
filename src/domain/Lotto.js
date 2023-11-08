c6883ef0c58bdc91094ed65a2250d661abf5c245
HyunseokLEE <unducklee@gmail.com>
2023-11-08 19:05:47 +0900

    feat: 1개의 로또 번호에 대한 오름차순 정렬 및 유효성 검사를 하는 Lotto 클래스 생성

---
+++ src/domain/Lotto.js
@@ -0,0 +1,20 @@
+import Validator from "../utils/Validator";
+
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers.sort((a, b) => a - b);
+  }
+
+  #validate(numbers) {
+    Validator.lottoNumber(numbers);
+  }
+
+  getLotto() {
+    return this.#numbers;
+  }
+}
+
+export default Lotto;
--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -14,6 +14,18 @@ const Validator = {
     if (purchaseAmount.replace(/0/g, "").length === 0)
       throw new Error(ERROR_MESSAGE.PURCHASE_AMOUNT_ZERO);
   },
+
+  lottoNumber(numbers) {
+    const number = numbers.join("");
+    if (number.replace(/\d/g, "").length > 0)
+      throw new Error("[ERROR] 로또 번호는 숫자로만 구성되어야 합니다.");
+    if (numbers.length !== new Set(numbers).size)
+      throw new Error("[ERROR] 로또 번호는 서로 중복될 수 없습니다.");
+    if (numbers.length !== 6)
+      throw new Error("[ERROR] 로또 번호는 6개로 구성되어야 합니다.");
+    if (RangeTest(numbers))
+      throw new Error("[ERROR] 로또 번호는 1과 45 사이의 값이어야 합니다.");
+  },
 };
 
 export default Validator;
