5afd95e66a228a3b06888aabcbf3637eab0a5fa6
Boeun <boeun0904@naver.com>
2023-11-08 23:46:30 +0900

    style(App): 줄 간격 생성

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Computer from './Computer.js';
 class App {
   async play() {
     const computer = new Computer();
+
     await computer.getPurchaseAmountFromUserInput();
     computer.issueLottoForPurchaseAmount();
     computer.printLotto();
+
     await computer.getWinningNumbersrFromUserInput();
     await computer.getBonusNumberFromUserInput();
+
     computer.printLottoWinningStatistics();
     computer.printTotalRateOfReturn();
+
     computer.resetLotto();
   }
 }
