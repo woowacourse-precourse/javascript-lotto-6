879ac84bc8596d9d72413bd4c384dafdb9c53b7a
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2023-11-08 13:44:51 +0900

    [Feat] 입력된 구입금액만큼 무작위 로또를 발급할 수 있다.
    * feat: 무작위 로또 생성 및 출력 함수 구현
    
    * docs: 무작위 로또 발급 기능 구현
    
    * style: 무작위 로또 생성 메서드명 변경

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App {
     const amount = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     if (!this.validPurchase(amount))
       throw new Error('[ERROR] invalid input amount.');
+    this.printMyLotto(Number(amount) / 1000);
   }
 
   validPurchase(amount) {
@@ -16,6 +17,20 @@ class App {
     if (Number(amount) % 1000 !== 0) return false;
 
     return true;
+  }
+
+  printMyLotto(volume) {
+    Console.print('\n' + volume + '개를 구매했습니다.');
+    this.createAndPrintRandomLotto(volume);
+  }
+
+  createAndPrintRandomLotto(volume) {
+    while (volume--) {
+      const lotto = Random.pickUniqueNumbersInRange(1, 45, 6);
+      const ascLotto = lotto.sort((n1, n2) => n1 - n2).join(', ');
+
+      Console.print('[' + ascLotto + ']');
+    }
   }
 }
 
