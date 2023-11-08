0a48b471c7ab32a17239378a657ab637ae50f62e
jeongin98 <eon7500@gmail.com>
2023-11-08 14:07:42 +0900

    Feat(WinningJudge): 당첨 결과 계산

--- Domain/WinningJudge.js
+++ Domain/WinningJudge.js
@@ -1,2 +1,26 @@
-class WinningJudge {}
+import Lotto from './Lotto.js';
+import WinningLotto from './WinningLotto.js';
+
+class WinningJudge {
+  #winner = {
+    first: 0,
+    second: 0,
+    third: 0,
+    fourth: 0,
+    fifth: 0,
+  };
+
+  getWinnerList() {
+    return this.#winner;
+  }
+
+  calculateWinner({ lottoList, winningLotto }) {
+    let winnerList;
+    lottoList.forEach((lotto) => {
+      const lottoNumber = lotto.getNumber();
+      // winnerList = this.countWinner(lottoNumber, winningLotto);
+    });
+    return winnerList;
+  }
+}
 export default WinningJudge;
