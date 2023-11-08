71336349089f6061b017e8d6496bb963c376b22a
OAO <ashgrayblue0@gmail.com>
2023-11-08 12:42:28 +0900

    test(WinNumberTest): rewrite test code for WinNumber.js

--- __tests__/WinNumberTest.js
+++ __tests__/WinNumberTest.js
@@ -18,22 +18,4 @@ describe('당첨 번호 클래스 테스트', () => {
       new WinNumber([' a', 47, 'c', 'd', 'e', 'f'], 1);
     }).toThrow('[ERROR]');
   });
-
-  test('보너스 번호가 정수가 아니면 에러가 발생한다.', () => {
-    expect(() => {
-      new WinNumber([1, 2, 3, 4, 5, 6], []);
-    }).toThrow('[ERROR]');
-  });
-
-  test('보너스 번호가 1 이상 45 이하의 정수가 아니면 에러가 발생한다.', () => {
-    expect(() => {
-      new WinNumber([1, 2, 3, 4, 5, 6], 47);
-    }).toThrow('[ERROR]');
-  });
-
-  test('보너스 번호가 당첨 번호와 중복되면 에러가 발생한다.', () => {
-    expect(() => {
-      new WinNumber([1, 2, 3, 4, 5, 6], 6);
-    }).toThrow('[ERROR]');
-  });
 });
