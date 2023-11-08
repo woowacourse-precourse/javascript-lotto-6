33b5711e7257409333fc67e8129dbffddf6ac5d3
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-08 18:42:47 +0900

    Refactor: 관리하는 동작과 관련된 기능들을 LottoManagement.js로 일부 함수 분리

--- src/LottoResultChecker.js
+++ src/LottoResultChecker.js
@@ -10,47 +10,6 @@ import {
 class LottoResultChecker {
   constructor() {}
 
-  compareInputNumAndRandomNum(inputArr, randomArrs) {
-    const counts = [];
-
-    for (const randomArr of randomArrs) {
-      const matchingCount = inputArr.reduce((accumalator, inputValue) => {
-        return accumalator + (randomArr.includes(inputValue) ? 1 : 0);
-      }, 0);
-      counts.push(matchingCount);
-    }
-
-    return counts;
-  }
-
-  getMatchingCounts(counts, bonus) {
-    const matchingCounts = {
-      three: 0,
-      four: 0,
-      five: 0,
-      fiveAndBonus: 0,
-      six: 0,
-    };
-    counts.map((count, i) => {
-      if (count === 3) {
-        matchingCounts.three += 1;
-      }
-      if (count === 4) {
-        matchingCounts.four += 1;
-      }
-      if (count === 5 && bonus[i] === 0) {
-        matchingCounts.five += 1;
-      }
-      if (count === 5 && bonus[i] === 1) {
-        matchingCounts.fiveAndBonus += 1;
-      }
-      if (count === 6) {
-        matchingCounts.six += 1;
-      }
-    });
-
-    return matchingCounts;
-  }
   calculateTotalProfit(matchingCountsResult) {
     const totalProfit =
       THREE_MATCH_PRICE * matchingCountsResult.three +
