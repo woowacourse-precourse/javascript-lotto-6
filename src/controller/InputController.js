e9e498f403df8968ee17fc0da402ad96f520c168
rlaltj <we00425@gmail.com>
2023-11-08 13:38:07 +0900

    Fix: 수정-예외사항 처리: 에러 메시지 출력 후 재입력
    기존 에러 메시지 출력 후 종료-> 재시작으로 변경

--- src/controller/InputController.js
+++ src/controller/InputController.js
@@ -1,11 +1,13 @@
 import Lotto from '../Lotto.js';
 import InputView from '../view/InputView.js';
+import OutputView from '../view/OutputView.js';
 import PurchaseController from './PurchaseController.js';
 import ValidateController from './ValidateController.js';
 import WinController from './WinController.js';
 
 export default class InputController {
   inputView = new InputView();
+  outputView = new OutputView();
   validateController = new ValidateController();
   purchaseController = new PurchaseController();
   amount = 0;
@@ -14,9 +16,9 @@ export default class InputController {
   lotto = {};
 
   async inputStart() {
-    await this.getAmount();
-    await this.getWinningNumbers();
-    await this.getBonusNumber();
+    await this.receiveAmount();
+    await this.receiveWinningNumbers();
+    await this.receiveBonusNumber();
 
     this.winController = new WinController(
       this.amount,
@@ -26,21 +28,42 @@ export default class InputController {
     );
   }
 
-  async getAmount() {
-    this.amount = await this.inputView.readAmount();
-    this.validateController.validateAmount(this.amount);
+  async receiveAmount() {
+    while (true) {
+      try {
+        this.amount = await this.inputView.readAmount();
+        this.validateController.validateAmount(this.amount);
+        break;
+      } catch (error) {
+        this.outputView.printMessage(error);
+      }
+    }
 
     this.purchaseController.issueLottos(this.amount / 1000);
   }
 
-  async getWinningNumbers() {
-    const numberString = await this.inputView.readWinningNumbers();
-    this.winningNumbers = numberString.split(',');
-    this.lotto = new Lotto(this.winningNumbers);
+  async receiveWinningNumbers() {
+    while (true) {
+      try {
+        const numberString = await this.inputView.readWinningNumbers();
+        this.winningNumbers = numberString.split(',');
+        this.lotto = new Lotto(this.winningNumbers);
+        break;
+      } catch (error) {
+        this.outputView.printMessage(error);
+      }
+    }
   }
 
-  async getBonusNumber() {
-    this.bonusNumber = await this.inputView.readBonusNumber();
-    this.validateController.validateBonusNumber(this.bonusNumber);
+  async receiveBonusNumber() {
+    while (true) {
+      try {
+        this.bonusNumber = await this.inputView.readBonusNumber();
+        this.validateController.validateBonusNumber(this.bonusNumber);
+        break;
+      } catch (error) {
+        this.outputView.printMessage(error);
+      }
+    }
   }
 }
