b5cebec0b86daf658047cfc5b709e39414462db5
kudongku <kudongku@naver.com>
2023-11-08 15:27:24 +0900

    input lottery number and bonus number

--- src/App.js
+++ src/App.js
@@ -12,9 +12,38 @@ class App {
       if (isNaN(purchaseAmount) || purchaseAmount < 1000) {
         throw new Error('[ERROR] 로또 구매 금액은 1,000원 이상이어야 합니다.');
       }
+
+      // 구매한 로또 번호를 생성하는 로직
+      const lottos = this.generateLottos(purchaseAmount);
+      console.log(lottos);
+
+      // 당첨 번호와 보너스 번호를 입력 받는 로직
+      const winningNumbers = await Console.readLineAsync(
+        '당첨 번호를 입력해 주세요 (1~45 사이의 숫자, 쉼표로 구분): '
+      );
+      const bonusNumber = parseInt(
+        await Console.readLineAsync(
+          '보너스 번호를 입력해 주세요 (1~45 사이의 숫자): '
+        )
+      );
+
+      // 당첨 번호와 보너스 번호를 파싱
+      const winningNumbersArray = winningNumbers.split(',').map(Number);
+
+      console.log(winningNumbersArray);
     } catch (error) {
       console.error(error.message);
     }
+  }
+  generateLottos(purchaseAmount) {
+    const numberOfLottosToBuy = Math.floor(purchaseAmount / 1000);
+    const lottos = [];
+
+    for (let i = 0; i < numberOfLottosToBuy; i++) {
+      lottos.push(new Lotto(Lotto.generateNumbers()));
+    }
+
+    return lottos;
   }
 }
 
