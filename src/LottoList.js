d44417082b7d9bae21548df084359e0ea74349a6
gilgor2 <misby0327@gmail.com>
2023-11-08 17:26:51 +0900

    feat(LottoList.js): 로또 목록 객체 구현
    로또 목록 객체와 상수 파일 구현

--- src/LottoList.js
+++ src/LottoList.js
@@ -1,14 +1,50 @@
+import { Random } from '@woowacourse/mission-utils';
 import Lotto from './Lotto';
+import {
+	LOTTO_NUMBERS_CEIL,
+	LOTTO_NUMBERS_CNT,
+	LOTTO_NUMBERS_FLOOR,
+	LOTTO_PRICE,
+} from './constant';
 
 class LottoList {
-    #lottoList;
+	#lottoList = [];
 
-    generateLottoList(budget) {
+	constructor(budget) {
+		this.#initializeLottoList(budget);
+	}
 
-    }
+	getLottoList() {
+		return this.#lottoList;
+	}
 
-    getLottoList() {
+	#initializeLottoList(budget) {
+		const lottoCnt = LottoList.countLottoToPurchase(budget);
+		this.#generateLottoList(lottoCnt);
+	}
 
-    }
+	#generateLottoList(lottoCnt) {
+		for (let i = 0; i < lottoCnt; i += 1) {
+			const newLotto = LottoList.generateLotto();
+			this.#lottoList.push(newLotto);
+		}
+	}
+
+	static generateLotto() {
+		const randomNumbers = Random.pickUniqueNumbersInRange(
+			LOTTO_NUMBERS_FLOOR,
+			LOTTO_NUMBERS_CEIL,
+			LOTTO_NUMBERS_CNT,
+		);
+		const lotto = new Lotto(randomNumbers);
+
+		return lotto;
+	}
+
+	static countLottoToPurchase(budgetStr) {
+		const budget = budgetStr * 1;
+
+		return budget / LOTTO_PRICE;
+	}
 }
 export default LottoList;
---
+++ src/constant.js
@@ -0,0 +1,4 @@
+export const LOTTO_PRICE = 1000;
+export const LOTTO_NUMBERS_CNT = 6;
+export const LOTTO_NUMBERS_FLOOR = 1;
+export const LOTTO_NUMBERS_CEIL = 45;
