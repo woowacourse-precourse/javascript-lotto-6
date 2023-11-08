dd5067450f1d9f76eca398ac3002063c7e300db6
yura <yuraa0302@gmail.com>
2023-11-08 22:40:04 +0900

    feat : 당첨 통계 출력

--- src/App.js
+++ src/App.js
@@ -24,15 +24,21 @@ class App {
   async buyLotto(money) {
     const lottoes = Lotto.buyAutomaticLotto(money);
     this.lottoResult.setLottoes(lottoes);
-
     const numbers = lottoes.map((lotto) => lotto.getNumbers());
     OutputView.printBuyResult(numbers);
-
     await this.inputNumbers();
   }
+
   async inputNumbers() {
     const numbers = await InputView.readNumbers();
     const bonus = await InputView.readBonus();
     this.lottoResult.setWinningNumber(numbers, bonus);
   }
+
+  async printResult() {
+    const result = this.lottoResult.getResult();
+    const { profit } = LottoResult.getProfitWithResult(result);
+    OutputView.printWinningResult(result.slice(1).reverse(), profit);
+  }
 }
+export default App;
--- src/OutputView.js
+++ src/OutputView.js
@@ -10,6 +10,36 @@ const OutputView = {
       Console.print(`[${number.join(", ")}]`);
     });
   },
+
+  /**
+   * @param {number} rank
+   * @returns {string}
+   */
+  getCorrectMessage: (rank) => {
+    if (rank > 2) {
+      return "${6 - rank + 2}개 일치";
+    }
+    if (rank === 2) {
+      return `${6 - 1}개 일치, 보너스 볼 일치`;
+    }
+    return `${6}개 일치`;
+  },
+
+  /**
+   * @param {number[]} result
+   * @param {number} profit
+   */
+  printWinningResult: (results, profit) => {
+    Console.print("\n당첨 통계\n---");
+    results.forEach((result, index) => {
+      Console.print(
+        `${OutputView.getCorrectMessage(5 - index)} (${[
+          2_000_000_000, 30_000_000, 1_500_000, 50_000, 5_000,
+        ][5 - index - 1].toLocaleString()}원) - ${result}개`
+      );
+    });
+    Console.print(`총 수익률은 ${profit}%입니다.`);
+  },
 };
 
 export default OutputView;
