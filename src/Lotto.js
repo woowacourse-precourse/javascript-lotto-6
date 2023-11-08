f3e306438e749f307f4277d7b476311c7e30addc
yunminwon <wymm20@naver.com>
2023-11-08 12:51:41 +0900

    기능 목록 업데이트

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,23 +1,23 @@
 class Lotto {
-  #numbers;
+  #numcheck;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+  constructor(numcheck) {
+    this.#validate(numcheck);
+    this.#numcheck = numcheck;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
+  #validate(numcheck) {
+    if (numcheck.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
-    if (this.duplicate(numbers)){
+    if (this.duplicate(numcheck)){
       throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.")
     }
   }
 
   checkWin(winNum, bonusNum){ 
     let winnernumber = winNum.map(num => parseInt(num, 10));
-    let winnercounting = this.#numbers.filter(numbers => winnernumber.includes(numbers)).length;
+    let winnercounting = this.#numcheck.filter(numcheck => winnernumber.includes(numcheck)).length;
     if (winnercounting >= 3){
       if (winnercounting == 3) return 0;
       if (winnercounting == 4) return 1;
@@ -29,14 +29,14 @@ class Lotto {
     
   }
 
-  checkBonus(bonusNum){ // 보너스 번호 없으면 2, 있으면 3을 리턴
-    let bonuscorrect = this.#numbers.includes(bonusNum);
+  checkBonus(bonusNum){ 
+    let bonuscorrect = this.#numcheck.includes(bonusNum);
     if (!bonuscorrect) return 2;
     if (bonuscorrect) return 3;
   }
 
-  duplicate(numbers){
-    return new Set(numbers).size !== numbers.length;
+  duplicate(numcheck){
+    return new Set(numcheck).size !== numcheck.length;
   }
 }
 
