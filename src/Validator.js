5925d245135a5660c44e0aaceaa129d84c817725
publdaze <publdaze@naver.com>
2023-11-08 20:40:40 +0900

    refactor: 오타 수정

--- src/Validator.js
+++ src/Validator.js
@@ -3,8 +3,8 @@ import LOTTO_INFO from './constants/lottoInfo.js';
 import { ERROR_MESSAGE } from './constants/message.js';
 import {
   isEmptyString,
-  isIntegarNumList,
-  isIntegarString,
+  isIntegerNumList,
+  isIntegerString,
   isValidPurchaseUnit,
   isInvalidNumberRange,
   hasInvalidNumberRange,
@@ -14,10 +14,8 @@ import {
 class Validator {
   static checkPurchaseAmount(input) {
     if (isEmptyString(input)) throw new CustomError(ERROR_MESSAGE.empty);
-    if (!isIntegarString(input))
-      throw new CustomError(ERROR_MESSAGE.notInteger);
-    if (!isValidPurchaseUnit(Number(input)))
-      throw new CustomError(ERROR_MESSAGE.purchaseUnit);
+    if (!isIntegerString(input)) throw new CustomError(ERROR_MESSAGE.notInteger);
+    if (!isValidPurchaseUnit(Number(input))) throw new CustomError(ERROR_MESSAGE.purchaseUnit);
   }
 
   static checkWinningNumbers(input) {
@@ -26,22 +24,17 @@ class Validator {
     const inputList = input.split(',');
     if (inputList.length !== LOTTO_INFO.lottoNumber.count)
       throw new CustomError(ERROR_MESSAGE.invalidWinningNumberCount);
-    if (!isIntegarNumList(inputList))
-      throw new CustomError(ERROR_MESSAGE.notIntegerList);
+    if (!isIntegerNumList(inputList)) throw new CustomError(ERROR_MESSAGE.notIntegerList);
 
     const numberList = inputList.map(Number);
-    if (hasInvalidNumberRange(numberList))
-      throw new CustomError(ERROR_MESSAGE.invalidWinningNumberRange);
-    if (hasDuplicateNumber(numberList))
-      throw new CustomError(ERROR_MESSAGE.duplicate);
+    if (hasInvalidNumberRange(numberList)) throw new CustomError(ERROR_MESSAGE.invalidWinningNumberRange);
+    if (hasDuplicateNumber(numberList)) throw new CustomError(ERROR_MESSAGE.duplicate);
   }
 
   static checkBonusNumbers(input) {
     if (isEmptyString(input)) throw new CustomError(ERROR_MESSAGE.empty);
-    if (!isIntegarString(input))
-      throw new CustomError(ERROR_MESSAGE.notInteger);
-    if (isInvalidNumberRange(Number(input)))
-      throw new CustomError(ERROR_MESSAGE.invalidWinningNumberRange);
+    if (!isIntegerString(input)) throw new CustomError(ERROR_MESSAGE.notInteger);
+    if (isInvalidNumberRange(Number(input))) throw new CustomError(ERROR_MESSAGE.invalidWinningNumberRange);
   }
 }
 
