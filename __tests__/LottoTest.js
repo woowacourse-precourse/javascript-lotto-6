3240f1a18238a2142dab60363c281dfd9c7f0178
Seoan Yoon <earthdyoon@gmail.com>
2023-11-08 23:39:46 +0900

    feat: 로또 생성 및 비교 테스트 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,37 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../src/model/Lotto';
+import { calculateMatchingCount } from '../src/utils/LottoUtil';
+import { ERROR } from '../src/Constants';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.lottoLengthErrorMessage);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.lottoDuplicateErrorMessage);
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호에 1 ~ 45 범위를 벗어나는 숫자가 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 100]);
+    }).toThrow(ERROR.lottoRangeErrorMessage);
+  });
+
+  test('로또 번호에 숫자가 아닌 문자가 포함되면 에외가 발생한다', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 'a']);
+    }).toThrow(ERROR.lottoInvalidErrorMessage);
+  });
+
+  test('두 로또 번호의 일치하는 숫자 갯수를 반환한다', () => {
+    const lottoA = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lottoB = new Lotto([5, 6, 7, 8, 9, 10]);
+    const count = calculateMatchingCount(lottoA, lottoB);
+
+    expect(count).toEqual(2);
+  });
 });
