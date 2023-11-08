e35f61c402acedc7ef23fd61c80911a2738d95a0
osohyun0224 <osohyun0224@naver.com>
2023-11-08 18:31:05 +0900

    test(ProfitCalculatorTest): 수익률 계산 테스트 코드 리팩토링
    - 테스트 코드 내에서 직접 정의되었던 PRIZE_MONEY 상수를 가져올 수 있도록 리팩토링 하였습니다.
    - 앞서 수익률 계산 클래스 구조를 리팩토링함에 따라 그에 맞춰 해당 클래스의 인스턴스를 생성 하도록 코드를 수정하였습니다.

--- __tests__/ProfitCalculatorTest.js
+++ __tests__/ProfitCalculatorTest.js
@@ -1,14 +1,12 @@
 import ProfitCalculator from "../src/models/ProfitCalculator.js";
-
-const PRIZE_MONEY = {
-  5: 5000, 
-  4: 50000,
-  3: 1500000,
-  2: 30000000,
-  1: 2000000000, 
-};
+import { PRIZE_MONEY, INITIAL_RESULT  } from "../src/constants/Constant.js";
 
 describe("ProfitCalculator 클래스 테스트", () => {
+  let profitCalculator;
+
+  beforeEach(() => {
+    profitCalculator = new ProfitCalculator();
+  });
 
   test("총 이익금액 테스트", () => {
     const result = {
@@ -20,14 +18,13 @@ describe("ProfitCalculator 클래스 테스트", () => {
     };
     
     const expectedTotalPrize = 3 * PRIZE_MONEY[5] + 2 * PRIZE_MONEY[4] + 1 * PRIZE_MONEY[3];
-    expect(ProfitCalculator.calculateTotalPrize(result)).toBe(expectedTotalPrize);
+    expect(profitCalculator.calculateTotalPrize(result)).toBe(expectedTotalPrize);
   });
 
   test("수익률 계산 테스트", () => {
     const totalPrize = 2050000;
     const purchasePrice = 3000;
     const expectedROI = (totalPrize / purchasePrice) * 100;
-    expect(ProfitCalculator.calculateROI(totalPrize, purchasePrice)).toBe(expectedROI);
+    expect(profitCalculator.calculateROI(totalPrize, purchasePrice)).toBe(expectedROI);
   });
-
 });
