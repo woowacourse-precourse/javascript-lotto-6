6bfae6716585dfa078e5e135e6a0b00d05ff6277
easyhyun00 <leejihyun0324@gmail.com>
2023-11-08 12:57:51 +0900

    test(lotto): 로또 예외 상황 테스트 케이스 추가
    - 로또 번호가 6개가 아닐 때 예외 발생
    - 로또 번호가 NaN 일 때 예외 발생

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,9 +1,12 @@
 import Lotto from "../src/model/Lotto";
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+  test("로또 번호의 개수가 6개가 아니면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
     }).toThrow("[ERROR]");
   });
 
@@ -17,11 +20,17 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, "가"]);
     }).toThrow("[ERROR]");
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, NaN]);
+    }).toThrow("[ERROR]");
   });
 
   test("로또 번호가 1부터 45까지 수가 아니면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([0, 2, 3, 4, 5, 6]);
+    }).toThrow("[ERROR]");
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
     }).toThrow("[ERROR]");
   });
 });
