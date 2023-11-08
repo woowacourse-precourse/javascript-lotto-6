3f1ca9ebc1699e7243380475bf307eb503a9d66a
guswns8431 <guswns8431@gmail.com>
2023-11-08 22:38:01 +0900

    fix : 테스트 코드 통과
    테스트 코드 통과

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Console } from '@woowacourse/mission-utils';
 import {
   inputUserWinningNumber,
   inputUserPurchaseAmount,
@@ -9,18 +10,40 @@ import LottoGame from './LottoGame.js';
 
 class App {
   async play() {
-    const purchaseAmount = await inputUserPurchaseAmount();
-    const user = new User(purchaseAmount);
-    const userWinningNumberInput = await inputUserWinningNumber();
-    const bonusNumber = await inputUserBonusNumber();
-    const lotto = new Lotto(userWinningNumberInput, bonusNumber);
-    const lottoGame = new LottoGame(
-      user.issuedLottoNumberList,
-      lotto.getNumbers(),
-      lotto.getBonusNumber(),
-    );
-    lottoGame.printResult();
-    lottoGame.printProfit();
+    const user = await userInputController();
+    await userWinningNumberController(user);
+  }
+}
+
+async function userInputController() {
+  while (true) {
+    try {
+      const purchaseAmount = await inputUserPurchaseAmount();
+      const user = new User(purchaseAmount);
+      return user;
+    } catch (e) {
+      Console.print(`[ERROR] ${e}`);
+    }
+  }
+}
+
+async function userWinningNumberController(user) {
+  while (true) {
+    try {
+      const userWinningNumberInput = await inputUserWinningNumber();
+      const bonusNumber = await inputUserBonusNumber();
+      const lotto = new Lotto(userWinningNumberInput, bonusNumber);
+      const lottoGame = new LottoGame(
+        user.issuedLottoNumberList,
+        lotto.getNumbers(),
+        lotto.getBonusNumber(),
+      );
+      lottoGame.printResult();
+      lottoGame.printProfit();
+      return;
+    } catch (e) {
+      Console.print(`[ERROR] ${e}`);
+    }
   }
 }
 
