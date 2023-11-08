2a8cf0f401bb7d40a1f899e1b6059693a8bf5473
ryan-dia <cjfdnjs158@naver.com>
2023-11-08 19:02:51 +0900

    test: 당첨번호와 유저가 구매한 번호를 올바르게 비교하는 메서드 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 import Lotto from '../src/Lotto.js';
+import { LOTTO } from '../src/constants/System.js';
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -25,4 +26,22 @@ describe('로또 클래스 테스트', () => {
       new Lotto([1, 2, 3, 4, 49, 6]);
     }).toThrow('[ERROR]');
   });
+
+  describe.each([
+    // winningNumbers, bonus, userLotto, matchCount, expectedRank]
+    [[1, 2, 3, 4, 5, 7], 6, [1, 2, 3, 4, 5, 6], 5 + 1, '2등'],
+    [[1, 2, 3, 4, 5, 6], 7, [1, 2, 3, 4, 5, 6], 6, '1등'],
+    [[1, 2, 3, 4, 5, 6], 9, [1, 2, 3, 4, 5, 8], 5, '3등'],
+    [[1, 2, 3, 4, 5, 6], 10, [1, 2, 3, 4, 8, 9], 4, '4등'],
+    [[1, 2, 3, 4, 5, 6], 10, [1, 2, 3, 11, 18, 19], 3, '5등'],
+  ])(
+    'Lotto 클래스의 compareWinningNumbers 메서드',
+    (winningNumbers, bonus, userLotto, matchCount, expectedRank) => {
+      it(`${matchCount}개가 일치하면 ${expectedRank}을 반환해야 합니다`, () => {
+        const lotto = Lotto.of(winningNumbers);
+        const result = lotto.compareWinningNumbers(userLotto, bonus);
+        expect(result).toBe(expectedRank);
+      });
+    },
+  );
 });
