38aa4caee02ef857ffbb0db86b04b1b2e41ec3b4
j8won <joowoni99@gmail.com>
2023-11-08 15:54:10 +0900

    refactor(LottoPrizeManagerTest): BonusNumberTest로 보너스 번호 예외 처리 테스트 분리

--- __tests__/LottoPrizeManagerTest.js
+++ __tests__/LottoPrizeManagerTest.js
@@ -1,26 +1,9 @@
 import LottoPrizeManager from "../src/LottoPrizeManager.js";
-import PRIZE from "../src/constant/PRIZE.js";
 
 const correctWinningNumber = "1,2,3,4,5,6";
 const correctBonusNumber = "7";
 
 describe("LottoPrizeManager 클래스 테스트", () => {
-  test.each([["100"], ["123s"], ["015"]])(
-    "보너스 번호가 로또 숫자 범위 내의 숫자가 아니라면 예외 처리한다 - %s",
-    (bonusNumber) => {
-      expect(() => {
-        new LottoPrizeManager(correctWinningNumber, bonusNumber);
-      }).toThrow(PRIZE.ERROR.BONUS_NUMBER_RANGE_NUMBER);
-    }
-  );
-
-  test("보너스 번호가 당첨 번호와 중복되면 예외 처리한다", () => {
-    const bonusNumber = "1";
-    expect(() => {
-      new LottoPrizeManager(correctWinningNumber, bonusNumber);
-    }).toThrow(PRIZE.ERROR.BONUS_NUMBER_DUPLICATE);
-  });
-
   test.each([
     ["FIRST", [1, 2, 3, 4, 5, 6]],
     ["SECOND", [1, 2, 3, 4, 5, 7]],
