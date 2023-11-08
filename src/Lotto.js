2868595c7c18eeb8c81e51d8b3c646b42e4a77b0
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-11-08 19:06:38 +0900

    feat: Lotto class 로또 번호 범위에 대한 예외 처리 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -14,12 +14,16 @@ class Lotto {
 
     const lottoNumbers = [];
     numbers.forEach(number => {
+      if (number <1 || number > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+
       if (lottoNumbers.includes(number)) {
         throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
       }
 
       lottoNumbers.push(number);
-    })
+    });
   }
 
   // TODO: 추가 기능 구현
