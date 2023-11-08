fc84f5134122c5dc954a9be164fc010dd9b8863d
baegyeong <beth0929@naver.com>
2023-11-08 17:37:32 +0900

    refactor(constant): GAME_INFO의 키 값 변경
    - 객체이름에 의미가 담겨 있기 때문에 키 값의 'Input' 삭제

--- src/Input.js
+++ src/Input.js
@@ -3,24 +3,20 @@ import { GAME_INFO } from "./constant/GAME_INFO.js";
 
 export const Input = {
   async inputPurchasePrice() {
-    const purchasePrice = await Console.readLineAsync(
-      GAME_INFO.INPUT_PURCHASE_PRICE
-    );
+    const purchasePrice = await Console.readLineAsync(GAME_INFO.PURCHASE_PRICE);
 
     return await +purchasePrice;
   },
 
   async inputLotto() {
-    let lottoArray = await Console.readLineAsync(GAME_INFO.INPUT_LOTTO);
+    let lottoArray = await Console.readLineAsync(GAME_INFO.LOTTO);
     lottoArray = await lottoArray.toString().split(",").map(Number);
 
     return lottoArray;
   },
 
   async inputBonusNumber() {
-    const bonusNumber = await Console.readLineAsync(
-      GAME_INFO.INPUT_BONUS_NUMBER
-    );
+    const bonusNumber = await Console.readLineAsync(GAME_INFO.BONUS_NUMBER);
 
     return await +bonusNumber;
   },
--- src/constant/GAME_INFO.js
+++ src/constant/GAME_INFO.js
@@ -1,6 +1,6 @@
 export const GAME_INFO = {
-  INPUT_PURCHASE_PRICE: "구입금액을 입력해 주세요.\n",
-  INPUT_LOTTO: "당첨 번호를 입력해 주세요.\n",
-  INPUT_BONUS_NUMBER: "보너스 번호를 입력해 주세요.\n",
+  PURCHASE_PRICE: "구입금액을 입력해 주세요.\n",
+  LOTTO: "당첨 번호를 입력해 주세요.\n",
+  BONUS_NUMBER: "보너스 번호를 입력해 주세요.\n",
   ENTER: " ",
 };
