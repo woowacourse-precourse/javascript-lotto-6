862c52d13777caa02380a3e8038a4eb4f1379c3c
Dobbymin <gmin0701@knu.ac.kr>
2023-11-08 11:01:52 +0900

    📝 refactor(getNumber): 입력받은 금액 예외처리

--- src/components/getNumber.js
+++ src/components/getNumber.js
@@ -1,8 +1,14 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { LOTTO } from '../pages/texts';
+import { ERROR, LOTTO } from '../pages/texts.js';
 
 export default async function getNumber() {
     let money = await MissionUtils.Console.readLineAsync(LOTTO.buy);
 
+    if (money % 1000 !== 0) throw new Error(ERROR.input_1000won);
+    if (isNaN(money)) throw new Error(ERROR.numeric);
+    if (money === '' || money === ' ') throw new Error(ERROR.numeric_null);
+
     return money;
 }
+
+getNumber();
