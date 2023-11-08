3ed8d717f9dd35642963f5223f0603519efffaca
Kidprogramer1039 <bnpo5231@naver.com>
2023-11-08 17:38:59 +0900

    feat: 로또 오류 함수 통합

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -7,8 +7,7 @@ export const LOTTO_RANGE = Object.freeze({
 
 export const ERROR_MESSAGE = Object.freeze({
 	lottoCount: '[ERROR] 로또 번호는 6개여야 합니다.',
-	lottoForm: '[ERROR] 숫자를 입력해주시기 바랍니다.',
+	lottoForm: '[ERROR] 1000원 단위로 입력해주시거나, 문자가 아닌 숫자를 입력해주시기 바랍니다.',
 	lottoRange: '[ERROR] 로또 숫자는 1~45 범위 이내로 입력해주시기 바랍니다.',
     lottoDuplication: '[ERROR] 로또 숫자가 중복됩니다.',
-	lottoDevide: '[ERROR] 1000원 단위로 입력해주시기 바랍니다.',
 });
