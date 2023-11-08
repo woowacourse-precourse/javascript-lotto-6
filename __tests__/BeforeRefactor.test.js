2f442f8bedbb5c3e1a91fb823b6a550801e14c69
lucia0929 <thenum1developer@gamil.com>
2023-11-08 19:32:51 +0900

    feat : (T-1-3) 금액 입력 예외 처리 테스트 구현

--- __tests__/BeforeRefactor.test.js
+++ __tests__/BeforeRefactor.test.js
@@ -18,6 +18,7 @@ describe("InputManager class", () => {
 
   describe("T-1-1 금액 입력 메세지 출력", () => {
     test("사용자에게 금액 입력 메세지를 출력해야 한다", async () => {
+      Console.readLineAsync.mockResolvedValue("1000");
       await inputManager.enterAmount();
       expect(Console.readLineAsync).toHaveBeenCalledWith("구입금액을 입력해 주세요.\n");
     });     
@@ -30,7 +31,23 @@ describe("InputManager class", () => {
       
       const amount = await inputManager.enterAmount();
       expect(Console.readLineAsync).toHaveBeenCalled();
-      expect(amount).toBe(validAmount);
+      expect(amount).toBe(parseInt(validAmount, 10));
+    });
+  });
+
+  describe("T-1-3 금액 입력 예외 처리", () => {
+    test("금액이 숫자가 아닌 경우 예외를 던져야 한다", async () => {
+      const invalidAmount = "abc";
+      Console.readLineAsync.mockResolvedValue(invalidAmount);
+
+      await expect(inputManager.enterAmount()).rejects.toThrow("[ERROR] 구입 금액은 숫자여야 합니다.");
+    });
+
+    test("1,000원으로 나누어 떨어지지 않을 경우 예외를 던져야 한다", async () => {
+      const invalidAmount = "1500";
+      Console.readLineAsync.mockResolvedValue(invalidAmount);
+
+      await expect(inputManager.enterAmount()).rejects.toThrow("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
     });
   });
 });
