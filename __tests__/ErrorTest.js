9f484022151f454a606fa1c0e29191ba994bc43f
Klomachenko <leekyumin0901@naver.com>
2023-11-08 21:19:38 +0900

    feat(errorTest): bonusNumberError 테스트 구현

--- __tests__/ErrorTest.js
+++ __tests__/ErrorTest.js
@@ -11,4 +11,10 @@ describe("ErrorMessages 클래스 테스트", () => {
     const result = ErrorMessages.lottoNumbersDuplicate();
     expect(result).toBe("[ERROR] 중복된 로또번호는 있을 수 없습니다.");
   });
+
+  it("bonusNumberError 메서드가 올바른 에러 메시지를 반환해야 합니다.", () => {
+    const result = ErrorMessages.bonusNumberError();
+    expect(result).toBe("[ERROR] 보너스 번호는 로또번호와 달라야 합니다.");
+  });
+
 });
