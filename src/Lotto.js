d934fa2231551ec4d5c9e23c8de5e482b956767c
meunrud <113085963+meunrud@users.noreply.github.com>
2023-11-08 04:27:45 +0000

    2차 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -9,21 +9,29 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
-    const uniqueNumbers = new Set(numbers);
-    if (uniqueNumbers.size !== numbers.length) {
-      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
-    }
-    // eslint-disable-next-line no-restricted-syntax
-    for (const number of numbers) {
-      if (typeof number !== 'number' || isNaN(number)) {
-        throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+    try {
+      if (numbers.length !== 6) {
+        throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
       }
-    }
-    if (numbers.some((item) => typeof item === 'string')) {
-      throw new Error('[ERROR]');
+  
+      const uniqueNumbers = new Set(numbers);
+      if (uniqueNumbers.size !== numbers.length) {
+        throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
+      }
+  
+      for (const number of numbers) {
+        if (typeof number !== 'number' || isNaN(number)) {
+          throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+        }
+      }
+  
+      if (numbers.some((item) => typeof item === 'string')) {
+        throw new Error('[ERROR]');
+      }
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+      // 에러 발생 후 다시 입력 요청
+      return;
     }
   }
 
