fa18b8ccd181de30285eda6eaa9aee13222ad605
hjs0522 <91768822+hjs0522@users.noreply.github.com>
2023-11-08 13:09:34 +0900

    Feat: "로또 번호 타당성 함수 구현" 번호 갯수 확인, 번호 중복 확인, 번호 범위 확인 하는 타당성 함수 개발

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6){
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+    if (numbers.some(number => number < 1 || number > 45)){
+      throw new Error("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
+    }
   }
 
   // TODO: 추가 기능 구현
