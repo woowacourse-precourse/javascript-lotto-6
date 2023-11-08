f7c0e2036baf40f9423883caa49bf8dfe99e3cb8
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-11-08 17:09:45 +0900

    :recycle: Refactor : 상수 객체 동결
    Object.freeze()로 상수 객체 동결

--- src/Constants/Constant.js
+++ src/Constants/Constant.js
@@ -1,4 +1,4 @@
-const Constant = {
+const Constant = Object.freeze({
   UNIT_OF_AMOUNT: 1000,
   MIN_RANDOM_NUMBER: 1,
   MAX_RANDOM_NUMBER: 45,
@@ -8,6 +8,6 @@ const Constant = {
   3: 1500000,
   2: 30000000,
   1: 2000000000,
-};
+});
 
 export default Constant;
--- src/Constants/Message.js
+++ src/Constants/Message.js
@@ -1,9 +1,9 @@
-export const SYSTEM_MESSAGE = {
+export const SYSTEM_MESSAGE = Object.freeze({
   INPUT_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
   PURCHASE_LOTTO_MESSAGE: '개를 구매했습니다.',
   INPUT_GOAL_NUMBER: '당첨 번호를 입력해 주세요.\n',
   INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
-};
+});
 
 export const resultMessage = (correctArray, rateOfReturn) =>
   `당첨 통계
@@ -15,11 +15,11 @@ export const resultMessage = (correctArray, rateOfReturn) =>
   6개 일치 (2,000,000,000원) - ${correctArray[4]}개
   총 수익률은 ${rateOfReturn}%입니다.`;
 
-export const ERROR_MESSAGE = {
+export const ERROR_MESSAGE = Object.freeze({
   IS_EMPTY: '[ERROR] 구입금액을 0원 이상 입력하세요.',
   INPUT_NUMBER: '[ERROR] 숫자를 입력하세요.',
   NOT_DISVISIBLE: '[ERROR] 1000원 단위로 입력하세요.',
   WRONG_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
   IS_DUPLICATED: '[ERROR] 로또 번호를 중복 없이 입력하세요.',
   NOT_IN_RANGE: '[ERROR] 1 ~ 45 이내의 숫자만을 입력하세요.',
-};
+});
