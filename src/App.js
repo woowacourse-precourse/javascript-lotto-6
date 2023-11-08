6662600061bc9e80296c4fb6650cfeed6e23cb7a
KangYunJi <dbswl701@ajou.ac.kr>
2023-11-08 23:24:33 +0900

    refactor: 잘못된 값을 입력한 경우, 에러 메시지를 출력하고 해당 부분부터 입력을 다시 받는다.

--- src/App.js
+++ src/App.js
@@ -14,22 +14,40 @@ class App {
     MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${result[4]}개`);
   }
 
-  async play() {
-    const money = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+  calculateLottoCount = async () => {
+    while(true) {
+      try {
+        const money = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
+        
+        if (isNaN(money)) throw new Error("[ERROR] 구입금액은 숫자여야 합니다.");
+        if (parseInt(money / 1000) !== money / 1000) throw new Error("[ERROR] 구입금액은 1000원 단위여야 합니다.");
+        if (money == 0) throw new Error("[ERROR] 구입금액은 0원 이상이어야 합니다.");  
 
-    // 예외처리
-    try{
-      if (isNaN(money)) throw new Error("[ERROR] 구입금액은 숫자여야 합니다.");
-      if (parseInt(money / 1000) !== money / 1000) throw new Error("[ERROR] 구입금액은 1000원 단위여야 합니다.");
-      if (money == 0) throw new Error("[ERROR] 구입금액은 0원 이상이어야 합니다.");  
-    } catch(error) {
-      console.log(error);
-      MissionUtils.Console.print(error.message);
+        const  count = money / 1000;
+        MissionUtils.Console.print(`${count}개를 구매했습니다.`);
+        return count;
+      } catch(error) {
+        MissionUtils.Console.print(error.message);
+      }
     }
+  }
 
-    // 구입금액에 해당하는 만큼 로또 발행
-    let count = money / 1000;
-    MissionUtils.Console.print(`${count}개를 구매했습니다.`);
+  inputBonus = async (win) => {
+    while(true) {
+      try {
+        const bonus = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+        if (isNaN(parseInt(bonus))) throw new Error("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        if (1 > bonus || bonus > 45) throw new Error("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
+        if (win.includes(bonus)) throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        return bonus;
+      } catch(error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
+  }
+  async play() {
+    let count = await this.calculateLottoCount();
+
     const lottoList = [];
     while(count--) {
       lottoList.push(MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b));
@@ -41,14 +59,10 @@ class App {
     const lotto = new Lotto(win.split(',').map(v=>Number(v)));
 
     // 보너스 번호 입력
-    const bonus = await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
-    if (isNaN(parseInt(bonus))) throw new Error("[ERROR] 보너스 번호는 숫자여야 합니다.");
-    if (1 > bonus || bonus > 45) throw new Error("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
-    if (win.includes(bonus)) throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    const bonus = await this.inputBonus(win);
 
     const result = lotto.stats(lottoList, bonus);
     this.printResult(result);
-
   }
 }
 
