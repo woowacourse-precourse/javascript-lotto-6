426cab1ac0bef8510bd6d85ca57f898220f4cba0
lottt105 <hyobin0601@naver.com>
2023-11-08 11:48:34 +0900

    feat: 로또 번호 입력 예외처리 기능 추가

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,5 +1,17 @@
 import { error, condition } from '../consts';
 
+const isInteger = (number) => {
+  if (!Number.isInteger(number)) {
+    throw error.numberTypeError;
+  }
+};
+
+const validateLottonRange = (number) => {
+  if (number < condition.lottoMinNumber || number > condition.lottoMaxNumber) {
+    throw error.numberRangeError;
+  }
+};
+
 const Validator = {
   validateInput(input) {
     if (input.trim().length === condition.inputEmptyLength) {
@@ -7,26 +19,27 @@ const Validator = {
     }
   },
 
-  validateNumbers(numbers) {
-    numbers.forEach((number) => this.validateNumber(number));
-  },
-
-  validateNumber(number) {
-    if (!Number.isInteger(number)) {
-      throw error.numberTypeError;
-    }
-    if (
-      number < condition.lottoMinNumber ||
-      number > condition.lottoMaxNumber
-    ) {
-      throw error.numberRangeError;
-    }
-  },
-
   validatePurchaseAmount(purchaseAmount) {
+    isInteger(purchaseAmount);
     if (purchaseAmount % condition.oneLottoPrice !== condition.correctRemain) {
       throw error.purchaseAmountError;
     }
+  },
+
+  validateWinnigNumbersString(winningNumbers) {
+    if (!Number(winningNumbers.split(condition.separator).join(''))) {
+      throw error.winningNumbersSeparatorError;
+    }
+  },
+
+  validateWinningNumbers(winningNumbers) {
+    if (winningNumbers.length !== condition.winningNumbersLength) {
+      throw error.winningNumbersLengthError;
+    }
+    if (new Set(winningNumbers).size !== condition.winningNumbersLength) {
+      throw error.winningNumbersDuplicationError;
+    }
+    winningNumbers.forEach((number) => validateLottonRange(number));
   },
 };
 
