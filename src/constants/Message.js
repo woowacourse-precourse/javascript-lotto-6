e69b4c00404dd9df73a87d5bf9d8bc4aeab269da
hyun812 <sh980812@naver.com>
2023-11-08 22:53:51 +0900

    feat: 로또 게임에 필요한 메시지 및 상수 추가

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -1,16 +1,19 @@
-import { standard } from "./Standard.js";
+import { standard } from './Standard.js';
 
 export const ERROR = {
   DIVIDE: `[ERROR] 로또 구입 금액이 ${standard.LOTTO_PRICE}원으로 나누어 떨어지지 않습니다.`,
   LENGTH: `[ERROR] 당첨 번호는 ${standard.LOTTO_MAX_COUNT}개 입니다.`,
-  DUPLICATION: "[ERROR] 중복되는 당첨 번호가 있습니다.",
-  NUMBER: "[ERROR] 입력 값은 숫자여야 합니다.",
+  DUPLICATION: '[ERROR] 중복되는 당첨 번호가 있습니다.',
+  NUMBER: '[ERROR] 입력 값은 숫자여야 합니다.',
+  RANGE: `[ERROR] ${standard.LOTTO_MIN_NUMBER}부터 ${standard.LOTTO_MAX_NUMBER} 사이의 숫자여야 합니다.`,
 };
 
 export const GAME = {
   INPUT_BUY_PRICE: `구입금액을 입력해 주세요.\n`,
-  PRINT_BUY_COUNT: `개를 구매했습니다.\n`,
-  INPUT_WINNING_NUMBERS: `당첨 번호를 입력해 주세요.\n`,
-  INPUT_BONUS_NUMBERS: `보너스 번호를 입력해 주세요.\n`,
-  PRINT_RESULT: "당첨 통계\n---\n",
+  PRINT_BUY_COUNT: `개를 구매했습니다.`,
+  INPUT_WINNING_NUMBERS: `\n당첨 번호를 입력해 주세요.\n`,
+  INPUT_BONUS_NUMBER: `\n보너스 번호를 입력해 주세요.\n`,
+  PRINT_RESULT: '\n당첨 통계\n---',
+  WINNING_CORRECT: ['3개 일치', '4개 일치', '5개 일치', '5개 일치, 보너스 볼 일치', '6개 일치'],
+  RESULT_RATE: '총 수익률은 ',
 };
--- src/constants/Standard.js
+++ src/constants/Standard.js
@@ -1,4 +1,8 @@
 export const standard = Object.freeze({
   LOTTO_PRICE: 1000,
   LOTTO_MAX_COUNT: 6,
+  LOTTO_MIN_NUMBER: 1,
+  LOTTO_MAX_NUMBER: 45,
+  WINNING_CORRECT_INIT: [0, 0, 0, 0, 0],
+  WINNING_CORRECT_PRICE: ['5,000', '50,000', '1,500,000', '30,000,000', '2,000,000,000'],
 });
