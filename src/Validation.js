26588e2aace4a6081849bc5b83e33efe934f4203
yura <yuraa0302@gmail.com>
2023-11-08 18:59:22 +0900

    feat : 한게임에 대한 로또번호들의 유효성 검사

--- src/Validation.js
+++ src/Validation.js
@@ -22,4 +22,41 @@ const Validation = {
       throw new Error("[ERROR] 1이상 45 이하의 로또 번호를 입력해 주세요.");
     }
   },
+
+  /**
+   * 로또 번호들 중에서 중복이 있는지
+   * @param {number[]} numbers
+   * @returns {boolean}
+   */
+  isDuplicate: (numbers) => new Set(numbers).size !== numbers.length,
+
+  /**
+   * 로또 번호들에 대한 유효성 검사
+   * @param {number[]} numbers
+   */
+  validateLottoNumbers: (numbers) => {
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개입니다.");
+    }
+    if (this.isDuplicate(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 6개입니다.");
+    }
+    numbers.forEach((number) => this.validateLottoNumber(number));
+  },
+
+  /**
+   * @param {number[]} numbers
+   * @param {number} bonus
+   */
+  validateLottoNumbersWithBonus: (numbers, bonus) => {
+    const concattedNumber = [...numbers, bonus];
+
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개입니다.");
+    }
+    if (this.isDuplicate(concattedNumber)) {
+      throw new Error("[ERROR] 로또 번호는 6개입니다.");
+    }
+    concattedNumber.forEach((number) => this.validateLottoNumber(number));
+  },
 };
