1066f1d941e5071a9e66c9a4dde6ddab3c62eea2
chorong <chorong4641@naver.com>
2023-11-08 22:50:33 +0900

    feat(play): 모든 기능을 합쳐서 진행이 이루어지도록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { lottoChecker } from "./LottoChecker.js";
+import { lottoBonusNumber, lottoBuyAmount, lottoWinningNumbers } from "./LottoInput.js";
+import { lottoMachine } from "./LottoMachine.js";
+import { printLottoAmount, printLottoNumbers, printLottoStatistics, printProfitRate } from "./LottoOutput.js";
+
+const PRICE = 1000;
+
 class App {
-  async play() {}
+  async play() {
+    const buyAmount = await lottoBuyAmount();
+
+    const lottoQuantity = buyAmount / PRICE;
+    printLottoAmount(lottoQuantity);
+
+    const randomNumbers = await lottoMachine(lottoQuantity);
+    printLottoNumbers(randomNumbers);
+
+    const lotto = await lottoWinningNumbers();
+    const bonusNumber = await lottoBonusNumber(lotto);
+    const matchedData = lottoChecker(randomNumbers, lotto, bonusNumber);
+    const winningCountData = printLottoStatistics(matchedData);
+    printProfitRate(buyAmount, winningCountData);
+  }
 }
 
 export default App;
