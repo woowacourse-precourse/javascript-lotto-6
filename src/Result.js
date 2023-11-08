dde89eefce1a0aa9e315616ca858f99625ee64e1
최진욱 <jinwook.fe@gmail.com>
2023-11-08 21:45:06 +0900

    [fix] 결과가 제대로 출력되지 않았던 오류 수정

--- src/Result.js
+++ src/Result.js
@@ -42,9 +42,11 @@ class Result {
     Console.print(`\n당첨 통계\n---`);
     Console.print(`3개 일치 (5,000원) - ${this.state.fifth.hit}개`);
     Console.print(`4개 일치 (50,000원) - ${this.state.fourth.hit}개`);
-    Console.print(`3개 일치 (1,500,000원) - ${this.state.third.hit}개`);
-    Console.print(`3개 일치 (30,000,000원) - ${this.state.second.hit}개`);
-    Console.print(`3개 일치 (2,000,000,000원) - ${this.state.first.hit}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.state.third.hit}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.state.second.hit}개`,
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.state.first.hit}개`);
   }
 }
 
