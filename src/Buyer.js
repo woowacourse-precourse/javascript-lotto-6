ab1ce5da8897522e6564b1b882febc9a74e51aa5
이동주 <leednj0113@naver.com>
2023-11-08 10:05:22 +0900

    feat(Buyer): 로또를 구입할 금액 입력

--- src/Buyer.js
+++ src/Buyer.js
@@ -3,6 +3,23 @@ import { Console } from "@woowacourse/mission-utils";
 class Buyer {
   #purchaseMoney;
   #lottos;
+
+  getPurchaseMoney() {
+    return this.#purchaseMoney;
+  }
+  async inputMoney() {
+    try {
+      const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+
+      this.#purchaseMoney = Number(input);
+    } catch (error) {
+      Console.print(error.message);
+
+      return this.inputMoney();
+    }
+
+    return this.#purchaseMoney;
+  }
 }
 
 export default Buyer;
