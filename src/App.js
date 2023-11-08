a085eb019df42cd7e2e516a02cc7cd0f1cd8d6e1
lee juyoung <juyung0903@naver.com>
2023-11-08 14:15:48 +0900

    refactor: 컨트롤러에서 전체 로직 포함한 playGame메소드 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import LottoController from "./controller/LottoController.js";
+import LottoController from './controller/LottoController.js';
 
 class App {
   constructor() {
     this.lottoController = new LottoController;
   }
   async play() {
-    await this.lottoController.sellLotto();
-    await this.lottoController.winningCalculation();
-    this.lottoController.lottoResult();
+    await this.lottoController.playGame();
   }
 }
 
--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -10,7 +10,7 @@ class LottoController {
     this.outputUi = new OutputUi();
     this.winningStatus = null;
   }
-
+  // 로또 구매 및 발행
   async sellLotto() {
     const PURCHASE_AMOUNT = await this.inputUi.askpurchaseAmount();
     this.lottoGame = new LottoGame(PURCHASE_AMOUNT);
@@ -18,15 +18,25 @@ class LottoController {
     this.outputUi.printPurchasedLottos(SORTED_LOTTO_NUMBERS);
   }
 
+  // 당첨현황 계산
   async winningCalculation() {
     const WINNING_NUMBER = await this.inputUi.askWinningNumber();
     const BONUS_NUMBER = await this.inputUi.askBonusNumber();
     this.winningStatus = this.lottoGame.getWinningStatus(WINNING_NUMBER, BONUS_NUMBER);
   }
 
-  async lottoResult() {
+  // 수익률 계산과 당첨현황 및 수익률 출력
+  lottoResult() {
+    const RATE_OF_RETURN = this.lottoGame.calculateRateOfReturn(this.winningStatus);
     this.outputUi.printWinnigStatus(this.winningStatus);
-    this.outputUi.printRateOfReturn(this.lottoGame.calculateRateOfReturn(this.winningStatus));
+    this.outputUi.printRateOfReturn(RATE_OF_RETURN);
+  }
+
+  // 전체 로직 실행
+  async playGame() {
+    await this.sellLotto();
+    await this.winningCalculation();
+    this.lottoResult();
   }
 }
 export default LottoController;
