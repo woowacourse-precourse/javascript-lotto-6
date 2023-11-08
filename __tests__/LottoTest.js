3b91eacf818de978b9a679e1a7e2b1f461949a73
zxxng <jeayeong123@naver.com>
2023-11-08 16:18:01 +0900

    test: 숫자 범위 체크하는 기능 티켓부스 이관으로 테스트 코드 수정

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,6 @@
 import Lotto from '../src/models/Lotto.js';
 
-describe('로또 클래스 테스트', () => {
+describe('Lotto 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -10,12 +10,6 @@ describe('로또 클래스 테스트', () => {
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
-  });
-
-  test('로또 번호의 숫자 범위가 1~45가 아니면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 333, 3, 4, 5, 50]);
     }).toThrow('[ERROR]');
   });
 
--- __tests__/TiketBoothTest.js
+++ __tests__/TiketBoothTest.js
@@ -3,11 +3,11 @@ import Lotto from '../src/models/Lotto';
 import TiketBooth from '../src/views/TicketBooth';
 
 describe('TiketBooth 클래스 테스트', () => {
+  const ticketBooth = new TiketBooth();
+
   test('당첨번호와 보너스번호가 반환되는지 테스트', async () => {
-    const ticketBooth = new TiketBooth();
     const mockWinningNumbers = '1,2,3,4,5,6';
     const mockBonusNumber = '7';
-
     Console.readLineAsync = jest
       .fn()
       .mockResolvedValueOnce(mockWinningNumbers)
@@ -20,5 +20,19 @@ describe('TiketBooth 클래스 테스트', () => {
       new Lotto(mockWinningNumbers.split(',').map(Number)),
       Number(mockBonusNumber),
     ]);
+  });
+
+  test('당첨 번호의 숫자 범위가 1~45가 아니면 예외가 발생한다.', async () => {
+    const mockWinningNumbers = '1,2,3,4,5,66';
+    Console.readLineAsync = jest.fn().mockResolvedValueOnce(mockWinningNumbers);
+
+    await expect(ticketBooth.receiveWinNumbers()).rejects.toThrow('[ERROR]');
+  });
+
+  test('보너스 번호의 숫자 범위가 1~45가 아니면 예외가 발생한다.', async () => {
+    const mockWinningNumbers = '77';
+    Console.readLineAsync = jest.fn().mockResolvedValueOnce(mockWinningNumbers);
+
+    await expect(ticketBooth.receiveWinNumbers()).rejects.toThrow('[ERROR]');
   });
 });
