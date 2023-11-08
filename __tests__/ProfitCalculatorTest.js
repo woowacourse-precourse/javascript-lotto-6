950994bcaddd6b0345510f7cccc62f5d3ed67e28
gilgor2 <misby0327@gmail.com>
2023-11-08 16:39:03 +0900

    test(ProfitCalculator.js): 총 수익률 계산자 객체에 대한 테스트 작성

--- __tests__/ProfitCalculatorTest.js
+++ __tests__/ProfitCalculatorTest.js
@@ -1,4 +1,5 @@
 import ProfitCalculator from '../src/ProfitCalculator';
+import WinningCondition from '../src/WinningCondition';
 
 describe('ProfitCalculator 클래스 테스트', () => {
 	test('로또 규칙에 명시된 금액에 맞게 수익률을 계산해야 한다.', async () => {
@@ -6,43 +7,27 @@ describe('ProfitCalculator 클래스 테스트', () => {
 		const budget = 8000;
 		const winningLottoList = [
 			{
-				condition: {
+				condition: new WinningCondition({
 					correctCnt: 3,
 					bonusCnt: 0,
 					winnings: 5000,
-				},
+				}),
 				count: 1,
 			},
 			{
-				condition: {
+				condition: new WinningCondition({
 					correctCnt: 4,
 					bonusCnt: 0,
 					winnings: 50000,
-				},
+				}),
 				count: 0,
 			},
 			{
-				condition: {
+				condition: new WinningCondition({
 					correctCnt: 5,
 					bonusCnt: 0,
 					winnings: 1500000,
-				},
-				count: 0,
-			},
-			{
-				condition: {
-					correctCnt: 5,
-					bonusCnt: 1,
-					winnings: 30000000,
-				},
-				count: 0,
-			},
-			{
-				condition: {
-					correctCnt: 6,
-					bonusCnt: 0,
-					winnings: 2000000000,
-				},
+				}),
 				count: 0,
 			},
 		];
