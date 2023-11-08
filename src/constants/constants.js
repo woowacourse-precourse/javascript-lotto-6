6c8ba11ba3fe94cce436925294ec4a260fa576af
leelsj2007 <leelsj2007@gmail.com>
2023-11-08 23:02:33 +0900

    style : 출력 상수 개행 부분 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,12 +6,12 @@ export const INPUT_MESSAGE = {
 
 export const OUTPUT_MESSAGE = {
     printPurchaseNumber : (count) => `\n${count}개 구매했습니다.`,
-    printWinningStatistics : "\n당첨 통계\n---\n",
-    printFifth : (count) => `3개 일치 (5,000원) - ${count}개\n`,
-    printFourth : (count) => `4개 일치 (50,000원) - ${count}개\n`,
-    printThird : (count) => `5개 일치 (1,500,000원) - ${count}개\n`,
-    printSecond : (count) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개\n`,
-    printFirst : (count) => `6개 일치 (2,000,000,000원) - ${count}개\n`,
+    printWinningStatistics : "\n당첨 통계\n---",
+    printFifth : (count) => `3개 일치 (5,000원) - ${count}개`,
+    printFourth : (count) => `4개 일치 (50,000원) - ${count}개`,
+    printThird : (count) => `5개 일치 (1,500,000원) - ${count}개`,
+    printSecond : (count) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`,
+    printFirst : (count) => `6개 일치 (2,000,000,000원) - ${count}개`,
     printRateReturn : (rate) => `총 수익률은 ${rate}%입니다.`,
 };
 
