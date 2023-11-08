d2894eec93a0c72a452ab2ce6114463bca504c0a
zxxng <jeayeong123@naver.com>
2023-11-08 11:13:24 +0900

    test: Analyzer private 함수 테스트코드 제외

--- __tests__/AnalyzerTest.js
+++ __tests__/AnalyzerTest.js
@@ -1,7 +1,7 @@
 import Analyzer from '../src/models/Analyzer';
 
 describe('Analyzer 클래스 테스트', () => {
-  const winnigResult = [
+  const matchedNumberList = [
     [6, 0],
     [5, 1],
     [5, 0],
@@ -13,20 +13,17 @@ describe('Analyzer 클래스 테스트', () => {
   const prizeMoeny = [2000000000, 30000000, 1500000, 50000, 5000];
   const totalPrize = prizeMoeny.reduce((sum, cur) => sum + cur, 0);
   const money = 7000;
-  const analyzer = new Analyzer(winnigResult, money);
+  const analyzer = new Analyzer();
+  analyzer.countWinningRank(matchedNumberList);
   const rankedLotto = analyzer.getRankedLotto();
 
-  test('당첨 결과에 따른 상금을 얻는다', () => {
+  test('로또 번호 매칭 결과에 따른 등수를 얻는다', () => {
     expect(rankedLotto.first).toBe(1);
     expect(rankedLotto.second).toBe(1);
     expect(rankedLotto.third).toBe(1);
     expect(rankedLotto.fourth).toBe(1);
     expect(rankedLotto.fifth).toBe(1);
     expect(rankedLotto.losingLotto).toBe(2);
-  });
-
-  test('총 상금을 계산한다', () => {
-    expect(analyzer.calculateTotalPrize()).toBe(totalPrize);
   });
 
   test('수익률을 계산한다', () => {
