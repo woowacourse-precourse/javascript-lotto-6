b3d2ed98a73cc1c5cb43617cc93d47ab5ce5ad86
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-08 12:41:45 +0900

    style: App.js 공백 수정
    가독성을 위해 각 기능 단위로 공백 설정

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ class App {
   async play() {
     const purchaseAmount = await LottoUi.inputPurchaseAmount();
     const purchasedLottos = LottoMachine.drawLottos(purchaseAmount);
+
     const winningNumbers = await LottoUi.inputWinningNumbers();
     const bonusNumber = await LottoUi.inputBonusNumber();
+
     const lotto = new Lotto(purchasedLottos);
     lotto.showResultOfLotto(winningNumbers, bonusNumber);
   }
