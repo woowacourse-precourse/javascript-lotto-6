48f7828077e264328417f9eb0a69ebb74a497bce
soominnnn <marisol03@naver.com>
2023-11-08 17:03:07 +0900

    ✅Test: 보너스 번호 범위 미 포함 테스트 추가

--- __tests__/BonusTest.js
+++ __tests__/BonusTest.js
@@ -13,4 +13,14 @@ describe("보너스 클래스 테스트", () => {
     const RESULT = () => new Bonus(input);
     expect(RESULT).toThrow("[ERROR] 숫자만 입력해주세요.");
   });
+
+  test.each([
+    [200],
+    [50],
+    [46],
+  ])
+  ("보너스 번호가 범위 내에 포함되지 않은 경우", (input) => {
+    const RESULT = () => new Bonus(input);
+    expect(RESULT).toThrow("[ERROR] 숫자는 1~45까지 입력해주세요.");
+  });
 });
