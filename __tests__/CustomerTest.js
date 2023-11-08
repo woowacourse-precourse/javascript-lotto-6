0e673122839f4ab6b73996d7bb546bebd44db8ae
flowersayo <flowersayo0811@ewhain.net>
2023-11-08 23:44:37 +0900

    style: 테스트 파일명 변경
    Buyer -> Customer

--- __tests__/CustomerTest.js
+++ __tests__/CustomerTest.js
@@ -2,8 +2,9 @@
 
 import Customer from '../src/domain/Customer';
 import { LOTTO } from '../src/Constant';
+import Lotto from '../src/domain/Lotto';
 
-describe('Buyer 도메인 로직 테스트', () => {
+describe('Customer 도메인 로직 테스트', () => {
   test.each([-1000, 0.1, 8.11, 999, 0, 99999])(
     `로또 구매 금액은 ${LOTTO.MONEY_LIMIT} 이하의 양의 정수여야 한다.`,
     (money) => {
