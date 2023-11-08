45af75de9d7c94bb2584457505319ad3083a8684
d-charlie-kim <d.charlie.kim@gmail.com>
2023-11-08 16:33:29 +0900

    Feat: 메세지 및 숫자들 상수 처리

---
+++ src/constants/message.js
@@ -0,0 +1,29 @@
+export const ERROR_MSG = {
+  INVALID_MONEY: '[ERROR] 구입금액 숫자 양식이 잘못되었습니다.',
+  INVALID_WINNER_NUMBERS: '[ERROR] 당첨 번호 숫자 양식이 잘못되었습니다.',
+  INVALID_BONUS_NUMBER: '[ERROR] 보너스 번호 숫자 양식이 잘못되었습니다.',
+  INVALID_BONUS_NUMBER_COUNT: '[ERROR] 보너스 번호 숫자는 1개여야 합니다.',
+  INVALID_WINNER_NUMBERS_COUNT: '[ERROR] 당첨 번호는 6개여야 합니다.',
+  DUPLICATED_WINNER_NUMBER: '[ERROR] 당첨 번호는 서로 중복될 수 없습니다.',
+  DUPLICATED_BONUS_NUMBER:
+    '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
+};
+
+export const INPUT_MSG = {
+  MONEY: '구입금액을 입력해 주세요.\n',
+  WINNER_NUMBERS: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+};
+
+export const OUTPUT_MSG = {
+  LOTTOS: '개를 구매했습니다.\n',
+  WINNERS: '당첨 통계\n---\n',
+  RATE_OF_RETURNS: ['총 수익률은 ', '%입니다.'],
+  RANK: {
+    FIRST: ['6개 일치 (2,000,000,000원) - ', '개'],
+    SECOND: ['5개 일치, 보너스 볼 일치 (30,000,000원) - ', '개'],
+    THIRD: ['5개 일치 (1,500,000원) - ', '개'],
+    FORTH: ['4개 일치 (50,000원) - ', '개'],
+    FIFTH: ['3개 일치 (5,000원) - ', '개'],
+  },
+};
---
+++ src/constants/numbers.js
@@ -0,0 +1,8 @@
+export const NUM_REGEXP = /^[0-9]+$/;
+
+export const LOTTO = {
+  UNIT: 1000,
+  MIN: 1,
+  MAX: 45,
+  COUNT: 6,
+};
