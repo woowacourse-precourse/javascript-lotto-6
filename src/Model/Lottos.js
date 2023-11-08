8a357132cf9fa5fbf252b2f03b6ad0c52237faae
yunji <dbswl330@gmail.com>
2023-11-08 14:26:27 +0900

    feat: 로또 티켓 구매 개수를 구하는 함수 구현 및 변수명 변경

--- src/Model/Lottos.js
+++ src/Model/Lottos.js
@@ -7,14 +7,17 @@ class Lottos {
     this.#lottoTickets = [];
   }
 
+  calculateLottoQuantity(purchaseAmount) {
+    this.validateAmount(purchaseAmount);
+    this.#lottoQuantity = purchaseAmount / 1000;
+  }
+
   validatePurchaseAmount(purchaseAmount) {
-    if (!Number.isInteger(Number(amount))) {
+    if (!Number.isInteger(purchaseAmount)) {
       throw new Error("[ERROR] 1000원 단위의 정수인 숫자를 입력해 주세요.");
     }
-    if (amount <= 0 || amount % 1000 !== 0) {
+    if (purchaseAmount <= 0 || purchaseAmount % 1000 !== 0) {
       throw new Error("[ERROR] 1000원 단위의 유효한 구매 금액을 입력하세요.");
     }
   }
-
-  check;
 }
