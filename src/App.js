8a297bd9859895fcb1bef5c501044ad730e2d96d
JoSooha <dlrhdcjs@naver.com>
2023-11-08 18:13:25 +0900

    feat: 유저 로또 생성 및 출력

--- src/App.js
+++ src/App.js
@@ -4,14 +4,36 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const count = await this.getMoneyReturnCount();
-    console.log("count", count);
+    const lottoCount = await this.getMoneyReturnCount();
+    console.log("lottoCount", lottoCount);
+    MissionUtils.Console.print(`${lottoCount}개를 구매했습니다.`);
+    const lotto = this.generateAndPrintLotto();
   }
 
   async getMoneyReturnCount() {
     const money = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
     if(money%1000 !== 0) throw new Error("[ERROR] 1000원 단위로 입력해 주세요.");
     return money/1000;
+  }
+
+  generateAndPrintLotto(count) {
+    const lottoArray = [];
+    for(let i=0; i<count; i++) {
+      const oneLotto = this.generateLotto();
+      MissionUtils.Console.print(oneLotto);
+      lottoArray.push(oneLotto);
+    }
+    return lottoArray;
+  }
+
+  generateLotto() {
+    const array = [];
+    for(let i=0; i<6; i++) {
+      const num = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      array.push(num);
+    }
+    array.sort((a,b)=>a-b);
+    return array
   }
   
 }
