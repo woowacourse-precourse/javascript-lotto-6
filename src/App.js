ff2aeead203e8a23557fb9d955b5c4939cbee8b5
AlgeMoya <kjhobd2@naver.com>
2023-11-08 21:04:16 +0900

    구입 금액 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print("구입금액을 입력해 주세요.");
+    let purchaseAmount = await Console.readLineAsync("");
+    if (isNaN(purchaseAmount)) {
+      throw new Error("[ERROR] 숫자만 입력할 수 있습니다.");
+    }
+    purchaseAmount = parseInt(purchaseAmount);
+    if (purchaseAmount < 1000) {
+      throw new Error("[ERROR] 최소 구매금액은 1000원입니다.");
+    }
+  }
 }
 
 export default App;
