797884c45b4814ed4bdaedf7bc28cc5064dfa104
gilgor2 <misby0327@gmail.com>
2023-11-08 16:28:03 +0900

    test(LottoRules.js): 변경된 로또 규칙 객체에 대한 테스트 작성

--- __tests__/LottoRulesTest.js
+++ __tests__/LottoRulesTest.js
@@ -3,21 +3,29 @@ import LottoRules from '../src/LottoRules';
 describe('LottoRules 클래스 테스트', () => {
 	test('일치하는 개수, 보너스 번호를 입력 받았을 때 올바른 금액을 반환한다.', () => {
 		// given
-		const cntExamples = [
-			[4, 0],
-			[3, 1],
-			[5, 1],
-			[6, 0],
+		const newRule = [
+			{ correctCnt: 3, bonusCnt: 0, winnings: 5000 },
+			{ correctCnt: 5, bonusCnt: 0, winnings: 1500000 },
+			{ correctCnt: 6, bonusCnt: 0, winnings: 2000000000 },
+			{ correctCnt: 4, bonusCnt: 0, winnings: 50000 },
+			{ correctCnt: 5, bonusCnt: 1, winnings: 30000000 },
 		];
-		const expectedWinnings = [50000, -1, 30000000, 2000000000];
+
 		// when
-		const rules = new LottoRules();
+		const rules = new LottoRules(newRule);
 
 		// then
-		cntExamples.forEach(([correctCnt, bonusCnt], i) => {
-			expect(rules.getWinnings(correctCnt, bonusCnt)).toBe(
-				expectedWinnings[i],
-			);
+		const totalRules = rules.getTotalRule();
+		totalRules.forEach((condition, i) => {
+			if (i > 0) {
+				const currentWinnings =
+					condition.getWinningCondition()
+						.winnings;
+				const prevWinnings =
+					totalRules[i - 1].getWinningCondition()
+						.winnings;
+				expect(currentWinnings > prevWinnings).toBe(true);
+			}
 		});
 	});
 });
