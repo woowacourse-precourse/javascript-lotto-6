0fde9c7672f53a33ab581908c933ba330e29a806
narinn-star <skfls2618@naver.com>
2023-11-08 23:09:59 +0900

    refactor : CustomError 적용

--- src/Validator/validator.js
+++ src/Validator/validator.js
@@ -1,4 +1,5 @@
 import { ERROR, LOTTO } from '../Constants/constants.js';
+import CustomError from '../Error/CustomError.js';
 
 class validator {
   static validatePurchaseAmount(amount) {
@@ -8,13 +9,13 @@ class validator {
 
   static validatePurchaseAmountDivided(amount) {
     if ((amount * 1000) % 1000 !== 0) {
-      throw new Error(ERROR.PREFIX + ERROR.PURCHASE_AMOUNT.NOT_PRIME_NUMBER);
+      throw new CustomError(ERROR.PURCHASE_AMOUNT.NOT_PRIME_NUMBER);
     }
   }
 
   static validatePurchaseAmountIsNumber(amount) {
     if (isNaN(amount)) {
-      throw new Error(ERROR.PREFIX + ERROR.PURCHASE_AMOUNT.IS_NOT_NUMBER);
+      throw new CustomError(ERROR.PURCHASE_AMOUNT.IS_NOT_NUMBER);
     }
   }
 
@@ -27,20 +28,20 @@ class validator {
 
   static validateLottoNumbersLength(numbers) {
     if (numbers.length !== 6) {
-      throw new Error(ERROR.PREFIX + ERROR.LOTTO_NUMBERS.INVALID_LENGTH);
+      throw new CustomError(ERROR.LOTTO_NUMBERS.INVALID_LENGTH);
     }
   }
 
   static validateLottoNumbersDuplicate(numbers) {
     if (new Set(numbers).size !== LOTTO.LOTTO_NUMBERS) {
-      throw new Error(ERROR.PREFIX + ERROR.LOTTO_NUMBERS.IS_DUPLICATE);
+      throw new CustomError(ERROR.LOTTO_NUMBERS.IS_DUPLICATE);
     }
   }
 
   static validateLottoNumbersIsNumber(numbers) {
     numbers.forEach(number => {
       if (isNaN(number)) {
-        throw new Error(ERROR.PREFIX + ERROR.LOTTO_NUMBERS.IS_NOT_NUMBER);
+        throw new CustomError(ERROR.LOTTO_NUMBERS.IS_NOT_NUMBER);
       }
     });
   }
@@ -48,7 +49,7 @@ class validator {
   static validateLottoNumbersRange(numbers) {
     numbers.forEach(number => {
       if (number < LOTTO.MIN_NUMBER || number > LOTTO.MAX_NUMBER) {
-        throw new Error(ERROR.LOTTO_NUMBERS.INVALID_RANGE);
+        throw new CustomError(ERROR.LOTTO_NUMBERS.INVALID_RANGE);
       }
     });
   }
@@ -61,17 +62,17 @@ class validator {
 
   static validateBonusNumberIsDuplicate(bonusNumber, winNumber) {
     if (winNumber.includes(bonusNumber)) {
-      throw new Error(ERROR.BONUS_NUMBER.IS_DUPLICATE);
+      throw new CustomError(ERROR.BONUS_NUMBER.IS_DUPLICATE);
     }
   }
   static validateBonusNumberIsNumber(bonusNumber) {
     if (isNaN(bonusNumber)) {
-      throw new Error(ERROR.BONUS_NUMBER.IS_NOT_NUMBER);
+      throw new CustomError(ERROR.BONUS_NUMBER.IS_NOT_NUMBER);
     }
   }
   static validateBonusNumberLength(bonusNumber) {
     if (bonusNumber > 10 || bonusNumber < 1) {
-      throw new Error(ERROR.BONUS_NUMBER.INVALID_LENGTH);
+      throw new CustomError(ERROR.BONUS_NUMBER.INVALID_LENGTH);
     }
   }
 }
