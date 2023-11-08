9f86b8f95a59c9b28b270816c68226a8f665d7a4
lucia0929 <thenum1developer@gamil.com>
2023-11-08 17:53:30 +0900

    feat : (C-1-1) 금액 입력 메세지 출력 기능 구현

---
+++ src/InputManager.js
@@ -0,0 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class InputManager {
+    async enterAmount() {
+        Console.print("구입금액을 입력해 주세요.\n");
+    }
+}
+  
+export default InputManager;
