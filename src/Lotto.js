3237cf92f2312dd610299edc2ff89fbe674fbb19
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-11-08 22:54:08 +0900

    Refactor : 테스트 케이스 적용(중복된 수 입력시 lotto객체 생성 오류 테스트, 입력된 번호 범위 검증 테스트)

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,8 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.#validateDuplicationNumber(numbers);
+    this.#validateNumberRange(numbers)
     this.#numbers = numbers;
   }
 
@@ -14,7 +16,25 @@ class Lotto {
 
   setNumbers(numbers) {
     this.#numbers = numbers;
+  #validateDuplicationNumber(numbers) {
+    const seen = {};
+
+    for (const item of numbers) {
+      if (seen[item]) {
+        throw new Error("[ERROR] : 중복된 수 없이 입력해주세요");
+      }
+      seen[item] = true;
+    }
   }
+
+  #validateNumberRange(numbers) {
+    for (const item of numbers) {
+      if ((numbers[item] > 45) || (numbers[item] < 1)) {
+        throw new Error("[ERROR] 입력번호는 1~45 사이 수여야 합니다.");
+      }
+    }
+  }
+
   getNumbers() {
     return this.#numbers;
   }
