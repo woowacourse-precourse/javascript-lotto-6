c35b40ad9716917af045412f46933f647d956689
jayming66 <mjw8941@gmail.com>
2023-11-08 12:20:52 +0900

    feat : 로또 상금 판단 및 결과 출력

--- src/App.js
+++ src/App.js
@@ -37,7 +37,44 @@ class App {
       })
 
 
+      const resultCount = [0, 0, 0, 0, 0]
+      let totalPrize = 0;
       
+      for (let i = 0; i < lottoResult.length; i++) {
+
+        if (lottoResult[i] === 6) {
+          resultCount[0]++;
+          totalPrize += 2000000000;
+        }
+        if (lottoResult[i] === 5 && lottoBonusResult[i] === 1) {
+          resultCount[1]++;
+          totalPrize += 30000000;
+        }
+        if (lottoResult[i] === 4 && lottoBonusResult[i] === 1) {
+          resultCount[2]++;
+          totalPrize += 1500000;
+        }
+        if (lottoResult[i] === 3 && lottoBonusResult[i] === 1) {
+          resultCount[3]++;
+          totalPrize += 50000;
+        }
+        if (lottoResult[i] === 2 && lottoBonusResult[i] === 1) {
+          resultCount[4]++;
+          totalPrize += 5000;
+        }
+      }
+      
+      Console.print('\n' + '당첨 통계');
+      Console.print(`---`);
+      Console.print(`3개 일치 (5,000원) - ${resultCount[4]}개`);
+      Console.print(`4개 일치 (50,000원) - ${resultCount[3]}개`);
+      Console.print(`5개 일치 (1,500,000원) - ${resultCount[2]}개`);
+      Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${resultCount[1]}개`);
+      Console.print(`6개 일치 (2,000,000,000원) - ${resultCount[0]}개`);
+      
+      const totalProfitRate = ((totalPrize / moneyInput) * 100).toFixed(1);
+
+      Console.print(`총 수익률은 ${totalProfitRate}%입니다.`);
       
     } catch (error) {
       Console.print(error.message);
