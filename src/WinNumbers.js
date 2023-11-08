01459dd2b82e63bf8fe4f382abcf83d0e34db0df
ssoferret <owosso1212@gmail.com>
2023-11-08 21:41:23 +0900

    style : 라인 정렬 정리.

--- src/WinNumbers.js
+++ src/WinNumbers.js
@@ -2,23 +2,23 @@ import Validate from "./Validate.js";
 import { Console } from '@woowacourse/mission-utils';
  
 class WinNumbers {
-    getNumbers = async() => {
-        const input = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
-        const inputNumbers = input.split(',').map(Number);
-        return inputNumbers;
-      }
-    
-    getBonus = async(numbers) => {
-      const bonus = Number(await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n'));
-      const validate = new Validate();
-      try {
-        validate.bonusInput(bonus, numbers)
-      } catch {
-        Console.print(validate.WARNING.BONUS);
-        await this.getBonus(numbers);
-      }
-      return bonus;
-    }
+  getNumbers = async() => {
+    const input = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+    const inputNumbers = input.split(',').map(Number);
+    return inputNumbers;
   }
+    
+  getBonus = async(numbers) => {
+    const bonus = Number(await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n'));
+    const validate = new Validate();
+    try {
+      validate.bonusInput(bonus, numbers)
+    } catch {
+      Console.print(validate.WARNING.BONUS);
+      await this.getBonus(numbers);
+    }
+    return bonus;
+  }
+}
   
-    export default WinNumbers;
+export default WinNumbers;
