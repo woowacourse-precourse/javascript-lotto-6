0a4c9ca36870a302514796528ad004829230ba0c
62savage <6s2avage@gmail.com>
2023-11-08 23:22:59 +0900

    chore(src/constants) : 로또 입력개수 조건을 상수 객체로 설정

---
+++ src/constants/Conditions.js
@@ -0,0 +1,10 @@
+const CONDITIONS = Object.freeze({
+  lotteryLength: 6,
+  fifthPrize: 5000,
+  fourthPrize: 50000,
+  thirdPrize: 1500000,
+  secondPrize: 30000000,
+  firstPrize: 2000000000,
+});
+
+export default CONDITIONS;
