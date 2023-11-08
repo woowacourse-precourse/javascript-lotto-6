a6703f9bb369856b316640ff6249765a0ffe0141
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-08 13:02:02 +0900

    ✅ test: 여러 장의 로또의 당첨 내역 및 통계 출력 테스트 추가

--- __tests__/LottoStatisticsTest.js
+++ __tests__/LottoStatisticsTest.js
@@ -17,7 +17,7 @@ describe('당첨 내역 및 통계 출력 테스트', () => {
     [1000, [1,2,3,7,8,9], [1,2,3,4,5,6], 12, 5000, 50.0],
     [1000, [1,2,3,4,5,6], [1,2,3,4,5,6], 12, 2000000000, 20000000.0],
   ])(
-    "이 테스트에 대한 구입 금액은 %s원, 발행된 로또는 %s, 당첨 번호는 %s, 보너스 번호는 %s, 당첨 금액은 %s, 수익률은 %s%여야한다.",
+    "로또 한 장 테스트 - 이 테스트에 대한 구입 금액은 %s원, 발행된 로또는 %s, 당첨 번호는 %s, 보너스 번호는 %s, 당첨 금액은 %s, 수익률은 %s%여야한다.",
     (money, randoms, winnings, bonus, expectedPrizeMoney, expectedStats) => {
 
       mockRandoms([randoms]);
@@ -25,7 +25,7 @@ describe('당첨 내역 및 통계 출력 테스트', () => {
       const tickets = new LottoTickets(money).publishTickets();
       const winningNumbers = new Lotto(winnings).returnWinningNumbers();
       const bonusNumber = new BonusNumber(bonus, winnings).returnValue();
-      const stats = new Lotto(winnings).calculateWinningStats(tickets, winningNumbers, bonusNumber);
+      const stats = new Lotto(winnings).calculateWinningStats(tickets, [...winningNumbers, bonusNumber]);
       const profit = new Lotto(winnings).getProfits(stats)
       const rate = new Lotto(winnings).calculateRate(profit, money) * 10
 
@@ -33,6 +33,32 @@ describe('당첨 내역 및 통계 출력 테스트', () => {
       expect(bonusNumber).toEqual(bonus)
       expect(profit).toEqual(expectedPrizeMoney)
       expect(rate).toEqual(expectedStats)
+    },
+  );
+
+  test(
+    "로또 여러 장 테스트",
+    () => {
+      const money = 2000;
+      const randoms = [[1,2,3,4,5,7], [3,5,10,23,33,45]]
+      const winnings = [1,3,6,10,45]
+      const bonus = 5
+      const result = [55000, 275.0]
+
+      mockRandoms(randoms);
+
+      const tickets = new LottoTickets(money).publishTickets();
+      const winningNumbers = new Lotto(winnings).returnWinningNumbers();
+      const bonusNumber = new BonusNumber(bonus, winnings).returnValue();
+      const stats = new Lotto(winnings).calculateWinningStats(tickets, [...winningNumbers, bonusNumber]);
+      console.log(`stats: ${stats}`)
+      const profit = new Lotto(winnings).getProfits(stats)
+      const rate = new Lotto(winnings).calculateRate(profit, money) * 10
+
+      expect(winningNumbers).toEqual(winnings)
+      expect(bonusNumber).toEqual(bonus)
+      expect(profit).toEqual(result[0])
+      expect(rate).toEqual(result[1])
     },
   );
 })
