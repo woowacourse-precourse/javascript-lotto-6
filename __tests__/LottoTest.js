f86e1b80056166070ee8dec77715494e79fc750e
unknown <pih9134@naver.com>
2023-11-08 19:12:18 +0900

    test: LottoTest for문 제거

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -2,23 +2,23 @@ import Lotto from '../src/Lotto.js';
 import { ERROR_MESSAGE, LOTTO_NUMBER } from '../src/constants.js';
 
 describe('Lotto 클래스 테스트', () => {
-  test('유효한 로또 번호일 경우.', () => {
+  test('유효한 로또 번호일 경우 통과된다.', () => {
     const validNumbers = [1, 2, 3, 4, 5, 6];
     const lotto = new Lotto(validNumbers);
     expect(lotto.getNumbers()).toEqual(validNumbers);
   });
 
-  test('6개가 아닌 로또 번호일 경우 예외 처리', () => {
+  test('6개가 아닌 로또 번호일 경우 예외 처리.', () => {
     const invalidNumbers = [1, 2, 3, 4, 5, 6, 7];
     expect(() => new Lotto(invalidNumbers)).toThrow(ERROR_MESSAGE.duplicationWinningNumbers);
   });
 
-  test('중복된 로또 번호일 경우 예외 처리', () => {
+  test('중복된 로또 번호일 경우 예외 처리.', () => {
     const invalidNumbers = [1, 2, 3, 4, 5, 5];
     expect(() => new Lotto(invalidNumbers)).toThrow(ERROR_MESSAGE.duplicationWinningNumbers);
   });
 
-  test('로또번호가 6개이고 1부터 45 사이의 수이다.', () => {
+  test('로또번호가 6개이고 1부터 45 사이의 수이어야 통과된다.', () => {
     const lotto = new Lotto();
     const numbers = lotto.getNumbers();
 
@@ -28,12 +28,17 @@ describe('Lotto 클래스 테스트', () => {
     );
   });
 
-  test('로또 번호가 오름차순으로 정렬되어 있다.', () => {
+  test('로또 번호가 오름차순으로 정렬되어 있어야 통과된다.', () => {
     const lotto = new Lotto();
     const numbers = lotto.getNumbers();
 
-    for (let i = 0; i < numbers.length - 1; i += 1) {
-      expect(numbers[i] < numbers[i + 1]).toBe(true);
-    }
+    const isSorted = numbers.every((number, index, array) => {
+      if (index === 0) {
+        return true;
+      }
+      return number > array[index - 1];
+    });
+
+    expect(isSorted).toBe(true);
   });
 });
