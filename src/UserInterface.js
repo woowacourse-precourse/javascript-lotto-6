c9b4feeb55bb207995531e3caa548d049810299c
Luna <chlwlal20@gmail.com>
2023-11-08 19:32:41 +0900

    feat(UserInterface): 당첨 번호 입력받기

--- src/UserInterface.js
+++ src/UserInterface.js
@@ -37,7 +37,51 @@ const UserInterface = () => {
     });
   };
 
-  return { getLottoNumbers, validateLottoNumbers, printLottoNumbers };
+  const getWinningNumbers = async () => {
+    const winningNumbers = [];
+    let isValid = false;
+    while (!isValid) {
+      const winningNumber = await Console.readLineAsync(
+        "당첨 번호를 입력해 주세요.\n"
+      );
+      try {
+        validateWinningNumbers(winningNumber);
+
+        winningNumber.split(",").forEach((number) => {
+          winningNumbers.push(parseInt(number, 10));
+        });
+        isValid = true;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return winningNumbers;
+  };
+
+  const validateWinningNumbers = (winningNumbers) => {
+    const winningNumbersArray = winningNumbers.split(",");
+    if (winningNumbersArray.length !== 6) {
+      throw new Error("[ERROR] 6개의 숫자를 입력해주세요.");
+    }
+
+    const regex = /^[0-9,]*$/;
+    if (!regex.test(winningNumbers)) {
+      throw new Error("[ERROR] 숫자와 쉼표만 입력해주세요.");
+    }
+
+    winningNumbersArray.forEach((number) => {
+      if (number < 1 || number > 45) {
+        throw new Error("[ERROR] 1~45 사이의 숫자만 입력해주세요.");
+      }
+    });
+
+    const set = new Set(winningNumbersArray);
+    if (set.size !== 6) {
+      throw new Error("[ERROR] 당첨 번호에 중복된 숫자가 있습니다.");
+    }
+    return winningNumbers;
+  }
+  return { getLottoNumbers, printLottoNumbers, getWinningNumbers };
 };
 
 export default UserInterface;
