90cac69d65ca1542c74640bc943a86d392d87fbd
박지윤 <tkrhkfld123@naver.com>
2023-11-08 15:09:50 +0900

    test: 로또 번호 출력 확인

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -125,3 +125,38 @@ describe("generateLottos 테스트", () => {
   });
 });
 
+describe("displayPurchasedLottos", () => {
+  it("로또 티켓을 올바르게 출력해야 합니다", () => {
+    // given
+    const app = new App();
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+    logSpy.mockClear();
+
+    const lottos = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
+
+    // when
+    app.displayPurchasedLottos(lottos);
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("8개를 구매했습니다.")); // 로또 티켓 개수 출력 확인
+    lottos.forEach(numbers => {
+      expect(logSpy).toHaveBeenCalledWith(`[${numbers.join(', ')}]`); // 각 로또 번호 출력 확인
+    });
+  });
+});
+
+
+
+
+
+
+
