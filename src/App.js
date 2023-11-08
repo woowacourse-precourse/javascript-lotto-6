1cf73a6d7a2738b570cef6ef7047759a9d41fd32
hzzzi <hji2004@naver.com>
2023-11-08 20:24:51 +0900

    refactor: 게임 데이터와 결과, 모듈 분리

--- src/App.js
+++ src/App.js
@@ -1,56 +1,38 @@
-import { Console } from '@woowacourse/mission-utils';
-import Ticket from './Ticket.js';
-import { OUTPUT_MESSAGE, LOTTO } from './Constants.js';
 import { getValidPurchasePrice, getValidLottoNumber, getValidBonusNumber } from './Validate.js';
-
-function getAmountOfTickets(purchasePrice) {
-  return purchasePrice / LOTTO.PRICE;
-}
-
-function showAmountOfTickets(amountOfTickets) {
-  Console.print(OUTPUT_MESSAGE.BUY_TICKET(amountOfTickets));
-}
+import {
+  getAmountOfTickets,
+  showAmountOfTickets,
+  makeTicketArray,
+  checkTicketRank,
+} from './GameData.js';
+import {
+  getRankCounts,
+  showResult,
+  calcEarning,
+  calcEarningRate,
+  showEarningRate,
+} from './Result.js';
 
 class App {
   async play() {
     const purchasePrice = await getValidPurchasePrice();
+
     const amountOfTickets = getAmountOfTickets(purchasePrice);
     showAmountOfTickets(amountOfTickets);
+    const ticketArr = makeTicketArray(amountOfTickets);
 
-    const ticketArr = [];
-    for (let i = 0; i < amountOfTickets; i++) {
-      const ticket = new Ticket();
-      ticket.generateRandomNumbers();
-      ticket.showNumbers();
-      ticketArr.push(ticket);
-    }
     const winningNumbers = await getValidLottoNumber();
     const bonusNumber = await getValidBonusNumber();
 
-    ticketArr.forEach((ticket) => {
-      const [intersection, isBonusNumberMatch] = ticket.compareNumbers(winningNumbers, bonusNumber);
-      ticket.updateRank(intersection, isBonusNumberMatch);
-    });
+    checkTicketRank(ticketArr, winningNumbers, bonusNumber);
 
-    const rankCounts = [0, 0, 0, 0, 0];
+    const rankCounts = getRankCounts(ticketArr);
+    showResult(rankCounts);
 
-    ticketArr.forEach((ticket) => {
-      if (ticket.Rank >= 1 && ticket.Rank <= 5) {
-        rankCounts[ticket.Rank - 1]++;
-      }
-    });
+    const earning = calcEarning(rankCounts);
+    const earningRate = calcEarningRate(earning, purchasePrice);
 
-    Console.print(OUTPUT_MESSAGE.RESULT(rankCounts));
-
-    const earning =
-      LOTTO.EARNING.RANK_5 * rankCounts[4] +
-      LOTTO.EARNING.RANK_4 * rankCounts[3] +
-      LOTTO.EARNING.RANK_3 * rankCounts[2] +
-      LOTTO.EARNING.RANK_2 * rankCounts[1] +
-      LOTTO.EARNING.RANK_1 * rankCounts[0];
-    const earningRate = Math.round((earning / purchasePrice) * 10000) / 100;
-
-    Console.print(OUTPUT_MESSAGE.EARNING(earningRate));
+    showEarningRate(earningRate);
   }
 }
 
