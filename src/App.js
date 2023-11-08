007e5be0c6659c5662be595203c09c08e559a99e
이호연 <ho991217@kakao.com>
2023-11-08 23:32:08 +0900

    docs: play 주석 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Statistics from './Statistics.js';
 import User from './User.js';
 
 class App {
+  /*
+   * 1. '사용자' 생성
+   * 2. '사용자'가 복권을 구매
+   * 3. '동행복권'에서 당첨 번호를 생성
+   * 4. '사용자'가 구매한 복권과 당첨 번호를 비교
+   * 5. '통계'를 생성
+   * 6. '통계'를 출력
+   */
   async play() {
     this.user = new User();
     await this.user.buy();
