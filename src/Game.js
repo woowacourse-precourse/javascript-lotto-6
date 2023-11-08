3f2bfc3eff312316dbd811f0d249c7ddf5e9d1d0
Klomachenko <leekyumin0901@naver.com>
2023-11-08 22:03:05 +0900

    feat(Game): getUserMoney  사용자로부터 구입 금액 입력 기능 구현

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 import InputOutput from "./InputOutput";
+import Validate from "./Validate";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class Game {
@@ -29,6 +30,13 @@ class Game {
     InputOutput.print(`${lottoCount}개를 구매했습니다.`);
     return lottoCount;
   }
+
+  getUserMoney = async () => {
+    const userInput = await InputOutput.getInput("구입금액을 입력해 주세요.");
+    if (!Validate.isNumeric(userInput)) throw new Error(ErrorMessages.invalidInput("숫자를 입력해주세요"));
+    if (!Validate.isMultipleOf1000(userInput)) throw new Error(ErrorMessages.invalidInput("돈은 천원 단위로 입력해주세요"));
+    return this.calculateLottoCountFromMoney(userInput);
+  };
 
 }
 
