47d3c8ab9c1480b748398d97b62eb570bdb58c68
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-08 15:44:46 +0900

    chore: fix typo

--- src/models/Draw.js
+++ src/models/Draw.js
@@ -5,11 +5,11 @@ const draw = {
   /*
    * purchased: 사용자가 구매한 로또
    * winning: 사용자가 입력한 당첨 번호
-   * bounus: 사용자가 입력한 보너스 번호
+   * bonus: 사용자가 입력한 보너스 번호
    */
-  getResult: function (purchased, winning, bounus) {
+  getResult: function (purchased, winning, bonus) {
     let matches = this.getLottoMatches(purchased, winning);
-    const count = this.getBounusMatches(matches, bounus);
+    const count = this.getBonusMatches(matches, bonus);
     matches.splice(3, 0, count);
 
     return matches;
@@ -27,7 +27,7 @@ const draw = {
     return matches;
   },
 
-  /* 로또번호와 당첨번호가 일치하는 개수를 센다. */
+  /* 로또번호(lotto)와 당첨번호(winning)가 일치하는 개수를 센다. */
   countMatches: function (lotto, winning) {
     let count = 0;
     winning.forEach((number) => {
@@ -38,17 +38,17 @@ const draw = {
   },
 
   /* 2등의 개수를 반환한다. */
-  getBounusMatches: function (matches, bounus) {
+  getBonusMatches: function (matches, bonus) {
     let count = 0;
 
-    if (bounus === this.pickBounusNumber()) {
+    if (bonus === this.pickBonusNumber()) {
       count = matches[2];
     }
 
     return count;
   },
 
-  pickBounusNumber: function () {
+  pickBonusNumber: function () {
     const number = Random.pickUniqueNumbersInRange(
       lottoInfo.START_INCLUSIVE,
       lottoInfo.END_INCLUSIVE,
