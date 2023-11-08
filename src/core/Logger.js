a2f642956eef1ac957c8c67dd6eef10c2556aa7e
fecapark <fecapark@gmail.com>
2023-11-08 14:30:40 +0900

    feat: 당첨 통계 출력 로직 구현 완료

--- src/core/Logger.js
+++ src/core/Logger.js
@@ -1,4 +1,7 @@
+//@ts-check
+
 import { Console } from "@woowacourse/mission-utils";
+import { WINNING_FIELD_INDEX, WINNING_PRICES } from "./const.js";
 
 export default class Logger {
   constructor() {}
@@ -21,10 +24,69 @@ export default class Logger {
 
   /**
    *
+   * @param {{
+   *    three: number;
+   *    four: number;
+   *    five: number;
+   *    bonusFive: number;
+   *    six: number;
+   *    returnRate: number;
+   * }} board
+   * @param {number} returnRate
+   */
+  printWinningBoard(board, returnRate) {
+    Console.print("당첨 통계");
+    Console.print("---");
+
+    Object.keys(board).forEach((fieldName) => {
+      Console.print(this.#getWinningFieldString(fieldName, board[fieldName]));
+    });
+
+    Console.print(`총 수익률은 ${this.#roundFrom(2, returnRate)}%입니다.`);
+  }
+
+  /**
+   *
    * @param {any[]} arr
    * @returns {string}
    */
   #arrayToPrettyString(arr) {
     return `[${arr.join(", ")}]`;
+  }
+
+  /**
+   *
+   * @param {string} fieldName
+   */
+  #getWinningFieldString(fieldName, amount) {
+    const money = this.#integerToMoneyString(WINNING_PRICES[fieldName]);
+
+    if (fieldName === "bonusFive") {
+      return `${WINNING_FIELD_INDEX[fieldName]}개 일치, 보너스 볼 일치 (${money}원) - ${amount}개`;
+    }
+
+    return `${WINNING_FIELD_INDEX[fieldName]}개 일치 (${money}원) - ${amount}개`;
+  }
+
+  /**
+   *
+   * @param {number} num
+   * @returns {string}
+   */
+  #integerToMoneyString(num) {
+    return num.toLocaleString();
+  }
+
+  /**
+   * @param {number} num
+   * @param {number} position
+   * @returns {number}
+   */
+  #roundFrom(position, num) {
+    position = Math.floor(position);
+    num *= Math.pow(10, position - 1);
+    num = Math.round(num);
+    num /= Math.pow(10, position - 1);
+    return num;
   }
 }
--- src/core/const.js
+++ src/core/const.js
@@ -10,3 +10,19 @@ export const LOTTO_NUMBER_RANGE = {
   min: 1,
   max: 45,
 };
+
+// Winning Board
+export const WINNING_PRICES = {
+  three: 5000,
+  four: 50000,
+  five: 1500000,
+  bonusFive: 30000000,
+  six: 2000000000,
+};
+export const WINNING_FIELD_INDEX = {
+  three: 3,
+  four: 4,
+  five: 5,
+  bonusFive: 5,
+  six: 6,
+};
