c277e7fc283c5e5ebf99953a3a7ca06738bcea32
mingzooo <joozzoo0223@gmail.com>
2023-11-08 21:45:02 +0900

    [feat] 로또 발행 클래스

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,24 @@
-class Lotto {
+export default class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+      this.#validate(numbers);
+      this.#numbers = numbers;
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+      if (numbers.length !== 6) {
+          throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      }
+      if (new Set(numbers).size !== 6) {
+          throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+      }
+      if (numbers.some((number) => number < 1 || number > 45)) {
+          throw new Error("[ERROR] 로또 번호의 범위는 1~45 사이여야 합니다.");
+      }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+      return this.#numbers;
+  }
 }
-
-export default Lotto;
