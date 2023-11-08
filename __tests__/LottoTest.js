5bc9a36b6c7396d9d371bd442d6f8a225452e084
조예진 <joyejin@joyejins-MacBook-Air.local>
2023-11-08 19:15:38 +0900

    test: Lotto 클래스 테스트 작성 완료

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -34,3 +34,99 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 });
+
+describe('로또 클래스의 calculateRank 함수 테스트', () => {
+  test.each([
+    [[42, 12, 11, 2, 9, 23], [42, 12, 11, 2, 9, 23], 1],
+    [[18, 31, 36, 2, 40, 12], [2, 12, 18, 31, 36, 40], 9],
+    [[19, 5, 8, 6, 7, 12], [5, 8, 6, 7, 12, 19], 45],
+  ])(
+    '로또 번호가 6개가 일치하면 1을 반환한다. 나의번호: %p, calculateRank(%p, %d) => 1',
+    (numbers, winningNumbers, bonusNumber) => {
+      const lotto = new Lotto(numbers);
+
+      expect(lotto.calculateRank(winningNumbers, bonusNumber)).toEqual(1);
+    },
+  );
+
+  test.each([
+    [[42, 12, 1, 2, 9, 23], [42, 12, 11, 2, 9, 23], 1],
+    [[18, 9, 36, 2, 40, 12], [2, 12, 18, 31, 36, 40], 9],
+    [[45, 5, 8, 6, 7, 12], [5, 8, 6, 7, 12, 19], 45],
+  ])(
+    '로또 번호가 5개가 일치하고 다른 하나가 보너스 번호라면 2를 반환한다. 나의번호: %p, calculateRank(%p, %d) => 2',
+    (numbers, winningNumbers, bonusNumber) => {
+      const lotto = new Lotto(numbers);
+
+      expect(lotto.calculateRank(winningNumbers, bonusNumber)).toEqual(2);
+    },
+  );
+
+  test.each([
+    [[42, 12, 11, 2, 9, 23], [42, 12, 45, 2, 9, 23], 1],
+    [[18, 31, 36, 2, 40, 12], [2, 12, 18, 31, 36, 30], 9],
+    [[19, 5, 8, 6, 7, 12], [5, 8, 6, 7, 12, 1], 45],
+  ])(
+    '로또 번호가 5개가 일치하면 3을 반환한다. 나의번호: %p, calculateRank(%p, %d) => 3',
+    (numbers, winningNumbers, bonusNumber) => {
+      const lotto = new Lotto(numbers);
+
+      expect(lotto.calculateRank(winningNumbers, bonusNumber)).toEqual(3);
+    },
+  );
+
+  test.each([
+    [[42, 12, 11, 2, 9, 23], [4, 12, 45, 2, 9, 23], 1],
+    [[18, 31, 36, 2, 40, 12], [2, 23, 18, 31, 36, 30], 9],
+    [[19, 5, 8, 6, 7, 12], [5, 8, 6, 39, 12, 1], 45],
+  ])(
+    '로또 번호가 4개가 일치하면 4을 반환한다. 나의번호: %p, calculateRank(%p, %d) => 4',
+    (numbers, winningNumbers, bonusNumber) => {
+      const lotto = new Lotto(numbers);
+
+      expect(lotto.calculateRank(winningNumbers, bonusNumber)).toEqual(4);
+    },
+  );
+
+  test.each([
+    [[42, 12, 11, 2, 9, 20], [4, 12, 45, 2, 9, 23], 1],
+    [[18, 31, 36, 42, 40, 12], [2, 23, 18, 31, 36, 30], 9],
+    [[19, 5, 8, 36, 7, 12], [5, 8, 6, 39, 12, 1], 45],
+  ])(
+    '로또 번호가 3개가 일치하면 5을 반환한다. 나의번호: %p, calculateRank(%p, %d) => 5',
+    (numbers, winningNumbers, bonusNumber) => {
+      const lotto = new Lotto(numbers);
+
+      expect(lotto.calculateRank(winningNumbers, bonusNumber)).toEqual(5);
+    },
+  );
+
+  test.each([
+    [[42, 45, 11, 2, 39, 20], [4, 12, 45, 2, 9, 23], 1, 6],
+    [[1, 31, 3, 42, 40, 12], [2, 23, 18, 31, 36, 30], 9, 7],
+    [[19, 15, 28, 36, 7, 9], [5, 8, 6, 39, 12, 1], 45, 8],
+  ])(
+    '로또 번호가 일치하지 않거나 2개 이하면 6, 7, 8을 반환한다. 나의번호: %p, calculateRank(%p, %d) => %d',
+    (numbers, winningNumbers, bonusNumber, rank) => {
+      const lotto = new Lotto(numbers);
+
+      expect(lotto.calculateRank(winningNumbers, bonusNumber)).toEqual(rank);
+    },
+  );
+
+  test.each([
+    [[2, 4, 14, 21, 27, 33], [2, 4, 14, 21, 27, 33], 5, 1],
+    [[6, 10, 11, 20, 23, 43], [6, 10, 11, 20, 23, 40], 43, 2],
+    [[1, 2, 19, 28, 34, 40], [1, 2, 19, 27, 34, 40], 3, 3],
+    [[3, 14, 25, 30, 40, 45], [3, 14, 24, 30, 40, 44], 34, 4],
+    [[5, 6, 12, 17, 20, 26], [5, 6, 12, 19, 21, 32], 35, 5],
+    [[1, 2, 19, 32, 38, 39], [1, 3, 19, 33, 37, 40], 2, 6],
+  ])(
+    '나의 번호: %p, calculateRank(%p, %d) => %d',
+    (numbers, winningNumbers, bonuseNumber, result) => {
+      const lotto = new Lotto(numbers);
+
+      expect(lotto.calculateRank(winningNumbers, bonuseNumber)).toEqual(result);
+    },
+  );
+});
