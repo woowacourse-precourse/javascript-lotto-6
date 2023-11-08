04c7bae82262913bf8d10af6a83d2ba12fb98d4b
dxwwan <kkw1700@gmail.com>
2023-11-08 20:29:29 +0900

    test: 로또 로직의 예외 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,35 @@
+import test from "node:test";
 import Lotto from "../src/Lotto.js";
 
-describe("로또 클래스 테스트", () => {
+describe("로또 클래스 예외 테스트", () => {
+  test("로또 번호가 6개가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test("로또 번호가 1보다 작거나 45보다 크면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호와 당첨 번호가 중복되면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6], [6]);
+    }).toThrow("[ERROR]");
+  });
+});
 });
