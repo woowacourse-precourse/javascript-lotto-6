e5c50fe3534678e44f3d097a3b954f632c486b93
최원서 <dnjstj88@naver.com>
2023-11-08 11:20:41 +0900

    fix:예외처리 버그 수정
    조건 중 예외처리 후 종료시키는 것이 아닌 다시 입력받는 부분에 대한 오류가 있어 예외처리 후 에러를 출력하고 다시 입력받을 수 있도록 수정하였다

--- src/App.js
+++ src/App.js
@@ -5,35 +5,70 @@ import Lottos from './Lottos.js';
 import Lotto from './Lotto.js';
 import WinningStatistics from './WinningStatistics.js';
 class App {
-  constructor() {}
-  async play() {
-    const buyMoney = await Console.readLineAsync(INPUT_MESSAGE.INPUT_BUY_MONEY);
-    this.Money = new Money(buyMoney);
-    this.Lottos = new Lottos(buyMoney);
-    this.Lottos.printNumOfLottos();
-    this.Lottos.printCreatedLottos();
-
-    let winningNum = await Console.readLineAsync(
-      INPUT_MESSAGE.INPUT_WINNINGNUM
-    );
-
+  async checkBuyMoney() {
+    while (1) {
+      try {
+        const buyMoney = await this.inputNum();
+        this.Money = new Money(buyMoney);
+        return buyMoney;
+      } catch (Err) {
+        Console.print(Err);
+      }
+    }
+  }
+  modifyWinningNum(winningNum) {
     winningNum = winningNum.split(',');
     winningNum.map((e, i) => {
       winningNum[i] = parseInt(e);
     });
-    this.Lotto = new Lotto(winningNum);
-
-    let bonusNum = await Console.readLineAsync(INPUT_MESSAGE.INPUT_BONUSNUM);
-    bonusNum = parseInt(bonusNum);
-    this.Lotto.checkNumRange(bonusNum);
-
+    return winningNum;
+  }
+  async inputWinningNum() {
+    while (1) {
+      try {
+        let winningNum = await Console.readLineAsync(
+          INPUT_MESSAGE.INPUT_WINNINGNUM
+        );
+        winningNum = this.modifyWinningNum(winningNum);
+        this.Lotto = new Lotto(winningNum);
+        return winningNum;
+      } catch (err) {
+        Console.print(err);
+      }
+    }
+    return winningNum;
+  }
+  async inputBonusNum() {
+    while (1) {
+      try {
+        let bonusNum = await Console.readLineAsync(
+          INPUT_MESSAGE.INPUT_BONUSNUM
+        );
+        bonusNum = parseInt(bonusNum);
+        this.Lotto.checkNumRange(bonusNum);
+        return bonusNum;
+      } catch (Err) {
+        Console.print(Err);
+      }
+    }
+  }
+  async inputNum() {
+    const buyMoney = await Console.readLineAsync(INPUT_MESSAGE.INPUT_BUY_MONEY);
+    return buyMoney;
+  }
+  async play() {
+    const buyMoney = await this.checkBuyMoney();
+    this.Lottos = new Lottos(buyMoney);
+    this.Lottos.printNumOfLottos();
+    this.Lottos.printCreatedLottos();
+    const winningNum = await this.inputWinningNum();
+    const bonusNum = await this.inputBonusNum();
     this.winningStatistics = new WinningStatistics(
       this.Lottos.createdLottos,
       bonusNum,
       winningNum,
       buyMoney
     );
-
     this.winningStatistics.printResult();
   }
 }
