00b07a819a4711f2bee88af32f8680c64c0185bb
greeneeya <suzythetravelerr@gmail.com>
2023-11-08 23:17:06 +0900

    test(host): 보너스 입력값 리턴 테스트

--- __tests__/HostTest.js
+++ __tests__/HostTest.js
@@ -40,7 +40,7 @@ describe('로또 주관사 테스트', () => {
     },
   );
   it('보너스 번호가 로또 번호와 중복일 경우, 예외 처리', async () => {
-    // give
+    // given
     const logSpy = getLogSpy();
     const WINNING_NUMBERS = [1, 2, 3, 4, 5, 6];
     mockQuestions(['6', '7']);
@@ -54,5 +54,18 @@ describe('로또 주관사 테스트', () => {
         '[ERROR] 보너스 번호는 로또 번호와 중복이 불가합니다.',
       ),
     );
+  });
+
+  it('1000 단위의 수이고, 중복이 아닐 시엔 입력 값을 리턴', async () => {
+    // given
+    const WINNING_NUMBERS = [1, 2, 3, 4, 5, 6];
+    const bonusInput = 7;
+    mockQuestions([bonusInput]);
+
+    //when
+    const bonus = await host.getBonusNumber(WINNING_NUMBERS);
+
+    //then
+    expect(bonus).toBe(bonusInput);
   });
 });
