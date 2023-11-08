a4bc13bd888cdd62866722a7530da15de47a0375
yongjae95 <64951@hanmail.net>
2023-11-08 20:20:20 +0900

    feat: 구매 금액 입력 및 유효성 검사

---
+++ src/  LottoPurchaseAmountValidator.js
@@ -0,0 +1,47 @@
+import { Console } from "@woowacourse/mission-utils";
+import { ERROR_MESSAGE, GAME_MESSAGE } from "./constants/message";
+
+class LottoPurchaseAmountValidator {
+  #purchaseAmount;
+
+  async getPurchaseAmount() {
+    let purchaseAmount;
+    let valid;
+
+    do {
+      purchaseAmount = await Console.readLineAsync(
+        GAME_MESSAGE.promptPurchaseAmount
+      );
+      Console.print(purchaseAmount);
+      valid = this.validatePurchaseAmount(purchaseAmount);
+    } while (!valid);
+
+    this.#purchaseAmount = purchaseAmount;
+  }
+
+  validatePurchaseAmount(purchaseAmount) {
+    try {
+      if (
+        purchaseAmount === true ||
+        purchaseAmount === false ||
+        isNaN(purchaseAmount)
+      )
+        throw new Error(ERROR_MESSAGE.invalidAmountNotPositiveInt);
+      if (purchaseAmount <= 0 || purchaseAmount % 1 !== 0)
+        throw new Error(ERROR_MESSAGE.invalidAmountNotPositiveInt);
+      if (purchaseAmount % 1000 !== 0)
+        throw new Error(ERROR_MESSAGE.invalidAmountNotDevideBy1000);
+
+      return true;
+    } catch (error) {
+      Console.print(error.message);
+      return false;
+    }
+  }
+
+  get purchaseAmount() {
+    return this.#purchaseAmount;
+  }
+}
+
+export default LottoPurchaseAmountValidator;
