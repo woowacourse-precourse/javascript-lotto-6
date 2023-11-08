d2f1028980a9f9a383a89bc6af81faf898b88141
Goonco <s07019@naver.com>
2023-11-08 14:24:50 +0900

    :white_check_mark: 로또 발급 기능 테스트 생성

--- __tests__/AdditionalTest.js
+++ __tests__/AdditionalTest.js
@@ -12,22 +12,50 @@ const mockQuestions = (inputs) => {
     });
   };
 
+const mockRandoms = (numbers) => {
+    MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+    numbers.reduce((acc, number) => acc.mockReturnValueOnce(number), MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
 describe("추가 테스트", ()=>{
     const lottoController = new LottoController();
 
     test("로또 구매 테스트", async () => {
         MissionUtils.Console.print = jest.fn();
-        mockQuestions(["1000j", "8500", "8000"]);
+        mockQuestions(["1000j", "8500", "3000"]);
 
-        expect(await lottoController.buyLotto()).toBe(8);
+        await lottoController.buyLotto();
         
         const logs = [
             ERRMSG_MONEY_NOT_MONEY,
             ERRMSG_MONEY_NOT_DIVIDED_BY_LOTTO_PRICE,
+            "3개"
         ]
 
         logs.forEach((log, count)=>{
             expect(MissionUtils.Console.print).toHaveBeenNthCalledWith(count+1, expect.stringContaining(log));
+        })
+    })
+
+    test("로또 발급 테스트", async () => {
+        MissionUtils.Console.print = jest.fn();
+        mockRandoms([
+            [8, 21, 23, 41, 42, 43],
+            [3, 5, 11, 16, 32, 38],
+            [7, 11, 16, 35, 36, 44],
+          ]
+        );
+        
+        lottoController.writeLotto();
+
+        const logs = [
+            "[8, 21, 23, 41, 42, 43]",
+            "[3, 5, 11, 16, 32, 38]",
+            "[7, 11, 16, 35, 36, 44]",
+        ]
+
+        logs.forEach((log, count)=>{
+            expect(MissionUtils.Console.print).toHaveBeenCalledWith(expect.stringContaining(log));
         })
     })
 })
