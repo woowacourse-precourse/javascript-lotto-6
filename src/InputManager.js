beee62994abc3ce9e462f1010202531a89b1dc71
lucia0929 <thenum1developer@gamil.com>
2023-11-08 19:17:16 +0900

    feat : (C-1-2) 금액 입력 공백 처리 기능 구현

--- src/InputManager.js
+++ src/InputManager.js
@@ -3,7 +3,8 @@ import { Console } from "@woowacourse/mission-utils";
 class InputManager {
     async enterAmount() {
         const inputAmount = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
-        return inputAmount;    
+        const cleanedAmount = inputAmount.replace(/\s+/g, ''); 
+        return cleanedAmount;    
     }
 }
   
