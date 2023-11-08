38efff220e0fcd005efa5fc97df48e14183b8a8b
heonq <gkagjsrb@naver.com>
2023-11-08 10:22:59 +0900

    test(User): 당첨 금액을 구하는 기능에 대한 테스트 작성 - 당첨 등수 통계를 구하는 테스트와 통합: 각각 테스트하려고 하는 메서드의 결과를 담은 배열과 예상 결과를 담은 배열을 생성하고 forEach를 통해 각 요소들을 테스트한다.

--- __tests__/UserTest.js
+++ __tests__/UserTest.js
@@ -49,12 +49,14 @@ describe('User 클래스 테스트', () => {
       [1, 2, 3, 4, 5, 7],
       [1, 2, 3, 4, 5, 6],
     ];
-    const expectedResult = [1, 1, 1, 1, 1];
     mockRandoms([...randoms]);
 
     const user = new User(5000);
     user.raffleLottos(mainNumbersArray, bonusNumber);
 
-    expect(user.printStatistic()).toEqual(expectedResult);
+    const testCases = [user.printStatistic(), user.calculateReward()];
+    const expectedResult = [[1, 1, 1, 1, 1], 2_031_555_000];
+
+    testCases.forEach((testCase, index) => expect(testCase).toEqual(expectedResult[index]));
   });
 });
