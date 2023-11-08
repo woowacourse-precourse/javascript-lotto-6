a2d0ec15788527bd635ed8ad9bab5a022cba947c
skiende74 <skiende74@gmail.com>
2023-11-08 18:13:12 +0900

    feat(WinningNumberInput): InputPrompter 변경에따른수정

--- src/WinningNumberInput.js
+++ src/WinningNumberInput.js
@@ -1,4 +1,5 @@
 import InputPrompter from "./InputPrompter.js";
+import Lotto from "./Lotto.js";
 
 class WinningNumberInput {
   static async collect() {
@@ -9,19 +10,16 @@ class WinningNumberInput {
   }
 
   static createWinningNumberInputPrompter() {
-    return new InputPrompter(
-      "\n당첨 번호를 입력해 주세요",
-      (str) => str.split(",").every((strNum) => !isNaN(Number(strNum))),
-      "[ERROR] 당첨 번호는 콤마(,)로 구분된 6개의 숫자여야 합니다."
-    );
+    return new InputPrompter("\n당첨 번호를 입력해 주세요", (str) => {
+      Lotto.validate(str.split(",").map((strNum) => Number(strNum)));
+    });
   }
 
   static createBonusNumberInputPrompter() {
-    return new InputPrompter(
-      "\n보너스 번호를 입력해 주세요.",
-      (strNum) => !Number.isNaN(Number(strNum)),
-      "[ERROR] 보너스 번호는 하나의 숫자여야 합니다."
-    );
+    return new InputPrompter("\n보너스 번호를 입력해 주세요.", (strNum) => {
+      if (Number.isNaN(Number(strNum)))
+        throw new Error("[ERROR] 보너스 번호는 하나의 숫자여야 합니다.");
+    });
   }
 }
 
