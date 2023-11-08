7ebd65d9278194158d3353216b8b502fbe1bc689
ChoiYongWon <yongwon0824@naver.com>
2023-11-08 17:44:54 +0900

    로또 결과 출력 메소드

--- src/LottoService.js
+++ src/LottoService.js
@@ -45,6 +45,16 @@ class LottoService {
         return [result, winning];
     }
 
+    printResult(result){
+        Console.print("당첨 통계")
+        Console.print("---")
+        Console.print(`3개 일치 (5,000원) - ${result[3]}개`)
+        Console.print(`4개 일치 (50,000원) - ${result[4]}개`)
+        Console.print(`5개 일치 (1,500,000원) - ${result[5]}개`)
+        Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[7]}개`)
+        Console.print(`6개 일치 (2,000,000,000원) - ${result[6]}개`)
+    }
+
     setNumber(numbers) {
         this.numbers = numbers;
     }
