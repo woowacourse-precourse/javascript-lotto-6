3327b89ccc457959cfa115fe13ab5addb0a64e12
kim-young-dong <opa9904@gmail.com>
2023-11-08 17:20:23 +0900

    refactor: errorMessage, inputText 상수 처리

---
+++ src/constant/errorMessages.js
@@ -0,0 +1,24 @@
+const ERROR_MESSAGES = {
+  paymentAmount: {
+    isntNumber: '[ERROR] 구입 금액은 숫자여야 합니다.',
+    isntInteger: '[ERROR] 구입 금액은 정수여야 합니다.',
+    outOfRange: '[ERROR] 구입 금액은 1000원 이상이어야 합니다.',
+  },
+
+  winningNumbers: {
+    length: '[ERROR] 당첨 번호는 6개여야 합니다.',
+    isntNumber: '[ERROR] 당첨 번호는 숫자여야 합니다.',
+    isntInteger: '[ERROR] 당첨 번호는 정수여야 합니다.',
+    outOfRange: '[ERROR] 당첨 번호는 1~45 사이여야 합니다.',
+    duplicated: '[ERROR] 당첨 번호는 중복될 수 없습니다.',
+  },
+
+  bonusNumber: {
+    isntNumber: '[ERROR] 보너스 번호는 숫자여야 합니다.',
+    isntInteger: '[ERROR] 보너스 번호는 정수여야 합니다.',
+    outOfRange: '[ERROR] 보너스 번호는 1~45 사이여야 합니다.',
+    duplicated: '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
+  },
+};
+
+export default ERROR_MESSAGES;
