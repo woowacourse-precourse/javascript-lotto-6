ca6a68be7a80b6e4d1148c5722865d9bbe0060c6
lucia0929 <thenum1developer@gamil.com>
2023-11-08 20:23:09 +0900

    feat : (C-2-2) 로또 번호 중복 없이 생성 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -33,6 +33,12 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복이 있습니다.");
+    }
+    if (numbers.some(num => num < 1 || num > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 값이어야 합니다.");
+    }
   }
 }
 
