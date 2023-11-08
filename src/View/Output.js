d59422ef8497fd527726d15aca0acdb532837fc0
yunji <dbswl330@gmail.com>
2023-11-08 16:29:40 +0900

    feat: 당첨 결과(통계)를 출력하는 함수 구현

--- src/View/Output.js
+++ src/View/Output.js
@@ -10,6 +10,18 @@ class Output {
       Console.print(ticket);
     });
   }
+
+  printWinningResult(results) {
+    Console.print("당첨 통계");
+    Console.print("---");
+    Console.print(`3개 일치 (5,000원) - ${results.three}개`);
+    Console.print(`4개 일치 (50,000원) - ${results.four}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${results.five}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${results.fiveBonus}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${results.six}개`);
+  }
 }
 
 export default Output;
