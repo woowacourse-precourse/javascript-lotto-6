9cd080128994cbe264b29f62237be59d09c78874
greeneeya <suzythetravelerr@gmail.com>
2023-11-08 21:52:25 +0900

    test(Host): Host 클래스 로직 변경으로 인한 테스트 코드 수정

--- __tests__/HostTest.js
+++ __tests__/HostTest.js
@@ -11,32 +11,48 @@ const mockQuestions = (inputs) => {
   });
 };
 
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe('로또 주관사 테스트', () => {
+  let host;
+  beforeEach(() => {
+    host = new Host();
+  });
   it.each([[['99']], [['wootecho']]])(
     '보너스 번호가 1~45 사이의 정수가 아닐 경우, 예외 처리',
-    async (inputs) => {
+    async (input) => {
       // given
-      mockQuestions(inputs);
+      const logSpy = getLogSpy();
+      const WINNING_NUMBERS = [1, 2, 3, 4, 5, 6];
+      mockQuestions([...input, '7']);
 
       // when
-      const winningNumbers = [1, 2, 3, 4, 5, 6];
+      await host.getBonusNumber(WINNING_NUMBERS);
 
       // then
-      await expect(Host.getBonusNumber(winningNumbers)).rejects.toThrow(
-        '[ERROR] 1~45 사이의 정수만 입력 가능합니다.',
+      expect(logSpy).toHaveBeenCalledWith(
+        expect.stringContaining('[ERROR] 1~45 사이의 정수만 입력 가능합니다.'),
       );
     },
   );
   it('보너스 번호가 로또 번호와 중복일 경우, 예외 처리', async () => {
     // give
-    mockQuestions(['6']);
+    const logSpy = getLogSpy();
+    const WINNING_NUMBERS = [1, 2, 3, 4, 5, 6];
+    mockQuestions(['6', '7']);
 
     // when
-    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    await host.getBonusNumber(WINNING_NUMBERS);
 
     // then
-    await expect(Host.getBonusNumber(winningNumbers)).rejects.toThrow(
-      '[ERROR] 보너스 번호는 로또 번호와 중복이 불가합니다.',
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(
+        '[ERROR] 보너스 번호는 로또 번호와 중복이 불가합니다.',
+      ),
     );
   });
 });
--- src/Host.js
+++ src/Host.js
@@ -2,52 +2,46 @@ import { Console } from '@woowacourse/mission-utils';
 import Lotto from './Lotto.js';
 
 export default class Host {
-  #winningNumbers;
-
-  constructor() {
-    this.#winningNumbers = null;
-  }
-
-  async enrollWinningNumbers() {
+  async getWinningNumbers() {
     const user = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
     const winningNumbers = user
       .split(',')
       .map((num) => parseInt(num, 10))
       .sort((a, b) => a - b);
-    this.#winningNumbers = winningNumbers;
-    return this.#enrollLotto();
+    return winningNumbers;
   }
 
-  async getBonusNumber() {
+  async getBonusNumber(winningNumbers) {
     try {
       const user = await Console.readLineAsync(
         '\n보너스 번호를 입력해 주세요.\n',
       );
       const bonus = parseInt(user, 10);
-      this.#validate(bonus, this.#winningNumbers);
+      this.#validate(bonus, winningNumbers);
       return bonus;
     } catch (error) {
       Console.print(error.message);
-      return this.getBonusNumber(this.#winningNumbers);
+      return this.getBonusNumber(winningNumbers);
     }
   }
 
-  #validate(bonus) {
+  #validate(bonus, winningNumbers) {
     if (!Number.isInteger(bonus) || bonus > 45 || bonus < 1) {
       throw new Error('[ERROR] 1~45 사이의 정수만 입력 가능합니다.');
     }
-    if (this.#winningNumbers.includes(bonus)) {
+    if (winningNumbers.includes(bonus)) {
       throw new Error('[ERROR] 보너스 번호는 로또 번호와 중복이 불가합니다.');
     }
   }
 
-  #enrollLotto() {
+  async enrollLotto(winningNumbers) {
     try {
-      const lotto = new Lotto(this.#winningNumbers);
+      const lotto = new Lotto(winningNumbers);
       return lotto;
     } catch (error) {
       Console.print(error.message);
-      return this.enrollWinningNumbers();
+      const newWinningNumbers = await this.getWinningNumbers();
+      return this.enrollLotto(newWinningNumbers);
     }
   }
 }
