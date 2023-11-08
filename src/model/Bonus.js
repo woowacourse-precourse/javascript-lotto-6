a37318fbbc76a1e79a729f36f42c8d8bb9da7052
soominnnn <marisol03@naver.com>
2023-11-08 18:55:20 +0900

    ✨feat: 로또 번호와 당첨 번호 일치 예외 처리 구현

--- src/model/Bonus.js
+++ src/model/Bonus.js
@@ -7,12 +7,18 @@ class Bonus {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
+
   #validate(numbers){
     if(!Number(numbers)) {
       throw new Error("[ERROR] 숫자만 입력해주세요.");
     }
     if(!(numbers >=1 && numbers <= 45)) {
       throw new Error("[ERROR] 숫자는 1~45까지 입력해주세요.");
+    }
+  }
+  LottoNumberValidate(winningNumber,userBonusNumber) {
+    if(winningNumber.indexOf(String(userBonusNumber)) !== -1) {
+      throw new Error("[ERROR] 보너스번호는 당첨번호와 같을 수 없습니다.");
     }
   }
   IsinFive(resultArray) {
