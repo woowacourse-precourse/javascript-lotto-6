d0a6aa518568afae2049734e74a412525a3675c5
fecapark <fecapark@gmail.com>
2023-11-08 13:23:16 +0900

    test: 보너스 번호 입력 테스트 작성 완료

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,4 +1,5 @@
 import Input from "../src/core/Input.js";
+import { WinningNumberError } from "../src/utils/Error.js";
 import { getLogSpy, mockQuestions } from "./utils.js";
 
 describe("Input 클래스 테스트", () => {
@@ -39,6 +40,22 @@ describe("Input 클래스 테스트", () => {
     const value = await input.askWinningNumbers();
 
     expect(value).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(logSpy).toHaveBeenCalledTimes(5);
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
+  });
+
+  test("보너스 번호 입력 테스트: 예외 상황이 생겨도 프로그램이 종료되지 않는가?", async () => {
+    const setInputs = ["1,2,3,4,5,6"];
+    const inputs = ["0", "46", "a", "", "3", "7"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(setInputs);
+    await input.askWinningNumbers();
+
+    mockQuestions(inputs);
+    const value = await input.askBounsNumber();
+
+    expect(value).toBe(7);
     expect(logSpy).toHaveBeenCalledTimes(5);
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("[ERROR]"));
   });
