53054d5a5dde99d4d12620f68c76528ed4ca23ef
irenee-14 <ireneee145@gmail.com>
2023-11-08 19:56:03 +0900

    feature: 사용자의 금액 입력 받아서 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+//
+import { Console } from "@woowacourse/mission-utils";
+//
+import UserInput from "./UserInput.js";
+import LottoGenerator from "./LottoGenerator.js";
+
+
 class App {
-  async play() {}
+  async play() {
+	const inputs = new UserInput();
+	const price = await inputs.inputPrice();
+	const lottos = new LottoGenerator(price);
+
+  }
 }
 
 export default App;
