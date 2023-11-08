fb54e9c8a36bcbfc52c89de3165bb9a56838fb32
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-08 12:45:43 +0900

    ♻️refactor: 코드 수정
    * style: 스타일 컨벤션 적용
    
    * fix: STATS 중복 추가 코드 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,42 +1,62 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import Validator from "./validator/Validator";
-import { TOTAL_LOTTO_NUMBERS } from "./constants/constants";
-import Output from "./view/Output";
+import Validator from './validator/Validator';
+import { WINNING_AMOUNTS } from './constants/constants';
 
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers
+    this.#numbers = numbers;
   }
 
-  #validate(numbers) { 
-    Validator.isNumbersValid(String(numbers))
+  #validate(numbers) {
+    Validator.isNumbersValid(String(numbers));
   }
 
   returnWinningNumbers() {
-    return String(this.#numbers).split(',').map(Number)
+    return String(this.#numbers).split(',').map(Number);
   }
 
   returnOneLotto() {
-    return this.#numbers
+    return this.#numbers;
   }
 
-  calculateWinningStats(lottos, winnings, bonusNumber) {
-    const STATS = [0, 0, 0, 0, 0]
-    const WINNING_NUMBERS = winnings.map(Number);
+  calculateWinningStats(lottos, numbers) {
+    const STATS = [0, 0, 0, 0, 0];
 
     lottos.forEach((lotto) => {
-      const RESULT = WINNING_NUMBERS.filter(el => lotto.includes(el)).length;
-      
-      if (RESULT === 5) {
-        STATS[lotto.includes(bonusNumber) ? 3 : 2]++;
+      const TOTAL = numbers.filter((el) => lotto.includes(el));
+      const TOTAL_MATCHES = TOTAL.length;
+
+      if (
+        TOTAL_MATCHES === 6 ||
+        (TOTAL_MATCHES === 5 && TOTAL.includes(numbers[numbers.length - 1]))
+      ) {
+        STATS[TOTAL_MATCHES - 2] += 1;
       }
-      STATS[RESULT - 3]++;
+      if ([3, 4, 5].includes(TOTAL_MATCHES)) {
+        STATS[TOTAL_MATCHES - 3] += 1;
+      }
     });
-    
-    return STATS
+
+    return STATS;
+  }
+
+  getProfits(STATS) {
+    const PRIZE_MONEYS = Object.values(WINNING_AMOUNTS).map(Number);
+    let profits = 0;
+
+    STATS.forEach((n, i) => {
+      if (n > 0) {
+        profits += PRIZE_MONEYS[4 - i];
+      }
+    });
+
+    return profits;
+  }
+
+  calculateRate(profits, money) {
+    return profits / Number(money);
   }
 }
 
