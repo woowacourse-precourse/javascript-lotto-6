240f25f1715cd2fc0e849e4b5a567e9d93bd466a
Baek <woonn97@gmail.com>
2023-11-08 10:12:55 +0900

    feat: 당첨 결과 구하는 기능
    * countMatchNumbers() 메서드를 통해 입력받은 숫자와 랜덤 숫자 일치하는 갯수 구하기
    * countMatchNumbers()메서드를 이용해 각 순위별 당첨 개수 구하기

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App {
     const countOfLotto = this.countOfLotto(inputPrice); // 로또 개수 구하기
     Console.print(`${countOfLotto}개를 구매했습니다.`);
     const randomNumbers = this.generateRandomNumber(countOfLotto); // 랜덤 숫자 생성
-    for (let i = 0; i < randomNumbers.length; i++) {
+    for (let i = 0; i < randomNumbers.length; i += 1) {
       this.sortNumbers(randomNumbers[i]); // 랜덤 숫자 정렬
       Console.print(randomNumbers[i]);
     }
@@ -14,6 +14,10 @@ class App {
     const inputPickNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
     const inputBonusNumbers = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
     const pickNumbers = inputPickNumbers.split(",").map((number) => Number(number)); // 당첨 번호 배열로 변환
+    const loto = new Lotto(pickNumbers); // 로또 인스턴스 생성
+    for (let i = 0; i < randomNumbers.length; i += 1) {
+      loto.winningResult(randomNumbers[i], Number(inputBonusNumbers)); // 당첨 결과 구하기
+    }
   }
 
   countOfLotto(inputPrice) {
--- src/Lotto.js
+++ src/Lotto.js
@@ -6,6 +6,13 @@ class Lotto {
     // numbers 입력받는 로또 당첨번호
     this.#validate(numbers);
     this.#numbers = numbers;
+    this.prizes = {
+      1: { matchCount: 0, winningMoney: 2000000000 },
+      2: { matchCount: 0, winningMoney: 30000000 },
+      3: { matchCount: 0, winningMoney: 1500000 },
+      4: { matchCount: 0, winningMoney: 50000 },
+      5: { matchCount: 0, winningMoney: 5000 },
+    };
   }
 
   #validate(numbers) {
@@ -14,6 +21,31 @@ class Lotto {
     }
   }
   // TODO: 추가 기능 구현
+  winningResult(randomNumbers, bonusNumber) {
+    // 당첨 결과 구하기
+    const count = this.countMatchNumbers(randomNumbers);
+    if (count === 6) {
+      this.prizes[1].matchCount += 1;
+    } else if (count === 5 && randomNumbers.includes(bonusNumber)) {
+      this.prizes[2].matchCount += 1;
+    } else if (count === 5) {
+      this.prizes[3].matchCount += 1;
+    } else if (count === 4) {
+      this.prizes[4].matchCount += 1;
+    } else if (count === 3) {
+      this.prizes[5].matchCount += 1;
+    }
+  }
+  countMatchNumbers(randomNumbers) {
+    // 당첨번호와 일치하는 숫자 개수 구하기
+    let count = 0;
+    for (let i = 0; i < randomNumbers.length; i++) {
+      if (this.#numbers.includes(randomNumbers[i])) {
+        count += 1;
+      }
+    }
+    return count;
+  }
 }
 
 export default Lotto;
