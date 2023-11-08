fee745324b4664c04fa14731f1305090e9be1a08
fecapark <fecapark@gmail.com>
2023-11-08 13:51:20 +0900

    feat: 로또 번호 출력 기능 구현

--- src/core/Logger.js
+++ src/core/Logger.js
@@ -1,3 +1,30 @@
+import { Console } from "@woowacourse/mission-utils";
+
 export default class Logger {
   constructor() {}
+
+  /**
+   *
+   * @param {number[][]} lottos
+   */
+  printLottoNumbers(lottos) {
+    const lottoAmount = lottos.length;
+
+    Console.print(`${lottoAmount}개를 구매했습니다.`);
+    lottos
+      .map((lotto) => lotto.sort())
+      .map((lotto) => this.#arrayToPrettyString(lotto))
+      .forEach((lotto) => {
+        Console.print(lotto);
+      });
+  }
+
+  /**
+   *
+   * @param {any[]} arr
+   * @returns {string}
+   */
+  #arrayToPrettyString(arr) {
+    return `[${arr.join(", ")}]`;
+  }
 }
