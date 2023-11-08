9bf58b545b27538f4dbe246035500501b8c8849b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-08 15:06:04 +0900

    rename: 입력값 유효 검증 파일명 변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,9 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
 import Calculator from "../utils/calc/Calculator.js";
 import { MESSAGE_ASK } from "../static/Static.js";
-import InputValidator from "../utils/validator/InputValidator.js";
+import Validator from "../utils/validator/Validator.js";
 import OutputView from "./OutputView.js";
-import { SEPARATOR } from "../static/Static.js";
 
 const InputView = {
   async readPurchasePrice() {
@@ -12,8 +11,7 @@ const InputView = {
         const purchasePrice = await Console.readLineAsync(
           MESSAGE_ASK.purchasePrice
         );
-        InputValidator.validateEmptyString(purchasePrice);
-        InputValidator.validatePurchasePrice(purchasePrice);
+        Validator.validatePurchasePrice(purchasePrice);
         const purchaseQuantity = Calculator.calcPurchaseQuantity(purchasePrice);
         return purchaseQuantity;
       } catch (error) {
@@ -29,13 +27,7 @@ const InputView = {
         const winningNums = await Console.readLineAsync(
           MESSAGE_ASK.winningNums
         );
-        InputValidator.validateEmptyString(winningNums);
-        const winningNumArr = winningNums.split(SEPARATOR.comma);
-        InputValidator.validateLength(winningNumArr);
-        InputValidator.validateDuplication(winningNumArr);
-        winningNumArr.map((num) => {
-          InputValidator.validateNumRange(num);
-        });
+        const winningNumArr = Validator.validateWinningNums(winningNums);
         return winningNumArr;
       } catch (error) {
         OutputView.printErrorMessage(error);
@@ -43,14 +35,12 @@ const InputView = {
     }
   },
 
-  async readBonusNum(winningNums) {
+  async readBonusNum(winningNumArr) {
     OutputView.printBlankLine();
     while (true) {
       try {
         const bonusNum = await Console.readLineAsync(MESSAGE_ASK.bonusNum);
-        InputValidator.validateEmptyString(bonusNum);
-        InputValidator.validateNumRange(bonusNum);
-        InputValidator.validateInWinningNums(bonusNum, winningNums);
+        Validator.valdiateBonusNum(bonusNum, winningNumArr);
         return bonusNum;
       } catch (error) {
         OutputView.printErrorMessage(error);
