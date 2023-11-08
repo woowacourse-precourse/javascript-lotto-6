016e6ad97ebb2b10c9335e086fc3208c717af2d0
손진영 <sm099@naver.com>
2023-11-08 13:17:58 +0900

    test(winningInfoGeneration): 랭킹 분포 테이블과 당첨 금액에 대해 2개의 test case로 분리

--- src/domain/confirmWinningResult/winningInfoGeneration/WinningInfoGeneration.test.js
+++ src/domain/confirmWinningResult/winningInfoGeneration/WinningInfoGeneration.test.js
@@ -20,7 +20,6 @@ describe('당첨 정보 생성 테스트', () => {
       ],
       output: {
         prize: PRIZES['1st'] + PRIZES['2nd'] + PRIZES['3rd'] + PRIZES['4th'] + PRIZES['5th'],
-        rankDistributionTable: { '1st': 1, '2nd': 1, '3rd': 1, '4th': 1, '5th': 1 },
       },
     },
     {
@@ -33,23 +32,58 @@ describe('당첨 정보 생성 테스트', () => {
       ],
       output: {
         prize: PRIZES['1st'] + PRIZES['2nd'] + PRIZES['4th'] + PRIZES['5th'],
-        rankDistributionTable: { '1st': 1, '2nd': 1, '4th': 1, '5th': 1 },
       },
     },
     {
       input: [{ matchCount: 0, hasBonusNumber: false }],
       output: {
         prize: 0,
+      },
+    },
+  ])('입력 값에 대해 벌어 들인 총 상금은 $output.prize원 이다.', ({ input, output }) => {
+    // given - when
+    const result = winningInfoGeneration.createWinningInfo(input);
+    // then
+    expect(result.prize).toBe(output.prize);
+  });
+
+  test.each([
+    {
+      input: [
+        { matchCount: 6, hasBonusNumber: false },
+        { matchCount: 5, hasBonusNumber: true },
+        { matchCount: 5, hasBonusNumber: false },
+        { matchCount: 4, hasBonusNumber: false },
+        { matchCount: 3, hasBonusNumber: false },
+      ],
+      output: {
+        rankDistributionTable: { '1st': 1, '2nd': 1, '3rd': 1, '4th': 1, '5th': 1 },
+      },
+    },
+    {
+      input: [
+        { matchCount: 6, hasBonusNumber: false },
+        { matchCount: 5, hasBonusNumber: true },
+        { matchCount: 4, hasBonusNumber: false },
+        { matchCount: 3, hasBonusNumber: false },
+        { matchCount: 2, hasBonusNumber: false },
+      ],
+      output: {
+        rankDistributionTable: { '1st': 1, '2nd': 1, '4th': 1, '5th': 1 },
+      },
+    },
+    {
+      input: [{ matchCount: 0, hasBonusNumber: false }],
+      output: {
         rankDistributionTable: null,
       },
     },
   ])(
-    '입력 값에 대한 당첨 결과로 총 상금은 $output.prize원 이며, 당첨 정보는 $output.rankDistributionTable 이다.',
+    '입력 값에 대한 랭킹 분포 테이블은 $output.rankDistributionTable과 같다.',
     ({ input, output }) => {
       // given - when
       const result = winningInfoGeneration.createWinningInfo(input);
       // then
-      expect(result.prize).toBe(output.prize);
       expect(result.rankDistributionTable).toStrictEqual(output.rankDistributionTable);
     },
   );
