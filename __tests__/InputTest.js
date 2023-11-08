792b5a915566c91803f0e584a27c2d80e65fdb24
hamo-o <yheel369@gmail.com>
2023-11-08 22:21:47 +0900

    test: 로또 번호 입력값 예외 테스트

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -40,3 +40,49 @@ describe('로또 금액 입력값 테스트(getLottoPrice)', () => {
     await expect(Input.getLottoPrice()).rejects.toThrow(ERROR.purchaseIsNumber);
   });
 });
+
+describe('로또 당첨 번호 입력 테스트(getLottoNumber)', () => {
+  test('중복된 번호가 있는 경우 에러 반환', async () => {
+    const input = ['1,2,3,4,5,5'];
+
+    mockQuestions(input);
+
+    await expect(Input.getLottoNumber()).rejects.toThrow(
+      ERROR.lottoIsNotDuplicate
+    );
+  });
+
+  test('번호가 쉼표를 기준으로 구분되지 않는 경우 에러 반환', async () => {
+    const input = ['1.2.3.4.5.6', ''];
+
+    mockQuestions(input);
+
+    await expect(Input.getLottoNumber()).rejects.toThrow(
+      ERROR.lottoContainComma
+    );
+  });
+
+  test('번호가 숫자가 아닌 경우 에러 반환', async () => {
+    const input = ['a,1,2,3,4,5', '1,2,3,4,5,'];
+
+    mockQuestions(input);
+
+    await expect(Input.getLottoNumber()).rejects.toThrow(ERROR.lottoIsNumber);
+  });
+
+  test('번호가 1부터 45까지의 숫자가 아닌 경우 에러 반환', async () => {
+    const input = ['1,2,3,4,5,46', '0,1,2,3,4,5'];
+
+    mockQuestions(input);
+
+    await expect(Input.getLottoNumber()).rejects.toThrow(ERROR.lottoRange);
+  });
+
+  test('번호가 6개가 아닌 경우 에러 반환', async () => {
+    const input = ['1,2,3,4,5,6,7', '1,2,3,4,5'];
+
+    mockQuestions(input);
+
+    await expect(Input.getLottoNumber()).rejects.toThrow(ERROR.lottoCount);
+  });
+});
