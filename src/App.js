1cc5e50c23f78e847e35778ad1f374f5f4f29ffb
SongInJae <pan0663@naver.com>
2023-11-08 14:50:34 +0900

    feat: 로또 구입 및 구입한 로또 번호 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 
 class App {
+  totalLotto = [];
+
   async payAmount() {
     const amount = await MissionUtils.Console.readLineAsync(
-      "구입금액을 입력해 주세요."
+      "구입금액을 입력해 주세요.\n"
     );
     // TODO: 숫자인지 확인
     return amount;
   }
-  async play() {}
+  quantityLotto(amount) {
+    // TODO: 1000으로 나눠 떨어지는지 확인
+    return amount / 1000;
+  }
+  perchaseLotto(number) {
+    MissionUtils.Console.print(`\n${number}를 구입했습니다.`);
+
+    for (let i = 0; i < number; i++) {
+      const number = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+      MissionUtils.Console.print(number);
+      this.totalLotto.push(new Lotto(number));
+    }
+  }
+  async play() {
+    const amount = await this.payAmount();
+    const quantity = this.quantityLotto(amount);
+    this.perchaseLotto(quantity);
+  }
 }
 
 export default App;
