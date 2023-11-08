9e24a5d6108efa2b3c32b3a68c4d203985f0fdf3
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-08 16:15:08 +0900

    test: 로또 클래스 단위 테스트 작성
    - 입력 받은 숫자가 6개가 아닐 경우
    - 정수(양수)가 아닐 경우
    - 숫자 범위가 1~45가 아닐 경우
    - 숫자가 중복된 경우
    - 입력 형식이 숫자 또는 숫자인 문자열 외의 것이 입력 될 경우
    - 올바른 값이 입력될 경우 예외는 발생하지 않는다

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -8,7 +8,6 @@ describe("로또 클래스 테스트", () => {
 		}).toThrow("[ERROR]");
 	});
 
-	// TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
 	test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
 		const testVal = [1, 2, 3, 4, 5, 5];
 		expect(() => {
@@ -16,5 +15,47 @@ describe("로또 클래스 테스트", () => {
 		}).toThrow("[ERROR]");
 	});
 
-	// 아래에 추가 테스트 작성 가능
+	test("로또 번호가 정수(양수)가 아닐 경우 예외가 발생한다.", () => {
+		const testMinus = [1, 2, 3, 4, 5, -2];
+		const testZero = [1, 2, 3, 4, 5, 0];
+		const testDouble = [1, 2, 3, 4, 5, 3.44];
+		expect(() => {
+			new Lotto(testMinus.join(","));
+		}).toThrow("[ERROR]");
+		expect(() => {
+			new Lotto(testZero.join(","));
+		}).toThrow("[ERROR]");
+		expect(() => {
+			new Lotto(testDouble.join(","));
+		}).toThrow("[ERROR]");
+	});
+
+	test("로또 번호 숫자 범위가 1~45가 아닐 경우 예외가 발생한다.", () => {
+		const testVal1 = [1, 2, 3, 4, 5, 75];
+		const testVal2 = [1, 2, 3, 4, 5, 46];
+		expect(() => {
+			new Lotto(testVal1.join(","));
+		}).toThrow("[ERROR]");
+		expect(() => {
+			new Lotto(testVal2.join(","));
+		}).toThrow("[ERROR]");
+	});
+
+	test("로또 번호에 숫자 또는 숫자인 문자열 외의 것이 입력 될 경우 예외가 발생한다.", () => {
+		const testString = [1, 2, 3, 4, 5, "a"];
+		const testArr = [1, 2, 3, 4, 5, [1]];
+		expect(() => {
+			new Lotto(testString.join(","));
+		}).toThrow("[ERROR]");
+		expect(() => {
+			new Lotto(testArr.join(","));
+		}).toThrow("[ERROR]");
+	});
+
+	test("올바른 입력값을 입력시 객체가 생성된다.", () => {
+		const testVal = [1, 2, 3, 4, 5, 6];
+		expect(() => {
+			new Lotto(testVal.join(","));
+		}).not.toThrow("[ERROR]");
+	});
 });
