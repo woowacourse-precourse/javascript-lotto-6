425a74e13c631c6739a6b753a05b78db7d3cb224
yura <yuraa0302@gmail.com>
2023-11-08 20:48:51 +0900

    feat : 당첨 번호와 보너스 번호 설정하기

--- src/LottoResult.js
+++ src/LottoResult.js
@@ -1,11 +1,29 @@
+import Validation from "./Validation";
+
 class LottoResult {
+  /** @type {Lotto[]}*/
   #lottoes;
+
+  /** @type {number[]}*/
+  #numbers;
+  /** @type {number}*/
+  #bonus;
 
   /**
    * @param {Lotto[]} lottoes
    */
   setLottoes(lottoes) {
     this.#lottoes = lottoes;
+  }
+
+  /**
+   * @param {number[]} numbers
+   * @param {number} bonus
+   */
+  setWinningNumber(numbers, bonus) {
+    Validation.validateLottoNumbersWithBonus(numbers, bonus);
+    this.#bonus = bonus;
+    this.#numbers = numbers;
   }
 }
 export default LottoResult;
