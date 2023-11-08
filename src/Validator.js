278b375e84fe442600a249331cc4b5036781e196
김영은 <jessicak0828@gmail.com>
2023-11-08 18:36:16 +0900

    feat: Validator 생성

--- src/Validator.js
+++ src/Validator.js
@@ -1,86 +1,48 @@
-
+import { ERROR, LOTTO } from './constants';
 class Validator {
-	constructor() {}
-	static validate(input, type) {
-		const array = input.split(',');
-		const numbers = array.map(number => +number);
-		Validator.checkInputLength(numbers, type);
-		Validator.checkIfBlankExists(input);
-		Validator.checkIfCharsExists(input, type);
-		Validator.checkNumbersRange(numbers);
-		if (type === TYPE_LOTTO) {
-			Validator.checkIfDuplicateExists(numbers);
-		}
-		return numbers;
+	static validateMoney(item) {
+		// this.validateNull(item);
+		this.validateIsInteger(+item);
+		this.validateUnit(+item);
+		return +item;
 	}
-
-	static checkInputLength(numbers, type) {
-		if (numbers.length === 1 && numbers[0] === EMPTY) {
-			throw new Error(LOTTO_ERROR.message.NO_INPUT);
-		}
-		if (type === TYPE_LOTTO) {
-			if (numbers.length !== 6) {
-				throw new Error(LOTTO_ERROR.message.LENGTH);
-			}
-		}
+	static validateArr(arr) {
+		this.validateLength(arr);
+		arr.forEach((item) => this.validateIsInteger(+item));
+		arr.forEach((item) => this.validateRange(+item));
+		this.validateDuplicate(arr);
+		return arr;
 	}
-
-	static checkIfBlankExists = input => {
-		if (input.match(' ')) {
-			throw new Error(LOTTO_ERROR.message.NO_BLANK);
-		}
-		return input;
-	};
-
-	static checkIfCharsExists(input, type) {
-		const reg1 = new RegExp(REG_EXP);
-		const reg2 = new RegExp(REG_EXP_EXCEPT_COMMA);
-
-		if (type === TYPE_LOTTO) {
-			if (input.match(reg1)) {
-				throw new Error(LOTTO_ERROR.message.NO_CHARS);
-			}
-		}
-		if (input.match(reg2)) {
-			throw new Error(BONUS_ERROR.message.NO_CHARS);
-		}
-		return input;
+	static validateBonus(item) {
+		// this.validateNull(item);
+		this.validateIsInteger(+item);
+		this.validateRange(+item);
+		return +item;
 	}
-
-	static checkIfDuplicateExists(numbers) {
-		if (numbers.length !== new Set(numbers).size()) {
-			throw new Error(LOTTO_ERROR.message.NO_DUPLICATE);
-		}
+	static validateNull(input) {
+		if (!input) throw new Error(ERROR.NO_INPUT);
 	}
-
-	static checkNumbersRange(numbers, type) {
-		if ((type === TYPE_LOTTO) | TYPE_BONUS) {
-			let max = 0;
-			const sortedNumArr = numbers.sort((a, b) => a - b);
-			sortedNumArr.forEach(number => {
-				if (number >= max) {
-					max = number;
-					return max;
-				}
-				if (
-					(max > POSSIBLE_MAX_NUMBER) |
-					(sortedNumArr[0] < POSSIBLE_MIN_NUMBER)
-				) {
-					throw new Error(LOTTO_ERROR.message.OK_INPUT);
-				}
-			});
-			return sortedNumArr;
+	static validateLength(arr) {
+		if (arr.length !== LOTTO.LENGTH) throw new Error(ERROR.ONLY_LENGTH_SIX);
+	}
+	static validateIsInteger(item) {
+		if (Number.isInteger(item)) {
+			return item;
 		}
-		if (type === TYPE_MONEY) {
-			const money = +numbers[0];
-			if (money < ONE_LOTTO_PRICE) {
-				throw new Error(MONEY_ERROR.message.MORE_MONEY);
-			}
-			if (money / ONE_LOTTO_PRICE !== 0) {
-				throw new Error(MONEY_ERROR.message.NO_DIVIDE_THOUSAND);
-			}
-			return money;
+		throw new Error(ERROR.ONLY_NUMBER);
+	}
+	static validateRange(item) {
+		if (item >= LOTTO.MIN_NUMBER && item <= LOTTO.MAX_NUMBER) {
+			return item;
 		}
+		throw new Error(ERROR.NUMBER_RANGE);
+	}
+	static validateUnit(item) {
+		if (item % LOTTO.PRICE !== 0) throw new Error(ERROR.MONEY_UNIT);
+	}
+	static validateDuplicate(arr) {
+		const set = new Set(arr);
+		if (set.size !== arr.length) throw new Error(ERROR.NO_NUMBER_DUPLICATE);
 	}
 }
 
