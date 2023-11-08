2cbb2c8c7abd4cde99140d107d9d0fcd0f47cff0
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-08 18:56:26 +0900

    feature: 당첨 개수 계산

--- src/App.js
+++ src/App.js
@@ -59,21 +59,57 @@ class App {
       const {match, bonus} = this.calculateScore(lottos[i], answers);
       matches.push(match);
       bonuses.push(bonus);
+  async getResult(lottos, answers) {
+    const results = [];
+    for (let i = 0; i < lottos.length; i++) {
+      const { match, bonus } = this.calculateScore(lottos[i], answers);
+      results.push({ match, bonus });
     }
     return this.calculateMatches(matches, bonuses);
+    return results;
   }
 
   async calculateScore(lotto, answers){
+  async calculateScore(lotto, answers) {
     let match = 0;
     let bonus = false;
+
     answers.forEach((answer) => {
       if (lotto.includes(answer)){
         match += 1;}
     })
     if (lotto.includes(answers[-1])){
+      if (lotto.includes(answer)) {
+        match += 1;
+      }
+    });
+    if (lotto.includes(answers[-1])) {
       bonus = true;
     }
     return {match, bonus};
+    return { match, bonus };
+  }
+
+  async calculateMatches(results) {
+    const matchCounts = {
+      3: 0,
+      4: 0,
+      5: 0,
+      5_1: 0,
+      6: 0,
+    };
+
+    results.forEach((result) => {
+      const { match, bonus } = result;
+      if (match === 5 && bonus) {
+        matchCounts[5_1] += 1;
+      } else {
+        matchCounts[match] += 1;
+      }
+    });
+    return this.printResult(matchCounts);
+  }
+
   }
 }
 
