58d432a969d1c1ca3e370612cd8d1dc2603c9e9a
Goonco <s07019@naver.com>
2023-11-08 14:41:46 +0900

    :white_check_mark: 당첨 로또 생성 기능 테스트 생성

--- __tests__/AdditionalTest.js
+++ __tests__/AdditionalTest.js
@@ -1,61 +1,102 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import LottoController from "../src/controller/LottoController.js";
-import { ERRMSG_MONEY_NOT_DIVIDED_BY_LOTTO_PRICE, ERRMSG_MONEY_NOT_MONEY } from "../src/constants/ErrorMessage.js";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import LottoController from '../src/controller/LottoController.js';
+import {
+  ERRMSG_MONEY_NOT_DIVIDED_BY_LOTTO_PRICE,
+  ERRMSG_MONEY_NOT_MONEY,
+  ERRMSG_LOTTO_NOT_MADE_WITH_UNIQUE_NUMBER,
+  ERRMSG_LOTTO_NOT_VALID_LOTTO_LENGTH,
+} from '../src/constants/ErrorMessage.js';
 
 const mockQuestions = (inputs) => {
-    MissionUtils.Console.readLineAsync = jest.fn();
-  
-    MissionUtils.Console.readLineAsync.mockImplementation(() => {
-      const input = inputs.shift();
-  
-      return Promise.resolve(input);
-    });
-  };
+  MissionUtils.Console.readLineAsync = jest.fn();
 
-const mockRandoms = (numbers) => {
-    MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
-    numbers.reduce((acc, number) => acc.mockReturnValueOnce(number), MissionUtils.Random.pickUniqueNumbersInRange);
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
 };
 
-describe("추가 테스트", ()=>{
-    const lottoController = new LottoController();
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
+};
 
-    test("로또 구매 테스트", async () => {
-        MissionUtils.Console.print = jest.fn();
-        mockQuestions(["1000j", "8500", "3000"]);
+describe('추가 테스트', () => {
+  const lottoController = new LottoController();
 
-        await lottoController.buyLotto();
-        
-        const logs = [
-            ERRMSG_MONEY_NOT_MONEY,
-            ERRMSG_MONEY_NOT_DIVIDED_BY_LOTTO_PRICE,
-            "3개"
-        ]
+  test('로또 구매 테스트', async () => {
+    MissionUtils.Console.print = jest.fn();
+    mockQuestions(['1000j', '8500', '3000']);
 
-        logs.forEach((log, count)=>{
-            expect(MissionUtils.Console.print).toHaveBeenNthCalledWith(count+1, expect.stringContaining(log));
-        })
-    })
+    await lottoController.buyLotto();
 
-    test("로또 발급 테스트", async () => {
-        MissionUtils.Console.print = jest.fn();
-        mockRandoms([
-            [8, 21, 23, 41, 42, 43],
-            [3, 5, 11, 16, 32, 38],
-            [7, 11, 16, 35, 36, 44],
-          ]
-        );
-        
-        lottoController.writeLotto();
+    const logs = [
+      ERRMSG_MONEY_NOT_MONEY,
+      ERRMSG_MONEY_NOT_DIVIDED_BY_LOTTO_PRICE,
+      '3개',
+    ];
 
-        const logs = [
-            "[8, 21, 23, 41, 42, 43]",
-            "[3, 5, 11, 16, 32, 38]",
-            "[7, 11, 16, 35, 36, 44]",
-        ]
+    logs.forEach((log, count) => {
+      expect(MissionUtils.Console.print).toHaveBeenNthCalledWith(
+        count + 1,
+        expect.stringContaining(log),
+      );
+    });
+  });
 
-        logs.forEach((log, count)=>{
-            expect(MissionUtils.Console.print).toHaveBeenCalledWith(expect.stringContaining(log));
-        })
-    })
-})
+  test('로또 발급 테스트', async () => {
+    MissionUtils.Console.print = jest.fn();
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+    ]);
+
+    lottoController.writeLotto();
+
+    const logs = [
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+    ];
+
+    logs.forEach((log, count) => {
+      expect(MissionUtils.Console.print).toHaveBeenCalledWith(
+        expect.stringContaining(log),
+      );
+    });
+  });
+
+  test('당첨 로또 생성', async () => {
+    MissionUtils.Console.print = jest.fn();
+    mockQuestions([
+      '1,2,3,4,5',
+      '6',
+      '1,2,2,3,4,5',
+      '6',
+      '1,2,3,4,5,6',
+      '6',
+      '1,2,3,4,5,6',
+      '7',
+    ]);
+
+    await lottoController.makeWinningLotto();
+
+    const logs = [
+      ERRMSG_LOTTO_NOT_VALID_LOTTO_LENGTH,
+      ERRMSG_LOTTO_NOT_MADE_WITH_UNIQUE_NUMBER,
+      ERRMSG_LOTTO_NOT_MADE_WITH_UNIQUE_NUMBER,
+    ];
+
+    logs.forEach((log, count) => {
+      expect(MissionUtils.Console.print).toHaveBeenNthCalledWith(
+        count + 1,
+        expect.stringContaining(log),
+      );
+    });
+  });
+});
