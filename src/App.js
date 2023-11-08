b22e438289c37149de8be4b022aa976c1ff93318
jonghyunRyu <willowise@naver.com>
2023-11-08 18:29:00 +0900

    chore(App): play함수에 주석 추가

--- src/App.js
+++ src/App.js
@@ -13,16 +13,17 @@ class App {
   }
 
   async play() {
+    // 로또 구매 및 구매한 로또 발행
     await this.Buy.tryBuyLotto();
     this.Buy.buyLottoEntire();
     this.View.printAllLotto(this.Buy.lottoAll);
-
+    // 당첨 번호와 보너스 번호 입력 받음
     await this.Winning.tryNumberInput();
     await this.Winning.tryBonusNumInput();
-
+    // 당첨 결과 및 총 수익률 계산하는 함수
     this.Winning.CompareAll(this.Buy.lottoAll);
     const rate = this.Rate.rateOfReturn(this.Winning.statistics, this.Buy.lottoEach);
-
+    // 당첨 결과 및 총 수익률 출력
     this.View.statisticsPrint(this.Winning.statistics);
     this.View.printRateOfReturn(rate);
   }
