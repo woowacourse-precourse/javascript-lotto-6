06504a0c3c60b0b0ad247dc65931696a77158256
JoSooha <dlrhdcjs@naver.com>
2023-11-08 17:14:32 +0900

    feat: 당첨 번호 범위 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    for(let i=0; i<6; i++) {
+      const num = numbers[i];
+      this.#validateNumber(num);
+    }
+  }
+
+  #validateNumber(number) {
+    if(isNaN(number) || number<1 || number>45) {
+      throw new Error("[ERROR] 각 번호는 1~45의 숫자여야 합니다.")
+    }
   }
 
   // TODO: 추가 기능 구현
