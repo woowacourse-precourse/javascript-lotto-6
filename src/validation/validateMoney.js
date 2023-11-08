babfd96df8413c55361b701181179348e1cbdaee
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 14:24:27 +0900

    fix: 금액 유효성 검사에서 타입 에러를 잡아내지 못하는 문제 해결

--- src/validation/validateMoney.js
+++ src/validation/validateMoney.js
@@ -1,14 +1,14 @@
 import validateMoneyError from '../constants/error';
 
+const regexOnlyNumbers = /^\d+$/;
+
 const validateMoney = (money) => {
+  if (!regexOnlyNumbers.test(money)) throw new Error('[ERROR]');
+
   const moneyToNumber = parseInt(money);
 
   if (!moneyToNumber) throw new Error('[ERROR]');
-
-  if (Number.isNaN(moneyToNumber)) throw new Error('[ERROR]');
-
   if (moneyToNumber < 1000) throw new Error('[ERROR]');
-
   if (moneyToNumber % 1000 !== 0) throw new Error('[ERROR]');
 };
 
