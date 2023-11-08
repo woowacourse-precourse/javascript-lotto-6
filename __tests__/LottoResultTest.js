48ad3698e625a5a315621fa6ff4dfa2d8b4c1890
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-08 14:47:24 +0900

    test(결과:1개의 로또):당첨 여부 확인 기능 테스트 수정
    * test 설명 수정
    * 당첨 되는/되지 않는 당첨 번호 를 각각 당첨 여부 확인 하도록 수정

--- __tests__/LottoResultTest.js
+++ __tests__/LottoResultTest.js
@@ -2,46 +2,22 @@ import { BONUS_NUMBER_TYPE } from "../src/constants.js";
 import { LottoResult } from "../src/lottoGame/result/Lotto.js";
 
 const numbers = [1,2,3,4,5,6];
-const winningNumbers = [1,2,3,4,44,45];
+const winWinningNumbers = [1,2,3,4,44,45];
+const loseWinningNumbers = [1,2,42,43,44,45];
 const bonusNumberType = BONUS_NUMBER_TYPE.useless;
 
-const lottoResult = new LottoResult(numbers, winningNumbers, bonusNumberType);
+const winWinningLottoResult = new LottoResult(numbers, winWinningNumbers, bonusNumberType);
+const loseWinningLottoResult = new LottoResult(numbers, loseWinningNumbers, bonusNumberType);
 
-const winningConditions = [
-    {
-        condition: {winningNumbersCount : 4, bonusNumberType : BONUS_NUMBER_TYPE.useless},
-    },    
-]
-
-const losingConditions = [
-    {
-        condition: {winningNumbersCount : 1, bonusNumberType : BONUS_NUMBER_TYPE.useless},
-    },
-    {
-        condition: {winningNumbersCount : 2, bonusNumberType : BONUS_NUMBER_TYPE.useless},
-    },
-    {
-        condition: {winningNumbersCount : 5, bonusNumberType : BONUS_NUMBER_TYPE.withOutFiveWinningNumbers},
-    },
-    {
-        condition: {winningNumbersCount : 5, bonusNumberType : BONUS_NUMBER_TYPE.withFiveWinningNumbers},
-    }
-]
 
 describe("1개의 로또 결과", ()=>{
     test("발행한 로또 번호 와 사용자가 입력한 당첨 번호/보너스 번호 비교 하여 일치 개수 계산", ()=>{
-        expect(lottoResult.getCountOfWinningNumbers()).toBe(4);
-        expect(lottoResult.getBonusNumberType()).toBe(BONUS_NUMBER_TYPE.useless);
+        expect(winWinningLottoResult.getCountOfWinningNumbers()).toBe(4);
+        expect(winWinningLottoResult.getBonusNumberType()).toBe(BONUS_NUMBER_TYPE.useless);
     })
 
-    test("당첨 여부 확인", ()=>{
-        winningConditions.forEach(({condition}) => {
-            expect(lottoResult.isWin(condition)).toBe(true)
-        })
-
-        losingConditions.forEach(({condition}) => {
-            expect(lottoResult.isWin(condition)).toBe(false)
-        })
-        
+    test("당첨 조건 과 비교해 당첨 여부 를 확인 한다.", ()=>{
+        expect(winWinningLottoResult.isWin()).toBe(true)
+        expect(loseWinningLottoResult.isWin()).toBe(false)
     })
 })
