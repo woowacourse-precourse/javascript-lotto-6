b5611c73adb1059c3620d911c6e808ca606d4745
soosoo22 <lllllh00@naver.com>
2023-11-08 16:34:20 +0900

    feat: [로또 번호 유효성 검사 및 예외 처리] 기능 추가
    Lotto 클래스에 유효성 검사 메서드와 해당 검사에서 발생할 수 있는 예외 처리를 추가함
    유효하지 않은 길이, 숫자 범위를 벗어남, 중복된 숫자 등을 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,31 @@
+import { ERROR } from './constant/constant.js';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.outOfRange(numbers);
+    this.#duplicate(numbers);
     this.#numbers = numbers;
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw ERROR.INVALID_LENGTH;
     }
   }
+  outOfRange(numbers) {
+    if (numbers.some((num) => !(num >= 1 && num <= 45))) throw ERROR.OUT_OF_RANGE;
+  }
+  #duplicate(numbers) {
+    const set = new Set(numbers);
+    if (set.size !== numbers.length) throw ERROR.DUPLICATE_NUMBER;
+  }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
