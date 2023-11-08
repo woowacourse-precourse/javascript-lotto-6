9c062f3001fb48f22b4a3e4875a46ad744a17ee3
yeokyoung song <spolice0324@gmail.com>
2023-11-08 17:13:11 +0900

    refactor : 가독성 및 15줄 이하 준수를 위한 코드 분리
    - 각각의 계산결과 반환들을 모두 분리된 메소드로 정의

--- src/Domain/LottoCalculator.js
+++ src/Domain/LottoCalculator.js
@@ -3,22 +3,36 @@ import { LOTTO_INFO } from '../Utils/constants';
 
 class LottoCalculator extends Lotto {
   #bonus;
-  
+
   calculateResult(lottos) {
+    const prizeResultMap = this.calculatePrizeResultMap(lottos);
+    const profits = this.calculateProfits(prizeResultMap);
+    const lottoSize = this.calculateLottoSize(prizeResultMap);
+    const earningRate = this.calculateEarningRate(profits, lottoSize);
+    return { prizeResultMap, earningRate };
+  }
+
+  calculatePrizeResultMap(lottos) {
     const prizeResultMap = new Map();
-    lottos.forEach((lotto) => {
+    lottos.forEach(lotto => {
       const prize = lotto.calculatePrize(this);
       prizeResultMap.set(prize, (prizeResultMap.get(prize) || 0) + 1);
     });
+    return prizeResultMap;
+  }
 
-    const profits = Array.from(prizeResultMap.entries())
+  calculateProfits(prizeResultMap) {
+    return Array.from(prizeResultMap.entries())
       .map(([prize, count]) => LOTTO_INFO.PRIZE[prize] * count)
       .reduce((a, b) => a + b);
-    const lottoSize = Array.from(prizeResultMap.values()).reduce(
-      (a, b) => a + b,
-    );
-    const earningRate = (profits / (lottoSize * LOTTO_INFO.PRICE)) * 100;
-    return { prizeResultMap, earningRate };
+  }
+
+  calculateLottoSize(prizeResultMap) {
+    return Array.from(prizeResultMap.values()).reduce((a, b) => a + b);
+  }
+
+  calculateEarningRate(profits, lottoSize) {
+    return (profits / (lottoSize * LOTTO_INFO.PRICE)) * 100;
   }
 
   set bonus(bonus) {
@@ -28,7 +42,6 @@ class LottoCalculator extends Lotto {
   get bonus() {
     return this.#bonus;
   }
-
 }
 
 export default LottoCalculator;
