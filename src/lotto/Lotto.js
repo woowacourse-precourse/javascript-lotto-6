01e3bc8c8dd31136b2960e8a931be1ec8f81abf9
ImxYJL <allensain14@gmail.com>
2023-11-08 15:15:50 +0900

    feat: 발행된 로또들의 당첨 결과 확인 기능 구현

--- src/lotto/Lotto.js
+++ src/lotto/Lotto.js
@@ -1,6 +1,7 @@
 import { print } from '../utility/console.js';
 import {
   LOTTO_CONSTANT,
+  LOTTO_RANK,
   ERROR_MESSAGE,
   FORMATTER,
 } from '../constant/constant.js';
@@ -23,8 +24,46 @@ class Lotto {
     }
   }
 
+  #countMatchedNumber(winningNumbers) {
+    let matchedNumberCount = 0;
+
+    this.#numbers.forEach((number) => {
+      if (winningNumbers.includes(number)) matchedNumberCount += 1;
+    });
+
+    return matchedNumberCount;
+  }
+
+  #hasBonusNumber(bonusNumber) {
+    if (this.#numbers.includes(bonusNumber)) return true;
+    return false;
+  }
+
   printNumbers() {
     print(FORMATTER.lottoPrintFormatter(this.#numbers));
+  }
+
+  getRank(winningNumbers, bonusNumber) {
+    const matchedNumberCount = this.#countMatchedNumber(winningNumbers);
+
+    if (matchedNumberCount === LOTTO_RANK.first.matchedNumber) {
+      return LOTTO_RANK.first.rank;
+    }
+    if (
+      this.#hasBonusNumber(bonusNumber) &&
+      matchedNumberCount === LOTTO_RANK.second.matchedNumber
+    ) {
+      return LOTTO_RANK.second.rank;
+    }
+    if (matchedNumberCount === LOTTO_RANK.third.matchedNumber) {
+      return LOTTO_RANK.third.rank;
+    }
+    if (matchedNumberCount === LOTTO_RANK.fourth.matchedNumber) {
+      return LOTTO_RANK.fourth.rank;
+    }
+    if (matchedNumberCount === LOTTO_RANK.fifth.matchedNumber) {
+      return LOTTO_RANK.fifth.rank;
+    }
   }
 }
 
--- src/lotto/LottoCenter.js
+++ src/lotto/LottoCenter.js
@@ -19,6 +19,36 @@ class LottoCenter {
       lotto.printNumbers();
     });
   }
+
+  inspectLottoWinningStatus(winningNumbers, bonusNumber) {
+    this.#publishedLottoList.forEach((lotto) => {
+      this.#setLottoResultList(lotto.getRank(winningNumbers, bonusNumber));
+    });
+  }
+
+  #setLottoResultList(rank) {
+    switch (rank) {
+      case LOTTO_RANK.first.rank:
+        this.#lottoResultsList[LOTTO_RANK.first.rank] += 1;
+        break;
+      case LOTTO_RANK.second.rank:
+        this.#lottoResultsList[LOTTO_RANK.second.rank] += 1;
+        break;
+      case LOTTO_RANK.third.rank:
+        this.#lottoResultsList[LOTTO_RANK.third.rank] += 1;
+        break;
+      case LOTTO_RANK.fourth.rank:
+        this.#lottoResultsList[LOTTO_RANK.fourth.rank] += 1;
+        break;
+      case LOTTO_RANK.fifth.rank:
+        this.#lottoResultsList[LOTTO_RANK.fifth.rank] += 1;
+        break;
+    }
+  }
+
+  getLottoResultsList() {
+    return { ...this.#lottoResultsList };
+  }
 }
 
 export default LottoCenter;
