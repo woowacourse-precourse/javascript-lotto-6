e641fc71b7070fefa377c0620bd57a9596a65160
greeneeya <suzythetravelerr@gmail.com>
2023-11-08 13:41:33 +0900

    feat: 금액이 1000원 단위가 아닐 경우, 예외처리한다.

--- src/Vendor.js
+++ src/Vendor.js
@@ -4,8 +4,11 @@ export default class Vendor {
   static async buyTickets() {
     const user = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     const paid = parseInt(user, 10);
-    if (!Number.isInteger(paid) || paid < 0) {
+    if (!Number.isInteger(paid)) {
       throw new Error('[ERROR] 금액은 숫자만 입력가능합니다.');
+    }
+    if (paid % 1000 !== 0 || paid < 1000) {
+      throw new Error('[ERROR] 로또는 1000원 단위로 구매 가능합니다.');
     }
     return paid;
   }
