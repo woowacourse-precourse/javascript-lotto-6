5449594a690d754e9fc5830a754b50544d4fb185
donggyun <donggyunkim12@gmail.com>
2023-11-08 16:04:22 +0900

    test(ValidateTest): 입력 금액 유효성 검사 -  숫자 입력 검사 테스트 구현

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,10 +1,17 @@
 import { ERROR_MESSAGE } from '../src/constant/message.js';
-import { validateDivisible, validateEmpty } from '../src/utils/validateFn.js';
+import {
+  validateDivisible,
+  validateEmpty,
+  validateFindEqual,
+  validateLength,
+  validateNumber,
+  validateRange,
+  validateUnique,
+} from '../src/utils/validateFn.js';
 
 describe('입력 금액 유효성 검사', () => {
   test('빈 문자열이 아닌 경우', () => {
     const validInput = '12';
-
     expect(validateEmpty(validInput)).toBe(true);
   });
   test('빈 문자열을 입력한 경우', () => {
@@ -16,16 +23,29 @@ describe('입력 금액 유효성 검사', () => {
     }
   });
 
+  test('입력값이 숫자인 경우', () => {
+    const invalidInput = ['1', '5', '77', '90'];
+    invalidInput.forEach((number) => {
+      expect(validateNumber(number)).toBe(true);
+    });
+  });
+  test('입력값이 숫자가 아닌경우', () => {
+    const invalidInput = ['a', 'b'];
+    try {
+      validateNumber(invalidInput);
+    } catch (error) {
+      expect(error).toEqual(ERROR_MESSAGE.INVALID_INPUT_NUMBER);
+    }
+  });
+
   test('1000으로 나누어 떨어지는 경우', () => {
     const validInput = [3000, 301000, 50000];
-
     validInput.forEach((money) => {
       expect(validateDivisible(money)).toBe(true);
     });
   });
   test('1000으로 나누어 떨어지지 않는 경우', () => {
     const invalidInput = [13, 3001, 8888];
-
     try {
       invalidInput.forEach((money) => {
         validateDivisible(money);
