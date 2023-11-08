7b910250b3fdbbf188b798f5b12f56d9670ccbbc
One-HyeWon <5876675@naver.com>
2023-11-08 23:32:41 +0900

    feat: Lotto.js 에러 테스트 및 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,27 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const uniqueNumbers = [...new Set(numbers)];
+    if (uniqueNumbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+    if (numbers.some((number) => number < 1 || number > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  getMatchingNumbers(winningNumbers) {
+    return this.#numbers.filter((number) => winningNumbers.includes(number))
+      .length;
+  }
+
+  isBonusMatch(bonusNumber) {
+    return this.#numbers.includes(bonusNumber);
+  }
 }
 
 export default Lotto;
