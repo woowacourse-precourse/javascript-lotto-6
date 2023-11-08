7b2dc3e67fae6e9e145615958fc61379056b0f81
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-08 23:03:59 +0900

    Refactor: 당첨번호 입력값에 대한 오류 상수화

--- src/constant/lotto.js
+++ src/constant/lotto.js
@@ -14,5 +14,10 @@ export const BONUS_ERROR_MESSAGE = {
   DUPLICATE_WINNING_NUMBER_ERROR:
     "[ERROR] 입력한 당첨 번호 외 숫자를 입력해 주세요.",
 };
-
+export const INPUT_ERROR_MESSAGE = {
+  INVALID_LOTTO_LENGTH_ERROR: "[ERROR] 로또 번호는 6개여야 합니다.",
+  INVALID_INPUT_NUMBER_ERROR:
+    "[ERROR] 입력한 숫자는 1부터 45 사이의 자연수이어야 합니다.",
+  DUPLICATE_INPUT_NUMBER_ERROR: "[ERROR] 중복된 숫자는 입력하면 안 됩니다.",
+};
 export const PRICE_ERROR_MESSAGE = "[ERROR] 구입금액을 올바르게 입력해 주세요.";
