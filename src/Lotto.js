bb8697428274fea5b7522132ac53434b471edfc2
sejineeee <sejinee0918@gmail.com>
2023-11-08 11:30:36 +0900

    refactor: 당첨번호만을 받는 클래스에서 로또 종이를 받는 클래스로 변경

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,12 +1,8 @@
-const MAX_NUMBER = 45;
-const MIN_NUMBER = 1;
-
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.validateNumbers(numbers);
     this.#numbers = numbers;
   }
 
@@ -16,76 +12,42 @@ class Lotto {
     }
   }
 
-  validateNumbers(numbers) {
-    numbers.forEach((number) => {
-      if (Number.isNaN(Number(number))) {
-        throw new Error('[ERROR] 숫자를 입력해주세요.');
-      }
-
-      if (Number(number) < MIN_NUMBER || Number(number) > MAX_NUMBER) {
-        throw new Error('[ERROR] 1부터 45 사이의 숫자를 입력해주세요.');
-      }
-
-      if (numbers.indexOf(number) !== numbers.lastIndexOf(number)) {
-        throw new Error('[ERROR] 중복된 숫자가 있는지 확인해주세요.');
-      }
-    });
+  getNumbers() {
+    return this.#numbers;
   }
 
-  validateBonusNumber(bonusNumber) {
-    if (bonusNumber === 0 || Number.isNaN(bonusNumber)) {
-      throw new Error('[ERROR] 입력된 값을 확인해주세요.');
-    }
-
-    if (
-      !Number.isInteger(bonusNumber) ||
-      bonusNumber < MIN_NUMBER ||
-      bonusNumber > MAX_NUMBER
-    ) {
-      throw new Error('[ERROR] 1부터 45 사이의 정수를 입력해주세요.');
-    }
-  }
-
-  getWinningList(myLottoList, bonusNumber) {
-    const list = {
-      first: [],
-      second: [],
-      third: [],
-      fourth: [],
-      fifth: [],
-    };
-
-    myLottoList.forEach((lotto) => {
-      let count = 0;
-
-      this.#numbers.forEach((number) => {
-        if (lotto.includes(Number(number))) {
-          count += 1;
-        }
-      });
-
-      if (count === 3) {
-        list.fifth.push(lotto);
-      }
-
-      if (count === 4) {
-        list.fourth.push(lotto);
-      }
-
-      if (count === 5) {
-        if (lotto.includes(bonusNumber)) {
-          list.second.push(lotto);
-        } else {
-          list.third.push(lotto);
-        }
-      }
-
-      if (count === 6) {
-        list.first.push(lotto);
+  getWinningCount(winningLotto) {
+    let count = 0;
+    winningLotto.forEach((number) => {
+      if (this.#numbers.includes(Number(number))) {
+        count += 1;
       }
     });
 
-    return list;
+    return count;
+  }
+
+  checkWinningResult(count, bonusLotto, winningResult) {
+    if (count === 3) {
+      winningResult.fifth.push(this.#numbers);
+    }
+
+    if (count === 4) {
+      winningResult.fourth.push(this.#numbers);
+    }
+
+    if (count === 5) {
+      if (this.#numbers.includes(bonusLotto)) {
+        winningResult.second.push(this.#numbers);
+      } else {
+        winningResult.third.push(this.#numbers);
+      }
+    }
+
+    if (count === 6) {
+      winningResult.first.push(this.#numbers);
+    }
   }
 }
+
 export default Lotto;
