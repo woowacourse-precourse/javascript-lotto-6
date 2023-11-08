dc3f6cbd3d34ed56439909c19cabaf74fbedaf34
bini59 <myuni0234@gmail.com>
2023-11-08 19:59:49 +0900

    fix(generateLotto): Lotto 객체 생성
    - 기존 array를 저장하던 것을 Lotto객체로 변경

--- src/Model.js
+++ src/Model.js
@@ -1,5 +1,7 @@
 import { Random } from '@woowacourse/mission-utils';
 
+import Lotto from './Lotto.js';
+
 class Model {
   #lottoNumbers = [];
 
@@ -13,8 +15,13 @@ class Model {
     const count = money / 1000;
 
     for (i = 0; i < count; i += 1) {
-      this.#lottoNumbers.push(Random.pickUniqueNumbersInRange(1, 45, 6));
+      const temp = Random.pickUniqueNumbersInRange(1, 45, 6);
+      temp.sort((a, b) => a - b);
+      const lotto = new Lotto(temp);
+      this.#lottoNumbers.push(lotto);
     }
+
+    return this.#lottoNumbers;
   }
 }
 
