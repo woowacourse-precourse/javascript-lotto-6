cc3ca454a4767b6cd6c97698a50b38472eb478ca
김영은 <jessicak0828@gmail.com>
2023-11-08 18:35:01 +0900

    feat: Lotto 클래스 코드 수정
    Validator import

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,37 +1,15 @@
-import { Random } from '@woowacourse/mission-utils';
 import Validator from './Validator';
-import {
-	ONE_LOTTO_PRICE,
-	POSSIBLE_MIN_NUMBER,
-	POSSIBLE_MAX_NUMBER,
-	LOTTO_NUMBERS_LENGTH
-} from './constants';
 class Lotto {
 	#numbers;
-	constructor(numbers) {
-		this.#validate(numbers);
-		this.#numbers = numbers; //number[]
-		this.money;
-		this.bonus; //number
-		this.randomArraySet;
+	constructor(arr) {
+		const numbers = Validator.validateArr(arr);
+		this.#numbers = this.#sortNumbers(numbers);
 	}
-	#validate(numbers) {
-		if (numbers.length !== 6) {
-			throw new CustomError(ERROR.name, '로또 번호는 6개여야 합니다.');
-		}
-		Validator.validate();
+	#sortNumbers(numbers) {
+		numbers.sort((a, b) => a - b);
 	}
-	static calculateLottosAmount(money) {
-		const amount = Math.floor(money / ONE_LOTTO_PRICE);
-		return amount;
-	}
-	static async makeNumbersArray() {
-		const array = await Random.pickUniqueNumbersInRange(
-			POSSIBLE_MIN_NUMBER,
-			POSSIBLE_MAX_NUMBER,
-			LOTTO_NUMBERS_LENGTH
-		); // return []
-		return array;
+	getNumbers() {
+		return this.#numbers;
 	}
 }
 
