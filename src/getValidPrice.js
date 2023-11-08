f992f903c6bc0ce6d0001c467219287d948c9016
minki3 <86992006+minki3@users.noreply.github.com>
2023-11-08 13:37:26 +0900

    refactor:대문자 ->소문자 변경

--- src/getValidPrice.js
+++ src/getValidPrice.js
@@ -1,13 +1,13 @@
-import lottoPriceInput from './LottoPriceInput.js';
-import priceCheck from './priceCheck.js';
-import { MissionUtils } from '@woowacourse/mission-utils';
+import lottoPriceInput from "./lottoPriceInput.js";
+import priceCheck from "./priceCheck.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 export default async function getValidPrice() {
   let validPrice = null;
 
   while (validPrice === null) {
     try {
-      const price = await lottoPriceInput('구입금액을 입력해 주세요.');
+      const price = await lottoPriceInput("구입금액을 입력해 주세요.");
       validPrice = priceCheck(price);
     } catch (error) {
       MissionUtils.Console.print(`${error}`);
