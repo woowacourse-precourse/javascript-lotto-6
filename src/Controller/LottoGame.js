687d57ba0c3c891b4d1554570b91e9394775893d
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2023-11-08 11:34:11 +0900

    feat: 로또 번호 생성 및 출력 기능

--- src/Controller/LottoGame.js
+++ src/Controller/LottoGame.js
@@ -1,21 +1,41 @@
 import InputView from '../View/InputView'
 import OutputView from '../View/OutputView'
+import { MissionUtils } from '@woowacourse/mission-utils'
 
 class LottoGame{
     #count
 
     constructor(){
-        this.count = 0
+        this.#count = 0
     }
     
     async startGame(){
         await this.getCount()
+
+        const lottoList = await this.getLottoNumList()
+        OutputView.printLottoList(lottoList)
     }
 
     async getCount(){
         const money = await InputView.getMoney()
         this.#count = Math.floor(money / 1000) 
         OutputView.printCount(this.#count) 
+    }
+
+    async getLottoNumList(){
+        const lottoNumList = []
+        for(let i=0; i<this.#count; i++){
+            const lottoNum = await this.getLottoNum()
+            lottoNumList.push(lottoNum)
+        }
+
+        return lottoNumList
+    }
+
+    async getLottoNum(){
+        const lottoNum = await MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6)
+        const sortedLottoNum = lottoNum.sort((a, b) => a - b)
+        return sortedLottoNum
     }
 }
 
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -2,8 +2,15 @@ import { MissionUtils } from '@woowacourse/mission-utils'
 
 const OutputView = {
     printCount(count){
-        MissionUtils.Console.print(`\n${count}개를 구매했습니다.\n`)
+        MissionUtils.Console.print(`\n${count}개를 구매했습니다.`)
     },
+
+    printLottoList(lottoList){
+        const length = lottoList.length
+        lottoList.forEach(lotto => {
+            MissionUtils.Console.print(lotto)
+        })
+    }
 }
 
 module.exports = OutputView
