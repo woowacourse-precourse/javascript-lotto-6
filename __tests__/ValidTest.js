ca1ac1a0769bd58b535c1318a8f2df7803cd4471
Klomachenko <leekyumin0901@naver.com>
2023-11-08 21:01:36 +0900

    feat(validTest): isMultipleOf1000 테스트 구현
    Description: 1000의 배수인지 확인하는 테스트 코드 추가

--- __tests__/ValidTest.js
+++ __tests__/ValidTest.js
@@ -10,4 +10,14 @@ describe("Validate 클래스 테스트", () => {
       expect(Validate.isNumeric("123abc")).toBe(false);
     });
   });
+
+  describe("isMultipleOf1000 메서드 테스트", () => {
+    test("1000의 배수인 경우 true를 반환해야 합니다.", () => {
+      expect(Validate.isMultipleOf1000("5000")).toBe(true);
+    });
+
+    test("1000의 배수가 아닌 경우 false를 반환해야 합니다.", () => {
+      expect(Validate.isMultipleOf1000("3001")).toBe(false);
+    });
+  });
 });
