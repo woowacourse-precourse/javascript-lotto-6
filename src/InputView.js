54b6b6130c53546a139973d53659663d43edfb02
Ivan <ivan@broj.company>
2023-11-08 21:12:54 +0900

    fix : 에러 처리 후 입력 다시받기 수정
    문제의 요구사항에 맞춰서 에러 처리 후 입력 다시받기 기능을 구현했습니다.

--- src/InputView.js
+++ src/InputView.js
@@ -3,17 +3,27 @@ import { Console } from '@woowacourse/mission-utils';
 const INPUT_MONEY_MESSAGE = '구입금액을 입력해 주세요.\n';
 const INPUT_LOTTO_NUMBERS_MESSAGE = '당첨 번호를 입력해 주세요.\n';
 const INPUT_BONUS_NUMBER_MESSAGE = '보너스 번ㅎ를을 입력해 주세요.\n';
-export default class InputView {
+class InputView {
   static async InputMoney() {
     const userInputMoney = await Console.readLineAsync(INPUT_MONEY_MESSAGE);
-    this.validateMoney(userInputMoney);
+    try {
+      this.validateMoney(userInputMoney);
+    } catch (error) {
+      Console.print(error.message);
+      return this.InputMoney();
+    }
     const money = Number(userInputMoney);
     return money;
   }
 
   static async InputLottoNumbers() {
     const userInputLottoNumbers = await Console.readLineAsync(INPUT_LOTTO_NUMBERS_MESSAGE);
-    this.validateLottoNumbers(userInputLottoNumbers);
+    try {
+      this.validateLottoNumbers(userInputLottoNumbers);
+    } catch (error) {
+      Console.print(error.message);
+      return this.InputLottoNumbers();
+    }
     const stringLottoNumbers = userInputLottoNumbers.split(',');
     const lottoNumbers = stringLottoNumbers.map(number => Number(number));
     return lottoNumbers;
@@ -21,7 +31,12 @@ export default class InputView {
 
   static async setBonusNumber() {
     const userInputBonusNumber = await Console.readLineAsync(INPUT_BONUS_NUMBER_MESSAGE);
-    this.validateBonusNumber(userInputBonusNumber);
+    try {
+      this.validateBonusNumber(userInputBonusNumber);
+    } catch (error) {
+      Console.print(error.message);
+      return this.setBonusNumber();
+    }
     const bonusNumber = Number(userInputBonusNumber);
     return bonusNumber;
   }
@@ -43,25 +58,26 @@ export default class InputView {
     const testLottoNumbers = input.split(',');
 
     if (testLottoNumbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.\n');
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
     if (new Set(testLottoNumbers).size !== 6) {
-      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.\n');
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
     }
     if (testLottoNumbers.some(number => Number(number) < 1 || Number(number) > 45)) {
-      throw new Error('[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.\n');
+      throw new Error('[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.');
     }
     if (testLottoNumbers.some(number => Number.isInteger(Number(number)) === false)) {
-      throw new Error('[ERROR] 로또 번호는 정수여야 합니다.\n');
+      throw new Error('[ERROR] 로또 번호는 정수여야 합니다.');
     }
   }
 
   static validateBonusNumber(input) {
     if (Number.isInteger(Number(input)) === false) {
-      throw new Error('[ERROR] 보너스 번호는 정수여야 합니다.\n');
+      throw new Error('[ERROR] 보너스 번호는 정수여야 합니다.');
     }
     if (Number(input) < 1 || Number(input) > 45) {
-      throw new Error('[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.\n');
+      throw new Error('[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.');
     }
   }
 }
+export default InputView;
