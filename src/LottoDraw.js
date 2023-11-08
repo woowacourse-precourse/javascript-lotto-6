3921fab273f99462c9856d43135dcf37983cb381
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-08 15:10:52 +0900

    rename: WinningNumbers => LottoDraw 로 이름 변경
    다른 클래스와 네이밍 규칙을 비슷하게 하기 위해 이름 변경

---
+++ src/LottoDraw.js
@@ -0,0 +1,50 @@
+import { Console } from "@woowacourse/mission-utils";
+import BonusWinningLotto from './BonusWinningLotto.js';
+import DefaultWinningLotto from './DefaultWinningLotto.js';
+
+class LottoDraw {
+
+    DEFAULT_WINNING_LOTTO;
+    BONUS_WINNING_LOTTO;
+    
+    DEFAULT_WINNING_NUMBERS_INPUT_MESSEGSE = '당첨 번호를 입력해 주세요.\n';
+    BONUS_WINNING_NUMBERS_INPUT_MESSEGSE = '보너스 번호를 입력해 주세요.\n';
+
+
+    async #getDefaultWinningNumbers() {
+        let DEFAULT_WINNING_NUMBERS = (await Console.readLineAsync(this.DEFAULT_WINNING_NUMBERS_INPUT_MESSEGSE)).split(',');
+        
+        DEFAULT_WINNING_NUMBERS = DEFAULT_WINNING_NUMBERS.map(Number);
+        this.DEFAULT_WINNING_LOTTO = new DefaultWinningLotto(DEFAULT_WINNING_NUMBERS);
+    }
+
+    async #getBonusWinningNumbers() {
+        const BONUS_WINNING_NUMBER = parseInt(await Console.readLineAsync(this.BONUS_WINNING_NUMBERS_INPUT_MESSEGSE));
+        
+        this.BONUS_WINNING_LOTTO = new BonusWinningLotto(this.DEFAULT_WINNING_LOTTO, BONUS_WINNING_NUMBER);
+    }
+
+    async getWinningNumbers() {
+        while(true){
+            try {
+                await this.#getDefaultWinningNumbers();
+            } catch(e){
+                await Console.print(e.message);
+                continue;
+            }
+            break;
+        }
+        Console.print('\n');
+        while(true){
+            try {
+                await this.#getBonusWinningNumbers();
+            } catch(e){
+                await Console.print(e.message);
+                continue;
+            }
+            break;
+        }
+    }
+}
+
+export default LottoDraw;
