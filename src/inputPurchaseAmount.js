4080ecddf82403de332547cccc2db06af492ef5d
CHHyuk <gksgur1038@naver.com>
2023-11-08 17:32:46 +0900

    구입 금액 입력, 입력값 판별

---
+++ src/inputPurchaseAmount.js
@@ -0,0 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import validatePurchaseAmount from './validates/validatePurchaseAmount';
+
+export default async function inputPurchaseAmount() {
+  try {
+    const purchaseAmount = await Console.readLineAsync('구입금액을 입력해주세요\n');
+    purchaseAmount = Number(purchaseAmount);
+    purchaseAmount = validatePurchaseAmount(purchaseAmount);
+    return purchaseAmount;
+  } catch (error) {
+    Console.print(error.message);
+    return inputPurchaseAmount();
+  }
+}
