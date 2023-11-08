a428f84861e246470ae11adc0387fd088a10786c
dxwwan <kkw1700@gmail.com>
2023-11-08 11:40:18 +0900

    fix: 테스트 경우 삭제

--- __tests__/PurchaseTest.js
+++ __tests__/PurchaseTest.js
@@ -2,12 +2,6 @@ import Purchase from "../src/Credit.js";
 import Lotto from "../src/Lotto.js";
 
 describe("로또 구매 테스트", () => {
-  test("로또 구매 갯수 테스트.", () => {
-    const purchase = new Purchase();
-    const result = purchase.getAmountOfLotto(3000);
-    expect(result).toBe(3);
-  });
-
   test("로또 구매 예외 테스트", () => {
     expect(() => {
       const purchase = new Purchase();
