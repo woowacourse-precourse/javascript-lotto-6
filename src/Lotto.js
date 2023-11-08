d6c80f42a2c7d5cfd3727d668d53d73303d2a2d0
mbeankong <tomy9909@gmail.com>
2023-11-08 23:50:15 +0900

    unfinished

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,23 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (numbers.some(number => typeof number !== "number")) {
+      throw new Error("[ERROR] 로또 번호는 숫자이어야 합니다.");
+    }
+
+    if (numbers.some(number => number < 1 || number > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
