af9b8240a4e76aff4805f104d6760583dc024440
JungHun98 <j7papa@naver.com>
2023-11-08 09:29:18 +0900

    feat: 상수 파일 정의

---
+++ src/datas/prize.js
@@ -0,0 +1,30 @@
+export const FIRST = 'first';
+export const SECOND = 'second';
+export const THIRD = 'third';
+export const FOURTH = 'fourth';
+export const FIFTH = 'fifth';
+
+export const prize = {
+  first: {
+    money: 2000000000,
+    standard: '6개 일치',
+  },
+  second: {
+    money: 30000000,
+    standard: '5개 일치, 보너스 볼 일치',
+  },
+  third: {
+    money: 1500000,
+    standard: '5개 일치',
+  },
+  fourth: {
+    money: 50000,
+    standard: '4개 일치',
+  },
+  fifth: {
+    money: 5000,
+    standard: '3개 일치',
+  },
+};
+
+export const rank = [FIRST, SECOND, THIRD, FOURTH, FIFTH];
