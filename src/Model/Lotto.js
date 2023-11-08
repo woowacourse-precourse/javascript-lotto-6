47b8f2cc8552610c6e5119fb8c02d1efcfdda1da
yunji <dbswl330@gmail.com>
2023-11-08 14:52:23 +0900

    feat: 로또 번호를 valiate하는 함수 구현

--- src/Model/Lotto.js
+++ src/Model/Lotto.js
@@ -7,12 +7,14 @@ class Lotto {
   }
 
   #validate(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+    }
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
