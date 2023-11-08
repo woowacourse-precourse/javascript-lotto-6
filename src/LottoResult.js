89da1b498648a5568bf9533f0a35944db712d1c3
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-08 20:40:05 +0900

    feat: print the result

--- src/LottoResult.js
+++ src/LottoResult.js
@@ -72,5 +72,18 @@ class LottoResult {
     this.#profit = (amount / (this.#lottoList.length * 1000)) * 100;
     this.#profit = Number(this.#profit.toFixed(1));
   }
+
+  printResult() {
+    Console.print(`\n당첨 통계`);
+    Console.print(`---`);
+    Console.print(`3개 일치 (5,000원) - ${this.#resultList[0]}개`);
+    Console.print(`4개 일치 (50,000원) - ${this.#resultList[1]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.#resultList[2]}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.#resultList[3]}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.#resultList[4]}개`);
+    Console.print(`총 수익률은 ${this.#profit}%입니다.`);
+  }
 }
 export default LottoResult;
