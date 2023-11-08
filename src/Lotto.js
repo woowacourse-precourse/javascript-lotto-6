5e18e9730b2b5c72242256cdc1f76a41bacfe01f
김선인 <kimsunin@gimseon-in-ui-MacBookAir.local>
2023-11-08 19:33:26 +0900

    test

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,33 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if ([...new Set(numbers)].length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되면 안됩니다.");
+    }
+  }
+  get getNumbers() {
+    return this.#numbers;
   }
 
   // TODO: 추가 기능 구현
+  compareNumbers(array, bonus) {
+    let count = 0;
+    for (let i = 0; i < 6; i++) {
+      if (array.includes(this.#numbers[i])) {
+        count++;
+      }
+    }
+
+    if (count == 6) return 5;
+    if (count == 5) {
+      if (array.includes(bonus)) return 4;
+      return 3;
+    }
+    if (count == 4) return 2;
+    if (count == 3) return 1;
+
+    return 0;
+  }
 }
 
 export default Lotto;
