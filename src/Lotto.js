0bbc82a30d255b8cf18adc9e1d8fd270ef615cb2
koblmom <nayun9191@gamil.com>
2023-11-08 20:57:09 +0900

    fix: 로또 당첨 번호 숫자 타입 확인 로직 오류 수정

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,17 +8,19 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      console.log(numbers);
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다. ");
     }
-    if (Number.isNaN(numbers)) {
-      throw new Error("[ERROR] 숫자만 입력해주세요. ");
+    if (
+      !Array.isArray(numbers) ||
+      numbers.some((number) => Number.isNaN(number))
+    ) {
+      throw new Error("[ERROR] 숫자만 입력해주세요.");
     }
     if (!numbers.every((number) => this.isNumberValid(number))) {
-      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다. ");
     }
     if (new Set(numbers).size !== numbers.length) {
-      throw new Error("[ERROR] 중복된 로또 번호가 있습니다.");
+      throw new Error("[ERROR] 중복된 로또 번호가 있습니다. ");
     }
   }
 
