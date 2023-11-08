4930f3576a745743e22742b7be46d94d11eb21d3
youngchann <lych96@gmail.com>
2023-11-08 16:27:28 +0900

    test: 배열 요소 타입 수정
    - 문자열에서 숫자로 수정

--- __tests__/MatchTest.js
+++ __tests__/MatchTest.js
@@ -2,8 +2,8 @@ import Match from '../src/module/Match';
 
 describe('Match 클래스 테스트', () => {
   test('로또 1개의 번호가 3개 일치하면 threeMatches가 1증가한다.', () => {
-    const PURCHASED_LOTTO_ARRAY = [['1', '2', '3', '4', '5', '6']];
-    const WINNING_NUMBERS = ['1', '2', '3', '11', '12', '13'];
+    const PURCHASED_LOTTO_ARRAY = [[1, 2, 3, 4, 5, 6]];
+    const WINNING_NUMBERS = '1,2,3,11,12,13';
     const BONUS = '14';
     const match = new Match(PURCHASED_LOTTO_ARRAY, WINNING_NUMBERS, BONUS);
     const matchCount = match.getMatchCount();
@@ -17,8 +17,8 @@ describe('Match 클래스 테스트', () => {
   });
 
   test('로또 1개의 번호가 4개 일치하면 fourMatches가 1증가한다.', () => {
-    const PURCHASED_LOTTO_ARRAY = [['1', '2', '3', '4', '5', '6']];
-    const WINNING_NUMBERS = ['1', '2', '3', '4', '12', '13'];
+    const PURCHASED_LOTTO_ARRAY = [[1, 2, 3, 4, 5, 6]];
+    const WINNING_NUMBERS = '1,2,3,4,12,13';
     const BONUS = '14';
     const match = new Match(PURCHASED_LOTTO_ARRAY, WINNING_NUMBERS, BONUS);
     const matchCount = match.getMatchCount();
@@ -32,8 +32,8 @@ describe('Match 클래스 테스트', () => {
   });
 
   test('로또 1개의 번호가 5개 일치하면 fiveMatches가 1증가한다.', () => {
-    const PURCHASED_LOTTO_ARRAY = [['1', '2', '3', '4', '5', '6']];
-    const WINNING_NUMBERS = ['1', '2', '3', '4', '5', '13'];
+    const PURCHASED_LOTTO_ARRAY = [[1, 2, 3, 4, 5, 6]];
+    const WINNING_NUMBERS = '1,2,3,4,5,13';
     const BONUS = '14';
     const match = new Match(PURCHASED_LOTTO_ARRAY, WINNING_NUMBERS, BONUS);
     const matchCount = match.getMatchCount();
@@ -47,8 +47,8 @@ describe('Match 클래스 테스트', () => {
   });
 
   test('로또 1개의 번호가 5개, 보너스 1개 일치하면 fiveBonusMatches가 1증가한다.', () => {
-    const PURCHASED_LOTTO_ARRAY = [['1', '2', '3', '4', '5', '6']];
-    const WINNING_NUMBERS = ['1', '2', '3', '4', '5', '13'];
+    const PURCHASED_LOTTO_ARRAY = [[1, 2, 3, 4, 5, 6]];
+    const WINNING_NUMBERS = '1,2,3,4,5,13';
     const BONUS = '6';
     const match = new Match(PURCHASED_LOTTO_ARRAY, WINNING_NUMBERS, BONUS);
     const matchCount = match.getMatchCount();
@@ -62,8 +62,8 @@ describe('Match 클래스 테스트', () => {
   });
 
   test('로또 1개의 번호가 6개 일치하면 sixMatches가 1증가한다.', () => {
-    const PURCHASED_LOTTO_ARRAY = [['1', '2', '3', '4', '5', '6']];
-    const WINNING_NUMBERS = ['1', '2', '3', '4', '5', '6'];
+    const PURCHASED_LOTTO_ARRAY = [[1, 2, 3, 4, 5, 6]];
+    const WINNING_NUMBERS = '1,2,3,4,5,6';
     const BONUS = '7';
     const match = new Match(PURCHASED_LOTTO_ARRAY, WINNING_NUMBERS, BONUS);
     const matchCount = match.getMatchCount();
@@ -78,13 +78,13 @@ describe('Match 클래스 테스트', () => {
 
   test('로또 5개 중 번호가 3,3,5개 일치하면 threeMatches가 2증가, fiveMatches 1증가한다.', () => {
     const PURCHASED_LOTTO_ARRAY = [
-      ['1', '2', '3', '4', '5', '6'],
-      ['1', '2', '3', '21', '22', '23'],
-      ['1', '2', '41', '42', '43', '44'],
-      ['1', '2', '11', '12', '13', '44'],
-      ['11', '12', '13', '14', '15', '16'],
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 21, 22, 23],
+      [1, 2, 41, 42, 43, 44],
+      [1, 2, 11, 12, 13, 44],
+      [11, 12, 13, 14, 15, 16],
     ];
-    const WINNING_NUMBERS = ['1', '2', '3', '41', '42', '43'];
+    const WINNING_NUMBERS = '1,2,3,41,42,43';
     const BONUS = '7';
     const match = new Match(PURCHASED_LOTTO_ARRAY, WINNING_NUMBERS, BONUS);
     const matchCount = match.getMatchCount();
