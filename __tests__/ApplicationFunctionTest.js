e58b009a4f5bd6978e601e1baf0df562b4580356
kelvin3476 <winrain03226@gmail.com>
2023-11-08 18:09:26 +0900

    test(ApplicationFunctionTest): 함수별 기능 테스트 삭제 및 수정

--- __tests__/ApplicationFunctionTest.js
+++ __tests__/ApplicationFunctionTest.js
@@ -1,7 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import LottoHandler from '../src/lottoHandler/LottoHandler';
-import View from '../src/view/View';
-
 
 describe('함수별 기능 테스트', () => {
   test('구매 로또 개수 추출하기', () => {
@@ -41,32 +39,6 @@ describe('함수별 기능 테스트', () => {
     };
 
     expect(result).toEqual(output);
-  });
-
-  test('당첨 통계 출력하기', () => {
-    const view = new View();
-    const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-
-    const input = {
-      FIFTH: 1,
-      FORTH: 0,
-      THIRD: 1,
-      SECOND: 0,
-      FIRST: 0,
-    };
-    const logs = [
-      '3개 일치 (5,000원) - 1개',
-      '4개 일치 (50,000원) - 0개',
-      '5개 일치 (1,500,000원) - 1개',
-      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
-      '6개 일치 (2,000,000,000원) - 0개',
-    ];
-
-    view.printRankResult(input);
-
-    logs.forEach(log => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
   });
 
   test('당첨 상금 추출하기', () => {
