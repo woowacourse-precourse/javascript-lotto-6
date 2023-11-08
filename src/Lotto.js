fc94982f073ca3f0ce427579b56bddfe845dfe30
lij0825 <dldlswns890@gmail.com>
2023-11-08 22:26:08 +0900

    feat : Lotto.js 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,26 @@
 class Lotto {
-  #numbers;
+    #numbers;
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
-
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    constructor(numbers) {
+        this.#validate(numbers);
+        this.#numbers = numbers;
     }
-  }
 
-  // TODO: 추가 기능 구현
+    #validate(numbers) {
+        if (numbers.length !== 6) {
+            throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+    // TODO: 추가 기능 구현
+    containsNumber(number) {
+        return this.#numbers.includes(number);
+    }
+
+    getMatchedNumbers(winningNumbers) {
+        return this.#numbers.filter((number) =>
+            winningNumbers.includes(number)
+        );
+    }
 }
 
 export default Lotto;
