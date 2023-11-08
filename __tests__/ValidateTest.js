f99d2b4f09fa634258e05c8f171e69ab8394b8b2
zelkovaria <hyein396@gmail.com>
2023-11-08 16:42:06 +0900

    feat: validateWinningNumbers 1~45 정수 중복 입력시 예외발생 test 구현

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -19,4 +19,12 @@ describe("ValidateTest", () => {
       ERROR_MESSAGE.moneyValue
     );
   });
+
+  test("당첨번호는 1~45이내 정수를 중복해서 입력하면 예외가 발생한다.", () => {
+    expect(() =>
+      Validator.validateWinningNumbers([1, 2, 3, 4, 5, 5]).toThrow(
+        ERROR_MESSAGE.invalidTicketNumbers
+      )
+    );
+  });
 });
