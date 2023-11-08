0bec3c85aa425fb19b1c001c5d24b9dc1f437365
hyewon <hyewon.parkhw@gmail.com>
2023-11-08 16:10:46 +0900

    feat: 로또 클래스 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,17 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호가 1-45 사이의 숫자가 아니라면 예외가 발생한다.",  () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, "a"]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 1-45 사이의 숫자가 아니라면 예외가 발생한다.",  () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      lotto.addBonusNumber("a");
+    }).toThrow("[ERROR]");
+  });
+
 });
