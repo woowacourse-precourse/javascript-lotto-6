9efa5c73810f801022277c1718074e7899978598
yheseul_kang <kangyheseul@naver.com>
2023-11-08 21:50:49 +0900

    Refactor : 코드 리펙토링

--- src/validator/bonusNumberValidation.js
+++ src/validator/bonusNumberValidation.js
@@ -2,18 +2,18 @@ export const bonusNumberValidation = bonusNumber => {
   Object.values(bonusNumberValidation.validation).forEach(({ errorMessage, isInvalid }) => {
     if(isInvalid(bonusNumber)) throw new Error (errorMessage);
     },
-  );
-};
+  )
+}
 
 bonusNumberValidation.validation = Object.freeze({
   limitedBonusNumber: Object.freeze({
-  errorMessage: '1~45 사이의 숫자만 입력해 주세요.',
+  errorMessage: '[ERROR]1~45 사이의 숫자만 입력해 주세요.',
   isInvalid(bonusNumber) {
-    if (bonusNumber < 1 && bonusNumber > 45)
+    if (bonusNumber < 1 && 45 < 
+bonusNumber)
     return true;
     }
-  }),
-
+  })
 })  
 
 export default bonusNumberValidation;
