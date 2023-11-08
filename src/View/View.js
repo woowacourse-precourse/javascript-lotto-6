fa803dffad6fe1082186140efb41581ccd9b06d3
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-11-08 23:01:34 +0900

    Feat :당첨통계 기능 작성

--- src/View/View.js
+++ src/View/View.js
@@ -40,4 +40,15 @@ export default class View {
         return this.lottoNumbers;
     }
 
+    printResult(lottoResult, earningRate) {
+        this.print(`
+당첨 통계
+---
+3개 일치 (5,000원) - ${lottoResult[4]}개
+4개 일치 (50,000원) - ${lottoResult[3]}개
+5개 일치 (1,500,000원) - ${lottoResult[2]}개
+5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResult[1]}개
+6개 일치 (2,000,000,000원) - ${lottoResult[0]}개
+총 수익률은 ${(earningRate.toFixed(1))}%입니다.`)
+    }
 }
