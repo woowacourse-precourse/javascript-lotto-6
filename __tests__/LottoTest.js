2809de79678c275f323d8ae9b2104add622f54b1
Brokyeom <goindol40@gmail.com>
2023-11-08 16:57:58 +0900

    test : 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,9 +15,42 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
-  test("로또 번호가 45 초과인 경우 예회가 발생한다.", () => {
+  test("로또 번호가 45 초과인 경우 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 문자열이 들어간 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, "a"]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 음수가 들어간 경우 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, -6]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 숫자가 아닌 경우 예외가 발생한다.", () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      lotto.addBonusNumber("a");
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 1-45 사이의 숫자가 아닌 경우 예외가 발생한다.", () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      lotto.addBonusNumber(46);
+    }).toThrow("[ERROR]");
+  });
+
+  test("보너스 번호가 음수인 경우 예외가 발생한다.", () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+      lotto.addBonusNumber(-1);
     }).toThrow("[ERROR]");
   });
 });
