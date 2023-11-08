20586e43b651505e4d8d5f109c7ca7987a3c0fef
Jihyun_Kim <jihyun3313@daum.net>
2023-11-08 20:27:30 +0900

    test: lottoMatchStart 메소드 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,28 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("lottoMatchStart 메소드 테스트", () => {
+
+    const randomNumbersArray = [
+        [8, 21, 23, 41, 42, 43],
+        [3, 5, 11, 16, 32, 38],
+        [7, 11, 16, 35, 36, 44],
+        [1, 8, 11, 31, 41, 42],
+        [13, 14, 16, 38, 42, 45],
+        [7, 11, 30, 40, 42, 43],
+        [2, 13, 22, 32, 38, 45],
+        [1, 3, 5, 14, 22, 45],
+      ];
+
+      const bonusNumber = 7;
+      const winningNumbers = [1,2,3,4,5,14];
+      
+      const lotto = new Lotto(winningNumbers);
+      const result = [0,1,0,0,0];
+      
+      expect(lotto.lottoMatchStart(randomNumbersArray, bonusNumber)).toEqual(result);
+
+    
+  });
+
 });
