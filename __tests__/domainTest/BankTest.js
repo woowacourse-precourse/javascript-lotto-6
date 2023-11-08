62b19e985ccf744f80166d6a4c248f9558f7d48d
apfp77 <apfp77@naver.com>
2023-11-08 20:30:02 +0900

    test(bank): 테스트 항목 추가
    - 당첨 금액의 총 합계
    - 수익률 계산

--- __tests__/domainTest/BankTest.js
+++ __tests__/domainTest/BankTest.js
@@ -1,17 +1,27 @@
 /* eslint-disable */
+import { WINNING_RANK_COUNT } from '../../src/constants/lotto';
+import { ZERO } from '../../src/constants/validate';
 import bank from '../../src/domain/Bank';
 
 describe.only('Bank Test', () => {
   test('당첨 금액의 총 합계 테스트', () => {
-    const rank = [5, 5, 5];
+    const ranks = {
+      [ZERO]: 0,
+      [WINNING_RANK_COUNT.firstPlace]: 0,
+      [WINNING_RANK_COUNT.secondPlace]: 0,
+      [WINNING_RANK_COUNT.thirdPlace]: 0,
+      [WINNING_RANK_COUNT.fourthPlace]: 0,
+      [WINNING_RANK_COUNT.fifthPlace]: 3,
+    };
     const total = 5000 + 5000 + 5000;
-    expect(bank.calculateTotalPrizeAmount(rank)).toBe(total);
+    expect(bank.calculateTotalPrizeAmount(ranks)).toBe(total);
   });
+
   test('수익률 계산 테스트', () => {
     const purchaseCost = 8000;
     const totalPriz = 5000;
     const profitRate = 62.5;
-    console.log(bank.calculateProfitRate(purchaseCost, totalPriz));
+    bank.calculateProfitRate(purchaseCost, totalPriz);
     expect(bank.calculateProfitRate(purchaseCost, totalPriz)).toBe(profitRate);
   });
 });
