7809c5457f45aa5bc127f1efb3017049e7954de0
Klomachenko <leekyumin0901@naver.com>
2023-11-08 21:12:01 +0900

    feat(Error): bonusNumberError 구현
    Description: 보너스 번호에 대한 에러 메시지 추가

--- src/Error.js
+++ src/Error.js
@@ -6,6 +6,10 @@ class ErrorMessages {
   static lottoNumbersDuplicate() {
     return "[ERROR] 중복된 로또번호는 있을 수 없습니다.";
   }
+
+  static bonusNumberError() {
+    return "[ERROR] 보너스 번호는 로또번호와 달라야 합니다.";
+  }
 }
 
 export default ErrorMessages;
