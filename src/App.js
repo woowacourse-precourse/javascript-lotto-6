4c9a12c80106e9caa0ac066d96e82903a45452dc
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-08 13:57:18 +0900

    feature: 로또 번호 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { STRINGS } from './constants/STRINGS';
 class App {
   async play() {}
+  async play() {
+    this.startGame();
+  }
+
+  async startGame() {
+      
+  }
+
+  async getLottoAmount(){
+    const INPUT_LOTTO_AMOUNT = await MissionUtils.Console.readLineAsync(STRINGS.PAY_INPUT);
+    return this.countLottoAmount(INPUT_LOTTO_AMOUNT);
+  }
+
 }
 
 export default App;
