f572e674afadfd6f0f367d85e7e7b44807e154ad
이동주 <leednj0113@naver.com>
2023-11-08 10:26:57 +0900

    feat(LottoDrawing): 당첨 로또 생성

--- src/LottoDrawing.js
+++ src/LottoDrawing.js
@@ -5,11 +5,24 @@ import Lotto from "./Lotto.js";
 class LottoDrawing {
   #winnigLotto;
   #bonusNumber;
+
+  getWinningLotto() {
+    return this.#winnigLotto;
+  }
   async inputWinningLotto() {
     try {
       const input = await Console.readLineAsync(
         "\n당첨 번호를 입력해 주세요.\n"
       );
+
+      const lotto = new Lotto(input.split(",").map(Number));
+      this.#winnigLotto = lotto;
+    } catch (error) {
+      Console.print(error.message);
+
+      return this.inputWinningLotto();
+    }
+  }
   }
 }
 
