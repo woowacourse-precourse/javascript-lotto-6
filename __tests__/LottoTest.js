60e13cca85abf22c61edf959bb524072bd007e86
minSsan <msp1125@hanyang.ac.kr>
2023-11-08 22:31:41 +0900

    test: Lotto 클래스 get_winning_rank 메소드 검증

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,7 +1,22 @@
 /* eslint-disable max-lines-per-function */
 import Lotto from "../src/Lotto.js";
+import WinningLotto from "../src/WinningLotto.js";
 
 describe("로또 클래스 테스트", () => {
+  const BONUS_NUMBER = 7;
+
+  let winning_lotto = new WinningLotto([1, 2, 3, 4, 5, 6]);
+  winning_lotto.set_bonus(BONUS_NUMBER);
+
+  const LOTTO = {
+    FIRST_WINNING: new Lotto([1, 2, 3, 4, 5, 6]),
+    SECOND_WINNING: new Lotto([1, 2, 3, 4, 5, 7]),
+    THIRD_WINNING: new Lotto([1, 2, 3, 4, 5, 8]),
+    FOURTH_WINNING: new Lotto([1, 2, 3, 4, 8, 9]),
+    FIFTH_WINNING: new Lotto([1, 2, 3, 8, 9, 10]),
+    NO_WINNING: new Lotto([1, 2, 8, 9, 10, 11]),
+  };
+
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -32,5 +47,30 @@ describe("로또 클래스 테스트", () => {
     expect(() => {
       new Lotto([1, 2, 3, "4", 45, 1]);
     }).toThrow("[ERROR]");
+  });
+
+  /** 등수 검증 */
+  test("get_winning_rank - 1등 검증", () => {
+    expect(LOTTO.FIRST_WINNING.get_winning_rank(winning_lotto)).toBe(1);
+  });
+
+  test("get_winning_rank - 2등 검증", () => {
+    expect(LOTTO.SECOND_WINNING.get_winning_rank(winning_lotto)).toBe(2);
+  });
+
+  test("get_winning_rank - 3등 검증", () => {
+    expect(LOTTO.THIRD_WINNING.get_winning_rank(winning_lotto)).toBe(3);
+  });
+
+  test("get_winning_rank - 4등 검증", () => {
+    expect(LOTTO.FOURTH_WINNING.get_winning_rank(winning_lotto)).toBe(4);
+  });
+
+  test("get_winning_rank - 5등 검증", () => {
+    expect(LOTTO.FIFTH_WINNING.get_winning_rank(winning_lotto)).toBe(5);
+  });
+
+  test("get_winning_rank - 순위 밖 검증", () => {
+    expect(LOTTO.NO_WINNING.get_winning_rank(winning_lotto)).toBe(0);
   });
 });
