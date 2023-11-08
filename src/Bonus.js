c31b9e6e6a9fc70980cc513b4f29ad988c6309b0
JoSooha <dlrhdcjs@naver.com>
2023-11-08 18:57:56 +0900

    feat: Bonus 번호 예외 처리 위한 Bonus class 구현

---
+++ src/Bonus.js
@@ -0,0 +1,32 @@
+class Bonus {
+    #number;
+  
+    constructor(number, winning) {
+      this.#validate(number, winning);
+      this.#number = number;
+    }
+  
+    #validate(number, winning) {
+      this.#validateNumber(num);
+      for(let i=0; i<6; i++) {
+        const num = winning[i];
+        this.#validateDuplicate(number, num);
+      }
+    }
+  
+    #validateNumber(number) {
+      const parsedNumber = Number(number)
+      if(isNaN(parsedNumber) || parsedNumber<1 || parsedNumber>45) {
+        throw new Error("[ERROR] 보너스 번호는 1~45의 숫자여야 합니다.")
+      }
+    }
+  
+    #validateDuplicate(number, num) {
+      if(number === num) throw new Error("[ERROR] 보너스 번호가 중복되었습니다.")
+    }
+  
+    // TODO: 추가 기능 구현
+  }
+  
+  export default Bonus;
+  
