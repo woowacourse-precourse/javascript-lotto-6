f273978fbfeaefedc3aeadecaa5531413a4fca41
Jinhyo Park <vkstk0131@yonsei.ac.kr>
2023-11-08 21:36:44 +0900

    test: 구입 금액이 0원보다 큰 금액인지

--- __tests__/LottoPriceTest.js
+++ __tests__/LottoPriceTest.js
@@ -12,4 +12,10 @@ describe('구입 금액 테스트', () => {
       Validations.isNumber(lottoPrice);
     }).toThrow('[ERROR] 숫자만 입력해 주세요.');
   });
+
+  test.each([['0', '-1000', '-1']])('0원 보다 큰 금액인지', lottoPrice => {
+    expect(() => {
+      Validations.isPlus(lottoPrice);
+    }).toThrow('[ERROR] 양수만 입력해 주세요.');
+  });
 });
