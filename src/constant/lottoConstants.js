b209f908e1411fe36bd83caa60c6a083c4a76cf2
BK <3031011@naver.com>
2023-11-08 18:49:54 +0900

    refactor: 상수의 불변성을 위해 객체 동결
    Object.freeze 메소드를 통해 객체를 변경할 수 없도록 동결

--- src/constant/lottoConstants.js
+++ src/constant/lottoConstants.js
@@ -1,4 +1,4 @@
-export const MESSAGE = {
+export const MESSAGE = Object.freeze({
   PURCHASED: '개를 구매했습니다.',
   WINNING_STATISTICS: '당첨통계',
   MATCH: '개 일치',
@@ -7,15 +7,15 @@ export const MESSAGE = {
   PIECES: '개',
   TOTAL_PROFIT_RATE: '총 수익률은',
   PERCENT: '%입니다.',
-};
+});
 
-export const PROMPT_MESSAGE = {
+export const PROMPT_MESSAGE = Object.freeze({
   INPUT_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
   INPUT_WINNING_NUMBER: '로또 당첨 번호를 입력해주세요.',
   INPUT_BONUS_NUMBER: '보너스 번호를 입력해주세요',
-};
+});
 
-export const ERROR_MESSAGE = {
+export const ERROR_MESSAGE = Object.freeze({
   NOT_A_NUMBER: '[ERROR] 구입금액은 숫자로 입력해야 합니다.',
   NOT_DIVISIBLE: '[ERROR] 로또는 1,000원 단위로 구매 가능합니다.',
   LESS_LOTTO_NUMBERS: '[ERROR] 로또 번호는 6개여야 합니다.',
@@ -23,33 +23,33 @@ export const ERROR_MESSAGE = {
   MUST_BE_NUMBER: '[ERROR] 로또 번호는 숫자여야 합니다.',
   INVALID_LOTTO_NUMBER_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
   INVALID_BONUS_NUMBER: '[ERROR] 보너스 번호는 당첨 번호에 포함되지 않은 수여야 합니다.',
-};
+});
 
-export const PRICE = {
+export const PRICE = Object.freeze({
   LOTTO: 1000,
-};
+});
 
-export const LOTTO_PRIZE_AMOUNT = {
+export const LOTTO_PRIZE_AMOUNT = Object.freeze({
   FIRST_PLACE: 2000000000,
   SECOND_PLACE: 30000000,
   THIRD_PLACE: 1500000,
   FOURTY_PLACE: 50000,
   LAST_PLACE: 5000,
-};
+});
 
-export const MATCH_COUNT = {
+export const MATCH_COUNT = Object.freeze({
   THREE: 3,
   FOUR: 4,
   FIVE: 5,
   SIX: 6,
-};
+});
 
-export const MAX = {
+export const MAX = Object.freeze({
   PROFIT_DECIMAL_LENGTH: 2,
   LOTTO_NUMBER: 45,
   LOTTO_NUMBERS: 6,
-};
+});
 
-export const MIN = {
+export const MIN = Object.freeze({
   LOTTO_NUMBER: 1,
-};
+});
