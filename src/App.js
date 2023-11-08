a1cbf392201c2914f834b052069fcc87c4e687b2
apfp77 <apfp77@naver.com>
2023-11-08 20:45:33 +0900

    feat(App): 컨트롤러 실행 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import LottoController from './services/Controller';
+
 class App {
-  async play() {}
+  #lottoController;
+
+  constructor() {
+    this.#lottoController = new LottoController();
+  }
+
+  async play() {
+    await this.#lottoController.play();
+  }
 }
 
 export default App;
