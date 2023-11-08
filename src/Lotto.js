159783612de3c93febc39fb531c69bb255f8bf70
Klomachenko <leekyumin0901@naver.com>
2023-11-08 21:39:20 +0900

    feat(Lotto): validate 로또 번호 유효성 검사 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+import Validate from "./Validate.js";
+import ErrorMessages from "./Error.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
@@ -8,7 +12,10 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(ErrorMessages.invalidInput("로또 번호는 6개여야 합니다."));
+    }
+    if (!Validate.isLottoNumbersFormat(numbers)) {
+      throw new Error(ErrorMessages.lottoFormatError);
     }
   }
 
