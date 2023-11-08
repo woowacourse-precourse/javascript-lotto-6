596be8b9019854e50b94d495f357a092bd5363d0
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-11-08 23:07:11 +0900

    feat: 로또가 보너스 번호와 일치하는지 확인하는 함수

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,7 +12,13 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  /**
+   * 해당 숫자를 포함하는지 여부를 반환합니다
+   * @param {number} number
+   * @return {boolean} 포함 여부 */
+  contains(number) {
+    return this.#numbers.includes(number);
+  }
 }
 
 export default Lotto;
