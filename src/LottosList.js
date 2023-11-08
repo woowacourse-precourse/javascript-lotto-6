1564fa41894b25ccf3fb3b71279ef0fe7246189a
sehyun <e6d1fe@gmail.com>
2023-11-08 13:59:25 +0900

    comment: 불필요한 주석, import 삭제

--- src/LottosList.js
+++ src/LottosList.js
@@ -1,25 +1,16 @@
 import { Random } from '@woowacourse/mission-utils';
-import { message, num } from './Constants.js';
+import { num } from './Constants.js';
 
 import Lotto from './Lotto.js';
-
-import validatePurchaseAmount from './Validation/validatePurchaseAmount.js';
 
 class LottosList {
   #lottosList = [];
   #lottoAmount;
 
   constructor(purchaseAmount) {
-    // this.validate(purchaseAmount);
     this.calculateLottoAmount(purchaseAmount);
     this.addLottosToList();
   }
-
-  // validate(purchaseAmount) {
-  //   if (!validatePurchaseAmount(purchaseAmount)) {
-  //     throw new Error(message.ERROR);
-  //   }
-  // }
 
   calculateLottoAmount(purchaseAmount) {
     return (this.#lottoAmount = purchaseAmount / 1000);
