ea8622813e7c55c4a2131e7d85b7935f742aeabb
westofsky <westofsky159@gmail.com>
2023-11-08 15:51:04 +0900

    feat: App의 실행이 되는 LottoController 생성
    제일 처음 시작은 LottoController.run()으로 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import LottoController from "./controller/LottoController.js";
+
 class App {
-  async play() {}
+  async play() {
+    const lottoController = new LottoController();
+    await lottoController.run();
+  }
 }
 
 export default App;
---
+++ src/controller/LottoController.js
@@ -0,0 +1,7 @@
+import { Console,Random } from '@woowacourse/mission-utils';
+class LottoController {
+  async run(){
+    
+  }
+}
+export default LottoController;
