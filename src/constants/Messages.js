071bb8ad977661c619002ab05b5096153fee0408
이태훈 <rhymint@gmail.com>
2023-11-08 19:36:05 +0900

    style : airbnb 스타일가이드에 맞춰 수정

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,22 +1,22 @@
 import SETTINGS from './Settings.js';
 
 const MESSAGES = Object.freeze({
-  inputBalance : '\n구입금액을 입력해 주세요.\n',
-  inputWinningNumbers : '\n당첨 번호를 입력해 주세요.\n',
-  inputBonusNumber : '\n보너스 번호를 입력해 주세요.\n',
+  inputBalance: '\n구입금액을 입력해 주세요.\n',
+  inputWinningNumbers: '\n당첨 번호를 입력해 주세요.\n',
+  inputBonusNumber: '\n보너스 번호를 입력해 주세요.\n',
 
-  outputLottoAmount : '개를 구매했습니다.',
-  outputResultTitle : '\n당첨 통계\n---',
-  
-  outputFifthPrize : `3개 일치 (${SETTINGS.fifthPrize.toLocaleString('ko-KR')}원) - `,
-  outputFourthPrize : `4개 일치 (${SETTINGS.fourthPrize.toLocaleString('ko-KR')}원) - `,
-  outputThirdPrize : `5개 일치 (${SETTINGS.thirdPrize.toLocaleString('ko-KR')}원) - `,
-  outputSecondPrize : `5개 일치, 보너스 볼 일치 (${SETTINGS.secondPrize.toLocaleString('ko-KR')}원) - `,
-  outputFirstPrize : `6개 일치 (${SETTINGS.firstPrize.toLocaleString('ko-KR')}원) - `,
+  outputLottoAmount: '개를 구매했습니다.',
+  outputResultTitle: '\n당첨 통계\n---',
+
+  outputFifthPrize: `3개 일치 (${SETTINGS.fifthPrize.toLocaleString('ko-KR')}원) - `,
+  outputFourthPrize: `4개 일치 (${SETTINGS.fourthPrize.toLocaleString('ko-KR')}원) - `,
+  outputThirdPrize: `5개 일치 (${SETTINGS.thirdPrize.toLocaleString('ko-KR')}원) - `,
+  outputSecondPrize: `5개 일치, 보너스 볼 일치 (${SETTINGS.secondPrize.toLocaleString('ko-KR')}원) - `,
+  outputFirstPrize: `6개 일치 (${SETTINGS.firstPrize.toLocaleString('ko-KR')}원) - `,
   outputReturnRate: `총 수익률은 `,
 
-  suffixAmount : '개',
-  suffixReturnRate : '%입니다.',
+  suffixAmount: '개',
+  suffixReturnRate: '%입니다.',
 });
 
 export default MESSAGES;
