5f3166e3f27cbaa1c2b4f86d5e96d2b65575d0e7
dev-yenya <dev.yenya@gmail.com>
2023-11-08 18:31:33 +0900

    ✅ test(lotto): Pass test for duplicate numbers in Lotto class
    - Ensure that the Lotto class successfully passes the test for detecting and rejecting duplicate numbers in the input array.

--- src/Lotto.js
+++ src/Lotto.js
@@ -15,6 +15,9 @@ class Lotto {
         throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
       }
     });
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
   }
 
   get numbers() {
