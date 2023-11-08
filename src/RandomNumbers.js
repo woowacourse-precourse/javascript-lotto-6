d499c0405166d8e90b727cad21f36574c62edb7a
seoyeon-jung <jungluchia1204@gmail.com>
2023-11-08 21:51:31 +0900

    remove: 필요없는 파일 삭제

--- src/RandomNumbers.js
+++
@@ -1,9 +0,0 @@
-import { Random } from "@woowacourse/mission-utils";
-
-class RandomNumbers {
-  generateNum() {
-    return Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
-  }
-}
-
-export default RandomNumbers;
