df8148177e5e66198934d6ba646c3c97fb151a58
lottt105 <hyobin0601@naver.com>
2023-11-08 13:31:08 +0900

    feat: 로또 오름차순 정렬 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,21 @@
+import Validator from './utils/Validator';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = this.#sortAscending(numbers);
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    Validator.validateLottoLength(numbers);
+    Validator.validateLottoDuplicate(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  #sortAscending(numbers) {
+    return [...numbers].sort((a, b) => a - b);
+  }
 }
 
 export default Lotto;
