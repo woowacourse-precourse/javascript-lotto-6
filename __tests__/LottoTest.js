76a1d74df31086a586a1d80725ad1f7e2d32ca17
kimjaehyuk <jhkim0097@gmail.com>
2023-11-08 16:00:17 +0900

    feat: 테스트코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,26 +1,54 @@
-import App from "../src/App.js";
-import Lotto from "../src/Lotto.js";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from '../src/Lotto.js';
+import { REWARD_INFOS } from '../src/constants.js';
+import Validator from '../src/Vaildator.js';
+import App from '../src/App.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // test("랜덤 로또 생성 테스트.", () =>{
-  //   const input = 6;
-  //   const app = new App();
+  test('로또 번호 출력이 올바른지 확인한다.', () => {
+    //given
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const logSpy = getLogSpy();
 
-  //   const lottos = app.generatRandomLottos(6);
+    //when
+    lotto.printNumbers();
 
-  //   expect(input).toEqual(lottos.length);
-  // });
+    //than
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining('[1, 2, 3, 4, 5, 6]')
+    );
+  });
+
+  test('로또 번호의 당첨 여부 판단이 올바른지 확인한다.', () => {
+    //given
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 10;
+    const topRankReward = REWARD_INFOS.find((info) => info.RANK == 1);
+
+    //when
+    const reward = lotto.findReward(winningNumbers, bonusNumber);
+
+    //than
+    expect(reward).toEqual(topRankReward);
+  });
 });
