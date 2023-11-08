0f58e1cae5c60c66d121fa9f46443c3bfb735f4a
Collection50 <fkaus65@naver.com>
2023-11-08 21:54:42 +0900

    test(LottoMachine): 구매한 로또 배열의 문자열과 개수를 반환하는 buy 메서드의 단위테스트 구현

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import LottoMachine from '../src/LottoMachine';
 import Lotto from '../src/Lotto';
+import { NUMBER, SYMBOLS } from '../src/constants';
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
+};
 
 describe('LottoMachine 테스트', () => {
   test('구매 개수만큼 Lotto 배열을 생성하는 pickLotto 메서드를 테스트한다.', () => {
@@ -16,6 +26,26 @@ describe('LottoMachine 테스트', () => {
       lottoMachine.pickLotto.mockImplementation(() => new Lotto(lotto));
 
       expect(lottoMachine.pickLotto(lottoCount) instanceof Lotto).toBeTruthy();
+    });
+  });
+
+  test('구매한 로또 배열의 문자열과 개수를 반환하는 buy 메서드를 테스트한다.', () => {
+    const lottoMachine = new LottoMachine();
+    const purchaseAmount = 2000;
+
+    const lottoArrays = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
+    ];
+
+    mockRandoms(lottoArrays);
+
+    const { lottos, lottoCount } = lottoMachine.buy(purchaseAmount);
+
+    expect(lottoCount).toBe(purchaseAmount / NUMBER.lottoPurchaseUnit);
+
+    lottoArrays.forEach((lotto) => {
+      expect(lottos).toContain(lotto.join(SYMBOLS.printDivider));
     });
   });
 });
