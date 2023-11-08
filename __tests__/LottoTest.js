78698743f4b3e2a203ca871dc8155a8e4bdd1ddf
soosoo22 <lllllh00@naver.com>
2023-11-08 18:30:51 +0900

    chore: Lotto 클래스 테스트 파일 내용 추가
    로또 번호가 1에서 45사이의 수가 아니면 예외가 발생
    로또 번호가 유효하지 않은 숫자 형식일때 예외가 발생
    로또 구입 금액이 1,000원 단위가 아닐 경우 예외가 발생

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,35 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../src/Lotto.js';
+import buyLotto from '../src/lottogame/BuyLotto.js';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 6개의 당첨번호를 입력해야 합니다.');
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 중복되는 번호가 존재합니다.');
   });
 
-  // 아래에 추가 테스트 작성 가능
+  test('로또 번호가 1에서 45사이의 수가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([100, 2, 3, 4, 5, 5]);
+    }).toThrow('[ERROR] 로또 번호의 숫자 범위는 1에서 45까지의 수입니다.');
+  });
+
+  test('로또 번호가 유효하지 않은 숫자 형식일때 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([1, '-', 3, 4, 5, 5]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 구입 금액이 1,000원 단위가 아닐 경우 예외가 발생한다.', () => {
+    expect(() => {
+      const payMoney = 8100;
+      buyLotto(payMoney);
+    }).toThrow('[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.');
+  });
 });
