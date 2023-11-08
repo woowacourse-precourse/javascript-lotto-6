b7b2e644244cf32a0e6bebcf457112172009ce71
Boeun <boeun0904@naver.com>
2023-11-08 19:51:17 +0900

    feat(App): 앱 실행 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Computer from './Computer.js';
+
 class App {
-  async play() {}
+  async play() {
+    const computer = new Computer();
+    await computer.issueLottoForUserInput();
+    computer.printLotto();
+    await computer.getWinningNumbersrFromUserInput();
+    await computer.getBonusNumberFromUserInput();
+    computer.printLottoWinningStatistics();
+    computer.printTotalRateOfReturn();
+    computer.resetLotto();
+  }
 }
 
 export default App;
