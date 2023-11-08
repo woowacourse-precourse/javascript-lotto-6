ce53efa22fe0c2db3554bfec6029a530f4ea9965
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-08 13:53:06 +0900

    Feat: 상수 분리

---
+++ src/constant/lotto.js
@@ -0,0 +1,8 @@
+export const LOTTO_MAX_NUMBER = 45;
+export const LOTTO_MIN_NUMBER = 1;
+export const LOTTO_PRICE = 1000;
+export const THREE_MATCH_PRICE = 5000;
+export const FOUR_MATCH_PRICE = 10000;
+export const FIVE_MATCH_PRICE = 1500000;
+export const FIVE_AND_BONUS_MATCH_PRICE = 30000000;
+export const SIX_MATCH_PRICE = 2000000000;
