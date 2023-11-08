38e4ff60466d4d759c93240e5bbef84104b74851
MAGHC <windowsapple95@gmail.com>
2023-11-08 21:21:36 +0900

    fix: receiveNumbs 오타수정

--- src/Receiver/Receiver.js
+++ src/Receiver/Receiver.js
@@ -15,7 +15,7 @@ const Receiver = class {
     return answer;
   }
 
-  static async receiveLottomNumbs() {
+  static async receiveLottomNums() {
     const answer = await MissionUtils.Console.readLineAsync(
       `${RANGE_START_NUM}~${RANGE_END_NUM} 사이의 당첨 번호 ${LOTTIO_SIZE}개를 입력해 주세요(입력 시','로 구분해주세요).`,
     );
