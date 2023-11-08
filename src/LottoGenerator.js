4087e1515675aaf96b9d15b5e9865a89c3cafaf2
meunrud <113085963+meunrud@users.noreply.github.com>
2023-11-08 05:54:13 +0000

    feat : 금액 만큼 로또를 반복 생성하는 기능 구현
    + generateRandomNumbers() : 랜덤 숫자 생성 함수 변경

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -6,8 +6,10 @@ class LottoGenerator {
   }
 
   generateRandomNumbers() {
-    return MissionUtils.Random.pickNumberInRange(1, 45);
+    this.LottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return this.LottoNumbers;
   }
+
 
   makeLottoNumber() {
     while (this.LottoNumbers.length < 6) {
@@ -16,5 +18,16 @@ class LottoGenerator {
         this.LottoNumbers.push(randomNumber);
       }
     }
+  }
+
+  purchaseLotto(count) {
+    const lottos = [];
+    for (let i = 0; i < count; i++) {
+      const lotto = new Lotto(this.generateRandomNumbers()); // 각 로또 번호를 Lotto 클래스로 감싸서 생성
+      lottos.push(lotto);
+      Console.print(`[${lotto.getNumbers().join(', ')}]`);
+      // 복사하여 새로운 로또 번호 배열 생성
+    }
+    return lottos;
   }
 }
