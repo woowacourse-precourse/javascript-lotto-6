8f2c05795910c6031f146c403a58ab4bbef1a1b0
irenee-14 <ireneee145@gmail.com>
2023-11-08 16:12:10 +0900

    test: lotto 입력값 테스트 실행

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,8 +10,18 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+	for (let i = 0; i < numbers.length ; i++)
+	{
+		if (isNaN(numbers[i]))
+			throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+		if (numbers.indexOf(numbers[i]) != numbers.lastIndexOf(numbers[i]))
+			throw new Error("[ERROR] 로또 번호는 중복되지 않은 숫자여야 합니다.");
+		if (numbers[i] < 1 || numbers[i] > 45)
+			throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
+		if (Number.isInteger(numbers[i]))
+			throw new Error("[ERROR] 로또 번호는 숫자여야 합니다.");
+	}
   }
-
   // TODO: 추가 기능 구현
 }
 
