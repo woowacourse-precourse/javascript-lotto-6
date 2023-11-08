709ebdfe2deae19407e0118cc1643fbcbc239904
fecapark <fecapark@gmail.com>
2023-11-08 12:45:47 +0900

    feat: 구입 금액 입력 로직 작성 완료

--- src/core/Input.js
+++ src/core/Input.js
@@ -1,3 +1,37 @@
+//@ts-check
+
+import { Console } from "@woowacourse/mission-utils";
+import Assert from "./Assert.js";
+
 export default class Input {
-  constructor() {}
+  /** @type {Assert} */
+  #assert;
+
+  constructor() {
+    this.#assert = new Assert();
+  }
+
+  /**
+   * @returns {Promise<number>}
+   */
+  async askPurchaseValue() {
+    let purchaseValue;
+
+    while (true) {
+      try {
+        const response = await Console.readLineAsync(
+          "구입금액을 입력해 주세요.\n"
+        );
+
+        purchaseValue = parseInt(response);
+        this.#assert.assertPurchaseValue(purchaseValue);
+
+        break;
+      } catch (error) {
+        Console.print(error.message + "\n");
+      }
+    }
+
+    return purchaseValue;
+  }
 }
