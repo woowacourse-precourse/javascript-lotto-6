4a9601fc34cfd4fc1bbae3c0335187e71909c252
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-08 12:45:08 +0900

    style: style convention 적용

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,73 +1,82 @@
-import { COMMON_ERROR_MESSAGE, PURCHASE_AMOUNT_ERROR_MESSAGE, UNIT, TOTAL_LOTTO_NUMBERS, BONUS_NUMBER_ERROR_MESSAGE, WINNING_NUMBERS_ERROR_MESSAGE } from "../constants/constants";
-import CustomError from '../view/CustomError'
+import {
+  COMMON_ERROR_MESSAGE,
+  PURCHASE_AMOUNT_ERROR_MESSAGE,
+  UNIT,
+  TOTAL_LOTTO_NUMBERS,
+  BONUS_NUMBER_ERROR_MESSAGE,
+  WINNING_NUMBERS_ERROR_MESSAGE
+} from '../constants/constants';
+import CustomError from '../view/CustomError';
 
 class Validator {
-  static isMoneyValid(input){
-    const NUMERIC_MONEY = Number(input)
+  static isMoneyValid(input) {
+    const NUMERIC_MONEY = Number(input);
+    console.log(input);
 
-    if (input === '') {
-     throw new CustomError(COMMON_ERROR_MESSAGE.emptyString)
+    if (NUMERIC_MONEY < UNIT) {
+      throw new CustomError(PURCHASE_AMOUNT_ERROR_MESSAGE.underThousand);
     }
+
     if (isNaN(NUMERIC_MONEY)) {
-      throw new CustomError(COMMON_ERROR_MESSAGE.onlyNumber)
+      throw new CustomError(COMMON_ERROR_MESSAGE.onlyNumber);
     }
     if (NUMERIC_MONEY % UNIT) {
-      throw new CustomError(PURCHASE_AMOUNT_ERROR_MESSAGE.wrongUnit)
-    }
-    if (NUMERIC_MONEY < UNIT) {
-      throw new CustomError(PURCHASE_AMOUNT_ERROR_MESSAGE.underThousan)
+      throw new CustomError(PURCHASE_AMOUNT_ERROR_MESSAGE.wrongUnit);
     }
   }
 
   static isNumbersValid(input) {
-    const WINNINGS = String(input).split(',').map(Number)
-    const MY_NUMBERS = WINNINGS.sort((a, b) => a + b).join('')
-    const REMOVE_DUPLICATE_NUMBERS = [...new Set(WINNINGS)].sort((a, b) => a + b).join('')
-    const FILTERED_NUMBERS = WINNINGS.filter(num => num < 1 || num > 45)
-    
+    const WINNINGS = String(input).split(',').map(Number);
+    const MY_NUMBERS = WINNINGS.sort((a, b) => a + b).join('');
+    const REMOVE_DUPLICATE_NUMBERS = [...new Set(WINNINGS)]
+      .sort((a, b) => a + b)
+      .join('');
+    const FILTERED_NUMBERS = WINNINGS.filter((num) => num < 1 || num > 45);
+
     if (!input) {
-      throw new CustomError(COMMON_ERROR_MESSAGE.emptyString)
+      throw new CustomError(COMMON_ERROR_MESSAGE.emptyString);
     }
 
-    if (input[input.length - 1] === ',') {
-      throw new CustomError(WINNING_NUMBERS_ERROR_MESSAGE.detectedLastComma)
-    }
-    
     if (WINNINGS.length > TOTAL_LOTTO_NUMBERS) {
-      throw new CustomError(`${WINNING_NUMBERS_ERROR_MESSAGE.wrongWinningNumber} 게임을 종료합니다.`)
+      throw new CustomError(
+        `${WINNING_NUMBERS_ERROR_MESSAGE.wrongWinningNumber} 게임을 종료합니다.`
+      );
     }
 
     if (MY_NUMBERS !== REMOVE_DUPLICATE_NUMBERS) {
-      throw new CustomError(`${COMMON_ERROR_MESSAGE.detectedDuplicate} 게임을 종료합니다.`)
+      throw new CustomError(
+        `${COMMON_ERROR_MESSAGE.detectedDuplicate} 게임을 종료합니다.`
+      );
     }
 
     if (FILTERED_NUMBERS.length) {
-      throw new CustomError(COMMON_ERROR_MESSAGE.wrongRange)
+      throw new CustomError(COMMON_ERROR_MESSAGE.wrongRange);
     }
   }
 
   static isBonusNumberValid(bonusNumber, winningNumbers) {
-    const WINNINGS = winningNumbers.split(',').map(Number)
-    const NUMERIC_NUMBER = Number(bonusNumber)
-    
-    if (!bonusNumber) {
-      throw new CustomError(COMMON_ERROR_MESSAGE.emptyString)
-    }
+    const WINNINGS =
+      typeof winningNumbers === 'string'
+        ? winningNumbers.split(',').map(Number)
+        : winningNumbers.map(Number);
+    const NUMERIC_NUMBER = Number(bonusNumber);
 
     if (isNaN(bonusNumber)) {
-      throw new CustomError(COMMON_ERROR_MESSAGE.onlyNumber)
+      throw new CustomError(COMMON_ERROR_MESSAGE.onlyNumber);
     }
 
     if (NUMERIC_NUMBER === 0) {
-      throw new CustomError(BONUS_NUMBER_ERROR_MESSAGE.underZero)
+      throw new CustomError(BONUS_NUMBER_ERROR_MESSAGE.underZero);
     }
 
     if (NUMERIC_NUMBER < 1 || NUMERIC_NUMBER > 45) {
-      throw new CustomError(COMMON_ERROR_MESSAGE.wrongRange)
+      throw new CustomError(COMMON_ERROR_MESSAGE.wrongRange);
     }
 
     if (WINNINGS.includes(NUMERIC_NUMBER)) {
-      throw new CustomError(BONUS_NUMBER_ERROR_MESSAGE.duplicatedNumberWithWinningNumbers)
+      throw new CustomError(
+        BONUS_NUMBER_ERROR_MESSAGE.duplicatedNumberWithWinningNumbers
+      );
     }
   }
 }
