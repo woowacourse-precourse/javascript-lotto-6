17dfaf8ba187835a89f9e31f17f7ece0b39695d8
Parksangwoo <psu8430@gmail.com>
2023-11-08 17:41:38 +0900

    feat: Lotto 관련 상수 업데이트

--- src/Constants/LottoContstants.js
+++ src/Constants/LottoContstants.js
@@ -5,8 +5,24 @@ const LOTTO_CONSTANTS = Object.freeze({
 	minLottoNumber: 1,
 });
 
+const LOTTO_RANK_STANDARD = Object.freeze({
+	1: 6,
+	2: 5,
+	3: 5,
+	4: 4,
+	5: 3,
+});
+
+const LOTTO_REWARD_CONSTANTS = Object.freeze({
+	1: 2000000000,
+	2: 30000000,
+	3: 1500000,
+	4: 50000,
+	5: 5000,
+});
+
 const DEFAULT_CONSTANT = Object.freeze({
 	splitStandardText: ',',
 });
 
-export { LOTTO_CONSTANTS, DEFAULT_CONSTANT };
+export { LOTTO_CONSTANTS, LOTTO_RANK_STANDARD, LOTTO_REWARD_CONSTANTS, DEFAULT_CONSTANT };
