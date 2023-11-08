b0f2ed5f85b1e5f66c466111c1629c8817781738
CSKIM <rlackstjq987@gmail.com>
2023-11-08 15:52:55 +0900

    feat: 사용자에게서 입력받고 검증한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 class App {
   async play() {}
+  async play() {
+    const payment = await this.getPayment();
+    const lottoCount = parseInt(payment / this.LOTTO_UNIT);
+  async getPayment() {
+    try {
+      const payment =
+        await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+      this.validatePayment(payment);
+      return payment;
+    } catch (e) {
+      Console.print(e.message);
+      return this.getPayment();
+    }
+  }
+
+  validatePayment(payment) {
+    if (!/^\d+$/g.test(payment)) {
+      throw new Error("[ERROR] 구입 금액은 숫자로 입력하셔야 합니다.");
+    } else if (payment <= 0) {
+      throw new Error("[ERROR] 구입 금액은 0원 이상 입력하셔야 합니다.");
+    } else if (payment % this.LOTTO_UNIT !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1000원으로 나누어져야 합니다.");
+    }
+  }
 }
 
 export default App;
---
+++ src/constant/LottoRules.js
@@ -0,0 +1,4 @@
+export const MIN_NUMBER = 1;
+export const MAX_NUMBER = 45;
+export const INPUT_LENGTH = 6;
+export const LOTTO_UNIT = 1000;
