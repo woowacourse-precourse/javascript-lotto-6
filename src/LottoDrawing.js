30b0b7ebc93ceaa5059609672daaec30a0bd65c8
이동주 <leednj0113@naver.com>
2023-11-08 10:26:08 +0900

    feat(LottoDrawing): 당첨 로또 번호를 입력

--- src/LottoDrawing.js
+++ src/LottoDrawing.js
@@ -5,6 +5,12 @@ import Lotto from "./Lotto.js";
 class LottoDrawing {
   #winnigLotto;
   #bonusNumber;
+  async inputWinningLotto() {
+    try {
+      const input = await Console.readLineAsync(
+        "\n당첨 번호를 입력해 주세요.\n"
+      );
+  }
 }
 
 export default LottoDrawing;
