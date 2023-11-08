d584da724471da2462153cbe0840525604f5102b
Hamin <ohm55545@naver.com>
2023-11-08 15:51:59 +0900

    test(lotto): TODO 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,6 +3,7 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
+    this.checkDuplicate(numbers);
     this.#numbers = numbers;
   }
 
@@ -13,6 +14,12 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  checkDuplicate(numbers) {
+    const set = new Set(numbers);
+    if (numbers.length !== set.length) {
+      throw new Error("[ERROR] 로또 번호에 중복이 있습니다.");
+    }
+  }
 }
 
 export default Lotto;
