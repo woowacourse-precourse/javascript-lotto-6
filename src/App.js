a0f6fde3c00df148b66d3b6dd0af7bd72869c477
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-11-08 21:43:33 +0900

    feat : 게임 시작 부분 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { InputConsole } from "./view/inputConsole";
+import { MissionUtils, Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
+import { Game } from "./Game";
+
 class App {
-  async play() {}
+  async play() {
+
+    const price = await InputConsole.inputPrice();
+    const winningNumbers = await InputConsole.inputWinningNumberList();
+    const bonusNumber = await InputConsole.inputBonusNumber();
+
+    const game = new Game(price, winningNumbers, bonusNumber);
+
+    game.start();
+
+  }
 }
 
 export default App;
