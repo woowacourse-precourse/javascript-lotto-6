95b8a6ef3024ec3e00632cdd40138d8e4ca787cf
bada308 <203190@jnu.ac.kr>
2023-11-08 16:03:10 +0900

    feat: 입력 검증 구현 - 구매 금액, 당첨 번호, 보너스 번호 검증 - 예외 발생 시 해당 입력부터 다시 실행
    feat: Validate 합쳐

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import Validator from "./utils/Validator.js";
+import Input from "./utils/Input.js";
 class App {
-  async play() {}
+  async play() {
+    const purchaseAmount = await this.#getValidatedPurchaseAmout();
+    const winningNumber = await this.#getValidatedWinningNumber();
+    const bonusNumber = await this.#getValidatedBonusNumber(winningNumber);
+  }
+
+  async #getValidatedPurchaseAmout() {
+    try {
+      const purchaseAmount = await Input.purchase();
+      Validator.purchase(purchaseAmount);
+      return purchaseAmount;
+    } catch (e) {
+      Output.printError(e.message);
+      return this.#getValidatedPurchaseAmout();
+    }
+  }
+
+  async #getValidatedWinningNumber() {
+    try {
+      const winningNumber = await Input.winningNumber();
+      Validator.winningNumbers(winningNumber);
+      return winningNumber;
+    } catch (e) {
+      Output.printError(e.message);
+      return this.#getValidatedWinningNumber();
+    }
+  }
+
+  async #getValidatedBonusNumber(winningNumber) {
+    try {
+      const bonusNumber = await Input.bonusNumber();
+      Validator.bonusNumber(winningNumber, bonusNumber);
+      return bonusNumber;
+    } catch (e) {
+      Output.printError(e.message);
+      return this.#getValidatedBonusNumber(winningNumber);
+    }
+  }
 }
 
 export default App;
