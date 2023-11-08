6f016a33d04599d0cea7778526c0b7f06f348a07
yooshnn <yooshnn@kakao.com>
2023-11-08 18:21:52 +0900

    test(ControlsTest.js): update test to deal with throw-catch

--- __tests__/ControlsTest.js
+++ __tests__/ControlsTest.js
@@ -22,40 +22,38 @@ describe('컨트롤러 테스트', () => {
     jest.restoreAllMocks();
   });
 
-  test('잘못된 금액 입력 예외 처리', async () => {
-    mockQuestions(['a']);
-    await expect(async () => Controls.getBet()).rejects.toThrow('[ERROR]');
-  });
-
-  test.each([[['a']], [['-5000']], [['303030']]])(
+  test.each([[['a', '1000']], [['-5000', '1000']], [['303030', '1000']]])(
     '잘못된 금액 입력 예외 처리',
     async (inputs) => {
+      const logSpy = getLogSpy();
       mockQuestions(inputs);
-      await expect(async () => await Controls.getBet()).rejects.toThrow(
-        '[ERROR]'
-      );
+
+      await (async () => await Controls.getBet())();
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
     }
   );
 
   test.each([
-    [['1,2,3,4,5', '45']],
-    [['1,2,3,4,5,a', '45']],
-    [['1,2,3,4,5,0', '45']],
+    [['1,2,3,4,5', '1,2,3,4,5,6', '45']],
+    [['1,2,3,4,5,a', '1,2,3,4,5,6', '45']],
+    [['1,2,3,4,5,0', '1,2,3,4,5,6', '45']],
   ])('잘못된 당첨 번호 예외 처리', async (inputs) => {
+    const logSpy = getLogSpy();
     mockQuestions(inputs);
-    await expect(async () => await Controls.writeWin()).rejects.toThrow(
-      '[ERROR]'
-    );
+
+    await (async () => await Controls.writeWin())();
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
   });
 
   test.each([
-    [['1,2,3,4,5,6', '1']],
-    [['1,2,3,4,5,6', '0']],
-    [['1,2,3,4,5,6', '!']],
+    [['1,2,3,4,5,6', '1', '1,2,3,4,5,6', '7']],
+    [['1,2,3,4,5,6', '0', '1,2,3,4,5,6', '7']],
+    [['1,2,3,4,5,6', '!', '1,2,3,4,5,6', '7']],
   ])('잘못된 보너스 번호 예외 처리', async (inputs) => {
+    const logSpy = getLogSpy();
     mockQuestions(inputs);
-    await expect(async () => await Controls.writeWin()).rejects.toThrow(
-      '[ERROR]'
-    );
+
+    await (async () => await Controls.writeWin())();
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('[ERROR]'));
   });
 });
