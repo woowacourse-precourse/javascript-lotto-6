e45d4e34734d54be0c467aff67d830116702ebb9
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-11-08 19:57:58 +0900

    chore: ESLint, Prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'max-depth': ['error', 2],
+    'max-lines-per-function': ['error', { max: 15 }],
+    'no-else-return': 'error',
+  },
+};
