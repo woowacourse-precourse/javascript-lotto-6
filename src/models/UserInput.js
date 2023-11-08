3041bf61d7c98e7a7ea2c9762d94e37c956b9ba1
HyunJaeyeon <bomul0109@gmail.com>
2023-11-08 13:02:58 +0900

    feat: 입력값 받기 [입력]
    - 구입금액, 당첨번호, 보너스번호 멘트 상수 처리

---
+++ src/models/UserInput.js
@@ -0,0 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+import USER_INPUT from './messages.js';
+
+class UserInput {
+  #line;
+
+  async #getLine(message) {
+    this.#line = await Console.readLineAsync(message);
+    return this.#line;
+  }
+
+  getTotalPrice = () => this.#getLine(USER_INPUT.totalPrice);
+
+  getWinningNumbers = () => this.#getLine(USER_INPUT.winningNumbers);
+
+  getBonusNumber = () => this.#getLine(USER_INPUT.bonusNumber);
+}
+export default UserInput;
