92b33b44537b564703d150f00922ebb8feb46dec
박지윤 <tkrhkfld123@naver.com>
2023-11-08 13:32:37 +0900

    style: 들여쓰기 하기

--- src/App.js
+++ src/App.js
@@ -40,27 +40,27 @@ class App {
   }
 
   // 2. 로또 구입하기: 구매한 로또 티켓 수 출력
-displayPurchasedLottos(lottos) {
-  console.log(lottos)
-  const lottosCount = lottos.length;
-  Console.print(`${lottosCount}개를 구매했습니다.`);
+  displayPurchasedLottos(lottos) {
+    console.log(lottos)
+    const lottosCount = lottos.length;
+    Console.print(`${lottosCount}개를 구매했습니다.`);
 
-  // 2. 로또 구입하기: 로또 번호 목록 출력
-  const fixedNumbers = [
-    [8, 21, 23, 41, 42, 43],
-    [3, 5, 11, 16, 32, 38],
-    [7, 11, 16, 35, 36, 44],
-    [1, 8, 11, 31, 41, 42],
-    [13, 14, 16, 38, 42, 45],
-    [7, 11, 30, 40, 42, 43],
-    [2, 13, 22, 32, 38, 45],
-    [1, 3, 5, 14, 22, 45],
-  ];
+    // 2. 로또 구입하기: 로또 번호 목록 출력
+    const fixedNumbers = [
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ];
 
-  fixedNumbers.forEach(numbers => {
-    Console.print(`[${numbers.join(', ')}]`);
-  });
-}
+    fixedNumbers.forEach(numbers => {
+      Console.print(`[${numbers.join(', ')}]`);
+    });
+  }
 }
 
 export default App;
