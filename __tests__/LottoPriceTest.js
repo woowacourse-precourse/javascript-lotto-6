e4bdcf54f5426b7e1f089fc654d00926aa246c96
Jinhyo Park <vkstk0131@yonsei.ac.kr>
2023-11-08 21:02:51 +0900

    test(LottoPrice): 구입 금액이 숫자 타입이 맞는지

--- __tests__/LottoPriceTest.js
+++ __tests__/LottoPriceTest.js
@@ -6,4 +6,10 @@ describe('구입 금액 테스트', () => {
   ])('공백이 없는지', (lottoPrice) => {
     expect(() => { Validations.hasSpace(lottoPrice) }).toThrow('[ERROR] 공백 없이 입력해 주세요.');
   });
+
+  test.each([
+    ['1000s', '2s000', 's30000']
+  ])('숫자가 맞는지', (lottoPrice) => {
+    expect(() => { Validations.isNumber(lottoPrice) }).toThrow('[ERROR] 숫자만 입력해 주세요.');
+  });
 });
