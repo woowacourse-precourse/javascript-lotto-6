7f557bbe745183d6365685d4a7fc3f9a876af1bd
FirstBright <mipo8890@gmail.com>
2023-11-08 17:25:15 +0900

    :sparkles: feat: 당첨 번호 로또범위 확인 추가

--- src/error/WinningNumberError.js
+++ src/error/WinningNumberError.js
@@ -9,6 +9,10 @@ function checkWinningNumber(winningNumbers){
     if (winningNumbers.some((el) => isNaN(el))) {
         throw new Error(ERROR.IS_NAN);
     }
+
+    if (winningNumbers.some((el) => el > LOTTO_MAX || el < LOTTO_MIN)) {
+        throw new Error(ERROR.RANGE);
+    }
 }
 
 export default checkWinningNumber;
