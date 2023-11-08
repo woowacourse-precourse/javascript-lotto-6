8d0a5ef086ac7617d7e76c0872c13bd7fbf3202b
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2023-11-08 23:37:34 +0900

    constant.js 파일 구현

--- src/Constants.js
+++ src/Constants.js
@@ -1,9 +1,30 @@
-// 정기적으로 출력되는 문구를 변수로 저장
-const GAME_MESSAGE = {
-  purchaseAmount: "구입 금액을 입력하세요\n",
-  guessNumbers: "당첨 번호를 입력하세요\n",
-  bonusNumber: "보너스 번호를 입력하세요\n",
-  static: "당첨 통계\n---",
-};
+export const ERROR_INVALID_NUMBER =
+  "[ERROR] 구매 금액은 유효한 숫자여야 합니다.";
+export const ERROR_NOT_IN_1000_UNITS =
+  "[ERROR] 구매 금액은 1,000원 단위이어야 합니다.";
+export const ERROR_INVALID_LOTTO_NUMBER_COUNT =
+  "[ERROR] 로또 번호는 6개여야 합니다.";
+export const ERROR_DUPLICATE_LOTTO_NUMBERS =
+  "[ERROR] 로또 번호는 중복되지 않은 숫자로 이루어져야 합니다.";
 
-module.exports = { GAME_MESSAGE };
+export const LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.\n";
+export const WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.\n";
+export const BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.\n";
+
+export const LOTTO_STATISTICS_HEADER = "당첨 통계\n---";
+export const PRIZE_3_MATCH = "3개 일치 (5,000원) - ";
+export const PRIZE_4_MATCH = "4개 일치 (50,000원) - ";
+export const PRIZE_5_MATCH = "5개 일치 (1,500,000원) - ";
+export const PRIZE_5_MATCH_WITH_BONUS =
+  "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+export const PRIZE_6_MATCH = "6개 일치 (2,000,000,000원) - ";
+export const TOTAL_PROFIT_PERCENTAGE = "총 수익률은 ";
+
+export const LOTTO_TICKET_PRICE = 1000;
+export const LOTTO_NUMBERS_COUNT = 6;
+
+export const MATCH_3 = 5_000;
+export const MATCH_4 = 50_000;
+export const MATCH_5 = 1_500_000;
+export const MATCH_5_WITH_BONUS = 30_000_000;
+export const MATCH_6 = 2_000_000_000;
