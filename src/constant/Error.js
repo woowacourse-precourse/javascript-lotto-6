76e016a51362e88c33c657612ad8251a27de41a4
pakxe <pigkill40@naver.com>
2023-11-08 16:42:54 +0900

    rename: 보너스 번호에 대한 오류 문구가 추가될 수 있으므로 명확한 의미로 프로퍼티명 변경

--- src/constant/Error.js
+++ src/constant/Error.js
@@ -8,7 +8,7 @@ const ERROR = Object.freeze({
   range: `번호는 ${LOTTO.minLottoNum} ~ ${LOTTO.maxLottoNum} 사이의 값으로 입력해주세요.`,
   count: `당첨 번호는 ${LOTTO.count}개로 입력해주세요.`,
   duplicated: '로또 번호는 중복이 없도록 입력해주세요.',
-  bonusNumber: '보너스 번호가 중복되지 않도록 입력해주세요.',
+  bonusNumberDup: '보너스 번호가 중복되지 않도록 입력해주세요.',
   moneyRange: `금액은 ${LOTTO.minMoney} ~ ${LOTTO.maxMoney} 사이의 값으로 입력해주세요.`,
   negativeMoney: '금액은 양수로 입력해주세요.',
 });
