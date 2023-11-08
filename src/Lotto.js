6c3084757e10bc706713557f7e84160a28b1aba8
lucia0929 <thenum1developer@gamil.com>
2023-11-08 20:15:07 +0900

    feat : (C-2-1) 구입 금액에 따른 로또 발행 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,32 @@
+import { Random } from "@woowacourse/mission-utils"
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
+  }
+
+  get numbers() {
+    return this.#numbers;
+  }
+
+  static generateNumbers() {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+      numbers.add(Random.pickNumberInRange(1, 45));
+    }
+    return [...numbers].sort((a, b) => a - b);
+  }
+
+  static generateMultipleLottos(amount) {
+    const numberOfLottos = Math.floor(amount / 1000);
+    const lottos = [];
+    for (let i = 0; i < numberOfLottos; i++) {
+      lottos.push(new Lotto(Lotto.generateNumbers()));
+    }
+    return lottos;
   }
 
   #validate(numbers) {
@@ -11,8 +34,6 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
