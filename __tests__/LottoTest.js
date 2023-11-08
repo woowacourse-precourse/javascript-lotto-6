7cab15aece576ebcb12465b15f6eb9a29d04378c
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-08 14:05:19 +0900

    Test(Lotto): test on sorting lotto number in birth
    test for checking ascending sort on lotto numbers.

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,12 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import Lotto from '../src/domain/Lotto.js';
+import { outputLottoNummbers } from '../src/view/outputView.js';
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -15,4 +23,13 @@ describe('로또 클래스 테스트', () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test('로또 번호가 오름차순으로 정렬된다.', () => {
+    const testLotto = new Lotto([6, 5, 4, 2, 3, 1]);
+    const logSpy = getLogSpy();
+    outputLottoNummbers([testLotto]);
+    const ascendingLottoNumber = '[1, 2, 3, 4, 5, 6]';
+    expect(() => {
+      expect(logSpy).toBe(ascendingLottoNumber);
+    });
+  });
 });
