aee5016efbeda58b5e5e221dff495b99a94fd53a
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 16:10:03 +0900

    fix: 금액을 입력하지 않았을 때 다른 문구가 뜨는 문제 해결

--- src/validation/validateMoney.js
+++ src/validation/validateMoney.js
@@ -1,13 +1,12 @@
 import { MONEY_ERROR } from "../errorMessages/errorMessage";
 
-const regexOnlyNumbers = /^\d+$/;
-
 const validateMoney = (money) => {
+  if (!money) throw new Error(MONEY_ERROR.none);
+
+  const regexOnlyNumbers = /^\d+$/;
   if (!regexOnlyNumbers.test(money)) throw new Error(MONEY_ERROR.type);
 
   const moneyToNumber = parseInt(money);
-
-  if (!moneyToNumber) throw new Error(MONEY_ERROR.none);
   if (moneyToNumber < 1000) throw new Error(MONEY_ERROR.over1000);
   if (moneyToNumber % 1000 !== 0) throw new Error(MONEY_ERROR.divide1000);
 };
