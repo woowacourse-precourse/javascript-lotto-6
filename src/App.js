ebea5548374a52084f15de288c8e6cf7d56620c1
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2023-11-08 23:45:25 +0900

    app.js 파일 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import {
+  countResults,
+  getValidPurchaseAmount,
+  getWinningNumbersAndBonus,
+  printStatistics,
+  purchaseLottos,
+} from "./LottoUtils.js";
+import * as CONSTANTS from "./Constants.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this.purchaseAmount = 0;
+    this.lottos = [];
+    this.winningResult = { winningNums: [], bonusNum: 0 };
+    this.statistics = {
+      first_prize: 0,
+      second_prize: 0,
+      third_prize: 0,
+      fourth_prize: 0,
+      fifth_prize: 0,
+      totalPrice: 0,
+    };
+  }
+  async play() {
+    this.purchaseAmount = await getValidPurchaseAmount();
+    const lottoCnt = this.purchaseAmount / CONSTANTS.LOTTO_TICKET_PRICE;
+    this.lottos = purchaseLottos(lottoCnt);
+    this.winningResult = await getWinningNumbersAndBonus();
+    this.statistics = countResults(
+      this.lottos,
+      this.winningResult.winningNums,
+      this.winningResult.bonusNum,
+      this.statistics
+    );
+    printStatistics(this.purchaseAmount, this.statistics);
+  }
 }
 
 export default App;
