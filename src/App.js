8a78af0527df8972b933f20a353ce12827cce128
Sohye <saeyan2477@gmail.com>
2023-11-08 20:19:07 +0900

    feat: View 클래스 구현
    - View 클래스 구현
      - 입력받는 뷰와 출력하는 뷰를 구분
    - App.js에 기존 로직 제거 후 View 클래스 연결

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import { Console, Random } from '@woowacourse/mission-utils';
-import { Validation } from './Validation.js';
-import { LOTTO_RULE, INPUT_MESSAGES, ERROR_MESSAGES } from './Constants.js';
-import { CustomError } from './CustomError.js';
-import LottoList from './LottoList.js';
-
-const LENGTH = LOTTO_RULE.LENGTH;
-const { MIN, MAX } = LOTTO_RULE.RANGE;
+import LottoGame from './LottoGame.js';
 
 class App {
-  #lottoApp;
-
   constructor() {
-    this.#lottoApp = new LottoList();
+    this.lottoGame = new LottoGame();
   }
 
   async play() {
-    await this.printLottoList();
-    const inputNumbers = await this.inputNumbers();
-    await this.inputBonusNumber(inputNumbers);
-  }
-
-  async printLottoList() {
-    const lottoList = await this.#lottoApp.makeLottoList();
-    Console.print(lottoList);
-  }
-
-  async inputNumbers() {
-    const input = await Console.readLineAsync(INPUT_MESSAGES.LOTTO_NUMBER);
-    return input.split(',').map((num) => Number(num.trim()));
-  }
-
-  async inputBonusNumber() {
-    const inputBonusNumber = await Console.readLineAsync(
-      INPUT_MESSAGES.BONUS_NUMBER,
-    );
-    const bonusNumber = Number(inputBonusNumber);
-
-    return bonusNumber;
+    await this.lottoGame.playGame()
   }
 }
 
