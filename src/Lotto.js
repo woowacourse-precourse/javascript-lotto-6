f22148fa43539f6b07e20f1ef2e33a060b758d47
Sohye <saeyan2477@gmail.com>
2023-11-08 18:12:23 +0900

    feat: Lotto 클래스 구현 - 로또 번호 일치 갯수
    - getMatchingCount 에서 로또 번호와 당첨 번호를 대조하여 몇 개가 일치하는지 확인 할 수 있다

--- src/Lotto.js
+++ src/Lotto.js
@@ -47,6 +47,13 @@ class Lotto {
   getNumbers() {
     return this.#numbers;
   }
+  
+  getMatchingCount(winningNumbers) {
+    const matchedNumbers = winningNumbers.filter((winningNumber) =>
+      this.#numbers.includes(Number(winningNumber)),
+    );
+    return matchedNumbers.length;
+  }
 }
 
 export default Lotto;
