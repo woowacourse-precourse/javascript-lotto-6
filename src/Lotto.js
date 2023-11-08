5ca574b6b3aa859fa390e2294a524d5e74915ba8
pongpongie <arevolvera@gmail.com>
2023-11-08 18:46:20 +0900

    feat: add Lotto.js and LottoGame.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,37 @@
+import { Random } from '@woowacourse/mission-utils';
+import { NUMBERS_COUNT, MAX_NUMBER, MIN_NUMBER } from './constants/Constants';
+import { areNumbersInRange } from './utils/Utils';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
+    this.#validate();
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  #validate() {
+    if (this.#numbers.length !== NUMBERS_COUNT) {
+      throw new Error(`[ERROR] 로또 번호는 ${NUMBERS_COUNT}개여야 합니다.`);
+    }
+
+    if (new Set(this.#numbers).size !== NUMBERS_COUNT) {
+      throw new Error('[ERROR] 로또 번호는 중복될 수 없습니다.');
+    }
+
+    if (!areNumbersInRange(this.#numbers)) {
+      throw new Error(`[ERROR] 로또 번호는 지정된 범위 내에 있어야 합니다.`);
     }
   }
 
-  // TODO: 추가 기능 구현
+  static generateRandom() {
+    const numbers = Random.pickUniqueNumbersInRange(
+      MIN_NUMBER,
+      MAX_NUMBER,
+      NUMBERS_COUNT,
+    );
+    return new Lotto(numbers);
+  }
 }
 
 export default Lotto;
---
+++ src/LottoGame.js
@@ -0,0 +1,84 @@
+import { Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
+import {
+  MAX_NUMBER,
+  MIN_NUMBER,
+  NUMBERS_COUNT,
+  TICKET_PRICE,
+  PRIZE_INFO,
+} from './constants/Constants';
+import { isValidAmount } from './utils/Utils';
+
+class LottoGame {
+  #lottos;
+
+  #winningNumbers;
+
+  #bonusNumber;
+
+  constructor() {
+    this.#lottos = [];
+    this.#winningNumbers = [];
+    this.#bonusNumber = null;
+  }
+
+  purchaseTickets(amount) {
+    if (!isValidAmount(amount)) {
+      throw new Error('[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.');
+    }
+
+    const numberOfTickets = amount / TICKET_PRICE;
+    for (let i = 0; i < numberOfTickets; i += 1) {
+      this.#lottos.push(Lotto.generateRandom());
+    }
+  }
+
+  drawWinningNumbers() {
+    this.#winningNumbers = Random.pickUniqueNumbersInRange(
+      MIN_NUMBER,
+      MAX_NUMBER,
+      NUMBERS_COUNT,
+    );
+    do {
+      this.#bonusNumber = Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    } while (this.#winningNumbers.includes(this.#bonusNumber));
+  }
+
+  calculateResults() {
+    const results = this.#lottos.map((lotto) => {
+      const matchedNumbers = lotto
+        .getNumbers()
+        .filter((number) => this.#winningNumbers.includes(number)).length;
+      const hasBonus = lotto.getNumbers().includes(this.#bonusNumber);
+
+      // Determine the rank based on matched numbers and bonus number
+      const rank = this.#determineRank(matchedNumbers, hasBonus);
+
+      return {
+        numbers: lotto.getNumbers(),
+        matchedNumbers,
+        rank,
+        prize: PRIZE_INFO[rank]?.prize || 0,
+      };
+    });
+
+    return results;
+  }
+
+  #determineRank(matchedNumbers, hasBonus) {
+    switch (matchedNumbers) {
+      case 6:
+        return 'FIRST';
+      case 5:
+        return hasBonus ? 'SECOND' : 'THIRD';
+      case 4:
+        return 'FOURTH';
+      case 3:
+        return 'FIFTH';
+      default:
+        return null;
+    }
+  }
+}
+
+export default LottoGame;
