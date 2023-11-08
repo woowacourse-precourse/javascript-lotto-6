1b5f8ccccdc5e8f472a73d018e47c9d4ab49217e
meunrud <113085963+meunrud@users.noreply.github.com>
2023-11-08 05:50:37 +0000

    feat : 당첨 번호 검증 함수 추가 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,19 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
+    }
+    // eslint-disable-next-line no-restricted-syntax
+    for (const number of numbers) {
+      if (typeof number !== 'number' || isNaN(number)) {
+        throw new Error('[ERROR] 로또 번호는 숫자여야 합니다.');
+      }
+    }
+    if (numbers.some((item) => typeof item === 'string')) {
+      throw new Error('[ERROR]');
+    }
   }
 
   // TODO: 추가 기능 구현
