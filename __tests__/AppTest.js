c632bb3671d1343de4c763fbcc366a4ae1abb50e
sejineeee <sejinee0918@gmail.com>
2023-11-08 16:46:27 +0900

    refactor: 사용하지 않는 테스트 제거

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -1,19 +1,6 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
 import App from '../src/App';
 
 describe('로또 클래스 테스트', () => {
-  beforeEach(() => {});
-
-  test.each([NaN, 0, 7900])(
-    '구매 금액이 숫자가 아니거나 0 또는 1000원 단위가 아니면 예외가 발생한다.',
-    (input) => {
-      const app = new App();
-      expect(() => {
-        app.validateAskPurchaseAmount(input);
-      }).toThrow('[ERROR]');
-    }
-  );
-
   test('createMyLottoList', () => {
     const app = new App();
     const purchaseAmount = 8000;
