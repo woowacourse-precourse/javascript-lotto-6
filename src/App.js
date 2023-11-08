04b0397387b8de4b91eb538f3ac2e4123f81cc9b
yhyem <yoonhyemin0120@gmail.com>
2023-11-08 23:42:30 +0900

    refactor: 전역 변수 분리

--- src/App.js
+++ src/App.js
@@ -2,34 +2,34 @@ import { Console, Random } from "@woowacourse/mission-utils";
 import Validate from "./Validate.js";
 import Lotto from "./Lotto.js";
 
-const validate = new Validate();
+const VALIDATOR = new Validate();
+const RESULT_LIST = [
+  { rank: 5, count: 0, price: 5000, describe: "3개 일치 (5,000원) - " },
+  { rank: 4, count: 0, price: 50000, describe: "4개 일치 (50,000원) - " },
+  {
+    rank: 3,
+    count: 0,
+    price: 1500000,
+    describe: "5개 일치 (1,500,000원) - ",
+  },
+  {
+    rank: 2,
+    count: 0,
+    price: 30000000,
+    describe: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
+  },
+  {
+    rank: 1,
+    count: 0,
+    price: 2000000000,
+    describe: "6개 일치 (2,000,000,000원) - ",
+  },
+];
 class App {
   inputPrice = 0;
   LottoList = [];
   prizeNumber = [];
   bonusNumber = 0;
-  resultList = [
-    { rank: 5, count: 0, price: 5000, describe: "3개 일치 (5,000원) - " },
-    { rank: 4, count: 0, price: 50000, describe: "4개 일치 (50,000원) - " },
-    {
-      rank: 3,
-      count: 0,
-      price: 1500000,
-      describe: "5개 일치 (1,500,000원) - ",
-    },
-    {
-      rank: 2,
-      count: 0,
-      price: 30000000,
-      describe: "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
-    },
-    {
-      rank: 1,
-      count: 0,
-      price: 2000000000,
-      describe: "6개 일치 (2,000,000,000원) - ",
-    },
-  ];
 
   async start() {
     while (true) {
@@ -37,7 +37,7 @@ class App {
         this.inputPrice = await Console.readLineAsync(
           "구입금액을 입력해 주세요.\n"
         );
-        validate.priceValidate(this.inputPrice);
+        VALIDATOR.priceValidate(this.inputPrice);
         break;
       } catch (e) {
         Console.print(e);
@@ -67,7 +67,7 @@ class App {
     while (true) {
       try {
         prize = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
-        validate.prizeValidate(prize);
+        VALIDATOR.prizeValidate(prize);
         break;
       } catch (e) {
         Console.print(e);
@@ -82,7 +82,7 @@ class App {
     while (true) {
       try {
         bonus = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
-        validate.bonusValidate(bonus);
+        VALIDATOR.bonusValidate(bonus);
         break;
       } catch (e) {
         Console.print(e);
@@ -95,13 +95,13 @@ class App {
     this.LottoList.map((lotto) => {
       new Lotto(lotto).getWinningList(
         this.prizeNumber,
-        this.resultList,
+        RESULT_LIST,
         this.bonusNumber
       );
     });
 
     Console.print("당첨 통계\n---");
-    this.resultList.map((result) =>
+    RESULT_LIST.map((result) =>
       Console.print(result.describe + result.count + "개")
     );
   }
@@ -109,7 +109,7 @@ class App {
   getProfitRate() {
     let profitRate = 0;
     let sum = 0;
-    this.resultList.map((result) => (sum += result.price * result.count));
+    RESULT_LIST.map((result) => (sum += result.price * result.count));
 
     profitRate = ((sum / this.inputPrice) * 100).toFixed(1);
     Console.print("총 수익률은 " + profitRate + "%입니다.");
