d1c64299c6cf164050f42d515540ec883913124a
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-08 19:26:53 +0900

    fix:랜덤 숫자 생성 API 미션에서 주어진 것으로 수정

--- src/utils/generateRandomNumbers.js
+++ src/utils/generateRandomNumbers.js
@@ -1,14 +1,7 @@
 import { Random } from '@woowacourse/mission-utils';
 
 export const generateRandomNumbers = (min, max, size) => {
-  let array = [];
-  let set = new Set();
-  while (set.size !== size) {
-    const number = Random.pickNumberInRange(min, max);
-    array.push(number);
-    set = new Set([...array]);
-    array = Array.from(set);
-  }
+  const number = Random.pickUniqueNumbersInRange(min, max, size);
 
-  return array;
+  return number;
 };
