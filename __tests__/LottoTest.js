8562648aa8c2a3c3f2237ae7a2802ef9327828c8
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 14:15:00 +0900

    test: 로또 번호 확인 테스트 코드 작성 및 유효성 관련 테스트 삭제

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,22 +1,7 @@
 import Lotto from '../src/Lotto.js';
+import LottoResult from '../src/LottoResult.js';
 
 describe('로또 클래스 테스트', () => {
-  test('빈 값인지 확인, 미통과시 예외 발생', () => {
-    //given
-    const lottoNumber = [];
-
-    //when & then
-    expect(() => validateLotto(lottoNumber)).toThrow(ERROR.type);
-  });
-
-  test('번호의 타입이 숫자인지 확인, 미통과시 예외 발생', () => {
-    //given
-    const lottoNumber = ['a', 1, 4, 5, 10, 12];
-
-    //when & then
-    expect(() => validateLotto(lottoNumber)).toThrow(ERROR.type);
-  });
-
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
@@ -30,11 +15,19 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  test('번호 범위가 1~45인지 확인, 미통과시 예외 발생', () => {
-    //given
-    const lottoNumber = [1, 4, 5, 10, 12, 46];
+  test('로또 번호 확인 테스트', () => {
+    const testLotto = new Lotto([1, 2, 3, 4, 5, 6]);
 
-    //when & then
-    expect(() => validateLotto(lottoNumber)).toThrow(ERROR.type);
+    expect(testLotto.determineResult([1, 2, 3, 4, 5, 6], 7)).toEqual(LottoResult.CASE_6);
+
+    expect(testLotto.determineResult([1, 2, 3, 4, 5, 7], 6)).toEqual(LottoResult.CASE_5_BONUS);
+
+    expect(testLotto.determineResult([1, 2, 3, 4, 5, 8], 7)).toEqual(LottoResult.CASE_5);
+
+    expect(testLotto.determineResult([1, 2, 3, 4, 8, 9], 7)).toEqual(LottoResult.CASE_4);
+
+    expect(testLotto.determineResult([1, 2, 3, 8, 9, 10], 7)).toEqual(LottoResult.CASE_3);
+
+    expect(testLotto.determineResult([1, 2, 8, 9, 10, 11], 7)).toEqual(undefined);
   });
 });
