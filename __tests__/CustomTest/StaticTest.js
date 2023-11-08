45f12db207c21730effa78393704ad2f09d85ca8
JonghyunLEE12 <najo7609@gmail.com>
2023-11-08 14:47:40 +0900

    Test : [ 테스트케이스 수정 ]

--- __tests__/CustomTest/StaticTest.js
+++ __tests__/CustomTest/StaticTest.js
@@ -1,75 +1,41 @@
 /* eslint-disable max-lines-per-function */
-import { MissionUtils } from '@woowacourse/mission-utils';
 import LottoUtill from '../../src/domain/utils/LottoUtill.js';
-import OutputView from '../../src/domain/views/OutputView.js';
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
-  return logSpy;
-};
 
 describe('LottoUtill Class 테스트', () => {
   test('당첨 통계 테스트', () => {
-    const Output = new OutputView();
     const lottoUtill = new LottoUtill();
 
     const userNumber = [[1, 2, 3, 4, 5, 6]];
     const winNumber = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 7;
 
-    const logSpy = getLogSpy();
-
-    const logs = [
-      '3개 일치 (5,000원) - 0개',
-      '4개 일치 (50,000원) - 0개',
-      '5개 일치 (1,500,000원) - 0개',
-      '5개 일치, 보너스 볼 일치 (30,000,000원) - 0개',
-      '6개 일치 (2,000,000,000원) - 1개',
-    ];
-
     const staticObject = lottoUtill.checkLottoCorrect(
       userNumber,
       winNumber,
       bonusNumber,
     );
-    Output.lottoStatic(staticObject);
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+
+    const expected = { 3: 0, 4: 0, 5: 0, 6: 1, bonus: 0 };
+    expect(staticObject).toStrictEqual(expected);
   });
 
   test('보너스 번호 테스트', () => {
-    const Output = new OutputView();
     const lottoUtill = new LottoUtill();
 
     const userNumber = [[1, 2, 3, 4, 5, 7]];
     const winNumber = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 7;
-
-    const logSpy = getLogSpy();
-
-    const logs = [
-      '3개 일치 (5,000원) - 0개',
-      '4개 일치 (50,000원) - 0개',
-      '5개 일치 (1,500,000원) - 0개',
-      '5개 일치, 보너스 볼 일치 (30,000,000원) - 1개',
-      '6개 일치 (2,000,000,000원) - 0개',
-    ];
-
     const staticObject = lottoUtill.checkLottoCorrect(
       userNumber,
       winNumber,
       bonusNumber,
     );
-    Output.lottoStatic(staticObject);
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+
+    const expected = { 3: 0, 4: 0, 5: 0, 6: 0, bonus: 1 };
+    expect(staticObject).toStrictEqual(expected);
   });
 
   test('5등,보너스 당첨 테스트', () => {
-    const Output = new OutputView();
     const lottoUtill = new LottoUtill();
 
     const userNumber = [
@@ -79,24 +45,35 @@ describe('LottoUtill Class 테스트', () => {
     const winNumber = [1, 2, 3, 4, 5, 6];
     const bonusNumber = 10;
 
-    const logSpy = getLogSpy();
+    const staticObject = lottoUtill.checkLottoCorrect(
+      userNumber,
+      winNumber,
+      bonusNumber,
+    );
+    const expected = { 3: 0, 4: 0, 5: 1, 6: 0, bonus: 1 };
+    expect(staticObject).toStrictEqual(expected);
+  });
 
-    const logs = [
-      '3개 일치 (5,000원) - 0개',
-      '4개 일치 (50,000원) - 0개',
-      '5개 일치 (1,500,000원) - 1개',
-      '5개 일치, 보너스 볼 일치 (30,000,000원) - 1개',
-      '6개 일치 (2,000,000,000원) - 0개',
+  test('수익률 테스트', () => {
+    const lottoUtill = new LottoUtill();
+
+    const userMoney = 2000;
+    const userNumber = [
+      [1, 2, 3, 4, 5, 6],
+      [1, 2, 3, 4, 5, 7],
     ];
+
+    const winNumber = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
 
     const staticObject = lottoUtill.checkLottoCorrect(
       userNumber,
       winNumber,
       bonusNumber,
     );
-    Output.lottoStatic(staticObject);
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-    });
+
+    const rateUtill = new LottoUtill(userMoney, staticObject);
+    const expected = 101500000;
+    expect(rateUtill.getRate()).toBe(expected);
   });
 });
