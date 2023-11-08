5469b621f556e678e23cdcd6d604fbaf2c72fff5
jeongin98 <eon7500@gmail.com>
2023-11-08 14:08:56 +0900

    Feat(WinningJudge): 당첨 순위 리스트 계산

--- Domain/WinningJudge.js
+++ Domain/WinningJudge.js
@@ -18,9 +18,24 @@ class WinningJudge {
     let winnerList;
     lottoList.forEach((lotto) => {
       const lottoNumber = lotto.getNumber();
-      // winnerList = this.countWinner(lottoNumber, winningLotto);
+      winnerList = this.countWinner(lottoNumber, winningLotto);
     });
     return winnerList;
+  }
+
+  countWinner(lotto, winningLotto) {
+    const matchingNumbers = winningLotto.filter((number) => lotto.includes(number));
+    const matchingCount = matchingNumbers.length;
+    const bonusNumber = winningLotto.getBonusNumber();
+    const isBonusNumberIncluded = lotto.includes(bonusNumber);
+
+    if (matchingCount === 6) this.winner.first += 1;
+    if (matchingCount === 5 && isBonusNumberIncluded) this.winner.second += 1;
+    if (matchingCount === 5) this.winner.third += 1;
+    if (matchingCount === 4) this.winner.fourth += 1;
+    if (matchingCount === 3) this.winner.fifth += 1;
+
+    return this.#winner;
   }
 }
 export default WinningJudge;
