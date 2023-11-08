42889b573d4a6e3ecddddf8b2e410bb8d262fe40
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-11-08 09:37:13 +0900

    test: 테스트 함수 분리

---
+++ __tests__/test.js
@@ -0,0 +1,47 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { CashCount } from '../src/CashCount.js';
+import { LottoMachine } from '../src/LottoMachine.js';
+
+//CashCount 클래스 테스트
+describe('cashcount 클래스 테스트', () => {
+  test('1000원 나누어 떨어지나', () => {
+    expect(() => {
+      new CashCount(123);
+    }).toThrow('[ERROR]');
+  });
+  test('숫자인가', () => {
+    expect(() => {
+      new CashCount(NaN);
+    }).toThrow('[ERROR]');
+  });
+});
+
+describe('LottoMachine 클래스 테스트', () => {
+  test('로또를 구매하고 배열로 반환한다.', () => {
+    const lottoMachine = new LottoMachine();
+    const lottos = lottoMachine.buyLottos(10000);
+
+    expect(Array.isArray(lottos)).toBe(true);
+  });
+
+  test('구매한 로또 개수가 올바르게 출력된다.', () => {
+    const lottoMachine = new LottoMachine();
+    const cash = 5000;
+    const cashCount = new CashCount(cash).count;
+
+    const consoleMock = jest.spyOn(MissionUtils.Console, 'print');
+    lottoMachine.buyLottos(cash);
+
+    expect(consoleMock).toHaveBeenCalledWith(`${cashCount}개를 구매했습니다.`);
+  });
+
+  test('구매한 로또의 개수와 반환된 배열의 길이가 일치한다.', () => {
+    const lottoMachine = new LottoMachine();
+    const cash = 10000;
+    const cashCount = new CashCount(cash).count;
+
+    const lottos = lottoMachine.buyLottos(cash);
+
+    expect(lottos.length).toBe(cashCount);
+  });
+});
