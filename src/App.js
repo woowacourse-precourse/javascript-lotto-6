31a16bf2ba481d40f28c67311ceb0c7122153c85
gelim <b4winter@qraod.com>
2023-11-08 19:59:56 +0900

    코드 리팩터링 -가독성 향상 - 주요 주석 정리

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import Lotto from "./Lotto.js";
-import { ERROR_MESSAGES } from "./Message.js";
+import Lotto from "./Lotto.js"; 
+import { ERROR_MESSAGES, MESSAGES } from "./Message.js";
 
 class App {
   constructor() {
-    this.price = 0;
-    this.lottoTickets = [];
-    this.winningNumbers = [];
-    this.bonusNumber = 0;
-    this.results = {
+    this.price = 0; // 로또 구매 금액을 저장할 변수
+    this.lottoTickets = []; // 구매한 로또 티켓 배열
+    this.winningNumbers = []; // 당첨 번호 배열
+    this.bonusNumber = 0; // 보너스 번호
+    this.results = { // 당첨 결과를 저장하는 객체
       1: 0, 
-      2: 0, 
-      3: 0, 
+      2: 0,
+      3: 0,
       4: 0, 
       5: 0, 
-    };
+    }; 
   }
 
   async play() {
     try {
-      await this.userBuyLotto();
-      await this.generateLottoTickets();
-      await this.inputWinningNumbers();
-      await this.inputBonusNumber();
-      await this.calculateResults();
-      await this.printResults();
+      await this.userBuyLotto(); // 로또 구매 금액을 입력 받는 함수 호출
+      await this.generateLottoTickets(); // 로또 티켓 생성 함수 호출
+      await this.inputWinningNumbers(); // 당첨 번호 입력 함수 호출
+      await this.inputBonusNumber(); // 보너스 번호 입력 함수 호출
+      await this.calculateResults(); // 당첨 결과 계산 함수 호출
+      await this.printResults(); // 당첨 결과 출력 함수 호출
     } catch (error) {
-      MissionUtils.Console.print(error.message);
+      MissionUtils.Console.print(error.message); // 에러가 발생한 경우 에러 메시지 출력
     }
   }
 
@@ -41,30 +41,37 @@ class App {
     if (purchaseAmount % 1000 !== 0) {
       throw new Error(`[ERROR] ${ERROR_MESSAGES.INVALID_PURCHASE_AMOUNT}`);
     }
-
+    // 로또 구매 금액을 1000원 단위로 설정
     this.price = purchaseAmount / 1000;
   }
 
+  // 구매한 티켓 개수 출력
   async generateLottoTickets() {
     MissionUtils.Console.print(`${this.price}${MESSAGES.TICKET_PURCHASED_MESSAGE}`);
-
+  
+    // 6개의 서로 다른 난수로 이루어진 로또 번호를 생성하고 오름차순으로 정렬
     for (let i = 0; i < this.price; i++) {
       const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+
+  
+      const lottoTicket = new Lotto(randomNumbers);
+    // 생성한 로또 번호 출력
       MissionUtils.Console.print(randomNumbers);
-      this.lottoTickets.push(randomNumbers);
+      this.lottoTickets.push(lottoTicket);
     }
   }
-
+    //로또 당첨번호 입력
   async inputWinningNumbers() {
     const userInput = await MissionUtils.Console.readLineAsync(MESSAGES.WINNING_NUMBERS_PROMPT);
     this.winningNumbers = this.parseNumbers(userInput);
   }
-
+    // 보너스 번호 입력
   async inputBonusNumber() {
     const userInput = await MissionUtils.Console.readLineAsync(MESSAGES.BONUS_NUMBER_PROMPT);
     this.bonusNumber = this.parseSingleNumber(userInput);
   }
 
+   // 당첨내역 출력
   calculateResults() {
     this.lottoTickets.forEach((lotto) => {
       const { matchCount, hasBonus } = this.checkMatchingNumbers(lotto);
@@ -97,7 +104,7 @@ class App {
 
     return { matchCount, hasBonus };
   }
-
+  // 당첨 확률출력
   printResults() {
     const winRate = this.calculateWinningRate();
     MissionUtils.Console.print("\n 당첨 통계 \n --- ");
