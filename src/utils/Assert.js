72d5c9f8c1c912d0f22f9e2fb3632a3ed6550a1d
fecapark <fecapark@gmail.com>
2023-11-08 11:19:01 +0900

    feat: 당첨 번호 검증 로직 추가

--- src/utils/Assert.js
+++ src/utils/Assert.js
@@ -1,11 +1,18 @@
 //@ts-check
 
-import { LOTTO_NUMBER_RANGE, MONEY_UNIT } from "../core/const";
-import { PurchaseValueError } from "./Error";
+import {
+  LOTTO_NUMBER_AMOUNT,
+  LOTTO_NUMBER_RANGE,
+  MONEY_UNIT,
+} from "../core/const";
+import { PurchaseValueError, WinningNumberError } from "./Error";
 
 export default class Assert {
   constructor() {}
 
+  /**
+   * @param {number} money
+   */
   assertPurchaseValue(money) {
     if (!this.#isPositiveFiniteInteger(money))
       throw new PurchaseValueError(PurchaseValueError.TYPE_NOT_NUMBER);
@@ -13,6 +20,30 @@ export default class Assert {
       throw new PurchaseValueError(PurchaseValueError.TYPE_NOT_PURCHASED);
     if (!this.#isMoneyHasNoChange(money))
       throw new PurchaseValueError(PurchaseValueError.TYPE_HAS_MONEY_CHANGE);
+  }
+
+  /**
+   * @param {string} winningNumberString
+   */
+  assertWinningNumber(winningNumberString) {
+    const winningNumberTokens = winningNumberString.split(",");
+    const winningNumbers = winningNumberTokens.map((token) =>
+      parseInt(token.trim())
+    );
+    const uniqueWinningNumbers = new Set(winningNumbers);
+
+    if (winningNumbers.length !== LOTTO_NUMBER_AMOUNT)
+      throw new WinningNumberError(WinningNumberError.TYPE_NOT_AMOUNT_6);
+
+    winningNumbers.forEach((winningNumber) => {
+      if (!this.#isPositiveFiniteInteger(winningNumber))
+        throw new WinningNumberError(WinningNumberError.TYPE_OUT_OF_RANGE);
+      if (!this.#isInRange(winningNumber))
+        throw new WinningNumberError(WinningNumberError.TYPE_OUT_OF_RANGE);
+    });
+
+    if (winningNumbers.length !== uniqueWinningNumbers.size)
+      throw new WinningNumberError(WinningNumberError.TYPE_DUPLICATED);
   }
 
   /**
