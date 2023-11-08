a742b145e3a2b2374a3c8df335dc1e855673cf3e
fecapark <fecapark@gmail.com>
2023-11-08 12:57:20 +0900

    feat: 당첨 번호 입력 로직 구현 완료

--- src/core/Input.js
+++ src/core/Input.js
@@ -2,10 +2,14 @@
 
 import { Console } from "@woowacourse/mission-utils";
 import Assert from "./Assert.js";
+import { parseToCommaSeperatedIntegers } from "../utils/parse.js";
 
 export default class Input {
   /** @type {Assert} */
   #assert;
+
+  /** @type {number[]} */
+  #winningNumbers = [];
 
   constructor() {
     this.#assert = new Assert();
@@ -33,5 +37,27 @@ export default class Input {
     }
 
     return purchaseValue;
+  }
+
+  /**
+   * @returns {Promise<number[]>}
+   */
+  async askWinningNumbers() {
+    while (true) {
+      try {
+        const response = await Console.readLineAsync(
+          "구입금액을 입력해 주세요.\n"
+        );
+
+        this.#winningNumbers = parseToCommaSeperatedIntegers(response);
+        this.#assert.assertWinningNumber(this.#winningNumbers);
+
+        break;
+      } catch (error) {
+        Console.print(error.message + "\n");
+      }
+    }
+
+    return this.#winningNumbers;
   }
 }
