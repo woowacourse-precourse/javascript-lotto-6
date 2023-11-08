98ab7a750ef1de96de814edaa61f7b866b1f38c0
heonq <gkagjsrb@naver.com>
2023-11-08 15:08:00 +0900

    refactor(User): 로또에 들어갈 임의의 숫자를 생성하고 로또를 생성하는 기능 분리 - LottoGenerator 객체를 생성

--- src/User.js
+++ src/User.js
@@ -1,8 +1,7 @@
-import { Random } from '@woowacourse/mission-utils';
-import Lotto from './Lotto.js';
 import CONSTANTS from '../utils/Constants.js';
 import MESSAGES from '../utils/Messages.js';
 import { PRIZE_REWARD } from '../utils/Prize.js';
+import LottoGenerator from '../utils/LottoGenerator.js';
 
 class User {
   lottos;
@@ -12,7 +11,7 @@ class User {
   #purchaseAmount;
 
   constructor(purchaseAmount) {
-    this.lottos = this.#generateLotto(purchaseAmount);
+    this.lottos = LottoGenerator.generateLotto(purchaseAmount);
     this.#prize = {
       fifthPrize: 0,
       fourthPrize: 0,
@@ -21,22 +20,6 @@ class User {
       firstPrize: 0,
     };
     this.#purchaseAmount = purchaseAmount;
-  }
-
-  #generateLotto(purchaseAmount) {
-    const lottoQuantity = purchaseAmount / CONSTANTS.eachLottoPrice;
-    const lottoNumbers = this.generateRandomNumbers(lottoQuantity);
-    return lottoNumbers.map((lottoNumber) => new Lotto(lottoNumber));
-  }
-
-  generateRandomNumbers(lottoQuantity) {
-    return Array.from({ length: lottoQuantity }, () =>
-      Random.pickUniqueNumbersInRange(
-        CONSTANTS.minimumNumber,
-        CONSTANTS.maximumNumber,
-        CONSTANTS.mainNumberCount,
-      ).sort((a, b) => a - b),
-    );
   }
 
   getLottoStringArray() {
