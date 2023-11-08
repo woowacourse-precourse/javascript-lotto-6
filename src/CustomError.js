068c8f9d316b5091f301dfcc0f1f28513f6fd62c
Sohye <saeyan2477@gmail.com>
2023-11-08 21:59:07 +0900

    fix: 파일 삭제
    - 파일 삭제

--- src/CustomError.js
+++
@@ -1,9 +0,0 @@
-import { ERROR_MESSAGES } from './Constants.js';
-
-export class CustomError extends Error {
-  static #prefix = ERROR_MESSAGES.PREFIX;
-
-  constructor(errorMessage) {
-    super(`${CustomError.#prefix} ${errorMessage}`);
-  }
-}
