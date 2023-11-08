c254a014cdbf032ee2c6cc4864b561e2d487cdb2
minSsan <msp1125@hanyang.ac.kr>
2023-11-08 21:32:17 +0900

    test: Lotto 클래스 테스트 추가 및 검증

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import Lotto from "../src/Lotto.js";
 
 describe("로또 클래스 테스트", () => {
@@ -15,4 +16,21 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호에 정수가 아닌 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5.5, 6]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 범위에서 벗어난 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 46, 1]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 number 타입이 아닌 다른 타입의 값이 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, "4", 45, 1]);
+    }).toThrow("[ERROR]");
+  });
 });
