413dce7163ff75ec38a0fde86bcde068aaab1e0e
Klomachenko <leekyumin0901@naver.com>
2023-11-08 20:55:11 +0900

    feat: isMultipleOf1000 구현
    Description: input이 1000의 배수인지 확인

--- src/Validate.js
+++ src/Validate.js
@@ -2,6 +2,10 @@ class Validate {
   static isNumeric(input) {
     return /^[0-9]+$/.test(input);
   }
+
+  static isMultipleOf1000(input) {
+    return +input % 1000 === 0;
+  }
 }
 
 export default Validate;
