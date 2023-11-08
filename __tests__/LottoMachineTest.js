92d15a5d65831c2923da87c702fe64dfcdcbd1eb
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-11-08 15:42:06 +0900

    test: validate 테스트 코드 추가

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -1,6 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import { LottoMachine } from '../src/LottoMachine.js';
-import { CashCount } from '../src/CashCount.js';
 
 describe('LottoMachine 클래스 테스트', () => {
   test('로또를 구매하고 배열로 반환한다.', () => {
@@ -12,8 +11,7 @@ describe('LottoMachine 클래스 테스트', () => {
 
   test('구매한 로또 개수가 올바르게 출력된다.', () => {
     const lottoMachine = new LottoMachine();
-    const cash = 5000;
-    const cashCount = new CashCount(cash).count;
+    const cashCount = 5;
 
     const consoleMock = jest.spyOn(MissionUtils.Console, 'print');
     lottoMachine.buyLottos(cashCount);
