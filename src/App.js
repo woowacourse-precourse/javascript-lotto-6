ea7702e0062e08cd4b4024a0d5c8655ba471a168
JoSooha <dlrhdcjs@naver.com>
2023-11-08 17:57:56 +0900

    feat: 구매 금액 입력 받기 구현 및 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Lotto from "./Lotto";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+
 class App {
-  async play() {}
+  async play() {
+    const count = await this.getMoneyReturnCount();
+    console.log("count", count);
+  }
+
+  async getMoneyReturnCount() {
+    const money = await MissionUtils.Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    if(money%1000 !== 0) throw new Error("[ERROR] 1000원 단위로 입력해 주세요.");
+    return money/1000;
+  }
+  
 }
 
 export default App;
