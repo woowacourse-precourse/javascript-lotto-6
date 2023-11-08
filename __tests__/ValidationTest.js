17ffe51e91f8057a35f4a85df283343180eba59c
kwoncha <rnjsckgus@gmail.com>
2023-11-08 11:23:53 +0900

    test: 1000원 단위가 아닐 시 예최처리 테스트 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,6 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-import { validateNumber, isIncludedBonusNumbers, validatedPrice, parsedNumber } from '../src/utils/validation/validation';
+import { ensureIsNumberString, ensureBonusNumberIsNotIncluded, validateLottoPurchase } from '../src/utils/validation/validation.js';
 
 const mockQuestions = inputs => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -15,6 +15,12 @@ describe('숫자 확인 함수 테스트', () => {
   test.each([[['1ㄷㅁ']], [['ㅇ3ㅎ']], [['    ']], [['0000100']], [['1000j']]])('숫자가 아닌 것에 대한 예외 처리', inputs => {
     mockQuestions(inputs);
 
-    expect(() => validateNumber(inputs[0])).toThrowError('[ERROR]');
+    expect(() => ensureIsNumberString(inputs[0])).toThrowError('[ERROR]');
+  });
+
+  test.each([[['1100']], [['1300']], [['4100']]])('1000단위가 아닌 수일 시 예외 처리', inputs => {
+    mockQuestions(inputs);
+
+    expect(() => validateLottoPurchase(inputs[0])).toThrowError('[ERROR]');
   });
 });
