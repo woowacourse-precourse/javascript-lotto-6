3afbc0d977a5ac7f0b739e98d9580551d10bb92c
leelsj2007 <leelsj2007@gmail.com>
2023-11-08 17:57:32 +0900

    feat : MyLotto객체 생성 기능

--- src/controller/lotto-controller.js
+++ src/controller/lotto-controller.js
@@ -1,4 +1,6 @@
+import MyLotto from "../domain/MyLotto.js";
 import InputView from "../view/input-view.js";
+import { Console } from "@woowacourse/mission-utils";
 
 class LottoController {
     #inputView;
@@ -9,6 +11,12 @@ class LottoController {
 
     async play() {
         let count = await this.#inputView.readPurchaseAmount();
+        let lottos = new MyLotto(count);
+        Console.print(`\n${count}개를 구매했습니다.`);
+        for (let element of lottos.getMyLottos()){
+            Console.print(element.getNumbers());
+        }
+
     }
 }
 
---
+++ src/domain/MyLotto.js
@@ -0,0 +1,24 @@
+import Lotto from "./Lotto.js";
+import lottoNumberGenerator from "../util/lotto-number-generator.js";
+
+class MyLotto {
+    #lottos;
+
+    constructor(purchaseAmount) {
+        this.#lottos = this.#generate(purchaseAmount);
+    }
+
+    #generate(purchaseAmount) {
+        let generator = [];
+        for (let i = 0; i < purchaseAmount; i++) {
+            generator.push(new Lotto(lottoNumberGenerator()));
+        }
+        return generator;
+    }
+
+    getMyLottos() {
+        return this.#lottos;
+    }
+}
+
+export default MyLotto;
