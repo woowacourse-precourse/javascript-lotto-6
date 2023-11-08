5dfde8855336cda3c122641e733c27c1ea89d1bf
kim <kajinori@gachon.ac.kr>
2023-11-08 22:20:07 +0900

    feat: Lotto 클래스 추가
    - 로또 티켓 생성 기능
    - 로또 번호 일치 확인 기능
    - 로또 수익률 계산 로직
    - 로또 결과 출력 기능

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,8 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Validator from './Validator.js';
+import { message } from './constant.js';
+import { numbers } from './constant.js';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +12,96 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    Validator.validateNumbers(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers.sort((a, b) => a - b);
+  }
+
+  getMatchCount(winningNumbers) {
+    let matchCount = 0;
+    this.#numbers.forEach((num) => {
+      if (winningNumbers.includes(num)) {
+        matchCount += 1;
+      }
+    });
+    return matchCount;
+  }
+
+  hasBonusNumber(bonusNumber) {
+    return this.#numbers.includes(bonusNumber);
+  }
+
+  getPrize(rank) {
+    const prizes = {
+      [message.THREE_MATCHED]: numbers.FIFTH_PRIZE_MONEY,
+      [message.FOUR_MATCHED]: numbers.FOURTH_PRIZE_MONEY,
+      [message.FIVE_MATCHED]: numbers.THIRD_PRIZE_MONEY,
+      [message.FIVE_WITH_BONUS_MATCHED]: numbers.SECOND_PRIZE_MONEY,
+      [message.SIX_MATCHED]: numbers.FIRST_PRIZE_MONEY,
+    };
+    return prizes[rank];
+  }
+
+  calculateEarningsRate(lottoTickets, winningNumbers, bonusNumber) {
+    const result = this.calculateResults(lottoTickets, winningNumbers, bonusNumber);
+    const { totalPrize, resultString } = this.generateResultString(result);
+
+    const totalCost = lottoTickets.length * numbers.THOUSAND;
+    const earningsRate = (totalPrize / totalCost) * numbers.PERCENT;
+
+    const finalResultString = `${resultString}총 수익률은 ${earningsRate.toFixed(1).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}%입니다.`;
+    MissionUtils.Console.print(finalResultString);
+  }
+
+  calculateResults(lottoTickets, winningNumbers, bonusNumber) {
+    const result = {
+      [message.THREE_MATCHED]: 0,
+      [message.FOUR_MATCHED]: 0,
+      [message.FIVE_MATCHED]: 0,
+      [message.FIVE_WITH_BONUS_MATCHED]: 0,
+      [message.SIX_MATCHED]: 0,
+    };
+
+    for (const lottoTicket of lottoTickets) {
+      const matchCount = lottoTicket.getMatchCount(winningNumbers);
+      const hasBonus = lottoTicket.hasBonusNumber(bonusNumber);
+
+      switch (matchCount) {
+        case 3:
+          result[message.THREE_MATCHED] += 1;
+          break;
+        case 4:
+          result[message.FOUR_MATCHED] += 1;
+          break;
+        case 5:
+          if (hasBonus) {
+            result[message.FIVE_WITH_BONUS_MATCHED] += 1;
+          } else {
+            result[message.FIVE_MATCHED] += 1;
+          }
+          break;
+        case 6:
+          result[message.SIX_MATCHED] += 1;
+          break;
+      }
+    }
+    return result;
+  }
+
+  generateResultString(result) {
+    let totalPrize = 0;
+    let resultString = "";
+
+    for (const rank in result) {
+      const prize = this.getPrize(rank);
+      const count = result[rank];
+      totalPrize += prize * count;
+      resultString += `${rank} - ${count}개\n`;
+    }
+    return { totalPrize, resultString };
+  }
 }
 
 export default Lotto;
