b98122f6d76e6e97685f5e3a74c059405a660439
gilgor2 <misby0327@gmail.com>
2023-11-08 16:29:39 +0900

    feat(LottoRules.js): 수정된 로또 규칙 객체 구현
    메인 기능 변경에 의해 다시 구현
    
    주의 : 기존 LottoRules.js와 아예 새롭게 구현함

--- src/LottoRules.js
+++ src/LottoRules.js
@@ -1,26 +1,36 @@
-class LottoRules {
-	#winningRule = [
-		{ correctCnt: 3, bonusCnt: 0, winnings: 5000 },
-		{ correctCnt: 4, bonusCnt: 0, winnings: 50000 },
-		{ correctCnt: 5, bonusCnt: 0, winnings: 1500000 },
-		{ correctCnt: 5, bonusCnt: 1, winnings: 30000000 },
-		{ correctCnt: 6, bonusCnt: 0, winnings: 2000000000 },
-	];
+import WinningCondition from './WinningCondition';
 
-	getWinnings(correctCnt, bonusCnt) {
-		const correctRank = this.#winningRule.find(
-			(condition) =>
-				condition.correctCnt === correctCnt &&
-				condition.bonusCnt === bonusCnt,
-		);
-		if (!correctRank) {
-			return -1;
-		}
-		return correctRank.winnings;
+const DEFAULT_RULES = [
+	{ correctCnt: 3, bonusCnt: 0, winnings: 5000 },
+	{ correctCnt: 4, bonusCnt: 0, winnings: 50000 },
+	{ correctCnt: 5, bonusCnt: 0, winnings: 1500000 },
+	{ correctCnt: 5, bonusCnt: 1, winnings: 30000000 },
+	{ correctCnt: 6, bonusCnt: 0, winnings: 2000000000 },
+];
+
+class LottoRules {
+	#winningRule = [];
+
+	constructor(newRules) {
+		this.#initializeWinningRules(newRules || DEFAULT_RULES);
+		this.#sortWinningRules();
 	}
 
 	getTotalRule() {
 		return this.#winningRule;
+	}
+
+	#initializeWinningRules(rules) {
+		const winningRule = rules.map((ruleObj) => new WinningCondition(ruleObj));
+		this.#winningRule = winningRule;
+	}
+
+	#sortWinningRules() {
+		this.#winningRule = this.#winningRule.sort(
+			(conditionA, conditionB) =>
+				conditionA.getWinningCondition().winnings -
+				conditionB.getWinningCondition().winnings,
+		);
 	}
 }
 export default LottoRules;
