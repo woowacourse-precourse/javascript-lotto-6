db7584ef067d09d0dd9e80e97bd79ab86dc98e4f
flowersayo <flowersayo0811@ewhain.net>
2023-11-08 23:38:42 +0900

    feat: LottoFactory 랜덤 로또 생성 테스트

--- src/domain/LottoFactory.js
+++ src/domain/LottoFactory.js
@@ -5,11 +5,11 @@ import Lotto from './Lotto.js';
 class LottoFactory {
   exchangeLottos(money) {
     const amount = money / LOTTO.MONEY_UNIT;
-    const lottos = Array.from({ length: amount }, this.#generateLotto);
+    const lottos = Array.from({ length: amount }, this.generateLotto);
     return lottos;
   }
 
-  #generateLotto() {
+  generateLotto() {
     const randomLottoNumbers = Random.pickUniqueNumbersInRange(
       LOTTO.MIN_NUMBER,
       LOTTO.MAX_NUMBER,
