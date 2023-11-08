8d7bceaa06694c3d63f724c4867d55649732688a
Hong sung soo <sow9707@daum.net>
2023-11-08 17:56:36 +0900

    test: 로또 클래스에 대한 속성값 업데이트 테스트 코드 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import Lotto from "../src/Lotto.js";
+import App from "../src/App.js";
+import WinningLotto from "../src/classes/WinningLotto.js";
+import Util from "../src/classes/util.js";
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +26,32 @@ describe("로또 클래스 테스트", () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test("로또 번호 6자리 숫자 생성 테스트", () => {
+
+    const lottoNumbers = Lotto.createNumber();
+    const result = lottoNumbers.length;
+
+    expect(result).toBe(6);
+  })
+  test("로또 번호 맞은 수 만큼 COUNT 값 업데이트 테스트", () => {
+    const winningNumber = [3, 4, 5, 6, 1, 2];
+    const lottoNumber = [1, 2, 3, 4, 5, 6];
+
+    const lotto = new Lotto(lottoNumber);
+    lotto.compareNumber(winningNumber);
+    const result = lotto.COUNT;
+
+    expect(result).toBe(6);
+  });
+
+  test("보너스 번호 맞으면 BONUS 속성값 업데이트 테스트", () => {
+    const bonusNumber = 3;
+    const lottoNumber = [1, 2, 3, 4, 5, 6];
+
+    const lotto = new Lotto(lottoNumber);
+    lotto.compareBonusNumber(bonusNumber);
+    const result = lotto.BONUS;
+
+    expect(result).toBe(true);
+  });
 });
