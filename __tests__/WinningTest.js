f995a414196ad717e2911b02e6b3103091f53217
Dabin Jeong <allempti@gmail.com>
2023-11-08 20:19:06 +0900

    style: 로또 당첨 테스트 설명 수정

--- __tests__/WinningTest.js
+++ __tests__/WinningTest.js
@@ -7,7 +7,7 @@ describe('로또 당첨 테스트', () => {
     lottoData = new LottoData(8000);
   });
 
-  test('로또 당첨 내역 계산', () => {
+  test('로또 번호 비교하고 당첨 개수 계산하기', () => {
     const winningNumber = [1, 2, 4, 8, 28, 40];
     const bonusNumber = 42;
     lottoData.lottos = [
@@ -20,8 +20,10 @@ describe('로또 당첨 테스트', () => {
       [13, 18, 24, 25, 39, 43],
       [1, 9, 12, 20, 22, 44],
     ];
+
     lottoData.calculateStats(winningNumber, bonusNumber);
     const lottoStats = lottoData.getLottoStats();
+
     expect(lottoStats[3]).toBe(1);
     expect(lottoStats[4]).toBe(0);
     expect(lottoStats[5]).toBe(0);
@@ -29,7 +31,7 @@ describe('로또 당첨 테스트', () => {
     expect(lottoStats[6]).toBe(0);
   });
 
-  test('수익률 계산', () => {
+  test('수익률 계산하기', () => {
     lottoData.lottoStats = { 3: 1, 4: 0, 5: 0, 6: 0, bonus: 0 };
     const profit = lottoData.calculateProfit();
     expect(profit).toBe('62.5');
