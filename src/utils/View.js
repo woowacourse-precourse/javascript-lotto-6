25f53d93b6080304241365e78e8d8335c10f3617
jonghyunRyu <willowise@naver.com>
2023-11-08 17:57:31 +0900

    refactor(View): 구매한 결과, 당첨 통계, 총 수익률 출력 함수를 분리 - View 클래스를 생성 - 출력 함수들을 App 클래스에서 분리

---
+++ src/utils/View.js
@@ -0,0 +1,28 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class View {
+
+  printAllLotto(lotto) {
+    Console.print('\n' + lotto.length + '개를 구매했습니다.');
+    
+    lotto.forEach((element) => {
+    Console.print("[" + element.join(", ") + "]");
+    });
+  }
+
+  statisticsPrint(statistics) {
+    Console.print("\n당첨 통계");
+    Console.print("---");
+    Console.print("3개 일치 (5,000원) - " + statistics[0] + '개');
+    Console.print("4개 일치 (50,000원) - " + statistics[1] + '개');
+    Console.print("5개 일치 (1,500,000원) - " + statistics[2] + '개');
+    Console.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + statistics[4] + '개');
+    Console.print("6개 일치 (2,000,000,000원) - " + statistics[3] + '개');
+  }
+
+  printRateOfReturn(rate) {
+    Console.print("총 수익률은 " + rate + "%입니다.");
+  }
+}
+
+export default View;
