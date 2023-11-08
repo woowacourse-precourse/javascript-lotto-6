2e8e7ef015581963ab819ce3550d09bdf824ae34
baegyeong <beth0929@naver.com>
2023-11-08 20:32:33 +0900

    refactor(constant):한 에러 메시지에 한가지 에러만 포함

--- src/constant/ERROR.js
+++ src/constant/ERROR.js
@@ -12,7 +12,7 @@ export const LOTTO_ERROR = {
   isSixSize: `${ERROR} 로또 번호는 ${RANGE.SIZE}개여야 합니다.`,
   isIncludesRange: `${ERROR} 로또 번호는 ${RANGE.MIN}부터 ${RANGE.MAX}까지 입력 가능합니다.`,
   isDuplicate: `${ERROR} 로또 번호는 서로 중복될 수 없습니다.`,
-  isNumber: `${ERROR} 로또 번호는 1 이상의 숫자만 입력하셔야 합니다.`,
+  isNumber: `${ERROR} 로또 번호는 숫자만 입력하셔야 합니다.`,
 };
 
 export const BONUS_NUMBER_ERROR = {
