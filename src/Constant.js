9beba0fab2d659c6d0c6f3ebf1425eb76462c34f
suneunkim <tjsdms89@gmail.com>
2023-11-08 15:34:12 +0900

    로또 구매 기능

---
+++ src/Constant.js
@@ -0,0 +1,13 @@
+export const CONSOLE_MESSAGE = {
+  ASK_LOTTO_PRICE: "구입금액을 입력해 주세요.",
+  ANSWER_LOTTO_ACCONT: "개를 구매했습니다.",
+  ASK_WINNING_NUMBER: "당첨 번호를 입력해 주세요.",
+  ASK_BONUS_NUMBER: "보너스 번호를 입력해 주세요.",
+};
+
+export const ERROR_MESSAGE = {
+  INVALID_LOTTO_PRICE: "[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.",
+  NUMBER_NOT_SIX: "로또 번호는 6개여야 합니다.",
+  NUMBER_DUPLICATION: "[ERROR] 로또 번호는 중복될 수 없습니다.",
+  NUMBER_OUT_OF_RANGE: "[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.",
+};
