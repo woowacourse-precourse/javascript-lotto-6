e71e4afef7746dc97f89c2c3d773a6f3c20e2f60
[NDCC] Park Jee_Hoon <jh.m02h.park@misumi.co.kr>
2023-11-08 13:26:17 +0900

    feat: 깃허브 계정 이슈로 인한 계정 변경 후 백업

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,7 @@
+import LottoValidate from "./validate/LottoValidate.js";
+import UserView from './view/UserView.js';
+import { LOTTO_LANK } from './comm/Util.js';
+
 class Lotto {
   #numbers;
 
@@ -7,12 +11,61 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    numbers.forEach((number) => {
+      new LottoValidate().randomNumberValidate(number);
+    });
   }
 
-  // TODO: 추가 기능 구현
+  userOutputPurchaseLotto() {
+    new UserView().userOutputPurchaseLottoNumber(this.#numbers);
+  }
+
+  resultLottoPrize = (winningNumber, bonusNumber) => {
+    const COMPARE_NUMBER_COUNT = [];
+    const COMPARE_BONUS_NUMBER_COUNT = [];
+    for(const LOTTO_ARR of this.#numbers){
+        const COMPARE_LOTTO_COUNT = this.compareLottoNumber(LOTTO_ARR, winningNumber);
+        COMPARE_NUMBER_COUNT.push(COMPARE_LOTTO_COUNT);
+        COMPARE_BONUS_NUMBER_COUNT.push(LOTTO_ARR.filter(number => number === Number(bonusNumber)).length);
+    }
+    const RESULT_RANK = this.resultLottoRank(COMPARE_NUMBER_COUNT,COMPARE_BONUS_NUMBER_COUNT);
+    const RESULT_RANK_COUNT = this.resultLankCount(RESULT_RANK);
+    return RESULT_RANK_COUNT;
+  }
+
+  compareLottoNumber = (purchaseLotto, winningNumber) => {
+    return purchaseLotto.filter(
+        number => winningNumber.includes(number)
+    ).length;
+  }
+
+  resultLottoRank = (numberCount, bonusNumberCount) => {
+    const RESULT_RANK = [];
+    numberCount.forEach((number, index) => {
+      const bonusNumber = bonusNumberCount[index];
+      if(number === 6){
+        RESULT_RANK.push(LOTTO_LANK.FIRST_RANK);
+      } else if(number+bonusNumber === 6) {
+        RESULT_RANK.push(LOTTO_LANK.SECOND_RANK);
+      } else if(number+bonusNumber === 5) {
+        RESULT_RANK.push(LOTTO_LANK.THIRD_RANK);
+      } else if(number+bonusNumber === 4) {
+        RESULT_RANK.push(LOTTO_LANK.FOURTH_RANK);
+      } else if(number+bonusNumber === 3) {
+        RESULT_RANK.push(LOTTO_LANK.FIFTH_RANK);
+      }
+    });
+    return RESULT_RANK;
+  }
+
+  resultLankCount = (resultLank) => {
+    const RESULT_RANK_COUNT_ARR = new Array(5).fill(0);
+    resultLank.forEach((data) => {
+      RESULT_RANK_COUNT_ARR[data-1]++;
+    });
+    return RESULT_RANK_COUNT_ARR.reverse();
+  }
+
 }
 
 export default Lotto;
---
+++ src/validate/InOutputValidate.js
@@ -0,0 +1,7 @@
+import { ERROR_MESSAGE } from "../comm/Message.js";
+
+class InOutputError{
+
+}
+
+export default InOutputError
