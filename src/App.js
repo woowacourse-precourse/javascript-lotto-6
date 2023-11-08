2ba9256b3dc22b509c12bae07910b5e0ae580ebb
김선인 <kimsunin@gimseon-in-ui-MacBookAir.local>
2023-11-08 19:09:42 +0900

    구입금액 입력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 class App {
   async play() {
-    console.log("test1");
+    const lottoCount = await this.getLottoPrice();
+    const lotto = await this.createLotto(lottoCount);
+  }
+
+  async getLottoPrice() {
+    //구입금액 입력받기
+    const lottoPrice = MissionUtils.Console.readLineAsync(
+      "구입금액을 입력해 주세요."
+    );
+    if (lottoPrice < 1000) {
+      throw new Error("[ERROR]구입금액은 1000원 이상이어야 합니다.");
+    }
+    if (lottoPrice % 1000 !== 0) {
+      throw new Error("[ERROR]구입금액은 1000단위여야 합니다.");
+    } else return lottoPrice % 1000;
+  }
+
+  //로또 생성
+  async createLotto(lottoCount) {
+    lottoNumber = Math.floor();
+    lotto = [];
   }
 }
 
