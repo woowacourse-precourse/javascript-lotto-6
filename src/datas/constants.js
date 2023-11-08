2d726bff01c3a9c09d40c31b9b6bc0bf3bad433f
JungHun98 <j7papa@naver.com>
2023-11-08 10:07:36 +0900

    feat: 상수 정의 숫자, 프롬프트

---
+++ src/datas/constants.js
@@ -0,0 +1,12 @@
+export const NUMBER = {
+  divisor: 1000,
+  lottoNumberLength: 6,
+};
+
+export const PROMPT = {
+  costInput: '구입금액을 입력해주세요\n',
+  prizeNumberInput: '당첨 번호를 입력해 주세요\n',
+  bonusNumberInput: '보너스 번호를 입력해 주세요\n',
+  prize: '당첨 통계',
+  dividingLine: '---',
+};
