f4dab0b243a7371bc1c93e33906ddc97b5adf161
ChoiYongWon <yongwon0824@naver.com>
2023-11-08 17:42:58 +0900

    feat: 로또 구매 메소드

--- src/LottoService.js
+++ src/LottoService.js
@@ -1,3 +1,6 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class LottoService {
     constructor() {
         this.numbers = []
@@ -9,6 +12,17 @@ class LottoService {
             5: 1500000,
             4: 50000,
             3: 5000
+        }
+    }
+
+    buyLotto(money) {
+        const amount = Math.floor(money / 1000);
+        Console.print(`${amount}개를 구매했습니다.`);
+        for(let i=0;i<amount;i++){
+            const randomNumber = Random.pickUniqueNumbersInRange(1, 45, 6);
+            const lotto = new Lotto(randomNumber);
+            this.db.push(lotto);
+            Console.print(lotto.toString());
         }
     }
 
