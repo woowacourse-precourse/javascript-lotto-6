c29aa356c4246c8d70ed36f30e2683e3999b71db
nari <naringst@gmail.com>
2023-11-08 21:50:12 +0900

    feat : 로또 클래스 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,5 +1,8 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
+  q2;
 
   constructor(numbers) {
     this.#validate(numbers);
@@ -12,7 +15,46 @@ class Lotto {
     }
   }
 
-  // TODO: 추가 기능 구현
+  //맞춘 개수 카운트
+  checkWinningCount(numbers, winningNumList) {
+    coincideNumCount = 0;
+    for (let i = 0; i < 6; i++) {
+      winningNumList.forEach((num) => {
+        if (Number(num) === numbers[i]) {
+          coincideNumCount++;
+        }
+      });
+    }
+    return coincideNumCount;
+  }
+  // 보너스 숫자 맞췄는지 여부 확인
+  isBonusTrue(numbers, bonusNum) {
+    if (numbers.include(bonusNum)) {
+      return true;
+    }
+    return false;
+  }
+
+  //맞춘 숫자에 따른 당첨 금액 계산
+  calcGetMoney(coincideNumCount, bonusTrue) {
+    if (coincideNumCount === 3) {
+      return 5000;
+    }
+
+    if (coincideNumCount === 4) {
+      return 50000;
+    }
+
+    if (coincideNumCount === 5) {
+      if (!bonusTrue) return 1500000;
+      return 30000000;
+    }
+
+    if (coincideNumCount === 6) {
+      return 2000000000;
+    }
+    return 0;
+  }
 }
 
 export default Lotto;
