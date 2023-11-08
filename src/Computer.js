49409986e5d1a6bd90983c0a3e6131ae7df62bdc
JungHun98 <j7papa@naver.com>
2023-11-08 09:45:09 +0900

    refactor: Computer 클래스 당첨 관련 상수 분리

--- src/Computer.js
+++ src/Computer.js
@@ -1,30 +1,4 @@
-const FIRST = 'first';
-const SECOND = 'second';
-const THIRD = 'third';
-const FOURTH = 'fourth';
-const FIFTH = 'fifth';
-
-const prizeMony = {
-  first: 2000000000,
-  second: 30000000,
-  third: 1500000,
-  fourth: 50000,
-  fifth: 5000,
-};
-
-const prizeConditionArr = [
-  { key: { mathNumber: 6, isMatchBonus: false }, value: FIRST },
-  { key: { mathNumber: 5, isMatchBonus: true }, value: SECOND },
-  { key: { mathNumber: 5, isMatchBonus: false }, value: THIRD },
-  { key: { mathNumber: 5, isMatchBonus: true }, value: THIRD },
-  { key: { mathNumber: 4, isMatchBonus: false }, value: FOURTH },
-  { key: { mathNumber: 4, isMatchBonus: true }, value: FOURTH },
-  { key: { mathNumber: 3, isMatchBonus: false }, value: FIFTH },
-  { key: { mathNumber: 3, isMatchBonus: true }, value: FIFTH },
-];
-
-const map = new Map();
-prizeConditionArr.forEach((elem) => map.set(JSON.stringify(elem.key), elem.value));
+import { prize, rank, conditionMap } from './datas/prize';
 
 class Computer {
   #cost;
@@ -35,20 +9,17 @@ class Computer {
   constructor(winnnersNumbers, bonusNumber, cost) {
     this.#winnerNumbers = winnnersNumbers;
     this.#bonusNumber = bonusNumber;
-    this.#prizeResult = {
-      [FIRST]: 0,
-      [SECOND]: 0,
-      [THIRD]: 0,
-      [FOURTH]: 0,
-      [FIFTH]: 0,
-    };
     this.#cost = cost;
+    this.#prizeResult = {};
+
+    rank.forEach((elem) => (this.#prizeResult[elem] = 0));
   }
 
   setPrizeResult(lottos) {
     const mathResult = lottos.map((lotto) => {
       const lottoNumbers = lotto.getNumbers();
       const mathNumbers = this.#winnerNumbers.filter((number) => lottoNumbers.includes(number));
+
       const mathNumber = mathNumbers.length;
       const isMatchBonus = lottoNumbers.includes(this.#bonusNumber);
 
@@ -57,7 +28,7 @@ class Computer {
 
     mathResult.forEach((elem) => {
       const keyString = JSON.stringify(elem);
-      const prize = map.get(keyString);
+      const prize = conditionMap.get(keyString);
 
       if (prize !== undefined) {
         this.#prizeResult[prize] += 1;
@@ -72,7 +43,8 @@ class Computer {
   getProfitRatio() {
     const places = Object.keys(this.#prizeResult);
     const totlaProfit = places.reduce((profit, place) => {
-      return profit + prizeMony[place] * this.#prizeResult[place];
+      const currentProfit = profit + prize[place].money * this.#prizeResult[place];
+      return currentProfit;
     }, 0);
     const profitRatio = (totlaProfit / this.#cost) * 100;
     const ratioString = `${Math.round((profitRatio * 10).toPrecision(15)) / 10}%`;
