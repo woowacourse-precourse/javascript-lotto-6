dfb2bb4a7708a797baaaf892d23062e8fb3c200f
Luna <chlwlal20@gmail.com>
2023-11-08 22:00:42 +0900

    feat(App.js): 기능 통합

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import UserInterface from './UserInterface.js';
+import LottoGame from './LottoGame.js';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const userInterface = UserInterface();
+      const lottoGame = new LottoGame();
+
+      const money = await userInterface.getLottoNumbers();
+      lottoGame.purchaseLotto(money);
+      const lottoTickets = lottoGame.getLottoTickets();
+      userInterface.printLottoNumbers(lottoTickets);
+
+      const winning = await userInterface.getWinningNumbers();
+      const bonus = await userInterface.getBonusNumber();
+      const count = await lottoGame.compareLotto(winning, bonus);
+      await userInterface.printResult(count);
+      const profit = await lottoGame.calculateProfitRate(count, money);
+      await userInterface.printProfitRate(profit);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
