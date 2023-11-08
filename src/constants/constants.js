beb8dd66a746395f13cbdb3c767c84a3f9bd4faa
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 15:43:16 +0900

    feat: 금액 및 로또 관련 상수 관리하는 객체 구현

---
+++ src/constants/constants.js
@@ -0,0 +1,12 @@
+const MONEY_CONSTANTS = Object.freeze({
+  minAmount: 1000,
+  countUnit: 1000,
+});
+
+const LOTTO_CONSTANTS = Object.freeze({
+  minRange: 1,
+  maxRange: 45,
+  digits: 6,
+});
+
+module.exports = { MONEY_CONSTANTS, LOTTO_CONSTANTS };
