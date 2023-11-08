b4fbd5c8004739d70c1bc4a74e6b1adf7ae9d244
getsetgo1 <dnwls1313@gmail.com>
2023-11-08 17:53:53 +0900

    숫자 중복, 숫자 크기 체크 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Console } from "@woowacourse/mission-utils";
+
 const duplicateError = "[ERROR] 로또 번호는 중복되면 안됩니다.";
 const numberError = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 class Lotto {
@@ -25,6 +27,9 @@ class Lotto {
       if (number > 45 || number < 1) throw Error(numberError);
     });
     return;
+  }
+  getArray() {
+    return this.#numbers;
   }
 
   // TODO: 추가 기능 구현
