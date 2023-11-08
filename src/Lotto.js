315be87885c1c67abdf3a81d9ad08f3d5cd2fed3
ChoByungHyun <cbj973@gmail.com>
2023-11-08 20:15:32 +0900

    feat: lotto파일 validate 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,15 +1,28 @@
+import { ERROR_MESSAGE } from "./constant/message.js";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
     this.#numbers = numbers;
+
+    this.#validate(numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw ERROR_MESSAGE.NUMBER_LENGTH;
     }
+
+    if (numbers.length !== new Set(numbers).size)
+      throw `${ERROR_MESSAGE.NUMBER_DUPLICATE}`;
+
+    numbers.forEach((element) => {
+      if (isNaN(element)) throw new Error(`${ERROR_MESSAGE.NUMBER_TYPE}`);
+      if (element > 45 || element < 0) throw `${ERROR_MESSAGE.NUMBER_RANGE}`;
+    });
+
+    return true;
   }
 
   // TODO: 추가 기능 구현
