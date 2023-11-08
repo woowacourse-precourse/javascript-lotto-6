ef3446916c34ee3623da05e71d98f1e1e733379c
Hchan2 <seongchan_@naver.com>
2023-11-08 21:35:04 +0900

    Feat: 입력 및 출력 메시지 상수 선언

--- src/constant/message.js
+++ src/constant/message.js
@@ -1,3 +1,9 @@
+export const INPUT_MESSAGES = Object.freeze({
+    buyLottoAmount: "구입금액을 입력해 주세요.",
+    winningLottoNumbers: "당첨 번호를 입력해주세요.",
+    bonusNumber: "보너스 번호를 입력해 주세요."
+})
+
 export const ERROR_MESSAGES = Object.freeze({
     prefix: "[ERROR]",
     invalidSingle: "1자리 숫자를 입력해주세요.",
@@ -9,3 +15,7 @@ export const ERROR_MESSAGES = Object.freeze({
     invalidThousandWonUnit: "1000원 단위로 입력해주세요. (예. 1000, 5000, 10000)",
     invalidMoreThousand: "1000원(1장) 이상 입력해주세요.",
 })
+
+export const BUY_MESSAGE = (buyAmount) => {
+    return `${buyAmount}개를 구매했습니다.`
+};
