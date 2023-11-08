61440fbb930f00f63728830474886c2be7f1477a
HyunseokLEE <unducklee@gmail.com>
2023-11-08 17:53:37 +0900

    feat: App.js의 play메서드로 프로그램시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import LottoController from "./Controller/LottoController";
+
 class App {
-  async play() {}
+  #lotto;
+  constructor() {
+    this.#lotto = new LottoController();
+  }
+
+  async play() {
+    await this.#lotto.LottoStart();
+  }
 }
 
 export default App;
---
+++ src/Controller/LottoController.js
@@ -0,0 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class LottoController {
+  constructor() {}
+
+  async LottoStart() {
+    Console.print("로또 미션 시작");
+  }
+}
+
+export default LottoController;
