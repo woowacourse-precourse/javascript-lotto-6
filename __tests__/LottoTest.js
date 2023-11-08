f38174748ff1904c1bc6643c4552938769a8639f
소인섭 <sis9745@gachon.ac.kr>
2023-11-08 20:39:32 +0900

    test: Lotto 클래스 인스턴스 생성 시, 배열 출력 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,5 @@
 import Lotto from '../src/Lotto.js';
+import { getLogSpy } from './mockMissionUtils.js';
 
 describe('로또 클래스 테스트', () => {
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
@@ -15,4 +16,15 @@ describe('로또 클래스 테스트', () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test('로또 인스턴스가 생성되면 로또 배열을 출력한다.', () => {
+    // given
+    const logSpy = getLogSpy();
+    const numbers = [1, 2, 3, 4, 5, 6];
+
+    // when
+    new Lotto(numbers);
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(numbers);
+  });
 });
