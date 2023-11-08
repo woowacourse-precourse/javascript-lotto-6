5fda22c88e4f29310b3bc95071ec7e5f0745c067
박지윤 <tkrhkfld123@naver.com>
2023-11-08 13:23:00 +0900

    feature: 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print('로또 게임을 시작합니다.');
+  }
 }
 
 export default App;
