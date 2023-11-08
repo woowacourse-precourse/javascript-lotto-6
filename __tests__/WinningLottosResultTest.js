ad7bf1a5fa40764ed2cc541ba9ce766d05204898
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-08 20:24:54 +0900

    test(결과:n개의 당첨된 로또들):수익률 계산, 당첨 내역 출력 기능 테스트 추가

--- __tests__/WinningLottosResultTest.js
+++ __tests__/WinningLottosResultTest.js
@@ -1,6 +1,19 @@
-import { BONUS_NUMBER_TYPE, WINNING_NUMBERS_COUNT_TYPE } from "../src/constants.js";
+import { Console } from "@woowacourse/mission-utils";
+import {  WINNING_NUMBERS_COUNT_TYPE } from "../src/constants.js";
 import { LottoResult } from "../src/lottoGame/result/Lotto.js";
 import { WinningLottosResult } from "../src/lottoGame/result/WinningLottos.js";
+import { getRoundedNumber } from "../src/utils/getRoundedNumber.js";
+
+let printTitleSpy; 
+let printWinningLottoWithBonusSpy;
+let printWinningLottoSpy;
+let printTotalProfitRateSpy;
+
+const getLogSpy = () => {
+    const logSpy = jest.spyOn(Console, "print");
+    logSpy.mockClear();
+    return logSpy;
+  };
 
 const winningNumbers = [1,33,35,31,2,3];
 const bonusNumber = 16
@@ -38,6 +51,11 @@ beforeEach(()=>{
     lottoResults = lottoNumbers.map((lottos)=> new LottoResult(lottos, winningNumbers, bonusNumber));
 
     winningLottosResult = new WinningLottosResult(lottoResults, userMoney);
+
+    printWinningLottoWithBonusSpy = jest.spyOn(winningLottosResult, "printWinningLottoWithBonus")
+    printWinningLottoSpy = jest.spyOn(winningLottosResult, "printWinningLotto")
+    printTotalProfitRateSpy = jest.spyOn(winningLottosResult, "printTotalProfitRate")
+    printTitleSpy =  jest.spyOn(winningLottosResult, "printTitle")
 })
 
 describe("n 개의 당첨된 로또들",()=>{
@@ -53,5 +71,35 @@ describe("n 개의 당첨된 로또들",()=>{
         expect(winningLottosResult.getWinningLottosResult().find(({condition})=> 
         condition.winningNumbersCount === WINNING_NUMBERS_COUNT_TYPE.second).count).toBe(fourthWinningLottoNumbers.length);
 
+    })
+
+    test("수익률 = 당첨 금액 / 구매한 금액 계산",()=>{
+        expect(winningLottosResult.getTotalProfitRate()).toBe(getRoundedNumber( (winningLottosResult.getTotalProfit() / userMoney) *100))
+    })
+
+    test("당첨 내역 출력",()=>{
+        const logSpy = getLogSpy();
+
+        winningLottosResult.print();
+
+        const logs = [
+            "당첨통계\n---\n",
+            "3개 일치 (5,000원) - 2개",
+            "4개 일치 (50,000원) - 0개",
+            "5개 일치 (1,500,000원) - 2개",
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+            "6개 일치 (2,000,000,000원) - 0개",
+            `총 수익률은 ${winningLottosResult.getTotalProfitRate()}%입니다.`
+        ]
+
+        expect(printTitleSpy).toHaveBeenCalledTimes(1)
+        expect(printWinningLottoWithBonusSpy).toHaveBeenCalledTimes(1)
+        expect(printWinningLottoSpy).toHaveBeenCalledTimes(4)
+        expect(printTotalProfitRateSpy).toHaveBeenCalledTimes(1)
+
+        logs.forEach((log) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+        });
+        
     })
 })
