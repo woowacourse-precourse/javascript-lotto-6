a915027e4f0df3ec519e4741ec4710659f20a21c
kwani6684 <jack0910@naver.com>
2023-11-08 21:32:01 +0900

    ♻️refactor: add Input class to manage inputs

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
 import Money from './Money.js';
 import RandomPick from './RandomPick.js';
 import Lotto from './Lotto.js';
-import WinInput from './WinInput.js';
+import WinInput from './Win.js';
 import Check from './Check.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 import ExceptionList from './ExceptionList.js';
-import BonusInput from './BonusInput.js';
+import BonusInput from './Bonus.js';
+import Input from './Input.js';
 
 class App {
-  getPayedMoney = async () => {
-    try {
-      let payedMoney = await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.\n');
-      let exception = new ExceptionList();
-      exception.noInputError(payedMoney);
-      exception.isNaNError(payedMoney);
-      exception.isZeroError(payedMoney);
-      exception.notThousandError(payedMoney);
-      return payedMoney;
-    } catch (error) {
-      MissionUtils.Console.print(error.message);
-      await this.getPayedMoney();
-    }
-  };
-  getWinNumber = async () => {
-    try {
-      const numberString = await MissionUtils.Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
-      let exception = new ExceptionList();
-      exception.noInputError(numberString);
-      let win = numberString.split(',');
-      exception.LengthError(win);
-      win.forEach((number) => {
-        exception.isNaNError(number);
-        exception.numberRangeError(number);
-      });
-      exception.sameNumberError(win);
-      return numberString;
-    } catch (error) {
-      MissionUtils.Console.print(error.message);
-      await getWinNumber();
-    }
-  };
-  getBonusNumber = async () => {
-    try {
-      const bonus = await MissionUtils.Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
-      let exception = new ExceptionList();
-      exception.noInputError(bonus);
-      exception.isNaNError(bonus);
-      exception.numberRangeError(bonus);
-      // exception.sameBonusError(win, bonus);
-      return bonus;
-    } catch (error) {
-      MissionUtils.Console.print(error.message);
-      await this.getBonusNumber();
-    }
-  };
   async play() {
-    let payed = await this.getPayedMoney();
+    const input = new Input();
+    let payed = await input.getPayedMoney();
     let newMoney = new Money(payed);
     newMoney.printCalLottoCount();
 
@@ -67,9 +23,8 @@ class App {
       let ticket = new Lotto(await numbers.getRandomNumber());
       lotto.push(ticket);
     }
-
-    const winNumber = new WinInput(await this.getWinNumber());
-    const bonusNumber = new BonusInput(await this.getBonusNumber());
+    const winNumber = new WinInput(await input.getWinNumber());
+    const bonusNumber = new BonusInput(await input.getBonusNumber());
     for (let i = 0; i < lotto.length; i++) {
       checkLotto[i] = {
         lotto: lotto[i],
