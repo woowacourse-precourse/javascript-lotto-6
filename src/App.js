3e1f157683f2e01da5a6810d3231d4eb412789f8
Nevacat <nevacattery217@naver.com>
2023-11-08 17:29:10 +0900

    feat: view와 controller를 연결중..(미완성)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import InputView from './view/InputView';
+import Lotto from './Lotto';
+import validate from './util/validation';
+
 class App {
-  async play() {}
+  #ticketCount;
+  #winningNumber;
+  #validTicketNumber;
+  #bonusNumber;
+  #lottoNumbers;
+
+  async play() {
+    this.#ticketCount = await InputView.moneyInput();
+    this.#winningNumber = await InputView.winningNumerInput();
+    this.#validTicketNumber = new Lotto(this.#winningNumber);
+    this.#bonusNumber = await InputView.bonusNumberInput();
+    this.#lottoNumbers = this.createLottoNumbers();
+  }
+  async createLottoNumbers() {
+    const lottoNumber = await Random.pickUniqueNumbersInRange(1, 45, 6);
+    return lottoNumber;
+  }
 }
 
 export default App;
