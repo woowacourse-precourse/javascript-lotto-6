fb6b888f31697ae1481db883a958a622c4a2874b
nkEllaaa <dhrtns515@gmail.com>
2023-11-08 14:26:17 +0900

    feat: 결과를 담아놓은 LottoResult 클래스 기능 구현

---
+++ src/LottoResult.js
@@ -0,0 +1,24 @@
+class LottoResult {
+  static CASE_3 = {
+    description: '3개 일치',
+    prize: 5000,
+  };
+  static CASE_4 = {
+    description: '4개 일치',
+    prize: 50000,
+  };
+  static CASE_5 = {
+    description: '5개 일치',
+    prize: 1500000,
+  };
+  static CASE_5_BONUS = {
+    description: '5개 일치, 보너스 볼 일치',
+    prize: 30000000,
+  };
+  static CASE_6 = {
+    description: '6개 일치',
+    prize: 2000000000,
+  };
+}
+
+export default LottoResult;
