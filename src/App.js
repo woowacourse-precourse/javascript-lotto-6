c2a957121fa5d7bfbf6eca8ce50bee4cf916089c
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-08 21:26:19 +0900

    feat:메인 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Lottos } from './Lottos.js';
+import { WinningLottosResult } from './lottoGame/result/WinningLottos.js';
+import { User } from './User.js';
+import { Input } from './interface/Input.js';
+import { LottoResult } from './lottoGame/result/Lotto.js';
+import Lotto from './Lotto.js';
+import { generateRandomNumbers } from './utils/generateRandomNumbers.js';
+
 class App {
-  async play() {}
+  async play() {
+    const user = new User();
+    await user.inputMoney();
+
+    const count = user.getMoney() / 1000;
+
+    const lottos = new Lottos(count);
+
+    lottos.print();
+
+    await user.inputWinningNumbers();
+    await user.inputBonusNumber();
+
+    const lottoResults = lottos
+      .get()
+      .map((lotto) => new LottoResult(lotto, user.getWinningNumbers(), user.getBonusNumber()));
+
+    const result = new WinningLottosResult(lottoResults, user.getMoney());
+    result.print();
+  }
 }
 
 export default App;
