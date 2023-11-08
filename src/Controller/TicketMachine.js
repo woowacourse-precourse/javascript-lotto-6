332b9d325019b4832881b29732e6ee8ad8809a15
sunub <bsc5672@gmail.com>
2023-11-08 18:25:47 +0900

    refactor : Ticket Machine을 대상으로 함수 올리기, 캡슐화를 사용
    전역에 있던 함수 선언을 클래스 내부로 옮겨 캡슐화 했고 예외를 App으로 던져 App 내에서 예외가 발생하는지 확인할 수 있게끔 리펙토링 했다.

--- src/Controller/TicketMachine.js
+++ src/Controller/TicketMachine.js
@@ -1,42 +1,41 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import LottoTicket from '../Model/LottoTicket.js';
 import Interface from '../View/Interface.js';
-import Printer from '../View/Printer.js';
 import ValidatorFactory from '../Validator/index.js';
 import Lotto from '../Lotto.js';
 
-function numOfPurchase(payment) {
-  return Number(payment) / 1000;
-}
-
-function lottoNumbers() {
-  return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
-}
-
-function createLottoTicket(amount) {
-  return Array.from({ length: amount }, () => []).map(() => {
-    const lotto = new Lotto(lottoNumbers());
-    return lotto;
-  });
-}
-
+// 티켓 머신 모듈
 class TicketMachine {
+  #validator;
   constructor() {
-    this.validator = ValidatorFactory.initialize('payment');
+    this.#validator = ValidatorFactory.initialize('payment');
   }
 
   async buyTicket() {
     const lottoTicket = LottoTicket.getInstance();
     try {
       const payment = await Interface.requestPayment();
-      const vertifiedPayment = this.validator.evaluate(payment);
-      const ticket = createLottoTicket(numOfPurchase(vertifiedPayment));
+      const vertifiedPayment = this.#validator.evaluate(payment);
+
+      const ticket = this.createLottoTicket(this.numOfPurchase(vertifiedPayment));
       lottoTicket.saveSpecificTypeData('ticket', ticket);
     } catch (error) {
-      Printer.print('[ERROR]');
-      Printer.print(error);
-      await this.buyTicket();
+      throw new Error(error);
     }
+  }
+  // 티켓 머신 클래스에 대한 유틸리티 함수들
+  createLottoTicket(amount) {
+    return Array.from({ length: amount }, () => []).map(() => {
+      const lotto = new Lotto(this.lottoNumbers());
+      return lotto;
+    });
+  }
+  lottoNumbers() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  }
+
+  numOfPurchase(payment) {
+    return Number(payment) / 1000;
   }
 }
 
