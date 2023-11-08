c8c0ce4dd7d72579cb4d809aacbdcc55c38375ab
stupidJoon <junsang.yu3@gmail.com>
2023-11-08 16:43:01 +0900

    refactor: change lotto array to class

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 /* eslint-disable */
 import { Console, Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto.js';
+
+const LOTTO_PRIZE_MONEY = {
+  '3': 5000, '4': 50000, '5': 1500000, '5-b': 30000000, '6': 2000000000
+};
 
 class App {
   async play() {
     Console.print('구입금액을 입력해 주세요.');
 
-    const money = await this.getLottoMoney();
+    const purchaseMoney = await this.getLottoMoney();
 
-    Console.print(`${money / 1000}개를 구매했습니다.`);
-    const lottos = await this.getLottos(money / 1000);
+    Console.print(`${purchaseMoney / 1000}개를 구매했습니다.`);
+    const lottos = await this.getLottos(purchaseMoney / 1000);
 
     Console.print('당첨 번호를 입력해 주세요.');
-    const winningLotto = (await Console.readLineAsync(''))
-      .trim()
-      .split(',')
-      .map(Number);
+    const winningLotto = new Lotto(
+      (await Console.readLineAsync(''))
+        .trim()
+        .split(',')
+        .map(Number)
+    );
 
     Console.print('보너스 번호를 입력해 주세요.');
     const bonus = Number(await Console.readLineAsync(''));
 
-    const { lottoMatches, lottoMoney } = this.getLottoPrize(lottos, winningLotto);
+    const prizes = this.getLottoPrizes(lottos, winningLotto, bonus);
+    const prizeMoney = Object.entries(prizes).reduce((acc, [key, value]) => acc + LOTTO_PRIZE_MONEY[key] * value, 0);
     Console.print('당첨 통계');
     Console.print('---');
-    Console.print(`3개 일치 (5,000원) - ${lottoMatches[3]}개`);
-    Console.print(`4개 일치 (50,000원) - ${lottoMatches[4]}개`);
-    Console.print(`5개 일치 (1,500,000원) - ${lottoMatches[5]}개`);
-    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoMatches['5-b']}개`);
-    Console.print(`6개 일치 (2,000,000,000원) - ${lottoMatches[6]}개`);
+    Console.print(`3개 일치 (5,000원) - ${prizes[3]}개`);
+    Console.print(`4개 일치 (50,000원) - ${prizes[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${prizes[5]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${prizes['5-b']}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${prizes[6]}개`);
 
-    Console.print(`총 수익률은 ${(lottoMoney / money * 100).toFixed(1)}%입니다.`);
+    Console.print(`총 수익률은 ${(prizeMoney / purchaseMoney * 100).toFixed(1)}%입니다.`);
   }
 
   async getLottoMoney() {
@@ -50,35 +58,23 @@ class App {
 
   async getLottos(count) {
     const lottos = Array.from({ length: count }, () => {
-      const lotto = Random.pickUniqueNumbersInRange(1, 45, 6);
-      lotto.sort((a, b) => a - b);
+      const lotto = new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6));
       Console.print(`[${lotto.join(', ')}]`);
       return lotto;
     });
     return lottos;
   }
 
-  getLottoPrize(lottos, winningLotto) {
+  getLottoPrizes(lottos, winningLotto, bonus) {
     const lottoMatches = Object.fromEntries(['3', '4', '5', '5-b', '6'].map(k => [k, 0]));
-    const lottoPrize = {
-      3: 5000, 4: 50000, 5: 1500000, '5-b': 30000000, 6: 2000000000
-    };
-    let lottoMoney = 0;
+
     lottos.forEach((lotto) => {
-      let lottoMatch = lotto.filter((lottoNumber) => winningLotto.includes(lottoNumber)).length;
-
+      let lottoMatch = lotto.compare(winningLotto, bonus);
       if (lottoMatch < 3) return;
-      if (lottoMatch === 5 && lotto.includes(bonus)) {
-        lottoMatch = '5-b';
-      }
-
       lottoMatches[lottoMatch] += 1;
-      lottoMoney += lottoPrize[lottoMatch];
     });
 
-    return {
-      lottoMatches, lottoMoney
-    };
+    return lottoMatches;
   }
 }
 
--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,36 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = numbers.sort((a, b) => a - b);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
-    if (new Set(numbers) !== numbers.length) {
+    if (new Set(numbers).size !== numbers.length) {
       throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
     }
+  }
+
+  join(seperator) {
+    return this.#numbers.join(seperator);
+  }
+
+  includes(number) {
+    return this.#numbers.includes(number);
+  }
+
+  compare(winningLotto, bonus) {
+    const lottoMatch = this.#numbers.filter(
+      (lottoNumber) => winningLotto.includes(lottoNumber),
+    ).length;
+
+    if (lottoMatch === 5 && this.#numbers.includes(bonus)) {
+      return '5-b';
+    }
+
+    return lottoMatch;
   }
 }
 
