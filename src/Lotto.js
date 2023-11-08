4496c725bd262c3604495d125dae136a0616aa76
hyunjun9788 <namhyunjun9788@gmail.com>
2023-11-08 23:04:29 +0900

    Refactor: 당첨번호 입력값에 대한 오류 상수화

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import { INPUT_ERROR_MESSAGE } from "./constant/lotto.js";
 class Lotto {
   #numbers;
 
@@ -9,7 +10,7 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(INPUT_ERROR_MESSAGE.INVALID_LOTTO_LENGTH_ERROR);
     }
     if (
       numbers.some(
@@ -17,12 +18,10 @@ class Lotto {
           isNaN(Number(number)) || number < 1 || number > 45 || number % 1 !== 0
       )
     ) {
-      throw new Error(
-        "[ERROR] 입력한 숫자는 1부터 45 사이의 자연수이어야 합니다."
-      );
+      throw new Error(INPUT_ERROR_MESSAGE.INVALID_INPUT_NUMBER_ERROR);
     }
     if (numbers.length !== new Set(numbers).size) {
-      throw new Error("[ERROR] 중복된 숫자는 입력하면 안 됩니다.");
+      throw new Error(INPUT_ERROR_MESSAGE.DUPLICATE_INPUT_NUMBER_ERROR);
     }
   }
 
