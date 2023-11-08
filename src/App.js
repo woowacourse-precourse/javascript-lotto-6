0d959b367edadcaea8d2bc88f7f621cd6472fb7f
Mijin Sim <202110106@bssm.hs.kr>
2023-11-08 17:45:17 +0900

    chore : 출력 메세지 위치 옮기기

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import Money from "./Money.js";
 import GAME_MESSAGES from "./constants/GameMessages.js";
 import Lotto from "./Lotto.js";
 import ERROR_MESSAGES from "./constants/ErrorMessage.js";
-import LOTTO_MESSAGES from "./constants/LottoMessages.js";
 
 class App {
   async play() {
     const money = new Money();
     const userMoney = await money.userMoney();
     const lottoCount = parseInt(userMoney / 1000, 10);
-    Console.print(`\n${lottoCount}${GAME_MESSAGES.COUNT_LOTTO}`);
     let lottos = this.generateLottoNumber(lottoCount);
     this.getWinningNumbers(lottos);
   }
@@ -39,10 +37,7 @@ class App {
     if (isNaN(bonusNumber)) throw new Error(ERROR_MESSAGES.IS_BONUS_NUMBER);
     if (winning.getNumber().includes(parseInt(bonusNumber, 10)))
       throw new Error(ERROR_MESSAGES.IS_BONUS_DUPLICATION);
-    if (
-      bonusNumber < LOTTO_MESSAGES.MIN_LOTTO_NUMBER ||
-      bonusNumber > LOTTO_MESSAGES.MAX_LOTTO_NUMBER
-    )
+    if (bonusNumber < 1 || bonusNumber > 45)
       throw new Error(ERROR_MESSAGES.IS_BONUS_RANGE);
     let winningNumber = winning.getNumber();
     this.winningLotto(winningNumber, bonusNumber, lottos);
@@ -50,13 +45,12 @@ class App {
 
   winningLotto(winningNumber, bonusNumber, lottos) {
     const winningResultArray = new Array(6).fill(0);
-    lottos.map((lotto) => {
+    for (let i = 0; i < lottos.length; i += 1) {
       winningResultArray[
-        this.isCorrectNumber(winningNumber, bonusNumber, lotto)
+        this.isCorrectNumber(winningNumber, bonusNumber, lottos[i])
       ] += 1;
-    });
+    }
 
-    Console.print(GAME_MESSAGES.WINNING_STATISTICS);
     this.printWinningResult(winningResultArray);
     this.printRateOfReturn(winningResultArray, lottos.length);
   }
@@ -74,40 +68,30 @@ class App {
   }
 
   printWinningResult(winningResultArray) {
+    Console.print(GAME_MESSAGES.WINNING_STATISTICS);
+    Console.print(`3개 일치 (5,000원) - ${winningResultArray[5]}개`);
+    Console.print(`4개 일치 (50,000원) - ${winningResultArray[4]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${winningResultArray[3]}개`);
     Console.print(
-      `${LOTTO_MESSAGES.COINCIDE_THREE_NUMBER}${winningResultArray[5]}개`,
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningResultArray[2]}개`,
     );
-    Console.print(
-      `${LOTTO_MESSAGES.COINCIDE_FOUR_NUMBER}${winningResultArray[4]}개`,
-    );
-    Console.print(
-      `${LOTTO_MESSAGES.COINCIDE_FIVE_NUMBER}${winningResultArray[3]}개`,
-    );
-    Console.print(
-      `${LOTTO_MESSAGES.COINCIDE_FIVE_BONUS_NUMBER}${winningResultArray[2]}개`,
-    );
-    Console.print(
-      `${LOTTO_MESSAGES.COINCIDE_SIX_NUMBER}${winningResultArray[1]}개`,
-    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${winningResultArray[1]}개`);
   }
 
   isCorrectNumber(winningNumber, bonusNumber, lotto) {
     let count = winningNumber.filter((x) => lotto.includes(x)).length;
 
-    if (count === LOTTO_MESSAGES.SIX_CORRECT) return 1;
-    if (count === LOTTO_MESSAGES.FIVE_CORRECT && lotto.includes(bonusNumber))
-      return 2;
-    if (count === LOTTO_MESSAGES.FIVE_CORRECT) return 3;
-    if (
-      count === LOTTO_MESSAGES.FOUR_CORRECT ||
-      count === LOTTO_MESSAGES.THREE_CORRECT
-    )
-      return 8 - count;
+    if (count === 6) return 1;
+    if (count === 5 && lotto.includes(bonusNumber)) return 2;
+    if (count === 5) return 3;
+    if (count === 4 || count === 3) return 8 - count;
 
     return 0;
   }
 
   generateLottoNumber(lottoCount) {
+    Console.print(`\n${lottoCount}${GAME_MESSAGES.COUNT_LOTTO}`);
+
     const lottos = [];
 
     for (let i = 0; i < lottoCount; i += 1) {
