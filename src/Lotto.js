a3d4a945e87f94a1e5235800cbe761d543819c72
ChoiYongWon <yongwon0824@naver.com>
2023-11-08 17:36:42 +0900

    feat: 일치하는 개수 반환 메소드

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,6 +13,10 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
+  // 일치하는 숫자 갯수 반환
+  getMatchCount(numbers) {
+    return numbers.filter(n => this.#numbers.includes(n)).length;
+  }
 }
 
 export default Lotto;
