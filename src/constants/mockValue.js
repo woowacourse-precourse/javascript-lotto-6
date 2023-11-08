9000d803e99b03c8229955828512bc8706dd5165
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-08 15:48:49 +0900

    docs: 테스트용 mock데이터 관련 상수파일 생성

---
+++ src/constants/mockValue.js
@@ -0,0 +1,11 @@
+const MOCK = Object.freeze({
+  PURCHASED_LOTTOS: [
+    [1, 3, 5, 7, 9, 10, 15],
+    [3, 5, 11, 16, 32, 38],
+    [9, 10, 15, 17, 19, 22],
+  ],
+  WINNING_NUMBERS: [1, 5, 9, 15, 17, 19],
+  BONUS_NUMBER: 20,
+});
+
+export default MOCK;
