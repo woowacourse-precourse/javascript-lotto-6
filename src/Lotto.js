b38eb1b0970140f55f4b836eb4010692a01bce17
djLee77 <dkdnl232@gmail.com>
2023-11-08 13:54:25 +0900

    refactor(Lotto): 로또 당첨번호 확인 메서드 클래스 분리
    -feat(LottoResultChecker)

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,5 @@
 import Constants from "./Constants/ValidateConstants.js";
+import LottoResultChecker from "./services/LottoResultChecker.js";
 
 class Lotto {
   #numbers;
@@ -29,25 +30,8 @@ class Lotto {
     }
   }
 
-  checkResult({ win, bonus }) {
-    const cntCorrect = this.#numbers.filter((number) =>
-      win.includes(number),
-    ).length;
-    const checkBonus = this.#numbers.includes(bonus);
-    this.result = this.#determineResult(cntCorrect, checkBonus);
-  }
-
-  #determineResult(cntCorrect, checkBonus) {
-    const prizeMap = {
-      6: "6개 일치 (2,000,000,000원)",
-      5: checkBonus
-        ? "5개 일치, 보너스 볼 일치 (30,000,000원)"
-        : "5개 일치 (1,500,000원)",
-      4: "4개 일치 (50,000원)",
-      3: "3개 일치 (5,000원)",
-    };
-
-    return prizeMap[cntCorrect] || "None";
+  checkResult(lottoData) {
+    this.result = LottoResultChecker.checkResult(this.#numbers, lottoData);
   }
 }
 
