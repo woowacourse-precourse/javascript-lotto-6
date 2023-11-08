429e7f50a40aab7839f161b62bbe4998434d8cff
songhyunseop <gustjq99455352@gmail.com>
2023-11-08 19:42:05 +0900

    Style: import 경로에 js확장자 추가

--- src/Domain/Computer.js
+++ src/Domain/Computer.js
@@ -1,5 +1,5 @@
 import { Random } from '@woowacourse/mission-utils';
-import { NUMBER } from '../Constant/stats';
+import { NUMBER } from '../Constant/stats.js';
 
 class Computer {
   getLotteryNumbers(numbers) {
