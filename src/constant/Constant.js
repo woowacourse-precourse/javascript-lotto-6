761fa2131a9f7822bf2f2a3cddcf1045a64efc02
yui880 <kimyuna521@gmail.com>
2023-12-02 13:55:37 +0900

    feat: 각 로또와 당첨 번호를 비교해서 순위를 계산하는 기능 구현

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -3,3 +3,52 @@ export const LOTTO_NUMBERS = Object.freeze({
   max: 45,
   count: 6,
 });
+
+export const RANK = {
+  first: {
+    match: 6,
+    bonus: false,
+    prize: 2_000_000_000,
+  },
+  second: {
+    match: 5,
+    bonus: true,
+    prize: 30_000_000,
+  },
+  third: {
+    match: 5,
+    bonus: false,
+    prize: 1_5000_000,
+  },
+  fourth: {
+    match: 4,
+    bonus: false,
+    prize: 50_000,
+  },
+  fifth: {
+    match: 3,
+    bonus: false,
+    prize: 5_000,
+  },
+  noWin: {
+    match: 0,
+    bonus: false,
+    prize: 0,
+  },
+};
+
+export const RANK_TITLE = {
+  first: 'first',
+  second: 'second',
+  third: 'third',
+  fourth: 'fourth',
+  fifth: 'fifth',
+  noWin: 'noWin',
+};
+
+const RANK_NAME = Object.keys(RANK);
+
+export const MATCH_TO_RANK = RANK_NAME.reduce((matchToRank, rank) => {
+  matchToRank[RANK[rank].match] = RANK_TITLE[rank];
+  return matchToRank;
+}, {});
--- src/model/LottoBundle.js
+++ src/model/LottoBundle.js
@@ -1,5 +1,6 @@
 import lottoNumberGenerator from '../utils/lottoNumberGenerator.js';
 import Lotto from '../Lotto.js';
+import { MATCH_TO_RANK, RANK, RANK_TITLE } from '../constant/Constant.js';
 
 class LottoBundle {
   #lottoList;
@@ -10,6 +11,25 @@ class LottoBundle {
 
   #buyLotto(quantity) {
     this.#lottoList = Array.from({ length: quantity }, () => new Lotto(lottoNumberGenerator()));
+  }
+
+  getRankList({ winningLotto, bonusNumber }) {
+    return Array.from(this.#lottoList, (lotto) =>
+      this.calculateRank({ lotto, winningLotto, bonusNumber }),
+    );
+  }
+
+  calculateRank({ lotto, winningLotto, bonusNumber }) {
+    const matchingCount = lotto.getMatchingCounts(winningLotto);
+    const hasBonusNumber = lotto.hasNumber(bonusNumber);
+
+    if (matchingCount === RANK.second.match && hasBonusNumber) {
+      return RANK_TITLE.second;
+    }
+    if (matchingCount < RANK.fifth.match) {
+      return RANK_TITLE.noWin;
+    }
+    return MATCH_TO_RANK[matchingCount];
   }
 }
 
