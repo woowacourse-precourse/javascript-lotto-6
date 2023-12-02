089765d30046a501bfff38fc6393c6d721d73503
yui880 <kimyuna521@gmail.com>
2023-12-02 17:27:07 +0900

    test: 로또 기능 테스트 작성

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -16,6 +16,66 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
+  describe('로또 기능 테스트', () => {
+    const opponent = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    test.each([
+      {
+        inputs: [6, 5, 4, 3, 2, 1],
+        expected: [1, 2, 3, 4, 5, 6],
+      },
+      {
+        inputs: [23, 17, 11, 2, 5, 34],
+        expected: [2, 5, 11, 17, 23, 34],
+      },
+    ])('로또를 생성하면 숫자가 오름차순으로 정렬되어 저장된다.', ({ inputs, expected }) => {
+      // when
+      const lotto = new Lotto(inputs);
+
+      // then
+      const result = lotto.getNumbers();
+      expect(result).toStrictEqual(expected);
+    });
+
+    test.each([
+      {
+        inputs: [23, 17, 11, 2, 5, 34],
+        expected: 2,
+      },
+      {
+        inputs: [2, 4, 6, 8, 10, 12],
+        expected: 3,
+      },
+    ])('로또끼리 비교해서 일치하는 개수가 몇개인지 반환한다.', ({ inputs, expected }) => {
+      // when
+      const lotto = new Lotto(inputs);
+
+      // then
+      const result = lotto.getMatchingCounts(opponent);
+      expect(result).toBe(expected);
+    });
+
+    test.each([
+      {
+        lottoNumbers: [23, 17, 11, 2, 5, 34],
+        bonusNumber: 2,
+        expected: true,
+      },
+      {
+        lottoNumbers: [2, 4, 6, 8, 10, 12],
+        bonusNumber: 14,
+        expected: false,
+      },
+    ])('로또가 특정 번호를 가지고 있는지 반환한다.', ({ lottoNumbers, bonusNumber, expected }) => {
+      // when
+      const lotto = new Lotto(lottoNumbers);
+
+      // then
+      const result = lotto.hasNumber(bonusNumber);
+      expect(result).toBe(expected);
+    });
+  });
+
   describe('로또 번호 검증 테스트', () => {
     test.each([[[1, 2, 3, 4, 5]], [1, 2, 3, 4, 5, 6, 7]])(
       '로또 번호가 6개가 아니면 예외가 발생한다.',
