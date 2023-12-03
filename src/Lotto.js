d2a580f550c0294393f52564f9951b87d358c42b
greeneeya <suzythetravelerr@gmail.com>
2023-12-03 20:14:08 +0900

    feat: 문자열일 경우, 예외 처리 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -13,10 +13,14 @@ class Lotto {
     if (new Set(numbers).size !== 6) {
       throw new Error('[ERROR] 각 로또 번호는 중복될 수 없습니다.');
     }
-    const isOutOfRange = numbers.some((number) => number > 45 || number < 1);
-    if (isOutOfRange) {
-      throw new Error('[ERROR] 각 수는 1-45 사이의 정수만 가능합니다.');
-    }
+    numbers.forEach((number) => {
+      if (!Number.isInteger(number)) {
+        throw new Error('[ERROR] 로또 번호는 1-45 사이의 정수만 가능합니다.');
+      }
+      if (number < 1 || number > 45) {
+        throw new Error('[ERROR] 로또 번호는 1-45 사이의 정수만 가능합니다.');
+      }
+    });
   }
 }
 
