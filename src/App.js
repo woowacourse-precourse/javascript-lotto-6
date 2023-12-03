b480340a82e0e375b9568531f19b19add16c2683
greeneeya <suzythetravelerr@gmail.com>
2023-12-03 21:39:49 +0900

    feat: 보너스 번호를 입력받는다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Lotto from './Lotto.js';
 import Vendor from './Vendor.js';
+import InputView from './views/InputView.js';
 
 class App {
   async play() {
     const vendor = new Vendor();
     await vendor.isssueTickets();
+    const winningNumbers = await InputView.getWinningNumbers();
+    const lotto = new Lotto(winningNumbers);
   }
 }
 const app = new App();
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -5,5 +5,24 @@ const InputView = {
     const paid = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     return Number(paid);
   },
+  async getWinningNumbers() {
+    const numbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    const cleanedNumbers = numbers.split(',').map((number) => Number(number));
+    return cleanedNumbers;
+  },
+  async getBonusNumber(winningNumbers) {
+    const number =
+      await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    if (winningNumbers.includes(number)) {
+      throw new Error('[ERROR] 로또 번호와 중복된 수는 입력 불가합니다.');
+    }
+    if (number < 1 || number > 45) {
+      throw new Error('[ERROR] 1-45 사이의 정수만 입력 가능합니다.');
+    }
+    if (!Number.isInteger(Number(number))) {
+      throw new Error('[ERROR] 1-45 사이의 정수만 입력 가능합니다.');
+    }
+    return number;
+  },
 };
 export default InputView;
