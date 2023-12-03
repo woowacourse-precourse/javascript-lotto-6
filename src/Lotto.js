ce5d33f1fee7dcd05ea94e1620ce705ec9989abc
greeneeya <suzythetravelerr@gmail.com>
2023-12-03 19:41:25 +0900

    feat: 로또 번호가 중복되는 경우, 예외 처리

--- src/Lotto.js
+++ src/Lotto.js
@@ -8,11 +8,12 @@ class Lotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 각 로또 번호는 중복될 수 없습니다.');
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
