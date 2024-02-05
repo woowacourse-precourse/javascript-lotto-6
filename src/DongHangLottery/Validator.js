fe7178ce84202d7654ce191c851c130162bf402a
pigpgw <ceh20002@naver.com>
2024-02-05 17:46:54 +0900

    feat:add check user input purchaseAmount validator

--- src/DongHangLottery/Validator.js
+++ src/DongHangLottery/Validator.js
@@ -1,8 +1,20 @@
 class Validator {
-  isValidPurchaseAmount(number) {
-    console.log('input seed', number, typeof number, number % 1000);
-    if (number % 1000 !== 0 && number !== 0) throw new Error('[ERROR]: 올바르지 않은 구매 요청 금액입니다.');
-    return true;
+  isValidPurchaseAmount(money) {
+    if (!this.isThousandWonUnit(money))
+      throw new Error('[ERROR]: 올바르지 않은 구매 요청 금액입니다.');
+    if (this.isNumber(money))
+      throw new Error('[ERROR]: 구매 금액은 숫자여야 합니다.');
+    return true
+  }
+
+  isThousandWonUnit(money) {
+    if (money % 1000 !== 0 && money !== 0) return true;
+    return false;
+  }
+
+  isNumber(money) {
+    if (!isNaN(money)) return true;
+    return false;
   }
 }
 
