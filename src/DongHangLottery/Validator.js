94b4b530ac4d127194577fc9404f44108b54911b
pigpgw <ceh20002@naver.com>
2024-02-05 17:51:50 +0900

    feat:modify check user input purchaseAmount validator

--- src/DongHangLottery/Validator.js
+++ src/DongHangLottery/Validator.js
@@ -2,13 +2,13 @@ class Validator {
   isValidPurchaseAmount(money) {
     if (!this.isThousandWonUnit(money))
       throw new Error('[ERROR]: 올바르지 않은 구매 요청 금액입니다.');
-    if (this.isNumber(money))
+    if (!this.isNumber(money))
       throw new Error('[ERROR]: 구매 금액은 숫자여야 합니다.');
     return true
   }
 
   isThousandWonUnit(money) {
-    if (money % 1000 !== 0 && money !== 0) return true;
+    if (money % 1000 === 0 && money !== 0) return true;
     return false;
   }
 
