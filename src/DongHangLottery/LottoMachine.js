1979e715e10152221354135a28864a585500e631
pigpgw <ceh20002@naver.com>
2024-02-06 12:00:18 +0900

    refactor: rename winningDetails to winningDetailsCount in LottoMachine
    Updated the field name in LottoMachine from winningDetails to winningDetailsCount for improved clarity and consistency in naming.

--- src/DongHangLottery/LottoMachine.js
+++ src/DongHangLottery/LottoMachine.js
@@ -4,7 +4,13 @@ import Validator from './Validator.js';
 class LottoMachine {
   #winningNumbers;
   #plusNumber;
-
+  #winningDetailsCount = {
+    FIRST: 0,
+    SECOND: 0,
+    THIRD: 0,
+    FOURTH: 0,
+    FIFTH: 0,
+  };
   constructor() {
     this.Validator = new Validator();
   }
@@ -24,33 +30,25 @@ class LottoMachine {
   }
 
   checkLottoResult(lottoList) {
-    const winningDetails = {
-      FIRST: 0,
-      SECOND: 0,
-      THIRD: 0,
-      FOURTH: 0,
-      FIFTH: 0,
-    };
-
     lottoList.forEach(lottoObj => {
       const lotto = lottoObj.getLottoNumber();
       const matchedNumbersCount = lotto.filter(item =>
         this.#winningNumbers.includes(item),
       ).length;
       const bonusNumberCount = Number(lotto.includes(this.#plusNumber));
-        console.log("check lotto result",lotto)
-      if (matchedNumbersCount === 6) winningDetails.FIRST += 1;
+      console.log('check lotto result', lotto);
+      if (matchedNumbersCount === 6) this.#winningDetailsCount.FIRST += 1;
       else if (matchedNumbersCount + bonusNumberCount === 6)
-        winningDetails.SECOND += 1;
+        this.#winningDetailsCount.SECOND += 1;
       else if (matchedNumbersCount + bonusNumberCount === 5)
-        winningDetails.THIRD += 1;
+        this.#winningDetailsCount.THIRD += 1;
       else if (matchedNumbersCount + bonusNumberCount === 4)
-        winningDetails.FOURTH += 1;
+        this.#winningDetailsCount.FOURTH += 1;
       else if (matchedNumbersCount + bonusNumberCount === 3)
-        winningDetails.FIFTH += 1;
+        this.#winningDetailsCount.FIFTH += 1;
       else return 0;
     });
-    return winningDetails;
+    return this.#winningDetailsCount;
   }
 }
 
